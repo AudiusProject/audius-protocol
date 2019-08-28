bfec95db401ee9c5033c40d55eda190cac0335b6
Sid Sethi <sidsethi93@gmail.com>
2019-08-28 11:16:14 -0700

    Fix non-square image uploads

--- creator-node/src/routes/files.js
+++ creator-node/src/routes/files.js
@@ -20,28 +20,42 @@ const resizeImage = require('../resizeImage')
 module.exports = function (app) {
   /** Store image in multiple-resolutions on disk + DB and make available via IPFS */
   app.post('/image_upload', authMiddleware, nodeSyncMiddleware, upload.single('file'), handleResponse(async (req, res) => {
-    const ipfs = req.app.get('ipfsAPI')
-
     if (!req.body.square) return errorResponseBadRequest('Must provide square boolean param in request body')
-    const squareFormat = (req.body.square === 'true')
-
-    // TODO logic for non-square images
-
-    // Resize image to desired resolutions
+    
+    const ipfs = req.app.get('ipfsAPI')
     const imageBufferOriginal = req.file.buffer
-    const imageBuffer1000x1000 = await resizeImage(req, imageBufferOriginal, 1000, squareFormat)
-    const imageBuffer480x480 = await resizeImage(req, imageBufferOriginal, 480, squareFormat)
-    const imageBuffer150x150 = await resizeImage(req, imageBufferOriginal, 150, squareFormat)
-    const imageBuffers = [imageBuffer1000x1000, imageBuffer480x480, imageBuffer150x150, imageBufferOriginal]
+    let imageBuffers
+    let ipfsAddResp
 
-    // Add directory with all images to IPFS
-    const ipfsAddResp = await ipfs.add([
-      { path: path.join(req.file.originalname, '1000x1000.jpg'), content: imageBuffer1000x1000 },
-      { path: path.join(req.file.originalname, '480x480.jpg'), content: imageBuffer480x480 },
-      { path: path.join(req.file.originalname, '150x150.jpg'), content: imageBuffer150x150 },
-      { path: path.join(req.file.originalname, 'original.jpg'), content: imageBufferOriginal }
-    ], { pin: true }
-    )
+    if (req.body.square === 'true') {
+      // Resize image to desired resolutions
+      const imageBuffer1000x1000 = await resizeImage(req, imageBufferOriginal, 1000, true)
+      const imageBuffer480x480 = await resizeImage(req, imageBufferOriginal, 480, true)
+      const imageBuffer150x150 = await resizeImage(req, imageBufferOriginal, 150, true)
+      imageBuffers = [imageBuffer1000x1000, imageBuffer480x480, imageBuffer150x150, imageBufferOriginal]
+
+      // Add directory with all images to IPFS
+      ipfsAddResp = await ipfs.add([
+        { path: path.join(req.file.originalname, '1000x1000.jpg'), content: imageBuffer1000x1000 },
+        { path: path.join(req.file.originalname, '480x480.jpg'), content: imageBuffer480x480 },
+        { path: path.join(req.file.originalname, '150x150.jpg'), content: imageBuffer150x150 },
+        { path: path.join(req.file.originalname, 'original.jpg'), content: imageBufferOriginal }
+      ], { pin: true }
+      )
+    } else if (req.body.square === 'false') {
+      // Resize image to desired resolutions
+      const imageBuffer2000x = await resizeImage(req, imageBufferOriginal, 2000, false)
+      const imageBuffer640x = await resizeImage(req, imageBufferOriginal, 640, false)
+      imageBuffers = [imageBuffer2000x, imageBuffer640x, imageBufferOriginal]
+
+      // Add directory with all images to IPFS
+      ipfsAddResp = await ipfs.add([
+        { path: path.join(req.file.originalname, '2000x.jpg'), content: imageBuffer2000x },
+        { path: path.join(req.file.originalname, '640x.jpg'), content: imageBuffer640x },
+        { path: path.join(req.file.originalname, 'original.jpg'), content: imageBufferOriginal }
+      ], { pin: true }
+      )
+    } else { return errorResponseBadRequest('Must provide square boolean param in request body') }
     req.logger.info('ipfs add resp', ipfsAddResp)
 
     // Get dir CID (last entry in returned array)
@@ -107,31 +121,77 @@ module.exports = function (app) {
     return successResponse(ipfsIDObj)
   }))
 
-  // Serve audius network IPFS segments
-  // This route does not handle responses by design, so we can pipe the gateway response
-  app.get('/ipfs/:multihash', async (req, res) => {
-    if (!(req.params && req.params.multihash)) {
-      return sendResponse(req, res, errorResponseBadRequest(`Invalid request, no multihash provided`))
+  /**
+   * Serve IPFS data hosted by creator node.
+   * @dev This route does not handle responses by design, so we can pipe the response to client.
+   */
+  app.get('/ipfs/:CID', async (req, res) => {
+    if (!(req.params && req.params.CID)) {
+      return sendResponse(req, res, errorResponseBadRequest(`Invalid request, no CID provided`))
     }
+    req.logger.info(req.params.CID)
 
-    // Do not act as a public gateway. Only serve IPFS files that are tracked by this creator node.
-    const reqMultihash = req.params.multihash
-    const queryResults = await models.File.findOne({ where: { multihash: reqMultihash } })
+    // Do not act as a public gateway. Only serve IPFS files that are hosted by this creator node.
+    const CID = req.params.CID
+    const queryResults = await models.File.findOne({ where: { multihash: CID } })
     if (!queryResults) {
-      return sendResponse(req, res, errorResponseNotFound(`No file found for provided segment multihash: ${reqMultihash}`))
+      return sendResponse(req, res, errorResponseNotFound(`No file found for provided CID: ${CID}`))
     }
 
     redisClient.incr('ipfsStandaloneReqs')
     const totalStandaloneIpfsReqs = parseInt(await redisClient.get('ipfsStandaloneReqs'))
-    logger.info(`IPFS Standalone Request - ${reqMultihash}`)
+    logger.info(`IPFS Standalone Request - ${CID}`)
     logger.info(`IPFS Stats - Standalone Requests: ${totalStandaloneIpfsReqs}`)
 
     const ipfs = req.app.get('ipfsAPI')
     try {
-      ipfs.catReadableStream(reqMultihash)
-        .on('data', streamData => { res.write(streamData) })
-        .on('end', () => { res.end() })
-        .on('error', e => { throw e })
+      await new Promise((resolve, reject) => {
+        ipfs.catReadableStream(CID)
+          .on('data', streamData => { res.write(streamData) })
+          .on('end', () => { res.end(); resolve() })
+          .on('error', e => { reject(e) })
+      })
+    } catch (e) {
+      return sendResponse(req, res, errorResponseServerError(e.message))
+    }
+  })
+
+  /**
+   * Serve images hosted by creator node on IPFS.
+   * @dev This route does not handle responses by design, so we can pipe the gateway response.
+   */
+  app.get('/ipfs/:dirCID/:filename', async (req, res) => {
+    if (!(req.params && req.params.dirCID && req.params.filename)) {
+      return sendResponse(req, res, errorResponseBadRequest(`Invalid request, no multihash provided`))
+    }
+    req.logger.info(req.params.dirCID, req.params.filename)
+
+    // Do not act as a public gateway. Only serve IPFS files that are tracked by this creator node.
+    const dirCID = req.params.dirCID
+    const filename = req.params.filename
+
+    const queryResults = await models.File.findOne({ where: { multihash: dirCID } })
+    if (!queryResults) {
+      return sendResponse(req, res, errorResponseNotFound(`No file found for provided dirCID: ${dirCID}`))
+    }
+    
+    // TODO - check if file with filename is also stored in CNODE
+
+    const ipfsPath = `${dirCID}/${filename}`
+
+    redisClient.incr('ipfsStandaloneReqs')
+    const totalStandaloneIpfsReqs = parseInt(await redisClient.get('ipfsStandaloneReqs'))
+    logger.info(`IPFS Standalone Request - ${ipfsPath}`)
+    logger.info(`IPFS Stats - Standalone Requests: ${totalStandaloneIpfsReqs}`)
+
+    const ipfs = req.app.get('ipfsAPI')
+    try {
+      await new Promise((resolve, reject) => {
+        ipfs.catReadableStream(ipfsPath)
+          .on('data', streamData => { res.write(streamData) })
+          .on('end', () => { res.end(); resolve() })
+          .on('error', e => { reject(e) })
+      })
     } catch (e) {
       return sendResponse(req, res, errorResponseServerError(e.message))
     }
