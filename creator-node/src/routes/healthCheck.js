282a5ba191fef15c4cedb36e735abfa0d845c5ed
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2019-08-30 11:03:15 -0700

    So many hats

--- creator-node/src/routes/healthCheck.js
+++ creator-node/src/routes/healthCheck.js
@@ -6,8 +6,18 @@ const versionInfo = require('../../.version.json')
 module.exports = function (app) {
   /** @dev TODO - Explore checking more than just DB (ex. IPFS) */
   app.get('/health_check', handleResponse(async (req, res) => {
-    const numConnections = (await sequelize.query("SELECT numbackends from pg_stat_database where datname = 'audius_creator_node'"))[0][0].numbackends
-    const connectionInfo = (await sequelize.query("select datname, usename, application_name, client_addr, wait_event_type, wait_event, state, query, backend_type from pg_stat_activity where datname = 'audius_creator_node'"))[0]
+    let numConnections = 0
+    let connectionInfo = null
+
+    let numConnectionsQuery = await sequelize.query("SELECT numbackends from pg_stat_database where datname = 'audius_creator_node'")
+    if (numConnectionsQuery && numConnectionsQuery[0] && numConnectionsQuery[0][0] && numConnectionsQuery[0][0].numbackends) {
+      numConnections = numConnectionsQuery[0][0].numbackends
+    }
+
+    let connectionInfoQuery = (await sequelize.query("select datname, usename, application_name, client_addr, wait_event_type, wait_event, state, query, backend_type from pg_stat_activity where datname = 'audius_creator_node'"))
+    if (connectionInfoQuery && connectionInfoQuery[0]) {
+      connectionInfo = connectionInfoQuery[0]
+    }
     req.logger.info('numConnections', numConnections)
     req.logger.info('connectionInfo', connectionInfo)
 
