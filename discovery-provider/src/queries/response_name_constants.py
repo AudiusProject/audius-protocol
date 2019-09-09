b8b23f840dbd7ae501605b575bba980637953cdf
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2019-09-09 13:16:32 -0700

    hard nit

--- discovery-provider/src/queries/response_name_constants.py
+++ discovery-provider/src/queries/response_name_constants.py
@@ -4,6 +4,7 @@ save_count = 'save_count' # integer - total save count of given track/playlist
 has_current_user_reposted = 'has_current_user_reposted' # boolean - has current user reposted given track/playlist
 has_current_user_saved = 'has_current_user_saved' # boolean - has current user saved given track/playlist
 followee_reposts = 'followee_reposts' # array - followees of current user that have reposted given track/playlist
+play_count = 'play_count' # integer - total number of plays for a given track
 
 # user metadata
 follower_count = 'follower_count' # integer - total follower count of given user
@@ -14,8 +15,6 @@ track_count = 'track_count' # integer - total count of tracks created by given u
 repost_count = 'repost_count' # integer - total count of reposts by given user
 does_current_user_follow = 'does_current_user_follow' # boolean - does current user follow given user
 current_user_followee_follow_count = 'current_user_followee_follow_count' # integer - number of followees of current user that also follow given user
-
-play_count = 'play_count' # integer - total number of plays for a given track
 
 # feed metadata
 activity_timestamp = 'activity_timestamp' # string - timestamp of relevant activity on underlying object,
