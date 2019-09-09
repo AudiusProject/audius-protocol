3536df3403f5996bbcd87a0c5012475acf412a43
Raymond Jacobson <ray@audius.co>
2019-09-08 18:45:15 -0700

    Add more info to error resp

--- creator-node/src/middlewares.js
+++ creator-node/src/middlewares.js
@@ -27,7 +27,11 @@ async function preMiddleware (req, res, next) {
 
   // error if self is not primary
   if (serviceEndpoint !== primaryEndpoint) {
-    return sendResponse(req, res, errorResponseUnauthorized('this node is not primary for user'))
+    return sendResponse(
+      req,
+      res,
+      errorResponseUnauthorized(`This node (${serviceEndpoint}) is not primary for user. Primary is: ${primaryEndpoint}`)
+    )
   }
 
   next()
