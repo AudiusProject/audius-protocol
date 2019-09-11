3aee416dbde95b27ac790c37270419c6f9325430
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2019-09-11 14:32:29 -0700

    We don't want to error if the values are default

--- creator-node/src/utils.js
+++ creator-node/src/utils.js
@@ -59,11 +59,10 @@ async function getIPFSPeerId (ipfs, config) {
   const ipfsClusterIP = config.get('ipfsClusterIP')
   const ipfsClusterPort = config.get('ipfsClusterPort')
 
-  if (!ipfsClusterIP || !ipfsClusterPort) throw new Error('ipfsCluster environment variable not set')
-
   let ipfsIDObj = await ipfs.id()
   
   // if it's a real host and port, generate a new ipfs id and override the addresses with this value
+  // if it's local or these variables aren't passed in, just return the regular ipfs.id() result
   if (ipfsClusterIP && ipfsClusterPort !== null && ipfsClusterIP !== '127.0.0.1' && ipfsClusterPort !== 0) {
     const addressStr = `/ip4/${ipfsClusterIP}/tcp/${ipfsClusterPort}/ipfs/${ipfsIDObj.id}`
     ipfsIDObj.addresses = [addressStr]
