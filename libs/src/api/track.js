1eb58d2bf1c3fe49db1c4e88c7761fdbc3f4630c
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2019-09-22 13:32:25 -0700

    Return new promise for promise.all in addtrackstochainandcnode

--- libs/src/api/track.js
+++ libs/src/api/track.js
@@ -190,20 +190,26 @@ class Tracks extends Base {
     }
 
     let trackIds = (await Promise.all(
-      trackMultihashAndUUIDList.map(async trackInfo => {
-        const metadataMultihash = trackInfo.metadataMultihash
-        const metadataFileUUID = trackInfo.metadataFileUUID
-        // Write metadata to chain
-        const multihashDecoded = Utils.decodeMultihash(metadataMultihash)
-        const { txReceipt, trackId } = await this.contracts.TrackFactoryClient.addTrack(
-          owner.user_id,
-          multihashDecoded.digest,
-          multihashDecoded.hashFn,
-          multihashDecoded.size
-        )
-        // Associate the track id with the file metadata and block number
-        await this.creatorNode.associateTrack(trackId, metadataFileUUID, txReceipt.blockNumber)
-        return trackId
+      trackMultihashAndUUIDList.map(trackInfo => {
+        return new Promise(async (resolve, reject) => {
+          try {
+            const metadataMultihash = trackInfo.metadataMultihash
+            const metadataFileUUID = trackInfo.metadataFileUUID
+            // Write metadata to chain
+            const multihashDecoded = Utils.decodeMultihash(metadataMultihash)
+            const { txReceipt, trackId } = await this.contracts.TrackFactoryClient.addTrack(
+              owner.user_id,
+              multihashDecoded.digest,
+              multihashDecoded.hashFn,
+              multihashDecoded.size
+            )
+            // Associate the track id with the file metadata and block number
+            await this.creatorNode.associateTrack(trackId, metadataFileUUID, txReceipt.blockNumber)
+            resolve(trackId)
+          } catch (e) {
+            reject(e)
+          }
+        })
       })
     ))
 
