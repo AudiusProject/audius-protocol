71f988242a8c78750bbb23ae987d8b17de974ce9
Sid Sethi <sidsethi93@gmail.com>
2019-09-24 16:57:39 -0700

    Lint

--- identity-service/sequelize/migrations/20190924230944-delete-auth-migrations.js
+++ identity-service/sequelize/migrations/20190924230944-delete-auth-migrations.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('AuthMigrations')
   },
   down: (queryInterface, Sequelize) => { }
-};
+}
