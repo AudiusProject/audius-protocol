bacf3eab81ad630701de9964338f183c3ce72fe2
Sid Sethi <sidsethi93@gmail.com>
2019-09-23 17:25:15 -0700

    Clean up comments

--- creator-node/src/fileManager.js
+++ creator-node/src/fileManager.js
@@ -67,17 +67,14 @@ async function saveFileToIPFSFromFS (req, srcPath, fileType, t) {
   // Adding a file through js-ipfs-api pins by default
   // Ensuring this multihash is available through garbage collection
   const multihash = (await ipfs.addFromFs(srcPath, { pin: false }))[0].hash
-  req.logger.info(`Time takin in saveFileToIpfsFromFS to add: ${Date.now() - codeBlockTimeStart}`)
+  req.logger.info(`Time taken in saveFileToIpfsFromFS to add: ${Date.now() - codeBlockTimeStart}`)
   codeBlockTimeStart = Date.now()
   const dstPath = path.join(req.app.get('storagePath'), multihash)
 
   // store segment file copy under multihash for easy future retrieval
   fs.copyFileSync(srcPath, dstPath)
 
-  req.logger.info(`Time takin in saveFileToIpfsFromFS to copyFileSync: ${Date.now() - codeBlockTimeStart}`)
-  codeBlockTimeStart = Date.now()
-
-  req.logger.info(`Time takin in saveFileToIpfsFromFS to pin: ${Date.now() - codeBlockTimeStart}`)
+  req.logger.info(`Time taken in saveFileToIpfsFromFS to copyFileSync: ${Date.now() - codeBlockTimeStart}`)
 
   // add reference to file to database
   const file = (await models.File.findOrCreate({ where:
