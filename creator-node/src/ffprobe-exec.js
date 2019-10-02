38d5a020ae4fc0fe6a3f69a8933af62bd1d01370
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2019-10-02 14:04:53 -0700

    Replace ffprobe-exec with master src code

--- creator-node/src/ffprobe-exec.js
+++ creator-node/src/ffprobe-exec.js
@@ -1,35 +1,40 @@
-const path = require('path')
-const fs = require('fs')
+const ffprobeStatic = require('ffprobe-static')
+var exec = require('child_process').exec
 
 const SEGMENT_REGEXP = /(segment[0-9]*.ts)/
 
 // Retrieve segment durations for an entire directory in a single child process
 // Standard output is parsed and returned as a dictionary of <segmentName, duration>
-async function getSegmentsDuration (req, segmentPath, filename, filedir) {
-  return new Promise((resolve, reject) => {
-    try {
-      let splitResults = filename.split('.')
-      let fileRandomName = splitResults[0]
-      let manifestPath = path.join(filedir, `${fileRandomName}.m3u8`)
-      let manifestContents = fs.readFileSync(manifestPath)
-      let splitManifest = manifestContents.toString().split('\n')
+async function getSegmentsDuration (req, segmentPath) {
+  let cmd = `find ${segmentPath}/ -maxdepth 1 -iname '*.ts' -print -exec ${ffprobeStatic.path} -v quiet -of csv=p=0 -show_entries format=duration {} \\;`
 
-      let segmentDurations = {}
-      for (let i = 0; i < splitManifest.length; i += 1) {
-        let matchedResults = splitManifest[i].match(SEGMENT_REGEXP)
-        if (matchedResults === null) {
-          continue
-        }
-        let segmentName = matchedResults[0]
-        let durationString = splitManifest[i - 1]
-        let durationSplit = durationString.split(':')
-        let duration = parseFloat(durationSplit[1])
-        segmentDurations[segmentName] = duration
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        req.logger.error(err)
+        reject(new Error(err))
+        return
       }
-      resolve(segmentDurations)
-    } catch (e) {
-      reject(new Error(`Failed - ${e}`))
-    }
+
+      // the entire stdout (buffered)
+      if (stdout) {
+        const segmentDurations = {}
+        const resultsArr = stdout.split('\n')
+        for (let i = 0; i < resultsArr.length - 2; i += 2) {
+          let matchedResults = resultsArr[i].match(SEGMENT_REGEXP)
+          if (matchedResults === null) {
+            req.logger.error(stdout)
+            req.logger.error(`NULL no matched results for ${resultsArr[i]}`)
+            continue
+          }
+          const segmentName = (matchedResults[0])
+          const duration = Number(resultsArr[i + 1])
+          segmentDurations[segmentName] = duration
+        }
+
+        resolve(segmentDurations)
+      } else { reject(new Error('Failed')) }
+    })
   })
 }
 
