4c517a8bfff56afd811b0d2e2b8f4b97908eb69c
Joseph Lee <joeylee0925@gmail.com>
2019-10-14 16:23:36 -0700

    Add pinned tracks to the social handles table in the identity service (#135)

---
+++ identity-service/src/routes/artistPick.js
@@ -0,0 +1,33 @@
+const { handleResponse, successResponse, errorResponseBadRequest } = require('../apiHelpers')
+const Sequelize = require('sequelize')
+const models = require('../models')
+
+module.exports = function (app) {
+  app.post('/artist_pick', handleResponse(async (req, res, next) => {
+    const { trackId, handle } = req.body
+
+    if (!handle) return errorResponseBadRequest('Please provide handle')
+
+    await models.SocialHandles.upsert({
+      handle,
+      pinnedTrackId: trackId || null
+    })
+    return successResponse()
+  }))
+
+  app.get('/artist_pick', handleResponse(async (req, res, next) => {
+    const { handles } = req.query
+    if (!handles) return errorResponseBadRequest('Please provide handles')
+
+    const userSocials = await models.SocialHandles.findAll({
+      where: {
+        handle: {
+          [Sequelize.Op.in]: handles
+        }
+      }
+    })
+    return successResponse({
+      artistPicks: userSocials.map(({ handle, pinnedTrackId }) => ({ handle, pinnedTrackId }))
+    })
+  }))
+}
