f965791b64db5a8ea518e386a1a1cda9e0e01f6a
Sid Sethi <sidsethi93@gmail.com>
2019-10-15 15:27:05 -0700

    Improve index names

--- creator-node/sequelize/migrations/20191014204226-add-indexes.js
+++ creator-node/sequelize/migrations/20191014204226-add-indexes.js
@@ -2,33 +2,33 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addIndex('Files', ['multihash'], {
-      name: 'Files_multihash_idx',
+      name: 'ss_Files_multihash_idx',
       using: 'btree',
       concurrently: true
     })
 
     await queryInterface.addIndex('AudiusUsers', ['cnodeUserUUID'], {
-      name: 'AudiusUsers_cnodeUserUUID_idx',
+      name: 'ss_AudiusUsers_cnodeUserUUID_idx',
       using: 'btree',
       concurrently: true
     })
 
     await queryInterface.addIndex('Tracks', ['cnodeUserUUID'], {
-      name: 'Tracks_cnodeUserUUID_idx',
+      name: 'ss_Tracks_cnodeUserUUID_idx',
       using: 'btree',
       concurrently: true
     })
 
     await queryInterface.addIndex('Files', ['cnodeUserUUID'], {
-      name: 'Files_cnodeUserUUID_idx',
+      name: 'ss_Files_cnodeUserUUID_idx',
       using: 'btree',
       concurrently: true
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex('Files', 'Files_multihash_idx')
-    await queryInterface.removeIndex('AudiusUsers', 'AudiusUsers_cnodeUserUUID_idx')
-    await queryInterface.removeIndex('Tracks', 'Tracks_cnodeUserUUID_idx')
-    await queryInterface.removeIndex('Files', 'Files_cnodeUserUUID_idx')
+    await queryInterface.removeIndex('Files', 'ss_Files_multihash_idx')
+    await queryInterface.removeIndex('AudiusUsers', 'ss_AudiusUsers_cnodeUserUUID_idx')
+    await queryInterface.removeIndex('Tracks', 'ss_Tracks_cnodeUserUUID_idx')
+    await queryInterface.removeIndex('Files', 'ss_Files_cnodeUserUUID_idx')
   }
 }
