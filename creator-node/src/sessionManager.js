26c5aacd6a186af92e72c776eb2e457d24c8e83e
Sid Sethi <sidsethi93@gmail.com>
2019-10-14 21:26:35 -0700

    Add redis cache to sessionMgr

--- creator-node/src/sessionManager.js
+++ creator-node/src/sessionManager.js
@@ -4,10 +4,12 @@ const { promisify } = require('util')
 const randomBytes = promisify(crypto.randomBytes)
 
 const models = require('./models')
+const redisClient = require('./redis')
 
 const sessionTokenHeaderKey = 'X-Session-ID'
 const sessionTokenLength = 40
 
+/** Manage sessions with redis and DB. */
 class SessionManager {
   static get sessionTokenHeader () {
     return sessionTokenHeaderKey
@@ -17,28 +19,37 @@ class SessionManager {
     const tokenBuffer = await randomBytes(sessionTokenLength)
     const token = base64url.encode(tokenBuffer)
 
-    await models.SessionToken.create({ cnodeUserUUID, token })
+    const session = await models.SessionToken.create({ cnodeUserUUID, token })
+    const resp = await redisClient.set(`SESSION.${token}`, session.cnodeUserUUID)
 
     return token
   }
 
+  /** Return cnodeUserUUID associated with token if exists, else null. */
   static async verifySession (sessionToken) {
-    const session = await _getTokenModel(sessionToken, false)
-    if (!session) {
-      return null
+    let session = await _getSessionFromRedis(sessionToken)
+    if (session) {
+      return session
     }
 
-    return session.cnodeUserUUID
+    session = await _getSessionFromDB(sessionToken, false)
+    return (session) ? session.cnodeUserUUID : null
   }
 
   static async deleteSession (sessionToken) {
-    const session = await _getTokenModel(sessionToken, true)
+    const session = await _getSessionFromDB(sessionToken, true)
     await session.destroy()
+    await redisClient.del(`SESSION.${sessionToken}`)
   }
 }
 
-async function _getTokenModel (sessionToken, throwOnError) {
+async function _getSessionFromRedis (token) {
+  return redisClient.get(`SESSION.${token}`)
+}
+
+async function _getSessionFromDB (sessionToken, throwOnError) {
   const session = await models.SessionToken.findOne({ where: { token: sessionToken } })
+
   if (throwOnError && !session) {
     throw new Error('Invalid session')
   }
--- libs/initScripts/local.js
+++ libs/initScripts/local.js
@@ -16,7 +16,7 @@ const discProvEndpoint1 = 'http://docker.for.mac.localhost:5000'
 const discProvEndpoint2 = 'http://docker.for.mac.localhost:5005'
 const creatorNodeEndpoint1 = 'http://docker.for.mac.localhost:4000'
 const creatorNodeEndpoint2 = 'http://docker.for.mac.localhost:4010'
-const versionStr = '0.1.0'
+const versionStr = '0.2.0'
 const amountOfAuds = 100000
 
 const throwArgError = () => {
