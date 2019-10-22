32a16e727d40c4fda8b40105a9448f816b20eed6
Raymond Jacobson <ray@audius.co>
2019-10-22 11:31:17 -0700

    Add more verbose comment

--- libs/src/sanityChecks/needsRecoveryEmail.js
+++ libs/src/sanityChecks/needsRecoveryEmail.js
@@ -1,4 +1,9 @@
-/** Sanity check for whether a user needs a recovery email to be sent. */
+/**
+ * Sanity check for whether a user needs a recovery email to be sent.
+ * Users with accounts created before email recovery existed need
+ * to be sent an email on their next log-in just in case they get logged
+ * out of their account.
+ */
 const needsRecoveryEmail = async (libs) => {
   const user = libs.userStateManager.getCurrentUser()
   if (!user || !user.wallet) return
