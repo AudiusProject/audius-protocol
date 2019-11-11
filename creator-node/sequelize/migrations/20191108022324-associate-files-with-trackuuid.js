1b96709fbe13f0a7831a29c8a815d36868c6f33a
Sid Sethi <sidsethi93@gmail.com>
2019-11-11 15:20:19 -0800

    Fix migration script

--- creator-node/sequelize/migrations/20191108022324-associate-files-with-trackuuid.js
+++ creator-node/sequelize/migrations/20191108022324-associate-files-with-trackuuid.js
@@ -4,30 +4,41 @@ module.exports = {
    * Fixes bug where track segment files were not associated with trackUUID upon track entry creation.
    */
   up: async (queryInterface, Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction()
+    // Set all file trackUUIDs to null before re-assigning.
+    await queryInterface.sequelize.query('update "Files" set "trackUUID" = null;')
 
     const tracks = (await queryInterface.sequelize.query(
-      'select "trackUUID", "metadataJSON"->\'track_segments\' as segments from "Tracks";',
-      { transaction }
+      'select "trackUUID", "metadataJSON"->\'track_segments\' as segments, "cnodeUserUUID" from "Tracks";'
     ))[0]
 
-    let updatedTracks = 0
-    let skippedTracks = 0
-
+    /** For every track, find all potential un-matched files, check for matches and associate. */
     for (const track of tracks) {
       const cids = track.segments.map(segment => segment.multihash)
-      if (cids.length === 0) { console.log(`track uuid ${track.trackUUID} has empty track segments`); continue }
+      if (cids.length === 0) {
+        console.log(`trackUUID ${track.trackUUID} has track segments length 0.`)
+        continue
+      }
 
-      // Find all segment files for CIDs in track
-      const segmentFiles = (await queryInterface.sequelize.query(
-        'select * from "Files" where "type" = \'track\' and "multihash" in (:cids);',
-        { replacements: { cids }, transaction }
+      const cnodeUserUUID = track.cnodeUserUUID
+
+      // Find all segment files for CIDs in track, that don't already have a trackUUID.
+      let segmentFiles = (await queryInterface.sequelize.query(
+        'select * from "Files" where "type" = \'track\' and "multihash" in (:cids) and "cnodeUserUUID" = (:cnodeUserUUID) and "trackUUID" is null;',
+        { replacements: { cids, cnodeUserUUID } }
+      ))[0]
+      if (segmentFiles.length === 0) {
+        console.log(`trackUUID ${track.trackUUID} has segmentFiles length 0.`)
+        continue
+      }
+
+      // Get all segment files for sourceFiles from above, to account for tracks that are superset of current track.
+      const sourceFiles = segmentFiles.map(segmentFile => segmentFile.sourceFile)
+      segmentFiles = (await queryInterface.sequelize.query(
+        'select * from "Files" where "type" = \'track\' and "sourceFile" in (:sourceFiles) and "cnodeUserUUID" = (:cnodeUserUUID) and "trackUUID" is null;',
+        { replacements: { sourceFiles, cnodeUserUUID } }
       ))[0]
 
-      // if segmentfiles already are associated with trackUUID, continue
-      if (segmentFiles[0].trackUUID === track.trackUUID) { skippedTracks++; continue }
-
-      // Group all segment files by sourceFile. Store as map { sourceFile: { cid: fileUUID } }
+      // Group all segment files by sourceFile. Store as map { sourceFile: { cid: fileUUID } }.
       const sourceFileMap = {}
       for (const segmentFile of segmentFiles) {
         if (segmentFile.sourceFile in sourceFileMap) {
@@ -40,22 +51,26 @@ module.exports = {
       // Check if segment files for sourceFile map 1-1 with track CIDs.
       let fileUUIDs = []
       for (const [, filesMap] of Object.entries(sourceFileMap)) {
-        if (Object.keys(filesMap).length !== cids.length) { fileUUIDs = []; continue }
+        fileUUIDs = []
+        if (Object.keys(filesMap).length !== cids.length) { continue }
         for (const cid of cids) {
-          if (cid in filesMap) { fileUUIDs.push(filesMap[cid]) }
+          if (cid in filesMap) {
+            fileUUIDs.push(filesMap[cid])
+          }
         }
         if (fileUUIDs.length === cids.length) { break }
+      }
+      if (fileUUIDs.length === 0) {
+        console.log(`trackUUID ${track.trackUUID} has 0 matching available fileUUIDs. segmentFiles length ${segmentFiles.length}.`)
+        continue
       }
 
       // associate
       await queryInterface.sequelize.query(
         'update "Files" set "trackUUID" = (:trackUUID) where "fileUUID" in (:fileUUIDs);',
-        { replacements: { trackUUID: track.trackUUID, fileUUIDs }, transaction }
+        { replacements: { trackUUID: track.trackUUID, fileUUIDs } }
       )
-      updatedTracks++
     }
-    await transaction.commit()
-    console.log(`updated tracks: ${updatedTracks}. skipped tracks ${skippedTracks}.`)
   },
   down: (queryInterface, Sequelize) => { }
 }
