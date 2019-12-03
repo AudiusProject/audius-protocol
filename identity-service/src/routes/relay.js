fb635696bdb3a06fe7bce1298c4caee66c859ada
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2019-12-03 15:40:03 -0800

    Lint

--- identity-service/src/routes/relay.js
+++ identity-service/src/routes/relay.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
   app.post('/relay', handleResponse(async (req, res, next) => {
     let body = req.body
     const redis = req.app.get('redis')
-    
+
     if (body && body.contractRegistryKey && body.contractAddress && body.senderAddress && body.encodedABI) {
       // send tx
       let receipt
@@ -36,8 +36,8 @@ module.exports = function (app) {
         } else {
           // if the tx fails, store it in redis with a 24 hour expiration
           const reqBodySHA = crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')
-          await redis.setex(`failedTx:${reqBodySHA}`, 60 /*seconds */ * 60 /*minutes*/ * 24 /*hours*/, JSON.stringify(req.body))
-          
+          await redis.setex(`failedTx:${reqBodySHA}`, 60 /* seconds */ * 60 /* minutes */ * 24 /* hours */, JSON.stringify(req.body))
+
           req.logger.error('Error in transaction:', e.message, reqBodySHA)
           return errorResponseServerError(`Something caused the transaction to fail for payload ${reqBodySHA}`)
         }
