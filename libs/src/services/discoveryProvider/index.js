5ce34f17ed5f9d5d8b4902943b8f1a53065a47e2
Raymond Jacobson <ray@audius.co>
2019-12-05 11:33:37 -0800

    Fix

--- libs/src/services/discoveryProvider/index.js
+++ libs/src/services/discoveryProvider/index.js
@@ -2,12 +2,9 @@ const axios = require('axios')
 const _ = require('lodash')
 
 const Utils = require('../../utils')
-const { DISCOVERY_PROVIDER_TIMESTAMP } = require('../ethContracts')
 
 const UNHEALTHY_BLOCK_DIFF = 50
-
 const DISCOVERY_PROVIDER_TIMESTAMP = '@audius/libs:discovery-provider-timestamp'
-const UNHEALTHY_BLOCK_DIFF = 50
 
 // TODO - webpack workaround. find a way to do this without checkout for .default property
 let urlJoin = require('proper-url-join')
