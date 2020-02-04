a946adf2e52b0b81c12c0c65aa7b31ee542a7af4
Sid Sethi <sidsethi93@gmail.com>
2020-02-04 15:03:04 -0800

    Fix discprov local test regression

--- discovery-provider/scripts/test.sh
+++ discovery-provider/scripts/test.sh
@@ -2,6 +2,10 @@
 
 # Audius Discovery Provider / Test
 # Runs configured unit test scripts
+# NOTE - the ipfs compose files have been moved from discprov to libs.
+#   Before running this test locally, bring up ipfs pod with libs/scripts/ipfs.sh
+
+
 source ./scripts/utilities.sh
 
 function cd_contracts_repo {
@@ -57,12 +61,12 @@ cd_discprov_repo
 # Stop dependencies, if present
 docker-compose -f docker-compose.base.yml stop
 docker-compose -f docker-compose.base.yml rm -f
-docker-compose -f docker-compose.ipfs.yml stop
-docker-compose -f docker-compose.ipfs.yml rm -f
+# docker-compose -f docker-compose.ipfs.yml stop
+# docker-compose -f docker-compose.ipfs.yml rm -f
 
 # Bring up local dependencies - postgres, redis, ipfs
 docker-compose -f docker-compose.base.yml up -d
-docker-compose -f docker-compose.ipfs.yml up -d
+# docker-compose -f docker-compose.ipfs.yml up -d
 sleep 5
 
 pytest -s -v --fulltrace 
