b7f5903e01a66df9acf39e8e059310b8222b8359
Raymond Jacobson <ray@audius.co>
2020-02-20 10:52:43 -0800

    Filter deletes from lexeme

--- discovery-provider/scripts/prod-web-server.sh
+++ discovery-provider/scripts/prod-web-server.sh
@@ -1,18 +1,16 @@
 # This is a development only script that allows you to start a web-server
 # without any celery dependencies. You can connect to a production RDS
 # instance without writing any local indexing data to the blockchain
-DB_URL=postgres+psycopg2://prod_disc_prov_audius:<password>@prod-disc-prov-db.cl38pgrjbaoh.us-west-1.rds.amazonaws.com:5432/audius_discovery
-
+# you may have to change the redis url in default_config.ini to redis://docker.for.mac.localhost:5379/0
+DB_URL=postgres+psycopg2://postgres:postgres@stage-discovery-1.c2xpmy12b95w.us-west-2.rds.amazonaws.com:5432/audius_discovery
 function stop {
   docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml down
 }
-
 # stop existing services
 stop
-
 # start new services
 docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml up --build -d redis-server
+# sh ./scripts/dev-server.sh
 docker build -t discprov_read_only .
-docker run -p 5000:5000 -e audius_db_url=$DB_URL discprov_read_only  /bin/bash -c ./scripts/dev-server.sh --build web-server
-
+docker run -p 5000:5000 -e audius_db_url=$DB_URL -e audius_db_url_read_replica=$DB_URL discprov_read_only  /bin/bash -c ./scripts/dev-server.sh --build web-server
 trap stop EXIT
