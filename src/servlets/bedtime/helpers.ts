d16c2b7409d1199d3f95f5f3cdeb5fa5a9ef4ad7
Raymond Jacobson <ray@audius.co>
2020-02-23 15:35:01 -0800

    Fix playlist images

--- src/servlets/bedtime/helpers.ts
+++ src/servlets/bedtime/helpers.ts
@@ -1,10 +1,17 @@
 import { formatGateway, getImageUrl } from '../utils/helpers'
 
-export const getCoverArtCID = (trackOrPlaylist: any) => (
-  trackOrPlaylist.cover_art_sizes
-    ? `${trackOrPlaylist.cover_art_sizes}/480x480.jpg`
-    : trackOrPlaylist.cover_art
-)
+export const getCoverArtCID = (trackOrPlaylist: any) => {
+  if (trackOrPlaylist.track_id) {
+    return trackOrPlaylist.cover_art_sizes
+      ? `${trackOrPlaylist.cover_art_sizes}/1000x1000.jpg`
+      : trackOrPlaylist.cover_art
+  } else if (trackOrPlaylist.playlist_id) {
+    return trackOrPlaylist.playlist_image_sizes_multihash
+      ? `${trackOrPlaylist.playlist_image_sizes_multihash}/1000x1000.jpg`
+      : trackOrPlaylist.playlist_image_multihash
+  }
+  return ''
+}
 
 export const getCoverArt = (trackOrPlaylist: any, user: any) => {
   const gateway = formatGateway(user.creator_node_endpoint, user.user_id)
