a6d62e172a40b395c539f0803618ec9de7a8bb12
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-02-26 14:25:23 -0800

    Discovery provider read-only server script updates (#297)

--- discovery-provider/scripts/prod-web-server.sh
+++ discovery-provider/scripts/prod-web-server.sh
@@ -1,7 +1,9 @@
 # This is a development only script that allows you to start a web-server
 # without any celery dependencies. You can connect to a production RDS
 # instance without writing any local indexing data to the blockchain
-DB_URL=postgres+psycopg2://prod_disc_prov_audius:<password>@prod-disc-prov-db.cl38pgrjbaoh.us-west-1.rds.amazonaws.com:5432/audius_discovery
+
+# you may have to change the redis url in default_config.ini to redis://docker.for.mac.localhost:5379/0
+DB_URL=postgres+psycopg2://<username>:<password>@<db_port>:5432/<db_name>
 
 function stop {
   docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml down
@@ -13,6 +15,6 @@ stop
 # start new services
 docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml up --build -d redis-server
 docker build -t discprov_read_only .
-docker run -p 5000:5000 -e audius_db_url=$DB_URL discprov_read_only  /bin/bash -c ./scripts/dev-server.sh --build web-server
+docker run -p 5000:5000 -e audius_db_url=$DB_URL -e audius_db_url_read_replica=$DB_URL discprov_read_only  /bin/bash -c ./scripts/dev-server.sh --build web-server
 
 trap stop EXIT
