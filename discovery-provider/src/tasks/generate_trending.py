f4b6177aec7d44a9b6c8f6e52daf3f4a93823d0e
vicky-g <60366641+vicky-g@users.noreply.github.com>
2020-03-11 11:34:40 -0700

    index cache fix (#315)
    * Fix cache-ing logic
    
    * converting datetime object to isoformat
    
    * removing white line
    
    Co-authored-by: Hareesh Nagaraj <hareesh.nagaraj@gmail.com>

--- discovery-provider/src/tasks/generate_trending.py
+++ discovery-provider/src/tasks/generate_trending.py
@@ -200,8 +200,12 @@ def generate_trending(db, time, genre, limit, offset):
 
             # Populate created at timestamps
             if track_entry[response_name_constants.track_id] in track_created_at_dict:
+                # datetime needs to be in isoformat for json.dumps() in `update_trending_cache()` to
+                # properly process the dp response and add to redis cache
+                # timespec = specifies additional components of the time to include
                 track_entry[response_name_constants.created_at] = \
-                        track_created_at_dict[track_entry[response_name_constants.track_id]]
+                        track_created_at_dict[track_entry[response_name_constants.track_id]] \
+                            .isoformat(timespec='seconds')
             else:
                 track_entry[response_name_constants.created_at] = None
 
