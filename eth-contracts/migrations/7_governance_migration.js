aa6ce2eabf57b592e264304dd8f87845d6f11e03
Sid Sethi <sidsethi93@gmail.com>
2020-03-25 12:51:24 -0400

    Address code review - minor changes

--- eth-contracts/migrations/7_governance_migration.js
+++ eth-contracts/migrations/7_governance_migration.js
@@ -3,18 +3,21 @@ const Governance = artifacts.require('Governance')
 
 const ownedUpgradeabilityProxyKey = web3.utils.utf8ToHex('OwnedUpgradeabilityProxy')
 
-const VotingPeriod = 10 // blocks
+// 48hr * 60 min/hr * 60 sec/min / ~15 sec/block = 11520 blocks
+const VotingPeriod = 11520
+// Required number of votes on proposal
+const VotingQuorum = 1
 
 module.exports = (deployer, network, accounts) => {
-  deployer.then(
-    async () => {
-      const registry = await Registry.deployed()
-      await deployer.deploy(
-        Governance,
-        registry.address,
-        ownedUpgradeabilityProxyKey,
-        VotingPeriod
-      )
-    }
-  )
+  deployer.then(async () => {
+    const registry = await Registry.deployed()
+
+    await deployer.deploy(
+      Governance,
+      registry.address,
+      ownedUpgradeabilityProxyKey,
+      VotingPeriod,
+      VotingQuorum
+    )
+  })
 }
