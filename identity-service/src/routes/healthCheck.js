115f2abc58e415c6387e7977be9c918a79879227
Vicky Guan <vicky@audius.co>
2020-04-01 16:18:48 -0700

    renaming consts with prefix RELAY_HEALTH, styling, and modifying return condition check

--- identity-service/src/routes/healthCheck.js
+++ identity-service/src/routes/healthCheck.js
@@ -10,10 +10,10 @@ const axios = require('axios')
 let notifDiscProv = config.get('notificationDiscoveryProvider')
 
 // Defaults used in relay health check endpoint
-const ONE_HOUR_AGO_BLOCKS = 720 // 5 blocks/sec = 720 blocks/hr
-const MAX_TRANSACTIONS = 100 // max transactions to look into
-const MAX_ERRORS = 5 // max acceptable errors for a 200 response
-const ACCOUNT = config.get('relayerPublicKey')
+const RELAY_HEALTH_ONE_HOUR_AGO_BLOCKS = 720 // 5 blocks/sec = 720 blocks/hr
+const RELAY_HEALTH_MAX_TRANSACTIONS = 100 // max transactions to look into
+const RELAY_HEALTH_MAX_ERRORS = 5 // max acceptable errors for a 200 response
+const RELAY_HEALTH_ACCOUNT = config.get('relayerPublicKey')
 
 module.exports = function (app) {
   /**
@@ -27,10 +27,11 @@ module.exports = function (app) {
 
     let endBlockNumber = req.query.endBlock || (await web3.eth.getBlockNumber())
     // In the case that no query params are defined and the endBlockNumber is less than 720, default startBlockNumber to 0
-    const defaultStartBlockNumber = endBlockNumber - ONE_HOUR_AGO_BLOCKS >= 0 ? endBlockNumber - ONE_HOUR_AGO_BLOCKS : 0
+    const defaultStartBlockNumber = endBlockNumber - RELAY_HEALTH_ONE_HOUR_AGO_BLOCKS >= 0
+      ? endBlockNumber - RELAY_HEALTH_ONE_HOUR_AGO_BLOCKS : 0
     let startBlockNumber = req.query.startBlock || defaultStartBlockNumber
-    let maxTransactions = req.query.maxTransactions || MAX_TRANSACTIONS
-    let maxErrors = req.query.maxErrors || MAX_ERRORS
+    let maxTransactions = req.query.maxTransactions || RELAY_HEALTH_MAX_TRANSACTIONS
+    let maxErrors = req.query.maxErrors || RELAY_HEALTH_MAX_ERRORS
 
     // Parse query strings into ints as all req.query values come in as strings
     startBlockNumber = parseInt(startBlockNumber)
@@ -39,7 +40,13 @@ module.exports = function (app) {
     maxErrors = parseInt(maxErrors)
 
     // If query params are invalid, throw server error
-    if (isNaN(startBlockNumber) || isNaN(endBlockNumber) || startBlockNumber < 0 || endBlockNumber < 0 || endBlockNumber < startBlockNumber) {
+    if (
+      isNaN(startBlockNumber) ||
+      isNaN(endBlockNumber) ||
+      startBlockNumber < 0 ||
+      endBlockNumber < 0 ||
+      endBlockNumber < startBlockNumber
+    ) {
       return errorResponseServerError(`Invalid start and/or end block. startBlock: ${startBlockNumber}, endBlock: ${endBlockNumber}`)
     }
 
@@ -47,7 +54,12 @@ module.exports = function (app) {
       return errorResponseServerError(`Block difference is over 1000. startBlock: ${startBlockNumber}, endBlock: ${endBlockNumber}`)
     }
 
-    if (isNaN(maxTransactions) || isNaN(maxErrors) || maxTransactions < 0 || maxErrors < 0) {
+    if (
+      isNaN(maxTransactions) ||
+      isNaN(maxErrors) ||
+      maxTransactions < 0 ||
+      maxErrors < 0
+    ) {
       return errorResponseServerError(`Invalid number of transactions and/or number of errors. maxTransactions: ${maxTransactions}, maxErrors: ${maxErrors}`)
     }
 
@@ -55,7 +67,7 @@ module.exports = function (app) {
     let txCounter = 0
 
     req.logger.info(
-      `Searching for transactions to/from account ${ACCOUNT} within blocks ${startBlockNumber} and ${endBlockNumber}`
+      `Searching for transactions to/from account ${RELAY_HEALTH_ACCOUNT} within blocks ${startBlockNumber} and ${endBlockNumber}`
     )
 
     // Iterate through the range of blocks, looking into the max number of transactions that are from audius
@@ -66,7 +78,7 @@ module.exports = function (app) {
       if (block && block.transactions.length) {
         for (const tx of block.transactions) {
           // If transaction is from audius account, determine success or fail status
-          if (ACCOUNT === tx.from) {
+          if (RELAY_HEALTH_ACCOUNT === tx.from) {
             const txHash = tx.hash
             const resp = await web3.eth.getTransactionReceipt(txHash)
             txCounter++
@@ -85,7 +97,7 @@ module.exports = function (app) {
       timeElapsed: Date.now() - start
     }
 
-    if (failureTxHashes > maxErrors) return errorResponseServerError(serverResponse)
+    if (failureTxHashes.length > maxErrors) return errorResponseServerError(serverResponse)
     else return successResponse(serverResponse)
   }))
 
