a9e4c3338464626f775e563b7ddbc2e0aadca9f6
Vicky Guan <vicky@audius.co>
2020-04-01 19:57:54 -0700

    adding set -e flag

--- libs/src/api/file.js
+++ libs/src/api/file.js
@@ -11,7 +11,7 @@ const publicGateways = [
   'https://cloudflare-ipfs.com/ipfs/'
 ]
 
-// Download url
+// Download url with url and filename params
 const downloadURL = (url, filename) => {
   if (document) {
     const link = document.createElement('a')
