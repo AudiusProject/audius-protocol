50401a88d6f0e579c7d4fb99c080e65c7dd1523e
Vicky Guan <vicky@audius.co>
2020-04-03 12:46:23 -0700

    adding command -v psql command to check if psql existts

--- creator-node/scripts/run-tests.sh
+++ creator-node/scripts/run-tests.sh
@@ -21,9 +21,9 @@ export logLevel='info'
 # taken from https://stackoverflow.com/a/36591842
 
 # if not circleci build, run docker exec command. else, run psql command
-if [ -z "${isCIBuild}"]; then
+if [ -z "${isCIBuild}" ]; then
   docker exec -i audius-creator-node_db_1 /bin/sh -c "psql -U postgres -tc \"SELECT 1 FROM pg_database WHERE datname = 'audius_creator_node_test'\" | grep -q 1 || psql -U postgres -c \"CREATE DATABASE audius_creator_node_test\""
-else
+elif [ -x "$(command -v psql)" ]; then
   psql -U postgres -h localhost -p $PG_PORT -tc "SELECT 1 FROM pg_database WHERE datname = 'audius_creator_node_test'" | grep -q 1 || psql -U postgres -h localhost -p $PG_PORT -c "CREATE DATABASE audius_creator_node_test"
 fi
 
