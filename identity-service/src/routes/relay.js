275617449fdea85d0b95452a7d0d043d0ef416cf
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-04-08 08:55:28 -0700

    First pass at better relay observability

--- identity-service/src/routes/relay.js
+++ identity-service/src/routes/relay.js
@@ -12,30 +12,33 @@ module.exports = function (app) {
     if (body && body.contractRegistryKey && body.contractAddress && body.senderAddress && body.encodedABI) {
       // send tx
       let receipt
+      const reqBodySHA = crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')
       try {
+        var txProps = {
+          contractRegistryKey: body.contractRegistryKey,
+          contractAddress: body.contractAddress,
+          encodedABI: body.encodedABI,
+          senderAddress: body.senderAddress,
+          gasLimit: body.gasLimit || null
+        }
         receipt = await txRelay.sendTransaction(
-          body.contractRegistryKey,
-          body.contractAddress,
-          body.encodedABI,
-          body.senderAddress,
-          false,
-          body.gasLimit || null)
+          req,
+          false, // resetNonce
+          txProps,
+          reqBodySHA)
       } catch (e) {
         if (e.message.includes('nonce')) {
           req.logger.warn('Nonce got out of sync, resetting. Original error message: ', e.message)
           // this is a retry in case we get an error about the nonce being out of sync
           // the last parameter is an optional bool that forces a nonce reset
           receipt = await txRelay.sendTransaction(
-            body.contractRegistryKey,
-            body.contractAddress,
-            body.encodedABI,
-            body.senderAddress,
-            true,
-            body.gasLimit || null)
+            req,
+            true, // resetNonce
+            txProps,
+            reqBodySHA)
           // no need to return success response here, regular code execution will continue after catch()
         } else {
           // if the tx fails, store it in redis with a 24 hour expiration
-          const reqBodySHA = crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')
           await redis.setex(`failedTx:${reqBodySHA}`, 60 /* seconds */ * 60 /* minutes */ * 24 /* hours */, JSON.stringify(req.body))
 
           req.logger.error('Error in transaction:', e.message, reqBodySHA)
