e8693a4a558511dd7ae25a53f4529a974244ccaa
vicky-g <60366641+vicky-g@users.noreply.github.com>
2020-04-15 11:17:31 -0700

    celery logs print with desired level and formatting (#369)
    * passing no params to getLogger() retrieves the root logger, not an empty string + added init logs for each service in dp
    
    * fixing lint errors
    
    * configuring personalized logger from setup and not using default celery logger
    
    * commenting out logging config for celery and adding a default value for configure_logging()

--- discovery-provider/src/worker.py
+++ discovery-provider/src/worker.py
@@ -1,3 +1,12 @@
+import logging
+import sys
+
 from src import create_celery
 
+logger = logging.getLogger(__name__)
+
 celery = create_celery()
+
+# Celery instances are initialized via command line
+# Grab the specific instance (beat or worker) and log it
+logger.info(f'Celery {sys.argv[len(sys.argv)-1]} initialized!')
