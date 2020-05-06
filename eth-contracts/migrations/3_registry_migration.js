79b9561a0f6a381e135ebf960a75ba3bd013aef7
Sid Sethi <sidsethi93@gmail.com>
2020-05-06 14:42:07 -0400

    Convert migrations to proxy pattern

--- eth-contracts/migrations/3_registry_migration.js
+++ eth-contracts/migrations/3_registry_migration.js
@@ -1,8 +1,27 @@
+const contractConfig = require('../contract-config.js')
+const encodeCall = require('../utils/encodeCall')
+
 const Registry = artifacts.require('Registry')
+const AdminUpgradeabilityProxy = artifacts.require('AdminUpgradeabilityProxy')
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
-    const registry = await deployer.deploy(Registry)
-    await registry.initialize()
+    const config = contractConfig[network]
+    const proxyAdminAddress = config.proxyAdminAddress || accounts[10]
+    const proxyDeployerAddress = config.proxyDeployerAddress || accounts[11]
+
+    // Deploy Registry logic and proxy contracts
+    const registry0 = await deployer.deploy(Registry, { from: proxyDeployerAddress })
+    const initializeCallData = encodeCall('initialize', [], [])
+    const registryProxy = await deployer.deploy(
+      AdminUpgradeabilityProxy,
+      registry0.address,
+      proxyAdminAddress,
+      initializeCallData,
+      { from: proxyDeployerAddress }
+    )
+
+    // Export to env for reference in future migrations
+    process.env.registryAddress = registryProxy.address
   })
 }
--- eth-contracts/migrations/6_claims_manager_migration.js
+++ eth-contracts/migrations/6_claims_manager_migration.js
@@ -15,24 +15,23 @@ const governanceKey = web3.utils.utf8ToHex('Governance')
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
     const config = contractConfig[network]
-    const token = await AudiusToken.deployed()
-    const registry = await Registry.deployed()
-    const registryAddress = registry.address
-    const tokenDeployerAcct = accounts[0]
     const controllerAddress = config.controllerAddress || accounts[0]
+    const proxyAdminAddress = config.proxyAdminAddress || accounts[10]
+    const proxyDeployerAddress = config.proxyDeployerAddress || accounts[11]
 
-    // TODO move to contractConfig
-    const [proxyAdminAddress, proxyDeployerAddress] = [accounts[10], accounts[11]]
+    const tokenAddress = process.env.tokenAddress
+    const registryAddress = process.env.registryAddress
 
+    const token = await AudiusToken.at(tokenAddress)
+    const registry = await Registry.at(registryAddress)
+
+    // Deploy ClaimsManager logic and proxy contracts + register proxy
     const claimsManager0 = await deployer.deploy(ClaimsManager, { from: proxyDeployerAddress })
-
     const initializeCallData = encodeCall(
       'initialize',
       ['address', 'address', 'address', 'bytes32', 'bytes32', 'bytes32'],
-      [token.address, registryAddress, controllerAddress, stakingProxyKey, serviceProviderFactoryKey, delegateManagerKey]
+      [tokenAddress, registryAddress, controllerAddress, stakingProxyKey, serviceProviderFactoryKey, delegateManagerKey]
     )
-
-    // Deploy new ClaimsManager
     const claimsManagerProxy = await deployer.deploy(
       AudiusAdminUpgradeabilityProxy,
       claimsManager0.address,
@@ -42,13 +41,11 @@ module.exports = (deployer, network, accounts) => {
       governanceKey,
       { from: proxyDeployerAddress }
     )
-
-    // Register ClaimsManager
-    await registry.addContract(claimsManagerProxyKey, claimsManagerProxy.address, { from: accounts[0] })
+    await registry.addContract(claimsManagerProxyKey, claimsManagerProxy.address, { from: proxyDeployerAddress })
 
     // Register ClaimsManager as minter
-    // Note that by default this is called from accounts[0] in ganache
+    // Note that by default this is called from proxyDeployerAddress in ganache
     // During an actual migration, this step should be run independently
-    await token.addMinter(claimsManagerProxy.address, { from: tokenDeployerAcct })
+    await token.addMinter(claimsManagerProxy.address, { from: proxyDeployerAddress })
   })
 }
