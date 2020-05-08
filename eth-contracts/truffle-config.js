6c3c9a8932afd83ebe211865d5312aa03760695b
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-05-08 12:52:42 -0700

    env var to optionally turn optimizer off (#440)
    * The 'cse' flag in the optimizer causes code coverage to break. So added an env var which optionally disables the compiler during coverage tests. Interestingly the README for the code coverage says it doesn't use the optimized builds(https://github.com/sc-forks/solidity-coverage/blob/master/README.md#usage-notes), but for some reason it's still fails when we have our optimizer enabled.

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -19,6 +19,8 @@ const getEnv = env => {
   }
   return value
 }
+let ENABLE_OPTIMIZER = true
+if (getEnv('ENABLE_OPTIMIZER') === 'false') ENABLE_OPTIMIZER = false
 
 // Values must be set in calling environment
 // Consult @hareeshnagaraj for details
@@ -26,6 +28,29 @@ const privateKey = getEnv('ETH_WALLET_PRIVATE_KEY')
 const liveNetwork = getEnv('ETH_LIVE_NETWORK')
 const liveNetworkId = getEnv('ETH_LIVE_NETWORK_ID')
 
+const solc = {
+  // 0.5.16 is latest 0.5.x version
+  // cannot use 0.6.x due to openzeppelin dependency, which are only 0.5.x compatible
+  version: '0.5.16',
+  parser: 'solcjs', // Leverages solc-js purely for speedy parsing
+  settings: {
+    evmVersion: 'istanbul', // istanbul is latest stable, and default setting
+  }
+}
+
+if (ENABLE_OPTIMIZER) {
+  solc.settings.optimizer = {
+    enabled: true,
+    runs: 200, // 200 is default value
+    details: {
+      orderLiterals: true,
+      deduplicate: true,
+      cse: true,
+      constantOptimizer: true,
+      yul: false // disabled as Yul optimizer is still experimental in 0.5.x
+    }
+  }
+}
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -53,26 +78,7 @@ module.exports = {
   },
   // https://solidity.readthedocs.io/en/develop/using-the-compiler.html#input-description
   compilers: {
-    solc: {
-      // 0.5.16 is latest 0.5.x version
-      // cannot use 0.6.x due to openzeppelin dependency, which are only 0.5.x compatible
-      version: '0.5.16',
-      parser: 'solcjs', // Leverages solc-js purely for speedy parsing
-      settings: {
-        evmVersion: 'istanbul', // istanbul is latest stable, and default setting
-        optimizer: {
-          enabled: true,
-          runs: 200, // 200 is default value
-          details: {
-            orderLiterals: true,
-            deduplicate: true,
-            cse: true,
-            constantOptimizer: true,
-            yul: false // disabled as Yul optimizer is still experimental in 0.5.x
-          }
-        }
-      }
-    }
+    solc: solc
   },
   mocha: {
     enableTimeouts: false
