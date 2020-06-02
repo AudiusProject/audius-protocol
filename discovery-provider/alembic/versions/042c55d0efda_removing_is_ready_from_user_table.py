26b797539609a901ff9d81fc85a2c35c46bf8ccf
Vicky Guan <vicky@audius.co>
2020-06-02 15:25:21 -0700

    updating down revision hash to the current head

--- discovery-provider/alembic/versions/042c55d0efda_removing_is_ready_from_user_table.py
+++ discovery-provider/alembic/versions/042c55d0efda_removing_is_ready_from_user_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '042c55d0efda'
-down_revision = '7b3f27e78b84'
+down_revision = 'e298de2becf2'
 branch_labels = None
 depends_on = None
 
