4d7319cad5093fdc593f9a8b26065b0cab58e812
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-06-04 15:43:59 -0700

    Update replica set working now

--- contracts/contracts/UserReplicaSetManager.sol
+++ contracts/contracts/UserReplicaSetManager.sol
@@ -39,4 +39,46 @@ contract UserReplicaSetManager is RegistryContract, SigningLogic {
         registry = RegistryInterface(_registryAddress);
         userFactoryRegistryKey = _userFactoryRegistryKey;
     }
+
+    // Called from dataOwnerWallet value as msg.sender
+    function registerCreatorNode(uint _spID) external {
+        spIdToCreatorNode[_spID] = msg.sender;
+    }
+
+    // Function used to 
+    function updateReplicaSet(
+        address _userWallet,
+        uint _primary,
+        uint[] calldata _secondaries,
+        uint _oldPrimary,
+        uint[] calldata _oldSecondaries) external
+      {
+          // Caller's notion of existing primary must match regisered value on chain
+          require(artistReplicaSets[_userWallet].primary == _oldPrimary,  "Invalid prior configuration"); 
+
+          // A valid updater can be one of the dataOwnerWallet or an existing creator node
+          bool validUpdater = false;
+          if (msg.sender == _userWallet || msg.sender == spIdToCreatorNode[_oldPrimary]) {
+              validUpdater = true;
+          }
+          for (uint i = 0; i < _oldSecondaries.length; i++) {
+              // Caller's notion of secondary values must match registered value on chain
+              require(
+                  artistReplicaSets[_userWallet].secondaries[i] == _oldSecondaries[i],
+                  "Invalid prior secondary configuration"
+              );
+
+              // A valid updater has been found
+              if (msg.sender == spIdToCreatorNode[_oldSecondaries[i]]) {
+                validUpdater = true;
+              }
+          }
+          require(validUpdater, "Invalid update operation");
+
+          // Perform replica set update
+          artistReplicaSets[_userWallet] = ReplicaSet({
+              primary: _primary,
+              secondaries: _secondaries
+          });
+      }
 }
