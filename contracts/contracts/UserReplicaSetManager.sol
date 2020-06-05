ec3f8b7655f6316a44a9508ee1f89236ebd09b8a
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-06-05 12:59:29 -0700

    Contract update

--- contracts/contracts/UserReplicaSetManager.sol
+++ contracts/contracts/UserReplicaSetManager.sol
@@ -11,6 +11,7 @@ contract UserReplicaSetManager is RegistryContract, SigningLogic {
 
     RegistryInterface private registry = RegistryInterface(0);
     bytes32 private userFactoryRegistryKey;
+    address deployer;
 
     // spID to ServiceProviderdelegate owner wallet
     mapping (uint => address) spIdToCreatorNode;
@@ -24,6 +25,7 @@ contract UserReplicaSetManager is RegistryContract, SigningLogic {
     // Current artist wallet to replica set
     mapping (address => ReplicaSet) artistReplicaSets;
 
+    // TODO: Capture deployer here
     constructor(
         address _registryAddress,
         bytes32 _userFactoryRegistryKey,
@@ -38,6 +40,7 @@ contract UserReplicaSetManager is RegistryContract, SigningLogic {
 
         registry = RegistryInterface(_registryAddress);
         userFactoryRegistryKey = _userFactoryRegistryKey;
+        deployer = msg.sender;
     }
 
     // Called from dataOwnerWallet value as msg.sender
@@ -56,13 +59,14 @@ contract UserReplicaSetManager is RegistryContract, SigningLogic {
           // Caller's notion of existing primary must match regisered value on chain
           require(artistReplicaSets[_userWallet].primary == _oldPrimary,  "Invalid prior configuration"); 
 
-          // A valid updater can be one of the dataOwnerWallet or an existing creator node
+          // A valid updater can be one of the dataOwnerWallet, existing creator node, or contract deployer
           bool validUpdater = false;
-          if (msg.sender == _userWallet || msg.sender == spIdToCreatorNode[_oldPrimary]) {
+          if (msg.sender == _userWallet || msg.sender == spIdToCreatorNode[_oldPrimary] || msg.sender == deployer) {
               validUpdater = true;
           }
+          // Caller's notion of secondary values must match registered value on chain
+          // A secondary node can also be considered a valid updater
           for (uint i = 0; i < _oldSecondaries.length; i++) {
-              // Caller's notion of secondary values must match registered value on chain
               require(
                   artistReplicaSets[_userWallet].secondaries[i] == _oldSecondaries[i],
                   "Invalid prior secondary configuration"
@@ -73,7 +77,13 @@ contract UserReplicaSetManager is RegistryContract, SigningLogic {
                 validUpdater = true;
               }
           }
-          require(validUpdater == validUpdater, "Invalid update operation");
+          require(validUpdater == true, "Invalid update operation");
+
+          // Confirm primary and every incoming secondary is valid
+          require(spIdToCreatorNode[_primary] != address(0x00), "Primary must exist"); 
+          for (uint i = 0; i < _secondaries.length; i++) {
+              require(spIdToCreatorNode[i] != address(0x00), "Secondary must exist");
+          }
 
           // Perform replica set update
           artistReplicaSets[_userWallet] = ReplicaSet({
