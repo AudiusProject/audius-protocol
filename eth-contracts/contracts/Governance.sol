AudiusProject/audius-protocol

commit bf555d0e296201d02b53c73c3365435b5cd5680a

[M05] Only active stakers can evaluate proposals
* Enable any party to evaluate proposal, corresponding test case updated

eth-contracts/contracts/Governance.sol
@@ -364,16 +364,6 @@ contract Governance is InitializableV2 {
         // proposal to 'Evaluating' so it should fail the status is 'InProgress' check
         proposals[_proposalId].outcome = Outcome.Evaluating;
 
-        // Require msg.sender is active Staker.
-        Staking stakingContract = Staking(stakingAddress);
-
-        require(
-            stakingContract.totalStakedForAt(
-                msg.sender, proposals[_proposalId].startBlockNumber
-            ) > 0,
-            "Governance::evaluateProposalOutcome: Caller must be active staker with non-zero stake."
-        );
-
         // Require proposal votingPeriod has ended.
         uint256 startBlockNumber = proposals[_proposalId].startBlockNumber;
         uint256 endBlockNumber = startBlockNumber.add(votingPeriod);

eth-contracts/test/governance.test.js
@@ -845,11 +845,8 @@ contract('Governance.sol', async (accounts) => {
         )
       })
 
-      it('Fail to call evaluate proposal from non-staker', async () => {
-        await _lib.assertRevert(
-          governance.evaluateProposalOutcome(proposalId, { from: accounts[15] }),
-          "Governance::evaluateProposalOutcome: Caller must be active staker with non-zero stake."
-        )
+      it('Call to evaluate proposal from non-staker will succeed', async () => {
+        governance.evaluateProposalOutcome(proposalId, { from: accounts[15] })
       })
 
       it('Fail to evaluate proposal before votingPeriod has ended', async () => {

commit d04ebcc5e4b04b49b0d927edbf1b7d7af69a70fd

[M05] Only active stakers can evaluate proposals (follow-up)
* Fix compilation error introduced by https://github.com/AudiusProject/audius-protocol/pull/572/files

eth-contracts/contracts/Governance.sol
@@ -381,6 +381,7 @@ contract Governance is InitializableV2 {
             "Governance::evaluateProposalOutcome: Registered contract address for targetContractRegistryKey has changed"
         );
 
+        Staking stakingContract = Staking(stakingAddress);
         // Calculate outcome
         Outcome outcome;
         // voting quorum not met -> proposal is invalid.