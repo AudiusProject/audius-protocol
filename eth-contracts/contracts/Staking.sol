17bca96d1c17ba2bf7645c7062a006baf9d019af
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-06-30 17:25:22 -0700

    [L06] Misleading comments, docstrings, and typographical errors (#546)
    Co-authored-by: Hareesh Nagaraj <hareesh.nagaraj@gmail.com>

--- eth-contracts/contracts/Staking.sol
+++ eth-contracts/contracts/Staking.sol
@@ -48,7 +48,7 @@ contract Staking is InitializableV2 {
     /**
      * @notice Function to initialize the contract
      * @param _stakingToken - address of ERC20 token that will be staked
-     * @param _governanceAddress - address for Governance proxy contract     * @param _test - address for Governance proxy contract
+     * @param _governanceAddress - address for Governance proxy contract
      */
     function initialize(
         address _stakingToken,
@@ -129,7 +129,7 @@ contract Staking is InitializableV2 {
     }
 
     /**
-     * @notice Update claim history by adding an event to the claim historry
+     * @notice Update claim history by adding an event to the claim history
      * @param _amount - amount to add to claim history
      * @param _stakerAccount - address of staker
      */
@@ -219,7 +219,8 @@ contract Staking is InitializableV2 {
     }
 
     /**
-     * @notice Stakes `_amount` tokens, transferring them from caller, and assigns them to `_accountAddress`
+     * @notice Stakes `_amount` tokens, transferring them from `_delegatorAddress` to `_accountAddress`,
+               only callable by DelegateManager
      * @param _accountAddress - The final staker of the tokens
      * @param _delegatorAddress - Address from which to transfer tokens
      * @param _amount - Number of tokens staked
@@ -242,7 +243,8 @@ contract Staking is InitializableV2 {
     }
 
     /**
-     * @notice Stakes `_amount` tokens, transferring them from caller, and assigns them to `_accountAddress`
+     * @notice Unstakes '_amount` tokens, transferring them from `_accountAddress` to `_delegatorAddress`,
+               only callable by DelegateManager
      * @param _accountAddress - The staker of the tokens
      * @param _delegatorAddress - Address from which to transfer tokens
      * @param _amount - Number of tokens unstaked
