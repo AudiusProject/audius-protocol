ceea610117a1e4810cd0880a02c213a1b0cb631a
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-07-15 10:20:43 -0700

    ALL WORK RELATED TO DELEGATEOWNERWALLET ADDRESS
    Propagating address, cont.
    
    Overriding now working
    
    Writing file now works
    
    Enabling all writes

--- libs/initScripts/local.js
+++ libs/initScripts/local.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const readline = require('readline');
+
 const initAudiusLibs = require('../examples/initAudiusLibs')
 const { distributeTokens } = require('./helpers/distributeTokens')
 const { setServiceVersion } = require('./helpers/version')
@@ -8,6 +11,8 @@ const {
 } = require('./helpers/spRegistration')
 const { deregisterLocalService } = require('./helpers/spRegistration')
 const { getClaimInfo, fundNewClaim } = require('./helpers/claim')
+const { getDataContractAccounts, getEthContractAccounts } = require('./helpers/utils')
+const { default: FontStyles } = require('../../identity-service/src/notifications/renderEmail/FontStyles')
 
 const serviceTypeList = ['discovery-provider', 'creator-node', 'content-service']
 const spDiscProvType = serviceTypeList[0]
@@ -114,6 +119,22 @@ const run = async () => {
       case 'query-sps':
         await queryLocalServices(audiusLibs, serviceTypeList)
         break
+      
+      case 'update-cnode-1-delegatewallet':
+        await _updateCnode1DelegateOwnerWallet(ethAccounts)
+        break
+
+      case 'update-cnode-2-delegatewallet':
+        await _updateCnode2DelegateOwnerWallet(ethAccounts)
+        break
+
+      case 'update-cnode-3-delegatewallet':
+        await _updateCnode3DelegateOwnerWallet(ethAccounts)
+        break
+
+      case 'update-cnode-4-delegatewallet':
+        await _updateCnode4DelegateOwnerWallet(ethAccounts)
+        break
 
       case 'init-all':
         await _initializeLocalEnvironment(audiusLibs, ethAccounts)
@@ -149,7 +170,8 @@ const _registerDiscProv2 = async (audiusLibs, ethAccounts) => {
 
 // Account 1
 const _registerCnode1 = async (audiusLibs, ethAccounts) => {
-  let audiusLibs2 = await initAudiusLibs(true, null, ethAccounts[1])
+  let acct = ethAccounts[1].toLowerCase()
+  let audiusLibs2 = await initAudiusLibs(true, null, acct)
   await registerLocalService(audiusLibs2, spCreatorNodeType, creatorNodeEndpoint1, amountOfAuds)
 }
 
@@ -169,6 +191,47 @@ const _registerCnode3 = async (audiusLibs, ethAccounts) => {
 const _registerCnode4 = async (audiusLibs, ethAccounts) => {
   let audiusLibs2 = await initAudiusLibs(true, null, ethAccounts[5])
   await registerLocalService(audiusLibs2, spCreatorNodeType, creatorNodeEndpoint4, amountOfAuds)
+}
+
+// Account 1 - Cnode 1 Delegate Wallet Update
+const _updateCnode1DelegateOwnerWallet = async (ethAccounts) => {
+  let acct = ethAccounts[1].toLowerCase()
+  let readPath = '../creator-node/docker-compose/development.env'
+  let writePath = readPath
+  let ganacheEthAccounts = await getEthContractAccounts()
+  // PKey is now recovered
+  let delegateWalletPkey = ganacheEthAccounts['private_keys'][`${acct}`]
+  await _updateDelegateOwnerWalletInDockerEnv(readPath, writePath, acct, delegateWalletPkey)
+}
+
+// Account 2 - Cnode 2 Delegate Wallet Update
+const _updateCnode2DelegateOwnerWallet = async (ethAccounts) => {
+  let acct = ethAccounts[2].toLowerCase()
+  let readPath = '../creator-node/docker-compose/dev/development2.env'
+  let ganacheEthAccounts = await getEthContractAccounts()
+  // PKey is now recovered
+  let delegateWalletPkey = ganacheEthAccounts['private_keys'][`${acct}`]
+  await _updateDelegateOwnerWalletInDockerEnv(readPath, readPath, acct, delegateWalletPkey)
+}
+
+// Account 4 - Cnode 3 Delegate Wallet Update
+const _updateCnode3DelegateOwnerWallet = async (ethAccounts) => {
+  let acct = ethAccounts[4].toLowerCase()
+  let readPath = '../creator-node/docker-compose/dev/development3.env'
+  let ganacheEthAccounts = await getEthContractAccounts()
+  // PKey is now recovered
+  let delegateWalletPkey = ganacheEthAccounts['private_keys'][`${acct}`]
+  await _updateDelegateOwnerWalletInDockerEnv(readPath, readPath, acct, delegateWalletPkey)
+}
+
+// Account 5 - Cnode 4 Delegate Wallet Update
+const _updateCnode4DelegateOwnerWallet = async (ethAccounts) => {
+  let acct = ethAccounts[5].toLowerCase()
+  let readPath = '../creator-node/docker-compose/dev/development4.env'
+  let ganacheEthAccounts = await getEthContractAccounts()
+  // PKey is now recovered
+  let delegateWalletPkey = ganacheEthAccounts['private_keys'][`${acct}`]
+  await _updateDelegateOwnerWalletInDockerEnv(readPath, readPath, acct, delegateWalletPkey)
 }
 
 const _deregisterAllSPs = async (audiusLibs, ethAccounts) => {
@@ -191,4 +254,38 @@ const _initAllVersions = async (audiusLibs) => {
   for (let serviceType of serviceTypeList) {
     await setServiceVersion(audiusLibs, serviceType, serviceVersions[serviceType])
   }
+}
+
+const _updateDelegateOwnerWalletInDockerEnv = async (readPath, writePath, delegateOwnerWallet, delegateWalletPkey) => {
+  const fileStream = fs.createReadStream(readPath)
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  let output = []
+  let walletFound = false
+  let pkeyFound = false
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    if (line.includes('delegateOwnerWallet')) {
+      output.push(`delegateOwnerWallet=${delegateOwnerWallet}`)
+      walletFound = true
+    } else if (line.includes('delegatePrivateKey')) {
+      output.push(`delegatePrivateKey=0x${delegateWalletPkey}`)
+      pkeyFound = true
+    } else {
+      output.push(line)
+    }
+  }
+
+  if (!walletFound) {
+    output.push(`delegateOwnerWallet=${delegateOwnerWallet}`)
+  }
+  if (!pkeyFound) {
+    output.push(`delegatePrivateKey=0x${delegateWalletPkey}`)
+  }
+
+  let finalUpdatedOutput = output.join("\n")
+  fs.writeFileSync(writePath, finalUpdatedOutput)
+  console.log(`Updated ${writePath} with ${delegateOwnerWallet}:${delegateWalletPkey}`)
 }
