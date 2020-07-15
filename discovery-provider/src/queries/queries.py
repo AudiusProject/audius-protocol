46e483a83511e06fd5a802de4f03a5698c5b01ae
Michael Piazza <michael.piazza.mp@gmail.com>
2020-07-15 00:24:18 -0700

    Original tracks endpoint working

--- discovery-provider/src/queries/queries.py
+++ discovery-provider/src/queries/queries.py
@@ -21,6 +21,7 @@ from src.queries.query_helpers import get_current_user_id, parse_sort_param, pop
     create_repost_count_subquery
 
 from src.queries.get_users import get_users
+from src.queries.get_tracks import get_tracks
 
 logger = logging.getLogger(__name__)
 bp = Blueprint("queries", __name__)
@@ -40,71 +41,8 @@ def get_users_route():
 # Returns all tracks (paginated) with each track's repost count
 # optionally filters by track ids
 @bp.route("/tracks", methods=("GET",))
-def get_tracks():
-    tracks = []
-    db = get_db_read_replica()
-    with db.scoped_session() as session:
-        # Create initial query
-        base_query = session.query(Track)
-        base_query = base_query.filter(Track.is_current == True, Track.is_unlisted == False, Track.stem_of == None)
-
-        # Conditionally process an array of tracks
-        if "id" in request.args:
-            # Retrieve argument from flask request object
-            # Ensures empty parameters are not processed
-            track_id_str_list = request.args.getlist("id")
-            track_id_list = []
-            try:
-                track_id_list = [int(y) for y in track_id_str_list]
-                # Update query with track_id list
-                base_query = base_query.filter(Track.track_id.in_(track_id_list))
-            except ValueError as e:
-                logger.error("Invalid value found in track id list", exc_info=True)
-                raise e
-
-        # Allow filtering of tracks by a certain creator
-        if "user_id" in request.args:
-            user_id = request.args.get("user_id", type=int)
-            base_query = base_query.filter(
-                Track.owner_id == user_id
-            )
-
-        # Allow filtering of deletes
-        # Note: There is no standard for boolean url parameters, and any value (including 'false')
-        # will be evaluated as true, so an explicit check is made for true
-        if ("filter_deleted" in request.args):
-            filter_deleted = request.args.get("filter_deleted")
-            if (filter_deleted.lower() == 'true'):
-                base_query = base_query.filter(
-                    Track.is_delete == False
-                )
-
-        if "min_block_number" in request.args:
-            min_block_number = request.args.get("min_block_number", type=int)
-            base_query = base_query.filter(
-                Track.blocknumber >= min_block_number
-            )
-
-        whitelist_params = ['created_at', 'create_date', 'release_date', 'blocknumber', 'track_id']
-        base_query = parse_sort_param(base_query, Track, whitelist_params)
-        query_results = paginate_query(base_query).all()
-        tracks = helpers.query_result_to_list(query_results)
-
-        track_ids = list(map(lambda track: track["track_id"], tracks))
-
-        current_user_id = get_current_user_id(required=False)
-
-        # bundle peripheral info into track results
-        tracks = populate_track_metadata(session, track_ids, tracks, current_user_id)
-
-        if "with_users" in request.args and request.args.get("with_users") != 'false':
-            user_id_list = get_users_ids(tracks)
-            users = get_users_by_id(session, user_id_list)
-            for track in tracks:
-                user = users[track['owner_id']]
-                if user:
-                    track['user'] = user
-
+def get_tracks_route():
+    tracks = get_tracks(request.args.to_dict())
     return api_helpers.success_response(tracks)
 
 
