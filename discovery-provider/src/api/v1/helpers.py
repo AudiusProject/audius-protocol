29636017eb0792de591d1016715d4e005be2c23e
Michael Piazza <michael.piazza.mp@gmail.com>
2020-07-15 15:28:56 -0700

    Move to opaque IDs for tracks (#648)
    * Handle opaque IDs
    
    * Move abort function
    
    * PR polish

---
+++ discovery-provider/src/api/v1/helpers.py
@@ -0,0 +1,29 @@
+from hashids import Hashids
+
+HASH_MIN_LENGTH = 5
+HASH_SALT = "azowernasdfoia"
+
+hashids = Hashids(min_length=5, salt=HASH_SALT)
+
+def encode_int_id(id):
+    return hashids.encode(id)
+
+def decode_string_id(id):
+    # Returns a tuple
+    decoded = hashids.decode(id)
+    if not len(decoded):
+        return None
+    return decoded[0]
+
+def extend_track(track):
+    track_id = encode_int_id(track["track_id"])
+    owner_id = encode_int_id(track["owner_id"])
+    track["id"] = track_id
+    track["owner_id"] = owner_id
+    return track
+
+def decode_with_abort(identifier, namespace):
+    decoded = decode_string_id(identifier)
+    if decoded is None:
+        namespace.abort(404, "Invalid ID: '{}'.".format(identifier))
+    return decoded
