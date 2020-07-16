9dbd5f8111dad635999ed75a59cb38f769673084
Michael Piazza <michael.piazza.mp@gmail.com>
2020-07-16 11:28:01 -0700

    Playlist Root Endpoint (#650)
    * Scaffolding playlists route
    
    * Playlists route working
    
    * PR Polish
    
    * PR revisions

--- discovery-provider/src/api/v1/helpers.py
+++ discovery-provider/src/api/v1/helpers.py
@@ -1,4 +1,5 @@
 from hashids import Hashids
+from flask_restx import fields
 
 HASH_MIN_LENGTH = 5
 HASH_SALT = "azowernasdfoia"
@@ -19,11 +20,35 @@ def extend_track(track):
     track_id = encode_int_id(track["track_id"])
     owner_id = encode_int_id(track["owner_id"])
     track["id"] = track_id
-    track["owner_id"] = owner_id
+    track["user_id"] = owner_id
     return track
+
+def extend_playlist(playlist):
+    playlist_id = encode_int_id(playlist["playlist_id"])
+    owner_id = encode_int_id(playlist["playlist_owner_id"])
+    playlist["id"] = playlist_id
+    playlist["user_id"] = owner_id
+    return playlist
 
 def decode_with_abort(identifier, namespace):
     decoded = decode_string_id(identifier)
     if decoded is None:
         namespace.abort(404, "Invalid ID: '{}'.".format(identifier))
     return decoded
+
+def make_response(name, namespace, modelType):
+    version_metadata = namespace.model("version_metadata", {
+        "service": fields.String(required=True),
+        "version": fields.String(required=True)
+    })
+
+    return namespace.model(name, {
+        "data": modelType,
+        "latest_chain_block":	fields.Integer(required=True),
+        "latest_indexed_block":	fields.Integer(required=True),
+        "owner_wallet":	fields.Integer(required=True),
+        "signature": fields.String(required=True),
+        "success": fields.Boolean(required=True),
+        "timestamp": fields.String(required=True)	,
+        "version": fields.Nested(version_metadata, required=True),
+    })
--- discovery-provider/src/queries/queries.py
+++ discovery-provider/src/queries/queries.py
@@ -22,6 +22,7 @@ from src.queries.query_helpers import get_current_user_id, parse_sort_param, pop
 
 from src.queries.get_users import get_users
 from src.queries.get_tracks import get_tracks
+from src.queries.get_playlists import get_playlists
 
 logger = logging.getLogger(__name__)
 bp = Blueprint("queries", __name__)
@@ -146,83 +147,8 @@ def get_stems_of(track_id):
 # Return playlist content in json form
 # optional parameters playlist owner's user_id, playlist_id = []
 @bp.route("/playlists", methods=("GET",))
-def get_playlists():
-    playlists = []
-    current_user_id = get_current_user_id(required=False)
-    filter_out_private_playlists = True
-
-    db = get_db_read_replica()
-    with db.scoped_session() as session:
-        try:
-            playlist_query = (
-                session.query(Playlist)
-                .filter(Playlist.is_current == True)
-            )
-
-            # playlist ids filter if the optional query param is passed in
-            if "playlist_id" in request.args:
-                playlist_id_str_list = request.args.getlist("playlist_id")
-                playlist_id_list = []
-                try:
-                    playlist_id_list = [int(y) for y in playlist_id_str_list]
-                    playlist_query = playlist_query.filter(Playlist.playlist_id.in_(playlist_id_list))
-                except ValueError as e:
-                    raise exceptions.ArgumentError("Invalid value found in playlist id list", e)
-
-            if "user_id" in request.args:
-                user_id = request.args.get("user_id", type=int)
-                # user id filter if the optional query param is passed in
-                playlist_query = playlist_query.filter(
-                    Playlist.playlist_owner_id == user_id
-                )
-
-                # if the current user is the same as the user passed in through the query param then we're trying
-                # to get playlists for, check if the users are the same. if they are the same, the current user is
-                # trying to request their own playlists, so allow them to see private playlists
-                if current_user_id and user_id and (int(current_user_id) == int(user_id)):
-                    filter_out_private_playlists = False
-
-            if filter_out_private_playlists:
-                playlist_query = playlist_query.filter(
-                    Playlist.is_private == False
-                )
-
-            # Filter out deletes unless we're fetching explicitly by id
-            if "playlist_id" not in request.args:
-                playlist_query = playlist_query.filter(
-                    Playlist.is_delete == False
-                )
-
-            playlist_query = playlist_query.order_by(desc(Playlist.created_at))
-            playlists = paginate_query(playlist_query).all()
-            playlists = helpers.query_result_to_list(playlists)
-
-            # retrieve playlist ids list
-            playlist_ids = list(map(lambda playlist: playlist["playlist_id"], playlists))
-
-            current_user_id = get_current_user_id(required=False)
-
-            # bundle peripheral info into playlist results
-            playlists = populate_playlist_metadata(
-                session,
-                playlist_ids,
-                playlists,
-                [RepostType.playlist, RepostType.album],
-                [SaveType.playlist, SaveType.album],
-                current_user_id
-            )
-
-            if "with_users" in request.args and request.args.get("with_users") != 'false':
-                user_id_list = get_users_ids(playlists)
-                users = get_users_by_id(session, user_id_list)
-                for playlist in playlists:
-                    user = users[playlist['playlist_owner_id']]
-                    if user:
-                        playlist['user'] = user
-
-        except sqlalchemy.orm.exc.NoResultFound:
-            pass
-
+def get_playlists_route():
+    playlists = get_playlists(request.args.to_dict())
     return api_helpers.success_response(playlists)
 
 
