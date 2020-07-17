ef0b5758d694cfabca6d5b83336da98899a5baa2
Raymond Jacobson <ray@audius.co>
2020-07-17 12:13:23 -0700

    Move get_remixes_of

--- discovery-provider/src/queries/queries.py
+++ discovery-provider/src/queries/queries.py
@@ -47,6 +47,7 @@ from src.queries.get_top_playlists import get_top_playlists
 from src.queries.get_top_followee_windowed import get_top_followee_windowed
 from src.queries.get_top_followee_saves import get_top_followee_saves
 from src.queries.get_top_genre_users import get_top_genre_users
+from src.queries.get_remixes_of import get_remixes_of
 
 
 logger = logging.getLogger(__name__)
@@ -336,106 +337,15 @@ def get_top_genre_users_route():
 # Get the tracks that are 'children' remixes of the requested track
 # The results are sorted by if the original artist has reposted or saved the track
 @bp.route("/remixes/<int:track_id>/children", methods=("GET",))
-def get_remixes_of(track_id):
+def get_remixes_of_route(track_id):
+    try:
+        remixes = get_remixes_of(track_id, to_dict(request.args))
+        return api_helpers.success_response(remixes)
+    except exceptions.ArgumentError as e:
+        return api_helpers.error_response(str(e), 400)
+    except Exception as e:
+        return api_helpers.error_response(str(e), 404)
 
-    db = get_db_read_replica()
-    with db.scoped_session() as session:
-        # Fetch the parent track to get the track's owner id
-        parent_track = session.query(Track).filter(
-            Track.is_current == True,
-            Track.track_id == track_id
-        ).first()
-
-        if (parent_track == None):
-            return api_helpers.error_response("Invalid track_id provided", 400)
-
-        track_owner_id = parent_track.owner_id
-
-        # Create subquery for save counts for sorting
-        save_count_subquery = create_save_count_subquery(session, SaveType.track)
-
-        # Create subquery for repost counts for sorting
-        repost_count_subquery = create_repost_count_subquery(session, RepostType.track)
-
-        # Get the 'children' remix tracks
-        # Use the track owner id to fetch reposted/saved tracks returned first
-        base_query = (
-            session.query(
-                Track
-            )
-            .join(
-                Remix,
-                and_(
-                    Remix.child_track_id == Track.track_id,
-                    Remix.parent_track_id == track_id
-                )
-            ).outerjoin(
-                Save,
-                and_(
-                    Save.save_item_id == Track.track_id,
-                    Save.save_type == SaveType.track,
-                    Save.is_current == True,
-                    Save.is_delete == False,
-                    Save.user_id == track_owner_id
-                )
-            ).outerjoin(
-                Repost,
-                and_(
-                    Repost.repost_item_id == Track.track_id,
-                    Repost.user_id == track_owner_id,
-                    Repost.repost_type == RepostType.track,
-                    Repost.is_current == True,
-                    Repost.is_delete == False
-                )
-            ).outerjoin(
-                repost_count_subquery,
-                repost_count_subquery.c['id'] == Track.track_id
-            ).outerjoin(
-                save_count_subquery,
-                save_count_subquery.c['id'] == Track.track_id
-            )
-            .filter(
-                Track.is_current == True,
-                Track.is_delete == False,
-                Track.is_unlisted == False
-            )
-            # 1. Co-signed tracks ordered by save + repost count
-            # 2. Other tracks ordered by save + repost count
-            .order_by(
-                desc(
-                    # If there is no "co-sign" for the track (no repost or save from the parent owner),
-                    # defer to secondary sort
-                    case(
-                        [
-                            (and_(Repost.created_at == None, Save.created_at == None), 0),
-                        ],
-                        else_ = (
-                            func.coalesce(repost_count_subquery.c.repost_count, 0) + \
-                            func.coalesce(save_count_subquery.c.save_count, 0)
-                        )
-                    )
-                ),
-                # Order by saves + reposts
-                desc(
-                    func.coalesce(repost_count_subquery.c.repost_count, 0) + \
-                    func.coalesce(save_count_subquery.c.save_count, 0)
-                ),
-                # Ties, pick latest track id
-                desc(Track.track_id)
-            )
-        )
-
-        (tracks, count) = paginate_query(base_query, True, True)
-        tracks = tracks.all()
-        tracks = helpers.query_result_to_list(tracks)
-        track_ids = list(map(lambda track: track["track_id"], tracks))
-        current_user_id = get_current_user_id(required=False)
-        tracks = populate_track_metadata(session, track_ids, tracks, current_user_id)
-
-        if "with_users" in request.args and request.args.get("with_users") != 'false':
-            add_users_to_tracks(session, tracks)
-
-    return api_helpers.success_response({ 'tracks': tracks, 'count': count }, 200)
 
 # Get the tracks that are 'parent' remixes of the requested track
 @bp.route("/remixes/<int:track_id>/parents", methods=("GET",))
