5593c986885ad62fd2745080172263d8fe3b3811
Michael Piazza <michael.piazza.mp@gmail.com>
2020-07-21 19:11:57 -0700

    /tracks

--- discovery-provider/src/api/v1/api.py
+++ discovery-provider/src/api/v1/api.py
@@ -2,8 +2,10 @@ from flask import Flask, Blueprint
 from flask_restx import Resource, Api
 from src.api.v1.users import ns as users_ns
 from src.api.v1.playlists import ns as playlists_ns
+from src.api.v1.tracks import ns as tracks_ns
 
 bp = Blueprint('api_v1', __name__, url_prefix="/v1")
 api_v1 = Api(bp, version='1.0', description='Audius V1 API')
 api_v1.add_namespace(users_ns)
 api_v1.add_namespace(playlists_ns)
+api_v1.add_namespace(tracks_ns)
--- discovery-provider/src/api/v1/users.py
+++ discovery-provider/src/api/v1/users.py
@@ -1,73 +1,14 @@
-import logging # pylint: disable=C0302
-from flask import Flask, Blueprint
+import logging
+from .tracks import favorite, track # pylint: disable=C0302
 from src.queries.get_saves import get_saves
 from src.queries.get_users import get_users
-# from src.queries.get_users import get_users
-from flask_restx import Resource, Namespace, fields
+from flask_restx import Resource, Namespace, fields, reqparse, inputs
 from src.queries.get_tracks import get_tracks
-from src import api_helpers
 from src.api.v1.helpers import abort_not_found, decode_with_abort, extend_favorite, extend_track, extend_user, make_response, success_response
 
 logger = logging.getLogger(__name__)
 
 ns = Namespace('users', description='User related operations')
-
-track_segment = ns.model('track_segment', {
-    "duration": fields.Float(required=True),
-    "multihash": fields.String(required=True)
-})
-
-# TODO: extend these...
-
-repost = ns.model('repost', {
-    "blockhash": fields.String(required=True),
-    "blocknumber": fields.Integer(required=True),
-    "created_at": fields.String(required=True),
-    "is_current": fields.Boolean(required=True),
-    "is_delete": fields.Boolean(required=True),
-    "repost_item_id": fields.Integer(required=True),
-    "repost_type": fields.String(required=True),
-    "user_id": fields.Integer(required=True)
-})
-
-favorite = ns.model('favorite', {
-    "blockhash": fields.String(required=True),
-    "blocknumber": fields.Integer(required=True),
-    "created_at": fields.String(required=True),
-    "is_current": fields.Boolean(required=True),
-    "is_delete": fields.Boolean(required=True),
-    "save_item_id":	fields.String(required=True),
-    "save_type": fields.String(required=True),
-    "user_id": fields.String(required=True)
-})
-
-track_element = ns.model('track_element', {
-    "parent_track_id": fields.Integer(required=True)
-})
-
-remix_parent = ns.model('remix_parent', {
-    "tracks": fields.List(fields.Nested(track_element))
-})
-
-stem_parent = ns.model('stem_parent', {
-    "category": fields.String(required=True),
-    "parent_track_id": fields.Integer(required=True)
-})
-
-download = ns.model('download_metadata', {
-    "cid": fields.String,
-    "is_downloadable": fields.Boolean(required=True),
-    "required_follow": fields.Boolean(required=True),
-})
-
-field_visibility = ns.model('field_visibility', {
-    "mood": fields.Boolean,
-    "tags": fields.Boolean,
-    "genre": fields.Boolean,
-    "share": fields.Boolean,
-    "play_count": fields.Boolean,
-    "remixes": fields.Boolean
-})
 
 user_model = ns.model('User', {
     "album_count": fields.Integer(required=True),
@@ -100,48 +41,6 @@ user_model = ns.model('User', {
     "updated_at": fields.String(required=True),
     "wallet": fields.String(required=True)
 })
-
-track = ns.model('Track', {
-    "blockhash": fields.String(required=True),
-    "blocknumber": fields.Integer(required=True),
-    "cover_art": fields.String,
-    "cover_art_sizes": fields.String,
-    "create_date": fields.String,
-    "created_at": fields.String,
-    "credits_splits": fields.String,
-    "description": fields.String,
-    "download": fields.Nested(download),
-    "field_visibility": fields.Nested(field_visibility),
-    "file_type": fields.String,
-    "followee_reposts": fields.List(fields.Nested(repost), required=True),
-    "followee_saves": fields.List(fields.Nested(favorite), required=True),
-    "genre": fields.String,
-    "has_current_user_reposted": fields.Boolean(required=True),
-    "has_current_user_saved": fields.Boolean(required=True),
-    "id": fields.String(required=True),
-    "is_current": fields.Boolean(required=True),
-    "is_delete": fields.Boolean(required=True),
-    "is_unlisted": fields.Boolean(required=True),
-    "isrc": fields.String,
-    "iswc": fields.String,
-    "length": fields.Integer,
-    "license": fields.String,
-    "metadata_multihash": fields.String(required=True),
-    "mood": fields.String,
-    "release_date": fields.String,
-    "remix_of": fields.Nested(remix_parent),
-    "repost_count": fields.Integer(required=True),
-    "route_id": fields.String(required=True),
-    "save_count": fields.Integer(required=True),
-    "stem_of": fields.Nested(stem_parent),
-    "tags": fields.String,
-    "title": fields.String(required=True),
-    "track_segments": fields.List(fields.Nested(track_segment)),
-    "updated_at": fields.String,
-    "user": fields.Nested(user_model, required=True),
-    "user_id": fields.String(required=True)
-})
-
 
 user_response = make_response("user_response", ns, fields.Nested(user_model))
 
@@ -183,3 +82,12 @@ class FavoritedTracks(Resource):
         favorites = list(map(extend_favorite, favorites))
         return success_response(favorites)
 
+search_parser = reqparse.RequestParser()
+search_parser.add_argument('query', required=True)
+@ns.route("/search")
+class SearchResult(Resource):
+    @ns.expect(search_parser)
+    def get(self):
+        args = search_parser.parse_args()
+        query = args["query"]
+        return query, 200
