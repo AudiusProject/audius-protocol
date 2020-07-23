5a13f2fd5538f143e5acfdd74d99dd820f8f5a1e
Joseph Lee <joeylee0925@gmail.com>
2020-07-23 14:40:03 -0400

    Add stream url (#678)
    * Added track stream url
    
    * Removed log
    
    * Fixed track stream url

--- discovery-provider/src/api/v1/tracks.py
+++ discovery-provider/src/api/v1/tracks.py
@@ -1,6 +1,9 @@
+from urllib.parse import urljoin
 import logging # pylint: disable=C0302
+from flask import redirect
 from flask_restx import Resource, Namespace, fields
 from src.queries.get_tracks import get_tracks
+from src.queries.get_track_user_creator_node import get_track_user_creator_node
 from src.api.v1.helpers import abort_not_found, decode_with_abort, encode_int_id, extend_favorite, extend_track, extend_user, make_response, success_response
 from .models.tracks import track
 
@@ -19,3 +22,20 @@ class Track(Resource):
             abort_not_found(encoded_id, ns)
         single_track = extend_track(tracks[0])
         return success_response(single_track)
+
+@ns.route("/<string:track_id>/stream")
+class TrackStream(Resource):
+    def get(self, track_id):
+        """Redirect to track mp3"""
+        encoded_id = decode_with_abort(track_id, ns)
+        args = {"track_id": encoded_id}
+        creator_nodes = get_track_user_creator_node(args)
+        if creator_nodes is None:
+            abort_not_found(encoded_id, ns)
+        creator_nodes = creator_nodes.split(',')
+        if not creator_nodes:
+            abort_not_found(encoded_id, ns)
+
+        primary_node = creator_nodes[0]
+        stream_url = urljoin(primary_node, 'tracks/stream/{}'.format(track_id))
+        return redirect(stream_url)
