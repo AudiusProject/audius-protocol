2c946959540db8e56668ab89dff98a6b888fb044
vicky guan <vicky@audius.co>
2020-07-22 22:10:38 -0700

    adding tests to cover checking fs for contents

--- creator-node/test/resizeImage.js
+++ creator-node/test/resizeImage.js
@@ -2,14 +2,29 @@ const { ipfs } = require('../src/ipfsClient')
 const resizeImageJob = require('../src/resizeImage')
 const config = require('../src/config')
 
-// const Jimp = require('jimp')
-// const ExifParser = require('exif-parser')
-// const ipfsClient = require('ipfs-http-client')
 const fs = require('fs')
 const path = require('path')
-
 const sinon = require('sinon')
 const assert = require('assert')
+
+// Image buffer for audiusDj.png test image
+const imageTestDir = 'images/audiusDjContent'
+const imageBuffer = fs.readFileSync(path.join(__dirname, imageTestDir, 'audiusDj.png'))
+
+// TODO: figure out if this parsing is necessary
+let storagePath = config.get('storagePath')
+// / in front of path = root ; local testing purposes, remove
+storagePath = storagePath.charAt(0) === '/' ? storagePath.slice(1) : storagePath
+
+// CIDs for audiusDj.png
+const DIR_CID_SQUARE = 'QmNfiyESzN4rNQikeHUiF4HBfAEKF38DTo1JtiDMukqwE9'
+const CID_1000 = 'QmZg29dJohTJdNodaiLrKcdTBhRhnbHcCijt3i88juyKzh'
+const CID_480 = 'QmcThUoKmADpRZmQCNa8W88tcBCHjhSpU8qCWRETks7bAR'
+const CID_150 = 'QmSFGj6Hos2RPjGnogeZ1AgNa8tAsdLFYg4tfzMxyLi4Mh'
+const DIR_CID_NOT_SQUARE = 'QmNWhyJ7UrWUjpnFhxVSefAsr2etbuYYLfAb3TSC4DujjY'
+const CID_640 = 'QmQmsktPHnTvneXpYYLCMmbE8xVp7wRGvmw1nX4zX2dS3v'
+const CID_2000 = 'QmdjTLFVyGyzG3pVRsQLSGQq9bK2T2bdth65wrW8xMQZDg'
+const CID_ORIGINAL = 'QmWAMpnZo2TC45mnENxXsPPdCDk5osDKEt7vY1FEU3x28L'
 
 describe('test resizeImage', () => {
   afterEach(function () {
@@ -33,7 +48,7 @@ describe('test resizeImage', () => {
           '1000x1000.jpg': 1000
         },
         square: true,
-        logContext: {} // todo: add working
+        logContext: {}
       }
     }
 
@@ -53,7 +68,6 @@ describe('test resizeImage', () => {
    */
   it('should throw error if ipfs is down', async () => {
     sinon.stub(ipfs, 'add').throws('ipfs is down!')
-    const imageBuffer = fs.readFileSync(path.join(__dirname, 'audiusDj.png'))
     const job = {
       data: {
         file: imageBuffer,
@@ -65,7 +79,7 @@ describe('test resizeImage', () => {
           '1000x1000.jpg': 1000
         },
         square: true,
-        logContext: {} // todo: add working
+        logContext: {}
       }
     }
 
@@ -84,7 +98,6 @@ describe('test resizeImage', () => {
    * Then: an error is thrown
    */
   it('should throw error if making a directory with new dest path fails', async () => {
-    const imageBuffer = fs.readFileSync(path.join(__dirname, 'audiusDj.png'))
     const job = {
       data: {
         file: imageBuffer,
@@ -96,7 +109,7 @@ describe('test resizeImage', () => {
           '1000x1000.jpg': 1000
         },
         square: true,
-        logContext: {} // todo: add working
+        logContext: {}
       }
     }
 
@@ -110,24 +123,85 @@ describe('test resizeImage', () => {
   })
 
   /**
-   * Given: we have successfully created the resized images to add to ipfs
-   * When: we add the resized images to ipfs
-   * Then: we ensure that what is added to fs is the same as what is added to ipfs
+   * Given: we have successfully resized the images (square)
+   * When: the images are added to the filesystem
+   * Then: the images should:
+   *  - be added in the proper file system path
+   *  - correctly resized (150x150, 480x480, 1000x1000, original)
+   *  - is not corrupted
    */
-  it('should pass with happy path (square)', async () => {
-    const imageBuffer = fs.readFileSync(path.join(__dirname, 'audiusDj.png'))
+  it('should pass with proper contents added to filesystem (square)', async () => {
     const job = {
       data: {
         file: imageBuffer,
         fileName: 'audiusDj',
-        storagePath: config.get('storagePath'),
+        storagePath,
         sizes: {
           '150x150.jpg': 150,
           '480x480.jpg': 480,
           '1000x1000.jpg': 1000
         },
         square: true,
-        logContext: {} // todo: add working
+        logContext: {}
+      }
+    }
+
+    try {
+      await resizeImageJob(job)
+    } catch (e) {
+      console.error(e)
+      assert.ok(false)
+    }
+
+    // Check fs contains the dir for square cids
+    const dirPath = path.join(storagePath, DIR_CID_SQUARE)
+    assert.ok(fs.existsSync(dirPath))
+
+    const dirContentCIDs = new Set([CID_150, CID_480, CID_1000, CID_ORIGINAL])
+
+    // Iterate through fs files
+    fs.readdir(dirPath, (err, files) => {
+      if (err) assert.fail(`Could not read directory at ${dirPath}`)
+
+      console.log(files)
+      // Check that 4 files (tentatively 150x150, 480x480, 1000x1000, original) are present
+      assert.deepStrictEqual(files.length, 4)
+
+      files.map(file => {
+        // Check that (150x150, 480x480, 1000x1000, original) files exist
+        assert.ok(dirContentCIDs.has(file))
+
+        // Check (150x150, 480x480, 1000x1000, original) file contents are proper
+        // by comparing the buffers
+        const fsBuf = fs.readFileSync(path.join(dirPath, file))
+        const expectedBuf = fs.readFileSync(path.join(__dirname, imageTestDir, DIR_CID_SQUARE, file))
+        // If comparison does not return 0, buffers are not the same
+        assert.deepStrictEqual(fsBuf.compare(expectedBuf), 0)
+
+        // Remove from set to test that only unique files are added
+        dirContentCIDs.delete(file)
+      })
+    })
+  })
+
+  /**
+   * Given: we have successfully created the resized images to add to ipfs
+   * When: we add the resized images to ipfs
+   * Then: we ensure that what is added to fs is the same as what is added to ipfs
+   */
+  it('should be properly added to ipfs (square)', async () => {
+    const job = {
+      data: {
+        file: imageBuffer,
+        fileName: 'audiusDj',
+        storagePath,
+        sizes: {
+          '150x150.jpg': 150,
+          '480x480.jpg': 480,
+          '1000x1000.jpg': 1000
+        },
+        square: true,
+        logContext: {}
       }
     }
 
@@ -161,23 +235,81 @@ describe('test resizeImage', () => {
   })
 
   /**
-   * Given: we have successfully created the resized images to add to ipfs
-   * When: we add the resized images to ipfs
-   * Then: we ensure that what is added to fs is the same as what is added to ipfs
+   * Given: we have successfully resized the images (not square)
+   * When: the images are added to the filesystem
+   * Then: the images should:
+   *  - be added in the proper file system path
+   *  - correctly resized (640x, 2400x, original)
+   *  - is not corrupted
    */
-  it('should pass with happy path (not square)', async () => {
-    const imageBuffer = fs.readFileSync(path.join(__dirname, 'audiusDj.png'))
+  it('should pass with proper contents added to filesystem (not square)', async () => {
     const job = {
       data: {
         file: imageBuffer,
         fileName: 'audiusDj',
-        storagePath: config.get('storagePath'),
+        storagePath,
         sizes: {
           '640x.jpg': 640,
           '2000x.jpg': 2000
         },
         square: false,
-        logContext: {} // todo: add working
+        logContext: {}
+      }
+    }
+
+    try {
+      await resizeImageJob(job)
+    } catch (e) {
+      console.error(e)
+      assert.ok(false)
+    }
+
+    // Check fs contains the dir for square cids
+    const dirPath = path.join(storagePath, DIR_CID_NOT_SQUARE)
+    assert.ok(fs.existsSync(dirPath))
+
+    const dirContentCIDs = new Set([CID_640, CID_2000, CID_ORIGINAL])
+
+    // Iterate through fs files
+    fs.readdir(dirPath, (err, files) => {
+      if (err) assert.fail(`Could not read directory at ${dirPath}`)
+
+      // Check that 3 files (tentatively 640x, 2000x, original) are present
+      assert.deepStrictEqual(files.length, 3)
+
+      files.map(file => {
+        // Check that (640x, 2000x, original) files exist
+        assert.ok(dirContentCIDs.has(file))
+
+        // Check (640x, 2000x, original) file contents are proper by comparing the buffers
+        const fsBuf = fs.readFileSync(path.join(dirPath, file))
+        const expectedBuf = fs.readFileSync(path.join(__dirname, imageTestDir, DIR_CID_NOT_SQUARE, file))
+        // If comparison does not return 0, buffers are not the same
+        assert.deepStrictEqual(fsBuf.compare(expectedBuf), 0)
+
+        // Remove from set to test that only unique files are added
+        dirContentCIDs.delete(file)
+      })
+    })
+  })
+
+  /**
+   * Given: we have successfully created the resized images to add to ipfs
+   * When: we add the resized images to ipfs
+   * Then: we ensure that what is added to fs is the same as what is added to ipfs
+   */
+  it('should pass with happy path (not square)', async () => {
+    const job = {
+      data: {
+        file: imageBuffer,
+        fileName: 'audiusDj',
+        storagePath,
+        sizes: {
+          '640x.jpg': 640,
+          '2000x.jpg': 2000
+        },
+        square: false,
+        logContext: {}
       }
     }
 
