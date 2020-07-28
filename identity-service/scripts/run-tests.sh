8469a75c82521c50e6bc574909402eea18c9bec6
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-07-28 12:02:18 -0700

    Redis config

--- identity-service/scripts/run-tests.sh
+++ identity-service/scripts/run-tests.sh
@@ -15,7 +15,7 @@ export ethOwnerWallet=''
 export isTestRun=true
 export isTestRun=true
 export redisHost='localhost'
-export redisPort='7379'
+export redisPort='6379'
 export dbUrl="postgres://postgres:postgres@localhost:$PG_PORT/audius_identity_service_test"
 export logLevel='debug' #should be error
 
