53fdc5a5ca85152127eced47f9accc011883f15a
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-07-31 18:43:08 -0700

    Libs hot reloading

--- identity-service/scripts/dev-server.sh
+++ identity-service/scripts/dev-server.sh
@@ -2,13 +2,15 @@
 set -o xtrace
 set -e
 
-link_libs=true
+link_libs=false
 
-if [ "$link_libs" = true ] ; then
+if [ "$link_libs" = true ]
+then
     cd ../audius-libs
     npm link
     cd ../app
     npm link @audius/libs
+    exec ./node_modules/.bin/nodemon --ignore "./emailCache" --watch src --watch ../audius-libs/ src/index.js
+else
+    exec ./node_modules/.bin/nodemon --ignore "./emailCache" src/index.js
 fi
-
-exec ./node_modules/.bin/nodemon --ignore "./emailCache" src/index.js
