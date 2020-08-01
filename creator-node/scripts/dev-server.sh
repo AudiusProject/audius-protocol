164c23092d103964d421287c9eb9eb62797dc629
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-07-31 17:51:51 -0700

    Enable optional linkage, disabled by default

--- creator-node/scripts/dev-server.sh
+++ creator-node/scripts/dev-server.sh
@@ -2,8 +2,13 @@
 set -o xtrace
 set -e
 
-cd ../audius-libs
-npm link
-cd ../app
-npm link @audius/libs
+link_libs=false
+
+if [ "$link_libs" = true ] ; then
+    cd ../audius-libs
+    npm link
+    cd ../app
+    npm link @audius/libs
+fi
+
 exec ./node_modules/.bin/nodemon src/index.js | ./node_modules/.bin/bunyan
--- identity-service/scripts/dev-server.sh
+++ identity-service/scripts/dev-server.sh
@@ -2,8 +2,13 @@
 set -o xtrace
 set -e
 
-cd ../audius-libs
-npm link
-cd ../app
-npm link @audius/libs
+link_libs=true
+
+if [ "$link_libs" = true ] ; then
+    cd ../audius-libs
+    npm link
+    cd ../app
+    npm link @audius/libs
+fi
+
 exec ./node_modules/.bin/nodemon --ignore "./emailCache" src/index.js
