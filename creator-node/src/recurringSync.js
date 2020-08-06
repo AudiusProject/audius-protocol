2071e337e87dbb25097b2a5fc78072ba09e90ca9
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-08-06 15:31:15 -0700

    First steps

--- creator-node/src/recurringSync.js
+++ creator-node/src/recurringSync.js
@@ -1,11 +1,12 @@
 const Bull = require('bull')
 const { logger } = require('./logging')
+const utils = require('./utils')
 const config = require('./config.js')
 
-class RecurringSync {
+class StateMachine {
   constructor () {
-    this.recurringSyncQueue = new Bull(
-      'recurring-syncs',
+    this.stateMachineQueue = new Bull(
+      'creator-node-state-machine',
       {
         redis: {
           port: config.get('redisPort'),
@@ -13,19 +14,65 @@ class RecurringSync {
         }
       }
     )
+
+    this.audiusLibs = null
   }
-  async init () {
-    await this.recurringSyncQueue.empty()
 
-    // run the task every 6 hours
-    this.recurringSyncQueue.add({}, { repeat: { cron: '0 */6 * * *' } })
+  async getSPInfo () {
+    const spID = config.get('spID')
+    const endpoint = config.get('creatorNodeEndpoint')
+    const delegateOwnerWallet = config.get('delegateOwnerWallet')
+    const delegatePrivateKey = config.get('delegatePrivateKey')
+    return {
+      spID,
+      endpoint,
+      delegateOwnerWallet,
+      delegatePrivateKey
+    }
+  }
 
-    // Email notification queue
-    this.recurringSyncQueue.process(async (job, done) => {
-      logger.info('Process recurring sync')
-      done()
+  async processStateMachineOperation (job) {
+    await utils.timeout(1000) 
+    logger.info('------------------Process state machine operation------------------')
+    logger.info(`job: ${job}`)
+    if (this.audiusLibs == null) {
+      logger.error(`Invalid libs instance`)
+      return
+    }
+
+    // Retrieve base information for state machine operations
+    let info = await this.getSPInfo()
+    if (info.spID == 0) {
+      console.error(`Invalid spID, processing no further operations ${info}`)
+      return
+    }
+    console.log(info)
+    logger.info('------------------END Process state machine operation------------------')
+  }
+
+  async init (audiusLibs) {
+    this.audiusLibs = audiusLibs
+    await this.stateMachineQueue.empty()
+
+    // TODO: Enable after dev
+    // Run the task every x hours
+    // this.stateMachineQueue.add({}, { repeat: { cron: '0 */x * * *' } })
+
+    this.stateMachineQueue.add({ startTime : Date.now() })
+
+    this.stateMachineQueue.process(async (job, done) => {
+      try {
+        await this.processStateMachineOperation(job)
+      } catch (e) {
+        logger.info(`Error processing ${e}`)
+      } finally {
+        // Restart job
+        // Can be replaced with cron after development is complete
+        this.stateMachineQueue.add({ startTime : Date.now() })
+        done()
+      }
     })
   }
 }
 
-module.exports = RecurringSync
+module.exports = StateMachine
