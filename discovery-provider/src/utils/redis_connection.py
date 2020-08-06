4a8744c732cff23bcc4d8398c653ef0d7511d18e
Raymond Jacobson <ray@audius.co>
2020-08-06 12:44:55 -0700

    Rework health check, using redis when applicable (#731)
    * Rework health check, using redis when applicable
    
    Creates unit tests for the queries and sets up unit test mocks
    
    * Fix tests
    
    * Remove unused .ini and add docstring
    
    * Fix env vars for tests
    
    * Fix tests

---
+++ discovery-provider/src/utils/redis_connection.py
@@ -0,0 +1,12 @@
+"""
+Interface for using a redis connection
+"""
+
+from redis import Redis
+from src.utils.config import shared_config
+
+redis_url = shared_config["redis"]["url"]
+redis = Redis.from_url(url=redis_url)
+
+def get_redis():
+    return redis
