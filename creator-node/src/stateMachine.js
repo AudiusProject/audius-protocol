5800f3ebcaaefba004bf1e8b14b3a0a9bb1b82a1
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-08-10 17:38:39 -0700

    Data calculation

--- creator-node/src/stateMachine.js
+++ creator-node/src/stateMachine.js
@@ -17,8 +17,10 @@ class StateMachine {
     )
 
     this.audiusLibs = audiusLibs
+    console.log(`${this.audiusLibs == null}`)
   }
 
+  // Helper function to retrieve all config based
   async getSPInfo () {
     const spID = config.get('spID')
     const endpoint = config.get('creatorNodeEndpoint')
@@ -31,7 +33,14 @@ class StateMachine {
       delegatePrivateKey
     }
   }
+
+  // Query eth-contracts chain for endpoint to ID info
   async recoverSpID () {
+    if (config.get('spID') != 0) {
+      console.log(`Recovered spID=${config.get('spID')}`)
+      return
+    }
+
     const recoveredSpID = await audiusLibs.ethContracts.ServiceProviderFactoryClient.getServiceProviderIdFromEndpoint(
       config.get('creatorNodeEndpoint')
     )
@@ -39,6 +48,8 @@ class StateMachine {
     config.set('spID', recoveredSpID)
   }
 
+  // Main state machine processing function
+  // TODO: Update job arguments appropriately
   async processStateMachineOperation (job) {
     logger.info('------------------Process state machine operation------------------')
     logger.info(`job: ${job}`)
@@ -47,23 +58,74 @@ class StateMachine {
       return
     }
 
-    // Retrieve base information for state machine operations
-    let info = await this.getSPInfo()
-    if (info.spID == 0) {
-      console.error(`Invalid spID, recovering ${info}`)
-      await this.getSPInfo()
+    // 1.) Retrieve base information for state machine operations
+    let spInfo = await this.getSPInfo()
+    if (spInfo.spID == 0) {
+      console.error(`Invalid spID, recovering ${spInfo}`)
+      await this.recoverSpID()
       return
     }
 
-    console.log(info)
-    // Retrieve all users for this creator node
+    console.log(spInfo)
+    // 2.) Retrieve all users for this creator node
     // TODO: Consider pagination here - already enabled in query
-    let cnodeUsers = await this.audiusLibs.discoveryProvider.getUsersForCreatorNode(info.spID)
-    console.log(`Users:`)
-    console.log(cnodeUsers)
+    let cnodeUsers = await this.audiusLibs.discoveryProvider.getUsersForCreatorNode(spInfo.spID)
+
+    // 3.) Determine list of nodes that share replica sets with this node
+    //     This includes all distinct nodes that are part of the list of cnodeUsers returned above
+
+    let nodeList = { } // Will be converted into array
+    let sharedRsets = { } // nodeId -> [rSets]
+
+    // TODO: Evaluate thread safety?
+    // What happens in the case of parallel updates to nodeList?
+    await Promise.all(cnodeUsers.map(async (user) => {
+      console.log(`<<<<<<`)
+      console.log(`From map: `)
+      const primary = user.primary
+      const secondaries = user.secondaries
+
+      // Append replica_set value
+      user.replica_set = [primary].concat(secondaries)
+
+      // Check if primary spID exists in nodeList, if not add to object
+      // Exclude own spID
+      if (!nodeList[primary] && primary !== spInfo.spID) {
+        nodeList[primary] = true
+      }
+      if (!sharedRsets[primary]) {
+        sharedRsets[primary] = []
+      }
+      sharedRsets[primary].push(user)
+
+      // Check if secondary spIDs exist in nodeList, if not add to object
+      for (const secondary of secondaries) {
+        console.log(`${user.wallet} - replicaID ${secondary} processing`)
+        if (secondary === spInfo.spID) {
+          console.log(`${user.wallet} - replicaID ${secondary} excluding ${secondary}`)
+          continue
+        }
+        if (!nodeList[secondary]) {
+          nodeList[secondary] = true
+        }
+        if (!sharedRsets[secondary]) {
+          // TODO: Evaluate thread safety?
+          sharedRsets[secondary] = []
+        }
+        sharedRsets[secondary].push(user)
+      } 
+    }))
+    // At this point, nodeList and sharedRsets are ready for further processing
+    nodeList = Object.keys(nodeList)
+    console.log('----')
+    console.log(nodeList)
+    console.log('----')
+    console.log(sharedRsets)
+    console.log(`<<<<<<`)
     logger.info('------------------END Process state machine operation------------------')
   }
 
+  // Initialize the creator node state machine
   async init () {
     await this.stateMachineQueue.empty()
 
