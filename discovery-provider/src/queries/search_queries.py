1f3270ca47ed024e1a26b25b1a2872012ddd6d20
Joseph Lee <joeylee0925@gmail.com>
2020-08-14 18:53:59 -0400

    Updated DP logger to be JSON for easier parsing (#752)
    * Updated DP logged to be JSON for easier parsing
    
    * Fix dp lint errors
    
    * Add timestamp to DP req log
    
    * Modify JSON logger to include timestamp
    
    * Fix lint

--- discovery-provider/src/queries/search_queries.py
+++ discovery-provider/src/queries/search_queries.py
@@ -295,7 +295,6 @@ def search_tags():
 
 def add_users(session, results):
     user_id_list = get_users_ids(results)
-    logger.warning(user_id_list)
     users = get_users_by_id(session, user_id_list)
     for result in results:
         user_id = None
