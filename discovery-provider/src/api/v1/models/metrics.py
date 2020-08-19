09223b1027b02b614d6b39fe799f563d555cf59c
Joseph Lee <joeylee0925@gmail.com>
2020-08-19 12:49:20 -0400

    Update DP response codes and metrics route (#755)
    * Update DP response codes and metrics route
    
    * Cleans up get_route_metrics_test to be more clear
    
    * Fix test names for get_route_metrics_test

--- discovery-provider/src/api/v1/models/metrics.py
+++ discovery-provider/src/api/v1/models/metrics.py
@@ -2,7 +2,6 @@ from flask_restx import fields
 from .common import ns
 
 route_metric = ns.model('route_metric', {
-    "route": fields.String,
     "timestamp": fields.String,
     "count": fields.Integer
 })
