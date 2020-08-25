831d21368e824fbbe6dbf6292fc0d9705a068e74
Raymond Jacobson <ray@audius.co>
2020-08-24 20:15:16 -0700

    Fix lint

--- creator-node/src/utils/requestRange.test.js
+++ creator-node/src/utils/requestRange.test.js
@@ -2,37 +2,37 @@ const assert = require('assert')
 const { getRequestRange } = require('./requestRange')
 
 describe('Test getRequestRange', () => {
-  it ('Should calculate start and end', () => {
+  it('Should calculate start and end', () => {
     const req = {
       header: () => 'bytes=1024-2048'
     }
     const { start, end } = getRequestRange(req)
-    assert.equal(start, 1024)
-    assert.equal(end, 2048)
+    assert.strictEqual(start, 1024)
+    assert.strictEqual(end, 2048)
   })
 
-  it ('Should calculate start with no end', () => {
+  it('Should calculate start with no end', () => {
     const req = {
       header: () => 'bytes=1024-'
     }
     const { start, end } = getRequestRange(req)
-    assert.equal(start, 1024)
-    assert.equal(end, undefined)
+    assert.strictEqual(start, 1024)
+    assert.strictEqual(end, undefined)
   })
 
-  it ('Should error at malformatted range', () => {
+  it('Should error at malformatted range', () => {
     const req = {
       header: () => ''
     }
     const range = getRequestRange(req)
-    assert.equal(range, -1)
+    assert.strictEqual(range, null)
   })
 
-  it ('Should error at non integer ranges', () => {
+  it('Should error at non integer ranges', () => {
     const req = {
       header: () => 'bytes=abc-def'
     }
     const range = getRequestRange(req)
-    assert.equal(range, -1)
+    assert.strictEqual(range, null)
   })
 })
