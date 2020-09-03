78c56ab8db7fdf9a51752423034f9e5a25dce9a5
vicky :) <60366641+vicky-g@users.noreply.github.com>
2020-09-02 17:17:00 -0700

    add metrics to every route in queries, search, and trending (#798)
    * add metrics to every route in queries.py
    
    * fix spacing
    
    * add metrics to trending and search

--- discovery-provider/src/queries/trending.py
+++ discovery-provider/src/queries/trending.py
@@ -11,6 +11,8 @@ from src.queries.query_helpers import get_pagination_vars
 from src.tasks.generate_trending import generate_trending, trending_cache_hits_key, \
     trending_cache_miss_key, trending_cache_total_key
 
+from src.utils.redis_metrics import record_metrics
+
 logger = logging.getLogger(__name__)
 bp = Blueprint("trending", __name__)
 
@@ -21,6 +23,7 @@ REDIS = redis.Redis.from_url(url=REDIS_URL)
 
 
 @bp.route("/trending/<time>", methods=("GET",))
+@record_metrics
 def trending(time):
     (limit, offset) = get_pagination_vars()
     # Increment total trending count
