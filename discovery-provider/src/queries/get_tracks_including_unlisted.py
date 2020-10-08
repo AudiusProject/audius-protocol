78296e3ee51f1e4774768806efc30235aeb77b19
Michael Piazza <michael.piazza.mp@gmail.com>
2020-10-07 17:40:32 -0700

    Add remixes and stems to API (#893)
    * Stems + Remixing + Remixes endpoints
    
    * Add caching
    
    * Cleanup
    
    * Revisions for client compatability
    
    * Lint fixes

--- discovery-provider/src/queries/get_tracks_including_unlisted.py
+++ discovery-provider/src/queries/get_tracks_including_unlisted.py
@@ -40,6 +40,7 @@ def get_tracks_including_unlisted(args):
     for i in identifiers:
         helpers.validate_arguments(i, ["handle", "id", "url_title"])
 
+    current_user_id = args.get("current_user_id")
     db = get_db_read_replica()
     with db.scoped_session() as session:
         def get_unpopulated_track():
@@ -80,19 +81,12 @@ def get_tracks_including_unlisted(args):
             def filter_fn(track):
                 input_track = identifiers_map[track["track_id"]]
                 route_id = helpers.create_track_route_id(input_track["url_title"],
-                                                        input_track["handle"])
+                                                         input_track["handle"])
 
                 return not track["is_unlisted"] or track["route_id"] == route_id
 
             tracks = list(filter(filter_fn, tracks))
 
-            if args.get("with_users", False):
-                user_id_list = get_users_ids(tracks)
-                users = get_users_by_id(session, user_id_list)
-                for track in tracks:
-                    user = users[track['owner_id']]
-                    if user:
-                        track['user'] = user
 
             track_ids = list(map(lambda track: track["track_id"], tracks))
             return (tracks, track_ids)
@@ -100,8 +94,15 @@ def get_tracks_including_unlisted(args):
         key = make_cache_key(args)
         (tracks, track_ids) = use_redis_cache(key, UNPOPULATED_TRACK_CACHE_DURATION_SEC, get_unpopulated_track)
 
+        # Add users
+        if args.get("with_users", False):
+            user_id_list = get_users_ids(tracks)
+            users = get_users_by_id(session, user_id_list, current_user_id)
+            for track in tracks:
+                user = users[track['owner_id']]
+                if user:
+                    track['user'] = user
         # Populate metadata
-        current_user_id = args.get("current_user_id")
         tracks = populate_track_metadata(
             session, track_ids, tracks, current_user_id)
 
