aff44d10c0d941a5a4829bf2a9463df5368b871c
Raymond Jacobson <ray@audius.co>
2020-10-12 15:38:18 -0700

    Use eventlet

--- discovery-provider/scripts/prod-server.sh
+++ discovery-provider/scripts/prod-server.sh
@@ -6,5 +6,5 @@
 #   "-b :5000" accept requests on port 5000
 #   "--access-logfile - --error-logfile" - log to stdout/stderr
 #   "src.wsgi:app" - app entry point in format: $(MODULE_NAME):$(VARIABLE_NAME)
-exec gunicorn -b :5000 --access-logfile - --error-logfile - src.wsgi:app --log-level=debug --workers 2 --threads 8
+exec gunicorn -b :5000 --access-logfile - --error-logfile - src.wsgi:app --log-level=debug --worker-class eventlet --workers 9
 
