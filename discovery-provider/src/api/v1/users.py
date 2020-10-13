ff4a976007a98c4af8874c8b46dcc2baf8c2f445
Raymond Jacobson <ray@audius.co>
2020-10-13 16:55:11 -0700

    Fix missing import (#929)

--- discovery-provider/src/api/v1/users.py
+++ discovery-provider/src/api/v1/users.py
@@ -14,8 +14,8 @@ from src.queries.get_followees_for_user import get_followees_for_user
 from src.queries.get_followers_for_user import get_followers_for_user
 
 from src.api.v1.helpers import abort_not_found, decode_with_abort, extend_activity, extend_favorite, extend_track, \
-    extend_user, format_limit, format_offset, make_response, search_parser, success_response, abort_bad_request_param, \
-    get_default_max, decode_string_id
+    extend_user, format_limit, format_offset, get_current_user_id, make_response, search_parser, success_response, abort_bad_request_param, \
+    get_default_max
 from .models.tracks import track, track_full
 from .models.activities import activity_model, activity_model_full
 from src.utils.redis_cache import cache
