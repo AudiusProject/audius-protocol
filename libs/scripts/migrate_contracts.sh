1e9425de29f8657ec1f5f84ca4b637510eb2f722
jowlee <joeylee0925@gmail.com>
2020-10-16 16:39:31 -0400

    Keep claim in migrate contracts script

--- libs/scripts/migrate_contracts.sh
+++ libs/scripts/migrate_contracts.sh
@@ -15,10 +15,12 @@ fi
 # COPY ETH CONTRACTS
 if [ -d "../eth-contracts/build/contracts" ]
 then
+  cp ./eth-contracts/ABIs/AudiusClaimDistributor.json .
   echo "Audius eth-contracts repo is present"
   cd ../eth-contracts/
   echo "Writing eth-contracts config files"
   node_modules/.bin/truffle exec scripts/migrate-contracts.js
+  mv ../libs/AudiusClaimDistributor.json ../libs/eth-contracts/ABIs/AudiusClaimDistributor.json
 else
   echo "INCORRECT REPOSITORY STRUCTURE. PLEASE FOLLOW README"
   exit 1
