ad755b349956446f250638189e0f95b57bc077e1
Raymond Jacobson <ray@audius.co>
2020-10-19 13:26:13 -0700

    Add eventlet support and gunicorn config vals (#951)

--- discovery-provider/scripts/prod-server.sh
+++ discovery-provider/scripts/prod-server.sh
@@ -6,5 +6,28 @@
 #   "-b :5000" accept requests on port 5000
 #   "--access-logfile - --error-logfile" - log to stdout/stderr
 #   "src.wsgi:app" - app entry point in format: $(MODULE_NAME):$(VARIABLE_NAME)
-exec gunicorn -b :5000 --access-logfile - --error-logfile - src.wsgi:app --log-level=debug --workers 2 --threads 8
 
+# Use specified number of workers if present
+if [[ -z "${audius_gunicorn_workers}" ]]
+then
+  WORKERS=2
+else
+  WORKERS="${audius_gunicorn_workers}"
+fi
+
+# Use specified number of threads if present (only used for "sync" workers)
+if [[ -z "${audius_gunicorn_threads}" ]]
+then
+  THREADS=8
+else
+  THREADS="${audius_gunicorn_threads}"
+fi
+
+# If a worker class is specified, use that. Otherwise, use sync workers.
+if [[ -z "${audius_gunicorn_worker_class}" ]]
+then
+  exec gunicorn -b :5000 --access-logfile - --error-logfile - src.wsgi:app --log-level=debug --workers=$WORKERS --threads=$THREADS
+else
+  WORKER_CLASS="${audius_gunicorn_worker_class}"
+  exec gunicorn -b :5000 --access-logfile - --error-logfile - src.wsgi:app --log-level=debug --worker-class=$WORKER_CLASS --workers=$WORKERS
+fi
