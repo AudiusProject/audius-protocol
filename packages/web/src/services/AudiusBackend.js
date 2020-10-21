330a476cb5b741b7ba5b7e49e6f643a0e55f7daa
Raymond Jacobson <ray@audius.co>
2020-10-20 18:26:20 -0700

    Fix updating website + donation for non creators (#68)

--- packages/web/src/services/AudiusBackend.js
+++ packages/web/src/services/AudiusBackend.js
@@ -1228,7 +1228,12 @@ class AudiusBackend {
         newMetadata.cover_photo_sizes = resp.dirCID
       }
 
-      if (newMetadata.twitter_handle || newMetadata.instagram_handle) {
+      if (
+        newMetadata.twitter_handle ||
+        newMetadata.instagram_handle ||
+        newMetadata.website ||
+        newMetadata.donation
+      ) {
         await fetch(`${IDENTITY_SERVICE}/social_handles`, {
           method: 'POST',
           headers: {
@@ -1237,7 +1242,9 @@ class AudiusBackend {
           body: JSON.stringify({
             handle: newMetadata.handle,
             twitterHandle: newMetadata.twitter_handle,
-            instagramHandle: newMetadata.instagram_handle
+            instagramHandle: newMetadata.instagram_handle,
+            website: newMetadata.website,
+            donation: newMetadata.donation
           })
         })
       }
