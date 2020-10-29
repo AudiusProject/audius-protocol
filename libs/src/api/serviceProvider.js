e0772b42a74c8817dcf7e74bd6a82290972083d2
vicky guan <vicky@audius.co>
2020-10-29 10:23:12 -0700

    add back import

--- libs/src/api/serviceProvider.js
+++ libs/src/api/serviceProvider.js
@@ -1,6 +1,6 @@
 const { sampleSize } = require('lodash')
 const { Base } = require('./base')
-const { timeRequestsAndSortByVersion } = require('../utils/network')
+const { timeRequests, timeRequestsAndSortByVersion } = require('../utils/network')
 
 const CREATOR_NODE_SERVICE_NAME = 'creator-node'
 const DISCOVERY_PROVIDER_SERVICE_NAME = 'discovery-provider'
