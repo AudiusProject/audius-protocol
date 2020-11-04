f37f6170a61b8a29aebee4b8942bd7dceef08047
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-11-04 17:59:51 -0500

    back to original order

--- creator-node/src/app.js
+++ creator-node/src/app.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 console.log('startup profiling - app.js - about to start requires')
+const { sendResponse, errorResponseServerError } = require('./apiHelpers')
+console.log('startup profiling - app.js - required apiHelpers', Math.floor((Date.now() - start) / 1000))
 const { logger, loggingMiddleware } = require('./logging')
 console.log('startup profiling - app.js - required logging', Math.floor((Date.now() - start) / 1000))
 const { userNodeMiddleware } = require('./userNodeMiddleware')
 console.log('startup profiling - app.js - required userNodeMiddleware', Math.floor((Date.now() - start) / 1000))
 const { readOnlyMiddleware } = require('./middlewares/readOnly/readOnlyMiddleware')
 console.log('startup profiling - app.js - required readOnlyMiddleware', Math.floor((Date.now() - start) / 1000))
-const { sendResponse, errorResponseServerError } = require('./apiHelpers')
-console.log('startup profiling - app.js - required apiHelpers', Math.floor((Date.now() - start) / 1000))
 const {
   userReqLimiter,
   trackReqLimiter,
@@ -24,10 +24,10 @@ const {
 console.log('startup profiling - app.js - required reqLimiter', Math.floor((Date.now() - start) / 1000))
 const config = require('./config')
 console.log('startup profiling - app.js - required config', Math.floor((Date.now() - start) / 1000))
-const contentBlacklistRoutes = require('./components/contentBlacklist/contentBlacklistController')
-console.log('startup profiling - app.js - required contentBlacklistController', Math.floor((Date.now() - start) / 1000))
 const healthCheckRoutes = require('./components/healthCheck/healthCheckController')
 console.log('startup profiling - app.js - required healthCheckController', Math.floor((Date.now() - start) / 1000))
+const contentBlacklistRoutes = require('./components/contentBlacklist/contentBlacklistController')
+console.log('startup profiling - app.js - required contentBlacklistController', Math.floor((Date.now() - start) / 1000))
 
 console.log('startup profiling - app.js - finished requires')
 
