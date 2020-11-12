53b0600647452261c922d2cd966b6bc292863f1d
Raymond Jacobson <ray@audius.co>
2020-11-11 17:31:10 -0800

    Add caching eth provider url endpoint (#117)
    * Add caching eth provider url endpoint
    
    * Wait for optimizely before libs init

--- apps/audius-client/packages/web/src/services/remote-config/defaults.ts
+++ apps/audius-client/packages/web/src/services/remote-config/defaults.ts
@@ -1,5 +1,7 @@
 import { IntKeys, StringKeys, DoubleKeys, BooleanKeys } from './RemoteConfig'
 
+const ETH_PROVIDER_URLS = process.env.REACT_APP_ETH_PROVIDER_URL || ''
+
 export const remoteConfigIntDefaults: { [key in IntKeys]: number | null } = {
   [IntKeys.IMAGE_QUICK_FETCH_TIMEOUT_MS]: 5000,
   [IntKeys.IMAGE_QUICK_FETCH_PERFORMANCE_BATCH_SIZE]: 20,
@@ -13,7 +15,8 @@ export const remoteConfigStringDefaults: {
 } = {
   [StringKeys.AUDIUS_LOGO_VARIANT]: null,
   [StringKeys.AUDIUS_LOGO_VARIANT_CLICK_TARGET]: null,
-  [StringKeys.APP_WIDE_NOTICE_TEXT]: null
+  [StringKeys.APP_WIDE_NOTICE_TEXT]: null,
+  [StringKeys.ETH_PROVIDER_URLS]: ETH_PROVIDER_URLS
 }
 export const remoteConfigDoubleDefaults: {
   [key in DoubleKeys]: number | null
