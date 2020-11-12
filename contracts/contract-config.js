bfdaa101e88c50253572f6a21e39e2288ae1b120
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-11-12 13:39:08 -0800

    Config addresses

--- contracts/contract-config.js
+++ contracts/contract-config.js
@@ -3,22 +3,32 @@
 module.exports = {
   development: {
     verifierAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0',
-    blacklisterAddress: null
+    blacklisterAddress: null,
+    nodeBootstrapAddress: null,
+    userReplicaSetBootstrapAddress: null
   },
   test_local: {
     verifierAddress: null,
-    blacklisterAddress: null
+    blacklisterAddress: null,
+    nodeBootstrapAddress: null,
+    userReplicaSetBootstrapAddress: null
   },
   audius_private: {
     verifierAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0',
-    blacklisterAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0'
+    blacklisterAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0',
+    nodeBootstrapAddress: null,
+    userReplicaSetBootstrapAddress: null
   },
   poa_mainnet: {
     verifierAddress: '0xbeef8E42e8B5964fDD2b7ca8efA0d9aef38AA996',
-    blacklisterAddress: '0xfeebEA99dE524ac668B6f151177EcA60b30A09c9'
+    blacklisterAddress: '0xfeebEA99dE524ac668B6f151177EcA60b30A09c9',
+    nodeBootstrapAddress: null,
+    userReplicaSetBootstrapAddress: null
   },
   poa_sokol: {
     verifierAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0',
-    blacklisterAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0'
+    blacklisterAddress: '0xbbbb93A6B3A1D6fDd27909729b95CCB0cc9002C0',
+    nodeBootstrapAddress: null,
+    userReplicaSetBootstrapAddress: null
   }
 }
