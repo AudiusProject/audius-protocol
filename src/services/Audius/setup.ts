561981ec05868a057b606dc6c11d68c5550f7120
Raymond Jacobson <ray@audius.co>
2020-11-18 15:44:03 -0800

    Fix metamask infinite reload bug (#13)

--- src/services/Audius/setup.ts
+++ src/services/Audius/setup.ts
@@ -34,6 +34,9 @@ export async function setup(this: AudiusClient): Promise<void> {
     this.isViewOnly = true
     this.libs = await configureReadOnlyLibs()
   } else {
+    // Turn off auto refresh (this causes infinite reload loops)
+    window.ethereum.autoRefreshOnNetworkChange = false
+
     // Metamask is installed
     window.web3 = new Web3(window.ethereum)
     try {
@@ -60,6 +63,7 @@ export async function setup(this: AudiusClient): Promise<void> {
         if (!this.libs) {
           this.libs = await configureReadOnlyLibs()
           this.isAccountMisconfigured = true
+          this.hasValidAccount = false
         }
       }
     } catch (err) {
