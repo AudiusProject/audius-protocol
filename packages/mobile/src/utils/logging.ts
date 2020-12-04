6acd17f6c3ff633b2d3bdd7a0e9eefe1d3298cc3
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-04 12:59:00 -0600

    Handle logs forwarded from WebApp (#21)

---
+++ packages/mobile/src/utils/logging.ts
@@ -0,0 +1,20 @@
+export type LOG_LEVEL = 'LOG' | 'WARNING' | 'DEBUG' | 'ERROR'
+
+const formatMessage = (message: string) => `WebApp: ${message}`
+
+export const handleWebAppLog = (level: LOG_LEVEL, message: string) => {
+  const formatted = formatMessage(message)
+  switch (level) {
+    case 'LOG':
+      console.log(formatted)
+      break
+    case 'WARNING':
+      console.warn(formatted)
+      break
+    case 'DEBUG':
+      console.debug(formatted)
+      break
+    case 'ERROR':
+      console.error(formatted)
+      break
+  }
