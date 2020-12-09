f2c0adbf334c735b1d8c9648b70e21b969321215
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-08 16:18:57 -0800

    fixes

--- discovery-provider/src/tasks/karma.py
+++ discovery-provider/src/tasks/karma.py
@@ -2,15 +2,10 @@ import logging
 import time
 from src.models import Track
 from src.tasks.celery_app import celery
-from src.tasks.generate_trending import generate_trending
-from src.queries.get_unpopulated_tracks import get_unpopulated_tracks
-from src.queries.get_trending_tracks import z, make_trending_cache_key
+from src.queries.get_trending_tracks import z, make_trending_cache_key, generate_unpopulated_trending
 from src.utils.redis_cache import pickle_and_set
 
 logger = logging.getLogger(__name__)
-
-# TODO: this limit gonna need to go higher
-TRENDING_LIMIT = 100
 
 def get_genres(session):
     """Returns all genres, in lowercase"""
@@ -21,31 +16,16 @@ def get_genres(session):
             Track.genre
         )).all()
     genres = filter(lambda x: x[0] is not None and x[0] != "", genres)
-    return list(map(lambda x: x[0].lower(), genres))
+    return list(map(lambda x: x[0], genres))
 
 time_ranges = ["week", "month", "year"]
-
-# TODO: make sure limit + offset are working correctly for this...
-def generate_unpopulated_trending(session, genre, time_range):
-    trending_tracks = generate_trending(
-        session, time_range, genre,
-        TRENDING_LIMIT, 0)
-
-    track_scores = [z(time_range, track) for track in trending_tracks['listen_counts']]
-    sorted_track_scores = sorted(track_scores, key=lambda k: k['score'], reverse=True)
-
-    track_ids = [track['track_id'] for track in sorted_track_scores]
-
-    tracks = get_unpopulated_tracks(session, track_ids)
-    return (tracks, track_ids)
 
 def update_karma(self, db, redis):
     logger.info('Running update karma')
     update_start = time.time()
     with db.scoped_session() as session:
         genres = get_genres(session)
-        # TODO: can just append this
-        genres.insert(0, None)
+        genres.append(None)
         for genre in genres:
             for time_range in time_ranges:
                 cache_start_time = time.time()
