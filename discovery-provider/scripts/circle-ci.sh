e20bae2853febde75a65b64f74f6b6ec94f0f3c9
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2020-12-09 17:37:25 -0800

    Disc prov CI fix

--- discovery-provider/scripts/circle-ci.sh
+++ discovery-provider/scripts/circle-ci.sh
@@ -6,11 +6,11 @@ function main {
   source venv/bin/activate
   cd_contracts_repo
   echo 'Migrating contracts'
-  node_modules/.bin/truffle migrate
+  node_modules/.bin/truffle migrate --network test_local
   echo 'Writing flask config'
   node_modules/.bin/truffle exec scripts/_contractsLocalSetup.js -run
   cd_discprov_repo
   pytest -s -v --fulltrace
 }
 
-main 
+main
