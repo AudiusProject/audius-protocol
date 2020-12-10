269d70e48d3abb1756a35410a074e6922349b8ee
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-10 13:34:36 -0800

    Handle notif reminder prompt (#23)

--- packages/mobile/src/utils/postMessage.ts
+++ packages/mobile/src/utils/postMessage.ts
@@ -1,10 +1,19 @@
-import { MessageType, Message } from "src/message"
+import { MessageType, Message } from "../message"
 
 export type MessageSender = {
   postMessage: (message: string) => void
 }
 
+const SPAMMY_MESSAGES = new Set([MessageType.GET_POSITION])
+
+// Stringifies the message, logs it, and sends it
 export const postMessage = (sender: MessageSender, message: Message) => {
   const stringified = JSON.stringify(message)
+
+  // Log it if it isn't spammy
+  if (!SPAMMY_MESSAGES.has(message.type)) {
+    console.debug(`Sending message to web client: ${stringified}`)
+  }
+
   sender.postMessage(stringified)
 }
