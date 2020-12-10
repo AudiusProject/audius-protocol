baf810c7475c2fb10e64b92be5967b647b81e6d3
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-10 13:39:31 -0800

    Handle notif reminder prompt (#138)
    * Handle notif reminder prompt
    
    * Pr revisions

--- audius-client/packages/web/src/services/native-mobile-interface/notifications.ts
+++ audius-client/packages/web/src/services/native-mobile-interface/notifications.ts
@@ -3,18 +3,24 @@ import { NativeMobileMessage } from './helpers'
 
 export class EnablePushNotificationsMessage extends NativeMobileMessage {
   constructor() {
-    super(MessageType.ENABLE_PUSH_NOTIFICATIONS, {})
+    super(MessageType.ENABLE_PUSH_NOTIFICATIONS)
   }
 }
 
 export class DisablePushNotificationsMessage extends NativeMobileMessage {
   constructor() {
-    super(MessageType.DISABLE_PUSH_NOTIFICATIONS, {})
+    super(MessageType.DISABLE_PUSH_NOTIFICATIONS)
   }
 }
 
 export class ResetNotificationsBadgeCount extends NativeMobileMessage {
   constructor() {
-    super(MessageType.RESET_NOTIFICATIONS_BADGE_COUNT, {})
+    super(MessageType.RESET_NOTIFICATIONS_BADGE_COUNT)
+  }
+}
+
+export class PromptPushNotificationPermissions extends NativeMobileMessage {
+  constructor() {
+    super(MessageType.PROMPT_PUSH_NOTIFICATION_REMINDER)
   }
 }
