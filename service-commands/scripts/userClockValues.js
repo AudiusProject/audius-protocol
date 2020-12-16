864a6e8fddacb0a2e5881eb38cb2fedbb97ca664
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2020-12-16 02:01:48 -0500

    Add a script to get clock values from user handle

---
+++ service-commands/scripts/userClockValues.js
@@ -0,0 +1,58 @@
+/**
+ * Find the creator nodes associated with a user and their respective clock values.
+ *
+ * Script usage: node userClockValues.js <size> <outFile>
+ */
+const axios = require('axios')
+
+const discoveryProvider = 'https://discoveryprovider.audius.co'
+
+async function run() {
+  try {
+    const { handle } = parseArgs()
+
+    const { wallet, creator_node_endpoint: creatorNodeEndpoint } = (
+      await axios({
+        url: `/v1/full/users/handle/${handle}`,
+        method: 'get',
+        baseURL: discoveryProvider
+      })
+    ).data.data[0]
+
+    const creatorNodes = creatorNodeEndpoint
+      ? creatorNodeEndpoint.split(',')
+      : []
+
+    creatorNodes.forEach(async creatorNode => {
+      const { clockValue } = (
+        await axios({
+          url: `/users/clock_status/${wallet}`,
+          method: 'get',
+          baseURL: creatorNode
+        })
+      ).data
+
+      console.log(creatorNode, clockValue)
+    })
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
+/**
+ * Process command line args, expects user handle as command line input.
+ */
+function parseArgs() {
+  const args = process.argv.slice(2)
+  const handle = args[0]
+
+  // check appropriate CLI usage
+  if (!handle) {
+    const errorMessage = `Incorrect script usage for input handle (${handle})\nPlease follow the structure 'node userClockValues.js <handle>'`
+    throw new Error(errorMessage)
+  }
+
+  return { handle }
+}
+
+run()
