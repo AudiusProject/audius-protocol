4484ee1bc9aaffc2a34d9d7a557f40cff4606f3c
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-15 18:18:18 -0800

    [AUD-13] Fix profile completion meter (#141)

--- apps/audius-client/packages/web/src/store/cache/tracks/utils/helpers.ts
+++ apps/audius-client/packages/web/src/store/cache/tracks/utils/helpers.ts
@@ -1,19 +1,26 @@
 import { TrackMetadata } from 'models/Track'
 import User from 'models/User'
 import { Kind } from 'store/types'
-import { put } from 'redux-saga/effects'
+import { put, select } from 'redux-saga/effects'
 import * as cacheActions from 'store/cache/actions'
 import { reformat as reformatUser } from 'store/cache/users/utils'
 import { makeUid } from 'utils/uid'
+import { getAccountUser } from 'store/account/selectors'
+import { uniqBy } from 'lodash'
 
 /**
  * Adds users from track metadata to cache.
+ * Dedupes and removes self.
  * @param metadataArray
  */
 export function* addUsersFromTracks<T extends TrackMetadata & { user?: User }>(
   metadataArray: T[]
 ) {
-  const users = metadataArray
+  const accountUser: ReturnType<typeof getAccountUser> = yield select(
+    getAccountUser
+  )
+  const currentUserId = accountUser?.user_id
+  let users = metadataArray
     .filter(m => m.user)
     .map(m => {
       const track = m as TrackMetadata & { user: User }
@@ -25,6 +32,11 @@ export function* addUsersFromTracks<T extends TrackMetadata & { user?: User }>(
     })
 
   if (!users.length) return
+
+  // Don't add duplicate users or self
+  users = uniqBy(users, 'id')
+  users = users.filter(user => !(currentUserId && user.id === currentUserId))
+
   yield put(
     cacheActions.add(Kind.USERS, users, /* replace */ false, /* persist */ true)
   )
