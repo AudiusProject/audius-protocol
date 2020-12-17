30a944002eb0ddc19ac2316ef3ca2a306f39f47e
Raymond Jacobson <ray@audius.co>
2020-12-16 20:09:00 -0800

    Bugfixes (#30)
    * Add loading version error to services table
    
    * Fix initial vote meter state
    
    * Fix registration min stake to reflect actual allowed value
    
    * Fix negative number logic check

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,14 +1,21 @@
 import Tooltip from 'components/Tooltip'
 import React from 'react'
+import clsx from 'clsx'
 import { ReactComponent as IconUhOh } from 'assets/img/uhOh.svg'
 
 import styles from './Error.module.css'
 
 const DEFAULT_ERROR_TEXT = 'Incomplete Data'
 
-const Error = ({ text = DEFAULT_ERROR_TEXT }: { text?: string }) => {
+const Error = ({
+  text = DEFAULT_ERROR_TEXT,
+  className
+}: {
+  text?: string
+  className?: string
+}) => {
   return (
-    <div className={styles.error}>
+    <div className={clsx(styles.error, className)}>
       <Tooltip text={text}>
         <IconUhOh className={styles.icon} />
       </Tooltip>
