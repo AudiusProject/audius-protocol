843a5e102cfb3087004a04de72bfb528791ec398
Michael Piazza <michael.piazza.mp@gmail.com>
2020-12-18 18:10:30 -0800

    [AUD-21] Add delegation Dashboard Chip (#40)
    * Add new delegator chip on SP page
    
    * Polish
    
    * Fix lint
    
    * PR fix

---
+++ src/components/DelegationStatsChip/DelegationStatsChip.tsx
@@ -0,0 +1,43 @@
+import InlineStat from 'components/InlineStat/InlineStat'
+import StatsChip from 'components/StatsChip/StatsChip'
+import React from 'react'
+import BN from 'bn.js'
+import { formatShortAud, formatWei } from 'utils/format'
+import { TICKER } from 'utils/consts'
+
+type DelegationStatsChipProps = {
+  deployerCut: number
+  delegated: BN
+  delegators: number
+  isLoading: boolean
+}
+
+const messages = {
+  deployerCut: 'Deployer Cut',
+  delegators: 'Delegators',
+  delegated: `Delegated ${TICKER}`
+}
+
+/**
+ * Shows stats about delegation. Lives on the SP page
+ */
+const DelegationStatsChip = ({
+  deployerCut,
+  delegated,
+  delegators,
+  isLoading
+}: DelegationStatsChipProps) => {
+  return (
+    <StatsChip
+      tooltipText={formatWei(delegated)}
+      primaryStat={formatShortAud(delegated)}
+      primaryStatName={messages.delegated}
+      isLoading={isLoading}
+    >
+      <InlineStat label={messages.deployerCut} value={`${deployerCut}%`} />
+      <InlineStat label={messages.delegators} value={delegators.toString()} />
+    </StatsChip>
+  )
+}
+
+export default DelegationStatsChip
