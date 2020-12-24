ba2f47bf2cb5e3a09a8861d51bfec0ae54194fc6
Raymond Jacobson <ray@audius.co>
2020-12-23 16:15:02 -0800

    Cast release date and use in sorting

--- discovery-provider/src/queries/get_tracks.py
+++ discovery-provider/src/queries/get_tracks.py
@@ -1,13 +1,77 @@
 import logging # pylint: disable=C0302
 
+from sqlalchemy import func 
+from sqlalchemy.sql.functions import coalesce
 from src.models import AggregatePlays, Track, User
 from src.utils import helpers
 from src.utils.db_session import get_db_read_replica
-from src.queries.query_helpers import paginate_query, parse_sort_param, \
+from src.queries.query_helpers import add_query_pagination, get_pagination_vars, paginate_query, parse_sort_param, \
   populate_track_metadata, get_users_ids, get_users_by_id
 from src.queries.get_unpopulated_tracks import get_unpopulated_tracks
 
 logger = logging.getLogger(__name__)
+
+def _get_tracks(session, args):
+    # Create initial query
+    base_query = session.query(Track)
+    base_query = base_query.filter(Track.is_current == True, Track.is_unlisted == False, Track.stem_of == None)
+
+    # Conditionally process an array of tracks
+    if "id" in args:
+        track_id_list = args.get("id")
+        try:
+            # Update query with track_id list
+            base_query = base_query.filter(Track.track_id.in_(track_id_list))
+        except ValueError as e:
+            logger.error("Invalid value found in track id list", exc_info=True)
+            raise e
+
+    # Allow filtering of tracks by a certain creator
+    if "user_id" in args:
+        user_id = args.get("user_id")
+        base_query = base_query.filter(
+            Track.owner_id == user_id
+        )
+
+    # Allow filtering of deletes
+    if "filter_deleted" in args:
+        filter_deleted = args.get("filter_deleted")
+        if filter_deleted:
+            base_query = base_query.filter(
+                Track.is_delete == False
+            )
+
+    if "min_block_number" in args:
+        min_block_number = args.get("min_block_number")
+        base_query = base_query.filter(
+            Track.blocknumber >= min_block_number
+        )
+
+    if "sort" in args:
+        if args["sort"] == "date":
+            base_query = base_query.order_by(
+                coalesce(
+                    func.to_date_safe(Track.release_date, 'Dy Mon DD YYYY HH24:MI:SS'),
+                    Track.created_at
+                ).desc(),
+                Track.track_id.desc()
+            )
+        elif args["sort"] == "plays":
+            base_query = base_query.join(
+                AggregatePlays,
+                AggregatePlays.play_item_id == Track.track_id
+            ).order_by(
+                AggregatePlays.count.desc()
+            )
+        else:
+            whitelist_params = ['created_at', 'create_date', 'release_date', 'blocknumber', 'track_id']
+            base_query = parse_sort_param(base_query, Track, whitelist_params)
+
+    query_results = add_query_pagination(base_query, args["limit"], args["offset"])
+    tracks = helpers.query_result_to_list(query_results.all())
+
+    return tracks
+
 
 def get_tracks(args):
     """
@@ -44,56 +108,11 @@ def get_tracks(args):
                 track_ids = list(map(lambda track: track["track_id"], tracks))
                 return (tracks, track_ids)
 
-            # Create initial query
-            base_query = session.query(Track)
-            base_query = base_query.filter(Track.is_current == True, Track.is_unlisted == False, Track.stem_of == None)
+            (limit, offset) = get_pagination_vars()
+            args["limit"] = limit
+            args["offset"] = offset
 
-            # Conditionally process an array of tracks
-            if "id" in args:
-                track_id_list = args.get("id")
-                try:
-                    # Update query with track_id list
-                    base_query = base_query.filter(Track.track_id.in_(track_id_list))
-                except ValueError as e:
-                    logger.error("Invalid value found in track id list", exc_info=True)
-                    raise e
-
-            # Allow filtering of tracks by a certain creator
-            if "user_id" in args:
-                user_id = args.get("user_id")
-                base_query = base_query.filter(
-                    Track.owner_id == user_id
-                )
-
-            # Allow filtering of deletes
-            if "filter_deleted" in args:
-                filter_deleted = args.get("filter_deleted")
-                if filter_deleted:
-                    base_query = base_query.filter(
-                        Track.is_delete == False
-                    )
-
-            if "min_block_number" in args:
-                min_block_number = args.get("min_block_number")
-                base_query = base_query.filter(
-                    Track.blocknumber >= min_block_number
-                )
-
-            if "sort" in args:
-                if args["sort"] == "date":
-                    base_query = base_query.order_by(Track.created_at.desc(), Track.track_id.desc())
-                elif args["sort"] == "plays":
-                    base_query = base_query.join(
-                        AggregatePlays,
-                        AggregatePlays.play_item_id == Track.track_id
-                    ).order_by(
-                        AggregatePlays.count.desc()
-                    )
-                else:
-                    whitelist_params = ['created_at', 'create_date', 'release_date', 'blocknumber', 'track_id']
-                    base_query = parse_sort_param(base_query, Track, whitelist_params)
-            query_results = paginate_query(base_query).all()
-            tracks = helpers.query_result_to_list(query_results)
+            tracks = _get_tracks(session, args)
 
             track_ids = list(map(lambda track: track["track_id"], tracks))
 
