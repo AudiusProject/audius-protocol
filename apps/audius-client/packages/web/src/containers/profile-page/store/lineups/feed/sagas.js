bc3f34e63790cc051f20aec90e63a0195783ecdc
Michael Piazza <michael.piazza.mp@gmail.com>
2021-01-07 18:37:42 -0800

    Fix unconfirmed reposted tracks not appearing on profile (#180)

--- apps/audius-client/packages/web/src/containers/profile-page/store/lineups/feed/sagas.js
+++ apps/audius-client/packages/web/src/containers/profile-page/store/lineups/feed/sagas.js
@@ -12,17 +12,31 @@ import {
 import { LineupSagas } from 'store/lineup/sagas'
 import { getUserId } from 'store/account/selectors'
 import { retrieveUserReposts } from './retrieveUserReposts'
+import { getTracks } from 'store/cache/tracks/selectors'
 
 function* getReposts({ offset, limit, payload }) {
   const handle = yield select(getProfileUserHandle)
+  const profileId = yield select(getProfileUserId)
   const currentUserId = yield select(getUserId)
-
   const reposts = yield call(retrieveUserReposts, {
     handle,
     currentUserId,
     offset,
     limit
   })
+
+  // If we're on our own profile, add any
+  // tracks that haven't confirmed yet
+  if (profileId === currentUserId) {
+    const repostIds = new Set(reposts.map(r => r.track_id).filter(Boolean))
+    const tracks = yield select(getTracks)
+    for (const track of Object.values(tracks)) {
+      if (track.has_current_user_reposted && !repostIds.has(track.track_id)) {
+        reposts.push(track)
+      }
+    }
+  }
+
   return reposts
 }
 
