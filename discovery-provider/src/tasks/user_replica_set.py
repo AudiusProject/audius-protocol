15a92a102caf75ca291444292a095fab6fe822b5
hareeshnagaraj <hareesh.nagaraj@gmail.com>
2021-01-13 17:46:34 +0000

    L2 POA Content Node writes now functioning

--- discovery-provider/src/tasks/user_replica_set.py
+++ discovery-provider/src/tasks/user_replica_set.py
@@ -37,6 +37,10 @@ def user_replica_set_state_update(
     # NOTE - events are stored only for debugging purposes and not used or persisted anywhere
     user_replica_set_events_lookup = {}
 
+    # This stores the state of the cnode object along with all events applied
+    # Data format is {"cnode_id": {"cnode_record", "events":[]}}
+    cnode_events_lookup = {}
+
     for tx_receipt in user_replica_set_mgr_txs:
         for event_type in user_replica_set_manager_event_types_arr:
             user_events_tx = getattr(user_contract.events, event_type)().processReceipt(tx_receipt)
@@ -47,12 +51,21 @@ def user_replica_set_state_update(
                 user_id = args._userId if "_userId" in args else None
                 user_ids.add(user_id)
 
+                # Check if _newCnodeId is present
+                # If cnode id is found in event args, update local lookup object
+                cnode_id = args._newCnodeId if "_newCnodeId" in args else None
+
                 # if the user id is not in the lookup object, it hasn't been initialized yet
                 # first, get the user object from the db(if exists or create a new one)
                 # then set the lookup object for user_id with the appropriate props
                 if user_id and (user_id not in user_replica_set_events_lookup):
                     ret_user = lookup_user_record(update_task, session, entry, block_number, block_timestamp)
                     user_replica_set_events_lookup[user_id] = {"user": ret_user, "events": []}
+
+                # def lookup_poa_cnode_record(self, update_task, session, entry, block_number, block_timestamp):
+                if cnode_id and (cnode_id not in cnode_events_lookup):
+                    ret_cnode = lookup_poa_cnode_record(self, update_task, session, entry, block_number, block_timestamp)
+                    cnode_events_lookup[cnode_id] = {"content_node": ret_cnode, "events": []}
 
                 # Add or update the value of the user record for this block in user_replica_set_events_lookup,
                 # ensuring that multiple events for a single user result in only 1 row insert operation
@@ -75,7 +88,7 @@ def user_replica_set_state_update(
                     )
                     user_record.creator_node_endpoint = creator_node_endpoint_str 
                     user_replica_set_events_lookup[user_id]["user"] = user_record
-
+                    user_replica_set_events_lookup[user_id]["events"].append(event_type)
                 elif event_type == user_replica_set_manager_event_types_lookup['add_or_update_content_node']:
                     # TODO: Handle indexing creator node fields separately
                     # PROCESSING THIS IS PENDING BELOW PR:
@@ -85,22 +98,29 @@ def user_replica_set_state_update(
                     logger.error('add_or_update_content_node EVENT FOUND')
                     logger.error(f'{event_type}')
                     logger.error(args)
-                    # parse_poa_cnode_record(update_task, session, entry, block_number, timestamp):
-                    parse_poa_cnode_record(
+                    cnode_record = parse_poa_cnode_record(
                         self,
                         update_task,
                         session,
                         entry,
-                        block_number,
-                        block_timestamp)
+                        cnode_events_lookup[cnode_id]["content_node"]
+                    )
+                    if cnode_record is not None:
+                        cnode_events_lookup[cnode_id]["content_node"] = cnode_record
+                        cnode_events_lookup[cnode_id]["events"].append(event_type)
             num_total_changes += len(user_events_tx)
 
     # for each record in user_replica_set_events_lookup, invalidate the old record and add the new record
     # we do this after all processing has completed so the user record is atomic by block, not tx
     for user_id, value_obj in user_replica_set_events_lookup.items():
-        logger.info(f"user_replica_set.py | Adding {value_obj['user']}")
+        logger.info(f"user_replica_set.py | Replica Set Processing Adding {value_obj['user']}")
         invalidate_old_user(session, user_id)
         session.add(value_obj["user"])
+
+    for content_node_id, value_obj in cnode_events_lookup.items():
+        logger.info(f"user_replica_set.py | Content Node Processing Adding {value_obj['content_node']}")
+        invalidate_old_cnode_record(session, content_node_id)
+        session.add(value_obj["content_node"])
 
     return num_total_changes, user_ids
 
@@ -150,21 +170,27 @@ def get_endpoint_string_from_sp_ids(
         endpoint_string = "{},{}".format(endpoint_string, secondary_endpoint)
     return endpoint_string
 
-def parse_poa_cnode_record(self, update_task, session, entry, block_number, timestamp):
-    logger.error(f"parse_poa_cnode_record {block_number}")
-    lookup_poa_cnode_record(
-        self,
-        update_task,
-        session,
-        entry,
-        block_number,
-        timestamp)
-    return None
+def parse_poa_cnode_record(self, update_task, session, entry, cnode_record):
+    event_args = entry["args"]
+    delegate_owner_wallet = event_args._newCnodeDelegateOwnerWallet
+    sp_ids = event_args._proposerSpIds
+    proposer_1 = event_args._proposer1Address
+    proposer_2 = event_args._proposer2Address
+    proposer_3 = event_args._proposer3Address
+    cnode_record.delegate_owner_wallet = delegate_owner_wallet
+    cnode_record.proposer_1_address = proposer_1
+    cnode_record.proposer_2_address = proposer_2
+    cnode_record.proposer_3_address = proposer_3
+    cnode_record.proposer_sp_ids = sp_ids
+    return cnode_record
 
 def lookup_poa_cnode_record(self, update_task, session, entry, block_number, block_timestamp):
     event_blockhash = update_task.web3.toHex(entry.blockHash)
     event_args = entry["args"]
+
+    # Arguments from the event
     cnode_id = event_args._newCnodeId
+
     cnode_record_exists = session.query(POAContentNode).filter_by(cnode_id=cnode_id).count() > 0
     logger.error(f"lookup_poa_cnode_record | {cnode_id} record exists={cnode_record_exists}")
     logger.error(f"{event_args}")
@@ -187,3 +213,17 @@ def lookup_poa_cnode_record(self, update_task, session, entry, block_number, blo
         )
     # update these fields regardless of type
     cnode_record.blockhash = event_blockhash
+    # cnode_record.cnode_id = cnode_id
+    return cnode_record
+
+def invalidate_old_cnode_record(session, cnode_id):
+    cnode_record_exists = session.query(POAContentNode).filter_by(cnode_id=cnode_id).count() > 0
+    if cnode_record_exists:
+        num_invalidated_records = (
+            session.query(POAContentNode)
+            .filter(POAContentNode.cnode_id == cnode_id, POAContentNode.is_current == True)
+            .update({"is_current": False})
+        )
+        assert (
+            num_invalidated_records > 0
+        ), "Update operation requires a current cnode to be invalidated"
