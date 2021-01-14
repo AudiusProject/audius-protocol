9c5d2bf11e295c47ef73b3c6ef7b7411209eda26
Joseph Lee <joeylee0925@gmail.com>
2021-01-14 12:52:37 -0500

    Prevent a route switch if already on login/signup (#196)
    * Prevent a route switch if already on login/signup
    
    * Add comment to push route on account fetch failure

--- audius-client/packages/web/src/store/account/mobileSagas.ts
+++ audius-client/packages/web/src/store/account/mobileSagas.ts
@@ -1,7 +1,7 @@
 import { takeEvery, put } from 'redux-saga/effects'
 import * as accountActions from 'store/account/reducer'
 import { push as pushRoute } from 'connected-react-router'
-import { SIGN_UP_PAGE } from 'utils/route'
+import { SIGN_UP_PAGE, SIGN_IN_PAGE, doesMatchRoute } from 'utils/route'
 import { MessageType } from 'services/native-mobile-interface/types'
 import { setNeedsAccountRecovery } from './reducer'
 import { ReloadMessage } from 'services/native-mobile-interface/linking'
@@ -11,7 +11,11 @@ export const ENTROPY_KEY = 'hedgehog-entropy-key'
 
 function* watchFetchAccountFailed() {
   yield takeEvery(accountActions.fetchAccountFailed.type, function* () {
-    yield put(pushRoute(SIGN_UP_PAGE))
+    // Do not push route if the user is already on the signup or signin page
+    // or else it will toggle the UI page.
+    if (!doesMatchRoute(SIGN_IN_PAGE) && !doesMatchRoute(SIGN_UP_PAGE)) {
+      yield put(pushRoute(SIGN_UP_PAGE))
+    }
   })
 }
 
