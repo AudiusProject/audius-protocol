f666c722e7d578bed892c04d732bbad8c5c4a7fe
Raymond Jacobson <ray@audius.co>
2021-01-22 13:19:42 -0800

    [AUD-181] Add tiny embed variant (#205)
    * [AUD-181] Add tiny embed variant
    
    * Add tiny container
    
    * Clean up

--- packages/web/src/containers/embed-modal/components/EmbedFrame.tsx
+++ packages/web/src/containers/embed-modal/components/EmbedFrame.tsx
@@ -4,15 +4,24 @@ import styles from './EmbedFrame.module.css'
 
 type EmbedFrameProps = {
   frameString: string
+  // Optional container width if provided
+  width?: number
 }
 
-const EmbedFrame = ({ frameString }: EmbedFrameProps) => {
+const EmbedFrame = ({ frameString, width }: EmbedFrameProps) => {
   if (!frameString) return null
-  return (
+  const frame = (
     <div
       className={styles.frame}
       dangerouslySetInnerHTML={{ __html: frameString }}
     />
+  )
+  return width ? (
+    <div className={styles.wrapper} style={{ width: `${width}px` }}>
+      {frame}
+    </div>
+  ) : (
+    frame
   )
 }
 
