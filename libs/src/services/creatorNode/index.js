4ce381e801efe0240c80e50a6cd1fa8274bc90c0
vicky guan <vicky@audius.co>
2021-01-28 12:43:31 -0800

    update err msg

--- libs/src/services/creatorNode/index.js
+++ libs/src/services/creatorNode/index.js
@@ -624,8 +624,8 @@ function _handleErrorHelper (e, requestUrl) {
     // delete headers, may contain tokens
     if (e.config && e.config.headers) delete e.config.headers
 
-    console.error(`Network error while making request to ${requestUrl}:\nStringified Error:${JSON.stringify(e)}\nError: ${e}`)
-    throw new Error(`Network error while making request to ${requestUrl}:\nStringified Error:${JSON.stringify(e)}\nError: ${e}`)
+    console.error(`Network error while making request to ${requestUrl}:\nStringified Error:${JSON.stringify(e)}\nError:`, e)
+    throw new Error(`Network error while making request to ${requestUrl}:\nStringified Error:${JSON.stringify(e)}\nError:`, e)
   } else {
     throw e
   }
