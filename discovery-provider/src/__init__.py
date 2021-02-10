5dcc71f94afa054669311fa9b15866d99963fd32
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-02-10 17:32:47 -0500

    here

--- discovery-provider/src/__init__.py
+++ discovery-provider/src/__init__.py
@@ -29,6 +29,7 @@ from src.utils.config import config_files, shared_config, ConfigIni
 from src.utils.ipfs_lib import IPFSClient
 from src.tasks import celery_app
 
+print('hello')
 # these global vars will be set in create_celery function
 web3endpoint = None
 web3 = None
