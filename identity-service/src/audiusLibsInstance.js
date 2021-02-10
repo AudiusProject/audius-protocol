557d63b7847b68f3307a345df1ec1e068ba52414
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-02-09 22:30:08 -0500

    Move code inside function

--- identity-service/src/audiusLibsInstance.js
+++ identity-service/src/audiusLibsInstance.js
@@ -3,43 +3,38 @@ const AudiusLibs = require('@audius/libs')
 const config = require('./config')
 const registryAddress = config.get('registryAddress')
 const web3ProviderUrl = config.get('web3Provider')
-
-async function initAudiusLibs () {
-  const dataWeb3 = await AudiusLibs.Utils.configureWeb3(web3ProviderUrl, null, false)
-  if (!dataWeb3) throw new Error('Web3 incorrectly configured')
-
-  let audiusInstance = new AudiusLibs({
-    discoveryProviderConfig: AudiusLibs.configDiscoveryProvider(),
-    ethWeb3Config: AudiusLibs.configEthWeb3(
-      config.get('ethTokenAddress'),
-      config.get('ethRegistryAddress'),
-      config.get('ethProviderUrl'),
-      config.get('ethOwnerWallet')
-    ),
-    web3Config: {
-      registryAddress,
-      useExternalWeb3: true,
-      externalWeb3Config: {
-        web3: dataWeb3,
-        // this is a stopgap since libs external web3 init requires an ownerWallet
-        // this is never actually used in the service's libs calls
-        ownerWallet: config.get('relayerPublicKey')
-      }
-    },
-    isServer: true
-  })
-
-  await audiusInstance.init()
-  return audiusInstance
-}
-
 class AudiusLibsWrapper {
   constructor () {
     this.audiusLibsInstance = null
   }
 
   async init () {
-    this.audiusLibsInstance = await initAudiusLibs()
+    const dataWeb3 = await AudiusLibs.Utils.configureWeb3(web3ProviderUrl, null, false)
+    if (!dataWeb3) throw new Error('Web3 incorrectly configured')
+
+    let audiusInstance = new AudiusLibs({
+      discoveryProviderConfig: AudiusLibs.configDiscoveryProvider(),
+      ethWeb3Config: AudiusLibs.configEthWeb3(
+        config.get('ethTokenAddress'),
+        config.get('ethRegistryAddress'),
+        config.get('ethProviderUrl'),
+        config.get('ethOwnerWallet')
+      ),
+      web3Config: {
+        registryAddress,
+        useExternalWeb3: true,
+        externalWeb3Config: {
+          web3: dataWeb3,
+          // this is a stopgap since libs external web3 init requires an ownerWallet
+          // this is never actually used in the service's libs calls
+          ownerWallet: config.get('relayerPublicKey')
+        }
+      },
+      isServer: true
+    })
+
+    await audiusInstance.init()
+    this.audiusLibsInstance = audiusInstance
   }
 
   get audiusLibs () {
