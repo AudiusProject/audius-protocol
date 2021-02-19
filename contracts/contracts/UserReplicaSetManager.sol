d1797c566be2c11c6d977b0f8e39fc7cada3dbcd
hareeshnagaraj <hareesh.nagaraj@gmail.com>
2021-02-19 17:36:38 +0000

    BS last minute NITs 3rd time

--- contracts/contracts/UserReplicaSetManager.sol
+++ contracts/contracts/UserReplicaSetManager.sol
@@ -159,7 +159,7 @@ contract UserReplicaSetManager is SigningLogicInitializable, RegistryContract {
     {
         _requireSeed();
         // For every entry in spIds/Nonces/Sigs
-        //  Recover signer using the signature for inner function (tmp is addOrUpdateCreatorNode)
+        //  Recover signer using the signature for inner function
         //  Confirm that the spId <-> recoveredSigner DOES exist and match what is stored on chain
         address proposer1DelegateOwnerWallet = _recoverProposeAddOrUpdateContentNodeSignerAddress(
             _cnodeSpId,
@@ -193,15 +193,15 @@ contract UserReplicaSetManager is SigningLogicInitializable, RegistryContract {
         );
         require(
             spIdToContentNodeWallets[_proposerSpIds[0]].delegateOwnerWallet == proposer1DelegateOwnerWallet,
-            "Invalid wallet provided for 1st proposer"
+            "Invalid delegateOwnerWallet provided for 1st proposer"
         );
         require(
             spIdToContentNodeWallets[_proposerSpIds[1]].delegateOwnerWallet == proposer2DelegateOwnerWallet,
-            "Invalid wallet provided for 2nd proposer"
+            "Invalid delegateOwnerWallet provided for 2nd proposer"
         );
         require(
             spIdToContentNodeWallets[_proposerSpIds[2]].delegateOwnerWallet == proposer3DelegateOwnerWallet,
-            "Invalid wallet provided for 3rd proposer"
+            "Invalid delegateOwnerWallet provided for 3rd proposer"
         );
 
         // Require distinct ownerWallet for each proposer
