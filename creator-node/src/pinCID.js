d8bed2f91e900b188250fd9e152b6600d815253e
jowlee <joeylee0925@gmail.com>
2021-02-25 11:02:29 -0500

    Add pin timeout

--- creator-node/src/pinCID.js
+++ creator-node/src/pinCID.js
@@ -3,12 +3,16 @@ const { CID } = require('ipfs-http-client-latest')
 const config = require('./config')
 const { logger } = require('./logging')
 
+// 30 Second ipfs timeout
+const IPFS_PIN_TIMEOUT = 30 /* sec */ * 1000 /* ms */
+
 /**
  * Pin CIDs from config
- * @param {*} ipfs 
+ * @param {*} ipfs
  */
 const pinCID = async (ipfs) => {
   try {
+    logger.info(`Starting Pin CID`)
     const addCIDs = config.get('pinAddCIDs')
       .split(',')
       .filter(cid => cid !== '')
@@ -16,10 +20,10 @@ const pinCID = async (ipfs) => {
 
     for (const cid of addCIDs) {
       try {
-        await ipfs.pin.add(cid, { recursive: true })
+        await ipfs.pin.add(cid, { recursive: true, timeout: IPFS_PIN_TIMEOUT })
         logger.info(`Pin CID: ${cid.toString()}`)
       } catch (err) {
-        logger.error(`Unable to pin CID: ${cid.toString()}`)
+        logger.error(`Unable to pin CID: ${cid.toString()} with err: ${err.message}`)
       }
     }
 
@@ -30,13 +34,14 @@ const pinCID = async (ipfs) => {
 
     for (const cid of removeCIDs) {
       try {
-        await ipfs.pin.rm(cid, { recursive: true })
+        await ipfs.pin.rm(cid, { recursive: true, timeout: IPFS_PIN_TIMEOUT })
         logger.info(`Remove pin CID: ${cid.toString()}`)
       } catch (err) {
-        logger.error(`Unable to remove pin CID: ${cid.toString()}`)
+        logger.error(`Unable to remove pin CID: ${cid.toString()} with err: ${err.message}`)
       }
     }
 
+    logger.info(`Finished Pin CID`)
   } catch (error) {
     logger.error(`Unable to run pinCID ${error.message}`)
   }
