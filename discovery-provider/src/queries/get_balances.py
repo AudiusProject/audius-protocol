d690ddc6242ff666e7969d694f88dafaa591f78d
Raymond Jacobson <ray@audius.co>
2021-02-24 19:50:01 -0800

    Update refresh balances

--- discovery-provider/src/queries/get_balances.py
+++ discovery-provider/src/queries/get_balances.py
@@ -5,8 +5,8 @@ from src.models import UserBalance
 
 logger = logging.getLogger(__name__)
 
-# How stale of current_user balance we will tolerate before refreshing.
-BALANCE_REFRESH_SEC_CURRENT_USER = 3 * 60
+# How stale of current_user or verified user balance we will tolerate before refreshing.
+BALANCE_REFRESH_SEC_PRIORITY_USER = 60
 
 # How stale of a non-zero user balance we tolerate before refreshing
 BALANCE_REFRESH_SEC_NONEMPTY_USER = 15 * 60
@@ -16,7 +16,7 @@ BALANCE_REFRESH_SEC_EMPTY_USER = 1 * 60 * 60
 
 REDIS_PREFIX = "USER_BALANCE_REFRESH"
 
-def does_user_balance_need_refresh(user_balance, is_current_user=True):
+def does_user_balance_need_refresh(user_balance, is_priority_user=True):
     '''Returns whether a given user_balance needs update.
     Very heuristic-y:
         - If we've never updated before (new balance entry), update now
@@ -29,8 +29,8 @@ def does_user_balance_need_refresh(user_balance, is_current_user=True):
         return True
 
     threshold = None
-    if is_current_user:
-        threshold = BALANCE_REFRESH_SEC_CURRENT_USER
+    if is_priority_user:
+        threshold = BALANCE_REFRESH_SEC_PRIORITY_USER
     elif int(user_balance.balance) > 0:
         threshold = BALANCE_REFRESH_SEC_NONEMPTY_USER
     else:
@@ -51,7 +51,6 @@ def get_balances(session, redis, user_ids, is_verified_ids_set=set()):
        Returns mapping { user_id: balance }
        Enqueues in Redis user balances requiring refresh.
     """
-
     # Find user balances
     query = ((
         session.query(UserBalance)
@@ -65,7 +64,7 @@ def get_balances(session, redis, user_ids, is_verified_ids_set=set()):
     # Find user_ids that don't yet have a balance
     user_ids_set = set(user_ids)
     fetched_user_ids_set = {x.user_id for x in query}
-    needs_balance_set = user_ids_set - fetched_user_ids_set + is_verified_ids_set
+    needs_balance_set = user_ids_set - fetched_user_ids_set
 
     # Add new balances to result set
     no_balance_dict = {user_id: 0 for user_id in needs_balance_set}
@@ -80,6 +79,11 @@ def get_balances(session, redis, user_ids, is_verified_ids_set=set()):
                      if does_user_balance_need_refresh(user_balance, False)]
 
     # Enqueue new balances to Redis refresh queue
-    enqueue_balance_refresh(redis, list(needs_balance_set) + needs_refresh)
+    # 1. All users who need a new balance
+    # 2. All users who need a balance refresh
+    # 3. All verified users (priority)
+    enqueue_balance_refresh(
+        redis,
+        list(needs_balance_set.union(is_verified_ids_set)) + needs_refresh)
 
     return result
