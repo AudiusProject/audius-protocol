d99ec8b2ca261fd238803d6ee143104837a232d5
Raymond Jacobson <ray@audius.co>
2021-03-02 13:43:03 -0800

    Fix Android onPress targets (#50)
    * Fix Android onPress targets
    
    * Clean up

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -50,8 +50,13 @@ const App = () => {
 
   return <Provider store={store}>
     <GoogleCast webRef={webRef} />
-    <Notifications webRef={webRef} />
     <WebApp webRef={webRef} />
+    {/*
+      Note: it is very important that Notifications is rendered after WebApp.
+      On Android, regardless of position: absolute, WebApp will steal all of Notifications
+      touch targets and onPress will not work.
+    */}
+    <Notifications webRef={webRef} />
     <Audio webRef={webRef} />
     <OAuth webRef={webRef} />
     <Airplay webRef={webRef} />
