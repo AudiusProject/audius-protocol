9a29c46d5daa40919f329735d19794e8928976c2
Joseph Lee <joeylee0925@gmail.com>
2021-03-05 13:01:05 -0500

    Merge pull request #72 from AudiusProject/jowlee-updateBuild-script
    Update script to peer IPFS in CI

--- packages/protocol-dashboard/scripts/updateBuild.js
+++ packages/protocol-dashboard/scripts/updateBuild.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
+const AbortController = require('abort-controller')
 
 const args = process.argv
 console.log(process.argv)
@@ -30,15 +31,42 @@ const config = {
 
 const endpoint = config[env].gaEndpoint
 
+const CONTENT_NODE_PEER_TIMEOUT = 1000 /* ms */ * 30 /* sec */
+
 const updateContentNodePeers = async () => {
-  const res = await fetch(`${endpoint}/protocol_dashboard/peer_content_nodes`)
-  const response = await res.json()
-  console.log({ response })
-  if (!response.success) {
-    console.error('unable to update content node ipfs peers')
+  const contentNodesRes = await fetch(`${endpoint}/protocol_dashboard/content_nodes`)
+  const contentNodes = await contentNodesRes.json()
+  const ipfsRes = await fetch(`${endpoint}/protocol_dashboard/ipfs`)
+  const ipfsId = await ipfsRes.json()
+  const addr = ipfsId.addresses[0]
+  const connections = {}
+  for (let cn of contentNodes) {
+    const controller = new AbortController()
+    const timeout = setTimeout(() => controller.abort(), CONTENT_NODE_PEER_TIMEOUT)
+    try {
+      // make a req to each CN /ipfs_peer_info with url query caller_ipfs_id
+      const response = await fetch(
+        `${cn.endpoint}/ipfs_peer_info?caller_ipfs_id=${encodeURIComponent(addr)}`,
+        { signal: controller.signal }
+      )
+      const responseJson = await response.json()
+      if (responseJson.data && responseJson.data.id) {
+        connections[cn.endpoint] = true
+      } else {
+        connections[cn.endpoint] = false
+      }
+    } catch (error) {
+      connections[cn.endpoint] = false
+    } finally {
+      clearTimeout(timeout)
+    }
+  }
+  if (Object.values(connections).every((isConnected) => !isConnected)) {
+    console.error('unable to update peer with a single ipfs content node')
     process.exit(1)
   }
   console.log('Added ipfs peers')
+  console.log(JSON.stringify(connections, null, ' '))
 }
 
 const updateGABuild = async () => {
