d8f5f2fcd5442caf03ae5936c635f81692fce354
Raymond Jacobson <ray@audius.co>
2021-03-08 16:09:50 -0800

    Hotfix verif fix (#260)
    * Fix
    
    * Allow staging on hotfix
    
    * Remove log

---
+++ apps/audius-client/packages/web/src/containers/sign-on/store/verifiedChecker.ts
@@ -0,0 +1,33 @@
+import { Nullable } from 'utils/typeUtils'
+
+type TwitterUser = {
+  verified: boolean
+}
+
+type InstagramUser = {
+  is_verified: boolean
+}
+
+type HandleCheckStatus = 'twitterReserved' | 'instagramReserved' | 'notReserved'
+
+export const checkHandle = (
+  isOauthVerified: boolean,
+  lookedUpTwitterUser: Nullable<TwitterUser>,
+  lookedUpInstagramUser: Nullable<InstagramUser>
+): HandleCheckStatus => {
+  const isEquivalentTwitterHandleVerified =
+    lookedUpTwitterUser && lookedUpTwitterUser.verified
+
+  const isEquivalentInstagramHandleVerified =
+    lookedUpInstagramUser && lookedUpInstagramUser.is_verified
+
+  if (!isOauthVerified) {
+    if (isEquivalentTwitterHandleVerified) {
+      return 'twitterReserved'
+    }
+    if (isEquivalentInstagramHandleVerified) {
+      return 'instagramReserved'
+    }
+  }
+  return 'notReserved'
+}
