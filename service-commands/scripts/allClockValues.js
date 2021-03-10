563dad7500ab3f1f991e8faff62f483652ab8db2
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-03-10 10:01:26 -0500

    Batch requests to get CID propogation

--- service-commands/scripts/allClockValues.js
+++ service-commands/scripts/allClockValues.js
@@ -1,107 +1,127 @@
 const fs = require('fs')
 
-const _ = require('lodash')
+const { map } = require('lodash')
 const axios = require('axios')
 const retry = require('async-retry')
-async function getUsers(discoveryProvider, limit, offset, maxRetries = 5) {
-    return await retry(
-        async () => {
-            const response = await axios({
-                method: 'get',
-                url: '/users',
-                baseURL: discoveryProvider,
-                params: { limit, offset, is_creator: true }
-            })
-
-            return response.data.data
-        },
-        {
-            retries: maxRetries
-        }
+async function getUsers(discoveryProvider, offset, limit) {
+  return (
+    await retry(
+      () =>
+        axios({
+          method: 'get',
+          url: '/users',
+          baseURL: discoveryProvider,
+          params: { offset, limit, is_creator: true }
+        }),
+      { retries: 5 }
     )
+  ).data.data.map(user => ({
+    ...user,
+    creator_node_endpoint: user.creator_node_endpoint
+      ? user.creator_node_endpoint.split(',').filter(endpoint => endpoint)
+      : []
+  }))
 }
-async function batchGetClockStatus(
-    creatorNode,
-    walletPublicKeys,
-    maxRetries = 5
-) {
-    return await retry(
-        async () => {
-            const response = await axios({
-                method: 'post',
-                url: '/users/batch_clock_status',
-                baseURL: creatorNode,
-                data: { walletPublicKeys }
-            })
-
-            return response.data.users
-        },
-        {
-            retries: maxRetries
-        }
+async function getClockValues(creatorNode, walletPublicKeys) {
+  return (
+    await retry(
+      () =>
+        axios({
+          method: 'post',
+          url: '/users/batch_clock_status',
+          baseURL: creatorNode,
+          data: { walletPublicKeys }
+        }),
+      { retries: 5 }
     )
+  ).data.users
+}
+async function filterCids(creatorNode, cids) {
+  const result = await Promise.all(
+    cids.map(
+      async cid =>
+        await retry(
+          () =>
+            console.log(creatorNode, cids) ||
+            axios({
+              method: 'head',
+              url: `/ipfs/${cid}`,
+              baseURL: creatorNode,
+              validateStatus: status => status === 200 || status === 404
+            }),
+          { retries: 5 }
+        )
+    )
+  )
+
+  console.log('done', creatorNode, cids)
+
+  return cids.filter((_, idx) => result[idx].status === 200)
 }
 async function run() {
-    let output = []
+  const discoveryProvider = 'https://discoveryprovider.audius.co/'
+  const batchSize = 50
 
-    prevBatch = true
-    for (let offset = 0; prevBatch; offset += 500) {
-        console.time(`offset ${offset}`)
-
-        batch = await getUsers('https://discoveryprovider.audius.co/', 500, offset)
-        prevBatch = batch.length != 0
-
-        batch = batch.filter(({ creator_node_endpoint }) => creator_node_endpoint)
-
-        const wallets = {}
-        batch.forEach(async ({ creator_node_endpoint, wallet }) => {
-            creator_node_endpoint
-                .split(',')
-                .filter(el => el)
-                .forEach(creatorNode => {
-                    wallets[creatorNode] = wallets[creatorNode] || []
-                    wallets[creatorNode].push(wallet)
-                })
-        })
-
-        const clockValues = {}
-        await Promise.all(
-            Object.keys(wallets).map(async creatorNode => {
-                clockValues[creatorNode] = _.chain(
-                    await batchGetClockStatus(creatorNode, wallets[creatorNode])
-                )
-                    .keyBy('walletPublicKey')
-                    .mapValues('clock')
-                    .value()
-            })
-        )
-
-        const batchOutput = await Promise.all(
-            batch.map(async ({ creator_node_endpoint, wallet, handle, user_id }) => ({
-                wallet,
-                handle,
-                user_id,
-                clockValues: creator_node_endpoint
-                    .split(',')
-                    .filter(el => el)
-                    .map((creatorNode, idx) => ({
-                        creatorNode,
-                        clock: clockValues[creatorNode][wallet],
-                        primary: idx === 0
-                    }))
-            }))
-        )
-
-        output = output.concat(batchOutput)
-
-        console.timeEnd(`offset ${offset}`)
+  const tracks = {}
+  JSON.parse(fs.readFileSync('tracks.json')).forEach(
+    ({ owner_id, metadata_multihash }) => {
+      tracks[owner_id] = tracks[owner_id] || []
+      tracks[owner_id].push(metadata_multihash)
     }
+  )
 
-    output = _.sortBy(output, ['user_id'])
+  const prevBatch = true
+  for (let offset = 0; prevBatch; offset += batchSize) {
+    console.time(`${offset} - ${offset + batchSize}`)
 
-    fs.writeFileSync('data.json', JSON.stringify(output, null, 4))
+    const batch = await getUsers(discoveryProvider, offset, batchSize)
+    const prevBatch = batch.length !== 0
 
-    process.exit(0)
+    const wallets = {}
+    batch.forEach(({ creator_node_endpoint, wallet }) => {
+      creator_node_endpoint.forEach(endpoint => {
+        wallets[endpoint] = wallets[endpoint] || []
+        wallets[endpoint].push(wallet)
+      })
+    })
+
+    const clockValues = {}
+    await Promise.all(
+      map(wallets, async (walletPublicKeys, creatorNode) => {
+        clockValues[creatorNode] = {}
+        ;(await getClockValues(creatorNode, walletPublicKeys)).forEach(
+          ({ walletPublicKey, clock }) => {
+            clockValues[creatorNode][walletPublicKey] = clock
+          }
+        )
+      })
+    )
+
+    const data = await Promise.all(
+      map(
+        batch,
+        async ({ user_id, handle, wallet, creator_node_endpoint }) => ({
+          user_id,
+          handle,
+          wallet,
+          trackCids: tracks[user_id],
+          creatorNodes: await Promise.all(
+            creator_node_endpoint.map(async (endpoint, idx) => ({
+              endpoint,
+              clock: clockValues[endpoint][wallet],
+              trackCids: await filterCids(endpoint, tracks[user_id] || []),
+              primary: idx === 0
+            }))
+          )
+        })
+      )
+    )
+
+    fs.writeFileSync(`data.${offset}.json`, JSON.stringify(data, null, 4))
+
+    console.timeEnd(`${offset} - ${offset + batchSize}`)
+  }
+
+  process.exit(0)
 }
-
 run()
