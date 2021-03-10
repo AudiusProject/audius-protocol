8bb29160b91c2e6eb244b867429d002974c1cbd0
SidSethi <SidSethi93@gmail.com>
2021-03-10 15:10:40 +0000

    Fix CI

--- creator-node/src/serviceRegistry.js
+++ creator-node/src/serviceRegistry.js
@@ -128,7 +128,8 @@ class ServiceRegistry {
    */
   async _registerNodeOnL2URSM () {
     // Wait until URSM contract has been deployed (for backwards-compatibility)
-    let retryTimeoutMs = (this.nodeConfig.get('snapbackDevModeEnabled')) ? 10000 /** 10sec */ : 600000 /* 10min */
+    // let retryTimeoutMs = (this.nodeConfig.get('snapbackDevModeEnabled')) ? 10000 /** 10sec */ : 600000 /* 10min */
+    let retryTimeoutMs = 10000 /** 10sec */
     while (true) {
       this.logInfo(`Attempting to init UserReplicaSetManagerClient on ${retryTimeoutMs}ms interval...`)
       try {
