ce728732c927e3b5605cd8176db072b796e5f222
hareeshnagaraj <hareesh.nagaraj@gmail.com>
2021-03-12 19:58:18 +0000

    RM unused

--- creator-node/src/userNodeMiddleware.js
+++ creator-node/src/userNodeMiddleware.js
@@ -1,6 +1,3 @@
-const { sendResponse, errorResponseUnauthorized } = require('./apiHelpers')
-const config = require('./config')
-
 async function userNodeMiddleware (req, res, next) {
   // Disable all UM specific filtering
   return next()
