4076bd93d9254ead47ab18cb392f54db12f4bdef
Joseph Lee <joeylee0925@gmail.com>
2021-03-12 15:20:50 -0500

    Update schema and user balance for associated wallet  (#1299)
    * Update associated_wallet schema and update disc. user balance on wallet update
    
    * Update schema in libs for associated wallets
    
    * Update validate metadata in creator for assciated wallet
    
    * Fix CN test
    
    * Add another check to CN metadata upload
    
    * Lint discovery
    
    * Update user schema validator in discovery and libs

--- creator-node/src/utils/validateAudiusUserMetadata.js
+++ creator-node/src/utils/validateAudiusUserMetadata.js
@@ -8,10 +8,15 @@ const validateAssociatedWallets = (metadataJSON) => {
   if ('user_id' in metadataJSON && 'associated_wallets' in metadataJSON) {
     const userId = metadataJSON['user_id']
     const walletMappings = metadataJSON['associated_wallets']
-    if (typeof walletMappings !== 'object') return true
+    if (typeof walletMappings !== 'object' || walletMappings === null) return true
     const message = `AudiusUserID:${userId}`
     return Object.keys(walletMappings).every(wallet => {
-      const signature = walletMappings[wallet]
+      if (
+        typeof walletMappings[wallet] !== 'object' ||
+        walletMappings[wallet] === null ||
+        !('signature' in walletMappings[wallet])
+      ) return false
+      const signature = walletMappings[wallet].signature
       const recoveredWallet = web3.eth.accounts.recover(message, signature)
       return recoveredWallet === wallet
     })
@@ -21,7 +26,9 @@ const validateAssociatedWallets = (metadataJSON) => {
 
 const validateMetadata = (req, metadataJSON) => {
   // Check associated wallets
-  if (!validateAssociatedWallets(metadataJSON)) {
+  if (typeof metadataJSON !== 'object' || metadataJSON === null) {
+    return false
+  } else if (!validateAssociatedWallets(metadataJSON)) {
     req.logger.info('Associated Wallets do not match signatures')
     return false
   }
