8c7daa4bbb210c8997f325c7b30a9703ec65da5e
Michael Piazza <michael.piazza.mp@gmail.com>
2021-03-11 19:36:53 -0800

    Fix lint

--- discovery-provider/src/queries/get_playlist_tracks.py
+++ discovery-provider/src/queries/get_playlist_tracks.py
@@ -3,7 +3,6 @@ import sqlalchemy
 
 from src.models import Playlist, Track
 from src.utils import helpers
-from src.utils.db_session import get_db_read_replica
 from src.queries.query_helpers import populate_track_metadata, add_users_to_tracks
 
 logger = logging.getLogger(__name__)
