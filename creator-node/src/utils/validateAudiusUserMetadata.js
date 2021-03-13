52bc90f132a1e2ed092c98bbef797ebe9f69bbe9
Saliou Diallo <sddioulde@hotmail.com>
2021-03-12 21:37:42 -0500

    Multi wallet & collectibles (#1301)
    * Multi Wallet Support (#1296)
    
    * Validate CN for associated_wallest in user metadata
    
    * Add associated wallets to Discovery DB
    
    * Add assocaited wallet to discovery schema
    
    * Update discovery indexing for user associated wallets
    
    * Add discovery endpoints for wallet - id
    
    * Add wallets schema to libs user validation
    
    * Update schema and user balance for associated wallet  (#1299)
    
    * Update associated_wallet schema and update disc. user balance on wallet update
    
    * Update schema in libs for associated wallets
    
    * Update validate metadata in creator for assciated wallet
    
    * Fix CN test
    
    * Add another check to CN metadata upload
    
    * Lint discovery
    
    * Update user schema validator in discovery and libs
    
    * Feature collectibles metadata multihash (#1292)
    
    * Support collectibles in libs
    
    * Support metadata multihash for collectibles in discovery
    
    * Add has_collectibles indexed field to user object
    
    * Fix wallet object check
    
    * Clean up
    
    * Do not check metadata from ipfs every time
    
    * Fix alembic collisions
    
    * Handle the null cases in user metadata indexing
    
    * Forgo image size changes
    
    * Fix clean metadata for new fields
    
    Co-authored-by: Joseph Lee <joeylee0925@gmail.com>
    Co-authored-by: Raymond Jacobson <ray@audius.co>
    Co-authored-by: Saliou Diallo <saliou@audius.co>

---
+++ creator-node/src/utils/validateAudiusUserMetadata.js
@@ -0,0 +1,42 @@
+const Web3 = require('web3')
+const web3 = new Web3()
+
+/**
+ * Validates that the user id was signed by the associated wallets
+ */
+const validateAssociatedWallets = (metadataJSON) => {
+  if ('user_id' in metadataJSON && 'associated_wallets' in metadataJSON) {
+    const userId = metadataJSON['user_id']
+    const walletMappings = metadataJSON['associated_wallets']
+    if (!walletMappings) return true
+    if (typeof walletMappings !== 'object') return true
+    const message = `AudiusUserID:${userId}`
+    return Object.keys(walletMappings).every(wallet => {
+      if (
+        typeof walletMappings[wallet] !== 'object' ||
+        walletMappings[wallet] === null ||
+        !('signature' in walletMappings[wallet])
+      ) return false
+      const signature = walletMappings[wallet].signature
+      const recoveredWallet = web3.eth.accounts.recover(message, signature)
+      return recoveredWallet === wallet
+    })
+  }
+  return true
+}
+
+const validateMetadata = (req, metadataJSON) => {
+  // Check associated wallets
+  if (typeof metadataJSON !== 'object' || metadataJSON === null) {
+    return false
+  } else if (!validateAssociatedWallets(metadataJSON)) {
+    req.logger.info('Associated Wallets do not match signatures')
+    return false
+  }
+
+  // TODO: Add more validation checks
+  return true
+}
+
+module.exports = validateMetadata
+module.exports.validateAssociatedWallets = validateAssociatedWallets
--- libs/src/api/user.js
+++ libs/src/api/user.js
@@ -17,7 +17,9 @@ const USER_PROPS = [
   'cover_photo_sizes',
   'bio',
   'location',
-  'creator_node_endpoint'
+  'creator_node_endpoint',
+  'associated_wallets',
+  'collectibles'
 ]
 // User metadata fields that are required on the metadata object and only can have
 // non-null values
@@ -680,8 +682,15 @@ class Users extends Base {
     this.OBJECT_HAS_PROPS(metadata, USER_PROPS, USER_REQUIRED_PROPS)
   }
 
-  // Metadata object may have extra fields. Only keep core fields in USER_PROPS and 'user_id'.
+  /**
+   * Metadata object may have extra fields.
+   * - Add what user props might be missing to normalize
+   * - Only keep core fields in USER_PROPS and 'user_id'.
+   */
   _cleanUserMetadata (metadata) {
+    USER_PROPS.forEach(prop => {
+      if (!(prop in metadata)) { metadata[prop] = null }
+    })
     return pick(metadata, USER_PROPS.concat('user_id'))
   }
 
