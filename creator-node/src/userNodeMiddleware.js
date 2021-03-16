a67ae8f2686143ef92232382e8551344f1a5a9e8
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2021-03-15 21:06:07 -0700

    Revert "[AUD-417] Update UM mode for promotion (#1300)"
    This reverts commit 8356c0af05404e6fced25eb353ef376369599b25.

--- creator-node/src/userNodeMiddleware.js
+++ creator-node/src/userNodeMiddleware.js
@@ -1,6 +1,18 @@
+const { sendResponse, errorResponseUnauthorized } = require('./apiHelpers')
+const config = require('./config')
+
 async function userNodeMiddleware (req, res, next) {
-  // Disable all UM specific filtering
-  return next()
+  const isUserMetadataNode = config.get('isUserMetadataNode')
+  const userNodeRegex = new RegExp(/(users|version|db_check|health_check|image_upload|ipfs|export)/gm)
+  if (isUserMetadataNode) {
+    const isValidUrl = userNodeRegex.test(req.url)
+    if (!isValidUrl) {
+      return sendResponse(req, res, errorResponseUnauthorized('Invalid route for user metadata node'))
+    }
+    next()
+  } else {
+    next()
+  }
 }
 
 module.exports = { userNodeMiddleware }
