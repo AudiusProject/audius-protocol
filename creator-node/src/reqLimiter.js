ce1385af94225f2266d19f72f8fd21d7031a9041
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-03-18 23:51:32 -0400

    Add Req Limiter

--- creator-node/src/reqLimiter.js
+++ creator-node/src/reqLimiter.js
@@ -95,6 +95,16 @@ const URSMRequestForSignatureReqLimiter = rateLimit({
   keyGenerator: ipKeyGenerator
 })
 
+const batchCidsExistReqLimiter = rateLimit({
+  store: new RedisStore({
+    client: client,
+    prefix: 'batchCidsExistReqLimiter',
+    expiry: 60 * 60 // one hour in seconds
+  }),
+  max: config.get('batchCidsExistReqLimiter'), // max requests per hour
+  keyGenerator: ipKeyGenerator
+})
+
 const onLimitReached = (req, res, options) => {
   req.logger.warn(req.rateLimit, `Rate Limit Hit`)
 }
@@ -162,5 +172,6 @@ module.exports = {
   metadataReqLimiter,
   imageReqLimiter,
   URSMRequestForSignatureReqLimiter,
+  batchCidsExistReqLimiter,
   getRateLimiterMiddleware
 }
