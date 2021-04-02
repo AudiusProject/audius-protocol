18b22dab1f0feebec34ee169027e315bfda50388
Joseph Lee <joeylee0925@gmail.com>
2021-04-02 15:36:42 -0400

    Fix audio modal drawer styles (#332)

--- packages/web/src/containers/audio-rewards-page/WalletModal.tsx
+++ packages/web/src/containers/audio-rewards-page/WalletModal.tsx
@@ -309,7 +309,11 @@ const WalletModal = () => {
       contentHorizontalPadding={24}
       useGradientTitle={false}
     >
-      <div className={wm(styles.modalContainer)}>
+      <div
+        className={wm(styles.modalContainer, {
+          [styles.sendModalContainer]: modalState?.stage === 'SEND'
+        })}
+      >
         <ModalContent
           modalState={modalState}
           onInputSendData={onInputSendData}
