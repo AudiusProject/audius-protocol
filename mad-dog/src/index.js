0992b5ace76f4ae3eca57dd32c05e6220215090d
hareeshnagaraj <hareesh.nagaraj@gmail.com>
2021-04-05 21:47:22 +0000

    Minor update

--- mad-dog/src/index.js
+++ mad-dog/src/index.js
@@ -48,7 +48,7 @@ const services = [
   [Service.DISCOVERY_PROVIDER, SetupCommand.HEALTH_CHECK],
   [Service.USER_METADATA_NODE, SetupCommand.HEALTH_CHECK],
   [Service.IDENTITY_SERVICE, SetupCommand.HEALTH_CHECK],
-  // ...contentNodeHealthChecks
+  ...contentNodeHealthChecks
 ]
 
 async function setupAllServices () {
