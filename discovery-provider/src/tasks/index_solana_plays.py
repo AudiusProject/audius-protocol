f739a2d001bd4b8644798a8edc68d6fc6100d549
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-04-08 23:55:49 -0400

    Format file

--- discovery-provider/src/tasks/index_solana_plays.py
+++ discovery-provider/src/tasks/index_solana_plays.py
@@ -1,17 +1,16 @@
 import binascii
 import codecs
 import logging
-from datetime import datetime
 
 import base58
 from solana.rpc.api import Client
 from src.models import Play
 from src.tasks.celery_app import celery
-from src.utils.redis_cache import get_pickled_key, pickle_and_set
 from src.utils.config import shared_config
+from src.utils.redis_cache import get_pickled_key, pickle_and_set
 
 # TODO: These are configs
-AUDIUS_PROGRAM = shared_config['solana']['program_address']
+AUDIUS_PROGRAM = shared_config["solana"]["program_address"]
 SECP_PROGRAM = "KeccakSecp256k11111111111111111111111111111"
 SLEEP_TIME = 1
 SOLANA_ENDPOINT = "https://devnet.solana.com"
@@ -41,41 +40,40 @@ def process_solana_plays():
 
     # slot_from = http_client.get_slot()["result"]
     transaction = http_client.get_confirmed_signature_for_address2(
-        AUDIUS_PROGRAM, limit=1
-    )
+        AUDIUS_PROGRAM, limit=1)
     if transaction["result"][0]["slot"] > slot_from:
         slot_from = transaction["result"][0]["slot"]
         pickle_and_set(redis, SOL_PLAYS_REDIS_KEY, slot_from)
         tx_info = http_client.get_confirmed_transaction(
-            transaction["result"][0]["signature"]
-        )
-        if SECP_PROGRAM in tx_info["result"]["transaction"]["message"]["accountKeys"]:
+            transaction["result"][0]["signature"])
+        if SECP_PROGRAM in tx_info["result"]["transaction"]["message"][
+                "accountKeys"]:
             audius_program_index = tx_info["result"]["transaction"]["message"][
-                "accountKeys"
-            ].index(AUDIUS_PROGRAM)
+                "accountKeys"].index(AUDIUS_PROGRAM)
             for instruction in tx_info["result"]["transaction"]["message"][
-                "instructions"
-            ]:
+                    "instructions"]:
                 if instruction["programIdIndex"] == audius_program_index:
                     hex_data = binascii.hexlify(
-                        bytearray(list(base58.b58decode(instruction["data"])))
-                    )
+                        bytearray(list(base58.b58decode(instruction["data"]))))
 
                     l1 = int(hex_data[2:4], 16)
                     start_data1 = 10
                     end_data1 = l1 * 2 + start_data1
 
-                    l2 = int(hex_data[end_data1 : end_data1 + 2], 16)
+                    l2 = int(hex_data[end_data1:end_data1 + 2], 16)
                     start_data2 = end_data1 + 8
                     end_data2 = l2 * 2 + start_data2
 
-                    l3 = int(hex_data[end_data2 : end_data2 + 2], 16)
+                    l3 = int(hex_data[end_data2:end_data2 + 2], 16)
                     start_data3 = end_data2 + 8
                     end_data3 = l3 * 2 + start_data3
 
-                    user_id = codecs.decode(hex_data[start_data1:end_data1], 'hex')
-                    track_id = codecs.decode(hex_data[start_data2:end_data2], 'hex')
-                    source = codecs.decode(hex_data[start_data3:end_data3], 'hex')
+                    user_id = codecs.decode(hex_data[start_data1:end_data1],
+                                            "hex")
+                    track_id = codecs.decode(hex_data[start_data2:end_data2],
+                                             "hex")
+                    source = codecs.decode(hex_data[start_data3:end_data3],
+                                           "hex")
 
                     logger.error("---------")
                     logger.error("---------")
@@ -88,12 +86,13 @@ def process_solana_plays():
                     logger.error("---------")
                     logger.error("---------")
                     with db.scoped_session() as session:
-                        session.add(Play(
-                            user_id=int(user_id),
-                            play_item_id=int(track_id),
-                            source=str(source, 'utf-8')
-                        ))
-                    logger.error('COMMITTED PLAY')
+                        session.add(
+                            Play(
+                                user_id=int(user_id),
+                                play_item_id=int(track_id),
+                                source=str(source, "utf-8"),
+                            ))
+                    logger.error("COMMITTED PLAY")
                     logger.error("---------")
 
     # TODO: RECORD PLAY DATA
@@ -119,9 +118,11 @@ def index_solana_plays(self):
         if have_lock:
             process_solana_plays()
         else:
-            logger.info("index_solana_plays.py | Failed to acquire index_solana_plays")
+            logger.info(
+                "index_solana_plays.py | Failed to acquire index_solana_plays")
     except Exception as e:
-        logger.error("index_solana_plays.py | Fatal error in main loop", exc_info=True)
+        logger.error("index_solana_plays.py | Fatal error in main loop",
+                     exc_info=True)
         raise e
     finally:
         if have_lock:
