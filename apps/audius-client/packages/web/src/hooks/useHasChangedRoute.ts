e0871df515d3a667ad39221e40111176c0f0896e
Joseph Lee <joeylee0925@gmail.com>
2021-04-09 13:21:49 -0400

    Close modal for top level mobile pages (#347)
    * Close modal for top level mobile pages
    
    * Update use route change hook to useInstance var and added to hooks readme

---
+++ apps/audius-client/packages/web/src/hooks/useHasChangedRoute.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import useInstanceVar from 'hooks/useInstanceVar'
+
+/**
+ * Custom hook that fires a callback when the route changes
+ * Normally, you can use `useEffect` on the pathname from useLocation, but this
+ * hook ignores the first call of useEffect
+ *
+ * @param {function} onRouteChange the callback fired when the route changes
+ */
+const useHasChangedRoute = (onRouteChange: () => void) => {
+  const { pathname } = useLocation()
+  const [getCurrentRoute, setCurrentRoute] = useInstanceVar(pathname)
+  useEffect(() => {
+    if (pathname !== getCurrentRoute()) {
+      setCurrentRoute(pathname)
+      onRouteChange()
+    }
+  }, [pathname, getCurrentRoute, setCurrentRoute, onRouteChange])
+}
+
+export default useHasChangedRoute
