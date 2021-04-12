5f68cf3b49b59b22840f7dff017093dd7d8b9cd4
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-04-12 15:41:56 -0400

    Remove py-cid and dependency check

--- discovery-provider/src/utils/ipfs_lib.py
+++ discovery-provider/src/utils/ipfs_lib.py
@@ -196,16 +196,6 @@ class IPFSClient:
     def ipfs_id_multiaddr(self):
         return self._multiaddr
 
-    def cid_is_valid(self, cid):
-        if not cid:
-            return False
-
-        try:
-            make_cid(cid)
-            return True
-        except Exception as e:
-            logger.error(f'IPFSCLIENT | Error in cid_is_valid {str(e)}')
-            return False
 
 def construct_image_dir_gateway_url(address, CID):
     """Construct the gateway url for an image directory.
