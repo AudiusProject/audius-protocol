245cefe1db537354fa22b6d36c4f4b06b6b03474
hareeshnagaraj <hareesh.nagaraj@gmail.com>
2021-04-13 18:40:51 +0000

    More measurements

--- service-commands/scripts/sendLocalSolanaListens.js
+++ service-commands/scripts/sendLocalSolanaListens.js
@@ -2,26 +2,36 @@
 const axios = require('axios')
 
 async function run() {
-    let numRequests = 2
+    let numRequests = 20
     let randomTrackIds = Array.from({ length: numRequests }, () => Math.floor(Math.random() * 10000000));
     console.log(randomTrackIds)
+    let start = Date.now()
+    let numSuccessfullyProcessed = 0
     await Promise.all(randomTrackIds.map(async (trackId) =>{
-        let randomUserId = Math.floor(Math.random() * 10000000)
-        console.log(`Logging listen for trackId=${trackId}, userId=${randomUserId}`)
-        let data = JSON.stringify({"userId": randomUserId, "solanaListen": true });
-        let requestConfig = {
-            method: 'post',
-            url: `http://localhost:7000/tracks/${trackId}/listen`,
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            data : data
+        try {
+            let randomUserId = Math.floor(Math.random() * 10000000)
+            console.log(`Logging listen for trackId=${trackId}, userId=${randomUserId}`)
+            let data = JSON.stringify({"userId": randomUserId, "solanaListen": true });
+            let requestConfig = {
+                method: 'post',
+                url: `http://localhost:7000/tracks/${trackId}/listen`,
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                data : data
+            }
+            let resp = (await axios(requestConfig)).data
+            let signature = resp.solTxSignature
+            console.log(`trackId=${trackId}, userId=${randomUserId} | Processed signature: ${signature}`)
+            numSuccessfullyProcessed += 1
+        } catch(e) {
+            console.log(`Failed writing for trackId=${trackId}, userId=${randomUserId}, ${e}`)
         }
-        let resp = (await axios(requestConfig)).data
-        let signature = resp.solTxSignature
-        console.log(`trackId=${trackId}, userId=${randomUserId} | Processed signature: ${signature}`)
     })
     )
+    let end = Date.now()
+    let duration = end - start
+    console.log(`Processed ${numSuccessfullyProcessed} listens in ${duration}ms`)
 }
 
 run()
