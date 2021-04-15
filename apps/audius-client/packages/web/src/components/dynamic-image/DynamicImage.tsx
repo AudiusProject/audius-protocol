9fe28db78deb1dcddf0ee5a5184bf464a29e6087
Raymond Jacobson <ray@audius.co>
2021-04-15 08:00:23 -0700

    Fix profile image empty state (#380)

--- apps/audius-client/packages/web/src/components/dynamic-image/DynamicImage.tsx
+++ apps/audius-client/packages/web/src/components/dynamic-image/DynamicImage.tsx
@@ -62,7 +62,7 @@ const fadeIn = (
     // Set default background color for static images (transparent background defaults)
     if (image.includes('/static')) {
       ref.current.style.backgroundColor = 'var(--neutral-light-5)'
-    } else {
+    } else if (!image.startsWith('data:image/png')) {
       ref.current.style.backgroundColor = 'unset'
     }
     // Allow gradient values for 'image' in addition to URIs
