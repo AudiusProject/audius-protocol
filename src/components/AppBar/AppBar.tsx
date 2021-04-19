d01aee0dfeb5a7dd292b8d6c833f7313b79d0b54
jowlee <joeylee0925@gmail.com>
2021-04-19 16:24:23 +0000

    Update app bar and mobile spacing

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -12,7 +12,6 @@ import { accountPage, AUDIUS_DAPP_URL, isCryptoPage } from 'utils/routes'
 import { useEthBlockNumber } from 'store/cache/protocol/hooks'
 import clsx from 'clsx'
 import { useLocation } from 'react-router-dom'
-import { TICKER } from 'utils/consts'
 import { createStyles } from 'utils/mobile'
 import desktopStyles from './AppBar.module.css'
 import mobileStyles from './AppBarMobile.module.css'
@@ -20,6 +19,8 @@ import { useIsMobile } from 'utils/hooks'
 import DisplayAudio from 'components/DisplayAudio'
 import UserImage from 'components/UserImage'
 import useOpenLink from 'hooks/useOpenLink'
+import getActiveStake from 'utils/activeStake'
+import { Utils } from '@audius/libs'
 
 const styles = createStyles({ desktopStyles, mobileStyles })
 
@@ -29,7 +30,10 @@ const messages = {
   launchApp: 'LAUNCH THE APP',
   connectMetaMask: 'Connect Metamask',
   metaMaskMisconfigured: 'Metamask Misconfigured',
-  block: 'Block'
+  block: 'Block',
+  wallet: 'WALLET',
+  staked: 'STAKED',
+  profileAlt: 'User Profile'
 }
 
 // TODO:
@@ -71,6 +75,8 @@ const Misconfigured = ({
 
 const UserAccountSnippet = ({ wallet }: UserAccountSnippetProps) => {
   const { user } = useUser({ wallet })
+  const activeStake = user ? getActiveStake(user) : Utils.toBN('0')
+
   const pushRoute = usePushRoute()
   const onClickUser = useCallback(() => {
     if (user) {
@@ -82,24 +88,39 @@ const UserAccountSnippet = ({ wallet }: UserAccountSnippetProps) => {
 
   return (
     <div className={styles.snippetContainer} onClick={onClickUser}>
-      <UserImage
-        wallet={user.wallet}
-        className={styles.snippetImg}
-        alt="User Profile"
-        useSkeleton={false}
-      />
-      <div className={styles.snippetText}>
+      <div className={styles.user}>
+        <UserImage
+          wallet={user.wallet}
+          imgClassName={styles.snippetImg}
+          alt={messages.profileAlt}
+          useSkeleton={false}
+        />
         <div className={styles.walletText}>
           {formatShortWallet(user.wallet)}
         </div>
+      </div>
+      <div className={styles.snippetText}>
         <DisplayAudio
           position={Position.BOTTOM}
           className={styles.tokenText}
-          label={TICKER}
           amount={user.audToken}
           shortFormat
         />
-      </div>
+         <p className={styles.userSnippetLabel}>
+          {messages.wallet}
+        </p>
+     </div>
+     <div className={styles.snippetText}>
+        <DisplayAudio
+          position={Position.BOTTOM}
+          className={styles.tokenText}
+          amount={activeStake}
+          shortFormat
+        />
+         <p className={styles.userSnippetLabel}>
+          {messages.staked}
+        </p>
+     </div>
     </div>
   )
 }
@@ -132,17 +153,17 @@ const AppBar: React.FC<AppBarProps> = (props: AppBarProps) => {
       <div className={styles.left}>
         <Logo className={styles.logo} />
         <div className={styles.name}>{messages.name}</div>
-      </div>
-      {!isMobile && (
-        <div className={styles.right}>
           <div
             className={clsx(styles.currentBlock, {
               [styles.show]: showBlock
             })}
           >
-            <div className={styles.title}>{messages.block}</div>
             <div className={styles.block}>{ethBlock}</div>
+            <div className={styles.title}>{messages.block}</div>
           </div>
+      </div>
+      {!isMobile && (
+        <div className={styles.right}>
           <div className={styles.userAccountSnippetContainer}>
             {isMisconfigured || isAccountMisconfigured ? (
               <Misconfigured
