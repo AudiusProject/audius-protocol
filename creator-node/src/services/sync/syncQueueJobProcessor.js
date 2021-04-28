736d4758b767c6edbee5011b786b3946e982b105
SidSethi <SidSethi93@gmail.com>
2021-04-28 23:03:13 +0000

    Minor

--- creator-node/src/services/sync/syncQueueJobProcessor.js
+++ creator-node/src/services/sync/syncQueueJobProcessor.js
@@ -14,7 +14,6 @@ const { serviceRegistry } = require('../../serviceRegistry')
  */
 const syncQueueJobProcessorFn = async (job) => {
   const { walletPublicKeys, creatorNodeEndpoint } = job.data
-  logger.info(`SIDTEST SYNCQUEUEJOBPROCFN BEGIN SYNC FOR ${walletPublicKeys} & ${creatorNodeEndpoint}`)
 
   await _ensureServiceRegistryInitialized()
 
@@ -25,17 +24,17 @@ const syncQueueJobProcessorFn = async (job) => {
   }
 }
 
+/**
+ * This creates a bunch of problems -> every child process will re-initialized serviceRegistry, making this a lot more expensive
+ * - also this does not fully init serviceRegistry -> notably, the child process does not recoverNodeL1Identity(),
+ *    meaning it does not set `nodeConfig.get(spID)`. everything seems to work, but i'm pretty hesitant about this pattern.
+ * - some thoughts, a better pattern may be to have some persistent child processes initialized at server startup that fully init
+ *    only once, and are reused for all sync ops. can set num processes = numCPUs
+ */
 const _ensureServiceRegistryInitialized = async () => {
-  logger.info(`SIDTEST _ensureServiceRegistryInitialized STARTING`)
-  if (!this.serviceRegistry.servicesInitialized) {
-    await this.serviceRegistry.initServices()
+  if (!serviceRegistry.servicesInitialized) {
+    await serviceRegistry.initServices()
   }
-
-  if (!this.serviceRegistry.servicesThatRequireServerInitialized) {
-    await this.serviceRegistry.initServicesThatRequireServer()
-  }
-
-  logger.info(`SIDTEST _ensureServiceRegistryInitialized STARTING`)
 }
 
 module.exports = syncQueueJobProcessorFn
