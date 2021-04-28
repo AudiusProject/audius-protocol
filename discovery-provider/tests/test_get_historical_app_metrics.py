793ffe9e80f8a19d775607facbad00ac74e1148d
Raymond Jacobson <ray@audius.co>
2021-04-27 19:19:11 -0700

    Add test

--- discovery-provider/tests/test_get_historical_app_metrics.py
+++ discovery-provider/tests/test_get_historical_app_metrics.py
@@ -61,7 +61,7 @@ def test_get_historical_app_metrics(app):
             )
         ])
 
-        aggregate_metrics = _get_historical_app_metrics(session)
+        aggregate_metrics = _get_historical_app_metrics(session, 0)
         daily_aggregate_metrics = aggregate_metrics['daily']
         monthly_aggregate_metrics = aggregate_metrics['monthly']
 
@@ -69,6 +69,44 @@ def test_get_historical_app_metrics(app):
         assert daily_aggregate_metrics[str(thirty_days_ago)]['top-app'] == 2
         assert daily_aggregate_metrics[str(thirty_days_ago)]['best-app'] == 1
         assert daily_aggregate_metrics[str(yesterday)]['best-app'] == 3
+
+        assert len(daily_aggregate_metrics.items()) == 2
+        assert monthly_aggregate_metrics[str(today - timedelta(days=367))]['top-app'] == 2
+        assert monthly_aggregate_metrics[str(today - timedelta(days=100))]['best-app'] == 4
+        assert monthly_aggregate_metrics[str(today - timedelta(days=100))]['other-app'] == 5
+
+
+def test_get_historical_app_metrics_with_min_count(app):
+    with app.app_context():
+        db_mock = get_db()
+
+    with db_mock.scoped_session() as session:
+        session.bulk_save_objects([
+            AggregateDailyAppNameMetrics(
+                application_name='best-app',
+                count=4,
+                timestamp=yesterday
+            ),
+            AggregateDailyAppNameMetrics(
+                application_name='best-app',
+                count=3,
+                timestamp=today
+            ),
+            AggregateMonthlyAppNameMetrics(
+                application_name='top-app',
+                count=6,
+                timestamp=today
+            )
+        ])
+
+        # Min count of 4
+        aggregate_metrics = _get_historical_app_metrics(session, 4)
+        daily_aggregate_metrics = aggregate_metrics['daily']
+        monthly_aggregate_metrics = aggregate_metrics['monthly']
+
+        assert len(daily_aggregate_metrics.items()) == 2
+        assert daily_aggregate_metrics[str(yesterday)]['best-app'] == 4
+        assert daily_aggregate_metrics[str(today)]['top-app'] == 6
 
         assert len(daily_aggregate_metrics.items()) == 2
         assert monthly_aggregate_metrics[str(today - timedelta(days=367))]['top-app'] == 2
