1bb41f171bb6ee0ff65c8244324ed41d520ff252
Raymond Jacobson <ray@audius.co>
2021-04-30 19:47:36 -0700

    Metrics backfill (#1447)

---
+++ discovery-provider/alembic/versions/29864cb80469_re_add_summed_uniques_column.py
@@ -0,0 +1,30 @@
+"""re_add_summed_uniques_column
+
+Revision ID: 29864cb80469
+Revises: b7185f6920d3
+Create Date: 2021-04-27 17:15:25.639631
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '29864cb80469'
+down_revision = 'b7185f6920d3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    connection = op.get_bind()
+    connection.execute('''
+    begin;
+        ALTER TABLE "aggregate_daily_unique_users_metrics" ADD COLUMN IF NOT EXISTS "summed_count" INTEGER;
+        ALTER TABLE "aggregate_monthly_unique_users_metrics" ADD COLUMN IF NOT EXISTS "summed_count" INTEGER;
+    commit;
+    ''')
+
+
+def downgrade():
+    # This migration has no downgrade, but can be run multiple times idempotently
+    pass
--- discovery-provider/tests/test_get_historical_app_metrics.py
+++ discovery-provider/tests/test_get_historical_app_metrics.py
@@ -6,6 +6,7 @@ from src.utils.db_session import get_db
 limit = 2
 today = date.today()
 yesterday = today - timedelta(days=1)
+day_before_yesterday = today - timedelta(days=2)
 thirty_days_ago = today - timedelta(days=30)
 
 def test_get_historical_app_metrics(app):
@@ -61,7 +62,7 @@ def test_get_historical_app_metrics(app):
             )
         ])
 
-        aggregate_metrics = _get_historical_app_metrics(session)
+        aggregate_metrics = _get_historical_app_metrics(session, 0)
         daily_aggregate_metrics = aggregate_metrics['daily']
         monthly_aggregate_metrics = aggregate_metrics['monthly']
 
@@ -74,3 +75,49 @@ def test_get_historical_app_metrics(app):
         assert monthly_aggregate_metrics[str(today - timedelta(days=367))]['top-app'] == 2
         assert monthly_aggregate_metrics[str(today - timedelta(days=100))]['best-app'] == 4
         assert monthly_aggregate_metrics[str(today - timedelta(days=100))]['other-app'] == 5
+
+
+def test_get_historical_app_metrics_with_min_count(app):
+    with app.app_context():
+        db_mock = get_db()
+
+    with db_mock.scoped_session() as session:
+        session.bulk_save_objects([
+            AggregateDailyAppNameMetrics(
+                application_name='best-app',
+                count=4,
+                timestamp=yesterday
+            ),
+            AggregateDailyAppNameMetrics(
+                application_name='best-app',
+                count=3,
+                timestamp=day_before_yesterday
+            ),
+            AggregateDailyAppNameMetrics(
+                application_name='top-app',
+                count=6,
+                timestamp=day_before_yesterday
+            ),
+            AggregateMonthlyAppNameMetrics(
+                application_name='other-app',
+                count=3,
+                timestamp=today - timedelta(days=100)
+            ),
+            AggregateMonthlyAppNameMetrics(
+                application_name='top-app',
+                count=6,
+                timestamp=today - timedelta(days=100)
+            )
+        ])
+
+        # Min count of 4
+        aggregate_metrics = _get_historical_app_metrics(session, 4)
+        daily_aggregate_metrics = aggregate_metrics['daily']
+        monthly_aggregate_metrics = aggregate_metrics['monthly']
+
+        assert len(daily_aggregate_metrics.items()) == 2
+        assert daily_aggregate_metrics[str(yesterday)]['best-app'] == 4
+        assert daily_aggregate_metrics[str(day_before_yesterday)]['top-app'] == 6
+
+        assert len(monthly_aggregate_metrics.items()) == 1
+        assert monthly_aggregate_metrics[str(today - timedelta(days=100))]['top-app'] == 6
