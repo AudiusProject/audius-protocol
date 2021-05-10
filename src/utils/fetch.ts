163ee8b18ce439e55b38d70a806aacb24c6bac61
Saliou Diallo <saliou@audius.co>
2021-05-10 13:40:10 -0400

    Use healthy dps to get aggregate metrics
    WIP

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -29,10 +29,38 @@ export const withTimeout = async (
 }
 
 export const fetchUntilSuccess = async (endpoints: string[]): Promise<any> => {
-  // Pick a random endpoint and make a call to that endpoint
-  const endpoint = endpoints[Math.floor(Math.random() * endpoints.length)]
-  console.info('Attempting endpoint: ', endpoint)
+  const allowList = [
+    'https://discoveryprovider.audius.co',
+    'https://discoveryprovider2.audius.co',
+    'https://discoveryprovider3.audius.co',
+    'https://discoveryprovider.audius1.prod-us-west-2.staked.cloud',
+    'https://discoveryprovider.audius6.prod-us-west-2.staked.cloud',
+    'https://discoveryprovider.mumbaudius.com',
+    'https://audius-discovery.nz.modulational.com',
+    'https://audius-dp.johannesburg.creatorseed.com',
+    'https://dn2.monophonic.digital',
+    'https://dn1.monophonic.digital',
+    'https://audius-discovery-1.altego.net',
+    'https://audius-disco.ams-x01.nl.supercache.org',
+    'https://audius-disco.dfw-x01.us.supercache.org',
+    'https://discovery-us-01.audius.openplayer.org',
+    'https://discovery-au-01.audius.openplayer.org'
+  ]
+
   try {
+    const response = await fetchWithTimeout('https://api.audius.co')
+    const allHealthyDps = response.data as string[]
+    const allowedHealthyDPs = allHealthyDps.filter(url =>
+      allowList.includes(url)
+    )
+    const allowedEndpoints = endpoints.filter(endpoint =>
+      allowedHealthyDPs.some(url => endpoint.startsWith(url))
+    )
+
+    // Pick a random endpoint from the allowed endpoints
+    const endpoint =
+      allowedEndpoints[Math.floor(Math.random() * allowedEndpoints.length)]
+    console.info('Attempting endpoint: ', endpoint)
     return await fetchWithTimeout(endpoint)
   } catch (e) {
     console.error(e)
