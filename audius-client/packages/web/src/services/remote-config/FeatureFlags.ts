4d9a0392491ab8a99d4f896b8502809fc4623702
vicky :) <60366641+vicky-g@users.noreply.github.com>
2021-05-11 16:40:24 -0700

    Add optimizely flag for new track content route (#403)
    * add optimizely flag for new track content route
    
    * upgrade libs to 1.1.26

--- audius-client/packages/web/src/services/remote-config/FeatureFlags.ts
+++ audius-client/packages/web/src/services/remote-config/FeatureFlags.ts
@@ -1,8 +1,12 @@
 /* FeatureFlags must be lowercase snake case */
 export enum FeatureFlags {
-  SOLANA_LISTEN_ENABLED = 'solana_listen_enabled'
+  TRENDING_UNDERGROUND = 'trending_underground',
+  SOLANA_LISTEN_ENABLED = 'solana_listen_enabled',
+  USE_TRACK_CONTENT_POLLING = 'use_track_content_polling'
 }
 
 export const flagDefaults: { [key in FeatureFlags]: boolean } = {
+  [FeatureFlags.TRENDING_UNDERGROUND]: false,
+  [FeatureFlags.USE_TRACK_CONTENT_POLLING]: true,
   [FeatureFlags.SOLANA_LISTEN_ENABLED]: false
 }
