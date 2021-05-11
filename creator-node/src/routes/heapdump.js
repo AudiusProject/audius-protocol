4d067b091048ff6ae31cd9f0db69d61cf38d6d3f
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-05-10 22:02:03 -0400

    Remove route

--- creator-node/src/routes/heapdump.js
+++
@@ -1,9 +0,0 @@
-const { handleResponse, successResponse } = require('../apiHelpers')
-const { dump } = require('../utils/heapdump')
-
-module.exports = function (app) {
-  app.get('/heapdump', handleResponse(async (req, res) => {
-    const file = dump()
-    return successResponse({ file })
-  }))
-}
--- creator-node/src/utils/heapdump.js
+++ creator-node/src/utils/heapdump.js
@@ -2,25 +2,15 @@ const heapdump = require('heapdump')
 const { logger } = require('../logging')
 const config = require('../config')
 
-const FIVE_MINUTES = 1000 * 60 * 5
-let dumpDebounce = false
-
 const dump = () => {
   const ts = Date.now()
   const endpoint = config.get('creatorNodeEndpoint').split('//')[1]
   const file = `./${endpoint}-${ts}.heapsnapshot`
 
-  if (!dumpDebounce) {
-    logger.info(`Taking heap dump at ${ts}`)
-    // this is a synchronous operation
+  try {
     heapdump.writeSnapshot(file)
-    dumpDebounce = true
-
-    setTimeout(() => {
-      dumpDebounce = false
-    }, FIVE_MINUTES)
-  } else {
-    logger.error(`Cannot take heap dump at ${ts} -- latest one too recent`)
+  } catch (e) {
+    logger.error(`Could not take heapdump: ${e.message}`)
   }
 
   return file
