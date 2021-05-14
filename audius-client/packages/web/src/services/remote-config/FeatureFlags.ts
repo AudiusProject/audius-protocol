2a446de07f441958e70f54337231ca5dca1cc833
vicky :) <60366641+vicky-g@users.noreply.github.com>
2021-05-14 16:11:34 -0700

    Add optimizely flag for resumable track upload + consume libs 1.2.1 (#422)
    * add optimizely flag for resumable upload track flow
    
    * consume 1.2.1 libs

--- audius-client/packages/web/src/services/remote-config/FeatureFlags.ts
+++ audius-client/packages/web/src/services/remote-config/FeatureFlags.ts
@@ -2,11 +2,13 @@
 export enum FeatureFlags {
   TRENDING_UNDERGROUND = 'trending_underground',
   SOLANA_LISTEN_ENABLED = 'solana_listen_enabled',
-  USE_TRACK_CONTENT_POLLING = 'use_track_content_polling'
+  USE_TRACK_CONTENT_POLLING = 'use_track_content_polling',
+  USE_RESUMABLE_TRACK_UPLOAD = 'use_resumable_track_upload'
 }
 
 export const flagDefaults: { [key in FeatureFlags]: boolean } = {
   [FeatureFlags.TRENDING_UNDERGROUND]: false,
   [FeatureFlags.USE_TRACK_CONTENT_POLLING]: true,
-  [FeatureFlags.SOLANA_LISTEN_ENABLED]: false
+  [FeatureFlags.SOLANA_LISTEN_ENABLED]: false,
+  [FeatureFlags.USE_RESUMABLE_TRACK_UPLOAD]: true
 }
