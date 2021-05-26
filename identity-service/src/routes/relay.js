f52d1120b1d294e4c977b2ab1aec909fbf4a50b0
Raymond Jacobson <ray@audius.co>
2021-05-26 11:33:54 -0500

    Add secret relay

--- identity-service/src/routes/relay.js
+++ identity-service/src/routes/relay.js
@@ -50,4 +50,48 @@ module.exports = function (app) {
       return successResponse({ receipt: receipt })
     } else return errorResponseBadRequest('Missing one of the required fields: contractRegistryKey, contractAddress, senderAddress, encodedABI')
   }))
+  app.post('/secret_relay', handleResponse(async (req, res, next) => {
+    let body = req.body
+    const redis = req.app.get('redis')
+
+    if (body && body.contractRegistryKey && body.contractAddress && body.senderAddress && body.encodedABI) {
+      // send tx
+      let receipt
+      const reqBodySHA = crypto.createHash('sha256').update(JSON.stringify(req.body)).digest('hex')
+      try {
+        var txProps = {
+          contractRegistryKey: body.contractRegistryKey,
+          contractAddress: body.contractAddress,
+          encodedABI: body.encodedABI,
+          senderAddress: body.senderAddress,
+          gasLimit: body.gasLimit || null
+        }
+        receipt = await txRelay.sendTransaction(
+          req,
+          false, // resetNonce
+          txProps,
+          reqBodySHA)
+      } catch (e) {
+        if (e.message.includes('nonce')) {
+          req.logger.warn('Nonce got out of sync, resetting. Original error message: ', e.message)
+          // this is a retry in case we get an error about the nonce being out of sync
+          // the last parameter is an optional bool that forces a nonce reset
+          receipt = await txRelay.sendTransaction(
+            req,
+            true, // resetNonce
+            txProps,
+            reqBodySHA)
+          // no need to return success response here, regular code execution will continue after catch()
+        } else {
+          // if the tx fails, store it in redis with a 24 hour expiration
+          await redis.setex(`failedTx:${reqBodySHA}`, 60 /* seconds */ * 60 /* minutes */ * 24 /* hours */, JSON.stringify(req.body))
+
+          req.logger.error('Error in transaction:', e.message, reqBodySHA)
+          return errorResponseServerError(`Something caused the transaction to fail for payload ${reqBodySHA}`)
+        }
+      }
+
+      return successResponse({ receipt: receipt })
+    } else return errorResponseBadRequest('Missing one of the required fields: contractRegistryKey, contractAddress, senderAddress, encodedABI')
+  }))
 }
