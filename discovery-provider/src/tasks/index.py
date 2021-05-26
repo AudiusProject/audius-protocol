3b606f07980759f8ba16544f49d7833f5f34e374
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2021-05-26 10:07:29 -0400

    Sliced revert blocks

--- discovery-provider/src/tasks/index.py
+++ discovery-provider/src/tasks/index.py
@@ -374,7 +374,8 @@ def revert_blocks(self, db, revert_blocks_list):
     if num_revert_blocks > 500:
         logger.error(f"index.py | {self.request.id} | Revert blocks list > 500:")
         logger.error(revert_blocks_list)
-        raise Exception('Unexpected revert, >0 blocks')
+        revert_blocks_list = revert_blocks_list[:500]
+        logger.error(f"index.py | {self.request.id} | Sliced revert blocks list {revert_blocks_list}")
 
     logger.error(f"index.py | {self.request.id} | Reverting {num_revert_blocks} blocks")
     logger.error(revert_blocks_list)
