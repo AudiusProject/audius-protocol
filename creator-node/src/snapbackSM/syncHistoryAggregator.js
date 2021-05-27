682836a7944c579de3cf07e764a5616a7f0e855d
vicky <vicky@audius.co>
2021-05-27 18:24:04 +0000

    remove unnecessary prefix + triggered key

--- creator-node/src/snapbackSM/syncHistoryAggregator.js
+++ creator-node/src/snapbackSM/syncHistoryAggregator.js
@@ -2,12 +2,8 @@ const moment = require('moment')
 
 const redisClient = require('../redis')
 const { logger: genericLogger } = require('../logging')
-const config = require('../config')
-
-const CREATOR_NODE_ENDPOINT = config.get('creatorNodeEndpoint')
 
 const SYNC_STATES = Object.freeze({
-  triggered: 'triggered',
   success: 'success',
   fail: 'fail'
 })
@@ -17,7 +13,7 @@ const EXPIRATION = 7 /* days */ * 24 /* hr */ * 60 /* min */ * 60 /* s */
 
 /**
  * Class intended to:
- * - Record the number of the current day's triggered, successful, and failed sync attempts
+ * - Record the number of the current day's successful and failed sync attempts
  * - Record the timestamp of the most recent successful and failed sync
  */
 
@@ -46,12 +42,7 @@ class SyncHistoryAggregator {
 
     try {
       // Update aggregate sync data
-      const aggregateSyncKeys = SyncHistoryAggregator.getAggregateSyncKeys(CREATOR_NODE_ENDPOINT)
-
-      // Get the TTL if the key exists for the number of triggered syncs, and update the value by 1
-      const aggregateSyncKeyTriggeredTTL = await this.getKeyTTL(aggregateSyncKeys.triggered)
-      await redisClient.incr(aggregateSyncKeys.triggered)
-      await redisClient.expire(aggregateSyncKeys.triggered, aggregateSyncKeyTriggeredTTL)
+      const aggregateSyncKeys = SyncHistoryAggregator.getAggregateSyncKeys()
 
       // Get the TTL if the key exists for the number of `state` syncs, and update the value by 1
       const aggregateSyncKeyStateTTL = await this.getKeyTTL(aggregateSyncKeys[state])
@@ -59,15 +50,15 @@ class SyncHistoryAggregator {
       await redisClient.expire(aggregateSyncKeys[state], aggregateSyncKeyStateTTL)
 
       // Update latest sync data
-      const latestSyncKeys = SyncHistoryAggregator.getLatestSyncKeys(CREATOR_NODE_ENDPOINT)
+      const latestSyncKeys = SyncHistoryAggregator.getLatestSyncKeys()
 
       const latestSyncKeyTTL = await this.getKeyTTL(latestSyncKeys[state])
       await redisClient.set(latestSyncKeys[state], timeOfEvent, 'EX', latestSyncKeyTTL)
 
-      logger.info(`SyncHistoryAggregator - Successfully tracked ${state} sync at ${timeOfEvent} from ${CREATOR_NODE_ENDPOINT}`)
+      logger.info(`SyncHistoryAggregator - Successfully tracked ${state} sync at ${timeOfEvent}`)
     } catch (e) {
       // Only log error to not block any main thread
-      logger.error(`SyncHistoryAggregator - Failed to track ${state} sync at ${timeOfEvent} from ${CREATOR_NODE_ENDPOINT}: ${e.toString()}`)
+      logger.error(`SyncHistoryAggregator - Failed to track ${state} sync at ${timeOfEvent}: ${e.toString()}`)
     }
   }
 
@@ -77,16 +68,18 @@ class SyncHistoryAggregator {
   }
 
   static async getAggregateSyncData () {
-    const { success, fail, triggered } = SyncHistoryAggregator.getAggregateSyncKeys(CREATOR_NODE_ENDPOINT)
+    const { success, fail } = SyncHistoryAggregator.getAggregateSyncKeys()
 
-    const successCount = await redisClient.get(success)
-    const failCount = await redisClient.get(fail)
-    const triggeredCount = await redisClient.get(triggered)
+    let successCount = await redisClient.get(success)
+    let failCount = await redisClient.get(fail)
+
+    successCount = successCount ? parseInt(successCount) : 0
+    failCount = failCount ? parseInt(failCount) : 0
 
     const currentAggregateData = {
-      success: successCount ? parseInt(successCount) : 0,
-      fail: failCount ? parseInt(failCount) : 0,
-      triggered: triggeredCount ? parseInt(triggeredCount) : 0
+      success: successCount,
+      fail: failCount,
+      triggered: successCount + failCount
     }
 
     // Structure: {triggered: <number>, success: <number>, fail: <number>}
@@ -94,7 +87,7 @@ class SyncHistoryAggregator {
   }
 
   static async getLatestSyncData () {
-    const { success, fail } = SyncHistoryAggregator.getLatestSyncKeys(CREATOR_NODE_ENDPOINT)
+    const { success, fail } = SyncHistoryAggregator.getLatestSyncKeys()
 
     const successDate = await redisClient.get(success)
     const failDate = await redisClient.get(fail)
@@ -108,20 +101,19 @@ class SyncHistoryAggregator {
     return currentLatestSyncData
   }
 
-  static getAggregateSyncKeys (creatorNodeEndpoint) {
-    // ex: creatornode.audius.co:::aggregateSync:::05212021:::success
-    const prefix = `${creatorNodeEndpoint}:::aggregateSync:::${new Date().toISOString().split('T')[0]}`
+  static getAggregateSyncKeys () {
+    // ex: aggregateSync:::05212021:::success
+    const prefix = `aggregateSync:::${new Date().toISOString().split('T')[0]}`
 
     return {
-      triggered: `${prefix}:::${SYNC_STATES.triggered}`,
       success: `${prefix}:::${SYNC_STATES.success}`,
       fail: `${prefix}:::${SYNC_STATES.fail}`
     }
   }
 
   static getLatestSyncKeys (creatorNodeEndpoint) {
-    // ex: creatornode.audius.co:::latestSync:::05212021:::success
-    const prefix = `${creatorNodeEndpoint}:::latestSync:::${new Date().toISOString().split('T')[0]}`
+    // ex: latestSync:::05212021:::success
+    const prefix = `latestSync:::${new Date().toISOString().split('T')[0]}`
 
     return {
       success: `${prefix}:::${SYNC_STATES.success}`,
