c8fe52c497183f0178db3938349df759678f718a
vicky <vicky@audius.co>
2021-06-03 23:44:42 +0000

    add sync data to verbose health check

--- creator-node/src/components/healthCheck/healthCheckComponentService.js
+++ creator-node/src/components/healthCheck/healthCheckComponentService.js
@@ -104,6 +104,9 @@ const healthCheckVerbose = async ({ libs } = {}, logger, sequelize, getMonitors,
     MONITORS.LATEST_SYNC_FAIL_TIMESTAMP
   ])
 
+  const latestDailySyncCount = await getAggregateSyncData()
+  const latestDailySyncTimestamps = await getLatestSyncData()
+
   const response = {
     ...basicHealthCheck,
     country,
