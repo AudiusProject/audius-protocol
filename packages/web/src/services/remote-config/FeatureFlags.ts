dfb763a5bbc8b52e87b62fe7ed1ce2f2b420779d
Saliou Diallo <sddioulde@hotmail.com>
2021-06-03 13:59:51 -0400

    Add feature flag for playlist updates (#430)

--- packages/web/src/services/remote-config/FeatureFlags.ts
+++ packages/web/src/services/remote-config/FeatureFlags.ts
@@ -3,12 +3,14 @@ export enum FeatureFlags {
   TRENDING_UNDERGROUND = 'trending_underground',
   SOLANA_LISTEN_ENABLED = 'solana_listen_enabled',
   USE_TRACK_CONTENT_POLLING = 'use_track_content_polling',
-  USE_RESUMABLE_TRACK_UPLOAD = 'use_resumable_track_upload'
+  USE_RESUMABLE_TRACK_UPLOAD = 'use_resumable_track_upload',
+  PLAYLIST_UPDATES_ENABLED = 'playlist_updates_enabled'
 }
 
 export const flagDefaults: { [key in FeatureFlags]: boolean } = {
   [FeatureFlags.TRENDING_UNDERGROUND]: false,
   [FeatureFlags.USE_TRACK_CONTENT_POLLING]: true,
   [FeatureFlags.SOLANA_LISTEN_ENABLED]: false,
-  [FeatureFlags.USE_RESUMABLE_TRACK_UPLOAD]: true
+  [FeatureFlags.USE_RESUMABLE_TRACK_UPLOAD]: true,
+  [FeatureFlags.PLAYLIST_UPDATES_ENABLED]: false
 }
