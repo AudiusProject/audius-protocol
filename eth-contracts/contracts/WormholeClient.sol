0b3fcdf93ac9d375150ddbcc80603042db4ccfa0
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-06-03 12:01:36 -0400

    Add signature recovery to verify

--- eth-contracts/contracts/WormholeClient.sol
+++ eth-contracts/contracts/WormholeClient.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol
 import "@openzeppelin/contracts-ethereum-package/contracts/utils/Address.sol";
 import "./InitializableV2.sol";
 
+
 interface Wormhole {
     function lockAssets(
         address asset,
@@ -24,8 +25,15 @@ contract WormholeClient is InitializableV2 {
     string private constant ERROR_TOKEN_NOT_CONTRACT = (
         "WormholeClient: Transfer token is not a contract"
     );
+
     string private constant ERROR_WORMHOLE_NOT_CONTRACT = (
         "WormholeClient: Wormhole is not a contract"
+    );
+
+    bytes32 public DOMAIN_SEPARATOR;
+
+    bytes32 public constant LOCK_ASSETS_TYPEHASH = (
+        0xc0d6f892604bee2cd37d2b5f77e4618e99d3c95eb3ff34d2d22388bf23c28bad
     );
 
     /// @dev ERC-20 token that will be transfered
@@ -59,10 +67,21 @@ contract WormholeClient is InitializableV2 {
         transferToken = ERC20(_tokenAddress);
         wormhole = Wormhole(_wormholeAddress);
         InitializableV2.initialize();
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes("wormholeclient")),
+                keccak256(bytes("1")),
+                1,
+                address(this)
+            )
+        );
     }
 
     /**
      * @notice transfer `_amount` of tokens from sender to target account
+     * @param _from - account to transfer from
      * @param _amount - amount of token to transfer
      * @param _recipient - foreign chain address of recipient
      * @param _targetChain -  id of the chain to transfer to
@@ -70,14 +89,45 @@ contract WormholeClient is InitializableV2 {
      * @param _refundDust - bool to refund dust
      */
     function lockAssets(
+        address _from,
         uint256 _amount,
         bytes32 _recipient,
         uint8 _targetChain,
         uint32 _nonce,
-        bool _refundDust
+        bool _refundDust,
+        uint _deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
     ) public {
-        transferToken.safeTransferFrom(msg.sender, address(this), _amount);
+        transferToken.safeTransferFrom(_from, address(this), _amount);
         transferToken.approve(address(wormhole), _amount);
+
+        // solium-disable security/no-block-members
+        require(_deadline >= block.timestamp, "AudiusToken: Deadline has expired");
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(
+                        LOCK_ASSETS_TYPEHASH,
+                        _from,
+                        _amount,
+                        _recipient,
+                        _targetChain,
+                        _nonce,
+                        _refundDust,
+                        _deadline
+                    )
+                )
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(
+            recoveredAddress != address(0) && recoveredAddress == _from,
+            "AudiusToken: Invalid signature"
+        );
 
         wormhole.lockAssets(
             address(transferToken),
