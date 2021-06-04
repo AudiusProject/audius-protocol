ac4b290508be704c8ad5a2715841be9125ee9f93
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-06-04 00:17:53 -0400

    Fix tests

--- eth-contracts/test/wormholeClient.test.js
+++ eth-contracts/test/wormholeClient.test.js
@@ -1,4 +1,5 @@
 import * as _lib from '../utils/lib.js'
+import * as _signatures from '../utils/signatures.js'
 
 const MockWormhole = artifacts.require('MockWormhole')
 const WormholeClient = artifacts.require('WormholeClient')
@@ -10,7 +11,7 @@ const wormholeClientProxyKey = web3.utils.utf8ToHex('WormholeClientProxy')
 
 const DEFAULT_AMOUNT = _lib.audToWeiBN(120)
 
-contract('WomrholeClient', async (accounts) => {
+contract('WormholeClient', async (accounts) => {
   let registry, governance, token, wormholeClient0, wormholeClientProxy, wormholeClient
   let mockWormhole
 
@@ -67,18 +68,48 @@ contract('WomrholeClient', async (accounts) => {
   })
 
   it('lock assets', async () => {
-    let sender = accounts[10]
-    await token.transfer(sender, DEFAULT_AMOUNT, { from: proxyDeployerAddress })
-    const initialOwnerBalance = await token.balanceOf(sender)
-    await token.approve(wormholeClient.address, DEFAULT_AMOUNT, { from: sender })
-    let tx = await wormholeClient.lockAssets(
-      DEFAULT_AMOUNT,
-      Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex'),
-      1,
-      123,
-      true,
-      { from: sender }
+    const amount = 100
+    const chainId = 1 // in ganache, the chain ID the token initializes with is always 1
+
+    const fromAcctPrivKey = Buffer.from('76195632b07afded1ae36f68635b6ff86791bd4579a27ca28ec7e539fed65c0e', 'hex')
+    const fromAcct = '0xaaa30A4bB636F15be970f571BcBe502005E9D66b'
+
+    const relayerAcct = accounts[6] // account that calls lockAssets
+
+    const recipient = Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex')
+
+    await token.transfer(fromAcct, amount, { from: tokenOwnerAddress })
+    await _lib.permit(token, fromAcct, fromAcctPrivKey, wormholeClient.address, amount, relayerAcct)
+
+    const nonce = (await wormholeClient.nonces(fromAcct)).toNumber()
+    const deadline = (await web3.eth.getBlock(await web3.eth.getBlockNumber())).timestamp + 25 // sufficiently far in future
+    const digest = _signatures.getLockAssetsDigest(
+      'AudiusWormholeClient',
+      wormholeClient.address,
+      chainId,
+      {
+        from: fromAcct,
+        amount: amount,
+        recipient,
+        targetChain: 1,
+        refundDust: true
+      },
+      nonce,
+      deadline
     )
-    assert.isTrue((await token.balanceOf(sender)).eq(initialOwnerBalance.sub(DEFAULT_AMOUNT)), 'sender balance should match')
+    const result = _signatures.sign(digest, fromAcctPrivKey)
+
+    await wormholeClient.lockAssets(
+      fromAcct,
+      amount,
+      recipient,
+      1,
+      true,
+      deadline,
+      result.v,
+      result.r,
+      result.s,
+      { from: relayerAcct }
+    )
   })
 })
