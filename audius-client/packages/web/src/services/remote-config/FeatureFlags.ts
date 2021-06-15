971d23ba3ee014fa26be804c04f3272c9f3c2d0d
vicky :) <60366641+vicky-g@users.noreply.github.com>
2021-06-15 15:10:46 -0700

    Fix optimizely bug (#446)
    * wip userId and sessionId feature flag
    
    * disable feature if anonymous_user + localstorage if condit
    
    * move logic to feature flag classes
    
    * init sessionId with uuid() at startup or localstorage val
    
    * comments + refactor feature flag logic
    
    * update comment
    
    * update const name and update field type
    
    * fix styling
    
    * fix styling again
    
    * import Nullable type

--- audius-client/packages/web/src/services/remote-config/FeatureFlags.ts
+++ audius-client/packages/web/src/services/remote-config/FeatureFlags.ts
@@ -7,10 +7,38 @@ export enum FeatureFlags {
   PLAYLIST_UPDATES_ENABLED = 'playlist_updates_enabled'
 }
 
+/**
+ * If optimizely errors, these default values are used.
+ */
 export const flagDefaults: { [key in FeatureFlags]: boolean } = {
   [FeatureFlags.TRENDING_UNDERGROUND]: false,
-  [FeatureFlags.USE_TRACK_CONTENT_POLLING]: true,
+  [FeatureFlags.USE_TRACK_CONTENT_POLLING]: false,
   [FeatureFlags.SOLANA_LISTEN_ENABLED]: false,
-  [FeatureFlags.USE_RESUMABLE_TRACK_UPLOAD]: true,
+  [FeatureFlags.USE_RESUMABLE_TRACK_UPLOAD]: false,
   [FeatureFlags.PLAYLIST_UPDATES_ENABLED]: false
 }
+
+export enum FeatureFlagCohortType {
+  /**
+   * Segments feature experiments by a user's id. If userId is not present,
+   * the feature is off.
+   */
+  USER_ID = 'user_id',
+  /**
+   * Segments feature experiments by a random uuid set in local storage defined by FEATURE_FLAG_LOCAL_STORAGE_SESSION_KEY.
+   * There should always be a value for sessionId. This is managed in Provider.ts
+   */
+  SESSION_ID = 'session_id'
+}
+
+export const flagCohortType: {
+  [key in FeatureFlags]: FeatureFlagCohortType
+} = {
+  [FeatureFlags.USE_TRACK_CONTENT_POLLING]: FeatureFlagCohortType.SESSION_ID,
+  [FeatureFlags.SOLANA_LISTEN_ENABLED]: FeatureFlagCohortType.SESSION_ID,
+  [FeatureFlags.USE_RESUMABLE_TRACK_UPLOAD]: FeatureFlagCohortType.SESSION_ID,
+  [FeatureFlags.TRENDING_UNDERGROUND]: FeatureFlagCohortType.USER_ID,
+  [FeatureFlags.PLAYLIST_UPDATES_ENABLED]: FeatureFlagCohortType.USER_ID
+}
+
+export const FEATURE_FLAG_LOCAL_STORAGE_SESSION_KEY = 'featureFlagSessionId'
