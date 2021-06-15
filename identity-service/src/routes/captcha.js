c44d17eac252f59a5b91e2e6c200d24c08c88557
Saliou Diallo <saliou@audius.co>
2021-06-15 13:18:12 -0400

    Update identity captcha scores get api

--- identity-service/src/routes/captcha.js
+++ identity-service/src/routes/captcha.js
@@ -3,12 +3,15 @@ const { handleResponse, successResponse, errorResponseForbidden } = require('../
 const { sequelize } = require('../models')
 
 module.exports = function (app) {
-  app.get('/scores/:handle', handleResponse(async req => {
+  app.get('/scores', handleResponse(async req => {
+    const handle = req.query.handle
+    if (!handle) {
+      return errorResponseBadRequest('Please provide handle.')
+    }
+
     if (req.headers['x-captcha-score'] !== config.get('captchaScoreSecret')) {
       return errorResponseForbidden('Not permissioned to view captcha scores.')
     }
-
-    const handle = req.params.handle
 
     const recaptchaEntries = await sequelize.query(
       `select "Users"."blockchainUserId" as "userId", "Users"."walletAddress" as "wallet", "BotScores"."recaptchaScore" as "score", "BotScores"."recaptchaContext" as "context"
