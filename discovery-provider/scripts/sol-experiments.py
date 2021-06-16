86152fe9acfda9aaea9f49fc6b3d828fc63b2124
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2021-06-16 11:04:53 -0400

    TMP

--- discovery-provider/scripts/sol-experiments.py
+++ discovery-provider/scripts/sol-experiments.py
@@ -16,8 +16,7 @@ WAUDIO_MINT_PUBKEY = '9zyPU1mjgzaVyQsYwKJJ7AhVz5bgx5uc1NPABvAcUXsT'
 
 FUNDER_PRIV_KEY = []
 
-''''
-tx_high = '3Hcz3K6QnKBiN5vzHkm5pFs3TwoU39St42ZC3rrio4fiu1oknPjRKVp4cXqCE5e9LE6NaP1FA144znHnM4yPtYaU'
+tx_high = None #'3Hcz3K6QnKBiN5vzHkm5pFs3TwoU39St42ZC3rrio4fiu1oknPjRKVp4cXqCE5e9LE6NaP1FA144znHnM4yPtYaU'
 
 signatures = solana_client.get_confirmed_signature_for_address2(TEST_USER_BANK_PROGRAM, before=tx_high)
 
@@ -29,8 +28,8 @@ for tx_info in history:
     print(tx_info)
     details = solana_client.get_confirmed_transaction(sig)
     print(details)
-'''
 
+'''
 token_program_key = PublicKey(WAUDIO_PROGRAM_ID)
 print(token_program_key)
 
@@ -43,17 +42,14 @@ print(funder_account.public_key())
 waudio_token = Token(conn=solana_client, pubkey=token_mint_key, program_id=token_program_key, payer=funder_account)
 print(waudio_token)
 
-'''
-Querying this balance:
-https://explorer.solana.com/address/Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ
-'''
+# Querying this balance:
+# https://explorer.solana.com/address/Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ
 
 bal_obj = waudio_token.get_balance(PublicKey('Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ'))
 print(waudio_token.get_balance(PublicKey('Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ')))
 
-'''
-eth_pubkey = c9D4B5727f7098F45ceF4AbfBc67bA53a714c247
-https://explorer.solana.com/address/E3Q1yeMU3LndGmcPD9msjzbBZEyb8JGtw7CBEwKcGL5g
-'''
+# eth_pubkey = c9D4B5727f7098F45ceF4AbfBc67bA53a714c247
+# https://explorer.solana.com/address/E3Q1yeMU3LndGmcPD9msjzbBZEyb8JGtw7CBEwKcGL5g
 bal_obj_2 = waudio_token.get_balance(PublicKey('E3Q1yeMU3LndGmcPD9msjzbBZEyb8JGtw7CBEwKcGL5g'))
 print(bal_obj_2)
+'''
