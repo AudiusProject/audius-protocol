00b79c673cd479af3537491e454fc6716579cd39
Saliou Diallo <saliou@audius.co>
2021-06-16 15:26:37 -0400

    Update user signals test

--- discovery-provider/tests/test_get_user_signals.py
+++ discovery-provider/tests/test_get_user_signals.py
@@ -2,47 +2,61 @@ from src.queries.get_user_signals import _get_user_signals
 from src.utils.db_session import get_db
 from tests.utils import populate_mock_db
 
-def make_user(blockhash, number, parenthash, is_current, user_id, handle):
+def make_user(
+        user_id,
+        handle,
+        profile_picture=None,
+        profile_picture_sizes=None,
+        cover_photo=None,
+        cover_photo_sizes=None
+    ):
     return {
-        "blockhash": blockhash,
-        "number": number,
-        "parenthash": parenthash,
-        "is_current": is_current,
-        "user_id": user_id,
-        "handle": handle
+        'user_id': user_id,
+        'handle': handle,
+        'profile_picture': profile_picture,
+        'profile_picture_sizes': profile_picture_sizes,
+        'cover_photo': cover_photo,
+        'cover_photo_sizes': cover_photo_sizes
     }
 
-def make_follow(blockhash, blocknumber, is_current, follower_user_id, followee_user_id):
+def make_follow(follower_user_id, followee_user_id):
     return {
-        "blockhash": blockhash,
-        "blocknumber": blocknumber,
-        "is_current": is_current,
-        "follower_user_id": follower_user_id,
-        "followee_user_id": followee_user_id
+        'follower_user_id': follower_user_id,
+        'followee_user_id': followee_user_id
     }
 
 def test_get_user_signals(app):
     with app.app_context():
         db = get_db()
 
-    blockhash1, blocknumber1 = '0x01', 1
-    blockhash2, blocknumber2 = '0x02', 2
-    blockhash3, blocknumber3 = '0x03', 3
-
     test_entities = {
         'users': [
-            make_user('0x00', 0, None, False, 1, handle="user1"),
-            make_user(blockhash1, blocknumber1, None, True, 1, handle="user1"),
-            make_user(blockhash2, blocknumber2, blockhash1, True, 2, handle="user2"),
-            make_user(blockhash2, blocknumber2, blockhash1, True, 3, handle="user3"),
-            make_user(blockhash2, blocknumber2, blockhash1, True, 4, handle="user4"),
-            make_user(blockhash2, blocknumber2, blockhash1, True, 5, handle="user5")
+            make_user(1, 'user1'),
+            make_user(2, 'user2'),
+            make_user(3, 'user3'),
+            make_user(4, 'user4'),
+            make_user(5, 'user5'),
+            make_user(6, 'user6', profile_picture='Qm0123456789abcdef0123456789abcdef0123456789ab'),
+            make_user(7, 'user7', profile_picture_sizes='Qm0123456789abcdef0123456789abcdef0123456789ab'),
+            make_user(8, 'user8', cover_photo='Qm0123456789abcdef0123456789abcdef0123456789ab'),
+            make_user(9, 'user9', cover_photo_sizes='Qm0123456789abcdef0123456789abcdef0123456789ab'),
+            make_user(
+                10,
+                'user10',
+                profile_picture='Qm0123456789abcdef0123456789abcdef0123456789ab',
+                cover_photo='Qm0123456789abcdef0123456789abcdef0123456789cd'
+            )
         ],
         'follows': [
-            make_follow(blockhash3, blocknumber3, True, 2, 1),
-            make_follow(blockhash3, blocknumber3, True, 3, 1),
-            make_follow(blockhash3, blocknumber3, True, 5, 1),
-            make_follow(blockhash3, blocknumber3, True, 1, 5)
+            make_follow(2, 1),
+            make_follow(3, 1),
+            make_follow(5, 1),
+            make_follow(1, 5),
+            make_follow(2, 6),
+            make_follow(3, 7),
+            make_follow(4, 8),
+            make_follow(5, 9),
+            make_follow(10, 4)
         ]
     }
 
@@ -52,3 +66,35 @@ def test_get_user_signals(app):
         user_signals = _get_user_signals(session, "user1")
         assert user_signals['num_followers'] == 3
         assert user_signals['num_following'] == 1
+        assert user_signals['has_profile_picture'] == False
+        assert user_signals['has_cover_photo'] == False
+
+        user_signals = _get_user_signals(session, "user6")
+        assert user_signals['num_followers'] == 1
+        assert user_signals['num_following'] == 0
+        assert user_signals['has_profile_picture'] == True
+        assert user_signals['has_cover_photo'] == False
+
+        user_signals = _get_user_signals(session, "user7")
+        assert user_signals['num_followers'] == 1
+        assert user_signals['num_following'] == 0
+        assert user_signals['has_profile_picture'] == True
+        assert user_signals['has_cover_photo'] == False
+
+        user_signals = _get_user_signals(session, "user8")
+        assert user_signals['num_followers'] == 1
+        assert user_signals['num_following'] == 0
+        assert user_signals['has_profile_picture'] == False
+        assert user_signals['has_cover_photo'] == True
+
+        user_signals = _get_user_signals(session, "user9")
+        assert user_signals['num_followers'] == 1
+        assert user_signals['num_following'] == 0
+        assert user_signals['has_profile_picture'] == False
+        assert user_signals['has_cover_photo'] == True
+
+        user_signals = _get_user_signals(session, "user10")
+        assert user_signals['num_followers'] == 0
+        assert user_signals['num_following'] == 1
+        assert user_signals['has_profile_picture'] == True
+        assert user_signals['has_cover_photo'] == True
