a76dc70064bbd199a315ff18bc4b84dc1bfc9ed1
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-06-16 11:03:11 -0400

    Add some more functions

--- discovery-provider/scripts/sol-experiments.py
+++ discovery-provider/scripts/sol-experiments.py
@@ -1,22 +1,22 @@
+import base58
 import solana
-
-from solana.rpc.api import Client
-from solana.publickey import PublicKey
 from solana.account import Account
+from solana.publickey import PublicKey
+from solana.rpc.api import Client
 from spl.token.client import Token
 
 solana_client = Client("https://audius.rpcpool.com")
 
 print("HI")
 
-TEST_USER_BANK_PROGRAM = '8a3KEEEXgWyeJcZr4G5Y8r19TdriEMziBSi2qSEJxT6z'
+TEST_USER_BANK_PROGRAM = "8a3KEEEXgWyeJcZr4G5Y8r19TdriEMziBSi2qSEJxT6z"
 
-WAUDIO_PROGRAM_ID = 'CYzPVv1zB9RH6hRWRKprFoepdD8Y7Q5HefCqrybvetja'
-WAUDIO_MINT_PUBKEY = '9zyPU1mjgzaVyQsYwKJJ7AhVz5bgx5uc1NPABvAcUXsT'
+WAUDIO_PROGRAM_ID = "CYzPVv1zB9RH6hRWRKprFoepdD8Y7Q5HefCqrybvetja"
+WAUDIO_MINT_PUBKEY = "9zyPU1mjgzaVyQsYwKJJ7AhVz5bgx5uc1NPABvAcUXsT"
 
 FUNDER_PRIV_KEY = []
 
-''''
+"""'
 tx_high = '3Hcz3K6QnKBiN5vzHkm5pFs3TwoU39St42ZC3rrio4fiu1oknPjRKVp4cXqCE5e9LE6NaP1FA144znHnM4yPtYaU'
 
 signatures = solana_client.get_confirmed_signature_for_address2(TEST_USER_BANK_PROGRAM, before=tx_high)
@@ -29,7 +29,7 @@ for tx_info in history:
     print(tx_info)
     details = solana_client.get_confirmed_transaction(sig)
     print(details)
-'''
+"""
 
 token_program_key = PublicKey(WAUDIO_PROGRAM_ID)
 print(token_program_key)
@@ -40,20 +40,55 @@ print(token_mint_key)
 funder_account = Account(FUNDER_PRIV_KEY[:32])
 print(funder_account.public_key())
 
-waudio_token = Token(conn=solana_client, pubkey=token_mint_key, program_id=token_program_key, payer=funder_account)
+waudio_token = Token(
+    conn=solana_client,
+    pubkey=token_mint_key,
+    program_id=token_program_key,
+    payer=funder_account,
+)
 print(waudio_token)
 
-'''
+"""
 Querying this balance:
 https://explorer.solana.com/address/Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ
-'''
+"""
 
-bal_obj = waudio_token.get_balance(PublicKey('Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ'))
-print(waudio_token.get_balance(PublicKey('Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ')))
+bal_obj = waudio_token.get_balance(
+    PublicKey("Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ")
+)
+print(
+    waudio_token.get_balance(PublicKey("Huc3gaSwGQ74nyjHoKYnJwNijcobecQq4KiuL1tdgtNZ"))
+)
 
-'''
+"""
 eth_pubkey = c9D4B5727f7098F45ceF4AbfBc67bA53a714c247
 https://explorer.solana.com/address/E3Q1yeMU3LndGmcPD9msjzbBZEyb8JGtw7CBEwKcGL5g
-'''
-bal_obj_2 = waudio_token.get_balance(PublicKey('E3Q1yeMU3LndGmcPD9msjzbBZEyb8JGtw7CBEwKcGL5g'))
+"""
+bal_obj_2 = waudio_token.get_balance(
+    PublicKey("E3Q1yeMU3LndGmcPD9msjzbBZEyb8JGtw7CBEwKcGL5g")
+)
 print(bal_obj_2)
+
+
+def get_address_pair(mint, hashed_eth_pk, program_id, spl_token_id):
+    base_pk, base_seed = get_base_address(mint, program_id)
+    derived_pk, derive_seed = get_derived_address(base_pk, hashed_eth_pk, spl_token_id)
+    return [(base_pk, base_seed), (derived_pk, derive_seed)]
+
+
+def get_base_address(mint, program_id):
+    return PublicKey.find_program_address([bytes(mint)], program_id)
+
+
+def get_derived_address(base, hashed_eth_pk, spl_token_id):
+    seed = base58.b58encode(hashed_eth_pk).decode()
+    return PublicKey.create_with_seed(base, seed, spl_token_id), seed
+
+
+mint = PublicKey(1)
+hashed_eth_pk = b""
+program_id = PublicKey(1)
+spl_token_id = PublicKey(1)
+
+
+print(get_address_pair(mint, hashed_eth_pk, program_id, spl_token_id))
