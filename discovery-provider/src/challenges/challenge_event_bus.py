069ac447ae40e224fd09fac3ed0d10012b73e4b0
Michael Piazza <michael.piazza.mp@gmail.com>
2021-06-18 19:11:49 +0000

    Fix challenges issues

--- discovery-provider/src/challenges/challenge_event_bus.py
+++ discovery-provider/src/challenges/challenge_event_bus.py
@@ -1,17 +1,15 @@
 import json
-import enum
 import logging
+from flask import current_app
+from src.utils.redis_connection import get_redis
+from src.challenges.profile_challenge import profile_challenge_manager
+from src.challenges.challenge_event import ChallengeEvent
 
 from collections import defaultdict
 
 logger = logging.getLogger(__name__)
 REDIS_QUEUE_PREFIX = 'challenges-event-queue'
 
-class ChallengeEvent(str, enum.Enum):
-    profile_update = 'profile_update'
-    repost = 'repost'
-    follow = 'follow'
-    favorite = 'favorite'
 
 class ChallengeEventBus:
     """`ChallengeEventBus` supports:
@@ -30,14 +28,16 @@ class ChallengeEventBus:
     def dispatch(self, session, event, block_number, user_id):
         """Dispatches an event + block_number + user_id to Redis queue"""
         event_json = self._event_to_json(event, block_number, user_id)
-        logger.debug(f"ChallengeEventBus: dispatch {event_json}")
+        logger.info(f"ChallengeEventBus: dispatch {event_json}")
         self._redis.rpush(REDIS_QUEUE_PREFIX, event_json)
 
     def process_events(self, session, max_events=1000):
-        """Dequeues `max_events` from Redis queue and processes them, forwarding to listening ChallengeManagers"""
-        # get the first max_events elements
+        """Dequeues `max_events` from Redis queue and processes them, forwarding to listening ChallengeManagers.
+        Returns the number of events it's processed.
+        """
+        # get the first max_events elements.
         events_json = self._redis.lrange(REDIS_QUEUE_PREFIX, 0, max_events)
-        logger.debug(f"ChallengeEventBus: dequeued {len(events_json)} events")
+        logger.info(f"ChallengeEventBus: dequeued {len(events_json)} events")
         # trim the first from the front of the list
         self._redis.ltrim(REDIS_QUEUE_PREFIX, len(events_json), -1)
         events_dicts = list(map(self._json_to_event, events_json))
@@ -57,6 +57,7 @@ class ChallengeEventBus:
             for listener in listeners:
                 listener.process(session, event_type, event_dicts)
 
+        return len(events_json)
     # Helpers
 
     def _event_to_json(self, event, block_number, user_id):
@@ -69,3 +70,20 @@ class ChallengeEventBus:
 
     def _json_to_event(self, event_json):
         return json.loads(event_json)
+
+def setup_challenge_bus():
+    redis = get_redis()
+    bus = ChallengeEventBus(redis)
+
+    # register listeners
+
+    # profile_challenge_mananger listeners
+    bus.register_listener(ChallengeEvent.profile_update, profile_challenge_manager)
+    bus.register_listener(ChallengeEvent.repost, profile_challenge_manager)
+    bus.register_listener(ChallengeEvent.follow, profile_challenge_manager)
+    bus.register_listener(ChallengeEvent.favorite, profile_challenge_manager)
+
+    return bus
+
+def get_challenge_bus():
+    return current_app.challenge_bus
