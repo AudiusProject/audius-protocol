70699d5c41226c3a8c273f246cc788d9d5699e34
Saliou Diallo <saliou@audius.co>
2021-06-21 13:28:44 -0400

    Add middleware to recover user handle if not set

--- identity-service/src/routes/captcha.js
+++ identity-service/src/routes/captcha.js
@@ -1,17 +1,15 @@
 const config = require('../config')
-const { handleResponse, successResponse, errorResponseBadRequest, errorResponseForbidden } = require('../apiHelpers')
+const { handleResponse, successResponse, errorResponseForbidden } = require('../apiHelpers')
 const { sequelize } = require('../models')
+const { userHandleMiddleware } = require('../userHandleMiddleware')
 
 module.exports = function (app) {
-  app.get('/scores', handleResponse(async req => {
-    const handle = req.query.handle
-    if (!handle) {
-      return errorResponseBadRequest('Please provide handle.')
-    }
-
+  app.get('/scores', userHandleMiddleware, handleResponse(async req => {
     if (req.headers['x-captcha-score'] !== config.get('captchaScoreSecret')) {
       return errorResponseForbidden('Not permissioned to view captcha scores.')
     }
+
+    const handle = req.query.handle
 
     const recaptchaEntries = await sequelize.query(
       `select "Users"."blockchainUserId" as "userId", "Users"."walletAddress" as "wallet", "BotScores"."recaptchaScore" as "score", "BotScores"."recaptchaContext" as "context"
