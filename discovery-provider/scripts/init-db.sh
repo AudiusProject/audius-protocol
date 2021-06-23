c4f12a3b8df5be2bf11d6fea68c1e400141c26ec
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-06-23 10:07:10 -0400

    Fix issue with postgres launching

--- discovery-provider/scripts/init-db.sh
+++ discovery-provider/scripts/init-db.sh
@@ -4,3 +4,15 @@ mkdir /db
 chown postgres:postgres /db
 chmod 700 /db
 sudo -u postgres initdb -D /db
+
+mkdir /run/postgresql
+chown -R postgres:postgres /run/postgresql
+
+sudo -u postgres postgres -D /db -h 127.0.0.1 &
+postgresPid="$!"
+export WAIT_HOSTS="localhost:5432"
+/wait
+
+sudo -u postgres createdb audius_discovery
+
+kill $postgresPid
