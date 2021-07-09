a6c549cc2f1f825b0fe53fc304f93c3773a2ea8f
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-07-09 10:04:08 -0400

    Add similar flow for CN

--- creator-node/scripts/start.sh
+++ creator-node/scripts/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 if [ -z "$ipfsHost" ]; then
+    if [ -z "$(ls -A /root/.ipfs)" ]; then
+        ipfs init --profile server
+    fi
+
     ipfs daemon &
     export ipfsHost=localhost
     export ipfsPort=5001
@@ -17,8 +21,21 @@ if [ -z "$redisHost" ]; then
 fi
 
 if [ -z "$dbUrl" ]; then
-    sudo -u postgres pg_ctl start -D /db
-    export dbUrl=postgresql://postgres:postgres@localhost:5432/audius_creator_node
+    if [ -z "$(ls -A /db)" ]; then
+        chown -R postgres:postgres /db
+        chmod 700 /db
+        sudo -u postgres pg_ctl init -D /db
+        echo "host all all 0.0.0.0/0 md5" >>/db/pg_hba.conf
+        echo "listen_addresses = '*'" >>/db/postgresql.conf
+        sudo -u postgres pg_ctl start -D /db
+        sudo -u postgres createdb audius_creator_node
+    else
+        sudo -u postgres pg_ctl start -D /db
+    fi
+
+    sudo -u postgres psql -c "ALTER USER postgres PASSWORD '${postgres_password:-postgres}';"
+
+    export dbUrl="postgresql://postgres:${postgres_password:-postgres}@localhost:5432/audius_creator_node"
     export WAIT_HOSTS="localhost:5432"
     /usr/bin/wait
 fi
