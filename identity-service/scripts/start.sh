f31c49cac512d5f6e3074f22f408aca0842c4faa
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-07-09 10:05:10 -0400

    Add same flow for Identity

--- identity-service/scripts/start.sh
+++ identity-service/scripts/start.sh
@@ -9,8 +9,21 @@ if [ -z "$redisHost" ]; then
 fi
 
 if [ -z "$dbUrl" ]; then
-    sudo -u postgres pg_ctl start -D /db
-    export dbUrl=postgresql://postgres:postgres@localhost:5432/audius_identity_service
+    if [ -z "$(ls -A /db)" ]; then
+        chown -R postgres:postgres /db
+        chmod 700 /db
+        sudo -u postgres pg_ctl init -D /db
+        echo "host all all 0.0.0.0/0 md5" >>/db/pg_hba.conf
+        echo "listen_addresses = '*'" >>/db/postgresql.conf
+        sudo -u postgres pg_ctl start -D /db
+        sudo -u postgres createdb audius_identity_service
+    else
+        sudo -u postgres pg_ctl start -D /db
+    fi
+
+    sudo -u postgres psql -c "ALTER USER postgres PASSWORD '${postgres_password:-postgres}';"
+
+    export dbUrl="postgresql://postgres:${postgres_password:-postgres}@localhost:5432/audius_identity_service"
     export WAIT_HOSTS="localhost:5432"
     /usr/bin/wait
 fi
