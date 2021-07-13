2a913d122325b7468d5b2d91a289ac99be51d25a
Sebastian Klingler <sliptype@gmail.com>
2021-07-13 16:42:36 -0500

    Aud-754 tiktok oauth (#1639)
    * Add initial route for tiktok oauth
    
    * Add route for fetching access token
    
    * Set up cors and double submit cookie
    
    * Return transcodedTrackCID from upload
    
    * Rate limit tiktok auth requests
    
    * Fix CORS issue
    
    * Add env var default values
    
    * Fix lint issues
    
    * Fix lint issues, round 2
    
    * Fix lint issues, round 3 ("The Charm")

--- identity-service/sequelize/migrations/20210707144138-protocol-service-provider.js
+++ identity-service/sequelize/migrations/20210707144138-protocol-service-provider.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -26,4 +26,4 @@ module.exports = {
   down: (queryInterface) => {
     return queryInterface.dropTable('ProtocolServiceProviders')
   }
-};
+}
