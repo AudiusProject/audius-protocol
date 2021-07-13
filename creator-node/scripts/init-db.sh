708836f8b3069cbdc05b17dbb0fc05f5cae6bad7
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-07-13 14:12:36 -0400

    Remove changes to creator and identity

--- creator-node/scripts/init-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-adduser --disabled-password postgres
-
-mkdir /run/postgresql /db
-chown -R postgres:postgres /run/postgresql /db
-chmod 700 /db
--- identity-service/scripts/init-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir /run/postgresql /db
-chown -R postgres:postgres /run/postgresql /db
-chmod 700 /db
