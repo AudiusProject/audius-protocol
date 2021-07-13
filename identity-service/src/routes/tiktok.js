0ea019019862c80836593909fdf6496abb084c1d
Sebastian Klingler <sliptype@gmail.com>
2021-07-13 20:11:38 +0000

    Fix lint issues

--- identity-service/src/routes/tiktok.js
+++ identity-service/src/routes/tiktok.js
@@ -1,12 +1,12 @@
-const axios = require("axios");
-const cors = require("cors");
-const config = require("../config.js");
+const axios = require('axios')
+const cors = require('cors')
+const config = require('../config.js')
 
 const {
   handleResponse,
   successResponse,
-  errorResponseBadRequest,
-} = require("../apiHelpers");
+  errorResponseBadRequest
+} = require('../apiHelpers')
 
 /**
  * This file contains the TikTok endpoints for oauth
@@ -15,49 +15,49 @@ const {
  */
 module.exports = function (app) {
   app.get(
-    "/tiktok",
+    '/tiktok',
     handleResponse(async (req, res, next) => {
-      const csrfState = Math.random().toString(36).substring(7);
-      res.cookie("csrfState", csrfState, { maxAge: 60000 });
+      const csrfState = Math.random().toString(36).substring(7)
+      res.cookie('csrfState', csrfState, { maxAge: 60000 })
 
-      let url = "https://open-api.tiktok.com/platform/oauth/connect/";
+      let url = 'https://open-api.tiktok.com/platform/oauth/connect/'
 
-      url += `?client_key=${config.get("tikTokAPIKey")}`;
-      url += "&scope=user.info.basic,share.sound.create";
-      url += "&response_type=code";
-      url += `&redirect_uri=${config.get("tikTokAuthOrigin")}`;
-      url += "&state=" + csrfState;
+      url += `?client_key=${config.get('tikTokAPIKey')}`
+      url += '&scope=user.info.basic,share.sound.create'
+      url += '&response_type=code'
+      url += `&redirect_uri=${config.get('tikTokAuthOrigin')}`
+      url += '&state=' + csrfState
 
-      res.redirect(url);
+      res.redirect(url)
     })
-  );
+  )
 
   const accessTokenCorsOptions = {
     credentials: true,
-    origin: config.get("tikTokAuthOrigin"),
-  };
+    origin: config.get('tikTokAuthOrigin')
+  }
 
-  app.options("/tiktok/access_token", cors(accessTokenCorsOptions));
+  app.options('/tiktok/access_token', cors(accessTokenCorsOptions))
   app.post(
-    "/tiktok/access_token",
+    '/tiktok/access_token',
     cors(accessTokenCorsOptions),
     handleResponse(async (req, res, next) => {
-      const { code, state } = req.body;
-      const { csrfState } = req.cookies;
+      const { code, state } = req.body
+      const { csrfState } = req.cookies
 
       if (!state || !csrfState || state !== csrfState) {
-        return errorResponseBadRequest("Invalid state");
+        return errorResponseBadRequest('Invalid state')
       }
 
-      let urlAccessToken = "https://open-api.tiktok.com/oauth/access_token/";
-      urlAccessToken += "?client_key=" + config.get("tikTokAPIKey");
-      urlAccessToken += "&client_secret=" + config.get("tikTokAPISecret");
-      urlAccessToken += "&code=" + code;
-      urlAccessToken += "&grant_type=authorization_code";
+      let urlAccessToken = 'https://open-api.tiktok.com/oauth/access_token/'
+      urlAccessToken += '?client_key=' + config.get('tikTokAPIKey')
+      urlAccessToken += '&client_secret=' + config.get('tikTokAPISecret')
+      urlAccessToken += '&code=' + code
+      urlAccessToken += '&grant_type=authorization_code'
 
-      const resp = await axios.post(urlAccessToken);
+      const resp = await axios.post(urlAccessToken)
 
-      return successResponse(resp);
+      return successResponse(resp)
     })
-  );
-};
+  )
+}
