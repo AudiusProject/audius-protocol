d0c9763fd4f81eada9cba29f84b7f75796143931
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-07-14 22:17:26 +0000

    Cleanup + bugfix
    Bugfix
    
    Discovery undisbursed endpoint for rewards (#1642)
    
    * Add undisbursed challenges endpoint
    
    * Fix lint in discovery
    
    * Add undisbursed endpoint route
    
    * Fix limit and endpoint name
    
    * Addressed comments
    
    * Fix lint
    
    * Rebase changes
    
    * Update discovery test around fixture
    
    * Pass through arg
    
    * Address comments
    
    * Remove unused imports lint
    
    Support rewards challenge fields (#1636)
    
    * Support UserEvents in discovery indexing
    
    * Support referrer and is_mobile_user in libs
    
    * Add events to tasks metadata
    
    * Address comments
    
    * Fix misc type issues
    
    * Remove unused imports
    
    * Fix get_attestation type bug
    
    * Fix lint
    
    * Update down revision
    
    Debugging
    
    Debugging
    
    Circle config update
    
    config
    
    Debug
    
    Debug
    
    Debug
    
    Consolidate some logic
    
    Consolidate some logic
    
    Final cleanup + polish

--- creator-node/src/monitors/syncHistory.js
+++ creator-node/src/monitors/syncHistory.js
@@ -1,60 +1,52 @@
 const SyncHistoryAggregator = require('../snapbackSM/syncHistoryAggregator')
 
-const get30DayRollingSyncSuccessCount = async () => {
+/**
+ * Get the total number of syncs that met 'status' per wallet per day for the given number of days
+ * @param {String} status Either 'success' or 'fail'
+ * @param {Number} days number of historical days to get data for
+ * @returns {Number} number of syncs per wallet per day that met the status
+ */
+const _getRollingSyncCount = async (status, days) => {
+  if (status !== 'success' && status !== 'fail') throw new Error('Invalid status for _getRollingSyncCount: must be "success" or "fail"')
+
   // Get the start and end dates of the rolling window
   const today = new Date()
   const rollingWindowStartDate = new Date(
-    today.setDate(today.getDate() - 30)
+    today.setDate(today.getDate() - days)
   )
   const rollingWindowEndDate = new Date()
 
-  // Get the success count from the date within the rolling window range
-  let rollingSyncSuccessCount = 0
+  // Get the count for this status from the date within the rolling window range
+  let rollingSyncStatusCount = 0
   let date = rollingWindowStartDate
 
   while (date <= rollingWindowEndDate) { // eslint-disable-line no-unmodified-loop-condition
     const redisDateKeySuffix = date.toISOString().split('T')[0] // ex.: "2021-05-04"
-    const { success } = SyncHistoryAggregator.getPerWalletSyncData(redisDateKeySuffix, {})
-    rollingSyncSuccessCount += success
+    const resp = await SyncHistoryAggregator.getDailyWalletSyncData(redisDateKeySuffix)
+    rollingSyncStatusCount += parseInt(resp[status], 10) || 0
 
     // Set the date to the next day
     date.setDate(date.getDate() + 1)
   }
 
-  return rollingSyncSuccessCount
+  return rollingSyncStatusCount
+}
+
+const get30DayRollingSyncSuccessCount = async () => {
+  return _getRollingSyncCount('success', 30)
 }
 
 const get30DayRollingSyncFailCount = async () => {
-  // Get the start and end dates of the rolling window
-  const today = new Date()
-  const rollingWindowStartDate = new Date(
-    today.setDate(today.getDate() - 30)
-  )
-  const rollingWindowEndDate = new Date()
-
-  // Get the fail count from the date within the rolling window range
-  let rollingSyncFailCount = 0
-  let date = rollingWindowStartDate
-
-  while (date <= rollingWindowEndDate) { // eslint-disable-line no-unmodified-loop-condition
-    const redisDateKeySuffix = date.toISOString().split('T')[0] // ex.: "2021-05-04"
-    const { fail } = SyncHistoryAggregator.getPerWalletSyncData(redisDateKeySuffix, {})
-    rollingSyncFailCount += fail
-
-    // Set the date to the next day
-    date.setDate(date.getDate() + 1)
-  }
-
-  return rollingSyncFailCount
+  return _getRollingSyncCount('fail', 30)
 }
 
 const getDailySyncSuccessCount = async () => {
-  const { success } = await SyncHistoryAggregator.getPerWalletSyncData()
+  const { success } = await SyncHistoryAggregator.getDailyWalletSyncData()
   return success
 }
 
 const getDailySyncFailCount = async () => {
-  const { fail } = await SyncHistoryAggregator.getPerWalletSyncData()
+  const { fail } = await SyncHistoryAggregator.getDailyWalletSyncData()
   return fail
 }
 
