809548a441eb31f2b5c1fb3d1e74f0081b8d80d3
Saliou Diallo <sddioulde@hotmail.com>
2021-07-16 11:29:23 -0400

    Add endpoint to save hCaptcha verification (#1652)
    Co-authored-by: Saliou Diallo <saliou@audius.co>

--- identity-service/src/routes/captcha.js
+++ identity-service/src/routes/captcha.js
@@ -1,8 +1,11 @@
 const config = require('../config')
-const { handleResponse, successResponse, errorResponseForbidden } = require('../apiHelpers')
+const { handleResponse, successResponse, errorResponseForbidden, errorResponseBadRequest, errorResponseServerError } = require('../apiHelpers')
 const models = require('../models')
 const { QueryTypes } = require('sequelize')
 const userHandleMiddleware = require('../userHandleMiddleware')
+const authMiddleware = require('../authMiddleware')
+const { verify } = require('hcaptcha')
+const HCAPTCHA_SECRET = config.get('hCaptchaSecret')
 
 module.exports = function (app) {
   app.get('/scores', userHandleMiddleware, handleResponse(async req => {
@@ -25,5 +28,43 @@ module.exports = function (app) {
     )
 
     return successResponse(recaptchaEntries)
+  }))
+
+  app.post('/score', authMiddleware, handleResponse(async req => {
+    const user = req.user
+    const token = req.body.token
+
+    if (!token) {
+      return errorResponseBadRequest('Please provide hCaptcha token.')
+    }
+
+    try {
+      const { success, hostname } = await verify(HCAPTCHA_SECRET, token)
+
+      if (!success) {
+        return errorResponseServerError('hCaptcha Verification failed.')
+      }
+
+      // save hCaptcha score for user in BotScores
+      await models.BotScores.create({
+        walletAddress: user.walletAddress,
+        // hCaptcha scores represent risk scores
+        // the score values go from 0 to 1
+        // the higher the score the worse it is
+        // e.g. 0 is 'safe' and 1 is 'bot'
+        // given we are using the free publisher tier of hCaptcha, we only get the binary success/fail and no score value
+        // therefore we use the score of 1 to denote that verification was ok and remain consistent with our other scores
+        recaptchaScore: 1,
+        // the recaptcha context is very important because that's how we know whether
+        // the score is from hCaptcha, meaning 0 is best
+        // or if it's from our other recpatcha scores, where 1 is best
+        recaptchaContext: 'hCaptcha',
+        recaptchaHostname: hostname
+      })
+      return successResponse({})
+    } catch (err) {
+      console.error(err)
+      return errorResponseServerError(err.message)
+    }
   }))
 }
