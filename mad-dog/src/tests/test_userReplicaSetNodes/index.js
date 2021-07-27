ecd89c647e0577440d9c9e875063f3f2738e33f5
vicky <vicky@audius.co>
2021-07-27 21:51:06 +0000

    wip

--- mad-dog/src/tests/test_userReplicaSetNodes/index.js
+++ mad-dog/src/tests/test_userReplicaSetNodes/index.js
@@ -1,9 +1,10 @@
 const { addAndUpgradeUsers } = require('../../helpers.js')
 const verifyValidCNs = require('./verifyValidCN')
-// const deregisterRandomCreatorNode = require('./deregisterRandomCreatorNode.js')
+const deregisterRandomCreatorNode = require('./deregisterRandomCreatorNode.js')
 const stopRandomCreatorNode = require('./stopRandomCreatorNode.js')
 const setNumCreatorNodes = require('./setNumCreatorNodes.js')
 const { uploadTracksforUsers } = require('../../utils/uploadTracksForUsers')
+const { logger } = require('../../../../creator-node/src/logging.js')
 
 const MAX_ATTEMPTS_TO_VALIDATE_REPLICA_SET = 10
 const WAIT_INTERVAL_TO_UPDATE_REPLICA_SET_MS = 20000
@@ -40,23 +41,41 @@ const userReplicaSetNodes = async ({
 
     // TODO: Implement spID as source of truth feature before uncommenting deregistration test code
 
-    // const deregisteredCreatorNodeId = await deregisterRandomCreatorNode(creatorNodeIDToInfoMapping)
-
-    // await new Promise(resolve => setTimeout(resolve, 10 * 1000))
-    // await verifyValidCNs(executeOne, executeAll, deregisteredCreatorNodeId, walletIndexToUserIdMap, creatorNodeIDToInfoMapping)
+    const deregisteredCreatorNodeId = await deregisterRandomCreatorNode(creatorNodeIDToInfoMapping)
 
     // Create a MadDog instance, responsible for taking down 1 node
-    const {
-      madDog,
-      removedCreatorNodeId
-    } = await stopRandomCreatorNode(creatorNodeIDToInfoMapping)
-
     let attempts = 0
     let passed = false
     let error
     while (attempts++ < MAX_ATTEMPTS_TO_VALIDATE_REPLICA_SET) {
       await new Promise(resolve => setTimeout(resolve, WAIT_INTERVAL_TO_UPDATE_REPLICA_SET_MS))
       try {
+        await verifyValidCNs(executeOne, executeAll, deregisteredCreatorNodeId, walletIndexToUserIdMap, creatorNodeIDToInfoMapping)
+        passed = true
+        break
+      } catch (e) {
+        error = e
+      }
+    }
+
+    if (!passed) {
+      return {
+        error: `[Deregistering] Error with verifying updated replica set: ${error.toString()}`
+      }
+    }
+
+    const {
+      madDog,
+      removedCreatorNodeId
+    } = await stopRandomCreatorNode(creatorNodeIDToInfoMapping)
+
+    error = null
+    attempts = 0
+    passed = false
+    while (attempts++ < MAX_ATTEMPTS_TO_VALIDATE_REPLICA_SET) {
+      await new Promise(resolve => setTimeout(resolve, WAIT_INTERVAL_TO_UPDATE_REPLICA_SET_MS))
+      try {
+        logger.info(`attempt ${attempts}`)
         await verifyValidCNs(executeOne, executeAll, removedCreatorNodeId, walletIndexToUserIdMap, creatorNodeIDToInfoMapping)
         passed = true
         break
@@ -67,7 +86,7 @@ const userReplicaSetNodes = async ({
 
     if (!passed) {
       return {
-        error: `Error with verifying updated replica set: ${error.toString()}`
+        error: `[Unavailability] Error with verifying updated replica set: ${error.toString()}`
       }
     }
 
