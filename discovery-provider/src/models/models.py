c1b8be5d5d2874002a6c99c3774791f7a85b1674
Marcus Pasell <rickyrombo@users.noreply.github.com>
2021-07-29 10:53:48 -0700

    Revert "Add permalink to Track model to ensure it's always present (#1692)"
    This reverts commit d035987a44662f6523ab418f9357fdf572f97198.

--- discovery-provider/src/models/models.py
+++ discovery-provider/src/models/models.py
@@ -261,24 +261,9 @@ class Track(Base):
         viewonly=True,
     )
 
-    user = relationship(
-        "User",
-        primaryjoin="and_(\
-            remote(Track.owner_id) == foreign(User.user_id),\
-            User.is_current)",
-        lazy="joined",
-        viewonly=True,
-    )
-
     @property
-    def _slug(self):
+    def slug(self):
         return self._routes[0].slug if self._routes else ""
-
-    @property
-    def permalink(self):
-        if self.user and self.user[0].handle and self._slug:
-            return f"/{self.user[0].handle}/{self._slug}"
-        return ""
 
     PrimaryKeyConstraint(is_current, track_id, blockhash, txhash)
 
