945fa06a51493a937c6b25f5c96f1ad0dbcdbaa7
Sebastian Klingler <sliptype@gmail.com>
2021-07-30 18:34:15 -0500

    Aud 792 tiktok oauth (#95)
    * Update deps to fix build
    
    * Add .env.dev to .gitignore
    
    * Update readme
    
    * Set up tiktok oauth
    
    * Get TikTok sharing working e2e
    
    * Fix canceling of auth
    
    * Fix styling of close button
    
    * Bump builds (#96)
    
    Co-authored-by: Raymond Jacobson <ray@audius.co>

--- apps/audius-client/packages/mobile/src/store/oauth/selectors.ts
+++ apps/audius-client/packages/mobile/src/store/oauth/selectors.ts
@@ -5,4 +5,6 @@ const getBaseState = (state: AppState) => state.oauth
 export const getIsOpen = (state: AppState) => getBaseState(state).isOpen
 export const getUrl = (state: AppState) => getBaseState(state).url
 export const getMessageId = (state: AppState) => getBaseState(state).messageId
+export const getMessageType = (state: AppState) =>
+  getBaseState(state).messageType
 export const getAuthProvider = (state: AppState) => getBaseState(state).provider
