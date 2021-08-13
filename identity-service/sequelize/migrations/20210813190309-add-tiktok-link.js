9ba804b85a0b4ffc0f22c116044b7c8bb3a1f737
Sebastian Klingler <sliptype@gmail.com>
2021-08-13 21:12:50 +0000

    Add tiktok to social handles

---
+++ identity-service/sequelize/migrations/20210813190309-add-tiktok-link.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * Adds a 'tikTok' field to SocialHandles
+ */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        "SocialHandles",
+        "tikTokHandle",
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction }
+      );
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      queryInterface.removeColumn("SocialHandles", "tikTokHandle");
+    });
+  },
+};
