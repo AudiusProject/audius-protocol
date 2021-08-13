9194bbc4937dfb31f49e757045fbe72fe2455c79
Sebastian Klingler <sliptype@gmail.com>
2021-08-13 21:26:52 +0000

    Fix lint

--- identity-service/sequelize/migrations/20210813190309-add-tiktok-link.js
+++ identity-service/sequelize/migrations/20210813190309-add-tiktok-link.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 /**
  * Adds a 'tikTok' field to SocialHandles
@@ -7,20 +7,20 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (transaction) => {
       await queryInterface.addColumn(
-        "SocialHandles",
-        "tikTokHandle",
+        'SocialHandles',
+        'tikTokHandle',
         {
           type: Sequelize.STRING,
-          allowNull: true,
+          allowNull: true
         },
         { transaction }
-      );
-    });
+      )
+    })
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (transaction) => {
-      queryInterface.removeColumn("SocialHandles", "tikTokHandle");
-    });
-  },
-};
+      queryInterface.removeColumn('SocialHandles', 'tikTokHandle')
+    })
+  }
+}
