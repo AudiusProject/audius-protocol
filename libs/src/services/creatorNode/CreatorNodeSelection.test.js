825413a75f91219fdca151822bee0ee441d8000c
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-08-13 22:04:12 +0000

    Remove service selection changes

--- libs/src/services/creatorNode/CreatorNodeSelection.test.js
+++ libs/src/services/creatorNode/CreatorNodeSelection.test.js
@@ -523,49 +523,4 @@ describe('test CreatorNodeSelection', () => {
     const healthyServices = [shouldBePrimary, shouldBeSecondary1, shouldBeSecondary2]
     healthyServices.map(service => assert(returnedHealthyServices.has(service)))
   })
-
-  it('correctly sorts healthy services by transcode queue size', async () => {
-    let healthCheckData = {
-      ...defaultHealthCheckData
-    }
-
-    const shouldBePrimary = 'https://noTranscodeTasks.audius.co'
-    nock(shouldBePrimary)
-      .get('/health_check/verbose')
-      .reply(200, { data: { ...healthCheckData, transcodeActive: 0, transcodeWaiting: 0 } })
-
-    const shouldBeSecondary1 = 'https://mostTranscodeTasks.audius.co'
-    nock(shouldBeSecondary1)
-      .get('/health_check/verbose')
-      .reply(200, { data: { ...healthCheckData, transcodeActive: 8, transcodeWaiting: 0 } })
-
-    const shouldBeSecondary2 = 'https://mediumTranscodeTasks.audius.co'
-    nock(shouldBeSecondary2)
-      .get('/health_check/verbose')
-      .reply(200, { data: { ...healthCheckData, transcodeActive: 4, transcodeWaiting: 0 } })
-
-    const cns = new CreatorNodeSelection({
-      creatorNode: mockCreatorNode,
-      numberOfNodes: 3,
-      ethContracts: mockEthContracts(
-        [shouldBeSecondary2, shouldBeSecondary1, shouldBePrimary],
-        '1.2.3'
-      ),
-      whitelist: null,
-      blacklist: null,
-      maxStorageUsedPercent: 50
-    })
-
-    const { primary, secondaries, services } = await cns.select()
-
-    assert(primary === shouldBePrimary)
-    assert(secondaries.length === 2)
-    assert(secondaries.includes(shouldBeSecondary1))
-    assert(secondaries.includes(shouldBeSecondary2))
-
-    const returnedHealthyServices = new Set(Object.keys(services))
-    assert(returnedHealthyServices.size === 3)
-    const healthyServices = [shouldBePrimary, shouldBeSecondary1, shouldBeSecondary2]
-    healthyServices.map(service => assert(returnedHealthyServices.has(service)))
-  })
 })
