1fb08fd157f43980b417f5140573d30d89cd9fda
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-08-24 11:22:49 -0400

    Fix imports

--- discovery-provider/tests/test_index_users.py
+++ discovery-provider/tests/test_index_users.py
@@ -6,9 +6,6 @@ from src.challenges.challenge_event import ChallengeEvent
 from src.database_task import DatabaseTask
 from src.models import AssociatedWallet, UserEvents
 from src.tasks.users import get_ipfs_metadata, lookup_user_record, parse_user_event
-from src.utils.db_session import get_db
-from src.challenges.challenge_event_bus import ChallengeEventBus, setup_challenge_bus
-from src.utils.user_event_constants import user_event_types_lookup
 from src.utils import helpers
 from src.utils.db_session import get_db
 from src.utils.redis_connection import get_redis
