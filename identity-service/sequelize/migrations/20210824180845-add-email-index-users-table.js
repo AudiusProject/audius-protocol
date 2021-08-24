84b9c8059527dd10f35ccd973f32fc1cda387bb5
Sebastian Klingler <sliptype@gmail.com>
2021-08-24 19:00:03 +0000

    Fix lint

--- identity-service/sequelize/migrations/20210824180845-add-email-index-users-table.js
+++ identity-service/sequelize/migrations/20210824180845-add-email-index-users-table.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addIndex("Users", ["email"]);
+    return queryInterface.addIndex('Users', ['email'])
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeIndex("Users", ["email"]);
-  },
-};
+    return queryInterface.removeIndex('Users', ['email'])
+  }
+}
