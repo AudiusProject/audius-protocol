af19fa545f32b6fb6ef55ce561657eee476cddba
Saliou Diallo <saliou@audius.co>
2021-08-26 19:21:34 -0400

    Use multiple endpoints for solana client requests and try all of them before failure

--- discovery-provider/src/utils/solana_client_manager.py
+++ discovery-provider/src/utils/solana_client_manager.py
@@ -1,3 +1,4 @@
+import random
 import time
 import logging
 from typing import Optional, Union
@@ -8,7 +9,7 @@ from src.utils.config import shared_config
 
 logger = logging.getLogger(__name__)
 
-SOLANA_ENDPOINT = shared_config["solana"]["endpoint"]
+SOLANA_ENDPOINTS = shared_config["solana"]["endpoint"]
 
 # maximum number of times to retry get_confirmed_transaction call
 DEFAULT_MAX_RETRIES = 5
@@ -18,30 +19,41 @@ DELAY_SECONDS = 0.2
 
 class SolanaClientManager:
     def __init__(self) -> None:
-        self.client = Client(SOLANA_ENDPOINT)
+        self.endpoints = SOLANA_ENDPOINTS.split(",")
+        self.clients = [Client(endpoint) for endpoint in self.endpoints]
 
     def get_client(self) -> Client:
-        return self.client
+        index = random.randrange(0, len(self.clients))
+        return self.clients[index]
 
     def get_sol_tx_info(self, tx_sig: str, retries=DEFAULT_MAX_RETRIES):
         """Fetches a solana transaction by signature with retries and a delay
 
         If not found, raise an exception
         """
-        while retries > 0:
-            try:
-                tx_info = self.client.get_confirmed_transaction(tx_sig)
-                if tx_info["result"] is not None:
-                    return tx_info
-            except Exception as e:
+        for index, client in random.sample(
+            list(enumerate(self.clients)), k=len(self.clients)
+        ):
+            endpoint = self.endpoints[index]
+            num_retries = retries
+            while num_retries > 0:
+                try:
+                    tx_info = client.get_confirmed_transaction(tx_sig)
+                    if tx_info["result"] is not None:
+                        return tx_info
+                except Exception as e:
+                    logger.error(
+                        f"solana_client_manager.py | get_sol_tx_info | Error fetching tx {tx_sig} from endpoint {endpoint}, {e}",
+                        exc_info=True,
+                    )
+                num_retries -= 1
+                time.sleep(DELAY_SECONDS)
                 logger.error(
-                    f"get_sol_tx_info | Error fetching tx {tx_sig}, {e}",
-                    exc_info=True,
+                    f"solana_client_manager.py | get_sol_tx_info | Retrying tx fetch: {tx_sig} with endpoint {endpoint}"
                 )
-            retries -= 1
-            time.sleep(DELAY_SECONDS)
-            logger.error(f"get_sol_tx_info | Retrying tx fetch: {tx_sig}")
-        raise Exception(f"get_sol_tx_info | Failed to fetch {tx_sig}")
+        raise Exception(
+            "solana_client_manager.py | get_sol_tx_info | All requests failed to fetch {tx_sig}"
+        )
 
     def get_confirmed_signature_for_address2(
         self,
@@ -49,5 +61,16 @@ class SolanaClientManager:
         before: Optional[str] = None,
         limit: Optional[int] = None,
     ):
-        """Pass-through for get_confirmed_signature_for_address2"""
-        return self.client.get_confirmed_signature_for_address2(account, before, limit)
+        """Fetches confirmed signatures for transactions given an address.
+        Iterates randomly across all clients before failure.
+        """
+        for client in random.sample(self.clients, k=len(self.clients)):
+            try:
+                return client.get_confirmed_signature_for_address2(
+                    account, before, limit
+                )
+            except Exception:
+                continue
+        raise Exception(
+            "solana_client_manager.py | get_confirmed_signature_for_address2 | All requests failed"
+        )
