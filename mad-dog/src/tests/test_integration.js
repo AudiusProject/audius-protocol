0230ad19c145cacb09c7275c55f9969c79f4ce3d
Isaac Solo <isaac@audius.co>
2021-08-30 08:55:24 -0700

    Add repost to core integration test (#1806)
    * Add repost to core integration test
    
    * Repost tracks from other users and formatting
    
    * Add verifications back
    
    * Add test summary and rreturn epost error
    
    * Fix transaction errors from repostiagain

--- mad-dog/src/tests/test_integration.js
+++ mad-dog/src/tests/test_integration.js
@@ -1,7 +1,9 @@
 const {
   OPERATION_TYPE,
   TrackUploadRequest,
-  TrackUploadResponse
+  TrackUploadResponse,
+  TrackRepostRequest,
+  TrackRepostResponse
 } = require('../operations.js')
 
 const path = require('path')
@@ -22,9 +24,10 @@ const {
   upgradeUsersToCreators,
   delay
 } = require('../helpers.js')
-const { getContentNodeEndpoints } = require('@audius/service-commands')
+const { getContentNodeEndpoints, getRepostersForTrack } = require('@audius/service-commands')
 const {
   uploadTrack,
+  repostTrack,
   getTrackMetadata,
   getUser,
   getUsers,
@@ -37,11 +40,14 @@ const {
 
 // NOTE - # of ticks = (TEST_DURATION_SECONDS / TICK_INTERVAL_SECONDS) - 1
 const TICK_INTERVAL_SECONDS = 5
-const TEST_DURATION_SECONDS = 10
+const TEST_DURATION_SECONDS = 30
 const TEMP_STORAGE_PATH = path.resolve('./local-storage/tmp/')
 
 const SECOND_USER_PIC_PATH = path.resolve('assets/images/duck.jpg')
 const THIRD_USER_PIC_PATH = path.resolve('assets/images/sid.png')
+const repostedTracks = []
+const uploadedTracks = []
+const userRepostedMap = {}
 
 /**
  * Randomly uploads tracks over the duration of the test,
@@ -95,23 +101,26 @@ module.exports = coreIntegration = async ({
     testDurationSeconds: TEST_DURATION_SECONDS
   })
 
+  const tracksAttemptedRepost = []
+
   // Register the request listener. The only request type this test
   // currently handles is to upload tracks.
   emitterTest.registerOnRequestListener(async (request, emit) => {
     const { type, walletIndex, userId } = request
+    let res
     switch (type) {
       case OPERATION_TYPE.TRACK_UPLOAD: {
         const track = getRandomTrackMetadata(userId)
 
         const randomTrackFilePath = await getRandomTrackFilePath(TEMP_STORAGE_PATH)
 
-        let res
         try {
           // Execute a track upload request against a single
           // instance of libs.
           const trackId = await executeOne(walletIndex, l =>
             uploadTrack(l, track, randomTrackFilePath)
           )
+          uploadedTracks.push({ trackId: trackId, userId: userId })
           res = new TrackUploadResponse(walletIndex, trackId, track)
         } catch (e) {
           logger.error(`Caught error [${e.message}] uploading track: [${JSON.stringify(track)}]\n${e.stack}`)
@@ -122,6 +131,46 @@ module.exports = coreIntegration = async ({
             false,
             e.message
           )
+          throw new Error(e)
+        }
+        // Emit the response event
+        emit(Event.RESPONSE, res)
+        break
+      }
+      case OPERATION_TYPE.TRACK_REPOST: {
+        // repost candidates include tracks from other users that have not already been reposted by current user
+        const repostCandidates = uploadedTracks
+          .filter(obj => obj.userId !== userId)
+          .filter(obj => {
+            if (!userRepostedMap[obj.userId]) {
+              return true
+            }
+            return !userRepostedMap[obj.userId].includes(obj.trackId)
+          })
+          .map(obj => obj.trackId)
+        if (repostCandidates.length === 0) {
+          const missingTrackMessage = 'No tracks available to repost'
+          logger.info(missingTrackMessage)
+          res = new TrackRepostResponse(walletIndex, null, userId, false)
+        } else {
+          const trackId = repostCandidates.slice(-1)[0]
+          try {
+            tracksAttemptedRepost.push(trackId)
+            await executeOne(walletIndex, l => {
+              repostTrack(l, trackId)
+            })
+            res = new TrackRepostResponse(walletIndex, trackId, userId)
+          } catch (e) {
+            logger.error(`Caught error [${e.message}] reposting track: [${trackId}]\n${e.stack}`)
+            res = new TrackRepostResponse(
+              walletIndex,
+              trackId,
+              userId,
+              false,
+              e.message
+            )
+            throw new Error(e)
+          }
         }
         // Emit the response event
         emit(Event.RESPONSE, res)
@@ -150,13 +199,23 @@ module.exports = coreIntegration = async ({
           }
         } else {
           if (!walletTrackMap[walletIndex]) {
-            walletTrackMap[walletIndex] = {}
+            walletTrackMap[walletIndex] = []
           }
-
-          walletTrackMap[walletIndex] = {
-            [trackId]: metadata
+          if (trackId) { // only add successfully uploaded tracks
+            walletTrackMap[walletIndex].push(trackId)
           }
         }
+        break
+      }
+      case OPERATION_TYPE.TRACK_REPOST: {
+        const { walletIndex, trackId, userId, success } = res
+        if (success) {
+          repostedTracks.push({ trackId: trackId, userId: userId })
+        }
+        if (!userRepostedMap[userId]) {
+          userRepostedMap[userId] = []
+        }
+        userRepostedMap[userId].push(trackId)
         break
       }
       default:
@@ -168,11 +227,16 @@ module.exports = coreIntegration = async ({
   // of events.
   emitterTest.registerOnTickListener(emit => {
     const requesterIdx = _.random(0, numUsers - 1)
-    const request = new TrackUploadRequest(
+    const uploadRequest = new TrackUploadRequest(
       requesterIdx,
       walletIdMap[requesterIdx]
     )
-    emit(Event.REQUEST, request)
+    const repostRequest = new TrackRepostRequest(
+      requesterIdx,
+      walletIdMap[requesterIdx]
+    )
+    emit(Event.REQUEST, uploadRequest)
+    emit(Event.REQUEST, repostRequest)
   })
 
   // Create users. Upgrade them to creators later
@@ -261,7 +325,7 @@ module.exports = coreIntegration = async ({
     const userId = walletIdMap[walletIndex]
     const tracks = walletTrackMap[walletIndex]
     if (!tracks) continue
-    for (const trackId of Object.keys(tracks)) {
+    for (const trackId of tracks) {
       trackUploadInfo.push({
         walletIndex,
         trackId,
@@ -275,10 +339,21 @@ module.exports = coreIntegration = async ({
   if (!allCIDsExistOnCNodes) {
     return { error: 'Not all CIDs exist on creator nodes.' }
   }
+
   const failedWallets = Object.values(failedUploads)
   if (failedWallets.length) {
     const userIds = failedWallets.map(w => walletIdMap[w])
-    logger.error(`Uploads failed for user IDs=[${userIds}]`)
+    return {
+      error: `Uploads failed for user IDs=[${userIds}].`
+    }
+  }
+
+  const allTracksReposted = await verifyTracksReposted(executeOne)
+  if (!allTracksReposted) {
+    const tracksWithNoRepost = tracksAttemptedRepost.filter(obj => !repostedTracks.map(obj => obj.trackId).includes(obj)) // filter attempted reposts that were not successfully reposted
+    return {
+      error: `Tracks without a repost=[${tracksWithNoRepost}.`
+    }
   }
 
   // Switch user primary (above tests have already confirmed all secondaries have latest state)
@@ -361,6 +436,8 @@ module.exports = coreIntegration = async ({
     }
   }
 
+  printTestSummary()
+
   return {}
 }
 
@@ -414,6 +491,18 @@ const verifyAllCIDsExistOnCNodes = async (trackUploads, executeOne) => {
   }
   logger.info('Completed verifying CIDs')
   return !failedCIDs.length
+}
+
+async function verifyTracksReposted (executeOne) {
+  await executeOne(0, l => l.waitForLatestBlock())
+  for (const { trackId, userId } of repostedTracks) {
+    const reposters = await executeOne(0, l => getRepostersForTrack(l, trackId))
+    const usersReposted = reposters.map(obj => obj.user_id)
+    if (!usersReposted.includes(userId)) {
+      return false
+    }
+  }
+  return true
 }
 
 /**
@@ -509,4 +598,10 @@ async function checkMetadataEquality ({ endpoints, metadataMultihash, userId }) 
       )
     }
   })
+}
+
+const printTestSummary = () => {
+  logger.info('\n------------------------ AUDIUS CORE INTEGRATION TEST Summary ------------------------')
+  logger.info(`uploadedTracks: ${uploadedTracks.length}                | Total uploaded tracks`)
+  logger.info(`repostedTracks: ${repostedTracks.length}                | Total reposted tracks`)
 }
