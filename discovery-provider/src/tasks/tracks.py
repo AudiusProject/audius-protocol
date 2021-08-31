a3bef07c07acb6e049743c3ec1d653b2159c0eb6
Raymond Jacobson <ray@audius.co>
2021-08-30 22:50:31 -0700

    Fix

--- discovery-provider/src/tasks/tracks.py
+++ discovery-provider/src/tasks/tracks.py
@@ -263,6 +263,7 @@ def update_track_routes_table(
     # Check DB if necessary
     if max_collision_id is None:
         logger.info(f"ray track id2: {track_record.track_id} {track_record.owner_id}")
+        logger.info(track_record)
         max_collision_id = (
             session.query(functions.max(TrackRoute.collision_id))
             .filter(
