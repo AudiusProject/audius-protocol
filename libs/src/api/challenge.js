149c1d1b4ebf396dcc39a1fa302cd563b4aeab12
Michael Piazza <michael.piazza.mp@gmail.com>
2021-09-17 19:37:20 -0400

    Working attestation methods

---
+++ libs/src/api/challenge.js
@@ -0,0 +1,249 @@
+const axios = require('axios')
+const { Base, Services } = require('./base')
+const { sampleSize } = require('lodash')
+const BN = require('bn.js')
+
+const GetAttestationError = Object.freeze({
+  CHALLENGE_INCOMPLETE: 'CHALLENGE_INCOMPLETE',
+  ALREADY_DISBURSED: 'ALREADY_DISBURSED',
+  INVALID_ORACLE: 'INVALID_ORACLE',
+  MISSING_CHALLENGES: 'MISSING_CHALLENGES',
+  INVALID_INPUT: 'INVALID_INPUT',
+  UNKNOWN_ERROR: 'UNKNOWN_ERROR'
+})
+
+const AAO_REQUEST_TIMEOUT_MS = 15 * 1000
+const WRAPPED_AUDIO_PRECISION = 10 ** 9
+
+/**
+ * @typedef {import("../services/solanaWeb3Manager/rewards.js").AttestationMeta} AttestationMeta
+ */
+
+class Challenge extends Base {
+  /**
+   *
+   * Top level method to aggregate attestations, submit them to RewardsManager, and evalute the result.
+   *
+   * TODO: needs error handling!
+   *
+   * @param {{
+   *   challengeId: string,
+   *   encodedUserId: string,
+   *   handle: string,
+   *   recipientEthAddress: string,
+   *   specifier: string,
+   *   oracleEthAddress: string,
+   *   amount: number,
+   *   quorumSize: number,
+   *   AAOEndpoint: string
+   * }} {
+   *   challengeId,
+   *   encodedUserId,
+   *   handle,
+   *   recipientEthAddress,
+   *   specifier,
+   *   oracleEthAddress,
+   *   amount,
+   *   quorumSize,
+   *   AAOEndpoint
+   *   }
+   * @memberof Challenge
+   */
+  async submitAndEvaluate ({
+    challengeId, encodedUserId, handle, recipientEthAddress, specifier, oracleEthAddress, amount, quorumSize, AAOEndpoint
+  }) {
+    const { discoveryNodeAttestations, aaoAttestation } = await this.aggregateAttestations({
+      challengeId, encodedUserId, handle, specifier, oracleEthAddress, amount, quorumSize, AAOEndpoint
+    })
+
+    const fullTokenAmount = new BN(amount * WRAPPED_AUDIO_PRECISION)
+
+    await this.solanaWeb3Manager.submitChallengeAttestations({
+      attestations: discoveryNodeAttestations,
+      oracleAttestation: aaoAttestation,
+      challengeId,
+      specifier,
+      recipientEthAddress,
+      tokenAmount: fullTokenAmount
+    })
+
+    await this.solanaWeb3Manager.evaluateChallengeAttestations({
+      challengeId,
+      specifier,
+      recipientEthAddress,
+      oracleEthAddress,
+      tokenAmount: fullTokenAmount
+    })
+  }
+
+  /**
+   *
+   * Aggregates attestations from Discovery Nodes and AAO.
+   *
+   * @typedef {Object} AttestationsReturn
+   * @property {Array<AttestationMeta>} discoveryNodeAttestations
+   * @property {AttestationMeta} aaoAttestation
+   *
+   * @param {{
+   *   challengeId,
+   *   encodedUserId,
+   *   handle,
+   *   specifier,
+   *   oracleEthAddress,
+   *   amount,
+   *   quorumSize,
+   *   AAOEndpoint,
+   *   endpoints = null
+   * }} {
+   *   challengeId,
+   *   encodedUserId,
+   *   handle,
+   *   specifier,
+   *   oracleEthAddress,
+   *   amount,
+   *   quorumSize,
+   *   AAOEndpoint,
+   *   endpoints = null
+   * }
+   * @returns {Promise<AttestationsReturn>} attestations
+   * @memberof Challenge
+   */
+  async aggregateAttestations ({ challengeId, encodedUserId, handle, specifier, oracleEthAddress, amount, quorumSize, AAOEndpoint, endpoints = null }) {
+    this.REQUIRES(Services.DISCOVERY_PROVIDER)
+
+    // If no endpoints array provided, select here
+    if (!endpoints) {
+      endpoints = await this.discoveryProvider.serviceSelector.findAll()
+    }
+    endpoints = sampleSize(endpoints, quorumSize)
+
+    const discprovAttestations = endpoints.map(e => this.getChallengeAttestation({ challengeId, encodedUserId, specifier, oracleEthAddress, discoveryProviderEndpoint: e }))
+    const AAOAttestation = this.getAAOAttestation({
+      challengeId,
+      specifier,
+      handle,
+      amount,
+      AAOEndpoint,
+      oracleEthAddress
+    })
+
+    // TODO: this code will fail sloppily if any of these attestations error
+
+    const res = await Promise.all([...discprovAttestations, AAOAttestation])
+    const discoveryNodeAttestations = res.slice(0, -1).map(r => r.success)
+    const aaoAttestation = res[res.length - 1].success
+    console.log({discoveryNodeAttestations, aaoAttestation})
+    return {
+      discoveryNodeAttestations,
+      aaoAttestation
+    }
+  }
+
+  /**
+   *
+   * Retrieves a Discovery Node attestation for a given userId.
+   *
+   * @typedef {Object} GetAttestationReturn
+   * @property {AttestationMeta} success
+   * @property {error} string
+   *
+   * @param {{
+   *   challengeId: string,
+   *   encodedUserId: string,
+   *   specifier: string,
+   *   oracleEthAddress: string,
+   *   discoveryProviderEndpoint: string
+   * }} {
+   *   challengeId,
+   *   encodedUserId,
+   *   specifier,
+   *   oracleEthAddress,
+   *   discoveryProviderEndpoint
+   * }
+   * @returns {Promise<GetAttestationReturn>}
+   * @memberof Challenge
+   */
+  async getChallengeAttestation ({ challengeId, encodedUserId, specifier, oracleEthAddress, discoveryProviderEndpoint }) {
+    this.REQUIRES(Services.DISCOVERY_PROVIDER)
+    try {
+      const res = await this.discoveryProvider.getChallengeAttestation(
+        challengeId,
+        encodedUserId,
+        specifier,
+        oracleEthAddress,
+        discoveryProviderEndpoint
+      )
+
+      const meta = {
+        ethAddress: res.owner_wallet,
+        signature: res.attestation
+      }
+
+      return { success: meta, error: null }
+    } catch (e) {
+      const err = e.message
+      console.log(err)
+      const mappedErr = GetAttestationError[err] || GetAttestationError.UNKNOWN_ERROR
+      return {
+        success: null,
+        error: mappedErr
+      }
+    }
+  }
+
+  /**
+   *
+   * Retrieves an AAO attestation for a given user handle.
+   *
+   * @typedef {Object} GetAAOAttestationReturn
+   * @property {AttestationMeta} success
+   * @property {error} string
+   *
+   * @param {{
+   *   challengeId: string,
+   *   specifier: string,
+   *   handle: string,
+   *   amount: number,
+   *   AAOEndpoint: string
+   *   oracleEthAddress: string
+   * }} {
+   *   challengeId,
+   *   specifier,
+   *   handle,
+   *   amount,
+   *   AAOEndpoint,
+   *   oracleEthAddress
+   * }
+   * @returns {Promise<GetAAOAttestationReturn>}
+   * @memberof Challenge
+   */
+  async getAAOAttestation ({ challengeId, specifier, handle, amount, AAOEndpoint, oracleEthAddress }) {
+    const data = {
+      challengeId,
+      challengeSpecifier: specifier,
+      amount
+    }
+    const request = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${AAOEndpoint}/attestation/${handle}`,
+      timeout: AAO_REQUEST_TIMEOUT_MS,
+      data
+    }
+
+    // TODO: error handling here!
+
+    const res = await axios(request)
+    return {
+      success: {
+        signature: res.data.result,
+        ethAddress: oracleEthAddress
+      },
+      error: null
+    }
+  }
+}
+
+module.exports = Challenge
