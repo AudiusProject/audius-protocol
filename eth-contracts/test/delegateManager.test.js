daffa6f335598d18f5ed07a5a8f24d6c7539029d
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2021-09-21 01:41:18 +0000

    Comment explaining why

--- eth-contracts/test/delegateManager.test.js
+++ eth-contracts/test/delegateManager.test.js
@@ -994,6 +994,7 @@ contract('DelegateManager', async (accounts) => {
           singleDelegateAmount,
           { from: delegator })
 
+        // Manually set block gas limit to ensure no out-of-gas reverts
         await delegateManager.delegateStake(
           stakerAccount,
           singleDelegateAmount,
