f090eaa26a2aced82451cca35c50d76772223455
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-09-28 10:50:48 -0400

    Add and process no redirect flow

--- discovery-provider/scripts/gen_nginx_conf.py
+++ discovery-provider/scripts/gen_nginx_conf.py
@@ -6,7 +6,7 @@ import os
 def main():
     print(
         """
-worker_processes  1;
+worker_processes 1;
 
 error_log logs/error.log;
 
@@ -15,10 +15,11 @@ events {
 }
 
 http {
-    lua_shared_dict my_limit_req_store 100m;
+    lua_shared_dict my_limit_count_store 100m;
 
     init_worker_by_lua_block {
-        local resty_random = require('resty.random')
+        require "resty.core"
+        local resty_random = require("resty.random")
         local seed = string.byte(resty_random.bytes(1))
         math.randomseed(seed)
     }
@@ -40,27 +41,27 @@ http {
         print(
             f"""
             access_by_lua_block {{
-                local limit_req = require "resty.limit.req"
+                if ngx.var.arg_no_redirect ~= "yes" then
+                    local limit_count = require "resty.limit.count"
 
-                local lim, err = limit_req.new("my_limit_req_store", 10, 20)
-                if not lim then
-                    ngx.log(ngx.ERR, "failed to instantiate a resty.limit.req object: ", err)
-                    return ngx.exit(500)
-                end
-
-                local key = ngx.var.binary_remote_addr
-                local delay, err = lim:incoming(key, true)
-                if not delay then
-                    if err == "rejected" then
-                        local backendurl = ({{"{'","'.join(secondary)}"}})[math.random(1, {len(secondary)})]
-                        return ngx.redirect("http://" ..  backendurl .. ngx.var.request_uri)
+                    local lim, err = limit_count.new("my_limit_count_store", 10, 1)
+                    if not lim then
+                        ngx.log(ngx.ERR, "failed to instantiate a resty.limit.req object: ", err)
+                        return ngx.exit(500)
                     end
-                    ngx.log(ngx.ERR, "failed to limit req: ", err)
-                    return ngx.exit(500)
-                end
 
-                if delay >= 0.001 then
-                    ngx.sleep(delay)
+                    local delay, err = lim:incoming("k", true)
+                    if not delay then
+                        if err == "rejected" then
+                            local backendurl = ({{"{'","'.join(secondary)}"}})[math.random(1, {len(secondary)})]
+                            local url = "http://" ..  backendurl .. ngx.var.request_uri
+                            url = url .. ((ngx.var.is_args == "?") and "&" or "?") .. "no_redirect=yes"
+                            return ngx.redirect(url)
+                        end
+
+                        ngx.log(ngx.ERR, "failed to limit req: ", err)
+                        return ngx.exit(500)
+                    end
                 end
             }}
 """
