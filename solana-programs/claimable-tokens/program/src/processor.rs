cc440f4f993a4f31fdc47c56d186458bd03f613a
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2021-10-06 15:48:46 +0000

    Initializing

--- solana-programs/claimable-tokens/program/src/processor.rs
+++ solana-programs/claimable-tokens/program/src/processor.rs
@@ -5,7 +5,7 @@ use crate::{
     instruction::ClaimableProgramInstruction,
     utils::program::{find_address_pair, EthereumAddress},
 };
-use borsh::BorshDeserialize;
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::next_account_info,
     account_info::AccountInfo,
@@ -19,7 +19,29 @@ use solana_program::{
     sysvar::rent::Rent,
     sysvar::Sysvar,
 };
+use solana_sdk::{secp256k1_instruction::{SIGNATURE_OFFSETS_SERIALIZED_SIZE}};
+
 use std::mem::size_of;
+
+/// Secp256k1 signature offsets data
+#[derive(Clone, Copy, Debug, Default, PartialEq, BorshDeserialize, BorshSerialize)]
+pub struct SecpSignatureOffsets {
+    /// Offset of 64+1 bytes
+    pub signature_offset: u16,
+    /// Index of signature instruction in buffer
+    pub signature_instruction_index: u8,
+    /// Offset to eth_address of 20 bytes
+    pub eth_address_offset: u16,
+    /// Index of eth address instruction in buffer
+    pub eth_address_instruction_index: u8,
+    /// Offset to start of message data
+    pub message_data_offset: u16,
+    /// Size of message data
+    pub message_data_size: u16,
+    /// Index on message instruction in buffer
+    pub message_instruction_index: u8,
+}
+
 
 /// Program state handler.
 pub struct Processor;
@@ -268,6 +290,18 @@ impl Processor {
         expected_message: &[u8],
         secp_instruction_data: Vec<u8>,
     ) -> Result<(), ProgramError> {
+        if secp_instruction_data[0] != 1 {
+            return Err(ClaimableProgramError::SignatureVerificationFailed.into());
+        }
+        let start = 1 * (SIGNATURE_OFFSETS_SERIALIZED_SIZE as usize);
+        let end = start + (SIGNATURE_OFFSETS_SERIALIZED_SIZE as usize);
+        let sig_offsets_struct = SecpSignatureOffsets::try_from_slice(
+            &secp_instruction_data[start..end]
+        )
+            .map_err(|_| ClaimableProgramError::SignatureVerificationFailed)?;
+
+        println!("sig_offsets_struct {:?}", sig_offsets_struct);
+
         let eth_address_offset = 12;
         let instruction_signer = secp_instruction_data
             [eth_address_offset..eth_address_offset + size_of::<EthereumAddress>()]
@@ -286,3 +320,87 @@ impl Processor {
         Ok(())
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use crate::processor::Processor;
+//     // use libsecp256k1::{SecretKey};
+//     use solana_program::{instruction::Instruction, pubkey::Pubkey, secp256k1_program};
+//     use solana_sdk::{secp256k1_instruction::{self, SecpSignatureOffsets, construct_eth_pubkey}, transaction::Transaction};
+//     // use sha3::Digest;
+ 
+//     #[test]
+//     fn test_eth_validation_bug() {
+//         let fake_sk = libsecp256k1::SecretKey::random(&mut rand_073::thread_rng());
+//         let fake_pk = libsecp256k1::PublicKey::from_secret_key(&fake_sk);
+//         // Don't need the real secret key
+//         let real_pk = libsecp256k1::PublicKey::from_secret_key(&libsecp256k1::SecretKey::random(&mut rand_073::thread_rng()));
+
+//         let fake_msg: Vec<u8> = (0..100).collect();
+//         let real_msg: Vec<u8> = (50..150).collect();
+//         let real_eth_pubkey = construct_eth_pubkey(&real_pk);
+//         let fake_eth_pubkey = construct_eth_pubkey(&fake_pk);
+
+//         let mut hasher = sha3::Keccak256::new();
+//         hasher.update(fake_msg.clone());
+//         let fake_message_hash = hasher.finalize();
+//         let mut fake_message_hash_arr = [0u8; 32];
+//         fake_message_hash_arr.copy_from_slice(&fake_message_hash.as_slice());
+//         let fake_message = libsecp256k1::Message::parse(&fake_message_hash_arr);
+//         let (fake_signature, fake_recovery_id) = libsecp256k1::sign(&fake_message, &fake_sk);
+//         let fake_signature_arr = fake_signature.serialize();
+
+//         let mut dummy_instr_data = vec![];
+//         let eth_addr_offset = dummy_instr_data.len();
+//         dummy_instr_data.extend_from_slice(&fake_eth_pubkey);
+//         let eth_sig_offset = dummy_instr_data.len(); 
+//         dummy_instr_data.extend_from_slice(&fake_signature_arr);
+//         dummy_instr_data.push(fake_recovery_id.serialize());
+//         let msg_offset = dummy_instr_data.len();
+//         dummy_instr_data.append(&mut fake_msg.clone());
+
+//         let dummy_instr_data_ind = 0;
+
+//         let secp_offsets_struct = SecpSignatureOffsets {
+//             eth_address_instruction_index: dummy_instr_data_ind,
+//             message_instruction_index: dummy_instr_data_ind,
+//             signature_instruction_index: dummy_instr_data_ind,
+//             eth_address_offset: eth_addr_offset as u16,
+//             message_data_offset: msg_offset as u16,
+//             message_data_size: fake_msg.len() as u16,
+//             signature_offset: eth_sig_offset as u16,
+//         };
+//         println!(
+//             "SECP TEST OFFSET {:?}",
+//             secp_offsets_struct
+//         );
+
+//         let mut secp_instr_data = vec![];
+//         secp_instr_data.push(1u8); // count
+//         secp_instr_data.append(&mut bincode::serialize(&secp_offsets_struct).unwrap());
+//         // Here's where we put the real pubkey, which our processor tries to validate
+//         secp_instr_data.extend_from_slice(&real_eth_pubkey);
+//         // Append dummy signature data
+//         let mut dummy_sig = (0..65).collect();
+//         println!("{:?} = dummy sig", dummy_sig);
+//         secp_instr_data.append(&mut dummy_sig);
+//         // Append the real message
+//         secp_instr_data.append(&mut real_msg.clone());
+
+//         let dummy_instruction = Instruction {
+//             program_id: Pubkey::new_unique(),
+//             accounts: vec![],
+//             data: dummy_instr_data.clone(),
+//         };
+
+//         let secp_instruction = Instruction {
+//             program_id: secp256k1_program::id(),
+//             accounts: vec![],
+//             data: secp_instr_data.clone(),
+//         };
+        
+//         let tx = Transaction::new_with_payer(&[dummy_instruction, secp_instruction], None);
+//         assert!(tx.verify_precompiles(false).is_ok());
+//         assert!(Processor::validate_eth_signature(&real_eth_pubkey, &real_msg, secp_instr_data.clone()).is_ok());
+//     }
+// }
