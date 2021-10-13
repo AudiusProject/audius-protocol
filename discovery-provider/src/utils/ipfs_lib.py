240e0887ff3579dee42803de10131288a78cb167
Vicky <vicky@audius.co>
2021-10-13 14:38:47 +0000

    revert gateway switch

--- discovery-provider/src/utils/ipfs_lib.py
+++ discovery-provider/src/utils/ipfs_lib.py
@@ -44,25 +44,25 @@ class IPFSClient:
         retrieved_from_gateway = False
         start_time = time.time()
 
-        # First try to retrieve from gateways.
+        # First try to retrieve from local ipfs node.
         try:
-            api_metadata = self.get_metadata_from_gateway(
-                multihash, metadata_format, user_replica_set
-            )
-            retrieved_from_gateway = api_metadata != metadata_format
+            api_metadata = self.get_metadata_from_ipfs_node(multihash, metadata_format)
+            retrieved_from_local_node = api_metadata != metadata_format
         except Exception:
             logger.error(
-                f"Failed to retrieve CID from gateway, {multihash}", exc_info=True
+                f"Failed to retrieve CID from local node, {multihash}", exc_info=True
             )
 
-        # Else, try to retrieve from local ipfs node.
-        if not retrieved_from_gateway:
+        # Else, try to retrieve from gateways.
+        if not retrieved_from_local_node:
             try:
-                api_metadata = self.get_metadata_from_ipfs_node(multihash, metadata_format)
-                retrieved_from_local_node = api_metadata != metadata_format
+                api_metadata = self.get_metadata_from_gateway(
+                    multihash, metadata_format, user_replica_set
+                )
+                retrieved_from_gateway = api_metadata != metadata_format
             except Exception:
                 logger.error(
-                    f"Failed to retrieve CID from local node, {multihash}", exc_info=True
+                    f"Failed to retrieve CID from gateway, {multihash}", exc_info=True
                 )
 
         retrieved_metadata = retrieved_from_gateway or retrieved_from_local_node
