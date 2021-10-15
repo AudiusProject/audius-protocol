cd36f160f9c92044dcc8b8c1d41058c60c6ad0fc
Ubuntu <sliptype@gmail.com>
2021-10-15 16:52:58 +0000

    Add debug mode to identity test runs

--- identity-service/scripts/run-tests.sh
+++ identity-service/scripts/run-tests.sh
@@ -2,6 +2,13 @@
 set -o xtrace
 set -e
 
+while getopts debug flag
+do
+  case "${flag}" in
+      d) debug=true;
+  esac
+done
+
 PG_PORT=$POSTGRES_TEST_PORT
 if [ -z "${PG_PORT}" ]; then
   PG_PORT=7432
@@ -58,7 +65,13 @@ elif [ -x "$(command -v psql)" ]; then
 fi
 
 # tests
-./node_modules/mocha/bin/mocha test/index.js --timeout 10000 --exit
+if [ "${debug}" ]; then
+  # If the -d debug flag is provided, run the tests with no timeout so that
+  # debugging does not get interrupted
+  ./node_modules/mocha/bin/mocha test/index.js --timeout 0 --exit
+else
+  ./node_modules/mocha/bin/mocha test/index.js --timeout 10000 --exit
+fi
 
 # linter
 
