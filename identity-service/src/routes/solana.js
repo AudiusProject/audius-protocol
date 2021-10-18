cf925796e3bb8f0fbd4faeac61bc088745d16468
Joseph Lee <joey@audius.co>
2021-10-18 13:17:35 +0000

    Add identity solana relay raw route

--- identity-service/src/routes/solana.js
+++ identity-service/src/routes/solana.js
@@ -24,11 +24,6 @@ const isValidInstruction = (instr) => {
   return true
 }
 
-const isValidTransactionSignature = (signature) => {
-  if (!signature || !signature.pubkey) return false
-  return true
-}
-
 solanaRouter.post('/relay', handleResponse(async (req, res, next) => {
   const redis = req.app.get('redis')
   const libs = req.app.get('audiusLibs')
@@ -62,6 +57,69 @@ solanaRouter.post('/relay', handleResponse(async (req, res, next) => {
   }
 
   return successResponse({ transactionSignature })
+}))
+
+
+solanaRouter.post('/relay/raw', handleResponse(async (req, res, next) => {
+  const redis = req.app.get('redis')
+  const { recentBlockhash, secpInstruction, instructions = [], signatures = [] } = req.body
+
+  const reqBodySHA = crypto.createHash('sha256').update(JSON.stringify({ secpInstruction, instructions, signatures })).digest('hex')
+
+  try {
+    const tx = new Transaction({ recentBlockhash })
+
+    if (secpInstruction) {
+      const secpTransactionInstruction = Secp256k1Program.createInstructionWithPublicKey({
+        publicKey: Buffer.from(secpInstruction.publicKey),
+        message: (new PublicKey(secpInstruction.message)).toBytes(),
+        signature: Buffer.from(secpInstruction.signature),
+        recoveryId: secpInstruction.recoveryId
+      })
+      tx.add(secpTransactionInstruction)
+    }
+
+    instructions.filter(isValidInstruction).forEach((instr) => {
+      const keys = instr.keys.map(key => ({
+        pubkey: new PublicKey(key.pubkey),
+        isSigner: key.isSigner,
+        isWritable: key.isWritable
+      }))
+      const txInstruction = new TransactionInstruction({
+        keys,
+        programId: new PublicKey(instr.programId),
+        data: Buffer.from(instr.data)
+      })
+      tx.add(txInstruction)
+    })
+
+    // Manually attach each signature ot the transaction
+    signatures.forEach(sig => {
+      tx.signatures.push({
+        publicKey: new PublicKey(sig.publicKey),
+        signature: sig.signature ? Buffer.from(sig.signature): sig.signature
+      })
+    })
+
+    const feePayerAccount = getFeePayer()
+    tx.partialSign(feePayerAccount)
+
+    const transactionSignature = await sendAndConfirmRawTransaction(
+      connection,
+      tx.serialize(), {
+      skipPreflight: true,
+      commitment: 'processed',
+      preflightCommitment: 'processed'
+    })
+
+    return successResponse({ transactionSignature })
+  } catch (e) {
+    // if the tx fails, store it in redis with a 24 hour expiration
+    await redis.setex(`solanaFailedTx:${reqBodySHA}`, 60 /* seconds */ * 60 /* minutes */ * 24 /* hours */, JSON.stringify(req.body))
+    req.logger.error('Error in solana transaction:', error, reqBodySHA)
+    const errorString = `Something caused the solana transaction to fail for payload ${reqBodySHA}`
+    return errorResponseServerError(errorString, { errorCode, error })
+  }
 }))
 
 module.exports = function (app) {
