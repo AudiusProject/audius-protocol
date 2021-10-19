db16ec0b70438b1cdd59c6a6b475c836a11244ee
Isaac Solo <isaac@audius.co>
2021-10-19 04:14:42 +0000

    update query

--- discovery-provider/src/tasks/index_aggregate_views.py
+++ discovery-provider/src/tasks/index_aggregate_views.py
@@ -78,8 +78,7 @@ def update_aggregate_table(
                 logger.info(f"index_aggregate_views.py | Updating {table_name}")
                 upsert = sa.text(query)
                 session.execute(upsert,
-                    {"most_recent_indexed_aggregate_block":most_recent_indexed_aggregate_block,
-                    "latest_indexed_block_num": latest_indexed_block_num}
+                    {"most_recent_indexed_aggregate_block":most_recent_indexed_aggregate_block}
                 )
 
                 logger.info(f"""index_aggregate_views.py | Finished updating
@@ -107,132 +106,319 @@ def update_aggregate_user(self):
     db = update_aggregate_user.db
     redis = update_aggregate_user.redis
     query = """
-        INSERT INTO aggregate_user_table
-        (
-            user_id,
-            track_count,
-            playlist_count,
-            album_count,
-            follower_count,
-            following_count,
-            repost_count,
-            track_save_count
-        )
-        (
+        -- WITHOUT block number column in aggregate_user_table
+        WITH aggregate_user_latest_blocknumber AS (
             SELECT
-                DISTINCT(u.user_id),
-                COALESCE (user_track.track_count, 0)       AS track_count,
-                COALESCE (user_playlist.playlist_count, 0) AS playlist_count,
-                COALESCE (user_album.album_count, 0)       AS album_count,
-                COALESCE (user_follower.follower_count, 0) AS follower_count,
-                COALESCE (user_followee.followee_count, 0) AS following_count,
-                COALESCE (user_repost.repost_count, 0)     AS repost_count,
-                COALESCE (user_track_save.save_count, 0)   AS track_save_count
-            FROM users u -- join on subquery for tracks created
+                :most_recent_indexed_aggregate_block AS blocknumber
+        ),
+        changed_users AS (
+            SELECT
+                user_id
+            FROM
+                users u
+            WHERE
+                u.is_current IS TRUE
+                AND u.blocknumber > (
+                    SELECT
+                        blocknumber
+                    FROM
+                        aggregate_user_latest_blocknumber
+                )
+            GROUP BY
+                user_id
+            UNION
+            ALL
+            SELECT
+                t.owner_id AS owner_id
+            FROM
+                tracks t
+            WHERE
+                t.is_current IS TRUE
+                AND t.blocknumber > (
+                    SELECT
+                        blocknumber
+                    FROM
+                        aggregate_user_latest_blocknumber
+                )
+            GROUP BY
+                t.owner_id
+            UNION
+            ALL
+            SELECT
+                p.playlist_owner_id AS owner_id
+            FROM
+                playlists p
+            WHERE
+                p.is_album IS FALSE
+                AND p.is_current IS TRUE
+                AND p.blocknumber > (
+                    SELECT
+                        blocknumber
+                    FROM
+                        aggregate_user_latest_blocknumber
+                )
+            GROUP BY
+                p.playlist_owner_id
+            UNION
+            ALL
+            SELECT
+                p.playlist_owner_id AS owner_id
+            FROM
+                playlists p
+            WHERE
+                p.is_album IS TRUE
+                AND p.is_current IS TRUE
+                AND p.blocknumber > (
+                    SELECT
+                        blocknumber
+                    FROM
+                        aggregate_user_latest_blocknumber
+                )
+            GROUP BY
+                p.playlist_owner_id
+            UNION
+            ALL (
+                SELECT
+                    f.followee_user_id AS followee_user_id
+                FROM
+                    follows f
+                WHERE
+                    f.is_current IS TRUE
+                    AND f.blocknumber > (
+                        SELECT
+                            blocknumber
+                        FROM
+                            aggregate_user_latest_blocknumber
+                    )
+                GROUP BY
+                    f.followee_user_id
+            )
+            UNION
+            ALL (
+                SELECT
+                    f.follower_user_id AS follower_user_id
+                FROM
+                    follows f
+                WHERE
+                    f.is_current IS TRUE
+                    AND f.blocknumber > (
+                        SELECT
+                            blocknumber
+                        from
+                            aggregate_user_latest_blocknumber
+                    )
+                GROUP BY
+                    f.follower_user_id
+            )
+            UNION
+            ALL (
+                SELECT
+                    r.user_id AS user_id
+                FROM
+                    reposts r
+                WHERE
+                    r.is_current IS TRUE
+                    AND r.blocknumber > (
+                        SELECT
+                            blocknumber
+                        from
+                            aggregate_user_latest_blocknumber
+                    )
+                GROUP BY
+                    r.user_id
+            )
+            UNION
+            ALL (
+                SELECT
+                    s.user_id AS user_id
+                FROM
+                    saves s
+                WHERE
+                    s.is_current IS TRUE
+                    AND s.save_type = 'track'
+                    AND s.blocknumber > (
+                        SELECT
+                            blocknumber
+                        FROM
+                            aggregate_user_latest_blocknumber
+                    )
+                GROUP BY
+                    s.user_id
+            )
+        )
+        INSERT INTO
+            aggregate_user_table (
+                user_id,
+                track_count,
+                playlist_count,
+                album_count,
+                follower_count,
+                following_count,
+                repost_count,
+                track_save_count
+            )
+        SELECT
+            DISTINCT(u.user_id),
+            COALESCE (user_track.track_count, 0) AS track_count,
+            COALESCE (user_playlist.playlist_count, 0) AS playlist_count,
+            COALESCE (user_album.album_count, 0) AS album_count,
+            COALESCE (user_follower.follower_count, 0) AS follower_count,
+            COALESCE (user_followee.followee_count, 0) AS following_count,
+            COALESCE (user_repost.repost_count, 0) AS repost_count,
+            COALESCE (user_track_save.save_count, 0) AS track_save_count
+        FROM
+            users u
             LEFT OUTER JOIN (
                 SELECT
                     t.owner_id AS owner_id,
                     count(t.owner_id) AS track_count
-                FROM tracks t
-                WHERE t.is_current IS TRUE
-                AND t.is_delete IS FALSE
-                AND t.is_unlisted IS FALSE
-                AND t.stem_of IS NULL
-                AND t.blocknumber > :most_recent_indexed_aggregate_block
-                AND t.blocknumber <= :latest_indexed_block_num
-                GROUP BY t.owner_id
-                ) AS user_track
-            ON user_track.owner_id = u.user_id -- join on subquery for playlists created
+                FROM
+                    tracks t
+                WHERE
+                    t.is_current IS TRUE
+                    AND t.is_delete IS FALSE
+                    AND t.is_unlisted IS FALSE
+                    AND t.stem_of IS NULL
+                    AND t.owner_id IN (
+                        select
+                            user_id
+                        from
+                            changed_users
+                    )
+                GROUP BY
+                    t.owner_id
+            ) as user_track ON user_track.owner_id = u.user_id
             LEFT OUTER JOIN (
                 SELECT
                     p.playlist_owner_id AS owner_id,
                     count(p.playlist_owner_id) AS playlist_count
-                FROM playlists p
-                WHERE p.is_album IS FALSE
-                AND p.is_current IS TRUE
-                AND p.is_delete IS FALSE
-                AND p.is_private IS FALSE
-                AND p.blocknumber > :most_recent_indexed_aggregate_block
-                AND p.blocknumber <= :latest_indexed_block_num
-                GROUP BY p.playlist_owner_id
-                ) AS user_playlist
-            ON user_playlist.owner_id = u.user_id -- join on subquery for albums created
+                FROM
+                    playlists p
+                WHERE
+                    p.is_album IS FALSE
+                    AND p.is_current IS TRUE
+                    AND p.is_delete IS FALSE
+                    AND p.is_private IS FALSE
+                    AND p.playlist_owner_id IN (
+                        select
+                            user_id
+                        from
+                            changed_users
+                    )
+                GROUP BY
+                    p.playlist_owner_id
+            ) AS user_playlist ON user_playlist.owner_id = u.user_id
             LEFT OUTER JOIN (
                 SELECT
                     p.playlist_owner_id AS owner_id,
                     count(p.playlist_owner_id) AS album_count
-                FROM playlists p
-                WHERE p.is_album IS TRUE
-                AND p.is_current IS TRUE
-                AND p.is_delete IS FALSE
-                AND p.is_private IS FALSE
-                AND p.blocknumber > :most_recent_indexed_aggregate_block
-                AND p.blocknumber <= :latest_indexed_block_num
-                GROUP BY p.playlist_owner_id
-                ) user_album
-            ON user_album.owner_id = u.user_id -- join on subquery for followers
+                FROM
+                    playlists p
+                WHERE
+                    p.is_album IS TRUE
+                    AND p.is_current IS TRUE
+                    AND p.is_delete IS FALSE
+                    AND p.is_private IS FALSE
+                    AND p.playlist_owner_id IN (
+                        SELECT
+                            user_id
+                        FROM
+                            changed_users
+                    )
+                GROUP BY
+                    p.playlist_owner_id
+            ) user_album ON user_album.owner_id = u.user_id
             LEFT OUTER JOIN (
                 SELECT
                     f.followee_user_id AS followee_user_id,
                     count(f.followee_user_id) AS follower_count
-                FROM follows f
-                WHERE f.is_current IS TRUE
-                AND f.is_delete IS FALSE
-                AND f.blocknumber > :most_recent_indexed_aggregate_block
-                AND f.blocknumber <= :latest_indexed_block_num
-                GROUP BY f.followee_user_id
-                ) user_follower
-            ON user_follower.followee_user_id = u.user_id -- join on subquery for followee
+                FROM
+                    follows f
+                WHERE
+                    f.is_current IS TRUE
+                    AND f.is_delete IS FALSE
+                    AND f.follower_user_id IN (
+                        SELECT
+                            user_id
+                        FROM
+                            changed_users
+                    )
+                GROUP BY
+                    f.followee_user_id
+            ) user_follower ON user_follower.followee_user_id = u.user_id
             LEFT OUTER JOIN (
                 SELECT
-                    f.follower_user_id        AS follower_user_id,
+                    f.follower_user_id AS follower_user_id,
                     count(f.follower_user_id) AS followee_count
-                FROM follows f
-                WHERE f.is_current IS TRUE
-                AND f.is_delete IS FALSE
-                AND f.blocknumber > :most_recent_indexed_aggregate_block
-                AND f.blocknumber <= :latest_indexed_block_num
-                GROUP BY f.follower_user_id
-                ) user_followee
-            ON user_followee.follower_user_id = u.user_id -- join on subquery for reposts
+                FROM
+                    follows f
+                WHERE
+                    f.is_current IS TRUE
+                    AND f.is_delete IS FALSE
+                    AND f.follower_user_id IN (
+                        SELECT
+                            user_id
+                        FROM
+                            changed_users
+                    )
+                GROUP BY
+                    f.follower_user_id
+            ) user_followee ON user_followee.follower_user_id = u.user_id
             LEFT OUTER JOIN (
                 SELECT
                     r.user_id AS user_id,
                     count(r.user_id) AS repost_count
-                FROM reposts r
-                WHERE r.is_current IS TRUE
-                AND r.is_delete IS FALSE
-                AND r.blocknumber > :most_recent_indexed_aggregate_block
-                AND r.blocknumber <= :latest_indexed_block_num
-                GROUP BY r.user_id
-                ) user_repost
-            ON user_repost.user_id = u.user_id -- join on subquery for track saves
+                FROM
+                    reposts r
+                WHERE
+                    r.is_current IS TRUE
+                    AND r.is_delete IS FALSE
+                    AND r.user_id IN (
+                        SELECT
+                            user_id
+                        from
+                            changed_users
+                    )
+                GROUP BY
+                    r.user_id
+            ) user_repost ON user_repost.user_id = u.user_id
             LEFT OUTER JOIN (
                 SELECT
                     s.user_id AS user_id,
                     count(s.user_id) AS save_count
-                FROM saves s
-                WHERE s.is_current IS TRUE
-                AND s.save_type = 'track'
-                AND s.is_delete IS FALSE
-                AND s.blocknumber > :most_recent_indexed_aggregate_block
-                AND s.blocknumber <= :latest_indexed_block_num
-                GROUP BY s.user_id
-                ) user_track_save
-            ON user_track_save.user_id = u.user_id
-            WHERE u.is_current IS TRUE
-            AND u.blocknumber > :most_recent_indexed_aggregate_block
-            AND u.blocknumber <= :latest_indexed_block_num
-        )
-        ON CONFLICT (user_id)
-        DO UPDATE SET
-            track_count = aggregate_user_table.track_count + EXCLUDED.track_count,
-            playlist_count = aggregate_user_table.playlist_count+EXCLUDED.playlist_count,
-            album_count = aggregate_user_table.album_count+EXCLUDED.album_count,
-            follower_count =  aggregate_user_table.follower_count+EXCLUDED.follower_count,
-            following_count = aggregate_user_table.following_count+EXCLUDED.following_count,
-            repost_count =  aggregate_user_table.repost_count+EXCLUDED.repost_count,
-            track_save_count = aggregate_user_table.track_save_count+EXCLUDED.track_save_count"""
+                FROM
+                    saves s
+                WHERE
+                    s.is_current IS TRUE
+                    AND s.save_type = 'track'
+                    AND s.is_delete IS FALSE
+                    AND s.user_id IN (
+                        select
+                            user_id
+                        from
+                            changed_users
+                    )
+                GROUP BY
+                    s.user_id
+            ) user_track_save ON user_track_save.user_id = u.user_id
+        WHERE
+            u.is_current IS TRUE
+            AND u.user_id in (
+                SELECT
+                    user_id
+                FROM
+                    changed_users
+            ) ON CONFLICT (user_id) DO
+        UPDATE
+        SET
+            track_count = EXCLUDED.track_count,
+            playlist_count = EXCLUDED.playlist_count,
+            album_count = EXCLUDED.album_count,
+            follower_count = EXCLUDED.follower_count,
+            following_count = EXCLUDED.following_count,
+            repost_count = EXCLUDED.repost_count,
+            track_save_count = EXCLUDED.track_save_count
+    """
     update_aggregate_table(db, redis, AGGREGATE_USER, most_recent_indexed_aggregate_user_block_redis_key, query)
 
 
