c0b222d16306ee9409555d8437e94b59c055c506
Marcus Pasell <rickyrombo@gmail.com>
2021-10-22 22:38:46 +0000

    Formatting...

--- identity-service/src/routes/authentication.js
+++ identity-service/src/routes/authentication.js
@@ -1,9 +1,5 @@
-const models = require("../models");
-const {
-  handleResponse,
-  successResponse,
-  errorResponseBadRequest,
-} = require("../apiHelpers");
+const models = require('../models')
+const { handleResponse, successResponse, errorResponseBadRequest } = require('../apiHelpers')
 
 module.exports = function (app) {
   /**
@@ -11,73 +7,54 @@ module.exports = function (app) {
    * into the Authentications table and the email to the Users table. This is the first step in the
    * authentication process
    */
-  app.post(
-    "/authentication",
-    handleResponse(async (req, res, next) => {
-      // body should contain {iv, cipherText, lookupKey}
-      const body = req.body;
+  app.post('/authentication', handleResponse(async (req, res, next) => {
+    // body should contain {iv, cipherText, lookupKey}
+    const body = req.body
 
-      if (body && body.iv && body.cipherText && body.lookupKey) {
-        try {
-          const transaction = await models.sequelize.transaction();
-          const existingRecord = await models.Authentication.findOne({
-            where: { lookupKey: body.lookupKey },
-            paranoid: false,
-          });
-          if (!existingRecord) {
-            await models.Authentication.create(
-              {
-                iv: body.iv,
-                cipherText: body.cipherText,
-                lookupKey: body.lookupKey,
-              },
-              { transaction }
-            );
-          } else {
-            await existingRecord.restore({ transaction });
-          }
-
-          const oldLookupKey = body.oldLookupKey;
-          if (oldLookupKey && oldLookupKey !== body.lookupKey) {
-            await models.Authentication.destroy(
-              { where: { lookupKey: oldLookupKey } },
-              { transaction }
-            );
-          }
-          await transaction.commit();
-          return successResponse();
-        } catch (err) {
-          req.logger.error("Error signing up a user", err);
-          return errorResponseBadRequest("Error signing up a user");
-        }
-      } else
-        return errorResponseBadRequest(
-          "Missing one of the required fields: iv, cipherText, lookupKey"
-        );
-    })
-  );
-
-  app.get(
-    "/authentication",
-    handleResponse(async (req, res, next) => {
-      let queryParams = req.query;
-
-      if (queryParams && queryParams.lookupKey) {
-        const lookupKey = queryParams.lookupKey;
-        const existingUser = await models.Authentication.findOne({
-          where: { lookupKey },
-        });
-
-        if (existingUser) {
-          return successResponse(existingUser);
+    if (body && body.iv && body.cipherText && body.lookupKey) {
+      try {
+        const transaction = await models.sequelize.transaction()
+        const existingRecord = await models.Authentication.findOne({
+          where: { lookupKey: body.lookupKey }, 
+          paranoid: false 
+        })
+        if (!existingRecord) {
+          await models.Authentication.create({
+            iv: body.iv,
+            cipherText: body.cipherText,
+            lookupKey: body.lookupKey
+          }, { transaction })
         } else {
-          return errorResponseBadRequest(
-            "No auth record found for provided lookupKey."
-          );
+          await existingRecord.restore({ transaction })
         }
-      } else {
-        return errorResponseBadRequest("Missing queryParam lookupKey.");
+
+        const oldLookupKey = body.oldLookupKey
+        if (oldLookupKey && oldLookupKey !== body.lookupKey) {
+          await models.Authentication.destroy({ where: { lookupKey: oldLookupKey } }, { transaction })
+        }
+        await transaction.commit()
+        return successResponse()
+      } catch (err) {
+        req.logger.error('Error signing up a user', err)
+        return errorResponseBadRequest('Error signing up a user')
       }
-    })
-  );
-};
+    } else return errorResponseBadRequest('Missing one of the required fields: iv, cipherText, lookupKey')
+  }))
+
+  app.get('/authentication', handleResponse(async (req, res, next) => {
+    let queryParams = req.query
+
+    if (queryParams && queryParams.lookupKey) {
+      const lookupKey = queryParams.lookupKey
+      const existingUser = await models.Authentication.findOne({ where: { lookupKey } })
+
+      if (existingUser) {
+        return successResponse(existingUser)
+      } else {
+        return errorResponseBadRequest('No auth record found for provided lookupKey.')
+      }
+    } else {
+      return errorResponseBadRequest('Missing queryParam lookupKey.')
+    }
+  }))
+}
