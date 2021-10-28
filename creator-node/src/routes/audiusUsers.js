a09f51626f919827c1f0c17b8da65c83e6a23b42
vicky :) <60366641+vicky-g@users.noreply.github.com>
2021-10-28 12:31:39 -0700

    Do not await md add ipfs on Content Nodes (#2014)

--- creator-node/src/routes/audiusUsers.js
+++ creator-node/src/routes/audiusUsers.js
@@ -35,7 +35,7 @@ module.exports = function (app) {
     // Save file from buffer to IPFS and disk
     let multihash, dstPath
     try {
-      const resp = await saveFileFromBufferToIPFSAndDisk(req, metadataBuffer, true)
+      const resp = await saveFileFromBufferToIPFSAndDisk(req, metadataBuffer)
       multihash = resp.multihash
       dstPath = resp.dstPath
     } catch (e) {
