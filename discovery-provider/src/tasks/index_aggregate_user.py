3a0e520751c57532f331df9c3e46c906a031e6dc
Isaac <isaac@audius.co>
2021-11-15 19:08:58 +0000

    lint

--- discovery-provider/src/tasks/index_aggregate_user.py
+++ discovery-provider/src/tasks/index_aggregate_user.py
@@ -374,13 +374,10 @@ def update_aggregate_table(
                     logger.info(f"index_aggregate_user.py | Repopulating {table_name}")
                     most_recent_indexed_aggregate_block = 0
                     session.execute("TRUNCATE TABLE {}".format(table_name))
-                elif (latest_indexed_block_num % REFRESH_COUNTER == 0):
+                elif latest_indexed_block_num % REFRESH_COUNTER == 0:
                     # refresh the past two weeks for data accuracy
                     logger.info(f"index_aggregate_user.py | Refreshing {table_name} for the past two weeks")
-                    most_recent_indexed_aggregate_block -= TWO_WEEKS_IN_BLOCKS
-                    if (most_recent_indexed_aggregate_block < 0):
-                        most_recent_indexed_aggregate_block = 0
-
+                    most_recent_indexed_aggregate_block -= max(TWO_WEEKS_IN_BLOCKS, 0)
                     session.execute("TRUNCATE TABLE {}".format(table_name))
 
                 logger.info(f"index_aggregate_user.py | Updating {table_name}")
