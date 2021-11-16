3b5efd0e4575b309649fa8a50c08ede41c5047e6
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-11-16 21:19:13 +0000

    Compile files to tsc

--- creator-node/scripts/start.sh
+++ creator-node/scripts/start.sh
@@ -77,7 +77,6 @@ if [[ "$devMode" == "true" ]]; then
         ./node_modules/.bin/nodemon --watch src/ src/index.ts | tee >(logger) | ./node_modules/.bin/bunyan
     fi
 else
-    tsc --project ./
     node build/src/index.js | tee >(logger)
     docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 10
 fi
