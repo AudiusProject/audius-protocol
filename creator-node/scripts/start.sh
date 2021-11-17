d86acde53f2af399d66c437252a068e7644a4b58
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-11-17 17:08:28 +0000

    Should be working

--- creator-node/scripts/start.sh
+++ creator-node/scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 link_libs=true
 
@@ -77,9 +78,6 @@ if [[ "$devMode" == "true" ]]; then
         ./node_modules/.bin/nodemon --watch src/ src/index.ts | tee >(logger) | ./node_modules/.bin/bunyan
     fi
 else
-    ls
-    ls build/
-    ls build/src
     node build/src/index.js | tee >(logger)
     docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 10
 fi
