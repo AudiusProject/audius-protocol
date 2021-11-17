7c02e8470a18536527e53ea8a910d93275019db2
Saliou Diallo <sddioulde@hotmail.com>
2021-11-17 11:57:19 -0500

    Fix gif preview (#663)
    Co-authored-by: Saliou Diallo <saliou@audius.co>

--- packages/web/src/containers/collectibles/ethCollectibleHelpers.ts
+++ packages/web/src/containers/collectibles/ethCollectibleHelpers.ts
@@ -264,29 +264,6 @@ export const isNotFromNullAddress = (event: OpenSeaEvent) => {
 }
 
 export const getFrameFromGif = async (url: string, name: string) => {
-  const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
-  const isSafariMobile =
-    navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i)
-  let preview
-  try {
-    // Firefox does not handle partial gif rendering well
-    if (isFirefox || isSafariMobile) {
-      throw new Error('partial gif not supported')
-    }
-    const req = await fetch(url, {
-      headers: {
-        // Extremely heuristic 200KB. This should contain the first frame
-        // and then some. Rendering this out into an <img tag won't allow
-        // animation to play. Some gifs may not load if we do this, so we
-        // can try-catch it.
-        Range: 'bytes=0-200000'
-      }
-    })
-    const ab = await req.arrayBuffer()
-    preview = new Blob([ab])
-  } catch (e) {
-    preview = await gifPreview(url)
-  }
-
+  const preview = await gifPreview(url)
   return URL.createObjectURL(preview)
 }
