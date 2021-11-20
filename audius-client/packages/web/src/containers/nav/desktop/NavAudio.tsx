6a594f718b342843544a88abb1becd53a9478c80
Raymond Jacobson <ray@audius.co>
2021-11-19 17:22:59 -0800

    Touchup audio nav hover states (#659)
    * Touchup audio nav hover states
    
    * Undo div change

--- audius-client/packages/web/src/containers/nav/desktop/NavAudio.tsx
+++ audius-client/packages/web/src/containers/nav/desktop/NavAudio.tsx
@@ -11,10 +11,9 @@ import { audioTierMapPng } from 'containers/user-badges/UserBadges'
 import { useSelectTierInfo } from 'containers/user-badges/hooks'
 import { useNavigateToPage } from 'hooks/useNavigateToPage'
 import { FeatureFlags } from 'services/remote-config/FeatureFlags'
-import { getAccountTotalBalance } from 'store/wallet/selectors'
 import { useSelector } from 'utils/reducer'
 import { AUDIO_PAGE } from 'utils/route'
-import { formatWei } from 'utils/wallet'
+import { formatWei, stringWeiToBN } from 'utils/wallet'
 
 import styles from './NavAudio.module.css'
 
@@ -23,11 +22,14 @@ const messages = {
 }
 
 const NavAudio = () => {
+  // TODO: remove this feature flag ASAP as rewards launches because we could show $AUDIO far earlier
+  // but need to wait for remote config
   const { isEnabled } = useFlag(FeatureFlags.SURFACE_AUDIO_ENABLED)
 
   const navigate = useNavigateToPage()
   const account = useSelector(getAccountUser)
-  const totalBalance = useSelector(getAccountTotalBalance) ?? null
+  const totalBalance =
+    account && account.balance ? stringWeiToBN(account.balance) : null
   const nonNullTotalBalance = totalBalance !== null
   const positiveTotalBalance =
     nonNullTotalBalance && totalBalance!.gt(new BN(0))
@@ -41,7 +43,10 @@ const NavAudio = () => {
   }
 
   return positiveTotalBalance ? (
-    <div className={cn(styles.audio, { [styles.show]: true })}>
+    <div
+      onClick={() => navigate(AUDIO_PAGE)}
+      className={cn(styles.audio, styles.hasBalance, { [styles.show]: true })}
+    >
       {audioBadge &&
         cloneElement(audioBadge, {
           height: 16,
