f4eb1bb7a6f1538ec604c6b6a303273d6576a895
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2021-11-24 13:46:21 -0500

    Switch to using pipeline

--- creator-node/src/utils.js
+++ creator-node/src/utils.js
@@ -4,7 +4,9 @@ const path = require('path')
 const { BufferListStream } = require('bl')
 const axios = require('axios')
 const spawn = require('child_process').spawn
+const stream = require('stream')
 const { promisify } = require('util')
+const pipeline = promisify(stream.pipeline)
 
 const { logger: genericLogger } = require('./logging')
 const models = require('./models')
@@ -484,20 +486,22 @@ async function createDirForFile (fileStoragePath) {
   await fs.ensureDir(dir)
 }
 
-async function writeStreamToFileSystem (stream, expectedStoragePath, createDir = false) {
+async function writeStreamToFileSystem (inputStream, expectedStoragePath, createDir = false) {
   if (createDir) {
     await createDirForFile(expectedStoragePath)
   }
 
-  const destinationStream = fs.createWriteStream(expectedStoragePath)
-  stream.pipe(destinationStream)
-  return new Promise((resolve, reject) => {
-    destinationStream.on('finish', () => {
-      resolve()
-    })
-    destinationStream.on('error', err => { reject(err) })
-    stream.on('error', err => { destinationStream.end(); reject(err) })
-  })
+  await streamFileToDiskHelper(inputStream, expectedStoragePath)
+}
+
+async function streamFileToDiskHelper (inputStream, expectedStoragePath) {
+  // https://nodejs.org/en/docs/guides/backpressuring-in-streams/
+  // Cleaner way to handle piping data between streams since this handles all
+  // events such as finish, error, end etc in addition to being async/awaited
+  await pipeline(
+    inputStream, // input stream
+    fs.createWriteStream(expectedStoragePath) // output stream
+  )
 }
 
 /**
