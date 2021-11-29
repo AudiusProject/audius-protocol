3c39baadc0deb79ed7d00c7259bdd43bb2a40a60
jowlee <joeylee0925@gmail.com>
2021-11-29 14:47:32 +0000

    Add back z and deps

--- discovery-provider/src/trending_strategies/aSPET_trending_tracks_strategy.py
+++ discovery-provider/src/trending_strategies/aSPET_trending_tracks_strategy.py
@@ -1,6 +1,8 @@
 import logging
 import time
 
+from datetime import datetime
+from dateutil.parser import parse
 from sqlalchemy.sql import text
 
 from src.trending_strategies.base_trending_strategy import BaseTrendingStrategy
@@ -24,6 +26,28 @@ i = 0.01
 q = 100000.0
 T = {"day": 1, "week": 7, "month": 30, "year": 365, "allTime": 100000}
 y = 3
+
+def z(time, track):
+    # pylint: disable=W,C,R
+    E = track["listens"]
+    e = track["windowed_repost_count"]
+    t = track["repost_count"]
+    x = track["windowed_save_count"]
+    A = track["save_count"]
+    o = track["created_at"]
+    l = track["owner_follower_count"]
+    j = track["karma"]
+    if l < y:
+        return {"score": 0, **track}
+    H = (N * E + F * e + O * x + R * t + i * A) * j
+    L = T[time]
+    K = datetime.now()
+    w = parse(o)
+    k = (K - w).days
+    Q = 1
+    if k > L:
+        Q = a((1.0 / q), (M(q, (1 - k / L))))
+    return {"score": H * Q, **track}
 
 class TrendingTracksStrategyaSPET(BaseTrendingStrategy):
     def __init__(self):
