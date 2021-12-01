11e09ec4de879bb6a322e5335f869806581037c7
Michael Piazza <michael.piazza.mp@gmail.com>
2021-12-01 07:02:38 +0000

    Libs: main rewards attester work

--- libs/src/index.js
+++ libs/src/index.js
@@ -29,6 +29,7 @@ const Web3 = require('./web3')
 const SolanaUtils = require('./services/solanaWeb3Manager/utils')
 
 const { Keypair } = require('@solana/web3.js')
+const RewardsAttester = require('./services/solanaWeb3Manager/rewardsAttester')
 
 class AudiusLibs {
   /**
@@ -490,3 +491,4 @@ module.exports.AudiusABIDecoder = AudiusABIDecoder
 module.exports.Utils = Utils
 module.exports.SolanaUtils = SolanaUtils
 module.exports.SanityChecks = SanityChecks
+module.exports.RewardsAttester = RewardsAttester
