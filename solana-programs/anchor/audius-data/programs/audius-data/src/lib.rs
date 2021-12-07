bd0be5fef1b238c8d8ea235cffb7c83e714fc649
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2021-12-07 18:43:20 -0500

    SECP example functioning with 'claim'

--- solana-programs/anchor/audius-data/programs/audius-data/src/lib.rs
+++ solana-programs/anchor/audius-data/programs/audius-data/src/lib.rs
@@ -4,6 +4,9 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod audius_data {
+    use anchor_lang::solana_program::sysvar;
+    use anchor_lang::solana_program::secp256k1_program;
+
     use super::*;
     // Initialize an admin address
     pub fn initialize_admin(ctx: Context<Initialize>, authority: Pubkey) -> ProgramResult {
@@ -17,7 +20,7 @@ pub mod audius_data {
         eth_address: [u8; 20],
         _handle_seed: [u8; 16],
         _user_bump: u8,
-        metadata: String
+        metadata: String,
     ) -> ProgramResult {
         let audius_user_acct = &mut ctx.accounts.user;
         audius_user_acct.eth_address = eth_address;
@@ -31,7 +34,33 @@ pub mod audius_data {
         Ok(())
     }
 
-    pub fn test(ctx: Context<Test>) -> ProgramResult {
+    pub fn init_user_sol(ctx: Context<InitializeUserSolIdentity>, sol_pub_key: Pubkey) -> ProgramResult {
+        let audius_user_acct = &mut ctx.accounts.user;
+        let index_current_instruction = sysvar::instructions::load_current_index_checked(
+            &ctx.accounts.sysvar_program
+        )?;
+        // instruction must contain at least one prior
+        if index_current_instruction < 1 {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+
+        // Eth_address offset (12) + address (20) + signature (65) = 97
+        // TODO: Validate message contents
+        let eth_address_offset = 12;
+        let secp_data = sysvar::instructions::load_instruction_at_checked(
+            0,
+            &ctx.accounts.sysvar_program)?;
+
+        if secp_data.program_id != secp256k1_program::id() {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+        let instruction_signer =
+            secp_data.data[eth_address_offset..eth_address_offset + 20].to_vec();
+
+        // Update if valid
+        if instruction_signer == audius_user_acct.eth_address {
+            audius_user_acct.solana_pub_key = sol_pub_key;
+        }
         Ok(())
     }
 }
@@ -66,9 +95,15 @@ pub struct InitializeUser<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Test<'info> {
-    pub secp_program: AccountInfo<'info>
+pub struct InitializeUserSolIdentity<'info> {
+    #[account(mut)]
+    pub user: Account<'info, User>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub sysvar_program: AccountInfo<'info>
 }
+
+// END Instructions
 
 // Storage
 #[account]
--- solana-programs/anchor/audius-data/tests/audius-data.ts
+++ solana-programs/anchor/audius-data/tests/audius-data.ts
@@ -2,7 +2,14 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { AudiusData } from '../target/types/audius_data';
 import BN from 'bn.js';
-const { SystemProgram, PublicKey } = anchor.web3;
+import * as secp256k1 from 'secp256k1';
+import ethWeb3 from 'web3';
+const { randomBytes } = require('crypto')
+
+// import * as keccak256 from "keccak256";
+
+import keccak256 from 'keccak256';
+const { SystemProgram, PublicKey, Transaction, Secp256k1Program } = anchor.web3;
 
 describe('audius-data', () => {
   const provider = anchor.Provider.local()
@@ -12,6 +19,9 @@ describe('audius-data', () => {
 
   const program = anchor.workspace.AudiusData as Program<AudiusData>;
 
+  const SystemSysVarProgramKey = new PublicKey("Sysvar1nstructions1111111111111111111111111")
+  const ethWeb3Utils = new ethWeb3()
+
   let adminKeypair = anchor.web3.Keypair.generate()
   let adminStgKeypair = anchor.web3.Keypair.generate()
   let userKeypair = anchor.web3.Keypair.generate()
@@ -19,6 +29,15 @@ describe('audius-data', () => {
   console.log(`adminKeypair = ${adminKeypair.publicKey.toString()}`)
   console.log(`adminStgKeypair ${adminStgKeypair.publicKey.toString()}`)
   console.log(`userKeypair ${userKeypair.publicKey.toString()}`)
+
+  let getRandomPrivateKey = () => {
+    const msg = randomBytes(32)
+    let privKey: Uint8Array
+    do {
+      privKey = randomBytes(32)
+    } while (!secp256k1.privateKeyVerify(privKey))
+    return privKey
+  }
 
   it('Initializing admin account!', async () => {
     // Add your test here.
@@ -41,19 +60,21 @@ describe('audius-data', () => {
   });
 
   it('Initializing user!', async () => {
-    let testEthAddr = '0x6A60013EB5ed20B2F0673D46ADE0b2Dcd00d2CDE'
+    let privKey = getRandomPrivateKey()
+    let pkString = Buffer.from(privKey).toString('hex')
+    let pubKey = ethWeb3Utils.eth.accounts.privateKeyToAccount(pkString)
+    let testEthAddr = pubKey.address
     let testEthAddrBytes = ethAddressToArray(testEthAddr)
     let handle = "testUserHandle"
     let handleBytes = Buffer.from(anchor.utils.bytes.utf8.encode(handle))
     let handleBytesArray = Array.from({...handleBytes, length: 16})
     let metadata = "QmTXWUkHWBUJ878gi2B25q6N4BWnjojLbSExyaqJxSDPrH"
-    let metadataBytes = anchor.utils.bytes.utf8.encode(metadata)
 
     const [user_account_pda, user_account_bump] = await PublicKey.findProgramAddress(
       [Buffer.from(handleBytesArray)],
       program.programId
     );
-    let tx = await program.rpc.initUser(
+     await program.rpc.initUser(
       Array.from(testEthAddrBytes),
       handleBytesArray,
       user_account_bump,
@@ -71,24 +92,126 @@ describe('audius-data', () => {
     )
 
     let userDataFromChain = await program.account.user.fetch(user_account_pda)
-    let returnedHex = `0x${toHexString(userDataFromChain.ethAddress)}`
+    let returnedHex = ethWeb3Utils.utils.bytesToHex(userDataFromChain.ethAddress)
     console.log(`Eth address from chain ${returnedHex} | Original eth ${testEthAddr}`)
+
+    let returnedSolFromChain = userDataFromChain.solanaPubKey
+    console.log(`returnedSolFromChain  = ${returnedSolFromChain}`)
 
     if (testEthAddr.toLowerCase() == returnedHex) {
       console.log(`Retrieved address!`)
     }
   });
+
+  it('Initializing + claiming user!', async () => {
+    let privKey = getRandomPrivateKey()
+    let pkString = Buffer.from(privKey).toString('hex')
+    let pubKey = ethWeb3Utils.eth.accounts.privateKeyToAccount(pkString)
+    let testEthAddr = pubKey.address
+    let testEthAddrBytes = ethAddressToArray(testEthAddr)
+    let handle = "testUserHandle2"
+    let handleBytes = Buffer.from(anchor.utils.bytes.utf8.encode(handle))
+    let handleBytesArray = Array.from({...handleBytes, length: 16})
+    let metadata = "QmTXWUkHWBUJ878gi2B25q6N4BWnjojLbSExyaqJxSDPrH"
+
+    const [user_account_pda, user_account_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(handleBytesArray)],
+      program.programId
+    );
+     await program.rpc.initUser(
+      Array.from(testEthAddrBytes),
+      handleBytesArray,
+      user_account_bump,
+      metadata,
+      {
+        accounts: {
+          admin: adminStgKeypair.publicKey,
+          payer: provider.wallet.publicKey,
+          user: user_account_pda,
+          authority: adminKeypair.publicKey,
+          systemProgram: SystemProgram.programId
+        },
+        signers: [adminKeypair]
+      }
+    )
+
+    let userDataFromChain = await program.account.user.fetch(user_account_pda)
+    let returnedHex = ethWeb3Utils.utils.bytesToHex(userDataFromChain.ethAddress)
+    console.log(`Eth address from chain ${returnedHex} | Original eth ${testEthAddr}`)
+
+    let returnedSolFromChain = userDataFromChain.solanaPubKey
+    console.log(`returnedSolFromChain  = ${returnedSolFromChain}`)
+
+    if (testEthAddr.toLowerCase() == returnedHex) {
+      console.log(`Retrieved address!`)
+    }
+
+    // New sol key that will be used to permission user updates
+    let newUserKey = anchor.web3.Keypair.generate()
+    console.log(`newUserKey = ${newUserKey.publicKey}`)
+    console.log(`ethAddressBytes = ${testEthAddrBytes}`)
+
+    // Generate signed SECP instruction
+    // Message as the incoming public key
+    let message = newUserKey.publicKey.toString()
+
+    let signedBytes = signBytes(Buffer.from(message), pkString)
+    const { signature, recoveryId } = signedBytes
+
+    // Get the public key in a compressed format
+    const ethPubkey = secp256k1.publicKeyCreate(privKey, false).slice(1)
+    const secpTransactionInstruction = Secp256k1Program.createInstructionWithPublicKey({
+      publicKey: Buffer.from(ethPubkey),
+      message: Buffer.from(message),
+      signature,
+      recoveryId
+    })
+
+    let tx = await provider.send(
+      (() => {
+        const tx = new Transaction();
+        tx.add(secpTransactionInstruction),
+        tx.add(
+          program.instruction.initUserSol(
+            newUserKey.publicKey,
+            {
+              accounts:
+              {
+                user: user_account_pda,
+                payer: provider.wallet.publicKey,
+                sysvarProgram: SystemSysVarProgramKey
+              }
+            }
+          )
+        )
+        return tx;
+      })(),
+      [
+        // Signers
+      ]
+    );
+    userDataFromChain = await program.account.user.fetch(user_account_pda)
+    returnedHex = ethWeb3Utils.utils.bytesToHex(userDataFromChain.ethAddress)
+    console.log(`Eth address from chain ${returnedHex} | Sol address from chain ${userDataFromChain.solanaPubKey}`)
+  });
+
 });
 
-function toHexString(byteArray) {
-  return byteArray.reduce((output, elem) =>
-    (output + ('0' + elem.toString(16)).slice(-2)),
-    '');
+let signBytes = (bytes: any, ethPrivateKey: WithImplicitCoercion<string> | { [Symbol.toPrimitive](hint: "string"): string; }) => {
+  const ethPrivateKeyArr = Buffer.from(ethPrivateKey, 'hex')
+  const msgHash = keccak256(bytes)
+  const signatureObj = secp256k1.ecdsaSign(
+    Uint8Array.from(msgHash),
+    ethPrivateKeyArr
+  )
+  const signature = Buffer.from(signatureObj.signature)
+  return {
+    signature,
+    recoveryId: signatureObj.recid
+  }
 }
-
 
 function ethAddressToArray(ethAddress: any) {
   const strippedEthAddress = ethAddress.replace('0x', '')
   return Uint8Array.of(...new BN(strippedEthAddress, 'hex').toArray('be'))
 }
-
