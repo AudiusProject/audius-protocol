e8c65338636f9f3f8b6a424eb56910c080ccbc42
Sebastian Klingler <sliptype@gmail.com>
2021-12-14 15:26:02 -0500

    [AUD-1115] Add Remote Config (#179)
    * Utilize remote-config from common, add useFlag and useRemoteVar
    
    * Remove any
    
    * Pin deps
    
    * Add back dropped dep
    
    * Fix issue with manual android version bump
    
    * Revert "Fix issue with manual android version bump"
    
    This reverts commit 6191935bff3729c873627fb1a3114becee3fceaa.
    
    * Update client sha

---
+++ audius-client/packages/mobile/src/services/remote-config/remote-config-instance.ts
@@ -0,0 +1,21 @@
+import * as optimizely from '@optimizely/react-sdk'
+import AsyncStorage from '@react-native-community/async-storage'
+import { remoteConfig } from 'audius-client/src/common/services/remote-config/remote-config'
+import Config from 'react-native-config'
+
+export const FEATURE_FLAG_ASYNC_STORAGE_SESSION_KEY = 'featureFlagSessionId'
+
+export const remoteConfigInstance = remoteConfig({
+  createOptimizelyClient: async () => {
+    return optimizely.createInstance({
+      sdkKey: Config.OPTIMIZELY_KEY
+    })
+  },
+  getFeatureFlagSessionId: async () =>
+    AsyncStorage.getItem(FEATURE_FLAG_ASYNC_STORAGE_SESSION_KEY),
+  setFeatureFlagSessionId: async id =>
+    AsyncStorage.setItem(FEATURE_FLAG_ASYNC_STORAGE_SESSION_KEY, id),
+  setLogLevel: () => optimizely.setLogLevel('warn')
+})
+
+remoteConfigInstance.init()
