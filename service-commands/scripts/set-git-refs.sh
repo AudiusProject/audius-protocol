e6c69882177d477178dee65215da7e3d745cad94
Joaquin Casares <joaquin@audius.co>
2021-12-15 18:57:09 -0600

    _ != -

--- service-commands/scripts/set-git-refs.sh
+++ service-commands/scripts/set-git-refs.sh
@@ -8,7 +8,7 @@ git checkout $1
 git pull
 
 if [[ -d $PROTOCOL_DIR/../audius-client ]]; then
-	cd $PROTOCOL_DIR/../audius_client
+	cd $PROTOCOL_DIR/../audius-client
 	git fetch origin
 	git checkout $2
 	git pull
