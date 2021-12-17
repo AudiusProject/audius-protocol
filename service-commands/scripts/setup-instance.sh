eec05cbc174d7c39526e3ca0cd1252cb43b45547
Joaquin Casares <joaquin@audius.co>
2021-12-16 19:44:38 -0600

    convert ws to tabs

--- service-commands/scripts/setup-instance.sh
+++ service-commands/scripts/setup-instance.sh
@@ -101,7 +101,7 @@ case "$service" in
 			"&& git clone https://github.com/AudiusProject/audius-protocol.git" \
 			"&& PROTOCOL_DIR=audius-protocol bash ~/audius-protocol/service-commands/scripts/set-git-refs.sh $audius_protocol_git_ref $audius_client_git_ref" \
 			"&& yes | bash audius-protocol/service-commands/scripts/provision-dev-env.sh"
-        reboot_instance $provider $name
+		reboot_instance $provider $name
 
 		wait_for_instance $provider $user $name
 
