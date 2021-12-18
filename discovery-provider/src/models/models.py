e96ccb85cc37c448712d7cc731471e71f8bb433f
Isaac <isaac@audius.co>
2021-12-18 02:54:41 +0000

    remove unnecessary index in model

--- discovery-provider/src/models/models.py
+++ discovery-provider/src/models/models.py
@@ -588,8 +588,6 @@ class HourlyPlayCounts(Base):
     hourly_timestamp = Column(DateTime, primary_key=True, nullable=False)
     play_count = Column(Integer, nullable=False, index=False)
 
-    Index("hourly_timestamp_idx", "hourly_timestamp", unique=True)
-
     def __repr__(self):
         return f"<HourlyPlayCounts(\
 hourly_timestamp={self.hourly_timestamp},\
