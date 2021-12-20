68c46c353d1ac71f0e777de7c90cb15555c632a8
Sebastian Klingler <sliptype@gmail.com>
2021-12-20 17:18:11 -0500

    [AUD-1007] Api Rewards Explainer (#185)
    * Add TabSlider
    
    * Add TrendingRewardsDrawer
    
    * Add TweetEmbed and LoadingSpinner
    
    * Use non common remote config
    
    * Add ButtonWithArrow
    
    * Add options to TweetEmbed
    
    * Open TweetEmbed links in browser
    
    * Adjust styling of drawer title
    
    * Add ApiRewardsDrawer
    
    * Address PR comments
    
    * Add styling
    
    * Update client sha

---
+++ audius-client/packages/mobile/src/components/api-rewards-drawer/ApiRewardsDrawer.tsx
@@ -0,0 +1,100 @@
+import React, { useCallback } from 'react'
+
+import {
+  getModalVisibility,
+  setVisibility
+} from 'audius-client/src/common/store/ui/modals/slice'
+import { Image, Linking, StyleSheet, View } from 'react-native'
+
+import AudiusAPI from 'app/assets/images/audiusAPI.png'
+import ButtonWithArrow from 'app/components/button-with-arrow'
+import Drawer from 'app/components/drawer'
+import GradientText from 'app/components/gradient-text'
+import Text from 'app/components/text'
+import { useDispatchWeb } from 'app/hooks/useDispatchWeb'
+import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
+import { useThemedStyles } from 'app/hooks/useThemedStyles'
+import { ThemeColors } from 'app/utils/theme'
+
+const messages = {
+  modalTitle: 'Audius API',
+  title: "It's easy to build your own app on Audius",
+  description: 'The top 10 Audius API apps each month win',
+  button: 'Learn More About The Audius API'
+}
+
+const API_LINK = 'https://audius.org/api'
+const MODAL_NAME = 'APIRewardsExplainer'
+
+const createStyles = (themeColors: ThemeColors) =>
+  StyleSheet.create({
+    content: {
+      padding: 32,
+      display: 'flex',
+      alignItems: 'center'
+    },
+    drawerTitle: {
+      marginTop: 8,
+      marginBottom: 32,
+      fontSize: 28
+    },
+    image: {
+      height: 100,
+      width: 120,
+      marginBottom: 32
+    },
+    title: {
+      marginBottom: 24,
+      color: themeColors.secondary,
+      fontSize: 24,
+      textAlign: 'center'
+    },
+    subtitle: {
+      color: themeColors.neutralLight4,
+      marginBottom: 24
+    },
+    buttonText: {
+      fontSize: 16
+    }
+  })
+
+const ApiRewardsDrawer = () => {
+  const dispatchWeb = useDispatchWeb()
+  const styles = useThemedStyles(createStyles)
+
+  const isOpen = useSelectorWeb(state => getModalVisibility(state, MODAL_NAME))
+
+  const handleClose = useCallback(() => {
+    dispatchWeb(setVisibility({ modal: MODAL_NAME, visible: false }))
+  }, [dispatchWeb])
+
+  const onClickAudiusAPI = useCallback(() => {
+    Linking.openURL(API_LINK)
+  }, [])
+
+  return (
+    <Drawer isOpen={isOpen} onClose={handleClose}>
+      <View style={styles.content}>
+        <GradientText
+          style={styles.drawerTitle}
+          text={messages.modalTitle}
+        ></GradientText>
+        <Image style={styles.image} source={AudiusAPI} />
+        <Text style={styles.title} weight='bold'>
+          {messages.title}
+        </Text>
+        <Text style={styles.subtitle} weight='bold'>
+          {messages.description}
+        </Text>
+        <ButtonWithArrow
+          title={messages.button}
+          textStyle={styles.buttonText}
+          style={styles.button}
+          onPress={onClickAudiusAPI}
+        />
+      </View>
+    </Drawer>
+  )
+}
+
+export default ApiRewardsDrawer
