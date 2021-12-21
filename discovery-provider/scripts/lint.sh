8ea95cfccdb36251bf542be7170666f8c2fb0862
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-12-20 23:58:19 -0500

    Add Python standards globally

--- discovery-provider/scripts/lint.sh
+++ discovery-provider/scripts/lint.sh
@@ -1,5 +1,9 @@
 set -e
 
 pylint src/
-pylint integration_tests/
+pylint tests/
+isort --check-only src/
+isort --check-only tests/
+black --check src/
+black --check tests/
 mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers src/
