95ef480b01d9ee5fff286cc01cae260b1c834c13
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2021-12-22 11:53:39 -0500

    Fix lint script

--- discovery-provider/scripts/lint.sh
+++ discovery-provider/scripts/lint.sh
@@ -2,9 +2,9 @@ set -e
 
 cd $PROTOCOL_DIR
 pylint discovery-provider/src/
-pylint discovery-provider/tests/
+pylint discovery-provider/integration_tests/
 isort --check-only discovery-provider/src/
-isort --check-only discovery-provider/tests/
+isort --check-only discovery-provider/integration_tests/
 black --check discovery-provider/src/
-black --check discovery-provider/tests/
+black --check discovery-provider/integration_tests/
 mypy --ignore-missing-imports --follow-imports=silent --show-column-numbers discovery-provider/src/
