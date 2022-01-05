becae58341f98c2bf3b3c9af15168f486ae301e4
Dylan Jeffers <dylan@audius.co>
2022-01-04 18:36:37 -0800

    [AUD-1034] Fix default CoverPhoto tiling (#722)

--- apps/audius-client/packages/web/src/components/general/CoverPhoto.tsx
+++ apps/audius-client/packages/web/src/components/general/CoverPhoto.tsx
@@ -1,12 +1,12 @@
 import React, { memo, useState } from 'react'
 
 import cn from 'classnames'
-import PropTypes from 'prop-types'
+import { FileWithPreview } from 'react-dropzone'
 import Lottie from 'react-lottie'
 
 import loadingSpinner from 'assets/animations/loadingSpinner.json'
 import imageCoverPhotoBlank from 'assets/img/imageCoverPhotoBlank.jpg'
-import { WidthSizes } from 'common/models/ImageSizes'
+import { CoverPhotoSizes, WidthSizes } from 'common/models/ImageSizes'
 import DynamicImage from 'components/dynamic-image/DynamicImage'
 import ImageSelectionButton from 'components/image-selection/ImageSelectionButton'
 import { useUserCoverPhoto } from 'hooks/useImageSize'
@@ -17,29 +17,49 @@ const messages = {
   imageName: 'Cover Photo'
 }
 
-const CoverPhoto = props => {
+type CoverPhotoProps = {
+  userId: number | null
+  coverPhotoSizes: CoverPhotoSizes | null
+  updatedCoverPhoto?: string
+  className?: string
+  loading?: boolean
+  error?: boolean
+  edit?: boolean
+  darken?: boolean
+  onDrop?: (
+    file: FileWithPreview[],
+    source: 'original' | 'unsplash' | 'url'
+  ) => Promise<void>
+}
+
+const CoverPhoto = ({
+  userId,
+  coverPhotoSizes,
+  updatedCoverPhoto,
+  className,
+  error,
+  edit = false,
+  darken = false,
+  onDrop
+}: CoverPhotoProps) => {
   const [processing, setProcessing] = useState(false)
-  const gradient = props.darken
+  const gradient = darken
     ? 'linear-gradient(180deg, rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.75) 100%)'
     : 'linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.05) 70%, rgba(0, 0, 0, 0.2) 100%)'
 
-  const image = useUserCoverPhoto(
-    props.userId,
-    props.coverPhotoSizes,
-    WidthSizes.SIZE_2000
-  )
-  let backgroundImage = {}
+  const image = useUserCoverPhoto(userId, coverPhotoSizes, WidthSizes.SIZE_2000)
+  let backgroundImage = ''
   let backgroundStyle = {}
   let immediate = false
-  if (props.coverPhotoSizes) {
-    if (image === imageCoverPhotoBlank && !props.updatedCoverPhoto) {
+  if (coverPhotoSizes) {
+    if (image === imageCoverPhotoBlank && !updatedCoverPhoto) {
       backgroundImage = `${gradient}, url(${imageCoverPhotoBlank})`
       backgroundStyle = {
         backgroundRepeat: 'repeat',
-        backgroundSize: '100% 100%, 35%'
+        backgroundSize: 'auto'
       }
     } else {
-      backgroundImage = `${gradient}, url(${props.updatedCoverPhoto || image})`
+      backgroundImage = `${gradient}, url(${updatedCoverPhoto || image})`
       backgroundStyle = {
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover'
@@ -50,14 +70,17 @@ const CoverPhoto = props => {
     immediate = true
   }
 
-  const onDrop = async (file, source) => {
+  const handleDrop = async (
+    file: Promise<FileWithPreview[]>,
+    source: 'original' | 'unsplash' | 'url'
+  ) => {
     setProcessing(true)
     const image = await file
-    await props.onDrop([].concat(image), source)
+    await onDrop?.(([] as FileWithPreview[]).concat(image), source)
     setProcessing(false)
   }
 
-  const loading = (
+  const loadingElement = (
     <div className={cn(styles.overlay, { [styles.processing]: processing })}>
       <Lottie
         options={{ loop: true, autoplay: true, animationData: loadingSpinner }}
@@ -66,7 +89,7 @@ const CoverPhoto = props => {
   )
 
   return (
-    <div className={cn(styles.coverPhoto, props.className)}>
+    <div className={cn(styles.coverPhoto, className)}>
       <DynamicImage
         image={backgroundImage}
         isUrl={false}
@@ -75,40 +98,23 @@ const CoverPhoto = props => {
         usePlaceholder={false}
         immediate={immediate}
       >
-        <div className={styles.spinner}>{processing ? loading : null}</div>
+        <div className={styles.spinner}>
+          {processing ? loadingElement : null}
+        </div>
       </DynamicImage>
       <div className={styles.button}>
-        {props.edit ? (
+        {edit ? (
           <ImageSelectionButton
             imageName={messages.imageName}
-            hasImage={!!image || props.updatedCoverPhoto}
-            error={!!props.error}
-            onSelect={onDrop}
+            hasImage={Boolean(image || updatedCoverPhoto)}
+            error={!!error}
+            onSelect={handleDrop}
             source='CoverPhoto'
           />
         ) : null}
       </div>
     </div>
   )
-}
-
-CoverPhoto.propTypes = {
-  userId: PropTypes.number,
-  coverPhotoSizes: PropTypes.object,
-  updatedCoverPhoto: PropTypes.string,
-  className: PropTypes.string,
-  loading: PropTypes.bool,
-  error: PropTypes.bool,
-  edit: PropTypes.bool,
-  darken: PropTypes.bool,
-  onDrop: PropTypes.func
-}
-
-CoverPhoto.defaultProps = {
-  loading: false,
-  edit: false,
-  darken: false,
-  onDrop: () => {}
 }
 
 export default memo(CoverPhoto)
