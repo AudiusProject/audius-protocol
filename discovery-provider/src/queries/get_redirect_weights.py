7f898b889e7b1702f42478f28c52d83d925b7821
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-01-07 12:08:56 -0500

    Use all endpoints if none

--- discovery-provider/src/queries/get_redirect_weights.py
+++ discovery-provider/src/queries/get_redirect_weights.py
@@ -11,7 +11,7 @@ bp = Blueprint("redirect_weights", __name__)
 
 
 @bp.route("/redirect_weights", methods=["GET"])
-def get_redirect_weights():
+def redirect_weights():
     endpoints, _ = get_all_other_nodes()
     loads = {}
     for endpoint in endpoints:
@@ -19,7 +19,10 @@ def get_redirect_weights():
         if response.status_code == 200:
             loads[endpoint] = int(response.text)
 
-    max_load = max(loads.values())
+    if len(loads) == 0:
+        loads = {endpoint: 1 for endpoint in endpoints}
+
+    max_load = max(loads.values(), default=1)
     redirect_weights = {}
     for endpoint, load in loads.items():
         redirect_weights[endpoint] = (max_load - load) + 5
