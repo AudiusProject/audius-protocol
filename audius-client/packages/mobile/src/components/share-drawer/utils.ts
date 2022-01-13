5d052ab19f48e474d0bf778a816a957cc0f3f5bf
Dylan Jeffers <dylan@audius.co>
2022-01-12 21:11:31 -0800

    [AUD-1273] consume new share state, handle collections (#218)

---
+++ audius-client/packages/mobile/src/components/share-drawer/utils.ts
@@ -0,0 +1,76 @@
+import { ShareModalContent } from 'audius-client/src/common/store/ui/share-modal/types'
+
+import { UserCollection } from 'app/models/Collection'
+import {
+  getCollectionRoute,
+  getTrackRoute,
+  getUserRoute
+} from 'app/utils/routes'
+import { getTwitterLink } from 'app/utils/twitter'
+
+import { messages } from './messages'
+
+export const getContentUrl = (content: ShareModalContent) => {
+  switch (content.type) {
+    case 'track': {
+      const { track } = content
+      return getTrackRoute(track, true)
+    }
+    case 'profile': {
+      const { profile } = content
+      return getUserRoute(profile)
+    }
+    case 'album': {
+      const { album, artist } = content
+      return getCollectionRoute(
+        ({ ...album, user: artist } as unknown) as UserCollection,
+        true
+      )
+    }
+    case 'playlist': {
+      const { playlist, creator } = content
+      return getCollectionRoute(
+        ({ ...playlist, user: creator } as unknown) as UserCollection,
+        true
+      )
+    }
+  }
+}
+
+export const getTwitterShareText = (content: ShareModalContent) => {
+  switch (content.type) {
+    case 'track': {
+      const {
+        track: { title },
+        artist: { handle }
+      } = content
+      return messages.trackShareText(title, handle)
+    }
+    case 'profile': {
+      const {
+        profile: { handle }
+      } = content
+      return messages.profileShareText(handle)
+    }
+    case 'album': {
+      const {
+        album: { playlist_name },
+        artist: { handle }
+      } = content
+      return messages.albumShareText(playlist_name, handle)
+    }
+    case 'playlist': {
+      const {
+        playlist: { playlist_name },
+        creator: { handle }
+      } = content
+      return messages.playlistShareText(playlist_name, handle)
+    }
+  }
+}
+
+export const getTwitterShareUrl = (content: ShareModalContent) => {
+  const url = getContentUrl(content)
+  const shareText = getTwitterShareText(content)
+  return getTwitterLink(url, shareText)
+}
