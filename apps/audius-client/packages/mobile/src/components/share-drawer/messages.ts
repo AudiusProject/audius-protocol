d8ac3c567f2ad37dd3a9556b9b98f7890a97fd8d
Dylan Jeffers <dylan@audius.co>
2022-01-12 21:11:31 -0800

    [AUD-1273] consume new share state, handle collections (#218)

---
+++ apps/audius-client/packages/mobile/src/components/share-drawer/messages.ts
@@ -0,0 +1,25 @@
+import { ShareType } from 'audius-client/src/common/store/ui/share-modal/types'
+
+const shareTypeMap: Record<ShareType, string> = {
+  track: 'Track',
+  profile: 'Profile',
+  album: 'Album',
+  playlist: 'Playlist'
+}
+
+export const messages = {
+  modalTitle: (asset: ShareType) => `Share ${shareTypeMap[asset]}`,
+  twitter: 'Share to Twitter',
+  tikTok: 'Share Sound to TikTok',
+  copyLink: (asset: ShareType) => `Copy Link to ${shareTypeMap[asset]}`,
+  shareSheet: (asset: ShareType) => `Share ${asset} via...`,
+  toast: (asset: ShareType) => `Copied Link to ${shareTypeMap[asset]}`,
+  trackShareText: (title: string, handle: string) =>
+    `Check out ${title} by ${handle} on @AudiusProject #Audius`,
+  profileShareText: (handle: string) =>
+    `Check out ${handle} on @AudiusProject #Audius`,
+  albumShareText: (albumName: string, handle: string) =>
+    `Check out ${albumName} by ${handle} @AudiusProject #Audius`,
+  playlistShareText: (playlistName: string, handle: string) =>
+    `Check out ${playlistName} by ${handle} @AudiusProject #Audius`
+}
