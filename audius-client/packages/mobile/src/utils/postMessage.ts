2cfbe56f27a946dfc3fc9f011b945b962f1970c2
Sebastian Klingler <sliptype@gmail.com>
2022-01-19 11:20:12 -0600

    [AUD-1306] Add logging around null message sender (#230)
    * Null check message sender
    
    * Add more logging

--- audius-client/packages/mobile/src/utils/postMessage.ts
+++ audius-client/packages/mobile/src/utils/postMessage.ts
@@ -1,3 +1,5 @@
+import { Maybe } from 'audius-client/src/common/utils/typeUtils'
+
 import { MessageType, Message } from '../message'
 
 export type MessageSender = {
@@ -7,12 +9,20 @@ export type MessageSender = {
 const SPAMMY_MESSAGES = new Set<string>([MessageType.GET_POSITION])
 
 // Stringifies the message, logs it, and sends it
-export const postMessage = (sender: MessageSender, message: Message) => {
+export const postMessage = (sender: Maybe<MessageSender>, message: Message) => {
   const stringified = JSON.stringify(message)
 
   // Log it if it isn't spammy
   if (!SPAMMY_MESSAGES.has(message.type)) {
     console.debug(`Sending message to web client: ${stringified}`)
+  }
+
+  // On some versions of android the MessageSender isn't available for a short period on
+  // startup. Logging more info to determine culprit message
+  if (!sender) {
+    throw new Error(
+      `MessageSender undefined. Trying to post message: ${stringified}`
+    )
   }
 
   sender.postMessage(stringified)
