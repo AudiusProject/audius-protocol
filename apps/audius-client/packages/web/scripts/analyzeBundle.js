796ea81399f4f20f172ee1abcbd222f2b0e0a33e
Dylan Jeffers <dylan@audius.co>
2022-01-20 15:54:39 -0800

    Lazy-load sign-in page, reinstate analyzeBundle (#809)

--- apps/audius-client/packages/web/scripts/analyzeBundle.js
+++ apps/audius-client/packages/web/scripts/analyzeBundle.js
@@ -1,13 +1,18 @@
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production'
 
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const webpackConfigProd = require('react-scripts/config/webpack.config')('production');
+const webpackConfigProd = require('react-scripts/config/webpack.config')(
+  'production'
+)
+const webpack = require('webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
-webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
+webpackConfigProd.plugins.push(new BundleAnalyzerPlugin())
+webpackConfigProd.output.filename = 'static/js/[name].js'
+webpackConfigProd.output.chunkFilename = 'static/js/[name].chunk.js'
 
 webpack(webpackConfigProd, (err, stats) => {
   if (err || stats.hasErrors()) {
-    console.error(err);
+    console.error(err)
   }
-});
+})
