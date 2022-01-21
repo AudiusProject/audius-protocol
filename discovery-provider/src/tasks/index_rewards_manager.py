9ebb3ae589d467534a244f2e6aa489cd23761eca
Joseph Lee <joeylee0925@gmail.com>
2022-01-20 22:02:07 -0500

    Remove skipping of disbursement on foreign user address / user challenge (#2333)
    * Remove skipping of disbursement on foreign user address / user challenge
    
    * Update missing eth recipient to custom error
    
    * Update index rewards test
    
    * Fix lint

--- discovery-provider/src/tasks/index_rewards_manager.py
+++ discovery-provider/src/tasks/index_rewards_manager.py
@@ -8,6 +8,7 @@ import base58
 from redis import Redis
 from sqlalchemy import desc
 from sqlalchemy.orm.session import Session
+from src.exceptions import MissingEthRecipientError
 from src.models import (
     ChallengeDisbursement,
     RewardManagerTransaction,
@@ -283,12 +284,19 @@ def process_batch_sol_reward_manager_txs(
                     f"index_rewards_manager.py | Challenge specifier {specifier} not found"
                     "while processing disbursement"
                 )
-                continue
             if eth_recipient not in users_map:
                 logger.error(
                     f"index_rewards_manager.py | eth_recipient {eth_recipient} not found while processing disbursement"
                 )
-                continue
+                tx_signature = tx["tx_sig"]
+                raise MissingEthRecipientError(
+                    eth_recipient,
+                    transfer_instr["challenge_id"],
+                    specifier,
+                    tx["tx_sig"],
+                    tx["slot"],
+                    f"Error: eth_recipient {eth_recipient} not found while indexing rewards manager for tx signature {tx_signature}",
+                )
 
             user_id = users_map[eth_recipient]
             logger.info(
