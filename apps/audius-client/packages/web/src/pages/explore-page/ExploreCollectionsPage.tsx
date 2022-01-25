554559ad878b2dd2fd76d8ec0e70faebd46e0915
Kyle Shanks <kj@audius.co>
2022-01-25 17:59:06 -0500

    [AUD-1331] Move Explore page state to common pages part of the store (#818)

--- apps/audius-client/packages/web/src/pages/explore-page/ExploreCollectionsPage.tsx
+++ apps/audius-client/packages/web/src/pages/explore-page/ExploreCollectionsPage.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import { connect } from 'react-redux'
 
+import { ExploreCollectionsVariant } from 'common/store/pages/explore/types'
 import { AppState } from 'store/types'
 import { isMobile } from 'utils/clientUtil'
 
 import ExploreCollectionsPageProvider from './ExploreCollectionsPageProvider'
 import DesktopCollectionsPage from './components/desktop/CollectionsPage'
 import MobileCollectionsPage from './components/mobile/CollectionsPage'
-import { ExploreCollectionsVariant } from './store/types'
 
 type OwnProps = {
   variant: ExploreCollectionsVariant
