93de849728bae092f8779b4883fb05e98580f989
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2022-01-26 20:40:17 -0500

    [AUD-1338] Consolidate batch size for indexing across sol tasks (#2391)
    * Ensure user bank and rewards manager indexing have identical batch configs to plays for speedier processing

--- discovery-provider/src/solana/constants.py
+++ discovery-provider/src/solana/constants.py
@@ -1,1 +1,10 @@
 WAUDIO_DECIMALS = 8
+
+# The size of the tx signatures for the program coming from the RPC pool per batch
+TX_SIGNATURES_BATCH_SIZE = 1000
+
+# Maximum number of batches to process at once
+TX_SIGNATURES_MAX_BATCHES = 100
+
+# Last N entries present in tx_signatures array during processing
+TX_SIGNATURES_RESIZE_LENGTH = 75
