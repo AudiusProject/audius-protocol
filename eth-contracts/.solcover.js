28f3b27b922d5dbd154842ffbe5b61072b45d6cb
Sid Sethi <3323835+SidSethi@users.noreply.github.com>
2022-01-31 20:16:45 +0000

    TNM

--- eth-contracts/.solcover.js
+++ eth-contracts/.solcover.js
@@ -1,7 +1,7 @@
 module.exports = {
   providerOptions: {
     // intentionally set to lower than 50 (default) to skip a test that requires 40 accounts
-    'total_accounts': 20
+    'total_accounts': 30
   },
   skipFiles: [
     'test'
