7a486f833a53fbff537f6c1d93d6c3a26f0e824e
Joseph Lee <joeylee0925@gmail.com>
2022-02-01 09:37:41 -0500

    Add amplitude user bank tracking (#822)
    * Add amplitude user bank tracking
    
    * Add user bank req obj

--- packages/web/src/services/audius-backend/waudio.ts
+++ packages/web/src/services/audius-backend/waudio.ts
@@ -1,10 +1,12 @@
 import { AccountInfo } from '@solana/spl-token'
 import { PublicKey } from '@solana/web3.js'
 
+import { Name } from 'common/models/Analytics'
 import { FeatureFlags } from 'common/services/remote-config'
 import { Nullable } from 'common/utils/typeUtils'
 import { waitForLibsInit } from 'services/audius-backend/eagerLoadUtils'
 import { remoteConfigInstance } from 'services/remote-config/remote-config-instance'
+import { track } from 'store/analytics/providers'
 
 // @ts-ignore
 const libs = () => window.audiusLibs
@@ -29,19 +31,31 @@ export const createUserBankIfNeeded = async () => {
     FeatureFlags.CREATE_WAUDIO_USER_BANK_ON_SIGN_UP
   )
   if (!userbankEnabled) return
+  const userId = libs().Account.getCurrentUser().user_id
   try {
     const userbankExists = await doesUserBankExist()
     if (userbankExists) return
     console.warn(`Userbank doesn't exist, attempting to create...`)
+    await track(Name.CREATE_USER_BANK_REQUEST, { userId })
     const { error, errorCode } = await createUserBank()
     if (error || errorCode) {
       console.error(
         `Failed to create userbank, with err: ${error}, ${errorCode}`
       )
+      await track(Name.CREATE_USER_BANK_FAILURE, {
+        userId,
+        errorCode,
+        error: (error as any).toString()
+      })
     } else {
       console.log(`Successfully created userbank!`)
     }
+    await track(Name.CREATE_USER_BANK_SUCCESS, { userId })
   } catch (err) {
+    await track(Name.CREATE_USER_BANK_FAILURE, {
+      userId,
+      errorMessage: (err as any).toString()
+    })
     console.error(`Failed to create userbank, with err: ${err}`)
   }
 }
