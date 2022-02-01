c65e9a8d075ac4124d92e802ea6c0635faa6f37b
Dylan Jeffers <dylan@audius.co>
2022-02-01 10:20:41 -0800

    Add makeStyles pattern (#254)

--- audius-client/packages/mobile/src/styles/typography.ts
+++ audius-client/packages/mobile/src/styles/typography.ts
@@ -1,6 +1,16 @@
 import { TextStyle } from 'react-native'
 
-export const fontByWeight = {
+type FontWeight =
+  | 'heavy'
+  | 'bold'
+  | 'demiBold'
+  | 'medium'
+  | 'regular'
+  | 'light'
+  | 'thin'
+  | 'ultraLight'
+
+export const fontByWeight: Record<FontWeight, string> = {
   heavy: 'AvenirNextLTPro-Heavy',
   bold: 'AvenirNextLTPro-Bold',
   demiBold: 'AvenirNextLTPro-DemiBold',
@@ -11,6 +21,25 @@ export const fontByWeight = {
   ultraLight: 'AvenirNextLTPro-UltLt'
 }
 
-export const font = (weight: keyof typeof fontByWeight): TextStyle => ({
+export const font = (weight: FontWeight): TextStyle => ({
   fontFamily: fontByWeight[weight]
 })
+
+export const typography = {
+  h1: {
+    fontSize: 18,
+    fontFamily: fontByWeight.bold,
+    marginBottom: 18 * 0.35
+  },
+  h2: {
+    fontSize: 14,
+    fontFamily: fontByWeight.medium,
+    marginBottom: 14 * 0.35
+  },
+  body: {
+    fontSize: 14,
+    fontFamily: fontByWeight.medium,
+    marginBottom: 14 * 0.35
+  },
+  fontByWeight
+}
