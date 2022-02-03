847393b2ef3cde142fcc79394e30962a5dedf3d1
Kyle Shanks <kj@audius.co>
2022-02-02 19:09:19 -0500

    Fix linting errors (#2425)

--- discovery-provider/src/queries/get_prev_track_entries.py
+++ discovery-provider/src/queries/get_prev_track_entries.py
@@ -1,5 +1,5 @@
-from src.models import Track
 from sqlalchemy import and_, func, or_
+from src.models import Track
 
 
 def get_prev_track_entries(session, entries):
@@ -14,40 +14,34 @@ def get_prev_track_entries(session, entries):
         prev_track_entries: (List<Track>) List of previous track entries corresponding to the passed track entries
     """
 
-    if (len(entries) == 0):
+    if len(entries) == 0:
         return []
 
     def get_prev_query_pairs(entry):
-        return [entry['track_id'], entry['blocknumber']]
+        return [entry["track_id"], entry["blocknumber"]]
 
     prev_query_pairs = map(get_prev_query_pairs, entries)
 
     prev_entries_subquery = (
         session.query(
-            Track.track_id,
-            func.max(Track.blocknumber).label('max_blocknumber')
+            Track.track_id, func.max(Track.blocknumber).label("max_blocknumber")
         )
         .filter(
             or_(
-                and_(
-                    Track.track_id == pair[0],
-                    Track.blocknumber < pair[1]
-                ) for pair in prev_query_pairs
+                and_(Track.track_id == pair[0], Track.blocknumber < pair[1])
+                for pair in prev_query_pairs
             )
         )
         .group_by(Track.track_id)
         .subquery()
     )
 
-    prev_entries_query = (
-        session.query(Track)
-        .join(
-            prev_entries_subquery,
-            and_(
-                prev_entries_subquery.c.track_id == Track.track_id,
-                prev_entries_subquery.c.max_blocknumber == Track.blocknumber
-            )
-        )
+    prev_entries_query = session.query(Track).join(
+        prev_entries_subquery,
+        and_(
+            prev_entries_subquery.c.track_id == Track.track_id,
+            prev_entries_subquery.c.max_blocknumber == Track.blocknumber,
+        ),
     )
 
     return prev_entries_query.all()
