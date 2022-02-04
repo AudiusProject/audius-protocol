6e140c8647595c163b4c46023cdba16035bfe386
Kyle Shanks <kyle.j.shanks@gmail.com>
2022-02-04 16:39:10 -0500

    Update image hooks to use dispatchweb (#266)

--- audius-client/packages/mobile/src/hooks/useCollectionCoverArt.ts
+++ audius-client/packages/mobile/src/hooks/useCollectionCoverArt.ts
@@ -7,6 +7,8 @@ import {
 import { fetchCoverArt } from 'audius-client/src/common/store/cache/collections/actions'
 import { useDispatch } from 'react-redux'
 
+import { useDispatchWeb } from './useDispatchWeb'
+
 export const useCollectionCoverArt = (
   collectionId: number,
   coverArtSizes: CoverArtSizes | null,
@@ -15,7 +17,8 @@ export const useCollectionCoverArt = (
   onDemand = false,
   load = true
 ) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatchWeb() as typeof useDispatch
+
   return useImageSize({
     dispatch,
     id: collectionId,
