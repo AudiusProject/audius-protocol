4668555683e0711791ec0e89bee1398b01561dca
Isaac <isaac@audius.co>
2022-02-07 22:57:21 +0000

    update alembic version

--- discovery-provider/alembic/versions/98e2a0a25ada_drop_aggregate_interval_plays_year_count.py
+++ discovery-provider/alembic/versions/98e2a0a25ada_drop_aggregate_interval_plays_year_count.py
@@ -1,7 +1,7 @@
 """drop aggregate_interval_plays year count
 
 Revision ID: 98e2a0a25ada
-Revises: 7104383ac0fe
+Revises: dc7f691adc79
 Create Date: 2022-01-20 20:32:50.898715
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "98e2a0a25ada"
-down_revision = "7104383ac0fe"
+down_revision = "dc7f691adc79"
 branch_labels = None
 depends_on = None
 
@@ -22,7 +22,7 @@ def upgrade():
         DROP MATERIALIZED VIEW IF EXISTS aggregate_interval_plays;
 
         -- same query as 92571f94989a but without year count
-        CREATE MATERIALIZED VIEW aggregate_interval_plays as
+        CREATE MATERIALIZED VIEW IF NOT EXISTS aggregate_interval_plays as
         SELECT
             tracks.track_id as track_id,
             tracks.genre as genre,
@@ -58,9 +58,9 @@ def upgrade():
             tracks.stem_of is Null;
 
         -- create primary key
-        CREATE INDEX interval_play_track_id_idx ON aggregate_interval_plays (track_id);
-        CREATE INDEX interval_play_week_count_idx ON aggregate_interval_plays (week_listen_counts);
-        CREATE INDEX interval_play_month_count_idx ON aggregate_interval_plays (month_listen_counts);
+        CREATE INDEX IF NOT EXISTS interval_play_track_id_idx ON aggregate_interval_plays (track_id);
+        CREATE INDEX IF NOT EXISTS interval_play_week_count_idx ON aggregate_interval_plays (week_listen_counts);
+        CREATE INDEX IF NOT EXISTS interval_play_month_count_idx ON aggregate_interval_plays (month_listen_counts);
     """
     )
 
@@ -71,7 +71,7 @@ def downgrade():
         """
         DROP MATERIALIZED VIEW IF EXISTS aggregate_interval_plays;
 
-        CREATE MATERIALIZED VIEW aggregate_interval_plays as
+        CREATE MATERIALIZED VIEW IF NOT EXISTS aggregate_interval_plays as
         SELECT
             tracks.track_id as track_id,
             tracks.genre as genre,
@@ -117,9 +117,9 @@ def downgrade():
             tracks.is_unlisted is False AND
             tracks.stem_of is Null;
 
-        CREATE INDEX interval_play_track_id_idx ON aggregate_interval_plays (track_id);
-        CREATE INDEX interval_play_week_count_idx ON aggregate_interval_plays (week_listen_counts);
-        CREATE INDEX interval_play_month_count_idx ON aggregate_interval_plays (month_listen_counts);
-        CREATE INDEX interval_play_year_count_idx ON aggregate_interval_plays (year_listen_counts);
+        CREATE INDEX IF NOT EXISTS interval_play_track_id_idx ON aggregate_interval_plays (track_id);
+        CREATE INDEX IF NOT EXISTS interval_play_week_count_idx ON aggregate_interval_plays (week_listen_counts);
+        CREATE INDEX IF NOT EXISTS interval_play_month_count_idx ON aggregate_interval_plays (month_listen_counts);
+        CREATE INDEX IF NOT EXISTS interval_play_year_count_idx ON aggregate_interval_plays (year_listen_counts);
     """
     )
