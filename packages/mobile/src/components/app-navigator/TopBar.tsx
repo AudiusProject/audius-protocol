9f4e0546c59b32daeb145163763282612312a5b3
Dylan Jeffers <dylan@audius.co>
2022-02-09 10:23:38 -0800

    [AUD-971] Add App TopBar (#278)

---
+++ packages/mobile/src/components/app-navigator/TopBar.tsx
@@ -0,0 +1,111 @@
+import { memo, useCallback } from 'react'
+
+import { StackHeaderProps } from '@react-navigation/stack'
+import { markAllAsViewed } from 'audius-client/src/components/notification/store/actions'
+import { Platform, View } from 'react-native'
+import { useDispatch } from 'react-redux'
+
+import AudiusLogo from 'app/assets/images/audiusLogoHorizontal.svg'
+import IconNotification from 'app/assets/images/iconNotification.svg'
+import IconSearch from 'app/assets/images/iconSearch.svg'
+import { useDispatchWeb } from 'app/hooks/useDispatchWeb'
+import { useNavigation } from 'app/hooks/useNavigation'
+import { open as openNotificationPanel } from 'app/store/notifications/actions'
+import { open as openSearch } from 'app/store/search/actions'
+import { makeStyles } from 'app/styles'
+import { useThemeColors } from 'app/utils/theme'
+
+import { IconButton } from '../core/IconButton'
+
+import { HeaderBackArrow } from './TopBarArrowBack'
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  root: {
+    height: Platform.OS === 'ios' ? 86 : 55,
+    borderBottomWidth: 1,
+    borderBottomColor: palette.neutralLight9,
+    backgroundColor: palette.white,
+    zIndex: 15
+  },
+  topBar: {
+    position: 'absolute',
+    bottom: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 20,
+    marginBottom: spacing(1)
+  },
+  iconNotification: {
+    height: 28,
+    width: 28
+  },
+  audiusLogo: {
+    height: 24,
+    width: 93,
+    marginRight: 10
+  },
+  iconSearch: {
+    height: 18,
+    width: 18
+  }
+}))
+
+type TopBarProps = StackHeaderProps
+
+export const TopBar = memo(
+  ({ navigation: headerNavigation, back }: TopBarProps) => {
+    const styles = useStyles()
+    const { neutralLight4 } = useThemeColors()
+    const navigation = useNavigation()
+    const dispatch = useDispatch()
+    const dispatchWeb = useDispatchWeb()
+
+    const handlePressNotification = useCallback(() => {
+      dispatch(openNotificationPanel())
+      dispatchWeb(markAllAsViewed())
+    }, [dispatch, dispatchWeb])
+
+    const handlePressHome = useCallback(() => {
+      navigation.navigate({
+        native: { screen: 'trending', params: undefined },
+        web: { route: 'trending' }
+      })
+    }, [navigation])
+
+    const handlePressSearch = useCallback(() => {
+      dispatch(openSearch())
+    }, [dispatch])
+
+    return (
+      <View style={styles.root}>
+        <View style={styles.topBar}>
+          {back ? (
+            <HeaderBackArrow onPress={headerNavigation.goBack} />
+          ) : (
+            <IconButton
+              icon={IconNotification}
+              styles={{ icon: styles.iconNotification }}
+              fill={neutralLight4}
+              onPress={handlePressNotification}
+            />
+          )}
+          <IconButton
+            icon={AudiusLogo}
+            fill={neutralLight4}
+            styles={{ icon: styles.audiusLogo }}
+            onPress={handlePressHome}
+          />
+          <IconButton
+            icon={IconSearch}
+            fill={neutralLight4}
+            styles={{ icon: styles.iconSearch }}
+            onPress={handlePressSearch}
+          />
+        </View>
+      </View>
+    )
+  }
+)
---
+++ packages/mobile/src/components/search/Fade.tsx
@@ -0,0 +1,23 @@
+import { ReactElement, useEffect, useRef } from 'react'
+
+import { Animated } from 'react-native'
+
+type FadeProps = {
+  in: boolean
+  children: ReactElement
+}
+export const Fade = ({ in: inProp, children }: FadeProps) => {
+  const fadeAnimation = useRef(new Animated.Value(inProp ? 1 : 0)).current
+
+  useEffect(() => {
+    Animated.timing(fadeAnimation, {
+      toValue: inProp ? 1 : 0,
+      duration: 10000,
+      useNativeDriver: true
+    }).start()
+  }, [fadeAnimation, inProp])
+
+  return (
+    <Animated.View style={{ opacity: fadeAnimation }}>{children}</Animated.View>
+  )
+}
