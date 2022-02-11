3326664196efe34e3a11c2a193147e4d14d224c7
Michael Piazza <michael.piazza.mp@gmail.com>
2022-02-11 15:23:45 -0800

    Identity changes
    More identity changes

--- identity-service/src/utils/rewardsReporter.js
+++ identity-service/src/utils/rewardsReporter.js
@@ -32,46 +32,54 @@ class RewardsReporter {
   constructor ({
     successSlackUrl,
     errorSlackUrl,
-    childLogger
+    childLogger = console,
+    source
   }) {
     this.successReporter = new SlackReporter({ slackUrl: successSlackUrl, childLogger })
     this.errorReporter = new SlackReporter({ slackUrl: errorSlackUrl, childLogger })
     this.childLogger = childLogger
+    this.source = source
   }
 
-  async reportSuccess ({ userId, challengeId, amount }) {
+  async reportSuccess ({ userId, challengeId, amount, specifier }) {
     const report = {
       status: 'success',
       userId,
       challengeId,
-      amount: amount.toString()
+      amount: amount.toString(),
+      source: this.source,
+      specifier
     }
     const slackMessage = this.successReporter.getJsonSlackMessage(report)
     await this.successReporter.postToSlack({ message: slackMessage })
     this.childLogger.info(report, `Rewards Reporter`)
   }
 
-  async reportFailure ({ userId, challengeId, amount, error, phase }) {
+  async reportFailure ({ userId, challengeId, amount, error, phase, specifier }) {
     const report = {
       status: 'failure',
       userId,
       challengeId,
       amount: amount.toString(),
       error: error.toString(),
-      phase
+      phase,
+      source: this.source,
+      specifier
     }
     const slackMessage = this.errorReporter.getJsonSlackMessage(report)
     await this.errorReporter.postToSlack({ message: slackMessage })
     this.childLogger.info(report, `Rewards Reporter`)
   }
 
-  async reportAAORejection ({ userId, challengeId, amount, error }) {
+  async reportAAORejection ({ userId, challengeId, amount, error, specifier }) {
     const report = {
       status: 'rejection',
       userId,
       challengeId,
       amount: amount.toString(),
-      error: error.toString()
+      error: error.toString(),
+      source: this.source,
+      specifier
     }
     const slackMessage = this.errorReporter.getJsonSlackMessage(report)
     await this.errorReporter.postToSlack({ message: slackMessage })
