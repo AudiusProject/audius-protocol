977bf94f935e149e8e187d0bbf5438e880c39e44
Saliou Diallo <saliou@audius.co>
2022-02-14 13:56:25 -0500

    WIP: bump libs and identity libs

--- libs/src/services/solanaWeb3Manager/index.js
+++ libs/src/services/solanaWeb3Manager/index.js
@@ -343,7 +343,7 @@ class SolanaWeb3Manager {
     logger = console,
     feePayerOverride = null
   }) {
-    console.log(`feePayerOverride: ${feePayerOverride} | pubKey ${SolanaUtils.newPublicKeyNullable(feePayerOverride)} | localFeePayerKey: ${this.feePayerKey}`)
+    logger.info(`feePayerOverride: ${feePayerOverride} | pubKey ${SolanaUtils.newPublicKeyNullable(feePayerOverride)} | localFeePayerKey: ${this.feePayerKey}`)
     return submitAttestations({
       rewardManagerProgramId: this.rewardManagerProgramId,
       rewardManagerAccount: this.rewardManagerProgramPDA,
