7bdf722e853a8d6a03e2d3492cc9a0c174714dbe
Michael Piazza <michael.piazza.mp@gmail.com>
2022-02-14 14:20:09 -0800

    Use correct amplitude package

--- identity-service/src/analytics.js
+++ identity-service/src/analytics.js
@@ -1,20 +1,19 @@
-const amplitude = require('amplitude-js')
+const amplitude = require('@amplitude/node')
 const config = require('./config')
 
 class AnalyticsProvider {
   constructor () {
     const AMPLITUDE_API_KEY = config.get('amplitudeAPIKey')
-    this.amplitudeInstance = amplitude.getInstance()
     try {
-      this.amplitudeInstance.init(AMPLITUDE_API_KEY)
+      this.amplitudeInstance = amplitude.init(AMPLITUDE_API_KEY)
     } catch (e) {
       console.log(`Failed to init amplitude with error: ${JSON.stringify(e)}`)
     }
   }
 
-  track (eventName, properties) {
+  async track (eventName, properties) {
     try {
-      this.amplitudeInstance.logEvent(eventName, properties)
+      await this.amplitudeInstance.logEvent(eventName, properties)
     } catch (e) {
       console.log(`Failed to log amplitude event with error: ${e}`)
     }
