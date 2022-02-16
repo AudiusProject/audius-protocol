0e1beb377aebe9904833cfc08f4e4023e6ea8ced
Marcus Pasell <rickyrombo@gmail.com>
2022-02-16 22:10:19 +0000

    Fix referrals

--- discovery-provider/src/tasks/users.py
+++ discovery-provider/src/tasks/users.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from typing import Any, Dict, Optional, Set, Tuple, TypedDict
+from typing import Any, Dict, Set, Tuple, TypedDict
 
 import base58
 from eth_account.messages import defunct_hash_message
@@ -529,7 +529,7 @@ def validate_signature(
 
 class UserEventsMetadata(TypedDict, total=False):
     referrer: int
-    is_mobile_user: Optional[bool]
+    is_mobile_user: bool
 
 
 def update_user_events(
