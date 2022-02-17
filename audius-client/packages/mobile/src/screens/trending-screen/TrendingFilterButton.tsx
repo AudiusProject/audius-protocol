7d694cedae30b4dfffec924a98f5e7e22c236602
Dylan Jeffers <dylan@audius.co>
2022-02-16 17:26:13 -0800

    [AUD-1473] Add TrendingFilterDrawer (#922)

--- audius-client/packages/mobile/src/screens/trending-screen/TrendingFilterButton.tsx
+++ audius-client/packages/mobile/src/screens/trending-screen/TrendingFilterButton.tsx
@@ -1,5 +1,15 @@
+import { useCallback } from 'react'
+
+import { getTrendingGenre } from 'audius-client/src/common/store/pages/trending/selectors'
+import { setVisibility } from 'audius-client/src/common/store/ui/modals/slice'
+import { Genre } from 'audius-client/src/common/utils/genres'
+
 import { Button } from 'app/components/core'
+import { useDispatchWeb } from 'app/hooks/useDispatchWeb'
+import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
 import { makeStyles } from 'app/styles'
+
+import { MODAL_NAME } from './TrendingFilterDrawer'
 
 const useStyles = makeStyles(({ spacing, palette }) => ({
   headerButton: {
@@ -14,14 +24,22 @@ const useStyles = makeStyles(({ spacing, palette }) => ({
     textTransform: 'none'
   }
 }))
+
 export const TrendingFilterButton = () => {
   const styles = useStyles()
+  const dispatchWeb = useDispatchWeb()
+  const trendingGenre = useSelectorWeb(getTrendingGenre) ?? Genre.ALL
+
+  const handlePress = useCallback(() => {
+    dispatchWeb(setVisibility({ modal: MODAL_NAME, visible: true }))
+  }, [dispatchWeb])
+
   return (
     <Button
       variant='primary'
-      title='All Genres'
+      title={trendingGenre}
       size='small'
-      onPress={() => {}}
+      onPress={handlePress}
       styles={{ root: styles.headerButton, text: styles.headerButtonText }}
     />
   )
--- audius-client/packages/web/src/common/store/pages/trending/actions.ts
+++ audius-client/packages/web/src/common/store/pages/trending/actions.ts
@@ -1,19 +1,22 @@
+import TimeRange from 'common/models/TimeRange'
+import { Genre } from 'common/utils/genres'
+
 export const SET_TRENDING_GENRE = 'TRENDING/SET_TRENDING_GENRE'
 export const SET_TRENDING_TIME_RANGE = 'TRENDING/SET_TRENDING_TIME_RANGE'
 export const SET_LAST_FETCHED_TRENDING_GENRE =
   'TRENDING/SET_LAST_FETCHED_TRENDING_GENRE'
 
-export const setTrendingGenre = genre => ({
+export const setTrendingGenre = (genre: Genre | null) => ({
   type: SET_TRENDING_GENRE,
   genre
 })
 
-export const setTrendingTimeRange = timeRange => ({
+export const setTrendingTimeRange = (timeRange: TimeRange) => ({
   type: SET_TRENDING_TIME_RANGE,
   timeRange
 })
 
-export const setLastFetchedTrendingGenre = genre => ({
+export const setLastFetchedTrendingGenre = (genre: Genre | null) => ({
   type: SET_LAST_FETCHED_TRENDING_GENRE,
   genre
 })
