18e1d86985b77f3453a711f2f6f4d62f5a13f5a8
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-02-20 10:51:18 -0500

    Revert "Set a timeout for this new threshold"
    This reverts commit a7817c08e8e2ec5412141af0c84217c70738e4ce.

--- discovery-provider/src/utils/ipfs_lib.py
+++ discovery-provider/src/utils/ipfs_lib.py
@@ -128,9 +128,7 @@ class IPFSClient:
                 ): url
                 for url in gateway_ipfs_urls
             }
-            for future in concurrent.futures.as_completed(
-                future_to_url, timeout=NEW_BLOCK_TIMEOUT_SECONDS * 1.2
-            ):
+            for future in concurrent.futures.as_completed(future_to_url):
                 url = future_to_url[future]
                 try:
                     r = future.result()
