796f28a32a4d21c1749ecc7b2f3bab2e83f8af99
Saliou Diallo <sddioulde@hotmail.com>
2022-02-22 15:33:02 -0500

    Make unique ids across accounts (#2583)
    * Make unique ids across accounts
    
    * Make masked id the primary key
    
    * Add salt
    
    * Update request headers
    
    * Make vars more descriptive
    
    * Add transaction to cognito record creation so that they're atomic
    
    * Add logs
    
    * Fix cognito flow identity migration and model and add logs for testing
    
    * Undo comments and remove test logs
    
    Co-authored-by: Saliou Diallo <saliou@audius.co>

---
+++ identity-service/src/models/cognitoFlowIdentities.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+  const CognitoFlowIdentities = sequelize.define('CognitoFlowIdentities', {
+    maskedIdentity: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {})
+  return CognitoFlowIdentities
+}
