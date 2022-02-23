be745a8efbba69b865d7cdb93de298df02d0cdd1
Dylan Jeffers <dylan@audius.co>
2022-02-23 14:38:23 -0800

    [AUD-1596] Add AccountSettingsScreen (#974)

---
+++ audius-client/packages/mobile/src/screens/settings-screen/AccountSettingsItem.tsx
@@ -0,0 +1,51 @@
+import { ComponentType } from 'react'
+
+import { ImageSourcePropType } from 'react-native'
+import { SvgProps } from 'react-native-svg'
+
+import { Button } from 'app/components/core'
+import { makeStyles } from 'app/styles'
+
+import { SettingsRowLabel } from './SettingRowLabel'
+import { SettingsRow } from './SettingsRow'
+import { SettingsRowDescription } from './SettingsRowDescription'
+
+type AccountSettingsItemProps = {
+  title: string
+  titleIconSource: ImageSourcePropType
+  description: string
+  buttonTitle: string
+  buttonIcon: ComponentType<SvgProps>
+}
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  root: {
+    marginBottom: spacing(6)
+  },
+  button: {
+    marginVertical: spacing(2)
+  },
+  buttonIcon: {
+    marginRight: spacing(3)
+  }
+}))
+
+export const AccountSettingsItem = (props: AccountSettingsItemProps) => {
+  const { title, titleIconSource, description, buttonTitle, buttonIcon } = props
+  const styles = useStyles()
+
+  return (
+    <SettingsRow style={styles.root}>
+      <SettingsRowLabel label={title} iconSource={titleIconSource} />
+      <SettingsRowDescription>{description}</SettingsRowDescription>
+      <Button
+        styles={{ root: styles.button, icon: styles.buttonIcon }}
+        title={buttonTitle}
+        icon={buttonIcon}
+        iconPosition='left'
+        variant='common'
+        fullWidth
+      />
+    </SettingsRow>
+  )
+}
