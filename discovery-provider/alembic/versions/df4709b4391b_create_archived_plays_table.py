799d6d88cd92a21011bdd82fc4ca15eb9124d5a1
Isaac <isaac@audius.co>
2022-02-23 04:57:23 +0000

    update alembic

--- discovery-provider/alembic/versions/df4709b4391b_create_archived_plays_table.py
+++ discovery-provider/alembic/versions/df4709b4391b_create_archived_plays_table.py
@@ -1,7 +1,7 @@
 """create archived plays table
 
 Revision ID: df4709b4391b
-Revises: b734b7b47fca
+Revises: d180b0c624de
 Create Date: 2022-01-04 23:23:40.814114
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "df4709b4391b"
-down_revision = "b734b7b47fca"
+down_revision = "d180b0c624de"
 branch_labels = None
 depends_on = None
 
