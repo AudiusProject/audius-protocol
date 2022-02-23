c1b03ab2c12282fbc7d8553654f23ce46fffcdc4
Saliou Diallo <saliou@audius.co>
2022-02-22 23:11:27 -0500

    Filter new user events on wallet existence

--- identity-service/src/notifications/processNotifications/playlistUpdateNotification.js
+++ identity-service/src/notifications/processNotifications/playlistUpdateNotification.js
@@ -76,28 +76,28 @@ async function processPlaylistUpdateNotifications (notifications, tx) {
   }, {})
   logger.info(`${logPrefix} calculated updates, num updates: ${userWalletsAndPlaylistUpdates.length}, time: ${Date.now() - startTime}ms`)
 
-  const newPlaylistUpdatePromises = userIds.map(userId => {
-    const walletAddress = userIdToWalletsMap[userId]
-    if (!walletAddress) return Promise.resolve()
+  const newUserEvents = userIds
+    .map(userId => {
+      const walletAddress = userIdToWalletsMap[userId]
+      if (!walletAddress) return null
 
-    const dbPlaylistUpdates = userWalletToPlaylistUpdatesMap[walletAddress] || {}
-    const fetchedPlaylistUpdates = userPlaylistUpdatesMap[userId]
-    Object.keys(fetchedPlaylistUpdates).forEach(playlistId => {
-      const fetchedLastUpdated = moment(fetchedPlaylistUpdates[playlistId]).utc()
-      dbPlaylistUpdates[playlistId] = {
-        // in case user favorited this track before and has no UserEvent record of it
-        userLastViewed: fetchedLastUpdated.subtract(1, 'seconds').valueOf(),
-        ...dbPlaylistUpdates[playlistId],
-        lastUpdated: fetchedLastUpdated.valueOf()
-      }
+      const dbPlaylistUpdates = userWalletToPlaylistUpdatesMap[walletAddress] || {}
+      const fetchedPlaylistUpdates = userPlaylistUpdatesMap[userId]
+      Object.keys(fetchedPlaylistUpdates).forEach(playlistId => {
+        const fetchedLastUpdated = moment(fetchedPlaylistUpdates[playlistId]).utc()
+        dbPlaylistUpdates[playlistId] = {
+          // in case user favorited this track before and has no UserEvent record of it
+          userLastViewed: fetchedLastUpdated.subtract(1, 'seconds').valueOf(),
+          ...dbPlaylistUpdates[playlistId],
+          lastUpdated: fetchedLastUpdated.valueOf()
+        }
+      })
+
+      return [walletAddress, dbPlaylistUpdates]
     })
-
-    return [walletAddress, dbPlaylistUpdates]
-  })
+    .filter(Boolean)
 
   logger.info(`${logPrefix} mapped updates, time: ${Date.now() - startTime}ms`)
-
-  const newUserEvents = await Promise.all(newPlaylistUpdatePromises)
 
   const results = await sequelize.query(`
     INSERT INTO "UserEvents" ("walletAddress", "playlistUpdates")
@@ -105,7 +105,7 @@ async function processPlaylistUpdateNotifications (notifications, tx) {
     ON CONFLICT ("walletAddress") DO UPDATE
       SET "playlistUpdates" = "excluded"."playlistUpdates"
   `, {
-    replacements: [newUserEvents],
+    replacements: newUserEvents,
     type: Sequelize.QueryTypes.INSERT
   })
 
