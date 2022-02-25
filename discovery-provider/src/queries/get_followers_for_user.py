7c6cf9f65cfc79ae350211442b08843347aec324
Steve Perkins <stereosteve@gmail.com>
2022-02-25 17:54:05 +0000

    fix slow query for get followers followees

--- discovery-provider/src/queries/get_followers_for_user.py
+++ discovery-provider/src/queries/get_followers_for_user.py
@@ -1,10 +1,27 @@
-from sqlalchemy import asc, desc, func
-from sqlalchemy.orm import aliased
-from src.models import Follow
-from src.queries import response_name_constants
+from sqlalchemy.sql import text
 from src.queries.get_unpopulated_users import get_unpopulated_users
-from src.queries.query_helpers import add_query_pagination, populate_user_metadata
+from src.queries.query_helpers import populate_user_metadata
 from src.utils.db_session import get_db_read_replica
+
+sql = text(
+    """
+SELECT
+    follower_user_id,
+    follower_count
+from
+    follows
+    left outer join aggregate_user on follower_user_id = user_id
+where
+    is_current = true
+    and is_delete = false
+    and followee_user_id = :followee_user_id
+order by
+    follower_count desc,
+    user_id asc
+offset :offset
+limit :limit;
+"""
+)
 
 
 def get_followers_for_user(args):
@@ -16,48 +33,12 @@ def get_followers_for_user(args):
 
     db = get_db_read_replica()
     with db.scoped_session() as session:
-        # correlated subquery sqlalchemy code:
-        # https://groups.google.com/forum/#!topic/sqlalchemy/WLIy8jxD7qg
-        inner_follow = aliased(Follow)
-        outer_follow = aliased(Follow)
 
-        # subquery to get a user's follower count
-        inner_select = (
-            session.query(func.count(inner_follow.followee_user_id))
-            .filter(
-                inner_follow.is_current == True,
-                inner_follow.is_delete == False,
-                inner_follow.followee_user_id == outer_follow.follower_user_id,
-            )
-            .correlate(outer_follow)
+        rows = session.execute(
+            sql,
+            {"followee_user_id": followee_user_id, "limit": limit, "offset": offset},
         )
-
-        # get all users that follow input user, sorted by their follower count desc
-        outer_select = (
-            session.query(
-                outer_follow.follower_user_id,
-                inner_select.as_scalar().label(response_name_constants.follower_count),
-            )
-            .filter(
-                outer_follow.followee_user_id == followee_user_id,
-                outer_follow.is_current == True,
-                outer_follow.is_delete == False,
-            )
-            .group_by(outer_follow.follower_user_id)
-            .order_by(
-                desc(response_name_constants.follower_count),
-                # secondary sort to guarantee determinism as explained here:
-                # https://stackoverflow.com/questions/13580826/postgresql-repeating-rows-from-limit-offset
-                asc(outer_follow.follower_user_id),
-            )
-        )
-        follower_user_ids_by_follower_count = add_query_pagination(
-            outer_select, limit, offset
-        ).all()
-
-        user_ids = [
-            user_id for (user_id, follower_count) in follower_user_ids_by_follower_count
-        ]
+        user_ids = [r[0] for r in rows]
 
         # get all users for above user_ids
         users = get_unpopulated_users(session, user_ids)
@@ -65,13 +46,4 @@ def get_followers_for_user(args):
         # bundle peripheral info into user results
         users = populate_user_metadata(session, user_ids, users, current_user_id)
 
-        # order by (follower_count desc, user_id asc) to match query sorting
-        # tuple key syntax from: https://stackoverflow.com/a/4233482/8414360
-        users.sort(
-            key=lambda user: (
-                user[response_name_constants.follower_count],
-                (user["user_id"]) * (-1),
-            ),
-            reverse=True,
-        )
     return users
