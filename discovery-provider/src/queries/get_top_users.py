81b8e255c437b037187e3845538533b213639733
Steve Perkins <stereosteve@gmail.com>
2022-02-28 17:45:54 +0000

    faster top_users query

--- discovery-provider/src/queries/get_top_users.py
+++ discovery-provider/src/queries/get_top_users.py
@@ -1,23 +1,32 @@
-from sqlalchemy.sql.expression import desc
-from src.models.models import AggregateUser, User
-from src.queries.query_helpers import helpers, paginate_query, populate_user_metadata
+from src.queries.query_helpers import get_pagination_vars, populate_user_metadata
 from src.utils.db_session import get_db_read_replica
+
+sql = """
+select users.*
+from users
+join aggregate_user using (user_id)
+where
+    is_current
+    and user_id in (
+        select user_id from aggregate_user
+        where track_count > 0
+        order by follower_count desc
+        limit :limit
+        offset :offset
+    )
+order by follower_count desc, user_id asc;
+"""
 
 
 def get_top_users(current_user_id):
     """Gets the top users by follows of all of Audius"""
+
     top_users = []
     db = get_db_read_replica()
     with db.scoped_session() as session:
-        top_users = (
-            session.query(User)
-            .select_from(AggregateUser)
-            .join(User, User.user_id == AggregateUser.user_id)
-            .filter(AggregateUser.track_count > 0, User.is_current)
-            .order_by(desc(AggregateUser.follower_count), User.user_id)
-        )
-        top_users = paginate_query(top_users).all()
-        top_users = helpers.query_result_to_list(top_users)
+        (limit, offset) = get_pagination_vars()
+        top_users = session.execute(sql, {"limit": limit, "offset": offset})
+        top_users = [dict(row) for row in top_users]
         user_ids = list(map(lambda user: user["user_id"], top_users))
         top_users = populate_user_metadata(
             session, user_ids, top_users, current_user_id
