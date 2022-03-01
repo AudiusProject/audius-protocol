32ae66ccf4e6b8b6604d1cdb7cd8576867e586c3
Dylan Jeffers <dylan@audius.co>
2022-03-01 13:05:30 -0800

    Fix profile track-tab loadMore (#1002)

--- audius-client/packages/mobile/src/screens/profile-screen/TracksTab.tsx
+++ audius-client/packages/mobile/src/screens/profile-screen/TracksTab.tsx
@@ -1,7 +1,10 @@
+import { useCallback } from 'react'
+
 import { tracksActions } from 'audius-client/src/common/store/pages/profile/lineups/tracks/actions'
 import { getProfileTracksLineup } from 'audius-client/src/common/store/pages/profile/selectors'
 
 import { Lineup } from 'app/components/lineup'
+import { useDispatchWeb } from 'app/hooks/useDispatchWeb'
 import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
 
 import { EmptyProfileTile } from './EmptyProfileTile'
@@ -10,6 +13,19 @@ import { getProfile } from './selectors'
 export const TracksTab = () => {
   const { profile } = useSelectorWeb(getProfile)
   const lineup = useSelectorWeb(getProfileTracksLineup)
+  const dispatchWeb = useDispatchWeb()
+
+  const loadMore = useCallback(
+    (offset: number, limit: number) => {
+      if (!profile) return
+      dispatchWeb(
+        tracksActions.fetchLineupMetadatas(offset, limit, false, {
+          userId: profile.user_id
+        })
+      )
+    },
+    [dispatchWeb, profile]
+  )
 
   if (!profile) return null
 
@@ -23,6 +39,8 @@ export const TracksTab = () => {
       listKey='profile-tracks'
       actions={tracksActions}
       lineup={lineup}
+      limit={profile.track_count}
+      loadMore={loadMore}
     />
   )
 }
