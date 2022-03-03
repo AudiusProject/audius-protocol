447389eed01faa7b2370a76537d9a2630eab5d19
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-03-02 23:09:37 -0500

    TN Final integration (#2652)

--- creator-node/test/snapbackSM.test.js
+++ creator-node/test/snapbackSM.test.js
@@ -7,6 +7,7 @@ const { getLibsMock } = require('./lib/libsMock')
 const utils = require('../src/utils')
 const { getApp } = require('./lib/app')
 const nodeConfig = require('../src/config')
+const { getIPFSMock } = require('./lib/ipfsMock')
 
 const constants = {
   primaryEndpoint: 'http://test_cn_primary.co',
@@ -69,7 +70,7 @@ describe('test SnapbackSM', function () {
 
   beforeEach(async function () {
     // init app to run migrations
-    const appInfo = await getApp()
+    const appInfo = await getApp(getIPFSMock(), getLibsMock())
     server = appInfo.server
 
     nodeConfig.set('spID', 1)
