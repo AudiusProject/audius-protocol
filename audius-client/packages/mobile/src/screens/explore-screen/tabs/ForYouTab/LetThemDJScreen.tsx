e053a3116597ea7e24e15bca160e53fbe62ed880
Kyle Shanks <kyle.j.shanks@gmail.com>
2022-03-10 17:56:44 -0500

    Add LetThemDJ and TopAlbums screens for explore screen (#1051)

---
+++ audius-client/packages/mobile/src/screens/explore-screen/tabs/ForYouTab/LetThemDJScreen.tsx
@@ -0,0 +1,35 @@
+import Status from 'audius-client/src/common/models/Status'
+import {
+  getCollections,
+  getStatus
+} from 'audius-client/src/common/store/pages/explore/exploreCollections/selectors'
+import { ExploreCollectionsVariant } from 'audius-client/src/common/store/pages/explore/types'
+import { isEqual } from 'lodash'
+
+import { CollectionList } from 'app/components/collection-list'
+import { Screen } from 'app/components/core'
+import { Header } from 'app/components/header'
+import { WithLoader } from 'app/components/with-loader/WithLoader'
+import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
+
+import { LET_THEM_DJ } from '../../collections'
+
+export const LetThemDJScreen = () => {
+  const status = useSelectorWeb(
+    state =>
+      getStatus(state, { variant: ExploreCollectionsVariant.LET_THEM_DJ }),
+    isEqual
+  )
+  const exploreData = useSelectorWeb(state =>
+    getCollections(state, { variant: ExploreCollectionsVariant.LET_THEM_DJ })
+  )
+
+  return (
+    <Screen>
+      <Header text={LET_THEM_DJ.title} />
+      <WithLoader loading={status === Status.LOADING}>
+        <CollectionList collection={exploreData} />
+      </WithLoader>
+    </Screen>
+  )
+}
