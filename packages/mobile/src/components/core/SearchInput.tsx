5646906eccb3001aaa52df1edf5827a3d788c160
Raymond Jacobson <ray@audius.co>
2022-03-11 12:35:49 -0800

    [AUD-1694] Add clear search icon (#1058)
    * [AUD-1694] Add clear search icon
    
    * Refactor

--- packages/mobile/src/components/core/SearchInput.tsx
+++ packages/mobile/src/components/core/SearchInput.tsx
@@ -1,10 +1,13 @@
-import { ComponentType, forwardRef } from 'react'
+import { ComponentType, forwardRef, useCallback } from 'react'
 
-import { TextInput, TextInputProps, View } from 'react-native'
+import { Animated, TextInput, TextInputProps, View } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 import { SvgProps } from 'react-native-svg'
 
-import IconSearch from 'app/assets/images/iconSearch.svg'
+import IconClose from 'app/assets/images/iconRemove.svg'
+import { usePressScaleAnimation } from 'app/hooks/usePressScaleAnimation'
 import { makeStyles } from 'app/styles'
+import { spacing } from 'app/styles/spacing'
 
 const useStyles = makeStyles(({ typography, palette, spacing }) => ({
   root: {
@@ -14,7 +17,7 @@ const useStyles = makeStyles(({ typography, palette, spacing }) => ({
     borderWidth: 1,
     paddingVertical: spacing(2),
     paddingLeft: spacing(3),
-    paddingRight: spacing(1),
+    paddingRight: spacing(2),
     borderColor: palette.neutralLight7,
     backgroundColor: palette.neutralLight10
   },
@@ -31,13 +34,30 @@ const useStyles = makeStyles(({ typography, palette, spacing }) => ({
 }))
 
 type SearchInputProps = TextInputProps & {
+  /**
+   * Default icon to show at the right side of the input
+   */
   Icon?: ComponentType<SvgProps>
+  /**
+   * Whether or not the search input should show a clear icon
+   */
+  clearable?: boolean
+  /**
+   * What happens when the user clicks the clear icon
+   */
+  onClear?: () => void
 }
 
 export const SearchInput = forwardRef<TextInput, SearchInputProps>(
   (props, ref) => {
-    const { style, Icon = IconSearch, ...other } = props
+    const { scale, handlePressIn, handlePressOut } = usePressScaleAnimation(0.8)
+
+    const { style, Icon, clearable, onClear, ...other } = props
     const styles = useStyles()
+
+    const handlePressIcon = useCallback(() => {
+      onClear?.()
+    }, [onClear])
 
     return (
       <View style={[styles.root, style]}>
@@ -50,12 +70,35 @@ export const SearchInput = forwardRef<TextInput, SearchInputProps>(
           returnKeyType='search'
           {...other}
         />
-        <Icon
-          style={{ height: styles.icon.height, width: styles.icon.width }}
-          fill={styles.icon.fill}
-          height={styles.icon.height}
-          width={styles.icon.width}
-        />
+        {clearable ? (
+          <Animated.View style={[{ transform: [{ scale }] }]}>
+            <TouchableWithoutFeedback
+              onPress={handlePressIcon}
+              onPressIn={handlePressIn}
+              onPressOut={handlePressOut}
+              hitSlop={{
+                top: spacing(2),
+                bottom: spacing(2),
+                left: spacing(2),
+                right: spacing(2)
+              }}
+            >
+              <IconClose
+                style={{ height: styles.icon.height, width: styles.icon.width }}
+                fill={styles.icon.fill}
+                height={styles.icon.height}
+                width={styles.icon.width}
+              />
+            </TouchableWithoutFeedback>
+          </Animated.View>
+        ) : Icon ? (
+          <Icon
+            style={{ height: styles.icon.height, width: styles.icon.width }}
+            fill={styles.icon.fill}
+            height={styles.icon.height}
+            width={styles.icon.width}
+          />
+        ) : null}
       </View>
     )
   }
