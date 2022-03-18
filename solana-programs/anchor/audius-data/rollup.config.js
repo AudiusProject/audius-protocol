d1b14e7f26306adee9e954fedd2b2596eb7a2b09
Dylan Jeffers <dylan@audius.co>
2022-03-17 17:30:41 -0700

    [AUD-1752] Consume @audius/solana in @audius/libs

---
+++ solana-programs/anchor/audius-data/rollup.config.js
@@ -0,0 +1,20 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+import pkg from './package.json'
+
+const extensions = ['.js', '.ts']
+
+export default {
+  input: 'lib/lib.ts',
+  output: [{ file: pkg.main, format: 'cjs', exports: 'named' }],
+  plugins: [
+    commonjs({ extensions }),
+    json(),
+    resolve({ extensions, preferBuiltins: true }),
+    typescript({include: ["./lib/lib.ts", "./lib/utils.ts"]})
+  ],
+  external: ["@project-serum/anchor", "@solana/web3.js", "web3-core", "secp256k1", "bn.js", "crypto", "keccak256", "readable-stream"] 
+}
