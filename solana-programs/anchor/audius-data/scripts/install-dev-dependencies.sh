d8929cdfe2246f8132a1d2c0407f4a4a5fe45523
CS Jiang <christine@audius.co>
2022-03-24 18:33:45 +0000

    pin dev toolchains versions

--- solana-programs/anchor/audius-data/scripts/install-dev-dependencies.sh
+++ solana-programs/anchor/audius-data/scripts/install-dev-dependencies.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+RUST_VERSION=1.59.0 # rustc version 1.59.0 (9d1b2106e 2022-02-23)
+SOLANA_CLI_VERSION=v1.9.1
+ANCHOR_CLI_VERSION=v0.22.1
+
 cd $PROTOCOL_DIR/solana-programs/anchor/audius-data
 echo "Installing dev deps for anchor audius-data development..."
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# pin rustc version
+rustup default "$RUST_VERSION"
 source $HOME/.cargo/env
 rustup component add rustfmt
+
 # install solana
-sh -c "$(curl -sSfL https://release.solana.com/v1.9.1/install)"
+sh -c "$(curl -sSfL https://release.solana.com/$SOLANA_CLI_VERSION/install)"
 # install yarn
 npm install -g yarn
 # install anchor
-npm i -g @project-serum/anchor-cli
+npm i -g "@project-serum/anchor-cli@$ANCHOR_CLI_VERSION"
 # verify anchor version
 anchor --version
 # install dependencies
