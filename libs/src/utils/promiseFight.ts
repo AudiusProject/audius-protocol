a3952c8bd988142520cb52bdeec4f116caab31a1
Dylan Jeffers <dylan@audius.co>
2022-03-25 16:31:50 -0700

    Rename T to T1

--- libs/src/utils/promiseFight.ts
+++ libs/src/utils/promiseFight.ts
@@ -4,12 +4,12 @@
  * @param captureErrored optional capture errored promises
  * @return A promise that resolves with the first promise that resolves
  */
-export async function promiseFight<T, T2> (promises: Array<Promise<T>>): Promise<T | T2>
-export async function promiseFight<T, T2> (promises: Array<Promise<T>>, captureErrored: boolean): Promise<{val: T, errored: T2[]}>
-export async function promiseFight<T, T2> (promises: Array<Promise<T>>, captureErrored?: boolean) {
+export async function promiseFight<T1, T2> (promises: Array<Promise<T1>>): Promise<T1 | T2>
+export async function promiseFight<T1, T2> (promises: Array<Promise<T1>>, captureErrored: boolean): Promise<{val: T1, errored: T2[]}>
+export async function promiseFight<T1, T2> (promises: Array<Promise<T1>>, captureErrored?: boolean) {
   const errored: T2[] = []
-  return await Promise.all<Array<Promise<T | T2>>>(promises.map(async p => {
-    return await p.then<T, T2>(
+  return await Promise.all<Array<Promise<T1 | T2>>>(promises.map(async p => {
+    return await p.then<T1, T2>(
       async val => await Promise.reject(val),
       async err => {
         if (captureErrored) errored.push(err)
