11341111854d629217f6595a8aef6d9ae71a972a
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2022-03-30 15:12:49 +0000

    Fixing build step

--- solana-programs/start.sh
+++ solana-programs/start.sh
@@ -113,17 +113,28 @@
     cargo run create-sender --eth-operator-address 0xF24936714293a0FaF39A022138aF58D874289132  --eth-sender-address 0xF24936714293a0FaF39A022138aF58D874289133 --reward-manager $reward_manager_account_key
 
     # Build anchor program
-    echo "Anchor build"
+    echo "--- Anchor build ---- "
     cd ../../anchor/audius-data
-    anchor build
+    set +x
 
     # Replace program ID with solana pubkey generated from anchor build
-    sed -i "s/Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS/$(solana-keygen pubkey target/deploy/audius_data-keypair.json)/g" Anchor.toml
-    sed -i "s/Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS/$(solana-keygen pubkey target/deploy/audius_data-keypair.json)/g" programs/audius-data/src/lib.rs
+    cur_address=$(grep -Po '(?<=declare_id!\(").*(?=")' programs/audius-data/src/lib.rs)
+
+    solana-keygen new -s --no-bip39-passphrase -o target/deploy/audius_data-keypair.json --force
+    anchor_program_id=$(solana-keygen pubkey target/deploy/audius_data-keypair.json)
+    echo "New address for audius-data: $anchor_program_id"
+
+    sed -i "s/$cur_address/$(solana-keygen pubkey target/deploy/audius_data-keypair.json)/g" Anchor.toml
+    sed -i "s/$cur_address/$(solana-keygen pubkey target/deploy/audius_data-keypair.json)/g" programs/audius-data/src/lib.rs
+
+    # Build AFTER updated program ID
+    anchor build
 
     anchor_program_id=$(solana-keygen pubkey target/deploy/audius_data-keypair.json)
     admin_keypair_publickey=$(solana-keygen pubkey adminKeypair.json)
+    admin_keypair_privatekey=$(cat adminKeypair.json)
     admin_storage_keypair_publickey=$(solana-keygen pubkey adminStorageKeypair.json)
+    admin_storage_keypair_privatekey=$(cat adminStorageKeypair.json)
 
     echo "--- Anchor deploy ---- "
     # Deploy anchor program
@@ -141,7 +152,9 @@ cat <<EOF
 {
     "anchorProgramId": "$anchor_program_id",
     "anchorAdminKeypairPublicKey": "$admin_keypair_publickey",
+    "anchorAdminKeypairPrivateKey": "$admin_keypair_privatekey",
     "anchorAdminStorageKeypairPublicKey": "$admin_storage_keypair_publickey",
+    "anchorAdminStorageKeypairPrivateKey": "$admin_storage_keypair_privatekey",
     "trackListenCountAddress": "$track_listen_count_address",
     "audiusEthRegistryAddress": "$audius_eth_registry_address",
     "validSigner": "$valid_signer",
