90a8e11980bce204c5aefc95c05eedd1339f4491
Marcus Pasell <rickyrombo@gmail.com>
2022-03-30 14:15:52 +0000

    Add linting flake8 plugin to ensure decorator order and route params in API declarations

---
+++ discovery-provider/flask_decorator_plugin/flask_decorator_plugin.py
@@ -0,0 +1,158 @@
+import ast
+import re
+import sys
+from typing import Any, Generator, List, Tuple, Type
+
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
+FDP001 = "FDP001: Non-route parameter specified in @api.doc(). Prefer using @api.expects() with a RequestParser instead for query parameters."
+FDP002 = 'FDP002: Decorator "{0}" out of order, should be higher in decorator list.'
+FDP003 = 'FDP003: Keyword arg "{0}" out of order, should be higher in @api.doc() keyword arg list.'
+
+DECORATOR_ORDER_MAP = {
+    "record_metrics": 0,
+    "doc": 1,
+    "expects": 2,
+    "marshal_with": 3,
+    "cache": 4,
+}
+
+API_DOC_KEYWORD_ORDER_MAP = {"id": 0, "description": 1, "params": 2, "responses": 3}
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self):
+        self.problems: List[Tuple[int, int, str]] = []
+        self._regex = re.compile(".*<.*:(.*)>.*")
+
+    def visit_ClassDef(self, node: ast.ClassDef):
+        route_args = []
+        can_check_route_args = False
+        for decorator in node.decorator_list:
+            if (
+                isinstance(decorator, ast.Call)
+                and isinstance(decorator.func, ast.Attribute)
+                and decorator.func.attr == "route"
+            ):
+                if len(decorator.args) > 0 and isinstance(
+                    decorator.args[0], ast.Constant
+                ):
+                    value = decorator.args[0].value
+                    route_args = self._regex.findall(value)
+                    can_check_route_args = True
+
+            for child in node.body:
+                if isinstance(child, ast.FunctionDef):
+                    decorators: List[str] = []
+                    for decorator in child.decorator_list:
+                        if isinstance(decorator, ast.Call):
+                            if isinstance(decorator.func, ast.Attribute):
+                                self._check_decorator_order(
+                                    decorator.func.attr,
+                                    decorators,
+                                    (decorator.lineno, decorator.col_offset),
+                                )
+                                decorators.append(decorator.func.attr)
+                                if decorator.func.attr == "doc":
+                                    processed_keywords: List[str] = []
+                                    for keyword in decorator.keywords:
+                                        if can_check_route_args:
+                                            self._check_route_params(
+                                                keyword, route_args
+                                            )
+                                        if keyword.arg:
+                                            self._check_doc_keyword_order(
+                                                keyword, processed_keywords
+                                            )
+                                            processed_keywords.append(keyword.arg)
+                            elif isinstance(decorator.func, ast.Name):
+                                self._check_decorator_order(
+                                    decorator.func.id,
+                                    decorators,
+                                    (decorator.lineno, decorator.col_offset),
+                                )
+                                decorators.append(decorator.func.id)
+                        elif isinstance(decorator, ast.Name):
+                            self._check_decorator_order(
+                                decorator.id,
+                                decorators,
+                                (decorator.lineno, decorator.col_offset),
+                            )
+                            decorators.append(decorator.id)
+
+        self.generic_visit(node)
+
+    def _check_decorator_order(
+        self,
+        decorator_name: str,
+        processed_decorators: List[str],
+        line_and_col: Tuple[int, int],
+    ):
+        """Ensures the order of decorators is correct"""
+        if decorator_name in DECORATOR_ORDER_MAP:
+            processed_decorators = list(
+                filter(lambda dec: dec in DECORATOR_ORDER_MAP, processed_decorators)
+            )
+            decorator_order_index = DECORATOR_ORDER_MAP[decorator_name]
+            mapped = map(
+                lambda dec: DECORATOR_ORDER_MAP[dec] <= decorator_order_index,
+                processed_decorators,
+            )
+            if not all(mapped):
+                self.problems.append(
+                    (line_and_col[0], line_and_col[1], FDP002.format(decorator_name))
+                )
+
+    def _check_doc_keyword_order(
+        self,
+        keyword: ast.keyword,
+        processed_keywords: List[str],
+    ):
+        """Checks that the @api.doc() keywords are in a defined order"""
+        if keyword.arg in API_DOC_KEYWORD_ORDER_MAP:
+            processed_keywords = list(
+                filter(lambda kw: kw in API_DOC_KEYWORD_ORDER_MAP, processed_keywords)
+            )
+            keyword_order_index = API_DOC_KEYWORD_ORDER_MAP[keyword.arg]
+            mapped = map(
+                lambda kw: API_DOC_KEYWORD_ORDER_MAP[kw] <= keyword_order_index,
+                processed_keywords,
+            )
+            if not all(mapped):
+                self.problems.append(
+                    (
+                        keyword.value.lineno,
+                        keyword.value.col_offset,
+                        FDP003.format(keyword.arg),
+                    )
+                )
+
+    def _check_route_params(self, keyword: ast.keyword, route_args):
+        """Checks that the @api.doc() decorator only includes params for the route."""
+        if keyword.arg == "params" and isinstance(keyword.value, ast.Dict):
+            for key in keyword.value.keys:
+                if isinstance(key, ast.Constant) and key.value not in route_args:
+                    self.problems.append(
+                        (
+                            key.lineno,
+                            key.col_offset,
+                            FDP001,
+                        )
+                    )
+
+
+class Plugin:
+    name = __name__
+    version = importlib_metadata.version(__name__)
+
+    def __init__(self, tree: ast.AST):
+        self._tree = tree
+
+    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
+        visitor = Visitor()
+        visitor.visit(self._tree)
+        for line, col, message in visitor.problems:
+            yield line, col, message, type(self)
---
+++ discovery-provider/flask_decorator_plugin/tests/flask_decorator_plugin_test.py
@@ -0,0 +1,93 @@
+import ast
+
+from flask_decorator_plugin import FDP001, FDP002, FDP003, Plugin
+
+non_route_param_example = """
+@full_ns.route("/<string:track_id>/remixes")
+class FullRemixesRoute(Resource):
+    @record_metrics
+    @full_ns.doc(
+        id="Get Track Remixes",
+        description="Get all tracks that remix the given track",
+        params={"track_id": "A Track ID", "some_param": "Not in the route"},
+    )
+    @full_ns.expect(pagination_with_current_user_parser)
+    @full_ns.marshal_with(remixes_response)
+    @cache(ttl_sec=10)
+    def get(self, track_id):
+        pass
+"""
+
+variable_route_param_example = """
+@ns.route(TRACK_ROUTE)
+class Track(Resource):
+    @record_metrics
+    @ns.doc(
+        id=\"Get Track\",
+        description=\"Gets a track by ID\",
+        params={\"track_id\": \"A Track ID\"},
+        responses={200: \"Success\", 400: \"Bad request\", 500: \"Server error\"},
+    )
+    @ns.marshal_with(track_response)
+    @cache(ttl_sec=5)
+    def get(self, track_id):
+        decoded_id = decode_with_abort(track_id, ns)
+        return get_single_track(decoded_id, None, ns)
+"""
+
+attribute_order_example = """
+@full_ns.route(\"/<string:track_id>/remixes\")
+class FullRemixesRoute(Resource):
+    @full_ns.doc(
+        id=\"Get Track Remixes\",
+        description=\"Get all tracks that remix the given track\",
+        params={\"track_id\": \"A Track ID\"},
+    )
+    @record_metrics
+    @full_ns.expect(pagination_with_current_user_parser)
+    @full_ns.marshal_with(remixes_response)
+    @cache(ttl_sec=10)
+    def get(self, track_id):
+        pass
+"""
+
+keyword_order_example = """
+@ns.route("/<string:track_id>")
+class Track(Resource):
+    @record_metrics
+    @ns.doc(
+        id=\"Get Track\",
+        description=\"Gets a track by ID\",
+        responses={200: \"Success\", 400: \"Bad request\", 500: \"Server error\"},
+        params={\"track_id\": \"A Track ID\"},
+    )
+    @ns.marshal_with(track_response)
+    @cache(ttl_sec=5)
+    def get(self, track_id):
+        decoded_id = decode_with_abort(track_id, ns)
+        return get_single_track(decoded_id, None, ns)
+"""
+
+
+def _results(s: str):
+    tree = ast.parse(s)
+    plugin = Plugin(tree)
+    return {f"{line}:{col} {msg}" for line, col, msg, _ in plugin.run()}
+
+
+def test_non_route_param():
+    assert _results(non_route_param_example) == {f"8:42 {FDP001}"}
+
+
+def test_variable_route_param():
+    assert _results(variable_route_param_example) == set()
+
+
+def test_attribute_order():
+    assert _results(attribute_order_example) == {
+        f'9:5 {FDP002.format("record_metrics")}'
+    }
+
+
+def test_keyword_order():
+    assert _results(keyword_order_example) == {f'9:15 {FDP003.format("params")}'}
