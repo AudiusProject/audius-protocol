dd21f8b953c9bb716ab27d313014a8a1b1143c51
jowlee <joeylee0925@gmail.com>
2022-03-30 21:44:53 +0000

    make migration idempotent

--- discovery-provider/alembic/versions/11060779bb3a_solana_slots_migration.py
+++ discovery-provider/alembic/versions/11060779bb3a_solana_slots_migration.py
@@ -27,75 +27,70 @@ def upgrade():
                 slot INTEGER NOT NULL
             );
 
-            ALTER TABLE users DROP CONSTRAINT users_pkey;
+            ALTER TABLE users DROP CONSTRAINT IF EXISTS users_pkey;
 
-            CREATE INDEX users_primary ON users (is_current, user_id, txhash);
+            CREATE INDEX IF NOT EXISTS users_primary ON users (is_current, user_id, txhash);
 
             ALTER TABLE users
-            ADD COLUMN slot INTEGER,
-            ADD COLUMN user_storage_account VARCHAR,
-            ADD COLUMN user_authority_account VARCHAR;
+            ADD COLUMN IF NOT EXISTS slot INTEGER,
+            ADD COLUMN IF NOT EXISTS user_storage_account VARCHAR,
+            ADD COLUMN IF NOT EXISTS user_authority_account VARCHAR;
             
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE users ALTER COLUMN blockhash DROP NOT NULL;
             ALTER TABLE users ALTER COLUMN blocknumber DROP NOT NULL;
 
-            ALTER TABLE tracks DROP CONSTRAINT tracks_pkey;
+            ALTER TABLE tracks DROP CONSTRAINT IF EXISTS tracks_pkey;
 
-            CREATE INDEX tracks_primary ON tracks (is_current, track_id, txhash);
+            CREATE INDEX IF NOT EXISTS tracks_primary ON tracks (is_current, track_id, txhash);
 
-            ALTER TABLE tracks
-            ADD COLUMN slot INTEGER;
+            ALTER TABLE tracks ADD COLUMN IF NOT EXISTS slot INTEGER;
 
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE tracks ALTER COLUMN blockhash DROP NOT NULL;
             ALTER TABLE tracks ALTER COLUMN blocknumber DROP NOT NULL;
 
-            ALTER TABLE playlists DROP CONSTRAINT playlists_pkey;
+            ALTER TABLE playlists DROP CONSTRAINT IF EXISTS playlists_pkey;
 
-            CREATE INDEX playlists_primary ON playlists (is_current, playlist_id, txhash);
+            CREATE INDEX IF NOT EXISTS playlists_primary ON playlists (is_current, playlist_id, txhash);
 
-            ALTER TABLE playlists
-            ADD COLUMN slot INTEGER;
+            ALTER TABLE playlists ADD COLUMN IF NOT EXISTS slot INTEGER;
 
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE playlists ALTER COLUMN blockhash DROP NOT NULL;
             ALTER TABLE playlists ALTER COLUMN blocknumber DROP NOT NULL;
 
-            ALTER TABLE reposts DROP CONSTRAINT reposts_pkey;
-            CREATE INDEX reposts_primary ON reposts (is_current, user_id, repost_item_id, repost_type, txhash);
+            ALTER TABLE reposts DROP CONSTRAINT IF EXISTS reposts_pkey;
+            CREATE INDEX IF NOT EXISTS reposts_primary ON reposts (is_current, user_id, repost_item_id, repost_type, txhash);
 
-            ALTER TABLE reposts
-            ADD COLUMN slot INTEGER;
+            ALTER TABLE reposts ADD COLUMN IF NOT EXISTS slot INTEGER;
 
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE reposts ALTER COLUMN blockhash DROP NOT NULL;
             ALTER TABLE reposts ALTER COLUMN blocknumber DROP NOT NULL;
 
-            ALTER TABLE saves DROP CONSTRAINT saves_pkey;
-            CREATE INDEX saves_primary ON saves (is_current, user_id, save_item_id, save_type, txhash);
+            ALTER TABLE saves DROP CONSTRAINT IF EXISTS saves_pkey;
+            CREATE INDEX IF NOT EXISTS saves_primary ON saves (is_current, user_id, save_item_id, save_type, txhash);
             
-            ALTER TABLE saves
-            ADD COLUMN slot INTEGER;
+            ALTER TABLE saves ADD COLUMN IF NOT EXISTS slot INTEGER;
 
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE saves ALTER COLUMN blockhash DROP NOT NULL;
             ALTER TABLE saves ALTER COLUMN blocknumber DROP NOT NULL;
 
-            ALTER TABLE ursm_content_nodes DROP CONSTRAINT ursm_content_nodes_pkey;
-            CREATE INDEX ursm_content_nodes_primary ON ursm_content_nodes (is_current, cnode_sp_id, txhash);
+            ALTER TABLE ursm_content_nodes DROP CONSTRAINT IF EXISTS ursm_content_nodes_pkey;
+            CREATE INDEX IF NOT EXISTS ursm_content_nodes_primary ON ursm_content_nodes (is_current, cnode_sp_id, txhash);
 
-            ALTER TABLE ursm_content_nodes
-            ADD COLUMN slot INTEGER;
+            ALTER TABLE ursm_content_nodes ADD COLUMN IF NOT EXISTS slot INTEGER;
 
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE ursm_content_nodes ALTER COLUMN blockhash DROP NOT NULL;
             ALTER TABLE ursm_content_nodes ALTER COLUMN blocknumber DROP NOT NULL;
 
-            ALTER TABLE follows DROP CONSTRAINT follows_pkey;
-            CREATE INDEX follows_primary ON follows (is_current, follower_user_id, followee_user_id, txhash);
+            ALTER TABLE follows DROP CONSTRAINT IF EXISTS follows_pkey;
+            CREATE INDEX IF NOT EXISTS follows_primary ON follows (is_current, follower_user_id, followee_user_id, txhash);
 
-            ALTER TABLE follows ADD COLUMN slot INTEGER;
+            ALTER TABLE follows ADD COLUMN IF NOT EXISTS slot INTEGER;
 
             -- Drop NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE follows ALTER COLUMN blockhash DROP NOT NULL;
@@ -118,9 +113,9 @@ def downgrade():
             ALTER TABLE users ADD PRIMARY KEY (is_current, user_id, blockhash, txhash);
 
             ALTER TABLE users
-            DROP COLUMN slot,
-            DROP COLUMN user_storage_account,
-            DROP COLUMN user_authority_account;
+            DROP COLUMN IF EXISTS slot,
+            DROP COLUMN IF EXISTS user_storage_account,
+            DROP COLUMN IF EXISTS user_authority_account;
             
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             DELETE FROM users where blockhash IS NULL or blocknumber IS NULL;
@@ -130,8 +125,7 @@ def downgrade():
             DROP INDEX IF EXISTS tracks_primary;
             ALTER TABLE tracks ADD PRIMARY KEY (is_current, track_id, blockhash, txhash);
 
-            ALTER TABLE tracks
-            DROP COLUMN slot;
+            ALTER TABLE tracks DROP COLUMN IF EXISTS slot;
 
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             DELETE FROM tracks where blockhash IS NULL or blocknumber IS NULL;
@@ -141,8 +135,7 @@ def downgrade():
             DROP INDEX IF EXISTS playlists_primary;
             ALTER TABLE playlists ADD PRIMARY KEY (is_current, playlist_id, playlist_owner_id, blockhash, txhash);
 
-            ALTER TABLE playlists
-            DROP COLUMN slot;
+            ALTER TABLE playlists DROP COLUMN IF EXISTS slot;
 
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             DELETE FROM playlists where blockhash IS NULL or blocknumber IS NULL;
@@ -152,8 +145,7 @@ def downgrade():
             DROP INDEX IF EXISTS reposts_primary;
             ALTER TABLE reposts ADD PRIMARY KEY (is_current, user_id, repost_item_id, repost_type, blockhash, txhash);
 
-            ALTER TABLE reposts
-            DROP COLUMN slot;
+            ALTER TABLE reposts DROP COLUMN IF EXISTS slot;
 
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             DELETE FROM reposts where blockhash IS NULL or blocknumber IS NULL;
@@ -163,8 +155,7 @@ def downgrade():
             DROP INDEX IF EXISTS saves_primary;
             ALTER TABLE saves ADD PRIMARY KEY (is_current, user_id, save_item_id, save_type, blockhash, txhash);
             
-            ALTER TABLE saves
-            DROP COLUMN slot;
+            ALTER TABLE saves DROP COLUMN IF EXISTS slot;
 
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             DELETE FROM saves where blockhash IS NULL or blocknumber IS NULL;
@@ -175,8 +166,7 @@ def downgrade():
             DROP INDEX IF EXISTS ursm_content_nodes_primary;
             ALTER TABLE ursm_content_nodes ADD PRIMARY KEY (is_current, cnode_sp_id, blockhash, txhash);
             
-            ALTER TABLE ursm_content_nodes
-            DROP COLUMN slot;
+            ALTER TABLE ursm_content_nodes DROP COLUMN IF EXISTS slot;
 
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             DELETE FROM ursm_content_nodes where blockhash IS NULL or blocknumber IS NULL;
@@ -185,7 +175,7 @@ def downgrade():
 
             DROP INDEX IF EXISTS follows_primary;
             ALTER TABLE follows ADD PRIMARY KEY (is_current, follower_user_id, followee_user_id, blockhash, txhash);
-            ALTER TABLE follows DROP COLUMN slot;
+            ALTER TABLE follows DROP COLUMN IF EXISTS slot;
 
             -- Add NOT NULL Constraint on POA blockhash and tx hash columns
             ALTER TABLE follows ALTER COLUMN blockhash SET NOT NULL;
