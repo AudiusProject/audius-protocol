8309d3c0782aef2cadc40ce938707e3ebe7c5dce
Johannes Naylor <jonaylor89@gmail.com>
2022-04-05 16:55:13 -0400

    [] update utils.sh to reflect recent gcloud changes

--- service-commands/scripts/utils.sh
+++ service-commands/scripts/utils.sh
@@ -74,14 +74,14 @@ gcp_image_to_flags() {
 }
 
 gcp_set_defaults() {
-	if [[ "$(gcloud config get-value 'compute/region')" == "" ]]; then
+	if [[ "$(gcloud config get 'compute/region')" == "" ]]; then
 		echo 'setting compute/region in gcloud'
-		gcloud config set-value 'compute/region' $DEFAULT_GCP_COMPUTE_REGION
+		gcloud config set 'compute/region' $DEFAULT_GCP_COMPUTE_REGION
 	fi
 
-	if [[ "$(gcloud config get-value 'compute/zone')" == "" ]]; then
+	if [[ "$(gcloud config get 'compute/zone')" == "" ]]; then
 		echo 'setting compute/zone in gcloud'
-		gcloud config set-value 'compute/zone' $DEFAULT_GCP_COMPUTE_ZONE
+		gcloud config set 'compute/zone' $DEFAULT_GCP_COMPUTE_ZONE
 	fi
 }
 
