4ae753f3a7783522eb2af898f10c80ec0ff83bbd
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2022-04-07 19:50:13 +0000

    Custom send working - cleaning up

--- identity-service/src/solana-client.js
+++ identity-service/src/solana-client.js
@@ -164,8 +164,7 @@ async function createAndVerifyMessage (
     instructionData
   )
 
-  let recentBlockHash = connection.getRecentBlockhash('confirmed')
-  let transaction = new solanaWeb3.Transaction(recentBlockHash)
+  let transaction = new solanaWeb3.Transaction()
 
   let secpInstruction = solanaWeb3.Secp256k1Program.createInstructionWithPublicKey(
     {
@@ -191,19 +190,125 @@ async function createAndVerifyMessage (
   })
 
   let feePayerAccount = getFeePayerKeypair(false)
-
-  let signature = await solanaWeb3.sendAndConfirmTransaction(
-    connection,
-    transaction,
-    [feePayerAccount],
-    {
-      skipPreflight: false,
-      commitment: config.get('solanaTxCommitmentLevel'),
-      preflightCommitment: config.get('solanaTxCommitmentLevel')
-    }
-  )
+  let signature = await sendAndSignTransaction(connection, transaction, feePayerAccount)
 
   return signature
+}
+
+const getUnixTs = () => {
+  return new Date().getTime() / 1000;
+};
+
+// Adapted from mango send function
+// https://github.com/blockworks-foundation/mango-ui/blob/b6abfc6c13b71fc17ebbe766f50b8215fa1ec54f/src/utils/send.tsx#L785
+async function sendAndSignTransaction (connection, transaction, signers) {
+  let recentBlockHash = (await connection.getRecentBlockhash('confirmed')).blockhash
+  transaction.recentBlockhash = recentBlockHash
+
+  transaction.sign(signers)
+  let rawTransaction = transaction.serialize()
+  const startTime = getUnixTs();
+  const txid = await connection.sendRawTransaction(
+    rawTransaction,
+    {
+      skipPreflight: true,
+    },
+  );
+
+  // Anonymous function to retry sending until confirmation
+  (async () => {
+    while (!done && getUnixTs() - startTime < timeout) {
+      connection.sendRawTransaction(rawTransaction, {
+        skipPreflight: true,
+      });
+      await sleep(300);
+    }
+  })();
+
+  try {
+    awaitTransactionSignatureConfirmation(txid, 1000, connection)
+  } catch(e) {
+    throw new Error(e)
+  } finally {
+    done = true
+  }
+  return txid
+}
+
+async function awaitTransactionSignatureConfirmation(
+  txid,
+  timeout,
+  connection,
+) {
+  let done = false;
+  const result = await new Promise((resolve, reject) => {
+    (async () => {
+      setTimeout(() => {
+        if (done) {
+          return;
+        }
+        done = true;
+        console.log('Timed out for txid', txid);
+        reject({ timeout: true });
+      }, timeout);
+      try {
+        connection.onSignature(
+          txid,
+          (result) => {
+            console.log('WS confirmed', txid, result);
+            done = true;
+            if (result.err) {
+              reject(result.err);
+            } else {
+              resolve(result);
+            }
+          },
+          connection.commitment,
+        );
+        console.log('Set up WS connection', txid);
+      } catch (e) {
+        done = true;
+        console.log('WS error in setup', txid, e);
+      }
+      while (!done) {
+        // eslint-disable-next-line no-loop-func
+        (async () => {
+          try {
+            const signatureStatuses = await connection.getSignatureStatuses([
+              txid,
+            ]);
+            const result = signatureStatuses && signatureStatuses.value[0];
+            if (!done) {
+
+              if (!result) {
+                // console.log('REST null result for', txid, result);
+              } else if (result.err) {
+                console.log('REST error for', txid, result);
+                done = true;
+                reject(result.err);
+              }
+              // @ts-ignore
+              else if (!(result.confirmations || result.confirmationStatus === "confirmed" || result.confirmationStatus === "finalized")) {
+                console.log('REST not confirmed', txid, result);
+              } else {
+                console.log('REST confirmed', txid, result);
+                done = true;
+                resolve(result);
+              }
+            }
+          }
+          catch (e) {
+            if (!done) {
+              console.log('REST connection error: txid', txid, e);
+            }
+          }
+        })();
+        await sleep(300);
+      }
+    })();
+  });
+  done = true;
+  return result;
 }
 
 exports.createAndVerifyMessage = createAndVerifyMessage
