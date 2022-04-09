7231c08ab87f4db23c0c08e58c63270ed4f9de98
Joaquin Casares <joaquin@audius.co>
2022-04-09 00:26:09 +0000

    no more logs

--- creator-node/src/routes/prometheusMetricsExporter.js
+++ creator-node/src/routes/prometheusMetricsExporter.js
@@ -6,11 +6,9 @@ const PrometheusMetric = require('../services/PrometheusMetric')
 const collectDefaultMetrics = Prometheus.collectDefaultMetrics
 const metricPrefix = 'audius_cn_'
 
-console.log('we were here')
 collectDefaultMetrics({ prefix: metricPrefix })
 
 module.exports = function (app) {
-  console.log('we were there')
   app.get('/prometheus_metrics', async (req, res) => {
     try {
       PrometheusMetric.populateCollectors()
