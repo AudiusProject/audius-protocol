5370d6db7f664c836f4171e5cc9773ef9acda336
Ubuntu <ubuntu@johannes-dev.us-central1-a.c.audius-infrastructure.internal>
2022-04-12 22:07:02 +0000

    test

---
+++ discovery-provider/src/queries/get_celery_tasks_unit_test.py
@@ -0,0 +1,13 @@
+
+
+from src.queries.get_celery_tasks import get_tasks
+
+
+def test_get_tasks():
+
+    # TODO : Mock Celery
+
+    tasks = get_tasks()
+
+    assert len(tasks) == 0
+    assert tasks == []
