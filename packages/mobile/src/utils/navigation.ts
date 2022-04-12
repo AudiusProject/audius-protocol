8082723621e47d730a4f790fb242eb068c68f3e0
Raymond Jacobson <ray@audius.co>
2022-04-12 09:08:32 -0700

    Enable full screen drawer swipe and disable on stack pages (#1202)

--- packages/mobile/src/utils/navigation.ts
+++ packages/mobile/src/utils/navigation.ts
@@ -1,27 +1,4 @@
 import { NavigationState } from '@react-navigation/native'
-import { Maybe } from 'audius-client/src/common/utils/typeUtils'
-
-/**
- * Navigation state selector that selects state for a given route
- * Can be passed to useNavigationState
- */
-export const getNavigationStateAtRoute = (routeSegments: string[]) => (
-  state?: NavigationState
-): Maybe<NavigationState> => {
-  if (!state) {
-    return undefined
-  }
-
-  if (!routeSegments.length) {
-    return state
-  }
-
-  const firstRouteSegment = routeSegments[0]
-  const route = state.routes.find(({ name }) => name === firstRouteSegment)
-  return getNavigationStateAtRoute(routeSegments.slice(1))(
-    route?.state as NavigationState
-  )
-}
 
 /**
  * Navigation state selector that selects the current route
