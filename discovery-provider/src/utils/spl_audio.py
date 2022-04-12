8162b196c368be6afc6a671ea205203630354426
Joaquin Casares <joaquin@audius.co>
2022-04-12 02:18:19 +0000

    lint after black upgrade

--- discovery-provider/src/utils/spl_audio.py
+++ discovery-provider/src/utils/spl_audio.py
@@ -1,7 +1,7 @@
 # Sol balances have 8 decimals, so they need to be increased to 18 to match eth balances
 from typing import Union
 
-SPL_TO_WEI = 10 ** 10
+SPL_TO_WEI = 10**10
 
 
 def to_wei(balance: Union[int, str]):
