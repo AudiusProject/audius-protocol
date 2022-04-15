27f4f6933d09303b564df76bce29fcc6fdb5bb99
Marcus Pasell <rickyrombo@gmail.com>
2022-04-15 20:44:24 +0000

    Make migration idempotent

--- discovery-provider/alembic/versions/8c10373f2615_user_tips.py
+++ discovery-provider/alembic/versions/8c10373f2615_user_tips.py
@@ -7,6 +7,7 @@ Create Date: 2022-04-14 00:06:55.572866
 """
 import sqlalchemy as sa
 from alembic import op
+from src.utils.alembic_helpers import table_exists
 
 # revision identifiers, used by Alembic.
 revision = "8c10373f2615"
@@ -17,49 +18,53 @@ depends_on = None
 
 def upgrade():
     # USER TIPS
-    op.create_table(
-        "user_tips",
-        sa.Column("user_tip_id", sa.Integer(), nullable=False, autoincrement=True),
-        sa.Column("signature", sa.String(), nullable=False),
-        sa.Column("sender_user_id", sa.Integer(), nullable=False),
-        sa.Column("receiver_user_id", sa.Integer(), nullable=False),
-        sa.Column("amount", sa.BigInteger(), nullable=False),
-        sa.Column("created_at", sa.DateTime(), nullable=False),
-        sa.PrimaryKeyConstraint("user_tip_id"),
-    )
-    op.create_index(
-        op.f("ix_user_tips_receiver_user_id"),
-        "user_tips",
-        ["receiver_user_id"],
-        unique=False,
-    )
-    op.create_index(
-        op.f("ix_user_tips_sender_user_id"),
-        "user_tips",
-        ["sender_user_id"],
-        unique=False,
-    )
+    if not table_exists("user_tips"):
+        op.create_table(
+            "user_tips",
+            sa.Column("user_tip_id", sa.Integer(), nullable=False, autoincrement=True),
+            sa.Column("signature", sa.String(), nullable=False),
+            sa.Column("sender_user_id", sa.Integer(), nullable=False),
+            sa.Column("receiver_user_id", sa.Integer(), nullable=False),
+            sa.Column("amount", sa.BigInteger(), nullable=False),
+            sa.Column("created_at", sa.DateTime(), nullable=False),
+            sa.PrimaryKeyConstraint("user_tip_id"),
+            keep_existing=True,
+        )
+        op.create_index(
+            op.f("ix_user_tips_receiver_user_id"),
+            "user_tips",
+            ["receiver_user_id"],
+            unique=False,
+        )
+        op.create_index(
+            op.f("ix_user_tips_sender_user_id"),
+            "user_tips",
+            ["sender_user_id"],
+            unique=False,
+        )
 
     # AGGREGATE USER TIPS
-    op.create_table(
-        "aggregate_user_tips",
-        sa.Column("sender_user_id", sa.Integer(), nullable=False),
-        sa.Column("receiver_user_id", sa.Integer(), nullable=False),
-        sa.Column("amount", sa.BigInteger(), nullable=False),
-        sa.PrimaryKeyConstraint("sender_user_id", "receiver_user_id"),
-    )
-    op.create_index(
-        op.f("ix_aggregate_user_tips_receiver_user_id"),
-        "aggregate_user_tips",
-        ["receiver_user_id"],
-        unique=False,
-    )
-    op.create_index(
-        op.f("ix_aggregate_user_tips_sender_user_id"),
-        "aggregate_user_tips",
-        ["sender_user_id"],
-        unique=False,
-    )
+    if not table_exists("aggregate_user_tips"):
+        op.create_table(
+            "aggregate_user_tips",
+            sa.Column("sender_user_id", sa.Integer(), nullable=False),
+            sa.Column("receiver_user_id", sa.Integer(), nullable=False),
+            sa.Column("amount", sa.BigInteger(), nullable=False),
+            sa.PrimaryKeyConstraint("sender_user_id", "receiver_user_id"),
+            keep_existing=True,
+        )
+        op.create_index(
+            op.f("ix_aggregate_user_tips_receiver_user_id"),
+            "aggregate_user_tips",
+            ["receiver_user_id"],
+            unique=False,
+        )
+        op.create_index(
+            op.f("ix_aggregate_user_tips_sender_user_id"),
+            "aggregate_user_tips",
+            ["sender_user_id"],
+            unique=False,
+        )
 
 
 def downgrade():
