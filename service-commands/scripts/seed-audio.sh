62d2f65a7dcfa144ecd9cca0dcb8cc122f5fc7f3
Marcus Pasell <rickyrombo@gmail.com>
2022-04-15 20:07:53 +0000

    Use -rc

--- service-commands/scripts/seed-audio.sh
+++ service-commands/scripts/seed-audio.sh
@@ -1,11 +1,11 @@
 # Gets the spl-token wallet from the discovery node api
 get-user-bank () {
-    curl "http://dn1_web-server_1:5000/users?id=$1" &> /dev/null | jq .data\[0\].spl_wallet | sed 's/"//g'
+    curl "http://dn1_web-server_1:5000/users?id=$1" &> /dev/null | jq -rc .data\[0\].spl_wallet
 }
 
 # Gets the private key for a wallet out of the seed cache
 get-eth-private-key () {
-    jq "map(select(.userId==$1).wallet.privKey)[0]" < ~/.audius/seed-cache.json | sed 's/"//g'
+    jq -rc "map(select(.userId==$1).wallet.privKey)[0]" < ~/.audius/seed-cache.json
 }
 
 # Mints $AUDIO and transfers it to a user bank
@@ -17,7 +17,7 @@ seed-audio () {
     echo "Configuring ownerWallet..."
     jq .ownerWallet < ~/.audius/solana-program-config.json > ~/.audius/owner-wallet.json
     echo "Minting Audio..."
-    mint=$(jq .splToken < ~/.audius/solana-program-config.json | sed 's/"//g')
+    mint=$(jq -rc .splToken < ~/.audius/solana-program-config.json)
     echo "spl-token mint $mint $amount"
     spl-token mint $mint $amount | grep "Signature"
     echo "Transferring..."
@@ -32,7 +32,7 @@ tip-audio () {
     amount=$3
     sender_eth=$(get-eth-private-key $sender_id)
     receiver_user_bank=$(get-user-bank $receiver_id)
-    mint=$(jq .splToken < ~/.audius/solana-program-config.json | sed 's/"//g')
+    mint=$(jq -rc .splToken < ~/.audius/solana-program-config.json)
     echo "${PROTOCOL_DIR}/solana-programs/claimable-tokens/cli/target/debug/claimable-tokens-cli transfer $mint $sender_eth $amount --recipient $receiver_user_bank"
     $PROTOCOL_DIR/solana-programs/claimable-tokens/cli/target/debug/claimable-tokens-cli transfer $mint $sender_eth $amount --recipient $receiver_user_bank
 }
