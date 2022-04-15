5e9a98ee5a722a67f2dd87c96f8ec4fd653347f1
Sebastian Klingler <sliptype@gmail.com>
2022-04-15 16:06:08 -0500

    [AUD-1906] Make artist suggestions pressable (#1227)
    * Make artist suggestions artist links pressable
    
    * Make artist profile pictures pressable
    
    * Allow scrolling on nontouchable elements in ArtistRecommendations
    
    * Improve import path

--- packages/mobile/src/screens/profile-screen/ArtistRecommendations/ArtistLink.tsx
+++ packages/mobile/src/screens/profile-screen/ArtistRecommendations/ArtistLink.tsx
@@ -1,23 +1,25 @@
 import { User } from 'audius-client/src/common/models/User'
-import { Pressable } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 
 import { Text } from 'app/components/core'
 import UserBadges from 'app/components/user-badges'
+import { GestureResponderHandler } from 'app/types/gesture'
 
 type ArtistLinkProps = {
   artist: User
+  onPress: GestureResponderHandler
 }
 
 export const ArtistLink = (props: ArtistLinkProps) => {
-  const { artist } = props
+  const { artist, onPress } = props
   const { name } = artist
 
   return (
-    <Pressable style={{ flexDirection: 'row' }}>
+    <TouchableOpacity style={{ flexDirection: 'row' }} onPress={onPress}>
       <Text color='secondary' variant='h3'>
         {name}
       </Text>
       <UserBadges user={artist} hideName badgeSize={8} />
-    </Pressable>
+    </TouchableOpacity>
   )
 }
