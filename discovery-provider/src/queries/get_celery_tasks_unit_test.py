3a579fb4ac44f29a01de7357b615176510340c4a
Johannes Naylor <jonaylor89@gmail.com>
2022-04-15 22:22:25 +0000

    remove test

--- discovery-provider/src/queries/get_celery_tasks_unit_test.py
+++
@@ -1,9 +0,0 @@
-from src.queries.get_celery_tasks import get_celery_tasks
-
-
-def test_no_tasks(celery_app):
-
-    tasks = get_celery_tasks(injected_app=celery_app)
-
-    assert len(tasks) == 0
-    assert tasks == []
