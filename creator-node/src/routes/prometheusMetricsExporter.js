95d008e4d4049671c907b77a85516f3d5b9d8fa5
Joaquin Casares <joaquin@audius.co>
2022-04-28 23:09:11 +0000

    remove handleResponse

--- creator-node/src/routes/prometheusMetricsExporter.js
+++ creator-node/src/routes/prometheusMetricsExporter.js
@@ -1,7 +1,6 @@
 const Prometheus = require('prom-client')
 
 const {
-  handleResponse,
   sendResponse,
   errorResponseServerError
 } = require('../apiHelpers')
@@ -10,17 +9,25 @@ const PrometheusMetric = require('../services/PrometheusMetric')
 const metricPrefix = 'audius_cn_'
 
 // collect default metrics recommended by Prometheus:
-// event loop lag, active handles, GC, and Node.js version
+// event loop lag, active handles, GC, and Node.js version.
+// must be called only once before '/prometheus_metrics' is hit
 Prometheus.collectDefaultMetrics({ prefix: metricPrefix })
 
 module.exports = function (app) {
   app.get(
     '/prometheus_metrics',
-    handleResponse(async (req, res) => {
+    async (req, res) => {
       try {
+        // these collect any internal "gauges" that report point-in-time metrics,
+        // as opposed to histograms which are great for timing pieces of code
         PrometheusMetric.populateCollectors()
+
+        // sets the content type to 'text/plain; version=0.0.4; charset=utf-8'
         res.set('Content-Type', Prometheus.register.contentType)
-        sendResponse(req, res, {
+
+        // uses the Prometheus client to generate a Prometheus-friendly text output
+        // for a Prometheus instance to scrape
+        return sendResponse(req, res, {
           statusCode: 200,
           object: await Prometheus.register.metrics()
         })
@@ -28,6 +35,6 @@ module.exports = function (app) {
         console.error('Prometheus Metrics Exporter error:', e)
         return errorResponseServerError(e.message)
       }
-    })
+    }
   )
 }
