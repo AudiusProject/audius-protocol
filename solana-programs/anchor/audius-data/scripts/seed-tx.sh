cb721b9988eea27b9fa572850707028a1dbdab48
jowlee <joeylee0925@gmail.com>
2022-04-28 17:14:02 +0000

    Fix filename

--- solana-programs/anchor/audius-data/scripts/seed-tx.sh
+++ solana-programs/anchor/audius-data/scripts/seed-tx.sh
@@ -7,7 +7,7 @@ set -x
 ANCHOR_PROGRAM_DIR="$PROTOCOL_DIR/solana-programs/anchor/audius-data"
 OWNER_KEYPAIR_PATH="$HOME/.config/solana/id.json"
 ADMIN_KEYPAIR_PATH="$PWD/adminKeypair.json"
-ADMIN_STORAGE_KEYPAIR_PATH="$PWD/adminStorageKeypair.json"
+ADMIN_ACCOUNT_KEYPAIR_PATH="$PWD/adminAccountKeypair.json"
 USER_KEYPAIR_PATH="$PWD/userKeypair.json"
 AUDIUS_DATA_PROGRAM_ID=$(solana-keygen pubkey $PWD/target/deploy/audius_data-keypair.json)
 
@@ -25,19 +25,19 @@ echo "Registering content nodes!"
 yarn run ts-node cli/main.ts -f initContentNode \
     -k "$OWNER_KEYPAIR_PATH" \
     --admin-keypair "$ADMIN_KEYPAIR_PATH" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --cn-sp-id 1
 
 yarn run ts-node cli/main.ts -f initContentNode \
     -k "$OWNER_KEYPAIR_PATH" \
     --admin-keypair "$ADMIN_KEYPAIR_PATH" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --cn-sp-id 2
 
 yarn run ts-node cli/main.ts -f initContentNode \
     -k "$OWNER_KEYPAIR_PATH" \
     --admin-keypair "$ADMIN_KEYPAIR_PATH" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --cn-sp-id 3
 
 echo "Init user"
@@ -45,7 +45,7 @@ echo "Init user"
 yarn run ts-node cli/main.ts -f initUser \
     -k "$OWNER_KEYPAIR_PATH" \
     --admin-keypair "$ADMIN_KEYPAIR_PATH" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --user-replica-set 1,2,3 \
     --user-id 1 \
     -e 0x0a93d8cb0Be85B3Ea8f33FA63500D118deBc83F7 | tee /tmp/initUserOutput.txt
@@ -60,7 +60,7 @@ solana-keygen new --no-bip39-passphrase --force -o "$USER_KEYPAIR_PATH"
 yarn run ts-node cli/main.ts -f initUserSolPubkey \
     -k "$OWNER_KEYPAIR_PATH" \
     --user-solana-keypair "$USER_KEYPAIR_PATH" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --user-account "$USER_ACCOUNT" \
     --eth-private-key d540ca11a0d12345f512e65e00bf8bf87435aa40b3731cbf0322971709eba60f
 
@@ -70,7 +70,7 @@ yarn run ts-node cli/main.ts -f createTrack \
     -k "$OWNER_KEYPAIR_PATH" \
     --user-solana-keypair "$USER_KEYPAIR_PATH" \
     --user-account "$USER_ACCOUNT" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --user-id 1 # metadata CID that would point off-chain is randomly generated here
 
 echo "Creating playlist"
@@ -79,7 +79,7 @@ yarn run ts-node cli/main.ts -f createPlaylist \
     -k "$OWNER_KEYPAIR_PATH" \
     --user-solana-keypair "$USER_KEYPAIR_PATH" \
     --user-account "$USER_ACCOUNT" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --user-id 1 | tee /tmp/createPlaylistOutput.txt # metadata CID that would point off-chain is randomly generated here 
 
 PLAYLIST_ID=$(cut -d '=' -f 3 <<< $(cat /tmp/createPlaylistOutput.txt | grep "Transacting on entity"))
@@ -90,7 +90,7 @@ yarn run ts-node cli/main.ts -f updatePlaylist \
     -k "$OWNER_KEYPAIR_PATH" \
     --user-solana-keypair "$USER_KEYPAIR_PATH" \
     --user-account "$USER_ACCOUNT" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --id "$PLAYLIST_ID" \
     --user-id 1 # metadata CID that would point off-chain is randomly generated here 
 
@@ -100,7 +100,7 @@ yarn run ts-node cli/main.ts -f deletePlaylist \
     -k "$OWNER_KEYPAIR_PATH" \
     --user-solana-keypair "$USER_KEYPAIR_PATH" \
     --user-account "$USER_ACCOUNT" \
-    --admin-account-keypair "$ADMIN_STORAGE_KEYPAIR_PATH" \
+    --admin-account-keypair "$ADMIN_ACCOUNT_KEYPAIR_PATH" \
     --id "$PLAYLIST_ID" \
     --user-id 1
 
