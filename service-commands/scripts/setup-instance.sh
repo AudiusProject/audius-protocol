1b5e21d77ba76348ceed4d6dffcb1df9a8e70ad4
Joaquin Casares <joaquin@audius.co>
2022-04-29 01:51:13 -0500

    Update setup-instance.sh

--- service-commands/scripts/setup-instance.sh
+++ service-commands/scripts/setup-instance.sh
@@ -131,10 +131,6 @@ case "$service" in
 		
 		echo -e "\nLogin using:\n"
 		echo -e "gcloud compute ssh $user@$name\n"
-
-		IP=$(get_ip_addr $provider $name)
-		echo -e "\nRun the following to create an SSL tunnel to allow the client into the remote-dev box:\n"
-		echo -e "ssh -N -L 3000:127.0.0.1:3000 -i ~/.ssh/google_compute_engine ubuntu@${IP}\n"
 		;;
 		
 esac
