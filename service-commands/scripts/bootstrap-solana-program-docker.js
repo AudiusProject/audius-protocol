4d06228377a289d3eaecfa51efa00753b0473946
jowlee <joeylee0925@gmail.com>
2022-04-29 15:16:38 +0000

    Use cached solana docker

---
+++ service-commands/scripts/bootstrap-solana-program-docker.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const { exec, execSync } = require('child_process')
+
+const solanaProgramsPath = `${process.env.PROTOCOL_DIR}/solana-programs`
+const tmp = `/tmp/solana-deploy`
+
+const writeMetadata = async () => {
+  const gitHash = await new Promise(resolve =>
+    exec(`cd ${solanaProgramsPath} && git rev-parse HEAD`, (_, stdout) =>
+      resolve(stdout)
+    )
+  )
+  fs.writeFileSync(
+    `${tmp}/metadata.json`,
+    JSON.stringify(
+      {
+        gitHash,
+        date: new Date().toISOString()
+      },
+      null,
+      ' '
+    )
+  )
+}
+
+const main = async () => {
+  // Spin up the solana validator from a blank state
+  execSync('A run solana-validator up', { stdio: 'inherit' })
+  // Compile all the solana programs and deploy to the validator
+  execSync('A run solana-programs up', { stdio: 'inherit' })
+
+  // Do some setup...
+  execSync(`rm -rf ${tmp}`, { stdio: 'inherit' })
+  execSync(`mkdir -p ${tmp}`, { stdio: 'inherit' })
+
+  // wait for memory to be written to disk
+  await new Promise(resolve => setTimeout(resolve, 1000 * 60))
+
+  // Copy the 'test-ledger' directory with all the information on chain to a local tmp folder
+  execSync(`docker cp solana:/test-ledger ${tmp}`, { stdio: 'inherit' })
+
+  // Copy the solana configs to the tmp folder
+  execSync(`cp ${solanaProgramsPath}/solana-program-config.json ${tmp}`, {
+    stdio: 'inherit'
+  })
+  // Copy the Dockerfile to the tmp folder
+  execSync(`cp ${solanaProgramsPath}/Dockerfile.cacheLedger ${tmp}`, {
+    stdio: 'inherit'
+  })
+
+  // Copy all JSON files tmp folder: solana config, admin account keypair, admin authority keypair, user keypair
+  execSync(`cp ${solanaProgramsPath}/anchor/audius-data/*.json ${tmp}`, {
+    stdio: 'inherit'
+  })
+
+  execSync(`mkdir -p ${tmp}/audius-data`, { stdio: 'inherit' })
+  execSync(
+    `cp -r ${solanaProgramsPath}/anchor/audius-data/target ${tmp}/audius-data`,
+    { stdio: 'inherit' }
+  )
+  await writeMetadata()
+  // Make the dockerfile
+  execSync(
+    `cd ${tmp} && docker build -f Dockerfile.cacheLedger -t audius/solana-programs:predeployed-latest .`,
+    { stdio: 'inherit' }
+  )
+
+  // TODO: Deploy to docker registry (dockerhub)
+  console.log('Be sure to publish the docker file if using externally')
+  console.log('ie. "docker push audius/solana-programs:predeployed-latest"')
+}
+
+main()
--- service-commands/src/setup.js
+++ service-commands/src/setup.js
@@ -144,6 +144,7 @@ const Service = Object.freeze({
   CONTRACTS: 'contracts',
   ETH_CONTRACTS: 'eth-contracts',
   SOLANA_VALIDATOR: 'solana-validator',
+  SOLANA_VALIDATOR_PREDEPLOYED: 'solana-validator-predeployed',
   SOLANA_PROGRAMS: 'solana-programs',
   IPFS: 'ipfs',
   IPFS_2: 'ipfs-2',
@@ -295,7 +296,10 @@ const performHealthCheckWithRetry = async (
 const performHealthCheck = async (service, serviceNumber) => {
   const url = getServiceURL(service, serviceNumber)
   let healthCheckRequestOptions = { method: 'get', url }
-  if (service === Service.SOLANA_VALIDATOR) {
+  if (
+    service === Service.SOLANA_VALIDATOR ||
+    service === Service.SOLANA_VALIDATOR_PREDEPLOYED
+  ) {
     healthCheckRequestOptions = {
       method: 'post',
       data: {
@@ -614,16 +618,15 @@ const allUp = async ({
 
   const setup = [
     [Service.NETWORK, SetupCommand.UP],
-    [Service.SOLANA_VALIDATOR, SetupCommand.UP],
-    [Service.SOLANA_VALIDATOR, SetupCommand.HEALTH_CHECK]
+    [Service.SOLANA_VALIDATOR_PREDEPLOYED, SetupCommand.UP],
+    [Service.SOLANA_VALIDATOR_PREDEPLOYED, SetupCommand.HEALTH_CHECK]
   ]
 
   const ipfsAndContractsCommands = [
     [Service.IPFS, SetupCommand.UP],
     [Service.IPFS_2, SetupCommand.UP],
     [Service.CONTRACTS, SetupCommand.UP],
-    [Service.ETH_CONTRACTS, SetupCommand.UP],
-    [Service.SOLANA_PROGRAMS, SetupCommand.UP]
+    [Service.ETH_CONTRACTS, SetupCommand.UP]
   ]
 
   let creatorNodesCommands = _.range(1, numCreatorNodes + 1).map(
