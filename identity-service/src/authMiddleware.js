2e9fbee68e1a5b5c6d8928960e224d554e763a4c
Michael Piazza <michael.piazza.mp@gmail.com>
2022-05-03 20:46:44 +0000

    Add reactions to identity

--- identity-service/src/authMiddleware.js
+++ identity-service/src/authMiddleware.js
@@ -54,6 +54,7 @@ async function authMiddleware (req, res, next) {
     if (!signature) throw new Error('[Error]: Encoded data signature missing')
 
     const walletAddress = recoverPersonalSignature({ data: encodedDataMessage, sig: signature })
+    console.log({walletAddress})
     let user = await models.User.findOne({
       where: { walletAddress },
       attributes: ['id', 'blockchainUserId', 'walletAddress', 'createdAt', 'handle']
