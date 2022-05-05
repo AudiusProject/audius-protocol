e1da5881429419ca9ed2e6b9a494d20844fcafa6
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-05-05 23:56:23 +0530

    Fix missing fi

--- discovery-provider/scripts/start.sh
+++ discovery-provider/scripts/start.sh
@@ -118,10 +118,11 @@ if [[ "$audius_discprov_dev_mode" == "true" ]]; then
     fi
 
     # filter tail to server logs with `docker exec -it <disc-prov container> tail -f /var/log/discprov-server.log`
-    ./scripts/dev-server.sh 2>&1 | tee >(logger -t server) & 
+    ./scripts/dev-server.sh 2>&1 | tee >(logger -t server) &
     if [[ "$audius_no_workers" != "true" ]] && [[ "$audius_no_workers" != "1" ]]; then
         watchmedo auto-restart --directory ./ --pattern=*.py --recursive -- celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) & # docker exec -it <disc-prov container> tail -f /var/log/discprov-worker.log
-        celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t beat) & # docker exec -it <disc-prov container> tail -f /var/log/discprov-beat.log
+        celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t beat) &                                                                         # docker exec -it <disc-prov container> tail -f /var/log/discprov-beat.log
+    fi
 else
     ./scripts/prod-server.sh 2>&1 | tee >(logger -t server) &
     if [[ "$audius_no_workers" != "true" ]] && [[ "$audius_no_workers" != "1" ]]; then
