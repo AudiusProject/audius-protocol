8b37626f4f62be633afcf57aa64e80d3ba04ec17
Dylan Jeffers <dylan@audius.co>
2022-05-09 10:11:22 -0700

    [C-458] Fix CollectiblesTab text and links (#1294)

--- apps/audius-client/packages/web/src/common/store/ui/collectible-details/selectors.ts
+++ apps/audius-client/packages/web/src/common/store/ui/collectible-details/selectors.ts
@@ -3,3 +3,7 @@ import { CommonState } from 'common/store'
 export const getCollectible = (state: CommonState) => {
   return state.ui.collectibleDetails.collectible
 }
+
+export const getCollectibleOwnerId = (state: CommonState) => {
+  return state.ui.collectibleDetails.ownerId
+}
