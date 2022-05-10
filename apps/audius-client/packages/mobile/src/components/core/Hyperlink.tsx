31a9653f835ed44525720c8c032613039e3fb69d
Sebastian Klingler <sliptype@gmail.com>
2022-05-10 16:11:50 -0400

    [C-424] Fix profile screen bio links on android (#1301)
    * Correctly position auto-links on profile screen on android
    
    * Adjust positioning of links
    
    * Allow links to be opened on android

--- apps/audius-client/packages/mobile/src/components/core/Hyperlink.tsx
+++ apps/audius-client/packages/mobile/src/components/core/Hyperlink.tsx
@@ -1,4 +1,11 @@
-import { ComponentProps, useCallback, useContext, useState } from 'react'
+import {
+  ComponentProps,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState
+} from 'react'
 
 import { Match } from 'autolinker/dist/es2015'
 import { LayoutRectangle, Linking, Text, View } from 'react-native'
@@ -14,6 +21,9 @@ const messages = {
 }
 
 const useStyles = makeStyles(({ palette, typography }) => ({
+  root: {
+    marginBottom: 3
+  },
   link: {
     color: palette.primary
   },
@@ -24,15 +34,16 @@ const useStyles = makeStyles(({ palette, typography }) => ({
     position: 'absolute'
   },
   hiddenLink: {
-    marginBottom: -3,
     opacity: 0
+  },
+  hiddenLinkText: {
+    marginTop: -3
   }
 }))
 
 type PositionedLink = {
   text: string
   match: Match
-  layout: LayoutRectangle
 }
 
 export type HyperlinkProps = ComponentProps<typeof Autolink> & {
@@ -42,11 +53,46 @@ export type HyperlinkProps = ComponentProps<typeof Autolink> & {
 }
 
 export const Hyperlink = (props: HyperlinkProps) => {
-  const { allowPointerEventsToPassThrough, source, ...other } = props
+  const {
+    allowPointerEventsToPassThrough,
+    source,
+    styles: stylesProp,
+    style,
+    ...other
+  } = props
   const styles = useStyles()
   const { toast } = useContext(ToastContext)
 
+  const linkContainerRef = useRef<View>(null)
+  const linkRefs = useRef<Record<number, Text>>({})
   const [links, setLinks] = useState<Record<number, PositionedLink>>({})
+  const [linkLayouts, setLinkLayouts] = useState<
+    Record<number, LayoutRectangle>
+  >({})
+  const [linkContainerLayout, setLinkContainerLayout] = useState<
+    LayoutRectangle
+  >()
+
+  /**
+   * Need to use `measureInWindow` instead of `onLayout` or `measure` because
+   * android doesn't return the correct layout for nested text elements
+   * */
+  useEffect(() => {
+    Object.entries(linkRefs.current).forEach(([index, ref]) => {
+      ref.measureInWindow((x, y, width, height) => {
+        setLinkLayouts(linkLayouts => ({
+          ...linkLayouts,
+          [index]: { x, y, width, height }
+        }))
+      })
+    })
+
+    if (linkContainerRef.current) {
+      linkContainerRef.current.measureInWindow((x, y, width, height) =>
+        setLinkContainerLayout({ x, y, width, height })
+      )
+    }
+  }, [linkRefs, linkContainerRef])
 
   const handlePress = useCallback(
     async (url: string) => {
@@ -69,14 +115,18 @@ export const Hyperlink = (props: HyperlinkProps) => {
             ...links,
             [index]: {
               text,
-              match,
-              layout: e.nativeEvent.layout
+              match
             }
           })
         }}
+        ref={el => {
+          if (linkRefs.current && el) {
+            linkRefs.current[index] = el
+          }
+        }}
         style={styles.hiddenLink}
       >
-        <Text style={styles.linkText}>{text}</Text>
+        <Text style={[styles.linkText, styles.hiddenLinkText]}>{text}</Text>
       </View>
     ),
     [links, styles]
@@ -86,6 +136,7 @@ export const Hyperlink = (props: HyperlinkProps) => {
     <>
       <View
         pointerEvents={allowPointerEventsToPassThrough ? 'none' : undefined}
+        ref={linkContainerRef}
       >
         <Autolink
           onPress={handlePress}
@@ -93,24 +144,33 @@ export const Hyperlink = (props: HyperlinkProps) => {
           renderLink={allowPointerEventsToPassThrough ? renderLink : undefined}
           email
           url
+          style={[styles.root, style, stylesProp?.root]}
           {...other}
         />
       </View>
 
       <View style={styles.linksContainer}>
-        {Object.values(links).map(({ layout, text, match }) => (
-          <Text
-            key={`${layout.x} ${layout.y}`}
-            style={[
-              styles.linkText,
-              styles.link,
-              { position: 'absolute', top: layout.y, left: layout.x }
-            ]}
-            onPress={() => handlePress(match.getAnchorHref())}
-          >
-            {text}
-          </Text>
-        ))}
+        {Object.entries(links).map(([index, { text, match }]) => {
+          const linkLayout = linkLayouts[index]
+
+          return linkLayout && linkContainerLayout ? (
+            <Text
+              key={`${linkLayout.x} ${linkLayout.y} ${index}`}
+              style={[
+                styles.linkText,
+                styles.link,
+                {
+                  position: 'absolute',
+                  top: linkLayout.y - linkContainerLayout.y,
+                  left: linkLayout.x - linkContainerLayout.x
+                }
+              ]}
+              onPress={() => handlePress(match.getAnchorHref())}
+            >
+              {text}
+            </Text>
+          ) : null
+        })}
       </View>
     </>
   )
