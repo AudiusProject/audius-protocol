bb4f5627c182686667ae57105ec3e3a8b2d759d7
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-05-11 07:19:38 +0000

    Update query for speed

--- discovery-provider/src/queries/get_tips.py
+++ discovery-provider/src/queries/get_tips.py
@@ -22,6 +22,7 @@ class GetTipsArgs(TypedDict):
     current_user_follows: Optional[str]
     unique_by: Optional[str]
     min_slot: int
+    transaction_signatures: List[int]
 
 
 class TipResult(TypedDict):
@@ -31,59 +32,89 @@ class TipResult(TypedDict):
     slot: int
     created_at: datetime
     followee_supporters: List[str]
-    total_supporter_count: int
+    transaction_signature: str
+
+
+# Example of query with inputs:
+# limit=100, offset=0, user_id=1, current_user_follows=sender_or_receiver
+# ---------------------------------------------------------
+# WITH followees AS
+# (
+#     SELECT
+#         follows.followee_user_id AS followee_user_id
+#     FROM
+#         follows
+#     WHERE
+#         follows.is_current = true
+#         AND follows.is_delete = false
+#         AND follows.follower_user_id = 1
+# )
+# ,
+# tips AS
+# (
+#     SELECT
+#         user_tips.signature AS signature,
+#         user_tips.slot AS slot,
+#         user_tips.sender_user_id AS sender_user_id,
+#         user_tips.receiver_user_id AS receiver_user_id,
+#         user_tips.amount AS amount,
+#         user_tips.created_at AS created_at
+#     FROM
+#         user_tips
+#         LEFT OUTER JOIN
+#             followees AS followees_for_receiver
+#             ON user_tips.receiver_user_id = followees_for_receiver.followee_user_id
+#         LEFT OUTER JOIN
+#             followees AS followees_for_sender
+#             ON user_tips.sender_user_id = followees_for_sender.followee_user_id
+#     WHERE
+#         followees_for_receiver.followee_user_id IS NOT NULL
+#         OR followees_for_sender.followee_user_id IS NOT NULL
+#     ORDER BY
+#         user_tips.slot DESC LIMIT 100 OFFSET 0
+# )
+# ,
+# followee_tippers AS
+# (
+#     SELECT
+#         aggregate_user_tips.sender_user_id AS sender_user_id,
+#         aggregate_user_tips.receiver_user_id AS receiver_user_id,
+#         followees_for_aggregate.followee_user_id AS followee_user_id
+#     FROM
+#         followees AS followees_for_aggregate
+#         LEFT OUTER JOIN
+#             aggregate_user_tips
+#             ON aggregate_user_tips.sender_user_id = followees_for_aggregate.followee_user_id
+# )
+# SELECT
+#     tips.signature AS tips_signature,
+#     tips.slot AS tips_slot,
+#     tips.sender_user_id AS tips_sender_user_id,
+#     tips.receiver_user_id AS tips_receiver_user_id,
+#     tips.amount AS tips_amount,
+#     tips.created_at AS tips_created_at,
+#     array_agg(followee_tippers.sender_user_id) AS array_agg_1
+# FROM
+#     tips
+#     LEFT OUTER JOIN
+#         followee_tippers
+#         ON followee_tippers.receiver_user_id = tips.receiver_user_id
+# GROUP BY
+#     tips.signature,
+#     tips.slot,
+#     tips.sender_user_id,
+#     tips.receiver_user_id,
+#     tips.amount,
+#     tips.created_at
+# ORDER BY
+#     tips.slot DESC LIMIT 100 OFFSET 0;
+#
 
 
 def _get_tips(session: Session, args: GetTipsArgs):
-    query: Query = (
-        session.query(UserTip, func.count(AggregateUserTips.sender_user_id))
-        .join(
-            AggregateUserTips,
-            AggregateUserTips.receiver_user_id == UserTip.receiver_user_id,
-        )
-        .group_by(UserTip)
-    )
-    if "user_id" in args and args["user_id"] is not None:
-        followers_query = (
-            session.query(Follow.followee_user_id)
-            .filter(
-                Follow.is_current == True,
-                Follow.is_delete == False,
-                Follow.follower_user_id == args["user_id"],
-            )
-            .cte("followers")
-        )
-        FollowersForAgg = aliased(followers_query, name="follows_for_agg_tips")
-        query = query.add_columns(
-            func.array_agg(FollowersForAgg.c.followee_user_id)
-        ).outerjoin(
-            FollowersForAgg,
-            FollowersForAgg.c.followee_user_id == AggregateUserTips.sender_user_id,
-        )
-        if "current_user_follows" in args and args["current_user_follows"] is not None:
-            FollowSender = aliased(followers_query, name="follows_for_sender")
-            FollowReceiver = aliased(followers_query, name="follows_for_receiver")
-            filter_cond = []
-            if (
-                args["current_user_follows"] == "receiver"
-                or args["current_user_follows"] == "sender_or_receiver"
-            ):
-                query = query.outerjoin(
-                    FollowReceiver,
-                    UserTip.receiver_user_id == FollowReceiver.c.followee_user_id,
-                )
-                filter_cond.append(FollowReceiver.c.followee_user_id != None)
-            if (
-                args["current_user_follows"] == "sender"
-                or args["current_user_follows"] == "sender_or_receiver"
-            ):
-                query = query.outerjoin(
-                    FollowSender,
-                    UserTip.sender_user_id == FollowSender.c.followee_user_id,
-                )
-                filter_cond.append(FollowSender.c.followee_user_id != None)
-            query = query.filter(or_(*filter_cond))
-
+    query: Query = session.query(UserTip)
+    if "transaction_signatures" in args and len(args["transaction_signatures"]) > 0:
+        query = query.filter(UserTip.signature.in_(args["transaction_signatures"]))
     if (
         "receiver_min_followers" in args
         and args["receiver_min_followers"] is not None
@@ -114,8 +145,89 @@ def _get_tips(session: Session, args: GetTipsArgs):
     if "min_slot" in args and args["min_slot"] > 0:
         query = query.filter(UserTip.slot >= args["min_slot"])
 
+    if "user_id" in args and args["user_id"] is not None:
+        # We have to get the other users that this user follows for three potential uses:
+        # 1) To filter tips to recipients the user follows (if necessary)
+        # 2) To filter tips to senders the user follows (if necessary)
+        # 3) To get the followees of the current user that have also tipped the receiver
+        followees_query = (
+            session.query(Follow.followee_user_id)
+            .filter(
+                Follow.is_current == True,
+                Follow.is_delete == False,
+                Follow.follower_user_id == args["user_id"],
+            )
+            .cte("followees")
+        )
+        # First, filter the senders/receivers as necessary
+        if "current_user_follows" in args and args["current_user_follows"] is not None:
+            FolloweesSender = aliased(followees_query, name="followees_for_sender")
+            FolloweesReceiver = aliased(followees_query, name="followees_for_receiver")
+            filter_cond = []
+            if (
+                args["current_user_follows"] == "receiver"
+                or args["current_user_follows"] == "sender_or_receiver"
+            ):
+                query = query.outerjoin(
+                    FolloweesReceiver,
+                    UserTip.receiver_user_id == FolloweesReceiver.c.followee_user_id,
+                )
+                filter_cond.append(FolloweesReceiver.c.followee_user_id != None)
+            if (
+                args["current_user_follows"] == "sender"
+                or args["current_user_follows"] == "sender_or_receiver"
+            ):
+                query = query.outerjoin(
+                    FolloweesSender,
+                    UserTip.sender_user_id == FolloweesSender.c.followee_user_id,
+                )
+                filter_cond.append(FolloweesSender.c.followee_user_id != None)
+            query = query.filter(or_(*filter_cond))
+
+            # Order and paginate before adding follower filters/aggregates
+            query = query.order_by(UserTip.slot.desc())
+            query = paginate_query(query)
+
+            # Get the tips for the user as a subquery
+            # because now we need to get the other users that tipped that receiver
+            # and joining on this already paginated/limited result will be much faster
+            Tips = query.cte("tips")
+            FolloweesAggregate = aliased(
+                followees_query, name="followees_for_aggregate"
+            )
+
+            # Get all of the followees joined on their aggregate user tips first
+            # rather than joining each on the tips separately to help with speed
+            FolloweeTippers = (
+                session.query(
+                    AggregateUserTips.sender_user_id,
+                    AggregateUserTips.receiver_user_id,
+                    FolloweesAggregate.c.followee_user_id,
+                )
+                .select_from(FolloweesAggregate)
+                .outerjoin(
+                    AggregateUserTips,
+                    AggregateUserTips.sender_user_id
+                    == FolloweesAggregate.c.followee_user_id,
+                )
+                .cte("followee_tippers")
+            )
+
+            # Now we have the tips listed multiple times, one for each followee sender.
+            # So group by the tip and aggregate up the followee sender IDs into a list
+            query = (
+                session.query(UserTip, func.array_agg(FolloweeTippers.c.sender_user_id))
+                .select_entity_from(Tips)
+                .outerjoin(
+                    FolloweeTippers,
+                    FolloweeTippers.c.receiver_user_id == Tips.c.receiver_user_id,
+                )
+                .group_by(Tips)
+            )
+
     query = query.order_by(UserTip.slot.desc())
     query = paginate_query(query)
+
     tips_results: List[UserTip] = query.all()
     return tips_results
 
@@ -123,9 +235,18 @@ def _get_tips(session: Session, args: GetTipsArgs):
 def get_tips(args: GetTipsArgs) -> List[TipResult]:
     db = get_db_read_replica()
     with db.scoped_session() as session:
-        tips_results: Union[
-            List[Tuple[UserTip, int, List[str]]], List[Tuple[UserTip, int]]
-        ] = _get_tips(session, args)
+        results: Union[List[Tuple[UserTip, List[str]]], List[UserTip]] = _get_tips(
+            session, args
+        )
+        tips_results: List[Tuple[UserTip, List[str]]] = []
+        # Wrap in tuple for consistency
+        if isinstance(results[0], UserTip):
+            tips_results = [(cast(UserTip, tip), []) for tip in results]
+        else:
+            # MyPy doesn't seem smart enough to figure this out, help it with a cast
+            tips_results = cast(List[Tuple[UserTip, List[str]]], results)
+
+        # Collect user IDs and fetch users
         user_ids = set()
         for result in tips_results:
             user_ids.add(result[0].sender_user_id)
@@ -144,18 +265,15 @@ def get_tips(args: GetTipsArgs) -> List[TipResult]:
                 "amount": result[0].amount,
                 "sender": users_map[result[0].sender_user_id],
                 "receiver": users_map[result[0].receiver_user_id],
-                "total_supporter_count": result[1],
                 "followee_supporters": list(
                     filter(
                         lambda id: id is not None,
-                        # Have to cast until https://github.com/python/mypy/issues/1178 is fixed
-                        cast(Tuple[UserTip, int, List[str]], result)[2],
+                        result[1],
                     )
-                )
-                if len(result) > 2
-                else [],
+                ),
                 "slot": result[0].slot,
                 "created_at": result[0].created_at,
+                "transaction_signature": result[0].signature,
             }
             for result in tips_results
         ]
