264b8eab631b219b445945f688a216c3df12f655
Michael Piazza <michael.piazza.mp@gmail.com>
2022-05-11 19:06:20 +0000

    Discovery: change column names

---
+++ discovery-provider/alembic/versions/0d2067242dd5_change_reaction_column_names.py
@@ -0,0 +1,39 @@
+"""Change reaction column names
+
+Revision ID: 0d2067242dd5
+Revises: 35198266d709
+Create Date: 2022-05-09 22:03:16.838837
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0d2067242dd5"
+down_revision = "35198266d709"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column("reactions", "entity_id", new_column_name="reacted_to")
+    op.alter_column("reactions", "entity_type", new_column_name="reaction_type")
+    op.alter_column("reactions", "reaction", new_column_name="reaction_value")
+    op.create_index(
+        op.f("ix_reactions_reacted_to_reaction_type"),
+        "reactions",
+        ["reacted_to", "reaction_type"],
+        unique=False,
+        info={"if_not_exists": True},
+    )
+
+
+def downgrade():
+    op.alter_column("reactions", "reacted_to", new_column_name="entity_id")
+    op.alter_column("reactions", "reaction_type", new_column_name="entity_type")
+    op.alter_column("reactions", "reaction_value", new_column_name="reaction")
+    op.drop_index(
+        op.f("ix_reactions_reacted_to_reaction_type"),
+        table_name="reaction",
+        info={"if_not_exists": True},
+    )
--- discovery-provider/src/models/reaction.py
+++ discovery-provider/src/models/reaction.py
@@ -8,9 +8,9 @@ class Reaction(Base, RepresentableMixin):
 
     id = Column(Integer, nullable=False, primary_key=True)
     slot = Column(Integer, nullable=False)
-    reaction = Column(Integer, nullable=False)
+    reaction_value = Column(Integer, nullable=False)
     sender_wallet = Column(String, nullable=False)
-    entity_type = Column(String, nullable=False)
-    entity_id = Column(String, nullable=False)
+    reaction_type = Column(String, nullable=False)
+    reacted_to = Column(String, nullable=False)
     timestamp = Column(DateTime, nullable=False)
     tx_signature = Column(String, nullable=True)
