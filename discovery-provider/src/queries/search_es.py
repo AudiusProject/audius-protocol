51a813cd1fcd4b9b69f402c3076a03991de199f5
Steve Perkins <steve@audius.co>
2022-05-13 19:39:05 +0000

    es: multisearch tracks, users, playlists

--- discovery-provider/src/queries/search_es.py
+++ discovery-provider/src/queries/search_es.py
@@ -1,4 +1,6 @@
-from src.utils.elasticdsl import esclient, pluck_hits
+from typing import Any
+
+from src.utils.elasticdsl import ES_PLAYLISTS, ES_TRACKS, ES_USERS, esclient, pluck_hits
 
 
 def search_es_full(args: dict):
@@ -8,38 +10,160 @@ def search_es_full(args: dict):
     # taken from search_queries.py
     search_str = args.get("query")
     current_user_id = args.get("current_user_id")
+    limit = args.get("limit")
+    offset = args.get("offset")
 
-    dsl = {
-        "function_score": {
-            "query": {
-                "bool": {
-                    "must": [
-                        {
-                            "multi_match": {
-                                "query": search_str,
-                                "fields": [
-                                    "title^2",
-                                    "artist.name",
-                                    "artist.handle",
+    mdsl: Any = []
+
+    # tracks
+    mdsl.extend(
+        [
+            {"index": ES_TRACKS},
+            {
+                "size": limit,
+                "from": offset,
+                "query": {
+                    "function_score": {
+                        "query": {
+                            "bool": {
+                                "must": [
+                                    {
+                                        "multi_match": {
+                                            "query": search_str,
+                                            "fields": [
+                                                "title^2",
+                                                "artist.name",
+                                                "artist.handle",
+                                            ],
+                                            "type": "cross_fields",
+                                        }
+                                    },
+                                    {"term": {"is_unlisted": {"value": False}}},
                                 ],
-                                "type": "cross_fields",
+                                "should": [
+                                    {"term": {"saved_by": {"value": current_user_id}}},
+                                    {
+                                        "term": {
+                                            "reposted_by": {"value": current_user_id}
+                                        }
+                                    },
+                                ],
                             }
                         },
-                        {"term": {"is_unlisted": {"value": False}}},
-                    ],
-                    "should": [
-                        {"term": {"saved_by": {"value": current_user_id}}},
-                        {"term": {"reposted_by": {"value": current_user_id}}},
-                    ],
-                }
+                        "field_value_factor": {
+                            "field": "repost_count",
+                            "factor": 1.2,
+                            "modifier": "log1p",
+                        },
+                    }
+                },
             },
-            "field_value_factor": {
-                "field": "repost_count",
-                "factor": 1.2,
-                "modifier": "log1p",
-            },
-        }
-    }
+        ]
+    )
 
-    found = esclient.search(index="tracks", query=dsl)
-    return {"tracks": pluck_hits(found)}
+    # users
+    mdsl.extend(
+        [
+            {"index": ES_USERS},
+            {
+                "size": limit,
+                "from": offset,
+                "query": {
+                    "function_score": {
+                        "query": {
+                            "bool": {
+                                "must": [
+                                    {
+                                        "multi_match": {
+                                            "query": search_str,
+                                            "fields": [
+                                                "name",
+                                                "handle",
+                                            ],
+                                            "type": "cross_fields",
+                                        }
+                                    },
+                                    {"term": {"is_deactivated": {"value": False}}},
+                                ],
+                                "should": [
+                                    {"term": {"is_verified": {"value": True}}},
+                                    # promote users that current user follows
+                                    # TODO: need to index user_id for this to work :(
+                                    # _id would work except that following_ids is numbers and _id is string
+                                    # {
+                                    #     "terms": {
+                                    #         "user_id": {
+                                    #             "index": ES_USERS,
+                                    #             "id": current_user_id,
+                                    #             "path": "following_ids",
+                                    #         },
+                                    #     }
+                                    # },
+                                ],
+                            }
+                        },
+                        "field_value_factor": {
+                            "field": "follower_count",
+                            "factor": 1.2,
+                            "modifier": "log1p",
+                        },
+                    }
+                },
+            },
+        ]
+    )
+
+    mdsl.extend(
+        [
+            {"index": ES_PLAYLISTS},
+            {
+                "size": limit,
+                "from": offset,
+                "query": {
+                    "function_score": {
+                        "query": {
+                            "bool": {
+                                "must": [
+                                    {
+                                        "multi_match": {
+                                            "query": search_str,
+                                            "fields": [
+                                                "playlist_name",
+                                                "description",
+                                            ],
+                                            "type": "cross_fields",
+                                        }
+                                    },
+                                    {"term": {"is_private": {"value": False}}},
+                                ],
+                                "should": [
+                                    {"term": {"saved_by": {"value": current_user_id}}},
+                                    {
+                                        "term": {
+                                            "reposted_by": {"value": current_user_id}
+                                        }
+                                    },
+                                ],
+                            }
+                        },
+                        "field_value_factor": {
+                            "field": "repost_count",
+                            "factor": 1.2,
+                            "modifier": "log1p",
+                        },
+                    }
+                },
+            },
+        ]
+    )
+
+    mfound = esclient.msearch(searches=mdsl)
+    found_tracks = mfound["responses"].pop(0)
+    found_users = mfound["responses"].pop(0)
+    found_playlists = mfound["responses"].pop(0)
+
+    return {
+        "tracks": pluck_hits(found_tracks),
+        "users": pluck_hits(found_users),
+        "playlists": pluck_hits(found_playlists),
+    }
