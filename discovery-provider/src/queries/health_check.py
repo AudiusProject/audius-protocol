6808c927861f067a3a98c2df5b888a9fa654a8fc
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-05-13 14:38:24 -0400

    Make celery_tasks_check response more human readable (#3076)
    * First pass
    
    * A cleanup
    
    * Cleanup
    
    * Add new human readable timestamp property
    
    * Lint
    
    * Import sort order
    
    * Cleanup

--- discovery-provider/src/queries/health_check.py
+++ discovery-provider/src/queries/health_check.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask import Blueprint, request
 from src.api_helpers import success_response
 from src.queries.get_alembic_version import get_alembic_version
-from src.queries.get_celery_tasks import get_celery_tasks
+from src.queries.get_celery_tasks import convert_epoch_to_datetime, get_celery_tasks
 from src.queries.get_db_seed_restore_status import get_db_seed_restore_status
 from src.queries.get_health import get_health, get_latest_ipld_indexed_block
 from src.queries.get_latest_play import get_latest_play
@@ -145,6 +145,11 @@ def es_health():
 @bp.route("/celery_tasks_check", methods=["GET"])
 def celery_tasks_check():
     tasks = get_celery_tasks()
+
+    for task in tasks.get("celery_tasks", []):
+        task["started_at_est_timestamp"] = convert_epoch_to_datetime(
+            task.get("started_at")
+        )
     return success_response(tasks, sign_response=False)
 
 
