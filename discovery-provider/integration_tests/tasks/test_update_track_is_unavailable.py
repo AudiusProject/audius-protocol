44e0beca488399c792a534b262c9a32e5859825a
vicky <vicky@audius.co>
2022-05-19 21:55:03 +0000

    fix fetch test

--- discovery-provider/integration_tests/tasks/test_update_track_is_unavailable.py
+++ discovery-provider/integration_tests/tasks/test_update_track_is_unavailable.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 # Mock out request response
-def _mock_response(self, json_data, status=200, raise_for_status=None):  # Error type
+def _mock_response(json_data, status=200, raise_for_status=None):
     mock_resp = mock.Mock()
 
     mock_resp.json = mock.Mock(return_value=json_data)
@@ -20,21 +20,23 @@ def _mock_response(self, json_data, status=200, raise_for_status=None):  # Error
     return mock_resp
 
 
-@mock.patch("src.tasks.update_track_is_available.requests.get")
-def test_fetch(self, mock_get):
-    """ "
+@mock.patch("src.tasks.update_track_is_available.requests")
+def test_fetch(mock_requests, app):
+    """
     Test fetching unavailable track ids from Content Node
     mock_get: reference to the mock requests.get
+    look at test_index_tracks.py for ref
     """
-    track_ids = ["1", "2", "3", "4", "5", "6", "7"]
+    track_ids = [1, 2, 3, 4, 5, 6, 7]
     mock_return = {
         "data": {"values": track_ids},
         "signer": "signer",
         "timestamp": "2022-05-19T19:50:56.630Z",
         "signature": "signature",
     }
-    # mock_resp = self._mock_response({'data': {'values': ['1', '2', '3', '4', '5', '6', '7']}, 'signer': '0x22d491bde2303f2f43325b2108d26f1eaba1e32b', 'timestamp': '2022-05-19T19:50:56.630Z', 'signature': '0x01c42b12498f42d0daddf2e3421592513a1c7243aaf9d4735474f3164c38f9ce6703e33bf7114cac1caee2894dcce61d53b23f5e4b7a5fc7290b73228befabab1c'})
-    mock_get.return_value = self._mock_response(mock_return)
+
+    mock_requests.get.return_value = _mock_response(mock_return, mock_return)
 
     fetch_response = fetch_unavailable_track_ids("http://content_node.com")
-    self.assertEqual(fetch_response, track_ids)
+
+    assert fetch_response == track_ids
