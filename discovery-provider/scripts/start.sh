2609fa0b1b4c78b493bd118b7b9b413ef191d15c
Joaquin Casares <joaquin@audius.co>
2022-05-19 05:45:00 +0000

    separate one process per container

--- discovery-provider/scripts/start.sh
+++ discovery-provider/scripts/start.sh
@@ -46,20 +46,28 @@ if [[ "$audius_discprov_dev_mode" == "true" ]]; then
         echo "Finished running migrations"
     fi
 
-    # filter tail to server/worker/beat logs with
-    # docker exec -it <container> tail -f /var/log/discprov-server.log
-    # docker exec -it <container> tail -f /var/log/discprov-worker.log
-    # docker exec -it <container> tail -f /var/log/discprov-beat.log
-    ./scripts/dev-server.sh
+    if [[ "$audius_service" == "server" ]]; then
+        ./scripts/dev-server.sh
+    fi
     if [[ "$audius_no_workers" != "true" ]] && [[ "$audius_no_workers" != "1" ]]; then
-        watchmedo auto-restart --directory ./ --pattern=*.py --recursive -- celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
-        celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t beat) &
+        if [[ "$audius_service" == "worker" ]]; then
+            watchmedo auto-restart --directory ./ --pattern=*.py --recursive -- celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel
+        fi
+        if [[ "$audius_service" == "beat" ]]; then
+            celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel
+        fi
     fi
 else
-    ./scripts/prod-server.sh
+    if [[ "$audius_service" == "server" ]]; then
+        ./scripts/prod-server.sh
+    fi
     if [[ "$audius_no_workers" != "true" ]] && [[ "$audius_no_workers" != "1" ]]; then
-        celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
-        celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t beat) &
+        if [[ "$audius_service" == "worker" ]]; then
+            celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel
+        fi
+        if [[ "$audius_service" == "beat" ]]; then
+            celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel
+        fi
     fi
 fi
 
