4ec475aa42c7089284c852a83424f746a9ededdf
Joaquin Casares <joaquin@audius.co>
2022-05-20 06:55:32 +0000

    set tags correctly

---
+++ logging/logspout/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+audius_loggly_tags=${audius_loggly_tags},logspout
+
+IFS=","
+for tag in ${audius_loggly_tags}
+do
+   tags="${tags} tag=\"${tag}\""
+done
+
+export SYSLOG_STRUCTURED_DATA="$(echo ${audius_loggly_token} | base64 -d)@41058 ${tags}"
+echo SYSLOG_STRUCTURED_DATA=${SYSLOG_STRUCTURED_DATA}
+
+/bin/logspout multiline+syslog+tcp://logs-01.loggly.com:514
