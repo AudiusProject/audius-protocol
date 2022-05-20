ae09609b064070cf3e1e0739a4db99989d11b437
vicky <vicky@audius.co>
2022-05-20 19:21:02 +0000

    working replica set fetch

--- discovery-provider/integration_tests/tasks/test_update_track_is_available.py
+++ discovery-provider/integration_tests/tasks/test_update_track_is_available.py
@@ -1,7 +1,13 @@
 import logging
 from unittest import mock
 
-from src.tasks.update_track_is_available import fetch_unavailable_track_ids
+from integration_tests.utils import populate_mock_db
+from src.models import Track, User
+from src.tasks.update_track_is_available import (
+    fetch_unavailable_track_ids,
+    query_replica_set_by_track_id,
+)
+from src.utils.db_session import get_db
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +27,7 @@ def _mock_response(json_data, status=200, raise_for_status=None):
 
 
 @mock.patch("src.tasks.update_track_is_available.requests")
-def test_fetch(mock_requests, app):
+def test_fetch_unavailable_track_ids(mock_requests, app):
     """
     Test fetching unavailable track ids from Content Node
     mock_get: reference to the mock requests.get
@@ -40,3 +46,71 @@ def test_fetch(mock_requests, app):
     fetch_response = fetch_unavailable_track_ids("http://content_node.com")
 
     assert fetch_response == track_ids
+
+
+def test_query_replica_set_by_track_id(app):
+    """Test that the query returns a mapping of track id, user id, and replica set"""
+
+    with app.app_context():
+        db = get_db()
+
+    test_entities = {
+        "tracks": [
+            {"track_id": 1, "owner_id": 1, "is_current": True},
+            {"track_id": 2, "owner_id": 1, "is_current": True},
+            {"track_id": 3, "owner_id": 2, "is_current": True},
+            {"track_id": 4, "owner_id": 3, "is_current": True},
+            {"track_id": 5, "owner_id": 3, "is_current": True},
+            {"track_id": 6, "owner_id": 3, "is_current": True},
+            {"track_id": 7, "owner_id": 3, "is_current": True},
+            # Data that this query should not pick up because track ids are not queried
+            {"track_id": 8, "owner_id": 3, "is_current": True},
+            {"track_id": 9, "owner_id": 3, "is_current": True},
+            {"track_id": 10, "owner_id": 3, "is_current": True}
+        ],
+        "users": [
+            {"user_id": 1, "primary_id": 7, "secondary_ids": [9, 13], "is_current": True},
+            {"user_id": 2, "primary_id": 11, "secondary_ids": [12, 10], "is_current": True},
+            {"user_id": 3, "primary_id": 11, "secondary_ids": [13, 10], "is_current": True},
+            # Data that this query should not pick up because data is not recent
+            {"user_id": 1, "primary_id": 6, "secondary_ids": [9, 13], "is_current": False},
+            {"user_id": 1, "primary_id": 4, "secondary_ids": [9, 13], "is_current": False},
+            {"user_id": 3, "primary_id": 7, "secondary_ids": [9, 1], "is_current": False},
+        ]
+    }
+
+    populate_mock_db(db, test_entities)
+
+    print_dummy_tracks_and_users(db)
+
+    expected_query_results = [(1, 1, 7, [9, 13]), (2, 1, 7, [9, 13]), (3, 2, 11, [12, 10]), (4, 3, 11, [13, 10]), (5, 3, 11, [13, 10]), (6, 3, 11, [13, 10]), (7, 3, 11, [13, 10])]
+    track_ids = [1, 2, 3, 4, 5, 6, 7]
+    sorted_actual_results = _sort_query_replica_set_by_track_id(
+         query_replica_set_by_track_id(db, track_ids)
+    )
+
+    assert len(sorted_actual_results) == len(track_ids)
+    assert sorted_actual_results == expected_query_results
+
+
+def print_dummy_tracks_and_users(db):
+    with db.scoped_session() as session:
+        tracks = session.query(
+            Track.track_id, Track.owner_id, Track.is_current
+        ).all()
+
+        print('tracks')
+        print(tracks)
+
+        users = session.query(
+            User.user_id, User.primary_id, User.secondary_ids, User.is_current 
+        ).all()
+
+        print('users')
+        print(users)
+
+
+def _sort_query_replica_set_by_track_id(list):
+    """sort by the track id in ascending order"""
+    list.sort(key=lambda entry: entry[0])
+    return list
--- discovery-provider/src/tasks/update_track_is_available.py
+++ discovery-provider/src/tasks/update_track_is_available.py
@@ -3,13 +3,14 @@ import logging
 import requests
 
 # from src.tasks.aggregates import init_task_and_acquire_lock
+from src.models import Track, User
 from src.tasks.celery_app import celery
 from src.utils.eth_contracts_helpers import fetch_all_registered_content_node_info
 
 logger = logging.getLogger(__name__)
 
 
-ALL_DELISTED_TRACKS_REDIS_KEY = "delisted_tracks_all"
+ALL_UNAVAILABLE_TRACKS_REDIS_KEY = "unavailable_tracks_all"
 
 
 def _update_track_is_available():
@@ -22,45 +23,54 @@ def fetch_unavailable_track_ids(node):
     return unavailable_track_ids
 
 
-def get_replica_set(track_id):
-    # select
-    #     tracks.track_id, users.user_id, users.primary_id, users.secondary_ids
-    # from
-    #     "tracks"
-    # left join "users" on tracks.owner_id = users.user_id
-    # where
-    #     users.is_current = true  and
-    #     tracks.track_id in (888,2552,353,467,53)
-    # limit 100;
-    pass
+def query_replica_set_by_track_id(db, track_ids):
+    query_results = None
+    with db.scoped_session() as session:
+        query_results = session.query(
+            Track.track_id, User.user_id, User.primary_id, User.secondary_ids
+        ).join(
+            User,
+            Track.owner_id == User.user_id,
+            isouter=True  # left join
+        ).filter(
+            User.is_current == True,
+            Track.is_current == True,
+            Track.track_id.in_(track_ids)
+        ).all()
+
+    return query_results
 
 
-def get_delisted_tracks_per_spID_redis_key(spID):
-    return f"delisted_tracks_{spID}"
+def get_unavailable_tracks_redis_key(spID):
+    '''Returns the key used to store the unavailable tracks on a sp'''
+    return f"unavailable_tracks_{spID}"
+
+# TODO: what happens when a worker fails? wrap in try/catch? not necessary?
 
 
 # ####### CELERY TASKS ####### #
 @celery.task(name="update_track_is_available", bind=True)
 def update_track_is_available(self):
-    # db = update_track_is_available.db
+    db = update_track_is_available.db
     redis = update_track_is_available.redis
 
     content_nodes = fetch_all_registered_content_node_info()
 
     for node in content_nodes:
-        # Keep mapping of spId to the unavailable tracks
+        # Keep mapping of spId to set of unavailable tracks
         unavailable_track_ids = fetch_unavailable_track_ids(node.endpoint)
-        key = get_delisted_tracks_per_spID_redis_key(node.spID)
-        redis.sadd(key, unavailable_track_ids)
+        spID_unavailable_tracks_key = get_unavailable_tracks_redis_key(node.spID)
+        redis.sadd(spID_unavailable_tracks_key, unavailable_track_ids)
 
         # Also keep aggregate list of unavailable tracks
-        redis.sadd(ALL_DELISTED_TRACKS_REDIS_KEY, unavailable_track_ids)
+        redis.sadd(ALL_UNAVAILABLE_TRACKS_REDIS_KEY, unavailable_track_ids)
 
-    # Check track availability on all delisted tracks
-    all_delisted_track_ids = redis.smembers(ALL_DELISTED_TRACKS_REDIS_KEY)
-    for track_id in all_delisted_track_ids:
-        pass
-        # replica_set = get_replica_set(track_id)
+    # Check track availability on all unavailable tracks
+    all_unavailable_track_ids = redis.smembers(ALL_UNAVAILABLE_TRACKS_REDIS_KEY)
+    for track_id in all_unavailable_track_ids:
+        replica_set = query_replica_set_by_track_id(db, [track_id])
+
+        # pass
         # try:
         #     init_task_and_acquire_lock(
         #         logger, db, redis, AGGREGATE_PLAYS_TABLE_NAME, _update_track_is_available
