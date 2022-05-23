7518460f12e8386d9a9d9c71831ed83bf9eaca1c
Joaquin Casares <joaquin@audius.co>
2022-05-23 04:58:18 +0000

    add hostname, if it exists

--- logspout/start.sh
+++ logspout/start.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 
-audius_loggly_tags=${audius_loggly_tags},logspout
+tag_csv=logspout
+if [[ "${audius_loggly_tags}" ]]; then
+   tag_csv=${tag_csv},${audius_loggly_tags}
+fi
+
+hostname=$(echo ${audius_discprov_url} | sed -e 's/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/')
+if [[ "${hostname}" ]]; then
+   tag_csv=${tag_csv},${hostname}
+fi
 
 IFS=","
-for tag in ${audius_loggly_tags}
+for tag in ${tag_csv}
 do
    tags="${tags} tag=\"${tag}\""
 done
