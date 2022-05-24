f3af4f01fc3f4350012d42307f6b62b88db3083e
Dylan Jeffers <dylan@audius.co>
2022-05-23 18:35:01 -0700

    [PAY-222] Add native notification core components (#1338)

---
+++ apps/audius-client/packages/mobile/src/screens/notifications-screen/Notification/NotificationHeader.tsx
@@ -0,0 +1,45 @@
+import { ComponentType, ReactNode } from 'react'
+
+import { View } from 'react-native'
+import { SvgProps } from 'react-native-svg'
+
+import { makeStyles } from 'app/styles'
+import { spacing } from 'app/styles/spacing'
+
+const useStyles = makeStyles(({ palette }) => ({
+  root: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: palette.neutralLight8,
+    borderBottomWidth: 1,
+    paddingBottom: spacing(4),
+    marginBottom: spacing(4)
+  },
+  iconColor: {
+    color: palette.secondary
+  },
+  icon: {
+    marginRight: spacing(2)
+  }
+}))
+
+type NotificationHeaderProps = {
+  icon: ComponentType<SvgProps>
+  children: ReactNode
+}
+
+export const NotificationHeader = (props: NotificationHeaderProps) => {
+  const styles = useStyles()
+  const { icon: Icon, children } = props
+  return (
+    <View style={styles.root}>
+      <Icon
+        fill={styles.iconColor.color}
+        height={30}
+        width={30}
+        style={styles.icon}
+      />
+      {children}
+    </View>
+  )
+}
---
+++ apps/audius-client/packages/mobile/src/screens/notifications-screen/Notification/UserNameLink.tsx
@@ -0,0 +1,39 @@
+import { useCallback, useContext } from 'react'
+
+import { User as UserType } from 'audius-client/src/common/models/User'
+import { NOTIFICATION_PAGE } from 'audius-client/src/utils/route'
+import { useDispatch } from 'react-redux'
+
+import { Text } from 'app/components/core'
+import { useNavigation } from 'app/hooks/useNavigation'
+import { NotificationsDrawerNavigationContext } from 'app/screens/notifications-screen/NotificationsDrawerNavigationContext'
+import { close } from 'app/store/notifications/actions'
+import { getUserRoute } from 'app/utils/routes'
+
+type UserNameLinkProps = {
+  user: UserType
+}
+
+export const UserNameLink = (props: UserNameLinkProps) => {
+  const { user } = props
+  const dispatch = useDispatch()
+  const { drawerHelpers } = useContext(NotificationsDrawerNavigationContext)
+  const navigation = useNavigation({ customNativeNavigation: drawerHelpers })
+
+  const onPress = useCallback(() => {
+    navigation.navigate({
+      native: {
+        screen: 'Profile',
+        params: { handle: user.handle, fromNotifications: true }
+      },
+      web: { route: getUserRoute(user), fromPage: NOTIFICATION_PAGE }
+    })
+    dispatch(close())
+  }, [user, navigation, dispatch])
+
+  return (
+    <Text fontSize='large' weight='medium' color='secondary' onPress={onPress}>
+      {user.name}
+    </Text>
+  )
+}
