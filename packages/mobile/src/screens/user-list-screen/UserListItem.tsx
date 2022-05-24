db67c2b90fb6de837ca9cca975f4495304d58c60
Dylan Jeffers <dylan@audius.co>
2022-05-24 13:51:37 -0700

    [PAY-203] Redesign native user list (#1346)

---
+++ packages/mobile/src/screens/user-list-screen/UserListItem.tsx
@@ -0,0 +1,138 @@
+import { useCallback } from 'react'
+
+import { FollowSource } from 'audius-client/src/common/models/Analytics'
+import { User } from 'audius-client/src/common/models/User'
+import { getUserId } from 'audius-client/src/common/store/account/selectors'
+import { Pressable, View, Animated } from 'react-native'
+
+import IconUser from 'app/assets/images/iconUser.svg'
+import { Text } from 'app/components/core'
+import {
+  FollowButton,
+  FollowsYouChip,
+  ProfilePicture
+} from 'app/components/user'
+import UserBadges from 'app/components/user-badges'
+import { useNavigation } from 'app/hooks/useNavigation'
+import { useColorAnimation } from 'app/hooks/usePressColorAnimation'
+import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
+import { makeStyles } from 'app/styles'
+import { formatCount } from 'app/utils/format'
+import { useThemeColors } from 'app/utils/theme'
+
+const messages = {
+  followers: (followerCount: number) =>
+    followerCount === 1 ? 'Follower' : 'Followers'
+}
+
+const useStyles = makeStyles(({ spacing, palette }) => ({
+  root: {
+    padding: spacing(4)
+  },
+  infoRoot: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: spacing(2)
+  },
+  photo: {
+    height: 72,
+    width: 72
+  },
+  userInfo: {
+    marginLeft: spacing(1),
+    flex: 1
+  },
+  userStats: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  followerStats: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  userIcon: {
+    color: palette.neutralLight4
+  },
+  displayName: {
+    marginBottom: spacing(1)
+  },
+  handle: {
+    marginBottom: spacing(2)
+  }
+}))
+
+type UserListItemProps = {
+  user: User
+}
+
+export const UserListItem = (props: UserListItemProps) => {
+  const { user } = props
+  const {
+    user_id,
+    handle,
+    name,
+    follower_count,
+    does_follow_current_user
+  } = user
+  const currentUserId = useSelectorWeb(getUserId)
+  const styles = useStyles()
+  const navigation = useNavigation()
+  const { white, neutralLight10 } = useThemeColors()
+  const { color, handlePressIn, handlePressOut } = useColorAnimation(
+    white,
+    neutralLight10
+  )
+
+  const handlePress = useCallback(() => {
+    navigation.push({
+      native: { screen: 'Profile', params: { handle } },
+      web: { route: handle }
+    })
+  }, [navigation, handle])
+
+  return (
+    <Animated.View style={{ backgroundColor: color }}>
+      <Pressable
+        style={styles.root}
+        onPress={handlePress}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+      >
+        <View style={styles.infoRoot}>
+          <ProfilePicture profile={user} style={styles.photo} />
+          <View style={styles.userInfo}>
+            <Text variant='h3' style={styles.displayName}>
+              {name}
+              <UserBadges user={user} badgeSize={10} hideName />
+            </Text>
+            <Text variant='body' style={styles.handle}>
+              @{handle}
+            </Text>
+            <View style={styles.userStats}>
+              <View style={styles.followerStats}>
+                <IconUser height={15} width={15} fill={styles.userIcon.color} />
+                <Text variant='body' color='neutralLight4'>
+                  {' '}
+                  <Text color='inherit' weight='bold'>
+                    {formatCount(follower_count)}{' '}
+                  </Text>
+                  {messages.followers(follower_count)}
+                </Text>
+              </View>
+              {does_follow_current_user ? <FollowsYouChip /> : null}
+            </View>
+          </View>
+        </View>
+        {currentUserId !== user_id ? (
+          <FollowButton
+            profile={user}
+            followSource={FollowSource.USER_LIST}
+            fullWidth
+            corners='pill'
+          />
+        ) : null}
+      </Pressable>
+    </Animated.View>
+  )
+}
