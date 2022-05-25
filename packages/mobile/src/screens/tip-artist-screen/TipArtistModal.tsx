c25f4ac594b77e363f61d818bfd396d14d2583fc
Dylan Jeffers <dylan@audius.co>
2022-05-25 13:21:17 -0700

    [PAY-247] Add TipArtistModal and basic navigation (#1359)

---
+++ packages/mobile/src/screens/tip-artist-screen/TipArtistModal.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { useAppScreenOptions } from '../app-screen/useAppScreenOptions'
+
+import { ConfirmTipScreen } from './ConfirmTipScreen'
+import { SendTipScreen } from './SendTipScreen'
+import { TipSentScreen } from './TipSentScreen'
+
+const Stack = createNativeStackNavigator()
+
+const screenOptionOverrides = { headerRight: () => null }
+
+export const TipArtistModal = () => {
+  const screenOptions = useAppScreenOptions(screenOptionOverrides)
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name='SendTip' component={SendTipScreen} />
+      <Stack.Screen name='ConfirmTip' component={ConfirmTipScreen} />
+      <Stack.Screen name='TipSent' component={TipSentScreen} />
+    </Stack.Navigator>
+  )
+}
