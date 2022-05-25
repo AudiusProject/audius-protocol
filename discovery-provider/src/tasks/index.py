ed0347f3e76a7fa564eb2b4eddaea22170382a46
vicky <vicky@audius.co>
2022-05-25 23:05:55 +0000

    revert indexing updates

--- discovery-provider/src/tasks/index.py
+++ discovery-provider/src/tasks/index.py
@@ -505,10 +505,6 @@ def process_state_changes(
             blacklisted_cids,
         ]
 
-        # Used for determining whether or not a track is available on the network
-        if tx_type == TRACK_FACTORY:
-            tx_processing_args.append(redis)
-
         (
             total_changes_for_tx_type,
             changed_entity_ids,
