06d83382ec44a5b4656d7784a1d7f18555128694
sliptype <sliptype@gmail.com>
2022-05-27 10:46:20 -0500

    Create legacy bundle for use in client

--- libs/rollup.config.js
+++ libs/rollup.config.js
@@ -73,6 +73,27 @@ const browserConfig = {
   external: external.filter((dep) => !internal.includes(dep))
 }
 
+const browserLegacyConfig = {
+  plugins: [
+    ignore(['web3', 'graceful-fs', 'node-localstorage']),
+    resolve({ extensions, preferBuiltins: true }),
+    commonjs({
+      extensions,
+      dynamicRequireTargets: [
+        'data-contracts/ABIs/*.json',
+        'eth-contracts/ABIs/*.json'
+      ]
+    }),
+    alias({
+      entries: [{ find: 'stream', replacement: 'stream-browserify' }]
+    }),
+    babel({ babelHelpers: 'bundled', extensions }),
+    json(),
+    typescript()
+  ],
+  external
+}
+
 const commonTypeConfig = {
   plugins: [dts()]
 }
@@ -82,7 +103,7 @@ export default [
    * SDK
    */
   {
-    input: 'src/sdk/index.ts',
+    input: 'src/index.js',
     output: [
       { file: pkg.main, format: 'cjs', exports: 'auto', sourcemap: true }
     ],
@@ -90,13 +111,14 @@ export default [
   },
 
   {
-    input: './src/sdk/index.ts',
+    input: './src/types.ts',
     output: [{ file: pkg.types, format: 'cjs' }],
     ...commonTypeConfig
   },
 
   /**
-   * Bundle for a browser environment
+   * SDK bundled for a browser environment (includes polyfills for node libraries)
+   * Does not include libs but does include polyfills
    */
   {
     input: 'src/sdk/index.ts',
@@ -104,6 +126,24 @@ export default [
       { file: pkg.browser, format: 'cjs', exports: 'auto', sourcemap: true }
     ],
     ...browserConfig
+  },
+
+  /**
+   * Legacy bundle for a browser environment
+   * Includes libs but does not include polyfills
+   */
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.legacy, format: 'cjs', exports: 'auto', sourcemap: true }
+    ],
+    ...browserLegacyConfig
+  },
+
+  {
+    input: './src/types.ts',
+    output: [{ file: pkg.legacyTypes, format: 'cjs' }],
+    ...commonTypeConfig
   },
 
   /**
