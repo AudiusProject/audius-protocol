93e0e17c44c09ae5e4b1fbc514ec0f3875e1b7d5
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-06-01 22:38:50 +0000

    Revert "This is how you set discoveryProviderWhitelist locally"
    This reverts commit 219cb9f59433e1106440ab46c785007896c0a4a8.

--- creator-node/compose/env/commonEnv.sh
+++ creator-node/compose/env/commonEnv.sh
@@ -6,4 +6,3 @@ export creatorNodeEndpoint=CREATOR_NODE_ENDPOINT
 export spOwnerWallet=SP_OWNER_WALLET
 export COMPOSE_HTTP_TIMEOUT=200
 export printSequelizeLogs=true
-export discoveryProviderWhitelist=http://dn1_web-server_1:5000
