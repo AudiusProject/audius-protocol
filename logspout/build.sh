3618c912a0855d3810167ba0b3e44e35dab3ee8e
Joaquin Casares <joaquin@audius.co>
2022-06-01 19:33:25 +0000

    add additional comment

--- logspout/build.sh
+++ logspout/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # https://github.com/gliderlabs/logspout/blob/818dd8260e52d2c148280d86170bdf5267b5c637/build.sh
+# due to: https://github.com/gliderlabs/logspout/blob/818dd8260e52d2c148280d86170bdf5267b5c637/Dockerfile#L9-L11
 
 set -e
 apk add --update go build-base git mercurial ca-certificates
