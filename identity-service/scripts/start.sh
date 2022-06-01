85b772cccaf1c52568e73ad65e5c522fbe1706b1
Joaquin Casares <joaquin@audius.co>
2022-06-01 19:31:51 +0000

    revert changes for identity service

--- identity-service/scripts/start.sh
+++ identity-service/scripts/start.sh
@@ -2,17 +2,12 @@
 
 /usr/bin/wait
 
-# enable rsyslog if not explicitly disabled by Dockerfile ARGs
-: "${enableRsyslog:=true}"
-
-# $enableRsyslog should be true
-# $logglyDisable should be empty/null
-# $logglyToken should be a nonzero length string
-if $enableRsyslog && [[ -z "$logglyDisable" && -n "$logglyToken" ]]; then
-    logglyTags=$(echo $logglyTags | python3 -c "print(' '.join(f'tag=\\\\\"{i}\\\\\"' for i in input().split(',')))")
-    mkdir -p /var/spool/rsyslog
-    mkdir -p /etc/rsyslog.d
-    cat >/etc/rsyslog.d/22-loggly.conf <<EOF
+if [[ -z "$logglyDisable" ]]; then
+    if [[ -n "$logglyToken" ]]; then
+        logglyTags=$(echo $logglyTags | python3 -c "print(' '.join(f'tag=\\\\\"{i}\\\\\"' for i in input().split(',')))")
+        mkdir -p /var/spool/rsyslog
+        mkdir -p /etc/rsyslog.d
+        cat >/etc/rsyslog.d/22-loggly.conf <<EOF
 \$WorkDirectory /var/spool/rsyslog # where to place spool files
 \$ActionQueueFileName fwdRule1   # unique name prefix for spool files
 \$ActionQueueMaxDiskSpace 1g    # 1gb space limit (use as much as possible)
@@ -20,11 +15,12 @@ if $enableRsyslog && [[ -z "$logglyDisable" && -n "$logglyToken" ]]; then
 \$ActionQueueType LinkedList    # run asynchronously
 \$ActionResumeRetryCount -1    # infinite retries if host is down
 template(name="LogglyFormat" type="string"
-string="<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$logglyToken@41058 $logglyTags] %msg%\n")
+ string="<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$logglyToken@41058 $logglyTags] %msg%\n")
 # Send messages to Loggly over TCP using the template.
 action(type="omfwd" protocol="tcp" target="logs-01.loggly.com" port="514" template="LogglyFormat")
 EOF
-    rsyslogd
+        rsyslogd
+    fi
 fi
 
 node src/index.js | tee >(logger)
