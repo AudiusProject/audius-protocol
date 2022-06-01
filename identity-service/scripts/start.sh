e6178499aee137b2bbfd4874b1a8c0ddd29f9991
Joaquin Casares <joaquin@audius.co>
2022-06-01 19:28:00 +0000

    extracting identity service changes from jc-inf-93

--- identity-service/scripts/start.sh
+++ identity-service/scripts/start.sh
@@ -2,12 +2,17 @@
 
 /usr/bin/wait
 
-if [[ -z "$logglyDisable" ]]; then
-    if [[ -n "$logglyToken" ]]; then
-        logglyTags=$(echo $logglyTags | python3 -c "print(' '.join(f'tag=\\\\\"{i}\\\\\"' for i in input().split(',')))")
-        mkdir -p /var/spool/rsyslog
-        mkdir -p /etc/rsyslog.d
-        cat >/etc/rsyslog.d/22-loggly.conf <<EOF
+# enable rsyslog if not explicitly disabled by Dockerfile ARGs
+: "${enableRsyslog:=true}"
+
+# $enableRsyslog should be true
+# $logglyDisable should be empty/null
+# $logglyToken should be a nonzero length string
+if $enableRsyslog && [[ -z "$logglyDisable" && -n "$logglyToken" ]]; then
+    logglyTags=$(echo $logglyTags | python3 -c "print(' '.join(f'tag=\\\\\"{i}\\\\\"' for i in input().split(',')))")
+    mkdir -p /var/spool/rsyslog
+    mkdir -p /etc/rsyslog.d
+    cat >/etc/rsyslog.d/22-loggly.conf <<EOF
 \$WorkDirectory /var/spool/rsyslog # where to place spool files
 \$ActionQueueFileName fwdRule1   # unique name prefix for spool files
 \$ActionQueueMaxDiskSpace 1g    # 1gb space limit (use as much as possible)
@@ -15,12 +20,11 @@ if [[ -z "$logglyDisable" ]]; then
 \$ActionQueueType LinkedList    # run asynchronously
 \$ActionResumeRetryCount -1    # infinite retries if host is down
 template(name="LogglyFormat" type="string"
- string="<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$logglyToken@41058 $logglyTags] %msg%\n")
+string="<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [$logglyToken@41058 $logglyTags] %msg%\n")
 # Send messages to Loggly over TCP using the template.
 action(type="omfwd" protocol="tcp" target="logs-01.loggly.com" port="514" template="LogglyFormat")
 EOF
-        rsyslogd
-    fi
+    rsyslogd
 fi
 
 node src/index.js | tee >(logger)
