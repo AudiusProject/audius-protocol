b7e8b5ab84a271ffcf51b22fe65dc7a8621c5b9e
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-06-01 07:17:46 +0000

    Only restart celery worker when tasks are changed

--- discovery-provider/scripts/start.sh
+++ discovery-provider/scripts/start.sh
@@ -112,7 +112,7 @@ if [[ "$audius_discprov_dev_mode" == "true" ]]; then
     # docker exec -it <container> tail -f /var/log/discprov-beat.log
     ./scripts/dev-server.sh 2>&1 | tee >(logger -t server) &
     if [[ "$audius_no_workers" != "true" ]] && [[ "$audius_no_workers" != "1" ]]; then
-        watchmedo auto-restart --directory ./ --pattern=*.py --recursive -- celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
+        watchmedo auto-restart --directory ./src/tasks --pattern=*.py --recursive -- celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
         celery -A src.worker.celery beat --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t beat) &
     fi
 else
