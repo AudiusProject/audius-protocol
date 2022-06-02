4efe525d67c92a69cd7bac97a83e4c25ad69abcf
sliptype <sliptype@gmail.com>
2022-06-02 17:35:52 +0000

    Fix formatting

--- identity-service/src/audiusLibsInstance.js
+++ identity-service/src/audiusLibsInstance.js
@@ -1,19 +1,20 @@
-const { libs: AudiusLibs } = require("@audius/sdk");
+const { libs: AudiusLibs } = require('@audius/sdk')
+
 const {
   setDefaultWasm,
-} = require("@certusone/wormhole-sdk/lib/cjs/solana/wasm");
+} = require('@certusone/wormhole-sdk/lib/cjs/solana/wasm')
 
-const config = require("./config");
-const registryAddress = config.get("registryAddress");
-const web3ProviderUrl = config.get("web3Provider");
+const config = require('./config')
+const registryAddress = config.get('registryAddress')
+const web3ProviderUrl = config.get('web3Provider')
 
 // Fixed address of the SPL token program
-const SOLANA_TOKEN_ADDRESS = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+const SOLANA_TOKEN_ADDRESS = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
 
 class AudiusLibsWrapper {
   constructor() {
-    this.audiusLibsInstance = null;
-    setDefaultWasm("node");
+    this.audiusLibsInstance = null
+    setDefaultWasm('node')
   }
 
   async init() {
@@ -21,53 +22,53 @@ class AudiusLibsWrapper {
       web3ProviderUrl,
       null,
       false
-    );
-    if (!dataWeb3) throw new Error("Web3 incorrectly configured");
+    )
+    if (!dataWeb3) throw new Error('Web3 incorrectly configured')
 
-    const discoveryProviderWhitelist = config.get("discoveryProviderWhitelist")
-      ? new Set(config.get("discoveryProviderWhitelist").split(","))
-      : null;
+    const discoveryProviderWhitelist = config.get('discoveryProviderWhitelist')
+      ? new Set(config.get('discoveryProviderWhitelist').split(","))
+      : null
 
-    const feePayerSecretKeys = config.get("solanaFeePayerWallets")
+    const feePayerSecretKeys = config.get('solanaFeePayerWallets')
       ? config
-          .get("solanaFeePayerWallets")
+          .get('solanaFeePayerWallets')
           .map((item) => item.privateKey)
           .map((key) => Uint8Array.from(key))
-      : null;
+      : null
 
     const solanaWeb3Config = AudiusLibs.configSolanaWeb3({
-      solanaClusterEndpoint: config.get("solanaEndpoint"),
-      mintAddress: config.get("solanaMintAddress"),
+      solanaClusterEndpoint: config.get('solanaEndpoint'),
+      mintAddress: config.get('solanaMintAddress'),
       solanaTokenAddress: SOLANA_TOKEN_ADDRESS,
       claimableTokenProgramAddress: config.get(
-        "solanaClaimableTokenProgramAddress"
+        'solanaClaimableTokenProgramAddress'
       ),
-      rewardsManagerProgramId: config.get("solanaRewardsManagerProgramId"),
-      rewardsManagerProgramPDA: config.get("solanaRewardsManagerProgramPDA"),
-      rewardsManagerTokenPDA: config.get("solanaRewardsManagerTokenPDA"),
+      rewardsManagerProgramId: config.get('solanaRewardsManagerProgramId'),
+      rewardsManagerProgramPDA: config.get('solanaRewardsManagerProgramPDA'),
+      rewardsManagerTokenPDA: config.get('solanaRewardsManagerTokenPDA'),
       // Never use the relay path in identity
       useRelay: false,
       feePayerSecretKeys,
-      confirmationTimeout: config.get("solanaConfirmationTimeout"),
-    });
+      confirmationTimeout: config.get('solanaConfirmationTimeout'),
+    })
 
     const wormholeConfig = AudiusLibs.configWormhole({
-      rpcHosts: config.get("wormholeRPCHosts"),
-      solBridgeAddress: config.get("solBridgeAddress"),
-      solTokenBridgeAddress: config.get("solTokenBridgeAddress"),
-      ethBridgeAddress: config.get("ethBridgeAddress"),
-      ethTokenBridgeAddress: config.get("ethTokenBridgeAddress"),
-    });
+      rpcHosts: config.get('wormholeRPCHosts'),
+      solBridgeAddress: config.get('solBridgeAddress'),
+      solTokenBridgeAddress: config.get('solTokenBridgeAddress'),
+      ethBridgeAddress: config.get('ethBridgeAddress'),
+      ethTokenBridgeAddress: config.get('ethTokenBridgeAddress'),
+    })
 
     let audiusInstance = new AudiusLibs({
       discoveryProviderConfig: {
         whitelist: discoveryProviderWhitelist,
       },
       ethWeb3Config: AudiusLibs.configEthWeb3(
-        config.get("ethTokenAddress"),
-        config.get("ethRegistryAddress"),
-        config.get("ethProviderUrl"),
-        config.get("ethOwnerWallet")
+        config.get('ethTokenAddress'),
+        config.get('ethRegistryAddress'),
+        config.get('ethProviderUrl'),
+        config.get('ethOwnerWallet')
       ),
       web3Config: {
         registryAddress,
@@ -76,21 +77,21 @@ class AudiusLibsWrapper {
           web3: dataWeb3,
           // this is a stopgap since libs external web3 init requires an ownerWallet
           // this is never actually used in the service's libs calls
-          ownerWallet: config.get("relayerPublicKey"),
+          ownerWallet: config.get('relayerPublicKey'),
         },
       },
       isServer: true,
-      captchaConfig: { serviceKey: config.get("recaptchaServiceKey") },
+      captchaConfig: { serviceKey: config.get('recaptchaServiceKey') },
       solanaWeb3Config,
       wormholeConfig,
-    });
+    })
 
-    await audiusInstance.init();
-    this.audiusLibsInstance = audiusInstance;
+    await audiusInstance.init()
+    this.audiusLibsInstance = audiusInstance
   }
 
   getAudiusLibs() {
-    return this.audiusLibsInstance;
+    return this.audiusLibsInstance
   }
 
   /**
@@ -98,19 +99,19 @@ class AudiusLibsWrapper {
    */
   async getAudiusLibsAsync() {
     if (this.audiusLibsInstance) {
-      return this.audiusLibsInstance;
+      return this.audiusLibsInstance
     }
     return new Promise((resolve) => {
       const i = setInterval(() => {
         if (this.audiusLibsInstance) {
-          clearInterval(i);
-          resolve(this.audiusLibsInstance);
+          clearInterval(i)
+          resolve(this.audiusLibsInstance)
         }
-      }, 1000);
-    });
+      }, 1000)
+    })
   }
 }
 
-const audiusLibsWrapper = new AudiusLibsWrapper();
+const audiusLibsWrapper = new AudiusLibsWrapper()
 
-module.exports = audiusLibsWrapper;
+module.exports = audiusLibsWrapper
