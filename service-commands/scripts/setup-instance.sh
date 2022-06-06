533347ee20cfa793352142a2734237c46c4a5b3e
Joaquin Casares <joaquin@audius.co>
2022-06-06 16:33:45 -0500

    Update setup-instance.sh

--- service-commands/scripts/setup-instance.sh
+++ service-commands/scripts/setup-instance.sh
@@ -115,7 +115,7 @@ case "$service" in
 
 		IP=$(get_ip_addr $provider $name)
 		echo -e "\nLog into ${IP} using:\n"
-		echo -e "ssh ${user}@${IP}\n"
+		echo -e "ssh -i ~/.ssh/google_compute_engine ${user}@${IP}\n"
 		;;
 		
 esac
