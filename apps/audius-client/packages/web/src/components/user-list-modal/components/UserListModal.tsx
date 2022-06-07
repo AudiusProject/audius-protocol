97984d9594b21bf0bc696bafb2038d42970296a4
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-06-06 17:10:00 -0700

    Update scrollbar in UserListModal (#1404)
    * Update scrollbar in UserListModal
    
    * bad merge

--- apps/audius-client/packages/web/src/components/user-list-modal/components/UserListModal.tsx
+++ apps/audius-client/packages/web/src/components/user-list-modal/components/UserListModal.tsx
@@ -1,7 +1,12 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useRef } from 'react'
 
-import { Modal, IconTrophy, IconUser, IconFollowing } from '@audius/stems'
-import SimpleBar from 'simplebar-react-legacy'
+import {
+  Modal,
+  IconTrophy,
+  IconUser,
+  IconFollowing,
+  Scrollbar
+} from '@audius/stems'
 
 import { ReactComponent as IconTip } from 'assets/img/iconTip.svg'
 import { useSelector } from 'common/hooks/useSelector'
@@ -31,8 +36,6 @@ import { AppState } from 'store/types'
 
 import styles from './UserListModal.module.css'
 
-const SIMPLE_BAR_ID = 'USER_LIST_SIMPLE_BAR'
-
 type UserListModalProps = {
   userListType: UserListType
   isOpen: boolean
@@ -49,11 +52,6 @@ const messages = {
   mutuals: 'Mutuals'
 }
 
-const getScrollParent = () => {
-  const simpleBarElement = window.document.getElementById(SIMPLE_BAR_ID)
-  return simpleBarElement || null
-}
-
 const UserListModal = ({
   userListType,
   isOpen,
@@ -63,6 +61,7 @@ const UserListModal = ({
   let selector: (state: AppState) => UserListStoreState
   let title: ReactElement | string
   const notificationTitle = useSelector(getPageTitle)
+  const scrollParentRef = useRef<HTMLElement>()
 
   switch (userListType) {
     case UserListType.FAVORITE:
@@ -155,20 +154,19 @@ const UserListModal = ({
       headerContainerClassName={styles.modalHeader}
       showDismissButton
     >
-      {/* Typescript complains about no valid constructor, possibly
-        due to the two simplebar packages we maintain.
-      // @ts-ignore */}
-      <SimpleBar
-        scrollableNodeProps={{ id: SIMPLE_BAR_ID }}
+      <Scrollbar
         className={styles.scrollable}
+        containerRef={containerRef => {
+          scrollParentRef.current = containerRef
+        }}
       >
         <UserList
           stateSelector={selector!}
           tag={tag}
-          getScrollParent={getScrollParent}
+          getScrollParent={() => scrollParentRef.current || null}
           beforeClickArtistName={onClose}
         />
-      </SimpleBar>
+      </Scrollbar>
     </Modal>
   )
 }
