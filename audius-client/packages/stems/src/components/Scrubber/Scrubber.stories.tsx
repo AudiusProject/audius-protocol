afd92e4316902fcc6b7a2d6d1cdb0d5b2db18402
Kyle Shanks <kyle.j.shanks@gmail.com>
2022-06-15 11:00:21 -0400

    Flesh out the stories for the current stems components a bit (#1470)

--- audius-client/packages/stems/src/components/Scrubber/Scrubber.stories.tsx
+++ audius-client/packages/stems/src/components/Scrubber/Scrubber.stories.tsx
@@ -9,14 +9,25 @@ export default {
 }
 
 const Template: Story<ScrubberProps> = args => <Scrubber {...args} />
-
-// Primary
-export const Primary = Template.bind({})
-const primaryProps: ScrubberProps = {
+const baseProps: ScrubberProps = {
   isPlaying: false,
   mediaKey: '1',
   elapsedSeconds: 0,
   totalSeconds: 100
 }
 
-Primary.args = primaryProps
+// Primary
+export const Primary = Template.bind({})
+Primary.args = { ...baseProps }
+
+// Disabled
+export const Disabled = Template.bind({})
+Disabled.args = { ...baseProps, isDisabled: true }
+
+// No Timestamps
+export const NoTimestamps = Template.bind({})
+NoTimestamps.args = { ...baseProps, includeTimestamps: false }
+
+// Mobile
+export const Mobile = Template.bind({})
+Mobile.args = { ...baseProps, isMobile: true }
