ebd9fbafbfa1207869b422879cb43fea5bcc00bf
Kyle Shanks <kyle.j.shanks@gmail.com>
2022-06-15 11:00:21 -0400

    Flesh out the stories for the current stems components a bit (#1470)

--- apps/audius-client/packages/stems/src/components/Button/Button.stories.tsx
+++ apps/audius-client/packages/stems/src/components/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { Story } from '@storybook/react'
 import * as Icons from 'components/Icons'
 
 import { Button } from './Button'
-import { ButtonProps } from './types'
+import { ButtonProps, Type } from './types'
 
 export default {
   component: Button,
@@ -11,13 +11,46 @@ export default {
   argTypes: { onClick: { action: 'clicked' } }
 }
 
-const Template: Story<ButtonProps> = args => <Button {...args} />
-
-// Primary
-export const Primary = Template.bind({})
-const primaryProps: ButtonProps = {
+const baseProps: ButtonProps = {
   leftIcon: <Icons.IconPlay />,
   text: 'Click Me'
 }
 
-Primary.args = primaryProps
+const Template: Story<ButtonProps> = args => <Button {...baseProps} {...args} />
+
+const BackgroundTemplate: Story<ButtonProps> = args => (
+  <div style={{ backgroundColor: '#555', padding: '16px' }}>
+    <Button {...baseProps} {...args} />
+  </div>
+)
+
+// Primary
+export const Primary = Template.bind({})
+
+// Disabled
+export const Disabled = Template.bind({})
+Disabled.args = { disabled: true }
+
+// Primary Alt
+export const PrimaryAlt = Template.bind({})
+PrimaryAlt.args = { type: Type.PRIMARY_ALT }
+
+// Secondary
+export const Secondary = Template.bind({})
+Secondary.args = { type: Type.SECONDARY }
+
+// Common
+export const Common = Template.bind({})
+Common.args = { type: Type.COMMON }
+
+// Common Alt
+export const CommonAlt = Template.bind({})
+CommonAlt.args = { type: Type.COMMON_ALT }
+
+// Glass
+export const Glass = BackgroundTemplate.bind({})
+Glass.args = { type: Type.GLASS }
+
+// White
+export const White = Template.bind({})
+White.args = { type: Type.WHITE }
--- apps/audius-client/packages/stems/src/components/ProgressBar/ProgressBar.stories.tsx
+++ apps/audius-client/packages/stems/src/components/ProgressBar/ProgressBar.stories.tsx
@@ -10,15 +10,25 @@ export default {
 }
 
 const Template: Story<ProgressBarProps> = args => <ProgressBar {...args} />
-
-// Primary
-export const Primary = Template.bind({})
-const primaryProps: ProgressBarProps = {
+const baseProps: ProgressBarProps = {
   min: 0,
   max: 10,
   value: 3
-  // minWrapper: ({ value }) => <span>{value} $AUDIO</span>,
-  // maxWrapper: ({ value }) => <span>{value} $AUDIO</span>
 }
 
-Primary.args = primaryProps
+// Primary
+export const Primary = Template.bind({})
+Primary.args = { ...baseProps }
+
+// With Labels
+export const WithLabels = Template.bind({})
+WithLabels.args = { ...baseProps, showLabels: true }
+
+// With Label Wrappers
+export const WithLabelWrappers = Template.bind({})
+WithLabelWrappers.args = {
+  ...baseProps,
+  showLabels: true,
+  minWrapper: ({ value }) => <span>{value} $AUDIO</span>,
+  maxWrapper: ({ value }) => <span>{value} $AUDIO</span>
+}
