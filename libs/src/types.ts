3941a7dca12b987ddd6e09bcec53eccd7a16cbd5
Sebastian Klingler <sliptype@gmail.com>
2022-06-16 11:14:12 -0500

    [C-502] Integrate api type generation into sdk (#3254)
    * Get generated types working with _makeRequest
    
    * Get types accurate
    
    * Clean up templates
    
    * Return url from stream function
    
    * Add jsdocs to parameters
    
    * Add resolve api
    
    * Add appName to requests
    
    * Throw proper errors
    
    * Make initialization synchronous
    
    * Add script to copy generated types
    
    * Clean up
    
    * Use unknown type to better represent _makeRequest response
    
    * Add ts-nocheck to generated files
    
    * Fix lint issues
    
    * Fix issue with dependencies
    
    * Remove prepare script

--- libs/src/types.ts
+++ libs/src/types.ts
@@ -1,6 +1,8 @@
 import AudiusLibs from './libs'
 
 export { sdk } from './sdk'
+export * as full from './sdk/generated/full'
+export * from './sdk/generated/default'
 
 export const libs: any = AudiusLibs
 export { Utils } from './utils'
