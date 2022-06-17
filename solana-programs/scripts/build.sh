668a6503992620afee4a9bb59c1e47dc96aec1fd
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-06-17 10:47:25 -0500

    Collect into function

--- solana-programs/scripts/build.sh
+++ solana-programs/scripts/build.sh
@@ -16,44 +16,32 @@ function replace_address {
     fi
 }
 
+function generate_key {
+    if [[ "$2" == "" ]]; then
+        echo $2 >$1
+    else
+        solana-keygen new -f -s --no-bip39-passphrase -o $1
+    fi
+}
+
 # cd into solana-programs
 cd $(dirname "$(readlink -f "$0")")/..
 
 mkdir -p target/deploy anchor/audius-data/target/deploy
 
-if [[ "$AUDIUS_ETH_REGISTRY_PRIVATE_KEY" != "" ]]; then
-    echo "$AUDIUS_ETH_REGISTRY_PRIVATE_KEY" >target/deploy/audius_eth_registry-keypair.json
-else
-    solana-keygen new -f -s --no-bip39-passphrase -o target/deploy/audius_eth_registry-keypair.json
-fi
+generate_key target/deploy/audius_eth_registry-keypair.json "$AUDIUS_ETH_REGISTRY_PRIVATE_KEY"
 replace_address audius_eth_registry/src/lib.rs target/deploy/audius_eth_registry-keypair.json
 
-if [[ "$TRACK_LISTEN_COUNT_PRIVATE_KEY" != "" ]]; then
-    echo "$TRACK_LISTEN_COUNT_PRIVATE_KEY" >target/deploy/track_listen_count-keypair.json
-else
-    solana-keygen new -f -s --no-bip39-passphrase -o target/deploy/track_listen_count-keypair.json
-fi
+generate_key target/deploy/track_listen_count-keypair.json "$TRACK_LISTEN_COUNT_PRIVATE_KEY"
 replace_address track_listen_count/src/lib.rs target/deploy/track_listen_count-keypair.json
 
-if [[ "$CLAIMABLE_TOKENS_PRIVATE_KEY" != "" ]]; then
-    echo "$CLAIMABLE_TOKENS_PRIVATE_KEY" >target/deploy/claimable_tokens-keypair.json
-else
-    solana-keygen new -f -s --no-bip39-passphrase -o target/deploy/claimable_tokens-keypair.json
-fi
+generate_key target/deploy/claimable_tokens-keypair.json "$CLAIMABLE_TOKENS_PRIVATE_KEY"
 replace_address claimable-tokens/program/src/lib.rs target/deploy/claimable_tokens-keypair.json
 
-if [[ "$REWARD_MANAGER_PRIVATE_KEY" != "" ]]; then
-    echo "$REWARD_MANAGER_PRIVATE_KEY" >target/deploy/audius_reward_manager-keypair.json
-else
-    solana-keygen new -f -s --no-bip39-passphrase -o target/deploy/audius_reward_manager-keypair.json
-fi
+generate_key target/deploy/audius_reward_manager-keypair.json "$REWARD_MANAGER_PRIVATE_KEY"
 replace_address reward-manager/program/src/lib.rs target/deploy/audius_reward_manager-keypair.json
 
-if [[ "$AUDIUS_DATA_PRIVATE_KEY" != "" ]]; then
-    echo "$AUDIUS_DATA_PRIVATE_KEY" >anchor/audius-data/target/deploy/audius_data-keypair.json
-else
-    solana-keygen new -f -s --no-bip39-passphrase -o anchor/audius-data/target/deploy/audius_data-keypair.json
-fi
+generate_key anchor/audius-data/target/deploy/audius_data-keypair.json "$AUDIUS_DATA_PRIVATE_KEY"
 replace_address \
     anchor/audius-data/programs/audius-data/src/lib.rs \
     anchor/audius-data/Anchor.toml \
