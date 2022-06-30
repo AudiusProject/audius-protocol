39c143df95a48ea2dd0378ac0d4521ec96b09f9d
vicky :) <60366641+vicky-g@users.noreply.github.com>
2022-06-30 13:48:20 -0400

    Removing unnecessary sql check (#3355)

--- creator-node/src/components/healthCheck/healthCheckComponentService.js
+++ creator-node/src/components/healthCheck/healthCheckComponentService.js
@@ -192,11 +192,6 @@ const healthCheck = async (
     logger.warn('Health check with no libs')
   }
 
-  // we have a /db_check route for more granular detail, but the service health check should
-  // also check that the db connection is good. having this in the health_check
-  // allows us to get auto restarts from liveness probes etc if the db connection is down
-  await sequelize.query('SELECT 1')
-
   if (
     !response.numberOfCPUs ||
     response.numberOfCPUs < MIN_NUBMER_OF_CPUS ||
