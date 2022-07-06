7725a9602c8870ae38be6abcd7cec389364eb457
Johannes Naylor <jonaylor89@gmail.com>
2022-07-05 21:09:42 -0400

    Add Slack Alerts from Network Monitoring

--- discovery-provider/plugins/network-monitoring/src/metrics/index.ts
+++ discovery-provider/plugins/network-monitoring/src/metrics/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios"
 import {
     allUserCountGauge,
     fullySyncedUsersCountGauge,
@@ -8,6 +9,7 @@ import {
     primaryUserCountGauge,
     unsyncedUsersCountGauge,
 } from "../prometheus"
+import { getEnv } from "../utils"
 import {
     getPrimaryUserCount,
     getAllUserCount,
@@ -70,6 +72,13 @@ export const generateMetrics = async (run_id: number) => {
     // Record duration for generating metrics and export to prometheus
     endTimer({ run_id: run_id })
 
+    await publishSlackReport({
+        fullySyncedUsersCount: fullySyncedUsersCount,
+        partiallySyncedUsersCount: partiallySyncedUserCount,
+        unsyncedUsersCount: unsyncedUsersCount,
+        usersWithNullPrimaryClock: usersWithNullPrimaryClock
+    })
+
     try {
         // Finish by publishing metrics to prometheus push gateway
         console.log(`[${run_id}] pushing metrics to gateway`);
@@ -82,3 +91,25 @@ export const generateMetrics = async (run_id: number) => {
     console.log(`[${run_id}] finish generating metrics`);
 }
 
+const publishSlackReport = async (metrics: Object) => {
+
+    const { slackUrl } = getEnv()
+
+    if (slackUrl === '') {
+        return
+    }
+
+    let message = `\`\`\`${JSON.stringify(metrics, null, 2)}\`\`\`` 
+    console.log(message)
+
+    try {
+        await axios.post(
+            slackUrl,
+            {
+                text: message,
+            }, 
+        )
+    } catch (e) {
+        console.log(`Error posting to slack in slack reporter ${(e as Error).toString()}`)
+    }
+}
