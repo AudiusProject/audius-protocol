12227a43090175e23fcaba002fe5f8d6365ac9c2
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-07-06 05:50:34 -0500

    Wrap in loop

--- dev/startup/proxy.sh
+++ dev/startup/proxy.sh
@@ -2,18 +2,22 @@
 
 apk add bind-tools
 
-port=5000
-for ip in $(dig +short discovery-provider); do
-  name=$(nslookup $ip | grep -o "discovery-provider-[0-9]\+")
-  replica=$(echo $name | grep -o "[0-9]\+")
-  nc -v -lk -p $(($port + $replica - 1)) -e nc $ip 5000 &
-done
+while sleep 1; do
+  for ip in $(dig +short discovery-provider); do
+    if [ ! -e "$ip" ]; then
+      name=$(nslookup $ip | grep -o "discovery-provider-[0-9]\+")
+      replica=$(echo $name | grep -o "[0-9]\+")
+      nc -v -lk -p $((5000 + $replica - 1)) -e nc $ip 5000 &
+      touch $ip
+    fi
+  done
 
-port=4000
-for ip in $(dig +short creator-node); do
-  name=$(nslookup $ip | grep -o "creator-node-[0-9]\+")
-  replica=$(echo $name | grep -o "[0-9]\+")
-  nc -v -lk -p $(($port + $replica - 1)) -e nc $ip 4000 &
+  for ip in $(dig +short creator-node); do
+    if [ ! -e "$ip" ]; then
+      name=$(nslookup $ip | grep -o "creator-node-[0-9]\+")
+      replica=$(echo $name | grep -o "[0-9]\+")
+      nc -v -lk -p $((4000 + $replica - 1)) -e nc $ip 4000 &
+      touch $ip
+    fi
+  done
 done
-
-wait
