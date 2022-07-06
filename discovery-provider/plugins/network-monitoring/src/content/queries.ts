513fb23f24fb77b5826bcfca62b6f0d8aebae928
Johannes Naylor <jonaylor89@gmail.com>
2022-07-06 16:24:27 +0000

    change order

--- discovery-provider/plugins/network-monitoring/src/content/queries.ts
+++ discovery-provider/plugins/network-monitoring/src/content/queries.ts
@@ -312,7 +312,7 @@ export const getPrimaryWalletBatch = async (
         FROM network_monitoring_users
         WHERE run_id = :run_id
         AND primaryspid = :spid
-        ORDER BY user_id DESC
+        ORDER BY user_id 
         OFFSET :offset
         LIMIT :limit;
     `, {
@@ -337,7 +337,7 @@ export const getSecondary1WalletBatch = async (
     FROM network_monitoring_users
     WHERE run_id = :run_id
     AND secondary1spid = :spid
-    ORDER BY user_id DESC
+    ORDER BY user_id 
     OFFSET :offset
     LIMIT :limit;
     `, {
@@ -362,7 +362,7 @@ export const getSecondary2WalletBatch = async (
     FROM network_monitoring_users
     WHERE run_id = :run_id
     AND secondary2spid = :spid
-    ORDER BY user_id DESC
+    ORDER BY user_id 
     OFFSET :offset
     LIMIT :limit;
     `, {
