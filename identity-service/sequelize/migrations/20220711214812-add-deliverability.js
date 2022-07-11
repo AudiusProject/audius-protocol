21e0c7d20a400bd6f68fa52e2497577b125960ff
Joseph Lee <joeylee0925@gmail.com>
2022-07-11 18:34:36 -0400

    Identity verify email (#3424)
    * Add is email deliverable to users and add max time to 2 day email
    
    * Lint fix

---
+++ identity-service/sequelize/migrations/20220711214812-add-deliverability.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'Users',
+      'isEmailDeliverable', {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Users', 'isEmailDeliverable')
+  }
+}
