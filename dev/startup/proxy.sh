d8c2d089ece72dc2f240a3acee2804b7cc498db4
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-07-12 12:49:16 -0400

    Add comments

--- dev/startup/proxy.sh
+++ dev/startup/proxy.sh
@@ -3,12 +3,18 @@
 apk add bind-tools
 
 while sleep 1; do
+  # iterate through all discovery-provider ips
   for ip in $(dig +short discovery-provider); do
+    # check if a file exists with the name of the ip (this file serves as a lock)
     if [ ! -e "$ip" ]; then
-      name=$(nslookup $ip | grep -o "discovery-provider-[0-9]\+")
-      replica=$(echo $name | grep -o "[0-9]\+")
+      name=$(nslookup $ip | grep -o "discovery-provider-[0-9]\+") # find hostname of the ip
+      replica=$(echo $name | grep -o "[0-9]\+")                   # extract replia number from hostname
+
+      # forward traffic on port 5000 + replica - 1 to the ip
+      # see https://unix.stackexchange.com/questions/10428/simple-way-to-create-a-tunnel-from-one-local-port-to-another/101906#comment777872_101906 for more details
       nc -v -lk -p $((5000 + $replica - 1)) -e nc $ip 5000 &
-      touch $ip
+
+      touch $ip # create a file with the name of the ip (this file serves as a lock)
     fi
   done
 
