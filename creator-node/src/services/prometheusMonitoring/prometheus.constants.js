f80ec7891c343cb8ff7c6aa8213e37c42429fd6d
Sid Sethi <3323835+SidSethi@users.noreply.github.com>
2022-07-12 03:33:32 +0000

    More

--- creator-node/src/services/prometheusMonitoring/prometheus.constants.js
+++ creator-node/src/services/prometheusMonitoring/prometheus.constants.js
@@ -4,7 +4,8 @@ const config = require('../../config')
 const { exponentialBucketsRange } = require('./prometheusUtils')
 const {
   JOB_NAMES: STATE_MACHINE_JOB_NAMES,
-  SyncType
+  SyncType,
+  SYNC_MODES
 } = require('../stateMachineManager/stateMachineConstants')
 
 /**
@@ -76,15 +77,16 @@ const MetricLabels = Object.freeze({
     ]
   },
   [MetricNames.FIND_SYNC_REQUEST_COUNTS_GAUGE]: {
+    sync_mode: Object.values(SYNC_MODES).map(_.snakeCase),
     result: [
       'not_checked', // Default value -- means the logic short-circuited before checking if the primary should sync to the secondary. This can be expected if this node wasn't the user's primary
       'no_sync_already_marked_unhealthy', // Sync not found because the secondary was marked unhealthy before being passed to the find-sync-requests job
       'no_sync_sp_id_mismatch', // Sync not found because the secondary's spID mismatched what the chain reported
       'no_sync_success_rate_too_low', // Sync not found because the success rate of syncing to this secondary is below the acceptable threshold
+      'no_sync_error_computing_sync_mode', // Sync not found because of failure to compute sync mode
       'no_sync_secondary_data_matches_primary', // Sync not found because the secondary's clock value and filesHash match primary's
       'no_sync_unexpected_error', // Sync not found because some uncaught error was thrown
-      'new_sync_request_enqueued_primary_to_secondary', // Sync was found from primary->secondary because all other conditions were met and primary clock value was greater than secondary
-      'new_sync_request_enqueued_secondary_to_primary', // Sync was found from secondary->primary because all other conditions were met and secondary clock value was greater than primary
+      'new_sync_request_enqueued', // Sync found because all other conditions were met
       'sync_request_already_enqueued', // Sync was found but a duplicate request has already been enqueued so no need to enqueue another
       'new_sync_request_unable_to_enqueue' // Sync was found but something prevented a new request from being created
     ]
