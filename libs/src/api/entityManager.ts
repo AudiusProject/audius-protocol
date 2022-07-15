c43503634b17a6a2c061e723eacb92c267b82ed5
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2022-07-15 14:29:56 +0000

    Lint

--- libs/src/api/entityManager.ts
+++ libs/src/api/entityManager.ts
@@ -32,9 +32,9 @@ export class EntityManager extends Base {
    * Generate random integer between two known values
    */
   getRandomInt(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
   }
 
   /**
