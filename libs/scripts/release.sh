1914cbf92c88056aa6983efe265d007ed7de32f3
Joaquin Casares <joaquin@audius.co>
2022-07-15 07:14:38 +0000

    reorg

--- libs/scripts/release.sh
+++ libs/scripts/release.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 
+# ONLY TO BE RUN ON MASTER
+# master is assumed when using -f and --hard git parameters
+
 set -ex
 
-# Finds the lastest commit in libs that looks like a version commit,
-# and gets the list of commits after that (with PR #)
-function git-libs-changelog () {
+# Finds the lastest commit that looks like a version commit,
+# and gets the list of commits after that
+function git-changelog () {
     # Find the latest release commit by using the last time the first `version` key changed
     version_line_number=$(grep -m 1 -n version package.json | cut -d: -f 1)
     release_commit=$(git blame -L ${version_line_number},+1 --porcelain -- package.json | awk 'NR==1{ print $1 }')
 
-    # Print the log as <author name> <commit message>,
-    # then grab the PR # from within the () at the end of the message
-    # and format it as "- (#PR) Author Name"
+    # Print the log as "- <commmiter short date> [<commit short hash>] <commit message> <author name>"
     git log --pretty=format:"- %cd [%h] %s %an" --date=short $release_commit..HEAD
 }
 
-# Makes a new branch off the master branch and bumps libs,
+# formats a commit message using the bumped ${VERSION} and ${CHANGE_LOG}
+function commit-message () {
+    echo "Bump ${STUB} to ${VERSION}
+
+## Changelog
+
+${CHANGE_LOG}"
+}
+
+# Make a new branch off the master branch, bumps npm,
 # commits with the relevant changelog, and pushes
-function bump-libs () {
+function bump-npm () {
     # Configure git client
     git config --global user.email "audius-infra@audius.co"
     git config --global user.name "audius-infra"
@@ -26,50 +36,54 @@ function bump-libs () {
     git checkout master -f
     git pull
 
-    # Working directory clean
+    # Ensure working directory clean
     git reset --hard origin/master
 
-    # Patch the version
-    version=$(npm version patch)
+    # grab change log early, before the version bump
+    CHANGE_LOG=$(git-changelog)
 
-    # Build libs
+    # Patch the version
+    VERSION=$(npm version patch)
+
+    # Build project
     npm i
     npm run build
 
-    # Publishing dry run
+    # Publishing dry run, prior to pushing a branch
     npm publish . --access public --dry-run
 
-    # Commit to a new branch
-    git checkout -b libs-$version
+    # Commit to a new branch, and tag
+    git checkout -b ${STUB}-${VERSION}
     git add .
-    git commit -m "Bump libs to $version\n\n## Changelog\n\n${CHANGE_LOG}"
+    git commit -m $(commit-message)
+    git tag -a @audius/${STUB}@${VERSION} -m $(commit-message)
 
-    # Push to the remote
-    git push -u origin libs-$version
+    # Push branch and tags to remote
+    git push -u origin ${STUB}-${VERSION}
+    git push origin --tags
 }
 
+# Merge the created branch into master, then delete the branch
 function merge-bump () {
-    version=$(jq -r '"v\(.version)"' package.json)
-
     git checkout master -f
-    git pull
-    git merge --no-ff libs-$version -m "Bump libs to $version\n\n## Changelog\n\n${CHANGE_LOG}"
+    git merge --no-ff ${STUB}-${VERSION} -m $(commit-message)
 
     # git push -u origin master
 
     # clean up release branches
-    # git branch -d libs-$version
-    # git push origin :libs-$version
+    # git push origin :${STUB}-${VERSION}
 }
 
-function publish-libs () {
-    # Publish
-    npm publish . --access public
+# publish to npm
+function publish () {
+    # npm publish . --access public
 }
 
+# configuration
+STUB=sdk
+cd ${PROTOCOL_DIR}/libs
 
-cd $PROTOCOL_DIR/libs
-CHANGE_LOG=$(git-libs-changelog)
-bump-libs
+# perform release
+bump-npm
 merge-bump
-# publish-libs
+publish
