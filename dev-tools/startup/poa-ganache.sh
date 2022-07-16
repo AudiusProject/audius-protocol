77275816f9bf1681928cab06209fa1dfc99929ab
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-07-16 10:56:40 -0400

    Verify contracts on POA for block explorer

---
+++ dev-tools/startup/poa-ganache.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+npx ganache \
+  --server.host 0.0.0.0 \
+  --wallet.deterministic \
+  --wallet.totalAccounts 50 \
+  --database.dbPath /usr/db \
+  --chain.networkId 12345 &
+
+while ! node -e "require('http').request('http://poa-blockscout:4000').end()"; do
+  sleep 1
+done
+
+contracts=(
+  "Registry"
+  "UserStorage"
+  "UserFactory"
+  "TrackStorage"
+  "TrackFactory"
+  "DiscoveryProviderStorage"
+  "DiscoveryProviderFactory"
+  "PlaylistStorage"
+  "PlaylistFactory"
+  "SocialFeatureFactory"
+  "SocialFeatureStorage"
+  "IPLDBlacklistFactory"
+  "UserLibraryFactory"
+  "UserReplicaSetManager"
+  "EntityManager"
+)
+
+# verify contracts in parallel
+for contract in ${contracts[@]}; do
+  npx truffle run verify --network predeploy $contract &
+done
+
+wait
