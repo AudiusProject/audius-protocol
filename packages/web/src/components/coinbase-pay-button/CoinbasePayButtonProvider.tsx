9a43bec38a13e3276dcf239e31af406d7fd36459
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-07-21 09:30:52 -0700

    [PAY-406] Web: Add Coinbase Pay Button (#1589)
    * Initial attempt at Coinbase Pay integration
    
    * Change CoinbasePay button assets
    
    * Update CoinbasePay button to use all images variants, fix the too-many-listeners bug that launches 12 experiences, and complete address
    
    * Add disabled state, useRef, SOL
    
    * Animate button, fix image imports
    
    * Remove test code
    
    * Fix broken package.json
    
    * Lazy load images
    
    * Update @coinbase/cbpay-js
    
    * Pin @types/webpack-env
    
    * Remove pixel test code
    
    * Make max button sizes
    
    * Use import() instead for lazy loading
    
    * UseAsync
    
    * Decouple image button from cbInstance
    
    * Use a provider and allow multiple buttons with different amounts and callbacks
    
    * Better name for context params
    
    * Fix duplicate session listeners, make logic easier to understand
    
    * Fix backwards wallet setting
    
    * Add opened status to state

---
+++ packages/web/src/components/coinbase-pay-button/CoinbasePayButtonProvider.tsx
@@ -0,0 +1,117 @@
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState
+} from 'react'
+
+import { initOnRamp } from '@coinbase/cbpay-js'
+
+export const allowedCoinbasePayTokens = ['SOL']
+
+type ResetParams = {
+  destinationWallet?: string
+  presetCryptoAmount?: number
+  presetFiatAmount?: number
+  onSuccess?: () => void
+  onExit?: () => void
+}
+
+export const CoinbasePayContext = createContext<{
+  isReady: boolean
+  isOpen: boolean
+  open: () => void
+  resetParams: (newProps: ResetParams) => void
+}>({
+  isReady: false,
+  isOpen: false,
+  open: () => {},
+  resetParams: (_) => {}
+})
+
+export const CoinbasePayButtonProvider = ({
+  destinationWalletAddress,
+  children
+}: {
+  destinationWalletAddress?: string
+  children: ReactNode
+}) => {
+  const [isReady, setIsReady] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+  const cbInstance = useRef<ReturnType<typeof initOnRamp>>()
+
+  const resetParams = useCallback(
+    ({
+      destinationWallet,
+      presetCryptoAmount,
+      presetFiatAmount,
+      onSuccess,
+      onExit
+    }: ResetParams) => {
+      const address = destinationWallet ?? destinationWalletAddress
+      if (address) {
+        setIsReady(false)
+        cbInstance.current?.destroy()
+        cbInstance.current = initOnRamp({
+          appId: '2cbd65dc-1710-4ae3-ab28-8947b08c22fb',
+          widgetParameters: {
+            destinationWallets: [
+              {
+                address,
+                assets: ['SOL']
+              }
+            ],
+            presetCryptoAmount,
+            presetFiatAmount
+          },
+          onReady: () => {
+            // Update loading/ready states.
+            setIsReady(true)
+          },
+          onSuccess: () => {
+            onSuccess?.()
+            setIsOpen(false)
+          },
+          onExit: () => {
+            onExit?.()
+            setIsOpen(false)
+          },
+          onEvent: (event: any) => {
+            // event stream
+            console.log(event)
+          },
+          experienceLoggedIn: 'embedded',
+          experienceLoggedOut: 'popup',
+          closeOnExit: true,
+          closeOnSuccess: true
+        })
+      }
+    },
+    [cbInstance, destinationWalletAddress, setIsOpen]
+  )
+
+  const open = useCallback(() => {
+    if (cbInstance.current) {
+      cbInstance.current.open()
+      setIsOpen(true)
+    }
+  }, [cbInstance, setIsOpen])
+
+  useEffect(() => {
+    resetParams({})
+  }, [resetParams])
+
+  return (
+    <CoinbasePayContext.Provider
+      value={{
+        isReady,
+        isOpen,
+        open,
+        resetParams
+      }}>
+      {children}
+    </CoinbasePayContext.Provider>
+  )
+}
