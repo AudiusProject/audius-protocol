5a9ac4eee9d600e73c5a88192dfaf212639cbc29
Kyle Shanks <kyle.j.shanks@gmail.com>
2022-07-21 15:42:04 -0400

    [C-687] Update some colors and styles for matrix mode (#1637)

--- packages/web/src/components/tracks-table/TableDragHandle.js
+++ packages/web/src/components/tracks-table/TableDragHandle.js
@@ -1,13 +1,21 @@
 import { ReactComponent as IconDrag } from 'assets/img/iconDrag.svg'
+import { getCurrentThemeColors } from 'utils/theme/theme'
 
 import styles from './TableDragHandle.module.css'
 
 const TableDragHandle = (props) => {
   const { loading, ...otherProps } = props
+  const themeColors = getCurrentThemeColors()
+
   return (
     <td className={styles.tableDragHandle} {...otherProps}>
       <div className={styles.wrapper}>
-        {!loading && <IconDrag className={styles.iconDrag} />}
+        {!loading && (
+          <IconDrag
+            className={styles.iconDrag}
+            color={themeColors['--neutral']}
+          />
+        )}
       </div>
     </td>
   )
