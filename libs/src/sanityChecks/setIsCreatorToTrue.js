1296e5b0171c8ef7be21f67eb3422c89831dd698
Raymond Jacobson <ray@audius.co>
2022-07-21 19:54:29 -0700

    Clean up

--- libs/src/sanityChecks/setIsCreatorToTrue.js
+++ libs/src/sanityChecks/setIsCreatorToTrue.js
@@ -1,10 +1,10 @@
 const setIsCreatorToTrue = async (libs) => {
-  console.error('Sanity Check - setIsCreatorToTrue')
   const user = libs.userStateManager.getCurrentUser()
 
   if (!user) return
 
   if (!user.is_creator) {
+    console.log('Setting is_creator to true')
     const newMetadata = { ...user, is_creator: true }
 
     await libs.User.updateCreator(user.user_id, newMetadata)
