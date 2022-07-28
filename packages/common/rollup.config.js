8d60d9ff1739066229f4d83a5be68d9a2961e5d4
Sebastian Klingler <sliptype@gmail.com>
2022-07-28 17:55:45 -0500

    Add rollup-plugin-dts and sync bn.js versions (#1658)

--- packages/common/rollup.config.js
+++ packages/common/rollup.config.js
@@ -1,22 +1,35 @@
+import dts from 'rollup-plugin-dts'
 import rollupTypescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
+import tsconfig from './tsconfig.json'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true
-    },
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    }
-  ],
-  plugins: [rollupTypescript()]
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+      }
+    ],
+    plugins: [rollupTypescript()]
+  },
+  {
+    input: './dist/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [
+      dts({
+        compilerOptions: tsconfig.compilerOptions
+      })
+    ]
+  }
+]
