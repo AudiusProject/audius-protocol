175e5d66cdf2ea2dd99617d9ce605ffcbc0ad34c
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-08-01 09:59:58 -0700

    Add ModalContentPages to stems (#1659)

--- packages/stems/src/components/Modal/index.ts
+++ packages/stems/src/components/Modal/index.ts
@@ -2,6 +2,7 @@ export { Modal } from './Modal'
 export { ModalContent } from './ModalContent'
 export { ModalHeader, ModalTitle } from './ModalHeader'
 export { ModalFooter } from './ModalFooter'
+export { ModalContentPages } from './ModalContentPages'
 export {
   ModalProps,
   Anchor,
