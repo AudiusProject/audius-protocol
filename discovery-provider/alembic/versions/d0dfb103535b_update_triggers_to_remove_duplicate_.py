060444f87c3f6151722c27b4ea035bc3b2acc353
Steve <steve@audius.co>
2022-08-03 18:44:03 +0000

    alembic helper to build sql

--- discovery-provider/alembic/versions/d0dfb103535b_update_triggers_to_remove_duplicate_.py
+++ discovery-provider/alembic/versions/d0dfb103535b_update_triggers_to_remove_duplicate_.py
@@ -5,28 +5,16 @@ Revises: 35584b65c55e
 Create Date: 2022-08-03 18:18:04.549325
 
 """
-from pathlib import Path
 
 import sqlalchemy as sa
 from alembic import op
+from src.utils.alembic_helpers import build_sql
 
 # revision identifiers, used by Alembic.
 revision = "d0dfb103535b"
 down_revision = "35584b65c55e"
 branch_labels = None
 depends_on = None
-
-
-def load_sql(name):
-    path = Path(__file__).parent.joinpath(f"../trigger_sql/{name}")
-    with open(path) as f:
-        return f.read()
-
-
-def build_sql(file_names):
-    files = [load_sql(f) for f in file_names]
-    inner_sql = "\n;\n".join(files)
-    return sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
 
 
 up_files = [
