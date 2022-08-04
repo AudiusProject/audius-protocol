2858f1789cfe62e5d9dd0fd73c53226cdb5faaf9
Dylan Jeffers <dylan@audius.co>
2022-08-04 11:37:35 -0700

    [C-771] Update native metro-config to enable @audius/sdk (#1671)

--- audius-client/packages/mobile/index.js
+++ audius-client/packages/mobile/index.js
@@ -2,7 +2,10 @@ import 'react-native-gesture-handler'
 import { AppRegistry, LogBox } from 'react-native'
 
 import { name as appName } from './app.json'
-import App from './src/App'
+
+require('node-libs-react-native/globals')
+
+const App = require('./src/App').default
 
 // Ignore LogBox logs for preferred log messages in external
 // React Native debug tools
