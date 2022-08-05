e9dca8633a1d29bc7d152664fed079d51377d15e
vicky <vicky@audius.co>
2022-08-05 19:19:28 +0000

    update tests

--- creator-node/test/secondarySyncFromPrimaryUtils.test.js
+++ creator-node/test/secondarySyncFromPrimaryUtils.test.js
@@ -4,30 +4,60 @@ import assert from 'assert'
 import proxyquire from 'proxyquire'
 
 describe('test secondarySyncFromPrimaryUtils', function () {
-  it('if force resync configs are not passed it, will not force resync', async function () {
-    assert.deepStrictEqual(await shouldForceResync(), false)
-    assert.deepStrictEqual(await shouldForceResync(null), false)
-  })
+  const mockedLibs = {
+    User: {
+      getUsers: () => {
+        return [{ primary_id: 1 }]
+      }
+    }
+  }
+  const mockLogContext = { context: 'test' }
 
-  it('if force resync configs are not proper, will not force resync', async function () {
-    assert.deepStrictEqual(await shouldForceResync({}), false)
+  it('if force resync configs are not passed it, will not force resync', async function () {
     assert.deepStrictEqual(
-      await shouldForceResync({
-        forceResync: true,
-        apiSigning: {
-          data: 'theres data but no signing stuff'
-        }
-      }),
+      await shouldForceResync({ libs: mockedLibs, logContext: mockLogContext }),
       false
     )
     assert.deepStrictEqual(
-      await shouldForceResync({
-        forceResync: true,
-        apiSigning: {
-          timestamp: 'theres a timestamp',
-          signature: 'but not data'
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        null
+      ),
+      false
+    )
+  })
+
+  it('if force resync configs are not proper, will not force resync', async function () {
+    assert.deepStrictEqual(
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {}
+      ),
+      false
+    )
+    assert.deepStrictEqual(
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {
+          forceResync: true,
+          apiSigning: {
+            data: 'theres data but no signing stuff'
+          }
         }
-      }),
+      ),
+      false
+    )
+    assert.deepStrictEqual(
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {
+          forceResync: true,
+          apiSigning: {
+            timestamp: 'theres a timestamp',
+            signature: 'but not data'
+          }
+        }
+      ),
       false
     )
   })
@@ -45,59 +75,49 @@ describe('test secondarySyncFromPrimaryUtils', function () {
     )
 
     assert.deepStrictEqual(
-      await shouldForceResync({
-        forceResync: true,
-        // dummy signature data
-        apiSigning: {
-          timestamp: '2022-08-02T21:38:42.670Z',
-          signature:
-            '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
-          data: { key: 'some data' }
-        },
-        // Mock libs
-        libs: {
-          User: {
-            getUsers: () => {
-              return [{ primary_id: 1 }]
-            }
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {
+          forceResync: true,
+          // dummy signature data
+          apiSigning: {
+            timestamp: '2022-08-02T21:38:42.670Z',
+            signature:
+              '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
+            data: { key: 'some data' }
           }
         }
-      }),
+      ),
       false
     )
   })
 
   it('if force resync configs fails to recover the wallet, do not force resync', async function () {
     assert.deepStrictEqual(
-      await shouldForceResync({
-        forceResync: true,
-        // dummy signature data
-        apiSigning: {
-          timestamp: '2022-08-02T21:38:42.670Z',
-          signature:
-            '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
-          data: { key: 'some data' }
-        },
-        // Mock libs
-        libs: {
-          User: {
-            getUsers: () => {
-              return [{ primary_id: 1 }]
-            }
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {
+          forceResync: true,
+          // dummy signature data
+          apiSigning: {
+            timestamp: '2022-08-02T21:38:42.670Z',
+            signature:
+              '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
+            data: { key: 'some data' }
           }
         }
-      }),
+      ),
       false
     )
   })
 
-  it('if the recovered wallet is equal to the wallet on chain for the user primary spID, return true', async function () {
+  it('if force resync configs recovers a different wallet, do not force resync', async function () {
     const { shouldForceResync } = proxyquire(
       '../src/services/sync/secondarySyncFromPrimaryUtils',
       {
         '../../apiSigning': {
           recoverWallet: () => {
-            return '0xCorrectPrimaryWallet'
+            return '0xdifferentprimarywallet'
           }
         },
         '../stateMachineManager/ContentNodeInfoManager': {
@@ -109,24 +129,54 @@ describe('test secondarySyncFromPrimaryUtils', function () {
     )
 
     assert.deepStrictEqual(
-      await shouldForceResync({
-        forceResync: true,
-        // dummy signature data
-        apiSigning: {
-          timestamp: '2022-08-02T21:38:42.670Z',
-          signature:
-            '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
-          data: { key: 'some data' }
-        },
-        // Mock libs
-        libs: {
-          User: {
-            getUsers: () => {
-              return [{ primary_id: 1 }]
-            }
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {
+          forceResync: true,
+          // dummy signature data
+          apiSigning: {
+            timestamp: '2022-08-02T21:38:42.670Z',
+            signature:
+              '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
+            data: { key: 'some data' }
           }
         }
-      }),
+      ),
+      false
+    )
+  })
+
+  it('if the recovered wallet is equal to the wallet on chain for the user primary spID, return true', async function () {
+    const { shouldForceResync } = proxyquire(
+      '../src/services/sync/secondarySyncFromPrimaryUtils',
+      {
+        '../../apiSigning': {
+          recoverWallet: () => {
+            return '0xcorrectprimarywallet'
+          }
+        },
+        '../stateMachineManager/ContentNodeInfoManager': {
+          getContentNodeInfoFromSpId: () => {
+            return { delegateOwnerWallet: '0xCorrectPrimaryWallet' }
+          }
+        }
+      }
+    )
+
+    assert.deepStrictEqual(
+      await shouldForceResync(
+        { libs: mockedLibs, logContext: mockLogContext },
+        {
+          forceResync: true,
+          // dummy signature data
+          apiSigning: {
+            timestamp: '2022-08-02T21:38:42.670Z',
+            signature:
+              '0xf33f4f9bc242acbd68ecb2c624b132cb606a71c1747253709dcb205406e3bb256be30dde81b6574cec8771f6f68ab175c6d5bf8448664900a73b03efbe16d63c1c',
+            data: { key: 'some data' }
+          }
+        }
+      ),
       true
     )
   })
