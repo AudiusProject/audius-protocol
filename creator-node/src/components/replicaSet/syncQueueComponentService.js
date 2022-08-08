227c36a12f7f8ccbad04dc573fb82ba2804b2a70
vicky <vicky@audius.co>
2022-08-08 16:59:39 +0000

    add jsdoc

--- creator-node/src/components/replicaSet/syncQueueComponentService.js
+++ creator-node/src/components/replicaSet/syncQueueComponentService.js
@@ -1,11 +1,16 @@
 /**
  * Enqueues sync operation into syncQueue for provided walletPublicKeys against provided creatorNodeEndpoint
+ * @param {Object} params See the SyncQueue for explicit params
  */
 const enqueueSync = async (params) => {
   const { serviceRegistry } = params
   await serviceRegistry.syncQueue.enqueueSync(params)
 }
 
+/**
+ * Enqueues an sync of MANUAL that will operate immediately
+ * @param {Object} params See the SyncImmediateQueue for explicit params
+ */
 const processSyncOfTypeImmediate = async (params) => {
   const { serviceRegistry } = params
   await serviceRegistry.syncImmediateQueue.processSyncOfTypeImmediate(params)
