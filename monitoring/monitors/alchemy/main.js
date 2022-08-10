cad6532cf70e267223f5bd4191083772d3b3f00b
Joaquin Casares <joaquin@audius.co>
2022-08-10 01:40:54 +0000

    sdk metrics

--- monitoring/monitors/alchemy/main.js
+++ monitoring/monitors/alchemy/main.js
@@ -2,8 +2,6 @@ const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
 const express = require('express')
 const promClient = require('prom-client')
 
-require('console-stamp')(console, '[HH:MM:ss.l]')
-
 // Setup Alchemy client
 const web3 = createAlchemyWeb3(
   process.env.WEBSOCKETS,
@@ -48,28 +46,84 @@ const enableDefaultMetrics = () => {
 enableDefaultMetrics()
 
 // Constants
-AUDIUS_CONTRACT = '0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998'
-AUDIUS_DECIMALS = 18
+const AUDIUS_CONTRACT = '0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998'
+const AUDIUS_DECIMALS = 18
 
 const scanWallets = async () => {
   // Wallet addresses to monitor all transactions for
   const addresses = {
-    communityTreasury: {
-      address: '0x4deca517d6817b6510798b7328f2314d3003abac',
+    registry: {
+      name: 'Registry',
+      address: '0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C',
       tokens: [],
     },
-    multiSig: {
-      address: '0xeABCcd75dA6c021c7553dB4A74CACC958812432A',
-      tokens: [],
-    },
-    tokenContract: {
+    token: {
+      name: 'Token',
       address: AUDIUS_CONTRACT,
+      tokens: [],
+    },
+    governance: {
+      name: 'Governance',
+      address: '0x4DEcA517D6817B6510798b7328F2314d3003AbAC',
+      tokens: [],
+    },
+    staking: {
+      name: 'Staking',
+      address: '0xe6D97B2099F142513be7A2a068bE040656Ae4591',
+      tokens: [],
+    },
+    delegateManager: {
+      name: 'DelegateManager',
+      address: '0x4d7968ebfD390D5E7926Cb3587C39eFf2F9FB225',
+      tokens: [],
+    },
+    claimsManager: {
+      name: 'ClaimsManager',
+      address: '0x44617F9dCEd9787C3B06a05B35B4C779a2AA1334',
+      tokens: [],
+    },
+    serviceTypeManager: {
+      name: 'ServiceTypeManager',
+      address: '0x9EfB0f4F38aFbb4b0984D00C126E97E21b8417C5',
+      tokens: [],
+    },
+    serviceProviderFactory: {
+      name: 'ServiceProviderFactory',
+      address: '0xD17A9bc90c582249e211a4f4b16721e7f65156c8',
+      tokens: [],
+    },
+    ethRewardsManager: {
+      name: 'EthRewardsManager',
+      address: '0x5aa6B99A2B461bA8E97207740f0A689C5C39C3b0',
+      tokens: [],
+    },
+    wormholeClient: {
+      name: 'WormholeClient',
+      address: '0x6E7a1F7339bbB62b23D44797b63e4258d283E095',
+      tokens: [],
+    },
+    trustedNotifierManager: {
+      name: 'TrustedNotifierManager',
+      address: '0x6f08105c8CEef2BC5653640fcdbBE1e7bb519D39',
+      tokens: [],
+    },
+    // ClaimableTokens: {
+    //   address: 'Ewkv3JahEFRKkcJmpoKB7pXbnUHwjAyXiwEo4ZY2rezQ',
+    //   tokens: [],
+    // },
+    // RewardsManager: {
+    //   address: 'DDZDcYdQFEMwcu2Mwo75yGFjJ1mUQyyXLWzhZLEVFcei',
+    //   tokens: [],
+    // },
+    multiSig: {
+      name: 'MultiSig',
+      address: '0xeABCcd75dA6c021c7553dB4A74CACC958812432A',
       tokens: [],
     },
   }
 
-  for (const addressName in addresses) {
-    const address = addresses[addressName].address
+  for (const key in addresses) {
+    const address = addresses[key].address
 
     // Get token balances
     const balances = await web3.alchemy.getTokenBalances(address)
@@ -79,20 +133,17 @@ const scanWallets = async () => {
       return token.tokenBalance !== '0'
     })
 
-    // Counter for SNo of final output
-    const i = 1
-
     // Minimize variable churn
     let name
     let symbol
     let decimals
 
     // Loop through all tokens with non-zero balance
-    for (token of nonZeroBalances) {
+    for (const token of nonZeroBalances) {
       // Get balance of token
       let balance = token.tokenBalance
 
-      if (token.contractAddress == AUDIUS_CONTRACT) {
+      if (token.contractAddress === AUDIUS_CONTRACT) {
         // Skip API call for common request
         name = 'Audius'
         decimals = AUDIUS_DECIMALS
@@ -110,16 +161,16 @@ const scanWallets = async () => {
       balance = balance.toFixed(2)
 
       // Collection for logs
-      tokenSummary = {
+      const tokenSummary = {
         name,
         balance,
         symbol,
       }
-      addresses[addressName].tokens.push(tokenSummary)
+      addresses[key].tokens.push(tokenSummary)
 
       // export metric for Prometheus
       METRICS[metricNames.BALANCE].set(
-        { address_name: addressName },
+        { address_name: addresses[key].name },
         parseFloat(balance),
       )
     }
@@ -142,7 +193,7 @@ const monitorTransfers = async () => {
     topics: ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef']
   }).on('data', (data) => {
     try {
-      balance = parseInt(data.data, 16)
+      let balance = parseInt(data.data, 16)
       balance = balance / Math.pow(10, AUDIUS_DECIMALS)
       balance = balance.toFixed(2)
       console.log(balance)
---
+++ monitoring/monitors/sdk/main.js
@@ -0,0 +1,151 @@
+const express = require('express')
+const promClient = require('prom-client')
+const Audius = require('@audius/libs')
+
+const ETH_TOKEN_ADDRESS = '0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998'
+const ETH_REGISTRY_ADDRESS = '0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C'
+const ETH_PROVIDER_ENDPOINT = 'https://eth.audius.co'
+const ETH_OWNER_WALLET = '0xC7310a03e930DD659E15305ed7e1F5Df0F0426C5'
+// const { sdk } = require('@audius/sdk')
+// const audiusLibs = sdk({
+//   appName: "audius-monitors-sdk",
+//   // ethContractsConfig: {
+//   //   ethWeb3Manager: "",
+//   //   tokenContractAddress: "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998",
+//   //   registryAddress: "0xd976d3b4f4e22a238c1A736b6612D22f17b6f64C",
+//   //   claimDistributionContractAddress: "https://eth.audius.co",
+//   //   wormholeContractAddress: "0xC7310a03e930DD659E15305ed7e1F5Df0F0426C5"
+//   // },
+//   ethWeb3Config: Audius.configEthWeb3(
+//         ETH_TOKEN_ADDRESS,
+//         ETH_REGISTRY_ADDRESS,
+//         ETH_PROVIDER_ENDPOINT,
+//         ETH_OWNER_WALLET
+//       )
+// })
+const audiusLibs = new Audius({
+  ethWeb3Config: Audius.configEthWeb3(
+    ETH_TOKEN_ADDRESS,
+    ETH_REGISTRY_ADDRESS,
+    ETH_PROVIDER_ENDPOINT,
+    ETH_OWNER_WALLET
+  ),
+  isServer: true,
+  enableUserReplicaSetManagerContract: true
+})
+
+// Setup Prometheus metrics
+const prefix = 'audius_monitors_sdk_'
+const metricNames = {
+  PROPOSALS: 'proposals',
+  OPEN_PROPOSALS: 'open_proposals',
+  PROPOSALS_BY_UNKNOWN_PROPOSERS: 'proposals_by_unknown_proposers',
+  API_FAILURE: 'api_failure',
+}
+const METRICS = Object.freeze({
+  [metricNames.PROPOSALS]: new promClient.Gauge({
+    name: `${prefix}${metricNames.PROPOSALS}`,
+    help: 'Number of proposals',
+    labelNames: ['proposer'],
+  }),
+  [metricNames.OPEN_PROPOSALS]: new promClient.Gauge({
+    name: `${prefix}${metricNames.OPEN_PROPOSALS}`,
+    help: 'Number of open proposals',
+    labelNames: ['proposer'],
+  }),
+  [metricNames.PROPOSALS_BY_UNKNOWN_PROPOSERS]: new promClient.Gauge({
+    name: `${prefix}${metricNames.PROPOSALS_BY_UNKNOWN_PROPOSERS}`,
+    help: 'The number of unknown proposers',
+  }),
+  [metricNames.API_FAILURE]: new promClient.Gauge({
+    name: `${prefix}${metricNames.API_FAILURE}`,
+    help: 'Count when alchemy calls fail.',
+  }),
+})
+METRICS[metricNames.OPEN_PROPOSALS].set(0)
+METRICS[metricNames.PROPOSALS_BY_UNKNOWN_PROPOSERS].set(0)
+METRICS[metricNames.API_FAILURE].set(0)
+const enableDefaultMetrics = () => {
+  const collectDefaultMetrics = promClient.collectDefaultMetrics
+  collectDefaultMetrics({ prefix })
+}
+enableDefaultMetrics()
+
+// Constants
+const PROPOSERS = new Set([
+  // '0xe5b256d302ea2f4e04B8F3bfD8695aDe147aB68d',
+  '0xc1f351FE81dFAcB3541e59177AC71Ed237BD15D0',
+  '0xD79819bAf3326FAbB7ba95b098e6515a0f6408B8',
+  '0x8C860adb28CA8A33dB5571536BFCF7D6522181e5',
+  '0x1BD9D60a0103FF2fA25169918392f118Bc616Dc9',
+  '0xA62c3ced6906B188A4d4A3c981B79f2AABf2107F',
+  '0xbdbB5945f252bc3466A319CDcC3EE8056bf2e569',
+])
+
+const scanGovernanceProposals = async () => {
+  const proposals = await audiusLibs.ethContracts.GovernanceClient.getProposals()
+  const lastProposal = proposals[proposals.length - 1]
+
+  METRICS[metricNames.PROPOSALS].set(
+    parseFloat(lastProposal.proposalId)
+  )
+
+  let currentBlockNumber = 15265449
+  currentBlockNumber = 15265423
+  if (lastProposal.blockNumber > currentBlockNumber) {
+    let count = 0
+    for (const proposal of proposals) {
+      if (proposal.blockNumber > currentBlockNumber) {
+        ++count
+      }
+    }
+    METRICS[metricNames.OPEN_PROPOSALS].set(
+      parseFloat(count)
+    )
+  }
+
+  if (!PROPOSERS.has(lastProposal.proposer)) {
+    let count = 0
+    for (const proposal of proposals) {
+      if (!PROPOSERS.has(proposal.proposer)) {
+        ++count
+      }
+    }
+    METRICS[metricNames.PROPOSALS_BY_UNKNOWN_PROPOSERS].set(
+      parseFloat(count)
+    )
+  }
+}
+const monitorGovernanceProposals = async () => {
+  try {
+    await scanGovernanceProposals()
+  } catch (error) {
+    console.log(error)
+    METRICS[metricNames.API_FAILURE].inc()
+  }
+}
+
+const main = async () => {
+  await audiusLibs.init()
+  monitorGovernanceProposals()
+  setInterval(function () { monitorGovernanceProposals() }, 30 * 1000)
+
+  // Start Prometheus exporter
+  const server = express()
+  const port = process.env.PORT || 3000
+  server.get('/metrics', async (req, res) => {
+    try {
+      res.set('Content-Type', promClient.register.contentType)
+      res.end(await promClient.register.metrics())
+    } catch (ex) {
+      res.status(500).end(ex)
+    }
+  })
+  server.listen(port, () => {
+    console.log(
+            `Server listening to ${port}, metrics exposed on /metrics endpoint`,
+    )
+  })
+}
+
+main()
