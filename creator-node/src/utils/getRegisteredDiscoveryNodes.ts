d9441f70101b0b2a420f11a948cb53c4312c8b17
Saliou Diallo <saliou@audius.co>
2022-08-11 12:08:27 -0400

    Add retries to fetching registered discovery nodes

--- creator-node/src/utils/getRegisteredDiscoveryNodes.ts
+++ creator-node/src/utils/getRegisteredDiscoveryNodes.ts
@@ -1,5 +1,6 @@
 import type Logger from 'bunyan'
 import { Redis } from 'ioredis'
+const asyncRetry = require('./asyncRetry')
 
 const THIRTY_MINUTES_IN_SECONDS = 60 * 30
 
@@ -29,36 +30,55 @@ export const getRegisteredDiscoveryNodes = async ({
   const cacheKey = 'all_registered_dnodes'
 
   let DNodes
+
+  // Fetch from Redis if present
   try {
-    // Fetch from Redis if present
     const dnodesList = await redis.get(cacheKey)
     if (dnodesList) {
       return JSON.parse(dnodesList)
     }
-
-    // Else, fetch from chain
-    const discoveryNodes =
-      await libs.ethContracts.ServiceProviderFactoryClient.getServiceProviderList(
-        'discovery-node'
-      )
-
-    // Write fetched value to Redis with 30min expiry
-    await redis.set(
-      cacheKey,
-      JSON.stringify(discoveryNodes),
-      'EX',
-      THIRTY_MINUTES_IN_SECONDS
-    )
-
-    DNodes = discoveryNodes
   } catch (e) {
     logger.error(
-      `registeredNodes | Error getting values in getRegisteredDiscoveryNodes: ${
+      `registeredNodes | Error reading discovery nodes from redis: ${
         (e as Error).message
       }`
     )
+  }
 
-    DNodes = []
+  // Else, fetch from chain
+  try {
+    DNodes = await asyncRetry({
+      asyncFn: async () => {
+        return libs.ethContracts.ServiceProviderFactoryClient.getServiceProviderList(
+          'discovery-node'
+        )
+      },
+      logger,
+      logLabel: 'fetch all the registered discovery nodes'
+    })
+  } catch (e) {
+    logger.error(
+      `registeredNodes | Error fetching registered discovery nodes: ${
+        (e as Error).message
+      }`
+    )
+    return []
+  }
+
+  // Write fetched value to Redis with 30min expiry
+  try {
+    await redis.set(
+      cacheKey,
+      JSON.stringify(DNodes),
+      'EX',
+      THIRTY_MINUTES_IN_SECONDS
+    )
+  } catch (e) {
+    logger.error(
+      `registeredNodes | Error writing discovery nodes to redis: ${
+        (e as Error).message
+      }`
+    )
   }
 
   return DNodes
