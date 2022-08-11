e84ece141164483b2793a62d18db7fadb7b0e15c
Michael Piazza <michael.piazza.mp@gmail.com>
2022-08-11 16:37:16 +0000

    Migration for dethroned notifs

---
+++ identity-service/sequelize/migrations/20220809211742-add_supporter_dethroned.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface) => {
+    return queryInterface.sequelize.query(
+      `ALTER TYPE "enum_SolanaNotifications_type" ADD VALUE 'SupporterDethroned'`
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+
+      // Recreate the old enum:
+
+      // Create a new enum
+      await queryInterface.sequelize.query(`
+        CREATE TYPE "enum_SolanaNotifications_type_new"
+        AS ENUM ('ChallengeReward', 'MilestoneListen', 'TipSend', 'TipReceive', 'Reaction', 'SupporterRankUp', 'SupportingRankUp');
+      `, { transaction })
+
+      // Set the column to the new enum
+      await queryInterface.sequelize.query(`
+        ALTER TABLE "SolanaNotifications"
+        ALTER COLUMN "type"
+          TYPE "enum_SolanaNotifications_type_new"
+          USING ("type"::text::"enum_SolanaNotifications_type_new");
+    `, { transaction })
+
+      // Drop old enum
+      await queryInterface.sequelize.query(`
+        DROP TYPE "enum_SolanaNotifications_type";
+      `, { transaction })
+
+      // Rename new enum
+      await queryInterface.sequelize.query(`
+        ALTER TYPE "enum_SolanaNotifications_type_new"
+        RENAME TO "enum_SolanaNotifications_type";
+      `, { transaction })
+    })
+  }
+};
