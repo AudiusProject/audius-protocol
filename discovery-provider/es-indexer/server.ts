adb7b453800b10e48a486efb08b452a911b6a16e
Steve Perkins <stereosteve@gmail.com>
2022-08-12 09:55:31 -0400

    collection sort + filter examples, add graphql codegen

--- discovery-provider/es-indexer/server.ts
+++ discovery-provider/es-indexer/server.ts
@@ -1,15 +1,54 @@
 import { Client } from '@elastic/elasticsearch'
+import { GetGetResult } from '@elastic/elasticsearch/lib/api/types'
 import { ApolloServer, gql } from 'apollo-server'
 import bodybuilder, { Bodybuilder } from 'bodybuilder'
 import DataLoader from 'dataloader'
 import Hashids from 'hashids'
+import {
+  Playlist,
+  PlaylistResolvers,
+  Track,
+  TrackResolvers,
+  User,
+  UserResolvers,
+  QueryResolvers,
+} from './src/generated/graphql'
 import { UserRow, TrackRow } from './src/types/db'
-import { PlaylistDoc, TrackDoc, UserDoc } from './src/types/docs'
+import { PlaylistDoc, RepostDoc, TrackDoc, UserDoc } from './src/types/docs'
 
 const typeDefs = gql`
+  scalar Date
+  scalar JSON
+
+  enum TrackSort {
+    length # todo: length or duration
+    # favorite_count # todo: need to map this field (or use save_count)
+    repost_count
+    # play_count # todo: need to map this field
+    title
+  }
+
+  enum UserSort {
+    name
+    handle
+    follower_count
+    following_count
+  }
+
+  enum PlaylistSort {
+    name
+    repost_count
+  }
+
+  enum SortDirection {
+    asc
+    desc
+  }
+
   type Track {
     id: String!
     title: String!
+    length: Int!
 
     favorite_count: Int!
     repost_count: Int!
@@ -35,10 +74,33 @@ const typeDefs = gql`
     is_follower: Boolean!
     is_followed: Boolean!
 
-    tracks(limit: Int = 3, offset: Int = 0): [Track!]!
-    playlists(limit: Int = 3, offset: Int = 0): [Playlist!]!
-    followers(limit: Int = 3, offset: Int = 0): [User!]!
-    following(limit: Int = 3, offset: Int = 0): [User!]!
+    tracks(
+      limit: Int = 3
+      offset: Int = 0
+      sort: TrackSort
+      sort_direction: SortDirection
+    ): [Track!]!
+
+    playlists(
+      limit: Int = 3
+      offset: Int = 0
+      sort: PlaylistSort
+      sort_direction: SortDirection
+    ): [Playlist!]!
+
+    followers(
+      limit: Int = 3
+      offset: Int = 0
+      sort: UserSort
+      sort_direction: SortDirection
+    ): [User!]!
+
+    following(
+      limit: Int = 3
+      offset: Int = 0
+      sort: UserSort
+      sort_direction: SortDirection
+    ): [User!]!
   }
 
   type Playlist {
@@ -60,6 +122,7 @@ const typeDefs = gql`
 
   type Query {
     users(handle: String): [User!]!
+    feed: JSON!
   }
 `
 
@@ -79,6 +142,7 @@ const indexNames = {
   users: 'users',
   tracks: 'tracks',
   playlists: 'playlists',
+  reposts: 'reposts',
 }
 
 function newMgetLoader(index: string) {
@@ -111,102 +175,197 @@ const trackLoader = new DataLoader<number | string, TrackDoc>(
 // Resolvers
 //
 
-const resolvers = {
-  Track: {
-    id: (parent: TrackDoc) => parent.track_id,
-    reposted_by: async (parent: TrackDoc, args: PaginationArgs) => {
-      return usersLoadMany(parent.reposted_by, args)
-    },
-    favorited_by: async (parent: TrackDoc, args: PaginationArgs) => {
-      return usersLoadMany(parent.saved_by, args)
-    },
-    is_saved: async (track: TrackDoc, _args: any, ctx: Ctx) => {
-      const me = await ctx.me
-      if (!me) return false
-      return track.saved_by.includes(me.user_id)
-    },
-    is_reposted: async (track: TrackDoc, _args: any, ctx: Ctx) => {
-      const me = await ctx.me
-      if (!me) return false
-      return track.reposted_by.includes(me.user_id)
-    },
-    stream_urls: async (track: TrackDoc, _args: any, ctx: Ctx) => {
-      const user = await ctx.es.user.load(track.owner_id)
-      return buildStreamUrls(user, track)
-    },
+const trackResolvers: TrackResolvers<Ctx, Track & TrackDoc> = {
+  async id(track) {
+    return track.track_id.toString()
   },
-  Playlist: {
-    id: (parent: PlaylistDoc) => parent.playlist_id,
-    name: (parent: PlaylistDoc) => parent.playlist_name,
-    tracks: fetchTracks,
-    favorite_count: (parent: PlaylistDoc) => parent.save_count,
-    is_saved: async (playlist: PlaylistDoc, _args: any, ctx: Ctx) => {
-      const me = await ctx.me
-      if (!me) return false
-      return playlist.saved_by.includes(me.user_id)
-    },
-    is_reposted: async (playlist: PlaylistDoc, _args: any, ctx: Ctx) => {
-      const me = await ctx.me
-      if (!me) return false
-      return playlist.reposted_by.includes(me.user_id)
-    },
-    reposted_by: async (parent: TrackDoc, args: PaginationArgs) => {
-      return usersLoadMany(parent.reposted_by, args)
-    },
-    favorited_by: async (parent: TrackDoc, args: PaginationArgs) => {
-      return usersLoadMany(parent.saved_by, args)
-    },
+
+  async reposted_by(track, args) {
+    return usersLoadMany(track.reposted_by, args) as any
   },
-  User: {
-    id: (parent: UserDoc) => parent.user_id,
-    tracks: fetchTracks,
-    playlists: async (parent: UserDoc, args: PaginationArgs) => {
-      const bb = bodybuilder()
-        .filter('term', 'playlist_owner_id', parent.user_id)
-        .filter('term', 'is_delete', false)
-        .filter('term', 'is_private', false)
-        .size(args.limit)
-        .from(args.offset)
-        .sort('created_at', 'desc')
-      return bbSearch(indexNames.playlists, bb)
-    },
-    followers: async (parent: UserDoc, args: PaginationArgs) => {
-      const bb = bodybuilder()
-        .filter('term', 'following_ids', parent.user_id)
-        .size(args.limit)
-        .from(args.offset)
-        .sort('follower_count', 'desc')
-      return bbSearch(indexNames.users, bb)
-    },
-    following: async (parent: UserDoc, args: PaginationArgs) => {
-      return usersLoadMany(parent.following_ids, args)
-    },
-    is_follower: async (user: UserDoc, args: PaginationArgs, ctx: Ctx) => {
-      const me = await ctx.me
-      if (!me) return false
-      return user.following_ids.includes(me.user_id)
-    },
-    is_followed: async (user: UserDoc, args: PaginationArgs, ctx: Ctx) => {
-      const me = await ctx.me
-      if (!me) return false
-      return me.following_ids.includes(user.user_id)
-    },
+
+  async favorited_by(track, args) {
+    return usersLoadMany(track.saved_by, args) as any
   },
-  Query: {
-    users: async (parent: any, args: any, ctx: Ctx) => {
-      const bb = bodybuilder()
-      if (args.handle) {
-        bb.filter('term', 'handle', args.handle)
-      }
-      return bbSearch(indexNames.users, bb)
-    },
+
+  async is_saved(track, _args, ctx) {
+    const me = await ctx.me
+    if (!me) return false
+    return track.saved_by.includes(me.user_id)
+  },
+
+  async is_reposted(track, _args, ctx) {
+    const me = await ctx.me
+    if (!me) return false
+    return track.reposted_by.includes(me.user_id)
+  },
+
+  async stream_urls(track, _args, ctx) {
+    const user = await ctx.es.user.load(track.owner_id)
+    return buildStreamUrls(user, track)
   },
 }
 
-async function fetchTracks(
-  parent: UserDoc | PlaylistDoc,
-  args: PaginationArgs
-) {
+const playlistResolvers: PlaylistResolvers<Ctx, Playlist & PlaylistDoc> = {
+  id: (playlist) => playlist.playlist_id.toString(),
+
+  name: (playlist) => playlist.playlist_name || '',
+
+  tracks: (playlist, args) => {
+    return fetchTracks(playlist, args) as any
+  },
+
+  favorite_count: (playlist) => playlist.save_count,
+
+  is_saved: async (playlist, args, ctx) => {
+    const me = await ctx.me
+    if (!me) return false
+    return playlist.saved_by.includes(me.user_id)
+  },
+
+  is_reposted: async (playlist, args, ctx) => {
+    const me = await ctx.me
+    if (!me) return false
+    return playlist.reposted_by.includes(me.user_id)
+  },
+
+  reposted_by: async (playlist, args) => {
+    return usersLoadMany(playlist.reposted_by, args) as any
+  },
+
+  favorited_by: async (playlist, args) => {
+    return usersLoadMany(playlist.saved_by, args) as any
+  },
+}
+
+const userResolvers: UserResolvers<Ctx, User & UserDoc> = {
+  id: (user) => user.user_id.toString(),
+
+  tracks: async (user, args, ctx) => {
+    const bb = bodybuilder()
+      .filter('term', 'owner_id', user.user_id.toString())
+      .filter('term', 'is_delete', false)
+      .filter('term', 'is_unlisted', false)
+      .size(args.limit)
+      .from(args.offset)
+      .sort(args.sort || 'created_at', args.sort_direction || 'desc')
+    const tracks = await bbSearch(indexNames.tracks, bb)
+    return tracks as any
+  },
+
+  playlists: async (user, args) => {
+    // sort enum rewrite
+    const sortRewrite: Record<string, string> = {
+      name: 'playlist_name',
+    }
+    let sortBy = args.sort as string | undefined
+    if (sortBy) {
+      sortBy = sortRewrite[sortBy]
+    }
+    const bb = bodybuilder()
+      .filter('term', 'playlist_owner_id', user.user_id)
+      .filter('term', 'is_delete', false)
+      .filter('term', 'is_private', false)
+      .size(args.limit)
+      .from(args.offset)
+      .sort(sortBy || 'created_at', args.sort_direction || 'desc')
+    return bbSearch(indexNames.playlists, bb)
+  },
+
+  followers: async (user, args) => {
+    const bb = bodybuilder()
+      .filter('term', 'following_ids', user.user_id)
+      .size(args.limit)
+      .from(args.offset)
+      .sort(args.sort || 'created_at', args.sort_direction || 'desc')
+    return bbSearch(indexNames.users, bb)
+  },
+
+  following: async (user, args) => {
+    const bb = bodybuilder()
+      .filter('terms', '_id', {
+        index: indexNames.users,
+        id: user.user_id.toString(),
+        path: 'following_ids',
+      })
+      .size(args.limit)
+      .from(args.offset)
+      .sort(args.sort || 'created_at', args.sort_direction || 'desc')
+    return bbSearch(indexNames.users, bb)
+  },
+
+  is_follower: async (user, args, ctx) => {
+    const me = await ctx.me
+    if (!me) return false
+    return user.following_ids.includes(me.user_id)
+  },
+
+  is_followed: async (user, args, ctx) => {
+    const me = await ctx.me
+    if (!me) return false
+    return me.following_ids.includes(user.user_id)
+  },
+}
+
+const queryResolvers: QueryResolvers<Ctx> = {
+  users: async (root, args, ctx) => {
+    const bb = bodybuilder()
+    if (args.handle) {
+      bb.filter('term', 'handle', args.handle)
+    }
+    return bbSearch(indexNames.users, bb)
+  },
+
+  feed: async (root, args, ctx) => {
+    const me = await ctx.me
+    if (!me) throw new Error(`feed requires current user`)
+    const bb = bodybuilder()
+      .filter('terms', 'user_id', {
+        index: indexNames.users,
+        id: me.user_id.toString(),
+        path: 'following_ids',
+      })
+      // .filter('term', 'repost_type', 'playlist')
+      .sort('created_at', 'desc')
+      .size(30)
+
+    const reposts = await bbSearch<RepostDoc>(indexNames.reposts, bb)
+
+    const ops = reposts.map((r) => {
+      const _index =
+        r.repost_type == 'track' ? indexNames.tracks : indexNames.playlists
+      return { _index, _id: r.repost_item_id.toString() }
+    })
+    const docs = await esc.mget({ docs: ops })
+
+    const stuff = reposts
+      .map((r, idx) => {
+        const { found, _source } = docs.docs[idx] as GetGetResult
+        if (!found) {
+          console.warn(`repost item not found`, r)
+        }
+        if (r.repost_type === 'track') {
+          const track = _source as TrackDoc
+          return { ...r, track_title: track.title }
+        } else {
+          const playlist = _source as PlaylistDoc
+          return { ...r, playlist_name: playlist.playlist_name }
+        }
+      })
+      .filter(Boolean)
+
+    return stuff
+  },
+}
+
+const resolvers = {
+  Track: trackResolvers,
+  Playlist: playlistResolvers,
+  User: userResolvers,
+  Query: queryResolvers,
+}
+
+async function fetchTracks(parent: UserDoc | PlaylistDoc, args: any) {
   const ids = parent.tracks
     .map((t) => t.track_id.toString())
     .slice(args.offset, args.offset + args.limit)
@@ -220,13 +379,13 @@ async function usersLoadMany(idList: any[], args: PaginationArgs) {
   return userLoader.loadMany(ids)
 }
 
-async function bbSearch(index: string, bb: Bodybuilder) {
+async function bbSearch<T>(index: string, bb: Bodybuilder) {
   return esc
     .search({
       index,
       ...bb.build(),
     })
-    .then((r) => r.hits.hits.map((h) => h._source))
+    .then((r) => r.hits.hits.map((h) => h._source as T))
 }
 
 //
@@ -276,6 +435,8 @@ export function buildStreamUrls(user: UserRow, track: TrackRow) {
 type Ctx = {
   es: {
     user: DataLoader<number | string, UserDoc>
+    track: DataLoader<number | string, TrackDoc>
+    playlist: DataLoader<number | string, PlaylistDoc>
   }
   me?: Promise<UserDoc | undefined>
 }
@@ -292,6 +453,8 @@ const server = new ApolloServer({
     const ctx: Ctx = {
       es: {
         user: new DataLoader(newMgetLoader(indexNames.users)),
+        track: new DataLoader(newMgetLoader(indexNames.tracks)),
+        playlist: new DataLoader(newMgetLoader(indexNames.playlists)),
       },
     }
 
---
+++ discovery-provider/es-indexer/src/generated/graphql.ts
@@ -0,0 +1,325 @@
+import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Date: any;
+  JSON: any;
+};
+
+export type Playlist = {
+  __typename?: 'Playlist';
+  created_at: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  favorite_count: Scalars['Int'];
+  favorited_by: Array<User>;
+  id: Scalars['String'];
+  is_reposted: Scalars['Boolean'];
+  is_saved: Scalars['Boolean'];
+  name: Scalars['String'];
+  repost_count: Scalars['Int'];
+  reposted_by: Array<User>;
+  tracks: Array<Track>;
+};
+
+
+export type PlaylistFavorited_ByArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type PlaylistReposted_ByArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type PlaylistTracksArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+export enum PlaylistSort {
+  Name = 'name',
+  RepostCount = 'repost_count'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  feed: Scalars['JSON'];
+  users: Array<User>;
+};
+
+
+export type QueryUsersArgs = {
+  handle?: InputMaybe<Scalars['String']>;
+};
+
+export enum SortDirection {
+  Asc = 'asc',
+  Desc = 'desc'
+}
+
+export type Track = {
+  __typename?: 'Track';
+  favorite_count: Scalars['Int'];
+  favorited_by: Array<User>;
+  id: Scalars['String'];
+  is_reposted: Scalars['Boolean'];
+  is_saved: Scalars['Boolean'];
+  length: Scalars['Int'];
+  repost_count: Scalars['Int'];
+  reposted_by: Array<User>;
+  stream_urls: Array<Scalars['String']>;
+  title: Scalars['String'];
+};
+
+
+export type TrackFavorited_ByArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type TrackReposted_ByArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+export enum TrackSort {
+  Length = 'length',
+  RepostCount = 'repost_count',
+  Title = 'title'
+}
+
+export type User = {
+  __typename?: 'User';
+  bio?: Maybe<Scalars['String']>;
+  follower_count: Scalars['Int'];
+  followers: Array<User>;
+  following: Array<User>;
+  following_count: Scalars['Int'];
+  handle: Scalars['String'];
+  id: Scalars['String'];
+  is_followed: Scalars['Boolean'];
+  is_follower: Scalars['Boolean'];
+  location?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+  playlists: Array<Playlist>;
+  tracks: Array<Track>;
+};
+
+
+export type UserFollowersArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+  sort?: InputMaybe<UserSort>;
+  sort_direction?: InputMaybe<SortDirection>;
+};
+
+
+export type UserFollowingArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+  sort?: InputMaybe<UserSort>;
+  sort_direction?: InputMaybe<SortDirection>;
+};
+
+
+export type UserPlaylistsArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+  sort?: InputMaybe<PlaylistSort>;
+  sort_direction?: InputMaybe<SortDirection>;
+};
+
+
+export type UserTracksArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+  sort?: InputMaybe<TrackSort>;
+  sort_direction?: InputMaybe<SortDirection>;
+};
+
+export enum UserSort {
+  FollowerCount = 'follower_count',
+  FollowingCount = 'following_count',
+  Handle = 'handle',
+  Name = 'name'
+}
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  Date: ResolverTypeWrapper<Scalars['Date']>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  JSON: ResolverTypeWrapper<Scalars['JSON']>;
+  Playlist: ResolverTypeWrapper<Playlist>;
+  PlaylistSort: PlaylistSort;
+  Query: ResolverTypeWrapper<{}>;
+  SortDirection: SortDirection;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  Track: ResolverTypeWrapper<Track>;
+  TrackSort: TrackSort;
+  User: ResolverTypeWrapper<User>;
+  UserSort: UserSort;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Boolean: Scalars['Boolean'];
+  Date: Scalars['Date'];
+  Int: Scalars['Int'];
+  JSON: Scalars['JSON'];
+  Playlist: Playlist;
+  Query: {};
+  String: Scalars['String'];
+  Track: Track;
+  User: User;
+};
+
+export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Date'], any> {
+  name: 'Date';
+}
+
+export interface JsonScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['JSON'], any> {
+  name: 'JSON';
+}
+
+export type PlaylistResolvers<ContextType = any, ParentType extends ResolversParentTypes['Playlist'] = ResolversParentTypes['Playlist']> = {
+  created_at?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  description?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  favorite_count?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  favorited_by?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, RequireFields<PlaylistFavorited_ByArgs, 'limit' | 'offset'>>;
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  is_reposted?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  is_saved?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  repost_count?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  reposted_by?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, RequireFields<PlaylistReposted_ByArgs, 'limit' | 'offset'>>;
+  tracks?: Resolver<Array<ResolversTypes['Track']>, ParentType, ContextType, RequireFields<PlaylistTracksArgs, 'limit' | 'offset'>>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  feed?: Resolver<ResolversTypes['JSON'], ParentType, ContextType>;
+  users?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, Partial<QueryUsersArgs>>;
+};
+
+export type TrackResolvers<ContextType = any, ParentType extends ResolversParentTypes['Track'] = ResolversParentTypes['Track']> = {
+  favorite_count?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  favorited_by?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, RequireFields<TrackFavorited_ByArgs, 'limit' | 'offset'>>;
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  is_reposted?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  is_saved?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  length?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  repost_count?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  reposted_by?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, RequireFields<TrackReposted_ByArgs, 'limit' | 'offset'>>;
+  stream_urls?: Resolver<Array<ResolversTypes['String']>, ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type UserResolvers<ContextType = any, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+  bio?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  follower_count?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  followers?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, RequireFields<UserFollowersArgs, 'limit' | 'offset'>>;
+  following?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType, RequireFields<UserFollowingArgs, 'limit' | 'offset'>>;
+  following_count?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  handle?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  is_followed?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  is_follower?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+  location?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  playlists?: Resolver<Array<ResolversTypes['Playlist']>, ParentType, ContextType, RequireFields<UserPlaylistsArgs, 'limit' | 'offset'>>;
+  tracks?: Resolver<Array<ResolversTypes['Track']>, ParentType, ContextType, RequireFields<UserTracksArgs, 'limit' | 'offset'>>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Date?: GraphQLScalarType;
+  JSON?: GraphQLScalarType;
+  Playlist?: PlaylistResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  Track?: TrackResolvers<ContextType>;
+  User?: UserResolvers<ContextType>;
+};
+
