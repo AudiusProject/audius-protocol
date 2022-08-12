248e5de5694bb0c3f20559a061c14e09efb849f5
Steve Perkins <stereosteve@gmail.com>
2022-08-12 10:15:38 -0400

    organize graphql server a bit

---
+++ discovery-provider/es-indexer/gqlapi/schema.ts
@@ -0,0 +1,111 @@
+import { gql } from 'apollo-server'
+
+export const typeDefs = gql`
+  scalar Date
+  scalar JSON
+
+  enum TrackSort {
+    length # todo: length or duration
+    # favorite_count # todo: need to map this field (or use save_count)
+    repost_count
+    # play_count # todo: need to map this field
+    title
+  }
+
+  enum UserSort {
+    name
+    handle
+    follower_count
+    following_count
+  }
+
+  enum PlaylistSort {
+    name
+    repost_count
+  }
+
+  enum SortDirection {
+    asc
+    desc
+  }
+
+  type Track {
+    id: String!
+    title: String!
+    length: Int!
+
+    favorite_count: Int!
+    repost_count: Int!
+    stream_urls: [String!]!
+
+    favorited_by(limit: Int = 3, offset: Int = 0): [User!]!
+    reposted_by(limit: Int = 3, offset: Int = 0): [User!]!
+
+    is_reposted: Boolean!
+    is_saved: Boolean!
+  }
+
+  type User {
+    id: String!
+    handle: String!
+    name: String!
+    bio: String
+    location: String
+
+    follower_count: Int!
+    following_count: Int!
+
+    is_follower: Boolean!
+    is_followed: Boolean!
+
+    tracks(
+      limit: Int = 3
+      offset: Int = 0
+      sort: TrackSort
+      sort_direction: SortDirection
+    ): [Track!]!
+
+    playlists(
+      limit: Int = 3
+      offset: Int = 0
+      sort: PlaylistSort
+      sort_direction: SortDirection
+    ): [Playlist!]!
+
+    followers(
+      limit: Int = 3
+      offset: Int = 0
+      sort: UserSort
+      sort_direction: SortDirection
+    ): [User!]!
+
+    following(
+      limit: Int = 3
+      offset: Int = 0
+      sort: UserSort
+      sort_direction: SortDirection
+    ): [User!]!
+  }
+
+  type Playlist {
+    id: String!
+    name: String!
+    description: String
+    created_at: String!
+
+    favorite_count: Int!
+    repost_count: Int!
+
+    favorited_by(limit: Int = 3, offset: Int = 0): [User!]!
+    reposted_by(limit: Int = 3, offset: Int = 0): [User!]!
+    tracks(limit: Int = 3, offset: Int = 0): [Track!]!
+
+    is_reposted: Boolean!
+    is_saved: Boolean!
+  }
+
+  type Query {
+    users(handle: String): [User!]!
+    feed: JSON!
+  }
+`
--- discovery-provider/es-indexer/gqlapi/server.ts
+++ discovery-provider/es-indexer/gqlapi/server.ts
@@ -12,124 +12,10 @@ import {
   User,
   UserResolvers,
   QueryResolvers,
-} from './src/generated/graphql'
-import { UserRow, TrackRow } from './src/types/db'
-import { PlaylistDoc, RepostDoc, TrackDoc, UserDoc } from './src/types/docs'
-
-const typeDefs = gql`
-  scalar Date
-  scalar JSON
-
-  enum TrackSort {
-    length # todo: length or duration
-    # favorite_count # todo: need to map this field (or use save_count)
-    repost_count
-    # play_count # todo: need to map this field
-    title
-  }
-
-  enum UserSort {
-    name
-    handle
-    follower_count
-    following_count
-  }
-
-  enum PlaylistSort {
-    name
-    repost_count
-  }
-
-  enum SortDirection {
-    asc
-    desc
-  }
-
-  type Track {
-    id: String!
-    title: String!
-    length: Int!
-
-    favorite_count: Int!
-    repost_count: Int!
-    stream_urls: [String!]!
-
-    favorited_by(limit: Int = 3, offset: Int = 0): [User!]!
-    reposted_by(limit: Int = 3, offset: Int = 0): [User!]!
-
-    is_reposted: Boolean!
-    is_saved: Boolean!
-  }
-
-  type User {
-    id: String!
-    handle: String!
-    name: String!
-    bio: String
-    location: String
-
-    follower_count: Int!
-    following_count: Int!
-
-    is_follower: Boolean!
-    is_followed: Boolean!
-
-    tracks(
-      limit: Int = 3
-      offset: Int = 0
-      sort: TrackSort
-      sort_direction: SortDirection
-    ): [Track!]!
-
-    playlists(
-      limit: Int = 3
-      offset: Int = 0
-      sort: PlaylistSort
-      sort_direction: SortDirection
-    ): [Playlist!]!
-
-    followers(
-      limit: Int = 3
-      offset: Int = 0
-      sort: UserSort
-      sort_direction: SortDirection
-    ): [User!]!
-
-    following(
-      limit: Int = 3
-      offset: Int = 0
-      sort: UserSort
-      sort_direction: SortDirection
-    ): [User!]!
-  }
-
-  type Playlist {
-    id: String!
-    name: String!
-    description: String
-    created_at: String!
-
-    favorite_count: Int!
-    repost_count: Int!
-
-    favorited_by(limit: Int = 3, offset: Int = 0): [User!]!
-    reposted_by(limit: Int = 3, offset: Int = 0): [User!]!
-    tracks(limit: Int = 3, offset: Int = 0): [Track!]!
-
-    is_reposted: Boolean!
-    is_saved: Boolean!
-  }
-
-  type Query {
-    users(handle: String): [User!]!
-    feed: JSON!
-  }
-`
-
-type PaginationArgs = {
-  limit: number
-  offset: number
-}
+} from './generated/graphql'
+import { UserRow, TrackRow } from '../src/types/db'
+import { PlaylistDoc, RepostDoc, TrackDoc, UserDoc } from '../src/types/docs'
+import { typeDefs } from './schema'
 
 //
 // DataLoaders
@@ -370,6 +256,11 @@ async function fetchTracks(parent: UserDoc | PlaylistDoc, args: any) {
     .map((t) => t.track_id.toString())
     .slice(args.offset, args.offset + args.limit)
   return trackLoader.loadMany(ids)
+}
+
+type PaginationArgs = {
+  limit: number
+  offset: number
 }
 
 async function usersLoadMany(idList: any[], args: PaginationArgs) {
