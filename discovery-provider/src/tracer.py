149dc3a8f10df68c8e37c7abbdd711bb7cf7aa2e
Johannes Naylor <jonaylor89@gmail.com>
2022-08-12 03:16:46 +0000

    add telemetry to discovery

--- discovery-provider/src/tracer.py
+++ discovery-provider/src/tracer.py
@@ -1,29 +1,29 @@
-# from opentelemetry import trace
-# from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-# from opentelemetry.instrumentation.celery import CeleryInstrumentor
-# from opentelemetry.instrumentation.logging import LoggingInstrumentor
-# from opentelemetry.instrumentation.requests import RequestsInstrumentor
-# from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
-# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.celery import CeleryInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
 
-# def configure_tracer():
-#     trace.set_tracer_provider(
-#         TracerProvider(resource=Resource.create({SERVICE_NAME: "discovery-provider"})),
-#     )
+def configure_tracer():
+    trace.set_tracer_provider(
+        TracerProvider(resource=Resource.create({SERVICE_NAME: "discovery-provider"})),
+    )
 
-#     jaeger_exporter = JaegerExporter(
-#         agent_host_name="jaeger",
-#         agent_port=6832,
-#     )
+    jaeger_exporter = JaegerExporter(
+        agent_host_name="jaeger",
+        agent_port=6832,
+    )
 
-#     trace.get_tracer_provider().add_span_processor(
-#         SimpleSpanProcessor(jaeger_exporter),
-#     )
+    trace.get_tracer_provider().add_span_processor(
+        SimpleSpanProcessor(jaeger_exporter),
+    )
 
-#     LoggingInstrumentor().instrument()
-#     RequestsInstrumentor().instrument()
-#     SQLAlchemyInstrumentor().instrument()
-#     CeleryInstrumentor().instrument()
+    LoggingInstrumentor().instrument()
+    RequestsInstrumentor().instrument()
+    SQLAlchemyInstrumentor().instrument()
+    CeleryInstrumentor().instrument()
