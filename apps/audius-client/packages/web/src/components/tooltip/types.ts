3e916e31ad375f32bcf8b0c47f284aee60933567
Raymond Jacobson <ray@audius.co>
2022-08-12 18:40:52 -0700

    [C-608] Fix tooltip bugs (#1701)

--- apps/audius-client/packages/web/src/components/tooltip/types.ts
+++ apps/audius-client/packages/web/src/components/tooltip/types.ts
@@ -12,11 +12,15 @@ export type TooltipProps = {
   color?: string
   // determines if it should display.
   disabled?: boolean
-  // Whether the tooltip gets mounted.
+  // Where the tooltip gets mounted.
   mount?: 'parent' | 'page' | 'body'
+  // Whether the tooltip should have a custom container/mount.
+  // Takes precedence over `mount`
+  getPopupContainer?: () => React.ReactNode
   mouseEnterDelay?: number
   mouseLeaveDelay?: number
   placement?: TooltipPlacement
+  // Should the tooltip go away when clicking on the underlying element?
   shouldDismissOnClick?: boolean
   // Whether there is a fixed max width, causing content to wrap onto the next line.
   shouldWrapContent?: boolean
