9e8dcad0835d416a118f7543cc7ff7744d5cbe6e
Johannes Naylor <jonaylor89@gmail.com>
2022-08-15 15:37:53 -0400

    refactor

--- identity-service/src/tracer.js
+++ identity-service/src/tracer.js
@@ -16,9 +16,6 @@ const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http')
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express')
 const { BunyanInstrumentation } = require('@opentelemetry/instrumentation-bunyan')
 
-const { JaegerExporter } = require('@opentelemetry/exporter-jaeger')
-const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base')
-
 // Not functionally required but gives some insight what happens behind the scenes
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
 
@@ -45,18 +42,6 @@ registerInstrumentations({
       })
     ]
 })
-
-const exporter = new JaegerExporter({
-    tags: [], // optional
-    // You can use the default UDPSender
-    host: 'jaeger', // optional
-    port: 6832, // optional
-    // OR you can use the HTTPSender as follows
-    // endpoint: 'http://localhost:14268/api/traces',
-    maxPacketSize: 65000 // optional
-})
-
-provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
 
 // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
 provider.register()
