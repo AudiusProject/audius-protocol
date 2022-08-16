8e753697da6b0442e98b92aae5eb48198e9c4a03
Dylan Jeffers <dylan@audius.co>
2022-08-16 12:18:59 -0700

    Remove common sagas from native (#1723)

--- packages/mobile/src/store/sagas.ts
+++ packages/mobile/src/store/sagas.ts
@@ -1,4 +1,3 @@
-import backendSagas, { setupBackend } from 'common/store/backend/sagas'
 import remoteConfig from 'common/store/remote-config/sagas'
 import { all, fork } from 'typed-redux-saga'
 
@@ -6,10 +5,8 @@ import initKeyboardEvents from './keyboard/sagas'
 import oauthSagas from './oauth/sagas'
 
 export default function* rootSaga() {
-  yield* fork(setupBackend)
   const sagas = [
     // config
-    ...backendSagas(),
     initKeyboardEvents,
     ...remoteConfig(),
     ...oauthSagas()
