f439784b1774ef8187f64ff96b90553f7ca46948
Johannes Naylor <jonaylor89@gmail.com>
2022-08-16 19:50:01 -0400

    format

--- discovery-provider/src/tracer.py
+++ discovery-provider/src/tracer.py
@@ -1,8 +1,6 @@
 from opentelemetry import trace
-
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 
