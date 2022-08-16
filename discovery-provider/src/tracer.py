cf84cb06465b24ac471739f872d167c6621226d2
Johannes Naylor <jonaylor89@gmail.com>
2022-08-16 17:46:29 -0400

    fix import

--- discovery-provider/src/tracer.py
+++ discovery-provider/src/tracer.py
@@ -1,12 +1,10 @@
 from opentelemetry import trace
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
 
 def configure_tracer():
