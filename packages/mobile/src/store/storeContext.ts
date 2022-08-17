97a1087048518b98dd78bc134fe46fa63aa0f16e
Sebastian Klingler <sliptype@gmail.com>
2022-08-17 11:28:51 -0400

    [C-841] Put getFeatureEnabled in storeContext (#1727)
    * Put getFeatureEnabled in storeContext
    
    * Undo appScreen changes
    
    * Revert notif saga changes
    
    * Await getFeatureEnabled
    
    * Improve types
    
    * Clean up sign up confirmation flag

--- packages/mobile/src/store/storeContext.ts
+++ packages/mobile/src/store/storeContext.ts
@@ -5,12 +5,16 @@ import { apiClient } from 'app/services/audius-api-client'
 import { audiusBackendInstance } from 'app/services/audius-backend-instance'
 import { fingerprintClient } from 'app/services/fingerprint'
 import { localStorage } from 'app/services/local-storage'
-import { remoteConfigInstance } from 'app/services/remote-config'
+import {
+  getFeatureEnabled,
+  remoteConfigInstance
+} from 'app/services/remote-config'
 import { walletClient } from 'app/services/wallet-client'
 
 export const storeContext: CommonStoreContext = {
   getLocalStorageItem: async (key) => AsyncStorage.getItem(key),
   setLocalStorageItem: async (key, value) => AsyncStorage.setItem(key, value),
+  getFeatureEnabled,
   remoteConfigInstance,
   audiusBackendInstance,
   apiClient,
