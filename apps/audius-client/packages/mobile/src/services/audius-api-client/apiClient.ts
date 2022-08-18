8f8bbeeb444e8e48671490b76face1f955330962
Sebastian Klingler <sliptype@gmail.com>
2022-08-18 16:29:06 -0500

    Update @audius/sdk to 0.0.37 (#1747)
    * Update @audius/sdk to 0.0.37
    
    * Remove unused ts-expect-error

--- apps/audius-client/packages/mobile/src/services/audius-api-client/apiClient.ts
+++ apps/audius-client/packages/mobile/src/services/audius-api-client/apiClient.ts
@@ -6,6 +6,5 @@ import { remoteConfigInstance } from '../remote-config'
 export const apiClient = new AudiusAPIClient({
   audiusBackendInstance,
   remoteConfigInstance,
-  // @ts-expect-error -- currently using two different versions of @audius/sdk to unblock mobile work
   audiusLibs
 })
