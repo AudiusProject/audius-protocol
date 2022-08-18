145b89988d798ff0b9161b1864f8327aa3e11592
Sid Sethi <3323835+SidSethi@users.noreply.github.com>
2022-08-18 15:01:21 +0000

    Fix test

--- creator-node/test/secondarySyncHealthTracker.test.js
+++ creator-node/test/secondarySyncHealthTracker.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-const SecondarySyncHealthTracker = require('../src/services/stateMachineManager/stateReconciliation/secondarySyncHealthTracker')
+const SecondarySyncHealthTracker = require('../src/services/stateMachineManager/stateReconciliation/SecondarySyncHealthTracker')
 const { getLibsMock } = require('./lib/libsMock')
 const { getApp } = require('./lib/app')
 
