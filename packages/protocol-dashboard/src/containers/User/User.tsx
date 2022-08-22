a05307ffe8f0093972cb7d317f91a6f5ca6745a2
Joseph Lee <joeylee0925@gmail.com>
2022-08-22 14:06:12 -0400

    Merge pull request #139 from AudiusProject/jowlee-fix-sp-with-no-delegators
    Fix sp with no registered services and leftover delegators

--- packages/protocol-dashboard/src/containers/User/User.tsx
+++ packages/protocol-dashboard/src/containers/User/User.tsx
@@ -137,7 +137,8 @@ const UserPage: React.FC<UserPageProps> = (props: UserPageProps) => {
             <DelegationStatsChip
               className={styles.delegationState}
               deployerCut={
-                (user as Operator | undefined)?.serviceProvider.deployerCut ?? 0
+                (user as Operator | undefined)?.serviceProvider?.deployerCut ??
+                0
               }
               delegated={inboundDelegation.amount ?? Utils.toBN('0')}
               minDelegation={
@@ -145,7 +146,7 @@ const UserPage: React.FC<UserPageProps> = (props: UserPageProps) => {
                 Utils.toBN('0')
               }
               delegators={
-                (user as Operator | undefined)?.delegators.length ?? 0
+                (user as Operator | undefined)?.delegators?.length ?? 0
               }
               isLoading={status === Status.Loading}
             />
@@ -178,7 +179,7 @@ const UserPage: React.FC<UserPageProps> = (props: UserPageProps) => {
         wallet={user?.wallet}
         timelineType={isServiceProvider ? 'ServiceProvider' : 'Delegator'}
       />
-      {isServiceProvider && (user as Operator).delegators.length > 0 && (
+      {(user as Operator)?.delegators?.length > 0 && (
         <DelegatorsTable
           wallet={user.wallet}
           className={styles.delegatorsContainer}
