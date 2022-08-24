2d3446f0ba8375ef12c985186aebfded92580e29
Saliou Diallo <saliou@audius.co>
2022-08-24 01:34:14 -0400

    Update logic and write first integration tests

--- discovery-provider/src/premium_content/premium_content_access_checker.py
+++ discovery-provider/src/premium_content/premium_content_access_checker.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from typing import Dict
+from typing import Dict, Optional, Tuple
 
 from src.models.tracks.track import Track
 from src.premium_content.helpers import does_user_have_nft_collection
@@ -17,31 +17,58 @@ class PremiumContentAccessChecker:
     # check if content is premium
     # - if not, then user has access
     # - if so, check whether user fulfills the conditions
+    #
+    # Returns a tuple of (bool, bool) -> (track is premium, user has access)
+    #
+    # Note: premium content id for type should exist, but just in case it does not,
+    # we return true for user access so that (non-existent) track does not change
+    # existing flow of the function caller.
     def check_access(
         self,
         user_id: int,
         premium_content_id: int,
         premium_content_type: PremiumContentType,
-    ):
-        is_premium, premium_conditions, content_owner_id = self._is_content_premium(
-            premium_content_id, premium_content_type
-        )
+    ) -> Tuple[bool, bool]:
+        # for now, we only allow tracks to be premium
+        # premium playlists will come later
+        if premium_content_type != "track":
+            return False, True
+
+        (
+            is_premium,
+            premium_conditions,
+            content_owner_id,
+        ) = self._is_content_premium(premium_content_id)
 
         if not is_premium:
-            return True
+            # premium_conditions should always be null here as it makes
+            # no sense to have a non-premium track with conditions
+            if premium_conditions:
+                logger.warn(
+                    f"premium_content_access_checker.py | _aggregate_conditions | non-premium content with id {premium_content_id} and type {premium_content_type} has premium conditions."
+                )
+            return False, True
 
-        self._evaluate_conditions(user_id, content_owner_id, premium_conditions)
+        if not premium_conditions:
+            # is_premium should always be false here as it makes
+            # no sense to have a premium track with no conditions
+            if is_premium:
+                logger.warn(
+                    f"premium_content_access_checker.py | _aggregate_conditions | premium content with id {premium_content_id} and type {premium_content_type} has no premium conditions."
+                )
+            return is_premium, True
 
+        does_user_have_access = self._evaluate_conditions(
+            user_id, content_owner_id, premium_conditions
+        )
+        return True, does_user_have_access
+
+    # Returns a tuple of (bool, Dict | None, int | None) -> (track is premium, track premium conditions, track owner id)
     def _is_content_premium(
-        self, premium_content_id: int, premium_content_type: PremiumContentType
-    ):
+        self, premium_content_id: int
+    ) -> Tuple[bool, Optional[Dict], Optional[int]]:
         db = db_session.get_db_read_replica()
         with db.scoped_session() as session:
-            # for now, we only allow tracks to be premium
-            # premium playlists will come later
-            if premium_content_type != "track":
-                return False, None, None
-
             track = (
                 session.query(Track)
                 .filter(
@@ -54,6 +81,7 @@ class PremiumContentAccessChecker:
 
             if not track:
                 return False, None, None
+
             return track.is_premium, track.premium_conditions, track.owner_id
 
     # There will eventually be another step prior to this one where
@@ -67,10 +95,10 @@ class PremiumContentAccessChecker:
             logging.info(
                 f"premium_content_access_checker.py | _aggregate_conditions | invalid conditions: {json.dumps(premium_conditions)}"
             )
-            return None
+            return False
 
         condition, value = list(premium_conditions.items())[0]
-        if condition != PREMIUM_CONTENT_CONDITIONS.NFT_COLLECTION:
+        if condition != PREMIUM_CONTENT_CONDITIONS.NFT_COLLECTION.value:
             return False
 
         return does_user_have_nft_collection(user_id, value)
