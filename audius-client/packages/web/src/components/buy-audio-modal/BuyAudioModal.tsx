9527e6239004c344c5a4ca552a41a764a1fce0ca
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-08-26 11:02:12 -0700

    Add all the pages to the BuyAudioModal (#1800)

--- audius-client/packages/web/src/components/buy-audio-modal/BuyAudioModal.tsx
+++ audius-client/packages/web/src/components/buy-audio-modal/BuyAudioModal.tsx
@@ -1,12 +1,27 @@
 import { useCallback } from 'react'
 
-import { Modal, ModalContent, ModalHeader, ModalTitle } from '@audius/stems'
+import {
+  buyAudioActions,
+  buyAudioSelectors,
+  BuyAudioStage
+} from '@audius/common'
+import {
+  Modal,
+  ModalContentPages,
+  ModalHeader,
+  ModalTitle
+} from '@audius/stems'
+import { useDispatch, useSelector } from 'react-redux'
 
 import IconGoldBadgeSrc from 'assets/img/tokenBadgeGold40@2x.png'
 import { useModalState } from 'common/hooks/useModalState'
 
 import styles from './BuyAudioModal.module.css'
 import { AmountInputPage } from './components/AmountInputPage'
+import { InProgressPage } from './components/InProgressPage'
+import { SuccessPage } from './components/SuccessPage'
+
+const { getBuyAudioFlowStage } = buyAudioSelectors
 
 const messages = {
   buyAudio: 'Buy Audio'
@@ -22,18 +37,54 @@ const IconGoldBadge = () => (
   />
 )
 
+const stageToPage = (stage: BuyAudioStage) => {
+  switch (stage) {
+    case BuyAudioStage.START:
+      return 0
+    case BuyAudioStage.PURCHASING:
+    case BuyAudioStage.CONFIRMING_PURCHASE:
+    case BuyAudioStage.SWAPPING:
+    case BuyAudioStage.CONFIRMING_SWAP:
+    case BuyAudioStage.TRANSFERRING:
+      return 1
+    case BuyAudioStage.FINISH:
+      return 2
+  }
+}
+
 export const BuyAudioModal = () => {
+  const dispatch = useDispatch()
   const [isOpen, setIsOpen] = useModalState('BuyAudio')
-  const onClose = useCallback(() => setIsOpen(false), [setIsOpen])
+  const stage = useSelector(getBuyAudioFlowStage)
+  const currentPage = stageToPage(stage)
+
+  const handleClose = useCallback(() => {
+    setIsOpen(false)
+  }, [setIsOpen])
+
+  const handleClosed = useCallback(
+    () => dispatch(buyAudioActions.restart()),
+    [dispatch]
+  )
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} bodyClassName={styles.modal}>
-      <ModalHeader onClose={onClose}>
+    <Modal
+      isOpen={isOpen}
+      onClose={handleClose}
+      onClosed={handleClosed}
+      bodyClassName={styles.modal}
+    >
+      <ModalHeader onClose={handleClose}>
         <ModalTitle title={messages.buyAudio} icon={<IconGoldBadge />} />
       </ModalHeader>
-      <ModalContent className={styles.modalContent}>
+      <ModalContentPages
+        contentClassName={styles.modalContent}
+        currentPage={currentPage}
+      >
         <AmountInputPage />
-      </ModalContent>
+        <InProgressPage />
+        <SuccessPage />
+      </ModalContentPages>
     </Modal>
   )
 }
