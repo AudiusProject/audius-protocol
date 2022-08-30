9542608c76d1f2d97586f35eb3800b1307a1f51e
Saliou Diallo <saliou@audius.co>
2022-08-30 18:35:26 -0400

    Fix circular dependency

--- discovery-provider/src/premium_content/signature.py
+++ discovery-provider/src/premium_content/signature.py
@@ -1,8 +1,12 @@
+import json
 from datetime import datetime
 from typing import TypedDict, cast
 
-from src.api_helpers import generate_signature
+from eth_account.messages import encode_defunct
 from src.premium_content.types import PremiumContentType
+from src.utils.config import shared_config
+from web3 import Web3
+from web3.auto import w3
 
 
 class PremiumContentSignatureData(TypedDict):
@@ -38,3 +42,37 @@ def get_premium_content_signature(
     }
     signature = generate_signature(data)
     return {"data": cast(PremiumContentSignatureData, data), "signature": signature}
+
+
+# Subclass JSONEncoder
+class DateTimeEncoder(json.JSONEncoder):
+    # Override the default method
+    def default(self, o):  # pylint: disable=E0202
+        if isinstance(o, (datetime.date, datetime.datetime)):
+            # the Z is required in JS date format
+            return o.isoformat() + " Z"
+        return json.JSONEncoder.default(self, o)
+
+
+# Generate signature and timestamp using data
+def generate_signature(data):
+    # convert sorted dictionary to string with no white spaces
+    to_sign_str = json.dumps(
+        data,
+        sort_keys=True,
+        ensure_ascii=False,
+        separators=(",", ":"),
+        cls=DateTimeEncoder,
+    )
+
+    # generate hash for if data contains unicode chars
+    to_sign_hash = Web3.keccak(text=to_sign_str).hex()
+
+    # generate SignableMessage for sign_message()
+    encoded_to_sign = encode_defunct(hexstr=to_sign_hash)
+
+    # sign to get signature
+    signed_message = w3.eth.account.sign_message(
+        encoded_to_sign, private_key=shared_config["delegate"]["private_key"]
+    )
+    return signed_message.signature.hex()
--- discovery-provider/src/queries/query_helpers.py
+++ discovery-provider/src/queries/query_helpers.py
@@ -10,7 +10,6 @@ from sqlalchemy.sql.expression import or_
 from src import exceptions
 from src.models.playlists.aggregate_playlist import AggregatePlaylist
 from src.models.playlists.playlist import Playlist
-from src.models.social.aggregate_plays import AggregatePlay
 from src.models.social.follow import Follow
 from src.models.social.repost import Repost, RepostType
 from src.models.social.save import Save, SaveType
@@ -1074,49 +1073,6 @@ def get_follower_count_dict(session, user_ids, max_block_number=None):
 
     follower_count_dict = dict(follower_counts)
     return follower_count_dict
-
-
-def get_track_play_counts(db, track_ids):
-    """Gets the track play counts for the given track_ids
-    Args:
-        db: sqlalchemy db session instance
-        track_ids: list of track ids
-
-    Returns:
-        dict of track id keys to track play count values
-    """
-
-    track_listen_counts = {}
-
-    if not track_ids:
-        return track_listen_counts
-
-    track_plays = (
-        db.query(AggregatePlay).filter(AggregatePlay.play_item_id.in_(track_ids)).all()
-    )
-
-    for track_play in track_plays:
-        track_listen_counts[track_play.play_item_id] = track_play.count
-
-    for track_id in track_ids:
-        if track_id not in track_listen_counts:
-            track_listen_counts[track_id] = 0
-
-    return track_listen_counts
-
-
-def get_sum_aggregate_plays(db):
-    """Gets the sum of all aggregate plays
-    Args:
-        db: sqlalchemy db session instance
-
-    Returns:
-        int of total play count
-    """
-
-    plays = db.query(func.sum(AggregatePlay.count)).scalar()
-
-    return int(plays)
 
 
 def get_pagination_vars():
