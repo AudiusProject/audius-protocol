3315c273f77fa73df9d3fc0dae20fa9dc54be5d1
Sebastian Klingler <sliptype@gmail.com>
2022-09-01 15:28:23 -0500

    Make tooltips use updated colors when theme is applied (#1843)

--- packages/web/src/utils/theme/theme.ts
+++ packages/web/src/utils/theme/theme.ts
@@ -11,6 +11,10 @@ const NATIVE_MOBILE = process.env.REACT_APP_NATIVE_MOBILE
 const THEME_KEY = 'theme'
 export const PREFERS_DARK_MEDIA_QUERY = '(prefers-color-scheme: dark)'
 
+export type ThemeColor = keyof typeof DefaultTheme &
+  keyof typeof DarkTheme &
+  keyof typeof MatrixTheme
+
 const applyTheme = (themeObject: { [key: string]: string }) => {
   Object.keys(themeObject).forEach((key) => {
     document.documentElement.style.setProperty(key, themeObject[key])
