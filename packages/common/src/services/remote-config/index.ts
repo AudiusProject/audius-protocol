67b97dde837aca021dc472edefbd38e81a25fca3
Sebastian Klingler <sliptype@gmail.com>
2022-09-07 10:18:01 -0500

    Clean up remote-config init handlers (#1866)
    * Clean up remote-config init handlers
    
    * Add didInitializeUser check
    
    * Address pr comments

--- packages/common/src/services/remote-config/index.ts
+++ packages/common/src/services/remote-config/index.ts
@@ -1,7 +1,7 @@
 export { IntKeys, StringKeys, DoubleKeys, BooleanKeys } from './types'
 export { AllRemoteConfigKeys } from './types'
 export { FeatureFlags } from './feature-flags'
-export { remoteConfig, USER_ID_AVAILABLE_EVENT } from './remote-config'
+export { remoteConfig } from './remote-config'
 export { RemoteConfigInstance } from './remote-config'
 export {
   remoteConfigIntDefaults,
--- packages/common/src/store/remote-config/sagas.ts
+++ packages/common/src/store/remote-config/sagas.ts
@@ -1,25 +1,13 @@
-import { eventChannel, END } from 'redux-saga'
-import { put, take } from 'typed-redux-saga'
+import { call, put } from 'typed-redux-saga'
 
 import { getContext } from '../effects'
 
 import { setDidLoad } from './slice'
 
-const CLIENT_READY_EVENT = 'CLIENT_READY'
-
 function* watchRemoteConfigLoad() {
   const remoteConfigInstance = yield* getContext('remoteConfigInstance')
-  // Emit event when provider is ready
-  const chan = eventChannel((emitter) => {
-    remoteConfigInstance.onClientReady(() => {
-      emitter(CLIENT_READY_EVENT)
-      emitter(END)
-    })
-    return () => {}
-  })
 
-  // await event before setting didLoad
-  yield* take(chan)
+  yield* call(remoteConfigInstance.waitForRemoteConfig)
   yield* put(setDidLoad())
 }
 
