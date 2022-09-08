8879a55252ea8e699609ad1a7c242578bc090775
Johannes Naylor <jonaylor89@gmail.com>
2022-09-08 15:37:00 +0000

    add comment

--- creator-node/src/utils/index.ts
+++ creator-node/src/utils/index.ts
@@ -1,3 +1,11 @@
+/**
+ * NOTE: we import everything explicitly an re-export it
+ * in order to support both commonjs style exports (`module.exports`)
+ * and the new module style exports (`export`).
+ * In the future, once we get a significant amount of the codebase
+ * typed, we'll remove the commonjs export syntax and just use
+ * the new way of importing/exporting
+ */
 import { strToReplicaSet } from './strToReplicaSet'
 import { timeout, getRandomInt, verifySignature } from './utils'
 import {
