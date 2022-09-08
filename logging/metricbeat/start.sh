9a38e9edbe039e42ad7880427e516a676551eabb
Joaquin Casares <joaquin@audius.co>
2022-09-08 23:18:18 +0000

    build and bump new beat images to account for service name changes in production and to support audius_loggly_disable

--- logging/metricbeat/start.sh
+++ logging/metricbeat/start.sh
@@ -7,7 +7,9 @@ ELASTIC_CLOUD_ID=$(echo ${ELASTIC_CLOUD_ID} | base64 -d)
 API_ID=$(echo ${API_ID} | base64 -d)
 API_KEY=$(echo ${API_KEY} | base64 -d)
 
-/usr/local/bin/docker-entrypoint \
-    -E output.elasticsearch.hosts=["${ELASTIC_ENDPOINT}"] \
-    -E cloud.id="${ELASTIC_CLOUD_ID}" \
-    -E cloud.auth="${API_ID}:${API_KEY}"
+if [[ -z "$audius_loggly_disable" ]]; then
+    /usr/local/bin/docker-entrypoint \
+        -E output.elasticsearch.hosts=["${ELASTIC_ENDPOINT}"] \
+        -E cloud.id="${ELASTIC_CLOUD_ID}" \
+        -E cloud.auth="${API_ID}:${API_KEY}"
+fi
