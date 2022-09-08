be3521be59ffa0d1610cc8c513d44d2b624e6933
Johannes Naylor <jonaylor89@gmail.com>
2022-09-08 14:08:59 -0400

    Split Up `legacyUtil.js` (#3834)

--- creator-node/src/utils/index.ts
+++ creator-node/src/utils/index.ts
@@ -1,28 +1,36 @@
-import type { ReplicaSet } from './strToReplicaSet'
+/**
+ * NOTE: we import everything explicitly an re-export it
+ * in order to support both commonjs style exports (`module.exports`)
+ * and the new module style exports (`export`).
+ * In the future, once we get a significant amount of the codebase
+ * typed, we'll remove the commonjs export syntax and just use
+ * the new way of importing/exporting
+ */
 import { strToReplicaSet } from './strToReplicaSet'
-import {
-  timeout,
-  getRandomInt,
-  verifySignature,
-  currentNodeShouldHandleTranscode,
-  validateStateForImageDirCIDAndReturnFileUUID,
-  findCIDInNetwork,
-  getAllRegisteredCNodes,
-  getIfAttemptedStateFix,
-  createDirForFile,
-  writeStreamToFileSystem,
-  _streamFileToDiskHelper,
-  runShellCommand,
-  stringifyMap,
-  verifyCIDMatchesExpected,
-  EMPTY_FILE_CID
-} from './legacyUtils'
+import { timeout, getRandomInt, verifySignature, stringifyMap } from './utils'
 import {
   validateMetadata,
   validateAssociatedWallets
 } from './validateAudiusUserMetadata'
+import {
+  findCIDInNetwork,
+  verifyCIDMatchesExpected,
+  EMPTY_FILE_CID
+} from './cidUtils'
+import {
+  createDirForFile,
+  writeStreamToFileSystem,
+  getIfAttemptedStateFix,
+  validateStateForImageDirCIDAndReturnFileUUID,
+  _streamFileToDiskHelper
+} from './fsUtils'
+import { runShellCommand } from './runShellCommand'
+import {
+  currentNodeShouldHandleTranscode,
+  getAllRegisteredCNodes
+} from './contentNodeUtils'
 
-export type { ReplicaSet }
+export type { ReplicaSet } from './strToReplicaSet'
 export {
   timeout,
   getRandomInt,
