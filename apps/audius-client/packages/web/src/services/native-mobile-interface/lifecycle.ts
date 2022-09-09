67136d36601e95659bd665b8a93f78e8d4d5949e
Sebastian Klingler <sliptype@gmail.com>
2022-09-09 17:33:59 -0500

    [C-1003] Native Account Sagas (#1890)
    * Split account sagas into mobile specific logic
    
    * Fix type error
    
    * Put back reachability code
    
    * Address pr comments

--- apps/audius-client/packages/web/src/services/native-mobile-interface/lifecycle.ts
+++ apps/audius-client/packages/web/src/services/native-mobile-interface/lifecycle.ts
@@ -1,22 +1,8 @@
-import { User } from '@audius/common'
-
 import { NativeMobileMessage } from './helpers'
 import { MessageType } from './types'
 
 export class RequestNetworkConnected extends NativeMobileMessage {
   constructor() {
     super(MessageType.REQUEST_NETWORK_CONNECTED, {})
-  }
-}
-
-export class SignedIn extends NativeMobileMessage {
-  constructor(account: User) {
-    super(MessageType.SIGNED_IN, { account })
-  }
-}
-
-export class FetchAccountFailed extends NativeMobileMessage {
-  constructor() {
-    super(MessageType.FETCH_ACCOUNT_FAILED)
   }
 }
