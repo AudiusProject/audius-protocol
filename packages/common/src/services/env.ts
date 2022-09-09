8f269fe18d44c5211754234c5f8f73342d0a218d
Dylan Jeffers <dylan@audius.co>
2022-09-08 21:04:00 -0700

    Fix merge conflicts

--- packages/common/src/services/env.ts
+++ packages/common/src/services/env.ts
@@ -1,14 +1,10 @@
 export type Environment = 'development' | 'staging' | 'production'
 
 export type Env = {
+  AAO_ENDPOINT?: string
   EAGER_DISCOVERY_NODES?: string
   EXPLORE_CONTENT_URL?: string
-<<<<<<< HEAD
   ENVIRONMENT?: Environment
-=======
-  SUGGESTED_FOLLOW_HANDLES: string
-  AAO_ENDPOINT?: string
   ORACLE_ETH_ADDRESSES?: string
-  ENVIRONMENT: string
->>>>>>> c54fab787 ([C-969] Add native audio-screen (#1873))
+  SUGGESTED_FOLLOW_HANDLES: string
 }
