ae576260f8d736d12f6d2ffeed8652250b47a53c
Johannes Naylor <jonaylor89@gmail.com>
2022-09-13 10:08:12 -0400

    remove telemetry

--- packages/web/src/utils/tracer.ts
+++ packages/web/src/utils/tracer.ts
@@ -3,10 +3,10 @@ import { trace } from '@opentelemetry/api'
 import { ZoneContextManager } from '@opentelemetry/context-zone'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { registerInstrumentations } from '@opentelemetry/instrumentation'
-// import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load'
-// import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
-// import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction'
-// import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request'
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load'
+import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction'
+import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request'
 import { Resource } from '@opentelemetry/resources'
 import {
   BatchSpanProcessor,
@@ -61,10 +61,10 @@ export const setupTracing = () => {
   // Registering instrumentations
   registerInstrumentations({
     instrumentations: [
-      // new UserInteractionInstrumentation(),
-      // new XMLHttpRequestInstrumentation(),
-      // new FetchInstrumentation(),
-      // new DocumentLoadInstrumentation()
+      new UserInteractionInstrumentation(),
+      new XMLHttpRequestInstrumentation(),
+      new FetchInstrumentation(),
+      new DocumentLoadInstrumentation()
     ]
   })
 }
