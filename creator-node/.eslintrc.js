e90543cf139428efdae1b71c24d0e00fea61faa8
Johannes Naylor <jonaylor89@gmail.com>
2022-09-19 03:10:59 +0000

    more rules

--- creator-node/.eslintrc.js
+++ creator-node/.eslintrc.js
@@ -24,6 +24,11 @@ module.exports = {
     'node/file-extension-in-import': ['error', 'never'],
     'node/prefer-global/process': ['error', 'always'],
     'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error',
     'node/exports-style': [
       'warn',
       'module.exports',
