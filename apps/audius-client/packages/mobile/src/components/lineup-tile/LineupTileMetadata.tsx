4fac333fe97651dc90a824b7283e459831f25283
Raymond Jacobson <ray@audius.co>
2022-09-20 13:22:34 -0700

    Implement small UI tweaks (#1952)
    * Apply accessbility patch for ios16 and xcode14
    
    * Factor drawer height into animation velocity
    
    * Make line up tiles touchable opacity
    
    * Fix lint

--- apps/audius-client/packages/mobile/src/components/lineup-tile/LineupTileMetadata.tsx
+++ apps/audius-client/packages/mobile/src/components/lineup-tile/LineupTileMetadata.tsx
@@ -1,10 +1,9 @@
 import { useCallback } from 'react'
 
 import type { Remix, User } from '@audius/common'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
 
 import IconVolume from 'app/assets/images/iconVolume.svg'
-import { Pressable } from 'app/components/core'
 import Text from 'app/components/text'
 import UserBadges from 'app/components/user-badges'
 import { useNavigation } from 'app/hooks/useNavigation'
@@ -91,49 +90,40 @@ export const LineupTileMetadata = ({
         style={trackTileStyles.imageContainer}
       />
       <View style={trackTileStyles.titles}>
-        <Pressable style={trackTileStyles.title} onPress={onPressTitle}>
-          {({ pressed }) => (
-            <>
-              <Text
-                style={[
-                  styles.titleText,
-                  isPlaying && styles.titlesActive,
-                  pressed && styles.titlesPressed
-                ]}
-                weight='bold'
-                numberOfLines={1}
-              >
-                {title}
-              </Text>
-              {!isPlaying ? null : (
-                <IconVolume fill={primary} style={styles.playingIndicator} />
-              )}
-            </>
-          )}
-        </Pressable>
-        <Pressable style={trackTileStyles.artist} onPress={handleArtistPress}>
-          {({ pressed }) => (
-            <>
-              <Text
-                style={[
-                  styles.titleText,
-                  isPlaying && styles.titlesActive,
-                  pressed && styles.titlesPressed
-                ]}
-                weight='medium'
-                numberOfLines={1}
-              >
-                {artistName}
-              </Text>
-              <UserBadges
-                user={user}
-                badgeSize={12}
-                style={styles.badge}
-                hideName
-              />
-            </>
-          )}
-        </Pressable>
+        <TouchableOpacity style={trackTileStyles.title} onPress={onPressTitle}>
+          <>
+            <Text
+              style={[styles.titleText, isPlaying && styles.titlesActive]}
+              weight='bold'
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            {!isPlaying ? null : (
+              <IconVolume fill={primary} style={styles.playingIndicator} />
+            )}
+          </>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={trackTileStyles.artist}
+          onPress={handleArtistPress}
+        >
+          <>
+            <Text
+              style={[styles.titleText, isPlaying && styles.titlesActive]}
+              weight='medium'
+              numberOfLines={1}
+            >
+              {artistName}
+            </Text>
+            <UserBadges
+              user={user}
+              badgeSize={12}
+              style={styles.badge}
+              hideName
+            />
+          </>
+        </TouchableOpacity>
       </View>
       {coSign && (
         <Text style={styles.coSignLabel} weight='heavy'>
