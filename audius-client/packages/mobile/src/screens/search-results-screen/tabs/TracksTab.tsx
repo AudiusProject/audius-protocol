a83fbc21cb423090902b833c8b7c1ae44a4b2669
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2022-09-21 12:54:32 -0400

    c-1127 Fix more search results track lineup (#1959)
    * fix more search results track lineup
    
    * reset
    
    * fix
    
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- audius-client/packages/mobile/src/screens/search-results-screen/tabs/TracksTab.tsx
+++ audius-client/packages/mobile/src/screens/search-results-screen/tabs/TracksTab.tsx
@@ -1,25 +1,58 @@
+import { useCallback, useContext } from 'react'
+
 import {
-  searchResultsPageTracksLineupActions as tracksActions,
+  lineupSelectors,
+  SearchKind,
   searchResultsPageSelectors,
-  SearchKind
+  searchResultsPageTracksLineupActions as tracksActions,
+  trimToAlphaNumeric
 } from '@audius/common'
-import { useSelector } from 'react-redux'
+import { useFocusEffect } from '@react-navigation/native'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Lineup } from 'app/components/lineup'
+
+import { SearchQueryContext } from '../SearchQueryContext'
 
 import { SearchResultsTab } from './SearchResultsTab'
 import { useFetchTabResultsEffect } from './useFetchTabResultsEffect'
 const { getSearchTracksLineup } = searchResultsPageSelectors
+const { makeGetLineupMetadatas } = lineupSelectors
+const getSearchTracksLineupMetadatas = makeGetLineupMetadatas(
+  getSearchTracksLineup
+)
 
 export const TracksTab = () => {
-  const lineup = useSelector(getSearchTracksLineup)
+  const lineup = useSelector(getSearchTracksLineupMetadatas)
+  const dispatch = useDispatch()
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        dispatch(tracksActions.reset())
+      }
+    }, [dispatch])
+  )
+  const { query, isTagSearch } = useContext(SearchQueryContext)
+  const loadMore = useCallback(
+    (offset: number, limit: number) => {
+      dispatch(
+        tracksActions.fetchLineupMetadatas(offset, limit, false, {
+          category: SearchKind.TRACKS,
+          query: trimToAlphaNumeric(query),
+          isTagSearch
+        })
+      )
+    },
+    [dispatch, isTagSearch, query]
+  )
+
   useFetchTabResultsEffect(SearchKind.TRACKS)
   return (
     <SearchResultsTab
       noResults={lineup?.entries.length === 0}
       status={lineup?.status}
     >
-      <Lineup actions={tracksActions} lineup={lineup} />
+      <Lineup actions={tracksActions} lineup={lineup} loadMore={loadMore} />
     </SearchResultsTab>
   )
 }
