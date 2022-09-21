415fe583c126fe584bf9726b6e4cc752ba930e8e
Johannes Naylor <jonaylor89@gmail.com>
2022-09-21 18:41:31 +0000

    rm sec and ms

--- discovery-provider/plugins/network-monitoring/src/metrics/index.ts
+++ discovery-provider/plugins/network-monitoring/src/metrics/index.ts
@@ -126,14 +126,11 @@ const publishSlackReport = async (metrics: Object) => {
 };
 
 const msToTime = (duration: number) => {
-  const milliseconds = Math.floor((duration % 1000) / 100)
-  const seconds = Math.floor((duration / 1000) % 60)
   const minutes = Math.floor((duration / (1000 * 60)) % 60)
   const hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
   const hoursStr = (hours < 10) ? "0" + hours : hours;
   const minutesStr = (minutes < 10) ? "0" + minutes : minutes;
-  const secondsStr = (seconds < 10) ? "0" + seconds : seconds;
 
-  return `${hoursStr}hr ${minutesStr}min ${secondsStr}sec ${milliseconds.toString()}ms`
+  return `${hoursStr}hr ${minutesStr}min`
 }
