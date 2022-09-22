56ab786f4c13292f19910d140be820205b134950
Michael Piazza <michael.piazza.mp@gmail.com>
2022-09-22 12:06:07 -0400

    [PAY-604] Use /abuse endpoint in Identity (#3860)
    * Integrate AAO /abuse endpoint
    
    * Integrate AAO
    
    * Polish
    
    * Skip abuse config
    
    * Fix migration
    
    * Fixes
    
    * Nullable columns for faster migration
    
    * polish

---
+++ identity-service/sequelize/migrations/20220912160855-abuse-columns.js
@@ -0,0 +1,57 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn('Users', 'isBlockedFromRelay', {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+      }, { transaction })
+
+      await queryInterface.addColumn('Users', 'isBlockedFromNotifications', {
+        type: Sequelize.BOOLEAN,
+        allowNull: true
+      }, { transaction })
+
+      await queryInterface.addColumn('Users', 'appliedRules', {
+        type: Sequelize.JSONB,
+        allowNull: true
+      }, { transaction })
+
+      await queryInterface.sequelize.query(
+        'UPDATE "Users" SET "isBlockedFromRelay" = true WHERE "isAbusive" = true AND "isAbusiveErrorCode" != \'9\'',
+        { transaction }
+      )
+
+      await queryInterface.sequelize.query(
+        'UPDATE "Users" SET "isBlockedFromNotifications" = true WHERE "isAbusive" = true AND "isAbusiveErrorCode" = \'9\'',
+        { transaction }
+      )
+
+      // Safe to drop this now since we've moved users
+      await queryInterface.removeColumn('Users', 'isAbusive', { transaction })
+      await queryInterface.removeColumn('Users', 'isAbusiveErrorCode', { transaction })
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn('Users', 'isBlockedFromRelay', { transaction })
+      await queryInterface.removeColumn('Users', 'isBlockedFromNotifications', { transaction })
+      await queryInterface.removeColumn('Users', 'appliedRules', { transaction })
+      await queryInterface.addColumn(
+        'Users',
+        'isAbusive', {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false
+        }
+      )
+      await queryInterface.addColumn(
+        'Users',
+        'isAbusiveErrorCode', {
+          type: Sequelize.STRING
+        }
+      )
+    })
+  }
+}
