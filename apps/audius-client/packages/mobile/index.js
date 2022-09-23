ada9d95ab3d53602630a310133d499217289b21c
Dylan Jeffers <dylan@audius.co>
2022-09-23 14:45:40 -0700

    [C-1172] Add screen freeze (#1990)

--- apps/audius-client/packages/mobile/index.js
+++ apps/audius-client/packages/mobile/index.js
@@ -2,13 +2,17 @@ import 'react-native-gesture-handler'
 import { AppRegistry, LogBox } from 'react-native'
 
 import { name as appName } from './app.json'
+
 // Needed for TextEncoder to work correctly
 import 'text-encoding-polyfill'
+import { enableFreeze } from 'react-native-screens'
 
 require('node-libs-react-native/globals')
 // Needed for @solana/web3.js to run correctly
 require('react-native-get-random-values')
 require('react-native-url-polyfill/auto')
+
+enableFreeze(true)
 
 const App = require('./src/App').default
 
