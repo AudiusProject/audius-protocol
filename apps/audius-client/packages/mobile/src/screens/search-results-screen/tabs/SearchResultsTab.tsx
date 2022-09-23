90d2da7080f5359cfd03f546d905621b976942a6
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2022-09-22 20:20:17 -0400

    keep relevant search results in store (#1973)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- apps/audius-client/packages/mobile/src/screens/search-results-screen/tabs/SearchResultsTab.tsx
+++ apps/audius-client/packages/mobile/src/screens/search-results-screen/tabs/SearchResultsTab.tsx
@@ -1,13 +1,11 @@
 import type { ReactNode } from 'react'
-import { useContext, useEffect, useState } from 'react'
 
-import { Status, searchResultsPageSelectors } from '@audius/common'
+import { searchResultsPageSelectors, Status } from '@audius/common'
 import { useSelector } from 'react-redux'
 
 import { WithLoader } from 'app/components/with-loader/WithLoader'
 
 import { EmptyResults } from '../EmptyResults'
-import { SearchFocusContext } from '../SearchFocusContext'
 const { getSearchStatus } = searchResultsPageSelectors
 
 type SearchResultsTabProps = {
@@ -18,29 +16,12 @@ type SearchResultsTabProps = {
 
 export const SearchResultsTab = (props: SearchResultsTabProps) => {
   const { children, noResults, status } = props
-  const { isFocused } = useContext(SearchFocusContext)
   const searchStatus = useSelector(getSearchStatus)
-  const [isRefreshing, setIsRefreshing] = useState(true)
-
-  useEffect(() => {
-    if (!isFocused) {
-      // Prevents a large rerender in the middle of a stack navigation.
-      // Note: having to manage status with navigation focus may not be needed
-      // when move common store into native.
-      setTimeout(() => {
-        setIsRefreshing(true)
-      }, 1000)
-    }
-    if (searchStatus === Status.LOADING) {
-      setIsRefreshing(false)
-    }
-  }, [isFocused, searchStatus])
 
   return (
     <WithLoader
       loading={Boolean(
-        isRefreshing ||
-          searchStatus === Status.LOADING ||
+        searchStatus === Status.LOADING ||
           (status === Status.LOADING && noResults)
       )}
     >
