a14fc8ddbfd2a46ede6794057089c70c3ae38a25
Dylan Jeffers <dylan@audius.co>
2022-09-23 22:12:17 -0700

    Fix now-playing navigation (#1992)

--- apps/audius-client/packages/mobile/src/screens/app-screen/AppTabBar.tsx
+++ apps/audius-client/packages/mobile/src/screens/app-screen/AppTabBar.tsx
@@ -1,11 +1,14 @@
 import { useRef } from 'react'
 
 import type { BottomTabBarProps } from '@react-navigation/bottom-tabs'
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { Animated } from 'react-native'
 
 import { BottomTabBar } from 'app/components/bottom-tab-bar'
 import { FULL_DRAWER_HEIGHT } from 'app/components/drawer'
 import NowPlayingDrawer from 'app/components/now-playing-drawer'
+
+import type { AppTabScreenParamList } from './AppTabScreen'
 
 type TabBarProps = BottomTabBarProps
 
@@ -14,10 +17,17 @@ export const AppTabBar = (props: TabBarProps) => {
   // When the drawer is open, the bottom bar should hide (animated away).
   // When the drawer is closed, the bottom bar should reappear (animated in).
   const translationAnim = useRef(new Animated.Value(FULL_DRAWER_HEIGHT)).current
+  const { navigation } = props
+  // For some reason bottom-bar navigation doesn't have .push in type
+  const nowPlayingNavigation =
+    navigation as unknown as NativeStackNavigationProp<AppTabScreenParamList>
 
   return (
     <>
-      <NowPlayingDrawer translationAnim={translationAnim} />
+      <NowPlayingDrawer
+        navigation={nowPlayingNavigation}
+        translationAnim={translationAnim}
+      />
       <BottomTabBar {...props} translationAnim={translationAnim} />
     </>
   )
