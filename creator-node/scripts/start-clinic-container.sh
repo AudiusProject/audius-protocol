01959a1736cb25c70ade4c2c70c265d137ffe93c
Johannes Naylor <jonaylor89@gmail.com>
2022-09-26 21:48:06 +0000

    update script

--- creator-node/scripts/start-clinic-container.sh
+++ creator-node/scripts/start-clinic-container.sh
@@ -1,7 +1,19 @@
 
+CREATOR_NODE_HOST_PORT=4040
+CREATOR_NODE_DEBUGGER_PORT=4041
+
 docker run \
+  --name audius-protocol-creator-node-load-test \
   --network audius-protocol_default \
-  --env-file ./env/base.env \
+  -v ./.clinic:/usr/src/app/.clinic \
+  -v ./compose/env/tmp/file-storage-${SP_OWNER_WALLET_INDEX}:/file_storage \
+  -v .:/usr/src/app \
+  -v /usr/src/app/node_modules \
+  -v ../libs:/usr/src/audius-libs \
+  -v ../solana-programs:/usr/src/solana-programs \
+  -p ${CREATOR_NODE_HOST_PORT}:${CREATOR_NODE_HOST_PORT} \
+  -p ${CREATOR_NODE_DEBUGGER_PORT}:${CREATOR_NODE_DEBUGGER_PORT} \
+  --env-file ./compose/env/base.env \
   -e redisHost=${COMPOSE_PROJECT_NAME}_creator-node-redis_1 \
   -e dbUrl=postgres://postgres:postgres@${COMPOSE_PROJECT_NAME}_creator-node-db_1:5432/audius_creator_node \
   -e WAIT_HOSTS=${COMPOSE_PROJECT_NAME}_creator-node-db_1:5432, ${COMPOSE_PROJECT_NAME}_creator-node-redis_1:6379 \
@@ -15,21 +27,5 @@ docker run \
   -e devMode=${DEV_MODE} \
   -e spOwnerWallet=${spOwnerWallet} \
   -e contentCacheLayerEnabled=${contentCacheLayerEnabled} \
-  -e loadTest=true \
-
-  -p ${CREATOR_NODE_HOST_PORT}:${CREATOR_NODE_HOST_PORT} \
-  -p ${CREATOR_NODE_DEBUGGER_PORT}:${CREATOR_NODE_DEBUGGER_PORT} \
-
-  # clinic output
-  -v $PROTOCOL_DIR/creator-node/.clinic:/usr/src/app/.clinic \
-  -v ./env/tmp/file-storage-${SP_OWNER_WALLET_INDEX}:/file_storage
-  -v ..:/usr/src/app
-  # Prevent hiding node_modules on container with host volume
-  # From https://github.com/barrysteyn/node-scrypt/issues/148
-  -v /usr/src/app/node_modules
-  # Mount audius libs as a directory
-  -v ../../libs:/usr/src/audius-libs
-  # Mount solana-programs
-  -v ../../solana-programs:/usr/src/solana-programs
-  --rm \
+  -e loadTest='true' \
   audius-protocol-creator-node
