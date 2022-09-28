76f68deeed229023878a60f9c2fcf380c70d5e2b
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-09-28 11:57:51 -0500

    Add additional script for dev setup

---
+++ dev-tools/setup-dev.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This script is meant for debian/ubuntu systems; support for other os might be added in the future
+
+# Install nvm and required node versions
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+for dir in contracts creator-node eth-contracts identity-service libs; do
+    cd "$PROTOCOL_DIR/$dir"
+    nvm install
+done
