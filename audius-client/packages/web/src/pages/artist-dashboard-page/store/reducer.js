eb7e46fca05fc8f55993f9acb67d57019e402008
Kyle Shanks <kyle.j.shanks@gmail.com>
2022-09-28 14:43:53 -0400

    Add new table for Artist Dashboard (#2018)

--- audius-client/packages/web/src/pages/artist-dashboard-page/store/reducer.js
+++ audius-client/packages/web/src/pages/artist-dashboard-page/store/reducer.js
@@ -4,12 +4,16 @@ import {
   FETCH_DASHBOARD,
   FETCH_DASHBOARD_SUCCEEDED,
   FETCH_DASHBOARD_FAILED,
+  FETCH_DASHBOARD_TRACKS,
+  FETCH_DASHBOARD_TRACKS_SUCCEEDED,
+  FETCH_DASHBOARD_TRACKS_FAILED,
   FETCH_DASHBOARD_LISTEN_DATA_SUCCEEDED,
   FETCH_DASHBOARD_LISTEN_DATA_FAILED
 } from './actions'
 
 const initialState = {
   status: Status.LOADING,
+  tracksStatus: Status.IDLE,
   tracks: [],
   unlistedTracks: [],
   collections: [],
@@ -17,6 +21,26 @@ const initialState = {
 }
 
 const actionsMap = {
+  [FETCH_DASHBOARD_TRACKS](state, action) {
+    return {
+      ...state,
+      tracksStatus: Status.LOADING
+    }
+  },
+  [FETCH_DASHBOARD_TRACKS_SUCCEEDED](state, action) {
+    return {
+      ...state,
+      tracks: action.tracks,
+      unlistedTracks: action.unlistedTracks,
+      tracksStatus: Status.SUCCESS
+    }
+  },
+  [FETCH_DASHBOARD_TRACKS_FAILED](state, action) {
+    return {
+      ...state,
+      tracksStatus: Status.ERROR
+    }
+  },
   [FETCH_DASHBOARD](state, action) {
     return {
       ...state,
