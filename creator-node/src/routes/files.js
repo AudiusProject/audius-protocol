0f1c4c1fb000a988a6675a31e682936627c77cfa
vicky <vicky@audius.co>
2022-09-29 19:25:22 +0000

    fix test

--- creator-node/src/routes/files.js
+++ creator-node/src/routes/files.js
@@ -217,8 +217,8 @@ const getCID = async (req, res) => {
   let fileFoundOnFS = false
   let storagePath, fsStats
 
-  for (let i = 0; i < storagePath.length; i++) {
-    const { path: _storagePath, pathType: _pathType } = storagePath[i]
+  for (let i = 0; i < storagePaths.length; i++) {
+    const { path: _storagePath, pathType: _pathType } = storagePaths[i]
 
     try {
       /**
