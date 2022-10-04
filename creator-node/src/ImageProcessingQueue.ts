6d80ec33c43e189c7b3f53ee6c4f15bd9d154fb2
Johannes Naylor <jonaylor89@gmail.com>
2022-10-04 15:45:03 +0000

    lint

--- creator-node/src/ImageProcessingQueue.ts
+++ creator-node/src/ImageProcessingQueue.ts
@@ -1,4 +1,5 @@
 import type { PrometheusRegistry } from './services/prometheusMonitoring/prometheusRegistry'
+
 import { Queue, QueueEvents, Worker } from 'bullmq'
 import path from 'path'
 import os from 'os'
