396d63c9c33bc329ce618ace87f7c2b50937d8c4
Dylan Jeffers <dylan@audius.co>
2022-10-03 20:58:26 -0700

    [C-1224] Prevent duplicate navigation pushes (#2053)

--- packages/mobile/src/hooks/useNavigation.ts
+++ packages/mobile/src/hooks/useNavigation.ts
@@ -1,10 +1,11 @@
-import { useCallback, useMemo } from 'react'
+import { useMemo } from 'react'
 
 // eslint-disable-next-line import/no-unresolved
 import type { DrawerNavigationHelpers } from '@react-navigation/drawer/lib/typescript/src/types'
 import type { ParamListBase } from '@react-navigation/native'
 import { useNavigation as useNavigationNative } from '@react-navigation/native'
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { throttle } from 'lodash'
 
 import type { AppTabScreenParamList } from 'app/screens/app-screen/AppTabScreen'
 
@@ -26,30 +27,35 @@ export const useNavigation = <
     useNavigationNative<NativeStackNavigationProp<ParamList>>()
   const nativeNavigation = customNativeNavigation || defaultNativeNavigation
 
-  const performNavigation = useCallback(
-    (method) =>
-      <RouteName extends keyof ParamList>(
-        ...config: UseNavigationConfig<ParamList, RouteName>
-      ) => {
-        const [screen, params] = config
-        method(screen, params)
-      },
+  const performNavigation = useMemo(
+    () =>
+      throttle(
+        (method) =>
+          <RouteName extends keyof ParamList>(
+            ...config: UseNavigationConfig<ParamList, RouteName>
+          ) => {
+            const [screen, params] = config
+            method(screen, params)
+          },
+        1000,
+        { leading: true }
+      ),
     []
   )
 
   return useMemo(
     () => ({
       ...nativeNavigation,
-      navigate: performNavigation(nativeNavigation.navigate),
+      navigate: performNavigation(nativeNavigation.navigate)!,
       push:
         'push' in nativeNavigation
-          ? performNavigation(nativeNavigation.push)
+          ? performNavigation!(nativeNavigation.push)!
           : () => {
               console.error('Push is not implemented for this navigator')
             },
       replace:
         'replace' in nativeNavigation
-          ? performNavigation(nativeNavigation.replace)
+          ? performNavigation(nativeNavigation.replace)!
           : () => {
               console.error('Replace is not implemented for this navigator')
             },
