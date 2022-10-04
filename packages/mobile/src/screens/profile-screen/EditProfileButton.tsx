1526acf9e318ea3a22e58f07ae7d6cee3ce2cb6c
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2022-10-04 15:34:42 -0400

    [C-1233] hide edit profile when offline (#2063)

--- packages/mobile/src/screens/profile-screen/EditProfileButton.tsx
+++ packages/mobile/src/screens/profile-screen/EditProfileButton.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 
 import type { ButtonProps } from 'app/components/core'
 import { Button } from 'app/components/core'
+import { OnlineOnly } from 'app/components/offline-placeholder/OnlineOnly'
 import { useNavigation } from 'app/hooks/useNavigation'
 
 import type { ProfileTabScreenParamList } from '../app-screen/ProfileTabScreen'
@@ -16,12 +17,14 @@ export const EditProfileButton = (props: EditProfileButtonProps) => {
   }, [navigation])
 
   return (
-    <Button
-      title='Edit Profile'
-      variant='secondary'
-      onPress={handlePress}
-      size='small'
-      {...props}
-    />
+    <OnlineOnly>
+      <Button
+        title='Edit Profile'
+        variant='secondary'
+        onPress={handlePress}
+        size='small'
+        {...props}
+      />
+    </OnlineOnly>
   )
 }
