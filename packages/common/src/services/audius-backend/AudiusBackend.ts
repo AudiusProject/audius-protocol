6efdd6e8585a776d7962364a72abcd07fdf276db
Raymond Jacobson <ray@audius.co>
2022-10-05 09:58:22 -0700

    [C-1185] Get user challenge polling daemon working in native (#2077)

--- packages/common/src/services/audius-backend/AudiusBackend.ts
+++ packages/common/src/services/audius-backend/AudiusBackend.ts
@@ -3206,7 +3206,7 @@ export const audiusBackend = ({
       address
     )
     if (!waudioBalance) {
-      console.error(`Failed to get waudio balance for address: ${address}`)
+      console.warn(`Failed to get waudio balance for address: ${address}`)
       return new BN('0')
     }
     return waudioBalance
