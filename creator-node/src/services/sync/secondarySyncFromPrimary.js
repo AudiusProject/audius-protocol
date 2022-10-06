e62b2e57afb44e9f0d844d3208ddacf01a7fe790
Theo Ilie <theoilie.ti@gmail.com>
2022-10-06 14:52:07 -0700

    Delete files from disk when deleting from db (#3954)

--- creator-node/src/services/sync/secondarySyncFromPrimary.js
+++ creator-node/src/services/sync/secondarySyncFromPrimary.js
@@ -4,6 +4,7 @@ const { logger: genericLogger, createChildLogger } = require('../../logging')
 const config = require('../../config')
 const models = require('../../models')
 const { saveFileForMultihashToFS } = require('../../fileManager')
+const DiskManager = require('../../diskManager')
 const {
   getOwnEndpoint,
   getUserReplicaSetEndpointsFromDiscovery
@@ -116,6 +117,23 @@ const handleSyncFromPrimary = async ({
         }
       }
 
+      // Store this user's file paths in redis to delete later (after wiping db)
+      let numFilesToDelete = 0
+      try {
+        numFilesToDelete = await DiskManager.gatherCNodeUserDataToDelete(
+          wallet,
+          logger
+        )
+      } catch (error) {
+        await DiskManager.clearFilePathsToDelete(wallet)
+        errorResponse = {
+          error,
+          result: 'failure_delete_disk_data'
+        }
+
+        throw error
+      }
+
       /**
        * Wipe local DB state
        *
@@ -132,10 +150,31 @@ const handleSyncFromPrimary = async ({
       }
 
       if (deleteError) {
+        await DiskManager.clearFilePathsToDelete(wallet)
         error = deleteError
         errorResponse = {
           error,
           result: 'failure_delete_db_data'
+        }
+
+        throw error
+      }
+
+      // Wipe disk - delete files whose paths we stored in redis earlier
+      try {
+        const numFilesDeleted =
+          await DiskManager.deleteAllCNodeUserDataFromDisk(
+            wallet,
+            numFilesToDelete,
+            logger
+          )
+        logger.info(
+          `Deleted ${numFilesDeleted}/${numFilesToDelete} files for ${wallet}`
+        )
+      } catch (error) {
+        errorResponse = {
+          error,
+          result: 'failure_delete_disk_data'
         }
 
         throw error
