0b6491f150e02a4962db018bec03e2b379e217c5
Johannes Naylor <jonaylor89@gmail.com>
2022-10-06 15:01:04 +0000

    type syncqueue component service

--- creator-node/src/components/replicaSet/syncQueueComponentService.ts
+++ creator-node/src/components/replicaSet/syncQueueComponentService.ts
@@ -1,8 +1,19 @@
+import type { SpanContext } from '@opentelemetry/api'
+
 /**
  * Enqueues sync operation into syncQueue for provided walletPublicKeys against provided creatorNodeEndpoint
  * @param {Object} params See the SyncQueue for explicit params
  */
-const enqueueSync = async (params) => {
+export const enqueueSync = async (params: {
+  wallet: string
+  creatorNodeEndpoint: string
+  blockNumber: number
+  forceResyncConfig: boolean
+  forceWipe: boolean
+  logContext: Object
+  parentSpanContext: SpanContext
+  serviceRegistry: any
+}) => {
   const { serviceRegistry } = params
   // eslint-disable-next-line node/no-sync
   await serviceRegistry.syncQueue.enqueueSync(params)
@@ -12,7 +23,16 @@ const enqueueSync = async (params) => {
  * Enqueues an sync of MANUAL that will operate immediately
  * @param {Object} params See the SyncImmediateQueue for explicit params
  */
-const processManualImmediateSync = async (params) => {
+export const processManualImmediateSync = async (params: {
+  wallet: string
+  creatorNodeEndpoint: string
+  blockNumber: number
+  forceResyncConfig: boolean
+  forceWipe: boolean
+  logContext: Object
+  parentSpanContext: SpanContext
+  serviceRegistry: any
+}) => {
   const { serviceRegistry } = params
   // eslint-disable-next-line node/no-sync
   await serviceRegistry.syncImmediateQueue.processManualImmediateSync(params)
