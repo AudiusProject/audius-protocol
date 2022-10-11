e43b6e92faa67b07d5a4a334c0f5371b65652067
Sid Sethi <3323835+SidSethi@users.noreply.github.com>
2022-10-11 14:17:56 -0400

    CON-451 CN track upload validation improvements (#4080)

--- creator-node/src/routes/tracks.js
+++ creator-node/src/routes/tracks.js
@@ -379,6 +379,9 @@ const validateTrackOwner = async ({
       asyncFn,
       logger,
       log: false,
+      minTimeout: 1000,
+      maxTimeout: Infinity,
+      factor: 2,
       retries: 10
     })
   } catch (e) {
