0c91259be9bf5cbaedc5fa7e6a2a833d4fa69db0
Sebastian Klingler <sliptype@gmail.com>
2022-10-13 11:32:30 -0500

    Prepend audius url to share url (#2142)

--- apps/audius-client/packages/mobile/src/utils/share.ts
+++ apps/audius-client/packages/mobile/src/utils/share.ts
@@ -1,4 +1,5 @@
 import { Share } from 'react-native'
+import Config from 'react-native-config'
 
 // Docs for react native share: https://facebook.github.io/react-native/docs/share
 
@@ -8,9 +9,11 @@ type ShareProps = {
 }
 
 const share = async ({ message, url }: ShareProps) => {
+  const fullUrl = `${Config.AUDIUS_URL}${url}`
+
   try {
     // on iOS we can have both message and URL, on android, we need to concat
-    const context = { message: message ? `${message} ${url}` : url }
+    const context = { message: message ? `${message} ${fullUrl}` : fullUrl }
     await Share.share(context)
   } catch (error) {
     console.error(error)
