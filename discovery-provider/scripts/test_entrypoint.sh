4c96885c28dcd32a391b0f1f918dfeb70240387e
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2022-10-18 23:37:51 -0500

    Speedup logic

--- discovery-provider/scripts/test_entrypoint.sh
+++ discovery-provider/scripts/test_entrypoint.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
+echo "Starting postgres"
+sudo -u postgres pg_ctl start -D /db -o "-c shared_preload_libraries=pg_stat_statements"
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+export audius_db_url="postgresql+psycopg2://postgres:postgres@localhost:5432/audius_discovery"
+export audius_db_url_read_replica="postgresql+psycopg2://postgres:postgres@localhost:5432/audius_discovery"
+
 echo "Starting redis"
 redis-server --daemonize yes
 export audius_redis_url="redis://localhost:6379/00"
 
-echo "Starting postgres"
-chown -R postgres:postgres /db
-chmod 700 /db
-sudo -u postgres pg_ctl init -D /db
-echo "host all all 0.0.0.0/0 md5" >>/db/pg_hba.conf
-echo "listen_addresses = '*'" >>/db/postgresql.conf
-sudo -u postgres pg_ctl start -D /db -o "-c shared_preload_libraries=pg_stat_statements"
-sudo -u postgres createdb audius_discovery
-
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD '${postgres_password:-postgres}';"
-
-export audius_db_url="postgresql+psycopg2://postgres:${postgres_password:-postgres}@localhost:5432/audius_discovery"
-export audius_db_url_read_replica="postgresql+psycopg2://postgres:${postgres_password:-postgres}@localhost:5432/audius_discovery"
+if nslookup "discovery-provider-elasticsearch" >/dev/null 2>&1; then
+    export audius_elasticsearch_url="http://discovery-provider-elasticsearch:9200"
+    export audius_elasticsearch_run_indexer="true"
+fi
 
 echo "Waiting for redis and postgres"
 WAIT_HOSTS="localhost:6379,localhost:5432" /wait
