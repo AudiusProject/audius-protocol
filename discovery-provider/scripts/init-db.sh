b6f83112870b7a5c8d49efea2b2a31dcabf1c3f1
Cheran <cheran.v.senthil@gmail.com>
2022-10-19 08:35:32 -0500

    Add flow for testing discovery provider (#4116)

--- discovery-provider/scripts/init-db.sh
+++ discovery-provider/scripts/init-db.sh
@@ -5,3 +5,9 @@ adduser --disabled-password postgres
 mkdir /run/postgresql /db
 chown -R postgres:postgres /run/postgresql /db
 chmod 700 /db
+sudo -u postgres pg_ctl init -D /db
+echo "host all all 0.0.0.0/0 md5" >>/db/pg_hba.conf
+echo "listen_addresses = '*'" >>/db/postgresql.conf
+sudo -u postgres pg_ctl start -D /db -o "-c shared_preload_libraries=pg_stat_statements"
+sudo -u postgres createdb audius_discovery
+sudo -u postgres pg_ctl stop -D /db
