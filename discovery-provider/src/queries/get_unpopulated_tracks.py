d1144075703e862aebe3ae8f8ca36c3c8c8a252e
isaac <isaac@audius.co>
2022-10-24 20:17:06 +0000

    Remove unpopulated cache for tracks, users, playlists

--- discovery-provider/src/queries/get_unpopulated_tracks.py
+++ discovery-provider/src/queries/get_unpopulated_tracks.py
@@ -4,40 +4,14 @@ from datetime import datetime
 from dateutil import parser
 from src.models.tracks.track import Track
 from src.utils import helpers, redis_connection
-from src.utils.redis_cache import (
-    get_all_json_cached_key,
-    get_track_id_cache_key,
-    set_json_cached_key,
-)
 
 logger = logging.getLogger(__name__)
 
-# Cache unpopulated tracks for 5 min
-ttl_sec = 5 * 60
 
 track_datetime_fields = []
 for column in Track.__table__.c:
     if column.type.python_type == datetime:
         track_datetime_fields.append(column.name)
-
-
-def get_cached_tracks(track_ids):
-    redis_track_id_keys = list(map(get_track_id_cache_key, track_ids))
-    redis = redis_connection.get_redis()
-    tracks = get_all_json_cached_key(redis, redis_track_id_keys)
-    for track in tracks:
-        if track:
-            for field in track_datetime_fields:
-                if track[field]:
-                    track[field] = parser.parse(track[field])
-    return tracks
-
-
-def set_tracks_in_cache(tracks):
-    redis = redis_connection.get_redis()
-    for track in tracks:
-        key = get_track_id_cache_key(track["track_id"])
-        set_json_cached_key(redis, key, track, ttl_sec)
 
 
 def get_unpopulated_tracks(
@@ -61,33 +35,11 @@ def get_unpopulated_tracks(
     Returns:
         Array of tracks
     """
-    # Check the cached tracks
-    cached_tracks_results = get_cached_tracks(track_ids)
-    has_all_tracks_cached = cached_tracks_results.count(None) == 0
-    if has_all_tracks_cached:
-        res = cached_tracks_results
-        if filter_deleted:
-            res = list(filter(lambda track: not track["is_delete"], res))
-        if filter_unlisted:
-            res = list(filter(lambda track: not track["is_unlisted"], res))
-        if exclude_premium:
-            res = list(filter(lambda track: not track["is_premium"], res))
-        return res
-
-    # Create a dict of cached tracks
-    cached_tracks = {}
-    for cached_track in cached_tracks_results:
-        if cached_track:
-            cached_tracks[cached_track["track_id"]] = cached_track
-
-    track_ids_to_fetch = filter(
-        lambda track_id: track_id not in cached_tracks, track_ids
-    )
 
     tracks_query = (
         session.query(Track)
         .filter(Track.is_current == True, Track.stem_of == None)
-        .filter(Track.track_id.in_(track_ids_to_fetch))
+        .filter(Track.track_id.in_(track_ids))
     )
 
     if filter_unlisted:
@@ -104,19 +56,9 @@ def get_unpopulated_tracks(
     queried_tracks = {track["track_id"]: track for track in tracks}
 
     # cache tracks for future use
-    set_tracks_in_cache(tracks)
 
     tracks_response = []
     for track_id in track_ids:
-        if track_id in cached_tracks:
-            if filter_unlisted and cached_tracks[track_id]["is_unlisted"]:
-                continue
-            if filter_deleted and cached_tracks[track_id]["is_delete"]:
-                continue
-            if exclude_premium and cached_tracks[track_id]["is_premium"]:
-                continue
-            tracks_response.append(cached_tracks[track_id])
-        elif track_id in queried_tracks:
-            tracks_response.append(queried_tracks[track_id])
+        tracks_response.append(queried_tracks[track_id])
 
     return tracks_response
--- discovery-provider/src/queries/get_unpopulated_users.py
+++ discovery-provider/src/queries/get_unpopulated_users.py
@@ -4,40 +4,13 @@ from datetime import datetime
 from dateutil import parser
 from src.models.users.user import User
 from src.utils import helpers, redis_connection
-from src.utils.redis_cache import (
-    get_all_json_cached_key,
-    get_user_id_cache_key,
-    set_json_cached_key,
-)
 
 logger = logging.getLogger(__name__)
-
-# Cache unpopulated users for 5 min
-ttl_sec = 5 * 60
 
 user_datetime_fields = []
 for column in User.__table__.c:
     if column.type.python_type == datetime:
         user_datetime_fields.append(column.name)
-
-
-def get_cached_users(user_ids):
-    redis_user_id_keys = list(map(get_user_id_cache_key, user_ids))
-    redis = redis_connection.get_redis()
-    users = get_all_json_cached_key(redis, redis_user_id_keys)
-    for user in users:
-        if user:
-            for field in user_datetime_fields:
-                if user[field]:
-                    user[field] = parser.parse(user[field])
-    return users
-
-
-def set_users_in_cache(users):
-    redis = redis_connection.get_redis()
-    for user in users:
-        key = get_user_id_cache_key(user["user_id"])
-        set_json_cached_key(redis, key, user, ttl_sec)
 
 
 def get_unpopulated_users(session, user_ids):
@@ -52,34 +25,19 @@ def get_unpopulated_users(session, user_ids):
     Returns:
         Array of users
     """
-    cached_users_results = get_cached_users(user_ids)
-    has_all_users_cached = cached_users_results.count(None) == 0
-    if has_all_users_cached:
-        return cached_users_results
-
-    cached_users = {}
-    for cached_user in cached_users_results:
-        if cached_user:
-            cached_users[cached_user["user_id"]] = cached_user
-
-    user_ids_to_fetch = filter(lambda user_id: user_id not in cached_users, user_ids)
 
     users = (
         session.query(User)
         .filter(User.is_current == True, User.wallet != None, User.handle != None)
-        .filter(User.user_id.in_(user_ids_to_fetch))
+        .filter(User.user_id.in_(user_ids))
         .all()
     )
     users = helpers.query_result_to_list(users)
     queried_users = {user["user_id"]: user for user in users}
 
-    set_users_in_cache(users)
 
     users_response = []
     for user_id in user_ids:
-        if user_id in cached_users:
-            users_response.append(cached_users[user_id])
-        elif user_id in queried_users:
-            users_response.append(queried_users[user_id])
+        users_response.append(queried_users[user_id])
 
     return users_response
