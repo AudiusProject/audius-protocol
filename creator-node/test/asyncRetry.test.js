0099f7e191bf68ed2ab0ae9ff9ed5f84137ea691
vicky <vicky@audius.co>
2022-10-24 23:12:55 +0000

    remove peersetmanager test + update async retry

--- creator-node/test/asyncRetry.test.js
+++ creator-node/test/asyncRetry.test.js
@@ -2,6 +2,27 @@ const assert = require('assert')
 const { asyncRetry } = require('../src/utils/asyncRetry')
 
 describe('test asyncRetry', function () {
+  it('will retry the default number of times', async function () {
+    const defaultNumberOfRetries = 5
+
+    let attempts = 0
+    try {
+      await asyncRetry({
+        asyncFn: async (bail, num) => {
+          attempts++
+          throw new Error(`Test ${num}`)
+        },
+        minTimeout: 0,
+        maxTimeout: 100
+      })
+
+      assert.fail('Should have thrown')
+    } catch (e) {
+      // First attempt = 1 + Default retries = 5 => 6 total attempts
+      assert.deepStrictEqual(attempts, defaultNumberOfRetries + 1)
+    }
+  })
+
   it('does not retry on bails', async function () {
     try {
       await asyncRetry({
@@ -69,6 +90,37 @@ describe('test asyncRetry', function () {
       assert.fail('Should have thrown')
     } catch (e) {
       assert.deepStrictEqual(e.message, 'vicky')
+    }
+  })
+
+  it('retry should not exceed input desired time', async function () {
+    let attempts = 0
+    try {
+      await asyncRetry({
+        asyncFn: async (bail, num) => {
+          attempts++
+
+          await new Promise((resolve) => {
+            setTimeout(() => {
+              resolve()
+            }, 200)
+          })
+
+          throw new Error('force retry')
+        },
+        options: {
+          minTimeout: 0,
+          maxTimeout: 100,
+          maxRetryTime: 401,
+          onRetry: console.log
+        }
+      })
+
+      assert.fail('Should have thrown')
+    } catch (e) {
+      // Should have been 2 attempts because maxRetryTime=401ms and each async fn call = 200ms
+      // So, a total of at most 2 attempts could have run, even though the default number of retries > 2
+      assert.deepStrictEqual(attempts, 2)
     }
   })
 })
