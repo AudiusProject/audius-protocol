8d5eff5a587a3332deaa1192f9ea4b9841b8d665
Sid Sethi <3323835+SidSethi@users.noreply.github.com>
2022-10-26 21:27:03 +0000

    nit

--- creator-node/src/utils/decisionTree.ts
+++ creator-node/src/utils/decisionTree.ts
@@ -69,33 +69,36 @@ module.exports = class DecisionTree {
     this.tree.push(stage)
 
     if (log) {
-      this.printLastStage(logLevel)
+      this.printLastStage({ logLevel })
     }
   }
 
   printTree({ logLevel = 'info' }) {
-    this._log(
-      `DecisionTree Full - ${JSON.stringify(this.tree, null, 2)}`,
+    this._log({
+      msg: `DecisionTree Full - ${JSON.stringify(this.tree, null, 2)}`,
       logLevel
-    )
+    })
   }
 
-  printLastStage(logLevel = 'info') {
+  printLastStage({ logLevel = 'info' }) {
     if (this.tree.length > 0) {
-      this._log(
-        `DecisionTree Last Stage - ${JSON.stringify(
+      this._log({
+        msg: `DecisionTree Last Stage - ${JSON.stringify(
           this.tree[this.tree.length - 1],
           null,
           2
         )}`,
         logLevel
-      )
+      })
     } else {
-      this._log('DecisionTree Last Stage - empty', logLevel)
+      this._log({
+        msg: 'DecisionTree Last Stage - empty',
+        logLevel
+      })
     }
   }
 
-  private _log(msg: string, logLevel = 'info') {
+  private _log({ msg = '', logLevel = 'info' }) {
     let logFn
     switch (logLevel) {
       case 'error':
