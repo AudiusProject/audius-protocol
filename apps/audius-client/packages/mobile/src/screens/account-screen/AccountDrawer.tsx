23625b8b2bfa2368fe4ad6770332fd5642caca41
Dylan Jeffers <dylan@audius.co>
2022-10-28 13:46:03 -0700

    Truncate balance in account-drawer (#2197)

--- apps/audius-client/packages/mobile/src/screens/account-screen/AccountDrawer.tsx
+++ apps/audius-client/packages/mobile/src/screens/account-screen/AccountDrawer.tsx
@@ -196,7 +196,7 @@ export const AccountDrawer = (props: AccountDrawerProps) => {
             width={spacing(7)}
           />
           <Text fontSize='large' weight='heavy'>
-            {totalBalance ? formatWei(totalBalance) : 0}
+            {totalBalance ? formatWei(totalBalance, true) : 0}
           </Text>
         </TouchableOpacity>
       </View>
