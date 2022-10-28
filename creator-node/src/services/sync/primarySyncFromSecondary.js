859420f7d7dbce06e09155946df28ced6f5b78fc
Theo Ilie <theoilie.ti@gmail.com>
2022-10-28 16:27:24 +0000

    Remove redis primarySyncFromSecondary fail-safes

--- creator-node/src/services/sync/primarySyncFromSecondary.js
+++ creator-node/src/services/sync/primarySyncFromSecondary.js
@@ -669,14 +669,9 @@ async function filterOutAlreadyPresentDBEntries({
       FETCHED_ENTRIES_SET_KEY,
       fetchedEntriesComparable
     )
-    // Fail-safe in case for some reason not all entries were written to redis
-    if (numFetchedEntriesAdded !== fetchedEntries.length) {
-      throw new Error(
-        `Failed to add all entries to redis set for ${FETCHED_ENTRIES_SET_KEY}`
-      )
-    }
     decisionTree.recordStage({
       name: 'filterOutAlreadyPresentDBEntries() Set FETCHED_ENTRIES_SET_KEY',
+      data: { numFetchedEntriesAdded },
       log: true
     })
 
@@ -724,12 +719,6 @@ async function filterOutAlreadyPresentDBEntries({
         LOCAL_DB_ENTRIES_SET_KEY,
         localEntriesComparable
       )
-      // Fail-safe in case for some reason not all entries were written to redis
-      if (numLocalEntriesAddedBatch !== localEntries.length) {
-        throw new Error(
-          `Failed to add all entries to redis set for ${LOCAL_DB_ENTRIES_SET_KEY}`
-        )
-      }
       numLocalEntriesAdded += numLocalEntriesAddedBatch
 
       // Move pagination cursor
