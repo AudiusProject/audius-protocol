8c05758a4a3ede929ccb18ce7649599154554a2b
isaac <isaac@audius.co>
2022-10-31 16:03:12 +0000

    Fix health check with web3 poa middleware

--- discovery-provider/src/utils/web3_provider.py
+++ discovery-provider/src/utils/web3_provider.py
@@ -8,6 +8,7 @@ from src.utils import helpers
 from src.utils.config import shared_config
 from src.utils.multi_provider import MultiProvider
 from web3 import HTTPProvider, Web3
+from web3.middleware import geth_poa_middleware
 
 web3: Optional[Web3] = None
 
@@ -18,6 +19,11 @@ def get_web3():
     if not web3:
         web3endpoint = helpers.get_web3_endpoint(shared_config)
         web3 = Web3(HTTPProvider(web3endpoint))
+        if shared_config["discprov"]["env"] == "stage":
+            # required middleware for POA
+            # https://web3py.readthedocs.io/en/latest/middleware.html#proof-of-authority
+            web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
         return web3
     return web3
 
