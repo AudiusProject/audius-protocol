580b5a3475a451825ebec6513ab1e9df46cfc837
Cheran <cheran.v.senthil@gmail.com>
2022-10-31 11:47:41 -0500

    Move local startup code (#4228)

--- dev-tools/startup/startup.sh
+++ dev-tools/startup/startup.sh
@@ -5,12 +5,13 @@
 name=$(nslookup $(hostname -i) | grep -o "\(creator-node\|discovery-provider\|identity-service\)-[0-9]\+")
 replica=$(echo $name | grep -o "[0-9]\+")
 
-if [[ -f "/tmp/dev-tools/startup/${name%-*}.sh" ]]; then
-  echo "Loading ${name%-*}.sh"
-  . "/tmp/dev-tools/startup/${name%-*}.sh"
-fi
-
 if [[ -f "/tmp/dev-tools/startup/$name.sh" ]]; then
   echo "Loading $name.sh"
   . "/tmp/dev-tools/startup/$name.sh"
+fi
+
+# -z "$startup_done" exists to allow name-replica.sh be the only script that is run
+if [[ -z "$startup_done" ]] && [[ -f "/tmp/dev-tools/startup/${name%-*}.sh" ]]; then
+  echo "Loading ${name%-*}.sh"
+  . "/tmp/dev-tools/startup/${name%-*}.sh"
 fi
