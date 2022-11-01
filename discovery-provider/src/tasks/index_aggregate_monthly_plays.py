87a616bf2bd9b9f58d72ea553d12a6f38d0cac9c
isaac <isaac@audius.co>
2022-11-01 21:48:16 +0000

    Rebuild aggregate monthly plays

--- discovery-provider/src/tasks/index_aggregate_monthly_plays.py
+++ discovery-provider/src/tasks/index_aggregate_monthly_plays.py
@@ -54,7 +54,8 @@ def _index_aggregate_monthly_plays(session):
     )
 
     # get the new latest checkpoint
-    new_id_checkpoint = (session.query(func.max(Play.id))).scalar()
+    most_recent_play_id = (session.query(func.max(Play.id))).scalar()
+    new_id_checkpoint = min(most_recent_play_id - prev_id_checkpoint, 10000) + prev_id_checkpoint
 
     if not new_id_checkpoint or new_id_checkpoint == prev_id_checkpoint:
         logger.info(
