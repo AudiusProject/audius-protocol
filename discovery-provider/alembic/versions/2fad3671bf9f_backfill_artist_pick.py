b7e331d25e1db800a97e341beee77ddee04b31a9
Michelle Brier <michelle.brier4@gmail.com>
2022-11-01 04:13:27 +0000

    sql params

--- discovery-provider/alembic/versions/2fad3671bf9f_backfill_artist_pick.py
+++ discovery-provider/alembic/versions/2fad3671bf9f_backfill_artist_pick.py
@@ -19,21 +19,29 @@ depends_on = None
 
 def build_sql(up):
     inner_sql = []
-    path = Path(__file__).parent.joinpath("../csvs/test_user_pinned_track_ids.csv")
+    path = Path(__file__).parent.joinpath("../csvs/staging_pinned_track_ids.csv")
     with open(path, 'r') as f:
+        i = 0
+        params = {}
         for entry in f:
             handle, pinned_track_id = entry.split(",")
             if up:
-                sql = f"UPDATE users SET artist_pick_track_id = {pinned_track_id} where is_current = True and handle = '{handle}';"
+                sql = f"UPDATE users SET artist_pick_track_id = :id{i} where is_current = True and handle = :h{i};"
+                params[f"id{i}"] = pinned_track_id
+                params[f"h{i}"] = handle
             else:
-                sql = f"UPDATE users SET artist_pick_track_id = null where is_current = True and handle = '{handle}';"
+                sql = f"UPDATE users SET artist_pick_track_id = null where is_current = True and handle = :h{i};"
+                params[f"h{i}"] = handle
             inner_sql.append(sql)
-    return sa.text("begin; \n\n " + "\n".join(inner_sql) + " \n\n commit;")
+            i += 1
+    return (sa.text("begin; \n\n " + "\n".join(inner_sql) + " \n\n commit;"), params)
 
 def upgrade():
     connection = op.get_bind()
-    connection.execute(build_sql(True))
+    sql, params = build_sql(True)
+    connection.execute(sql, params)
 
 def downgrade():
     connection = op.get_bind()
-    connection.execute(build_sql(False))
+    sql, params = build_sql(False)
+    connection.execute(sql, params)
