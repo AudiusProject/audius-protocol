9826ab412f3e9eff4cc66aaeb7775d8e6e2f0ec8
sabrina <sabrina.kiamilev@gmail.com>
2022-11-02 05:12:17 +0000

    Add migration file for playlist routes table with backfill query

--- discovery-provider/alembic/versions/bead88b41a20_create_playlist_routes_table.py
+++ discovery-provider/alembic/versions/bead88b41a20_create_playlist_routes_table.py
@@ -33,110 +33,101 @@ def upgrade():
         sa.PrimaryKeyConstraint("owner_id", "slug"),
         sa.Index("playlist_id", "is_current"),
     )
-    # bind = op.get_bind()
-    # session = Session(bind=bind)
+    bind = op.get_bind()
+    session = Session(bind=bind)
 
-    # # Bring over existing routes (current tracks)
-    # session.execute(
-    #     sa.text(
-    #         """
-    #         INSERT INTO playlist_routes (
-    #             playlist_id
-    #             , owner_id
-    #             , slug
-    #             , title_slug
-    #             , collision_id
-    #             , is_current
-    #             , blockhash
-    #             , blocknumber
-    #             , txhash
-    #         )
-    #         SELECT
-    #             playlist_id
-    #             , owner_id
-    #             , CONCAT(SPLIT_PART(route_id, '/', 2),  '-', playlist_id)
-    #                 AS slug
-    #             , CONCAT(SPLIT_PART(route_id, '/', 2),  '-', playlist_id)
-    #                 AS title_slug
-    #             , 0 AS collision_id
-    #             , is_current
-    #             , blockhash
-    #             , blocknumber
-    #             ,txhash
-    #         FROM tracks
-    #         WHERE is_current
-    #         GROUP BY
-    #             owner_id
-    #             , playlist_id
-    #             , route_id
-    #             , is_current
-    #             , blockhash
-    #             , blocknumber
-    #             , txhash;
-    #         """
-    #     )
-    # )
+    # Bring over existing routes (current tracks)
+    session.execute(
+        sa.text(
+            """
+            INSERT INTO playlist_routes (
+                playlist_id
+                , owner_id
+                , slug
+                , title_slug
+                , collision_id
+                , is_current
+                , blockhash
+                , blocknumber
+                , txhash
+            )
+            SELECT
+                playlist_id
+                , playlist_owner_id
+                , CONCAT(REPLACE(LOWER(playlist_name), ' ', '-'),  '-', playlist_id)
+                    AS slug
+                , CONCAT(REPLACE(LOWER(playlist_name), ' ', '-'),  '-', playlist_id)
+                    AS title_slug
+                , 0 AS collision_id
+                , is_current
+                , blockhash
+                , blocknumber
+                ,txhash
+            FROM playlists
+            WHERE is_current
+            GROUP BY
+                playlist_owner_id
+                , playlist_id
+                , playlist_name
+                , is_current
+                , blockhash
+                , blocknumber
+                , txhash;
+            """
+        )
+    )
 
-    # # Bring over existing routes (non-current tracks)
-    # session.execute(
-    #     sa.text(
-    #         """
-    #         INSERT INTO playlist_routes (
-    #             playlist_id
-    #             , owner_id
-    #             , slug
-    #             , title_slug
-    #             , collision_id
-    #             , is_current
-    #             , blockhash
-    #             , blocknumber
-    #             , txhash
-    #         )
-    #         SELECT
-    #             p.playlist_id
-    #             , p.owner_id
-    #             , p.slug
-    #             , p.title_slug
-    #             , p.collision_id
-    #             , p.is_current
-    #             , p.blockhash
-    #             , p.blocknumber
-    #             , p.txhash
-    #         FROM (
-    #             SELECT
-    #                 nc.playlist_id
-    #                 , nc.playlist_owner_id
-    #                 , CONCAT(
-    #                     # TODO: UPDATE ROUTE ID
-    #                         SPLIT_PART(nc.route_id, '/', 2),
-    #                         '-',
-    #                         nc.playlist_id
-    #                     ) AS slug
-    #                 , CONCAT(
-    #                         SPLIT_PART(nc.route_id, '/', 2),
-    #                         '-',
-    #                         nc.playlist_id
-    #                     ) AS title_slug
-    #                 , 0 AS collision_id
-    #                 , nc.is_current
-    #                 , nc.blockhash
-    #                 , nc.blocknumber
-    #                 , nc.txhash
-    #                 , ROW_NUMBER() OVER (
-    #                         PARTITION BY nc.route_id
-    #                         ORDER BY nc.blocknumber DESC
-    #                     ) AS rank
-    #             FROM playlists AS c_playlists
-    #             JOIN playlists AS nc
-    #             ON c_playlists.playlist_id = nc.playlist_id
-    #             WHERE NOT nc.is_current
-    #             AND c_playlists.is_current
-    #             AND NOT nc.route_id = c_playlists.route_id
-    #         ) p
-    #         WHERE p.rank = 1;
-    #         """
-    #     )
-    # )
+    # Bring over existing routes (non-current tracks)
+    session.execute(
+        sa.text(
+            """
+            INSERT INTO playlist_routes (
+                playlist_id
+                , owner_id
+                , slug
+                , title_slug
+                , collision_id
+                , is_current
+                , blockhash
+                , blocknumber
+                , txhash
+            )
+            SELECT
+                p.playlist_id
+                , p.playlist_owner_id
+                , p.slug
+                , p.title_slug
+                , p.collision_id
+                , p.is_current
+                , p.blockhash
+                , p.blocknumber
+                , p.txhash
+            FROM (
+                SELECT
+                    nc.playlist_id
+                    , nc.playlist_owner_id
+                    , CONCAT(REPLACE(LOWER(nc.playlist_name), ' ', '-'),  '-', nc.playlist_id) AS slug
+                    , CONCAT(REPLACE(LOWER(nc.playlist_name), ' ', '-'),  '-', nc.playlist_id) AS title_slug
+                    , 0 AS collision_id
+                    , nc.is_current
+                    , nc.blockhash
+                    , nc.blocknumber
+                    , nc.txhash
+                    , ROW_NUMBER() OVER (
+                            PARTITION BY nc.playlist_name
+                            ORDER BY nc.blocknumber DESC
+                        ) AS rank
+                FROM playlists AS c_playlists
+                JOIN playlists AS nc
+                ON c_playlists.playlist_id = nc.playlist_id
+                WHERE NOT nc.is_current
+                AND c_playlists.is_current
+                AND NOT nc.playlist_name = c_playlists.playlist_name
+            ) p
+            WHERE p.rank = 1;
+            """
+        )
+    )
 
 
 def downgrade():
