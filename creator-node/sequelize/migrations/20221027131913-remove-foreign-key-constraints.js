11fa1376fa075006a00d5ad3a609b3f0fe83817e
Theo Ilie <theoilie.ti@gmail.com>
2022-11-03 16:21:41 +0000

    Fix copy/paste and quotes

--- creator-node/sequelize/migrations/20221027131913-remove-foreign-key-constraints.js
+++ creator-node/sequelize/migrations/20221027131913-remove-foreign-key-constraints.js
@@ -7,28 +7,28 @@ module.exports = {
     )
 
     await queryInterface.sequelize.query(
-      `ALTER TABLE IF EXISTS AudiusUsers DROP CONSTRAINT IF EXISTS "AudiusUsers_coverArtFileUUID_fkey";`,
+      `ALTER TABLE IF EXISTS "AudiusUsers" DROP CONSTRAINT IF EXISTS "AudiusUsers_coverArtFileUUID_fkey";`,
       { transaction }
     )
 
     await queryInterface.sequelize.query(
-      `ALTER TABLE IF EXISTS AudiusUsers DROP CONSTRAINT IF EXISTS "AudiusUsers_metadataFileUUID_fkey";`,
+      `ALTER TABLE IF EXISTS "AudiusUsers" DROP CONSTRAINT IF EXISTS "AudiusUsers_metadataFileUUID_fkey";`,
       { transaction }
     )
 
     await queryInterface.sequelize.query(
-      `ALTER TABLE IF EXISTS AudiusUsers DROP CONSTRAINT IF EXISTS "AudiusUsers_profilePicFileUUID_fkey";`,
+      `ALTER TABLE IF EXISTS "AudiusUsers" DROP CONSTRAINT IF EXISTS "AudiusUsers_profilePicFileUUID_fkey";`,
       { transaction }
     )
 
     await queryInterface.sequelize.query(
       // This trailing space is intentional because the constraint was created with a trailing space in its name
-      `ALTER TABLE IF EXISTS AudiusUsers DROP CONSTRAINT IF EXISTS "Tracks_coverArtFileUUID_fkey ";`,
+      `ALTER TABLE IF EXISTS "Tracks" DROP CONSTRAINT IF EXISTS "Tracks_coverArtFileUUID_fkey ";`,
       { transaction }
     )
 
     await queryInterface.sequelize.query(
-      `ALTER TABLE IF EXISTS AudiusUsers DROP CONSTRAINT IF EXISTS "Tracks_metadataFileUUID_fkey";`,
+      `ALTER TABLE IF EXISTS "Tracks" DROP CONSTRAINT IF EXISTS "Tracks_metadataFileUUID_fkey";`,
       { transaction }
     )
     await transaction.commit()
