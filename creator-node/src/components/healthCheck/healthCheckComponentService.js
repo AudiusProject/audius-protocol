7853453dc9cec6403b451607c456ec8fbe312b7f
Sid Sethi <SidSethi93@gmail.com>
2022-11-07 11:43:51 -0500

    CON-488 Expose CN config in route (excludes sensitive configs) (#4302)

--- creator-node/src/components/healthCheck/healthCheckComponentService.js
+++ creator-node/src/components/healthCheck/healthCheckComponentService.js
@@ -279,8 +279,15 @@ const healthCheckDuration = async () => {
   return { success: true }
 }
 
+const configCheck = async () => {
+  // https://github.com/mozilla/node-convict/tree/master/packages/convict#configtostring
+  const data = config.toString()
+  return data
+}
+
 module.exports = {
   healthCheck,
   healthCheckVerbose,
-  healthCheckDuration
+  healthCheckDuration,
+  configCheck
 }
