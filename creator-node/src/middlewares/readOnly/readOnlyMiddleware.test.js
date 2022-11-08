b9fd77d685aeb36a14f883d256bf9c1b544ce5d3
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2022-11-08 18:38:12 -0500

    Make server read only until spID has been recovered (#4318)
    * Make server read only until spID has been recovered
    
    * More comprehensive spIDNotDefined check

--- creator-node/src/middlewares/readOnly/readOnlyMiddleware.test.js
+++ creator-node/src/middlewares/readOnly/readOnlyMiddleware.test.js
@@ -9,12 +9,15 @@ const { resFactory, loggerFactory } = require('../../../test/lib/reqMock')
 describe('Test read-only middleware', function () {
   beforeEach(function () {
     config.reset('isReadOnlyMode')
+    config.reset('spID')
   })
 
   it('Should pass if read-only enabled and is GET request', function () {
     const method = 'GET'
     const isReadOnlyMode = true
+    const spIDNotDefined = false
     config.set('isReadOnlyMode', isReadOnlyMode)
+    config.set('spID', 1)
     let nextCalled = false
 
     readOnlyMiddleware({ method }, {}, function () {
@@ -22,7 +25,7 @@ describe('Test read-only middleware', function () {
     })
 
     assert.deepStrictEqual(
-      readOnlyMiddlewareHelper(isReadOnlyMode, method),
+      readOnlyMiddlewareHelper(isReadOnlyMode, spIDNotDefined, method),
       true
     )
     assert.deepStrictEqual(nextCalled, true)
@@ -30,8 +33,10 @@ describe('Test read-only middleware', function () {
 
   it('Should fail if read-only enabled and is not GET request', function () {
     const isReadOnlyMode = true
+    const spIDNotDefined = false
     const method = 'POST'
     config.set('isReadOnlyMode', isReadOnlyMode)
+    config.set('spID', 1)
     let nextCalled = false
 
     const logger = loggerFactory()
@@ -47,7 +52,7 @@ describe('Test read-only middleware', function () {
 
     assert.deepStrictEqual(res.statusCode, 500)
     assert.deepStrictEqual(
-      readOnlyMiddlewareHelper(isReadOnlyMode, method),
+      readOnlyMiddlewareHelper(isReadOnlyMode, spIDNotDefined, method),
       false
     )
     assert.deepStrictEqual(nextCalled, false)
@@ -56,17 +61,52 @@ describe('Test read-only middleware', function () {
   it('Should pass if read-only not enabled and is POST request', function () {
     const method = 'POST'
     const isReadOnlyMode = false
+    const spIDNotDefined = false
     config.set('isReadOnlyMode', isReadOnlyMode)
+    config.set('spID', 1)
     let nextCalled = false
 
-    readOnlyMiddleware({ method }, {}, function () {
+    const logger = loggerFactory()
+    const req = {
+      method,
+      logger
+    }
+    const res = resFactory()
+
+    readOnlyMiddleware(req, res, function () {
       nextCalled = true
     })
 
     assert.deepStrictEqual(
-      readOnlyMiddlewareHelper(isReadOnlyMode, method),
+      readOnlyMiddlewareHelper(isReadOnlyMode, spIDNotDefined, method),
       true
     )
     assert.deepStrictEqual(nextCalled, true)
+  })
+
+  it('Should fail if spID not defined and is POST request', function () {
+    const method = 'POST'
+    const isReadOnlyMode = false
+    const spIDNotDefined = true
+    config.set('isReadOnlyMode', isReadOnlyMode)
+    config.set('spID', 0)
+    let nextCalled = false
+
+    const logger = loggerFactory()
+    const req = {
+      method,
+      logger
+    }
+    const res = resFactory()
+
+    readOnlyMiddleware(req, res, function () {
+      nextCalled = true
+    })
+
+    assert.deepStrictEqual(
+      readOnlyMiddlewareHelper(isReadOnlyMode, spIDNotDefined, method),
+      false
+    )
+    assert.deepStrictEqual(nextCalled, false)
   })
 })
