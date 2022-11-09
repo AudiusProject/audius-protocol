17afcdb8987eda9e38da6428e1dfb095e9fa64fa
Sebastian Klingler <sliptype@gmail.com>
2022-11-09 16:51:26 -0600

    Use package version number (#2239)

--- apps/audius-client/packages/mobile/src/hooks/useUpdateRequired.ts
+++ apps/audius-client/packages/mobile/src/hooks/useUpdateRequired.ts
@@ -1,12 +1,14 @@
 import { StringKeys } from '@audius/common'
-import VersionNumber from 'react-native-version-number'
 import semver from 'semver'
 
 import { useRemoteVar } from 'app/hooks/useRemoteConfig'
 
+import packageInfo from '../../package.json'
+
+const { version } = packageInfo
+
 export const useUpdateRequired = () => {
-  const { appVersion } = VersionNumber
   const minAppVersion = useRemoteVar(StringKeys.MIN_APP_VERSION)
 
-  return { updateRequired: semver.lt(appVersion, minAppVersion) }
+  return { updateRequired: semver.lt(version, minAppVersion) }
 }
