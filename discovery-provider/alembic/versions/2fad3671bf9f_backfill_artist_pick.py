a183e16da3dd32b60b4c29bd714259e9bb4b28fd
Michelle Brier <michelle.brier4@gmail.com>
2022-11-09 03:24:49 +0000

    Bulk update SQL

--- discovery-provider/alembic/versions/2fad3671bf9f_backfill_artist_pick.py
+++ discovery-provider/alembic/versions/2fad3671bf9f_backfill_artist_pick.py
@@ -9,7 +9,9 @@ from pathlib import Path
 
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.types import String
+from sqlalchemy.types import Integer
+from sqlalchemy.dialects.postgresql import ARRAY
 
 # revision identifiers, used by Alembic.
 revision = '2fad3671bf9f'
@@ -18,23 +20,33 @@ branch_labels = None
 depends_on = None
 
 def build_sql(up):
-    inner_sql = []
-    path = Path(__file__).parent.joinpath("../csvs/staging_pinned_track_ids.csv")
+    path = Path(__file__).parent.joinpath("../csvs/user_pinned_track_ids.csv")
     with open(path, 'r') as f:
-        i = 0
-        params = {}
+        handles = []
+        pinned_track_ids = []
         for entry in f:
             handle, pinned_track_id = entry.split(",")
+
+            handles.append(handle)
             if up:
-                sql = f"UPDATE users SET artist_pick_track_id = :id{i} where is_current = True and handle = :h{i};"
-                params[f"id{i}"] = pinned_track_id
-                params[f"h{i}"] = handle
-            else:
-                sql = f"UPDATE users SET artist_pick_track_id = null where is_current = True and handle = :h{i};"
-                params[f"h{i}"] = handle
-            inner_sql.append(sql)
-            i += 1
-    return (sa.text("begin; \n\n " + "\n".join(inner_sql) + " \n\n commit;"), params)
+                pinned_track_id = int(pinned_track_id.strip())
+                pinned_track_ids.append(pinned_track_id)
+
+    params = {
+        'handles': handles,
+        'pinned_track_ids': pinned_track_ids,
+    }
+    if up:
+        inner_sql = "UPDATE users SET artist_pick_track_id = data_table.pinned_track_id FROM (SELECT unnest(:handles) AS handle, unnest(:pinned_track_ids) AS pinned_track_id) AS data_table WHERE users.is_current = True AND users.handle = data_table.handle;"
+    else:
+        inner_sql = "UPDATE users SET artist_pick_track_id = null FROM (SELECT unnest(:handles) AS handle) AS data_table WHERE users.is_current = True AND users.handle = data_table.handle;"
+
+    sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
+    sql = sql.bindparams(sa.bindparam("handles", ARRAY(String)))
+    if up:
+        sql = sql.bindparams(sa.bindparam("pinned_track_ids", ARRAY(Integer)))
+
+    return (sql, params)
 
 def upgrade():
     connection = op.get_bind()
