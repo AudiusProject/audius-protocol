4cbd60c7bb6bcfae8140fbb641fe9323762d1274
Sebastian Klingler <sliptype@gmail.com>
2022-11-17 14:21:12 -0600

    [C-1515] Allow repeat pushes via useNavigation inside nested navigator (#2280)
    * Allow repeat pushes via useNavigation inside nested navigator
    
    * Improve types

--- apps/audius-client/packages/mobile/src/utils/navigation.ts
+++ apps/audius-client/packages/mobile/src/utils/navigation.ts
@@ -1,4 +1,6 @@
-import type { NavigationState } from '@react-navigation/native'
+import type { Maybe } from '@audius/common'
+import type { NavigationProp, NavigationState } from '@react-navigation/native'
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 /**
  * Navigation state selector that selects the current route
@@ -20,4 +22,22 @@ export const getRoutePath = (state: NavigationState, routePath?: string[]) => {
 export const getPrimaryRoute = (state: NavigationState) => {
   // The route at index 2 is the primary route
   return getRoutePath(state)?.[2]
+}
+
+/**
+ * Given a navigator, get the nearest stack navigator in the hierarchy
+ */
+export const getNearestStackNavigator = (
+  navigator: NavigationProp<any>
+): Maybe<NativeStackNavigationProp<any>> => {
+  if (navigator.getState?.()?.type === 'stack') {
+    return navigator as unknown as NativeStackNavigationProp<any>
+  }
+  const parent = navigator.getParent()
+
+  if (!parent) {
+    return undefined
+  }
+
+  return getNearestStackNavigator(parent)
 }
