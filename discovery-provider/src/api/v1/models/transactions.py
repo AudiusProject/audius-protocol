c71aba6954e62bd933cc9386e4f1dbcb03565f70
Reed <3893871+dharit-tan@users.noreply.github.com>
2022-11-17 18:34:55 -0500

    Add method to transaction API (#4378)

--- discovery-provider/src/api/v1/models/transactions.py
+++ discovery-provider/src/api/v1/models/transactions.py
@@ -8,11 +8,17 @@ class TransactionType(fields.String):
         return value.lower()
 
 
+class TransactionMethod(fields.String):
+    def format(self, value: str):
+        return value.lower()
+
+
 transaction_details = ns.model(
     "transaction_details",
     {
         "transaction_date": fields.String(required=True),
         "transaction_type": TransactionType(required=True, discriminator=True),
+        "method": TransactionMethod(required=True),
         "signature": fields.String(required=True),
         "user_bank": fields.String(required=True),
         "change": fields.String(required=True),
