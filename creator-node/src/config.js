6a30aadcb2ce466e54927e2ac8a87ef00f5f9d28
Theo Ilie <theoilie.ti@gmail.com>
2022-11-19 03:12:52 +0000

    Revert test-only config change

--- creator-node/src/config.js
+++ creator-node/src/config.js
@@ -291,7 +291,7 @@ const config = convict({
     doc: 'Whether or not cluster logic should be enabled (running multiple instances of the app to better utuilize multiple logical cores)',
     format: Boolean,
     env: 'clusterModeEnabled',
-    default: false
+    default: true
   },
 
   /** Upload settings */
