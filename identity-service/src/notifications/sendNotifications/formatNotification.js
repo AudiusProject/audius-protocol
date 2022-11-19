485e8723dda62cc5041b49620b3feef715b734a3
Raymond Jacobson <ray@audius.co>
2022-11-18 19:43:41 -0800

    Skip notifs

--- identity-service/src/notifications/sendNotifications/formatNotification.js
+++ identity-service/src/notifications/sendNotifications/formatNotification.js
@@ -353,7 +353,7 @@ async function _processCreateNotifications(notif, tx) {
 
   // If the initiator is the main audius account, skip the notification
   // NOTE: This is a temp fix to not stall identity service
-  if (notif.initiator === 51) {
+  if (notif.initiator === 51 || notif.initiator === 1873428) {
     return []
   }
 
