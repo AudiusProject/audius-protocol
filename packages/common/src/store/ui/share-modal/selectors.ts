8e88d8d66172cd99fc9c5d65d79cb88380cecbdd
Dylan Jeffers <dylan@audius.co>
2022-11-25 19:47:11 -0800

    [C-1549] Fix mobile share-drawer (#2307)

--- packages/common/src/store/ui/share-modal/selectors.ts
+++ packages/common/src/store/ui/share-modal/selectors.ts
@@ -1,9 +1,9 @@
-import { createSelector } from '@reduxjs/toolkit'
-
 import { CommonState } from 'store/commonStore'
 
 export const shareModalState = (state: CommonState) => state.ui.shareModal
 
-export const getShareState = createSelector(shareModalState, (state) => {
-  return state
-})
+export const getShareState = (state: CommonState) => shareModalState(state)
+export const getShareContent = (state: CommonState) =>
+  shareModalState(state).content
+export const getShareSource = (state: CommonState) =>
+  shareModalState(state).source
