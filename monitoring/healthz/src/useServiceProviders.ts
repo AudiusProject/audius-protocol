797d5f3ed845611dd4f0f91c9dab956bed520577
Steve Perkins <stereosteve@users.noreply.github.com>
2022-11-29 12:04:07 -0500

    import healthz codebase (#4331)
    * import healthz codebase
    
    * Add storage to healthz (#4373)
    
    * add auto_upgrade column
    
    * use progress for storage ui, highlight behind nodes
    
    Co-authored-by: Raymond Jacobson <ray@audius.co>

---
+++ monitoring/healthz/src/useServiceProviders.ts
@@ -0,0 +1,63 @@
+import useSWR from 'swr'
+
+const prodEndpoint =
+  'https://api.thegraph.com/subgraphs/name/audius-infra/audius-network-mainnet'
+
+const stagingEndpoint =
+  'https://api.thegraph.com/subgraphs/name/audius-infra/audius-network-goerli'
+
+const gql = `
+query ServiceProviders($type: String) {
+  serviceNodes(where: {isRegistered: true, type: $type}) {
+    endpoint
+    isRegistered
+  }
+}
+`
+
+export type SP = {
+  endpoint: string
+  isRegistered: boolean
+
+  health?: any
+  apiJson?: any
+  discoveryHealth?: any
+}
+
+export function theGraphFetcher(
+  env: string,
+  type: 'content-node' | 'discovery-node'
+) {
+  return fetch(env == 'staging' ? stagingEndpoint : prodEndpoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: gql,
+      variables: {
+        type,
+      },
+    }),
+  }).then(async (resp) => {
+    const data = await resp.json()
+    const sps = data.data.serviceNodes as SP[]
+    const hostSortKey = (sp: SP) =>
+      new URL(sp.endpoint).hostname.split('.').reverse().join('.')
+    sps.sort((a, b) => (hostSortKey(a) < hostSortKey(b) ? -1 : 1))
+    // console.log(sps)
+    return sps
+  })
+}
+
+export function useServiceProviders(
+  env: string,
+  type: 'content-node' | 'discovery-node'
+) {
+  return useSWR<SP[]>([env, type], theGraphFetcher)
+}
+
+export function useDiscoveryProviders() {
+  return useServiceProviders('prod', 'discovery-node')
+}
+
+export function useContentProviders() {
+  return useServiceProviders('prod', 'content-node')
+}
