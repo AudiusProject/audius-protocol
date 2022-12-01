cffe964ecacff5f7f92cd6490019e4ae30036c32
Dylan Jeffers <dylan@audius.co>
2022-12-01 10:02:57 -0800

    Fix react-native-threads on android (#2331)

--- apps/audius-client/packages/mobile/react-native.config.js
+++ apps/audius-client/packages/mobile/react-native.config.js
@@ -3,10 +3,15 @@ module.exports = {
     'react-native-video': {
       platforms: {
         android: {
-          sourceDir: '../node_modules/react-native-video/android-exoplayer',
-        },
-      },
+          sourceDir: '../node_modules/react-native-video/android-exoplayer'
+        }
+      }
     },
+    'react-native-threads': {
+      platforms: {
+        android: null // disable Android platform, other platforms will still autolink if provided
+      }
+    }
   },
   assets: ['./src/assets/fonts']
-};
+}
