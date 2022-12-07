cd42703132798d612cdcecf8a9ebb170551819e7
isaac <isaac@audius.co>
2022-12-07 21:24:39 +0000

    add offset

--- discovery-provider/src/queries/get_unclaimed.py
+++ discovery-provider/src/queries/get_unclaimed.py
@@ -1,10 +1,11 @@
-from random import randrange
+import random
 
 from src import exceptions
 from src.models.playlists.aggregate_playlist import AggregatePlaylist
 from src.models.tracks.aggregate_track import AggregateTrack
 from src.models.users.aggregate_user import AggregateUser
 from src.utils.db_session import get_db_read_replica
+from src.tasks.entity_manager.utils import TRACK_ID_OFFSET, PLAYLIST_ID_OFFSET, USER_ID_OFFSET
 
 MAX_USER_ID = 999999999  # max for reward specifier id
 MAX_POSTGRES_ID = 2147483647
@@ -21,9 +22,9 @@ def get_unclaimed(type):
     with db.scoped_session() as session:
         for _ in range(10):
             is_claimed = True
-            random_id = randrange(MAX_POSTGRES_ID)
+            random_id = None
             if type == "user":
-                random_id = randrange(MAX_USER_ID)
+                random_id = random.randint(USER_ID_OFFSET, MAX_USER_ID)
                 is_claimed = (
                     session.query(AggregateUser.user_id).filter(
                         AggregateUser.user_id == random_id
@@ -31,6 +32,8 @@ def get_unclaimed(type):
                 ).first()
 
             if type == "track":
+                random_id = random.randint(TRACK_ID_OFFSET, MAX_POSTGRES_ID)
+
                 is_claimed = (
                     session.query(AggregateTrack.track_id).filter(
                         AggregateTrack.track_id == random_id
@@ -38,6 +41,8 @@ def get_unclaimed(type):
                 ).first()
 
             if type == "playlist":
+                random_id = random.randint(PLAYLIST_ID_OFFSET, MAX_POSTGRES_ID)
+
                 is_claimed = (
                     session.query(AggregatePlaylist.playlist_id).filter(
                         AggregatePlaylist.playlist_id == random_id
