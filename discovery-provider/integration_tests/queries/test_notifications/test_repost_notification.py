493511805ddf7d053d1ca02a280f1513fe6dbdb2
Joseph Lee <joeylee0925@gmail.com>
2022-12-08 16:30:18 +0000

    Add fetch notifs method

---
+++ discovery-provider/integration_tests/queries/test_notifications/test_repost_notification.py
@@ -0,0 +1,71 @@
+from datetime import datetime, timedelta
+import logging
+
+from integration_tests.utils import populate_mock_db
+from src.queries.get_notifications import get_notifications
+from src.utils.db_session import get_db
+from src.models.social.repost import Repost
+from src.models.notifications.notification import Notification
+from src.models.tracks.track import Track
+from src.models.tracks.aggregate_track import AggregateTrack
+
+logger = logging.getLogger(__name__)
+
+t1 = datetime(2020, 10, 10, 10, 35, 0)
+t2 = t1 - timedelta(hours=1)
+t3 = t1 - timedelta(hours=2)
+t4 = t1 - timedelta(hours=3)
+
+
+def test_get_repost_notifications(app):
+    with app.app_context():
+        db_mock = get_db()
+
+        test_entities = {
+            "users": [{"user_id": i+1} for i in range(20)],
+            "tracks": [{"track_id": 1, "owner_id": 1}],
+            "playlists": [{"playlist_id": 1, "playlist_owner_id": 1}]
+        }
+        populate_mock_db(db_mock, test_entities)
+
+        test_actions = {
+            "reposts": [{
+                "user_id": i+2,
+                "repost_item_id": 1,
+                "repost_type": "track"
+            } for i in range(4)] + [{
+                "user_id": i+2,
+                "repost_item_id": 1,
+                "repost_type": "playlist"
+            } for i in range(11)]
+        }
+        populate_mock_db(db_mock, test_actions)
+
+        with db_mock.scoped_session() as session:
+            args = {"limit": 10, "user_id": 1}
+            u1_notifiations = get_notifications(session, args)
+            assert len(u1_notifiations) == 3
+            assert u1_notifiations[0]["group_id"] == 'repost:1:type:track'
+            assert u1_notifiations[0]["is_seen"] == False
+            assert len(u1_notifiations[0]["actions"]) == 4
+            for reposter_user_id in range(2, 6):
+                assert any(
+                    act['data']['user_id'] == reposter_user_id 
+                    for act in u1_notifiations[0]["actions"]
+                )
+
+            assert u1_notifiations[1]["group_id"] == 'repost:1:type:playlist'
+            assert u1_notifiations[1]["is_seen"] == False
+            assert len(u1_notifiations[1]["actions"]) == 11
+            for reposter_user_id in range(2, 13):
+                assert any(
+                    act['data']['user_id'] == reposter_user_id 
+                    for act in u1_notifiations[1]["actions"]
+                )
+
+            assert u1_notifiations[2]["group_id"] == 'milestone:PLAYLIST_REPOST_COUNT:id:1:threshold:10'
+            assert u1_notifiations[2]["is_seen"] == False
+            assert u1_notifiations[2]["actions"][0]["type"] == "milestone"
+            assert u1_notifiations[2]["actions"][0]["data"]["type"] == "PLAYLIST_REPOST_COUNT"
+            assert u1_notifiations[2]["actions"][0]["data"]["type"] == "PLAYLIST_REPOST_COUNT"
+            assert u1_notifiations[2]["actions"][0]["data"]["threshold"] == 10
---
+++ discovery-provider/src/queries/get_notifications.py
@@ -0,0 +1,213 @@
+import logging
+from sqlalchemy import bindparam, text
+from typing import Optional, TypedDict, List, Union
+from datetime import datetime
+from collections import defaultdict
+
+from sqlalchemy.orm.session import Session
+
+logger = logging.getLogger(__name__)
+
+
+class GetNotificationArgs(TypedDict):
+    user_id: int
+    timestamp: Optional[int]
+    limit: Optional[int]
+
+
+notification_groups_sql = text(
+"""
+--- Create Intervals of user seen
+WITH user_seen as (
+  SELECT
+    LAG(seen_at, 1, now()::timestamp) OVER ( ORDER BY seen_at desc ) AS seen_at,
+    seen_at as prev_seen_at
+  FROM
+    notification_seen
+---  Not sure if we should add the following - redundant, but could be faster/slower
+---  WHERE
+---    (:timestamp_offset is NULL or seen_at < :timestamp_offset)
+  WHERE
+    user_id = :user_id
+---  Not sure if we should add the following - redundant, but could be faster/slower
+---  ORDER BY
+---    seen_at desc
+)
+SELECT
+    n.group_id as group_id,
+    array_agg(n.id),
+    CASE 
+      WHEN user_seen.seen_at is not NULL THEN now()::timestamp != user_seen.seen_at
+      ELSE EXISTS(SELECT 1 from notification_seen where user_id = :user_id)
+    END as is_seen,
+    CASE 
+      WHEN user_seen.seen_at is not NULL THEN user_seen.seen_at
+      ELSE (
+        SELECT seen_at from notification_seen
+        WHERE user_id = :user_id
+        ORDER BY seen_at ASC
+        limit 1
+      )
+    END as seen_at,
+    user_seen.prev_seen_at,
+    count(n.group_id)
+FROM
+    notification n
+LEFT JOIN user_seen on
+  user_seen.seen_at >= n.timestamp and user_seen.prev_seen_at < n.timestamp
+WHERE
+  :user_id = ANY(n.user_ids) AND
+  (:timestamp_offset is NULL or n.timestamp <= :timestamp_offset) AND
+  (:group_id_offset is NULL OR n.group_id < :group_id_offset)
+GROUP BY
+  n.group_id, user_seen.seen_at, user_seen.prev_seen_at
+ORDER BY
+  user_seen.seen_at desc NULLS LAST,
+  n.group_id desc
+limit :limit;
+"""
+)
+
+
+MAX_LIMIT = 50
+DEFAULT_LIMIT = 20
+
+
+class NotificationGroup(TypedDict):
+    group_id: str
+    notification_ids: List[int]
+    is_seen: bool
+    seen_at: datetime
+    prev_seen_at: datetime
+    count: int
+
+
+def get_notification_groups(session: Session, args: GetNotificationArgs):
+    """
+    Gets the user's notifiations in the database
+    """
+    # 
+
+    limit = min(args.get('limit', DEFAULT_LIMIT), MAX_LIMIT)
+
+    rows = session.execute(
+        notification_groups_sql,
+        {"user_id": args["user_id"], "limit": limit, "timestamp_offset": None, "group_id_offset": None},
+    )
+    res: List[NotificationGroup] = [{
+      "group_id": r[0],
+      "notification_ids": r[1],
+      "is_seen": r[2] if r[2] != None else False,
+      "seen_at": r[3],
+      "prev_seen_at": r[4],
+      "count": r[5],
+    } for r in rows]
+    logger.info(res)
+
+    return res
+
+
+class FollowNotification(TypedDict):
+    follower_user_id: int
+    followee_user_id: int
+
+
+class RepostNotification(TypedDict):
+    type: str
+    user_id: int
+    repost_item_id: int
+
+
+class SaveNotification(TypedDict):
+    type: str
+    user_id: int
+    save_item_id: int
+
+
+class MilestoneNotification(TypedDict):
+    type: str
+    threshold: int
+
+
+class RemixNotification(TypedDict):
+    parent_track_id: int
+    track_id: int
+
+
+class CosignRemixNotification(TypedDict):
+    parent_track_id: int
+    track_id: int
+
+
+NotificationData = Union[
+  FollowNotification,
+  RepostNotification,
+  SaveNotification,
+  MilestoneNotification,
+  RemixNotification,
+  CosignRemixNotification
+]
+
+
+class NotificationAction(TypedDict):
+    specifier: str
+    type: str
+    timestamp: datetime
+    data: NotificationData
+
+
+class Notification(TypedDict):
+    group_id: str
+    is_seen: bool
+    seen_at: datetime
+    actions: List[NotificationAction]
+
+
+notifications_sql = text("""
+SELECT
+    id,
+    type,
+    specifier,
+    timestamp,
+    data
+FROM notification n
+WHERE n.id in :notification_ids
+""")
+notifications_sql = notifications_sql.bindparams(bindparam("notification_ids", expanding=True))
+
+
+def get_notifications(session: Session, args: GetNotificationArgs):
+    notifications = get_notification_groups(session, args)
+    notification_ids = []
+    logger.info(notifications)
+    for notification in notifications:
+        notification_ids.extend(notification["notification_ids"])
+
+    rows = session.execute(
+        notifications_sql,
+        {"notification_ids": notification_ids}
+    )
+
+    notification_id_data = defaultdict()
+
+    for row in rows:
+        notification_id_data[row[0]] = {
+            "type": row[1],
+            "specifier": row[2],
+            "timestamp": row[3],
+            "data": row[4]
+        }
+
+    notifications_and_actions = [
+      {
+        **notification,
+        "actions": [
+          notification_id_data[id]
+          for id in notification["notification_ids"]
+        ]
+      } for notification in notifications
+    ]
+
+    logger.info(notifications_and_actions)
+
+    return notifications_and_actions
