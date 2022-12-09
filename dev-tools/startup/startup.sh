512a89895a245fa18b407e1310362544998b3c97
Cheran <cheran.v.senthil@gmail.com>
2022-12-09 13:04:14 -0600

    Add ability to set-env with local compose (#4410)

--- dev-tools/startup/startup.sh
+++ dev-tools/startup/startup.sh
@@ -5,6 +5,20 @@
 name=$(nslookup $(hostname -i) | grep -o "\(creator-node\|discovery-provider\|identity-service\)-[0-9]\+")
 replica=$(echo $name | grep -o "[0-9]\+")
 
+if [[ -f "/tmp/dev-tools/startup/$name.env" ]]; then
+  set -o allexport
+  echo "Loading $name.env"
+  . "/tmp/dev-tools/startup/$name.env"
+  set +o allexport
+fi
+
+if [[ -f "/tmp/dev-tools/startup/${name%-*}.sh" ]]; then
+  set -o allexport
+  echo "Loading ${name%-*}.env"
+  . "/tmp/dev-tools/startup/${name%-*}.env"
+  set +o allexport
+fi
+
 if [[ -f "/tmp/dev-tools/startup/$name.sh" ]]; then
   echo "Loading $name.sh"
   . "/tmp/dev-tools/startup/$name.sh"
