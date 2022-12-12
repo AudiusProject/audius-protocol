c77e4d16dd617a643e45a210f6ca75742d4e1b51
Johannes Naylor <jonaylor89@gmail.com>
2022-12-12 18:54:16 +0100

    update migration to disable rather than drop trigger

--- discovery-provider/alembic/versions/ec3b20d7bce3_backfill_track_cid.py
+++ discovery-provider/alembic/versions/ec3b20d7bce3_backfill_track_cid.py
@@ -88,85 +88,20 @@ def remove_temp_table():
     op.get_bind().execute(sql)
 
 
-def remove_track_triggers():
+def disable_track_triggers():
     inner_sql = f"""
-        drop trigger if exists on_track on tracks;
-        drop trigger if exists trg_tracks on tracks;
+        alter table track disable trigger on_track;
+        alter table track disable trg_tracks on_track;
         """
     sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
     op.get_bind().execute(sql)
 
 
-def restore_track_triggers():
+def enable_track_triggers():
     inner_sql = f"""
-        create or replace function handle_track() returns trigger as $$
-        declare
-        old_row tracks%ROWTYPE;
-        new_val int;
-        delta int := 0;
-        parent_track_owner_id int;
-        begin
-        insert into aggregate_track (track_id) values (new.track_id) on conflict do nothing;
-        insert into aggregate_user (user_id) values (new.owner_id) on conflict do nothing;
-
-        update aggregate_user
-        set track_count = (
-            select count(*)
-            from tracks t
-            where t.is_current is true
-            and t.is_delete is false
-            and t.is_unlisted is false
-            and t.is_available is true
-            and t.stem_of is null
-            and t.owner_id = new.owner_id
-        )
-        where user_id = new.owner_id
-        ;
-
-        -- If remix, create notification
-        begin
-            if new.remix_of is not null AND new.is_unlisted = FALSE and new.is_available = true AND new.is_delete = FALSE AND new.stem_of IS NULL then
-            select owner_id into parent_track_owner_id from tracks where is_current and track_id = (new.remix_of->'tracks'->0->>'parent_track_id')::int limit 1;
-            if parent_track_owner_id is not null then
-                insert into notification
-                (blocknumber, user_ids, timestamp, type, specifier, group_id, data)
-                values
-                (
-                new.blocknumber,
-                ARRAY [parent_track_owner_id],
-                new.updated_at,
-                'remix',
-                new.owner_id,
-                'remix:track:' || new.track_id || ':parent_track:' || (new.remix_of->'tracks'->0->>'parent_track_id')::int || ':blocknumber:' || new.blocknumber,
-                json_build_object('track_id', new.track_id, 'parent_track_id', (new.remix_of->'tracks'->0->>'parent_track_id')::int)
-                )
-                on conflict do nothing;
-            end if;
-            end if;
-            exception
-                when others then null;
-            end;
-
-        return null;
-        end;
-        $$ language plpgsql;
-
-        create trigger on_track
-        after insert or update on tracks
-        for each row execute procedure handle_track();
-
-
-
-        create or replace function on_new_row() returns trigger as $$
-        begin
-            PERFORM pg_notify(TG_TABLE_NAME, json_build_object('track_id', new.track_id)::text);
-            return null;
-        end;
-        $$ language plpgsql;
-
-        create trigger trg_tracks
-        after insert or update on tracks
-        for each row execute procedure on_new_row();
+    alter table track enable trigger on_track;
+    alter table track enable trigger trg_tracks;
+    
         """
     sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
     op.get_bind().execute(sql)
@@ -174,7 +109,7 @@ def restore_track_triggers():
 
 def upgrade():
     copy_mapping_into_temp_table()
-    remove_track_triggers()
+    disable_track_triggers()
 
     ###############################################################
     # Update the tracks table using the new temporary mapping table
@@ -188,13 +123,13 @@ def upgrade():
     sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
     op.get_bind().execute(sql)
 
-    restore_track_triggers()
+    enable_track_triggers()
     remove_temp_table()
 
 
 def downgrade():
     copy_mapping_into_temp_table()
-    remove_track_triggers()
+    disable_track_triggers()
 
     ################################################################################
     # Update the tracks table by setting track cid to null for tracks in the mapping
@@ -208,5 +143,5 @@ def downgrade():
     sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
     op.get_bind().execute(sql)
 
-    restore_track_triggers()
+    enable_track_triggers()
     remove_temp_table()
