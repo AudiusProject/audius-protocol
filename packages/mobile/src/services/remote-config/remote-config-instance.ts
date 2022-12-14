2a94ee8505768c96af64ab8d9e09a7403de780f2
Raymond Jacobson <ray@audius.co>
2022-12-14 07:46:36 -0800

    [C-1666] Use custom optimizely datafile (#2391)

--- packages/mobile/src/services/remote-config/remote-config-instance.ts
+++ packages/mobile/src/services/remote-config/remote-config-instance.ts
@@ -1,15 +1,32 @@
 import type { Environment } from '@audius/common'
-import { remoteConfig } from '@audius/common'
+import { ErrorLevel, remoteConfig } from '@audius/common'
 import * as optimizely from '@optimizely/optimizely-sdk'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Config from 'react-native-config'
 
+import { reportToSentry } from 'app/utils/reportToSentry'
+
 export const FEATURE_FLAG_ASYNC_STORAGE_SESSION_KEY = 'featureFlagSessionId-2'
+
+const OPTIMIZELY_KEY = Config.OPTIMIZELY_KEY
+const DATA_FILE_URL = 'https://experiments.audius.co/datafiles/%s.json'
 
 export const remoteConfigInstance = remoteConfig({
   createOptimizelyClient: async () => {
     return optimizely.createInstance({
-      sdkKey: Config.OPTIMIZELY_KEY
+      sdkKey: OPTIMIZELY_KEY,
+      datafileOptions: {
+        urlTemplate: DATA_FILE_URL
+      },
+      errorHandler: {
+        handleError: (error) => {
+          reportToSentry({
+            level: ErrorLevel.Error,
+            error,
+            name: 'Optimizely failed to load'
+          })
+        }
+      }
     })
   },
   getFeatureFlagSessionId: async () => {
