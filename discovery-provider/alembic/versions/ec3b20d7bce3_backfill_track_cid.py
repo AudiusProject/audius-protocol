c10576280bcb263e1d24602890ce4b4f376973d9
isaac <isaac@audius.co>
2022-12-19 17:59:06 +0000

    Fix backfill track cid tmp removal

--- discovery-provider/alembic/versions/ec3b20d7bce3_backfill_track_cid.py
+++ discovery-provider/alembic/versions/ec3b20d7bce3_backfill_track_cid.py
@@ -26,55 +26,57 @@ def copy_mapping_into_temp_table():
     # Create temporary table and copy track id <> cid mapping into table
     # Also create index on track_id
     ####################################################################
-    inner_sql = f"""
-        create table if not exists tmp_track_cid_mapping (track_id varchar unique, track_cid varchar(46));
-        """
-    sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
-    op.get_bind().execute(sql)
+    try:
+        inner_sql = f"""
+            create table if not exists tmp_track_cid_mapping (track_id varchar unique, track_cid varchar(46));
+            """
+        sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
+        op.get_bind().execute(sql)
 
-    path_tmp = Path(__file__).parent.joinpath("../tmp")
-    path_csv = Path(__file__).parent.joinpath("../tmp/track_cids.csv")
-    path_zip = Path(__file__).parent.joinpath("../tmp/track_cids.csv.zip")
+        path_tmp = Path(__file__).parent.joinpath("../tmp")
+        path_csv = Path(__file__).parent.joinpath("../tmp/track_cids.csv")
+        path_zip = Path(__file__).parent.joinpath("../tmp/track_cids.csv.zip")
 
-    env = os.getenv("audius_discprov_env")
-    if env != "stage" and env != "prod":
-        return
-    if env == "stage":
-        aws_url = "https://s3.us-west-1.amazonaws.com/download.staging.audius.co/track_cids.csv.zip"
-    else:
-        aws_url = (
-            "https://s3.us-west-1.amazonaws.com/download.audius.co/track_cids.csv.zip"
-        )
+        env = os.getenv("audius_discprov_env")
+        if env != "stage" and env != "prod":
+            return
+        if env == "stage":
+            aws_url = "https://s3.us-west-1.amazonaws.com/download.staging.audius.co/track_cids.csv.zip"
+        else:
+            aws_url = (
+                "https://s3.us-west-1.amazonaws.com/download.audius.co/track_cids.csv.zip"
+            )
 
-    os.mkdir(path_tmp)
-    print(f"Migration - downloading {aws_url}")
-    urllib.request.urlretrieve(aws_url, path_zip)
-    print("Migration - download complete")
+        if os.path.isdir(path_tmp):
+            shutil.rmtree(path_tmp)
+        os.mkdir(path_tmp)
+        print(f"Migration - downloading {aws_url}")
+        urllib.request.urlretrieve(aws_url, path_zip)
+        print("Migration - download complete")
 
-    with zipfile.ZipFile(path_zip, "r") as zip_ref:
-        zip_ref.extractall(path_tmp)
+        with zipfile.ZipFile(path_zip, "r") as zip_ref:
+            zip_ref.extractall(path_tmp)
 
-    cursor = op.get_bind().connection.cursor()
-    with open(path_csv, "r") as f:
-        # Skip the header row of the csv
-        next(f)
-        cursor.copy_from(
-            f, "tmp_track_cid_mapping", sep=",", columns=("track_id", "track_cid")
-        )
+        cursor = op.get_bind().connection.cursor()
+        with open(path_csv, "r") as f:
+            # Skip the header row of the csv
+            next(f)
+            cursor.copy_from(
+                f, "tmp_track_cid_mapping", sep=",", columns=("track_id", "track_cid")
+            )
 
-    #############################################################
-    # Change the track id column type to be integer and add index
-    #############################################################
-    inner_sql = f"""
-        alter table tmp_track_cid_mapping alter column track_id type integer using track_id::int;
-        create index if not exists tmp_track_cid_mapping_idx on tmp_track_cid_mapping USING btree (track_id);
-        """
-    sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
-    op.get_bind().execute(sql)
-
-    os.remove(path_csv)
-    os.remove(path_zip)
-    shutil.rmtree(path_tmp, ignore_errors=True)
+        #############################################################
+        # Change the track id column type to be integer and add index
+        #############################################################
+        inner_sql = f"""
+            alter table tmp_track_cid_mapping alter column track_id type integer using track_id::int;
+            create index if not exists tmp_track_cid_mapping_idx on tmp_track_cid_mapping USING btree (track_id);
+            """
+        sql = sa.text("begin; \n\n " + inner_sql + " \n\n commit;")
+        op.get_bind().execute(sql)
+    finally:
+        if os.path.isdir(path_tmp):
+            shutil.rmtree(path_tmp)
 
 
 def remove_temp_table():
