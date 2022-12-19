a8c3bcd1a75dc903a0fe30c2e44f9f968d46a16c
Michael Piazza <michael.piazza.mp@gmail.com>
2022-12-19 17:29:02 -0500

    Avoid updating or publishing already seen notifications (#4506)
    * Don't reprocess notifs
    
    * Add indices on slot and blocknumber
    
    * Fix blocknumber

--- identity-service/src/notifications/index.js
+++ identity-service/src/notifications/index.js
@@ -434,9 +434,15 @@ class NotificationProcessor {
       timeout
     )
     const { info: metadata, owners, milestones } = notificationsFromDN
-    const notifications = await filterOutAbusiveUsers(
+    let notifications = await filterOutAbusiveUsers(
       notificationsFromDN.notifications
     )
+
+    // Ensure we don't process any notifs
+    // that are already in the db
+    const latestBlock = await models.Notification.max('blocknumber')
+    notifications = notifications.filter((n) => n.blocknumber >= latestBlock)
+
     logger.info(
       `notifications main indexAll job - query notifications from discovery node complete in ${
         Date.now() - time
@@ -555,9 +561,15 @@ class NotificationProcessor {
       timeout
     )
     const metadata = notificationsFromDN.info
-    const notifications = await filterOutAbusiveUsers(
+    let notifications = await filterOutAbusiveUsers(
       notificationsFromDN.notifications
     )
+
+    // Ensure we don't process any notifs
+    // that are already in the db
+    const latestSlot = await models.SolanaNotification.max('slot')
+    notifications = notifications.filter((n) => n.slot >= latestSlot)
+
     logger.info(
       `${logLabel} - query solana notifications from discovery node complete in ${
         Date.now() - time
