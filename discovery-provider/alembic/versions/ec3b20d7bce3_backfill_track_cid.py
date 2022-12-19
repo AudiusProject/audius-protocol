257179f6897b85c97999973b2db2689239f68761
isaac <isaac@audius.co>
2022-12-19 17:09:32 +0000

    fix

--- discovery-provider/alembic/versions/ec3b20d7bce3_backfill_track_cid.py
+++ discovery-provider/alembic/versions/ec3b20d7bce3_backfill_track_cid.py
@@ -1,7 +1,7 @@
 """backfill_track_cid
 
 Revision ID: ec3b20d7bce3
-Revises: f6f009132212
+Revises: fe2c3daaa912
 Create Date: 2022-12-08 14:34:59.163989
 
 """
@@ -16,7 +16,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "ec3b20d7bce3"
-down_revision = "f6f009132212"
+down_revision = "fe2c3daaa912"
 branch_labels = None
 depends_on = None
 
