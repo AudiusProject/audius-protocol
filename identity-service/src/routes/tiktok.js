89991fd97f3e7edcf957b5d8631809b800cebd1e
Sebastian Klingler <sliptype@gmail.com>
2022-12-20 11:40:58 -0600

    Fix TikTok auth + improve association (#4511)
    * Add logs
    
    * Fix tiktok auth
    
    * Error if tiktok user is already associated

--- identity-service/src/routes/tiktok.js
+++ identity-service/src/routes/tiktok.js
@@ -67,7 +67,9 @@ module.exports = function (app) {
       try {
         // Fetch user's accessToken
         const accessTokenResponse = await axios.post(urlAccessToken)
-        const { access_token: accessToken } = accessTokenResponse.data
+        const {
+          data: { access_token: accessToken }
+        } = accessTokenResponse.data
 
         // Fetch TikTok user from the TikTok API
         const userResponse = await axios.post(
@@ -90,15 +92,25 @@ module.exports = function (app) {
 
         const { user: tikTokUser } = data
 
-        // Store the user id, and current profile for user in db
-        await models.TikTokUser.findOrCreate({
+        const existingTikTokUser = await models.TikTokUser.findOne({
           where: { uuid: tikTokUser.open_id },
-          defaults: {
+          blockchainUserId: {
+            [models.Sequelize.Op.not]: null
+          }
+        })
+
+        if (existingTikTokUser) {
+          return errorResponseBadRequest(
+            `Another Audius profile has already been authenticated with TikTok user @${tikTokUser.display_name}!`
+          )
+        } else {
+          // Store the user id, and current profile for user in db
+          await models.TikTokUser.upsert({
             uuid: tikTokUser.open_id,
             profile: tikTokUser,
             verified: tikTokUser.is_verified
-          }
-        })
+          })
+        }
 
         return successResponse(accessTokenResponse.data)
       } catch (err) {
