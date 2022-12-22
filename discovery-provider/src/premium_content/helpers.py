5f6322c1f997bdac669fcfa559f7eb1584da2714
Saliou Diallo <saliou@audius.co>
2022-12-22 10:17:49 -0500

    Add tip gate for premium content

--- discovery-provider/src/premium_content/helpers.py
+++ discovery-provider/src/premium_content/helpers.py
@@ -2,6 +2,7 @@ import logging
 from typing import Any
 
 from src.models.social.follow import Follow
+from src.models.users.user_tip import UserTip
 from src.utils import db_session
 
 logger = logging.getLogger(__name__)
@@ -24,5 +25,17 @@ def does_user_follow_artist(user_id: int, follow_user_id: int):
             .filter(Follow.follower_user_id == user_id)
             .filter(Follow.followee_user_id == follow_user_id)
             .one_or_none()
+        )
+        return True if result else False
+
+
+def does_user_support_artist(user_id: int, supporting_user_id: int):
+    db = db_session.get_db_read_replica()
+    with db.scoped_session() as session:
+        result = (
+            session.query(UserTip)
+            .filter(UserTip.sender_user_id == user_id)
+            .filter(UserTip.receiver_user_id == supporting_user_id)
+            .all()
         )
         return True if result else False
