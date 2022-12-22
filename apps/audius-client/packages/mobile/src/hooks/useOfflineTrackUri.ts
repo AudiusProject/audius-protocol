3dca9044c87579b70d9b71db3a4fa9635fb6a94d
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2022-12-22 10:31:48 -0500

    [C-1698] Fix first offline playback stalled (#2446)

--- apps/audius-client/packages/mobile/src/hooks/useOfflineTrackUri.ts
+++ apps/audius-client/packages/mobile/src/hooks/useOfflineTrackUri.ts
@@ -1,17 +1,20 @@
+import { useSelector } from 'react-redux'
 import { useAsync } from 'react-use'
 
 import {
   getLocalAudioPath,
   isAudioAvailableOffline
 } from 'app/services/offline-downloader'
+import { getOfflineTracks } from 'app/store/offline-downloads/selectors'
 
 import { useIsOfflineModeEnabled } from './useIsOfflineModeEnabled'
 
 export const useOfflineTrackUri = (trackId?: string) => {
   const isOfflineModeEnabled = useIsOfflineModeEnabled()
+  const offlineTracks = useSelector(getOfflineTracks)
 
   return useAsync(async () => {
-    if (!trackId || !isOfflineModeEnabled) return
+    if (!trackId || !isOfflineModeEnabled || !offlineTracks[trackId]) return
     if (!(await isAudioAvailableOffline(trackId))) return
     const audioFilePath = getLocalAudioPath(trackId)
     return `file://${audioFilePath}`
