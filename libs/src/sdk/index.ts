d0c1638c869d09d24575f45f4216df5441c246dd
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2022-12-23 12:50:41 -0800

    Revert "[PAY-759] [PAY-758] Add ChatsApi to SDK (#4416)"
    This reverts commit 2e8b21b718799b8b2322b8fa74ff37abf10ad6aa.

--- libs/src/sdk/index.ts
+++ libs/src/sdk/index.ts
@@ -3,9 +3,3 @@ export * as full from './api/generated/full'
 export * from './api/generated/default'
 export { TracksApi } from './api/TracksApi'
 export { ResolveApi } from './api/ResolveApi'
-export {
-  GetAudioTransactionHistorySortMethodEnum,
-  GetAudioTransactionHistorySortDirectionEnum
-} from './api/generated/full'
-export * from './api/chats/clientTypes'
-export * from './api/chats/serverTypes'
