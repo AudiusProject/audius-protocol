a7f4a62bf845aae68cd4381f1d6148dd17b6e433
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2022-12-23 11:52:19 -0500

    Fix offline mode QA backdoor (#2457)

--- packages/mobile/src/hooks/useFetchAllFavoritedTracks.ts
+++ packages/mobile/src/hooks/useFetchAllFavoritedTracks.ts
@@ -1,11 +1,20 @@
+import { useMemo } from 'react'
+
 import type { APIFavorite, ID } from '@audius/common'
-import { decodeHashId, encodeHashId, accountSelectors } from '@audius/common'
+import {
+  decodeHashId,
+  encodeHashId,
+  accountSelectors,
+  savedPageSelectors
+} from '@audius/common'
+import moment from 'moment'
 import { useSelector } from 'react-redux'
 import { useAsync } from 'react-use'
 
 import { apiClient } from 'app/services/audius-api-client'
 
 import { useIsOfflineModeEnabled } from './useIsOfflineModeEnabled'
+const { getLocalSaves } = savedPageSelectors
 
 const { getUserId } = accountSelectors
 
@@ -44,8 +53,28 @@ export const fetchAllFavoritedTracks = async (currentUserId: number) => {
 export const useFetchAllFavoritedTracks = () => {
   const currentUserId = useSelector(getUserId)
   const isOfflineModeEnabled = useIsOfflineModeEnabled()
+  const localSaves = useSelector(getLocalSaves)
+  const localTrackSaves = useMemo(
+    () =>
+      Object.keys(localSaves)
+        .filter((localSaveId) =>
+          localSaves[localSaveId]?.includes('kind:TRACKS')
+        )
+        .map((trackId) => ({
+          trackId: parseInt(trackId),
+          favoriteCreatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+        })),
+    [localSaves]
+  )
+
   return useAsync(async () => {
     if (!isOfflineModeEnabled || !currentUserId) return
-    return fetchAllFavoritedTracks(currentUserId)
-  }, [currentUserId, isOfflineModeEnabled, fetchAllFavoritedTracks])
+    const fetchedFavoritedTracks = await fetchAllFavoritedTracks(currentUserId)
+    return [...localTrackSaves, ...fetchedFavoritedTracks]
+  }, [
+    currentUserId,
+    isOfflineModeEnabled,
+    localTrackSaves,
+    fetchAllFavoritedTracks
+  ])
 }
