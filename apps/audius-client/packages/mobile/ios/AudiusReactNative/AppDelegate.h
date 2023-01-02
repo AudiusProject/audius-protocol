58ba0b9bb88cd83667a0b91f4d427b58006f00d5
Dylan Jeffers <dylan@audius.co>
2023-01-02 15:59:48 -0800

    [C-1738] Upgrade react-native to 0.68.5 (#2495)

--- apps/audius-client/packages/mobile/ios/AudiusReactNative/AppDelegate.h
+++ apps/audius-client/packages/mobile/ios/AudiusReactNative/AppDelegate.h
@@ -1,8 +1,9 @@
+#import <UserNotifications/UNUserNotificationCenter.h>
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
 
-@property (nonatomic, strong) UIWindow *window;
+@property(nonatomic, strong) UIWindow *window;
 
 @end
