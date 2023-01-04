1aba1a57c75ba7d0f6869eee6f7c43944523b90d
Isaac Solo <isaac@audius.co>
2023-01-04 12:55:11 -0800

    Fix spid key in availability indexing

--- discovery-provider/src/utils/eth_contracts_helpers.py
+++ discovery-provider/src/utils/eth_contracts_helpers.py
@@ -61,14 +61,13 @@ def fetch_all_registered_content_nodes(
             for i in ids_list
         }
         for future in concurrent.futures.as_completed(fetch_cnode_futures):
-            single_cnode_fetch_op = fetch_cnode_futures[future]
+            cn_spID = fetch_cnode_futures[future]
             try:
                 cn_endpoint_info = future.result()
                 # Validate the endpoint on chain
                 # As endpoints get deregistered, this peering system must not slow down with failed connections
                 #   or unanticipated load
                 eth_sp_endpoint = cn_endpoint_info[1]
-                cn_spID = cn_endpoint_info[2]
 
                 valid_endpoint = is_fqdn(eth_sp_endpoint)
                 # Only valid FQDN strings are worth validating
