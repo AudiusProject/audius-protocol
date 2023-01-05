c0c608f4f6dc4b8687248de675bda875cf9c315f
Cheran Senthilkumar <cheran.v.senthil@gmail.com>
2023-01-05 11:46:47 -0600

    Set dev env var

--- dev-tools/setup.sh
+++ dev-tools/setup.sh
@@ -32,7 +32,7 @@ debian | ubuntu)
 
     # Add user to docker group
     sudo usermod -aG docker "$USER"
-    
+
     # Increase file watchers
     echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
     ;;
@@ -77,4 +77,4 @@ ln -sf "$PROTOCOL_DIR/dev-tools/audius-cmd" "$HOME/.local/bin/audius-cmd"
 echo "export PROTOCOL_DIR=$PROTOCOL_DIR" >>~/.profile
 echo "export PATH=$HOME/.local/bin:$PATH" >>~/.profile
 
-[[ "$ID" =~ ^(debian|ubuntu)$ ]] && . "$PROTOCOL_DIR/dev-tools/setup-dev.sh"
+[[ "$AUDIUS_DEV" != "false" ]] && . "$PROTOCOL_DIR/dev-tools/setup-dev.sh"
