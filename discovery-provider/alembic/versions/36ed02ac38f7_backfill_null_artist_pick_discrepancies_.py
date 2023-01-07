4c369c2d8a2452f647be81eb48ec0823ace17c5f
Michelle Brier <michelle.brier4@gmail.com>
2023-01-06 16:34:35 -0800

    Linter

--- discovery-provider/alembic/versions/36ed02ac38f7_backfill_null_artist_pick_discrepancies_.py
+++ discovery-provider/alembic/versions/36ed02ac38f7_backfill_null_artist_pick_discrepancies_.py
@@ -6,9 +6,9 @@ Create Date: 2023-01-06 23:42:58.493746
 
 """
 import os
-from alembic import op
-import sqlalchemy as sa
 
+import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '36ed02ac38f7'
