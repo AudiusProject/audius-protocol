a7931a560d120ac92f732faa8a64083dbdb8f107
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-01-09 12:12:37 -0500

    [C-1703] fix button padding for remove download playlist (#2544)

--- audius-client/packages/mobile/src/components/core/Button.tsx
+++ audius-client/packages/mobile/src/components/core/Button.tsx
@@ -142,7 +142,7 @@ const useStyles = makeStyles(
       large: {
         button: {
           height: spacing(12),
-          paddingHorizontal: spacing(12)
+          paddingHorizontal: spacing(8)
         },
         text: {
           fontSize: 18
