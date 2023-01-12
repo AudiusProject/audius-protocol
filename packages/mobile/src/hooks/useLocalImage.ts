0e24bbc7fa0a7c67b4b09b54a0ff0ebd0f0b7b9c
Sebastian Klingler <sliptype@gmail.com>
2023-01-12 11:25:35 -0600

    [C-1726] useLocalImage (#2564)

--- packages/mobile/src/hooks/useLocalImage.ts
+++ packages/mobile/src/hooks/useLocalImage.ts
@@ -1,8 +1,9 @@
-import { useCallback, useMemo } from 'react'
+import { useCallback } from 'react'
 
-import { SquareSizes } from '@audius/common'
+import { SquareSizes, reachabilitySelectors } from '@audius/common'
 import type { ImageURISource } from 'react-native'
 import { exists } from 'react-native-fs'
+import { useSelector } from 'react-redux'
 import { useAsync } from 'react-use'
 import type { AsyncState } from 'react-use/lib/useAsync'
 
@@ -10,50 +11,72 @@ import {
   getLocalCollectionCoverArtPath,
   getLocalTrackCoverArtPath
 } from 'app/services/offline-downloader'
+const { getIsReachable } = reachabilitySelectors
 
-export const useLocalCollectionImage = (trackId?: string) => {
-  const getLocalPath = useCallback(
-    (size: string) =>
-      trackId ? getLocalCollectionCoverArtPath(trackId, size) : undefined,
-    [trackId]
-  )
-  return useLocalImage(getLocalPath)
+const getLocalTrackImagePath = (trackId?: string) => (size: string) =>
+  trackId ? getLocalTrackCoverArtPath(trackId, size) : undefined
+
+const getLocalCollectionImagePath = (collectionId?: string) => (size: string) =>
+  collectionId ? getLocalCollectionCoverArtPath(collectionId, size) : undefined
+
+export const getLocalTrackImageSource = (trackId?: string) => {
+  return getLocalImageSource(getLocalTrackImagePath(trackId))
 }
 
-export const useLocalTrackImage = (trackId?: string) => {
-  const getLocalPath = useCallback(
-    (size: string) =>
-      trackId ? getLocalTrackCoverArtPath(trackId, size) : undefined,
-    [trackId]
-  )
-  return useLocalImage(getLocalPath)
+export const getLocalCollectionImageSource = (collectionId?: string) => {
+  return getLocalImageSource(getLocalCollectionImagePath(collectionId))
+}
+
+export const getLocalImageSource = async (
+  getLocalPath: (size: string) => string | undefined
+) => {
+  const imageSources = Object.values(SquareSizes)
+    .reverse()
+    .map(
+      (size): ImageURISource => ({
+        uri: `file://${getLocalPath(size.toString())}`,
+        width: parseInt(size.split('x')[0]),
+        height: parseInt(size.split('x')[1])
+      })
+    )
+    .filter((source) => !!source.uri)
+
+  const verifiedSources: ImageURISource[] = []
+  for (const source of imageSources) {
+    if (source?.uri && (await exists(source.uri))) {
+      verifiedSources.push(source)
+    }
+  }
+  return verifiedSources
 }
 
 export const useLocalImage = (
   getLocalPath: (size: string) => string | undefined
 ): AsyncState<ImageURISource[]> => {
-  const imageSources = useMemo(
-    () =>
-      Object.values(SquareSizes)
-        .reverse()
-        .map(
-          (size): ImageURISource => ({
-            uri: `file://${getLocalPath(size.toString())}`,
-            width: parseInt(size.split('x')[0]),
-            height: parseInt(size.split('x')[1])
-          })
-        )
-        .filter((source) => !!source.uri),
-    [getLocalPath]
-  )
+  const isNotReachable = useSelector(getIsReachable) === false
 
   return useAsync(async () => {
-    const verifiedSources: ImageURISource[] = []
-    for (const source of imageSources) {
-      if (source?.uri && (await exists(source.uri))) {
-        verifiedSources.push(source)
-      }
+    // Only check for local images if not reachable
+    if (isNotReachable) {
+      return []
     }
-    return verifiedSources
-  }, [getLocalPath, imageSources])
+
+    return await getLocalImageSource(getLocalPath)
+  }, [getLocalPath])
+}
+
+export const useLocalTrackImage = (trackId?: string) => {
+  const getLocalPath = useCallback(
+    (size: string) => getLocalTrackImagePath(trackId)(size),
+    [trackId]
+  )
+  return useLocalImage(getLocalPath)
+}
+
+export const useLocalCollectionImage = (collectionId?: string) => {
+  const getLocalPath = useCallback(
+    (size: string) => getLocalCollectionImagePath(collectionId)(size),
+    [collectionId]
+  )
+  return useLocalImage(getLocalPath)
 }
