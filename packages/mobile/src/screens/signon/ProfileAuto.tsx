0da932cc7d60dcfd271271bd52ac79827ffaec8a
Sebastian Klingler <sliptype@gmail.com>
2023-01-17 14:13:05 -0600

    [C-1823] Fix twitter auth on android (#2599)

--- packages/mobile/src/screens/signon/ProfileAuto.tsx
+++ packages/mobile/src/screens/signon/ProfileAuto.tsx
@@ -24,6 +24,7 @@ import LoadingSpinner from 'app/components/loading-spinner'
 import { SocialButton } from 'app/components/social-button'
 import { TikTokAuthButton } from 'app/components/tiktok-auth'
 import { useFeatureFlag } from 'app/hooks/useRemoteConfig'
+import { useToast } from 'app/hooks/useToast'
 import { track, make } from 'app/services/analytics'
 import * as oauthActions from 'app/store/oauth/actions'
 import {
@@ -117,7 +118,8 @@ const messages = {
   verifiedTileHeader: 'Verified?',
   verifiedTileContent:
     'If the linked account is verified, your Audius account will be verified to match!',
-  manual: "I'd rather fill out my profile manually"
+  manual: "I'd rather fill out my profile manually",
+  error: 'Something went wrong, please try again'
 }
 
 let didAnimation = false
@@ -151,6 +153,7 @@ type ProfileAutoProps = NativeStackScreenProps<
 const ProfileAuto = ({ navigation }: ProfileAutoProps) => {
   const styles = useStyles()
   const dispatch = useDispatch()
+  const { toast } = useToast()
   const { neutralLight4, staticWhite } = useThemeColors()
   const twitterInfo = useSelector(getTwitterInfo)
   const twitterError = useSelector(getTwitterError)
@@ -345,11 +348,11 @@ const ProfileAuto = ({ navigation }: ProfileAutoProps) => {
   ])
 
   useEffect(() => {
-    if (twitterError | instagramError | tikTokError) {
+    if (twitterError || instagramError || tikTokError) {
       setIsLoading(false)
-      // TODO: sk - show an error message?
+      toast({ content: messages.error })
     }
-  }, [twitterError, instagramError, tikTokError])
+  }, [twitterError, instagramError, tikTokError, toast])
 
   const handleTwitterPress = () => {
     setIsLoading(true)
@@ -377,6 +380,7 @@ const ProfileAuto = ({ navigation }: ProfileAutoProps) => {
   }
 
   const handleTikTokPress = () => {
+    dispatch(oauthActions.setTikTokError(null))
     setIsLoading(true)
     track(
       make({
