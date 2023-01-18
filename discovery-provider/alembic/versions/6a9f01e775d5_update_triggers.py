8210c913dc696f3447a8b80e36a1f1b3ebe5cac5
Joseph Lee <joeylee0925@gmail.com>
2023-01-18 17:08:55 +0000

    Update import util

--- discovery-provider/alembic/versions/6a9f01e775d5_update_triggers.py
+++ discovery-provider/alembic/versions/6a9f01e775d5_update_triggers.py
@@ -5,28 +5,15 @@ Revises: efafdb22df81
 Create Date: 2023-01-17 18:39:06.300212
 
 """
-from pathlib import Path
 
-import sqlalchemy
 from alembic import op
+from src.utils.alembic_helpers import build_sql
 
 # revision identifiers, used by Alembic.
 revision = "6a9f01e775d5"
 down_revision = "efafdb22df81"
 branch_labels = None
 depends_on = None
-
-
-def load_sql(name):
-    path = Path(__file__).parent.joinpath(f"../trigger_sql/{name}")
-    with open(path) as f:
-        return f.read()
-
-
-def build_sql(file_names):
-    files = [load_sql(f) for f in file_names]
-    inner_sql = "\n;\n".join(files)
-    return sqlalchemy.text("begin; \n\n " + inner_sql + " \n\n commit;")
 
 
 up_files = [
