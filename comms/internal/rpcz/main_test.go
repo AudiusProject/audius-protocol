2782a351786bfdd796b31ca319b6426a80239835
Michelle Brier <michelle.brier4@gmail.com>
2023-01-18 10:03:53 -0800

    remove empty line

--- comms/internal/rpcz/main_test.go
+++ comms/internal/rpcz/main_test.go
@@ -16,7 +16,6 @@ import (
 func TestMain(m *testing.M) {
 	// setup
 	os.Setenv("audius_db_url", "postgresql://postgres:postgres@localhost:5454/comtest?sslmode=disable")
-
 	err := db.Dial()
 	if err != nil {
 		log.Fatal(err)
