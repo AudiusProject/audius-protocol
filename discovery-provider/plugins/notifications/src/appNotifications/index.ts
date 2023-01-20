af28c442c8418c794e318c06edb02116d5a9414c
Joseph Lee <joeylee0925@gmail.com>
2023-01-20 12:00:29 -0500

    Add e2e test for notifications with discovery and identity db

--- discovery-provider/plugins/notifications/src/appNotifications/index.ts
+++ discovery-provider/plugins/notifications/src/appNotifications/index.ts
@@ -1,14 +1,18 @@
-import { logger } from '../logger'
-import { NotificationRow } from '../types/dn'
-import { FollowNotification } from './mappers/follow'
+import { Knex } from 'knex'
 
-type EntitiesToFetch = {
-  'users': Set<number>
-}
+import { logger } from '../logger'
+import { FollowNotification } from '../types/appNotifications'
+import { NotificationRow } from '../types/dn'
+import { Follow } from './mappers/follow'
 
 export class AppNotifications {
+  dnDB: Knex
+  identityDB: Knex
 
-  constructor() { }
+  constructor(dnDB: Knex, identityDB: Knex) {
+    this.dnDB = dnDB
+    this.identityDB = identityDB
+  }
 
   async process(notifications: NotificationRow[]) {
     const mappedNotifications = notifications.map(this.mapNotification).filter(Boolean)
@@ -20,9 +24,10 @@ export class AppNotifications {
   }
 
 
-  mapNotification = (notification: NotificationRow) {
+  mapNotification = (notification: NotificationRow) => {
     if (notification.type == 'follow') {
-      return new FollowNotification(notification)
+      const followNotification = notification as NotificationRow & { data: FollowNotification }
+      return new Follow(this.dnDB, this.identityDB, followNotification)
     }
     logger.info(`Notification type: ${notification.type} has no handler`)
   }
