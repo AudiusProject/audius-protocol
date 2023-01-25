5b112264f29326501ae961fca1baa08f45cdfac5
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-01-25 17:18:48 -0500

    Performance improvements for offline mode (#2690)
    Co-authored-by: sliptype <sliptype@gmail.com>

--- packages/mobile/src/hooks/useReachabilityState.ts
+++ packages/mobile/src/hooks/useReachabilityState.ts
@@ -2,27 +2,32 @@ import { useEffect, useCallback } from 'react'
 
 import { reachabilitySelectors } from '@audius/common'
 import { useSelector } from 'react-redux'
+import { usePrevious } from 'react-use'
 
 type OnBecomeReachable = () => any
 type OnBecomeUnreachable = () => any
+
+const { getIsReachable } = reachabilitySelectors
 
 export const useReachabilityState = (
   onBecomeReachable: OnBecomeReachable | null,
   onBecomeUnreachable: OnBecomeUnreachable | null
 ) => {
-  const currentReachability = useSelector(reachabilitySelectors.getIsReachable)
+  const currentReachability = useSelector(getIsReachable)
+  const prevReachability = usePrevious(currentReachability)
+
   const handleReachabilityStateChange = useCallback(
     (nextReachabilityState: boolean) => {
-      if (nextReachabilityState && onBecomeReachable) {
+      if (nextReachabilityState && !prevReachability && onBecomeReachable) {
         console.info('Become reachable')
         onBecomeReachable()
       }
-      if (!nextReachabilityState && onBecomeUnreachable) {
+      if (!nextReachabilityState && prevReachability && onBecomeUnreachable) {
         console.info('Become unreachable')
         onBecomeUnreachable()
       }
     },
-    [onBecomeReachable, onBecomeUnreachable]
+    [onBecomeReachable, onBecomeUnreachable, prevReachability]
   )
 
   useEffect(() => {
