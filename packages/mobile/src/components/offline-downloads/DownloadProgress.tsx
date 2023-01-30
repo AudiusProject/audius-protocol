bbc24773a8d24f7d794e9cbb863c98e3a0e8e52b
Raymond Jacobson <ray@audius.co>
2023-01-30 14:50:43 -0800

    [C-1963] Add download progress bar (#2730)

---
+++ packages/mobile/src/components/offline-downloads/DownloadProgress.tsx
@@ -0,0 +1,56 @@
+import { View } from 'react-native'
+import { useSelector } from 'react-redux'
+
+import { Text } from 'app/components/core'
+import { ProgressBar } from 'app/components/progress-bar'
+import { getOfflineDownloadStatus } from 'app/store/offline-downloads/selectors'
+import { OfflineTrackDownloadStatus } from 'app/store/offline-downloads/slice'
+import { makeStyles } from 'app/styles'
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  root: {
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    marginRight: spacing(2)
+  },
+  text: {
+    marginBottom: 2
+  },
+  progressBar: {
+    width: 98,
+    height: spacing(1),
+    borderRadius: 8,
+    marginVertical: 0,
+    backgroundColor: palette.neutralLight4
+  }
+}))
+
+export const DownloadProgress = () => {
+  const styles = useStyles()
+  const downloadStatus = useSelector(getOfflineDownloadStatus)
+  const numDownloads = Object.keys(downloadStatus).length
+  const numDownloadsComplete = Object.values(downloadStatus).filter(
+    (status) =>
+      status === OfflineTrackDownloadStatus.SUCCESS ||
+      status === OfflineTrackDownloadStatus.ERROR
+  ).length
+
+  // Only render if there are active downloads
+  if (numDownloadsComplete === numDownloads) return null
+
+  return (
+    <View style={styles.root}>
+      <Text style={styles.text} color='neutral' weight='demiBold' fontSize='xs'>
+        {`${numDownloadsComplete}/${numDownloads}`}
+      </Text>
+      <ProgressBar
+        style={{
+          root: styles.progressBar
+        }}
+        progress={numDownloadsComplete}
+        max={numDownloads}
+      />
+    </View>
+  )
+}
