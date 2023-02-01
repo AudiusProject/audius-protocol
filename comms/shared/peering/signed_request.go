929a7bd056375e66d488a5ceafd58a43d6d82bde
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-02-01 10:28:44 -0800

    Add new ReadSigned method for manual signature verifying

--- comms/shared/peering/signed_request.go
+++ comms/shared/peering/signed_request.go
@@ -10,31 +10,39 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func ReadSignedRequest(c echo.Context) (payload []byte, wallet string, err error) {
+func ReadSignedRequest(c echo.Context) ([]byte, string, error) {
+	var payload []byte
+	var err error
 	if c.Request().Method == "GET" {
 		payload = []byte(c.Request().URL.Path)
 	} else if c.Request().Method == "POST" {
 		payload, err = io.ReadAll(c.Request().Body)
 	} else {
-		err = errors.New("unsupported request type")
+		err = errors.New("Unsupported request method " + c.Request().Method)
 	}
-	if err != nil {
-		return
+	if (err != nil) {
+		return nil, "", err
 	}
 
 	sigHex := c.Request().Header.Get(config.SigHeader)
-	sig, err := base64.StdEncoding.DecodeString(sigHex)
+	wallet, err := ReadSigned(sigHex, payload)
+	return payload, wallet, err
+}
+
+
+func ReadSigned(signatureHex string, signedData []byte) (string, error) {
+	sig, err := base64.StdEncoding.DecodeString(signatureHex)
 	if err != nil {
 		err = errors.New("bad sig header: " + err.Error())
-		return
+		return "", err
 	}
 
 	// recover
-	hash := crypto.Keccak256Hash(payload)
+	hash := crypto.Keccak256Hash(signedData)
 	pubkey, err := crypto.SigToPub(hash[:], sig)
 	if err != nil {
-		return
+		return "", err
 	}
-	wallet = crypto.PubkeyToAddress(*pubkey).Hex()
-	return
+	wallet := crypto.PubkeyToAddress(*pubkey).Hex()
+	return wallet, nil
 }
