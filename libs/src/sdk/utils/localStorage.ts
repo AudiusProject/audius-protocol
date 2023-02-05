b1d3870f2db1b49b7a65b65eedc5801f95c0ccf8
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-02-05 02:08:25 -0800

    Add local storage type and Promise.any ponyfill

---
+++ libs/src/sdk/utils/localStorage.ts
@@ -0,0 +1,5 @@
+export type LocalStorage = {
+  getItem: (key: string) => Promise<string | null> | string | null
+  setItem: (key: string, value: string) => Promise<void> | void
+  removeItem: (key: string) => Promise<void> | void
+}
