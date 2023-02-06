88254a4ca3f2fb4898d927ca6d2e437cdde730cd
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-02-05 18:17:27 -0800

    Add ts-jest

--- libs/jest.config.ts
+++ libs/jest.config.ts
@@ -100,7 +100,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
