a77f53c427d8421a2b1c96da702f541a707a7085
Dylan Jeffers <dylan@audius.co>
2023-02-09 11:34:40 -0800

    Remove clear offline downloads button (#2811)

--- packages/mobile/src/screens/favorites-screen/FavoritesScreen.tsx
+++ packages/mobile/src/screens/favorites-screen/FavoritesScreen.tsx
@@ -7,19 +7,13 @@ import IconAlbum from 'app/assets/images/iconAlbum.svg'
 import IconFavorite from 'app/assets/images/iconFavorite.svg'
 import IconNote from 'app/assets/images/iconNote.svg'
 import IconPlaylists from 'app/assets/images/iconPlaylists.svg'
-import {
-  Button,
-  Screen,
-  ScreenContent,
-  ScreenHeader
-} from 'app/components/core'
+import { Screen, ScreenContent, ScreenHeader } from 'app/components/core'
 import { TopTabNavigator } from 'app/components/top-tab-bar'
 import { useAppTabScreen } from 'app/hooks/useAppTabScreen'
 import {
   useIsOfflineModeEnabled,
   useReadOfflineOverride
 } from 'app/hooks/useIsOfflineModeEnabled'
-import { clearOfflineDownloads } from 'app/store/offline-downloads/slice'
 
 import { AlbumsTab } from './AlbumsTab'
 import { DownloadFavoritesSwitch } from './DownloadFavoritesSwitch'
@@ -73,10 +67,6 @@ export const FavoritesScreen = () => {
           <View style={{ flexDirection: 'row' }}>
             <DownloadProgress />
             <DownloadFavoritesSwitch />
-            <Button
-              title='clear'
-              onPress={() => dispatch(clearOfflineDownloads())}
-            />
           </View>
         ) : null}
       </ScreenHeader>
