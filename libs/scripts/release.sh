5778b58255d4a4b157802c930977ed8f11208b16
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-02-09 19:32:49 -0800

    Temp disable main/release branch check

--- libs/scripts/release.sh
+++ libs/scripts/release.sh
@@ -59,11 +59,11 @@ function git-reset () {
         fi
 
         # only allow commits found on main or release branches to be deployed
-        echo "commit has to be on main or a release branch"
-        git branch -a --contains ${GIT_COMMIT} \
-            | tee /dev/tty \
-            | grep -Eq 'remotes/origin/main|remotes/origin/release' \
-            || exit 1
+        # echo "commit has to be on main or a release branch"
+        # git branch -a --contains ${GIT_COMMIT} \
+        #     | tee /dev/tty \
+        #     | grep -Eq 'remotes/origin/main|remotes/origin/release' \
+        #     || exit 1
 
         # Ensure working directory clean
         git reset --hard ${GIT_COMMIT}
