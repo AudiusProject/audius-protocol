b47587d6d32acc6826b8e86a284b3b5d43af77a5
Johannes Naylor <jonaylor89@gmail.com>
2023-02-14 01:02:32 +0100

    start working on storage layer persistence

--- comms/storage/persistence/persistence.go
+++ comms/storage/persistence/persistence.go
@@ -1,57 +1,74 @@
-// Package longterm handles taking content from the temp store and storing it in the long-term store.
-package longterm
+// Package persistence handles taking content from the temp store and storing it in the persistence store.
+package persistence
 
 import (
 	"context"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
-	"os"
 	"strings"
+	"golang.org/x/exp/slices"
 
 	"comms.audius.co/storage/decider"
 	"comms.audius.co/storage/transcode"
 	"github.com/nats-io/nats.go"
+
 	"gocloud.dev/blob"
 	_ "gocloud.dev/blob/fileblob"
 )
 
-type LongTerm struct {
+var (
+	prefixWhitelist []string = []string{"https", "gcs", "s3", "azblob"}
+)
+
+type Persistence struct {
 	streamToStoreFrom string
 	storageDecider    decider.StorageDecider
 	jsc               nats.JetStreamContext
 	blobStore         *blob.Bucket
 }
 
-// New creates a struct that listens to streamToStoreFrom and downloads content from the temp store to the long-term store.
-func New(thisNodePubKey, streamToStoreFrom, blobDriverURL string, storageDecider decider.StorageDecider, jsc nats.JetStreamContext) *LongTerm {
-	if strings.HasPrefix(blobDriverURL, "file://") {
-		dir := blobDriverURL[len("file://"):]
-		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-			log.Fatalln("failed to create long-term storage dir: ", err)
-		}
+// New creates a struct that listens to streamToStoreFrom and downloads content from the temp store to the persistent store.
+func New(thisNodePubKey, streamToStoreFrom, blobDriverURL string, storageDecider decider.StorageDecider, jsc nats.JetStreamContext) (*Persistence, error) {
+
+	// TODO: this logic should probably be on the config side
+	prefix, _, _ := strings.Cut(blobDriverURL, "://")
+	if !slices.Contains(prefixWhitelist, prefix) {
+		return nil, errors.New("blobDriverURL's prefix isn't valid. Valid prefixes include: " + strings.Join(prefixWhitelist, ","))
 	}
+
+	// S3 = https://github.com/google/go-cloud/blob/master/blob/s3blob/example_test.go#L73
+	// GCS = https://github.com/google/go-cloud/blob/master/blob/gcsblob/example_test.go#L57
+	// i.e. we should be good to have credentials set as env vars
+
+	// same for azure: Go CDK uses the environment variables 
+	// AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY, and AZURE_STORAGE_SAS_TOKEN 
+	// to configure the credentials
+	// AZURE_STORAGE_ACCOUNT is required, along with one of the other two.
+
 	b, err := blob.OpenBucket(context.Background(), blobDriverURL)
 	if err != nil {
 		log.Fatalln("failed to open bucket", blobDriverURL)
+		return nil, err
 	}
 
-	longTerm := &LongTerm{
+	persistence := &Persistence{
 		streamToStoreFrom: streamToStoreFrom,
 		storageDecider:    storageDecider,
 		jsc:               jsc,
 		blobStore:         b,
 	}
 
-	longTerm.runStorer(thisNodePubKey)
-	return longTerm
+	persistence.runStorer(thisNodePubKey)
+	return persistence, nil
 }
 
-// GetKeyForFileName returns the key (including namespaced shard) for long-term storage for fileName, which MUST end in "_<string>.<extension>".
+// GetKeyForFileName returns the key (including namespaced shard) for persistent storage for fileName, which MUST end in "_<string>.<extension>".
 // fileName can optionally have a shard already prepended (e.g., "<shard>/<cuid>_<string>.<extension>")
-func (lt *LongTerm) getKeyForFileName(fileName string) (string, error) {
+func (persist *Persistence) getKeyForFileName(fileName string) (string, error) {
 	if idx := strings.LastIndex(fileName, "_"); idx != -1 {
 		cuid := fileName[:idx]
 		suffix := fileName[idx+1:]
@@ -61,7 +78,7 @@ func (lt *LongTerm) getKeyForFileName(fileName string) (string, error) {
 			cuid = cuid[strings.Index(cuid, "/")+1:]
 		}
 
-		namespacedShard, err := lt.storageDecider.GetNamespacedShardForID(cuid)
+		namespacedShard, err := persist.storageDecider.GetNamespacedShardForID(cuid)
 		if err != nil {
 			return "", fmt.Errorf("unable to parse shard from fileName %q with cuid %q: %v", fileName, cuid, err)
 		}
@@ -71,21 +88,21 @@ func (lt *LongTerm) getKeyForFileName(fileName string) (string, error) {
 	}
 }
 
-// Get returns a file from the long-term store.
-func (lt *LongTerm) Get(fileName string) (io.Reader, error) {
-	key, err := lt.getKeyForFileName(fileName)
+// Get returns a file from the persistent store.
+func (persist *Persistence) Get(fileName string) (io.Reader, error) {
+	key, err := persist.getKeyForFileName(fileName)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key for %q: %v", fileName, err)
 	}
-	reader, err := lt.blobStore.NewReader(context.Background(), key, nil)
+	reader, err := persist.blobStore.NewReader(context.Background(), key, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read %q: %v", key, err)
 	}
 	return reader, nil
 }
 
-func (lt *LongTerm) GetKeysIn(shard string) ([]string, error) {
-	it := lt.blobStore.List(&blob.ListOptions{Prefix: shard})
+func (persist *Persistence) GetKeysIn(shard string) ([]string, error) {
+	it := persist.blobStore.List(&blob.ListOptions{Prefix: shard})
 
 	keys := []string{}
 
@@ -107,8 +124,8 @@ type KeyAndMD5 struct {
 	MD5 string `json:"md5"`
 }
 
-func (lt *LongTerm) GetKeysAndMD5sIn(shard string) ([]*KeyAndMD5, error) {
-	it := lt.blobStore.List(&blob.ListOptions{Prefix: shard})
+func (persist *Persistence) GetKeysAndMD5sIn(shard string) ([]*KeyAndMD5, error) {
+	it := persist.blobStore.List(&blob.ListOptions{Prefix: shard})
 
 	keysAndMD5s := []*KeyAndMD5{}
 	for {
@@ -119,7 +136,7 @@ func (lt *LongTerm) GetKeysAndMD5sIn(shard string) ([]*KeyAndMD5, error) {
 		if err != nil {
 			return nil, err
 		}
-		attrs, err := lt.blobStore.Attributes(context.Background(), obj.Key)
+		attrs, err := persist.blobStore.Attributes(context.Background(), obj.Key)
 		if err != nil {
 			return nil, err
 		}
@@ -134,9 +151,9 @@ type ShardAndFile struct {
 	MD5      string `json:"md5"`
 }
 
-func (lt *LongTerm) GetJobResultsFor(id string) ([]*ShardAndFile, error) {
+func (persist *Persistence) GetJobResultsFor(id string) ([]*ShardAndFile, error) {
 	// Get the job from the KV store for the given ID
-	kv, err := lt.jsc.KeyValue(lt.streamToStoreFrom[len("KV_"):])
+	kv, err := persist.jsc.KeyValue(persist.streamToStoreFrom[len("KV_"):])
 	if err != nil {
 		return nil, fmt.Errorf("could not get KV store: %v", err)
 	}
@@ -151,23 +168,23 @@ func (lt *LongTerm) GetJobResultsFor(id string) ([]*ShardAndFile, error) {
 	}
 
 	// Get the shard that the job's output should be stored in
-	shard, err := lt.storageDecider.GetNamespacedShardForID(job.ID)
+	shard, err := persist.storageDecider.GetNamespacedShardForID(job.ID)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get shard for %q: %v", job.ID, err)
 	}
 
-	// Get the resulting outputs of each job from long-term storage
+	// Get the resulting outputs of each job from persistent storage
 	files := []*ShardAndFile{}
 	for _, jobOutput := range job.Results {
 		outputKey := shard + "/" + jobOutput.Name
-		exists, err := lt.blobStore.Exists(context.Background(), outputKey)
+		exists, err := persist.blobStore.Exists(context.Background(), outputKey)
 		if err != nil {
-			return nil, fmt.Errorf("error: failed to check if %q exists in long-term storage: %v", outputKey, err)
+			return nil, fmt.Errorf("error: failed to check if %q exists in persistent storage: %v", outputKey, err)
 		}
 		if exists {
-			attrs, err := lt.blobStore.Attributes(context.Background(), outputKey)
+			attrs, err := persist.blobStore.Attributes(context.Background(), outputKey)
 			if err != nil {
-				return nil, fmt.Errorf("error: failed to get attributes for %q in long-term storage: %v", outputKey, err)
+				return nil, fmt.Errorf("error: failed to get attributes for %q in persistent storage: %v", outputKey, err)
 			}
 			files = append(files, &ShardAndFile{
 				Shard:    shard,
@@ -185,36 +202,36 @@ func (lt *LongTerm) GetJobResultsFor(id string) ([]*ShardAndFile, error) {
 	return files, nil
 }
 
-// runStorer runs a goroutine to pull tracks from temp NATS object storage to long-term object storage.
-func (lt *LongTerm) runStorer(thisNodePubKey string) {
+// runStorer runs a goroutine to pull tracks from temp NATS object storage to persistent object storage.
+func (persist *Persistence) runStorer(thisNodePubKey string) {
 	// Create a per-node explicit pull consumer on the stream that backs the track upload status KV bucket
 	storerDurable := fmt.Sprintf("STORER_%s", thisNodePubKey)
-	_, err := lt.jsc.AddConsumer(lt.streamToStoreFrom, &nats.ConsumerConfig{
+	_, err := persist.jsc.AddConsumer(persist.streamToStoreFrom, &nats.ConsumerConfig{
 		Durable:       storerDurable,
 		AckPolicy:     nats.AckExplicitPolicy,
 		DeliverPolicy: nats.DeliverAllPolicy, // Using the "all" policy means when a node registers it will download every track that it needs
 		ReplayPolicy:  nats.ReplayInstantPolicy,
 	})
 	if err != nil {
-		log.Fatalf("Error creating consumer for long-term file storer: %q\n", err)
+		log.Fatalf("Error creating consumer for persistent file storer: %q\n", err)
 	}
 
 	// Create a subscription on the consumer for every node
 	// Subject can be empty since it defaults to all subjects bound to the stream
-	storerSub, err := lt.jsc.PullSubscribe("", storerDurable, nats.BindStream(lt.streamToStoreFrom))
+	storerSub, err := persist.jsc.PullSubscribe("", storerDurable, nats.BindStream(persist.streamToStoreFrom))
 	if err != nil {
-		log.Fatalf("Error creating subscription for long-term file storer: %q\n", err)
+		log.Fatalf("Error creating subscription for persistent file storer: %q\n", err)
 	}
 
-	// Watch KV store to download files to long-term storage
+	// Watch KV store to download files to persistent storage
 	// TODO: Maybe there should be an exit channel for in case we restart StorageServer without restarting the whole program? (e.g., if we want to update StorageDecider to pass it a new slice of storage node pubkeys)
 	go func() {
 		for {
 			msgs, err := storerSub.Fetch(1)
 			if err == nil {
 				msg := msgs[0]
-				if err := lt.processMessage(msg); err != nil {
-					log.Printf("longterm processMessage failed: %q\n", err)
+				if err := persist.processMessage(msg); err != nil {
+					log.Printf("persistent processMessage failed: %q\n", err)
 					msg.Nak()
 				} else {
 					msg.Ack()
@@ -226,11 +243,11 @@ func (lt *LongTerm) runStorer(thisNodePubKey string) {
 	}()
 }
 
-// processMessage will move file to longterm storage if this server is responsible for the shard.
+// processMessage will move file to persistent storage if this server is responsible for the shard.
 // should only return an error if storage failed... error will stall consumer and retry until succeeds.
 // reason being: if storage fails for this file it probably will for the next and we shouldn't just go ACKing all these storage operations that failed.
 // if a Nak is too extreme... we could instead retry this N times and then Ack in the calling function
-func (lt *LongTerm) processMessage(msg *nats.Msg) error {
+func (persist *Persistence) processMessage(msg *nats.Msg) error {
 	job := transcode.Job{}
 	err := json.Unmarshal(msg.Data, &job)
 	if err != nil {
@@ -240,13 +257,13 @@ func (lt *LongTerm) processMessage(msg *nats.Msg) error {
 
 	if job.Status == transcode.JobStatusDone {
 
-		shouldStore, err := lt.storageDecider.ShouldStore(job.ID)
+		shouldStore, err := persist.storageDecider.ShouldStore(job.ID)
 		if err != nil {
 			return fmt.Errorf("failed to determine if should store %q: %q", job.ID, err)
 		}
 		if shouldStore {
 			fmt.Printf("Storing file with ID %q\n", job.ID)
-			return lt.moveTempToLongTerm(job)
+			return persist.moveTempToPersistent(job)
 		} else {
 			fmt.Printf("Not storing file with ID %q\n", job.ID)
 		}
@@ -255,18 +272,18 @@ func (lt *LongTerm) processMessage(msg *nats.Msg) error {
 	return nil
 }
 
-func (lt *LongTerm) moveTempToLongTerm(job transcode.Job) error {
-	// Store results in long-term shard, which is different from the temp store's bucket name
-	shard, err := lt.storageDecider.GetNamespacedShardForID(job.ID)
+func (persist *Persistence) moveTempToPersistent(job transcode.Job) error {
+	// Store results in persistent shard, which is different from the temp store's bucket name
+	shard, err := persist.storageDecider.GetNamespacedShardForID(job.ID)
 	if err != nil {
 		return fmt.Errorf("failed to get shard for %q: %v", job.ID, err)
 	}
 
 	for _, tmpObj := range job.Results {
-		ltKey := shard + "/" + tmpObj.Name
+		persistKey := shard + "/" + tmpObj.Name
 
 		// Get object from temp store
-		objStore, err := lt.jsc.ObjectStore(tmpObj.Bucket)
+		objStore, err := persist.jsc.ObjectStore(tmpObj.Bucket)
 		if err != nil {
 			return fmt.Errorf("failed to get object from temp store: %v", err)
 		}
@@ -276,9 +293,9 @@ func (lt *LongTerm) moveTempToLongTerm(job transcode.Job) error {
 		}
 
 		// Open a *blob.Writer for the blob at key=shard/tmpObj.Name
-		writer, err := lt.blobStore.NewWriter(context.Background(), ltKey, nil)
+		writer, err := persist.blobStore.NewWriter(context.Background(), persistKey, nil)
 		if err != nil {
-			return fmt.Errorf("failed to write %q: %v", ltKey, err)
+			return fmt.Errorf("failed to write %q: %v", persistKey, err)
 		}
 
 		// Copy the data
