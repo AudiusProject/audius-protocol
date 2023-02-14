e998201729b020bdbf1b411a9a0f1f75e9b1fe79
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2023-02-14 16:41:59 +0100

    C-2119 Show downloaded collections only (#2842)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- apps/audius-client/packages/mobile/src/screens/favorites-screen/PlaylistsTab.tsx
+++ apps/audius-client/packages/mobile/src/screens/favorites-screen/PlaylistsTab.tsx
@@ -1,32 +1,21 @@
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useState } from 'react'
 
-import {
-  useProxySelector,
-  reachabilitySelectors,
-  shallowCompare
-} from '@audius/common'
+import { reachabilitySelectors } from '@audius/common'
 import Animated, { FadeIn, FadeOut, Layout } from 'react-native-reanimated'
 import { useSelector } from 'react-redux'
 
 import { CollectionList } from 'app/components/collection-list'
-import { VirtualizedScrollView, Button } from 'app/components/core'
+import { Button, VirtualizedScrollView } from 'app/components/core'
 import { EmptyTileCTA } from 'app/components/empty-tile-cta'
 import { useIsOfflineModeEnabled } from 'app/hooks/useIsOfflineModeEnabled'
 import { useNavigation } from 'app/hooks/useNavigation'
-import type { AppState } from 'app/store'
-import {
-  getIsDoneLoadingFromDisk,
-  getOfflineTrackStatus
-} from 'app/store/offline-downloads/selectors'
-import { OfflineDownloadStatus } from 'app/store/offline-downloads/slice'
 
 import type { FavoritesTabScreenParamList } from '../app-screen/FavoritesTabScreen'
 
 import { FilterInput } from './FilterInput'
 import { NoTracksPlaceholder } from './NoTracksPlaceholder'
 import { OfflineContentBanner } from './OfflineContentBanner'
-import { getAccountCollections } from './selectors'
-import { buildCollectionIdsToNumPlayableTracksMap } from './utils'
+import { useCollectionScreenData } from './useCollectionScreenData'
 
 const { getIsReachable } = reachabilitySelectors
 
@@ -37,75 +26,15 @@ const messages = {
 
 export const PlaylistsTab = () => {
   const navigation = useNavigation<FavoritesTabScreenParamList>()
-  const [filterValue, setFilterValue] = useState('')
-  const isOfflineModeEnabled = useIsOfflineModeEnabled()
-  const isReachable = useSelector(getIsReachable)
-  const isDoneLoadingFromDisk = useSelector(getIsDoneLoadingFromDisk)
-  const offlineDownloadStatus = useProxySelector(
-    (state: AppState) => {
-      if (isDoneLoadingFromDisk && isOfflineModeEnabled && !isReachable) {
-        return getOfflineTrackStatus(state)
-      }
-      // We don't need offline download status when we're not offline. This saves us rerenders while we're downloading things and updating the offline download slice.
-      return undefined
-    },
-    [isReachable, isOfflineModeEnabled, isDoneLoadingFromDisk]
-  )
-
-  const userPlaylists = useProxySelector(
-    (state: AppState) => {
-      if (isOfflineModeEnabled && !isReachable) {
-        if (!isDoneLoadingFromDisk) {
-          return []
-        }
-      }
-      return getAccountCollections(state, filterValue).filter((collection) => {
-        if (collection.is_album) {
-          return false
-        }
-        if (isOfflineModeEnabled && !isReachable) {
-          const trackIds =
-            collection.playlist_contents.track_ids.map(
-              (trackData) => trackData.track
-            ) ?? []
-
-          // Don't show a playlist in Offline Mode if it has at least one track but none of the tracks have been downloaded yet
-          return (
-            !collection.is_album &&
-            (trackIds.length === 0 ||
-              trackIds.some((t) => {
-                return (
-                  offlineDownloadStatus &&
-                  offlineDownloadStatus[t.toString()] ===
-                    OfflineDownloadStatus.SUCCESS
-                )
-              }))
-          )
-        }
-        return true
-      })
-    },
-    [
-      filterValue,
-      isReachable,
-      isOfflineModeEnabled,
-      isDoneLoadingFromDisk,
-      offlineDownloadStatus
-    ],
-    shallowCompare
-  )
-
-  const numPlayableTracksMap = useMemo(() => {
-    return buildCollectionIdsToNumPlayableTracksMap(
-      userPlaylists,
-      isOfflineModeEnabled && !isReachable,
-      offlineDownloadStatus || {}
-    )
-  }, [isOfflineModeEnabled, isReachable, offlineDownloadStatus, userPlaylists])
-
   const handleNavigateToNewPlaylist = useCallback(() => {
     navigation.push('CreatePlaylist')
   }, [navigation])
+
+  const [filterValue, setFilterValue] = useState('')
+  const { filteredCollections: userPlaylists, collectionIdsToNumTracks } =
+    useCollectionScreenData(filterValue, 'playlists')
+  const isOfflineModeEnabled = useIsOfflineModeEnabled()
+  const isReachable = useSelector(getIsReachable)
 
   return (
     <VirtualizedScrollView listKey='favorites-playlists-view'>
@@ -138,7 +67,7 @@ export const PlaylistsTab = () => {
               listKey='favorites-playlists'
               scrollEnabled={false}
               collection={userPlaylists}
-              collectionIdsToNumTracks={numPlayableTracksMap}
+              collectionIdsToNumTracks={collectionIdsToNumTracks}
             />
           </Animated.View>
         </>
