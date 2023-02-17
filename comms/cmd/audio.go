4f8bcaa44b0dac1c7fba5e6dc4fbbae28c5253bc
Johannes Naylor <jonaylor89@gmail.com>
2023-02-18 00:10:54 +0100

    add audio subcommand

--- comms/cmd/audio.go
+++ comms/cmd/audio.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"math/rand"
@@ -14,7 +13,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/cryptix/wav"
 	"github.com/spf13/cobra"
 )
 
@@ -37,7 +35,7 @@ var audioCmd = &cobra.Command{
 				continue
 			}
 
-			filename := fmt.Sprintf("audio-seed-%d.wav", i)
+			filename := fmt.Sprintf("audio-seed-%d.mp3", i)
 
 			nodeNumber := rand.Intn(4)
 			node := idToNode[nodeNumber]
@@ -58,127 +56,19 @@ func init() {
 	audioCmd.Flags().IntVarP(&audioCount, "count", "c", 100, "the number of random audio files")
 }
 
-// Implements io.ReadWriteSeeker for testing purposes.
-type FileBuffer struct {
-    buffer []byte
-    offset int64
-}
-
-// Creates new buffer that implements io.ReadWriteSeeker for testing purposes.
-func NewFileBuffer(initial []byte) FileBuffer {
-    if initial == nil {
-        initial = make([]byte, 25, 100)
-    }
-    return FileBuffer{
-        buffer: initial,
-        offset: 0,
-    }
-}
-
-func (fb FileBuffer) Bytes() []byte {
-    return fb.buffer
-}
-
-func (fb FileBuffer) Len() int {
-    return len(fb.buffer)
-}
-
-func (fb FileBuffer) Read(b []byte) (int, error) {
-    available := len(fb.buffer) - int(fb.offset)
-    if available == 0 {
-        return 0, io.EOF
-    }
-    size := len(b)
-    if size > available {
-        size = available
-    }
-    copy(b, fb.buffer[fb.offset:fb.offset+int64(size)])
-    fb.offset += int64(size)
-    return size, nil
-}
-
-func (fb FileBuffer) Write(b []byte) (int, error) {
-    copied := copy(fb.buffer[fb.offset:], b)
-    if copied < len(b) {
-        fb.buffer = append(fb.buffer, b[copied:]...)
-    }
-    fb.offset += int64(len(b))
-    return len(b), nil
-}
-
-func (fb FileBuffer) Seek(offset int64, whence int) (int64, error) {
-    var newOffset int64
-    switch whence {
-    case io.SeekStart:
-        newOffset = offset
-    case io.SeekCurrent:
-        newOffset = fb.offset + offset
-    case io.SeekEnd:
-        newOffset = int64(len(fb.buffer)) + offset
-    default:
-        return 0, errors.New("Unknown Seek Method")
-    }
-    if newOffset > int64(len(fb.buffer)) || newOffset < 0 {
-        return 0, fmt.Errorf("Invalid Offset %d", offset)
-    }
-    fb.offset = newOffset
-    return newOffset, nil
-}
-
-func (fb FileBuffer) Close() error {
-	return nil
-}
-
-func generateWhiteNoise() ([]byte, error) {
-	buffer := NewFileBuffer(nil)
-
-	bits := 32
-	rate := 44100
-	duration := 60
-
-	meta := wav.File{
-		Channels:        1,
-		SampleRate:      uint32(rate),
-		SignificantBits: uint16(bits),
-	}
-
-	writer, err := meta.NewWriter(buffer)
-	if err != nil {
-		return nil, err
-	}
-	defer writer.Close()
-
-
-	for i := 0; i < (duration)*(rate); i += 1 {
-		var sample []byte = make([]byte, bits/8)
-
-		_, err := rand.Read(sample)
-		if err != nil {
-			return nil, err
-		}
-
-		err = writer.WriteSample(sample)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return buffer.Bytes(), nil
-}
-
 func generateWhiteNoiseFFmpeg() ([]byte, error){
+
+	id := rand.Int()
+	tmpFile := fmt.Sprintf("tmp-%d.mp3", id)
+
+	duration := 60 + 10
 	cmd := exec.Command("ffmpeg", "-y", // Yes to all
 		"-f", // audio/video filtering framework
 		"lavfi", // provides generic audio filtering for audio/video signals
 		"-i", // input flag
-		`anoisesrc=d=${duration}`, // generate a noise audio signal for the duration
-        "pipe:1", // output to stdout
+		fmt.Sprintf("anoisesrc=d=%d", duration), // generate a noise audio signal for the duration
+		tmpFile,
     )
-
-    resultBuffer := bytes.NewBuffer(make([]byte, 5*1024*1024)) // pre allocate 5MiB buffer
-
-    cmd.Stderr = os.Stderr // bind log stream to stderr
-    cmd.Stdout = resultBuffer // stdout result will be written here
 
     err := cmd.Start() // Start a process on another goroutine
 	if err != nil {
@@ -190,7 +80,9 @@ func generateWhiteNoiseFFmpeg() ([]byte, error){
 		return nil, err
 	}
 
-	return resultBuffer.Bytes(), nil 
+	data, err := os.ReadFile(tmpFile)
+
+	return data, nil 
 }
 
 func uploadAudio(node string, audioData []byte, filename string) error {
@@ -216,7 +108,7 @@ func uploadAudio(node string, audioData []byte, filename string) error {
 		if _, ok := r.(*bytes.Reader); ok {
 			h := make(textproto.MIMEHeader)
 			h.Set("Content-Disposition", fmt.Sprintf(`form-data; name="%s"; filename="%s"`, key, filename))
-			h.Set("Content-Type", "audio/wav")
+			h.Set("Content-Type", "audio/mpeg")
 			if fw, err = w.CreatePart(h); err != nil {
 				fmt.Printf("Error creating file form field %+v\n", err)
 				return err
