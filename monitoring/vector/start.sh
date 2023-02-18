2bdb14a478c6029939656e5de3070a70cb3d48c5
Raymond Jacobson <ray@audius.co>
2023-02-17 16:55:41 -0800

    Update start.sh

--- monitoring/vector/start.sh
+++ monitoring/vector/start.sh
@@ -1,3 +1,24 @@
+#!/usr/bin/env sh
+
+# set hostname to ${audius_discprov_url}, else ${creatorNodeEndpoint}
+if [[ "${audius_discprov_url}" ]]; then
+  hostname=${audius_discprov_url}
+elif [[ "${creatorNodeEndpoint}" ]]; then
+  hostname=${creatorNodeEndpoint}
+# if neither hostname is not defined, set audius_delegate_owner_wallet as a proxy for hostname
+elif [[ "${audius_delegate_owner_wallet}" ]]; then
+  hostname=${audius_delegate_owner_wallet}
+elif [[ "${delegateOwnerWallet}"]]; then
+  hostname=${delegateOwnerWallet}
+fi
+
+# use regex to extract domain in url (source: https://stackoverflow.com/a/2506635/8674706)
+if [[ "${hostname}" ]]; then
+   hostname=$(echo ${hostname} | sed -e 's/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/')
+fi
+
+tags=${hostname}
+
 # Generate a vector.toml
 cat <<-EOF > $PWD/vector.toml
 [api]
@@ -8,13 +29,13 @@ address = "0.0.0.0:8686"
 type = "docker_logs"
 docker_host = "/var/run/docker.sock"
 include_containers = [ "audius/" ]
-exclude_containers = []
+exclude_containers = [ "audius/comms" ]
 
 [sinks.axiom]
 type = "axiom"
 inputs = [ "docker" ]
 token = $audius_axiom_token
-dataset = "vector.dev"
+dataset = $audius_axiom_dataset
 url = "https://cloud.axiom.co"
 org_id = "audius-Lu52"
 
@@ -25,4 +46,4 @@ encoding.codec = "json"
 EOF
 
 # Launch vector
-sleep infinity
+vector --config $PWD/vector.toml
