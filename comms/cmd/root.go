f215866822cbb4a894f7628e71d58b0c066a7eb6
Johannes Naylor <jonaylor89@gmail.com>
2023-02-21 22:51:53 +0100

    rm big comment blocks

--- comms/cmd/root.go
+++ comms/cmd/root.go
@@ -6,20 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "comms",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "The Audius Protocol implemented in Go",
+	Long: ``,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
