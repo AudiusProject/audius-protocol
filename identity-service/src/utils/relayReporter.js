263794382d22298a19aaaeaeddbb431cc76a7466
Alec Savvy <alecsavvy@outlook.com>
2023-02-23 16:45:13 -0700

    relay reporter base

---
+++ identity-service/src/utils/relayReporter.js
@@ -0,0 +1,40 @@
+const AnalyticsProvider = require('../analytics')
+
+const eventName = (chain, event) => (`Relay ${chain}: ${event}`)
+
+module.exports = class RelayReporter {
+    constructor({
+        shouldReportAnalytics = true,
+        childLogger = console,
+    }) {
+        this.childLogger = childLogger
+        this.shouldReportAnalytics = shouldReportAnalytics
+        this.analyticsProvider = new AnalyticsProvider()
+    }
+
+    async reportStart({ chain, userId }) {
+        await this.report({ eventName: eventName('Start', chain), userId, data: {} })
+    }
+
+    async reportSuccess({ chain, userId }) {
+        await this.report({ eventName: eventName('Success', chain), userId, data: {} })
+    }
+
+    async reportError({ chain, userId }) {
+        await this.report({ eventName: eventName('Error', chain), userId, data: {} })
+    }
+
+    async report({ eventName, userId, data }) {
+        try {
+            if (this.shouldReportAnalytics) {
+                this.analyticsProvider.track(
+                    eventName,
+                    userId,
+                    data
+                )
+            }
+        } catch (e) {
+            console.error(`RelayReporter error: ${JSON.stringify(e)}`)
+        }
+    }
+}
