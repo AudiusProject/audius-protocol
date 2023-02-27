68f4b5caba72c5d84dfce90c38d60ef7f514d2a5
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-02-27 13:40:17 -0500

    [PAY-894] Chat screen empty states (#2932)

--- apps/audius-client/packages/mobile/src/screens/chat-screen/ChatListScreen.tsx
+++ apps/audius-client/packages/mobile/src/screens/chat-screen/ChatListScreen.tsx
@@ -1,41 +1,93 @@
 import { useEffect } from 'react'
 
 import { chatActions, chatSelectors, Status } from '@audius/common'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 
 import IconCompose from 'app/assets/images/iconCompose.svg'
 import IconMessage from 'app/assets/images/iconMessage.svg'
+import Button, { ButtonType } from 'app/components/button'
 import { Screen, FlatList, ScreenContent } from 'app/components/core'
 import LoadingSpinner from 'app/components/loading-spinner'
 import { useNavigation } from 'app/hooks/useNavigation'
 import type { AppTabScreenParamList } from 'app/screens/app-screen'
 import { makeStyles } from 'app/styles'
-import { useThemePalette } from 'app/utils/theme'
+import { useThemePalette, useColor } from 'app/utils/theme'
 
 import { ChatListItem } from './ChatListItem'
 
 const { getChats, getChatsStatus } = chatSelectors
 
 const messages = {
-  title: 'Messages'
+  title: 'Messages',
+  startConversation: 'Start a Conversation!',
+  connect:
+    'Connect with other Audius users by\nstarting a private direct message!',
+  writeMessage: 'Write a Message'
 }
 
 const useStyles = makeStyles(({ spacing, palette, typography }) => ({
   rootContainer: {
     display: 'flex',
-    justifyContent: 'center',
     flexGrow: 1
   },
   spinnerContainer: {
-    height: spacing(28)
+    height: spacing(28),
+    padding: spacing(10)
   },
   loadingSpinner: {
     height: spacing(20),
     width: spacing(20),
     alignSelf: 'center'
+  },
+  startConversationContainer: {
+    marginVertical: spacing(8),
+    marginHorizontal: spacing(4),
+    backgroundColor: palette.white,
+    borderWidth: 1,
+    borderColor: palette.neutralLight7,
+    borderRadius: spacing(2),
+    padding: spacing(6)
+  },
+  startConversationTitle: {
+    fontSize: typography.fontSize.xxl,
+    fontFamily: typography.fontByWeight.bold,
+    textAlign: 'center',
+    lineHeight: typography.fontSize.xxl * 1.3,
+    color: palette.neutral
+  },
+  connect: {
+    fontSize: typography.fontSize.medium,
+    textAlign: 'center',
+    lineHeight: typography.fontSize.medium * 1.3,
+    color: palette.neutral,
+    marginTop: spacing(2)
+  },
+  writeMessageButton: {
+    marginTop: spacing(6)
   }
 }))
+
+const ChatsEmpty = ({ onPress }: { onPress: () => void }) => {
+  const white = useColor('white')
+  const styles = useStyles()
+  return (
+    <View style={styles.startConversationContainer}>
+      <Text style={styles.startConversationTitle}>
+        {messages.startConversation}
+      </Text>
+      <Text style={styles.connect}>{messages.connect}</Text>
+      <Button
+        title={messages.writeMessage}
+        renderIcon={() => <IconCompose fill={white} />}
+        iconPosition='left'
+        onPress={onPress}
+        containerStyle={styles.writeMessageButton}
+        type={ButtonType.PRIMARY}
+      />
+    </View>
+  )
+}
 
 export const ChatListScreen = () => {
   const styles = useStyles()
@@ -49,10 +101,11 @@ export const ChatListScreen = () => {
     dispatch(chatActions.fetchMoreChats())
   }, [dispatch])
 
+  const navigateToChatUserList = () => navigation.navigate('ChatUserList')
   const iconCompose = (
     <IconCompose
       fill={palette.neutralLight4}
-      onPress={() => navigation.navigate('ChatUserList')}
+      onPress={navigateToChatUserList}
     />
   )
 
@@ -60,6 +113,7 @@ export const ChatListScreen = () => {
     <Screen
       url='/chat'
       title={messages.title}
+      variant='secondary'
       icon={IconMessage}
       topbarRight={iconCompose}
     >
@@ -70,6 +124,9 @@ export const ChatListScreen = () => {
               data={chats}
               renderItem={({ item, index }) => <ChatListItem chat={item} />}
               keyExtractor={(item) => item.chat_id}
+              ListEmptyComponent={() => (
+                <ChatsEmpty onPress={navigateToChatUserList} />
+              )}
             />
           ) : (
             <View style={styles.spinnerContainer}>
