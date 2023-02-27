7c5c29ea9bdc4c51f9b3403e56239485c5034b34
Alec Savvy <alecsavvy@outlook.com>
2023-02-27 15:20:05 -0700

    cache all nodes

--- discovery-provider/src/utils/get_all_other_nodes.py
+++ discovery-provider/src/utils/get_all_other_nodes.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Tuple
 from src.utils import web3_provider
 from src.utils.config import shared_config
 from src.utils.helpers import is_fqdn, load_eth_abi_values
+from src.utils.redis_cache import get_json_cached_key, set_json_cached_key
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +13,7 @@ eth_abi_values = load_eth_abi_values()
 REWARDS_CONTRACT_ABI = eth_abi_values["EthRewardsManager"]["abi"]
 SP_FACTORY_REGISTRY_KEY = bytes("ServiceProviderFactory", "utf-8")
 DISCOVERY_NODE_SERVICE_TYPE = bytes("discovery-node", "utf-8")
+ALL_OTHER_NODES_CACHE_KEY = "discprov-all-other-nodes"
 
 
 # Perform eth web3 call to fetch endpoint info
@@ -96,3 +98,18 @@ def get_all_other_nodes() -> Tuple[List[str], List[str]]:
             )
 
     return all_other_nodes, all_other_wallets
+
+
+def get_all_other_nodes_cached(redis) -> Tuple[List[str], List[str]]:
+    """
+    Attempts to get the number of discovery nodes from redis
+    if that doesn't exist it will fetch using `get_all_other_nodes()`
+    and set the result in the cache with an expiration of 1 hour
+    """
+
+    cached_nodes = get_json_cached_key(redis, ALL_OTHER_NODES_CACHE_KEY)
+    if cached_nodes:
+        return cached_nodes
+    nodes = get_all_other_nodes()
+    set_json_cached_key(redis, ALL_OTHER_NODES_CACHE_KEY, nodes, 3600)
+    return nodes
