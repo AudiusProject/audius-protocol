630930b4885a19e4234a65a111324e7b168b88b7
Joseph Lee <joeylee0925@gmail.com>
2023-02-28 15:03:32 -0500

    Fix supporter rank up migration

--- discovery-provider/alembic/trigger_sql/handle_supporter_rank_ups.sql
+++ discovery-provider/alembic/trigger_sql/handle_supporter_rank_ups.sql
@@ -1,6 +1,7 @@
 create or replace function handle_supporter_rank_up() returns trigger as $$
 declare
   user_bank_tx user_bank_txs%ROWTYPE;
+  has_previous_supporter boolean;
   dethroned_user_id int;
 begin
   select * into user_bank_tx from user_bank_txs where user_bank_txs.slot = new.slot limit 1;
@@ -30,9 +31,10 @@ begin
       )
     on conflict do nothing;
 
-    if new.rank = 1 then 
-      select sender_user_id into dethroned_user_id from supporter_rank_ups where rank=1 order by slot desc limit 1;
-      if dethroned_user_id is not NULL then
+    if new.rank = 1 then
+      select sender_user_id into dethroned_user_id from supporter_rank_ups where rank=1 and receiver_user_id=new.receiver_user_id and slot < new.slot order by slot desc limit 1;
+      select true into has_previous_supporter from supporter_rank_ups where rank=new.receiver_user_id and receiver_user_id=1 HAVING COUNT(*) >= 2;
+      if has_previous_supporter is true and dethroned_user_id is not NULL then
         -- create a notification for the sender and receiver
         insert into notification
           (slot, user_ids, timestamp, type, specifier, group_id, data)
