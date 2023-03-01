799fef38b748f76466d63bf22489e559dc19f09d
Raymond Jacobson <ray@audius.co>
2023-02-28 18:24:03 -0800

    Insert new false is_current rows for bad diacritics

--- discovery-provider/alembic/versions/e68928955812_nfc_unicode_routes.py
+++ discovery-provider/alembic/versions/e68928955812_nfc_unicode_routes.py
@@ -10,11 +10,11 @@ import unicodedata
 import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.types import Integer, String
+from sqlalchemy.types import Boolean, Integer, String
 
 # revision identifiers, used by Alembic.
-revision = 'e68928955812'
-down_revision = '9b4e12c0c428'
+revision = "e68928955812"
+down_revision = "9b4e12c0c428"
 branch_labels = None
 depends_on = None
 
@@ -27,54 +27,103 @@ def track_route_updates(connection):
     track_routes = connection.execute(
         """
             SELECT
-                track_id,
                 slug,
-                title_slug
+                title_slug,
+                collision_id,
+                owner_id,
+                track_id,
+                blockhash,
+                blocknumber,
+                txhash
             FROM track_routes
             WHERE
                 is_current = True
         """
     )
-    track_ids = []
     slugs = []
     title_slugs = []
+    collision_ids = []
+    owner_ids = []
+    track_ids = []
+    blockhashes = []
+    blocknumbers = []
+    txhashes = []
+    is_currents = []
     for route in list(track_routes):
-        [track_id, slug, title_slug] = route
-        nfc_slug = unicodedata.normalize('NFC', slug)
+        [
+            slug,
+            title_slug,
+            collision_id,
+            owner_id,
+            track_id,
+            blockhash,
+            blocknumber,
+            txhash,
+        ] = route
+        nfc_slug = unicodedata.normalize("NFC", slug)
         if nfc_slug != slug:
-            nfc_title_slug = unicodedata.normalize('NFC', title_slug)
-            track_ids.append(track_id)
+            nfc_title_slug = unicodedata.normalize("NFC", title_slug)
             slugs.append(nfc_slug)
             title_slugs.append(nfc_title_slug)
+            collision_ids.append(collision_id)
+            owner_ids.append(owner_id)
+            track_ids.append(track_id)
+            blockhashes.append(blockhash)
+            blocknumbers.append(blocknumber)
+            txhashes.append(txhash)
+            is_currents.append(False)
 
-    if len(track_ids) == 0: # no updates
+    if len(track_ids) == 0:  # no updates
         return
 
     params = {
-        "track_ids": track_ids,
         "slugs": slugs,
-        "title_slugs": title_slugs
+        "title_slugs": title_slugs,
+        "collision_ids": collision_ids,
+        "owner_ids": owner_ids,
+        "track_ids": track_ids,
+        "blockhashes": blockhashes,
+        "blocknumbers": blocknumbers,
+        "txhashes": txhashes,
+        "is_currents": is_currents,
     }
 
     sql = sa.text(
         """
-            UPDATE track_routes SET
-                slug = data_table.slug,
-                title_slug = data_table.title_slug
-            FROM
-                (SELECT
-                    unnest(:track_ids) as track_id,
-                    unnest(:slugs) AS slug,
-                    unnest(:title_slugs) as title_slug
-                ) AS data_table
-            WHERE
-                track_routes.is_current = True
-                AND track_routes.track_id = data_table.track_id
+            INSERT INTO track_routes
+            (
+                slug,
+                title_slug,
+                collision_id,
+                owner_id,
+                track_id,
+                blockhash,
+                blocknumber,
+                txhash,
+                is_current
+            )
+            SELECT * FROM UNNEST(
+                :slugs,
+                :title_slugs,
+                :collision_ids,
+                :owner_ids,
+                :track_ids,
+                :blockhashes,
+                :blocknumbers,
+                :txhashes,
+                :is_currents
+            )
         """
     )
-    sql = sql.bindparams(sa.bindparam("track_ids", ARRAY(Integer)))
     sql = sql.bindparams(sa.bindparam("slugs", ARRAY(String)))
     sql = sql.bindparams(sa.bindparam("title_slugs", ARRAY(String)))
+    sql = sql.bindparams(sa.bindparam("collision_ids", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("owner_ids", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("track_ids", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("blockhashes", ARRAY(String)))
+    sql = sql.bindparams(sa.bindparam("blocknumbers", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("txhashes", ARRAY(String)))
+    sql = sql.bindparams(sa.bindparam("is_currents", ARRAY(Boolean)))
 
     connection.execute(sql, params)
 
@@ -87,56 +136,107 @@ def playlist_route_updates(connection):
     playlist_routes = connection.execute(
         """
             SELECT
-                playlist_id,
                 slug,
-                title_slug
+                title_slug,
+                collision_id,
+                owner_id,
+                playlist_id,
+                blockhash,
+                blocknumber,
+                txhash
             FROM playlist_routes
             WHERE
                 is_current = True
         """
     )
-    playlist_ids = []
+
     slugs = []
     title_slugs = []
+    collision_ids = []
+    owner_ids = []
+    playlist_ids = []
+    blockhashes = []
+    blocknumbers = []
+    txhashes = []
+    is_currents = []
     for route in list(playlist_routes):
-        [track_id, slug, title_slug] = route
-        nfc_slug = unicodedata.normalize('NFC', slug)
+        [
+            slug,
+            title_slug,
+            collision_id,
+            owner_id,
+            playlist_id,
+            blockhash,
+            blocknumber,
+            txhash,
+        ] = route
+        nfc_slug = unicodedata.normalize("NFC", slug)
         if nfc_slug != slug:
-            nfc_title_slug = unicodedata.normalize('NFC', title_slug)
-            playlist_ids.append(track_id)
+            nfc_title_slug = unicodedata.normalize("NFC", title_slug)
             slugs.append(nfc_slug)
             title_slugs.append(nfc_title_slug)
+            collision_ids.append(collision_id)
+            owner_ids.append(owner_id)
+            playlist_ids.append(playlist_id)
+            blockhashes.append(blockhash)
+            blocknumbers.append(blocknumber)
+            txhashes.append(txhash)
+            is_currents.append(False)
 
-    if len(playlist_ids) == 0: # no updates
+    if len(playlist_ids) == 0:  # no updates
         return
 
     params = {
-        "playlist_ids": playlist_ids,
         "slugs": slugs,
-        "title_slugs": title_slugs
+        "title_slugs": title_slugs,
+        "collision_ids": collision_ids,
+        "owner_ids": owner_ids,
+        "playlist_ids": playlist_ids,
+        "blockhashes": blockhashes,
+        "blocknumbers": blocknumbers,
+        "txhashes": txhashes,
+        "is_currents": is_currents,
     }
 
     sql = sa.text(
         """
-            UPDATE playlist_routes SET
-                slug = data_table.slug,
-                title_slug = data_table.title_slug
-            FROM
-                (SELECT
-                    unnest(:playlist_ids) as playlist_id,
-                    unnest(:slugs) AS slug,
-                    unnest(:title_slugs) as title_slug
-                ) AS data_table
-            WHERE
-                playlist_routes.is_current = True
-                AND playlist_routes.playlist_id = data_table.playlist_id
+            INSERT INTO playlist_routes
+            (
+                slug,
+                title_slug,
+                collision_id,
+                owner_id,
+                playlist_id,
+                blockhash,
+                blocknumber,
+                txhash,
+                is_current
+            )
+            SELECT * FROM UNNEST(
+                :slugs,
+                :title_slugs,
+                :collision_ids,
+                :owner_ids,
+                :playlist_ids,
+                :blockhashes,
+                :blocknumbers,
+                :txhashes,
+                :is_currents
+            )
         """
     )
-    sql = sql.bindparams(sa.bindparam("playlist_ids", ARRAY(Integer)))
     sql = sql.bindparams(sa.bindparam("slugs", ARRAY(String)))
     sql = sql.bindparams(sa.bindparam("title_slugs", ARRAY(String)))
+    sql = sql.bindparams(sa.bindparam("collision_ids", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("owner_ids", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("playlist_ids", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("blockhashes", ARRAY(String)))
+    sql = sql.bindparams(sa.bindparam("blocknumbers", ARRAY(Integer)))
+    sql = sql.bindparams(sa.bindparam("txhashes", ARRAY(String)))
+    sql = sql.bindparams(sa.bindparam("is_currents", ARRAY(Boolean)))
 
     connection.execute(sql, params)
+
 
 def upgrade():
     connection = op.get_bind()
