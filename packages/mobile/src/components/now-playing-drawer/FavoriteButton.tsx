c84b31369f2750294f3c485168c856b05297f07d
Kyle Shanks <kyle.j.shanks@gmail.com>
2023-03-02 13:21:31 -0500

    [C-2194] Disabled the repost and favorite buttons in the NowPlayingDrawer for track owner (#2957)

--- packages/mobile/src/components/now-playing-drawer/FavoriteButton.tsx
+++ packages/mobile/src/components/now-playing-drawer/FavoriteButton.tsx
@@ -7,28 +7,30 @@ import { AnimatedButton } from 'app/components/core'
 import { colorize } from 'app/utils/colorizeLottie'
 import { useThemeColors } from 'app/utils/theme'
 
-type FavoriteButtonProps = Omit<AnimatedButtonProps, 'iconJSON'>
+type FavoriteButtonProps = Omit<AnimatedButtonProps, 'iconJSON'> & {
+  isOwner?: boolean
+}
 
 export const FavoriteButton = (props: FavoriteButtonProps) => {
-  const { iconIndex } = props
-  const { neutral, primary } = useThemeColors()
+  const { iconIndex, isOwner = false } = props
+  const { neutralLight6, neutral, primary } = useThemeColors()
 
   const iconJSON = useMemo(() => {
     const ColorizedFavoriteOnIcon = colorize(IconFavoriteOnLight, {
       // icon_Favorites Outlines 2.Group 1.Fill 1
-      'layers.0.shapes.0.it.1.c.k.0.s': neutral,
+      'layers.0.shapes.0.it.1.c.k.0.s': isOwner ? neutralLight6 : neutral,
       // icon_Favorites Outlines 2.Group 1.Fill 1
-      'layers.0.shapes.0.it.1.c.k.1.s': primary
+      'layers.0.shapes.0.it.1.c.k.1.s': isOwner ? neutralLight6 : primary
     })
 
     const ColorizedFavoriteOffIcon = colorize(IconFavoriteOffLight, {
       // icon_Favorites Outlines 2.Group 1.Fill 1
-      'layers.0.shapes.0.it.1.c.k.0.s': primary,
+      'layers.0.shapes.0.it.1.c.k.0.s': isOwner ? neutralLight6 : primary,
       // icon_Favorites Outlines 2.Group 1.Fill 1
-      'layers.0.shapes.0.it.1.c.k.1.s': neutral
+      'layers.0.shapes.0.it.1.c.k.1.s': isOwner ? neutralLight6 : neutral
     })
     return [ColorizedFavoriteOnIcon, ColorizedFavoriteOffIcon]
-  }, [neutral, primary])
+  }, [isOwner, neutral, neutralLight6, primary])
 
   return (
     <AnimatedButton {...props} haptics={iconIndex === 0} iconJSON={iconJSON} />
