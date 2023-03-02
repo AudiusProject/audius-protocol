4770fbb567289bf3f7e79bbc0479b9bafae32f52
Dylan Jeffers <dylan@audius.co>
2023-03-01 16:22:47 -0800

    [C-2232] Fix android share-to-twitter url (#2954)

--- packages/mobile/src/utils/twitter.ts
+++
@@ -1,6 +0,0 @@
-export const getTwitterLink = (url: string | null, text: string) => {
-  const textString = `?text=${encodeURIComponent(text)}`
-  const urlString = url ? `&url=${encodeURIComponent(url)}` : ''
-
-  return `http://twitter.com/share${textString}${urlString}`
-}
