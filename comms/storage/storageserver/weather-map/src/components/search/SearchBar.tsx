2142e8b22fb027afcd34a4e31e03c12b4323aaa8
Theo Ilie <theoilie.ti@gmail.com>
2023-03-03 13:39:07 -0800

    Make light mode search bar text more readable

--- comms/storage/storageserver/weather-map/src/components/search/SearchBar.tsx
+++ comms/storage/storageserver/weather-map/src/components/search/SearchBar.tsx
@@ -18,7 +18,7 @@ export default function SearchBar() {
             type="text"
             name="audius-search"
             id="audius-search"
-            className="block w-full rounded-full border-gray-300 px-4 text-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 dark:bg-gray-900 dark:text-neutral-100 sm:text-sm"
+            className="block w-full rounded-full border-gray-300 px-4 text-gray-700 shadow-sm focus:border-purple-500 focus:ring-purple-500 dark:bg-gray-900 dark:text-neutral-100 sm:text-sm"
             placeholder="File name, S3 key, shard, or job ID"
             value={query}
             onChange={(e: ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
