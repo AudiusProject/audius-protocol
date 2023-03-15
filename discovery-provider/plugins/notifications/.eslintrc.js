06beabeff5a16013cfbb8283981cd1a10eb890e9
Dylan Jeffers <dylan@audius.co>
2023-03-15 16:56:34 -0700

    Add eslint, prettier, typescript, nvm config

---
+++ discovery-provider/plugins/notifications/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
