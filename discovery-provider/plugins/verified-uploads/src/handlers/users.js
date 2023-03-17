886d2ab86c361c56f3cc27dd55628d03bd9cdfcf
Alec Savvy <alecsavvy@outlook.com>
2023-03-16 22:23:05 -0600

    user creation event

--- discovery-provider/plugins/verified-uploads/src/handlers/users.js
+++ discovery-provider/plugins/verified-uploads/src/handlers/users.js
@@ -1,10 +1,18 @@
 export default async ({ slack, db }, { user_id }) => {
-  const user = await db("users")
+  const result = await db("users")
     .select("handle", "name")
     .where("user_id", "=", user_id)
     .first()
     .catch(console.error);
 
-  if (user) {
+  if (result) {
+    const { handle, name } = result;
+    const header = `:pikawave: a new challenger has appeared! *@${handle}*`;
+    const body = {
+      Id: user_id,
+      Name: name,
+      Handle: handle,
+    };
+    await slack.sendMsg(header, body).catch(console.error);
   }
 };
