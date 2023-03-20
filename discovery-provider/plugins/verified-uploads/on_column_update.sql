d2bce99b89e0d6b7f09e63771c62926594ba2aed
Alec Savvy <alecsavvy@outlook.com>
2023-03-20 11:54:55 -0600

    progress

--- discovery-provider/plugins/verified-uploads/on_column_update.sql
+++ discovery-provider/plugins/verified-uploads/on_column_update.sql
@@ -1,3 +1,19 @@
+CREATE OR REPLACE FUNCTION public.on_is_verified_update()
+  RETURNS trigger
+  LANGUAGE plpgsql
+  as $$
+  begin
+    if TG_TABLE_NAME == "users"
+      if new.is_verified is distinct from old.is_verified
+        PERFORM pg_notify('users_verified', json_build_object('user_id', new.user_id, 'is_verified', new.is_verified)::text);
+  end;
+  $$
+
+
+
+
+
+/*
 CREATE OR REPLACE FUNCTION public.on_column_update()
  RETURNS trigger
  LANGUAGE plpgsql
--- discovery-provider/plugins/verified-uploads/src/handlers/users.js
+++ discovery-provider/plugins/verified-uploads/src/handlers/users.js
@@ -10,16 +10,43 @@ export default async ({ slack, dp_db, id_db }, { user_id }) => {
 
     // check identity db in twitter or instagram tables to see
     // which one verified the user
+    const ig = await id_db("InstagramUsers")
+      .select("blockchainUserId")
+      .where("blockchainUserId", "=", user_id)
+      .first()
+      .catch(console.error);
 
-    // join on InstagramUsers / TwitterUsers "blockchainUserId" field
-    // that is discprov user id
+    const twitter = await id_db("TwitterUsers")
+      .select("blockchainUserId")
+      .where("blockchainUserId", "=", user_id)
+      .first()
+      .catch(console.error);
+
+    const tiktok = await id_db("TikTokUsers")
+      .select("blockchainUserId")
+      .where("blockchainUserId", "=", user_id)
+      .first()
+      .catch(console.error);
+
+    let source = "unknown";
+    switch ((ig, twitter, tiktok)) {
+      case (undefined, undefined, tiktok):
+        source = "tiktok";
+        break;
+      case (undefined, twitter, undefined):
+        source = "twitter";
+        break;
+      case (ig, undefined, undefined):
+        source = "instagram";
+        break;
+    }
 
     const header = `:pikawave: a new challenger has appeared! *@${handle}*`;
     const body = {
       userId: user_id,
       handle: handle,
       link: `https://audius.co/${handle}`,
-      source: null, // TODO
+      source,
     };
     await slack.sendMsg(header, body).catch(console.error);
   }
