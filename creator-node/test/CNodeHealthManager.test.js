003cc4e99a8c8e71ae5d9a5e975982db4e2e0def
Raymond Jacobson <ray@audius.co>
2023-03-21 08:52:44 -0700

    [INF-369] Add content node flakey test retries (#4973)

--- creator-node/test/CNodeHealthManager.test.js
+++ creator-node/test/CNodeHealthManager.test.js
@@ -393,6 +393,7 @@ describe('test CNodeHealthManager -- determinePeerHealth()', function () {
 })
 
 describe('test CNodeHealthManager -- isNodeHealthyOrInGracePeriod()', function () {
+  this.retries(3) // TODO: Flakey test
   let sandbox
   beforeEach(function () {
     sandbox = sinon.createSandbox()
@@ -433,12 +434,11 @@ describe('test CNodeHealthManager -- isNodeHealthyOrInGracePeriod()', function (
       false
     )
     expect(isHealthy).to.be.true
-    expect(isNodeHealthyStub).to.have.been.calledOnceWithExactly(
-      secondary
-    )
+    expect(isNodeHealthyStub).to.have.been.calledOnceWithExactly(secondary)
   })
 
   it('(for primary) returns true when health check fails during grace period, then false when grace period ends, then true when health check starts passing again', async function () {
+    this.retries(3) // TODO: Flakey test
     // Mock CNodeHealthManager to use the config with our shorter grace period
     const { CNodeHealthManager: CNodeHealthManagerMock } = proxyquire(
       '../src/services/stateMachineManager/CNodeHealthManager.ts',
