a855d7046e2659ab0dfceb197a091918f8b02e9f
Alec Savvy <alecsavvy@outlook.com>
2023-03-21 10:49:39 -0600

    compare created time

--- discovery-provider/plugins/verified-uploads/src/handlers/tracks.js
+++ discovery-provider/plugins/verified-uploads/src/handlers/tracks.js
@@ -18,17 +18,20 @@ export default async ({ track_id }) => {
       "users.name",
       "tracks.track_id",
       "users.is_verified",
-      "track_routes.slug"
+      "track_routes.slug",
+      "tracks.created_at",
+      "tracks.updated_at"
     )
     .where("tracks.track_id", "=", trackId)
     .where("users.is_verified", "=", true)
-    .orderBy("tracks.blocknumber", "desc")
-    .limit(2)
+    .first()
     .catch(console.error);
 
-  if (results && results.length === 1) {
+  console.log(results);
+
+  if (results && results.created_at === results.updated_at) {
     const { title, mood, release_date, is_premium, handle, name, genre, slug } =
-      results[0];
+      results;
     console.log(`received new verified track from ${handle}`);
     const { sendMsg } = slack;
     const header = `:audius-spin: New upload from *${name}* 🔥`;
