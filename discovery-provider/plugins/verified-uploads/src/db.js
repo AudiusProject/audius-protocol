e8b122f88285ceb450891ef816b36d22eda5f636
Alec Savvy <alecsavvy@outlook.com>
2023-03-22 10:45:23 -0600

    additional logs

--- discovery-provider/plugins/verified-uploads/src/db.js
+++ discovery-provider/plugins/verified-uploads/src/db.js
@@ -34,6 +34,7 @@ export default async (topic, callback) => {
   const conn = await dp_db.client.acquireConnection().catch(console.error);
   const sql = `LISTEN ${topic}`;
   conn.on("notification", async (msg) => {
+    console.log(`received: ${msg}`);
     callback(JSON.parse(msg.payload));
   });
   conn.on("end", (err) => {
