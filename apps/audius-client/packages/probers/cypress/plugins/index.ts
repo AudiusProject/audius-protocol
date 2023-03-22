39cc6dedb0ea096e95b10c54c982b6e5ba965419
Sebastian Klingler <sliptype@gmail.com>
2023-03-22 15:32:08 -0500

    Fix upload and repost probers tests and lint (#3076)

--- apps/audius-client/packages/probers/cypress/plugins/index.ts
+++ apps/audius-client/packages/probers/cypress/plugins/index.ts
@@ -1,15 +1,16 @@
-import fs from "fs-extra";
-import path from "path";
+import path from 'path'
+
+import fs from 'fs-extra'
 
 function getConfigurationByFile(file: string) {
-  const pathToConfigFile = path.resolve("cypress", "config", `${file}.json`);
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
 
-  return fs.readJson(pathToConfigFile);
+  return fs.readJson(pathToConfigFile)
 }
 
 const pluginConfig: Cypress.PluginConfig = (on, config) => {
-  const file = config.env.configFile || "dev";
-  return getConfigurationByFile(file);
-};
+  const file = config.env.configFile || 'dev'
+  return getConfigurationByFile(file)
+}
 
-export default pluginConfig;
+export default pluginConfig
--- apps/audius-client/packages/probers/cypress/support/commands.ts
+++ apps/audius-client/packages/probers/cypress/support/commands.ts
@@ -1,3 +1,3 @@
-import "@testing-library/cypress/add-commands";
-import "cypress-file-upload";
-import "cypress-wait-until";
+import '@testing-library/cypress/add-commands'
+import 'cypress-file-upload'
+import 'cypress-wait-until'
