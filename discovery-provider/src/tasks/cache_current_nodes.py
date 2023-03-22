2d90e646dc75ff3efe199672727c72b36a70507e
Alec Savvy <alecsavvy@outlook.com>
2023-03-22 17:08:19 -0600

    add redis lock

--- discovery-provider/src/tasks/cache_current_nodes.py
+++ discovery-provider/src/tasks/cache_current_nodes.py
@@ -16,17 +16,29 @@ logger = logging.getLogger(__name__)
 @celery.task(name="cache_current_nodes", bind=True)
 @save_duration_metric(metric_group="celery_task")
 def cache_current_nodes_task(self):
+    redis = cache_current_nodes_task.redis
+    # Define lock acquired boolean
+    have_lock = False
+    # Define redis lock object
+    # Max duration of lock is 4hrs or 14400 seconds
+    update_lock = redis.lock("cache_current_nodes_lock", blocking_timeout=25, timeout=14400)
     try:
-        logger.info("cache_current_nodes.py | fetching all other nodes")
-        nodes = get_all_other_nodes()[0]
-        current_node = get_node_endpoint()
-        # add current node to list
-        if current_node is not None:
-            nodes.append(current_node)
+        have_lock = update_lock.acquire(blocking=False)
+        if have_lock:
+            logger.info("cache_current_nodes.py | fetching all other nodes")
+            nodes = get_all_other_nodes()[0]
+            current_node = get_node_endpoint()
+            # add current node to list
+            if current_node is not None:
+                nodes.append(current_node)
 
-        # only get redis connection after nodes are collected
-        redis = cache_current_nodes_task.redis
-        set_json_cached_key(redis, ALL_NODES_CACHE_KEY, nodes)
-        logger.info("cache_current_nodes.py | set current nodes in redis")
+            set_json_cached_key(redis, ALL_NODES_CACHE_KEY, nodes)
+            logger.info("cache_current_nodes.py | set current nodes in redis")
+        else:
+            logger.info("cache_current_nodes.py | Failed to acquire lock")
     except Exception as e:
         logger.error(f"cache_current_nodes.py | ERROR caching node info {e}")
+        raise e
+    finally:
+        if have_lock:
+            update_lock.release()
