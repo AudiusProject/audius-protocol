6b80fc720ab75c8fa8f587207a7b5b798d38326d
Cheran <cheran.v.senthil@gmail.com>
2023-03-27 23:43:11 +0530

    Fix conflicting dbs (#5009)

--- dev-tools/startup/init-db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE discovery_provider_1;
-
-CREATE DATABASE creator_node_1;
-CREATE DATABASE creator_node_2;
-CREATE DATABASE creator_node_3;
-
-CREATE DATABASE identity_service;
--- dev-tools/startup/initdb/02_dbs.sql
+++ dev-tools/startup/initdb/02_dbs.sql
@@ -2,3 +2,10 @@ create database com1 WITH TEMPLATE audius_discovery;
 create database com2 WITH TEMPLATE audius_discovery;
 create database com3 WITH TEMPLATE audius_discovery;
 create database com4 WITH TEMPLATE audius_discovery;
+CREATE DATABASE discovery_provider_1;
+
+CREATE DATABASE creator_node_1;
+CREATE DATABASE creator_node_2;
+CREATE DATABASE creator_node_3;
+
+CREATE DATABASE identity_service;
