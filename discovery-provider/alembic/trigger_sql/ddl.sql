52ee6bb5e915b7e10790be6f044f7227b5f42db2
sliptype <sliptype@gmail.com>
2023-03-30 14:48:02 -0500

    Add to_timestamp_safe

--- discovery-provider/alembic/trigger_sql/ddl.sql
+++ discovery-provider/alembic/trigger_sql/ddl.sql
@@ -6,3 +6,24 @@
 begin;
   comment on table users is 'the users table';
 commit;
+
+
+-- 3/29/23: define `to_timestamp_safe`
+begin;
+  CREATE OR REPLACE FUNCTION to_timestamp_safe(p_timestamp VARCHAR, p_format VARCHAR)
+  RETURNS TIMESTAMP
+  LANGUAGE plpgsql
+  as $$
+  DECLARE
+      ret_timestamp TIMESTAMP;
+  BEGIN
+      IF p_timestamp = '' THEN
+          RETURN NULL;
+      END IF;
+      RETURN to_timestamp( p_timestamp, p_format );
+  EXCEPTION
+  WHEN others THEN
+      RETURN null;
+  END;
+  $$;
+commit;
--- discovery-provider/src/queries/get_tracks.py
+++ discovery-provider/src/queries/get_tracks.py
@@ -162,9 +162,8 @@ def _get_tracks(session, args):
             base_query = base_query.order_by(
                 sort_fn(
                     coalesce(
-                        func.to_date_safe(
-                            TrackWithAggregates.release_date,
-                            "Dy Mon DD YYYY HH24:MI:SS",
+                        func.to_timestamp_safe(
+                            TrackWithAggregates.release_date, "Dy Mon DD YYYY HH24:MI:SS GMTTZHTZM"
                         ),
                         func.date_trunc('day', TrackWithAggregates.created_at)
                     )
@@ -204,12 +203,11 @@ def _get_tracks(session, args):
             base_query = base_query.order_by(
                 coalesce(
                     # This func is defined in alembic migrations
-                    func.to_date_safe(
-                        TrackWithAggregates.release_date, "Dy Mon DD YYYY HH24:MI:SS"
+                    func.to_timestamp_safe(
+                        TrackWithAggregates.release_date, "Dy Mon DD YYYY HH24:MI:SS GMTTZHTZM"
                     ),
-                    func.date_trunc('day', TrackWithAggregates.created_at)
-                ).desc(),
-                TrackWithAggregates.track_id.desc(),
+                    TrackWithAggregates.created_at,
+                ).desc()
             )
         elif args["sort"] == "plays":
             base_query = base_query.join(
