a096686b6d39b6c790c7841e391dc8eb9bfb41e3
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-04-05 10:34:59 -0700

    [PAY-1143] Desktop: Overflow menu on Chat Header (#3169)

--- apps/audius-client/packages/web/src/pages/chat-page/components/ChatHeader.tsx
+++ apps/audius-client/packages/web/src/pages/chat-page/components/ChatHeader.tsx
@@ -1,9 +1,27 @@
 import { forwardRef, useCallback } from 'react'
 
-import { useProxySelector, chatSelectors } from '@audius/common'
-import { IconButton, IconCompose, IconSettings } from '@audius/stems'
+import {
+  useProxySelector,
+  chatSelectors,
+  chatActions,
+  User
+} from '@audius/common'
+import {
+  IconButton,
+  IconCompose,
+  IconSettings,
+  IconKebabHorizontal,
+  PopupMenu,
+  IconBlockMessages,
+  IconUnblockMessages,
+  IconUser,
+  IconTrash
+} from '@audius/stems'
+import { push as pushRoute } from 'connected-react-router'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { useModalState } from 'common/hooks/useModalState'
+import { profilePage } from 'utils/route'
 
 import styles from './ChatHeader.module.css'
 import { ChatUser } from './ChatUser'
@@ -11,21 +29,31 @@ import { ChatUser } from './ChatUser'
 const messages = {
   header: 'Messages',
   settings: 'Settings',
-  compose: 'Compose'
+  compose: 'Compose',
+  chatSettings: 'Chat Settings',
+  block: 'Block Messages',
+  unblock: 'Unblock Messages',
+  delete: 'Delete Conversation',
+  visit: "Visit User's Profile"
 }
 
-const { getOtherChatUsers } = chatSelectors
+const { getOtherChatUsers, getBlockees } = chatSelectors
+const { blockUser, unblockUser } = chatActions
 
 type ChatHeaderProps = { currentChatId?: string }
 
 export const ChatHeader = forwardRef<HTMLDivElement, ChatHeaderProps>(
   ({ currentChatId }, ref) => {
+    const dispatch = useDispatch()
     const [, setCreateChatVisible] = useModalState('CreateChat')
     const [, setInboxSettingsVisible] = useModalState('InboxSettings')
     const users = useProxySelector(
       (state) => getOtherChatUsers(state, currentChatId),
       [currentChatId]
     )
+    const user: User | null = users[0] ?? null
+    const blockeeList = useSelector(getBlockees)
+    const isBlocked = user && blockeeList.includes(user.user_id)
 
     const handleComposeClicked = useCallback(() => {
       setCreateChatVisible(true)
@@ -34,6 +62,44 @@ export const ChatHeader = forwardRef<HTMLDivElement, ChatHeaderProps>(
     const handleSettingsClicked = useCallback(() => {
       setInboxSettingsVisible(true)
     }, [setInboxSettingsVisible])
+
+    const handleUnblockClicked = useCallback(() => {
+      dispatch(unblockUser({ userId: user.user_id }))
+    }, [dispatch, user])
+
+    const handleBlockClicked = useCallback(() => {
+      dispatch(blockUser({ userId: user.user_id }))
+    }, [dispatch, user])
+
+    const handleVisitClicked = useCallback(() => {
+      dispatch(pushRoute(profilePage(user.handle)))
+    }, [dispatch, user])
+
+    const overflowItems = [
+      isBlocked
+        ? {
+            text: messages.unblock,
+            icon: <IconUnblockMessages />,
+            onClick: handleUnblockClicked
+          }
+        : {
+            text: messages.block,
+            icon: <IconBlockMessages />,
+            onClick: handleBlockClicked
+          },
+      {
+        text: messages.delete,
+        icon: <IconTrash />,
+        onClick: () => {
+          // TODO: Store changes for chat deletion
+        }
+      },
+      {
+        text: messages.visit,
+        icon: <IconUser />,
+        onClick: handleVisitClicked
+      }
+    ]
 
     return (
       <div ref={ref} className={styles.root}>
@@ -53,7 +119,24 @@ export const ChatHeader = forwardRef<HTMLDivElement, ChatHeaderProps>(
           </div>
         </div>
         <div className={styles.right}>
-          {users.length > 0 ? <ChatUser user={users[0]} /> : null}
+          {user ? <ChatUser user={user} /> : null}
+          {user ? (
+            <div className={styles.overflow}>
+              <PopupMenu
+                items={overflowItems}
+                transformOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                renderTrigger={(ref, trigger) => (
+                  <IconButton
+                    ref={ref}
+                    aria-label={messages.chatSettings}
+                    icon={<IconKebabHorizontal />}
+                    onClick={trigger}
+                  />
+                )}
+              />
+            </div>
+          ) : null}
         </div>
       </div>
     )
