db82134fb2de5e8d8a8debca0f6ffe7cc4ecae30
Saliou Diallo <saliou@audius.co>
2023-04-05 11:57:49 -0400

    Prevent ipfs route from streaming mp3

---
+++ creator-node/src/utils/fileUtils.ts
@@ -0,0 +1,16 @@
+import isMp3 from 'is-mp3'
+
+const readChunk = require('read-chunk')
+
+const ffmpegCharCodes = Array.from('FFmpeg').map(s => s.charCodeAt(0))
+
+export const isMp3File = async (path: string) => {
+  const chunk1 = await readChunk(path, 0, 3)
+  if (isMp3(chunk1)) {
+    return true
+  }
+
+  // For track segments, return true if the extracted bytes read 'FFmpeg'.
+  const chunk2 = await readChunk(path, 25, 6)
+  return ffmpegCharCodes.every((code, i) => code === chunk2[i])
+}
