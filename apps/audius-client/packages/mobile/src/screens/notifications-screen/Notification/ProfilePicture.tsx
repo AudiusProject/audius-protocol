bfbfe99eb7f30fb2b991a91fb543b47bea7bf6d0
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-04-11 15:02:09 -0400

    [PAY-748] Fix supporter count alignment (#3203)

--- apps/audius-client/packages/mobile/src/screens/notifications-screen/Notification/ProfilePicture.tsx
+++ apps/audius-client/packages/mobile/src/screens/notifications-screen/Notification/ProfilePicture.tsx
@@ -8,13 +8,15 @@ import { ProfilePicture as ProfilePictureBase } from 'app/components/user'
 import { useNavigation } from 'app/hooks/useNavigation'
 import { makeStyles } from 'app/styles'
 
+import { PROFILE_PICTURE_BORDER_WIDTH } from './constants'
+
 const useStyles = makeStyles(({ palette, spacing }) => ({
   image: {
     height: spacing(10) - 2,
     width: spacing(10) - 2,
     borderRadius: spacing(5),
     borderColor: palette.white,
-    borderWidth: 2,
+    borderWidth: PROFILE_PICTURE_BORDER_WIDTH,
     overflow: 'hidden',
     backgroundColor: palette.neutralLight4,
     marginRight: spacing(2)
