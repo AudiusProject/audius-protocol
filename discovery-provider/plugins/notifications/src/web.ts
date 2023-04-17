73f54e3c63a71140481e18bf8162eaead99bedf1
Alec Savvy <alecsavvy@outlook.com>
2023-04-17 13:41:13 -0600

    move stuff over from identity

---
+++ discovery-provider/plugins/notifications/src/web.ts
@@ -0,0 +1,66 @@
+import webpush from 'web-push'
+import { logger } from './logger'
+
+// setup and configuration
+let webPushIsConfigured = false
+const vapidKeys = {
+    publicKey: process.env.BROWSER_PUSH_VAPID_PUBLIC_KEY,
+    privateKey: process.env.BROWSER_PUSH_VAPID_PRIVATE_KEY
+  }
+  const browserPushGCMAPIKey = process.env.BROWSER_PUSH_GCM_API_KEY
+  if (vapidKeys.publicKey && vapidKeys.privateKey && browserPushGCMAPIKey) {
+    webpush.setGCMAPIKey(browserPushGCMAPIKey)
+    webpush.setVapidDetails(
+      'mailto:contact@audius.co', // taken from identity
+      vapidKeys.publicKey,
+      vapidKeys.privateKey
+    )
+    // set config to true
+    webPushIsConfigured = true
+  } else {
+    logger.warn(
+      'Web Push is not configured. Browser Push API Notifs will NOT be sent'
+    )
+  }
+
+// utility functions
+const sendBrowserNotification = async (userId: string, title: string, message: string) => {
+    let numSentNotifs = 0
+  
+    try {
+      if (!webPushIsConfigured) return numSentNotifs
+      // TODO: convert to knex
+      const notificationBrowsers =
+        await models.NotificationBrowserSubscription.findAll({
+          where: { userId, enabled: true }
+        })
+      await Promise.all(
+        notificationBrowsers.map(async (notificationBrowser) => {
+          const pushSubscription = {
+            endpoint: notificationBrowser.endpoint,
+            keys: {
+              p256dh: notificationBrowser.p256dhKey,
+              auth: notificationBrowser.authKey
+            }
+          }
+          try {
+            await webpush.sendNotification(
+              pushSubscription,
+              JSON.stringify({ message, title: `${title} • Audius` })
+            )
+            numSentNotifs++
+          } catch (err) {
+            if (err.statusCode === 410) {
+              // If the send Notification response was not successful
+              // delete the browser subscription as it is no longer valid
+              await notificationBrowser.destroy()
+            }
+          }
+        })
+      )
+    } catch (err) {
+      logger.error(`Error in sending Push API browser notifications`)
+    }
+  
+    return numSentNotifs
+  }
