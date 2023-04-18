cba322724ce8e10c4672e99f0358a749687ec00a
Steve Perkins <stereosteve@users.noreply.github.com>
2023-04-18 11:19:40 -0400

    Show how long ws was connected.

--- monitoring/healthz/src/DMs.tsx
+++ monitoring/healthz/src/DMs.tsx
@@ -14,17 +14,17 @@ export function DMs() {
   if (!sps) return null
 
   return (
-    <div>
+    <div style={{ padding: 50 }}>
       <EnvironmentSlector />
       <h1>DMs</h1>
       <table className="table">
         <thead>
           <tr>
             <th>Host</th>
+            <th>Ver</th>
             <th>Server</th>
             <th>Websocket</th>
-            <th>Ver</th>
-            <th>Wallet</th>
+            <th>Lasted</th>
           </tr>
         </thead>
         <tbody>
@@ -41,14 +41,19 @@ function DMRow({ sp }: { sp: SP }) {
   const { data: health } = useQuery([`${sp.endpoint}/health_check`], fetchUrl)
 
   const [wsResult, setWsResult] = useState('')
+  const [connectedAt, setConnectedAt] = useState<Date>()
+  const [disconnectedAt, setDisonnectedAt] = useState<Date>()
 
   useEffect(() => {
     const endpoint = `${sp.endpoint.replace('https', 'wss')}/comms/debug/ws`
     const webSocket = new WebSocket(endpoint)
-    // webSocket.onclose = (ev) => {
-    //   console.log(ev)
-    //   setWsResult('closed')
-    // }
+    webSocket.onclose = (ev) => {
+      console.log(ev)
+      if (wsResult == 'OK') {
+        setWsResult('closed')
+      }
+      setDisonnectedAt(new Date())
+    }
     webSocket.onerror = (ev) => {
       console.log('err', ev)
       setWsResult('error')
@@ -56,6 +61,8 @@ function DMRow({ sp }: { sp: SP }) {
     webSocket.onopen = (ev) => {
       console.log('OPEN', ev)
       setWsResult('OK')
+      setConnectedAt(new Date())
+      setDisonnectedAt(undefined)
     }
   }, [])
 
@@ -76,24 +83,41 @@ function DMRow({ sp }: { sp: SP }) {
   return (
     <tr>
       <td>
-        <a href={`${sp.endpoint}/comms`} target="_blank">
-          {sp.endpoint}
-        </a>
+        <div>
+          <a href={`${sp.endpoint}/comms`} target="_blank">
+            {sp.endpoint}
+          </a>
+        </div>
       </td>
+      <td>{health?.data && <div>{health.data.version}</div>}</td>
       <td>
         <b style={{ color: sidecarStatus == 200 ? 'darkgreen' : 'red' }}>
           {sidecarStatus}
         </b>
       </td>
       <td>
-        <b style={{ color: wsResult == 'OK' ? 'darkgreen' : 'red' }}>
+        <b
+          style={{
+            color: wsResult == 'OK' ? 'darkgreen' : 'red',
+          }}
+        >
           {wsResult}
         </b>
+        <small style={{ marginLeft: 20, color: 'darkgreen' }}>
+          {connectedAt?.toLocaleTimeString()}
+        </small>
+        <small style={{ marginLeft: 20, color: 'red' }}>
+          {disconnectedAt?.toLocaleTimeString()}
+        </small>
       </td>
-      <td>{health?.data && <div>{health.data.version}</div>}</td>
-      <td>
-        <pre style={{ fontSize: '60%' }}>{health?.signer}</pre>
-      </td>
+      <td>{timeDiffInSeconds(connectedAt, disconnectedAt)}</td>
     </tr>
   )
+}
+
+function timeDiffInSeconds(start: Date | undefined, end: Date | undefined) {
+  if (start && end && end.getTime() > start.getTime()) {
+    return `${(end.getTime() - start.getTime()) / 1000}s`
+  }
+  return ''
 }
