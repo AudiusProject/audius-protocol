bb170cc43f7cd7d4c1506d0b1d0c45687fee6f47
Steve Perkins <stereosteve@users.noreply.github.com>
2023-04-24 15:35:31 -0400

    comms logs go to vector, use slog, reduce log noise.

--- comms/main.go
+++ comms/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"comms.audius.co/cmd"
+	"golang.org/x/exp/slog"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout))
+	slog.SetDefault(logger)
+
 	cmd.Execute()
 }
