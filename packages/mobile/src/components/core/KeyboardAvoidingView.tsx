0aa1cce6d60b73fe83a119f8cb0e1b33cf5b8cfc
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-04-24 12:17:45 -0400

    [PAY-1122] Mobile chats keyboard improvements (#3262)

---
+++ packages/mobile/src/components/core/KeyboardAvoidingView.tsx
@@ -0,0 +1,95 @@
+import React, { useCallback, useEffect, useRef } from 'react'
+
+import type { ViewStyle, StyleProp } from 'react-native'
+import { Keyboard, Animated, Easing } from 'react-native'
+
+import { makeStyles } from 'app/styles'
+
+const useStyles = makeStyles(({ spacing, palette, typography }) => ({
+  rootContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%'
+  }
+}))
+
+type KeyboardAvoidingViewProps = {
+  style: StyleProp<ViewStyle>
+  heightOffsetRatio?: number
+  keyboardShowingDuration?: number
+  keyboardHidingDuration?: number
+  children: React.ReactNode
+}
+
+/**
+ * Handrolled implementation of KeyboardAvoidingView. Allows
+ * customization of the ratio of the keyboard height by which to translate
+ * the content upwards (default 0.75), and the duration of the in/out animations.
+ *
+ * Mainly built to solve a bug with react-native default KeyboardAvoidingView
+ * where the content inside the View would not translate upwards far enough.
+ */
+export const KeyboardAvoidingView = ({
+  style,
+  // 0.75 seems to work well but need to test on more screen sizes.
+  heightOffsetRatio = 0.75,
+  keyboardShowingDuration = 100,
+  keyboardHidingDuration = 100,
+  children
+}: KeyboardAvoidingViewProps) => {
+  const styles = useStyles()
+  const keyboardHeight = useRef(new Animated.Value(0))
+
+  const handleKeyboardWillShow = useCallback(
+    (event) => {
+      Animated.timing(keyboardHeight.current, {
+        toValue: -event.endCoordinates.height * heightOffsetRatio,
+        duration: keyboardShowingDuration,
+        easing: Easing.inOut(Easing.quad),
+        useNativeDriver: true
+      }).start()
+    },
+    [heightOffsetRatio, keyboardShowingDuration]
+  )
+
+  const handleKeyboardWillHide = useCallback(
+    (event) => {
+      Animated.timing(keyboardHeight.current, {
+        toValue: 0,
+        duration: keyboardHidingDuration,
+        easing: Easing.inOut(Easing.quad),
+        useNativeDriver: true
+      }).start()
+    },
+    [keyboardHidingDuration]
+  )
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener(
+      'keyboardWillShow',
+      handleKeyboardWillShow
+    )
+    const hideSubscription = Keyboard.addListener(
+      'keyboardWillHide',
+      handleKeyboardWillHide
+    )
+    return () => {
+      Keyboard.removeSubscription(showSubscription)
+      Keyboard.removeSubscription(hideSubscription)
+    }
+  }, [handleKeyboardWillHide, handleKeyboardWillShow])
+
+  return (
+    <Animated.View
+      style={[
+        style,
+        styles.rootContainer,
+        {
+          transform: [{ translateY: keyboardHeight.current }]
+        }
+      ]}
+    >
+      {children}
+    </Animated.View>
+  )
+}
