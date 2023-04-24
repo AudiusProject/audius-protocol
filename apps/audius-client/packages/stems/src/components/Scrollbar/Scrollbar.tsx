511617afd1237df260c95860a5a6cc47d380634b
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-04-24 13:42:20 -0500

    [C-2512] Fix scrolling on modals and related artists (#3266)

--- apps/audius-client/packages/stems/src/components/Scrollbar/Scrollbar.tsx
+++ apps/audius-client/packages/stems/src/components/Scrollbar/Scrollbar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useMemo } from 'react'
 import cn from 'classnames'
 import { uniqueId } from 'lodash'
 import PerfectScrollbar from 'react-perfect-scrollbar'
+import useMeasure from 'react-use-measure'
 
 import styles from './Scrollbar.module.css'
 import { ScrollbarProps } from './types'
@@ -19,6 +20,10 @@ export const Scrollbar = ({
   id,
   ...props
 }: ScrollbarProps) => {
+  // Do not remove:
+  // useMeasure is required for infinite scrolling to work
+  useMeasure()
+
   const timerRef = useRef<NodeJS.Timeout | null>(null)
   const elementId = useMemo(() => id || uniqueId('scrollbar-'), [id])
 
