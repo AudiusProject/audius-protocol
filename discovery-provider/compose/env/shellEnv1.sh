4a525e00973e8c40590d4b799176efc4e2de261a
Theo Ilie <theoilie.ti@gmail.com>
2023-04-25 10:00:32 -0700

    Document audius-compose, and remove legacy compose folders (#5157)

--- discovery-provider/compose/env/shellEnv1.sh
+++
@@ -1,5 +0,0 @@
-export COMPOSE_PROJECT_NAME="dn1"
-export audius_server_port="5000"
-export audius_db_port="5432"
-export audius_redis_port="5380"
-export audius_es_port="9200"
--- discovery-provider/compose/env/shellEnv8.sh
+++
@@ -1,4 +0,0 @@
-export COMPOSE_PROJECT_NAME="dn8"
-export audius_server_port="5007"
-export audius_db_port="5439"
-export audius_redis_port="5388"
