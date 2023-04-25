784a8fb865bc131c8166bf18d78704c4e6f76a3b
Raymond Jacobson <ray@audius.co>
2023-04-25 16:07:23 -0700

    [C-2527] Remove cognito from mobile (#3275)

--- apps/audius-client/packages/web/src/utils/constants.ts
+++ apps/audius-client/packages/web/src/utils/constants.ts
@@ -3,6 +3,8 @@ export const SHARE_TOAST_TIMEOUT_MILLIS = 1500
 export const NEW_WALLET_CONNECTED_TOAST_TIMEOUT_MILLIS = 3000
 export const CLAIM_REWARD_TOAST_TIMEOUT_MILLIS = 3000
 
+export const COGNITO_SCRIPT_URL = 'https://cdn.cognitohq.com/flow.js'
+
 export const STATIC_EXPLORE_CONTENT_URL =
   'https://download.audius.co/static-resources/explore-content.json'
 
