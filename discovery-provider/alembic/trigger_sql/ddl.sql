404d0908c73566b046df656db8de4526b50f2e01
Isaac Solo <isaac@audius.co>
2023-04-27 16:13:25 -0700

    wip

--- discovery-provider/alembic/trigger_sql/ddl.sql
+++ discovery-provider/alembic/trigger_sql/ddl.sql
@@ -90,6 +90,6 @@ begin;
     alter table tracks
     add column if not exists ai_attribution_user_id integer;
 
-    alter table user
+    alter table users
     add column if not exists allow_ai_attribution boolean not null default false;
 commit;
