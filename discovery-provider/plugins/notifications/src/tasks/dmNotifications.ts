772e07b42d74b24c4b4ef872219b4cbeecae892b
Alec Savvy <alecsavvy@outlook.com>
2023-05-01 10:22:32 -0600

    change to false for now

--- discovery-provider/plugins/notifications/src/tasks/dmNotifications.ts
+++ discovery-provider/plugins/notifications/src/tasks/dmNotifications.ts
@@ -198,7 +198,7 @@ export async function sendDMNotifications(discoveryDB: Knex, identityDB: Knex) {
 
     // Send push notifications
     for (const notification of notifications) {
-      await notification.pushNotification({ isLiveEmailEnabled: true, isBrowserPushEnabled: true })
+      await notification.pushNotification({ isLiveEmailEnabled: true, isBrowserPushEnabled: false })
     }
 
     // Set last indexed timestamps in redis
