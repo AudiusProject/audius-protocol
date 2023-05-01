354351121c87a464ca3e82d1bd7d5e366ea212e9
Isaac Solo <isaac@audius.co>
2023-05-01 12:17:57 -0700

    Fix browser push when user doesn't have browsers

--- discovery-provider/plugins/notifications/src/processNotifications/mappers/userNotificationSettings.ts
+++ discovery-provider/plugins/notifications/src/processNotifications/mappers/userNotificationSettings.ts
@@ -439,7 +439,7 @@ export class UserNotificationSettings {
   async getUserNotificationBrowsers(userId: number): Promise<WebPush[]> {
     if (!globalThis.webPushIsConfigured) return []
     const settings = await this.getUserBrowserSettings([userId])
-    const browsers = settings[userId].browser
+    const browsers = settings[userId]?.browser ?? []
 
     const isWebPush = (browser: Browser): boolean => {
       return (browser as WebPush).p256dhKey !== undefined
