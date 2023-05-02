f893a8c1cb4dfaf5b4c1d1abd4d4050b9ffe03cd
Michael Piazza <michael.piazza.mp@gmail.com>
2023-05-01 21:20:38 -0400

    Smaller index

--- discovery-provider/alembic/trigger_sql/ddl.sql
+++ discovery-provider/alembic/trigger_sql/ddl.sql
@@ -139,5 +139,5 @@ COMMIT;
 -- 5/1/23: add ai_attribution_user_id index
 
 BEGIN;
-    create index if not exists tracks_ai_attribution_user_id on tracks (ai_attribution_user_id, is_current) where is_current = true;
+    create index if not exists tracks_ai_attribution_user_id on tracks (ai_attribution_user_id, is_current) where is_current = true and ai_attribution_user_id is not null;
 COMMIT;
