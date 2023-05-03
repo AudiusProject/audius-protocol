a8807afa51ab23c1a36aa25adba22baeade2ab73
Dylan Jeffers <dylan@audius.co>
2023-05-03 13:20:43 -0700

    Fix internal/external audius urls (#3320)

--- apps/audius-client/packages/common/src/utils/urlUtils.ts
+++ apps/audius-client/packages/common/src/utils/urlUtils.ts
@@ -1,6 +1,6 @@
 const audiusUrlRegex =
   // eslint-disable-next-line no-useless-escape
-  /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+\.)?(audius.co)(\/.+)?/gim
+  /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?(staging\.)?(audius.co)(\/.+)?/gim
 
 export const isAudiusUrl = (url: string) => new RegExp(audiusUrlRegex).test(url)
 export const getPathFromAudiusUrl = (url: string) =>
