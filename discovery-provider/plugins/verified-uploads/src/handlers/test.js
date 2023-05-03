73e7bb6a32c2a17d5e4940be1b3bb9b58464e06e
Alec Savvy <alecjsavvy@gmail.com>
2023-05-03 15:17:49 -0600

    add new guard and test for old uploads (#5132)

---
+++ discovery-provider/plugins/verified-uploads/src/handlers/test.js
@@ -0,0 +1,15 @@
+import { isOldUpload } from "./tracks.js";
+
+const oldDate = "Thu Nov 05 2020 20:36:07 GMT-0600";
+const isOld = isOldUpload(oldDate);
+
+const newDate = new Date().getTime(); // uploaded right now
+const isNew = isOldUpload(newDate);
+
+if (!isOld) {
+  throw new Error("failed");
+}
+
+if (isNew) {
+  throw new Error("failed here");
+}
