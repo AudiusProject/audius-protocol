9e01ef92b3a0aa0daa1182113226e3964af3f194
Alec Savvy <alecsavvy@outlook.com>
2023-05-16 10:09:02 -0600

    default to local RPC and fallback to gateway

--- discovery-provider/src/utils/web3_provider.py
+++ discovery-provider/src/utils/web3_provider.py
@@ -2,26 +2,35 @@
 Interface for using a web3 provider
 """
 
+import logging
 import os
 from typing import Optional
 
+import requests
 from src.utils.config import shared_config
 from src.utils.multi_provider import MultiProvider
 from web3 import HTTPProvider, Web3
 from web3.middleware import geth_poa_middleware
 
+logger = logging.getLogger(__name__)
+
 web3: Optional[Web3] = None
-LOCAL_RPC = "http://chain:8545"  # TODO: Needs nethermind locally I think
+LOCAL_RPC = "http://chain:8545"
 
 
 def get_web3(web3endpoint=None):
-    # only use ACDC web3 provider when
-    # final_poa_block is indexed
-
     # pylint: disable=W0603
     global web3
     if not web3endpoint:
-        web3endpoint = os.getenv("audius_web3_host")
+        # attempt local rpc, check if healthy
+        r = requests.get(LOCAL_RPC + "/health")
+        if r.status_code == 200:
+            web3endpoint = LOCAL_RPC
+            logger.info("web3_provider.py | using local RPC")
+        else:
+            # if local rpc isn't healthy fall back to gateway
+            web3endpoint = os.getenv("audius_web3_host")
+            logger.warn("web3_provider.py | falling back to gateway RPC")
     web3 = Web3(HTTPProvider(web3endpoint))
 
     # required middleware for POA
@@ -33,6 +42,7 @@ def get_web3(web3endpoint=None):
 eth_web3: Optional[Web3] = None
 
 
+# mainnet eth, not ACDC
 def get_eth_web3():
     # pylint: disable=W0603
     global eth_web3
