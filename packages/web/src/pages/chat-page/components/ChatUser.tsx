2104b2b60152e52443afeb82efb15d687b92c8b1
Michael Piazza <michael.piazza.mp@gmail.com>
2023-05-16 21:34:01 -0400

    [PAY-1183] Make clicking ChatUser handle/displayname lead to profile (#3368)

--- packages/web/src/pages/chat-page/components/ChatUser.tsx
+++ packages/web/src/pages/chat-page/components/ChatUser.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useCallback } from 'react'
 
 import { User } from '@audius/common'
 import cn from 'classnames'
@@ -6,6 +6,8 @@ import cn from 'classnames'
 import { ArtistPopover } from 'components/artist/ArtistPopover'
 import { ProfilePicture } from 'components/notification/Notification/components/ProfilePicture'
 import UserBadges from 'components/user-badges/UserBadges'
+import { useGoToRoute } from 'hooks/useGoToRoute'
+import { profilePage } from 'utils/route'
 
 import styles from './ChatUser.module.css'
 
@@ -17,20 +19,30 @@ export const ChatUser = ({
   user: User
   children?: ReactNode
   textClassName?: string
-}) => (
-  <div className={styles.root}>
-    <ProfilePicture user={user} className={styles.profilePicture} />
-    <div className={cn(styles.text, textClassName)}>
-      <ArtistPopover handle={user.handle}>
-        <div className={styles.nameAndBadge}>
-          <span className={styles.name}>{user.name}</span>
-          <UserBadges userId={user.user_id} badgeSize={14} />
-        </div>
-      </ArtistPopover>
-      <ArtistPopover handle={user.handle}>
-        <span className={styles.handle}>@{user.handle}</span>
-      </ArtistPopover>
+}) => {
+  const goToRoute = useGoToRoute()
+  const goToProfile = useCallback(
+    () => goToRoute(profilePage(user.handle)),
+    [goToRoute, user]
+  )
+
+  return (
+    <div className={styles.root}>
+      <ProfilePicture user={user} className={styles.profilePicture} />
+      <div className={cn(styles.text, textClassName)}>
+        <ArtistPopover handle={user.handle}>
+          <div className={styles.nameAndBadge} onClick={goToProfile}>
+            <span className={styles.name}>{user.name}</span>
+            <UserBadges userId={user.user_id} badgeSize={14} />
+          </div>
+        </ArtistPopover>
+        <ArtistPopover handle={user.handle}>
+          <span className={styles.handle} onClick={goToProfile}>
+            @{user.handle}
+          </span>
+        </ArtistPopover>
+      </div>
+      {children}
     </div>
-    {children}
-  </div>
-)
+  )
+}
