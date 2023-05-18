5b0a71e824b086c7530b7719456016a2a41941dd
Michelle Brier <michelle.brier4@gmail.com>
2023-05-18 14:05:42 -0700

    Rename encodedABI in Transactions table in identity (#5253)

--- identity-service/src/relay/txRelay.js
+++ identity-service/src/relay/txRelay.js
@@ -150,7 +150,7 @@ const sendTransactionInternal = async (req, web3, txProps, reqBodySHA) => {
 
   const existingTx = await models.Transaction.findOne({
     where: {
-      encodedABI: hashedData // this should always be unique
+      encodedNonceAndSignature: hashedData // this should always be unique
     }
   })
 
@@ -382,7 +382,7 @@ const sendTransactionInternal = async (req, web3, txProps, reqBodySHA) => {
     contractFn: decodedABI.name,
     contractAddress: contractAddress,
     senderAddress: senderAddress,
-    encodedABI: hashedData,
+    encodedNonceAndSignature: hashedData,
     decodedABI: decodedABI,
     receipt: txReceipt
   })
