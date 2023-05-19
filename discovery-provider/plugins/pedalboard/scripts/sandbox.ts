5425dfddac13a3543141cfaf233fa30fbacd3b84
Alec Savvy <alecsavvy@outlook.com>
2023-05-18 21:35:53 -0600

    health check

--- discovery-provider/plugins/pedalboard/scripts/sandbox.ts
+++ discovery-provider/plugins/pedalboard/scripts/sandbox.ts
@@ -9,7 +9,7 @@ const main = async () => {
     await new App()
         .repeat(3000, async (app) => console.log(`Repeat one: ${new Date().getTime()}`))
         .repeat(10000, async (app) => console.log(`Repeat two: ${new Date().getTime()}`))
-        .scan(Table.Users, async (self, row) => {}) 
+        .scan(Table.Tracks, async (self, row) => { })
         .spawn(async (app) => console.log("spawned"))
         .run()
 }
---
+++ discovery-provider/plugins/pedalboard/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import App from './app'
+import { Table } from './models'
+import { Users } from './models'
+
+const server = async (self: App) => {
+    const port = 3000
+    const app = express()
+
+    app.get('/health', async (_req, res) => {
+        const db = self.getDnDb()
+        const user = await db<Users>(Table.Users).where('user_id', '=', 1).andWhere('is_current', '=', true).first();
+        const db_status = !(user === undefined);
+        res.json({
+            health: 'amped 🎸',
+            db_status: db_status ? "plugged in 🎙️" : "unplugged 🔌",
+        })
+    })
+
+    app.listen(port, () => {
+        console.log(`Health check listening on port ${port}`)
+    })
+}
