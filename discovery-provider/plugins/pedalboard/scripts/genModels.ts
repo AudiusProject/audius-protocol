636d6454b842fbe3324a0fa8783559ced97c2848
Alec Savvy <alecsavvy@outlook.com>
2023-05-18 19:46:56 -0600

    gen models

--- discovery-provider/plugins/pedalboard/scripts/genModels.ts
+++ discovery-provider/plugins/pedalboard/scripts/genModels.ts
@@ -2,10 +2,11 @@ import { knex } from "knex";
 import { updateTypes } from "knex-types";
 
 const db = knex({
+    client: "pg",
     connection: ""
 });
 
-updateTypes(db, { output: "../src/models.ts" }).catch((err) => {
+updateTypes(db, { output: "./src/models.ts" }).catch((err) => {
     console.error(err);
     process.exit(1);
 });
