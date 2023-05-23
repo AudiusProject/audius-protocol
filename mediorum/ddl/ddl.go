204a5e06ab460769cbd015da30d43a117aee4992
Steve Perkins <stereosteve@users.noreply.github.com>
2023-05-23 15:17:19 -0400

    exit with error if truncate fails (#5277)

--- mediorum/ddl/ddl.go
+++ mediorum/ddl/ddl.go
@@ -16,13 +16,19 @@ func Migrate(db *sql.DB) {
 
 	// flare-178: disable cid beam
 	// clear out existing data
-	db.Exec(`
+	log.Println("truncate cid tables... ")
+	_, err := db.Exec(`
 	drop trigger if exists handle_cid_change on "Files";
 	truncate table cid_cursor cascade;
 	truncate table cid_log cascade;
 	truncate table cid_lookup cascade;
 	drop table if exists cid_temp;
 	`)
+	if err != nil {
+		log.Fatal("truncate cid failed", err)
+	} else {
+		log.Println("truncate cid ok")
+	}
 }
 
 func mustExec(db *sql.DB, ddl string) {
