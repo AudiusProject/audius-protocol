eed7fe6b05d24a5936607bb59489332c104789eb
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-05-24 14:42:10 -0400

    [PAY-1253] Mobile message resend button (#3413)

---
+++ apps/audius-client/packages/mobile/src/screens/chat-screen/ResendMessageButton.tsx
@@ -0,0 +1,78 @@
+import { useCallback } from 'react'
+
+import { chatActions, chatSelectors } from '@audius/common'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+
+import IconError from 'app/assets/images/iconError.svg'
+import { makeStyles } from 'app/styles'
+import { useThemeColors } from 'app/utils/theme'
+
+const { getChatMessageById } = chatSelectors
+const { sendMessage } = chatActions
+
+const messages = {
+  error: 'Message Failed to Send. Tap to Retry.'
+}
+
+const useStyles = makeStyles(({ spacing, palette, typography }) => ({
+  errorContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: spacing(1),
+    marginTop: spacing(2)
+  },
+  errorText: {
+    fontSize: typography.fontSize.xs,
+    letterSpacing: 0.2,
+    color: palette.accentRed
+  },
+  iconError: {
+    width: spacing(3),
+    height: spacing(3)
+  }
+}))
+
+type ResendMessageButtonProps = {
+  messageId: string
+  chatId: string
+}
+
+export const ResendMessageButton = ({
+  messageId,
+  chatId
+}: ResendMessageButtonProps) => {
+  const styles = useStyles()
+  const { accentRed } = useThemeColors()
+  const dispatch = useDispatch()
+
+  const message = useSelector((state) =>
+    getChatMessageById(state, chatId, messageId)
+  )
+
+  const handleErrorPress = useCallback(() => {
+    if (message) {
+      dispatch(
+        sendMessage({
+          chatId,
+          message: message.message,
+          resendMessageId: message.message_id
+        })
+      )
+    }
+  }, [chatId, dispatch, message])
+
+  return (
+    <TouchableOpacity onPress={handleErrorPress}>
+      <View style={styles.errorContainer}>
+        <IconError
+          fill={accentRed}
+          width={styles.iconError.width}
+          height={styles.iconError.height}
+        />
+        <Text style={styles.errorText}>{messages.error}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
