c6bf3d90190133541f8d17f03fffff6804225d75
Steve Perkins <stereosteve@users.noreply.github.com>
2023-05-25 15:09:39 -0400

    re-add placeholder Files table

--- mediorum/ddl/cid_lookup.sql
+++ mediorum/ddl/cid_lookup.sql
@@ -1,5 +1,27 @@
 begin;
 
+
+-- creator-node creates the Files table, but we need to create it here in case we're running via audius-compose without CNs
+CREATE TABLE IF NOT EXISTS "Files" (
+    "id" SERIAL PRIMARY KEY,
+    "ownerId" INTEGER NOT NULL,
+    "multihash" text NOT NULL,
+    "sourceFile" text,
+    "storagePath" text NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    "fileUUID" uuid NOT NULL UNIQUE,
+    "cnodeUserUUID" uuid,
+    "type" character varying(16),
+    "fileName" text,
+    "dirMultihash" text,
+    -- "trackBlockchainId" integer, -- this is part of the table, but CN migration sets it and breaks if it already exists. initdb/init.sql will create it if running without CNs
+    "clock" integer NOT NULL,
+    "skipped" boolean DEFAULT false NOT NULL
+);
+
+
+
 -- cid_lookup is the main table used to determine which host has a given CID
 -- multihash column is used for both multihash and dirMultihash
 create table if not exists cid_lookup (
