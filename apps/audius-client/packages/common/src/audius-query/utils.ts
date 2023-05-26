e64883f5c77932ab09f236cee95f8a37f2081e6f
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-05-26 17:58:25 -0500

    [C-2653] Add shallow argument to audius-query hooks (#3450)

--- apps/audius-client/packages/common/src/audius-query/utils.ts
+++ apps/audius-client/packages/common/src/audius-query/utils.ts
@@ -1,10 +1,11 @@
+import { mapValues } from 'lodash'
+
 import { Kind } from 'models/Kind'
 import { CommonState } from 'store/reducers'
 
 import * as cacheSelectors from '../store/cache/selectors'
 
 import { EntityMap } from './types'
-
 export function capitalize(str: string) {
   return str.replace(str[0], str[0].toUpperCase())
 }
@@ -15,8 +16,18 @@ export const getKeyFromFetchArgs = (fetchArgs: any) => {
 
 export const selectCommonEntityMap = (
   state: CommonState,
-  kind?: Kind
+  kind?: Kind,
+  shallow?: boolean
 ): EntityMap | null => {
+  if (kind && shallow) {
+    return {
+      [kind]: mapValues(
+        cacheSelectors.getCache(state, { kind }).entries,
+        'metadata'
+      )
+    }
+  }
+
   const entityMap: EntityMap = {
     [Kind.USERS]: cacheSelectors.getAllEntries(state, { kind: Kind.USERS })
   }
