aa44c8b27951ed4280972dd937702816a93d977a
Sebastian Klingler <sliptype@gmail.com>
2023-05-31 12:04:23 -0500

    [C-2520] sdk trackUpload schema validation (#5320)

--- libs/src/sdk/services/Storage/types.ts
+++ libs/src/sdk/services/Storage/types.ts
@@ -15,7 +15,7 @@ export type StorageService = {
     template
   }: {
     file: File
-    onProgress: ProgressCB
+    onProgress?: ProgressCB
     template: FileTemplate
   }) => Promise<UploadResponse>
 }
---
+++ libs/src/sdk/types/HashId.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+import { decodeHashId } from '../utils/hashId'
+
+export const HashId = z.string().transform<number>((data: string, context) => {
+  const id = decodeHashId(data)
+  if (id === null) {
+    context.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'Hash id is invalid'
+    })
+
+    return z.NEVER
+  }
+  return id
+})
