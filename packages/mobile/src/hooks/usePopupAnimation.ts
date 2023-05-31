27001658ef8568eb1c88b2dedb49c6e342293429
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-05-31 16:34:20 -0400

    [PAY-1293] Adjust portal and animations to get more consistent behavior on Android (#3475)

--- packages/mobile/src/hooks/usePopupAnimation.ts
+++ packages/mobile/src/hooks/usePopupAnimation.ts
@@ -1,4 +1,3 @@
-import type { MutableRefObject } from 'react'
 import { useCallback, useEffect, useRef } from 'react'
 
 import { Animated } from 'react-native'
@@ -6,58 +5,62 @@ import { Animated } from 'react-native'
 import {
   BACKGROUND_OPACITY,
   MOUNT_ANIMATION_DURATION_MS,
-  UNMOUNT_ANIMATION_DURATION_MS,
-  REACTION_CONTAINER_HEIGHT
+  REACTION_CONTAINER_HEIGHT,
+  UNMOUNT_ANIMATION_DURATION_MS
 } from 'app/screens/chat-screen/constants'
 
 export const usePopupAnimation = (
   onClose: () => void
-): [
-  MutableRefObject<Animated.Value>,
-  MutableRefObject<Animated.Value>,
-  MutableRefObject<Animated.Value>,
-  () => void
-] => {
-  const backgroundOpacityAnim = useRef(new Animated.Value(0))
-  const otherOpacityAnim = useRef(new Animated.Value(0))
-  const translationAnim = useRef(new Animated.Value(REACTION_CONTAINER_HEIGHT))
+): [Animated.Value, Animated.Value, Animated.Value, () => void] => {
+  const backgroundOpacityAnim = useRef(new Animated.Value(0)).current
+  const otherOpacityAnim = useRef(new Animated.Value(0)).current
+  const translationAnim = useRef(
+    new Animated.Value(REACTION_CONTAINER_HEIGHT)
+  ).current
+
   const beginMountAnimation = useCallback(() => {
-    Animated.parallel([
-      Animated.timing(backgroundOpacityAnim.current, {
-        toValue: BACKGROUND_OPACITY,
-        useNativeDriver: true,
-        duration: MOUNT_ANIMATION_DURATION_MS
-      }),
-      Animated.timing(otherOpacityAnim.current, {
-        toValue: 1,
-        useNativeDriver: true,
-        duration: MOUNT_ANIMATION_DURATION_MS
-      }),
-      Animated.spring(translationAnim.current, {
-        toValue: 0,
-        useNativeDriver: true
-      })
-    ]).start()
-  }, [])
+    Animated.parallel(
+      [
+        Animated.timing(backgroundOpacityAnim, {
+          toValue: BACKGROUND_OPACITY,
+          useNativeDriver: true,
+          duration: MOUNT_ANIMATION_DURATION_MS
+        }),
+        Animated.timing(otherOpacityAnim, {
+          toValue: 1,
+          useNativeDriver: true,
+          duration: MOUNT_ANIMATION_DURATION_MS
+        })
+      ],
+      { stopTogether: true }
+    ).start()
+    Animated.spring(translationAnim, {
+      toValue: 0,
+      useNativeDriver: true
+    }).start()
+  }, [backgroundOpacityAnim, otherOpacityAnim, translationAnim])
 
   const beginUnmountAnimation = useCallback(() => {
-    Animated.parallel([
-      Animated.timing(backgroundOpacityAnim.current, {
-        toValue: 0,
-        useNativeDriver: true,
-        duration: UNMOUNT_ANIMATION_DURATION_MS
-      }),
-      Animated.timing(otherOpacityAnim.current, {
-        toValue: 0,
-        useNativeDriver: true,
-        duration: UNMOUNT_ANIMATION_DURATION_MS
-      })
-    ]).start(onClose)
-    Animated.spring(translationAnim.current, {
+    Animated.spring(translationAnim, {
       toValue: REACTION_CONTAINER_HEIGHT,
       useNativeDriver: true
     }).start()
-  }, [onClose])
+    Animated.parallel(
+      [
+        Animated.timing(backgroundOpacityAnim, {
+          toValue: 0,
+          useNativeDriver: true,
+          duration: UNMOUNT_ANIMATION_DURATION_MS
+        }),
+        Animated.timing(otherOpacityAnim, {
+          toValue: 0,
+          useNativeDriver: true,
+          duration: UNMOUNT_ANIMATION_DURATION_MS
+        })
+      ],
+      { stopTogether: true }
+    ).start(onClose)
+  }, [onClose, backgroundOpacityAnim, otherOpacityAnim, translationAnim])
 
   useEffect(() => {
     beginMountAnimation()
