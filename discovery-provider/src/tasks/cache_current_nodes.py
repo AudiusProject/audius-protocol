2da981acedad48d7cd49317e43103bdfccc042c1
Dylan Jeffers <dylan@audius.co>
2023-05-30 18:51:19 -0700

    [C-2695] Expose content-nodes in discovery-node health-check (#5324)

--- discovery-provider/src/tasks/cache_current_nodes.py
+++ discovery-provider/src/tasks/cache_current_nodes.py
@@ -2,8 +2,10 @@ import logging
 
 from src.tasks.celery_app import celery
 from src.utils.get_all_other_nodes import (
-    ALL_NODES_CACHE_KEY,
-    get_all_other_nodes,
+    ALL_CONTENT_NODES_CACHE_KEY,
+    ALL_DISCOVERY_NODES_CACHE_KEY,
+    get_all_other_content_nodes,
+    get_all_other_discovery_nodes,
     get_node_endpoint,
 )
 from src.utils.prometheus_metric import save_duration_metric
@@ -27,15 +29,21 @@ def cache_current_nodes_task(self):
     try:
         have_lock = update_lock.acquire(blocking=False)
         if have_lock:
-            logger.info("cache_current_nodes.py | fetching all other nodes")
-            nodes = get_all_other_nodes()[0]
+            logger.info("cache_current_nodes.py | fetching all other discovery nodes")
+            discovery_nodes = get_all_other_discovery_nodes()[0]
             current_node = get_node_endpoint()
             # add current node to list
             if current_node is not None:
-                nodes.append(current_node)
+                discovery_nodes.append(current_node)
 
-            set_json_cached_key(redis, ALL_NODES_CACHE_KEY, nodes)
-            logger.info("cache_current_nodes.py | set current nodes in redis")
+            set_json_cached_key(redis, ALL_DISCOVERY_NODES_CACHE_KEY, discovery_nodes)
+            logger.info("cache_current_nodes.py | set current discovery nodes in redis")
+
+            logger.info("cache_current_nodes.py | fetching all other content nodes")
+            content_nodes = get_all_other_content_nodes()[0]
+
+            set_json_cached_key(redis, ALL_CONTENT_NODES_CACHE_KEY, content_nodes)
+            logger.info("cache_current_nodes.py | set current content nodes in redis")
         else:
             logger.info("cache_current_nodes.py | Failed to acquire lock")
     except Exception as e:
