0c261db74b62297262ef5f835784dde6d763c6f5
Theo Ilie <theoilie.ti@gmail.com>
2023-06-01 16:59:20 -0700

    Hotfix mediorum /contact, blacklist v2 tracks, and fix hanging on stream (#5353)

--- mediorum/server/serve_contact.go
+++ mediorum/server/serve_contact.go
@@ -6,10 +6,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type ContactResponse struct {
+	Email string `json:"email"`
+}
+
 func (ss *MediorumServer) serveContact(c echo.Context) error {
+	if ss.trustedNotifier != nil {
+		return c.JSON(200, ContactResponse{Email: ss.trustedNotifier.Email})
+	}
+
 	email := os.Getenv("nodeOperatorEmailAddress")
 	if email == "" {
 		return c.String(200, "Email address unavailable at the moment")
 	}
-	return c.String(200, email)
+	return c.JSON(200, ContactResponse{Email: email})
 }
