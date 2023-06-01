02da0bdf5713f9e954333be3881e097a471d9bba
Michael Piazza <michael.piazza.mp@gmail.com>
2023-06-01 18:04:45 -0400

    [PAY-1324][PAY-1326][PAY-1323][PAY-1327][PAY-1331][PAY-1328][PAY-1320] DMs UI fixes 2 (#3491)

--- apps/audius-client/packages/web/src/components/user-badges/FollowsYouBadge.tsx
+++ apps/audius-client/packages/web/src/components/user-badges/FollowsYouBadge.tsx
@@ -6,10 +6,27 @@ const messages = {
   followsYou: 'Follows You'
 }
 
-const FollowsYouBadge = ({ className = '' }: { className?: string }) => {
+type FollowsYouBadgeProps = {
+  className?: string
+  /** For badges appearing in a list, expose a variant with a transparent background */
+  variant?: 'standard' | 'list'
+}
+
+const FollowsYouBadge = ({
+  className = '',
+  variant = 'standard'
+}: FollowsYouBadgeProps) => {
   const wm = useWithMobileStyle(styles.mobile)
   return (
-    <div className={wm(styles.badge, className)}>{messages.followsYou}</div>
+    <div
+      className={wm(
+        styles.badge,
+        { [styles.list]: variant === 'list' },
+        className
+      )}
+    >
+      {messages.followsYou}
+    </div>
   )
 }
 
