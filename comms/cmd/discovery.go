50b6fab7a4b662388ba2e409f3e5d582cf4d4a79
Steve Perkins <stereosteve@users.noreply.github.com>
2023-06-06 14:00:47 -0400

    internal commands to ban / unban users from chat (#5375)

--- comms/cmd/discovery.go
+++ comms/cmd/discovery.go
@@ -1,8 +1,15 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"time"
+
 	"comms.audius.co/discovery"
+	"comms.audius.co/discovery/config"
 	"comms.audius.co/discovery/db"
+	"comms.audius.co/discovery/rpcz"
+	"comms.audius.co/discovery/schema"
 	"github.com/spf13/cobra"
 )
 
@@ -28,4 +35,62 @@ var discoveryMigrationsCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(discoveryCmd)
 	rootCmd.AddCommand(discoveryMigrationsCmd)
+	rootCmd.AddCommand(&cobra.Command{
+		Use: "ban",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("banning:", args)
+			sendInternalRpc("internal.chat.ban", map[string]any{
+				"user_ids": args,
+			})
+		},
+	})
+	rootCmd.AddCommand(&cobra.Command{
+		Use: "unban",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("unbanning:", args)
+			sendInternalRpc("internal.chat.unban", map[string]any{
+				"user_ids": args,
+			})
+		},
+	})
+}
+
+func sendInternalRpc(method string, params any) error {
+	paramsJson, err := json.Marshal(params)
+	if err != nil {
+		panic(err)
+	}
+
+	rawRpc := &schema.RawRPC{
+		Method:    method,
+		Params:    paramsJson,
+		Timestamp: time.Now().UnixMilli(),
+	}
+
+	err = db.Dial()
+	if err != nil {
+		panic(err)
+	}
+
+	discoveryConfig := config.Parse()
+	if err != nil {
+		panic(err)
+	}
+
+	proc, err := rpcz.NewProcessor(discoveryConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	log, err := rpcz.PrepareSignedRpcLog(rawRpc, discoveryConfig.MyPrivateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = proc.ApplyAndPublish(log)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
