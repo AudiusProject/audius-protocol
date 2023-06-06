85dce913fee8c77ff0a83cc652b5c5bb5db05e60
Alec Savvy <alecsavvy@outlook.com>
2023-06-06 07:25:56 -0600

    dotenv and solana config

--- discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/index.ts
+++ discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/index.ts
@@ -1,6 +1,7 @@
-import { log } from "logger";
+import dotenv from 'dotenv'
 import { main } from "./main";
 
 (async () => {
+  dotenv.config()
   await main().catch(console.error);
 })();
--- discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/libs.ts
+++ discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/libs.ts
@@ -1,25 +1,33 @@
 import { AudiusLibs } from "@audius/sdk";
 
+const config = {
+  solanaWeb3Config: {
+    solanaClusterEndpoint: process.env.solanaClusterEndpoint,
+    mintAddress: process.env.mintAddress,
+    solanaTokenAddress: process.env.solanaTokenAddress,
+    claimableTokenPDA: process.env.claimableTokenPDA,
+    feePayerAddress: process.env.feePayerAddress,
+    claimableTokenProgramAddress: process.env.claimableTokenProgramAddress,
+    rewardsManagerProgramId: process.env.rewardsManagerProgramId,
+    rewardsManagerProgramPDA: process.env.rewardsManagerProgramPDA,
+    rewardsManagerTokenPDA: process.env.rewardsManagerTokenPDA,
+    useRelay: process.env.useRelay
+  }
+}
+
+// TODO: promote this into a packages instead of tcr
+ // @ts-ignore
 export const initAudiusLibs = async (): Promise<AudiusLibs> => {
-  const config = {
-    web3Config,
-    ethWeb3Config,
-    solanaWeb3Config,
-    identityServiceConfig,
-    discoveryProviderConfig,
-    creatorNodeConfig,
-    comstockConfig,
-    wormholeConfig,
-    hedgehogConfig,
-    isServer,
-    logger,
-    isDebug,
-    preferHigherPatchForPrimary,
-    preferHigherPatchForSecondaries,
-    localStorage,
+  const libsConfig = {
+    solanaWeb3Config: config.solanaWeb3Config,
+    isServer: true,
+    logger: console,
+    isDebug: false,
+    localStorage: false,
     isStorageV2Only: false,
   };
-  const libs = new AudiusLibs(config);
+  // @ts-ignore
+  const libs = new AudiusLibs(libsConfig);
   await libs.init();
   return libs;
 };
