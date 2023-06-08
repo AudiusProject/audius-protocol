dd6862ea3434f7d484f2d475e9da6cb9399c47f7
Raymond Jacobson <ray@audius.co>
2023-06-08 12:26:57 -0700

    Better impl

--- discovery-provider/nginx_conf/port.lua
+++ discovery-provider/nginx_conf/port.lua
@@ -21,7 +21,7 @@ end
 
 function _M.get_is_port_exposed(ip, port)
     local tcp = ngx.socket.tcp()
-    tcp:settimeout(1000)
+    tcp:settimeout(10)
     local res, err = tcp:connect(ip, port)
     tcp:close()
     if not res then
@@ -30,14 +30,43 @@ function _M.get_is_port_exposed(ip, port)
     end
 
     local udp = ngx.socket.udp()
-    udp:settimeout(1000)
-    local res, err = udp:setpeername(ip, port)
+    udp:settimeout(10)
+    assert(udp:sendto("ping", ip, port))
+    local res, err = udp:receive()
     if not res then
-        ngx.log(ngx.ERR, "Could not peer udp: ", err)
+        ngx.log(ngx.ERR, "Could receive udp message: ", err)
+        return false
+    end
+    if res != "pong" then
+        ngx.log(ngx.ERR, "Received wrong udp message: ", res)
         return false
     end
 
     return true
+end
+
+function _M.udp_heartbeat()
+    local sock, err = ngx.req.socket(true)
+    if not sock then
+        ngx.log(ngx.ERR, "failed to get the request socket: ", err)
+        return
+    end
+
+    local data, err, partial = sock:receive()
+    if not data then
+        data = partial
+        if not data then
+            ngx.log(ngx.ERR, "failed to receive the data: ", err)
+            return
+        end
+    end
+
+    ngx.log(ngx.INFO, "Received udp: ", data)
+
+    local bytes, err = sock:send("pong")
+    if not bytes then
+        ngx.log(ngx.ERR, "failed to send: ", err)
+    end
 end
 
 return _M
