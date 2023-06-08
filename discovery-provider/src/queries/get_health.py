510c1719bd6e03b11405d2c807c58441f41526ec
Theo Ilie <theoilie.ti@gmail.com>
2023-06-08 13:18:04 -0700

    Just call it content_nodes

--- discovery-provider/src/queries/get_health.py
+++ discovery-provider/src/queries/get_health.py
@@ -331,9 +331,7 @@ def get_health(args: GetHealthArgs, use_redis_cache: bool = True) -> Tuple[Dict,
         "audius_db_url", ""
     )
     discovery_nodes = get_all_other_nodes.get_all_other_discovery_nodes_cached(redis)
-    healthy_content_nodes = get_all_other_nodes.get_all_healthy_content_nodes_cached(
-        redis
-    )
+    content_nodes = get_all_other_nodes.get_all_healthy_content_nodes_cached(redis)
     final_poa_block = helpers.get_final_poa_block()
     backfilled_cid_data = get_backfilled_cid_data(redis)
     health_results = {
@@ -374,10 +372,7 @@ def get_health(args: GetHealthArgs, use_redis_cache: bool = True) -> Tuple[Dict,
         "latest_block_num": latest_block_num,
         "latest_indexed_block_num": latest_indexed_block_num,
         "final_poa_block": final_poa_block,
-        "network": {
-            "discovery_nodes": discovery_nodes,
-            "healthy_content_nodes": healthy_content_nodes,
-        },
+        "network": {"discovery_nodes": discovery_nodes, "content_nodes": content_nodes},
         "backfilled_cid_data": backfilled_cid_data,
     }
 
