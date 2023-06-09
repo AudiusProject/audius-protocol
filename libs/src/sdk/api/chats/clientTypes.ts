474e024f864c505f103fae7ee77f4345173c5e38
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-06-09 11:59:59 -0700

    Update ChatsApi to use Zod for types and input validation (#5393)

--- libs/src/sdk/api/chats/clientTypes.ts
+++ libs/src/sdk/api/chats/clientTypes.ts
@@ -1,4 +1,6 @@
-import type {
+import { z } from 'zod'
+
+import {
   CommsResponse,
   ChatPermission,
   ChatMessage,
@@ -7,69 +9,103 @@ import type {
 
 // REQUEST PARAMETERS
 
-export type ChatGetAllRequest = {
-  limit?: number
-  before?: string
-  after?: string
-}
+export const ChatGetAllRequestSchema = z.object({
+  limit: z.optional(z.number()),
+  before: z.optional(z.string()),
+  after: z.optional(z.string())
+})
 
-export type ChatGetRequest = {
-  chatId: string
-}
+export type ChatGetAllRequest = z.infer<typeof ChatGetAllRequestSchema>
 
-export type ChatGetMessagesRequest = {
-  chatId: string
-  limit?: number
-  before?: string
-  after?: string
-}
+export const ChatGetRequestSchema = z.object({
+  chatId: z.string()
+})
 
-export type ChatCreateRequest = {
-  userId: string
-  invitedUserIds: string[]
-}
+export type ChatGetRequest = z.infer<typeof ChatGetRequestSchema>
 
-export type ChatInviteRequest = {
-  chatId: string
-  userId: string
-  invitedUserIds: string[]
-}
+export const ChatGetMessagesRequestSchema = z.object({
+  chatId: z.string(),
+  limit: z.optional(z.number()),
+  before: z.optional(z.string()),
+  after: z.optional(z.string())
+})
 
-export type ChatMessageRequest = {
-  chatId: string
-  messageId?: string
-  message: string
-}
+export type ChatGetMessagesRequest = z.infer<
+  typeof ChatGetMessagesRequestSchema
+>
 
-export type ChatReactRequest = {
-  chatId: string
-  messageId: string
-  reaction: string | null
-}
+export const ChatCreateRequestSchema = z.object({
+  userId: z.string(),
+  invitedUserIds: z.array(z.string()).min(1)
+})
 
-export type ChatReadRequest = {
-  chatId: string
-}
+export type ChatCreateRequest = z.infer<typeof ChatCreateRequestSchema>
 
-export type ChatBlockRequest = {
-  userId: string
-}
+export const ChatInviteRequestSchema = z.object({
+  chatId: z.string(),
+  userId: z.string(),
+  invitedUserIds: z.array(z.string()).min(1)
+})
 
-export type ChatDeleteRequest = {
-  chatId: string
-}
+export type ChatInviteRequest = z.infer<typeof ChatInviteRequestSchema>
 
-export type ChatPermitRequest = {
-  permit: ChatPermission
-}
+export const ChatMessageRequestSchema = z.object({
+  chatId: z.string(),
+  messageId: z.optional(z.string()),
+  message: z.string()
+})
 
-export type ChatValidateCanCreateRequest = {
-  userIds: string[]
-}
+export type ChatMessageRequest = z.infer<typeof ChatMessageRequestSchema>
 
-export type ChatGetPermissionRequest = {
-  userIds?: string[]
-}
+export const ChatReactRequestSchema = z.object({
+  chatId: z.string(),
+  messageId: z.string(),
+  reaction: z.nullable(z.string())
+})
+
+export type ChatReactRequest = z.infer<typeof ChatReactRequestSchema>
+
+export const ChatReadRequestSchema = z.object({ chatId: z.string() })
+
+export type ChatReadRequest = z.infer<typeof ChatReadRequestSchema>
+
+export const ChatBlockRequestSchema = z.object({
+  userId: z.string()
+})
+
+export type ChatBlockRequest = z.infer<typeof ChatBlockRequestSchema>
+
+export const ChatDeleteRequestSchema = z.object({ chatId: z.string() })
+
+export type ChatDeleteRequest = z.infer<typeof ChatDeleteRequestSchema>
+
+export const ChatPermitRequestSchema = z.object({
+  permit: z.nativeEnum(ChatPermission)
+})
+
+export type ChatPermitRequest = z.infer<typeof ChatPermitRequestSchema>
+
+export const ChatValidateCanCreateRequestSchema = z.object({
+  userIds: z.array(z.string()).min(1)
+})
+
+export type ChatValidateCanCreateRequest = z.infer<
+  typeof ChatValidateCanCreateRequestSchema
+>
+
+export const ChatGetPermissionRequestSchema = z.object({
+  userIds: z.array(z.string()).min(1)
+})
+
+export type ChatGetPermissionRequest = z.infer<
+  typeof ChatGetPermissionRequestSchema
+>
+
+export const ChatUnfurlRequestSchema = z.object({
+  urls: z.array(z.string()).min(1)
+})
+
+export type ChatUnfurlRequest = z.infer<typeof ChatUnfurlRequestSchema>
 
 export type TypedCommsResponse<T> = Omit<CommsResponse, 'data'> & {
   data: T
