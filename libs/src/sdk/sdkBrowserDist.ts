d0352d167e2bf9603059032a7acf5f4f1002bd8a
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-06-08 17:31:30 -0700

    Expose ParseRequestError (and other necessary config)

--- libs/src/sdk/sdkBrowserDist.ts
+++ libs/src/sdk/sdkBrowserDist.ts
@@ -1,2 +1,12 @@
+import { ParseRequestError } from '../legacy'
+import { developmentConfig, productionConfig, stagingConfig } from './config'
 import { sdk } from './sdk'
+import { DiscoveryNodeSelector } from './services'
 ;(window as any).audiusSdk = sdk
+;(window as any).audiusSdk.DiscoveryNodeSelector = DiscoveryNodeSelector
+;(window as any).audiusSdk.config = {
+  staging: stagingConfig,
+  production: productionConfig,
+  development: developmentConfig
+}
+;(window as any).audiusSdk.ParseRequestError = ParseRequestError
