b12fe5e7af5abf407e9384ed084cdc5cf3ea8c48
sliptype <sliptype@gmail.com>
2023-06-13 14:37:23 -0500

    Add tests

--- libs/src/sdk/api/tracks/TracksApi.test.ts
+++ libs/src/sdk/api/tracks/TracksApi.test.ts
@@ -89,9 +89,7 @@ describe('TracksApi', () => {
       expect(result).toStrictEqual({
         blockHash: 'a',
         blockNumber: 1,
-        error: false,
-        trackId: 1,
-        transcodedTrackCID: 'a'
+        trackId: 1
       })
     })
 
@@ -110,6 +108,29 @@ describe('TracksApi', () => {
             buffer: Buffer.from([]),
             name: 'trackArt'
           }
+        })
+      }).rejects.toThrow()
+    })
+  })
+
+  describe('deleteTrack', () => {
+    it('deletes a track if valid metadata is provided', async () => {
+      const result = await tracks.deleteTrack({
+        userId: '7eP5n',
+        trackId: 'x5pJ3Aj'
+      })
+
+      expect(result).toStrictEqual({
+        blockHash: 'a',
+        blockNumber: 1
+      })
+    })
+
+    it('throws an error if invalid metadata is provided', async () => {
+      await expect(async () => {
+        await tracks.deleteTrack({
+          userId: '7eP5n',
+          trackId: 1 as any
         })
       }).rejects.toThrow()
     })
