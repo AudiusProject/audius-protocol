5bea10556a8de96071e4c293c9564a7e67fed12d
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-06-15 12:11:45 -0700

    Wait for libs to init before initting SDK (#3604)

--- packages/web/src/services/audius-sdk/audiusSdk.ts
+++ packages/web/src/services/audius-sdk/audiusSdk.ts
@@ -19,6 +19,13 @@ const SDK_LOADED_EVENT_NAME = 'AUDIUS_SDK_LOADED'
 
 const initSdk = async () => {
   inProgress = true
+  // We wait for libs here because AudiusBackend needs to register a listener that
+  // will let AudiusAPIClient know that libs has loaded, and without it AudiusAPIClient
+  // retries failed requests ad nauseum with no delays or backoffs and won't ever get
+  // the signal that libs is loaded. It sucks. But the easiest thing to do right now...
+  console.debug('[audiusSdk] Waiting for libs init...')
+  await waitForLibsInit()
+  console.debug('[audiusSdk] Libs initted, initializing SDK...')
   const discoveryNodeSelector = await discoveryNodeSelectorService.getInstance()
   const audiusSdk = sdk({
     appName: 'audius-client',
@@ -67,13 +74,7 @@ const initSdk = async () => {
       }
     }
   })
-  // We wait for libs here because AudiusBackend needs to register a listener that
-  // will let AudiusAPIClient know that libs has loaded, and without it AudiusAPIClient
-  // retries failed requests ad nauseum with no delays or backoffs and won't ever get
-  // the signal that libs is loaded. It sucks. But the easiest thing to do right now...
-  console.debug('[audiusSdk] Waiting for libs init...')
-  await waitForLibsInit()
-  console.debug('[audiusSdk] Libs initted, SDK initted.')
+  console.debug('[audiusSdk] SDK initted.')
   window.audiusSdk = audiusSdk
   inProgress = false
   window.dispatchEvent(new CustomEvent(SDK_LOADED_EVENT_NAME))
