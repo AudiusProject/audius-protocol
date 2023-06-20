5129945dcecba1336702eb750ae8cd8e4cc4a11d
Michelle Brier <michelle.brier4@gmail.com>
2023-06-20 15:31:03 -0700

    v2 edit track

--- dev-tools/commands/src/index.mjs
+++ dev-tools/commands/src/index.mjs
@@ -15,6 +15,7 @@ import "./tip-audio.mjs";
 import "./unfollow.mjs";
 import "./auth-headers.mjs";
 import "./upload-track.mjs";
+import "./edit-track.mjs";
 
 async function main() {
   program.parseAsync(process.argv);
