0aff88247382434467feacf68b0b30f33bfaaa8b
Isaac Solo <isaac@audius.co>
2023-06-21 14:58:48 -0700

    Fix handler

--- identity-service/src/rateLimiter.js
+++ identity-service/src/rateLimiter.js
@@ -130,6 +130,8 @@ const getRateLimiter = ({
   max,
   expiry = DEFAULT_EXPIRY,
   keyGenerator = (req) => getIP(req).ip,
+  handler,
+  message,
   skip
 }) => {
   return rateLimit({
@@ -141,6 +143,8 @@ const getRateLimiter = ({
     max, // max requests per hour
     skip,
     keyGenerator,
+    handler,
+    message,
     onLimitReached
   })
 }
@@ -210,6 +214,7 @@ const recoverSigner = (encodedABI) => {
 const rateLimitMessage = 'Too many requests, please try again later'
 const getRelayBlocklistMiddleware = (req, res, next) => {
   const signer = recoverSigner(req.body.encodedABI)
+  req.body.signer = signer
   const blocklist = config.get('blocklistPublicKeyFromRelay')
   if (blocklist && blocklist.includes(signer)) {
     return res.status(429).send(rateLimitMessage)
@@ -259,14 +264,14 @@ const getRelayRateLimiterMiddleware = () => {
       const signer = recoverSigner(req.body.encodedABI)
       return ':::' + key + ':' + signer
     },
-    handler: (req, res, options) => {
-      const signer = recoverSigner(req.body.encodedABI)
+    handler: (req, res) => {
       try {
+        const signer = recoverSigner(req.body.encodedABI)
         req.logger.error(`Rate limited sender ${signer}`)
       } catch (error) {
-        req.logger.error(`Cannot relay without sender address`)
+        req.logger.info(`Cannot relay without sender address`)
       }
-      res.status(options.statusCode).send(options.message)
+      res.status(429).send(rateLimitMessage)
     }
   })
 }
