fa0dce45a171d9417b1e7899e01396e2dbf8546b
Dylan Jeffers <dylan@audius.co>
2023-06-23 20:31:17 -0700

    Fix user profile always showing artist tabs (#3648)

---
+++ apps/audius-client/packages/mobile/src/screens/profile-screen/useIsArtist.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+
+import { accountActions, accountSelectors } from '@audius/common'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { useSelectProfile } from './selectors'
+
+const { fetchHasTracks } = accountActions
+const { getUserId, getAccountHasTracks } = accountSelectors
+
+export const useIsArtist = () => {
+  const { user_id, track_count } = useSelectProfile(['user_id', 'track_count'])
+  const accountHasTracks = useSelector(getAccountHasTracks)
+  const currentUserId = useSelector(getUserId)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (accountHasTracks === null && currentUserId === user_id) {
+      dispatch(fetchHasTracks())
+    }
+  }, [accountHasTracks, currentUserId, user_id, dispatch])
+
+  return (user_id === currentUserId && accountHasTracks) || track_count > 0
+}
