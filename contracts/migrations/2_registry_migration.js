94e023b8a0c971e53cf7c1a2cb641d9345e28154
Raymond Jacobson <ray@audius.co>
2023-06-26 12:26:05 -0700

    [PLAT-1014] Delete old POA contracts (#5482)

--- contracts/migrations/2_registry_migration.js
+++
@@ -1,9 +0,0 @@
-const Registry = artifacts.require('Registry')
-
-module.exports = (deployer) => {
-  deployer.then(async () => {
-    let registryDeployTx = await deployer.deploy(Registry)
-    // Set environment variable
-    process.env.dataContractsRegistryAddress = registryDeployTx.address
-  })
-}
