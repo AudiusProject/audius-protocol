1f82d2b6f65c70516ca36801ea1a59e3351b4719
Steve Perkins <stereosteve@users.noreply.github.com>
2023-06-27 11:12:00 -0400

    make wallet user_id lookup deterministic
    avoids selecting a errant is_current user row that has null handle,
    as a last resort if there are dupes, sorts by user_id asc to make deterministic.

--- comms/discovery/db/queries/get_users.go
+++ comms/discovery/db/queries/get_users.go
@@ -7,7 +7,14 @@ import (
 )
 
 const getUserIDFromWallet = `
-select user_id from users where is_current = TRUE and wallet = LOWER($1)
+select user_id
+from users
+where is_current = TRUE
+  and handle IS NOT NULL
+  and is_available = TRUE
+  and is_deactivated = FALSE
+  and wallet = LOWER($1)
+order by user_id asc
 `
 
 func GetUserIDFromWallet(q db.Queryable, ctx context.Context, walletAddress string) (int32, error) {
