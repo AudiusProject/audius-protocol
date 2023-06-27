160b325b773ae1baf4b9a39d8ca453ea6bcbaad6
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-06-26 22:02:39 -0500

    [C-2677] Remix Settings Modal layout complete (#3647)

--- packages/web/src/pages/upload-page/components/TrackModalArray.tsx
+++ packages/web/src/pages/upload-page/components/TrackModalArray.tsx
@@ -1,9 +1,13 @@
 import { ReleaseDateModalField } from '../fields/ReleaseDateModalField'
+import { RemixModalField } from '../fields/RemixModalField'
+
+import styles from './TrackModalArray.module.css'
 
 export const TrackModalArray = () => {
   return (
-    <>
+    <div className={styles.root}>
       <ReleaseDateModalField />
-    </>
+      <RemixModalField />
+    </div>
   )
 }
