670d0f047ad59f9ba3d63c4a61270589b81fc00f
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-06-27 15:22:52 -0400

    [PAY-1529] Add initial types for usdc purchases (#3651)

--- apps/audius-client/packages/common/src/models/Wallet.ts
+++ apps/audius-client/packages/common/src/models/Wallet.ts
@@ -4,8 +4,10 @@ import { Brand } from 'utils/typeUtils'
 
 export type StringWei = Brand<string, 'stringWEI'>
 export type StringAudio = string
+export type StringUSDC = Brand<string, 'stringUSDC'>
 export type BNWei = Brand<BN, 'BNWei'>
 export type BNAudio = Brand<BN, 'BNAudio'>
+export type BNUSDC = Brand<BN, 'BNUSDC'>
 
 export type WalletAddress = string
 export type SolanaWalletAddress = Brand<string, 'solanaWallet'>
