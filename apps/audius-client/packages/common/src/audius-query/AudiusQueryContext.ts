9173c8599c675d93719f6f91ef19689e4a77f6cb
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2023-06-27 11:16:52 -0400

    Add Sentry error logging to Audius query C-2800 (#3656)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- apps/audius-client/packages/common/src/audius-query/AudiusQueryContext.ts
+++ apps/audius-client/packages/common/src/audius-query/AudiusQueryContext.ts
@@ -5,10 +5,13 @@ import { AudiusSdk } from '@audius/sdk'
 import type { AudiusAPIClient } from 'services/audius-api-client'
 import { AudiusBackend } from 'services/index'
 
+import { ReportToSentryArgs } from '../models'
+
 export type AudiusQueryContextType = {
   apiClient: AudiusAPIClient
   audiusSdk: () => Promise<AudiusSdk>
   audiusBackend: AudiusBackend
+  reportToSentry: (args: ReportToSentryArgs) => void
 }
 
 export const AudiusQueryContext = createContext<AudiusQueryContextType | null>(
