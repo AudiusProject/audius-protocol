e1526462bd75fc9e4b0ff7861341295cd0d98e62
Michelle Brier <michelle.brier4@gmail.com>
2023-06-28 23:49:41 -0700

    typo

--- mediorum/ddl/add_fields_to_uploads.sql
+++ mediorum/ddl/add_fields_to_uploads.sql
@@ -3,6 +3,6 @@ begin;
 ALTER TABLE uploads
 ADD COLUMN IF NOT EXISTS user_id integer,
 ADD COLUMN IF NOT EXISTS preview_start_seconds integer,
-ADD COLUMN IF NOT EXISTS updated_at timestamp without timezone;
+ADD COLUMN IF NOT EXISTS updated_at timestamp with time zone;
 
 commit;
