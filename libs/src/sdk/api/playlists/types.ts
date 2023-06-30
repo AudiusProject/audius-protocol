7554a256eb8ce7bc51f9b0b79e011177bcc8401d
sliptype <sliptype@gmail.com>
2023-06-30 16:11:29 -0500

    Add jsdoc comment

--- libs/src/sdk/api/playlists/types.ts
+++ libs/src/sdk/api/playlists/types.ts
@@ -30,6 +30,10 @@ export const RepostPlaylistSchema = z
     playlistId: HashId,
     metadata: z.optional(
       z.object({
+        /**
+         * Is this a save of a repost? Used to dispatch notifications
+         * when a user favorites another user's repost
+         */
         isRepostOfRepost: z.boolean()
       })
     )
