2776d2e23a1ded9dc408e9d110ee8bf8f84f4068
Saliou Diallo <sddioulde@hotmail.com>
2023-07-05 17:38:51 -0400

    Merge pull request #142 from AudiusProject/sd-skip
    Temporarily skip bad dates

--- packages/protocol-dashboard/src/components/UniqueUsersChart/UniqueUsersChart.tsx
+++ packages/protocol-dashboard/src/components/UniqueUsersChart/UniqueUsersChart.tsx
@@ -2,6 +2,7 @@ import LineChart from 'components/LineChart'
 import React, { useState } from 'react'
 import { useApiCalls } from 'store/cache/analytics/hooks'
 import { Bucket, MetricError } from 'store/cache/analytics/slice'
+import { datesToSkip } from 'utils/consts'
 
 type OwnProps = {}
 
@@ -17,8 +18,14 @@ const UniqueUsersChart: React.FC<UniqueUsersChartProps> = () => {
     labels = []
     data = []
   } else {
-    labels = apiCalls?.map(a => new Date(a.timestamp).getTime() / 1000) ?? null
-    data = apiCalls?.map(a => a.summed_unique_count) ?? null
+    labels =
+      apiCalls
+        ?.filter(a => !datesToSkip.has(a.timestamp))
+        ?.map(a => new Date(a.timestamp).getTime() / 1000) ?? null
+    data =
+      apiCalls
+        ?.filter(a => !datesToSkip.has(a.timestamp))
+        ?.map(a => a.summed_unique_count) ?? null
   }
   return (
     <LineChart
@@ -28,7 +35,7 @@ const UniqueUsersChart: React.FC<UniqueUsersChartProps> = () => {
       labels={labels}
       selection={bucket}
       error={error}
-      options={[Bucket.ALL_TIME, Bucket.MONTH, Bucket.WEEK]}
+      options={[Bucket.ALL_TIME, Bucket.MONTH]}
       onSelectOption={(option: string) => setBucket(option as Bucket)}
       showLeadingDay
     />
