fe0665b12549bc39f7422a9113185c94e8fe4994
Alec Savvy <alecsavvy@outlook.com>
2023-07-06 09:38:47 -0600

    setup todo comments

--- discovery-provider/plugins/pedalboard/apps/relay/src/index.ts
+++ discovery-provider/plugins/pedalboard/apps/relay/src/index.ts
@@ -4,7 +4,14 @@ import { webServer } from "./server";
 type SharedData = {};
 
 const main = async () => {
-  await new App<SharedData>({}).task(webServer).run();
+  // TODO: read in and validate process.env
+  // TODO: connect to libs/sdk
+
+  await new App<SharedData>({})
+    .tick({ minutes: 5 }, async (app) => {/** TODO: update and cache health check */})
+    .tick({ seconds: 10 }, async (app) => {/** TODO: check health of local node */})
+    .tick({ minutes: 10 }, async (app) => {/** TODO: fund relayer wallets if empty */})
+    .task(webServer).run();
 };
 
 (async () => {
