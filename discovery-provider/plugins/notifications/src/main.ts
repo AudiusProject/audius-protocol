f0815bec86bace65decaa6cb4ef3fa56d7a8ed09
Alec Savvy <alecjsavvy@gmail.com>
2023-07-06 10:26:15 -0600

    rm spammy log (#5556)

--- discovery-provider/plugins/notifications/src/main.ts
+++ discovery-provider/plugins/notifications/src/main.ts
@@ -147,7 +147,6 @@ export class Processor {
         )
         this.lastWeeklyEmailSent = moment.utc()
       }
-      logger.info('Completed processing scheduled emails')
       // free up event loop + batch queries to postgres
       await new Promise((r) => setTimeout(r, config.pollInterval))
     }
