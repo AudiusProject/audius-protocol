4cfc492caa1e7090ca444e962222b61b8ae29581
sliptype <sliptype@gmail.com>
2023-07-13 12:51:30 -0500

    Set bootstrap nodes default for StorageNodeSelector

--- libs/src/sdk/config/staging.ts
+++ libs/src/sdk/config/staging.ts
@@ -5,12 +5,46 @@
 /* eslint-disable prettier/prettier */
 import type { ServicesConfig } from './types'
 export const servicesConfig: ServicesConfig = {
-  "minVersion": "0.3.66",
+  "minVersion": "0.3.83",
   "discoveryNodes": [
     "https://discoveryprovider2.staging.audius.co",
     "https://discoveryprovider3.staging.audius.co",
     "https://discoveryprovider.staging.audius.co",
     "https://discoveryprovider5.staging.audius.co"
+  ],
+  "storageNodes": [
+    {
+      "endpoint": "https://usermetadata.staging.audius.co",
+      "delegateOwnerWallet": "0x671ddce7B4E676C9467F87e4031a917b5D6f75F0"
+    },
+    {
+      "endpoint": "https://creatornode5.staging.audius.co",
+      "delegateOwnerWallet": "0xDC2BDF1F23381CA2eC9e9c70D4FD96CD8645D090"
+    },
+    {
+      "endpoint": "https://creatornode6.staging.audius.co",
+      "delegateOwnerWallet": "0x68039d001D87E7A5E6B06fe0825EA7871C1Cd6C2"
+    },
+    {
+      "endpoint": "https://creatornode7.staging.audius.co",
+      "delegateOwnerWallet": "0x1F8e7aF58086992Ef4c4fc0371446974BBbC0D9F"
+    },
+    {
+      "endpoint": "https://creatornode8.staging.audius.co",
+      "delegateOwnerWallet": "0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"
+    },
+    {
+      "endpoint": "https://creatornode9.staging.audius.co",
+      "delegateOwnerWallet": "0x140eD283b33be2145ed7d9d15f1fE7bF1E0B2Ac3"
+    },
+    {
+      "endpoint": "https://creatornode10.staging.audius.co",
+      "delegateOwnerWallet": "0xf7C96916bd37Ad76D4EEDd6536B81c29706C8056"
+    },
+    {
+      "endpoint": "https://creatornode11.staging.audius.co",
+      "delegateOwnerWallet": "0x4c88d2c0f4c4586b41621aD6e98882ae904B98f6"
+    }
   ],
   "web3ProviderUrl": "https://poa-gateway.staging.audius.co",
   "entityManagerContractAddress": "0x1Cd8a543596D499B9b6E7a6eC15ECd2B7857Fd64",
