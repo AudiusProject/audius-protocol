342453ca8d9d7683663c5cee762a84edc5f2d106
Theo Ilie <theoilie.ti@gmail.com>
2023-07-14 20:37:49 -0700

    Re-seed cid_lookup instead of falling back to every node

--- mediorum/ddl/ddl.go
+++ mediorum/ddl/ddl.go
@@ -6,6 +6,9 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"math/rand"
+	"os"
+	"time"
 
 	_ "embed"
 )
@@ -27,10 +30,26 @@ func Migrate(db *sql.DB) {
 	mustExec(db, mediorumMigrationTable)
 	runMigration(db, cidLookupDDL)
 	runMigration(db, delistStatusesDDL)
-	runMigration(db, `drop table if exists server_healths;`)
 
-	// delete legacy blobs
-	shouldExec(db, `delete from blobs where key not like 'ba%'`)
+	// stagger re-seeding between 1-3hrs. TODO: safe to remove after everyone runs it once
+	min := time.Duration(60)
+	max := time.Duration(180)
+	diff := max.Minutes() - min.Minutes()
+	randomTime := min + time.Duration(rand.Float64()*diff)*time.Minute
+	time.AfterFunc(randomTime, func() {
+		ddl := `begin; truncate mediorum_migrations; drop table cid_lookup; drop table cid_cursor; drop table cid_log; commit;`
+		h := md5string(ddl)
+		var alreadyRan bool
+		db.QueryRow(`select count(*) = 1 from mediorum_migrations where hash = $1`, h).Scan(&alreadyRan)
+		if alreadyRan {
+			fmt.Printf("hash %s exists skipping ddl \n", h)
+			return
+		}
+
+		mustExec(db, `insert into mediorum_migrations values ($1, now()) on conflict do nothing`, h)
+		mustExec(db, ddl)
+		os.Exit(0)
+	})
 }
 
 func runMigration(db *sql.DB, ddl string) {
@@ -52,13 +71,6 @@ func mustExec(db *sql.DB, ddl string, va ...interface{}) {
 	if err != nil {
 		fmt.Println(ddl)
 		log.Fatal(err)
-	}
-}
-
-func shouldExec(db *sql.DB, ddl string, va ...interface{}) {
-	_, err := db.Exec(ddl, va...)
-	if err != nil {
-		fmt.Println("ddl shouldExec failed", ddl, err)
 	}
 }
 
