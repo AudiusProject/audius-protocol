635acf7dc108b819f6d86cc875460e03641fe82a
Michelle Brier <michelle.brier4@gmail.com>
2023-07-17 13:08:45 -0700

    remove index, rename migration after rebase

--- discovery-provider/ddl/migrations/0013_add_track_preview_cols.sql
+++ discovery-provider/ddl/migrations/0013_add_track_preview_cols.sql
@@ -5,6 +5,4 @@ add column if not exists preview_cid character varying,
 add column if not exists audio_upload_id character varying,
 add column if not exists preview_start_seconds float;
 
-create index tracks_preview_cid_idx on public.tracks using btree (preview_cid, is_current, is_delete);
-
 commit;
