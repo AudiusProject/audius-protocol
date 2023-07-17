28e993353fb9c4e717c46508c90d9b248328b3a7
Isaac Solo <isaac@audius.co>
2023-07-17 15:01:50 -0700

    create em_logs table

--- discovery-provider/ddl/migrations/0014_add_em_logs.sql
+++ discovery-provider/ddl/migrations/0014_add_em_logs.sql
@@ -5,7 +5,8 @@ create table if not exists em_logs (
     entity_id integer not null,
     blocknumber integer not null references blocks(number),
     txhash text not null,
-    prev_record jsonb not null
+    prev_record jsonb not null,
+    PRIMARY KEY(txhash)
 );
 
 commit;
