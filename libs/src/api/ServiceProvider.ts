2a24dbd9b8199316cf33b8c982c884fc793725b1
Theo Ilie <theoilie.ti@gmail.com>
2023-07-19 13:42:47 -0700

    Remove usage of (legacy-only) /health_check/verbose

--- libs/src/api/ServiceProvider.ts
+++ libs/src/api/ServiceProvider.ts
@@ -1,40 +1,12 @@
 import { sampleSize } from 'lodash'
 
 import { Base, BaseConstructorArgs } from './base'
-import { timeRequests } from '../utils/network'
 import { getNStorageNodes } from '../utils/getNStorageNodes'
-import { CreatorNodeSelection } from '../services/creatorNode'
 
-import type { Nullable, ServiceWithEndpoint } from '../utils'
+import type { ServiceWithEndpoint } from '../utils'
 
 const CONTENT_NODE_SERVICE_NAME = 'content-node'
 const DISCOVERY_NODE_SERVICE_NAME = 'discovery-node'
-
-// Default timeout for each content node's sync and health check
-const CONTENT_NODE_DEFAULT_SELECTION_TIMEOUT = 7500
-// Default time at which responses are considered equal weighting.
-// Content nodes that reply within 200ms of eachother are given equal footing
-// in selection
-const CONTENT_NODE_SELECTION_EQUIVALENCY_DELTA = 200
-
-type AutoSelectCreatorNodesConfig = {
-  // total number of nodes to fetch (2 secondaries means 3 total)
-  numberOfNodes?: number
-  // whether or not to include only specified nodes (default no whitelist)
-  whitelist?: Nullable<Set<string>>
-  // whether or not to exclude any nodes (default no blacklist)
-  blacklist?: Nullable<Set<string>>
-  // whether or not to perform sync check
-  performSyncCheck?: boolean
-  // ms applied to each request made to a content node
-  timeout?: number
-  equivalencyDelta?: number
-  preferHigherPatchForPrimary?: boolean
-  preferHigherPatchForSecondaries?: boolean
-  log?: boolean
-  // override going to chain to generate a list of all service endpoints
-  getServices?: () => Promise<string[]>
-}
 
 /**
  * API methods to interact with Audius service providers.
@@ -64,85 +36,6 @@ export class ServiceProvider extends Base {
     return await this.ethContracts.ServiceProviderFactoryClient.getServiceProviderList(
       CONTENT_NODE_SERVICE_NAME
     )
-  }
-
-  /**
-   * Fetches healthy Content Nodes filtered down to a given whitelist and blacklist
-   */
-  async getSelectableCreatorNodes(
-    whitelist: Nullable<Set<string>> = null, // whether or not to include only specified nodes (default no whiltelist)
-    blacklist: Nullable<Set<string>> = null, // whether or not to exclude any nodes (default no blacklist)
-    timeout = CONTENT_NODE_DEFAULT_SELECTION_TIMEOUT
-  ) {
-    let creatorNodes = await this.listCreatorNodes()
-
-    // Filter whitelist
-    if (whitelist) {
-      creatorNodes = creatorNodes.filter((node) => whitelist.has(node.endpoint))
-    }
-    // Filter blacklist
-    if (blacklist) {
-      creatorNodes = creatorNodes.filter(
-        (node) => !blacklist.has(node.endpoint)
-      )
-    }
-
-    // Time requests and get version info
-    const timings = await timeRequests({
-      requests: creatorNodes.map((node) => ({
-        id: node.endpoint,
-        url: `${node.endpoint}/health_check/verbose`
-      })),
-      sortByVersion: true,
-      timeout,
-      headers: {
-        'User-Agent':
-          'Axios - @audius/sdk - ServiceProvider.ts#getSelectableCreatorNodes'
-      }
-    })
-
-    const services: { [id: string]: any } = {}
-    timings.forEach((timing) => {
-      if (timing.response && timing.request.id)
-        services[timing.request.id] = timing.response.data.data
-    })
-
-    return services
-  }
-
-  /**
-   * Fetches healthy Content Nodes and autoselects a primary
-   * and two secondaries.
-   */
-  async autoSelectCreatorNodes({
-    numberOfNodes = 3,
-    whitelist = null,
-    blacklist = null,
-    performSyncCheck = true,
-    timeout = CONTENT_NODE_DEFAULT_SELECTION_TIMEOUT,
-    equivalencyDelta = CONTENT_NODE_SELECTION_EQUIVALENCY_DELTA,
-    preferHigherPatchForPrimary = true,
-    preferHigherPatchForSecondaries = true,
-    log = true,
-    getServices
-  }: AutoSelectCreatorNodesConfig) {
-    const creatorNodeSelection = new CreatorNodeSelection({
-      creatorNode: this.creatorNode,
-      ethContracts: this.ethContracts,
-      logger: this.logger,
-      numberOfNodes,
-      whitelist,
-      blacklist,
-      timeout,
-      equivalencyDelta,
-      preferHigherPatchForPrimary,
-      preferHigherPatchForSecondaries,
-      getServices
-    })
-
-    const { primary, secondaries, services } =
-      await creatorNodeSelection.select(performSyncCheck, log)
-    return { primary, secondaries, services }
   }
 
   /**
