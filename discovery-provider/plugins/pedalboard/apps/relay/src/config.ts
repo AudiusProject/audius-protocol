ab33b3b3a5801fda8f2997c1539b4f7045ca5a12
Alec Savvy <alecjsavvy@gmail.com>
2023-07-20 19:18:25 -0600

    As/plat 1136 build relay plugin (#5639)

--- discovery-provider/plugins/pedalboard/apps/relay/src/config.ts
+++ discovery-provider/plugins/pedalboard/apps/relay/src/config.ts
@@ -1,4 +1,6 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"
+import { stagingConfig, productionConfig, developmentConfig } from "@audius/sdk"
+import { logger } from "./logger";
 
 export type Config = {
   environment: string;
@@ -6,17 +8,30 @@ export type Config = {
   entityManagerContractAddress: string;
   entityManagerContractRegistryKey: string;
   requiredConfirmations: number;
+  serverHost: string;
+  serverPort: number;
 };
 
 export const readConfig = (): Config => {
-  dotenv.config();
+  dotenv.config()
+  const entityManagerContractAddress = (): string => {
+    switch (process.env.ENVIRONMENT) {
+      case "prod":
+        return productionConfig.entityManagerContractAddress
+      case "stage":
+        return stagingConfig.entityManagerContractAddress
+      default:
+        return developmentConfig.entityManagerContractAddress
+    }
+  }
+  logger.info(`running on ${process.env.ENVIRONMENT} network`)
   return {
-    environment: process.env.environment || "local",
-    rpcEndpoint: process.env.rpcEndpoint || "http://localhost:8545",
-    entityManagerContractAddress:
-      process.env.entityManagerContractAddress || "",
-    entityManagerContractRegistryKey:
-      process.env.entityManagerContractRegistryKey || "EntityManager",
+    environment: process.env.environment || "dev",
+    rpcEndpoint: process.env.rpcEndpoint || "http://chain:8545",
+    entityManagerContractAddress: entityManagerContractAddress(),
+    entityManagerContractRegistryKey: "EntityManager",
     requiredConfirmations: parseInt(process.env.requiredConfirmations || "1"),
+    serverHost: process.env.serverHost || "0.0.0.0",
+    serverPort: parseInt(process.env.serverPort || "6001")
   };
 };
