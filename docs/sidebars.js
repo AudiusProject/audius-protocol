829b7a1b5497a34abd208eb7939f318af2863df2
Nikki Kang <kangaroo233@gmail.com>
2023-07-24 13:07:19 -0700

    more

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -96,6 +96,7 @@ module.exports = {
             },
             {
               type: "doc",
+              label: "Resolve",
               id: "developers/resolve-api",
             },
             {
