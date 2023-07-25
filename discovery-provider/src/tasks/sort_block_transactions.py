6503ab733d6e90a5a5e6e7447c1c460e3cc329f5
Raymond Jacobson <ray@audius.co>
2023-07-25 01:28:48 -0700

    Fix sort test

--- discovery-provider/src/tasks/sort_block_transactions.py
+++ discovery-provider/src/tasks/sort_block_transactions.py
@@ -11,12 +11,7 @@ def sort_block_transactions(
     # Sort transactions by transactionIndex after we have hit
     # indexing_transaction_index_sort_order_start_block.
     if block.number > indexing_transaction_index_sort_order_start_block:
-        sorted_txs = sorted(
-            transactions,
-            key=lambda entry: entry.get("transactionIndex", 0),
-        )
+        sorted_txs = sorted(transactions, key=lambda entry: entry["transactionIndex"])
     else:
-        sorted_txs = sorted(
-            transactions, key=lambda entry: entry.get("transactionHash", "")
-        )
+        sorted_txs = sorted(transactions, key=lambda entry: entry["transactionHash"])
     return sorted_txs
