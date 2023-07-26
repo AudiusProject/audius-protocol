9cf1dc9ec7cf44bf04f5957670ec53115dbd12e4
sabrina <sabrina@audius.co>
2023-07-26 08:28:19 -0400

    Add album routes to playlist permalinks

--- discovery-provider/es-indexer/src/indexers/PlaylistIndexer.ts
+++ discovery-provider/es-indexer/src/indexers/PlaylistIndexer.ts
@@ -215,7 +215,8 @@ export class PlaylistIndexer extends BaseIndexer<PlaylistDoc> {
     row.repost_count = row.reposted_by.length
     row.save_count = row.saved_by.length
     const slug = row.routes[row.routes.length - 1]
-    row.permalink = `/${row.user.handle}/playlist/${slug}`
+    const collection_type = row.is_album ? 'album' : 'playlist'
+    row.permalink = `/${row.user.handle}/${collection_type}/${slug}`
   }
 
   private async getTracks(trackIds: number[]) {
