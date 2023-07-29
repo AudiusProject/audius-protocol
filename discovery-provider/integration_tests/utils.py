ebb9ff6de81376bcf2267279a78a5bc72f4e7a71
Michael Piazza <michael.piazza.mp@gmail.com>
2023-07-28 21:24:18 -0400

    Tests: collections library

--- discovery-provider/integration_tests/utils.py
+++ discovery-provider/integration_tests/utils.py
@@ -258,6 +258,7 @@ def populate_mock_db(db, entities, block_offset=None):
                 is_current=user_meta.get("is_current", True),
                 handle=user_meta.get("handle", str(i)),
                 handle_lc=user_meta.get("handle", str(i)).lower(),
+                name=user_meta.get("name", str(i)),
                 artist_pick_track_id=user_meta.get("artist_pick_track_id"),
                 wallet=user_meta.get("wallet", str(i)),
                 bio=user_meta.get("bio", str(i)),
