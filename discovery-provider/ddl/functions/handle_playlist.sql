0d17010f791ab26935d206698cdfe7028e75dcf5
Alec Savvy <alecjsavvy@gmail.com>
2023-08-01 15:30:36 -0600

    PLAT-1161: skip push on playlist update and if track already existed (#5714)
    Co-authored-by: Isaac Solo <isaac@audius.co>

--- discovery-provider/ddl/functions/handle_playlist.sql
+++ discovery-provider/ddl/functions/handle_playlist.sql
@@ -85,7 +85,7 @@ begin
                 new.updated_at,
                 'track_added_to_playlist',
                 track_owner_id,
-                'track_added_to_playlist:playlist_id:' || new.playlist_id || ':track_id:' || (track_item->>'track')::int || ':blocknumber:' || new.blocknumber,
+                'track_added_to_playlist:playlist_id:' || new.playlist_id || ':track_id:' || (track_item->>'track')::int,
                 json_build_object('track_id', (track_item->>'track')::int, 'playlist_id', new.playlist_id, 'playlist_owner_id', new.playlist_owner_id)
               )
             on conflict do nothing;
