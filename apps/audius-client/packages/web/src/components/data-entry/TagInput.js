a36d6efdb989edeee8b55f145b42680f0df2eb15
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-08-01 16:09:02 -0500

    Fix tag input (#3832)

--- apps/audius-client/packages/web/src/components/data-entry/TagInput.js
+++ apps/audius-client/packages/web/src/components/data-entry/TagInput.js
@@ -34,11 +34,14 @@ class TagInput extends Component {
 
   newTagInputRef = createRef()
 
-  componentDidUpdate = () => {
+  componentDidUpdate = (prevProps, prevState) => {
     if (this.state.typingMode && this.newTagInputRef.current) {
       this.newTagInputRef.current.focus()
     }
-    if (this.state.tags !== this.props.tags) {
+    if (
+      prevState.tags === this.state.tags &&
+      prevProps.tags !== this.props.tags
+    ) {
       this.setState({ tags: this.props.tags })
     }
   }
