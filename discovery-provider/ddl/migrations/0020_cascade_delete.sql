c930ed1565b087720af3a4897f2065ed06678e49
Isaac Solo <isaac@audius.co>
2023-08-07 14:08:38 -0700

    wip migrations

--- discovery-provider/ddl/migrations/0020_cascade_delete.sql
+++ discovery-provider/ddl/migrations/0020_cascade_delete.sql
@@ -1,87 +1,49 @@
-alter table revert_blocks
-drop constraint revert_blocks_blocknumber_fkey;
-alter table revert_blocks
-add constraint revert_blocks_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-
-alter table follows
-drop constraint follows_blocknumber_fkey;
-alter table follows
-add constraint follows_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from follows where is_current = false;
-
-
-alter table playlists
-drop constraint playlists_blocknumber_fkey;
-alter table playlists
-add constraint playlists_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from playlists where is_current = false;
+-- for all tables with is_current
+-- use a fkey constraint that cascades delete
+-- delete is_current false
+begin;
+CREATE OR REPLACE FUNCTION drop_fk_constraints(_ref_table_name text)
+RETURNS VOID AS
+$$
+DECLARE
+   _constraint_name text;
+   _table_name text;
+BEGIN
+   FOR _constraint_name, _table_name IN
+      SELECT conname, conrelid::regclass::text
+      FROM   pg_constraint
+      WHERE  confrelid = _ref_table_name::regclass
+   LOOP
+      EXECUTE format('ALTER TABLE %s DROP CONSTRAINT IF EXISTS %s', 
+                     quote_ident(_table_name), 
+                     quote_ident(_constraint_name));
+   END LOOP;
+END
+$$ LANGUAGE plpgsql;
+SELECT drop_fk_constraints('blocks');
 
 
-alter table reposts
-drop constraint reposts_blocknumber_fkey;
-alter table reposts
-add constraint reposts_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from reposts where is_current = false;
 
-alter table saves
-drop constraint saves_blocknumber_fkey;
-alter table saves
-add constraint saves_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from saves where is_current = false;
+CREATE OR REPLACE FUNCTION add_fk_constraints_and_delete_rows(_ref_table_name text, _ref_column_name text, _table_names text[])
+RETURNS VOID AS
+$$
+DECLARE
+   _table_name text;
+BEGIN
+   FOREACH _table_name IN ARRAY _table_names
+   LOOP
+      EXECUTE format('ALTER TABLE %s ADD CONSTRAINT %s FOREIGN KEY (%s) REFERENCES %s (%s) ON DELETE CASCADE', 
+                     quote_ident(_table_name), 
+                     quote_ident(_table_name || '_' || _ref_column_name || '_fkey'),
+                     quote_ident(_ref_column_name),
+                     quote_ident(_ref_table_name),
+                     quote_ident(_ref_column_name));
 
+      EXECUTE format('DELETE FROM %s WHERE is_current = false', 
+                     quote_ident(_table_name));
+   END LOOP;
+END
+$$ LANGUAGE plpgsql;
 
-alter table tracks
-drop constraint tracks_blocknumber_fkey;
-alter table tracks
-add constraint tracks_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from tracks where is_current = false;
+SELECT add_fk_constraints_and_delete_rows('blocks', 'blocknumber', ARRAY['app_delegates', 'delegations', 'developer_apps', 'grants', 'users', 'tracks', 'playlists', 'follows', 'saves', 'reposts', 'subscriptions', 'track_price_history']);
 
-
-alter table developer_apps
-drop constraint app_delegates_blocknumber_fkey;
-alter table developer_apps
-add constraint app_delegates_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from developer_apps where is_current = false;
-
-
-alter table grants
-drop constraint delegations_blocknumber_fkey;
-alter table grants
-drop constraint delegations_blockhash_fkey;
-
-alter table grants
-add constraint delegations_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from grants where is_current = false;
-
-alter table users
-drop constraint users_blocknumber_fkey;
-alter table users
-add constraint users_blocknumber_fkey
-foreign key (blocknumber)
-references blocks(number)
-on delete cascade;
-delete from users where is_current = false;
