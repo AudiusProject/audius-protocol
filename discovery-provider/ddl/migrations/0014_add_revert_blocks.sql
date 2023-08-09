25004eac3d9a2fea8392a2d5cce2157383a21140
Isaac Solo <isaac@audius.co>
2023-08-09 16:52:15 -0700

    Add cascading deletes to revert_block fkey (#5769)

--- discovery-provider/ddl/migrations/0014_add_revert_blocks.sql
+++ discovery-provider/ddl/migrations/0014_add_revert_blocks.sql
@@ -1,4 +1,5 @@
-create table if not exists revert_blocks (
-    blocknumber integer not null primary key references blocks(number),
-    prev_records jsonb not null
-);
+create table
+    if not exists revert_blocks (
+        blocknumber integer not null primary key references blocks (number) on delete cascade,
+        prev_records jsonb not null
+    );
