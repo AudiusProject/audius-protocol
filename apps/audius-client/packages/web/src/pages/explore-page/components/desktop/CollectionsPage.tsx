b94e5f6865fca8af3893ac6d5b8298ab2f7330eb
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2023-08-09 09:20:35 -0700

    QA-588 Fix collection card profile link (#3853)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- apps/audius-client/packages/web/src/pages/explore-page/components/desktop/CollectionsPage.tsx
+++ apps/audius-client/packages/web/src/pages/explore-page/components/desktop/CollectionsPage.tsx
@@ -63,7 +63,10 @@ const CollectionsPage = ({
       <ArtistPopover handle={playlist.user.handle}>
         <span
           className={styles.userName}
-          onClick={(e: MouseEvent) => goToProfilePage(e, playlist.user.handle)}
+          onClick={(e: MouseEvent) => {
+            e.preventDefault()
+            goToProfilePage(e, playlist.user.handle)
+          }}
         >
           {playlist.user.name}
         </span>
