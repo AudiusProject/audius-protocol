e368c997bdde7096913c29044e7e708641f1507e
Alec Savvy <alecsavvy@outlook.com>
2023-08-14 06:34:15 -0600

    pull network from web3 obj

--- discovery-provider/plugins/pedalboard/apps/relay/src/txRelay.ts
+++ discovery-provider/plugins/pedalboard/apps/relay/src/txRelay.ts
@@ -32,18 +32,17 @@ export const relayTransaction = async (
   const {
     entityManagerContractAddress,
     entityManagerContractRegistryKey,
-    acdcChainId,
-    requiredConfirmations,
     aao,
   } = config;
   const { encodedABI, contractRegistryKey, gasLimit } = req;
   const { reqIp } = headers;
 
   const discoveryDb = app.getDnDb();
+  const { chainId } = await web3.getNetwork()
   const sender = AudiusABIDecoder.recoverSigner({
     encodedAbi: encodedABI,
     entityManagerAddress: entityManagerContractAddress,
-    chainId: acdcChainId,
+    chainId: chainId.toString(),
   });
   const isBlockedFromRelay = await detectAbuse(aao, discoveryDb, sender, reqIp);
   if (isBlockedFromRelay) {
