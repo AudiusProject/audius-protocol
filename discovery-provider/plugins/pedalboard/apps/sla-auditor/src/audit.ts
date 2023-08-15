aa6e0f2effa1c252f46ff2f5cf519f1779ce3801
Raymond Jacobson <ray@audius.co>
2023-08-15 16:02:34 -0700

    [PLAT-1182] Productionize SLA auditor discovery plugin (#5797)

--- discovery-provider/plugins/pedalboard/apps/sla-auditor/src/audit.ts
+++ discovery-provider/plugins/pedalboard/apps/sla-auditor/src/audit.ts
@@ -1,23 +1,35 @@
 import { auditVersions } from "./audits/version";
+import { SharedData } from "./config";
 import { propose } from "./proposal";
+import App from "basekit/src/app";
 
 export type Node = {
   delegateOwnerWallet: string;
   endpoint: string;
   owner: string;
   spID: number;
-  type: "discovery-node" | "content-node";
+  type: string;
 };
 
 export const audit = async (app: App<SharedData>) => {
   const { libs } = app.viewAppData();
+  const db = app.getDnDb();
+
   const discoveryNodes: Node[] =
-    await libs.ServiceProvider.listDiscoveryProviders();
-  const contentNodes: Node[] = await libs.ServiceProvider.listCreatorNodes();
+    (await libs.ServiceProvider?.listDiscoveryProviders()) ?? [];
+  const contentNodes: Node[] =
+    (await libs.ServiceProvider?.listCreatorNodes()) ?? [];
 
   const nodes = [...discoveryNodes, ...contentNodes];
 
-  const proposals = await auditVersions(nodes);
+  const minVersions: { "discovery-node": string; "content-node": string } =
+    ((await libs.ethContracts?.getExpectedServiceVersions()) ?? {}) as {
+      "discovery-node": string;
+      "content-node": string;
+    };
+
+  const proposals = await auditVersions(db, nodes, minVersions);
+
   for (const proposal of proposals) {
     await propose(app, proposal);
   }
