695a33f01297190fdb6a8d5ae74e405c2f535e30
Saliou Diallo <saliou@audius.co>
2023-08-16 13:10:07 -0400

    Add Anchor tests and docs

---
+++ solana-programs/staking-bridge/README.md
@@ -0,0 +1,67 @@
+# Staking Bridge
+
+This program has 2 main functions:
+1. To swap SOL USDC tokens to SOL AUDIO tokens via the Raydium AMM Program.
+2. To convert SOL AUDIO tokens to ETH AUDIO tokens via the Wormhole Token Bridge.
+
+The program has a PDA (Program Derived Address) account that will own the tokens.
+The program methods are permissionless, meaning that anyone can interact with them as long as they're willing to pay for the transactions.
+
+Please note that this program and its Anchor tests are set up to only work with the Solana mainnet cluster at the moment.
+
+## Building the project
+Make sure you have `anchor` installed. See https://www.anchor-lang.com/docs/installation. If you follow the installation steps, you should also have Rust, Solana, and Yarn installed.
+
+Build the project.
+```
+anchor build
+```
+
+Anchor uses Yarn, so please sure you have it installed, and run
+```
+yarn install
+```
+
+Note that I've been using Node 16 for this project, as I was running into issues with older Node versions for the `@solana/spl-token` and `@solana/web3.js` modules.
+
+## Setting up your Solana cluster and environment
+We are going to be invoking programs on the Solana mainnet.
+So, for convenience, you can configure your cluster to use the Solana mainnet by default, and also use your chosen keypair file as the default Solana account by running:
+```
+./scripts/setupTestEnv.sh <fee-payer-keypair-file>
+```
+For example,
+```
+./scripts/setupTestEnv.sh id.json
+```
+
+The [program](https://explorer.solana.com/address/HEDM7Zg7wNVSCWpV4TF7zp6rgj44C43CXnLtpY68V7bV) is already deployed, and all your transactions should have very low fees, in the order of <0.01 SOL.
+Note that you can make changes to the rust code, run `anchor build` and `anchor test`, and the redeployment will still cost very little because of the program having already been previously deployed.
+You can also run `anchor test --skip-deploy` if your Solana Rust program has not changed and you do not need to redeploy.
+
+## Testing the PDA creation
+Note that the PDA has already been created, so attempting to create it again will fail. Here is [the PDA account](https://explorer.solana.com/address/GwVsdGg5ZjJRzxP1wVhZBDKaS1BgdbV8sVvE4wDE36dU).
+```
+feePayerSecret=$(cat <fee-payer-keypair-file>) anchor run test-create-pda -- --skip-deploy
+```
+
+## Testing the Raydium swap
+As written in the test, this will swap 0.001 USDC tokens to its AUDIO equivalent.
+```
+feePayerSecret=$(cat <fee-payer-keypair-file>) anchor run test-swap -- --skip-deploy
+```
+
+## Testing the Wormhole transfer
+As written in the test, this will send 0.001 AUDIO tokens to the wormhole for a given recipient to subsequently redeem.
+
+```
+feePayerSecret=$(cat <fee-payer-keypair-file>) anchor run test-wormhole -- --skip-deploy
+```
+
+## Redeeming the Wormhole tokens
+There is a `scripts/redeemWormholeTokens.ts` you can use to redeem your AUDIO tokens that are in the Wormhole. You will need an eth provider url via which the transaction is sent, an eth private key to sign the transaction, and your Solana transaction ID which executed the transfer of your tokens to the Wormhole, like so:
+```
+ethProviderUrl=<eth-provider-url> ethPrivateKey=<eth-private-key> node scripts/redeemWormholeTokens.ts <transaction-id>
+```
+
+If you get an error redeeming the tokens, please try again a bit later.
--- solana-programs/staking-bridge/tests/staking-bridge.ts
+++ solana-programs/staking-bridge/tests/staking-bridge.ts
@@ -1,16 +1,293 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { StakingBridge } from "../target/types/staking_bridge";
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { StakingBridge } from '../target/types/staking_bridge'
+import { CHAIN_ID_ETH } from '@certusone/wormhole-sdk'
+import {
+  TOKEN_PROGRAM_ID,
+  getOrCreateAssociatedTokenAccount,
+} from '@solana/spl-token'
+import {
+  ETH_AUDIO_TOKEN_ADDRESS,
+  SOL_AUDIO_DECIMALS,
+  SOL_AUDIO_TOKEN_ADDRESS,
+  SOL_USDC_TOKEN_ADDRESS,
+  ammProgram,
+  bridgeId,
+  serumDexProgram,
+  serumMarketPublicKey,
+  tokenBridgeId
+} from './constants'
+import { getAssociatedPoolKeys, getMarket, getVaultOwnerAndNonce } from './raydiumTestUtils'
+import { formatEthAddress, getMinimumAmountOutFromAmountIn, getPostMessageData } from './wormholeTestUtils'
 
-describe("staking-bridge", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  SYSVAR_CLOCK_PUBKEY,
+  SYSVAR_RENT_PUBKEY,
+} = anchor.web3
+
+// Read in fee payer from the environment variable
+const FEE_PAYER_SECRET = process.env.feePayerSecret
+const feePayerKeypair = Keypair.fromSecretKey(
+  Uint8Array.from(JSON.parse(FEE_PAYER_SECRET))
+)
+const feePayerPublicKey = feePayerKeypair.publicKey
+
+const SOL_USDC_TOKEN_ADDRESS_KEY = new PublicKey(SOL_USDC_TOKEN_ADDRESS)
+const SOL_AUDIO_TOKEN_ADDRESS_KEY = new PublicKey(SOL_AUDIO_TOKEN_ADDRESS)
+
+const endpoint = 'https://api.mainnet-beta.solana.com'
+const connection = new Connection(endpoint, 'confirmed');
+
+async function signTransaction(transaction) {
+  transaction.partialSign(feePayerKeypair)
+  return transaction
+}
+async function signAllTransactions(transactions: any[]) {
+  transactions.forEach((transaction) => {
+    transaction.partialSign(feePayerKeypair)
+  })
+  return transactions
+}
+
+describe('staking-bridge', () => {
+  const wallet = {
+    publicKey: feePayerPublicKey,
+    signTransaction,
+    signAllTransactions
+  }
+  const provider = new anchor.AnchorProvider(connection, wallet, { skipPreflight: true });
+  anchor.setProvider(provider)
 
   const program = anchor.workspace.StakingBridge as Program<StakingBridge>;
 
-  it("Is initialized!", async () => {
+  const [stakingBridgePda, stakingBridgePdaBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from('staking_bridge')],
+    program.programId
+  )
+
+  it('creates the staking bridge pda', async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .createPda()
+      .accounts({
+        stakingBridgePda,
+        payer: feePayerPublicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+    console.log('Your transaction signature', tx);
+  })
+
+  it('swaps SOL USDC to SOL AUDIO', async () => {
+    const market = await getMarket(connection, serumMarketPublicKey.toString(), serumDexProgram.toString())
+    console.log("serum market info:", JSON.stringify(market))
+
+    const poolKeys = await getAssociatedPoolKeys({
+      programId: ammProgram,
+      serumProgramId: serumDexProgram,
+      marketId: market.address,
+      baseMint: market.baseMint,
+      quoteMint: market.quoteMint
+    })
+    console.log("amm poolKeys: ", JSON.stringify(poolKeys))
+
+    const { vaultOwner, vaultNonce } = await getVaultOwnerAndNonce(serumMarketPublicKey, serumDexProgram)
+    if (vaultNonce.toNumber() != market.vaultSignerNonce) {
+      console.log(
+        "withdraw vaultOwner:",
+        vaultOwner.toString(),
+        "vaultNonce: ",
+        vaultNonce.toNumber(),
+        "market.vaultSignerNonce:",
+        market.vaultSignerNonce.toString()
+      )
+      throw ("vaultSignerNonce incorrect!")
+    }
+
+    // Get associated token accounts for the staking bridge PDA.
+    // Create them if they don't exist.
+    const usdcTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      feePayerKeypair,
+      SOL_USDC_TOKEN_ADDRESS_KEY,
+      stakingBridgePda,
+      true // allowOwnerOffCurve: we need this since the owner is a program
+    )
+    const audioTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      feePayerKeypair,
+      SOL_AUDIO_TOKEN_ADDRESS_KEY,
+      stakingBridgePda,
+      true // allowOwnerOffCurve: we need this since the owner is a program
+    )
+
+    // Amount of SOL USDC to be swapped for a minimum amount of SOL AUDIO expected to be received from the swap.
+    const wholeAmountIn = 0.001
+    const { amountIn, minimumAmountOut } = getMinimumAmountOutFromAmountIn(wholeAmountIn);
+
+    const accounts = {
+      programId: poolKeys.programId,
+      amm: poolKeys.id,
+      ammAuthority: poolKeys.authority,
+      ammOpenOrders: poolKeys.openOrders,
+      ammTargetOrders: poolKeys.targetOrders,
+      poolCoinTokenAccount: poolKeys.baseVault,
+      poolPcTokenAccount: poolKeys.quoteVault,
+      serumProgram: serumDexProgram,
+      serumMarket: serumMarketPublicKey,
+      serumBids: market.bids,
+      serumAsks: market.asks,
+      serumEventQueue: market.eventQueue,
+      serumCoinVaultAccount: market.baseVault,
+      serumPcVaultAccount: market.quoteVault,
+      serumVaultSigner: vaultOwner,
+      userSourceTokenAccount: usdcTokenAccount.address,
+      userDestinationTokenAccount: audioTokenAccount.address,
+      userSourceOwner: stakingBridgePda,
+      splTokenProgram: TOKEN_PROGRAM_ID,
+    }
+    console.log('Raydium swap accounts:', { accounts })
+
+    // Add your test here.
+    const tx = await program.methods
+      .raydiumSwap(
+        amountIn,
+        minimumAmountOut,
+        vaultNonce,
+        stakingBridgePdaBump,
+      )
+      .accounts(accounts)
+      .rpc();
     console.log("Your transaction signature", tx);
-  });
-});
+  })
+
+  it('posts the wormhole token bridge transfer message', async () => {
+    const messageKeypair = Keypair.generate()
+    const messagePublicKey = messageKeypair.publicKey
+
+    // How many SOL AUDIO tokens to convert into ETH AUDIO tokens
+    const wholeAmount = 0.001
+    const {
+      nonce,
+      amount,
+      fee
+    } = getPostMessageData({
+      wholeAmount,
+      solTokenDecimals: SOL_AUDIO_DECIMALS
+    })
+
+    // Associated token account owned by the PDA
+    const pdaAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      feePayerKeypair,
+      SOL_AUDIO_TOKEN_ADDRESS_KEY,
+      stakingBridgePda,
+      true // allowOwnerOffCurve: we need this since the owner is a program
+    )
+    const pdaAtaKey = pdaAta.address
+    console.log({ pdaAtaKey })
+
+    // PDAs
+    const [config, configBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from('config')],
+      tokenBridgeId
+    )
+    const [wrappedMint, wrappedMintBump] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from('wrapped'),
+        new anchor.BN(CHAIN_ID_ETH).toArrayLike(Buffer, 'be', 2),
+        Buffer.from(formatEthAddress(ETH_AUDIO_TOKEN_ADDRESS), 'hex')
+      ],
+      tokenBridgeId
+    )
+    const [wrappedMeta, wrappedMetaBump] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from('meta'),
+        wrappedMint.toBuffer()
+      ],
+      tokenBridgeId
+    )
+    const [authoritySigner, authoritySignerBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from('authority_signer')],
+      tokenBridgeId
+    )
+    const [bridgeConfig, bridgeConfigBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from('Bridge')],
+      bridgeId
+    )
+    const [emitter, emitterBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from('emitter')],
+      tokenBridgeId
+    )
+    const [sequence, sequenceBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from('Sequence'), emitter.toBuffer()],
+      bridgeId
+    )
+    const [feeCollector, feeCollectorBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from('fee_collector')],
+      bridgeId
+    )
+
+    // Bumps array to pass into instruction
+    const bumps = [
+      configBump,
+      wrappedMintBump,
+      wrappedMetaBump,
+      authoritySignerBump,
+      bridgeConfigBump,
+      emitterBump,
+      sequenceBump,
+      feeCollectorBump,
+      stakingBridgePdaBump
+    ]
+
+    // Accounts
+    const accounts = {
+      programId: tokenBridgeId,
+      bridgeId,
+      payer: feePayerPublicKey,
+      message: messagePublicKey,
+      from: pdaAtaKey,
+      fromOwner: stakingBridgePda,
+
+      // bridge PDAs
+      config,
+      wrappedMint,
+      wrappedMeta,
+      authoritySigner,
+      bridgeConfig,
+      emitter,
+      sequence,
+      feeCollector,
+
+      // system vars
+      clock: SYSVAR_CLOCK_PUBKEY,
+      rent: SYSVAR_RENT_PUBKEY,
+      splToken: new PublicKey(TOKEN_PROGRAM_ID),
+      systemProgram: SystemProgram.programId,
+    }
+    console.log('Wormhole transfer accounts:', { accounts })
+
+    // Signers
+    // fromOwner is also a signer but the program will use
+    // invoke_signed() internally to sign it with the PDA
+    const signers = [feePayerKeypair, messageKeypair]
+
+    // Send the transaction
+    const tx = await program.methods
+      .postWormholeMessage(
+        nonce,
+        amount,
+        fee,
+        ...bumps
+      )
+      .accounts(accounts)
+      .signers(signers)
+      .rpc();
+    console.log('Your transaction signature', tx);
+  })
+})
