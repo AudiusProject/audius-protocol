1e376fec34a41de33ace0d4a7366b31a3fba3ae3
Kyle Shanks <kyle.j.shanks@gmail.com>
2023-08-21 16:46:58 -0400

    [C-2911] Update new select page of the upload flow (#3910)

--- packages/web/src/pages/upload-page/components/SelectPageNew.tsx
+++ packages/web/src/pages/upload-page/components/SelectPageNew.tsx
@@ -2,16 +2,11 @@ import { useCallback, useState } from 'react'
 
 import {
   Nullable,
-  playerActions,
-  playerSelectors,
   UploadType,
   removeNullable,
   newCollectionMetadata
 } from '@audius/common'
-import { Button, ButtonType, IconArrow } from '@audius/stems'
 import cn from 'classnames'
-import { useDispatch, useSelector } from 'react-redux'
-import { useUnmount } from 'react-use'
 
 import { Dropzone } from 'components/upload/Dropzone'
 import InvalidFileType from 'components/upload/InvalidFileType'
@@ -20,10 +15,7 @@ import { processFiles } from '../store/utils/processFiles'
 import { UploadFormState } from '../types'
 
 import styles from './SelectPage.module.css'
-import TracksPreview from './TracksPreview'
-const { pause } = playerActions
-
-const { getPlaying } = playerSelectors
+import { TracksPreviewNew } from './TracksPreviewNew'
 
 type ErrorType = { reason: 'size' | 'type' } | null
 
@@ -34,8 +26,6 @@ type SelectPageProps = {
 
 export const SelectPageNew = (props: SelectPageProps) => {
   const { formState, onContinue } = props
-  const dispatch = useDispatch()
-  const playing = useSelector(getPlaying)
 
   const [tracks, setTracks] = useState(formState.tracks ?? [])
   const [uploadType, setUploadType] = useState(
@@ -43,8 +33,6 @@ export const SelectPageNew = (props: SelectPageProps) => {
   )
   const [uploadTrackError, setUploadTrackError] =
     useState<Nullable<ErrorType>>(null)
-  const [previewIndex, setPreviewIndex] = useState(-1)
-  const [preview, setPreview] = useState<Nullable<HTMLAudioElement>>()
 
   const onSelectTracks = useCallback(
     async (selectedFiles: File[]) => {
@@ -92,35 +80,6 @@ export const SelectPageNew = (props: SelectPageProps) => {
     [setTracks, setUploadType, tracks, uploadType]
   )
 
-  const stopPreview = useCallback(() => {
-    if (preview) {
-      preview.pause()
-      preview.currentTime = 0
-    }
-    setPreview(null)
-    setPreviewIndex(-1)
-  }, [preview])
-
-  const playPreview = useCallback(
-    (index: number) => {
-      // Stop existing music if some is playing.
-      if (playing) {
-        dispatch(pause())
-      }
-
-      if (preview) stopPreview()
-      const audio = tracks[index].preview
-      audio.play()
-      setPreview(audio)
-      setPreviewIndex(index)
-    },
-    [dispatch, playing, preview, stopPreview, tracks]
-  )
-
-  useUnmount(() => {
-    stopPreview()
-  })
-
   const handleContinue = useCallback(() => {
     if (uploadType !== undefined && tracks) {
       switch (uploadType) {
@@ -138,7 +97,7 @@ export const SelectPageNew = (props: SelectPageProps) => {
 
   const textAboveIcon = tracks.length > 0 ? 'More to Upload?' : undefined
   return (
-    <div className={cn(styles.page)}>
+    <div>
       <div className={styles.select}>
         <div className={styles.dropzone}>
           <Dropzone
@@ -159,33 +118,13 @@ export const SelectPageNew = (props: SelectPageProps) => {
           })}
         >
           {tracks.length > 0 ? (
-            <div>
-              <TracksPreview
-                tracks={tracks}
-                uploadType={uploadType}
-                previewIndex={previewIndex}
-                onRemove={onRemoveTrack}
-                setUploadType={setUploadType}
-                playPreview={playPreview}
-                stopPreview={stopPreview}
-              />
-              <div className={styles.count}>
-                {tracks.length === 1
-                  ? `${tracks.length} track uploaded`
-                  : `${tracks.length} tracks uploaded`}
-              </div>
-              <div className={styles.continue}>
-                <Button
-                  type={ButtonType.PRIMARY_ALT}
-                  text='Continue'
-                  name='continue'
-                  rightIcon={<IconArrow />}
-                  onClick={handleContinue}
-                  textClassName={styles.continueButtonText}
-                  className={styles.continueButton}
-                />
-              </div>
-            </div>
+            <TracksPreviewNew
+              tracks={tracks}
+              uploadType={uploadType}
+              onRemove={onRemoveTrack}
+              setUploadType={setUploadType}
+              onContinue={handleContinue}
+            />
           ) : null}
         </div>
       </div>
