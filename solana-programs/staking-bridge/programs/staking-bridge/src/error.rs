512b08821a6a1643a2b2082294a6f36d0cada91b
Saliou Diallo <saliou@audius.co>
2023-08-23 14:20:39 -0400

    Remove unused error codes

--- solana-programs/staking-bridge/programs/staking-bridge/src/error.rs
+++ solana-programs/staking-bridge/programs/staking-bridge/src/error.rs
@@ -6,18 +6,8 @@ pub enum StakingBridgeErrorCode {
     NotCallingRaydiumAmmProgram,
     #[msg("Invalid serum DEX program.")]
     InvalidSerumDexProgram,
-    #[msg("Invalid source token mint.")]
-    InvalidSourceTokenMint,
-    #[msg("Invalid destination token mint.")]
-    InvalidDestinationTokenMint,
-    #[msg("Source token account not owned by PDA.")]
-    SourceTokenAccountNotOwnedByPDA,
-    #[msg("Destination token account not owned by PDA.")]
-    DestinationTokenAccountNotOwnedByPDA,
     #[msg("Not calling Wormhole Token Bridge program.")]
     NotCallingWormholeTokenBridgeProgram,
     #[msg("Invalid Wormhole Core Bridge program.")]
     InvalidWormholeCoreBridgeProgram,
-    #[msg("Wormhole token account not owned by PDA.")]
-    WormholeTokenAccountNotOwnedByPDA,
 }
