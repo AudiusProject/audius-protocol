fd05827defb0eebe5b28614a97da25d8c7ad8bb1
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-08-28 15:48:58 -0700

    Purchases API should return encoded ids

--- discovery-provider/src/api/v1/users.py
+++ discovery-provider/src/api/v1/users.py
@@ -16,6 +16,7 @@ from src.api.v1.helpers import (
     extend_activity,
     extend_challenge_response,
     extend_favorite,
+    extend_purchase,
     extend_supporter,
     extend_supporting,
     extend_track,
@@ -2153,7 +2154,7 @@ class FullPurchases(Resource):
             "sort_direction": args.get("sort_direction", None),
         }
         purchases = get_usdc_purchases(args)
-        return success_response(purchases)
+        return success_response(list(map(extend_purchase, purchases)))
 
 
 @full_ns.route("/<string:id>/purchases/count")
@@ -2205,7 +2206,7 @@ class FullSales(Resource):
             "sort_direction": args.get("sort_direction", None),
         }
         purchases = get_usdc_purchases(args)
-        return success_response(purchases)
+        return success_response(list(map(extend_purchase, purchases)))
 
 
 @full_ns.route("/<string:id>/sales/count")
