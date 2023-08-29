4b40ebf5cee75325989d4158000e606ece87ca82
Steve Perkins <stereosteve@users.noreply.github.com>
2023-08-29 12:43:31 -0400

    mediorum: prune old ops + add SkipBroadcast crudr option (#5868)

--- mediorum/server/replicate.go
+++ mediorum/server/replicate.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"mediorum/cidutil"
+	"mediorum/crudr"
 	"mediorum/server/signature"
 	"mime/multipart"
 	"net/http"
@@ -75,7 +76,7 @@ func (ss *MediorumServer) replicateToMyBucket(fileName string, file io.Reader) e
 			Host:      ss.Config.Self.Host,
 			Key:       fileName,
 			CreatedAt: time.Now().UTC(),
-		})
+		}, crudr.WithSkipBroadcast())
 	}
 
 	return nil
@@ -97,7 +98,7 @@ func (ss *MediorumServer) dropFromMyBucket(fileName string) error {
 	found := ss.crud.DB.Where("host = ? AND key = ?", ss.Config.Self.Host, fileName).First(&existingBlob)
 	if found.Error == nil {
 		logger.Info("deleting blob record")
-		return ss.crud.Delete(existingBlob)
+		return ss.crud.Delete(existingBlob, crudr.WithSkipBroadcast())
 	}
 
 	return nil
