4a5dd3a39707c6fbed5df8a968de03b62d55faaf
Sebastian Klingler <sliptype@gmail.com>
2023-08-30 11:01:51 -0500

    Improve sentry-cli usage in CI (#3972)

--- apps/audius-client/packages/mobile/ios/scripts/bundleReactNative.sh
+++ apps/audius-client/packages/mobile/ios/scripts/bundleReactNative.sh
@@ -3,5 +3,4 @@ export SENTRY_PROPERTIES=sentry.properties
 export EXTRA_PACKAGER_ARGS=\"--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map\" 
 export PROJECT_ROOT=$PWD/.. 
  
-cp ../node_modules/@sentry/cli/bin/sentry-cli .
 sentry-cli react-native xcode ../../../node_modules/react-native/scripts/react-native-xcode.sh 
