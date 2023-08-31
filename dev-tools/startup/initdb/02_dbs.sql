3805264c46a316a2e3bdb408b7e40061b35ac8e6
Steve Perkins <steve@audius.co>
2023-08-31 15:53:08 +0000

    remove disused docker-compose.comms.yml

--- dev-tools/startup/initdb/02_dbs.sql
+++ dev-tools/startup/initdb/02_dbs.sql
@@ -1,11 +1,6 @@
 CREATE DATABASE discovery_provider;
 CREATE DATABASE discovery_provider_1;
 
--- create database com1 WITH TEMPLATE audius_discovery;
--- create database com2 WITH TEMPLATE audius_discovery;
--- create database com3 WITH TEMPLATE audius_discovery;
--- create database com4 WITH TEMPLATE audius_discovery;
-
 CREATE DATABASE creator_node_1;
 CREATE DATABASE creator_node_2;
 CREATE DATABASE creator_node_3;
