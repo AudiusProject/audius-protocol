12c28ca60e33d6d058c644a3ab74d718ac3b1f3a
Dharit Tantiviramanond <dharit.tan@gmail.com>
2023-08-31 00:26:53 -0400

    kick2

--- libs/src/services/solana/transactionHandler.ts
+++ libs/src/services/solana/transactionHandler.ts
@@ -195,6 +195,7 @@ export class TransactionHandler {
       (await this.connection.getLatestBlockhash('confirmed')).blockhash
 
     // Construct the txn
+
     const tx = new Transaction({ recentBlockhash })
     instructions.forEach((i) => tx.add(i))
     tx.feePayer = feePayerAccount.publicKey
