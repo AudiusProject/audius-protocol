276e33d4f64b19058263d674efb8c69c5baeef3c
Sebastian Klingler <sliptype@gmail.com>
2023-08-31 18:06:10 -0500

    Fix CI (partially) (#3997)

--- apps/audius-client/scripts/postinstall.sh
+++ apps/audius-client/scripts/postinstall.sh
@@ -1,11 +1,15 @@
 npm run patch-package
+
 npm run build
 npm run install-hooks
-# Symlink react-native into the mobile package bc npm doesn't
-# support nohoist
-cd packages/mobile/node_modules
-ln -s ../../../node_modules/react-native react-native
-cd ../ios
-if command -v pod >/dev/null; then
-  pod install
+
+if [[ -z "${SKIP_POD_INSTALL}" ]]; then
+  # Symlink react-native into the mobile package bc npm doesn't
+  # support nohoist
+  cd packages/mobile/node_modules
+  ln -s ../../../node_modules/react-native react-native
+  cd ../ios
+  if command -v pod >/dev/null; then
+    pod install
+  fi
 fi
