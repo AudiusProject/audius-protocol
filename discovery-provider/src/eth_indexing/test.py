c5fda92c03dd121171067ae0c6cf2a626b20ed72
Isaac Solo <isaac@audius.co>
2023-08-31 11:58:45 -0700

    Log celery worker pid

---
+++ discovery-provider/src/eth_indexing/test.py
@@ -0,0 +1,93 @@
+import AttributeDict
+import HexBytes
+
+events = [
+    AttributeDict(
+        {
+            "args": AttributeDict(
+                {
+                    "from": "0x2Fd6391CD144cEE7173b84aDbe37780e76643F3b",
+                    "to": "0x3208684f96458c540Eb08F6F01b9e9afb2b7d4f0",
+                    "value": 3109006616890000000000,
+                }
+            ),
+            "event": "Transfer",
+            "logIndex": 0,
+            "transactionIndex": 0,
+            "transactionHash": HexBytes(
+                "0x893e8c88844a62d8351d541175b6580a30f63e11418956a11116079706e14d94"
+            ),
+            "address": "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998",
+            "blockHash": HexBytes(
+                "0x9cacd8697180f7e75af4a483d7b28c42e2068337382ad56cdb2a34dfad0b446f"
+            ),
+            "blockNumber": 17943308,
+        }
+    ),
+    AttributeDict(
+        {
+            "args": AttributeDict(
+                {
+                    "from": "0x3208684f96458c540Eb08F6F01b9e9afb2b7d4f0",
+                    "to": "0x22ba2662ccf6A0191675bed265A0F295669a0D1e",
+                    "value": 62180132337800000000,
+                }
+            ),
+            "event": "Transfer",
+            "logIndex": 2,
+            "transactionIndex": 0,
+            "transactionHash": HexBytes(
+                "0x893e8c88844a62d8351d541175b6580a30f63e11418956a11116079706e14d94"
+            ),
+            "address": "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998",
+            "blockHash": HexBytes(
+                "0x9cacd8697180f7e75af4a483d7b28c42e2068337382ad56cdb2a34dfad0b446f"
+            ),
+            "blockNumber": 17943308,
+        }
+    ),
+    AttributeDict(
+        {
+            "args": AttributeDict(
+                {
+                    "from": "0x3208684f96458c540Eb08F6F01b9e9afb2b7d4f0",
+                    "to": "0xC730EF0f4973DA9cC0aB8Ab291890D3e77f58F79",
+                    "value": 3046826484552200000000,
+                }
+            ),
+            "event": "Transfer",
+            "logIndex": 3,
+            "transactionIndex": 0,
+            "transactionHash": HexBytes(
+                "0x893e8c88844a62d8351d541175b6580a30f63e11418956a11116079706e14d94"
+            ),
+            "address": "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998",
+            "blockHash": HexBytes(
+                "0x9cacd8697180f7e75af4a483d7b28c42e2068337382ad56cdb2a34dfad0b446f"
+            ),
+            "blockNumber": 17943308,
+        }
+    ),
+    AttributeDict(
+        {
+            "args": AttributeDict(
+                {
+                    "from": "0x21a31Ee1afC51d94C2eFcCAa2092aD1028285549",
+                    "to": "0x3Aff86656A65F3d81B3E0B4C4F8d4199f3B3Fbde",
+                    "value": 26109999999990000000000,
+                }
+            ),
+            "event": "Transfer",
+            "logIndex": 322,
+            "transactionIndex": 89,
+            "transactionHash": HexBytes(
+                "0x8b31365e4cfb944a3af87d534f57b3d2655bf80de74baec74fce13f9f4c062e2"
+            ),
+            "address": "0x18aAA7115705e8be94bfFEBDE57Af9BFc265B998",
+            "blockHash": HexBytes(
+                "0xfcf6c4a219c4baf3059b8c1c0215e3bdabcd9fb5c785fe793adc096a83d29246"
+            ),
+            "blockNumber": 17943319,
+        }
+    ),
+]
