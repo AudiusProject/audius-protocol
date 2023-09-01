65794ebb3fb45442e82f11a48c9bdcd81b40ffee
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-09-01 19:42:11 -0400

    Add checks for AMM and serum market addresses in staking-bridge (#5899)
    Co-authored-by: Raymond Jacobson <ray@audius.co>

--- solana-programs/staking-bridge/programs/staking-bridge/src/error.rs
+++ solana-programs/staking-bridge/programs/staking-bridge/src/error.rs
@@ -6,6 +6,10 @@ pub enum StakingBridgeErrorCode {
     NotCallingRaydiumAmmProgram,
     #[msg("Invalid serum DEX program.")]
     InvalidSerumDexProgram,
+    #[msg("Invalid USDC-$AUDIO AMM program")]
+    InvalidAmmProgram,
+    #[msg("Invalid serum market program.")]
+    InvalidSerumMarketProgram,
     #[msg("Not calling Wormhole Token Bridge program.")]
     NotCallingWormholeTokenBridgeProgram,
     #[msg("Invalid Wormhole Core Bridge program.")]
--- solana-programs/staking-bridge/programs/staking-bridge/tests/raydium_tests.rs
+++ solana-programs/staking-bridge/programs/staking-bridge/tests/raydium_tests.rs
@@ -7,6 +7,8 @@ use staking_bridge::raydium::check_swap_programs;
 use staking_bridge::constant::{
     RAYDIUM_AMM_PROGRAM_ADDRESS,
     SERUM_DEX_PROGRAM_ADDRESS,
+    AUDIO_USDC_RAYDIUM_AMM_ADDRESS,
+    AUDIO_USDC_SERUM_MARKET_ADDRESS
 };
 
 mod utils;
@@ -19,10 +21,14 @@ use utils::{
 fn test_swap_programs() {
     let raydium_key = &RAYDIUM_AMM_PROGRAM_ADDRESS.parse::<Pubkey>().unwrap();
     let serum_key = &SERUM_DEX_PROGRAM_ADDRESS.parse::<Pubkey>().unwrap();
+    let amm_key = &AUDIO_USDC_RAYDIUM_AMM_ADDRESS.parse::<Pubkey>().unwrap();
+    let market_key = &AUDIO_USDC_SERUM_MARKET_ADDRESS.parse::<Pubkey>().unwrap();
     let other_key = &Pubkey::new_unique();
 
     let (raydium_lamports, raydium_data) = (&mut 0, &mut vec![]);
     let (serum_lamports, serum_data) = (&mut 0, &mut vec![]);
+    let (amm_lamports, amm_data) = (&mut 0, &mut vec![]);
+    let (market_lamports, market_data) = (&mut 0, &mut vec![]);
     let (bad_lamports, bad_data) = (&mut 0, &mut vec![]);
     let raydium_program = &make_readonly_account_info(
         raydium_key,
@@ -36,6 +42,18 @@ fn test_swap_programs() {
         serum_data,
         other_key
     );
+    let amm_program = &make_readonly_account_info(
+        amm_key,
+        amm_lamports,
+        amm_data,
+        other_key
+    );
+    let market_program = &make_readonly_account_info(
+        market_key,
+        market_lamports,
+        market_data,
+        other_key
+    );
     let bad_account_info = &make_readonly_account_info(
         other_key,
         bad_lamports,
@@ -46,18 +64,40 @@ fn test_swap_programs() {
     let bad_raydium_program_result = check_swap_programs(
       bad_account_info.clone(),
       serum_program.clone(),
+      amm_program.clone(),
+      market_program.clone(),
     );
     assert_error(bad_raydium_program_result, StakingBridgeErrorCode::NotCallingRaydiumAmmProgram);
 
     let bad_serum_program_result = check_swap_programs(
       raydium_program.clone(),
       bad_account_info.clone(),
+      amm_program.clone(),
+      market_program.clone(),
     );
     assert_error(bad_serum_program_result, StakingBridgeErrorCode::InvalidSerumDexProgram);
+
+    let bad_amm_program_result = check_swap_programs(
+      raydium_program.clone(),
+      serum_program.clone(),
+      bad_account_info.clone(),
+      market_program.clone(),
+    );
+    assert_error(bad_amm_program_result, StakingBridgeErrorCode::InvalidAmmProgram);
+
+    let bad_market_program_result = check_swap_programs(
+      raydium_program.clone(),
+      serum_program.clone(),
+      amm_program.clone(),
+      bad_account_info.clone(),
+    );
+    assert_error(bad_market_program_result, StakingBridgeErrorCode::InvalidSerumMarketProgram);
 
     let result = check_swap_programs(
       raydium_program.clone(),
       serum_program.clone(),
+      amm_program.clone(),
+      market_program.clone(),
     );
     assert_eq!(result, Ok(()));
 }
