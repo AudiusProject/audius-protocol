45fb7aa3e73f2b6cbf3ee7e03b2b7d036fece7ec
Alec Savvy <alecsavvy@outlook.com>
2023-09-02 17:22:24 -0600

    format

--- discovery-provider/plugins/pedalboard/apps/relay/src/middleware/logging.ts
+++ discovery-provider/plugins/pedalboard/apps/relay/src/middleware/logging.ts
@@ -1,14 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { logger } from "../logger";
 
-export const incomingRequestLogger = (request: Request, response: Response, next: NextFunction) => {
-    logger.info({ request }, "incoming request")
-    next()
-}
+export const incomingRequestLogger = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  logger.info({ request }, "incoming request");
+  next();
+};
 
-export const outgoingRequestLogger = (request: Request, response: Response, next: NextFunction) => {
-    // in milliseconds
-    const responseTime = new Date().getTime() - response.locals.ctx.startTime.getTime()
-    logger.info({ request, response, responseTime }, "request completed")
-    next()
-}
+export const outgoingRequestLogger = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // in milliseconds
+  const responseTime =
+    new Date().getTime() - response.locals.ctx.startTime.getTime();
+  logger.info({ request, response, responseTime }, "request completed");
+  next();
+};
