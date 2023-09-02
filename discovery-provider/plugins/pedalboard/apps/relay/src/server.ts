902b11a83bdee7d5c8803e1e662762426d282391
Alec Savvy <alecsavvy@outlook.com>
2023-09-02 17:21:56 -0600

    request logger

--- discovery-provider/plugins/pedalboard/apps/relay/src/server.ts
+++ discovery-provider/plugins/pedalboard/apps/relay/src/server.ts
@@ -2,7 +2,6 @@ import { healthCheck } from "./routes/health";
 import express from "express";
 import { relayTransaction } from "./txRelay";
 import { errorHandler } from "./middleware/errorHandler";
-import { validationError } from "./error";
 
 export const app = express();
 
@@ -11,15 +10,6 @@ app.get("/relay/health", healthCheck);
 
 /** Writes */
 app.post("/relay", relayTransaction);
-
-/** Error Tests */
-app.get("/errorapp", async (req, res, next) => {
-  validationError(next, "validation from apperror");
-});
-
-app.get("/errorthrown", async (req, res, next) => {
-  throw new Error("KABLAM!");
-});
 
 /** Register top level middlewares */
 app.use(errorHandler);
