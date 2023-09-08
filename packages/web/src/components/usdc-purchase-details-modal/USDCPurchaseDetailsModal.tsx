97ad99a46ef247bd0d49d87c13d5ecc26a85ab19
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-09-08 17:54:41 -0400

    checkpoint

---
+++ packages/web/src/components/usdc-purchase-details-modal/USDCPurchaseDetailsModal.tsx
@@ -0,0 +1,124 @@
+import {
+  USDCPurchaseDetails,
+  useUSDCPurchaseDetailsModal
+} from '@audius/common'
+import {
+  HarmonyButton,
+  HarmonyButtonType,
+  IconCart,
+  Modal,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle
+} from '@audius/stems'
+
+import { Icon } from 'components/Icon'
+
+import styles from './USDCPurchaseDetailsModal.module.css'
+
+const messages = {
+  by: 'By',
+  cost: 'Cost',
+  date: 'Date',
+  done: 'Done',
+  messageBuyer: 'Message Buyer',
+  purchasedBy: 'Purchased By',
+  purchaseDetails: 'Purchase Details',
+  saleDetails: 'Sale Details',
+  track: 'Track',
+  trackPurchased: 'Track Purchased',
+  visitTrack: 'Visit Track'
+}
+
+type ContentProps = {
+  purchaseDetails: USDCPurchaseDetails
+  onClose: () => void
+}
+
+const PurchaseModalContent = ({ purchaseDetails, onClose }: ContentProps) => {
+  const onClickVisitTrack = () => {}
+  return (
+    <>
+      <ModalHeader>
+        <ModalTitle
+          iconClassName={styles.icon}
+          icon={<Icon icon={IconCart} />}
+          title={messages.purchaseDetails}
+        />
+      </ModalHeader>
+      <ModalContent className={styles.content}>{/* TODO */}</ModalContent>
+      <ModalFooter className={styles.footer}>
+        <HarmonyButton
+          className={styles.button}
+          variant={HarmonyButtonType.GHOST}
+          text={messages.visitTrack}
+          onClick={onClickVisitTrack}
+        />
+        <HarmonyButton
+          className={styles.button}
+          text={messages.done}
+          onClick={onClose}
+        />
+      </ModalFooter>
+    </>
+  )
+}
+const SaleModalContent = ({ purchaseDetails, onClose }: ContentProps) => {
+  const onClickMessageBuyer = () => {}
+  return (
+    <>
+      <ModalHeader>
+        <ModalTitle
+          iconClassName={styles.icon}
+          icon={<Icon icon={IconCart} />}
+          title={messages.purchaseDetails}
+        />
+      </ModalHeader>
+      <ModalContent className={styles.content}>{/* TODO */}</ModalContent>
+      <ModalFooter className={styles.footer}>
+        <HarmonyButton
+          className={styles.button}
+          variant={HarmonyButtonType.GHOST}
+          text={messages.messageBuyer}
+          onClick={onClickMessageBuyer}
+        />
+        <HarmonyButton
+          className={styles.button}
+          text={messages.done}
+          onClick={onClose}
+        />
+      </ModalFooter>
+    </>
+  )
+}
+
+export const USDCPurchaseDetailsModal = () => {
+  const { isOpen, data, onClose, onClosed } = useUSDCPurchaseDetailsModal()
+  const { variant, purchaseDetails } = data
+
+  if (!purchaseDetails) {
+    console.error(
+      `USDCPurchaseDetailsModal (${variant}) rendered with empty purchase details`
+    )
+    return null
+  }
+  return (
+    <Modal
+      bodyClassName={styles.modalBody}
+      isOpen={isOpen}
+      onClose={onClose}
+      onClosed={onClosed}
+      size={'small'}
+    >
+      {variant === 'purchase' ? (
+        <PurchaseModalContent
+          purchaseDetails={purchaseDetails}
+          onClose={onClose}
+        />
+      ) : (
+        <SaleModalContent purchaseDetails={purchaseDetails} onClose={onClose} />
+      )}
+    </Modal>
+  )
+}
