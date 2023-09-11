1189db3058f11c7e613bb72d6270d9093b1be464
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-09-11 17:20:52 -0400

    hook up sales page details modal

--- packages/web/src/components/usdc-purchase-details-modal/USDCPurchaseDetailsModal.tsx
+++ packages/web/src/components/usdc-purchase-details-modal/USDCPurchaseDetailsModal.tsx
@@ -1,9 +1,13 @@
 import { useCallback } from 'react'
 
 import {
+  CommonState,
   USDCPurchaseDetails,
+  chatActions,
+  chatSelectors,
   formatUSDCWeiToUSDString,
   useGetTrackById,
+  useInboxUnavailableModal,
   useUSDCPurchaseDetailsModal
 } from '@audius/common'
 import {
@@ -19,6 +23,7 @@ import {
   ModalTitle
 } from '@audius/stems'
 import moment from 'moment'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { Icon } from 'components/Icon'
 import { Link } from 'components/link'
@@ -28,6 +33,9 @@ import { UserNameAndBadges } from 'components/user-name-and-badges/UserNameAndBa
 import { useGoToRoute } from 'hooks/useGoToRoute'
 
 import styles from './USDCPurchaseDetailsModal.module.css'
+
+const { getCanCreateChat } = chatSelectors
+const { createChat } = chatActions
 
 const messages = {
   by: 'By',
@@ -139,7 +147,28 @@ const PurchaseModalContent = ({ purchaseDetails, onClose }: ContentProps) => {
 }
 
 const SaleModalContent = ({ purchaseDetails, onClose }: ContentProps) => {
-  const onClickMessageBuyer = () => {}
+  const dispatch = useDispatch()
+  const { onOpen: openInboxUnavailableModal } = useInboxUnavailableModal()
+
+  const { canCreateChat } = useSelector((state: CommonState) =>
+    getCanCreateChat(state, { userId: purchaseDetails.buyerUserId })
+  )
+
+  const onClickMessageBuyer = useCallback(() => {
+    onClose()
+    if (canCreateChat) {
+      dispatch(createChat({ userIds: [purchaseDetails.buyerUserId] }))
+    } else {
+      openInboxUnavailableModal({ userId: purchaseDetails.buyerUserId })
+    }
+  }, [
+    canCreateChat,
+    onClose,
+    openInboxUnavailableModal,
+    purchaseDetails,
+    dispatch
+  ])
+
   return (
     <>
       <ModalHeader>
@@ -149,7 +178,32 @@ const SaleModalContent = ({ purchaseDetails, onClose }: ContentProps) => {
           title={messages.purchaseDetails}
         />
       </ModalHeader>
-      <ModalContent className={styles.content}>{/* TODO */}</ModalContent>
+      <ModalContent className={styles.content}>
+        <div className={styles.trackRow}>
+          <DetailSection label={messages.trackPurchased}>
+            <TrackLink onClick={onClose} id={purchaseDetails.contentId} />
+          </DetailSection>
+          <DynamicTrackArtwork id={purchaseDetails.contentId} />
+        </div>
+        <DetailSection label={messages.purchasedBy}>
+          <Text size='large' color='secondary'>
+            <UserNameAndBadges
+              onNavigateAway={onClose}
+              userId={purchaseDetails.buyerUserId}
+            />
+          </Text>
+        </DetailSection>
+        <DetailSection label={messages.date}>
+          <Text size='large'>
+            {moment(purchaseDetails.createdAt).format('M/D/YY')}
+          </Text>
+        </DetailSection>
+        <DetailSection label={messages.cost}>
+          <Text size='large'>{`$${formatUSDCWeiToUSDString(
+            purchaseDetails.amount
+          )}`}</Text>
+        </DetailSection>
+      </ModalContent>
       <ModalFooter className={styles.footer}>
         <HarmonyButton
           className={styles.button}
