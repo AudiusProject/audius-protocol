5afce4d9b54ba19fdfe390e8af500e803cd786c0
sliptype <sliptype@gmail.com>
2023-09-12 14:15:37 -0500

    test

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -24,7 +24,7 @@ import { reportToSentry } from './store/errors/reportToSentry'
 import './services/webVitals'
 import './index.css'
 
-const test: number = '123'
+const test: number = '1234'
 
 const AudiusApp = () => {
   return (
