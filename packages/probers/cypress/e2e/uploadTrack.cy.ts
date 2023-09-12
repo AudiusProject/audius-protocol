03bebf6caea3f48c6b8491898f123c0a1fc9500a
Dylan Jeffers <dylan@audius.co>
2023-09-12 16:20:22 -0700

    Prevent actual upload

--- packages/probers/cypress/e2e/uploadTrack.cy.ts
+++ packages/probers/cypress/e2e/uploadTrack.cy.ts
@@ -149,8 +149,8 @@ describe('Upload', () => {
 
     cy.findByRole('button', { name: /continue/i }).click()
 
-    cy.findByRole('dialog', { name: /confirm upload/i }).within(() => {
-      cy.findByRole('button', { name: /upload/i }).click()
-    })
+    // cy.findByRole('dialog', { name: /confirm upload/i }).within(() => {
+    //   cy.findByRole('button', { name: /upload/i }).click()
+    // })
   })
 })
