c82c87f4a12d20379062b5e23d7e92d9b93b06ad
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-09-13 12:36:53 -0700

    Fix track extension on select page

--- packages/web/src/components/upload/TrackPreviewNew.tsx
+++ packages/web/src/components/upload/TrackPreviewNew.tsx
@@ -51,8 +51,6 @@ export const TrackPreviewNew = (props: TrackPreviewProps) => {
     onRemove
   } = props
 
-  const fileExtension = fileType.split('/')[1] ?? null
-
   return (
     <div className={styles.trackPreviewNew}>
       {displayIndex ? (
@@ -67,7 +65,6 @@ export const TrackPreviewNew = (props: TrackPreviewProps) => {
       />
       <Text className={styles.titleText} size='small'>
         {trackTitle}
-        {fileExtension ? `.${fileExtension}` : null}
       </Text>
       <div className={styles.sizeContainer}>
         <Text
