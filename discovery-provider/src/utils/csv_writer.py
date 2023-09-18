ca39cda2da2945bf6d658932865ec38c53d7eca7
Saliou Diallo <saliou@audius.co>
2023-09-17 23:28:15 -0400

    Add comment

--- discovery-provider/src/utils/csv_writer.py
+++ discovery-provider/src/utils/csv_writer.py
@@ -5,6 +5,13 @@ from typing import List
 
 # Write CSV to string buffer before returning the contents of the string.
 # This function takes a list of dictionaries, where each dictionary represents a row.
+# Example:
+#   rows = [{"name": "John", "age": 30}, {"name": "Jane", "age": 25}]
+#   write_csv(rows)
+# Returns:
+#    name,age
+#    John,30
+#    Jane,25
 def write_csv(rows: List[dict]):
     if not rows:
         return ""
