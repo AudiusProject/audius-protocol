8e15dd9557579194a856449f60730074cd2994c4
Alec Savvy <alecsavvy@outlook.com>
2023-09-18 22:07:42 -0600

    login works

--- discovery-provider/plugins/pedalboard/ddex-uploader/src/Login.tsx
+++ discovery-provider/plugins/pedalboard/ddex-uploader/src/Login.tsx
@@ -1,3 +1,37 @@
+import { useEffect, useRef } from "react";
+import { audiusSdk } from "./audiusSdk"
+import { State, appStore } from "./store";
+
 export const Login = () => {
-    return <div>Login Page</div>
+    const buttonDivRef = useRef(null);
+    const setUser = appStore((state) => state.setUser)
+    const user = appStore((state) => state.user)
+    const toState = appStore((state) => state.toState)
+
+    async function loadOauth() {
+        audiusSdk.oauth?.init({
+          successCallback: setUser,
+          errorCallback: console.error
+        });
+        audiusSdk.oauth?.renderButton({
+          // @ts-ignore
+          element: buttonDivRef.current,
+          scope: "write"
+        });
+      }
+    
+      useEffect(() => {
+        loadOauth();
+      }, []);
+
+      // once this page is done, move to next
+      if (user) toState(State.Ingest)
+
+    return <div>
+        <>
+          <div className="centered">
+            <div ref={buttonDivRef} />
+          </div>
+        </>
+    </div>
 }
--- discovery-provider/plugins/pedalboard/ddex-uploader/src/audiusSdk.ts
+++ discovery-provider/plugins/pedalboard/ddex-uploader/src/audiusSdk.ts
@@ -1,11 +1,11 @@
-import { sdk } from "@audius/sdk/src/sdk"
+import { AudiusSdk } from "@audius/sdk/src/sdk"
 import Web3  from "web3"
 
-// typescript thing
 (window as any).Web3 = Web3
 
-export const audiusSdk = sdk({
-    apiKey: "",
-    apiSecret: "",
+// manually put type for type hints
+export const audiusSdk: AudiusSdk = (window as any).audiusSdk({
+    apiKey: "f4393adc26615a2e59d16b34589c0edc7388f73d",
+    apiSecret: "cc9cf610b414e6ef0c58815892ded9e3a1ae2b70a4ce00cc0f9d8a0b722af79f",
     appName: "ddex-uploader"
 })
