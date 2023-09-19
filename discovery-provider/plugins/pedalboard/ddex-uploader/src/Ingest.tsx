32984c4e720cda64c497a26f014e8d4996130020
Alec Savvy <alecsavvy@outlook.com>
2023-09-18 22:15:25 -0600

    login and upload flow

--- discovery-provider/plugins/pedalboard/ddex-uploader/src/Ingest.tsx
+++ discovery-provider/plugins/pedalboard/ddex-uploader/src/Ingest.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from 'react'
+import { FileUploader } from "react-drag-drop-files"
+//@ts-ignore
+import XMLParser from "react-xml-parser"
+import { appStore } from './store';
+
+const fileTypes = ["XML"]
+const parser = new XMLParser()
+
 export const Ingest = () => {
-    return <div>Ingest Page</div>
+        const [file, setFile] = useState<File | null>(null)
+        const setRawXml = appStore((state) => state.setRawXml)
+        const setParsedXml = appStore((state) => state.setParsedXml)
+
+        // fx
+        const readFile = async () => {
+            const rawXml = await file?.text();
+            if (rawXml === undefined) return
+            setRawXml(rawXml)
+
+            const parsedXml = parser.parseFromString(rawXml)
+            setParsedXml(parsedXml)
+        }
+        useEffect(() => {
+            readFile();
+        }, [file])
+    return (
+        <>
+        <FileUploader handleChange={setFile} name="ddex-file" types={fileTypes} />
+        </>
+    )
 }
