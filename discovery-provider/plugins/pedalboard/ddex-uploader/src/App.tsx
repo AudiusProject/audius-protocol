bddb2183219973e4af389d4ed8417d6ef87e2971
Alec Savvy <alecsavvy@outlook.com>
2023-09-18 19:47:51 -0600

    upload initial file

--- discovery-provider/plugins/pedalboard/ddex-uploader/src/App.tsx
+++ discovery-provider/plugins/pedalboard/ddex-uploader/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { FileUploader } from "react-drag-drop-files"
+import { JsonView, allExpanded, darkStyles } from 'react-json-view-lite';
+//@ts-ignore
+import XMLParser from "react-xml-parser"
 import './App.css'
 
+const fileTypes = ["XML"]
+const parser = new XMLParser()
+
 function App() {
-  const [count, setCount] = useState(0)
+  // state
+  const [file, setFile] = useState<File | null>(null)
+  const [fileText, setFileText] = useState<any | null>(null)
+
+  // fx
+  const readFile = () => {
+    file?.text().then((txt) => setFileText(parser.parseFromString(txt)))
+    console.dir({ fileText })
+  }
+  useEffect(readFile, [file])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      DDEX Uploader
+      <FileUploader handleChange={setFile} name="ddex-file" types={fileTypes} />
+      {fileText && <JsonView data={fileText} shouldExpandNode={allExpanded} style={darkStyles} />}
     </>
   )
 }
