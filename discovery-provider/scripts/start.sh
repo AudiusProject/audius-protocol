c1391ca294720cf61fa56ab219e8a9b97df77e00
isaac <isaac@audius.co>
2023-09-20 12:41:18 -0700

    Revert "[PROTO-1250] Move celery to eventlets (#5975)"
    This reverts commit 81b3bc017a83bc75659489c4dffa24d6f70a4b03.

--- discovery-provider/scripts/start.sh
+++ discovery-provider/scripts/start.sh
@@ -52,7 +52,7 @@ if [[ "$audius_discprov_dev_mode" == "true" ]]; then
         [ -e /var/celerybeat-schedule ] && rm /var/celerybeat-schedule
         [ -e /var/celerybeat.pid ] && rm /var/celerybeat.pid
         audius_service=beat celery -A src.worker.celery beat --schedule=/var/celerybeat-schedule --pidfile=/var/celerybeat.pid --loglevel WARNING 2>&1 | tee >(logger -t beat) &
-        audius_service=worker watchmedo auto-restart --directory ./ --pattern=*.py --recursive -- celery -A src.worker.celery worker --pool eventlet --concurrency 50 --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
+        audius_service=worker watchmedo auto-restart --directory ./ --pattern=*.py --recursive -- celery -A src.worker.celery worker --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
     fi
 else
     if [[ "$audius_no_server" != "true" ]] && [[ "$audius_no_server" != "1" ]]; then
@@ -64,7 +64,7 @@ else
         [ -e /var/celerybeat-schedule ] && rm /var/celerybeat-schedule
         [ -e /var/celerybeat.pid ] && rm /var/celerybeat.pid
         audius_service=beat celery -A src.worker.celery beat --schedule=/var/celerybeat-schedule --pidfile=/var/celerybeat.pid --loglevel WARNING 2>&1 | tee >(logger -t beat) &
-        audius_service=worker celery -A src.worker.celery worker --pool eventlet --concurrency 50 --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
+        audius_service=worker celery -A src.worker.celery worker --max-memory-per-child 300000 --loglevel $audius_discprov_loglevel 2>&1 | tee >(logger -t worker) &
     fi
 fi
 
