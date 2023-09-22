3508a699de96d9e64718f4dc1c151db290341c38
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-09-22 16:38:03 -0500

    [PAY-1770] Make purchase pill a button (#5969)

--- packages/web/src/components/premium-content-purchase-modal/PremiumContentPurchaseModal.tsx
+++ packages/web/src/components/premium-content-purchase-modal/PremiumContentPurchaseModal.tsx
@@ -1,60 +1,43 @@
-import { useCallback } from 'react'
-
 import {
-  combineStatuses,
-  premiumContentSelectors,
-  purchaseContentActions,
   useGetTrackById,
+  usePremiumContentPurchaseModal,
   useUSDCBalance
 } from '@audius/common'
 import { IconCart, Modal, ModalContent, ModalHeader } from '@audius/stems'
-import { useDispatch, useSelector } from 'react-redux'
 
-import { useModalState } from 'common/hooks/useModalState'
 import { Icon } from 'components/Icon'
 import { Text } from 'components/typography'
 
 import styles from './PremiumContentPurchaseModal.module.css'
 import { PurchaseDetailsPage } from './components/PurchaseDetailsPage'
 
-const { getPurchaseContentId } = premiumContentSelectors
-
 const messages = {
   completePurchase: 'Complete Purchase'
 }
 
-const usePremiumContentPurchaseModalState = () => {
-  const trackId = useSelector(getPurchaseContentId)
-  const dispatch = useDispatch()
-  const [isOpen, setIsOpen] = useModalState('PremiumContentPurchase')
-  const { data: balance, status: balanceStatus } = useUSDCBalance()
-  const { data: track, status: trackStatus } = useGetTrackById(
+export const PremiumContentPurchaseModal = () => {
+  const {
+    isOpen,
+    onClose,
+    onClosed,
+    data: { contentId: trackId }
+  } = usePremiumContentPurchaseModal()
+
+  const { data: balance } = useUSDCBalance()
+  const { data: track } = useGetTrackById(
     { id: trackId! },
     { disabled: !trackId }
   )
 
-  const status = combineStatuses([balanceStatus, trackStatus])
-
-  const handleClose = useCallback(() => {
-    setIsOpen(false)
-    dispatch(purchaseContentActions.cleanup())
-  }, [setIsOpen, dispatch])
-
-  return { isOpen, handleClose, track, balance, status }
-}
-
-export const PremiumContentPurchaseModal = () => {
-  const { balance, isOpen, handleClose, track } =
-    usePremiumContentPurchaseModalState()
-
   return (
     <Modal
       isOpen={isOpen}
-      onClose={handleClose}
+      onClose={onClose}
+      onClosed={onClosed}
       bodyClassName={styles.modal}
       dismissOnClickOutside
     >
-      <ModalHeader onClose={handleClose} showDismissButton>
+      <ModalHeader onClose={onClose} showDismissButton>
         <Text
           variant='label'
           color='neutralLight2'
@@ -71,7 +54,7 @@ export const PremiumContentPurchaseModal = () => {
           <PurchaseDetailsPage
             track={track}
             currentBalance={balance}
-            onViewTrackClicked={handleClose}
+            onViewTrackClicked={onClose}
           />
         ) : null}
       </ModalContent>
