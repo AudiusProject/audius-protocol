5497c743e66b8a9febe9c5716bd830ac7d4df609
Raymond Jacobson <ray@audius.co>
2023-09-27 14:26:33 -0700

    [C-607] Fix modal scroll bar

--- packages/stems/src/components/Scrollbar/Scrollbar.tsx
+++ packages/stems/src/components/Scrollbar/Scrollbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useMemo } from 'react'
+import { useEffect, useRef, useMemo, forwardRef, Ref, useCallback } from 'react'
 
 import { ResizeObserver } from '@juggle/resize-observer'
 import cn from 'classnames'
@@ -15,64 +15,71 @@ import { ScrollbarProps } from './types'
  * `Scrollbar` uses react-perfect-scrollbar (https://www.npmjs.com/package/react-perfect-scrollbar)
  * under the hood. For advanced use cases, refer to the documentation.
  */
-export const Scrollbar = ({
-  children,
-  className,
-  id,
-  forward,
-  ...props
-}: ScrollbarProps) => {
-  // Do not remove:
-  // useMeasure ref is required for infinite scrolling to work
-  const [ref] = useMeasure({ polyfill: ResizeObserver })
-  const timerRef = useRef<NodeJS.Timeout | null>(null)
-  const elementId = useMemo(() => id || uniqueId('scrollbar-'), [id])
+export const Scrollbar = forwardRef(
+  (
+    { children, className, id, forward, isHidden, ...props }: ScrollbarProps,
+    forwardedRef: Ref<PerfectScrollbar>
+  ) => {
+    // Do not remove:
+    // useMeasure ref is required for infinite scrolling to work
+    const [ref] = useMeasure({ polyfill: ResizeObserver })
+    const timerRef = useRef<NodeJS.Timeout | null>(null)
+    const elementId = useMemo(() => id || uniqueId('scrollbar-'), [id])
 
-  useEffect(() => {
-    return () => {
-      if (timerRef.current !== null) {
-        clearTimeout(timerRef.current)
+    useEffect(() => {
+      return () => {
+        if (timerRef.current !== null) {
+          clearTimeout(timerRef.current)
+        }
       }
-    }
-  }, [])
+    }, [])
 
-  const hideScrollbar = () => {
-    const element = document.getElementById(elementId)
-    if (element) {
-      element.classList.remove('scrollbar--hovered-visible')
-    }
-    if (timerRef.current !== null) {
-      clearTimeout(timerRef.current)
-    }
-  }
-
-  const showScrollbar = () => {
-    const element = document.getElementById(elementId)
-    if (element) {
-      element.classList.add('scrollbar--hovered-visible')
-    }
-    if (timerRef.current !== null) {
-      clearTimeout(timerRef.current)
-    }
-    timerRef.current = setTimeout(() => {
+    const hideScrollbar = useCallback(() => {
       const element = document.getElementById(elementId)
       if (element) {
         element.classList.remove('scrollbar--hovered-visible')
       }
-    }, 1400)
+      if (timerRef.current !== null) {
+        clearTimeout(timerRef.current)
+      }
+    }, [elementId, timerRef])
+
+    const showScrollbar = useCallback(() => {
+      if (isHidden) return
+      const element = document.getElementById(elementId)
+      if (element) {
+        element.classList.add('scrollbar--hovered-visible')
+      }
+      if (timerRef.current !== null) {
+        clearTimeout(timerRef.current)
+      }
+      timerRef.current = setTimeout(() => {
+        const element = document.getElementById(elementId)
+        if (element) {
+          element.classList.remove('scrollbar--hovered-visible')
+        }
+      }, 1400)
+    }, [elementId, timerRef, isHidden])
+
+    useEffect(() => {
+      if (isHidden) {
+        hideScrollbar()
+      }
+    }, [isHidden, hideScrollbar])
+
+    const content = forward ? children : <div ref={ref}>{children}</div>
+
+    return (
+      <PerfectScrollbar
+        {...props}
+        ref={forwardedRef}
+        id={elementId}
+        className={cn(styles.scrollbar, className)}
+        onMouseEnter={showScrollbar}
+        onMouseLeave={hideScrollbar}
+      >
+        {content}
+      </PerfectScrollbar>
+    )
   }
-
-  const content = forward ? children : <div ref={ref}>{children}</div>
-
-  return (
-    <PerfectScrollbar
-      {...props}
-      id={elementId}
-      className={cn(styles.scrollbar, className)}
-      onMouseEnter={showScrollbar}
-      onMouseLeave={hideScrollbar}
-    >
-      {content}
-    </PerfectScrollbar>
-  )
-}
+)
