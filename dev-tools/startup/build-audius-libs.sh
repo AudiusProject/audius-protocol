ba8da51ce5983e11327b2cf2025202201b521181
sliptype <sliptype@gmail.com>
2023-09-27 12:01:04 -0500

    Fix hot reload

--- dev-tools/startup/build-audius-libs.sh
+++ dev-tools/startup/build-audius-libs.sh
@@ -8,11 +8,11 @@ const abi = JSON.parse(fs.readFileSync(0, "utf-8"));
 process.stdout.write(JSON.stringify({ contractName: abi.contractName, abi: abi.abi }, null, 2));
 EOF
 
-for abi in ../data-contracts/ABIs/*; do
+for abi in ./data-contracts/ABIs/*; do
   node strip_abi.js <"$abi" >src/data-contracts/ABIs/$(basename "$abi")
 done
 
-for abi in ../eth-contracts/ABIs/*; do
+for abi in ./eth-contracts/ABIs/*; do
   node strip_abi.js <"$abi" >src/eth-contracts/ABIs/$(basename "$abi")
 done
 
