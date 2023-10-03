353fa83797606ea2fd37c1a287ac78c471089377
Alec Savvy <alecsavvy@outlook.com>
2023-10-02 21:23:01 -0600

    rm unnecessary logs

--- discovery-provider/plugins/verified-uploads/src/handlers/queue.js
+++ discovery-provider/plugins/verified-uploads/src/handlers/queue.js
@@ -2,13 +2,12 @@
 const queuedMessages = []
 
 export const queueMessage = (msg) => {
-  console.log({ msg }, 'queueing msg')
   queuedMessages.push(msg)
 }
 
 export const consume = async (callback) => {
   while (true) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) => setTimeout(resolve, 500))
     const nextMsg = queuedMessages.shift()
     if (nextMsg !== undefined) {
       callback(nextMsg)
