261fb904278ebd9e2299f90579aa324d1622d1f9
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-10-03 15:18:48 -0400

    [PAY-1939] Convert purchase content drawer to full screen variant (#6205)

--- packages/mobile/src/components/premium-track-purchase-drawer/PremiumTrackPurchaseDrawer.tsx
+++ packages/mobile/src/components/premium-track-purchase-drawer/PremiumTrackPurchaseDrawer.tsx
@@ -12,11 +12,12 @@ import {
   usePurchaseContentFormConfiguration
 } from '@audius/common'
 import { Formik, useFormikContext } from 'formik'
-import { Linking, View } from 'react-native'
+import { Linking, View, ScrollView, TouchableOpacity } from 'react-native'
 import { useDispatch } from 'react-redux'
 import { toFormikValidationSchema } from 'zod-formik-adapter'
 
 import IconCart from 'app/assets/images/iconCart.svg'
+import IconCloseAlt from 'app/assets/images/iconCloseAlt.svg'
 import IconError from 'app/assets/images/iconError.svg'
 import { Button, LockedStatusBadge, Text } from 'app/components/core'
 import { NativeDrawer } from 'app/components/drawer'
@@ -63,24 +64,37 @@ const messages = {
 
 const useStyles = makeStyles(({ spacing, typography, palette }) => ({
   drawer: {
-    paddingTop: spacing(6),
-    paddingHorizontal: spacing(4),
-    paddingBottom: spacing(8),
-    gap: spacing(6),
-    backgroundColor: palette.white
+    paddingHorizontal: spacing(4)
+  },
+  formContainer: {
+    flex: 1
+  },
+  formContentContainer: {
+    paddingVertical: spacing(6),
+    gap: spacing(4)
+  },
+  formActions: {
+    flex: 0,
+    paddingTop: spacing(4),
+    paddingBottom: spacing(6),
+    columnGap: spacing(4)
+  },
+  headerContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: palette.neutralLight8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingVertical: spacing(4)
   },
   titleContainer: {
     ...flexRowCentered(),
     gap: spacing(2),
-    marginBottom: spacing(2),
-    alignSelf: 'center'
-  },
-  title: {
-    fontSize: typography.fontSize.xl,
-    fontFamily: typography.fontByWeight.heavy,
-    color: palette.neutralLight2,
-    textTransform: 'uppercase',
-    lineHeight: typography.fontSize.xl * 1.25
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'center',
+    // Matches close icon width
+    paddingRight: spacing(6)
   },
   trackTileContainer: {
     ...flexRowCentered(),
@@ -111,13 +125,45 @@ const useStyles = makeStyles(({ spacing, typography, palette }) => ({
   }
 }))
 
+const PremiumTrackPurchaseDrawerHeader = ({
+  onClose
+}: {
+  onClose: () => void
+}) => {
+  const styles = useStyles()
+  const { neutralLight2, neutralLight4 } = useThemeColors()
+  return (
+    <View style={styles.headerContainer}>
+      <TouchableOpacity activeOpacity={0.7} onPress={onClose}>
+        <IconCloseAlt
+          width={spacing(6)}
+          height={spacing(6)}
+          fill={neutralLight4}
+        />
+      </TouchableOpacity>
+      <View style={styles.titleContainer}>
+        <IconCart width={spacing(6)} height={spacing(6)} fill={neutralLight2} />
+        <Text
+          variant='label'
+          fontSize='large'
+          color='neutralLight2'
+          weight='heavy'
+          textTransform='uppercase'
+          noGutter
+        >
+          {messages.title}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
 // The bulk of the form rendering is in a nested component because we want access
 // to the FormikContext, which can only be used in a component which is a descendant
 // of the `<Formik />` component
 const RenderForm = ({ track }: { track: PurchasableTrackMetadata }) => {
   const styles = useStyles()
-  const { specialLightGreen, neutralLight2, accentRed, secondary } =
-    useThemeColors()
+  const { specialLightGreen, accentRed, secondary } = useThemeColors()
 
   const { submitForm, resetForm } = useFormikContext()
 
@@ -140,21 +186,17 @@ const RenderForm = ({ track }: { track: PurchasableTrackMetadata }) => {
   }, [])
 
   return (
-    <View style={styles.drawer}>
-      <View style={styles.titleContainer}>
-        <IconCart fill={neutralLight2} />
-        <Text style={styles.title}>{messages.title}</Text>
-      </View>
-      <TrackDetailsTile trackId={track.track_id} />
-      <PayExtraFormSection amountPresets={payExtraAmountPresetValues} />
-      <PurchaseSummaryTable
-        {...purchaseSummaryValues}
-        isPurchaseSuccessful={isPurchaseSuccessful}
-      />
-      {isPurchaseSuccessful ? (
-        <PurchaseSuccess track={track} />
-      ) : (
-        <>
+    <>
+      <ScrollView contentContainerStyle={styles.formContentContainer}>
+        <TrackDetailsTile trackId={track.track_id} />
+        <PayExtraFormSection amountPresets={payExtraAmountPresetValues} />
+        <PurchaseSummaryTable
+          {...purchaseSummaryValues}
+          isPurchaseSuccessful={isPurchaseSuccessful}
+        />
+        {isPurchaseSuccessful ? (
+          <PurchaseSuccess track={track} />
+        ) : (
           <View>
             <View style={styles.payToUnlockTitleContainer}>
               <Text weight='heavy' textTransform='uppercase' fontSize='small'>
@@ -170,32 +212,36 @@ const RenderForm = ({ track }: { track: PurchasableTrackMetadata }) => {
               )}
             </Text>
           </View>
-          <Button
-            onPress={submitForm}
-            disabled={isUnlocking}
-            title={
-              isUnlocking
-                ? messages.purchasing
-                : messages.buy(formatPrice(price))
-            }
-            variant={'primary'}
-            size='large'
-            color={specialLightGreen}
-            iconPosition='left'
-            icon={isUnlocking ? LoadingSpinner : undefined}
-            fullWidth
-          />
-        </>
-      )}
-      {error ? (
-        <View style={styles.errorContainer}>
-          <IconError fill={accentRed} width={spacing(5)} height={spacing(5)} />
-          <Text weight='medium' colorValue={accentRed}>
-            {messages.error}
-          </Text>
-        </View>
-      ) : null}
-    </View>
+        )}
+      </ScrollView>
+      <View style={styles.formActions}>
+        {error ? (
+          <View style={styles.errorContainer}>
+            <IconError
+              fill={accentRed}
+              width={spacing(5)}
+              height={spacing(5)}
+            />
+            <Text weight='medium' colorValue={accentRed}>
+              {messages.error}
+            </Text>
+          </View>
+        ) : null}
+        <Button
+          onPress={submitForm}
+          disabled={isUnlocking}
+          title={
+            isUnlocking ? messages.purchasing : messages.buy(formatPrice(price))
+          }
+          variant={'primary'}
+          size='large'
+          color={specialLightGreen}
+          iconPosition='left'
+          icon={isUnlocking ? LoadingSpinner : undefined}
+          fullWidth
+        />
+      </View>
+    </>
   )
 }
 
@@ -223,21 +269,27 @@ export const PremiumTrackPurchaseDrawer = () => {
 
   return (
     <NativeDrawer
+      drawerStyle={styles.drawer}
+      drawerHeader={PremiumTrackPurchaseDrawerHeader}
       drawerName={PREMIUM_TRACK_PURCHASE_MODAL_NAME}
       onClosed={handleClosed}
+      isGestureSupported={false}
+      isFullscreen
     >
       {isLoading ? (
         <View style={styles.spinnerContainer}>
           <LoadingSpinner />
         </View>
       ) : (
-        <Formik
-          initialValues={initialValues}
-          validationSchema={toFormikValidationSchema(validationSchema)}
-          onSubmit={onSubmit}
-        >
-          <RenderForm track={track} />
-        </Formik>
+        <View style={styles.formContainer}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={toFormikValidationSchema(validationSchema)}
+            onSubmit={onSubmit}
+          >
+            <RenderForm track={track} />
+          </Formik>
+        </View>
       )}
     </NativeDrawer>
   )
