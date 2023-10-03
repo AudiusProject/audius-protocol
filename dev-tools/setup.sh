5363f78fbda670e83c92d3cc3b6f37b70338a0ec
Daniel Phelps <daniel@audius.co>
2023-10-03 16:58:49 -0700

    Run CI jobs without GCP

--- dev-tools/setup.sh
+++ dev-tools/setup.sh
@@ -10,11 +10,6 @@ debian | ubuntu)
     sudo apt-get update
     sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
-    # Setup repository for docker
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL "https://download.docker.com/linux/$ID/gpg" | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$ID $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
     # Install dependencies
     sudo apt-get update
     sudo apt-get install -y \
