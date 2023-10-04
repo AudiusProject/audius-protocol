6a739965d634ad494b94e7161db90829b0aba59d
Alec Savvy <alecsavvy@outlook.com>
2023-10-03 22:19:41 -0600

    start xml parsing and rm sandbox file

--- discovery-provider/plugins/pedalboard/apps/ddex-uploader/src/Validate.tsx
+++ discovery-provider/plugins/pedalboard/apps/ddex-uploader/src/Validate.tsx
@@ -1,35 +1,13 @@
 import { appStore } from "./store";
 
-interface XmlNode {
-  name: string;
-  attributes: { [key: string]: string };
-  children: XmlNode[];
-  value: string;
-}
-
-// Recursively search for XML nodes by an array of names and return an array of matching nodes in the same order
-function searchXmlNodesByNames(node: XmlNode, targetNames: string[]): XmlNode[] {
-  let results: XmlNode[] = [];
-
-  if (targetNames.includes(node.name)) {
-    results.push(node);
-  }
-
-  for (const child of node.children) {
-    const childResults = searchXmlNodesByNames(child, targetNames);
-    results = results.concat(childResults);
-  }
-
-  return results;
-}
+const parser = new DOMParser()
 
 export const Validate = () => {
-    const { children }: { children: XmlNode[] } = appStore((state) => state.xmlParsed)
-    const names = ["DisplayArtistName", "LabelName", "Genre", "Image", "ISRC", "TechnicalSoundRecordingDetails"]
-    const foundNodes = children.flatMap(node => searchXmlNodesByNames(node, names))
+    const xml = appStore((state) => state.xmlRaw)!
+    const xmlDoc = parser.parseFromString(xml, "text/xml")
 
     return (<div>
         Validation Page
-        <div>{foundNodes.map((node) => <div> {node.name}: {node.value}</div>)}</div>
+        <div>{xmlDoc.getElementsByTagName("MessageHeader")[0].getElementsByTagName("MessageId")[0].childNodes[0].nodeValue}</div>
     </div>)
 }
---
+++ discovery-provider/plugins/pedalboard/apps/ddex-uploader/src/ernParser.ts
@@ -0,0 +1,1 @@
+// TODO: do this on the backend
