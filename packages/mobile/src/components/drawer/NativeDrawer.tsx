195fa187cbb08db318d1b9a7ef1502c460698aed
Dylan Jeffers <dylan@audius.co>
2023-10-05 13:08:58 -0700

    Fix drawers not closing

--- packages/mobile/src/components/drawer/NativeDrawer.tsx
+++ packages/mobile/src/components/drawer/NativeDrawer.tsx
@@ -19,7 +19,7 @@ type NativeDrawerProps = SetOptional<DrawerProps, 'isOpen' | 'onClose'> & {
  */
 export const NativeDrawer = (props: NativeDrawerProps) => {
   const {
-    blockClose = true,
+    blockClose = false,
     drawerName,
     onClose: onCloseProp,
     ...other
