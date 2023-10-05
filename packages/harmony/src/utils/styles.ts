ca7f8ee37c230b31340ac7598f21f24ade13b732
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2023-10-05 15:18:12 -0700

    [C-3133] Port HarmonyButton to harmony package (#6242)

---
+++ packages/harmony/src/utils/styles.ts
@@ -0,0 +1,11 @@
+import { kebabCase } from 'lodash'
+
+/** Converts a camelCased variable name such as `primaryDark1` to an
+ * equivalent CSS variable name (`--primary-dark-1`). Useful for passing
+ * values like color names to a component to be converted to a CSS
+ * property set via `style`
+ */
+export const toCSSVariableName = (name: string) => {
+  // TODO: support harmony variable names (prefix harmony?)
+  return `--harmony-${kebabCase(name)}`
+}
