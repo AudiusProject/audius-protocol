c2c03e59b43601bb7d53a87dcb79db8c6eb3591f
Raymond Jacobson <ray@audius.co>
2023-10-12 16:39:41 -0700

    Fix lint

--- packages/web/src/workers/dominantColor.worker.js
+++ packages/web/src/workers/dominantColor.worker.js
@@ -119,8 +119,8 @@ export default () => {
   const calculateEuclideanDistance = (c1, c2) => {
     return Math.sqrt(
       Math.pow(c1.r - c2.r, 2) +
-      Math.pow(c1.g - c2.g, 2) +
-      Math.pow(c1.b - c2.b, 2)
+        Math.pow(c1.g - c2.g, 2) +
+        Math.pow(c1.b - c2.b, 2)
     )
   }
 
