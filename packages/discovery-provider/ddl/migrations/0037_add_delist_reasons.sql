fe4861e4b9dddb2a3735d80b831144d2c4058acb
Theo Ilie <theoilie.ti@gmail.com>
2023-10-12 17:06:27 -0700

    Fix sql (no alter type inside txn)

--- packages/discovery-provider/ddl/migrations/0037_add_delist_reasons.sql
+++ packages/discovery-provider/ddl/migrations/0037_add_delist_reasons.sql
@@ -1,27 +1,4 @@
-BEGIN;
-
-DO $$ BEGIN
-    ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'ACR_COUNTER_NOTICE';
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
-
-DO $$ BEGIN
-    ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'DMCA_RETRACTION';
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
-
-DO $$ BEGIN
-    ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'DMCA_COUNTER_NOTICE';
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
-
-DO $$ BEGIN
-    ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'DMCA_AND_ACR_COUNTER_NOTICE';
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
-
-COMMIT;
+ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'ACR_COUNTER_NOTICE';
+ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'DMCA_RETRACTION';
+ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'DMCA_COUNTER_NOTICE';
+ALTER TYPE delist_track_reason ADD VALUE IF NOT EXISTS 'DMCA_AND_ACR_COUNTER_NOTICE';
