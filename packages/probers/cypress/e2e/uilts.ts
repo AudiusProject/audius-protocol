87e4c4cc215de1633a81deca7654fe2d2f6d454b
Dylan Jeffers <dylan@audius.co>
2023-10-13 15:43:13 -0700

    Revert and fix block-confirmation check

--- packages/probers/cypress/e2e/uilts.ts
+++ packages/probers/cypress/e2e/uilts.ts
@@ -2,19 +2,21 @@ export const waitForTransaction = (transactionNumber: number) => {
   cy.intercept({ method: 'POST', url: '**/relay' }).as(
     `relayCheck-${transactionNumber}`
   )
-  cy.intercept({
-    url: '**/block_confirmation*'
-  }).as(`blockConfirmation-${transactionNumber}`)
+  cy.wait(`@relayCheck-${transactionNumber}`).then((xhr) => {
+    const { blockHash, blockNumber } = xhr.response.body.receipt
+    cy.intercept({
+      url: '**/block_confirmation*',
+      query: { blockhash: blockHash, blocknumber: String(blockNumber) }
+    }).as(`blockConfirmation-${blockNumber}`)
 
-  cy.wait(`@relayCheck-${transactionNumber}`)
-
-  waitForBlockConfirmation(`@blockConfirmation-${transactionNumber}`)
+    waitForBlockConfirmation(`@blockConfirmation-${blockNumber}`)
+  })
 }
 
 const waitForBlockConfirmation = (routeAlias, retries = 3) => {
-  cy.wait(routeAlias).then((xhr) => {
-    const { block_found, block_passed } = xhr.response.body.data
-    if (block_found && block_passed) {
+  cy.wait(routeAlias, { timeout: 10000 }).then((xhr) => {
+    const { block_passed } = xhr.response.body.data
+    if (block_passed) {
     } else if (retries > 0) waitForBlockConfirmation(routeAlias, retries - 1)
     // wait for the next response
     else throw new Error('All requests returned non-200 response')
