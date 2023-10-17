337070cb9a841ab97eda36d83c72efcb104ff137
Dharit Tantiviramanond <dharit.tan@gmail.com>
2023-10-17 02:08:34 -0400

    [PAY-2009] Challenge cooldown UI

--- packages/web/src/pages/audio-rewards-page/components/modals/ChallengeRewardsModal/AudioMatchingRewardsModalContent.tsx
+++ packages/web/src/pages/audio-rewards-page/components/modals/ChallengeRewardsModal/AudioMatchingRewardsModalContent.tsx
@@ -4,7 +4,9 @@ import {
   ChallengeName,
   OptimisticUserChallenge,
   challengeRewardsConfig,
-  formatNumberCommas
+  formatNumberCommas,
+  audioRewardsPageSelectors,
+  isAudioMatchingChallenge
 } from '@audius/common'
 import { IconArrowRight, IconCloudUpload, Text } from '@audius/harmony'
 import {
@@ -13,8 +15,11 @@ import {
   HarmonyButtonType
 } from '@audius/stems'
 import cn from 'classnames'
+import moment from 'moment'
+import { useSelector } from 'react-redux'
 
 import LoadingSpinner from 'components/loading-spinner/LoadingSpinner'
+import { SummaryTable } from 'components/summary-table'
 import { useNavigateToPage } from 'hooks/useNavigateToPage'
 import { useWithMobileStyle } from 'hooks/useWithMobileStyle'
 import { isMobile } from 'utils/clientUtil'
@@ -23,6 +28,10 @@ import { EXPLORE_PREMIUM_TRACKS_PAGE, UPLOAD_PAGE } from 'utils/route'
 import { ProgressDescription } from './ProgressDescription'
 import { ProgressReward } from './ProgressReward'
 import styles from './styles.module.css'
+
+const { getUndisbursedUserChallenges } = audioRewardsPageSelectors
+
+const COOLDOWN_DAYS = 7
 
 const messages = {
   rewardMapping: {
@@ -38,7 +47,10 @@ const messages = {
   viewPremiumTracks: 'View Premium Tracks',
   uploadTrack: 'Upload Track',
   totalEarned: (amount: string) => `Total $AUDIO Earned: ${amount}`,
-  claimAudio: (amount: string) => `Claim ${amount} $AUDIO`
+  claimAudio: (amount: string) => `Claim ${amount} $AUDIO`,
+  upcomingRewards: 'Upcoming Rewards',
+  laterToday: 'Later Today',
+  readyToClaim: 'Ready to Claim!'
 }
 
 type AudioMatchingChallengeName =
@@ -72,6 +84,45 @@ const ClaimInProgressSpinner = () => (
   <LoadingSpinner className={styles.spinner} />
 )
 
+/**
+ * Filters for only $AUDIO matching challenges and generates row
+ * items in correct format for SummaryTable. Also calculates the
+ * total claimable amount.
+ */
+const useUndisbursedChallengesCooldown = () => {
+  // Only show the cooldown for the $AUDIO matching challenges.
+  // TODO: PAY-2030 Use cooldown_days from api instead of
+  // isAudioMatchingChallenge and COOLDOWN_DAYS.
+  const challenges = useSelector(getUndisbursedUserChallenges)
+    .filter((c) => isAudioMatchingChallenge(c.challenge_id))
+    .map((c) => ({ ...c, created_at: moment(c.created_at) }))
+  const now = moment.utc()
+  // Only challenges past the cooldown period are claimable
+  const claimableAmount = challenges
+    .filter((c) => now.diff(c.created_at, 'days') >= COOLDOWN_DAYS)
+    .reduce((acc, curr) => acc + curr.amount, 0)
+  // Challenges are already ordered by completed_blocknumber ascending.
+  // Convert to local time and group challenges by date claimable.
+  const cooldownItems = new Array(7)
+  challenges
+    .filter((c) => now.diff(c.created_at, 'days') < COOLDOWN_DAYS)
+    .forEach((c) => {
+      const diff = now.diff(c.created_at, 'days')
+      cooldownItems[diff] = {
+        ...cooldownItems[diff],
+        id: c.specifier,
+        label: c.created_at.add(7, 'days').local().format('ddd M/D'),
+        value: (cooldownItems[diff]?.value ?? 0) + c.amount
+      }
+    })
+  const cooldownSummary = {
+    id: messages.readyToClaim,
+    label: messages.readyToClaim,
+    value: claimableAmount
+  }
+  return { cooldownItems, cooldownSummary, claimableAmount }
+}
+
 /** Implements custom ChallengeRewardsContent for the $AUDIO matching challenges */
 export const AudioMatchingRewardsModalContent = ({
   challenge,
@@ -84,6 +135,8 @@ export const AudioMatchingRewardsModalContent = ({
   const wm = useWithMobileStyle(styles.mobile)
   const navigateToPage = useNavigateToPage()
   const { fullDescription } = challengeRewardsConfig[challengeName]
+  const { cooldownItems, cooldownSummary, claimableAmount } =
+    useUndisbursedChallengesCooldown()
 
   const audioClaimedSoFar = challenge
     ? challenge.amount * challenge.current_step_count -
@@ -140,22 +193,27 @@ export const AudioMatchingRewardsModalContent = ({
           </div>
         </>
       ) : (
-        <div className={styles.progressCard}>
-          <div className={styles.progressInfo}>
-            {progressDescription}
-            {progressReward}
+        <>
+          <div className={styles.progressCard}>
+            <div className={styles.progressInfo}>
+              {progressDescription}
+              {progressReward}
+            </div>
+            {progressStatusLabel}
           </div>
-          {progressStatusLabel}
-        </div>
+          <SummaryTable
+            title={messages.upcomingRewards}
+            items={cooldownItems}
+            summaryItem={cooldownSummary}
+          />
+        </>
       )}
       {challenge?.claimableAmount && challenge.claimableAmount > 0 ? (
         <HarmonyButton
           fullWidth
           iconRight={claimInProgress ? ClaimInProgressSpinner : IconArrowRight}
           disabled={claimInProgress}
-          text={messages.claimAudio(
-            formatNumberCommas(challenge.claimableAmount)
-          )}
+          text={messages.claimAudio(formatNumberCommas(claimableAmount))}
           onClick={onClaimRewardClicked}
         />
       ) : (
