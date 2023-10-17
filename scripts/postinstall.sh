e408753a28f63a50be2b407ac7f50ae7d0b7a28c
sliptype <sliptype@gmail.com>
2023-10-17 15:19:00 -0500

    Remove post install build

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,6 @@
 npm run install-hooks
 npm run patch-package
 
-if [[ -z "${SKIP_BUILD}" ]]; then
-  npm run build
-fi
-
 if [[ -z "${SKIP_POD_INSTALL}" ]]; then
   # Symlink react-native into the mobile package bc npm doesn't
   # support nohoist
