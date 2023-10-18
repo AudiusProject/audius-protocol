9a1b3b0388a94ba0f359d5cee4b4c612957d4299
Dylan Jeffers <dylan@audius.co>
2023-10-18 11:11:35 -0700

    [C-2769] Migrate from altool to notarytool (#6372)

--- packages/web/scripts/dist.js
+++ packages/web/scripts/dist.js
@@ -8,9 +8,9 @@
 const fs = require('fs')
 const path = require('path')
 
+const { notarize } = require('@electron/notarize')
 const program = require('commander')
 const builder = require('electron-builder')
-const notarize = require('electron-notarize').notarize
 const fse = require('fs-extra')
 
 const PRODUCTION_APP_ID = 'co.audius.app'
