011f5af7e18316bc17dc5fd28a3447696ad32ca7
sliptype <sliptype@gmail.com>
2023-10-18 17:12:08 -0500

    Split libs bundles into separate tasks

--- packages/libs/rollup.config.ts
+++ packages/libs/rollup.config.ts
@@ -1,12 +1,12 @@
-import commonjs from '@rollup/plugin-commonjs'
+import alias from '@rollup/plugin-alias'
 import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
-import nodePolyfills from 'rollup-plugin-polyfill-node'
-import alias from '@rollup/plugin-alias'
 import ignore from 'rollup-plugin-ignore'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
