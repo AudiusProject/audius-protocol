785a815c7ed0feb338d88f0c3bc9c194df4674d4
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-10-19 16:21:12 -0400

    [PAY-2060] Fix wrong challenge claimed so far amounts (#6398)

--- packages/discovery-provider/src/utils/spl_audio.py
+++ packages/discovery-provider/src/utils/spl_audio.py
@@ -4,6 +4,8 @@ from typing import Union
 SPL_TO_WEI = 10**10
 SPL_TO_WEI_PADDING = "0" * 10
 
+LAMPORTS_PER_SOL = 10**8
+
 
 def to_wei(balance: Union[int, str]):
     return int(balance) * SPL_TO_WEI if balance else 0
@@ -11,3 +13,7 @@ def to_wei(balance: Union[int, str]):
 
 def to_wei_string(spl_amount: Union[int, str]):
     return f"{spl_amount}{SPL_TO_WEI_PADDING}"
+
+
+def from_lamports(spl_amount: str):
+    return int(spl_amount) / LAMPORTS_PER_SOL if spl_amount else 0
