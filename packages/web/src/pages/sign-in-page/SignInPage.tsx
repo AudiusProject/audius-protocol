ab0f822a1845a2b794ff42751ea0a05bf000e48c
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-10-19 17:46:08 -0700

    layout for signin page

--- packages/web/src/pages/sign-in-page/SignInPage.tsx
+++ packages/web/src/pages/sign-in-page/SignInPage.tsx
@@ -1,21 +1,25 @@
 import { useCallback } from 'react'
 
-import { Button } from '@audius/harmony'
-import { Button as ButtonTmp } from '@audius/stems'
+import { Button, ButtonType, Flex, Text } from '@audius/harmony'
 import { Form, Formik } from 'formik'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import audiusLogoColored from 'assets/img/audiusLogoColored.png'
 import { signIn } from 'common/store/pages/signon/actions'
 import { TextField } from 'components/form-fields'
+import PreloadImage from 'components/preload-image/PreloadImage'
 import { SIGN_UP_PAGE } from 'utils/route'
 
+import styles from './SignInPage.module.css'
+
 const messages = {
-  header: 'Sign Into Audius',
+  title: 'Sign Into Audius',
   emailLabel: 'Email',
   passwordLabel: 'Password',
   signIn: 'Sign In',
-  createAccount: 'Create An Account'
+  createAccount: 'Create An Account',
+  forgotPassword: 'Forgot password?'
 }
 
 type SignInValues = {
@@ -39,21 +43,60 @@ export const SignInPage = () => {
   )
 
   return (
-    <div>
-      <h1>{messages.header}</h1>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form>
-          <TextField name='email' label={messages.emailLabel} />
-          <TextField name='password' label={messages.passwordLabel} />
-          <Button text={messages.signIn} type='submit' />
-        </Form>
-      </Formik>
-      <ButtonTmp
+    <Flex
+      className={styles.root}
+      h='100%'
+      w={480}
+      // want 80px but don't have var for it
+      pv='4xl'
+      ph='2xl'
+      direction='column'
+      gap='2xl'
+      justifyContent='space-between'
+    >
+      {/* TODO: confirm 40px spacing value */}
+      <Flex direction='column' gap='2xl' alignItems='center'>
+        <PreloadImage
+          src={audiusLogoColored}
+          className={styles.logo}
+          alt='Audius Colored Logo'
+        />
+        <Flex w='100%' direction='row' justifyContent='flex-start'>
+          <Text variant='heading' size='l' tag='h1' color='heading'>
+            {messages.title}
+          </Text>
+        </Flex>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          <Form className={styles.form}>
+            <Flex direction='column' gap='2xl' w='100%'>
+              <Flex direction='column' gap='l'>
+                <TextField name='email' label={messages.emailLabel} />
+                <TextField
+                  name='password'
+                  label={messages.passwordLabel}
+                  type='password'
+                />
+              </Flex>
+              <Flex direction='column' gap='l'>
+                <Button text={messages.signIn} type='submit' />
+                <Flex direction='row' alignItems='flexStart'>
+                  <Text color='heading'>
+                    {/* TODO: link destination */}
+                    <Link to={''}>{messages.forgotPassword}</Link>
+                  </Text>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Form>
+        </Formik>
+      </Flex>
+      <Button
         // @ts-ignore
-        as={Link}
+        asChild={Link}
+        variant={ButtonType.SECONDARY}
         to={SIGN_UP_PAGE}
         text={messages.createAccount}
       />
-    </div>
+    </Flex>
   )
 }
