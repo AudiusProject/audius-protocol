aa3a213788acf940e544df6b1aabe0c07d8e3f49
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-10-23 13:42:08 -0700

    Add audius values page wrapper

--- packages/web/src/pages/sign-in-page/SignInPage.tsx
+++ packages/web/src/pages/sign-in-page/SignInPage.tsx
@@ -1,14 +1,22 @@
 import { useCallback } from 'react'
 
-import { Box, Button, ButtonType, Flex, Text } from '@audius/harmony'
+import {
+  Box,
+  Button,
+  ButtonType,
+  Flex,
+  IconArrowRight,
+  Text,
+  TextInput
+} from '@audius/harmony'
 import { Form, Formik } from 'formik'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import audiusLogoColored from 'assets/img/audiusLogoColored.png'
 import { signIn } from 'common/store/pages/signon/actions'
-import { TextField } from 'components/form-fields'
 import PreloadImage from 'components/preload-image/PreloadImage'
+import { PageWithAudiusValues } from 'pages/sign-on/components/desktop/PageWithAudiusValues'
 import { SIGN_UP_PAGE } from 'utils/route'
 
 import styles from './SignInPage.module.css'
@@ -43,62 +51,68 @@ export const SignInPage = () => {
   )
 
   return (
-    <Flex
-      className={styles.root}
-      h='100%'
-      w={480}
-      // want 80px but don't have var for it
-      pv='4xl'
-      ph='2xl'
-      direction='column'
-      gap='2xl'
-      justifyContent='space-between'
-    >
-      {/* TODO: confirm 40px spacing value */}
-      <Flex direction='column' gap='2xl' alignItems='center'>
-        <PreloadImage
-          src={audiusLogoColored}
-          className={styles.logo}
-          alt='Audius Colored Logo'
-        />
-        <Flex w='100%' direction='row' justifyContent='flex-start'>
-          <Text variant='heading' size='l' tag='h1' color='heading'>
-            {messages.title}
-          </Text>
-        </Flex>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          <Box w='100%'>
-            <Form>
-              <Flex direction='column' gap='2xl' w='100%'>
-                <Flex direction='column' gap='l'>
-                  <TextField name='email' label={messages.emailLabel} />
-                  <TextField
-                    name='password'
-                    label={messages.passwordLabel}
-                    type='password'
-                  />
-                </Flex>
-                <Flex direction='column' gap='l'>
-                  <Button text={messages.signIn} type='submit' />
-                  <Flex direction='row' alignItems='flexStart'>
-                    <Text color='heading'>
-                      {/* TODO: link destination */}
-                      <Link to={''}>{messages.forgotPassword}</Link>
-                    </Text>
+    <PageWithAudiusValues>
+      <Flex
+        className={styles.root}
+        h='100%'
+        w={480}
+        // want 80px but don't have var for it
+        pv='4xl'
+        ph='2xl'
+        direction='column'
+        gap='2xl'
+        justifyContent='space-between'
+      >
+        {/* TODO: confirm 40px spacing value */}
+        <Flex direction='column' gap='2xl' alignItems='center'>
+          <PreloadImage
+            src={audiusLogoColored}
+            className={styles.logo}
+            alt='Audius Colored Logo'
+          />
+          <Flex w='100%' direction='row' justifyContent='flex-start'>
+            <Text variant='heading' size='l' tag='h1' color='heading'>
+              {messages.title}
+            </Text>
+          </Flex>
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Box w='100%'>
+              <Form>
+                <Flex direction='column' gap='2xl' w='100%'>
+                  <Flex direction='column' gap='l'>
+                    <TextInput name='email' label={messages.emailLabel} />
+                    <TextInput
+                      name='password'
+                      label={messages.passwordLabel}
+                      type='password'
+                    />
+                  </Flex>
+                  <Flex direction='column' gap='l'>
+                    <Button
+                      text={messages.signIn}
+                      iconRight={IconArrowRight}
+                      type='submit'
+                    />
+                    <Flex direction='row' alignItems='flexStart'>
+                      <Text color='heading'>
+                        {/* TODO: link destination */}
+                        <Link to={''}>{messages.forgotPassword}</Link>
+                      </Text>
+                    </Flex>
                   </Flex>
                 </Flex>
-              </Flex>
-            </Form>
-          </Box>
-        </Formik>
+              </Form>
+            </Box>
+          </Formik>
+        </Flex>
+        <Button
+          // @ts-ignore
+          asChild={Link}
+          variant={ButtonType.SECONDARY}
+          to={SIGN_UP_PAGE}
+          text={messages.createAccount}
+        />
       </Flex>
-      <Button
-        // @ts-ignore
-        asChild={Link}
-        variant={ButtonType.SECONDARY}
-        to={SIGN_UP_PAGE}
-        text={messages.createAccount}
-      />
-    </Flex>
+    </PageWithAudiusValues>
   )
 }
