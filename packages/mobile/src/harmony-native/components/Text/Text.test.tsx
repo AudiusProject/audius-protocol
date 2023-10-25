0759020df45f5e8f9c571d5af8cd53355b376e14
Dylan Jeffers <dylan@audius.co>
2023-10-24 17:41:00 -0700

    [C-2892] Add native typography

--- packages/mobile/src/harmony-native/components/Text/Text.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react-native'
-
-import { Text } from './Text'
-
-test('renders text', () => {
-  render(<Text>hello world</Text>)
-
-  expect(screen.getByText(/hello world/i)).toBeOnTheScreen()
-})
--- packages/mobile/src/harmony-native/components/Text/Text.tsx
+++
@@ -1,8 +0,0 @@
-import type { TextProps as TextPropsBase } from 'react-native'
-import { Text as TextBase } from 'react-native'
-
-type TextProps = TextPropsBase
-
-export const Text = (props: TextProps) => {
-  return <TextBase {...props} />
-}
