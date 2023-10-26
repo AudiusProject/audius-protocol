24a0da07c26d2998dba227e24f4d03071c7c740c
JD Francis <jd@audius.co>
2023-10-26 14:08:01 -0500

    slight rework to implementation

--- packages/harmony/src/components/button/SocialButton/SocialButton.tsx
+++ packages/harmony/src/components/button/SocialButton/SocialButton.tsx
@@ -34,12 +34,27 @@ const getButtonLogo = (type: SocialMedia) => {
   }
 }
 
+const getSocialButtonProps = (type: SocialMedia): ButtonProps => {
+  switch (type) {
+    case 'instagram':
+      return { className: styles.instagram }
+    case 'tiktok':
+      return { hexColor: '#fe2c55' }
+    case 'twitter':
+      return { hexColor: '#1ba1f1' }
+    default:
+      return {}
+  }
+}
+
 export const SocialButton = (props: SocialButtonProps) => {
   const { socialType, className, ...rest } = props
+  const socialButtonProps = getSocialButtonProps(socialType)
   return (
     <Button
+      {...socialButtonProps}
       {...rest}
-      className={cn(styles.root, styles[socialType], className)}
+      className={cn(styles.root, socialButtonProps.className, className)}
     >
       {getButtonLogo(socialType)}
     </Button>
