3f1459c5cf669c7c742110c42794b6647338b6e7
JD Francis <jd.johndavidfrancis@gmail.com>
2023-10-26 15:37:14 -0500

    Implement SocialButton in Harmony (#6473)

---
+++ packages/harmony/src/components/button/SocialButton/SocialButton.tsx
@@ -0,0 +1,62 @@
+import cn from 'classnames'
+
+import { IconInstagram, IconTikTok, IconTwitter } from 'components/typography'
+
+import { Button } from '../Button/Button'
+import type { ButtonProps } from '../types'
+
+import styles from './SocialButton.module.css'
+
+type SocialMedia = 'tiktok' | 'instagram' | 'twitter'
+
+// Omitting aria-label from original type purely for showing in Storybook
+export type SocialButtonProps = Omit<ButtonProps, 'aria-label'> & {
+  /**
+   * Which social media.
+   */
+  socialType: SocialMedia
+  /**
+   * Aria label text. Required since these buttons just have icons
+   */
+  'aria-label': string
+}
+
+const getButtonLogo = (type: SocialMedia) => {
+  switch (type) {
+    case 'instagram':
+      return <IconInstagram />
+    case 'tiktok':
+      return <IconTikTok />
+    case 'twitter':
+      return <IconTwitter />
+    default:
+      return undefined
+  }
+}
+
+const getSocialButtonProps = (type: SocialMedia): ButtonProps => {
+  switch (type) {
+    case 'instagram':
+      return { className: styles.instagram }
+    case 'tiktok':
+      return { hexColor: '#fe2c55' }
+    case 'twitter':
+      return { hexColor: '#1ba1f1' }
+    default:
+      return {}
+  }
+}
+
+export const SocialButton = (props: SocialButtonProps) => {
+  const { socialType, className, ...rest } = props
+  const socialButtonProps = getSocialButtonProps(socialType)
+  return (
+    <Button
+      {...socialButtonProps}
+      {...rest}
+      className={cn(styles.root, socialButtonProps.className, className)}
+    >
+      {getButtonLogo(socialType)}
+    </Button>
+  )
+}
