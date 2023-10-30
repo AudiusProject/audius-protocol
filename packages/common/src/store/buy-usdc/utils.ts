3bea7829f6b3a732d26d5a34597348acdfd98491
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-10-30 22:56:38 +0100

    Buy Crypto via SOL (#6496)

--- packages/common/src/store/buy-usdc/utils.ts
+++ packages/common/src/store/buy-usdc/utils.ts
@@ -6,7 +6,8 @@ import {
   MAX_CONTENT_PRICE_CENTS,
   MAX_USDC_PURCHASE_AMOUNT_CENTS,
   MIN_CONTENT_PRICE_CENTS,
-  MIN_USDC_PURCHASE_AMOUNT_CENTS
+  MIN_USDC_PURCHASE_AMOUNT_CENTS,
+  BUY_TOKEN_VIA_SOL_SLIPPAGE_BPS
 } from 'services/remote-config/defaults'
 import { getContext } from 'store/effects'
 import { getFeePayer } from 'store/solana/selectors'
@@ -55,12 +56,18 @@ export function* getBuyUSDCRemoteConfig() {
       IntKeys.BUY_TOKEN_WALLET_POLL_MAX_RETRIES
     ) ?? undefined
 
+  // Only used in the BuyCryptoViaSol flow
+  const slippage =
+    remoteConfigInstance.getRemoteVar(IntKeys.BUY_TOKEN_VIA_SOL_SLIPPAGE_BPS) ??
+    BUY_TOKEN_VIA_SOL_SLIPPAGE_BPS
+
   return {
     minContentPriceCents,
     maxContentPriceCents,
     minUSDCPurchaseAmountCents,
     maxUSDCPurchaseAmountCents,
     maxRetryCount,
-    retryDelayMs
+    retryDelayMs,
+    slippage
   }
 }
