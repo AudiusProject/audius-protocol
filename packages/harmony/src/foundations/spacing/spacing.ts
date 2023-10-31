670016ac0bfaa8b932a201dbccaaec70deb4f3b9
Dylan Jeffers <dylan@audius.co>
2023-10-31 16:52:34 -0700

    [C-3227] Add emotion and theme to web and mobile (#6518)

---
+++ packages/harmony/src/foundations/spacing/spacing.ts
@@ -0,0 +1,45 @@
+export const spacing = {
+  /* --- Base Unit Variables --- */
+  unit: 4,
+  unitHalf: 2,
+  negativeUnit: -4,
+
+  /* --- 4*n Unit Variables --- */
+  unit1: 4,
+  unit2: 8,
+  unit3: 12,
+  unit4: 16,
+  unit5: 20,
+  unit6: 24,
+  unit7: 28,
+  unit8: 32,
+  unit9: 36,
+  unit10: 40,
+  unit11: 44,
+  unit12: 48,
+  unit13: 52,
+  unit14: 56,
+  unit15: 60,
+  unit16: 64,
+  unit17: 68,
+  unit18: 72,
+  unit19: 76,
+  unit20: 80,
+  unit21: 84,
+  unit22: 88,
+  unit23: 92,
+  unit24: 96,
+
+  /* --- Spacing Variables --- */
+  xs: 4,
+  s: 8,
+  m: 12,
+  l: 16,
+  xl: 24,
+  '2xl': 32,
+  '3xl': 48,
+  '4xl': 64
+}
+
+export type Spacing = typeof spacing
+export type SpacingOptions = keyof Spacing
