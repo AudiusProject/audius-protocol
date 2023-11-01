f8667a072b72b680ee28bf7a51d0621ed5ba0496
Dylan Jeffers <dylan@audius.co>
2023-11-01 12:55:54 -0700

    Fixes lint and typecheck (#6545)

--- packages/mobile/src/components/core/AddressTile.tsx
+++ packages/mobile/src/components/core/AddressTile.tsx
@@ -64,7 +64,7 @@ export const AddressTile = ({
     Clipboard.setString(address)
     toast({ content: messages.copied, type: 'info' })
     if (analytics) trackEvent(make(analytics))
-  }, [address, toast])
+  }, [address, analytics, toast])
 
   return (
     <View style={styles.addressContainer}>
