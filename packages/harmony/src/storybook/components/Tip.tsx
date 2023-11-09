a09fb823a2763ade08f7eaeda8574106461a0fc1
Dylan Jeffers <dylan@audius.co>
2023-11-09 10:11:06 -0800

    [C-3288] Add component statuses (#6624)

---
+++ packages/harmony/src/storybook/components/Tip.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from 'react'
+
+import { useTheme } from '@storybook/theming'
+
+import { Flex } from 'components'
+
+type TipProps = {
+  title: string
+  description: ReactNode
+  className?: string
+}
+
+export const Tip = (props: TipProps) => {
+  const { title, description, className } = props
+  const theme = useTheme()
+  return (
+    <Flex
+      borderRadius='m'
+      border='default'
+      pv='m'
+      ph='l'
+      gap='s'
+      direction='column'
+      w={984}
+      className={className}
+    >
+      <p
+        css={{
+          fontWeight: `${theme.typography.weight.bold} !important`,
+          margin: 0
+        }}
+      >
+        {title}
+      </p>
+      <p css={{ margin: 0 }}>{description}</p>
+    </Flex>
+  )
+}
