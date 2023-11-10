7e6e5c009c1ff3ea39d437f7b0037bd1091cd21e
Raymond Jacobson <ray@audius.co>
2023-11-10 12:52:30 -0800

    Remove logs

--- packages/probers/cypress.config.ts
+++ packages/probers/cypress.config.ts
@@ -18,9 +18,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       const configFile = config.env.configFile || 'dev'
-      console.log({configFile})
       const envConfig = require(`./cypress/config/${configFile}.json`)
-      console.log({envConfig})
       const mergedConfig = {...defaultConfig, ...envConfig}
       console.info(`Using config:\n ${JSON.stringify(mergedConfig, null, 2)}`)
       return mergedConfig
