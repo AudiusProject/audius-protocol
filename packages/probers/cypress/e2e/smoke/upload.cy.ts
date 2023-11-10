f9077326b67b43edb7d213f4e1954a12768153df
Raymond Jacobson <ray@audius.co>
2023-11-10 12:51:17 -0800

    Improve cypress config ergo and fix smoke tests after vite

--- packages/probers/cypress/e2e/smoke/upload.cy.ts
+++ packages/probers/cypress/e2e/smoke/upload.cy.ts
@@ -1,5 +1,4 @@
 import user from '../../fixtures/user.json'
-
 describe('Smoke test -- upload page', () => {
   beforeEach(() => {
     localStorage.setItem('HAS_REQUESTED_BROWSER_PUSH_PERMISSION', 'true')
@@ -9,7 +8,7 @@ describe('Smoke test -- upload page', () => {
     const base64Entropy = Buffer.from(user.entropy).toString('base64')
     cy.visit(`trending?login=${base64Entropy}`)
 
-    cy.findByText(user.name, { timeout: 10000 }).should('exist')
+    cy.findByText(user.name, { timeout: Cypress.env('initialLoadTimeout') }).should('exist')
 
     cy.visit('upload')
 
