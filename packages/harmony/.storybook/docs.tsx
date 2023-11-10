b70b56ed7d35f7fb68964912408b926073132516
Dylan Jeffers <dylan@audius.co>
2023-11-09 22:44:24 -0800

    Simplify work

--- packages/harmony/.storybook/docs.tsx
+++ packages/harmony/.storybook/docs.tsx
@@ -5,34 +5,17 @@ import { ThemeProvider } from '../src/foundations/theme'
 import { addons } from '@storybook/preview-api'
 import { UPDATE_DARK_MODE_EVENT_NAME, useDarkMode } from 'storybook-dark-mode'
 
-let previousTheme = undefined
-
 export const HarmonyDocsContainer = (props: DocsContainerProps) => {
   // @ts-ignore globals are available
   const currentTheme = props.context.store.globals.globals.theme || 'day'
   const isDark = useDarkMode()
 
   useEffect(() => {
-    if (!previousTheme) {
-      if (isDark && currentTheme === 'day') {
-        addons.getChannel().emit(UPDATE_DARK_MODE_EVENT_NAME)
-      } else if (!isDark && currentTheme !== 'day') {
-        addons.getChannel().emit(UPDATE_DARK_MODE_EVENT_NAME)
-      }
-    } else {
-      if (
-        (currentTheme === 'dark' || currentTheme === 'matrix') &&
-        previousTheme === 'day'
-      ) {
-        addons.getChannel().emit(UPDATE_DARK_MODE_EVENT_NAME)
-      } else if (
-        (previousTheme === 'dark' || previousTheme === 'matrix') &&
-        currentTheme === 'day'
-      ) {
-        addons.getChannel().emit(UPDATE_DARK_MODE_EVENT_NAME)
-      }
+    if (isDark && currentTheme === 'day') {
+      addons.getChannel().emit(UPDATE_DARK_MODE_EVENT_NAME)
+    } else if (!isDark && currentTheme !== 'day') {
+      addons.getChannel().emit(UPDATE_DARK_MODE_EVENT_NAME)
     }
-    previousTheme = currentTheme
   }, [currentTheme])
 
   return (
