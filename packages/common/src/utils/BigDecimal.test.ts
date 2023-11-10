51b3f6fe12a0a7cb6168801cbb9a7ff9a50fd582
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-11-09 21:51:25 -0800

    Create BigDecimal

---
+++ packages/common/src/utils/BigDecimal.test.ts
@@ -0,0 +1,232 @@
+import { AUDIO, BigDecimal, wAUDIO } from './BigDecimal'
+
+describe('BigDecimal', function () {
+  describe('constructor', function () {
+    it('constructs properly using bigint', function () {
+      expect(
+        new BigDecimal(
+          BigInt('1234567890123456789012345678901234567890')
+        ).toString()
+      ).toBe('1234567890123456789012345678901234567890')
+    })
+    it('constructs properly using negative bigint', function () {
+      expect(
+        new BigDecimal(
+          BigInt('-1234567890123456789012345678901234567890')
+        ).toString()
+      ).toBe('-1234567890123456789012345678901234567890')
+    })
+    it('constructs properly using bigint and decimal places', function () {
+      expect(
+        new BigDecimal(
+          BigInt('1234567890123456789012345678901234567890'),
+          10
+        ).toString()
+      ).toBe('123456789012345678901234567890.1234567890')
+    })
+    it('constructs properly using string', function () {
+      expect(new BigDecimal('123.456').toString()).toBe('123.456')
+    })
+    it('constructs properly using negative string', function () {
+      expect(new BigDecimal('-123.456').toString()).toBe('-123.456')
+    })
+    it('constructs properly using string and decimal places', function () {
+      expect(new BigDecimal('123.456').toString()).toBe('123.456')
+    })
+    it('constructs properly using number', function () {
+      expect(new BigDecimal(123.456).toString()).toBe('123.456')
+    })
+    it('constructs properly using negative number', function () {
+      expect(new BigDecimal(-123.456).toString()).toBe('-123.456')
+    })
+    it('constructs properly using another BigDecimal', function () {
+      expect(new BigDecimal(new BigDecimal(1, 3)).toString()).toBe('1.000')
+    })
+    it('constructs properly using another negative BigDecimal', function () {
+      expect(new BigDecimal(new BigDecimal(-1, 3)).toString()).toBe('-1.000')
+    })
+    it('constructs properly using another BigDecimal and decimal places', function () {
+      expect(new BigDecimal(new BigDecimal(1, 3), 10).toString()).toBe(
+        '1.0000000000'
+      )
+    })
+    it('throws if using a number that uses scientific notation', function () {
+      expect(() => new BigDecimal(0.00000000000000001).toString()).toThrow(
+        'Number must not be in scientific notation'
+      )
+    })
+    it('throws if using NaN', function () {
+      expect(() => new BigDecimal(NaN).toString()).toThrow(
+        'Number must be finite'
+      )
+    })
+    it('throws if using infinite number', function () {
+      expect(() => new BigDecimal(Number.POSITIVE_INFINITY).toString()).toThrow(
+        'Number must be finite'
+      )
+    })
+    it('throws if using bad string', function () {
+      expect(() => new BigDecimal('hello').toString()).toThrow()
+    })
+  })
+  describe('floor', function () {
+    it('floors positive decimals correctly', function () {
+      expect(new BigDecimal(1.2345).floor().toString()).toBe('1.0000')
+    })
+    it('floors negative decimals correctly', function () {
+      expect(new BigDecimal(-1.2345).floor().toString()).toBe('-2.0000')
+    })
+    it('floors 0 correctly', function () {
+      expect(new BigDecimal(0, 3).floor().toString()).toBe('0.000')
+    })
+    it('floors whole numbers correctly', function () {
+      expect(new BigDecimal(4, 3).floor().toString()).toBe('4.000')
+    })
+    it('floors large numbers correctly', function () {
+      expect(
+        new BigDecimal(BigInt('1234567890123456789099999999999999999999'), 20)
+          .floor()
+          .toString()
+      ).toBe('12345678901234567890.00000000000000000000')
+    })
+    it('floors tiny numbers correctly', function () {
+      expect(new BigDecimal(BigInt('1'), 20).floor().toString()).toBe(
+        '0.00000000000000000000'
+      )
+    })
+    it('floors to arbitrary decimal places correctly', function () {
+      expect(
+        new BigDecimal(BigInt('1234567890123456789099999999999999999999'), 20)
+          .floor(2)
+          .toString()
+      ).toBe('12345678901234567890.99000000000000000000')
+    })
+    it('throws when decimal places are out of range', function () {
+      expect(() =>
+        new BigDecimal(BigInt('1234567890123456789099999999999999999999'), 20)
+          .floor(50)
+          .toString()
+      ).toThrow('Digits must be non-negative')
+    })
+    it('floors to zero if decimal places is beyond number', function () {
+      expect(new BigDecimal(BigInt('12345'), 3).floor(-10).toString()).toBe(
+        '0.000'
+      )
+    })
+  })
+
+  describe('ceil', function () {
+    it('ceils positive decimals correctly', function () {
+      expect(new BigDecimal(1.2345).ceil().toString()).toBe('2.0000')
+    })
+    it('ceils negative decimals correctly', function () {
+      expect(new BigDecimal(-1.2345).ceil().toString()).toBe('-1.0000')
+    })
+    it('ceils 0 correctly', function () {
+      expect(new BigDecimal(0, 3).ceil().toString()).toBe('0.000')
+    })
+    it('ceils whole numbers correctly', function () {
+      expect(new BigDecimal(4, 3).ceil().toString()).toBe('4.000')
+    })
+    it('ceils large numbers correctly', function () {
+      expect(
+        new BigDecimal(BigInt('1234567890123456789099999999999999999999'), 20)
+          .ceil()
+          .toString()
+      ).toBe('12345678901234567891.00000000000000000000')
+    })
+    it('ceils tiny numbers correctly', function () {
+      expect(new BigDecimal(BigInt('1'), 20).ceil().toString()).toBe(
+        '1.00000000000000000000'
+      )
+    })
+    it('ceils to arbitrary decimal places correctly', function () {
+      expect(
+        new BigDecimal(BigInt('1234567890123456789000000000000000000001'), 20)
+          .ceil(2)
+          .toString()
+      ).toBe('12345678901234567890.01000000000000000000')
+    })
+    it('throws when decimal places are out of range', function () {
+      expect(() =>
+        new BigDecimal(BigInt('1234567890123456789000000000000000000001'), 20)
+          .ceil(50)
+          .toString()
+      ).toThrow('Digits must be non-negative')
+    })
+    it('ceils to 1s place of represented place if decimal places is beyond number', function () {
+      expect(new BigDecimal(BigInt('12345'), 3).ceil(-10).toString()).toBe(
+        '10000000000.000'
+      )
+    })
+  })
+
+  describe('toPrecision', function () {
+    it('work on positive numbers', function () {
+      expect(new BigDecimal('1234.56789').toPrecision(7).toString()).toBe(
+        '1234.56700'
+      )
+    })
+    it('works on negative numbers', function () {
+      expect(new BigDecimal('-1234.56789').toPrecision(7).toString()).toBe(
+        '-1234.56700'
+      )
+    })
+    it('works when precision is more than the number of digits', function () {
+      expect(new BigDecimal('-1234.56789').toPrecision(100).toString()).toBe(
+        '-1234.56789'
+      )
+    })
+    it('works when precision is less than the number of decimal places', function () {
+      expect(new BigDecimal('-1234.56789').toPrecision(2).toString()).toBe(
+        '-1200.00000'
+      )
+    })
+  })
+
+  describe('toFixed', function () {
+    it('rounds up positive numbers when appropriate', function () {
+      expect(new BigDecimal('1234.5678').toFixed(2)).toBe('1234.57')
+      expect(new BigDecimal('1234.5678').toFixed()).toBe('1235')
+    })
+    it('rounds down positive numbers when appropriate', function () {
+      expect(new BigDecimal('8765.4321').toFixed(2)).toBe('8765.43')
+      expect(new BigDecimal('8765.4321').toFixed()).toBe('8765')
+    })
+    it('rounds up negative numbers when appropriate', function () {
+      expect(new BigDecimal('-1234.5678').toFixed(2)).toBe('-1234.57')
+      expect(new BigDecimal('-1234.5678').toFixed()).toBe('-1235')
+    })
+    it('rounds down negative numbers when appropriate', function () {
+      expect(new BigDecimal('-8765.4321').toFixed(2)).toBe('-8765.43')
+      expect(new BigDecimal('-8765.4321').toFixed()).toBe('-8765')
+    })
+    it('has no decimal places when called without args', function () {
+      expect(new BigDecimal('1', 5).toFixed()).toBe('1')
+    })
+    it('has as many decimal places as specified', function () {
+      expect(new BigDecimal('1', 5).toFixed(3)).toBe('1.000')
+    })
+    it('adds additional decimal places if beyond the precision of the BigDecimal', function () {
+      expect(new BigDecimal('1').toFixed(3)).toBe('1.000')
+    })
+    it('throws when using non-negative decimal places', function () {
+      expect(() => new BigDecimal('1').toFixed(-1)).toThrow(
+        'decimalPlaces must be non-negative'
+      )
+    })
+  })
+
+  describe('helpers', function () {
+    it('converts wAUDIO to AUDIO', function () {
+      expect(AUDIO(wAUDIO(BigInt('12345678901234567890'))).toString()).toBe(
+        '123456789012.345678900000000000'
+      )
+    })
+    it('converts AUDIO to wAUDIO', function () {
+      expect(wAUDIO(AUDIO(BigInt('12345678901234567890'))).toString()).toBe(
+        '12.34567890'
+      )
+    })
+  })
+})
---
+++ packages/common/src/utils/BigDecimal.ts
@@ -0,0 +1,213 @@
+const parseBigDecimalString = (value: string, decimalPlaces?: number) => {
+  let [whole, decimal] = value.split('.')
+  decimal = decimal ?? ''
+  if (decimalPlaces !== undefined) {
+    decimal = decimal.padEnd(decimalPlaces, '0').substring(0, decimalPlaces)
+  }
+  return {
+    value: BigInt(`${whole}${decimal}`),
+    decimalPlaces: decimalPlaces ?? decimal.length
+  }
+}
+
+type BigDecimalCtorArgs = {
+  value: bigint
+  decimalPlaces: number
+}
+
+/**
+ * BigDecimal uses a BigInt and the number of decimal digits to represent a
+ * fixed precision decimal number. It's useful for representing currency,
+ * especially cryptocurrency, as balances and amounts are quite large but still
+ * have decimal representations and formats.
+ *
+ * This class is not meant to be constructed manually, but rather using helpers
+ * such as {@link AUDIO}, {@link wAUDIO}, {@link SOL}, and {@link USDC}.
+ *
+ * This class is not meant to be persisted and have math operated on it. It's
+ * primarily convenience utilites for formatting these large numbers according
+ * to their decimal counts. To do math on BigDecimal, do math on the internal
+ * value instead.
+ *
+ * @example
+ * // Math on values
+ * wAUDIO(4).value + wAUDIO(5).value // 900000000n
+ *
+ * @example
+ * // Automatically formats to fixed precision decimals
+ * SOL(1).toString() // '1.000000000'
+ *
+ * @example
+ * // Convert USDC to cents
+ * USDC(1.32542).toFixed(2) // '1.33'
+ *
+ * @example
+ * // Get the number of decimals in AUDIO
+ * AUDIO(0).decimalPlaces // 18
+ *
+ * @see {@link AUDIO} for the Ethereum ERC-20 AUDIO token
+ * @see {@link wAUDIO} for the Solana SPL "wrapped" AUDIO token
+ * @see {@link SOL} for the Solana native SOL token
+ * @see {@link USDC} for the Solana Circle USDC stablecoin token
+ */
+export class BigDecimal {
+  public value: bigint
+  public decimalPlaces: number
+  constructor(
+    value: BigDecimalCtorArgs | bigint | number | string,
+    decimalPlaces?: number
+  ) {
+    switch (typeof value) {
+      case 'number': {
+        if (!Number.isFinite(value)) {
+          throw new Error('Number must be finite')
+        }
+        if (value.toString() === value.toExponential()) {
+          throw new Error('Number must not be in scientific notation')
+        }
+        const parsed = parseBigDecimalString(value.toString(), decimalPlaces)
+        this.value = parsed.value
+        this.decimalPlaces = parsed.decimalPlaces
+        break
+      }
+      case 'string': {
+        const parsed = parseBigDecimalString(value, decimalPlaces)
+        this.value = parsed.value
+        this.decimalPlaces = parsed.decimalPlaces
+        break
+      }
+      case 'object': {
+        if (value instanceof BigDecimal) {
+          const parsed = parseBigDecimalString(value.toString(), decimalPlaces)
+          this.value = parsed.value
+          this.decimalPlaces = parsed.decimalPlaces
+        } else {
+          this.value = value.value
+          this.decimalPlaces = value.decimalPlaces
+        }
+        break
+      }
+      default:
+        this.value = value
+        this.decimalPlaces = decimalPlaces ?? 0
+    }
+  }
+
+  public ceil(decimalPlaces?: number) {
+    const digits = this.decimalPlaces - (decimalPlaces ?? 0)
+    return this._ceil(digits)
+  }
+
+  private _ceil(digits?: number) {
+    const digitsCount = digits ?? this.decimalPlaces
+    if (digitsCount < 0) {
+      throw new RangeError('Digits must be non-negative')
+    }
+    const divisor = BigInt(10 ** digitsCount)
+    const bump = this.value % divisor > 0 ? BigInt(1) : BigInt(0)
+    return new BigDecimal({
+      value: (this.value / divisor + bump) * divisor,
+      decimalPlaces: this.decimalPlaces
+    })
+  }
+
+  public floor(decimalPlaces?: number) {
+    const digits = this.decimalPlaces - (decimalPlaces ?? 0)
+    return this._floor(digits)
+  }
+
+  private _floor(digits?: number) {
+    const digitsCount = digits ?? this.decimalPlaces
+    if (digitsCount < 0) {
+      throw new RangeError('Digits must be non-negative')
+    }
+    const divisor = BigInt(10 ** digitsCount)
+    const signOffset =
+      this.value < 0 && digitsCount > 0
+        ? BigInt(-1 * 10 ** digitsCount)
+        : BigInt(0)
+    return new BigDecimal({
+      value: (this.value / divisor) * divisor + signOffset,
+      decimalPlaces: this.decimalPlaces
+    })
+  }
+
+  public toPrecision(significantDigits: number) {
+    const signOffset = this.value < 0 ? -1 : 0
+    const digits = Math.max(
+      this.value.toString().length - significantDigits + signOffset,
+      0
+    )
+    return this.value > 0 ? this._floor(digits) : this._ceil(digits)
+  }
+
+  public toFixed(decimalPlaces?: number) {
+    const decimalCount = decimalPlaces ?? 0
+    const [whole, decimalOrUndefined] = this.toString().split('.')
+    const decimal = (decimalOrUndefined ?? '').padEnd(decimalCount + 1, '0')
+    const decimalTruncated = decimal.substring(0, decimalCount - 1)
+
+    const digitToRound =
+      decimalCount > 0
+        ? Number(decimal[decimalCount - 1])
+        : Number(whole[whole.length - 1])
+    const testDigit = Number(decimal[decimalCount])
+    const roundedDigit = testDigit >= 5 ? digitToRound + 1 : digitToRound
+
+    if (decimalCount > 0) {
+      return `${whole}.${decimalTruncated}${roundedDigit}`
+    } else if (decimalCount === 0) {
+      return whole.substring(0, whole.length - 1) + roundedDigit
+    } else {
+      throw new Error('decimalPlaces must be non-negative')
+    }
+  }
+
+  /**
+   * @override
+   */
+  public toString() {
+    const str = this.value.toString().padStart(this.decimalPlaces + 1, '0')
+    return this.decimalPlaces > 0
+      ? `${str.substring(0, str.length - this.decimalPlaces)}.${str.substring(
+          str.length - this.decimalPlaces
+        )}`
+      : str
+  }
+}
+
+const createTokenConstructor =
+  (decimalPlaces: number) => (value: string | number | BigDecimal | bigint) =>
+    new BigDecimal(value, decimalPlaces)
+
+/**
+ * Constructs a BigDecimal representing an amount of Ethereum ERC-20 AUDIO
+ * tokens, which have 18 decimal places.
+ *
+ * Used on the protocol dashboard and in the governance and staking systems.
+ * Also used for balance totals after adding linked wallets on the Rewards page.
+ */
+export const AUDIO = createTokenConstructor(18)
+/**
+ * Constructs a BigDecimal representing an amount of Solana SPL AUDIO tokens,
+ * which have 8 decimal places.
+ *
+ * Used for in-app experiences, like tipping and rewards.
+ */
+export const wAUDIO = createTokenConstructor(8)
+/**
+ * Constructs a BigDecimal representing an amount of Solana native SOL tokens,
+ * which have 9 decimal places.
+ *
+ * Used as an intermediary token for purchasing wAUDIO and for paying for fees
+ * of Solana transactions on the platform.
+ */
+export const SOL = createTokenConstructor(9)
+/**
+ * Constructs a BigDecimal representing an amount of Solana SPL USDC tokens,
+ * which have 6 decimal places.
+ *
+ * Used for purchasing content in-app, and getting "USD" prices via Jupiter
+ * for the wAUDIO token and SOL.
+ */
+export const USDC = createTokenConstructor(6)
