8c7d756dc88c1ac37610d6d13fa355cb658e98d8
Dylan Jeffers <dylan@audius.co>
2023-11-09 21:19:15 -0800

    [C-3325] Selectable pill docs

--- packages/harmony/src/components/input/SelectablePill/SelectablePill.stories.tsx
+++ packages/harmony/src/components/input/SelectablePill/SelectablePill.stories.tsx
@@ -1,42 +1,62 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { Flex, Text } from 'components'
 import { IconHeart } from 'icons'
 
 import { SelectablePill } from './SelectablePill'
 
 const meta: Meta<typeof SelectablePill> = {
   title: 'Components/Input/SelectablePill',
-  component: SelectablePill
+  component: SelectablePill,
+  args: {
+    label: 'Label'
+  }
 }
 
 export default meta
 
 type Story = StoryObj<typeof SelectablePill>
 
-export const Default: Story = {
-  args: {
-    label: 'Option'
-  }
+export const Primary: Story = {
+  render: (props) => (
+    <Flex gap='4xl' alignItems='center'>
+      <SelectablePill {...props} size='default' />
+      <SelectablePill {...props} size='large' />
+    </Flex>
+  )
 }
 
-export const Large: Story = {
-  args: {
-    label: 'Option',
-    size: 'large'
-  }
+export const Size: Story = {
+  render: (props) => (
+    <Flex direction='column' gap='2xl'>
+      <Flex gap='4xl' alignItems='center'>
+        <Text>Small</Text>
+        <SelectablePill {...props} size='default' />
+      </Flex>
+      <Flex gap='4xl' alignItems='center'>
+        <Text>Large</Text>
+        <SelectablePill {...props} size='large' />
+      </Flex>
+    </Flex>
+  )
 }
 
-export const WithIcon: Story = {
-  args: {
-    label: 'Option',
-    icon: IconHeart
-  }
+export const States: Story = {
+  render: () => (
+    <Flex gap='4xl' alignItems='center'>
+      <SelectablePill label='Default' />
+      <SelectablePill label='Hover' _isHovered />
+      <SelectablePill label='Active' isSelected />
+      <SelectablePill label='Disabled' isSelected={false} disabled />
+      <SelectablePill label='Active Disabled' isSelected disabled />
+    </Flex>
+  )
 }
 
-export const LargeWithIcon: Story = {
-  args: {
-    label: 'Option',
-    size: 'large',
-    icon: IconHeart
-  }
+export const PillAsMenu: Story = {
+  render: () => <Text>TODO</Text>
+}
+
+export const LeadingIcon: Story = {
+  render: (props) => <SelectablePill icon={IconHeart} {...props} />
 }
