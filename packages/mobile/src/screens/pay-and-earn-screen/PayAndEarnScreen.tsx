1a5591c2e196998c0a67fa37b012ef51d1fbec69
Saliou Diallo <saliou@audius.co>
2023-11-13 23:30:28 -0500

    Add usdc card on pay and earn screen

--- packages/mobile/src/screens/pay-and-earn-screen/PayAndEarnScreen.tsx
+++ packages/mobile/src/screens/pay-and-earn-screen/PayAndEarnScreen.tsx
@@ -1,5 +1,172 @@
+import React, { useCallback } from 'react'
+
+import type { BNUSDC } from '@audius/common'
+import {
+  Status,
+  combineStatuses,
+  formatCurrencyBalance,
+  formatUSDCWeiToFloorCentsNumber,
+  useUSDCBalance
+} from '@audius/common'
+import BN from 'bn.js'
 import { View } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+
+import IconQuestionCircle from 'app/assets/images/iconQuestionCircle.svg'
+import LogoUSDCInverted from 'app/assets/images/logoUSDCInverted.svg'
+import {
+  ScrollView,
+  Screen,
+  Button,
+  Text,
+  ScreenContent
+} from 'app/components/core'
+import LoadingSpinner from 'app/components/loading-spinner'
+import { makeStyles, flexRowCentered } from 'app/styles'
+import { spacing } from 'app/styles/spacing'
+import { useColor } from 'app/utils/theme'
+
+const messages = {
+  title: 'PAYMENTs & EARNINGS',
+  buyAndSell: 'Buy and sell music with USDC.',
+  learnMore: 'Learn More',
+  addFunds: 'Add Funds'
+}
+
+const useStyles = makeStyles(({ spacing, palette, typography }) => ({
+  root: {
+    paddingVertical: spacing(8),
+    paddingHorizontal: spacing(3)
+  },
+  spinner: {
+    width: spacing(8),
+    height: spacing(8)
+  },
+  card: {},
+  cardHeader: {
+    padding: spacing(6),
+    borderTopLeftRadius: spacing(3),
+    borderTopRightRadius: spacing(3),
+    backgroundColor: palette.accentOrange
+  },
+  logoContainer: {
+    ...flexRowCentered(),
+    gap: spacing(1)
+  },
+  usdc: {
+    fontSize: spacing(5),
+    fontWeight: '900',
+    color: palette.white,
+    opacity: 0.8
+  },
+  balance: {
+    fontSize: spacing(10.5),
+    fontWeight: '900',
+    color: palette.white,
+    lineHeight: spacing(13)
+  },
+  buyAndSell: {
+    fontSize: spacing(4),
+    fontWeight: '500',
+    color: palette.white,
+    marginTop: spacing(2)
+  },
+  learnMore: {
+    ...flexRowCentered(),
+    gap: spacing(1),
+    marginTop: spacing(2)
+  },
+  cardButtons: {
+    padding: spacing(6),
+    borderBottomLeftRadius: spacing(3),
+    borderBottomRightRadius: spacing(3),
+    backgroundColor: palette.white
+  },
+  addFundsButton: {
+    marginHorizontal: spacing(6)
+  }
+}))
+
+const USDCCard = () => {
+  const styles = useStyles()
+  const white = useColor('white')
+  const statuses: Status[] = []
+  const { data: usdcBalance, balanceStatus: usdcBalanceStatus } =
+    useUSDCBalance({
+      isPolling: true,
+      pollingInterval: 3000
+    })
+  if (usdcBalance === null) {
+    statuses.push(usdcBalanceStatus)
+  }
+  const usdcBalanceStatusCombined = combineStatuses(statuses)
+  const isUsdcBalanceLoading =
+    usdcBalance === null || usdcBalanceStatusCombined === Status.LOADING
+  const balanceCents = formatUSDCWeiToFloorCentsNumber(
+    (usdcBalance ?? new BN(0)) as BNUSDC
+  )
+  const usdcBalanceFormatted = formatCurrencyBalance(balanceCents / 100)
+
+  // TODO: Implement
+  const handleAddFunds = useCallback(() => {}, [])
+
+  if (isUsdcBalanceLoading) {
+    return <LoadingSpinner style={[styles.spinner]} />
+  }
+
+  return (
+    <View style={styles.card}>
+      <LinearGradient
+        style={styles.cardHeader}
+        colors={['#2775CAdd', '#2775CA']}
+        useAngle
+        angle={90}
+      >
+        <View style={styles.logoContainer}>
+          <LogoUSDCInverted height={spacing(5)} width={spacing(5)} />
+          <Text style={styles.usdc}>USDC</Text>
+        </View>
+        <Text style={styles.balance}>${usdcBalanceFormatted}</Text>
+        <Text style={styles.buyAndSell}>{messages.buyAndSell}</Text>
+        <View style={styles.learnMore}>
+          <IconQuestionCircle
+            height={spacing(4)}
+            width={spacing(4)}
+            fill={white}
+          />
+          <Text fontSize='small' weight='bold' color='white'>
+            {messages.learnMore}
+          </Text>
+        </View>
+      </LinearGradient>
+      <View style={styles.cardButtons}>
+        <Button
+          style={styles.addFundsButton}
+          title={messages.addFunds}
+          onPress={handleAddFunds}
+          variant='common'
+          size='large'
+          fullWidth
+        />
+      </View>
+    </View>
+  )
+}
 
 export const PayAndEarnScreen = () => {
-  return <View></View>
+  const styles = useStyles()
+  return (
+    <Screen
+      url='/payandearn'
+      variant='secondary'
+      title={messages.title}
+      style={styles.root}
+    >
+      <ScreenContent>
+        <ScrollView>
+          <USDCCard />
+        </ScrollView>
+      </ScreenContent>
+    </Screen>
+  )
 }
