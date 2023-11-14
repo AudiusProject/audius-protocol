407e3a35f2e334fbdfc413d3e6a36592e897646f
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-11-13 18:08:50 -0800

    placeholder images; spacing fixes

--- packages/web/src/pages/sign-up-page/components/FollowArtistTile.tsx
+++ packages/web/src/pages/sign-up-page/components/FollowArtistTile.tsx
@@ -1,6 +1,6 @@
 import { HTMLProps } from 'react'
 
-import { UserMetadata, imageProfilePicEmpty } from '@audius/common'
+import { UserMetadata } from '@audius/common'
 import {
   Avatar,
   Box,
@@ -13,34 +13,30 @@ import {
   Text
 } from '@audius/harmony'
 
+import audiusCoverPhoto from 'assets/img/4-Conductor-16-9.jpg'
+import audiusProfilePic from 'assets/img/appIcon240.png'
+
 type FollowArtistTileProps = {
   user: UserMetadata
 } & HTMLProps<HTMLInputElement>
 
 const FollowArtistTile = (props: FollowArtistTileProps) => {
   const {
-    user: {
-      user_id,
-      name,
-      cover_photo,
-      profile_picture_sizes,
-      is_verified,
-      track_count,
-      follower_count
-    },
+    user: { user_id, name, is_verified, track_count, follower_count },
     checked,
     onChange
   } = props
   return (
     <Paper css={{ minWidth: 200 }}>
-      <Flex w='100%' h='100%' direction='column' alignItems='center'>
-        <Box w='100%' h={68} css={{ backgroundImage: `url(${cover_photo})` }} />
-        <Box w={72} h={72}>
-          <Avatar
-            variant='strong'
-            src={profile_picture_sizes ?? imageProfilePicEmpty}
-          />
+      <Flex w='100%' h='100%' direction='column' alignItems='center' gap='3xl'>
+        <Box w={72} h={72} css={{ position: 'absolute', top: 34 }}>
+          <Avatar variant='strong' src={audiusProfilePic} />
         </Box>
+        <Box
+          w='100%'
+          h={68}
+          css={{ backgroundImage: `url(${audiusCoverPhoto})` }}
+        />
         <Flex direction='column' alignItems='center' gap='l' p='s'>
           <Flex direction='column' alignItems='center' gap='s'>
             <Flex direction='row' gap='xs' alignItems='center'>
