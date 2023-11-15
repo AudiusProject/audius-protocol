7ec407dcd9e108281432486e68e20dfccb3befb4
Saliou Diallo <sddioulde@hotmail.com>
2023-11-15 00:59:21 -0500

    [PAY-2165][PAY-2163] Add payments and earnings to mobile (#6688)
    Co-authored-by: Saliou Diallo <saliou@audius.co>

--- packages/mobile/src/screens/app-drawer-screen/left-nav-drawer/LeftNavDrawer.tsx
+++ packages/mobile/src/screens/app-drawer-screen/left-nav-drawer/LeftNavDrawer.tsx
@@ -1,28 +1,44 @@
-import type { User } from '@audius/common'
+import { useMemo } from 'react'
+
+import type { BNUSDC, BNWei, User } from '@audius/common'
 import {
+  Status,
   FeatureFlags,
   StringKeys,
   accountSelectors,
   useAccountHasClaimableRewards,
   chatSelectors,
-  Name
+  Name,
+  useSelectTierInfo,
+  isNullOrUndefined,
+  formatWei,
+  walletSelectors,
+  formatCurrencyBalance,
+  formatUSDCWeiToFloorCentsNumber,
+  useUSDCBalance
 } from '@audius/common'
 import type { DrawerContentComponentProps } from '@react-navigation/drawer'
 import { DrawerContentScrollView } from '@react-navigation/drawer'
+import BN from 'bn.js'
 import { View } from 'react-native'
 import Config from 'react-native-config'
 import { useSelector } from 'react-redux'
 
 import IconCrown from 'app/assets/images/iconCrown.svg'
+import IconDonate from 'app/assets/images/iconDonate.svg'
 import IconEmbed from 'app/assets/images/iconEmbed.svg'
 import IconListeningHistory from 'app/assets/images/iconListeningHistory.svg'
 import IconMessage from 'app/assets/images/iconMessage.svg'
 import IconSettings from 'app/assets/images/iconSettings.svg'
 import IconUpload from 'app/assets/images/iconUpload.svg'
 import IconUser from 'app/assets/images/iconUser.svg'
+import LogoUSDC from 'app/assets/images/logoUSDC.svg'
+import { IconAudioBadge } from 'app/components/audio-rewards'
+import { Text } from 'app/components/core'
+import Skeleton from 'app/components/skeleton'
 import { useFeatureFlag, useRemoteVar } from 'app/hooks/useRemoteConfig'
 import { make, track } from 'app/services/analytics'
-import { makeStyles } from 'app/styles'
+import { flexRowCentered, makeStyles } from 'app/styles'
 import { spacing } from 'app/styles/spacing'
 
 import { AppDrawerContextProvider } from '../AppDrawerContext'
@@ -33,13 +49,15 @@ import { VanityMetrics } from './VanityMetrics'
 
 const { getAccountUser } = accountSelectors
 const { getHasUnreadMessages } = chatSelectors
+const { getAccountTotalBalance } = walletSelectors
 
 const isStaging = Config.ENVIRONMENT === 'staging'
 
 const messages = {
-  profile: 'Profile',
-  audio: '$AUDIO & Rewards',
-  upload: 'Upload a Track',
+  profile: 'Your Profile',
+  payAndEarn: 'Pay & Earn',
+  rewards: 'Rewards',
+  upload: 'Upload',
   listeningHistory: 'Listening History',
   settings: 'Settings',
   featureFlags: 'Feature Flags'
@@ -51,14 +69,44 @@ type AccountDrawerProps = DrawerContentComponentProps & {
 }
 
 const useStyles = makeStyles(({ spacing, palette }) => ({
-  notificationBubble: {
-    height: spacing(3),
-    width: spacing(3),
-    borderRadius: spacing(3),
-    backgroundColor: palette.secondary,
-    marginLeft: spacing(2)
+  tokens: {
+    ...flexRowCentered(),
+    padding: spacing(0.5),
+    marginLeft: spacing(4),
+    borderWidth: 1,
+    borderColor: palette.neutralLight8,
+    backgroundColor: palette.neutralLight10,
+    borderRadius: spacing(25),
+    gap: spacing(2)
+  },
+  tokenAmount: {
+    paddingRight: spacing(1.5),
+    paddingVertical: spacing(0.5)
   }
 }))
+
+/**
+ * Pulls balances from account and wallet selectors. Will prefer the wallet
+ * balance once it has loaded. Otherwise, will return the account balance if
+ * available. Falls back to 0 if neither wallet or account balance are available.
+ */
+const useTotalBalanceWithFallback = () => {
+  const account = useSelector(getAccountUser)
+  const walletTotalBalance = useSelector(getAccountTotalBalance)
+
+  return useMemo(() => {
+    if (!isNullOrUndefined(walletTotalBalance)) {
+      return walletTotalBalance
+    } else if (
+      !isNullOrUndefined(account) &&
+      !isNullOrUndefined(account.total_balance)
+    ) {
+      return new BN(account.total_balance) as BNWei
+    }
+
+    return null
+  }, [account, walletTotalBalance])
+}
 
 export const LeftNavDrawer = (props: AccountDrawerProps) => {
   const { navigation: drawerHelpers, ...other } = props
@@ -81,17 +129,25 @@ const WrappedLeftNavDrawer = () => {
   const { isEnabled: isFeatureFlagAccessEnabled } = useFeatureFlag(
     FeatureFlags.FEATURE_FLAG_ACCESS
   )
+  const accountUser = useSelector(getAccountUser) as User
+  const { user_id } = accountUser
+  const { tier } = useSelectTierInfo(user_id)
+  const audioBalance = useTotalBalanceWithFallback()
+  const isAudioBalanceLoading = isNullOrUndefined(audioBalance)
+
+  const { data: usdcBalance, balanceStatus: usdcBalanceStatus } =
+    useUSDCBalance({ isPolling: false })
+  const isUsdcBalanceLoading =
+    usdcBalance === null || usdcBalanceStatus === Status.LOADING
+  const balanceCents = formatUSDCWeiToFloorCentsNumber(
+    (usdcBalance ?? new BN(0)) as BNUSDC
+  )
+  const usdcBalanceFormatted = formatCurrencyBalance(balanceCents / 100)
 
   return (
     <DrawerContentScrollView>
       <AccountDetails />
       <VanityMetrics />
-      <LeftNavLink
-        icon={IconUser}
-        label={messages.profile}
-        to='Profile'
-        params={{ handle: 'accountUser' }}
-      />
       {isChatEnabled ? (
         <LeftNavLink
           icon={IconMessage}
@@ -101,21 +157,52 @@ const WrappedLeftNavDrawer = () => {
           onPress={() => {
             track(make({ eventName: Name.CHAT_ENTRY_POINT, source: 'navmenu' }))
           }}
-        >
-          {hasUnreadMessages ? (
-            <View style={styles.notificationBubble} />
-          ) : null}
-        </LeftNavLink>
+          showNotificationBubble={hasUnreadMessages}
+        />
       ) : null}
       <LeftNavLink
-        icon={IconCrown}
-        label={messages.audio}
-        to='AudioScreen'
+        icon={IconDonate}
+        label={messages.payAndEarn}
+        to='PayAndEarnScreen'
         params={null}
       >
-        {hasClaimableRewards ? (
-          <View style={styles.notificationBubble} />
-        ) : null}
+        <View style={styles.tokens}>
+          <LogoUSDC height={spacing(5)} width={spacing(5)} />
+          {isUsdcBalanceLoading ? (
+            <Skeleton
+              style={styles.tokenAmount}
+              height={spacing(4.5)}
+              width={spacing(6)}
+            />
+          ) : (
+            <Text style={styles.tokenAmount} fontSize='small' weight='bold'>
+              ${usdcBalanceFormatted}
+            </Text>
+          )}
+        </View>
+      </LeftNavLink>
+      <LeftNavLink
+        icon={IconCrown}
+        label={messages.rewards}
+        to='AudioScreen'
+        params={null}
+        showNotificationBubble={hasClaimableRewards}
+      >
+        <View style={styles.tokens}>
+          <IconAudioBadge
+            tier={tier}
+            showNoTier
+            height={spacing(5)}
+            width={spacing(5)}
+          />
+          {isAudioBalanceLoading ? (
+            <Skeleton style={styles.tokenAmount} height={18} width={24} />
+          ) : (
+            <Text style={styles.tokenAmount} fontSize='small' weight='bold'>
+              {formatWei(audioBalance, true, 0)}
+            </Text>
+          )}
+        </View>
       </LeftNavLink>
       <LeftNavLink
         icon={IconUpload}
@@ -133,6 +220,12 @@ const WrappedLeftNavDrawer = () => {
         label={messages.listeningHistory}
         to='ListeningHistoryScreen'
         params={null}
+      />
+      <LeftNavLink
+        icon={IconUser}
+        label={messages.profile}
+        to='Profile'
+        params={{ handle: 'accountUser' }}
       />
       <LeftNavLink
         icon={IconSettings}
