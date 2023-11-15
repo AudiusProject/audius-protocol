d02563c13ea70f22b1b82aab72df12564e648996
Theo Ilie <theoilie.ti@gmail.com>
2023-11-14 21:43:04 -0800

    [PROTO-1430] Add external wallets to /d (fka /up)

--- monitoring/uptime/node-ui/src/providers/AudiusLibsProvider.tsx
+++ monitoring/uptime/node-ui/src/providers/AudiusLibsProvider.tsx
@@ -1,5 +1,5 @@
-import type { Web3 as Web3Type } from 'web3'
 import type { AudiusLibs as AudiusLibsType } from '@audius/sdk/dist/WebAudiusLibs.d.ts'
+import { useAccount } from 'wagmi'
 import {
   ReactNode,
   createContext,
@@ -8,117 +8,66 @@ import {
   useEffect
 } from 'react'
 import { useEnvVars } from '../providers/EnvVarsProvider'
+import useWeb3Signer from '../hooks/useWeb3Signer'
+
+// This should be import type { Web3 } from web3, but Audius libs forces us to use a version of web3 without this type
+type Web3Type = any
 
 type AudiusLibsContextType = {
-  web3: Web3Type | null
   audiusLibs: AudiusLibsType | null
   isLoading: boolean
+  isReadOnly: boolean // read-only means the user can't approve transactions (i.e., no external wallet connected)
 }
 const AudiusLibsContext = createContext<AudiusLibsContextType>({
-  web3: null,
   audiusLibs: null,
-  isLoading: true
+  isLoading: true,
+  isReadOnly: true
 })
 
 export const AudiusLibsProvider = ({ children }: { children: ReactNode }) => {
-  // const [web3, setWeb3] = useState(null)
   const [audiusLibs, setAudiusLibs] = useState<AudiusLibsType | null>(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [isReadOnly, setIsReadOnly] = useState(true)
+  const { address } = useAccount()
   const envVars = useEnvVars()
-  const {
-    env,
+  const connectedWeb3 = useWeb3Signer()
 
-    // ethNetworkId,
-    ethTokenAddress,
-    ethRegistryAddress,
-    ethProviderUrl,
-    ethOwnerWallet,
+  // @ts-expect-error ts(2741). This is only here for debugging and should eventually be removed
+  window.audiusLibs = audiusLibs
 
-    // entityManagerAddress,
-
-    identityServiceEndpoint,
-
-    wormholeContractAddress,
-    claimDistributionContractAddress,
-    solanaClusterEndpoint,
-    wAudioMintAddress,
-    usdcMintAddress,
-    solanaTokenProgramAddress,
-    claimableTokenPda,
-    solanaFeePayerAddress,
-    claimableTokenProgramAddress,
-    rewardsManagerProgramId,
-    rewardsManagerProgramPda,
-    rewardsManagerTokenPda
-  } = envVars
+  // @ts-expect-error ts(2339) - should be removed eventually after debugging (fine for now)
+  window.connectedWeb3 = connectedWeb3
 
   const initLibraries = async () => {
-    // Dynamically import hefty libraries so that we don't have to include them in the main index bundle
-    // const { default: Web3 } = (await import('web3'))
-    const audiusSdkModule = await import('@audius/sdk/dist/web-libs.js')
-    const AudiusLibs = audiusSdkModule.libs as unknown as typeof AudiusLibsType
-    // const Utils = audiusSdkModule.Utils
-
-    // Configure Audius libs in read-only mode if there's no MetaMask in the browser
-
-    const ethWeb3Config = AudiusLibs.configEthWeb3(
-      ethTokenAddress,
-      ethRegistryAddress,
-      ethProviderUrl,
-      ethOwnerWallet,
-      claimDistributionContractAddress,
-      wormholeContractAddress
-    )
-
-    const solanaWeb3Config = AudiusLibs.configSolanaWeb3({
-      claimableTokenPDA: claimableTokenPda,
-      solanaClusterEndpoint: solanaClusterEndpoint,
-      mintAddress: wAudioMintAddress,
-      usdcMintAddress: usdcMintAddress,
-      solanaTokenAddress: solanaTokenProgramAddress,
-      // @ts-expect-error ts(2322) Type 'string' is not assignable to type 'PublicKey'. This happens because libs has a bug where it sets the wrong type.
-      feePayerAddress: solanaFeePayerAddress,
-      claimableTokenProgramAddress,
-      rewardsManagerProgramId,
-      rewardsManagerProgramPDA: rewardsManagerProgramPda,
-      rewardsManagerTokenPDA: rewardsManagerTokenPda,
-      useRelay: true
-    })
-
-    const identityServiceConfig = {
-      url: identityServiceEndpoint
+    if (address && connectedWeb3) {
+      const audiusLibs = await initLibsWithAccount(
+        envVars,
+        address,
+        connectedWeb3
+      )
+      setAudiusLibs(audiusLibs)
+      setIsReadOnly(false)
+    } else {
+      const audiusLibs = await initLibsWithoutAccount(envVars)
+      setAudiusLibs(audiusLibs)
+      setIsReadOnly(true)
     }
 
-    const audiusLibsConfig = {
-      ethWeb3Config,
-      solanaWeb3Config,
-      identityServiceConfig,
-      discoveryProviderConfig: {},
-      isServer: false,
-      isDebug: env === 'staging' || env === 'development'
-    }
-
-    // @ts-expect-error ts(2345). It's complaining about not passing all the config args, but they're optional so we can ignore
-    const audiusLibs = new AudiusLibs(audiusLibsConfig)
-    await audiusLibs.init()
-
-    // TODO: Configure libs differently if MetaMask is present
-
-    // setWeb3(web3Instance)
-    setAudiusLibs(audiusLibs)
-    // @ts-expect-error ts(2741). This is only here for debugging and should eventually be removed
-    window.audiusLibs = audiusLibs
     setIsLoading(false)
   }
 
   useEffect(() => {
-    if (ethProviderUrl) {
+    if (envVars.ethProviderUrl) {
       void initLibraries()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [envVars])
+  }, [envVars, address, connectedWeb3])
 
-  const contextValue = { web3: {} as Web3Type, audiusLibs, isLoading } // TODO: Probably return web3 as well
+  const contextValue = {
+    audiusLibs,
+    isLoading,
+    isReadOnly
+  }
   return (
     <AudiusLibsContext.Provider value={contextValue}>
       {children}
@@ -128,3 +77,127 @@ export const AudiusLibsProvider = ({ children }: { children: ReactNode }) => {
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAudiusLibs = () => useContext(AudiusLibsContext)
+
+// Returns an Audius libs instance that isn't connected to any wallet, so it can't approve transactions
+const initLibsWithoutAccount = async (
+  envVars: ReturnType<typeof useEnvVars>
+): Promise<AudiusLibsType> => {
+  // Dynamically import hefty libraries so that we don't have to include them in the main index bundle
+  const audiusSdkModule = await import('@audius/sdk/dist/web-libs.js')
+  const AudiusLibs = audiusSdkModule.libs as unknown as typeof AudiusLibsType
+
+  const ethWeb3Config = AudiusLibs.configEthWeb3(
+    envVars.ethTokenAddress,
+    envVars.ethRegistryAddress,
+    envVars.ethProviderUrl,
+    envVars.ethOwnerWallet,
+    envVars.claimDistributionContractAddress,
+    envVars.wormholeContractAddress
+  )
+
+  const solanaWeb3Config = AudiusLibs.configSolanaWeb3({
+    claimableTokenPDA: envVars.claimableTokenPda,
+    solanaClusterEndpoint: envVars.solanaClusterEndpoint,
+    mintAddress: envVars.wAudioMintAddress,
+    usdcMintAddress: envVars.usdcMintAddress,
+    solanaTokenAddress: envVars.solanaTokenProgramAddress,
+    // @ts-expect-error ts(2322) Type 'string' is not assignable to type 'PublicKey'. This happens because libs has a bug where it sets the wrong type.
+    feePayerAddress: envVars.solanaFeePayerAddress,
+    claimableTokenProgramAddress: envVars.claimableTokenProgramAddress,
+    rewardsManagerProgramId: envVars.rewardsManagerProgramId,
+    rewardsManagerProgramPDA: envVars.rewardsManagerProgramPda,
+    rewardsManagerTokenPDA: envVars.rewardsManagerTokenPda,
+    useRelay: true
+  })
+
+  const identityServiceConfig = {
+    url: envVars.identityServiceEndpoint
+  }
+
+  const audiusLibsConfig = {
+    ethWeb3Config,
+    solanaWeb3Config,
+    identityServiceConfig,
+    discoveryProviderConfig: {},
+    isServer: false,
+    isDebug: envVars.env === 'staging' || envVars.env === 'development'
+  }
+
+  // @ts-expect-error ts(2345). It's complaining about not passing all the config args, but they're optional so we can ignore
+  const audiusLibs = new AudiusLibs(audiusLibsConfig)
+  await audiusLibs.init()
+  return audiusLibs
+}
+
+// Returns an Audius libs instance connected to the wallet in the browser (e.g., MetaMask), so it can approve transactions
+const initLibsWithAccount = async (
+  envVars: ReturnType<typeof useEnvVars>,
+  ownerWallet: string,
+  connectedWeb3: Web3Type
+): Promise<AudiusLibsType> => {
+  // Dynamically import hefty libraries so that we don't have to include them in the main index bundle
+  const audiusSdkModule = await import('@audius/sdk/dist/web-libs.js')
+  const AudiusLibs = audiusSdkModule.libs as unknown as typeof AudiusLibsType
+
+  const web3Config = {
+    registryAddress: envVars.ethRegistryAddress,
+    entityManagerAddress: envVars.entityManagerAddress,
+    useExternalWeb3: true,
+    externalWeb3Config: {
+      web3: connectedWeb3,
+      ownerWallet
+    }
+  }
+
+  const ethWeb3Config = {
+    tokenAddress: envVars.ethTokenAddress,
+    registryAddress: envVars.ethRegistryAddress,
+    // Set it in read-only mode first instead of passing `connectedWeb3` here.
+    // connectedWeb3 isn't compatible to pass here because libs is only setup to work with Metamask's window.ethereum
+    providers: envVars.ethProviderUrl,
+    ownerWallet,
+    claimDistributionContractAddress: envVars.claimDistributionContractAddress,
+    wormholeContractAddress: envVars.wormholeContractAddress
+  }
+
+  const solanaWeb3Config = AudiusLibs.configSolanaWeb3({
+    claimableTokenPDA: envVars.claimableTokenPda,
+    solanaClusterEndpoint: envVars.solanaClusterEndpoint,
+    mintAddress: envVars.wAudioMintAddress,
+    usdcMintAddress: envVars.usdcMintAddress,
+    solanaTokenAddress: envVars.solanaTokenProgramAddress,
+    // @ts-expect-error ts(2322) Type 'string' is not assignable to type 'PublicKey'. This happens because libs has a bug where it sets the wrong type.
+    feePayerAddress: envVars.solanaFeePayerAddress,
+    claimableTokenProgramAddress: envVars.claimableTokenProgramAddress,
+    rewardsManagerProgramId: envVars.rewardsManagerProgramId,
+    rewardsManagerProgramPDA: envVars.rewardsManagerProgramPda,
+    rewardsManagerTokenPDA: envVars.rewardsManagerTokenPda,
+    useRelay: true
+  })
+
+  const identityServiceConfig = {
+    url: envVars.identityServiceEndpoint
+  }
+
+  const audiusLibsConfig = {
+    web3Config,
+    ethWeb3Config,
+    solanaWeb3Config,
+    identityServiceConfig,
+    discoveryProviderConfig: {},
+    isServer: false,
+    isDebug: envVars.env === 'staging' || envVars.env === 'development'
+  }
+
+  // @ts-expect-error ts(2345). It's complaining about not passing all the config args, but they're optional so we can ignore
+  const audiusLibs = new AudiusLibs(audiusLibsConfig)
+  await audiusLibs.init()
+
+  // Set audiusLibs to use our own connectedWeb3 instead of using the one that libs creates
+  // because it isn't compatible with external wallets outside of directly using Metamask's window.ethereum
+  if (audiusLibs.ethWeb3Manager) {
+    audiusLibs.ethWeb3Manager.web3 = connectedWeb3
+  }
+
+  return audiusLibs
+}
--- monitoring/uptime/node-ui/src/providers/EnvVarsProvider.tsx
+++ monitoring/uptime/node-ui/src/providers/EnvVarsProvider.tsx
@@ -41,51 +41,55 @@ interface EnvVars {
 
 // Local testing sets overrides for env vars that would normally be fetched from the node
 const envVarOverrides = {
-  env: import.meta.env.VITE_ENV_OVERRIDE as string,
-  nodeType: import.meta.env.VITE_NODE_TYPE_OVERRIDE as string,
-  audiusUrl: import.meta.env.VITE_AUDIUS_URL_OVERRIDE as string,
+  env: (import.meta.env.VITE_ENV_OVERRIDE ?? '') as string,
+  nodeType: (import.meta.env.VITE_NODE_TYPE_OVERRIDE ?? '') as string,
+  audiusUrl: (import.meta.env.VITE_AUDIUS_URL_OVERRIDE ?? '') as string,
 
-  ethNetworkId: import.meta.env.VITE_ETH_NETWORK_ID_OVERRIDE as string,
-  ethTokenAddress: import.meta.env.VITE_ETH_TOKEN_ADDRESS_OVERRIDE as string,
-  ethRegistryAddress: import.meta.env
-    .VITE_ETH_REGISTRY_ADDRESS_OVERRIDE as string,
-  ethProviderUrl: import.meta.env.VITE_ETH_PROVIDER_URL_OVERRIDE as string,
-  ethOwnerWallet: import.meta.env.VITE_ETH_OWNER_WALLET_OVERRIDE as string,
+  ethNetworkId: (import.meta.env.VITE_ETH_NETWORK_ID_OVERRIDE ?? '') as string,
+  ethTokenAddress: (import.meta.env.VITE_ETH_TOKEN_ADDRESS_OVERRIDE ??
+    '') as string,
+  ethRegistryAddress: (import.meta.env.VITE_ETH_REGISTRY_ADDRESS_OVERRIDE ??
+    '') as string,
+  ethProviderUrl: (import.meta.env.VITE_ETH_PROVIDER_URL_OVERRIDE ??
+    '') as string,
+  ethOwnerWallet: (import.meta.env.VITE_ETH_OWNER_WALLET_OVERRIDE ??
+    '') as string,
 
-  queryProposalStartBlock: import.meta.env
-    .VITE_QUERY_PROPOSAL_START_BLOCK_OVERRIDE as string,
-  gqlUri: import.meta.env.VITE_QQL_URI_OVERRIDE as string,
-  gqlBackupUri: import.meta.env.VITE_QQL_BACKUP_URI_OVERRIDE as string,
+  queryProposalStartBlock: (import.meta.env
+    .VITE_QUERY_PROPOSAL_START_BLOCK_OVERRIDE ?? '') as string,
+  gqlUri: (import.meta.env.VITE_QQL_URI_OVERRIDE ?? '') as string,
+  gqlBackupUri: (import.meta.env.VITE_QQL_BACKUP_URI_OVERRIDE ?? '') as string,
 
-  entityManagerAddress: import.meta.env
-    .VITE_ENTITY_MANAGER_ADDRESS_OVERRIDE as string,
+  entityManagerAddress: (import.meta.env.VITE_ENTITY_MANAGER_ADDRESS_OVERRIDE ??
+    '') as string,
 
-  identityServiceEndpoint: import.meta.env
-    .VITE_IDENTITY_SERVICE_ENDPOINT_OVERRIDE as string,
+  identityServiceEndpoint: (import.meta.env
+    .VITE_IDENTITY_SERVICE_ENDPOINT_OVERRIDE ?? '') as string,
 
-  wormholeContractAddress: import.meta.env
-    .VITE_WORMHOLE_CONTRACT_ADDRESS_OVERRIDE as string,
-  claimDistributionContractAddress: import.meta.env
-    .VITE_CLAIM_DISTRIBUTION_CONTRACT_ADDRESS_OVERRIDE as string,
-  solanaClusterEndpoint: import.meta.env
-    .VITE_SOLANA_CLUSTER_ENDPOINT_OVERRIDE as string,
-  wAudioMintAddress: import.meta.env
-    .VITE_WAUDIO_MINT_ADDRESS_OVERRIDE as string,
-  usdcMintAddress: import.meta.env.VITE_USDC_MINT_ADDRESS_OVERRIDE as string,
-  solanaTokenProgramAddress: import.meta.env
-    .VITE_SOLANA_TOKEN_PROGRAM_ADDRESS_OVERRIDE as string,
-  claimableTokenPda: import.meta.env
-    .VITE_CLAIMABLE_TOKEN_PDA_OVERRIDE as string,
-  solanaFeePayerAddress: import.meta.env
-    .VITE_SOLANA_FEE_PAYER_ADDRESS_OVERRIDE as string,
-  claimableTokenProgramAddress: import.meta.env
-    .VITE_CLAIMABLE_TOKEN_PROGRAM_ADDRESS_OVERRIDE as string,
-  rewardsManagerProgramId: import.meta.env
-    .VITE_REWARDS_MANAGER_PROGRAM_ID_OVERRIDE as string,
-  rewardsManagerProgramPda: import.meta.env
-    .VITE_REWARDS_MANAGER_PROGRAM_PDA_OVERRIDE as string,
-  rewardsManagerTokenPda: import.meta.env
-    .VITE_REWARDS_MANAGER_TOKEN_PDA_OVERRIDE as string
+  wormholeContractAddress: (import.meta.env
+    .VITE_WORMHOLE_CONTRACT_ADDRESS_OVERRIDE ?? '') as string,
+  claimDistributionContractAddress: (import.meta.env
+    .VITE_CLAIM_DISTRIBUTION_CONTRACT_ADDRESS_OVERRIDE ?? '') as string,
+  solanaClusterEndpoint: (import.meta.env
+    .VITE_SOLANA_CLUSTER_ENDPOINT_OVERRIDE ?? '') as string,
+  wAudioMintAddress: (import.meta.env.VITE_WAUDIO_MINT_ADDRESS_OVERRIDE ??
+    '') as string,
+  usdcMintAddress: (import.meta.env.VITE_USDC_MINT_ADDRESS_OVERRIDE ??
+    '') as string,
+  solanaTokenProgramAddress: (import.meta.env
+    .VITE_SOLANA_TOKEN_PROGRAM_ADDRESS_OVERRIDE ?? '') as string,
+  claimableTokenPda: (import.meta.env.VITE_CLAIMABLE_TOKEN_PDA_OVERRIDE ??
+    '') as string,
+  solanaFeePayerAddress: (import.meta.env
+    .VITE_SOLANA_FEE_PAYER_ADDRESS_OVERRIDE ?? '') as string,
+  claimableTokenProgramAddress: (import.meta.env
+    .VITE_CLAIMABLE_TOKEN_PROGRAM_ADDRESS_OVERRIDE ?? '') as string,
+  rewardsManagerProgramId: (import.meta.env
+    .VITE_REWARDS_MANAGER_PROGRAM_ID_OVERRIDE ?? '') as string,
+  rewardsManagerProgramPda: (import.meta.env
+    .VITE_REWARDS_MANAGER_PROGRAM_PDA_OVERRIDE ?? '') as string,
+  rewardsManagerTokenPda: (import.meta.env
+    .VITE_REWARDS_MANAGER_TOKEN_PDA_OVERRIDE ?? '') as string
 }
 const endpoint = (import.meta.env.VITE_NODE_URL_OVERRIDE ||
   window.location.origin) as string
@@ -102,7 +106,7 @@ export const EnvVarsProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     const fetchEnvVars = async () => {
       try {
-        const response = await fetch(`${endpoint}/up_api/env`)
+        const response = await fetch(`${endpoint}/d_api/env`)
         if (!response.ok) {
           throw new Error('Network response was not ok')
         }
@@ -115,7 +119,6 @@ export const EnvVarsProvider = ({ children }: { children: ReactNode }) => {
 
     // Stage and prod nodes fetch env vars exposed in audius-docker-compose
     if (!envVars.env || !envVars.nodeType) {
-      console.log('fetching env vars') // TODO: Remove eventually, but it's fine to have some debugging at this early stage
       void fetchEnvVars()
     }
   })
