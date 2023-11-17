369cd6267402ac0cc5b28155a4d424f4986d5b67
Raymond Jacobson <ray@audius.co>
2023-11-16 22:28:08 -0800

    Wire up onramp

--- packages/web/src/components/add-funds-modal/AddFundsModal.tsx
+++ packages/web/src/components/add-funds-modal/AddFundsModal.tsx
@@ -1,8 +1,13 @@
 import { useCallback, useState } from 'react'
 
-import { useAddFundsModal } from '@audius/common'
+import {
+  useAddFundsModal,
+  buyUSDCActions,
+  USDCOnRampProvider
+} from '@audius/common'
 import { ModalContent, ModalHeader } from '@audius/stems'
 import cn from 'classnames'
+import { useDispatch } from 'react-redux'
 
 import { AddFunds, Method } from 'components/add-funds/AddFunds'
 import { Text } from 'components/typography'
@@ -20,8 +25,11 @@ const messages = {
 
 type Page = 'add-funds' | 'crypto-transfer'
 
+const DEFAULT_PURCHASE_AMOUNT_CENTS = 10 * 100
+
 export const AddFundsModal = () => {
   const { isOpen, onClose } = useAddFundsModal()
+  const dispatch = useDispatch()
   const mobile = isMobile()
 
   const [page, setPage] = useState<Page>('add-funds')
@@ -39,10 +47,17 @@ export const AddFundsModal = () => {
       if (method === 'crypto') {
         setPage('crypto-transfer')
       } else {
-        // Open payments
+        dispatch(
+          buyUSDCActions.onrampOpened({
+            provider: USDCOnRampProvider.STRIPE,
+            purchaseInfo: {
+              desiredAmount: DEFAULT_PURCHASE_AMOUNT_CENTS
+            }
+          })
+        )
       }
     },
-    [setPage]
+    [setPage, dispatch]
   )
 
   return (
