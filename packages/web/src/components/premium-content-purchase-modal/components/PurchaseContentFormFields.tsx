ca493ecbf83c7c65657f9e7fce5183daa22135e6
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-11-20 13:31:33 -0500

    [PAY-2182] Fix <p> descendent of <p> warnings in SummaryTable (#6744)

--- packages/web/src/components/premium-content-purchase-modal/components/PurchaseContentFormFields.tsx
+++ packages/web/src/components/premium-content-purchase-modal/components/PurchaseContentFormFields.tsx
@@ -107,6 +107,7 @@ export const PurchaseContentFormFields = ({
           disabled: isExistingBalanceDisabled,
           value: (
             <Text
+              as='span' // Needed to avoid <p> inside <p> warning
               variant='title'
               color={
                 purchaseMethod === PurchaseMethod.EXISTING_BALANCE
