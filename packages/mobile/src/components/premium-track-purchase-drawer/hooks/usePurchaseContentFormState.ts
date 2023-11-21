57e24d8220fcb526e64d951db7480905e63592c4
Dharit Tantiviramanond <dharit.tan@gmail.com>
2023-11-21 14:38:15 -0500

    remove cancelPolling()

--- packages/mobile/src/components/premium-track-purchase-drawer/hooks/usePurchaseContentFormState.ts
+++ packages/mobile/src/components/premium-track-purchase-drawer/hooks/usePurchaseContentFormState.ts
@@ -17,12 +17,7 @@ export const usePurchaseContentFormState = ({ price }: { price: number }) => {
   const error = useSelector(getPurchaseContentError)
   const isUnlocking = !error && isContentPurchaseInProgress(stage)
 
-  const {
-    data: currentBalance,
-    recoveryStatus,
-    refresh,
-    cancelPolling
-  } = useUSDCBalance()
+  const { data: currentBalance, recoveryStatus, refresh } = useUSDCBalance()
 
   // Refresh balance on successful recovery
   useEffect(() => {
@@ -30,12 +25,6 @@ export const usePurchaseContentFormState = ({ price }: { price: number }) => {
       refresh()
     }
   }, [recoveryStatus, refresh])
-
-  useEffect(() => {
-    if (isUnlocking) {
-      cancelPolling()
-    }
-  }, [isUnlocking, cancelPolling])
 
   const purchaseSummaryValues = usePurchaseSummaryValues({
     price,
