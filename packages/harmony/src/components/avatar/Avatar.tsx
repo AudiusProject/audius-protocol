61bc83c977eee242a669c2520c7f3399d2d03adc
JD Francis <jd@audius.co>
2023-11-21 14:06:18 -0600

    clean up bad avatar merge

--- packages/harmony/src/components/avatar/Avatar.tsx
+++ packages/harmony/src/components/avatar/Avatar.tsx
@@ -39,12 +39,7 @@ export const Avatar = (props: AvatarProps) => {
       variant === 'strong'
         ? '0px 0.5px 1.5px 0px rgba(0, 0, 0, 0.03), 0px 1.5px 5px 0px rgba(0, 0, 0, 0.08), 0px 6px 15px 0px rgba(0, 0, 0, 0.10)'
         : 'none',
-    backgroundImage: `url(${src})`,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: src ? 'unset' : color.neutral.n400,
-    backgroundSize: 'cover',
+    backgroundColor: color.neutral.n400,
     position: 'relative',
     zIndex: 1
   }
@@ -52,8 +47,8 @@ export const Avatar = (props: AvatarProps) => {
   const imgCss: CSSObject = {
     height: sizeMap[size],
     width: sizeMap[size],
-    backgroundImage: `url(${src})`,
-    backgroundColor: 'unset',
+    backgroundImage: src ? `url(${src})` : undefined,
+    backgroundColor: src ? 'unset' : color.neutral.n400,
     backgroundSize: 'cover',
     zIndex: 1,
     display: 'flex',
