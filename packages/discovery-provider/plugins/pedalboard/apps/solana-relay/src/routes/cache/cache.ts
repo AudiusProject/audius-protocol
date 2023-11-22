1caa1c56839ecd98a7dee4180aff8c2c2abc52ac
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-11-22 14:46:24 -0800

    check signature

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/routes/cache/cache.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/routes/cache/cache.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express'
 import { BadRequestError } from '../../errors'
 import { cacheTransaction } from '../../redis'
 import { logger } from '../../logger'
+import { VersionedTransaction } from '@solana/web3.js'
+import base58 from 'bs58'
 
 type RequestBody = {
   signature: string
@@ -17,6 +19,16 @@ export const cache = async (
     const { signature, transaction } = req.body
     if (!signature || !transaction) {
       throw new BadRequestError()
+    }
+    const decoded = Buffer.from(transaction, 'base64')
+    const tx = VersionedTransaction.deserialize(decoded)
+    const sig = tx.signatures[0]
+    if (!sig) {
+      throw new BadRequestError('No signature on transaction')
+    }
+    const txSignature = base58.encode(sig)
+    if (txSignature !== signature) {
+      throw new BadRequestError('Invalid signature')
     }
     await cacheTransaction(signature, transaction)
     res.status(200).send({ signature, transaction })
