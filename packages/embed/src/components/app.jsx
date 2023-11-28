603d3819aef5563ed6c1b4be2ba921e4f622d182
Raymond Jacobson <ray@audius.co>
2023-11-28 11:51:43 -0800

    Embed QA

--- packages/embed/src/components/app.jsx
+++ packages/embed/src/components/app.jsx
@@ -6,8 +6,8 @@ import { useParams, useSearchParams } from 'react-router-dom'
 import { CSSTransition } from 'react-transition-group'
 
 import '@audius/stems/dist/stems.css'
+import '@audius/harmony/dist/avenir.css'
 import '@audius/harmony/dist/harmony.css'
-import './font.css'
 
 import {
   initTrackSessionStart,
