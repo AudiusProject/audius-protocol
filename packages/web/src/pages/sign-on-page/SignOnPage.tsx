16e6bc776e0fb7bc802c6784e1dc6d1222126310
Dylan Jeffers <dylan@audius.co>
2023-12-04 16:55:51 -0800

    [C-3459] Improve sign-up responsiveness (#6844)

--- packages/web/src/pages/sign-on-page/SignOnPage.tsx
+++ packages/web/src/pages/sign-on-page/SignOnPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { ReactNode, useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 import {
   Flex,
@@ -9,6 +9,7 @@ import {
   useTheme
 } from '@audius/harmony'
 import { Link, Route, Switch, useRouteMatch } from 'react-router-dom'
+import { useEffectOnce, useMeasure } from 'react-use'
 
 import djBackground from 'assets/img/2-DJ-4-3.jpg'
 import djPortrait from 'assets/img/DJportrait.jpg'
@@ -17,6 +18,7 @@ import { useMedia } from 'hooks/useMedia'
 import { SignInPage } from 'pages/sign-in-page/SignInPage'
 import { AudiusValues } from 'pages/sign-on-page/AudiusValues'
 import SignUpPage from 'pages/sign-up-page'
+import { ScrollView } from 'pages/sign-up-page/components/layout'
 import {
   SIGN_IN_PAGE,
   SIGN_UP_CREATE_LOGIN_DETAILS,
@@ -32,20 +34,13 @@ const messages = {
   createAccount: 'Create an Account'
 }
 
-export const SignOnPage = () => {
-  const { isMobile } = useMedia()
-  const [isExpanded, setIsExpanded] = useState(false)
-  const [isLoaded, setIsLoaded] = useState(false)
+type RootProps = {
+  children: ReactNode
+}
+
+const DesktopSignOnRoot = (props: RootProps) => {
+  const { children } = props
   const { spacing, motion } = useTheme()
-  const [panelHeight, setPanelHeight] = useState<number | null>(null)
-  const onSignInPage = !!useRouteMatch(SIGN_IN_PAGE)
-
-  const panelRef = useRef<HTMLDivElement>(null)
-
-  const collapsedMobilePageMatch = useRouteMatch({
-    path: [SIGN_IN_PAGE, SIGN_UP_PAGE, SIGN_UP_EMAIL_PAGE],
-    exact: true
-  })
 
   const collapsedDesktopPageMatch = useRouteMatch({
     path: [
@@ -59,116 +54,10 @@ export const SignOnPage = () => {
     exact: true
   })
 
-  const isPageExpanded = isMobile
-    ? !collapsedMobilePageMatch
-    : !collapsedDesktopPageMatch
-
-  useEffect(() => {
-    setIsLoaded(true)
-  }, [])
-
-  useEffect(() => {
-    setIsExpanded(isPageExpanded)
-  }, [isPageExpanded])
-
-  useEffect(() => {
-    setPanelHeight(null)
-  }, [onSignInPage])
-
-  useEffect(() => {
-    if (panelRef.current && panelHeight === null) {
-      // wait for content to finish rendering
-      const calculateHeightTimeout = setTimeout(() => {
-        const height = panelRef.current?.getBoundingClientRect().height
-        if (height) {
-          setPanelHeight(height)
-        }
-      }, 400)
-      return () => clearTimeout(calculateHeightTimeout)
-    }
-  }, [panelHeight])
-
-  const routes = (
-    <Switch>
-      <Route path={SIGN_IN_PAGE}>
-        <SignInPage />
-      </Route>
-      <Route path={SIGN_UP_PAGE}>
-        <SignUpPage />
-      </Route>
-    </Switch>
-  )
-
-  if (isMobile) {
-    return (
-      <Flex direction='column' w='100%'>
-        <Flex
-          ref={panelRef}
-          direction='column'
-          borderBottomLeftRadius={!isExpanded ? '2xl' : undefined}
-          borderBottomRightRadius={!isExpanded ? '2xl' : undefined}
-          h={isExpanded ? '100%' : panelHeight || 'auto'}
-          css={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            backgroundColor: 'white',
-            zIndex: 1,
-            transition: `height ${motion.calm}, border-radius ${motion.calm}, transform ${motion.calm} 0.5s`,
-            transform: isLoaded ? 'translateY(0px)' : 'translateY(-100%)'
-          }}
-        >
-          {routes}
-        </Flex>
-        <Flex
-          direction='column'
-          alignItems='center'
-          pb='2xl'
-          css={{
-            paddingTop: panelHeight ?? 0,
-            flexGrow: 1,
-            backgroundImage: `radial-gradient(77.16% 77.16% at 50% 51.81%, rgba(91, 35, 225, 0.80) 0%, rgba(113, 41, 230, 0.64) 67.96%, rgba(162, 47, 235, 0.50) 100%), url(${djPortrait})`,
-            backgroundColor: 'lightgray',
-            backgroundSize: 'cover',
-            backgroundPosition: 'bottom'
-          }}
-        >
-          <Switch>
-            <Route path={SIGN_UP_PAGE}>
-              <AudiusValues
-                css={{
-                  margin: 'auto',
-                  opacity: isLoaded ? 1 : 0,
-                  transition: `opacity ${motion.expressive} 1s`
-                }}
-              />
-            </Route>
-            <Route path={SIGN_IN_PAGE}>
-              <Text
-                variant='title'
-                strength='weak'
-                color='staticWhite'
-                css={{
-                  marginTop: 'auto',
-                  opacity: isLoaded ? 1 : 0,
-                  transition: `opacity ${motion.expressive} 1s`
-                }}
-              >
-                {messages.newToAudius}{' '}
-                <TextLink variant='inverted' showUnderline asChild>
-                  <Link to={SIGN_UP_PAGE}>{messages.createAccount}</Link>
-                </TextLink>
-              </Text>
-            </Route>
-          </Switch>
-        </Flex>
-      </Flex>
-    )
-  }
+  const isExpanded = !collapsedDesktopPageMatch
 
   return (
-    <Flex w='100%' p='unit20'>
+    <Flex w='100%' p='unit14' justifyContent='center'>
       <Link
         to={TRENDING_PAGE}
         css={{
@@ -181,37 +70,188 @@ export const SignOnPage = () => {
         <IconCloseAlt color='staticWhite' />
       </Link>
       <BackgroundWaves zIndex={0} />
-      <Paper w='100%'>
-        <Flex
+      <Paper
+        w='100%'
+        css={{
+          flex: 1,
+          maxWidth: 1440,
+          minWidth: 744,
+          overflow: 'hidden'
+        }}
+      >
+        <ScrollView
           direction='column'
-          w={isExpanded ? '100%' : 480}
           h='100%'
+          w={isExpanded ? '100%' : 584}
+          flex={isExpanded ? undefined : '1 0 0'}
           css={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
+            maxWidth: isExpanded ? '100%' : 584,
+            minWidth: 400,
             background: 'white',
             zIndex: 1,
             transition: `width ${motion.expressive}`
           }}
         >
-          {routes}
-        </Flex>
+          {children}
+        </ScrollView>
         <Flex
           alignItems='center'
           justifyContent='center'
+          flex='1 0 0'
           css={{
-            marginLeft: 480,
-            flexGrow: 1,
+            zIndex: 0,
+            overflow: 'hidden',
             backgroundImage: `radial-gradient(77.16% 77.16% at 50% 51.81%, rgba(91, 35, 225, 0.80) 0%, rgba(113, 41, 230, 0.64) 67.96%, rgba(162, 47, 235, 0.50) 100%), url(${djBackground})`,
             backgroundColor: 'lightgray',
             backgroundSize: 'cover',
             backgroundPosition: 'center'
           }}
         >
-          <AudiusValues />
+          {isExpanded ? null : <AudiusValues />}
         </Flex>
       </Paper>
     </Flex>
+  )
+}
+
+type MobileSignOnRootProps = RootProps & {
+  isLoaded?: boolean
+}
+
+type PanelState = 'collapsed' | 'expanding' | 'expanded' | 'collapsing'
+
+const MobileSignOnRoot = (props: MobileSignOnRootProps) => {
+  const { children, isLoaded } = props
+  const [panelState, setPanelState] = useState<PanelState>('collapsed')
+  const { motion } = useTheme()
+  const [ref, { height: panelHeight }] = useMeasure<HTMLDivElement>()
+  const collapsedPanelHeight = useRef(panelHeight)
+
+  const collapsedMobilePageMatch = useRouteMatch({
+    path: [SIGN_IN_PAGE, SIGN_UP_PAGE, SIGN_UP_EMAIL_PAGE],
+    exact: true
+  })
+
+  const shouldPageExpand = !collapsedMobilePageMatch
+
+  useLayoutEffect(() => {
+    if (panelState === 'collapsed') {
+      collapsedPanelHeight.current = panelHeight
+    }
+  }, [panelState, panelHeight])
+
+  // TODO: use `onTransitionEnd`
+  useEffect(() => {
+    if (shouldPageExpand) {
+      setPanelState('expanding')
+      const timeout = setTimeout(() => {
+        setPanelState('expanded')
+      }, 100)
+      return () => clearTimeout(timeout)
+    } else if (!shouldPageExpand && panelState === 'expanded') {
+      setPanelState('collapsing')
+      const timeout = setTimeout(() => {
+        setPanelState('collapsed')
+      }, 500)
+      return () => clearTimeout(timeout)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shouldPageExpand])
+
+  const panelHeightMap = {
+    collapsed: 'auto',
+    expanding: panelHeight,
+    expanded: '100%',
+    collapsing: collapsedPanelHeight.current
+  }
+
+  return (
+    <Flex direction='column' w='100%'>
+      <Flex
+        ref={ref}
+        direction='column'
+        borderBottomLeftRadius={panelState === 'expanded' ? undefined : '2xl'}
+        borderBottomRightRadius={panelState === 'expanded' ? undefined : '2xl'}
+        h={panelHeightMap[panelState]}
+        css={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          backgroundColor: 'white',
+          zIndex: 1,
+          transition: `height ${motion.calm}, border-radius ${motion.calm}, transform ${motion.calm} 0.5s`,
+          transform: isLoaded ? 'translateY(0px)' : 'translateY(-100%)'
+        }}
+      >
+        {children}
+      </Flex>
+      <Flex
+        direction='column'
+        alignItems='center'
+        pb='2xl'
+        css={{
+          paddingTop: collapsedPanelHeight.current,
+          flexGrow: 1,
+          backgroundImage: `radial-gradient(77.16% 77.16% at 50% 51.81%, rgba(91, 35, 225, 0.80) 0%, rgba(113, 41, 230, 0.64) 67.96%, rgba(162, 47, 235, 0.50) 100%), url(${djPortrait})`,
+          backgroundColor: 'lightgray',
+          backgroundSize: 'cover',
+          backgroundPosition: 'bottom'
+        }}
+      >
+        <Switch>
+          <Route path={SIGN_UP_PAGE}>
+            <AudiusValues
+              css={{
+                margin: 'auto',
+                opacity: isLoaded ? 1 : 0,
+                transition: `opacity ${motion.expressive} 1s`
+              }}
+            />
+          </Route>
+          <Route path={SIGN_IN_PAGE}>
+            <Text
+              variant='title'
+              strength='weak'
+              color='staticWhite'
+              css={{
+                marginTop: 'auto',
+                opacity: isLoaded ? 1 : 0,
+                transition: `opacity ${motion.expressive} 1s`
+              }}
+            >
+              {messages.newToAudius}{' '}
+              <TextLink variant='inverted' showUnderline asChild>
+                <Link to={SIGN_UP_PAGE}>{messages.createAccount}</Link>
+              </TextLink>
+            </Text>
+          </Route>
+        </Switch>
+      </Flex>
+    </Flex>
+  )
+}
+
+export const SignOnPage = () => {
+  const { isMobile } = useMedia()
+
+  const [isLoaded, setIsLoaded] = useState(false)
+  const SignOnRoot = isMobile ? MobileSignOnRoot : DesktopSignOnRoot
+
+  useEffectOnce(() => {
+    setIsLoaded(true)
+  })
+
+  return (
+    <SignOnRoot isLoaded={isLoaded}>
+      <Switch>
+        <Route path={SIGN_IN_PAGE}>
+          <SignInPage />
+        </Route>
+        <Route path={SIGN_UP_PAGE}>
+          <SignUpPage />
+        </Route>
+      </Switch>
+    </SignOnRoot>
   )
 }
--- packages/web/src/pages/sign-up-page/pages/SelectArtistsPage.tsx
+++ packages/web/src/pages/sign-up-page/pages/SelectArtistsPage.tsx
@@ -9,6 +9,7 @@ import {
   useGetTopArtistsInGenre
 } from '@audius/common'
 import { Flex, Text, SelectablePill, Paper, useTheme } from '@audius/harmony'
+import { useSpring, animated } from '@react-spring/web'
 import { Form, Formik } from 'formik'
 import { useDispatch } from 'react-redux'
 import { z } from 'zod'
@@ -33,6 +34,8 @@ import {
   ScrollView
 } from '../components/layout'
 import { SelectArtistsPreviewContextProvider } from '../utils/selectArtistsPreviewContext'
+
+const AnimatedFlex = animated(Flex)
 
 const messages = {
   header: 'Follow At Least 3 Artists',
@@ -99,6 +102,17 @@ export const SelectArtistsPage = () => {
 
   const ArtistsList = isMobile ? Flex : Paper
 
+  const styles = useSpring({
+    from: {
+      opacity: 0,
+      transform: 'translateX(100%)'
+    },
+    to: {
+      opacity: 1,
+      transform: 'translateX(0%)'
+    }
+  })
+
   return (
     <Formik
       initialValues={initialValues}
@@ -110,10 +124,11 @@ export const SelectArtistsPage = () => {
         return (
           <ScrollView as={Form} gap={isMobile ? undefined : '3xl'}>
             <AccountHeader mode='viewing' />
-            <Flex
+            <AnimatedFlex
               direction='column'
               mh={isMobile ? undefined : '5xl'}
               mb={isMobile ? undefined : 'xl'}
+              style={styles}
             >
               <Flex
                 direction='column'
@@ -193,7 +208,7 @@ export const SelectArtistsPage = () => {
                   </Flex>
                 </ArtistsList>
               </SelectArtistsPreviewContextProvider>
-            </Flex>
+            </AnimatedFlex>
             <PageFooter
               sticky
               buttonProps={{
