13da65127a57899f9a6f6bc8a905eef262d27c89
Saliou Diallo <saliou@audius.co>
2023-12-08 13:52:11 -0500

    Add download gated columns to dn track metadata

---
+++ packages/discovery-provider/ddl/migrations/0042_add_track_metadata_columns.sql
@@ -0,0 +1,14 @@
+begin;
+
+-- add new columns
+alter table tracks add column if not exists orig_file_cid varchar;
+alter table tracks add column if not exists orig_filename varchar;
+alter table tracks add column if not exists is_download_gated boolean not null default false;
+alter table tracks add column if not exists download_conditions jsonb;
+
+-- set default values for is_download_gated and download_conditions
+-- based on if the track is usdc purchase gated
+update tracks set is_download_gated = true where is_premium is true and (download->>'is_downloadable')::boolean is true;
+update tracks set download_conditions = premium_conditions where is_premium is true and (download->>'is_downloadable')::boolean is true;
+
+commit;
