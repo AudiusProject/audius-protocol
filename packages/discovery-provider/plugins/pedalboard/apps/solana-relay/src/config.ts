1f9949824618f348a618ee8a87c559ac64292f83
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-12-11 10:56:18 -0500

    [PAY-2178] Initial implementation of payment router indexing (#6892)

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/config.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/config.ts
@@ -46,6 +46,9 @@ const readConfig = () => {
     audius_solana_user_bank_program_address: str({
       default: 'testHKV1B56fbvop4w6f2cTGEub9dRQ2Euta5VmqdX9'
     }),
+    audius_solana_payment_router_program_address: str({
+      default: 'apaySbqV1XAmuiGszeN4NyWrXkkMrnuJVoNhzmS1AMa'
+    }),
     audius_solana_rewards_manager_program_address: str({
       default: 'testLsJKtyABc9UXJF8JWFKf1YH4LmqCWBC42c6akPb'
     }),
@@ -89,6 +92,7 @@ const readConfig = () => {
     rewardsManagerProgramId: env.audius_solana_rewards_manager_program_address,
     rewardsManagerAccountAddress: env.audius_solana_rewards_manager_account,
     claimableTokenProgramId: env.audius_solana_user_bank_program_address,
+    paymentRouterProgramId: env.audius_solana_payment_router_program_address,
     trackListenCountProgramId: env.audius_solana_track_listen_count_address,
     usdcMintAddress: env.audius_solana_usdc_mint,
     waudioMintAddress: env.audius_solana_waudio_mint,
