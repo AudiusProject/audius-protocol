78821ee514a1d303fc348f241cc96ffe9d0eaa64
sliptype <sliptype@gmail.com>
2023-12-14 11:16:22 -0600

    Simple ClientOnly

--- packages/web/src/components/client-only/ClientOnly.tsx
+++ packages/web/src/components/client-only/ClientOnly.tsx
@@ -1,18 +1,25 @@
-import { ReactNode, Suspense, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 type ClientOnlyProps = {
   children: ReactNode
   fallback?: ReactNode
 }
 
+/**
+ * A simple wrapper that only renders its children on the client.
+ * This is used to disable certain parts of the app from rendering on the server.
+ *
+ * If you need to lazy load something on the client only, use vike-react/ClientOnly
+ */
 export const ClientOnly = (props: ClientOnlyProps) => {
-  const { children, fallback } = props
+  const { children, fallback = null } = props
 
-  const [clientChildren, setClientChildren] = useState(fallback)
+  const [isClientSide, setIsClientSide] = useState(false)
 
+  // useEffect only runs on the client
   useEffect(() => {
-    setClientChildren(children)
+    setIsClientSide(true)
   }, [])
 
-  return <Suspense fallback={fallback}>{clientChildren}</Suspense>
+  return <>{isClientSide ? children : fallback}</>
 }
