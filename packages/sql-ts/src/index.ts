add28e8e0e819d58e6ad425f152a08dbeea7f467
Steve Perkins <stereosteve@users.noreply.github.com>
2023-12-18 14:02:57 -0500

    Quick fix to use release_date for feed ordering

--- packages/sql-ts/src/index.ts
+++ packages/sql-ts/src/index.ts
@@ -1,17 +1,19 @@
+import 'dotenv/config'
+
 import Knex from 'knex'
 import sqlts from '@rmp135/sql-ts'
 import { writeFileSync } from 'fs'
 
 const kpg = Knex({
   client: 'pg',
-  connection: process.env.audius_db_url,
+  connection: process.env.audius_db_url
 })
 
 async function updateDbTypes() {
   const config = {
     singularTableNames: true,
     tableNameCasing: 'pascal',
-    interfaceNameFormat: '${table}Row',
+    interfaceNameFormat: '${table}Row'
   }
   const tsString = await sqlts.toTypeScript(config, kpg as any)
   console.log(tsString)
