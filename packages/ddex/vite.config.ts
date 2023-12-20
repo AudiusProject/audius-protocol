528174f194079b33136950da13483e846cd1a1fb
Michelle Brier <michelle.brier4@gmail.com>
2023-12-20 15:35:50 -0800

    try path aliasing

--- packages/ddex/vite.config.ts
+++ packages/ddex/vite.config.ts
@@ -1,10 +1,14 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import {nodePolyfills} from 'vite-plugin-node-polyfills'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths({
+      root: './packages/ddex'
+    }),
     nodePolyfills({
       globals: {
         Buffer: true,
@@ -14,6 +18,16 @@ export default defineConfig({
       protocolImports: true,
     }),
   ],
+  resolve: {
+    alias: {
+      components: "/src/components",
+      pages: "/src/pages",
+      providers: "/src/providers",
+      utils: "/src/utils",
+      hooks: "/src/hooks",
+      assets: "/src/assets",
+    },
+  },
   // Set to /ddex/ in Dockerfile. Leave unset ('/') if deploying standalone in the future (e.g., to Cloudflare Pages).
   base: process.env.DDEX_BASE_URL || '/',
   build: {
