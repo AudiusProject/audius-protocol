31de81cf61593edcbcb4204d47b894c958580d23
Michelle Brier <michelle.brier4@gmail.com>
2023-12-19 18:08:39 -0800

    update eslint

--- packages/ddex/.eslintrc.cjs
+++ packages/ddex/.eslintrc.cjs
@@ -14,5 +14,14 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
 }
