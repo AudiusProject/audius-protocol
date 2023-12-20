b2c900e321f642ff5814d5f4400c086ee306fa13
Theo Ilie <theoilie.ti@gmail.com>
2023-12-20 12:15:04 -0800

    Add lock to tracks table for migration (#6998)

--- packages/discovery-provider/ddl/migrations/0044_set_release_to_created_where_null.sql
+++ packages/discovery-provider/ddl/migrations/0044_set_release_to_created_where_null.sql
@@ -1,5 +1,8 @@
 begin;
 
+SELECT pg_cancel_backend(pid) FROM pg_stat_activity WHERE state = 'active' and pid <> pg_backend_pid();
+lock table tracks in access exclusive mode;
+
 UPDATE tracks
 SET release_date = created_at
 WHERE release_date IS NULL;
