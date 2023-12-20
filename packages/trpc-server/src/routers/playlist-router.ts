2232553cf670ee74d088a8402179884bdbc9a843
Steve Perkins <stereosteve@users.noreply.github.com>
2023-12-20 14:45:52 -0500

    playlists.containTrackId method

--- packages/trpc-server/src/routers/playlist-router.ts
+++ packages/trpc-server/src/routers/playlist-router.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
 import { TRPCError } from '@trpc/server'
+import { esc } from './search-router'
 
 export const playlistRouter = router({
   get: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
@@ -9,5 +10,26 @@ export const playlistRouter = router({
       throw new TRPCError({ code: 'NOT_FOUND' })
     }
     return row
-  })
+  }),
+
+  containTrackId: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const found = await esc.search({
+        index: 'playlists',
+        query: {
+          bool: {
+            must: [
+              { term: { 'playlist_contents.track_ids.track': input } },
+              { term: { is_delete: false } },
+              { term: { is_private: false } },
+            ],
+            must_not: [],
+            should: [],
+          },
+        },
+        _source: false,
+      })
+      return found.hits.hits.map((h) => h._id)
+    }),
 })
