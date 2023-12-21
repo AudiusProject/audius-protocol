64de705ea6248b3a392fe8d09a355fbb58b85c3f
Alec Savvy <alecsavvy@outlook.com>
2023-12-20 17:07:17 -0700

    separate out for each case

--- packages/discovery-provider/ddl/migrations/0044_correct_picture_sizes.sql
+++ packages/discovery-provider/ddl/migrations/0044_correct_picture_sizes.sql
@@ -1,4 +1,7 @@
 update users
-set cover_photo_sizes = regexp_replace(cover_photo_sizes, '.*content\/([^\/]+)\/.*', '\1', 'g'),
-    profile_picture_sizes = regexp_replace(profile_picture_sizes, '.*content\/([^\/]+)\/.*', '\1', 'g')
+set cover_photo_sizes = regexp_replace(cover_photo_sizes, '.*content\/([^\/]+)\/.*', '\1', 'g')
 where cover_photo_sizes like '%:%';
+
+update users
+set profile_picture_sizes = regexp_replace(profile_picture_sizes, '.*content\/([^\/]+)\/.*', '\1', 'g')
+where profile_picture_sizes like '%:%';
