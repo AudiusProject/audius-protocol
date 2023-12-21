9e337d5e774409370823c9a2e233e955650eb34d
Theo Ilie <theoilie.ti@gmail.com>
2023-12-21 15:48:01 -0800

    Add ddex to audius-compose and improve its build (#7013)

--- packages/ddex/scripts/docker-entrypoint.sh
+++ packages/ddex/scripts/docker-entrypoint.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
-FIRST_RUN_FLAG="./.firstrun"
+# Directory where build artifacts are stored in the image before being copied to the volume
+TEMP_BUILD_DIR="/tmp/dist"
 
-if [ ! -f "$FIRST_RUN_FLAG" ]; then
-    echo "Building dist..."
-    npm run build
+# Directory mounted as a volume
+VOLUME_DIR="/app/dist"
 
-    if [ $? -eq 0 ]; then
-        echo "Successfully built dist"
-        touch "$FIRST_RUN_FLAG"
-    else
-        echo "'npm run build' failed with exit code $?. Exiting..."
-        exit 1
-    fi
-else
-    echo "dist already built"
+# Calculate the current checksum of the temporary build directory
+CHECKSUM_FILE="$VOLUME_DIR/.checksum"
+current_checksum=$(find "$TEMP_BUILD_DIR" -type f -exec md5sum {} \; | md5sum | cut -d' ' -f1)
+
+# Read the last checksum (if it exists)
+last_checksum=""
+if [ -f "$CHECKSUM_FILE" ]; then
+    last_checksum=$(cat "$CHECKSUM_FILE")
 fi
 
-exec npm run serve
+# Compare checksums and copy if different
+if [ "$current_checksum" != "$last_checksum" ]; then
+    echo "Changes detected. Updating files in volume..."
+    cp -r $TEMP_BUILD_DIR/* $VOLUME_DIR/
+    echo "$current_checksum" > "$CHECKSUM_FILE"
+else
+    echo "No changes detected. No update needed."
+fi
+
+# Keep the container running by tailing /dev/null
+tail -f /dev/null
