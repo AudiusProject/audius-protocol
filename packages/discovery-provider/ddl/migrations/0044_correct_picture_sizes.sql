2287702b25b91c0ae235432e37f6cdad226a311b
Alec Savvy <alecsavvy@outlook.com>
2023-12-20 17:02:10 -0700

    update to fix users wo manual handles

--- packages/discovery-provider/ddl/migrations/0044_correct_picture_sizes.sql
+++ packages/discovery-provider/ddl/migrations/0044_correct_picture_sizes.sql
@@ -1,4 +1,4 @@
 update users
 set cover_photo_sizes = regexp_replace(cover_photo_sizes, '.*content\/([^\/]+)\/.*', '\1', 'g'),
     profile_picture_sizes = regexp_replace(profile_picture_sizes, '.*content\/([^\/]+)\/.*', '\1', 'g')
-where handle_lc in ('nestamusic', 'taskforce', 'kingfallou', 'letsdancebirds', 'fuxsmrt', 'niko5462', 'wc0b0z', 'adubtor', 'ramey02', 'hoodrichclement', 'olilc', 'freshmoods', 'garworld', 'nikkikang', 'notjulian', 'producent', 'raymondphil2', 'bestmusictracks', 'dejayjd', 'mikeshoe504', 'derfoe', 'rodneycarroll5', 'plasticcogliquid');
+where cover_photo_sizes like '%:%';
