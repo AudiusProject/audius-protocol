759c52ddf30848089e7d28d1493891d79b811564
Raymond Jacobson <ray@audius.co>
2023-12-21 08:41:32 -0800

    ⚠️ [C-3538] Remove moment from common (#6992)

--- packages/common/src/utils/dayjs.ts
+++ packages/common/src/utils/dayjs.ts
@@ -1,9 +1,13 @@
 import dayjs, { Dayjs } from 'dayjs'
+import advancedFormat from 'dayjs/plugin/advancedFormat'
+import duration from 'dayjs/plugin/duration'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
+dayjs.extend(advancedFormat)
+dayjs.extend(duration)
 
 export default dayjs
 export { Dayjs }
