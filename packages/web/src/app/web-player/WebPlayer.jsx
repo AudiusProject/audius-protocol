ceb49b316d16e8e5fba2b9533502ce5c9b7a8d9c
Raymond Jacobson <ray@audius.co>
2023-12-28 17:25:38 -0800

    Revert "Re-enable ToS banner"
    This reverts commit 21e34cad24e73740833b970e757684e6c62e6b2e.

--- packages/web/src/app/web-player/WebPlayer.jsx
+++ packages/web/src/app/web-player/WebPlayer.jsx
@@ -32,7 +32,6 @@ import AppRedirectListener from 'components/app-redirect-popover/AppRedirectList
 import { AppRedirectPopover } from 'components/app-redirect-popover/components/AppRedirectPopover'
 import { AppBannerWrapper } from 'components/banner/AppBannerWrapper'
 import { DownloadAppBanner } from 'components/banner/DownloadAppBanner'
-import { TermsOfServiceUpdateBanner } from 'components/banner/TermsOfServiceUpdateBanner'
 import { UpdateAppBanner } from 'components/banner/UpdateAppBanner'
 import { Web3ErrorBanner } from 'components/banner/Web3ErrorBanner'
 import { ChatListener } from 'components/chat-listener/ChatListener'
@@ -451,7 +450,7 @@ class WebPlayer extends Component {
 
           {/* Product Announcement Banners */}
           {/* <DirectMessagesBanner /> */}
-          <TermsOfServiceUpdateBanner />
+          {/* <TermsOfServiceUpdateBanner /> */}
 
           <Web3ErrorBanner />
           {/* Other banners' logic is self-contained, but since this one uses the IPC
