edf24bc4af4c014c659d1457c2aee0b40af43edb
amendelsohn <andrew.j.mendelsohn@gmail.com>
2024-01-03 14:16:48 -0800

    trpc call works!

--- packages/trpc-server/src/routers/playlist-router.ts
+++ packages/trpc-server/src/routers/playlist-router.ts
@@ -1,8 +1,6 @@
 import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
 import { TRPCError } from '@trpc/server'
-import { esc } from './search-router'
-import { PlaylistRow } from '../db-tables'
 
 export type PlaylistRouteData = {
   playlist_id: number
@@ -19,36 +17,4 @@ export const playlistRouter = router({
     }
     return row
   }),
-
-  containTrackId: publicProcedure
-    .input(
-      z.object({ trackId: z.number(), collectionType: z.string().optional() })
-    )
-    .query(async ({ ctx, input }) => {
-      const found = await esc.search<PlaylistRow>({
-        index: 'playlists',
-        query: {
-          bool: {
-            must: [
-              { term: { 'playlist_contents.track_ids.track': input.trackId } },
-              { term: { is_delete: false } },
-              { term: { is_private: false } },
-              ...(input.collectionType === 'album'
-                ? [{ term: { is_album: true } }]
-                : []),
-              ...(input.collectionType === 'playlist'
-                ? [{ term: { is_album: false } }]
-                : []),
-            ],
-            must_not: [],
-            should: [],
-          },
-        },
-        _source: false,
-      })
-
-      return await ctx.loaders.playlistLoader.loadMany(
-        found.hits.hits.map((h) => parseInt(h._id))
-      )
-    }),
 })
--- packages/trpc-server/src/routers/track-router.ts
+++ packages/trpc-server/src/routers/track-router.ts
@@ -1,6 +1,14 @@
 import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
 import { TRPCError } from '@trpc/server'
+import { esc } from './search-router'
+
+type AlbumBacklinkMetadata = {
+  playlist_id: number
+  playlist_name: string
+  permalink: string
+  playlist_owner_id: number
+}
 
 export const trackRouter = router({
   get: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
@@ -9,5 +17,36 @@ export const trackRouter = router({
       throw new TRPCError({ code: 'NOT_FOUND' })
     }
     return row
-  })
+  }),
+
+  getAlbumBacklink: publicProcedure
+    .input(z.object({ trackId: z.number() }))
+    .query(async ({ input }) => {
+      const found = await esc.search<AlbumBacklinkMetadata>({
+        index: 'playlists',
+        query: {
+          bool: {
+            must: [
+              { term: { 'playlist_contents.track_ids.track': input.trackId } },
+              { term: { is_delete: false } },
+              { term: { is_private: false } },
+              { term: { is_album: true } },
+            ],
+            must_not: [],
+            should: [],
+          },
+        },
+        size: 1,
+        sort: [{ created_at: { order: 'desc' } }],
+        _source: [
+          'playlist_id',
+          'playlist_name',
+          'permalink',
+          'playlist_owner_id',
+        ],
+      })
+
+      const hits = found.hits.hits.map((h) => h._source)
+      return hits.length > 0 ? hits[0] : null
+    }),
 })
