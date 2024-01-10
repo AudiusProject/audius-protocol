fb9c2fe8388f218b39dc34ce89c0475c62c923b9
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2024-01-10 12:45:24 -0800

    [Protocol Dashboard] Hook up OAuth popup to creating dashboard wallet user [C-3562] (#7104)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- protocol-dashboard/src/services/Audius/sdk.ts
+++ protocol-dashboard/src/services/Audius/sdk.ts
@@ -1,11 +1,65 @@
-import { sdk } from '@audius/sdk'
+import {
+  DiscoveryNodeSelector,
+  EntityManager,
+  developmentConfig,
+  productionConfig,
+  sdk,
+  stagingConfig
+} from '@audius/sdk'
 
-let audiusSdk = null
+let instance = null
+let isReady = false
+let sdkResolve = null
+const sdkPromise = new Promise(resolve => {
+  sdkResolve = resolve
+})
 
-const initAudiusSdk = () => {
-  audiusSdk = sdk({ appName: 'Audius Protocol Dashboard' })
+const initAudiusSdk = ({
+  signTransaction,
+  getAddress
+}: {
+  signTransaction: (data: any) => Promise<string>
+  getAddress: () => Promise<string> | string
+}) => {
+  const env = import.meta.env.VITE_ENVIRONMENT
+  const bootstrapConfig =
+    env === 'development'
+      ? developmentConfig
+      : env === 'staging'
+      ? stagingConfig
+      : productionConfig
+  const {
+    discoveryNodes,
+    entityManagerContractAddress,
+    web3ProviderUrl,
+    identityServiceUrl
+  } = bootstrapConfig
+  const dnSelector = new DiscoveryNodeSelector({
+    bootstrapServices: discoveryNodes
+    // initialSelectedNode: 'https://discoveryprovider.staging.audius.co'
+  })
+  instance = sdk({
+    appName: 'Audius Protocol Dashboard',
+    services: {
+      auth: { signTransaction, getAddress },
+      discoveryNodeSelector: dnSelector,
+      entityManager: new EntityManager({
+        contractAddress: entityManagerContractAddress,
+        web3ProviderUrl: web3ProviderUrl,
+        identityServiceUrl: identityServiceUrl,
+        discoveryNodeSelector: dnSelector
+      })
+    }
+  })
+  sdkResolve()
+  isReady = true
 }
 
-initAudiusSdk()
+const audiusSdk = async () => {
+  if (!isReady) {
+    await sdkPromise
+  }
+  return instance
+}
 
-export { audiusSdk }
+export { audiusSdk, initAudiusSdk, isReady }
