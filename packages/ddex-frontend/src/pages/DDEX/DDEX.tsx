245f7f10d24946876d2b0fa5719b85d3d1dfe5cb
Theo Ilie <theoilie.ti@gmail.com>
2024-01-11 17:25:40 -0800

    Add uploadedBy and improve pagination ends (#7133)

--- packages/ddex-frontend/src/pages/DDEX/DDEX.tsx
+++ packages/ddex-frontend/src/pages/DDEX/DDEX.tsx
@@ -62,9 +62,11 @@ const ManageAudiusAccount = ({
 }
 
 const XmlImporter = ({
-  audiusSdk
+  audiusSdk,
+  uploader
 }: {
   audiusSdk: AudiusSdk | undefined | null
+  uploader: DecodedUserToken | null
 }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
   const [isDragging, setIsDragging] = useState(false)
@@ -134,6 +136,7 @@ const XmlImporter = ({
 
     const formData = new FormData()
     formData.append('file', selectedFile)
+    formData.append('uploadedBy', uploader?.userId || '')
 
     try {
       const response = await fetch('/api/upload', {
@@ -295,14 +298,14 @@ const Ddex = () => {
               className={styles.page}
             >
               <Flex gap='xl' direction='column'>
-                <XmlImporter audiusSdk={audiusSdk} />
+                <XmlImporter audiusSdk={audiusSdk} uploader={currentUser} />
                 {/* <ManageAudiusAccount
                   currentUser={currentUser || fakeUser}
                   isAdmin={true}
                   onChangeUser={handleOauth}
                   oauthError={oauthError}
                 />
-                <XmlImporter audiusSdk={audiusSdk} /> */}
+                <XmlImporter audiusSdk={audiusSdk}  uploader={fakeUser} /> */}
 
                 {isAdmin && (
                   <>
