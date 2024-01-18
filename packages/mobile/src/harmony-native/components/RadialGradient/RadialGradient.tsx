eba929996c97dc3ae52cbcfd2f32688e8edb2852
Dylan Jeffers <dylan@audius.co>
2024-01-18 15:43:40 -0800

    [C-3678, C-3657] Fix android radial-gradient bug, Fix sign-up panel top border-radius (#7236)

--- packages/mobile/src/harmony-native/components/RadialGradient/RadialGradient.tsx
+++ packages/mobile/src/harmony-native/components/RadialGradient/RadialGradient.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import { css } from '@emotion/native'
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
 import type { RadialGradientProps } from 'react-native-radial-gradient'
 import RNRadialGradient from 'react-native-radial-gradient'
 
@@ -10,20 +10,31 @@ const fullSize = css({ height: '100%', width: '100%' })
 // RadialGradient that uses percentages instead of pixels for center and radius
 // This makes RadialGradient much more useful and dynamic
 export const RadialGradient = (props: RadialGradientProps) => {
-  const { center: centerProp, radius: radiusProp, style, ...other } = props
+  const {
+    center: centerProp = [50, 50],
+    radius: radiusProp,
+    style,
+    ...other
+  } = props
   const [{ height, width }, setDimensions] = useState({
     height: 0,
     width: 0
   })
 
-  // Note using x,y coordinates because it allows dynamic updates
-  // This technically works even though the types don't allow it
-  const center = centerProp
-    ? ({
-        x: (centerProp[0] * width) / 100,
-        y: (centerProp[1] * height) / 100
-      } as unknown as RadialGradientProps['center'])
+  let center = centerProp
+    ? [(centerProp[0] * width) / 100, (centerProp[1] * height) / 100]
     : undefined
+
+  // We update the center prop to use x,y coordinates on iOS due to
+  // bug where center prop cannot be updated on iOS
+  if (Platform.OS === 'ios') {
+    center = center
+      ? ({
+          x: center[0],
+          y: center[1]
+        } as unknown as RadialGradientProps['center'])
+      : undefined
+  }
 
   // Since we cant have ellipse gradients, we use the average of height and width
   const radius = radiusProp
