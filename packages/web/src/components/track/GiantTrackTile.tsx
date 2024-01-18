f355286e4c60fe82b8bce8f2aa6cd6c494772601
Reed <3893871+dharit-tan@users.noreply.github.com>
2024-01-18 18:29:42 -0500

    Re-add downloads when lossless downloads flag disabled (#7235)

--- packages/web/src/components/track/GiantTrackTile.tsx
+++ packages/web/src/components/track/GiantTrackTile.tsx
@@ -61,6 +61,7 @@ import styles from './GiantTrackTile.module.css'
 import { GiantTrackTileProgressInfo } from './GiantTrackTileProgressInfo'
 import InfoLabel from './InfoLabel'
 import { PlayPauseButton } from './PlayPauseButton'
+import DownloadButtons from 'components/download-buttons/DownloadButtons'
 
 const { requestOpen: openPublishTrackConfirmationModal } =
   publishTrackConfirmationModalUIActions
@@ -503,9 +504,23 @@ export const GiantTrackTile = ({
       </>
     )
   }
+
   const renderScheduledReleaseRow = () => {
     return (
       <ScheduledReleaseGiantLabel released={released} isUnlisted={isUnlisted} />
+    )
+  }
+
+  const renderDownloadButtons = () => {
+    return (
+      <DownloadButtons
+        className={styles.downloadButtonsContainer}
+        trackId={trackId}
+        isOwner={isOwner}
+        following={following}
+        hasDownloadAccess={hasDownloadAccess}
+        onDownload={onDownload}
+      />
     )
   }
 
@@ -668,10 +683,7 @@ export const GiantTrackTile = ({
         </div>
       </div>
 
-      {isStreamGated &&
-      streamConditions &&
-      isLosslessDownloadsEnabled &&
-      !hasDownloadableAssets ? (
+      {isStreamGated && streamConditions ? (
         <GatedTrackSection
           isLoading={isLoading}
           trackId={trackId}
@@ -715,6 +727,7 @@ export const GiantTrackTile = ({
           </UserGeneratedText>
         ) : null}
         {renderTags()}
+        {!isLosslessDownloadsEnabled ? renderDownloadButtons() : null}
         {isLosslessDownloadsEnabled && hasDownloadableAssets ? (
           <Box pt='l' w='100%'>
             <DownloadSection trackId={trackId} onDownload={onDownload} />
