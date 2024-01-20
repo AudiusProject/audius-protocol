19f40563256d37e7a6d10dead869e5943a3a1936
sliptype <sliptype@gmail.com>
2024-01-19 18:02:32 -0600

    Fix postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,7 +8,9 @@ if [[ -z "${SKIP_POD_INSTALL}" ]]; then
   ln -s ../../../node_modules/react-native react-native
   ln -s ../../../node_modules/react-native-code-push react-native-code-push
   cd ../ios
-  bundle check || bundle install || true
+  if command -v bundle >/dev/null; then
+    bundle check || bundle install
+  fi
   if command -v pod >/dev/null; then
     pod install
   fi
