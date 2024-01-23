f300c707ebf886048816f18c0bb0738875e410eb
alecsavvy <alecjsavvy@gmail.com>
2024-01-23 09:23:49 -0700

    use abis within healthz (#7276)

--- monitoring/healthz/src/utils/abis.ts
+++ monitoring/healthz/src/utils/abis.ts
@@ -1,9 +1,5 @@
-import { abi as RegistryAbi } from "../../../../packages/libs/src/eth-contracts/ABIs/Registry.json"
-import { abi as ServiceProviderFactoryAbi } from "../../../../packages/libs/src/eth-contracts/ABIs/ServiceProviderFactory.json"
-import { abi as ServiceTypeManagerAbi } from "../../../../packages/libs/src/eth-contracts/ABIs/ServiceTypeManager.json"
+import { abi as RegistryAbi } from './abis/Registry.json'
+import { abi as ServiceProviderFactoryAbi } from './abis/ServiceProviderFactory.json'
+import { abi as ServiceTypeManagerAbi } from './abis/ServiceTypeManager.json'
 
-export {
-    RegistryAbi,
-    ServiceProviderFactoryAbi,
-    ServiceTypeManagerAbi
-}
+export { RegistryAbi, ServiceProviderFactoryAbi, ServiceTypeManagerAbi }
