8dcc037556fcb04722d1cb269f3d6ff5c1fa665e
Nikki Kang <kangaroo233@gmail.com>
2024-01-24 13:23:44 -0700

    fix ci again again

--- protocol-dashboard/vite.config.ts
+++ protocol-dashboard/vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   plugins: [
     react(),
     wasm(),
-    topLevelAwait(),
     svgr(),
 
     nodePolyfills({
