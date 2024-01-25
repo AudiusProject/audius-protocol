6d4fd3c9c183def8525af0871a6fbfb9224f5ea7
Michelle Brier <michelle.brier4@gmail.com>
2024-01-24 19:27:24 -0800

    use correct mongodb env var

--- packages/ddex/server/src/index.ts
+++ packages/ddex/server/src/index.ts
@@ -21,7 +21,7 @@ const port = process.env.DDEX_PORT || 3001
 ;(async () => {
   try {
     const dbUrl =
-      process.env.audius_db_url ||
+      process.env.DDEX_MONGODB_URL ||
       'mongodb://mongo:mongo@localhost:27017/ddex?authSource=admin'
     await dialDb(dbUrl)
 
