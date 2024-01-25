3540e14e6db8f066ad2a57bbe2c5d74197cc94d5
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2024-01-25 09:47:41 -0700

    Merge "Link SP to Audius Profile" feature branch to main (#7298)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>
    Co-authored-by: Theo Ilie <theoilie.ti@gmail.com>
    Co-authored-by: Michelle Brier <michelle.brier4@gmail.com>
    Co-authored-by: Raymond Jacobson <ray@audius.co>
    Co-authored-by: audius-infra <audius-infra@audius.co>
    Co-authored-by: Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
    Co-authored-by: Steve Perkins <steve@audius.co>
    Co-authored-by: Dylan Jeffers <dylan@audius.co>
    Co-authored-by: Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
    Co-authored-by: isaac <isaac@audius.co>
    Co-authored-by: Kyle Shanks <kyle.j.shanks@gmail.com>
    Co-authored-by: sliptype <sliptype@gmail.com>

--- protocol-dashboard/vite.config.ts
+++ protocol-dashboard/vite.config.ts
@@ -1,55 +1,50 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   plugins: [
     react(),
     wasm(),
-    topLevelAwait(),
-    tsconfigPaths({
-      root: 'protocol-dashboard'
-    }),
     svgr(),
-    
+
     nodePolyfills({
       exclude: ['fs'],
       globals: {
         Buffer: true,
         global: true,
-        process: true,
+        process: true
       },
-      protocolImports: true,
-    }),
+      protocolImports: true
+    })
   ],
 
   resolve: {
     alias: {
-      components: "/src/components",
-      containers: "/src/containers",
-      services: "/src/services",
-      utils: "/src/utils",
-      store: "/src/store",
-      hooks: "/src/hooks",
-      models: "/src/models",
-      types: "/src/types",
-      assets: "/src/assets",
-    },
+      components: '/src/components',
+      containers: '/src/containers',
+      services: '/src/services',
+      utils: '/src/utils',
+      store: '/src/store',
+      hooks: '/src/hooks',
+      models: '/src/models',
+      types: '/src/types',
+      assets: '/src/assets'
+    }
   },
 
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0'
   },
   // Base URL. Set DASHBOARD_BASE_URL to /dashboard/ in Dockerfile.
   // When deploying: leave DASHBOARD_BASE_URL unset (or set to './')
   base: process.env.VITE_DASHBOARD_BASE_URL || './',
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  }
 })
