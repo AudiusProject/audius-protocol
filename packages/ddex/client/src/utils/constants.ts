4697081c2c4ec3f33e9321ad86e9dc4346cf6926
Michelle Brier <michelle.brier4@gmail.com>
2024-01-25 19:59:53 -0800

    make packges/ddex/webapp for webapp

--- packages/ddex/client/src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export enum FeatureFlags {
-  DDEX_UPLOADS = 'ddex_uploads',
-  DDEX_ADMIN = 'ddex_admin'
-}
--- packages/ddex/client/src/utils/trpc.ts
+++
@@ -1,5 +0,0 @@
-import { createTRPCReact } from '@trpc/react-query'
-
-import type { AppRouter } from '../../../server/src'
-
-export const trpc = createTRPCReact<AppRouter>()
