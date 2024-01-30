d40e5ad80138ddce0aa4046cab1149a8ea61613a
KJ Shanks <kyle.j.shanks@gmail.com>
2024-01-29 17:16:58 -0800

    Convert Feed page store js files to ts

--- packages/common/src/store/pages/feed/actions.ts
+++ packages/common/src/store/pages/feed/actions.ts
@@ -1,25 +1,54 @@
-// @ts-nocheck
-// TODO(nkang) - convert to TS
+import { FeedFilter } from 'models/FeedFilter'
+import { ID } from 'models/Identifiers'
+
 export const FETCH_SUGGESTED_FOLLOW_USERS = 'FEED/FETCH_SUGGESTED_FOLLOW_USERS'
 export const FOLLOW_USERS = 'FEED/FOLLOW_USERS'
 export const SET_SUGGESTED_FOLLOWS = 'FEED/SET_SUGGESTED_FOLLOWS'
 export const SET_FEED_FILTER = 'FEED/SET_FEED_FILTER'
 
-export const fetchSuggestedFollowUsers = () => ({
-  type: FETCH_SUGGESTED_FOLLOW_USERS
-})
+export type FetchSuggestedFollowUsersAction = {
+  type: typeof FETCH_SUGGESTED_FOLLOW_USERS
+}
 
-export const followUsers = (userIds) => ({
+export type FollowUsersAction = {
+  type: typeof FOLLOW_USERS
+  userIds: ID[]
+}
+
+export type SetSuggestedFollowsAction = {
+  type: typeof SET_SUGGESTED_FOLLOWS
+  userIds: ID[]
+}
+
+export type SetFeedFilterAction = {
+  type: typeof SET_FEED_FILTER
+  filter: FeedFilter
+}
+
+export type FeedPageAction =
+  | FetchSuggestedFollowUsersAction
+  | FollowUsersAction
+  | SetSuggestedFollowsAction
+  | SetFeedFilterAction
+
+export const fetchSuggestedFollowUsers =
+  (): FetchSuggestedFollowUsersAction => ({
+    type: FETCH_SUGGESTED_FOLLOW_USERS
+  })
+
+export const followUsers = (userIds: ID[]): FollowUsersAction => ({
   type: FOLLOW_USERS,
   userIds
 })
 
-export const setSuggestedFollows = (userIds) => ({
+export const setSuggestedFollows = (
+  userIds: ID[]
+): SetSuggestedFollowsAction => ({
   type: SET_SUGGESTED_FOLLOWS,
   userIds
 })
 
-export const setFeedFilter = (filter) => ({
+export const setFeedFilter = (filter: FeedFilter): SetFeedFilterAction => ({
   type: SET_FEED_FILTER,
   filter
 })
