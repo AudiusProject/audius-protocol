a31aaf0ecc8eaef97eadb4114979844b682658aa
Dylan Jeffers <dylan@audius.co>
2024-01-30 16:31:15 -0800

    Fix all settled

--- packages/common/src/utils/allSettled.ts
+++ packages/common/src/utils/allSettled.ts
@@ -1,17 +1,16 @@
-export const allSettled =
-  // eslint-disable-next-line no-restricted-properties
-  Promise.allSettled.bind(Promise) ||
-  ((promises: any[]) =>
-    Promise.all(
-      promises.map((p: Promise<any>) =>
-        p
-          .then((value: any) => ({
-            status: 'fulfilled',
-            value
-          }))
-          .catch((reason: any) => ({
-            status: 'rejected',
-            reason
-          }))
+export const allSettled = Promise.allSettled
+  ? Promise.allSettled.bind(Promise)
+  : (promises: any[]) =>
+      Promise.all(
+        promises.map((p: Promise<any>) =>
+          p
+            .then((value: any) => ({
+              status: 'fulfilled',
+              value
+            }))
+            .catch((reason: any) => ({
+              status: 'rejected',
+              reason
+            }))
+        )
       )
-    ))
