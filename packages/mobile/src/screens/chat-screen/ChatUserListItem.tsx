f78602d4dc3b45e54e8f90bb6d645a41c7130da6
JD Francis <jd@audius.co>
2024-01-31 10:44:51 -0800

    Update chat user list item profile pic

--- packages/mobile/src/screens/chat-screen/ChatUserListItem.tsx
+++ packages/mobile/src/screens/chat-screen/ChatUserListItem.tsx
@@ -9,6 +9,7 @@ import {
   formatCount,
   useInboxUnavailableModal
 } from '@audius/common'
+import { css } from '@emotion/native'
 import { useSelector } from 'audius-client/src/common/hooks/useSelector'
 import { View, TouchableOpacity, Keyboard } from 'react-native'
 import { useDispatch } from 'react-redux'
@@ -18,8 +19,7 @@ import {
   IconKebabHorizontal,
   IconUser
 } from '@audius/harmony-native'
-import { Text } from 'app/components/core'
-import { ProfilePicture } from 'app/components/user'
+import { Text, ProfilePicture } from 'app/components/core'
 import { UserBadges } from 'app/components/user-badges'
 import { trpc } from 'app/services/trpc-client-mobile'
 import { setVisibility } from 'app/store/drawers/slice'
@@ -42,10 +42,6 @@ const useStyles = makeStyles(({ spacing, palette, typography }) => ({
   rootContainer: {
     backgroundColor: palette.white,
     flexGrow: 1
-  },
-  profilePicture: {
-    height: spacing(18),
-    width: spacing(18)
   },
   border: {
     borderBottomColor: palette.neutralLight8,
@@ -217,8 +213,9 @@ export const ChatUserListItem = ({
       <View style={styles.border}>
         <View style={styles.userContainer}>
           <ProfilePicture
-            profile={user}
-            style={[styles.profilePicture, !canCreateChat ? styles.dim : null]}
+            userId={user.user_id}
+            size='large'
+            style={!canCreateChat ? styles.dim : null}
           />
           <View style={styles.userDetailsContainer}>
             <View style={styles.topHalfContainer}>
