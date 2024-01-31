19dcb9076d4e7884bd6e3dcbde5d96cffddd8c76
JD Francis <jd@audius.co>
2024-01-30 18:01:14 -0800

    Add margin props to Avatar & Use new profile pic in left nav

--- packages/mobile/src/harmony-native/components/Avatar/Avatar.tsx
+++ packages/mobile/src/harmony-native/components/Avatar/Avatar.tsx
@@ -4,12 +4,15 @@ import { View } from 'react-native'
 import type { SetRequired } from 'type-fest'
 
 import { useTheme } from '@audius/harmony-native'
+import type { MarginProps } from 'app/harmony-native/utils/styleProps'
+import { useMargin } from 'app/harmony-native/utils/styleProps'
 
 import type { FastImageProps } from '../FastImage/FastImage'
 import { FastImage } from '../FastImage/FastImage'
 
 export type AvatarProps = Omit<HarmonyAvatarProps, 'src'> &
-  SetRequired<FastImageProps, 'accessibilityLabel'>
+  SetRequired<FastImageProps, 'accessibilityLabel'> &
+  MarginProps
 
 const sizeMap = {
   auto: '100%' as const,
@@ -41,6 +44,8 @@ export const Avatar = (props: AvatarProps) => {
 
   const { color, shadows } = useTheme()
 
+  const margin = useMargin(other)
+
   const borderRadius = 9999
 
   const rootCss = css({
@@ -63,7 +68,7 @@ export const Avatar = (props: AvatarProps) => {
   })
 
   return (
-    <View style={[rootCss, style]}>
+    <View style={[rootCss, margin, style]}>
       {source !== undefined ? (
         <FastImage style={imageCss} source={source} {...other}>
           {children}
---
+++ packages/mobile/src/harmony-native/utils/styleProps.ts
@@ -0,0 +1,25 @@
+import type { SpacingOptions } from '@audius/harmony'
+import { css } from '@emotion/native'
+import { useTheme } from '@emotion/react'
+
+export type MarginProps = {
+  m?: SpacingOptions
+  mt?: SpacingOptions
+  mr?: SpacingOptions
+  mb?: SpacingOptions
+  ml?: SpacingOptions
+  mx?: SpacingOptions
+  my?: SpacingOptions
+}
+export const useMargin = <T extends MarginProps>(props: T) => {
+  const { spacing } = useTheme()
+  return css({
+    ...(props.m && { margin: spacing[props.m] }),
+    ...(props.mt && { marginTop: spacing[props.mt] }),
+    ...(props.mr && { marginRight: spacing[props.mr] }),
+    ...(props.mb && { marginBottom: spacing[props.mb] }),
+    ...(props.ml && { marginLeft: spacing[props.ml] }),
+    ...(props.mx && { marginHorizontal: spacing[props.mx] }),
+    ...(props.my && { marginVertical: spacing[props.my] })
+  })
+}
