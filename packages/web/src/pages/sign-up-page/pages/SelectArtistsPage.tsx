ed8944d6fce54a7cd93f5b46f07d87bb02dbec27
Dylan Jeffers <dylan@audius.co>
2024-02-05 13:51:41 -0800

    Fix electric subgenre select-artist requests (#7327)

--- packages/web/src/pages/sign-up-page/pages/SelectArtistsPage.tsx
+++ packages/web/src/pages/sign-up-page/pages/SelectArtistsPage.tsx
@@ -176,7 +176,7 @@ export const SelectArtistsPage = () => {
                       name='genre'
                       label={convertGenreLabelToValue(genre as Genre)}
                       size={isMobile ? 'small' : 'large'}
-                      value={genre}
+                      value={convertGenreLabelToValue(genre as Genre)}
                       isSelected={currentGenre === genre}
                     />
                   ))}
