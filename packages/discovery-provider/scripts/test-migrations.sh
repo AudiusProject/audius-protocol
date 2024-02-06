de17911351880be7e3f0e5cbf8cd73bc131ca897
Steve Perkins <stereosteve@users.noreply.github.com>
2024-02-06 14:59:56 -0500

    run pg_migrate.sh test in CI

--- packages/discovery-provider/scripts/test-migrations.sh
+++ packages/discovery-provider/scripts/test-migrations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running pg_migrate.sh migrations"
-(cd ddl ; DB_URL="$audius_db_url" bash pg_migrate.sh)
+(cd ddl || exit ; DB_URL="$audius_db_url" bash pg_migrate.sh test)
 echo "Finished running pg_migrate.sh migrations"
