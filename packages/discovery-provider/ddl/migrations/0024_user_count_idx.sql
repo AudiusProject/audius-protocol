eda0116e776e1f85ef05fc74be8ffa3490640194
Steve Perkins <stereosteve@users.noreply.github.com>
2024-02-06 16:05:00 -0500

    Add local test setup for pg_migrate.sh. Fix idempotency

--- packages/discovery-provider/ddl/migrations/0024_user_count_idx.sql
+++ packages/discovery-provider/ddl/migrations/0024_user_count_idx.sql
@@ -1,9 +1,9 @@
 BEGIN;
-  CREATE INDEX ix_users_active_count
+  CREATE INDEX IF NOT EXISTS ix_users_active_count
   ON "users" ("is_deactivated", "is_current", "handle_lc", "is_available")
   where
     "is_deactivated" = false
     and "is_current" = true
-    and "handle_lc" is not null	
+    and "handle_lc" is not null
     and "is_available" = true;
 COMMIT;
