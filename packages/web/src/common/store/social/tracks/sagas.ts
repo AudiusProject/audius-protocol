4b7be8b0767b0b9dabef5a82e9b6aaf01eae568d
Saliou Diallo <saliou@audius.co>
2024-02-07 11:13:25 -0500

    Pass correct nft access signature based on mp3 vs original download

--- packages/web/src/common/store/social/tracks/sagas.ts
+++ packages/web/src/common/store/social/tracks/sagas.ts
@@ -658,7 +658,9 @@ function* downloadTrackV1Helper({
 
     const trackId = track.track_id
     const nftAccessSignatureMap = yield* select(getNftAccessSignatureMap)
-    const nftAccessSignature = nftAccessSignatureMap[trackId]?.mp3 ?? null
+    const nftAccessSignature = original
+      ? nftAccessSignatureMap[trackId]?.original ?? null
+      : nftAccessSignatureMap[trackId]?.mp3 ?? null
     queryParams = (yield* call(getQueryParams, {
       audiusBackendInstance,
       nftAccessSignature
@@ -769,7 +771,9 @@ function* downloadTracks({
     let queryParams: QueryParams = {}
 
     const nftAccessSignatureMap = yield* select(getNftAccessSignatureMap)
-    const nftAccessSignature = nftAccessSignatureMap[parentTrackId]?.mp3 ?? null
+    const nftAccessSignature = original
+      ? nftAccessSignatureMap[parentTrackId]?.original ?? null
+      : nftAccessSignatureMap[parentTrackId]?.mp3 ?? null
     queryParams = (yield* call(getQueryParams, {
       audiusBackendInstance,
       nftAccessSignature
