f0fb0f68eedf4cd3ee92585b18389d456699d8f7
Alec Savvy <alecsavvy@outlook.com>
2024-02-09 12:01:41 -0700

    update timings and set dryrun to true on cron

--- packages/discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/main.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/main.ts
@@ -13,11 +13,15 @@ export const main = async () => {
     .run()
 }
 
-// Friday at 12:05 pm PST, extra five minutes for trending to calculate
-cron.schedule('5 12 * * 5', () => {
+// Friday at 12:01 pm PST, extra five minutes for trending to calculate
+cron.schedule('1 12 * * 5', () => {
   initSharedData().then((data) => {
     // make new appdata instance to satisfy types
     const appData = new App<SharedData>({ appData: data })
+    appData.updateAppData((data) => {
+      data.dryRun = false
+      return data
+    })
     announceTopFiveTrending(appData).catch(e => console.error("Announcement failed: ", e))
     disburseTrendingRewards(appData).catch(e => console.error("Disbursment failed: ", e))
   })
