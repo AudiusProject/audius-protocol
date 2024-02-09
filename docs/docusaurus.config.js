8ffa3d3bf6fd5def1c21614c397528155c0a50b2
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2024-02-09 10:35:30 -0800

    add google analytics to docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         },
         theme: {
           customCss: './src/css/custom.css'
+        },
+        gtag: {
+          trackingID: 'G-XSRDQBKXVX',
+          anonymizeIP: true,
         }
       })
     ]
