00b4df5d0d576534f784951d4ccf4fec80cb323b
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2024-02-12 11:00:42 -0800

    Improve Connect MetaMask UX, esp. when both MetaMask and Phantom are installed [C-3750] (#7537)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- protocol-dashboard/src/components/ConnectMetaMaskModal/ConnectMetaMaskModal.tsx
+++ protocol-dashboard/src/components/ConnectMetaMaskModal/ConnectMetaMaskModal.tsx
@@ -1,40 +1,99 @@
-import React, { useCallback } from 'react'
+import React, { ReactNode, useCallback, useEffect } from 'react'
 import Modal from 'components/Modal'
 import Button, { ButtonType } from 'components/Button'
 import styles from './ConnectMetaMaskModal.module.css'
+import {
+  ETH_NETWORK_ID,
+  decimalNetworkIdToHexNetworkId
+} from 'utils/switchNetwork'
+import { getMetamaskChainId } from 'services/Audius/setup'
 
 const messages = {
-  title: 'Connect MetaMask to Continue',
-  description: 'Please sign in with MetaMask to continue',
+  connectTitle: 'Connect MetaMask to Continue',
+  misconfiguredTitle: 'Your MetaMask is Misconfigured',
+  description: 'Please sign in with MetaMask to continue.',
   okayBtn: 'OKAY',
-  openMetaMaskBtn: 'Open MetaMask'
+  openMetaMaskBtn: 'Open MetaMask',
+  openExtensionBtn: 'Open Wallet',
+  chooseMetaMaskOnPhantom:
+    'Please choose MetaMask as the extension you want to connect with.',
+  ifNoPopupWindow:
+    'If you do not see the prompt to select your extension, open Phantom and ',
+  setMetaMaskDefault: 'set MetaMask as your default app wallet',
+  setMetaMaskDefaultSuffix: ' in your Phantom settings.',
+  openPhantomPrompt: 'It looks like you have Phantom installed - make sure to ',
+  openPhantomPromptAddition:
+    'It looks like you have Phantom installed, so make sure to also ',
+  setMetaMaskDefaultAdditionSuffix: ' in your Phantom settings.',
+  setEthereumMainnet:
+    'Please make sure you set the network on MetaMask to Ethereum Mainnet.'
 }
 
 type OwnProps = {
   isOpen: boolean
   onClose: () => void
+  isMisconfigured?: boolean
 }
 
-type ConnectMetaMaskModalProps = OwnProps
-
-const ConnectMetaMaskModal: React.FC<ConnectMetaMaskModalProps> = ({
-  isOpen,
-  onClose
-}: ConnectMetaMaskModalProps) => {
+type ConnectMetaMaskContentProps = {
+  onClose: () => void
+}
+const ConnectMetaMaskContent = ({ onClose }: ConnectMetaMaskContentProps) => {
   const onOpenMetaMask = useCallback(() => {
     if (window.ethereum) {
       window.ethereum.request({ method: 'eth_requestAccounts' })
     }
   }, [])
+  const isPhantomInstalled = window.phantom?.solana?.isPhantom
+
+  const isSelectingExtensionOnPhantom =
+    isPhantomInstalled && window.ethereum.isSelectingExtension
+  let description: ReactNode
+  if (isPhantomInstalled) {
+    if (isSelectingExtensionOnPhantom) {
+      description = (
+        <>
+          <p>{messages.chooseMetaMaskOnPhantom}</p>
+          <p>
+            {messages.ifNoPopupWindow}
+            <a
+              className={styles.externalLink}
+              target="_blank"
+              rel="noreferrer"
+              href="https://help.phantom.app/hc/en-us/articles/12182813094163-How-to-use-Phantom-Extension-Alongside-Other-Crypto-Wallets"
+            >
+              {messages.setMetaMaskDefault}
+            </a>
+            {messages.setMetaMaskDefaultSuffix}
+          </p>
+        </>
+      )
+    } else {
+      description = (
+        <>
+          <p>{messages.description}</p>
+          <p>
+            {messages.openPhantomPrompt}
+            <a
+              className={styles.externalLink}
+              target="_blank"
+              rel="noreferrer"
+              href="https://help.phantom.app/hc/en-us/articles/12182813094163-How-to-use-Phantom-Extension-Alongside-Other-Crypto-Wallets"
+            >
+              {messages.setMetaMaskDefault}
+            </a>
+            {messages.setMetaMaskDefaultSuffix}
+          </p>
+        </>
+      )
+    }
+  } else {
+    description = messages.description
+  }
+
   return (
-    <Modal
-      title={messages.title}
-      className={styles.container}
-      isOpen={isOpen}
-      onClose={onClose}
-      isCloseable={true}
-    >
-      <div className={styles.description}>{messages.description}</div>
+    <>
+      <div className={styles.description}>{description}</div>
       <div className={styles.btnContainer}>
         <Button
           text={messages.okayBtn}
@@ -42,6 +101,7 @@ const ConnectMetaMaskModal: React.FC<ConnectMetaMaskModalProps> = ({
           type={ButtonType.PRIMARY_ALT}
           onClick={onClose}
         />
+
         <Button
           text={messages.openMetaMaskBtn}
           className={styles.openMetaMaskBtn}
@@ -49,6 +109,87 @@ const ConnectMetaMaskModal: React.FC<ConnectMetaMaskModalProps> = ({
           onClick={onOpenMetaMask}
         />
       </div>
+    </>
+  )
+}
+
+const MisconfiguredMetaMaskContent = ({
+  onClose
+}: ConnectMetaMaskContentProps) => {
+  const isPhantomInstalled =
+    window.phantom?.solana?.isPhantom || !!window.ethereum.isPhantom
+
+  const onOpenMetaMask = async () => {
+    const chainId = await getMetamaskChainId()
+    if (chainId !== ETH_NETWORK_ID) {
+      window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: decimalNetworkIdToHexNetworkId(ETH_NETWORK_ID) }]
+      })
+    } else {
+      window.ethereum.enable()
+    }
+  }
+  return (
+    <>
+      <div className={styles.description}>
+        <p>{messages.setEthereumMainnet}</p>
+        {isPhantomInstalled ? (
+          <p>
+            {messages.openPhantomPromptAddition}{' '}
+            <a
+              className={styles.externalLink}
+              target="_blank"
+              rel="noreferrer"
+              href="https://help.phantom.app/hc/en-us/articles/12182813094163-How-to-use-Phantom-Extension-Alongside-Other-Crypto-Wallets"
+            >
+              {messages.setMetaMaskDefault}
+            </a>
+            {messages.setMetaMaskDefaultAdditionSuffix}
+          </p>
+        ) : null}
+      </div>
+      <div className={styles.btnContainer}>
+        <Button
+          text={messages.okayBtn}
+          className={styles.okayBtn}
+          type={ButtonType.PRIMARY_ALT}
+          onClick={onClose}
+        />
+
+        <Button
+          text={messages.openExtensionBtn}
+          className={styles.openMetaMaskBtn}
+          type={ButtonType.PRIMARY}
+          onClick={onOpenMetaMask}
+        />
+      </div>
+    </>
+  )
+}
+
+type ConnectMetaMaskModalProps = OwnProps
+
+const ConnectMetaMaskModal: React.FC<ConnectMetaMaskModalProps> = ({
+  isOpen,
+  onClose,
+  isMisconfigured
+}: ConnectMetaMaskModalProps) => {
+  return (
+    <Modal
+      title={
+        isMisconfigured ? messages.misconfiguredTitle : messages.connectTitle
+      }
+      className={styles.container}
+      isOpen={isOpen}
+      onClose={onClose}
+      isCloseable={true}
+    >
+      {isMisconfigured ? (
+        <MisconfiguredMetaMaskContent onClose={onClose} />
+      ) : (
+        <ConnectMetaMaskContent onClose={onClose} />
+      )}
     </Modal>
   )
 }
