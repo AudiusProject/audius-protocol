eff2cbb797af1a8376fdfcc80002b5a531c52745
Alec Savvy <alecsavvy@outlook.com>
2024-02-15 10:34:32 -0700

    rm log

--- packages/discovery-provider/plugins/pedalboard/apps/relay/src/txRelay.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/relay/src/txRelay.ts
@@ -1,17 +1,17 @@
-import { config, wallets, web3 } from ".";
-import { internalError } from "./error";
-import { logger } from "./logger";
-import { confirm } from "./web3";
+import { config, wallets, web3 } from '.'
+import { internalError } from './error'
+import { logger } from './logger'
+import { confirm } from './web3'
 import {
   TransactionReceipt,
-  TransactionRequest,
-} from "@ethersproject/abstract-provider";
-import { NextFunction, Request, Response } from "express";
+  TransactionRequest
+} from '@ethersproject/abstract-provider'
+import { NextFunction, Request, Response } from 'express'
 
 export type RelayedTransaction = {
-  receipt: TransactionReceipt;
-  transaction: TransactionRequest;
-};
+  receipt: TransactionReceipt
+  transaction: TransactionRequest
+}
 
 export const relayTransaction = async (
   _req: Request,
@@ -19,34 +19,32 @@ export const relayTransaction = async (
   next: NextFunction
 ) => {
   // pull info from validated request
-  const { validatedRelayRequest } = res.locals.ctx;
-  const { encodedABI, gasLimit, contractAddress } = validatedRelayRequest;
+  const { validatedRelayRequest } = res.locals.ctx
+  const { encodedABI, gasLimit, contractAddress } = validatedRelayRequest
 
-  const senderWallet = wallets.selectNextWallet();
-  const address = await senderWallet.getAddress();
+  const senderWallet = wallets.selectNextWallet()
+  const address = await senderWallet.getAddress()
 
   // gather some transaction params
-  const nonce = await web3.getTransactionCount(address);
+  const nonce = await web3.getTransactionCount(address)
 
-  const to = contractAddress;
-  const value = "0x00";
-  const data = encodedABI;
+  const to = contractAddress
+  const value = '0x00'
+  const data = encodedABI
 
   // assemble, sign, and send transaction
-  const transaction = { nonce, gasLimit, to, value, data };
-  await senderWallet.signTransaction(transaction);
-  const submit = await senderWallet.sendTransaction(transaction);
-
-  logger.info({ validatedRelayRequest }, "HEREEEEE")
+  const transaction = { nonce, gasLimit, to, value, data }
+  await senderWallet.signTransaction(transaction)
+  const submit = await senderWallet.sendTransaction(transaction)
 
   // query chain until tx is mined
   try {
-    const receipt = await confirm(submit.hash);
+    const receipt = await confirm(submit.hash)
     receipt.blockNumber += config.finalPoaBlock
-    res.send({ receipt });
+    res.send({ receipt })
   } catch (e) {
-    internalError(next, e as string);
-    return;
+    internalError(next, e as string)
+    return
   }
-  next();
-};
+  next()
+}
