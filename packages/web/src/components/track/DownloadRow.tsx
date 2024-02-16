d4dab566cf3a93f6afc7846343d338593a6b8624
Reed <3893871+dharit-tan@users.noreply.github.com>
2024-02-16 17:29:32 -0500

    [PAY-2509] Bring lossless downloads mobile web up to spec (#7638)

--- packages/web/src/components/track/DownloadRow.tsx
+++ packages/web/src/components/track/DownloadRow.tsx
@@ -12,6 +12,7 @@ import { shallowEqual, useSelector } from 'react-redux'
 import { Icon } from 'components/Icon'
 import LoadingSpinner from 'components/loading-spinner/LoadingSpinner'
 import Tooltip from 'components/tooltip/Tooltip'
+import { useIsMobile } from 'hooks/useIsMobile'
 
 import styles from './DownloadRow.module.css'
 
@@ -47,6 +48,7 @@ export const DownloadRow = ({
   filename,
   isLoading
 }: DownloadRowProps) => {
+  const isMobile = useIsMobile()
   const track = useSelector(
     (state: CommonState) => getTrack(state, { id: trackId }),
     shallowEqual
@@ -78,12 +80,14 @@ export const DownloadRow = ({
       direction='row'
       alignItems='center'
       justifyContent='space-between'
+      w='100%'
+      gap='xs'
     >
-      <Flex gap='xl' alignItems='center'>
+      <Flex gap='xl' alignItems='center' w='100%' css={{ overflow: 'hidden' }}>
         <Text variant='body' color='subdued'>
           {index}
         </Text>
-        <Flex direction='column' gap='xs'>
+        <Flex direction='column' gap='xs' css={{ overflow: 'hidden' }} w='100%'>
           <Text variant='body' strength='default'>
             {category
               ? stemCategoryFriendlyNames[category]
@@ -91,7 +95,15 @@ export const DownloadRow = ({
               ? stemCategoryFriendlyNames[track?.stem_of?.category]
               : messages.fullTrack}
           </Text>
-          <Text variant='body' color='subdued'>
+          <Text
+            variant='body'
+            color='subdued'
+            css={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              'white-space': 'nowrap'
+            }}
+          >
             {getDownloadFilename({
               filename: filename ?? track?.orig_filename,
               isOriginal
@@ -100,7 +112,7 @@ export const DownloadRow = ({
         </Flex>
       </Flex>
       <Flex gap='2xl'>
-        {size ? (
+        {size && !isMobile ? (
           <Text variant='body' size='s' color='subdued'>
             {formatBytes(size)}
           </Text>
