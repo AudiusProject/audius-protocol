9d3815543d9d285c41f6dd3be869e6d92b54f5f4
Dylan Jeffers <dylan@audius.co>
2024-02-16 09:54:52 -0800

    Fix mobile play icons (#7628)

--- packages/mobile/src/components/track-list/TablePlayButton.tsx
+++ packages/mobile/src/components/track-list/TablePlayButton.tsx
@@ -2,12 +2,10 @@ import type { StyleProp, ViewStyle } from 'react-native'
 import { TouchableOpacity } from 'react-native'
 
 import { IconPause, IconPlay } from '@audius/harmony-native'
-import { makeStyles } from 'app/styles'
 
 type TablePlayButtonProps = {
   playing?: boolean
   paused: boolean
-  hideDefault?: boolean
   onPress?: () => void
   styles?: {
     root: StyleProp<ViewStyle>
@@ -15,33 +13,18 @@ type TablePlayButtonProps = {
   }
 }
 
-const useStyles = makeStyles(({ palette, spacing }) => ({
-  root: {},
-  svg: {},
-  hideDefault: {}
-}))
-
 export const TablePlayButton = ({
   playing = false,
   paused,
-  hideDefault = true,
   onPress = () => {},
   styles: propStyles
 }: TablePlayButtonProps) => {
-  const styles = useStyles()
-
   return (
-    <TouchableOpacity style={[styles.root, propStyles?.root]} onPress={onPress}>
+    <TouchableOpacity style={propStyles?.root} onPress={onPress}>
       {playing && !paused ? (
-        <IconPause style={[styles.svg, propStyles?.svg]} />
+        <IconPause color='white' style={propStyles?.svg} />
       ) : (
-        <IconPlay
-          style={[
-            styles.svg,
-            hideDefault && !playing ? styles.hideDefault : {},
-            propStyles?.svg
-          ]}
-        />
+        <IconPlay color='default' style={propStyles?.svg} />
       )}
     </TouchableOpacity>
   )
