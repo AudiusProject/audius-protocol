8951239d3e309e6c5949b2407a04769d6de1e0a1
Alec Savvy <alecsavvy@outlook.com>
2024-02-17 22:04:45 -0700

    rm sdk stuff for now

--- packages/warpcast-frame/src/sdk.ts
+++ packages/warpcast-frame/src/sdk.ts
@@ -1,24 +1,6 @@
-import { AudiusSdk, TrackResponse, UserResponse, sdk } from '@audius/sdk'
+import { TrackResponse, UserResponse } from '@audius/sdk'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-
-const StageApiKey = import.meta.env.VITE_API_KEY_STAGE || ''
-const StageApiSecret = import.meta.env.VITE_API_SECRET_STAGE || ''
-const ProdAPiKey = import.meta.env.VITE_API_KEY_PROD || ''
-const ProdApiSecret = import.meta.env.VITE_API_SECRET_PROD || ''
-
-// const audiusSdkStage = sdk({
-//   apiKey: StageApiKey,
-//   apiSecret: StageApiSecret
-// })
-
-// const audiusSdkProd = sdk({
-//   apiKey: ProdAPiKey,
-//   apiSecret: ProdApiSecret
-// })
-
-// export const getSdk = (env: string): AudiusSdk =>
-//   env === 'stage' ? audiusSdkStage : audiusSdkProd
 
 export const getBaseUrl = (env: string): string =>
   env == 'stage'
