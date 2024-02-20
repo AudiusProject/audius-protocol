578530d1f5b2a721609b17a3a042dee44e09ce29
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-02-20 14:58:33 -0500

    change playlist_id to int

--- packages/discovery-provider/src/models/playlists/playlists_tracks_relations.py
+++ packages/discovery-provider/src/models/playlists/playlists_tracks_relations.py
@@ -8,7 +8,7 @@ from src.models.model_utils import RepresentableMixin
 class PlaylistsTracksRelations(Base, RepresentableMixin):
     __tablename__ = "playlists_tracks_relations"
 
-    playlist_id = Column(String, nullable=False, primary_key=True, index=True)
+    playlist_id = Column(Integer, nullable=False, primary_key=True, index=True)
     track_id = Column(Integer, nullable=False, primary_key=True, index=True)
     is_delete = Column(Boolean, nullable=False)
     created_at = Column(
