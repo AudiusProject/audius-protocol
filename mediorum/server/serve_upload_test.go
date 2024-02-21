0521793e8372a0de304c513f3ab5b568a020b0a6
Steve Perkins <stereosteve@users.noreply.github.com>
2024-02-21 11:50:54 -0500

    test upload placement

--- mediorum/server/serve_upload_test.go
+++ mediorum/server/serve_upload_test.go
@@ -1,12 +1,7 @@
 package server
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"mime/multipart"
-	"net/http"
-	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -14,36 +9,18 @@ import (
 )
 
 func TestUploadFile(t *testing.T) {
-	ss := testNetwork[0]
+	s1 := testNetwork[0]
 	s2 := testNetwork[1]
 
-	var b bytes.Buffer
-	w := multipart.NewWriter(&b)
-
-	{
-		fw, err := w.CreateFormField("template")
-		assert.NoError(t, err)
-		fw.Write([]byte("audio"))
-	}
-
-	fw, err := w.CreateFormFile(filesFormFieldName, "beep.wav")
-	assert.NoError(t, err)
-
-	hit, err := os.Open("testdata/beep.wav")
-	assert.NoError(t, err)
-
-	io.Copy(fw, hit)
-	hit.Close()
-	w.Close()
-
-	resp, err := http.Post(ss.Config.Self.Host+"/uploads", w.FormDataContentType(), &b)
-	assert.NoError(t, err)
-	assert.Equal(t, resp.StatusCode, 200)
-
-	dec := json.NewDecoder(resp.Body)
 	var uploads []Upload
-	err = dec.Decode(&uploads)
-	assert.NoError(t, err)
+
+	resp := s1.reqClient.R().
+		SetFile("files", "testdata/beep.wav").
+		SetFormData(map[string]string{"template": "audio"}).
+		SetSuccessResult(&uploads).
+		MustPost(s1.Config.Self.Host + "/uploads")
+
+	assert.Equal(t, resp.StatusCode, 200)
 	uploadId := uploads[0].ID
 
 	// force sweep (since blob changes SkipBroadcast)
@@ -64,5 +41,58 @@ func TestUploadFile(t *testing.T) {
 	}
 
 	assert.Equal(t, u2.TranscodeProgress, 1.0)
-	assert.Len(t, u2.TranscodedMirrors, ss.Config.ReplicationFactor)
+	assert.Len(t, u2.TranscodedMirrors, s1.Config.ReplicationFactor)
+}
+
+func TestUploadPlacement(t *testing.T) {
+	s1 := testNetwork[0]
+	s2 := testNetwork[1]
+	s3 := testNetwork[2]
+	// s4 := testNetwork[3]
+	s5 := testNetwork[4]
+
+	examplePlacement := []string{
+		s3.Config.Self.Host,
+		s5.Config.Self.Host,
+	}
+
+	var uploads []Upload
+
+	resp := s1.reqClient.R().
+		SetFile("files", "testdata/tom.wav").
+		SetFormData(map[string]string{
+			"template":       "audio",
+			"placementHosts": strings.Join(examplePlacement, ","),
+		}).
+		SetSuccessResult(&uploads).
+		MustPost(s1.Config.Self.Host + "/uploads")
+
+	assert.Equal(t, resp.StatusCode, 200)
+	assert.Equal(t, examplePlacement, uploads[0].PlacementHosts)
+	uploadId := uploads[0].ID
+
+	// force sweep (since blob changes SkipBroadcast)
+	for _, s := range testNetwork {
+		s.crud.ForceSweep()
+	}
+
+	// poll for complete
+	var u2 *Upload
+	for i := 0; i < 3; i++ {
+		resp, err := s2.reqClient.R().SetSuccessResult(&u2).Get(s2.Config.Self.Host + "/uploads/" + uploadId)
+		assert.NoError(t, err)
+		assert.Equal(t, resp.StatusCode, 200)
+		if u2.Status == JobStatusDone {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+
+	assert.Equal(t, u2.TranscodeProgress, 1.0)
+
+	assert.Len(t, u2.Mirrors, len(examplePlacement))
+	assert.Len(t, u2.TranscodedMirrors, len(examplePlacement))
+
+	assert.ElementsMatch(t, u2.Mirrors, examplePlacement)
+	assert.ElementsMatch(t, u2.TranscodedMirrors, examplePlacement)
 }
