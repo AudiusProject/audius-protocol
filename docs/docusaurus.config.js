dc3fbdcbaecb249dee7ebeab45904f2a1dc51e91
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2024-02-20 16:25:43 -0800

    file shuffling

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,15 +23,15 @@ const config = {
   organizationName: 'AudiusProject', // Usually your GitHub org/user name.
   projectName: 'docs.audius.org', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en']
+    locales: ['en'],
   },
 
   presets: [
@@ -45,17 +45,17 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/AudiusProject/docs.audius.org/'
+          editUrl: 'https://github.com/AudiusProject/docs.audius.org/',
         },
         theme: {
-          customCss: './src/css/custom.css'
+          customCss: './src/css/custom.css',
         },
         gtag: {
           trackingID: 'G-XSRDQBKXVX',
           anonymizeIP: true,
-        }
-      })
-    ]
+        },
+      }),
+    ],
   ],
 
   themeConfig:
@@ -68,98 +68,61 @@ const config = {
         logo: {
           alt: 'Audius Docs Logo',
           src: 'img/logo.png',
-          srcDark: 'img/logo-white.png'
+          srcDark: 'img/logo-white.png',
         },
         items: [
           {
-            label: 'Protocol',
-            to: '/protocol',
-            position: 'left'
-          },
-          {
-            label: 'Token',
-            to: '/token',
-            position: 'left'
+            label: 'Learn',
+            to: '/',
+            position: 'right',
           },
           {
             label: 'Developers',
-            to: '/developers/sdk',
-            position: 'left'
+            to: '/developers/introduction/overview',
+            position: 'right',
           },
           {
+            label: 'Run a Node',
+            to: '/',
+            position: 'right',
+          },
+          {
+            label: 'Reference',
+            to: '/',
+            position: 'right',
+          },
+          {
+            'aria-label': 'Discord',
+            className: 'navbar--discord-link',
             href: 'https://discord.com/invite/audius',
-            label: 'Discord',
-            position: 'right'
+            position: 'right',
           },
           {
+            'aria-label': 'GitHub',
+            className: 'navbar--github-link',
             href: 'https://github.com/AudiusProject',
-            label: 'GitHub',
-            position: 'right'
-          }
-        ]
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Welcome',
-                to: '/'
-              },
-              {
-                label: 'Protocol Overview',
-                to: '/protocol'
-              },
-              {
-                label: 'Developers',
-                to: '/developers/sdk'
-              }
-            ]
+            position: 'right',
           },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.com/invite/audius'
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/audius'
-              }
-            ]
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/AudiusProject'
-              }
-            ]
-          }
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Audius, Inc. Built with Docusaurus.`
       },
+
       algolia: {
         appId: '5HE2PIGNOV',
         // This API key is "search-only" and safe to be published
         apiKey: '347af1fe50a2533f274a4a64a695c64c',
         indexName: 'audius',
-        contextualSearch: true
+        contextualSearch: true,
       },
       prism: {
         theme: prismThemes.github,
-        darkTheme: prismThemes.dracula
-      }
+        darkTheme: prismThemes.dracula,
+      },
     }),
 
   markdown: {
-    mermaid: true
+    mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid']
+  themes: ['@docusaurus/theme-mermaid'],
 }
 
 export default config
--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,100 +1,70 @@
 module.exports = {
-  // home: ['welcome'],
-
   developers: [
     {
       type: 'category',
-      label: 'SDK',
-      link: {
-        type: 'doc',
-        id: 'developers/sdk/sdk',
-      },
-      items: [
-        'developers/sdk/albums',
-        'developers/sdk/playlists',
-        'developers/sdk/resolve',
-        'developers/sdk/tracks',
-        'developers/sdk/users',
-        'developers/sdk-oauth-methods',
-        'developers/advanced-options',
-      ],
-      collapsed: true,
+      label: 'Introduction',
+      items: ['developers/introduction/overview', 'developers/introduction/resources'],
+      collapsed: false,
     },
-    'developers/rest-api',
     {
       type: 'category',
-      label: 'Subgraph',
+      label: 'Guides',
       items: [
-        'developers/subgraph/data',
+        'developers/guides/log-in-with-audius',
+        'developers/guides/rest-api',
+        'developers/guides/hedgehog',
+      ],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      label: 'SDK',
+      items: [
+        'developers/sdk/overview',
+        'developers/sdk/tracks',
+        'developers/sdk/users',
+        'developers/sdk/playlists',
+        'developers/sdk/albums',
+        'developers/sdk/resolve',
+        'developers/sdk/oauth',
+        'developers/sdk/advanced-options',
+      ],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      label: 'SubGraph',
+      items: [
+        'developers/subgraph/overview',
         'developers/subgraph/entities',
         'developers/subgraph/queries',
       ],
-      collapsed: true,
-    },
-    'developers/hedgehog',
-    'developers/log-in-with-audius',
-    'developers/upload-track-metadata',
-  ],
-
-  protocol: [
-    'protocol/protocol',
-    'protocol/whitepaper',
-    {
-      type: 'category',
-      label: 'Content Node',
-      items: [
-        'protocol/content-node/content-node-overview',
-        'protocol/content-node/content-node-architecture',
-      ],
-      collapsed: true,
-    },
-    {
-      type: 'category',
-      label: 'Discovery Node',
-      items: [
-        'protocol/discovery-node/discovery-node-overview',
-        'protocol/discovery-node/discovery-node-architecture',
-      ],
-      collapsed: true,
-    },
-    {
-      type: 'category',
-      label: 'How To Guides',
-      items: [
-        'protocol/link-audius-account-to-protocol-dashboard',
-      ],
-      collapsed: true,
+      collapsed: false,
     },
   ],
 
-  token: [
-    'token/audio',
-    'token/staking',
-    'token/governance',
+  learn: [
     {
       type: 'category',
-      label: 'Running a Node',
-      link: {
-        type: 'doc',
-        id: 'token/running-a-node/introduction',
-      },
+      label: 'Introduction',
+      items: ['learn/introduction/getting-started'],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      label: 'Core Concepts',
+      items: ['learn/concepts/protocol', 'learn/concepts/token', 'learn/concepts/nodes'],
+      collapsed: false,
+    },
+    {
+      type: 'category',
+      label: 'Architecture',
       items: [
-        'token/running-a-node/hardware-requirements',
-        {
-          type: 'category',
-          label: 'Setup a Node',
-          items: [
-            'token/running-a-node/setup/wallet-management',
-            'token/running-a-node/setup/installation',
-            'token/running-a-node/setup/registration',
-            'token/running-a-node/setup/multi-sig-registration',
-            'token/running-a-node/setup/claiming',
-            'token/running-a-node/setup/advanced',
-          ],
-        },
-        'token/running-a-node/sla',
+        'learn/architecture/overview',
+        'learn/architecture/content-node',
+        'learn/architecture/discovery-node',
       ],
-      collapsed: true,
+      collapsed: false,
     },
   ],
 }
