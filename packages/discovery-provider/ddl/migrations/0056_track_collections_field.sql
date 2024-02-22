625ef25bfa3d2d569a8588e5e541bc4ef8c5024e
amendelsohn <andrew.j.mendelsohn@gmail.com>
2024-02-22 15:37:02 -0800

    tested migration on local

--- packages/discovery-provider/ddl/migrations/0056_track_collections_field.sql
+++ packages/discovery-provider/ddl/migrations/0056_track_collections_field.sql
@@ -3,4 +3,15 @@ BEGIN;
   alter table tracks
   add column if not exists collections_containing_track jsonb default null;
 
+UPDATE tracks SET collections_containing_track = subquery.collections_containing_track FROM (
+  SELECT
+      CAST(jsonb_array_elements(playlist_contents->'track_ids')->>'track' AS INTEGER) AS track_id,
+      ARRAY_AGG(playlist_id) as collections_containing_track
+  FROM playlists
+  GROUP BY track_id
+  -- WHERE collection_list IS NOT NULL ?
+) AS subquery
+WHERE tracks.track_id = subquery.track_id;
+and track_id IS NOT NULL ON CONFLICT DO NOTHING;
+
 COMMIT;
