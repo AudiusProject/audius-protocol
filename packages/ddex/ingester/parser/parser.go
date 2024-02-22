336dc7ac52fb82a865c12557697005480c372b3d
Theo Ilie <theoilie.ti@gmail.com>
2024-02-22 10:08:32 -0800

    [PROTO-1669] Find Mongo changes when offline and clean up DDEX (#7680)

--- packages/ddex/ingester/parser/parser.go
+++ packages/ddex/ingester/parser/parser.go
@@ -9,72 +9,37 @@ import (
 	"ingester/constants"
 	"io"
 	"log"
-	"log/slog"
 	"net/http"
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Parser struct {
-	s3Downloader        *s3manager.Downloader
-	mongoClient         *mongo.Client
-	rawBucket           string
-	indexedBucket       string
-	deliveriesColl      *mongo.Collection
-	pendingReleasesColl *mongo.Collection
-	usersColl           *mongo.Collection
-	ctx                 context.Context
-	logger              *slog.Logger
+	*common.BaseIngester
 }
 
 // RunNewParser starts the parser service, which listens for new delivery documents in the Mongo "deliveries" collection and turns them into Audius format track format.
 func RunNewParser(ctx context.Context) {
-	logger := slog.With("service", "parser")
-	_, s3Session := common.InitS3Client(logger)
-	mongoClient := common.InitMongoClient(ctx, logger)
-	defer mongoClient.Disconnect(ctx)
-
 	p := &Parser{
-		s3Downloader:        s3manager.NewDownloader(s3Session),
-		mongoClient:         mongoClient,
-		rawBucket:           common.MustGetenv("AWS_BUCKET_RAW"),
-		indexedBucket:       common.MustGetenv("AWS_BUCKET_INDEXED"),
-		deliveriesColl:      mongoClient.Database("ddex").Collection("deliveries"),
-		pendingReleasesColl: mongoClient.Database("ddex").Collection("pending_releases"),
-		usersColl:           mongoClient.Database("ddex").Collection("users"),
-		ctx:                 ctx,
-		logger:              logger,
+		BaseIngester: common.NewBaseIngester(ctx, "parser"),
 	}
+	defer p.MongoClient.Disconnect(ctx)
 
+	// Run migration to create artist name index
 	if err := p.createArtistNameIndex(); err != nil {
 		log.Fatal(err)
 	}
 
-	pipeline := mongo.Pipeline{bson.D{{Key: "$match", Value: bson.D{{Key: "operationType", Value: "insert"}}}}}
-	changeStream, err := p.deliveriesColl.Watch(ctx, pipeline)
-	if err != nil {
-		panic(err)
-	}
-	p.logger.Info("Watching collection 'deliveries'")
-	defer changeStream.Close(ctx)
-
-	for changeStream.Next(ctx) {
-		p.processDelivery(changeStream)
-	}
-
-	if err := changeStream.Err(); err != nil {
-		log.Fatal(err)
-	}
+	p.ProcessChangeStream(p.DeliveriesColl, p.processDelivery)
 }
 
 // createArtistNameIndex creates an index on the name (display name) field in the 'users' collection
 func (p *Parser) createArtistNameIndex() error {
-	_, err := p.usersColl.Indexes().CreateOne(p.ctx, mongo.IndexModel{
+	_, err := p.UsersColl.Indexes().CreateOne(p.Ctx, mongo.IndexModel{
 		Keys: bson.M{"name": 1},
 	})
 	if err != nil {
@@ -86,30 +51,27 @@ func (p *Parser) createArtistNameIndex() error {
 func (p *Parser) getArtistID(artistName string) (string, error) {
 	// Search Mongo for an exact match on artist name
 	filter := bson.M{"name": artistName}
-	cursor, err := p.usersColl.Find(p.ctx, filter)
+	cursor, err := p.UsersColl.Find(p.Ctx, filter)
 	if err != nil {
 		return "", err
 	}
-	defer cursor.Close(p.ctx)
+	defer cursor.Close(p.Ctx)
 
 	var results []bson.M
-	if err = cursor.All(p.ctx, &results); err != nil {
+	if err = cursor.All(p.Ctx, &results); err != nil {
 		return "", err
 	}
 
-	// Fail if multiple artists have their display name set to the same thing
+	// Fail if multiple artists have their display name set to the same string
 	if len(results) > 1 {
-		var matchingHandles []string
+		var handles []string
 		for _, result := range results {
 			if handle, ok := result["handle"].(string); ok {
-				matchingHandles = append(matchingHandles, "@"+handle)
+				handles = append(handles, "@"+handle)
 			}
 		}
-
-		// Join the artist IDs with commas
-		idsStr := strings.Join(matchingHandles, ", ")
-
-		return "", fmt.Errorf("error: more than one artist found with the same display name: : %s", idsStr)
+		idsStr := strings.Join(handles, ", ")
+		return "", fmt.Errorf("error: more than one artist found with the same display name: %s", idsStr)
 	}
 
 	// Fail if no artist is found, and use /v1/users/search to display potential matches in the error message
@@ -168,18 +130,19 @@ func (p *Parser) processDelivery(changeStream *mongo.ChangeStream) {
 		log.Fatal(err)
 	}
 	delivery := changeDoc.FullDocument
-	p.logger.Info("Processing new delivery", "_id", delivery.ID)
+	p.Logger.Info("Processing new delivery", "_id", delivery.ID)
 
 	xmlData := delivery.XmlContent.Data
-	createTrackRelease, createAlbumRelease, errs := parseSonyXML(xmlData, p.indexedBucket, delivery.ID.Hex())
+	createTrackRelease, createAlbumRelease, errs := parseSonyXML(xmlData, p.IndexedBucket, delivery.ID.Hex())
 	if len(errs) != 0 {
-		p.logger.Error("Failed to parse delivery. Printing errors...")
+		p.Logger.Error("Failed to parse delivery. Printing errors...")
 		for _, err := range errs {
-			p.logger.Error(err.Error())
+			p.Logger.Error(err.Error())
 		}
 		p.failAndUpdateStatus(delivery.ID, fmt.Errorf("failed to parse delivery: %v", errs))
+		return
 	}
-	p.logger.Info("Parsed delivery", "createTrackRelease", fmt.Sprintf("%+v", createTrackRelease), "createAlbumRelease", fmt.Sprintf("%+v", createAlbumRelease))
+	p.Logger.Info("Parsed delivery", "createTrackRelease", fmt.Sprintf("%+v", createTrackRelease), "createAlbumRelease", fmt.Sprintf("%+v", createAlbumRelease))
 
 	// If there's an album release, the tracks we parsed out are actually part of the album release
 	if len(createAlbumRelease) > 0 {
@@ -192,6 +155,7 @@ func (p *Parser) processDelivery(changeStream *mongo.ChangeStream) {
 		artistID, err := p.getArtistID(track.Metadata.ArtistName)
 		if err != nil {
 			p.failAndUpdateStatus(delivery.ID, fmt.Errorf("track '%s' failed to find artist ID for '%s': %v", track.Metadata.Title, track.Metadata.ArtistName, err))
+			return
 		}
 		track.Metadata.ArtistID = artistID
 	}
@@ -200,18 +164,21 @@ func (p *Parser) processDelivery(changeStream *mongo.ChangeStream) {
 		artistID, err := p.getArtistID(album.Metadata.PlaylistOwnerName)
 		if err != nil {
 			p.failAndUpdateStatus(delivery.ID, fmt.Errorf("album '%s' failed to find artist ID for '%s': %v", album.Metadata.PlaylistName, album.Metadata.PlaylistOwnerName, err))
+			return
 		}
 		album.Metadata.PlaylistOwnerID = artistID
 	}
 
-	session, err := p.mongoClient.StartSession()
+	session, err := p.MongoClient.StartSession()
 	if err != nil {
 		p.failAndUpdateStatus(delivery.ID, err)
+		return
 	}
-	err = mongo.WithSession(p.ctx, session, func(sessionCtx mongo.SessionContext) error {
+	err = mongo.WithSession(p.Ctx, session, func(sessionCtx mongo.SessionContext) error {
 		if err := session.StartTransaction(); err != nil {
 			return err
 		}
+		defer session.EndSession(p.Ctx)
 
 		// 2. Write each release in "delivery_xml" in the delivery as a bson doc in the 'pending_releases' collection
 		for _, track := range createTrackRelease {
@@ -222,12 +189,12 @@ func (p *Parser) processDelivery(changeStream *mongo.ChangeStream) {
 				"publish_date":         track.Metadata.ReleaseDate,
 				"created_at":           time.Now(),
 			}
-			result, err := p.pendingReleasesColl.InsertOne(p.ctx, pendingRelease)
+			result, err := p.PendingReleasesColl.InsertOne(p.Ctx, pendingRelease)
 			if err != nil {
 				session.AbortTransaction(sessionCtx)
 				return err
 			}
-			p.logger.Info("Inserted pending track release", "_id", result.InsertedID)
+			p.Logger.Info("Inserted pending track release", "_id", result.InsertedID)
 		}
 		for _, album := range createAlbumRelease {
 			pendingRelease := bson.M{
@@ -237,12 +204,12 @@ func (p *Parser) processDelivery(changeStream *mongo.ChangeStream) {
 				"publish_date":         album.Metadata.ReleaseDate,
 				"created_at":           time.Now(),
 			}
-			result, err := p.pendingReleasesColl.InsertOne(p.ctx, pendingRelease)
+			result, err := p.PendingReleasesColl.InsertOne(p.Ctx, pendingRelease)
 			if err != nil {
 				session.AbortTransaction(sessionCtx)
 				return err
 			}
-			p.logger.Info("Inserted pending album release", "_id", result.InsertedID)
+			p.Logger.Info("Inserted pending album release", "_id", result.InsertedID)
 		}
 
 		// 3. Set delivery status for delivery in 'deliveries' collection
@@ -258,8 +225,6 @@ func (p *Parser) processDelivery(changeStream *mongo.ChangeStream) {
 	if err != nil {
 		p.failAndUpdateStatus(delivery.ID, err)
 	}
-
-	session.EndSession(p.ctx)
 }
 
 func (p *Parser) setDeliveryStatus(documentId primitive.ObjectID, status string, err error, ctx context.Context) error {
@@ -273,15 +238,15 @@ func (p *Parser) setDeliveryStatus(documentId primitive.ObjectID, status string,
 		update = bson.M{"$set": bson.M{"delivery_status": status}}
 	}
 
-	_, updateErr := p.deliveriesColl.UpdateByID(ctx, documentId, update)
+	_, updateErr := p.DeliveriesColl.UpdateByID(ctx, documentId, update)
 	return updateErr
 }
 
-func (p *Parser) failAndUpdateStatus(documentId primitive.ObjectID, err error) {
-	updateErr := p.setDeliveryStatus(documentId, constants.DeliveryStatusError, err, p.ctx)
+func (p *Parser) failAndUpdateStatus(documentID primitive.ObjectID, err error) {
+	updateErr := p.setDeliveryStatus(documentID, constants.DeliveryStatusError, err, p.Ctx)
 	if updateErr != nil {
-		log.Fatal(fmt.Errorf("failed to set error on delivery status: %v. original err: %v", updateErr, err))
+		p.Logger.Error("Failed to set error on delivery status", "documentID", documentID, "updateErr", updateErr, "originalErr", err)
 	} else {
-		log.Fatal(err)
+		p.Logger.Error("Set delivery status to error", "documentID", documentID, "error", err)
 	}
 }
