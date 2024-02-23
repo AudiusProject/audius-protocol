2ea47dca39d9a4c59db55d3df55aa2bab5a1f26d
amendelsohn <andrew.j.mendelsohn@gmail.com>
2024-02-22 16:04:39 -0800

    update type in migration

--- packages/discovery-provider/ddl/migrations/0056_track_collections_field.sql
+++ packages/discovery-provider/ddl/migrations/0056_track_collections_field.sql
@@ -1,17 +1,18 @@
 BEGIN;
 
-  alter table tracks
-  add column if not exists collections_containing_track jsonb default null;
+ALTER TABLE tracks
+ADD column IF NOT EXISTS collections_containing_track INTEGER[] DEFAULT NULL;
 
 UPDATE tracks SET collections_containing_track = subquery.collections_containing_track FROM (
   SELECT
       CAST(jsonb_array_elements(playlist_contents->'track_ids')->>'track' AS INTEGER) AS track_id,
-      ARRAY_AGG(playlist_id) as collections_containing_track
+      ARRAY_AGG(playlist_id) AS collections_containing_track
   FROM playlists
+  WHERE playlist_id IS NOT NULL
   GROUP BY track_id
-  -- WHERE collection_list IS NOT NULL ?
 ) AS subquery
-WHERE tracks.track_id = subquery.track_id;
-and track_id IS NOT NULL ON CONFLICT DO NOTHING;
+WHERE tracks.track_id = subquery.track_id
+	AND subquery.track_id IS NOT NULL
+AND tracks.track_id IS NOT NULL;
 
 COMMIT;
