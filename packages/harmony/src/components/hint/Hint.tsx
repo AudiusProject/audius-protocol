144ff4217e1a515bf8c785f33335efdd16d07003
Dylan Jeffers <dylan@audius.co>
2024-02-29 12:06:12 -0800

    [C-3918] Fix wallet and dev-apps icons

--- packages/harmony/src/components/hint/Hint.tsx
+++ packages/harmony/src/components/hint/Hint.tsx
@@ -1,32 +1,41 @@
+import { ReactNode } from 'react'
+
 import type { IconComponent } from 'components/icon'
+import { Flex } from 'components/layout'
 import { Paper, PaperProps } from 'components/layout/Paper'
 import { Text } from 'components/text'
 
 type HintProps = {
   icon: IconComponent
+  actions?: ReactNode
 } & PaperProps
 
 /*
  * A way of informing the user of important details in line in a prominent way.
  */
 export const Hint = (props: HintProps) => {
-  const { icon: Icon, children, ...other } = props
+  const { icon: Icon, children, actions, ...other } = props
   return (
     <Paper
       role='alert'
+      backgroundColor='surface2'
       ph='l'
       pv='m'
-      backgroundColor='surface2'
-      alignItems='center'
-      gap='l'
+      direction='column'
+      gap='m'
       shadow='flat'
       border='strong'
       {...other}
     >
-      <Icon size='l' color='default' />
-      <Text variant='body' color='default'>
-        {children}
-      </Text>
+      <Flex gap='l' alignItems='center'>
+        <Icon size='l' color='default' />
+        <Text variant='body' color='default'>
+          {children}
+        </Text>
+      </Flex>
+      <Flex pl='unit10' gap='l'>
+        {actions}
+      </Flex>
     </Paper>
   )
 }
