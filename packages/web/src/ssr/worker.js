b991afbaf9a8ef7c58425491286e183c7704d7f6
Dylan Jeffers <dylan@audius.co>
2024-03-11 15:24:52 -0700

    [C-3938] Fix harmony cache for SPA (#7789)

--- packages/web/src/ssr/worker.js
+++ packages/web/src/ssr/worker.js
@@ -19,10 +19,10 @@ addEventListener('fetch', (event) => {
   const sentry =
     typeof SENTRY_DSN !== 'undefined'
       ? new Toucan({
-        dsn: SENTRY_DSN,
-        context: event,
-        request: event.request
-      })
+          dsn: SENTRY_DSN,
+          context: event,
+          request: event.request
+        })
       : null
   try {
     event.respondWith(handleEvent(event))
