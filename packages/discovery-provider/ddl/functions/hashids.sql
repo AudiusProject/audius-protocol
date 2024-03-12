c54047df8a8872c3e5094414f537a81fcf16644c
Steve Perkins <stereosteve@users.noreply.github.com>
2024-03-12 10:58:22 -0400

    Add backlink

--- packages/discovery-provider/ddl/functions/hashids.sql
+++ packages/discovery-provider/ddl/functions/hashids.sql
@@ -1,3 +1,6 @@
+--- modified from: https://github.com/array-analytics/plpg_hashids
+
+
 -- our usage of these functions places all of them in a separate schema, with "hashids" being the default name
 -- change this or don't use it (but know that the other sql functions will not work out of the box, they are assuming the hashids schema).
 DROP SCHEMA IF EXISTS hashids CASCADE;
