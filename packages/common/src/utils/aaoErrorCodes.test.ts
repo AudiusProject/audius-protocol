8af40e6e09ac7c22b9672fe55241e6b269cdc40a
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-03-11 22:08:15 -0700

    Fix common tests to be vitest

--- packages/common/src/utils/aaoErrorCodes.test.ts
+++ packages/common/src/utils/aaoErrorCodes.test.ts
@@ -1,4 +1,7 @@
+import { describe, test, expect } from 'vitest'
+
 import { getAAOErrorEmojis } from './aaoErrorCodes'
+
 describe('aaErrorCodes', () => {
   test('returns the correct emoji', () => {
     expect(getAAOErrorEmojis(0)).toBe('😓')
--- packages/common/src/utils/timeUtil.test.ts
+++ packages/common/src/utils/timeUtil.test.ts
@@ -1,3 +1,5 @@
+import { describe, test, expect } from 'vitest'
+
 import dayjs from './dayjs'
 import {
   formatSeconds,
