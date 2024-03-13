c0c488cd73b97b295657eb943fc88de3133f6e58
nicoback2 <36916764+nicoback2@users.noreply.github.com>
2024-03-12 20:30:26 -1000

    [Mobile] ⚠️ Install pending CodePush update upon foregrounding after 1hr [C-3791] (#7661)
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- packages/mobile/src/screens/root-screen/useSyncCodePush.ts
+++ packages/mobile/src/screens/root-screen/useSyncCodePush.ts
@@ -1,15 +1,22 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
+import { playerSelectors } from '@audius/common/store'
 import codePush from 'react-native-code-push'
+import { useSelector } from 'react-redux'
 import { useEffectOnce } from 'react-use'
 
-import { useEnterForeground } from 'app/hooks/useAppState'
+import { useEnterBackground, useEnterForeground } from 'app/hooks/useAppState'
+
+const { getPlaying } = playerSelectors
 
 export const useSyncCodePush = () => {
   const [
     isPendingMandatoryCodePushUpdate,
     setIsPendingMandatoryCodePushUpdate
   ] = useState(false)
+
+  const playing = useSelector(getPlaying)
+  const timeLastBackgrounded = useRef<number | null>(null)
 
   const codePushSync = useCallback(() => {
     codePush.sync(
@@ -26,12 +33,24 @@ export const useSyncCodePush = () => {
         console.info('New CodePush Status: ', newStatus)
         codePush.getUpdateMetadata(codePush.UpdateState.PENDING).then((res) => {
           if (res != null && res.isMandatory) {
+            // If there's a pending (downloaded but not yet installed) update, and it's mandatory, show the mandatory update UI
             setIsPendingMandatoryCodePushUpdate(true)
+          } else if (
+            res !== null &&
+            !playing &&
+            timeLastBackgrounded.current != null
+          ) {
+            // If there's a pending update and it's not mandatory, and the user backgrounded the app for over an hour and came back, and the user is NOT playing music, restart the app to apply the new update
+            const time = new Date().getTime() / 1000
+            if (time - timeLastBackgrounded.current > 3600) {
+              codePush.restartApp(true)
+            }
           }
+          timeLastBackgrounded.current = null
         })
       }
     )
-  }, [])
+  }, [playing])
 
   useEffectOnce(() => {
     codePushSync()
@@ -39,6 +58,10 @@ export const useSyncCodePush = () => {
 
   useEnterForeground(() => {
     codePushSync()
+  })
+
+  useEnterBackground(() => {
+    timeLastBackgrounded.current = new Date().getTime() / 1000
   })
 
   return { isPendingMandatoryCodePushUpdate }
