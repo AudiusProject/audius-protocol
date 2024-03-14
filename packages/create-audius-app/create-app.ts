b09c943c87db0f5d37046074d31b8dec3c167e1e
sliptype <sliptype@gmail.com>
2024-03-14 13:18:45 -0500

    Add create-audius-app

---
+++ packages/create-audius-app/create-app.ts
@@ -0,0 +1,123 @@
+import retry from 'async-retry'
+import { red, green, cyan } from 'picocolors'
+import fs from 'fs'
+import path from 'path'
+import { downloadAndExtractExample, existsInRepo } from './helpers/examples'
+import { tryGitInit } from './helpers/git'
+import { install } from './helpers/install'
+import { isFolderEmpty } from './helpers/is-folder-empty'
+import { getOnline } from './helpers/is-online'
+import { isWriteable } from './helpers/is-writeable'
+
+export class DownloadError extends Error {}
+
+export async function createApp({
+  appPath,
+  example
+}: {
+  appPath: string
+  example: string
+}): Promise<void> {
+  if (example) {
+    const found = await existsInRepo(example)
+
+    if (!found) {
+      console.error(
+        `Could not locate an example named ${red(
+          `"${example}"`
+        )}. It could be due to the following:\n`,
+        `1. Your spelling of example ${red(
+          `"${example}"`
+        )} might be incorrect.\n`
+      )
+      process.exit(1)
+    }
+  }
+
+  const root = path.resolve(appPath)
+
+  if (!(await isWriteable(path.dirname(root)))) {
+    console.error(
+      'The application path is not writable, please check folder permissions and try again.'
+    )
+    console.error(
+      'It is likely you do not have write permissions for this folder.'
+    )
+    process.exit(1)
+  }
+
+  const appName = path.basename(root)
+
+  fs.mkdirSync(root, { recursive: true })
+  if (!isFolderEmpty(root, appName)) {
+    process.exit(1)
+  }
+
+  const isOnline = await getOnline()
+  const originalDirectory = process.cwd()
+
+  console.log(`Creating a new Audius app in ${green(root)}.`)
+  console.log()
+
+  process.chdir(root)
+
+  /**
+   * If an example repository is provided, clone it.
+   */
+  try {
+    console.log(
+      `Downloading files for example ${cyan(
+        example
+      )}. This might take a moment.`
+    )
+    console.log()
+    await retry(() => downloadAndExtractExample(root, example), {
+      retries: 3
+    })
+  } catch (reason) {
+    function isErrorLike(err: unknown): err is { message: string } {
+      return (
+        typeof err === 'object' &&
+        err !== null &&
+        typeof (err as { message?: unknown }).message === 'string'
+      )
+    }
+    throw new DownloadError(isErrorLike(reason) ? reason.message : reason + '')
+  }
+  console.log('Installing packages. This might take a couple of minutes.')
+  console.log()
+
+  await install(isOnline)
+  console.log()
+
+  if (tryGitInit(root)) {
+    console.log('Initialized a git repository.')
+    console.log()
+  }
+
+  let cdpath: string
+  if (path.join(originalDirectory, appName) === appPath) {
+    cdpath = appName
+  } else {
+    cdpath = appPath
+  }
+
+  console.log(`${green('Success!')} Created ${appName} at ${appPath}`)
+
+  console.log('Inside that directory, you can run several commands:')
+  console.log()
+  console.log(cyan('  npm run dev'))
+  console.log('    Starts the development server.')
+  console.log()
+  console.log(cyan('  npm run build'))
+  console.log('    Builds the app for production.')
+  console.log()
+  console.log(cyan('  npm run start'))
+  console.log('    Runs the built app in production mode.')
+  console.log()
+  console.log('We suggest that you begin by typing:')
+  console.log()
+  console.log(cyan('  cd'), cdpath)
+  console.log(`  ${cyan('npm run dev')}`)
+  console.log()
+}
