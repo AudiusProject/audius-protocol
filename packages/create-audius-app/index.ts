b702fe59520d114d1f3512bb35c8f3254979fd6f
Sebastian Klingler <sliptype@gmail.com>
2024-03-15 11:17:45 -0500

    [INF-667] Add create-audius-app (#7846)

---
+++ packages/create-audius-app/index.ts
@@ -0,0 +1,143 @@
+#!/usr/bin/env node
+import { cyan, green, red, bold } from 'picocolors'
+import Commander from 'commander'
+import path from 'path'
+import prompts from 'prompts'
+import { createApp, DownloadError } from './create-app'
+import { validateNpmName } from './helpers/validate-pkg'
+import packageJson from './package.json'
+import { isFolderEmpty } from './helpers/is-folder-empty'
+import fs from 'fs'
+
+let projectPath: string = ''
+
+const handleSigTerm = () => process.exit(0)
+
+process.on('SIGINT', handleSigTerm)
+process.on('SIGTERM', handleSigTerm)
+
+const program = new Commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${green('<project-directory>')} [options]`)
+  .action((name) => {
+    projectPath = name
+  })
+  .option(
+    '-e, --example [name]',
+    `
+
+  An example to bootstrap the app with. You can use an example name
+  from the Audius repo. They are found in AudiusProject/audius-protocol/packages/libs/src/sdk/examples
+  For example: --example react
+`,
+    'react'
+  )
+  .allowUnknownOption()
+  .parse(process.argv)
+
+async function run() {
+  if (typeof projectPath === 'string') {
+    projectPath = projectPath.trim()
+  }
+
+  if (!projectPath) {
+    const res = await prompts({
+      type: 'text',
+      name: 'path',
+      message: 'What is your project named?',
+      initial: 'my-app',
+      validate: (name: string) => {
+        const validation = validateNpmName(path.basename(path.resolve(name)))
+        if (validation.valid) {
+          return true
+        }
+        return 'Invalid project name: ' + validation.problems[0]
+      }
+    })
+
+    if (typeof res.path === 'string') {
+      projectPath = res.path.trim()
+    }
+  }
+
+  if (!projectPath) {
+    console.log(
+      '\nPlease specify the project directory:\n' +
+        `  ${cyan(program.name())} ${green('<project-directory>')}\n` +
+        'For example:\n' +
+        `  ${cyan(program.name())} ${green('my-audius-app')}\n\n` +
+        `Run ${cyan(`${program.name()} --help`)} to see all options.`
+    )
+    process.exit(1)
+  }
+
+  const resolvedProjectPath = path.resolve(projectPath)
+  const projectName = path.basename(resolvedProjectPath)
+
+  const validation = validateNpmName(projectName)
+  if (!validation.valid) {
+    console.error(
+      `Could not create a project called ${red(
+        `"${projectName}"`
+      )} because of npm naming restrictions:`
+    )
+
+    validation.problems.forEach((p) =>
+      console.error(`    ${red(bold('*'))} ${p}`)
+    )
+    process.exit(1)
+  }
+
+  if (program.example === true) {
+    console.error(
+      'Please provide an example name, otherwise remove the example option.'
+    )
+    process.exit(1)
+  }
+
+  /**
+   * Verify the project dir is empty or doesn't exist
+   */
+  const root = path.resolve(resolvedProjectPath)
+  const appName = path.basename(root)
+  const folderExists = fs.existsSync(root)
+
+  if (folderExists && !isFolderEmpty(root, appName)) {
+    process.exit(1)
+  }
+
+  const example = program.example.trim()
+
+  try {
+    await createApp({
+      appPath: resolvedProjectPath,
+      example
+    })
+  } catch (reason) {
+    if (!(reason instanceof DownloadError)) {
+      throw reason
+    }
+
+    console.error(
+      `Could not download "${example}" because of a connectivity issue between your machine and GitHub.\n`
+    )
+    process.exit(1)
+  }
+}
+
+run().catch((reason) => {
+  console.log()
+  console.log('Aborting installation.')
+  if (reason.command) {
+    console.log(`  ${cyan(reason.command)} has failed.`)
+  } else {
+    console.log(
+      red('Unexpected error. Please report it as a bug:') + '\n',
+      reason
+    )
+  }
+  console.log()
+
+  process.exit(1)
+})
