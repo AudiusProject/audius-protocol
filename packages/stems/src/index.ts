60cf65a25f8937145647e49d403cb8e2f53d9bb2
Dylan Jeffers <dylan@audius.co>
2024-03-15 21:03:47 -0700

    Remove animations.css

--- packages/stems/src/index.ts
+++ packages/stems/src/index.ts
@@ -1,7 +1,6 @@
 import './assets/styles/sizes.css'
 import './assets/styles/fonts.css'
 import './assets/styles/colors.css'
-import './assets/styles/animations.css'
 import './assets/styles/shadows.css'
 import './assets/styles/transforms.css'
 import './assets/styles/layers.css'
