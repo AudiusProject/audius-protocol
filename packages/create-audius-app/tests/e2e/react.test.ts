cd4bca23bc81f0dd437cfcb4ac5e71672544eb38
sliptype <sliptype@gmail.com>
2024-03-21 17:26:00 -0500

    Full e2e test

--- packages/create-audius-app/tests/e2e/react.test.ts
+++ packages/create-audius-app/tests/e2e/react.test.ts
@@ -1,19 +1,47 @@
 import { test, expect } from '@playwright/test'
 
-test('renders', async ({ page }) => {
+test('auths, fetches tracks, and favorites a track', async ({
+  page,
+  context
+}) => {
+  test.slow()
+
+  // Set entropy so we don't need to do OTP
+  const entropy = process.env.CREATE_AUDIUS_APP_TEST_ENTROPY ?? ''
+  await context.addInitScript((entropy) => {
+    if (window.location.hostname === 'audius.co') {
+      window.localStorage.setItem('hedgehog-entropy-key', entropy)
+    }
+  }, entropy)
+
   await page.goto('localhost:4173')
 
-  await expect(
-    page.getByRole('heading', { name: 'React + @audius/sdk' })
-  ).toBeVisible()
-})
+  const pagePromise = context.waitForEvent('page')
+  await page.getByRole('button', { name: 'Continue With Audius' }).click()
 
-test('fetches and renders tracks', async ({ page }) => {
-  await page.goto('localhost:4173')
+  // Oauth popup
+  const authPage = await pagePromise
+  await authPage.waitForLoadState()
+  await authPage.getByRole('button', { name: 'Continue' }).click()
 
+  await expect(page.getByText('Continue With Audius')).not.toBeVisible()
+
+  // Fetch tracks
+  await page.getByRole('textbox').fill('createaudiusapptracks')
   await page.getByRole('button', { name: 'Get Tracks' }).click()
 
-  await expect(
-    page.getByRole('button', { name: 'Favorite' }).first()
-  ).toBeVisible()
+  // Set up block_confirmation response listener
+  const responsePromise = page.waitForResponse(async (response) => {
+    if (response.url().includes('block_confirmation')) {
+      const json = await response.json()
+      return json.data.block_passed
+    }
+  })
+
+  // TODO: handle favorite/unfavorite
+  await page.getByRole('button', { name: 'Favorite' }).first().click()
+
+  // Confirm track is favorited
+  await expect(page.getByText('Favorited!')).toBeVisible()
+  await responsePromise
 })
