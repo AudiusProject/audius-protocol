2bedd83c10d49f95e17dc4f13a794ee6739f88a0
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-03-22 12:30:26 -0700

    Fix stems, fix smokes

--- packages/web/e2e/smoke.test.ts
+++ packages/web/e2e/smoke.test.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test'
 
-const base64Entropy = 'YmRhYmE4MjRiNmUwMmFiNzg2OGM1YTJkZmRmYzdlOWY'
-
 test('should load an album page', async ({ page }) => {
-  await page.goto('df/album/probers_album_do_not_delete-512')
+  await page.goto('df/album/probers_album_do_not_delete-512', {
+    waitUntil: 'load'
+  })
   const heading = page.getByRole('heading', {
     name: 'probers_album_do_not_delete',
     level: 1
@@ -12,13 +12,15 @@ test('should load an album page', async ({ page }) => {
 })
 
 test('should load the feed page', async ({ page }) => {
-  await page.goto(`feed?login=${base64Entropy}`)
+  await page.goto('feed', { waitUntil: 'load' })
   const heading = page.getByRole('heading', { name: 'Your Feed', level: 1 })
   await expect(heading).toBeAttached({ timeout: 10000 })
 })
 
 test('should load a playlist page', async ({ page }) => {
-  await page.goto('df/playlist/probers_playlist_do_not_delete-511')
+  await page.goto('df/playlist/probers_playlist_do_not_delete-511', {
+    waitUntil: 'load'
+  })
   const heading = page.getByRole('heading', {
     name: 'PROBERS_PLAYLIST_DO_NOT_DELETE'
   })
@@ -26,7 +28,7 @@ test('should load a playlist page', async ({ page }) => {
 })
 
 test('should load a remix page', async ({ page }) => {
-  await page.goto('df/probers_remix_do_not_delete-2859')
+  await page.goto('df/probers_remix_do_not_delete-2859', { waitUntil: 'load' })
   const heading = page.getByRole('heading', {
     name: 'probers_remix_do_not_delete'
   })
@@ -34,13 +36,13 @@ test('should load a remix page', async ({ page }) => {
 })
 
 test('should load a remixes page', async ({ page }) => {
-  await page.goto('mb430/traektoria-source-2217/remixes')
+  await page.goto('mb430/traektoria-source-2217/remixes', { waitUntil: 'load' })
   const heading = page.getByRole('heading', { name: 'Remixes', level: 1 })
   await expect(heading).toBeAttached({ timeout: 10000 })
 })
 
 test('should load a track page', async ({ page }) => {
-  await page.goto('sebastian12/bachgavotte-1')
+  await page.goto('sebastian12/bachgavotte-1', { waitUntil: 'load' })
   const heading = page.getByRole('heading', {
     name: 'probers_track_do_not_delete',
     level: 1
@@ -49,7 +51,7 @@ test('should load a track page', async ({ page }) => {
 })
 
 test('should load trending page', async ({ page }) => {
-  await page.goto('trending')
+  await page.goto('trending', { waitUntil: 'load' })
   const heading = page.getByRole('heading', {
     name: 'Trending',
     level: 1
@@ -58,7 +60,7 @@ test('should load trending page', async ({ page }) => {
 })
 
 test('should load upload page', async ({ page }) => {
-  await page.goto(`upload?login=${base64Entropy}`)
+  await page.goto(`upload`, { waitUntil: 'load' })
   const heading = page.getByRole('heading', {
     name: 'Upload Your Music',
     level: 1
