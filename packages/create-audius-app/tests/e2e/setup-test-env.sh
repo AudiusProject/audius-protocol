33beafef358435127d802d35b42e3fff72cb2000
sliptype <sliptype@gmail.com>
2024-03-25 16:51:39 -0500

    Make setup-test-env cross platform compatible

--- packages/create-audius-app/tests/e2e/setup-test-env.sh
+++ packages/create-audius-app/tests/e2e/setup-test-env.sh
@@ -1,3 +1,4 @@
 # Replace the API key in the example app with a test key
-find ./examples/react/src/App.tsx -type f -exec sed -i "" "s/\/\/ apiKey: \"Your API Key goes here\"/,apiKey: \"$CREATE_AUDIUS_APP_TEST_API_KEY\"/g" {} \;
-find ./examples/react/src/App.tsx -type f -exec sed -i "" "s/\/\/ apiSecret: \"Your API Secret goes here\"/apiSecret: \"$CREATE_AUDIUS_APP_TEST_API_SECRET\"/g" {} \;
+find ./examples/react/src/App.tsx -type f -exec sed -i.bak "s/\/\/ apiKey: \"Your API Key goes here\"/,apiKey: \"$CREATE_AUDIUS_APP_TEST_API_KEY\"/g" {} \;
+find ./examples/react/src/App.tsx -type f -exec sed -i.bak "s/\/\/ apiSecret: \"Your API Secret goes here\"/apiSecret: \"$CREATE_AUDIUS_APP_TEST_API_SECRET\"/g" {} \;
+rm ./examples/react/src/App.tsx.bak
