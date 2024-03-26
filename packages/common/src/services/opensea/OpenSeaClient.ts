c721f003b62526167c5a90ed9176975a21e8b8e0
Saliou Diallo <saliou@audius.co>
2024-03-26 17:16:23 -0400

    Fix opensea api calls

--- packages/common/src/services/opensea/OpenSeaClient.ts
+++ packages/common/src/services/opensea/OpenSeaClient.ts
@@ -2,7 +2,8 @@ import { Nullable } from '~/utils'
 
 import { OpenSeaCollection, OpenSeaEvent, OpenSeaNft } from './types'
 
-const OPENSEA_NUM_ASSETS_LIMIT = 50
+const OPENSEA_NUM_ASSETS_LIMIT = 200
+const OPENSEA_NUM_EVENTS_LIMIT = 50
 
 export class OpenSeaClient {
   private readonly apiUrl: string
@@ -25,21 +26,21 @@ export class OpenSeaClient {
 
   async getNftTransferEventsForWallet(
     wallet: string,
-    limit = OPENSEA_NUM_ASSETS_LIMIT
+    limit = OPENSEA_NUM_EVENTS_LIMIT
   ): Promise<OpenSeaEvent[]> {
     let res: Response
     let json: { next: string | undefined; asset_events: OpenSeaEvent[] }
     let events: OpenSeaEvent[]
     let next: string | undefined
     res = await fetch(
-      `${this.apiUrl}/api/v2/events?account=${wallet}&limit=${limit}&event_type=transfer&chain=ethereum`
+      `${this.apiUrl}/api/v2/events/accounts/${wallet}?limit=${limit}&event_type=transfer&chain=ethereum`
     )
     json = await res.json()
     next = json.next
     events = json.asset_events
     while (next) {
       res = await fetch(
-        `${this.apiUrl}/api/v2/events?account=${wallet}&limit=${limit}&event_type=transfer&chain=ethereum&next=${next}`
+        `${this.apiUrl}/api/v2/events/accounts/${wallet}?limit=${limit}&event_type=transfer&chain=ethereum&next=${next}`
       )
       json = await res.json()
       next = json.next
@@ -48,20 +49,23 @@ export class OpenSeaClient {
     return events.map((event) => ({ ...event, wallet }))
   }
 
-  async getNftsForWallet(wallet: string): Promise<OpenSeaNft[]> {
+  async getNftsForWallet(
+    wallet: string,
+    limit = OPENSEA_NUM_ASSETS_LIMIT
+  ): Promise<OpenSeaNft[]> {
     let res: Response
     let json: { next: string | undefined; nfts: OpenSeaNft[] }
     let nfts: OpenSeaNft[]
     let next: string | undefined
     res = await fetch(
-      `${this.apiUrl}/api/v2/chain/ethereum/account/${wallet}/nfts`
+      `${this.apiUrl}/api/v2/chain/ethereum/account/${wallet}/nfts?limit=${limit}`
     )
     json = await res.json()
     next = json.next
     nfts = json.nfts
     while (next) {
       res = await fetch(
-        `${this.apiUrl}/api/v2/chain/ethereum/account/${wallet}/nfts&next=${next}`
+        `${this.apiUrl}/api/v2/chain/ethereum/account/${wallet}/nfts?limit=${limit}&next=${next}`
       )
       json = await res.json()
       next = json.next
