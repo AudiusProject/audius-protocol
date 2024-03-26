efa189a58162f48c991c9f00bcad4e1d5c1d7b69
Saliou Diallo <saliou@audius.co>
2024-03-26 01:09:15 -0400

    Add paginate to helius client in DN

--- packages/discovery-provider/src/helius_client/helius_client.py
+++ packages/discovery-provider/src/helius_client/helius_client.py
@@ -6,7 +6,7 @@ from src.utils.config import shared_config
 
 logger = logging.getLogger(__name__)
 
-helius_num_assets_per_page_limit = 1000
+HELIUS_NUM_ASSETS_PER_PAGE_LIMIT = 1000
 
 
 class HeliusClient:
@@ -16,37 +16,44 @@ class HeliusClient:
         self.api_url = api_url
 
     def get_nfts_for_wallet(self, wallet: str):
-        body = {
-            "id": "test-drive",  # todo: what should this be
-            "jsonrpc": "2.0",
-            "method": "getAssetsByOwner",
-            "params": {
-                "ownerAddress": wallet,
-                "page": 1,
-                "limit": helius_num_assets_per_page_limit,
-                "after": "",
-                "before": "",
-                "displayOptions": {
-                    "showUnverifiedCollections": False,
-                    "showCollectionMetadata": True,
-                },
-            },
-        }
+        nfts = []
         try:
-            # todo: add pagination
-            response = requests.post(self.api_url, json=body)
-            json = response.json()
-            result = json.get("result", {})
-            # total = result.get("total")
-            # limit = result.get("limit")
-            # page = result.get("page")
-            items = result.get("items", [])
+            page = 1
+            while True:
+                body = {
+                    "id": "test-drive",  # todo: what should this be
+                    "jsonrpc": "2.0",
+                    "method": "getAssetsByOwner",
+                    "params": {
+                        "ownerAddress": wallet,
+                        "page": page,
+                        "limit": HELIUS_NUM_ASSETS_PER_PAGE_LIMIT,
+                        "sortBy": {"sortBy": "id", "sortDirection": "asc"},
+                        "displayOptions": {
+                            "showUnverifiedCollections": False,
+                            "showCollectionMetadata": True,
+                        },
+                    },
+                }
+                response = requests.post(self.api_url, json=body)
+                json = response.json()
+                result = json.get("result", {})
+                items = result.get("items", [])
+                nfts.extend(items)
+                is_empty_result = len(items) == 0
+                is_result_length_below_limit = (
+                    len(items) < HELIUS_NUM_ASSETS_PER_PAGE_LIMIT
+                )
+                if is_empty_result or is_result_length_below_limit:
+                    break
+                else:
+                    page += 1
             return items
         except Exception as e:
             logger.error(
-                f"helius_client.py | Error getting nfts for wallet {wallet}: {e}"
+                f"helius_client.py | Encountered error while fetching nfts from Helius for wallet ${wallet}: Returning nfts obtained so far...\nError: ${e}"
             )
-            return []
+            return nfts
 
 
 helius_client = HeliusClient(shared_config["nft"]["helius_das_api_url"])
