e3605c1f52714fda0b08c239881252606bcf5d0f
isaac <isaac@audius.co>
2024-03-29 11:35:12 -0700

    fix test

--- packages/common/src/utils/timeUtil.test.ts
+++ packages/common/src/utils/timeUtil.test.ts
@@ -50,35 +50,27 @@ describe('formatDate', () => {
 describe('formatDateWithTimezoneOffset', () => {
   test('should format date correctly with mocked timezone offset', () => {
     const originalTimezone = dayjs.tz.guess()
-    dayjs.tz.setDefault('Pacific/Auckland')
     const offsetFormattedDate = formatDateWithTimezoneOffset(
       '2023-12-17T12:00:00Z'
     )
     const expectedDate = '12/18/23'
     expect(offsetFormattedDate).toBe(expectedDate)
-    dayjs.tz.setDefault(originalTimezone)
   })
 })
 
 describe('utcToLocalTime', () => {
   test('should convert UTC to local time', () => {
-    const originalTimezone = dayjs.tz.guess()
-    dayjs.tz.setDefault('Pacific/Auckland')
     const localTime = utcToLocalTime('2023-12-17T12:00:00Z')
     expect(localTime.isValid()).toBe(true)
     expect(localTime.month()).toBe(11) // note 0 index
     expect(localTime.date()).toBe(18)
     expect(localTime.hour()).toBe(1)
-    dayjs.tz.setDefault(originalTimezone)
   })
 })
 
 describe('getLocalTimezone', () => {
   test('should return the local timezone', () => {
-    const originalTimezone = dayjs.tz.guess()
-    dayjs.tz.setDefault('Asia/Taipei')
     const timezone = getLocalTimezone()
-    expect(timezone).toBe('GMT+8')
-    dayjs.tz.setDefault(originalTimezone)
+    expect(timezone).toBe(dayjs().format('z'))
   })
 })
