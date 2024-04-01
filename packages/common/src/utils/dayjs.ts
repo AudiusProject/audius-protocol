e13f9471dc58ea3f70e4b3c64776c9371a63362a
isaac <isaac@audius.co>
2024-04-01 16:37:46 -0700

    Add timezone back

--- packages/common/src/utils/dayjs.ts
+++ packages/common/src/utils/dayjs.ts
@@ -2,8 +2,10 @@ import dayjs, { Dayjs } from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import duration from 'dayjs/plugin/duration'
 import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
 dayjs.extend(utc)
+dayjs.extend(timezone)
 dayjs.extend(advancedFormat)
 dayjs.extend(duration)
 
