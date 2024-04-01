4828685ff8b670d27ad907f2a336bda8d40d2bac
isaac <isaac@audius.co>
2024-04-01 08:00:32 -0700

    restrict import

--- packages/common/.eslintrc.js
+++ packages/common/.eslintrc.js
@@ -9,6 +9,18 @@ module.exports = {
         message:
           'Do NOT use `Promise.allSettled` as it will be undefined. Use `allSettled` from `utils/allSettled.ts` instead.'
       }
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['dayjs/plugin/timezone'],
+            message:
+              'Avoid using dayjs timezone plugin because it does not work in React Native Hermes'
+          }
+        ]
+      }
     ]
   }
 }
