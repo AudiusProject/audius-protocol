77a3038a6c576a4b122d08c5654bbc18d1b0a5df
sliptype <sliptype@gmail.com>
2024-04-01 13:42:18 -0500

    Make npm i output developer-friendly messages

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,22 +1,53 @@
-npm run install-hooks
-npm run patch-package
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+printf "${GREEN}Installing git hooks...\n${NC}"
+{
+  npm run install-hooks
+} > /dev/null
+
+printf "${GREEN}Applying patches...\n${NC}"
+{
+  npm run patch-package
+} > /dev/null
+
 
 if [[ -z "${SKIP_POD_INSTALL}" ]]; then
-  # Symlink react-native into the mobile package bc npm doesn't
-  # support nohoist
-  cd packages/mobile/node_modules
-  ln -s ../../../node_modules/react-native react-native
-  ln -s ../../../node_modules/react-native-code-push react-native-code-push
-  cd ../ios
-  if command -v bundle >/dev/null; then
-    bundle check || bundle install
-  fi
-  if command -v pod >/dev/null; then
-    pod install
-  fi
-  cd ../../..
+  printf "${GREEN}Installing cocoapods...\n${NC}"
+  {
+    # Symlink react-native into the mobile package bc npm doesn't
+    # support nohoist
+    cd packages/mobile/node_modules
+
+    source_path=../../../node_modules/react-native
+    target_path=react-native
+    if [ ! -e "$target_path" ]; then
+      ln -s "$source_path" "$target_path"
+    fi
+
+    source_path=../../../node_modules/react-native-code-push
+    target_path=react-native-code-push
+    if [ ! -e "$target_path" ]; then
+      ln -s "$source_path" "$target_path"
+    fi
+
+    cd ../ios
+
+    if command -v bundle >/dev/null; then
+      bundle check || bundle install
+    fi
+    if command -v pod >/dev/null; then
+      pod install
+    fi
+    cd ../../..
+  } > /dev/null
 fi
 
 if [[ -z "${CI}" ]]; then
-  ./dev-tools/setup.sh
+  printf "${GREEN}Setting up audius-compose...\n${NC}"
+  {
+    ./dev-tools/setup.sh
+  } > /dev/null
 fi
+
+printf "\n${GREEN}Audius monorepo ready!\n${NC}"
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,8 @@
 set -e
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
 should_error=false
 
 if ! [ -z $CI ]; then
@@ -9,16 +12,19 @@ fi
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
 
-nvm install
-cp .python-version-dev .python-version
-pyenv install -s
-rbenv install -s
+printf "${GREEN}Confirming node, python, and ruby environents...\n${NC}"
+{
+  nvm install
+  cp .python-version-dev .python-version
+  pyenv install -s
+  rbenv install -s
+} > /dev/null
 
 required_node_version=$(<.nvmrc)
 current_node_version=$(node --version)
 
 if [ $current_node_version != $required_node_version ]; then
-  echo "Invalid node version. Expected $required_node_version, got $current_node_version"
+  printf "${RED}Invalid node version. Expected $required_node_version, got $current_node_version\n${NC}"
   should_error=true
 fi
 
@@ -26,7 +32,7 @@ required_ruby_version=$(<'.ruby-version')
 current_ruby_version=$(ruby --version)
 
 if [[ ! $current_ruby_version =~ $required_ruby_version ]]; then
-  echo "Invalid ruby version. Expected $required_ruby_version, got $current_ruby_version"
+  printf "${RED}Invalid ruby version. Expected $required_ruby_version, got $current_ruby_version\n${NC}"
   should_error=true
 fi
 
@@ -34,10 +40,12 @@ required_python_version=$(<'.python-version')
 current_python_version=$(python --version)
 
 if [[ ! $current_python_version =~ $required_python_version ]]; then
-  echo "Invalid python version. Expected $required_python_version, got $current_python_version"
+  printf "${RED}Invalid python version. Expected $required_python_version, got $current_python_version\n${NC}"
   should_error=true
 fi
 
 if [[ $should_error = true ]]; then
   exit 1
 fi
+
+printf "\n${GREEN}Environment correctly set up!\n${NC}"
