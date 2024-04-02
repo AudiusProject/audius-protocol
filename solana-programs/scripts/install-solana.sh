120fe381e5c0e9374f6f0e6ab317ddcf9e9d6bfe
Raymond Jacobson <ray@audius.co>
2024-04-01 17:03:31 -0700

    WIP

--- solana-programs/scripts/install-solana.sh
+++ solana-programs/scripts/install-solana.sh
@@ -3,11 +3,11 @@
 set -ex
 
 if [[ "$(uname -m)" == "x86_64" ]]; then
-    curl -SfL https://github.com/solana-labs/solana/releases/download/v1.10.11/solana-release-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf - -C $HOME
+    curl -SfL https://github.com/solana-labs/solana/releases/download/v1.18.6/solana-release-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf - -C $HOME
 else
-    cargo install --locked --root $HOME/solana-release --version 1.16.3 solana-cli solana-keygen
-    cargo install --locked --root $HOME/solana-release --version 1.16.3 --bin solana-test-validator solana-validator
-    cargo install --locked --root $HOME/solana-release --version 3.1.0 spl-token-cli
+    cargo install --locked --root $HOME/solana-release --version 1.18.6 solana-cli solana-keygen
+    cargo install --locked --root $HOME/solana-release --version 1.18.6 --bin solana-test-validator solana-validator
+    cargo install --locked --root $HOME/solana-release --version 3.3.0 spl-token-cli
 fi
 
-cargo install --git https://github.com/coral-xyz/anchor --tag v0.28.0 anchor-cli --locked
+cargo install --git https://github.com/coral-xyz/anchor --tag v0.29.0 anchor-cli --locked
