df44c1db5735ada033870b8b18d4a21a8c77582e
Saliou Diallo <saliou@audius.co>
2024-04-02 18:53:16 -0400

    Fix dayjs timezone import

--- packages/common/src/utils/dayjs.ts
+++ packages/common/src/utils/dayjs.ts
@@ -1,7 +1,6 @@
 import dayjs, { Dayjs } from 'dayjs'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import duration from 'dayjs/plugin/duration'
-import tz from 'dayjs/plugin/timezone'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 
@@ -9,7 +8,6 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.extend(advancedFormat)
 dayjs.extend(duration)
-dayjs.extend(tz)
 
 export default dayjs
 export { Dayjs }
