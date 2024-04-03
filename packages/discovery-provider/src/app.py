40edbf905cde0896476419d6dad6a0df46623ba5
Daniel Phelps <daniel@audius.co>
2024-04-03 16:02:44 -0700

    Send cache_current_nodes celery task at startup

--- packages/discovery-provider/src/app.py
+++ packages/discovery-provider/src/app.py
@@ -528,6 +528,7 @@ def configure_celery(celery, test_config=None):
     celery.finalize()
 
     # Start tasks that should fire upon startup
+    celery.send_task("cache_current_nodes")
     celery.send_task("cache_entity_counts")
     celery.send_task("index_nethermind", queue="index_nethermind")
     celery.send_task("index_user_bank", queue="index_sol")
