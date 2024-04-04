024ffe6670b78cdcd417a0ba5c94ca39def159fa
sliptype <sliptype@gmail.com>
2024-04-04 18:50:39 -0500

    Add publish-packages.sh and get things building and linting

---
+++ scripts/publish-packages.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+set -e
+
+# Ensure that all public packages are in this list,
+# otherwise they won't get built/linted/tested before being published
+turbo run build lint typecheck test \
+    --filter=create-audius-app \
+    --filter=@audius/fixed-decimal \
+    --filter=@audius/harmony \
+    --filter=@audius/sdk \
+    --filter=@audius/spl \
+
+changeset publish  
