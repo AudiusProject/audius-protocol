65ae54202e633bf0c1b03403f4e6b016ef8c4753
sliptype <sliptype@gmail.com>
2024-04-05 16:42:42 -0500

    Use preview:stage for e2e tests

--- packages/web/playwright.config.ts
+++ packages/web/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:4173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -101,8 +101,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:spa:stage',
-    url: 'http://localhost:3001',
+    command: 'npm run preview:stage',
+    url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
     stderr: 'pipe',
