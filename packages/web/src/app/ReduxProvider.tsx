abd23aa02a2fda1af61772092ba0e921e5913e2e
JD Francis <jd.johndavidfrancis@gmail.com>
2024-04-05 15:29:27 -0500

    RTL test setup + SSR store ref refactor (#8042)

--- packages/web/src/app/ReduxProvider.tsx
+++ packages/web/src/app/ReduxProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react'
+import { ReactNode, useRef } from 'react'
 
 import { Provider } from 'react-redux'
 import { Persistor, persistStore } from 'redux-persist'
@@ -6,25 +6,39 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 import { useIsMobile } from 'hooks/useIsMobile'
 import { configureStore } from 'store/configureStore'
+import { AppState } from 'store/types'
 import logger from 'utils/logger'
 
 import { useSsrContext } from '../ssr/SsrContext'
 
 import { useHistoryContext } from './HistoryProvider'
 
-export const ReduxProvider = ({ children }: { children: ReactNode }) => {
+export const ReduxProvider = ({
+  children,
+  initialStoreState
+}: {
+  children: ReactNode
+  // Optional (for testing purposes) - sets up an initial store state
+  initialStoreState?: Partial<AppState>
+}) => {
   const { pageProps, isServerSide } = useSsrContext()
   const { history } = useHistoryContext()
   const isMobile = useIsMobile()
 
-  const [store, setStore] = useState<ReturnType<typeof configureStore>>()
-  const [persistor, setPersistor] = useState<Persistor>()
+  const storeRef = useRef<ReturnType<typeof configureStore>>()
+  const persistorRef = useRef<Persistor>()
 
-  if (!store) {
-    const store = configureStore(history, isMobile, pageProps, isServerSide)
-    setStore(store)
+  if (!storeRef.current) {
+    const store = configureStore(
+      history,
+      isMobile,
+      pageProps,
+      isServerSide,
+      initialStoreState
+    )
+    storeRef.current = store
     const persistor = persistStore(store)
-    setPersistor(persistor)
+    persistorRef.current = persistor
 
     // Mount store to window for easy access
     if (typeof window !== 'undefined') {
@@ -37,11 +51,11 @@ export const ReduxProvider = ({ children }: { children: ReactNode }) => {
     }
   }
 
-  return store && persistor ? (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistorRef.current!}>
         {() => children}
       </PersistGate>
     </Provider>
-  ) : null
+  )
 }
