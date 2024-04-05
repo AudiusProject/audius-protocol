d655f80fbcc1c0faf1aa43271aba836cdfe23479
Theo Ilie <theoilie.ti@gmail.com>
2024-04-04 22:18:12 -0700

    [PROTO-1753] Make ddex publisher+web work with refactored parser

--- packages/ddex/publisher/src/models/publishedReleases.ts
+++ packages/ddex/publisher/src/models/publishedReleases.ts
@@ -1,19 +1,14 @@
 import mongoose from 'mongoose'
-import {
-  createTrackReleaseSchema,
-  createAlbumReleaseSchema,
-} from './pendingReleases'
+import { releaseSchema } from './pendingReleases'
 
 // DDEX releases that have been published
 const publishedReleasesSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   delivery_remote_path: { type: String, required: true },
-  publish_date: Date,
   entity_id: String,
   blockhash: String,
   blocknumber: Number,
-  track: createTrackReleaseSchema,
-  album: createAlbumReleaseSchema,
+  release: releaseSchema,
   created_at: Date,
 })
 
