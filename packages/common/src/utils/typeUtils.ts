fba7a9a093c4d0f5a9363ba0a74c36c34e41fe22
JD Francis <jd@audius.co>
2024-04-05 15:10:23 -0500

    move deep partial

--- packages/common/src/utils/typeUtils.ts
+++ packages/common/src/utils/typeUtils.ts
@@ -56,3 +56,15 @@ export type PickRename<
 > = Omit<Type, Key> & {
   [k in RenamedKey]: Type[Key]
 }
+
+
+// Adjusted from Terry: https://stackoverflow.com/questions/61132262/typescript-deep-partial
+export type DeepPartial<T> = T extends any[]
+  ? T
+  : T extends Set<any>
+  ? T
+  : T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>
+    }
+  : T
