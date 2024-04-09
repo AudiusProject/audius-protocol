20b875b97aa2baa391003eb5cac16d6937af9020
Dylan Jeffers <dylan@audius.co>
2024-04-08 17:43:55 -0700

    [C-4001] Add interacive paper variant

--- packages/harmony/src/components/layout/Paper/Paper.tsx
+++ packages/harmony/src/components/layout/Paper/Paper.tsx
@@ -1,4 +1,6 @@
-import styled from '@emotion/styled'
+import { forwardRef } from 'react'
+
+import { useTheme } from '@emotion/react'
 
 import { Flex } from '../Flex'
 
@@ -8,20 +10,47 @@ import type { PaperProps } from './types'
  * Base layout component used as a building block for creating pages
  * and other components.
  * */
-export const Paper = styled(Flex)<PaperProps>((props) => {
+export const Paper = forwardRef<HTMLDivElement, PaperProps>((props, ref) => {
   const {
-    theme,
     backgroundColor = 'white',
     border,
     borderRadius = 'm',
-    shadow = 'mid'
+    shadow = 'mid',
+    ...other
   } = props
 
-  return {
+  const { onClick } = other
+
+  const theme = useTheme()
+
+  const css = {
     boxShadow: theme.shadows[shadow],
     border: border && `1px solid ${theme.color.border[border]}`,
     borderRadius: theme.cornerRadius[borderRadius],
     backgroundColor: theme.color.background[backgroundColor],
-    overflow: 'hidden'
+    overflow: 'hidden',
+    transition: theme.motion.hover
   }
+
+  const interactiveCss = {
+    '&:hover': {
+      transform: 'scale(1.01)',
+      boxShadow: theme.shadows.far
+    },
+    '&:active': {
+      transform: 'scale(0.995)',
+      boxShadow: theme.shadows.near,
+      transition: theme.motion.press
+    }
+  }
+
+  return (
+    <Flex
+      css={[css, onClick && interactiveCss]}
+      role={onClick ? 'button' : undefined}
+      tabIndex={onClick ? 0 : undefined}
+      ref={ref}
+      {...other}
+    />
+  )
 })
