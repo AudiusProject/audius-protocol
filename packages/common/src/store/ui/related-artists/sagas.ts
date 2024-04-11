92215024621b751e7c1a5966d4ad5595ca336c1e
Randy Schott <1815175+schottra@users.noreply.github.com>
2024-04-11 11:56:01 -0400

    remove temp hack

--- packages/common/src/store/ui/related-artists/sagas.ts
+++ packages/common/src/store/ui/related-artists/sagas.ts
@@ -37,13 +37,12 @@ export function* fetchRelatedArtists(action: PayloadAction<{ artistId: ID }>) {
     let suggestedFollows = relatedArtists
       .filter((user) => !user.does_current_user_follow)
       .slice(0, 5)
-    if (suggestedFollows.length !== 0) {
+    if (suggestedFollows.length === 0) {
       const showTopArtistRecommendationsPercent =
         remoteConfigInstance.getRemoteVar(
           DoubleKeys.SHOW_ARTIST_RECOMMENDATIONS_FALLBACK_PERCENT
         ) || 0
-      const showTopArtists =
-        true || Math.random() < showTopArtistRecommendationsPercent
+      const showTopArtists = Math.random() < showTopArtistRecommendationsPercent
 
       if (showTopArtists) {
         suggestedFollows = yield* call(fetchTopArtists)
