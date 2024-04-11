f39edfa744b8d3ad3d656dcac04fc4afe5faaf2f
sliptype <sliptype@gmail.com>
2024-04-11 15:24:32 -0500

    Only install deps if necessary

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -3,6 +3,12 @@
 
 set -e
 
+# If node_modules exists in cache, run postinstall. Otherwise, run npm ci
+[[ ! -d node_modules ]] || CI=true npm run postinstall
+[[ -d node_modules ]] || CI=true npm ci
+
+npx playwright install
+
 # Ensure that all public packages are in this list,
 # otherwise they won't get built/linted/tested before being published
 turbo run build lint typecheck test \
