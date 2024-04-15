169cdeda4aa4aece1782a2e1351a610f4bab70b7
Michelle Brier <michelle.brier4@gmail.com>
2024-04-15 14:10:26 -0700

    Parse DDEX deals (redo after refactor) (#8053)

--- packages/ddex/publisher/src/services/publisherService.ts
+++ packages/ddex/publisher/src/services/publisherService.ts
@@ -42,14 +42,13 @@ const formatTrackMetadata = (
     copyrightLine: metadata.copyright_line,
     producerCopyrightLine: metadata.producer_copyright_line,
     parentalWarningType: metadata.parental_warning_type,
+    isStreamGated: metadata.is_stream_gated,
+    streamConditions: metadata.stream_conditions,
+    isDownloadGated: metadata.is_download_gated,
+    downloadConditions: metadata.download_conditions,
     // isUnlisted: // TODO: set visibility
     // origFilename:
     // isOriginalAvailable:
-    // isStreamGated:
-    // streamConditions:
-    // isDownloadable:
-    // isDownloadGated:
-    // downloadConditions:
     // remixOf:
   }
 }
