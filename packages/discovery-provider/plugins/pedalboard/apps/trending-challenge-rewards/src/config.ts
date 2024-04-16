b095fe6dd8484d8b73a7002dab9e9c734953cd52
Alec Savvy <alecsavvy@outlook.com>
2024-04-16 09:01:14 -0600

    add prefix to all vars and change crontab

--- packages/discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/config.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/trending-challenge-rewards/src/config.ts
@@ -12,7 +12,6 @@ export type SharedData = {
   libs: AudiusLibs
   localEndpoint: string
   dryRun: boolean
-  dateToRun: string
 }
 
 let sharedData: SharedData | undefined = undefined
@@ -24,13 +23,13 @@ export const initSharedData = async (): Promise<SharedData> => {
 
   // default to true if undefined, otherwise explicitly state false to not do dry run
   const dryRun = !(
-    (process.env.dryRun || 'true').toLocaleLowerCase() === 'false'
+    (process.env.tcrDryRun || 'true').toLocaleLowerCase() === 'false'
   )
 
-  const oracleEthAddress = process.env.oracleEthAddress
-  const AAOEndpoint = process.env.AAOEndpoint
-  const feePayerOverride = process.env.feePayerOverride
-  const localEndpoint = process.env.localEndpoint
+  const oracleEthAddress = process.env.tcrOracleEthAddress
+  const AAOEndpoint = process.env.tcrAAOEndpoint
+  const feePayerOverride = process.env.tcrFeePayerOverride
+  const localEndpoint = process.env.tcrLocalEndpoint
 
   const dateToRun = process.env.dateToRun
 
@@ -40,7 +39,6 @@ export const initSharedData = async (): Promise<SharedData> => {
   if (feePayerOverride === undefined)
     throw new Error('feePayerOverride undefined')
   if (localEndpoint === undefined) throw new Error('localEndpoint undefined')
-  if (dateToRun === undefined) throw new Error('dateToRun undefined')
 
   sharedData = {
     oracleEthAddress,
@@ -48,19 +46,8 @@ export const initSharedData = async (): Promise<SharedData> => {
     feePayerOverride,
     libs,
     localEndpoint,
-    dryRun,
-    dateToRun
+    dryRun
   }
   // @ts-ignore
   return sharedData
-}
-
-export const condition = (app: App<SharedData>): boolean => {
-  // "Fri 09:43:00 GMT-0600"
-  const { dateToRun } = app.viewAppData()
-  const date = Date.parse(dateToRun)
-  const timeToDisburse = moment(date)
-  const now = moment()
-  if (now.isSame(timeToDisburse, 'seconds')) return true
-  return false
 }
