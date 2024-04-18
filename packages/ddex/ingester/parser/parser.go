6ebd47aabd1c840d1e5ae7434b7cc3a7715f421e
Michelle Brier <michelle.brier4@gmail.com>
2024-04-17 17:37:14 -0700

    add comment

--- packages/ddex/ingester/parser/parser.go
+++ packages/ddex/ingester/parser/parser.go
@@ -219,7 +219,8 @@ func (p *Parser) parseRelease(unprocessedRelease *common.UnprocessedRelease, del
 				return nil, err
 			}
 		}
-		// For albums/EPs without a ValidityStartDate: use the latest ValidityStartDate from the tracks on the album
+		// For albums/EPs without a ValidityStartDate: use the latest ValidityStartDate from the tracks on the album.
+		// This case is possible because deals for tracks are required but a deal for the album release is not required.
 		if parsedRelease.IsMainRelease && parsedRelease.ReleaseType != common.TrackReleaseType && parsedRelease.ValidityStartDate.IsZero() {
 			var maxTrackValidityStartDate time.Time
 			for _, elem := range release.ParsedReleaseElems {
