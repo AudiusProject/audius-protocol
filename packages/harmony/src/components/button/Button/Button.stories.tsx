81850f1e6308b597a1162bd4b5fdcadc26358423
Dylan Jeffers <dylan@audius.co>
2024-04-18 11:39:39 -0700

    Revert "Test figma code-connect (#8139)"
    This reverts commit bc84708ab217b73713f4d2acb5a7a46e76b9d5f8.

--- packages/harmony/src/components/button/Button/Button.stories.tsx
+++ packages/harmony/src/components/button/Button/Button.stories.tsx
@@ -11,22 +11,15 @@ import { ButtonProps } from './types'
 
 const meta: Meta<typeof Button> = {
   title: 'Buttons/Button',
-  component: Button,
-  parameters: {
-    design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/hcVGqf35jfxueO7klqXlma/%F0%9F%A7%B0-Harmony-Component-Library?type=design&node-id=143-534&mode=dev',
-      examples: [Primary]
-    }
-  }
+  component: Button
 }
 
 export default meta
 
 type Story = StoryObj<typeof Button>
 
-export function Primary(props: ButtonProps) {
-  return <Button {...props}>Button</Button>
+export const Primary: Story = {
+  render: (props: ButtonProps) => <Button {...props}>Button</Button>
 }
 
 export const Variants: Story = {
