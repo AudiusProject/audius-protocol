448dd0a3636cdb4f5c7a4b9a7a1ed4165aca6207
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-04-19 16:07:06 -0400

    refactor

---
+++ packages/web/src/pages/dashboard-page/components/AlbumsTab.tsx
@@ -0,0 +1,199 @@
+import { useState, useCallback, useMemo } from 'react'
+
+import {
+  isContentUSDCPurchaseGated,
+  User,
+  Collection
+} from '@audius/common/models'
+import { accountSelectors } from '@audius/common/store'
+import { Nullable } from '@audius/common/utils'
+import {
+  IconVisibilityHidden,
+  IconVisibilityPublic,
+  IconCart,
+  Paper
+} from '@audius/harmony'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { TracksTable, TracksTableColumn } from 'components/tracks-table'
+import { useGoToRoute } from 'hooks/useGoToRoute'
+
+import { makeGetDashboard } from '../store/selectors'
+
+const { getAccountAlbums } = accountSelectors
+
+// Pagination Constants
+export const tablePageSize = 50
+
+const messages = {
+  filterInputPlacehoder: 'Search Tracks',
+  allReleases: 'All Releases',
+  public: 'Public',
+  premium: 'Premium',
+  specialAcess: 'SpecialAccess',
+  gated: 'Gated',
+  hidden: 'Hidden',
+  tracks: 'Tracks',
+  albums: 'Albums'
+}
+
+const albumTableColumns: TracksTableColumn[] = [
+  'spacer',
+  'trackName',
+  'releaseDate',
+  'reposts',
+  'overflowMenu'
+]
+
+export type DataSourceAlbum = Collection & {
+  key: string
+  name: string
+  date: string
+  saves: number
+  reposts: number
+}
+
+export enum AlbumFilters {
+  PUBLIC = 'Public',
+  PREMIUM = 'Premium',
+  HIDDEN = 'Hidden'
+}
+
+const formatAlbumMetadata = (album: Collection): DataSourceAlbum => {
+  return {
+    ...album,
+    key: String(album.playlist_id),
+    name: album.playlist_name,
+    date: album.created_at,
+    saves: album.save_count,
+    reposts: album.repost_count
+  }
+}
+
+export const useArtistDashboardAlbumFilters = ({
+  selectedFilter,
+  filterText
+}: {
+  selectedFilter: Nullable<AlbumFilters>
+  filterText: string
+}) => {
+  const albumsRaw = useSelector(getAccountAlbums) ?? []
+  const albums = albumsRaw?.map((album) => formatAlbumMetadata(album))
+
+  const { hasOnlyOneSection, publicAlbums, hiddenAlbums, premiumAlbums } =
+    useMemo(() => {
+      const publicAlbums = albums.filter(
+        (data) => data.is_private === false && !data.is_stream_gated
+      )
+      const hiddenAlbums = albums.filter((data) => !!data.is_private)
+      const premiumAlbums = albums.filter(
+        (data) =>
+          data.is_stream_gated &&
+          isContentUSDCPurchaseGated(data.stream_conditions)
+      )
+
+      const arrays = [publicAlbums, hiddenAlbums, premiumAlbums]
+      const nonEmptyArrays = arrays.filter((arr) => arr.length > 0)
+      const hasOnlyOneSection = nonEmptyArrays.length === 1
+
+      return {
+        hasOnlyOneSection,
+        publicAlbums,
+        hiddenAlbums,
+        premiumAlbums
+      }
+    }, [albums])
+
+  const filterButtonOptions = useMemo(() => {
+    const filterButtonAlbumOptions = [
+      {
+        id: AlbumFilters.PUBLIC,
+        label: messages.public,
+        icon: IconVisibilityPublic,
+        value: AlbumFilters.PUBLIC
+      }
+    ]
+    if (premiumAlbums.length) {
+      filterButtonAlbumOptions.push({
+        id: AlbumFilters.PREMIUM,
+        label: messages.premium,
+        icon: IconCart,
+        value: AlbumFilters.PREMIUM
+      })
+    }
+    if (hiddenAlbums.length) {
+      filterButtonAlbumOptions.push({
+        id: AlbumFilters.HIDDEN,
+        label: messages.hidden,
+        icon: IconVisibilityHidden,
+        value: AlbumFilters.HIDDEN
+      })
+    }
+
+    return filterButtonAlbumOptions
+  }, [hiddenAlbums, premiumAlbums])
+
+  let filteredData: DataSourceAlbum[] = albums
+  switch (selectedFilter) {
+    case AlbumFilters.PUBLIC:
+      filteredData = publicAlbums
+      break
+    case AlbumFilters.PREMIUM:
+      filteredData = premiumAlbums
+      break
+    case AlbumFilters.HIDDEN:
+      filteredData = hiddenAlbums
+      break
+    default:
+      filteredData = albums
+      break
+  }
+
+  if (filterText) {
+    filteredData = filteredData.filter((data) =>
+      data.name.toLowerCase().includes(filterText.toLowerCase())
+    )
+  }
+
+  return {
+    data: albums,
+    filteredData,
+    filterButtonOptions,
+    hasOnlyOneSection
+  }
+}
+
+type AlbumsTabProps = {
+  selectedFilter: Nullable<AlbumFilters>
+  filterText: string
+  onClickRow: (record: any) => void
+}
+
+export const AlbumsTab = ({
+  selectedFilter,
+  filterText,
+  onClickRow
+}: AlbumsTabProps) => {
+  const { account } = useSelector(makeGetDashboard())
+  const { filteredData } = useArtistDashboardAlbumFilters({
+    selectedFilter,
+    filterText
+  })
+
+  if (!filteredData.length || !account) return null
+
+  return (
+    <Paper w='100%' direction='column' mt='xl'>
+      <TracksTable
+        data={filteredData}
+        disabledTrackEdit
+        columns={albumTableColumns}
+        onClickRow={onClickRow}
+        pageSize={tablePageSize}
+        userId={account.user_id}
+        showMoreLimit={5}
+        totalRowCount={account.track_count}
+      />
+    </Paper>
+  )
+}
---
+++ packages/web/src/pages/dashboard-page/components/TracksTab.tsx
@@ -0,0 +1,270 @@
+import { useState, useCallback, useMemo } from 'react'
+
+import {
+  isContentUSDCPurchaseGated,
+  Track,
+  User,
+  Status,
+  isContentFollowGated,
+  isContentCollectibleGated
+} from '@audius/common/models'
+import { Nullable } from '@audius/common/utils'
+import {
+  IconSpecialAccess,
+  IconCollectible,
+  IconVisibilityHidden,
+  IconVisibilityPublic,
+  IconCart
+} from '@audius/harmony'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { TracksTable, TracksTableColumn } from 'components/tracks-table'
+import { useGoToRoute } from 'hooks/useGoToRoute'
+
+import { getDashboardTracksStatus, makeGetDashboard } from '../store/selectors'
+import { fetchTracks } from '../store/slice'
+
+// Pagination Constants
+export const tablePageSize = 50
+
+const messages = {
+  filterInputPlacehoder: 'Search Tracks',
+  allReleases: 'All Releases',
+  public: 'Public',
+  premium: 'Premium',
+  specialAcess: 'SpecialAccess',
+  gated: 'Gated',
+  hidden: 'Hidden',
+  tracks: 'Tracks'
+}
+
+const tracksTableColumns: TracksTableColumn[] = [
+  'spacer',
+  'trackName',
+  'releaseDate',
+  'length',
+  'plays',
+  'reposts',
+  'overflowMenu'
+]
+
+export type DataSourceTrack = Track & {
+  key: string
+  name: string
+  date: string
+  time?: number
+  saves: number
+  reposts: number
+  plays: number
+}
+
+const formatTrackMetadata = (metadata: Track, i: number): DataSourceTrack => {
+  return {
+    ...metadata,
+    key: `${metadata.title}_${metadata.dateListened}_${i}`,
+    name: metadata.title,
+    date: metadata.created_at,
+    time: metadata.duration,
+    saves: metadata.save_count,
+    reposts: metadata.repost_count,
+    plays: metadata.play_count
+  }
+}
+
+export enum TrackFilters {
+  PUBLIC = 'Public',
+  PREMIUM = 'Premium',
+  SPECIAL_ACCESS = 'SpecialAccess',
+  COLLECTIBLE_GATED = 'CollectibleGated',
+  HIDDEN = 'Hidden'
+}
+
+export const useArtistDashboardTrackFilters = ({
+  selectedFilter,
+  filterText
+}: {
+  selectedFilter: Nullable<TrackFilters>
+  filterText: string
+}) => {
+  const { tracks: tracksRaw } = useSelector(makeGetDashboard())
+  const tracks = tracksRaw
+    .map((track: Track, i: number) => formatTrackMetadata(track, i))
+    .filter((meta) => !meta.is_invalid)
+
+  const {
+    hasOnlyOneSection,
+    publicTracks,
+    specialAccessTracks,
+    hiddenTracks,
+    premiumTracks,
+    collectibleGatedTracks
+  } = useMemo(() => {
+    const publicTracks = tracks.filter(
+      (data) => data.is_unlisted === false && !data.is_stream_gated
+    )
+    const specialAccessTracks = tracks.filter(
+      (data) =>
+        data.is_stream_gated &&
+        (isContentFollowGated(data.stream_conditions) ||
+          isContentFollowGated(data.stream_conditions))
+    )
+    const hiddenTracks = tracks.filter((data) => !!data.is_unlisted)
+    const premiumTracks = tracks.filter(
+      (data) =>
+        data.is_stream_gated &&
+        isContentUSDCPurchaseGated(data.stream_conditions)
+    )
+    const collectibleGatedTracks = tracks.filter(
+      (data) =>
+        data.is_stream_gated &&
+        isContentCollectibleGated(data.stream_conditions)
+    )
+
+    const arrays = [
+      publicTracks,
+      specialAccessTracks,
+      hiddenTracks,
+      premiumTracks,
+      collectibleGatedTracks
+    ]
+    const nonEmptyArrays = arrays.filter((arr) => arr.length > 0)
+    const hasOnlyOneSection = nonEmptyArrays.length === 1
+
+    return {
+      hasOnlyOneSection,
+      publicTracks,
+      specialAccessTracks,
+      hiddenTracks,
+      premiumTracks,
+      collectibleGatedTracks
+    }
+  }, [tracks])
+
+  let filteredData: DataSourceTrack[] = tracks
+  switch (selectedFilter) {
+    case TrackFilters.PUBLIC:
+      filteredData = publicTracks
+      break
+    case TrackFilters.PREMIUM:
+      filteredData = premiumTracks
+      break
+    case TrackFilters.SPECIAL_ACCESS:
+      filteredData = specialAccessTracks
+      break
+    case TrackFilters.COLLECTIBLE_GATED:
+      filteredData = collectibleGatedTracks
+      break
+    case TrackFilters.HIDDEN:
+      filteredData = hiddenTracks
+      break
+    default:
+      filteredData = tracks
+      break
+  }
+
+  const filterButtonOptions = useMemo(() => {
+    const filterButtonTrackOptions = [
+      {
+        id: TrackFilters.PUBLIC,
+        label: messages.public,
+        icon: IconVisibilityPublic,
+        value: TrackFilters.PUBLIC
+      }
+    ]
+    if (premiumTracks.length) {
+      filterButtonTrackOptions.push({
+        id: TrackFilters.PREMIUM,
+        label: messages.premium,
+        icon: IconCart,
+        value: TrackFilters.PREMIUM
+      })
+    }
+    if (specialAccessTracks.length) {
+      filterButtonTrackOptions.push({
+        id: TrackFilters.SPECIAL_ACCESS,
+        label: messages.specialAcess,
+        icon: IconSpecialAccess,
+        value: TrackFilters.SPECIAL_ACCESS
+      })
+    }
+    if (collectibleGatedTracks.length) {
+      filterButtonTrackOptions.push({
+        id: TrackFilters.COLLECTIBLE_GATED,
+        label: messages.gated,
+        icon: IconCollectible,
+        value: TrackFilters.COLLECTIBLE_GATED
+      })
+    }
+    if (hiddenTracks.length) {
+      filterButtonTrackOptions.push({
+        id: TrackFilters.HIDDEN,
+        label: messages.hidden,
+        icon: IconVisibilityHidden,
+        value: TrackFilters.HIDDEN
+      })
+    }
+
+    return filterButtonTrackOptions
+  }, [collectibleGatedTracks, hiddenTracks, premiumTracks, specialAccessTracks])
+
+  if (filterText) {
+    filteredData = filteredData.filter((data) =>
+      data.name.toLowerCase().includes(filterText.toLowerCase())
+    )
+  }
+
+  return {
+    data: tracks,
+    filteredData,
+    filterButtonOptions,
+    hasOnlyOneSection
+  }
+}
+
+type TracksTabProps = {
+  selectedFilter: Nullable<TrackFilters>
+  filterText: string
+  onClickRow: (record: any) => void
+}
+
+export const TracksTab = ({
+  selectedFilter,
+  filterText,
+  onClickRow
+}: TracksTabProps) => {
+  const dispatch = useDispatch()
+  const tracksStatus = useSelector(getDashboardTracksStatus)
+  const { account } = useSelector(makeGetDashboard())
+
+  const handleFetchPage = useCallback(
+    (page: number) => {
+      dispatch(
+        fetchTracks({ offset: page * tablePageSize, limit: tablePageSize })
+      )
+    },
+    [dispatch]
+  )
+
+  const { filteredData } = useArtistDashboardTrackFilters({
+    selectedFilter,
+    filterText
+  })
+
+  if (!filteredData.length || !account) return null
+
+  return (
+    <TracksTable
+      data={filteredData}
+      disabledTrackEdit
+      columns={tracksTableColumns}
+      onClickRow={onClickRow}
+      fetchPage={handleFetchPage}
+      pageSize={tablePageSize}
+      userId={account.user_id}
+      showMoreLimit={5}
+      totalRowCount={account.track_count}
+      loading={tracksStatus === Status.LOADING}
+      isPaginated
+    />
+  )
+}
