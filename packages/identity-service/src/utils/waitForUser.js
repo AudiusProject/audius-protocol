ed6b54299e237651f93a34896db216100ea7d42a
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-04-18 22:30:39 -0700

    I hate js

--- packages/identity-service/src/utils/waitForUser.js
+++ packages/identity-service/src/utils/waitForUser.js
@@ -3,42 +3,47 @@ const RETRY_TIMEOUT_MS = 45 * 1000
 /** How long to wait between retries */
 const RETRY_DELAY_MS = 1000
 
-export const waitForUser = async ({
-  userId,
-  handle,
-  blockNumber: blocknumber,
-  audiusLibsInstance,
-  logger
-}) => {
-  let retryCount = 0
-  while (retryCount < RETRY_TIMEOUT_MS / RETRY_DELAY_MS) {
-    try {
-      logger.info({ userId, handle, blocknumber }, 'req')
-      const u = await audiusLibsInstance.discoveryProvider._makeRequest(
-        {
-          endpoint: 'users',
-          queryParams: { id: userId }
-        },
-        true,
-        0,
-        true,
-        blocknumber
-      )
-      if (!u || !u[0]) {
-        throw new Error('User not found')
-      }
-      return u[0]
-    } catch (e) {
-      if (e instanceof Error && e.message.startsWith('Requested blocknumber')) {
-        logger.warn(
-          { blocknumber, handle, userId, retryCount },
-          'Block number not passed... waiting...'
+module.exports = {
+  waitForUser: async ({
+    userId,
+    handle,
+    blockNumber: blocknumber,
+    audiusLibsInstance,
+    logger
+  }) => {
+    let retryCount = 0
+    while (retryCount < RETRY_TIMEOUT_MS / RETRY_DELAY_MS) {
+      try {
+        logger.info({ userId, handle, blocknumber }, 'req')
+        const u = await audiusLibsInstance.discoveryProvider._makeRequest(
+          {
+            endpoint: 'users',
+            queryParams: { id: userId }
+          },
+          true,
+          0,
+          true,
+          blocknumber
         )
-        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS))
-        retryCount++
-        continue
+        if (!u || !u[0]) {
+          throw new Error('User not found')
+        }
+        return u[0]
+      } catch (e) {
+        if (
+          e instanceof Error &&
+          e.message.startsWith('Requested blocknumber')
+        ) {
+          logger.warn(
+            { blocknumber, handle, userId, retryCount },
+            'Block number not passed... waiting...'
+          )
+          await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS))
+          retryCount++
+          continue
+        }
+        throw e
       }
-      throw e
     }
   }
 }
