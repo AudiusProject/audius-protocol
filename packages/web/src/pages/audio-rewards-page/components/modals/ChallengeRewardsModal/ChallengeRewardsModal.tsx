8a83bd08ac350a2dc5d3a8cf717306908c82ee63
Saliou Diallo <saliou@audius.co>
2024-04-21 14:03:14 -0400

    Update reward modals

--- packages/web/src/pages/audio-rewards-page/components/modals/ChallengeRewardsModal/ChallengeRewardsModal.tsx
+++ packages/web/src/pages/audio-rewards-page/components/modals/ChallengeRewardsModal/ChallengeRewardsModal.tsx
@@ -1,6 +1,10 @@
 import { useCallback, useEffect, useContext, useMemo } from 'react'
 
-import { useFeatureFlag } from '@audius/common/hooks'
+import {
+  formatCooldownChallenges,
+  useChallengeCooldownSchedule,
+  useFeatureFlag
+} from '@audius/common/hooks'
 import { FeatureFlags } from '@audius/common/services'
 import {
   accountSelectors,
@@ -28,6 +32,7 @@ import {
   IconTwitter as IconTwitterBird,
   SocialButton,
   Button,
+  Text,
   ProgressBar
 } from '@audius/harmony'
 import cn from 'classnames'
@@ -36,6 +41,7 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import QRCode from 'assets/img/imageQR.png'
 import { useModalState } from 'common/hooks/useModalState'
+import { SummaryTable } from 'components/summary-table'
 import Toast from 'components/toast/Toast'
 import { ToastContext } from 'components/toast/ToastContext'
 import Tooltip from 'components/tooltip/Tooltip'
@@ -51,7 +57,6 @@ import PurpleBox from '../../PurpleBox'
 import ModalDrawer from '../ModalDrawer'
 
 import { AudioMatchingRewardsModalContent } from './AudioMatchingRewardsModalContent'
-import { CooldownRewardsModalContent } from './CooldownRewardsModalContent'
 import { ProgressDescription } from './ProgressDescription'
 import { ProgressReward } from './ProgressReward'
 import styles from './styles.module.css'
@@ -86,6 +91,7 @@ export const useRewardsModalType = (): [
   return [modalType, setModalType]
 }
 const inviteLink = getCopyableLink('/signup?ref=%0')
+
 const messages = {
   audio: '$AUDIO',
   everyDollarSpent: ' Every Dollar Spent',
@@ -109,6 +115,9 @@ const messages = {
   verifiedChallenge: 'VERIFIED CHALLENGE',
   claimAmountLabel: '$AUDIO available to claim',
   claimedSoFar: '$AUDIO claimed so far',
+  upcomingRewards: 'Upcoming Rewards',
+  cooldownDescription:
+    'Note: There is a 7 day waiting period from completion until you can claim your reward.',
 
   // Profile checks
   profileCheckNameAndHandle: 'Name & Handle',
@@ -125,7 +134,7 @@ type InviteLinkProps = {
   inviteLink: string
 }
 
-const InviteLink = ({ className, inviteLink }: InviteLinkProps) => {
+export const InviteLink = ({ className, inviteLink }: InviteLinkProps) => {
   const wm = useWithMobileStyle(styles.mobile)
 
   const onButtonClick = useCallback(() => {
@@ -162,7 +171,7 @@ type TwitterShareButtonProps = {
   inviteLink: string
 }
 
-const TwitterShareButton = ({
+export const TwitterShareButton = ({
   modalType,
   inviteLink
 }: TwitterShareButtonProps) => {
@@ -181,7 +190,7 @@ const TwitterShareButton = ({
   )
 }
 
-const ProfileChecks = () => {
+export const ProfileChecks = () => {
   const completionStages = useSelector(getCompletionStages)
   const wm = useWithMobileStyle(styles.mobile)
 
@@ -229,21 +238,49 @@ type BodyProps = {
 }
 
 const ChallengeRewardsBody = ({ dismissModal }: BodyProps) => {
+  const { isEnabled: isRewardsCooldownEnabled } = useFeatureFlag(
+    FeatureFlags.REWARDS_COOLDOWN
+  )
+  const { toast } = useContext(ToastContext)
+  const claimStatus = useSelector(getClaimStatus)
+  const aaoErrorCode = useSelector(getAAOErrorCode)
+  const claimInProgress =
+    claimStatus === ClaimStatus.CLAIMING ||
+    claimStatus === ClaimStatus.WAITING_FOR_RETRY
+  const undisbursedUserChallenges = useSelector(getUndisbursedUserChallenges)
   const [modalType] = useRewardsModalType()
   const userHandle = useSelector(getUserHandle)
   const dispatch = useDispatch()
   const wm = useWithMobileStyle(styles.mobile)
   const isMobile = useIsMobile()
-
   const userChallenges = useSelector(getOptimisticUserChallenges)
   const challenge = userChallenges[modalType]
-  const undisbursedUserChallenges = useSelector(getUndisbursedUserChallenges)
-
+  const isCooldownChallenge = challenge && challenge.cooldown_days > 0
+  const currentStepCount = challenge?.current_step_count || 0
   const { fullDescription, progressLabel, isVerifiedChallenge } =
     challengeRewardsConfig[modalType]
   const { modalButtonInfo } = getChallengeConfig(modalType)
+  const {
+    cooldownChallenges,
+    summary,
+    isEmpty: isCooldownChallengesEmpty
+  } = useChallengeCooldownSchedule({ challengeId: challenge?.challenge_id })
 
-  const currentStepCount = challenge?.current_step_count || 0
+  // We could just depend on undisbursedAmount here
+  // But DN may have not indexed the challenge so check for client-side completion too
+  // Note that we can't handle aggregate challenges optimistically
+  let audioToClaim = 0
+  let audioClaimedSoFar = 0
+  if (challenge?.challenge_type === 'aggregate') {
+    audioToClaim = challenge.claimableAmount
+    audioClaimedSoFar = challenge.disbursed_amount
+  } else if (challenge?.state === 'completed') {
+    audioToClaim = challenge.totalAmount
+    audioClaimedSoFar = 0
+  } else if (challenge?.state === 'disbursed') {
+    audioToClaim = 0
+    audioClaimedSoFar = challenge.totalAmount
+  }
 
   let linkType: 'complete' | 'inProgress' | 'incomplete'
   if (challenge?.state === 'completed') {
@@ -256,36 +293,33 @@ const ChallengeRewardsBody = ({ dismissModal }: BodyProps) => {
   const buttonInfo = modalButtonInfo?.[linkType] ?? null
   const buttonLink = buttonInfo?.link(userHandle)
 
-  const goToRoute = useCallback(() => {
-    if (!buttonLink) return
-    dispatch(pushRoute(buttonLink))
-    dismissModal()
-  }, [buttonLink, dispatch, dismissModal])
+  const showProgressBar =
+    challenge &&
+    challenge.max_steps > 1 &&
+    challenge.challenge_type !== 'aggregate'
 
-  const progressDescription = (
-    <ProgressDescription
-      label={
-        isVerifiedChallenge ? (
-          <div className={styles.verifiedChallenge}>
-            <IconVerified />
-            {messages.verifiedChallenge}
-          </div>
-        ) : (
-          'Task'
-        )
-      }
-      description={fullDescription?.(challenge)}
-    />
+  const progressDescriptionLabel = isVerifiedChallenge ? (
+    <div className={styles.verifiedChallenge}>
+      <IconVerified />
+      {messages.verifiedChallenge}
+    </div>
+  ) : (
+    'Task'
+  )
+  const progressDescription = isRewardsCooldownEnabled ? (
+    <div className={styles.audioMatchingDescription}>
+      <Text variant='body'>{fullDescription?.(challenge)}</Text>
+      {isCooldownChallenge ? (
+        <Text variant='body' color='subdued'>
+          {messages.cooldownDescription}
+        </Text>
+      ) : null}
+    </div>
+  ) : (
+    fullDescription?.(challenge)
   )
 
-  const progressReward = (
-    <ProgressReward
-      amount={formatNumberCommas(challenge?.totalAmount ?? '')}
-      subtext={messages.audio}
-    />
-  )
-
-  const progressStatusLabel = (
+  const renderProgressStatusLabel = () => (
     <div
       className={cn(styles.progressStatus, {
         [styles.incomplete]: challenge?.state === 'incomplete',
@@ -312,33 +346,25 @@ const ChallengeRewardsBody = ({ dismissModal }: BodyProps) => {
     </div>
   )
 
-  const { toast } = useContext(ToastContext)
-  const claimStatus = useSelector(getClaimStatus)
-  const aaoErrorCode = useSelector(getAAOErrorCode)
-  const claimInProgress =
-    claimStatus === ClaimStatus.CLAIMING ||
-    claimStatus === ClaimStatus.WAITING_FOR_RETRY
+  const inviteLink = useMemo(
+    () => (userHandle ? fillString(messages.inviteLink, userHandle) : ''),
+    [userHandle]
+  )
 
-  // We could just depend on undisbursedAmount here
-  // But DN may have not indexed the challenge so check for client-side completion too
-  // Note that we can't handle aggregate challenges optimistically
-  let audioToClaim = 0
-  let audioClaimedSoFar = 0
-  if (challenge?.challenge_type === 'aggregate') {
-    audioToClaim = challenge.claimableAmount
-    audioClaimedSoFar = challenge.disbursed_amount
-  } else if (challenge?.state === 'completed') {
-    audioToClaim = challenge.totalAmount
-    audioClaimedSoFar = 0
-  } else if (challenge?.state === 'disbursed') {
-    audioToClaim = 0
-    audioClaimedSoFar = challenge.totalAmount
-  }
+  const errorContent =
+    claimStatus === ClaimStatus.ERROR ? (
+      <div className={styles.claimError}>{getErrorMessage(aaoErrorCode)}</div>
+    ) : null
 
-  const showProgressBar =
-    challenge &&
-    challenge.max_steps > 1 &&
-    challenge.challenge_type !== 'aggregate'
+  useEffect(() => {
+    if (claimStatus === ClaimStatus.SUCCESS) {
+      toast(messages.rewardClaimed, CLAIM_REWARD_TOAST_TIMEOUT_MILLIS)
+      dispatch(showConfetti())
+    }
+    if (claimStatus === ClaimStatus.ALREADY_CLAIMED) {
+      toast(messages.rewardAlreadyClaimed, CLAIM_REWARD_TOAST_TIMEOUT_MILLIS)
+    }
+  }, [claimStatus, toast, dispatch])
 
   const onClaimRewardClicked = useCallback(() => {
     if (challenge) {
@@ -363,43 +389,111 @@ const ChallengeRewardsBody = ({ dismissModal }: BodyProps) => {
     }
   }, [challenge, dispatch, undisbursedUserChallenges])
 
-  useEffect(() => {
-    if (claimStatus === ClaimStatus.SUCCESS) {
-      toast(messages.rewardClaimed, CLAIM_REWARD_TOAST_TIMEOUT_MILLIS)
-      dispatch(showConfetti())
-    }
-    if (claimStatus === ClaimStatus.ALREADY_CLAIMED) {
-      toast(messages.rewardAlreadyClaimed, CLAIM_REWARD_TOAST_TIMEOUT_MILLIS)
-    }
-  }, [claimStatus, toast, dispatch])
+  const goToRoute = useCallback(() => {
+    if (!buttonLink) return
+    dispatch(pushRoute(buttonLink))
+    dismissModal()
+  }, [buttonLink, dispatch, dismissModal])
 
-  const inviteLink = useMemo(
-    () => (userHandle ? fillString(messages.inviteLink, userHandle) : ''),
-    [userHandle]
-  )
+  const renderCooldownSummaryTable = () => {
+    if (isCooldownChallenge && !isCooldownChallengesEmpty) {
+      return (
+        <SummaryTable
+          title={messages.upcomingRewards}
+          items={formatCooldownChallenges(cooldownChallenges)}
+          summaryItem={summary}
+          secondaryTitle={messages.audio}
+          summaryLabelColor='accent'
+          summaryValueColor='default'
+        />
+      )
+    }
+    return null
+  }
 
-  const errorContent =
-    claimStatus === ClaimStatus.ERROR ? (
-      <div className={styles.claimError}>{getErrorMessage(aaoErrorCode)}</div>
-    ) : null
-  const { isEnabled: isRewardsCooldownEnabled } = useFeatureFlag(
-    FeatureFlags.REWARDS_COOLDOWN
-  )
-  if (isRewardsCooldownEnabled && challenge && challenge.cooldown_days > 0) {
-    return (
-      <CooldownRewardsModalContent
-        errorContent={errorContent}
-        onNavigateAway={dismissModal}
-        onClaimRewardClicked={onClaimRewardClicked}
-        claimInProgress={claimInProgress}
-        challenge={challenge}
-        challengeName={modalType}
-        onClickProgress={goToRoute}
-        progressIcon={buttonInfo?.rightIcon}
-        progressLabel={buttonInfo?.label}
-      />
-    )
-  } else if (!isRewardsCooldownEnabled && isAudioMatchingChallenge(modalType)) {
+  const renderProgressBar = () => {
+    if (showProgressBar) {
+      return (
+        <div className={wm(styles.progressBarSection)}>
+          {isMobile ? (
+            <h3>Progress</h3>
+          ) : modalType === 'profile-completion' ? (
+            <ProfileChecks />
+          ) : null}
+          <ProgressBar
+            className={wm(styles.progressBar)}
+            value={currentStepCount}
+            max={challenge?.max_steps}
+          />
+        </div>
+      )
+    }
+    return null
+  }
+
+  const renderReferralContent = () => {
+    if (userHandle && (modalType === 'referrals' || modalType === 'ref-v')) {
+      return (
+        <div className={wm(styles.buttonContainer)}>
+          <TwitterShareButton modalType={modalType} inviteLink={inviteLink} />
+          <div className={styles.buttonSpacer} />
+          <InviteLink inviteLink={inviteLink} />
+        </div>
+      )
+    }
+    return null
+  }
+
+  const renderMobileInstallContent = () => {
+    if (modalType === 'mobile-install') {
+      return (
+        <div className={wm(styles.qrContainer)}>
+          <img className={styles.qr} src={QRCode} alt='QR Code' />
+          <div className={styles.qrTextContainer}>
+            <h2 className={styles.qrText}>{messages.qrText}</h2>
+            <h3 className={styles.qrSubtext}>{messages.qrSubtext}</h3>
+          </div>
+        </div>
+      )
+    }
+    return null
+  }
+
+  const renderClaimButton = () => {
+    if (audioToClaim > 0) {
+      return (
+        <>
+          <div className={styles.claimRewardAmountLabel}>
+            {`${audioToClaim} ${messages.claimAmountLabel}`}
+          </div>
+          <Button
+            variant='primary'
+            isLoading={claimInProgress}
+            iconRight={IconCheck}
+            onClick={onClaimRewardClicked}
+          >
+            {messages.claimYourReward}
+          </Button>
+        </>
+      )
+    }
+    return null
+  }
+
+  const renderClaimedSoFarContent = () => {
+    if (audioClaimedSoFar > 0 && challenge?.state !== 'disbursed') {
+      return (
+        <div className={styles.claimRewardClaimedAmountLabel}>
+          {`(${formatNumberCommas(audioClaimedSoFar)} ${
+            messages.claimedSoFar
+          })`}
+        </div>
+      )
+    }
+    return null
+  }
+
+  if (isAudioMatchingChallenge(modalType)) {
     return (
       <AudioMatchingRewardsModalContent
         errorContent={errorContent}
@@ -415,61 +509,43 @@ const ChallengeRewardsBody = ({ dismissModal }: BodyProps) => {
       <div className={wm(styles.container)}>
         {isMobile ? (
           <>
-            {progressDescription}
+            <ProgressDescription
+              label={progressDescriptionLabel}
+              description={progressDescription}
+            />
             <div className={wm(styles.progressCard)}>
               <div className={wm(styles.progressInfo)}>
-                {progressReward}
-                {showProgressBar ? (
-                  <div className={wm(styles.progressBarSection)}>
-                    <h3>Progress</h3>
-                    <ProgressBar
-                      className={wm(styles.progressBar)}
-                      value={currentStepCount}
-                      max={challenge?.max_steps}
-                    />
-                  </div>
-                ) : null}
+                <ProgressReward
+                  amount={formatNumberCommas(challenge?.totalAmount ?? '')}
+                  subtext={messages.audio}
+                />
+                {renderProgressBar()}
               </div>
-              {progressStatusLabel}
+              {renderProgressStatusLabel()}
             </div>
             {modalType === 'profile-completion' ? <ProfileChecks /> : null}
           </>
         ) : (
-          <div className={styles.progressCard}>
-            <div className={styles.progressInfo}>
-              {progressDescription}
-              {progressReward}
-            </div>
-            {showProgressBar ? (
-              <div className={wm(styles.progressBarSection)}>
-                {modalType === 'profile-completion' ? <ProfileChecks /> : null}
-                <ProgressBar
-                  className={wm(styles.progressBar)}
-                  value={currentStepCount}
-                  max={challenge?.max_steps}
+          <>
+            <div className={styles.progressCard}>
+              <div className={styles.progressInfo}>
+                <ProgressDescription
+                  label={progressDescriptionLabel}
+                  description={progressDescription}
+                />
+                <ProgressReward
+                  amount={formatNumberCommas(challenge?.totalAmount ?? '')}
+                  subtext={messages.audio}
                 />
               </div>
-            ) : null}
-            {progressStatusLabel}
-          </div>
-        )}
-
-        {userHandle && (modalType === 'referrals' || modalType === 'ref-v') ? (
-          <div className={wm(styles.buttonContainer)}>
-            <TwitterShareButton modalType={modalType} inviteLink={inviteLink} />
-            <div className={styles.buttonSpacer} />
-            <InviteLink inviteLink={inviteLink} />
-          </div>
-        ) : null}
-        {modalType === 'mobile-install' ? (
-          <div className={wm(styles.qrContainer)}>
-            <img className={styles.qr} src={QRCode} alt='QR Code' />
-            <div className={styles.qrTextContainer}>
-              <h2 className={styles.qrText}>{messages.qrText}</h2>
-              <h3 className={styles.qrSubtext}>{messages.qrSubtext}</h3>
+              {renderProgressBar()}
+              {renderProgressStatusLabel()}
             </div>
-          </div>
-        ) : null}
+            {renderCooldownSummaryTable()}
+          </>
+        )}
+        {renderReferralContent()}
+        {renderMobileInstallContent()}
         {buttonLink && challenge?.state !== 'completed' ? (
           <Button
             variant='primary'
@@ -482,28 +558,8 @@ const ChallengeRewardsBody = ({ dismissModal }: BodyProps) => {
           </Button>
         ) : null}
         <div className={wm(styles.claimRewardWrapper)}>
-          {audioToClaim > 0 ? (
-            <>
-              <div className={styles.claimRewardAmountLabel}>
-                {`${audioToClaim} ${messages.claimAmountLabel}`}
-              </div>
-              <Button
-                variant='primary'
-                isLoading={claimInProgress}
-                iconRight={IconCheck}
-                onClick={onClaimRewardClicked}
-              >
-                {messages.claimYourReward}
-              </Button>
-            </>
-          ) : null}
-          {audioClaimedSoFar > 0 && challenge?.state !== 'disbursed' ? (
-            <div className={styles.claimRewardClaimedAmountLabel}>
-              {`(${formatNumberCommas(audioClaimedSoFar)} ${
-                messages.claimedSoFar
-              })`}
-            </div>
-          ) : null}
+          {renderClaimButton()}
+          {renderClaimedSoFarContent()}
         </div>
         {errorContent}
       </div>
