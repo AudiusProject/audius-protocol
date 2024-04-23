e2076b815728024e30e13845f0cbecbe82bf5ca3
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-04-22 19:42:59 -0700

    get more error info

--- packages/web/src/common/store/pages/audio-rewards/sagas.ts
+++ packages/web/src/common/store/pages/audio-rewards/sagas.ts
@@ -32,7 +32,7 @@ import {
   waitForValue
 } from '@audius/common/utils'
 import { AUDIO } from '@audius/fixed-decimal'
-import { ChallengeId } from '@audius/sdk'
+import { ChallengeId, ResponseError } from '@audius/sdk'
 import {
   call,
   fork,
@@ -270,7 +270,7 @@ function* claimChallengeRewardAsync(
             })
             // Handle the individual specifier failures here to let the other
             // ones continue to claim and not reject the all() call.
-            .catch((error) => {
+            .catch((error: unknown) => {
               return {
                 ...specifierWithAmount,
                 error
@@ -290,14 +290,21 @@ function* claimChallengeRewardAsync(
     )
     yield* put(setUserChallengesDisbursed({ challengeId, specifiers: claimed }))
 
+    const isResponseError = (error: Error): error is ResponseError =>
+      'response' in error
     const errors = results.filter((r): r is ErrorResult => 'error' in r)
     if (errors.length > 0) {
       for (const res of errors) {
         const error =
           res.error instanceof Error ? res.error : new Error(String(res.error))
+        let response, responseBody
+        if (isResponseError(error)) {
+          response = error.response
+          responseBody = yield* call(async () => await error.response.json())
+        }
         console.error(
-          `Failed to claim specifier: ${res.specifier} for amount: ${res.amount} with error:`,
-          error
+          `Failed to claim challenge: ${challengeId} specifier: ${res.specifier} for amount: ${res.amount} with error:`,
+          response ?? error
         )
         yield* call(reportToSentry, {
           error,
@@ -305,7 +312,9 @@ function* claimChallengeRewardAsync(
           additionalInfo: {
             challengeId,
             specifier: res.specifier,
-            amount: res.amount
+            amount: res.amount,
+            responseBody,
+            response
           }
         })
       }
