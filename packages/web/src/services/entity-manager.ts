02405feda395b52a8adce8c9849b2e90229a7220
Raymond Jacobson <ray@audius.co>
2024-04-23 15:43:04 -0700

    Add comment to discovery relay

--- packages/web/src/services/entity-manager.ts
+++ packages/web/src/services/entity-manager.ts
@@ -22,6 +22,7 @@ export const makeEntityManagerInstance = (
     contractAddress: servicesConfig.entityManagerContractAddress,
     web3ProviderUrl: servicesConfig.web3ProviderUrl,
     identityServiceUrl: servicesConfig.identityServiceUrl,
+    // TODO: PROTO-1328, remove & default to true
     useDiscoveryRelay: true,
     discoveryNodeSelector
   })
