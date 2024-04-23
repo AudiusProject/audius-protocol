2386a8b4cac1bcdd7c0b340f93af43dfeb960889
JD Francis <jd@audius.co>
2024-04-23 14:18:56 -0500

    fix typo

--- packages/web/src/components/collection/desktop/PublishButton.tsx
+++ packages/web/src/components/collection/desktop/PublishButton.tsx
@@ -13,7 +13,7 @@ import { Tooltip } from 'components/tooltip'
 import { PublishConfirmationModal } from './PublishConfirmationModal'
 
 const { getCollection } = collectionPageSelectors
-const { getCollecitonHasHiddenTracks } = cacheCollectionsSelectors
+const { getCollectionHasHiddenTracks } = cacheCollectionsSelectors
 
 const messages = {
   publish: 'Make Public',
@@ -33,7 +33,7 @@ export const PublishButton = (props: PublishButtonProps) => {
     (state: CommonState) => getCollection(state, { id: collectionId })
   ) as Collection
   const hasHiddenTracks = useSelector((state: CommonState) =>
-    getCollecitonHasHiddenTracks(state, { id: collectionId })
+    getCollectionHasHiddenTracks(state, { id: collectionId })
   )
 
   const [isConfirming, toggleIsConfirming] = useToggle(false)
@@ -64,8 +64,8 @@ export const PublishButton = (props: PublishButtonProps) => {
           text={
             hasHiddenTracks
               ? messages.hiddenTracksTooltipText(
-                  is_album ? 'album' : 'playlist'
-                )
+                is_album ? 'album' : 'playlist'
+              )
               : messages.emptyPlaylistTooltipText
           }
         >
