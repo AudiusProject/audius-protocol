deb3f2adf6fbe646bc49801676aa428e763c3fce
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-04-23 23:05:53 -0700

    [PAY-2747] Fix attestation decoding and add Secp256k1Program utils (#8210)

--- packages/spl/src/index.ts
+++ packages/spl/src/index.ts
@@ -1,6 +1,7 @@
 export { ClaimableTokensProgram } from './claimable-tokens/ClaimableTokensProgram'
 export { RewardManagerInstruction } from './reward-manager/constants'
 export { RewardManagerProgram } from './reward-manager/RewardManagerProgram'
+export { Secp256k1Program } from './secp256k1/Secp256k1Program'
 export { ethAddress } from './layout-utils'
 export * from './associated-token'
 export * from './payment-router'
