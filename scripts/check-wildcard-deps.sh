d3992c10bd6749de1151456ffae5d102a6cb9035
sliptype <sliptype@gmail.com>
2024-04-24 17:19:12 -0500

    Better error output

--- scripts/check-wildcard-deps.sh
+++ scripts/check-wildcard-deps.sh
@@ -6,7 +6,7 @@ should_error=false
 echo "Checking public packages for dependencies with '*' version..."
 
 # Find all package.json files in the current directory and its subdirectories
-find . -name 'package.json' -not \( -path "./node_modules/*" -o -path "./**/node_modules/*" \) | while read -r file; do
+while IFS= read -r file; do
     # Check if "private" is not set to true in the package.json file
     if ! grep -q '"private":\s*true' "$file"; then
         echo "Checking $file"
@@ -23,10 +23,13 @@ find . -name 'package.json' -not \( -path "./node_modules/*" -o -path "./**/node
             should_error=true
         fi
     fi
-done
+done <<< "$(find . -name 'package.json' -not \( -path "./node_modules/*" -o -path "./**/node_modules/*" \))"
 
 if [[ $should_error = true ]]; then
-  exit 1
+    echo "Public packages should not contain dependencies with '*' version!"
+    echo "If you are trying to use an internal package from the monorepo, please specify the exact version. It will get updated automatically via changesets."
+    echo "If you are trying to use the latest version of an external package, please use 'latest'"
+    exit 1
 fi
 
 echo "No '*' version dependencies found in public packages!"
