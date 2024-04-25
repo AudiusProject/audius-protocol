3936dc0f5378e8e4b1cf4a97ebc4ffc19eba0fc3
sliptype <sliptype@gmail.com>
2024-04-25 16:04:54 -0500

    Add slack notify

---
+++ scripts/publish-packages-slack-template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "Generating Package Statuses..."
+
+for package in $(jq -r -c '.[]' /tmp/packages.json)     
+do
+  package_name=$(echo $package | jq -r '.name')
+  package_version=$(echo $package | jq -r '.version')
+  package_url="https://www.npmjs.com/package/$package_name"
+  PACKAGES=$(echo "${PACKAGES}\n<${package_url}|${package_name} ${package_version}>")
+done
+
+echo "Writing template..."
+jq -n --arg header ":rocket: Packages successfully published to npm!"\
+    --arg packages "*Packages*: $PACKAGES"\
+    -f .circleci/slack-templates/publish-packages-template.jq\
+  > /tmp/publish-packages-template.json
