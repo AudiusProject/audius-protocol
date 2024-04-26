011bf861ecdb894828c49c04cf8d8130a106e02a
Steve Perkins <stereosteve@users.noreply.github.com>
2024-04-26 11:38:21 -0400

    move ddex scratchy code to ddex/processor

---
+++ packages/ddex/processor/src/parseDelivery.ts
@@ -0,0 +1,453 @@
+import { Genre } from '@audius/sdk'
+import * as cheerio from 'cheerio'
+import decompress from 'decompress'
+import { mkdtemp, readFile, readdir } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { releaseRepo, userRepo, xmlRepo } from './db'
+
+type CH = cheerio.Cheerio<cheerio.Element>
+
+export type DDEXRelease = {
+  ref: string
+  isrc?: string
+  icpn?: string
+  title: string
+  subTitle?: string
+  artists: string[]
+  genre: string
+  subGenre: string
+  releaseDate: string
+  releaseType: string
+
+  isMainRelease: boolean
+  audiusGenre?: Genre
+  audiusUser?: string
+
+  problems: string[]
+  soundRecordings: DDEXSoundRecording[]
+  images: DDEXImage[]
+  deal?: AudiusSupportedDeal
+} & ReleaseAndSoundRecordingSharedFields
+
+type ReleaseAndSoundRecordingSharedFields = {
+  copyrightLine?: CopyrightPair
+  producerCopyrightLine?: CopyrightPair
+  parentalWarningType?: string
+}
+
+type CopyrightPair = {
+  text: string
+  year: string
+}
+
+export type DDEXSoundRecording = {
+  ref: string
+  isrc?: string
+  filePath: string
+  fileName: string
+  title: string
+  artists: string[]
+  releaseDate: string
+  genre: string
+  subGenre: string
+  rightsController?: {
+    name: string
+    roles: string[]
+    // rightShareUnknown: string
+  }
+
+  audiusGenre?: Genre
+} & ReleaseAndSoundRecordingSharedFields
+
+export type DDEXImage = {
+  ref: string
+  filePath: string
+  fileName: string
+}
+
+type DealFields = {
+  validityStartDate: string
+  isDownloadable: boolean
+}
+
+export type DealFree = DealFields & {
+  audiusDealType: 'Free'
+}
+
+export type DealPayGated = DealFields & {
+  audiusDealType: 'PayGated'
+  priceUsd: number
+}
+
+export type DealFollowGated = DealFields & {
+  audiusDealType: 'FollowGated'
+}
+
+export type DealTipGated = DealFields & {
+  audiusDealType: 'TipGated'
+}
+
+export type DealEthGated = DealFields & {
+  audiusDealType: 'NFTGated'
+  chain: 'eth'
+  address: string
+  standard?: string
+  name: string
+  slug?: string
+  imageUrl?: string
+  externalLink?: string
+}
+
+export type DealSolGated = DealFields & {
+  audiusDealType: 'NFTGated'
+  chain: 'sol'
+  address: string
+  name: string
+  imageUrl?: string
+  externalLink?: string
+}
+
+export type AudiusSupportedDeal =
+  | DealFree
+  | DealPayGated
+  | DealFollowGated
+  | DealTipGated
+  | DealEthGated
+  | DealSolGated
+
+export async function parseDelivery(maybeZip: string) {
+  if (maybeZip.endsWith('.zip')) {
+    const tempDir = await mkdtemp(join(tmpdir(), 'ddex-'))
+    await decompress(maybeZip, tempDir)
+    return await processDeliveryDir(tempDir)
+    // await rm(tempDir, { recursive: true })
+  } else if (maybeZip.endsWith('.xml')) {
+    return await parseDdexXmlFile(maybeZip)
+  } else {
+    return await processDeliveryDir(maybeZip)
+  }
+}
+
+export async function reParsePastXml() {
+  // loop over db xml and reprocess
+  const rows = xmlRepo.all()
+  for (const row of rows) {
+    await parseDdexXml(row.xmlUrl, row.xmlText)
+  }
+}
+
+// recursively find + parse xml files in a dir
+async function processDeliveryDir(dir: string) {
+  const files = await readdir(dir, { recursive: true })
+
+  const work = files
+    .filter((f) => f.toLowerCase().endsWith('.xml'))
+    .map((f) => join(dir, f))
+    .map((f) => parseDdexXmlFile(f))
+
+  return Promise.all(work)
+}
+
+// read xml from disk + parse
+export async function parseDdexXmlFile(xmlUrl: string) {
+  const xmlText = await readFile(xmlUrl, 'utf8')
+  return parseDdexXml(xmlUrl, xmlText)
+}
+
+// actually parse ddex xml
+export async function parseDdexXml(xmlUrl: string, xmlText: string) {
+  // todo: would be nice to skip this on reParse
+  xmlRepo.upsert(xmlUrl, xmlText)
+
+  const $ = cheerio.load(xmlText, { xmlMode: true })
+
+  function toTexts($doc: CH) {
+    return $doc.map((_, el) => $(el).text()).get()
+  }
+
+  function toText($el: CH) {
+    return $el.first().text().trim()
+  }
+
+  function cline($el: CH) {
+    const year = toText($el.find('CLine > Year'))
+    const text = toText($el.find('CLine > CLineText'))
+    if (year && text) return { year, text }
+  }
+
+  function pline($el: CH) {
+    const year = toText($el.find('PLine > Year'))
+    const text = toText($el.find('PLine > PLineText'))
+    if (year && text) return { year, text }
+  }
+
+  // function
+
+  //
+  // parse deals
+  //
+  const releaseDeals: Record<string, AudiusSupportedDeal> = {}
+  $('ReleaseDeal').each((_, el) => {
+    const $el = $(el)
+    const ref = $el.find('DealReleaseReference').text()
+    $el.find('DealTerms').each((_, el) => {
+      const $el = $(el)
+
+      const cmt = $el.find('CommercialModelType')
+      const commercialModelType = cmt.attr('UserDefinedValue') || cmt.text()
+      const usageTypes = toTexts($el.find('UseType'))
+      const territoryCode = toTexts($el.find('TerritoryCode'))
+
+      // only consider Worldwide
+      const isWorldwide = territoryCode.includes('Worldwide')
+      if (!isWorldwide) {
+        return
+      }
+
+      const common: DealFields = {
+        isDownloadable: usageTypes.includes('PermanentDownload'),
+        validityStartDate: $el.find('ValidityPeriod > StartDate').text(),
+      }
+
+      if (commercialModelType == 'FreeOfChargeModel') {
+        releaseDeals[ref] = {
+          ...common,
+          audiusDealType: 'Free',
+        }
+      } else if (commercialModelType == 'PayAsYouGoModel') {
+        const priceUsd = parseFloat(
+          $el.find('WholesalePricePerUnit[CurrencyCode="USD"]').text()
+        )
+        if (priceUsd) {
+          releaseDeals[ref] = {
+            ...common,
+            audiusDealType: 'PayGated',
+            priceUsd,
+          }
+        }
+      } else if (
+        commercialModelType == 'FollowGated' ||
+        commercialModelType == 'TipGated'
+      ) {
+        releaseDeals[ref] = {
+          ...common,
+          audiusDealType: commercialModelType,
+        }
+      } else if (commercialModelType == 'NFTGated') {
+        const chain = $el.find('Chain').text()
+        const address = $el.find('Address').text()
+        const name = $el.find('Name').text()
+        const imageUrl = $el.find('ImageUrl').text()
+        const externalLink = $el.find('ExternalLink').text()
+
+        // eth specific
+        const standard = $el.find('Standard').text()
+        const slug = $el.find('Slug').text()
+
+        switch (chain) {
+          case 'eth':
+            releaseDeals[ref] = {
+              ...common,
+              audiusDealType: 'NFTGated',
+              chain,
+              address,
+              name,
+              imageUrl,
+              externalLink,
+              standard,
+              slug,
+            }
+            break
+          case 'sol':
+            releaseDeals[ref] = {
+              ...common,
+              audiusDealType: 'NFTGated',
+              chain,
+              address,
+              name,
+              imageUrl,
+              externalLink,
+            }
+            break
+        }
+      }
+    })
+  })
+
+  //
+  // parse resources
+  //
+
+  const soundResources: Record<string, DDEXSoundRecording> = {}
+  const imageResources: Record<string, DDEXImage> = {}
+
+  $('SoundRecording').each((_, el) => {
+    const $el = $(el)
+
+    const recording: DDEXSoundRecording = {
+      ref: $el.find('ResourceReference').text(),
+      isrc: $el.find('ISRC').text(),
+
+      filePath: $el.find('FilePath').text(),
+      fileName: $el.find('FileName').text(),
+      title: $el.find('TitleText:first').text(),
+      artists: toTexts($el.find('DisplayArtist PartyName FullName')),
+      genre: $el.find('GenreText').text(),
+      subGenre: $el.find('SubGenre').text(),
+      releaseDate: $el
+        .find('OriginalResourceReleaseDate, ResourceReleaseDate')
+        .first()
+        .text(),
+
+      copyrightLine: cline($el),
+      producerCopyrightLine: pline($el),
+      parentalWarningType: toText($el.find('ParentalWarningType')),
+    }
+
+    const rightsController = $el.find('RightsController').first()
+    if (rightsController.length) {
+      recording.rightsController = {
+        name: toText(rightsController.find('PartyName FullName')),
+        roles: toTexts(rightsController.find('RightsControllerRole')),
+        // rightShareUnknown: toText(rightsController.find('RightShareUnknown'))
+      }
+    }
+
+    recording.audiusGenre = resolveAudiusGenre(
+      recording.subGenre,
+      recording.genre
+    )
+
+    soundResources[recording.ref] = recording
+  })
+
+  $('Image').each((_, el) => {
+    const $el = $(el)
+    const ref = $el.find('ResourceReference').text()
+
+    imageResources[ref] = {
+      ref,
+      filePath: $el.find('FilePath').text(),
+      fileName: $el.find('FileName').text(),
+    }
+  })
+
+  //
+  // parse releases
+  //
+
+  const releases = $('Release')
+    .toArray()
+    .map((el) => {
+      const $el = $(el)
+
+      const ref = $el.find('ReleaseReference').text()
+      const deal = releaseDeals[ref]
+
+      const releaseDate =
+        deal?.validityStartDate ||
+        $el.find('ReleaseDate').text() ||
+        $el.find('GlobalOriginalReleaseDate').text()
+
+      const release: DDEXRelease = {
+        ref,
+        isrc: $el.find('ISRC').text(),
+        icpn: $el.find('ICPN').text(),
+        title: $el.find('ReferenceTitle TitleText').text(),
+        subTitle: $el.find('ReferenceTitle SubTitle').text(),
+        artists: toTexts($el.find('DisplayArtist PartyName FullName')),
+        genre: $el.find('GenreText').text(),
+        subGenre: $el.find('SubGenre').text(),
+        releaseDate,
+        releaseType: $el.find('ReleaseType').text(),
+
+        copyrightLine: cline($el),
+        producerCopyrightLine: pline($el),
+        parentalWarningType: toText($el.find('ParentalWarningType')),
+
+        isMainRelease: $el.attr('IsMainRelease') == 'true',
+
+        problems: [],
+        soundRecordings: [],
+        images: [],
+        deal,
+      }
+
+      // resolve audius genre
+      release.audiusGenre = resolveAudiusGenre(release.subGenre, release.genre)
+      if (!release.audiusGenre) {
+        release.problems.push(`NoGenre`)
+      }
+
+      // resolve audius user
+      release.audiusUser = userRepo.match(release.artists)
+      if (!release.audiusUser) {
+        release.problems.push(`NoUser`)
+      }
+
+      // resolve resources
+      $el
+        .find('ReleaseResourceReferenceList > ReleaseResourceReference')
+        .each((_, el) => {
+          const ref = $(el).text()
+
+          if (soundResources[ref]) {
+            release.soundRecordings.push(soundResources[ref])
+          } else if (imageResources[ref]) {
+            release.images.push(imageResources[ref])
+          } else {
+            release.problems.push(`MissingRef: ${ref}`)
+          }
+        })
+
+      return release
+    })
+
+  // resolve any missing images to main release if possible
+  const mainRelease = releases.find((r) => r.isMainRelease)
+  for (const release of releases) {
+    if (!release.images.length) {
+      if (mainRelease?.images.length) {
+        release.images = mainRelease.images
+      } else {
+        release.problems.push('NoImage')
+      }
+    }
+  }
+
+  // create or replace this release in db
+  for (const release of releases) {
+    releaseRepo.upsert(xmlUrl, release)
+  }
+
+  return releases
+}
+
+//
+// helpers
+//
+
+function resolveAudiusGenre(
+  genre: string,
+  subgenre: string
+): Genre | undefined {
+  // first try subgenre, then genre
+  for (let searchTerm of [subgenre, genre]) {
+    searchTerm = searchTerm.toLowerCase()
+    for (const v of Object.values(Genre)) {
+      if (v.toLowerCase() == searchTerm) {
+        return v
+      }
+    }
+  }
+
+  // hard code some genre matching??
+  if (subgenre == 'Dance') {
+    return Genre.ELECTRONIC
+  }
+
+  // maybe try some edit distance magic?
+  // for now just log
+  console.warn(`failed to resolve genre: subgenre=${subgenre} genre=${genre}`)
+}
---
+++ packages/ddex/processor/src/server.ts
@@ -0,0 +1,449 @@
+import 'dotenv/config'
+
+import { serve } from '@hono/node-server'
+import { Context, Hono } from 'hono'
+import { deleteCookie, getSignedCookie, setSignedCookie } from 'hono/cookie'
+import { html } from 'hono/html'
+import { decode } from 'hono/jwt'
+import { prettyJSON } from 'hono/pretty-json'
+import { HtmlEscapedString } from 'hono/utils/html'
+import { dbUpsert, releaseRepo, userRepo, xmlRepo } from './db'
+import { reParsePastXml } from './parseDelivery'
+import { readAssetWithCaching } from './publishRelease'
+import { fileTypeFromBuffer } from 'file-type'
+
+const { NODE_ENV, DDEX_KEY, COOKIE_SECRET } = process.env
+const COOKIE_NAME = 'audiusUser'
+
+const API_HOST =
+  NODE_ENV == 'production'
+    ? 'https://discoveryprovider2.audius.co'
+    : 'https://discoveryprovider2.staging.audius.co'
+
+const app = new Hono()
+app.use(prettyJSON({ space: 4 }))
+
+app.get('/', async (c) => {
+  const me = await getAudiusUser(c)
+
+  return c.html(
+    Layout(html`
+      <div class="container">
+        <h1>ddexer</h1>
+
+        ${c.req.query('loginRequired')
+          ? html`<mark>Please login to continue</mark><br />`
+          : ''}
+        ${me
+          ? html`
+              <h4>Welcome back ${me.name}</h4>
+              <a href="/auth/logout" role="button">log out</a>
+            `
+          : html` <a role="button" href="/auth">login</a> `}
+      </div>
+    `)
+  )
+})
+
+app.get('/auth', (c) => {
+  if (!DDEX_KEY) {
+    return c.text('DDEX_KEY is required', 500)
+  }
+  const base = 'https://staging.audius.co/oauth/auth?'
+  const params = new URLSearchParams({
+    scope: 'write',
+    redirect_uri: 'http://localhost:8989/auth/success',
+    api_key: DDEX_KEY!,
+    rseponse_mode: 'query',
+  })
+  const u = base + params.toString()
+  return c.redirect(u)
+})
+
+app.get('/auth/redirect', async (c) => {
+  try {
+    const uri = c.req.query('redirect_uri')
+    const token = c.req.query('token')
+    if (!token) {
+      throw new Error('no token')
+    }
+
+    const { payload } = decode(token!)
+    if (!payload.userId) {
+      throw new Error('invalid payload')
+    }
+
+    // upsert user record
+    dbUpsert('users', {
+      id: payload.userId,
+      handle: payload.handle,
+      name: payload.name,
+    })
+
+    const params = new URLSearchParams({ token })
+    return c.redirect(`${uri}/?` + params.toString())
+  } catch (e) {
+    console.log(e)
+    return c.body('invalid jwt', 400)
+  }
+})
+
+app.get('/auth/success', async (c) => {
+  try {
+    const token = c.req.query('token')
+    if (!token) {
+      throw new Error('no token')
+    }
+
+    const { payload } = decode(token!)
+    if (!payload.userId) {
+      throw new Error('invalid payload')
+    }
+
+    // upsert user record
+    dbUpsert('users', {
+      id: payload.userId,
+      handle: payload.handle,
+      name: payload.name,
+    })
+
+    const j = JSON.stringify(payload)
+    await setSignedCookie(c, COOKIE_NAME, j, COOKIE_SECRET!)
+    return c.redirect('/')
+  } catch (e) {
+    console.log(e)
+    return c.body('invalid jwt', 400)
+  }
+})
+
+// ====================== AUTH REQUIRED ======================
+
+app.use('*', async (c, next) => {
+  const me = await getAudiusUser(c)
+  if (!me) return c.redirect('/?loginRequired=true')
+  await next()
+})
+
+app.get('/auth/whoami', async (c) => {
+  const me = await getAudiusUser(c)
+  return c.json({ me })
+})
+
+app.get('/auth/logout', async (c) => {
+  deleteCookie(c, COOKIE_NAME)
+  return c.redirect('/')
+})
+
+app.get('/releases', (c) => {
+  const rows = releaseRepo.all()
+
+  let lastXmlUrl = ''
+  const xmlSpacer = (xmlUrl: string) => {
+    if (xmlUrl != lastXmlUrl) {
+      lastXmlUrl = xmlUrl
+      return html`<tr>
+        <td colspan="10" style="background: black">
+          <kbd>${xmlUrl}</kbd>
+        </td>
+      </tr>`
+    }
+  }
+
+  return c.html(
+    Layout(html`
+      <h1>Releases</h1>
+
+      <form method="POST" action="/releases/reparse">
+        <button>rematch</button>
+      </form>
+
+      <table class="striped">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Key</th>
+            <th>Release Type</th>
+            <th>Is Main</th>
+            <th>Audius User</th>
+            <th>Audius Genre</th>
+            <th>Problems</th>
+            <th>Publish Errors</th>
+            <th>Published?</th>
+            <th>debug</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${rows.map(
+            (row) =>
+              html` ${xmlSpacer(row.xmlUrl!)}
+                <tr>
+                  <td>${row._parsed?.ref}</td>
+                  <td>
+                    <a href="/releases/${encodeURIComponent(row.key)}"
+                      >${row.key}</a
+                    >
+                  </td>
+                  <td>${row._parsed?.releaseType}</td>
+                  <td>${row._parsed?.isMainRelease ? 'Yes' : ''}</td>
+                  <td>${row._parsed?.audiusUser}</td>
+                  <td>${row._parsed?.audiusGenre}</td>
+                  <td>
+                    <mark><b>${row.status}</b></mark>
+                    ${row._parsed?.problems?.map(
+                      (p) => html`<mark>${p}</mark>`
+                    )}
+                  </td>
+                  <td>
+                    ${row.publishErrorCount > 0 &&
+                    html`<a
+                      href="/releases/${encodeURIComponent(row.key)}/error"
+                      >${row.publishErrorCount}</a
+                    >`}
+                  </td>
+                  <td>
+                    ${row.entityType == 'track' &&
+                    html` <a href="${API_HOST}/v1/full/tracks/${row.entityId}">
+                      ${row.entityId}
+                    </a>`}
+                    ${row.entityType == 'album' &&
+                    html` <a
+                      href="${API_HOST}/v1/full/playlists/${row.entityId}"
+                    >
+                      ${row.entityId}
+                    </a>`}
+                  </td>
+                  <td>
+                    <a href="/xmls/${encodeURIComponent(row.xmlUrl)}">xml</a>
+                    <a
+                      href="/releases/${encodeURIComponent(
+                        row.key
+                      )}/json?pretty"
+                      >json</a
+                    >
+                  </td>
+                </tr>`
+          )}
+        </tbody>
+      </table>
+    `)
+  )
+})
+
+app.post('/releases/reparse', async (c) => {
+  await reParsePastXml()
+  return c.redirect('/releases')
+})
+
+app.get('/releases/:key', (c) => {
+  const row = releaseRepo.get(c.req.param('key'))
+  if (!row) return c.json({ error: 'not found' }, 404)
+  if (c.req.query('json') != undefined) {
+    return c.json(row)
+  }
+
+  const parsedRelease = row._parsed!
+  return c.html(
+    Layout(html`
+      <div style="display: flex; gap: 20px">
+        <img
+          src="/release/${row.key}/images/${parsedRelease.images[0].ref}"
+          style="width: 200px; height: 200px"
+        />
+
+        <div style="flex-grow: 1">
+          <h3>${parsedRelease.title}</h3>
+          <h4>${parsedRelease.artists.join(', ')}</h4>
+
+          ${parsedRelease.soundRecordings.map(
+            (sr) => html`
+              <article>
+                <button
+                  class="outline contrast"
+                  style="margin-right: 8px"
+                  onClick="play('/release/${row.key}/soundRecordings/${sr.ref}')"
+                >
+                  play
+                </button>
+                <a href="/release/${row.key}/soundRecordings/${sr.ref}">
+                  <b>${sr.title}</b>
+                </a>
+                by ${sr.artists.join(', ')}
+              </article>
+            `
+          )}
+
+          <audio id="playa" controls />
+        </div>
+      </div>
+      <script>
+        function play(url) {
+          playa.onloadstart = () => {
+            console.log('loading...')
+          }
+          playa.oncanplay = () => {
+            console.log('OK')
+          }
+          if (playa.src.includes(url)) {
+            playa.paused ? playa.play() : playa.pause()
+          } else {
+            playa.src = url
+            playa.play()
+          }
+        }
+      </script>
+    `)
+  )
+})
+
+app.get('/release/:key/:type/:ref/:name?', async (c) => {
+  const key = c.req.param('key')!
+  const type = c.req.param('type')
+  const ref = c.req.param('ref')
+  const row = releaseRepo.get(key)
+  if (!row) return c.json({ error: 'not found' }, 404)
+
+  const collection =
+    type == 'images' ? row._parsed?.images : row._parsed?.soundRecordings
+  const asset = collection!.find((i) => i.ref == ref)
+  if (!asset) return c.json({ error: 'not found' }, 404)
+
+  const ok = await readAssetWithCaching(
+    row.xmlUrl,
+    asset.filePath,
+    asset.fileName
+  )
+
+  // some mime stuff
+  if (asset.fileName.endsWith('flac')) {
+    c.header('Content-Type', 'audio/flac')
+  } else {
+    const ft = await fileTypeFromBuffer(ok.buffer)
+    if (ft) {
+      c.header('Content-Type', ft.mime)
+    }
+  }
+  return c.body(ok.buffer)
+})
+
+app.get('/xmls/:xmlUrl', (c) => {
+  const row = xmlRepo.get(c.req.param('xmlUrl'))
+  if (!row) return c.json({ error: 'not found' }, 404)
+  c.header('Content-Type', 'text/xml')
+  return c.body(row.xmlText)
+})
+
+app.get('/releases/:key/json', (c) => {
+  const row = releaseRepo.get(c.req.param('key'))
+  if (!row) return c.json({ error: 'not found' }, 404)
+  c.header('Content-Type', 'application/json')
+  return c.body(row?.json)
+})
+
+app.get('/releases/:key/error', (c) => {
+  const row = releaseRepo.get(c.req.param('key'))
+  if (!row) return c.json({ error: 'not found' }, 404)
+  return c.text(row.lastPublishError)
+})
+
+app.get('/users', (c) => {
+  const users = userRepo.all()
+  return c.html(
+    Layout(
+      html`<h1>Users</h1>
+
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>handle</th>
+              <th>name</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${users.map(
+              (user) =>
+                html`<tr>
+                  <td>${user.id}</td>
+                  <td>${user.handle}</td>
+                  <td>${user.name}</td>
+                </tr>`
+            )}
+          </tbody>
+        </table> `
+    )
+  )
+})
+
+type JwtUser = {
+  userId: string
+  email: string
+  name: string
+  handle: string
+  verified: boolean
+  profilePicture: {
+    '150x150': string
+    '480x480': string
+    '1000x1000': string
+  }
+}
+
+async function getAudiusUser(c: Context) {
+  const j = await getSignedCookie(c, COOKIE_SECRET!, COOKIE_NAME)
+  if (!j) return
+  return JSON.parse(j) as JwtUser
+}
+
+function Layout(inner: HtmlEscapedString | Promise<HtmlEscapedString>) {
+  return html`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>ddex</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+        />
+        <style>
+          :root {
+            --pico-font-size: 16px;
+            --pico-line-height: 1.3;
+            // --pico-border-radius: 1rem;
+            // --pico-spacing: 0.5rem;
+            // --pico-form-element-spacing-vertical: 0.5rem;
+          }
+          h1 {
+            --pico-typography-spacing-vertical: 0.5rem;
+          }
+          button {
+            --pico-font-weight: 700;
+          }
+          mark {
+            margin-right: 1rem;
+          }
+        </style>
+      </head>
+      <body>
+        <div style="display: flex; gap: 15px; padding: 10px;">
+          <b>ddex</b>
+          <a href="/">home</a>
+          <a href="/auth/whoami">whoami</a>
+          <a href="/releases">releases</a>
+          <a href="/users">users</a>
+        </div>
+        <div style="padding: 50px;">${inner}</div>
+      </body>
+    </html>
+  `
+}
+
+export function startServer() {
+  const port = 8989
+  console.log(`Server is running on port ${port}`)
+
+  serve({
+    fetch: app.fetch,
+    port,
+  })
+}
