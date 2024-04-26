e15388fa0b03a1d99145f2f7f331f162fc16b53a
Raymond Jacobson <ray@audius.co>
2024-04-26 15:28:30 -0700

    Update deploy instructions

--- packages/ddex/entrypoint/src/components/DistributorCard.tsx
+++ packages/ddex/entrypoint/src/components/DistributorCard.tsx
@@ -92,6 +92,7 @@ export const DistributorCard = ({
               variant='body'
               size='s'
               color='default'
+              textAlign='center'
             >
               {data?.name ?? ''}
             </Text>
