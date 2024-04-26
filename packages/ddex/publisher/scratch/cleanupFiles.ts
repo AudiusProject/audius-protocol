0690827469c0b4fab210d43fbe2352f02536ed10
Steve Perkins <stereosteve@users.noreply.github.com>
2024-04-26 11:53:39 -0400

    restore ddex/publisher part 2

--- packages/ddex/publisher/scratch/cleanupFiles.ts
+++
@@ -1,9 +0,0 @@
-import { rm } from 'fs/promises'
-
-// s3 cache is now in /tmp/ddex_cache
-// this will nuke that whole folder
-// in future should make it delete items older than 7 days or something
-// to make retries cheaper
-export async function cleanupFiles() {
-  return rm('/tmp/ddex_cache', { recursive: true })
-}
