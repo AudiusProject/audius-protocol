c3857164c709072180da1d6677d9ab7a0fdda3bc
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-05-02 15:13:12 -0700

    fix gen script

--- packages/libs/src/sdk/api/generator/gen.js
+++ packages/libs/src/sdk/api/generator/gen.js
@@ -22,7 +22,7 @@ const GENERATED_DIR = 'src/sdk/api/generated'
 
 const spawnOpenAPIGenerator = async (openApiGeneratorArgs) => {
   console.info('Running OpenAPI Generator:')
-  const fullCmd = `docker run --add-host=audius-protocol-discovery-provider-1:host-gateway --rm -v "${
+  const fullCmd = `docker run --add-host=audius-protocol-discovery-provider-1:host-gateway --user $(id -u):$(id -g) --rm -v "${
     process.env.PWD
   }:/local" openapitools/openapi-generator-cli ${openApiGeneratorArgs.join(
     ' '
