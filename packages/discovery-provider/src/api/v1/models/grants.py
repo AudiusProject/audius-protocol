9ba23eceaf5edd17661075594f29232356ca0427
Raymond Jacobson <ray@audius.co>
2024-05-02 18:03:28 -0700

    Reapply "[PAY-2761] Add managed users endpoints (#8238)"
    This reverts commit aa9e735d910508c06706151413c464f3c64a0878.

---
+++ packages/discovery-provider/src/api/v1/models/grants.py
@@ -0,0 +1,33 @@
+from flask_restx import fields
+
+from .common import ns
+from .users import user_model_full
+
+# Describes a grant made from user to another user
+grant = ns.model(
+    "grant",
+    {
+        "grantee_address": fields.String(required=True),
+        "user_id": fields.String(required=True),
+        "is_revoked": fields.Boolean(required=True),
+        "is_approved": fields.Boolean(required=True),
+        "created_at": fields.String(required=True),
+        "updated_at": fields.String(required=True),
+    },
+)
+
+managed_user = ns.model(
+    "managed_user",
+    {
+        "user": fields.Nested(user_model_full, required=True),
+        "grant": fields.Nested(grant, required=True),
+    },
+)
+
+user_manager = ns.model(
+    "user_manager",
+    {
+        "manager": fields.Nested(user_model_full, required=True),
+        "grant": fields.Nested(grant, required=True),
+    },
+)
