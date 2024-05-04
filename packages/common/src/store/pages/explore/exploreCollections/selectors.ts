f9b96495440253ccd9896be46c13526cc018aec2
Dylan Jeffers <dylan@audius.co>
2024-05-03 18:07:04 -0700

    Fix issue

--- packages/common/src/store/pages/explore/exploreCollections/selectors.ts
+++ packages/common/src/store/pages/explore/exploreCollections/selectors.ts
@@ -1,6 +1,8 @@
+import { getCollections as getCachedCollections } from '~/store/cache/collections/selectors'
+import { getUsers } from '~/store/cache/users/selectors'
 import { CommonState } from '~/store/commonStore'
 
-import { Status } from '../../../../models'
+import { Collection, Status } from '../../../../models'
 import { ExploreCollectionsVariant } from '../types'
 
 const getBaseState = (state: CommonState) => state.pages.exploreCollections
@@ -17,3 +19,24 @@ export const getCollectionIds = (
   state: CommonState,
   { variant }: { variant: ExploreCollectionsVariant }
 ) => getBaseState(state)[variant]?.collectionIds ?? []
+
+export const getCollections = (
+  state: CommonState,
+  { variant }: { variant: ExploreCollectionsVariant }
+) => {
+  const baseState = getBaseState(state)[variant]
+  const collectionIds = baseState ? baseState.collectionIds : []
+  const collections = getCachedCollections(state, { ids: collectionIds })
+
+  const collectionsList = collectionIds.map((id) => collections[id])
+
+  const userIds = collectionsList.map((c: Collection) => c.playlist_owner_id)
+  const users = getUsers(state, { ids: userIds })
+
+  const userCollections = collectionsList.map((c: Collection) => ({
+    ...c,
+    user: users[c.playlist_owner_id]
+  }))
+
+  return userCollections.filter((playlist) => !playlist.user.is_deactivated)
+}
