cdc426a87f3a53583babdded973b047ee6f75145
JD Francis <jd@audius.co>
2024-05-08 13:48:24 -0500

    Rework to use harmony

--- packages/mobile/src/components/lineup-tile/LineupTileAccessStatus.tsx
+++ packages/mobile/src/components/lineup-tile/LineupTileAccessStatus.tsx
@@ -9,18 +9,21 @@ import {
   gatedContentSelectors
 } from '@audius/common/store'
 import { formatPrice } from '@audius/common/utils'
-import { Access } from '@audius/sdk'
-import { TouchableOpacity, View } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { IconLock, IconLockUnlocked } from '@audius/harmony-native'
-import { Text } from 'app/components/core'
+import type { FlexProps } from '@audius/harmony-native'
+import {
+  Flex,
+  IconLock,
+  IconLockUnlocked,
+  useTheme,
+  Text
+} from '@audius/harmony-native'
 import LoadingSpinner from 'app/components/loading-spinner'
 import { useIsUSDCEnabled } from 'app/hooks/useIsUSDCEnabled'
 import { setVisibility } from 'app/store/drawers/slice'
-import { flexRowCentered, makeStyles } from 'app/styles'
-import { spacing } from 'app/styles/spacing'
-import { useColor } from 'app/utils/theme'
+import { makeStyles } from 'app/styles'
 
 const { getGatedContentStatusMap } = gatedContentSelectors
 const { setLockedContentId } = gatedContentActions
@@ -32,30 +35,9 @@ const messages = {
 }
 
 const useStyles = makeStyles(({ palette, spacing, typography }) => ({
-  root: {
-    ...flexRowCentered(),
-    paddingVertical: spacing(1),
-    paddingHorizontal: spacing(3),
-    backgroundColor: palette.accentBlue,
-    borderRadius: spacing(1),
-    gap: spacing(1)
-  },
-  text: {
-    fontFamily: typography.fontByWeight.bold,
-    fontSize: typography.fontSize.small,
-    color: palette.staticWhite
-  },
   loadingSpinner: {
     width: spacing(4),
     height: spacing(4)
-  },
-  usdcPurchase: {
-    backgroundColor: palette.specialLightGreen
-  },
-  unlocked: {
-    borderRadius: spacing(3),
-    paddingVertical: spacing(0.5),
-    paddingHorizontal: spacing(2.5)
   }
 }))
 
@@ -71,26 +53,27 @@ export const LineupTileAccessStatus = ({
   hasStreamAccess: boolean | undefined
 }) => {
   const styles = useStyles()
+  const { color } = useTheme()
   const dispatch = useDispatch()
   const isUSDCEnabled = useIsUSDCEnabled()
   const { onOpen: openPremiumContentPurchaseModal } =
     usePremiumContentPurchaseModal()
   const gatedTrackStatusMap = useSelector(getGatedContentStatusMap)
   const gatedTrackStatus = gatedTrackStatusMap[contentId]
-  const staticWhite = useColor('staticWhite')
   const isUSDCPurchase =
     isUSDCEnabled && isContentUSDCPurchaseGated(streamConditions)
   const isUnlocking = gatedTrackStatus === 'UNLOCKING'
 
   const handlePress = useCallback(() => {
-    if (isUSDCPurchase && !hasStreamAccess) {
+    if (hasStreamAccess) {
+      return
+    }
+    if (isUSDCPurchase) {
       openPremiumContentPurchaseModal(
         { contentId, contentType },
         { source: ModalSource.TrackTile }
       )
-      return
-    }
-    if (contentId) {
+    } else if (contentId) {
       dispatch(setLockedContentId({ id: contentId }))
       dispatch(setVisibility({ drawer: 'LockedContent', visible: true }))
     }
@@ -109,31 +92,50 @@ export const LineupTileAccessStatus = ({
     ? messages.unlocking
     : messages.locked
 
-  const showButtonText =
-    (isUSDCPurchase && !hasStreamAccess && !isUnlocking) || !isUSDCPurchase
+  const showButtonText = !isUSDCPurchase || (!hasStreamAccess && !isUnlocking)
+
+  const backgroundColor = isUSDCPurchase
+    ? color.special.lightGreen
+    : color.special.blue
+
+  const lockedStyles: FlexProps = {
+    borderRadius: 's',
+    ph: 'm',
+    pv: 'xs',
+    style: { backgroundColor }
+  }
+
+  const unlockedStyles: FlexProps = {
+    borderRadius: 'l',
+    ph: 's',
+    pv: '2xs',
+    style: { backgroundColor }
+  }
 
   return (
     <TouchableOpacity onPress={handlePress}>
-      <View
-        style={[
-          styles.root,
-          isUSDCPurchase ? styles.usdcPurchase : null,
-          hasStreamAccess ? styles.unlocked : null
-        ]}
+      <Flex
+        {...(hasStreamAccess || isUnlocking ? unlockedStyles : lockedStyles)}
+        direction='row'
+        alignItems='center'
+        gap='xs'
       >
         {hasStreamAccess ? (
-          <IconLockUnlocked
-            fill={staticWhite}
-            width={spacing(4)}
-            height={spacing(4)}
-          />
+          <IconLockUnlocked color='staticWhite' size='xs' />
         ) : isUnlocking ? (
-          <LoadingSpinner style={styles.loadingSpinner} fill={staticWhite} />
+          <LoadingSpinner
+            style={styles.loadingSpinner}
+            fill={color.icon.staticWhite}
+          />
         ) : (
-          <IconLock fill={staticWhite} width={spacing(4)} height={spacing(4)} />
+          <IconLock color='staticWhite' size='s' />
         )}
-        {showButtonText ? <Text style={styles.text}>{buttonText}</Text> : null}
-      </View>
+        {showButtonText ? (
+          <Text color='staticWhite' variant='label' size='m'>
+            {buttonText}
+          </Text>
+        ) : null}
+      </Flex>
     </TouchableOpacity>
   )
 }
