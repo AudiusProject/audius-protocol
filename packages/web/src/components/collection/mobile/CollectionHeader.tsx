16baea715b4c78868c3a344a07e53a9ad7865e2b
amendelsohn <andrew.j.mendelsohn@gmail.com>
2024-05-07 17:37:20 -0700

    include access check in dog ear calculation

--- packages/web/src/components/collection/mobile/CollectionHeader.tsx
+++ packages/web/src/components/collection/mobile/CollectionHeader.tsx
@@ -189,10 +189,11 @@ const CollectionHeader = ({
   }
 
   const renderDogEar = () => {
-    // Omitting isOwner and hasStreamAccess to ensure we always show gated DogEars
     const DogEarType = getDogEarType({
       isUnlisted,
-      streamConditions
+      streamConditions,
+      isOwner,
+      hasStreamAccess
     })
     if (!isLoading && DogEarType) {
       return (
