6870d55e8bcb7f9b7bc94899665867fdf1b4c96a
Alec Savvy <alecsavvy@outlook.com>
2024-05-09 13:20:52 -0600

    use ip lib

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/utils/ipData.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/utils/ipData.ts
@@ -1,6 +1,7 @@
 import { Logger } from "pino"
 import { Request } from "express"
 import axios from "axios"
+import { getClientIp } from "request-ip"
 import { config } from "../config"
 import { logger } from "../logger"
 
@@ -41,18 +42,12 @@ export const getRequestIpData = async (logger: Logger, req: Request): Promise<Lo
 // Prioritizes 'X-Forwarded-For' if it exists. Otherwise returns the request objects ip.
 // Throws if neither can be found.
 export const getIP = (req: Request): string => {
-    const ip = req.ip
-    const forwardedFor = req.get('X-Forwarded-For')
-
-    if (forwardedFor) {
-        return forwardedFor
+    const clientIP = getClientIp(req)
+    // https://github.com/pbojinov/request-ip?tab=readme-ov-file#how-it-works
+    if (clientIP === null) {
+        throw new Error("could not find client ip")
     }
-
-    if (!forwardedFor && ip) {
-        return ip
-    }
-
-    throw new Error("ip not found in 'X-Forwarded-For' header nor in req.ip")
+    return clientIP
 }
 
 // Utility to gather the IP from an incoming express request.
