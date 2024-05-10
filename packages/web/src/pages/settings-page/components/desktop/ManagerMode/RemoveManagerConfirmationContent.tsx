40f12e9185b994b3058449f27d992f947ace30d1
Nikki Kang <kangaroo233@gmail.com>
2024-05-09 22:49:08 -0400

    pr review

--- packages/web/src/pages/settings-page/components/desktop/ManagerMode/RemoveManagerConfirmationContent.tsx
+++ packages/web/src/pages/settings-page/components/desktop/ManagerMode/RemoveManagerConfirmationContent.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 
 import { useRemoveManager } from '@audius/common/api'
 import { Status } from '@audius/common/models'
