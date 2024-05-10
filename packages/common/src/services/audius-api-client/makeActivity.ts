70d4d946e5698ea971f78f10f219db6c6824b823
amendelsohn <andrew.j.mendelsohn@gmail.com>
2024-05-10 16:55:11 -0700

    Fix stream_conditions in saved and library fetch

--- packages/common/src/services/audius-api-client/makeActivity.ts
+++ packages/common/src/services/audius-api-client/makeActivity.ts
@@ -4,6 +4,7 @@ import { UserTrackMetadata, UserCollectionMetadata } from '~/models'
 
 import { makePlaylist, makeTrack } from './ResponseAdapter'
 import { APIActivity, APIActivityV2, isApiActivityV2 } from './types'
+import { pickBy } from 'lodash'
 
 export const makeActivity = (
   activity: APIActivity | APIActivityV2
@@ -13,7 +14,16 @@ export const makeActivity = (
       return undefined
     }
     if (activity.itemType === 'track') {
-      return makeTrack(full.TrackFullToJSON(activity.item as full.TrackFull))
+      // HACK to cover for sdk
+      // https://linear.app/audius/issue/PAY-2994/oneofmodel-breaking-premium-conditions-in-client
+      const picked = pickBy(activity.item.streamConditions, (value) => !!value)
+      const trackFull: full.TrackFull = {
+        ...activity.item,
+        // @ts-ignore
+        streamConditions: picked
+      }
+
+      return makeTrack(full.TrackFullToJSON(trackFull as full.TrackFull))
     } else if (activity.itemType === 'playlist') {
       return makePlaylist(
         full.PlaylistFullWithoutTracksToJSON(
