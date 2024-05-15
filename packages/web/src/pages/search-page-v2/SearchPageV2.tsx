d3f86cc881f146e8c0ff305bc9083ef3b0cffeb0
sliptype <sliptype@gmail.com>
2024-05-15 11:05:23 -0500

    Make categories clickable

--- packages/web/src/pages/search-page-v2/SearchPageV2.tsx
+++ packages/web/src/pages/search-page-v2/SearchPageV2.tsx
@@ -1,9 +1,7 @@
 import { Status } from '@audius/common/models'
-import { Nullable } from '@audius/common/utils'
+import { Maybe } from '@audius/common/utils'
 import Header from 'components/header/desktop/Header'
 import Page from 'components/page/Page'
-import { useSearchParams } from 'react-router-dom-v5-compat'
-import { fullSearchResultsPage } from 'utils/route'
 import {
   Flex,
   IconAlbum,
@@ -12,15 +10,14 @@ import {
   IconPlaylists,
   IconUser,
   LoadingSpinner,
+  RadioGroup,
   SelectablePill,
   Text
 } from '@audius/harmony'
 import { capitalize } from 'lodash'
-
-type SearchHeaderProps = {
-  title: string
-  query: Nullable<string>
-}
+import { useParams } from 'react-router-dom'
+import { useHistoryContext } from 'app/HistoryProvider'
+import { ChangeEvent, useCallback } from 'react'
 
 type Filter =
   | 'genre'
@@ -47,29 +44,61 @@ const categories = {
   playlists: { icon: IconPlaylists, filters: ['genre', 'mood'] }
 } satisfies Record<string, Category>
 
+type CategoryKey = keyof typeof categories
+
+type SearchHeaderProps = {
+  category?: CategoryKey
+  setCategory: (category: CategoryKey) => void
+  title: string
+  query: Maybe<string>
+}
+
 const SearchHeader = (props: SearchHeaderProps) => {
-  const secondary = (
-    <Flex ml='l'>
-      <Text variant='heading' strength='weak'>
-        &#8220;{props.query}&#8221;
-      </Text>
-    </Flex>
+  const { category: categoryKey = 'all', setCategory, query, title } = props
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value
+      setCategory(value as CategoryKey)
+    },
+    [setCategory]
   )
+
   return (
     <Header
       {...props}
-      primary={props.title}
-      secondary={secondary}
+      primary={title}
+      secondary={
+        query ? (
+          <Flex ml='l'>
+            <Text variant='heading' strength='weak'>
+              &#8220;{query}&#8221;
+            </Text>
+          </Flex>
+        ) : null
+      }
       rightDecorator={
-        <Flex gap='s' direction='row'>
+        <RadioGroup
+          direction='row'
+          gap='s'
+          aria-label={'Select search category'}
+          name='searchcategory'
+          value={categoryKey}
+          onChange={handleChange}
+        >
           {Object.entries(categories).map(([key, category]) => (
             <SelectablePill
+              aria-label={`${key} search category`}
+              icon={(category as Category).icon}
+              key={key}
               label={capitalize(key)}
               size='large'
-              icon={(category as Category).icon}
+              type='radio'
+              value={key}
+              checked={key === categoryKey}
             />
           ))}
-        </Flex>
+        </RadioGroup>
       }
       variant='main'
     />
@@ -77,10 +106,27 @@ const SearchHeader = (props: SearchHeaderProps) => {
 }
 
 export const SearchPageV2 = () => {
-  const [searchParams, setSearchParams] = useSearchParams()
-  const query = searchParams.get('q')
+  const { category, query } = useParams<{
+    category: CategoryKey
+    query: string
+  }>()
+  const { history } = useHistoryContext()
 
-  const header = <SearchHeader query={query} title={'Search'} />
+  const setCategory = useCallback(
+    (category: CategoryKey) => {
+      history.push(`/search/${query}/${category}`)
+    },
+    [history, query]
+  )
+
+  const header = (
+    <SearchHeader
+      query={query}
+      title={'Search'}
+      category={category as CategoryKey}
+      setCategory={setCategory}
+    />
+  )
 
   return (
     <Page
