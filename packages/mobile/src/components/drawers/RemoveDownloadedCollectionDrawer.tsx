e43be4522c843ef771c42b70a2ca49be5a189189
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-05-16 14:47:37 -0400

    [PAY-3039] Fix copy for removing offline download albums

--- packages/mobile/src/components/drawers/RemoveDownloadedCollectionDrawer.tsx
+++ packages/mobile/src/components/drawers/RemoveDownloadedCollectionDrawer.tsx
@@ -1,6 +1,7 @@
 import { useCallback } from 'react'
 
 import { cacheCollectionsSelectors } from '@audius/common/store'
+import { capitalize } from 'lodash'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { useDrawer } from 'app/hooks/useDrawer'
@@ -11,11 +12,10 @@ const { getCollection } = cacheCollectionsSelectors
 
 const messages = {
   header: 'Are You Sure?',
-  description: (isAlbum: boolean) =>
-    `Are you sure you want to remove this ${
-      isAlbum ? 'album' : 'playlist'
-    } from your device?`,
-  confirm: 'Remove Downloaded Playlist'
+  description: (contentType: string) =>
+    `Are you sure you want to remove this ${contentType} from your device?`,
+  confirm: (contentType: string) =>
+    `Remove Downloaded ${capitalize(contentType)}`
 }
 
 const drawerName = 'RemoveDownloadedCollection'
@@ -28,6 +28,7 @@ export const RemoveDownloadedCollectionDrawer = () => {
   const isAlbum = useSelector(
     (state) => !!getCollection(state, { id: collectionId })?.is_album
   )
+  const contentType = isAlbum ? 'album' : 'playlist'
 
   const handleConfirm = useCallback(() => {
     dispatch(requestRemoveDownloadedCollection({ collectionId }))
@@ -36,7 +37,11 @@ export const RemoveDownloadedCollectionDrawer = () => {
   return (
     <ConfirmationDrawer
       drawerName={drawerName}
-      messages={{ ...messages, description: messages.description(isAlbum) }}
+      messages={{
+        ...messages,
+        confirm: messages.confirm(contentType),
+        description: messages.description(contentType)
+      }}
       onConfirm={handleConfirm}
     />
   )
