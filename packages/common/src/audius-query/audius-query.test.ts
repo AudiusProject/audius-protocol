a5508b216e3367a1acdeb80fbde82872983c425f
sliptype <sliptype@gmail.com>
2024-05-22 16:27:27 -0500

    Add docs

--- packages/common/src/audius-query/audius-query.test.ts
+++ packages/common/src/audius-query/audius-query.test.ts
@@ -18,7 +18,7 @@ describe('audius-query', function () {
       reducerPath: 'testApi',
       endpoints: {
         getById: {
-          fetch: async ({ id }: { id: ID }, _: any) => {
+          fetch: async ({ id, myArg }: { id: ID; myArg?: string }, _: any) => {
             return { id }
           },
           fetchBatch: fetchBatchSpy,
@@ -35,17 +35,30 @@ describe('audius-query', function () {
     })
 
     it('batches requests when fetchBatch is specified', async function () {
-      const item1 = testApi.fetch.getById({ id: 1 }, mockContext)
-      const item2 = testApi.fetch.getById({ id: 2 }, mockContext)
+      const item1 = testApi.fetch.getById({ id: 1, myArg: 'test' }, mockContext)
+      const item2 = testApi.fetch.getById({ id: 2, myArg: 'test' }, mockContext)
 
       expect(await item1).toEqual({ track_id: 1 })
       expect(await item2).toEqual({ track_id: 2 })
       expect(fetchBatchSpy).toHaveBeenCalledTimes(1)
+      expect(fetchBatchSpy).toHaveBeenCalledWith(
+        { ids: [1, 2], myArg: 'test' },
+        mockContext
+      )
     })
 
     it('performs multiple batches when requests fall outside of the batch period', async function () {
       const item1 = testApi.fetch.getById({ id: 1 }, mockContext)
       await new Promise((resolve) => setTimeout(resolve, 100))
+      const item2 = testApi.fetch.getById({ id: 2 }, mockContext)
+
+      expect(await item1).toEqual({ track_id: 1 })
+      expect(await item2).toEqual({ track_id: 2 })
+      expect(fetchBatchSpy).toHaveBeenCalledTimes(2)
+    })
+
+    it("performs multiple batches when arguments don't match", async function () {
+      const item1 = testApi.fetch.getById({ id: 1, myArg: 'test' }, mockContext)
       const item2 = testApi.fetch.getById({ id: 2 }, mockContext)
 
       expect(await item1).toEqual({ track_id: 1 })
