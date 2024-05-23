276f714cf5bd025b04edc004954ba555fbd2003f
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-05-22 17:43:01 -0700

    [PAY-2912] Add purchase album support to SDK (#8593)

--- packages/commands/src/purchase-content.mjs
+++ packages/commands/src/purchase-content.mjs
@@ -117,3 +117,39 @@ program.command('purchase-track')
     }
     process.exit(0)
   })
+
+program.command('purchase-album')
+  .description('Buys an album using USDC')
+  .argument('<id>', 'The album ID')
+  .argument('<price>', 'The expected price of the album', parseFloat)
+  .option('-f, --from [from]', 'The account purchasing the content (handle)')
+  .option(
+    '-e, --extra-amount [amount]',
+    'Extra amount to pay in addition to the price (in dollars)'
+    , parseFloat)
+  .action(async (id, price, { from, extraAmount }) => {
+    const audiusLibs = await initializeAudiusLibs(from)
+    const userIdNumber = audiusLibs.userStateManager.getCurrentUserId()
+    const userId = Utils.encodeHashId(userIdNumber)
+    const albumId = Utils.encodeHashId(id)
+
+    // extract privkey and pubkey from hedgehog
+    // only works with accounts created via audius-cmd
+    const wallet = audiusLibs?.hedgehog?.getWallet()
+    const privKey = wallet?.getPrivateKeyString()
+    const pubKey = wallet?.getAddressString()
+
+    // init sdk with priv and pub keys as api keys and secret
+    // this enables writes via sdk
+    const audiusSdk = await initializeAudiusSdk({ apiKey: pubKey, apiSecret: privKey })
+
+    try {
+      console.log('Purchasing album...', { albumId, userId, price, extraAmount })
+      const response = await audiusSdk.albums.purchase({ albumId, userId, price, extraAmount })
+      console.log(chalk.green('Successfully purchased album'))
+      console.log(chalk.yellow('Transaction Signature:'), response)
+    } catch (err) {
+      program.error(err)
+    }
+    process.exit(0)
+  })
