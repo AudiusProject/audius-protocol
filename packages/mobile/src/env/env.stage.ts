92aed96002e677431015353c0c7a6d3d1f110af5
Isaac Solo <isaac@audius.co>
2024-05-24 07:28:43 -0700

    Change frontend RPC (#8612)

--- packages/mobile/src/env/env.stage.ts
+++ packages/mobile/src/env/env.stage.ts
@@ -69,7 +69,8 @@ export const env: Env = {
   SENTRY_DSN: 'https://4b15a7a2f2e2459997408b39a0c4942c@s.audius.co/1851611',
   SOL_BRIDGE_ADDRESS: null,
   SOL_TOKEN_BRIDGE_ADDRESS: null,
-  SOLANA_CLUSTER_ENDPOINT: 'https://audius-fe.rpcpool.com',
+  SOLANA_CLUSTER_ENDPOINT:
+    'https://solana-mainnet.g.alchemy.com/v2/v-pjHiJSTwzbZ5lAFHQosAHaJf4oi0zR',
   SOLANA_FEE_PAYER_ADDRESS: 'E3CfijtAJwBSHfwFEViAUd3xp7c8TBxwC1eXn1Fgxp8h',
   SOLANA_RELAY_ENDPOINT: 'https://discoveryprovider.staging.audius.co',
   SOLANA_TOKEN_PROGRAM_ADDRESS: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
