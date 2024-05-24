17030dba8240336a71544abfe9111725c312f78c
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-05-23 23:53:27 -0700

    Add user_payout_wallet_history_table

---
+++ packages/discovery-provider/src/models/users/user_payout_wallet_history.py
@@ -0,0 +1,21 @@
+from typing import Self
+
+from sqlalchemy import BigInteger, Column, DateTime, Integer, String, text
+
+from src.models.base import Base
+from src.models.model_utils import RepresentableMixin
+
+
+class UserPayoutWalletHistory(Base, RepresentableMixin):
+    __tablename__ = "user_payout_wallet_history"
+
+    user_id = Column(Integer, nullable=False, primary_key=True)
+    spl_usdc_payout_wallet = Column(String, nullable=False)
+    blocknumber = Column(BigInteger, nullable=False)
+    block_timestamp = Column(DateTime, nullable=False, primary_key=True)
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+
+    def equals(self, rec: Self):
+        return self.spl_usdc_payout_wallet == rec.spl_usdc_payout_wallet
