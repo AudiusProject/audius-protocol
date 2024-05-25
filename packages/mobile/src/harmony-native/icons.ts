7b192e5b788fa50813b378b8dc79813df21ca710
Raymond Jacobson <ray@audius.co>
2024-05-25 12:23:03 -0700

    Fix lint

--- packages/mobile/src/harmony-native/icons.ts
+++ packages/mobile/src/harmony-native/icons.ts
@@ -145,7 +145,7 @@ export { default as IconValidationX } from '@audius/harmony/src/assets/icons/Val
 export { default as IconSoundwave } from '@audius/harmony/src/assets/icons/Soundwave.svg'
 export { default as IconCreditCard } from '@audius/harmony/src/assets/icons/CreditCard.svg'
 export { default as IconWaveform } from '@audius/harmony/src/assets/icons/Waveform.svg'
-export { default as IconMoneyBracket } from '@audius/harmony/src/assets/icons/IconMoneyBracket.svg'
+export { default as IconMoneyBracket } from '@audius/harmony/src/assets/icons/MoneyBracket.svg'
 
 // Two Tone / Special Styling
 
