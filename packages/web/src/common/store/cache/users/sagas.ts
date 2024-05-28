cda55fba023b19ab8fcabf217780ee6460468e2b
Randy Schott <1815175+schottra@users.noreply.github.com>
2024-05-28 15:43:50 -0400

    fix an optional Id parse that breaks when logged out

--- packages/web/src/common/store/cache/users/sagas.ts
+++ packages/web/src/common/store/cache/users/sagas.ts
@@ -1,7 +1,7 @@
 import {
   DefaultSizes,
-  Id,
   Kind,
+  OptionalId,
   User,
   UserMetadata,
   userMetadataListFromSDK
@@ -71,7 +71,7 @@ function* retrieveUserByHandle(handle: string, retry: boolean) {
     [sdk.full.users, sdk.full.users.getUserByHandle],
     {
       handle,
-      userId: Id.parse(userId)
+      userId: OptionalId.parse(userId)
     }
   )
   return userMetadataListFromSDK(users)
