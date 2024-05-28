34642e5043ecc22abf214baca3dabcaa3303554b
Dylan Jeffers <dylan@audius.co>
2024-05-28 15:41:28 -0700

    Fix lint

--- packages/harmony/src/components/layout/Divider/types.ts
+++ packages/harmony/src/components/layout/Divider/types.ts
@@ -1,6 +1,5 @@
 import type { ReactNode } from 'react'
-
-import { BorderColors } from 'foundations'
+import { BorderColors } from '../../../foundations/color'
 
 export type DividerProps = {
   orientation?: 'horizontal' | 'vertical'
