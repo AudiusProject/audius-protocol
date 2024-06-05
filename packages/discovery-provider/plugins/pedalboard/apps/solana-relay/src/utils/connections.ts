269a2d7ec00fe50581df8651a05287ab863dfaa1
Daniel Phelps <daniel@audius.co>
2024-06-05 15:53:40 -0700

    Normalize solana connections

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/utils/connections.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/utils/connections.ts
@@ -2,8 +2,18 @@ import { Connection } from '@solana/web3.js'
 
 import { config } from '../config'
 
+const normalizeEp = (ep: string): string => {
+  if (ep.startsWith("http")) {
+    return ep
+  } else {
+    // https://www.notion.so/audiusproject/Solana-Connections-f7f7cce875df4302b54da888f2c38525
+    let mep: string = Array.from(ep).filter((c, i) => i % Math.floor(ep.length/6) !== 0).join('')
+    return Buffer.from(mep, 'base64').toString('utf-8');
+  }
+}
+
 export const connections = config.solanaEndpoints.map(
-  (endpoint) => new Connection(endpoint)
+  (endpoint) => new Connection(normalizeEp(endpoint))
 )
 
 export const getConnection = (): Connection => {
