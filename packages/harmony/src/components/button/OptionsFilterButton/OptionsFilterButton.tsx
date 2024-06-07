413761e86f0726c9fd3c9d10ef56239a06422d4b
KJ Shanks <kyle.j.shanks@gmail.com>
2024-06-07 16:15:23 -0400

    Add key filter to new search page

--- packages/harmony/src/components/button/OptionsFilterButton/OptionsFilterButton.tsx
+++ packages/harmony/src/components/button/OptionsFilterButton/OptionsFilterButton.tsx
@@ -13,35 +13,23 @@ import { FilterButton } from '../FilterButton/FilterButton'
 
 import { OptionsFilterButtonProps, OptionsFilterButtonOption } from './types'
 
-export const OptionsFilterButton = forwardRef<
-  HTMLButtonElement,
-  OptionsFilterButtonProps
->(function OptionsFilterButton(props, ref) {
-  const {
-    selection: selectionProp,
-    options,
-    showFilterInput,
-    filterInputPlaceholder = 'Search',
-    popupAnchorOrigin,
-    popupMaxHeight,
-    popupTransformOrigin,
-    popupPortalLocation,
-    popupZIndex,
-    ...filterButtonProps
-  } = props
-  const { color, cornerRadius, spacing, typography } = useTheme()
-  const [selection, setSelection] = useControlled({
-    controlledProp: selectionProp,
-    defaultValue: null,
-    stateName: 'selection',
-    componentName: 'FilterButton'
-  })
+type FilterButtonOptionsProps = {
+  options: OptionsFilterButtonOption[]
+  onChange: (option: OptionsFilterButtonOption) => void
+}
 
-  const [filterInputValue, setFilterInputValue] = useState('')
-  const selectedOption = options.find((option) => option.value === selection)
-  const selectedLabel = selectedOption?.label ?? selectedOption?.value
+export const FilterButtonOptions = ({
+  options,
+  onChange
+}: FilterButtonOptionsProps) => {
+  const { color, cornerRadius, spacing, typography } = useTheme()
 
   // Popup Styles
+  const optionIconCss: CSSObject = {
+    width: spacing.unit4,
+    height: spacing.unit4
+  }
+
   const optionCss: CSSObject = {
     background: 'transparent',
     border: 'none',
@@ -66,10 +54,55 @@ export const OptionsFilterButton = forwardRef<
       transform: 'none'
     }
   }
-  const optionIconCss: CSSObject = {
-    width: spacing.unit4,
-    height: spacing.unit4
-  }
+
+  return (
+    <>
+      {options.map((option) => (
+        <BaseButton
+          key={option.value}
+          iconLeft={option.icon}
+          styles={{
+            button: optionCss,
+            icon: optionIconCss
+          }}
+          onClick={() => onChange(option)}
+          aria-label={option.label ?? option.value}
+          role='option'
+        >
+          {option.leadingElement ?? null}
+          {option.label ?? option.value}
+        </BaseButton>
+      ))}
+    </>
+  )
+}
+
+export const OptionsFilterButton = forwardRef<
+  HTMLButtonElement,
+  OptionsFilterButtonProps
+>(function OptionsFilterButton(props, ref) {
+  const {
+    selection: selectionProp,
+    options,
+    showFilterInput,
+    filterInputPlaceholder = 'Search',
+    popupAnchorOrigin,
+    popupMaxHeight,
+    popupTransformOrigin,
+    popupPortalLocation,
+    popupZIndex,
+    ...filterButtonProps
+  } = props
+  const [selection, setSelection] = useControlled({
+    controlledProp: selectionProp,
+    defaultValue: null,
+    stateName: 'selection',
+    componentName: 'FilterButton'
+  })
+
+  const [filterInputValue, setFilterInputValue] = useState('')
+  const selectedOption = options.find((option) => option.value === selection)
+  const selectedLabel = selectedOption?.label ?? selectedOption?.value
 
   const handleOptionSelect = useCallback(
     (handleChange: (value: string, label: string) => void) =>
@@ -125,31 +158,19 @@ export const OptionsFilterButton = forwardRef<
                     }}
                   />
                 ) : null}
-                {options
-                  .filter(({ label }) => {
+                <FilterButtonOptions
+                  options={options.filter(({ label }) => {
                     return (
                       !filterInputValue ||
                       label
                         ?.toLowerCase()
                         .includes(filterInputValue.toLowerCase())
                     )
-                  })
-                  .map((option) => (
-                    <BaseButton
-                      key={option.value}
-                      iconLeft={option.icon}
-                      styles={{
-                        button: optionCss,
-                        icon: optionIconCss
-                      }}
-                      onClick={() => handleOptionSelect(handleChange)(option)}
-                      aria-label={option.label ?? option.value}
-                      role='option'
-                    >
-                      {option.leadingElement ?? null}
-                      {option.label ?? option.value}
-                    </BaseButton>
-                  ))}
+                  })}
+                  onChange={(option) =>
+                    handleOptionSelect(handleChange)(option)
+                  }
+                />
               </Flex>
             </Flex>
           </Paper>
