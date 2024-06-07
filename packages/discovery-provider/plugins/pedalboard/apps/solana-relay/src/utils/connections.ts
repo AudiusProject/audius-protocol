861345b5f29b62e5c76f1445275886a8dff1f9dd
Daniel Phelps <daniel@audius.co>
2024-06-07 09:44:27 -0700

    No doc

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/utils/connections.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/utils/connections.ts
@@ -6,7 +6,6 @@ const normalizeEp = (ep: string): string => {
   if (ep.startsWith("http")) {
     return ep
   } else {
-    // https://www.notion.so/audiusproject/Solana-Connections-f7f7cce875df4302b54da888f2c38525
     let mep: string = Array.from(ep).filter((c, i) => i % Math.floor(ep.length/6) !== 0).join('')
     return Buffer.from(mep, 'base64').toString('utf-8');
   }
