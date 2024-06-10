0d51c7bab28aacc7042dbe78ddc8690128d4627a
KJ Shanks <kyle.j.shanks@gmail.com>
2024-06-10 13:00:03 -0400

    Add NoResultsTile to new search page

---
+++ packages/web/src/pages/search-page-v2/NoResultsTile.tsx
@@ -0,0 +1,36 @@
+import { IconSearch, Paper, Text, useTheme } from '@audius/harmony'
+
+import { useMedia } from 'hooks/useMedia'
+const messages = {
+  noResults: 'No Results',
+  description:
+    'Apply filters or search to discover tracks, profile, playlists, and albums.'
+}
+
+export const NoResultsTile = () => {
+  const { color } = useTheme()
+  const { isMobile } = useMedia()
+  return (
+    <Paper
+      pv={isMobile ? '2xl' : '3xl'}
+      ph={isMobile ? 'l' : '3xl'}
+      mv='s'
+      mh='xl'
+      direction='column'
+      gap={isMobile ? 's' : 'l'}
+      alignItems='center'
+      w={isMobile ? 'auto' : '100%'}
+    >
+      <IconSearch size={isMobile ? 'l' : '2xl'} fill={color.icon.default} />
+      <Text
+        variant={isMobile ? 'title' : 'heading'}
+        size={isMobile ? 'l' : 'm'}
+      >
+        {messages.noResults}
+      </Text>
+      <Text variant='body' size={isMobile ? 'm' : 'l'}>
+        {messages.description}
+      </Text>
+    </Paper>
+  )
+}
