1a4121eed7536d93927fa8e87bdaf4a5ff7c5954
Dylan Jeffers <dylan@audius.co>
2024-06-10 12:36:07 -0700

    [QA-1342] Fix default SSR tags (#8719)

--- packages/web/src/ssr/index/+onRenderHtml.tsx
+++ packages/web/src/ssr/index/+onRenderHtml.tsx
@@ -1,11 +1,36 @@
 // For all routes except the explicitly defined ones
 // simply render the SPA without SSR
 
+import { renderToString } from 'react-dom/server'
+import { Helmet } from 'react-helmet'
 import { escapeInject, dangerouslySkipEscape } from 'vike/server'
 
+import { MetaTags } from 'components/meta-tags/MetaTags'
 import { getIndexHtml } from 'ssr/getIndexHtml'
+import { getDefaultSEOFields } from 'utils/seo'
 
 export function render() {
+  const seoMetadata = getDefaultSEOFields()
+
+  const pageHtml = renderToString(
+    <>
+      <MetaTags {...seoMetadata} />
+      <div />
+    </>
+  )
+
+  const helmet = Helmet.renderStatic()
+
   const html = getIndexHtml()
+    .replace(`<div id="root"></div>`, `<div id="root">${pageHtml}</div>`)
+    .replace(
+      `<meta property="helmet" />`,
+      `
+      ${helmet.title.toString()}
+      ${helmet.meta.toString()}
+      ${helmet.link.toString()}
+      `
+    )
+
   return escapeInject`${dangerouslySkipEscape(html)}`
 }
