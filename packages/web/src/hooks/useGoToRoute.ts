31b704853ef5c4dc82ae16847eedc15cf22a88d0
Raymond Jacobson <ray@audius.co>
2024-06-14 16:18:04 -0400

    [PAY-2837] Handle disallowed actions when acting as managed user (#8837)

--- packages/web/src/hooks/useGoToRoute.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react'
-
-import { push as pushRoute } from 'connected-react-router'
-import { useDispatch } from 'react-redux'
-
-export function useGoToRoute() {
-  const dispatch = useDispatch()
-  return useCallback((route: string) => dispatch(pushRoute(route)), [dispatch])
-}
