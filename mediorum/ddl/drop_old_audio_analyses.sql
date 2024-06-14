ab37ea4f03f9c48396f5a44c583ed61f4a095021
Michelle Brier <michelle.brier4@gmail.com>
2024-06-14 01:04:37 -0700

    optimize ddl and fix typo

--- mediorum/ddl/drop_old_audio_analyses.sql
+++ mediorum/ddl/drop_old_audio_analyses.sql
@@ -9,7 +9,7 @@ BEGIN
         audio_analyzed_by = NULL,
         audio_analyzed_at = NULL,
         audio_analysis_results = NULL
-    WHERE created_at <= '2024-06-18 07:00:00'::timestamp;
+    WHERE created_at <= '2024-06-18 07:00:00'::timestamp AND audio_analysis_status IS NOT NULL AND audio_analysis_status != '';
 END $$;
 
 commit;
