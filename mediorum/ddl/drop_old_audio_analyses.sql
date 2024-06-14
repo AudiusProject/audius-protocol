1efa39c372db0d63e03a7e3eec7cb9f61d872fdb
Michelle Brier <michelle.brier4@gmail.com>
2024-06-14 00:31:13 -0700

    drop old analyses

---
+++ mediorum/ddl/drop_old_audio_analyses.sql
@@ -0,0 +1,15 @@
+begin;
+
+DO $$ 
+BEGIN
+    UPDATE uploads
+    SET 
+        audio_analysis_status = NULL,
+        audio_analysis_error = NULL,
+        audio_analyzed_by = NULL,
+        audio_analyzed_at = NULL,
+        audio_analysis_results = NULL
+    WHERE created_at <= '2024-06-18 07:00:00'::timestamp;
+END $$;
+
+commit;
