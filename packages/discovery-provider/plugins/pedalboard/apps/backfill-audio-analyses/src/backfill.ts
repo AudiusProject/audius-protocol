377d36fe1c48016524a539fbcc327a6439a3dfcb
Michelle Brier <michelle.brier4@gmail.com>
2024-06-18 14:32:19 -0700

    clean up. fix req sigs

--- packages/discovery-provider/plugins/pedalboard/apps/backfill-audio-analyses/src/backfill.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/backfill-audio-analyses/src/backfill.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
-import _ from 'lodash'
-import { ethers } from 'ethers'
+import stringify from 'fast-json-stable-stringify'
 import { Semaphore } from 'await-semaphore'
 import { Knex } from 'knex'
 import { App } from '@pedalboard/basekit'
@@ -12,8 +11,8 @@ import {
   getCachedHealthyContentNodes
 } from './redis'
 
-// Batch size for fetching tracks
-const BACKFILL_BATCH_SIZE = config.testRun ? 100 : 1000
+const Web3 = require('web3')
+const web3 = new Web3()
 
 // Concurrency control
 const MAX_CONCURRENT_REQUESTS = 10
@@ -24,25 +23,28 @@ const REQUEST_TIMEOUT = 5000 // 5s
 interface Track {
   track_id: number
   track_cid: string
-  audio_upload_id: string
+  audio_upload_id: string | null
   musical_key: string | null
   bpm: number | null
   audio_analysis_error_count: number | null
 }
 
-function generateSignature(data: any, privateKey: string): string {
-  const toSignStr = JSON.stringify(data, Object.keys(data).sort())
-  const toSignHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(toSignStr))
-  const signingKey = new ethers.utils.SigningKey(privateKey)
-  const signature = signingKey.signDigest(toSignHash)
-  return ethers.utils.joinSignature(signature)
+async function generateSignature(data: any, privateKey: string) {
+  // sort keys and stringify data
+  const dataStr = stringify(data)
+  const toSignHash = web3.utils.keccak256(dataStr)
+  const signedMessage = await web3.eth.accounts.sign(
+    toSignHash,
+    privateKey
+  )
+  return signedMessage.signature
 }
 
 async function analyzeAudio(
   contentNodes: string[],
   track: Track
 ): Promise<void> {
-  const audioUploadId = track.audio_upload_id
+  const audioUploadId = track.audio_upload_id || ""
   const trackCid = track.track_cid
   // only analyze streamable tracks
   if (!trackCid) return
@@ -50,6 +52,7 @@ async function analyzeAudio(
   const isLegacyTrack = !audioUploadId && trackCid.startsWith("Qm")
 
   const release = await semaphore.acquire() // Acquire a semaphore permit
+  // allow up to 5 attempts to trigger audio analysis for this track
   for (let i = 0; i < 5; i++) {
     // Choose a random content node
     const contentNode = contentNodes[Math.floor(Math.random() * contentNodes.length)]
@@ -58,23 +61,35 @@ async function analyzeAudio(
         `Querying ${contentNode} for audio analysis for track ID ${track.track_id}, track CID ${trackCid}, upload ID ${audioUploadId}`
       )
       let analysisUrl = `${contentNode}/uploads/${audioUploadId}/analyze`
+      let cid = ""
       if (isLegacyTrack) {
         analysisUrl = `${contentNode}/tracks/legacy/${trackCid}/analyze`
+        cid = trackCid
       }
-      const timestamp = Math.floor(Date.now() / 1000) // current timestamp in seconds
+      const timestamp = Math.floor(Date.now()) // current timestamp in ms
       const dataToSign = {
-        trackId: track.track_id,
+        upload_id: audioUploadId,
+        cid,
+        shouldCache: 1,
         timestamp,
-        uploadId: audioUploadId
+        trackId: track.track_id,
+        userId: 0,
       }
-      const signature = generateSignature(dataToSign, config.delegatePrivateKey)
-      const queryParams = new URLSearchParams({ signature })
+      const signature = await generateSignature(dataToSign, config.delegatePrivateKey)
+      const signatureEnvelope = {
+        data: JSON.stringify(dataToSign),
+        signature: signature
+      }
+      const queryParams = new URLSearchParams({ signature: JSON.stringify(signatureEnvelope) })
+
       const response = await axios.post(
         `${analysisUrl}?${queryParams.toString()}`,
         null,
         { timeout: REQUEST_TIMEOUT }
       )
-      if (response.status !== 200) {
+      if (response.status == 200) {
+        break
+      } else {
         console.log(
           `Error triggering audio analysis on ${contentNode}: Received ${response.status} response. Attempt #${i+1} to trigger audio analysis for track ID ${track.track_id}, track CID ${trackCid}, upload ID ${audioUploadId}. Trying another content node...`
         )
@@ -95,9 +110,9 @@ async function analyzeAudio(
         break
       }
     }
-
-    release() // Release the semaphore permit
   }
+
+  release() // Release the semaphore permit
 }
 
 async function fetchTracks(
@@ -115,8 +130,8 @@ async function fetchTracks(
     .limit(limit)
 }
 
-// Trigger audio analyses for BACKFILL_BATCH_SIZE tracks at a time
-async function processBatches(db: Knex): Promise<void> {
+// Trigger audio analyses for batchSize tracks at a time
+async function processBatches(db: any, batchSize: number): Promise<void> {
   let offset
   while (true) {
     console.time('Batch processing time')
@@ -130,7 +145,7 @@ async function processBatches(db: Knex): Promise<void> {
     if (offset == null) {
       offset = 0
     }
-    const tracks = await fetchTracks(offset, BACKFILL_BATCH_SIZE, db)
+    const tracks = await fetchTracks(offset, batchSize, db)
     if (tracks.length === 0) {
       console.timeEnd('Batch processing time')
       break
@@ -140,7 +155,7 @@ async function processBatches(db: Knex): Promise<void> {
     )
     await Promise.all(analyzePromises)
 
-    offset += BACKFILL_BATCH_SIZE
+    offset += batchSize
     await storeDbOffset(offset)
     console.timeEnd('Batch processing time')
     console.log(
@@ -167,5 +182,7 @@ async function processBatches(db: Knex): Promise<void> {
 
 export const backfill = async (app: App<SharedData>) => {
   const db = app.getDnDb()
-  await processBatches(db)
+// Batch size for fetching tracks
+  const BACKFILL_BATCH_SIZE = config.testRun ? 100 : 1000
+  await processBatches(db, BACKFILL_BATCH_SIZE)
 }
