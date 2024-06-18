78c875a514892d5ce5f1f022a8094efc488bfdc2
sliptype <sliptype@gmail.com>
2024-06-18 17:21:48 -0500

    Update indexNames

--- packages/es-indexer/src/indexNames.ts
+++ packages/es-indexer/src/indexNames.ts
@@ -1,7 +1,7 @@
 export const indexNames = {
-  playlists: 'playlists18',
+  playlists: 'playlists19',
   reposts: 'reposts13',
   saves: 'saves13',
-  tracks: 'tracks18',
+  tracks: 'tracks19',
   users: 'users16',
 }
