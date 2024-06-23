c76b9c83a60f7f1c3a35ff9260bd2c5fbf41deb3
Michelle Brier <michelle.brier4@gmail.com>
2024-06-22 21:04:47 -0700

    audio analysis repairer: handle null results (#8924)

--- packages/discovery-provider/src/app.py
+++ packages/discovery-provider/src/app.py
@@ -442,7 +442,7 @@ def configure_celery(celery, test_config=None):
             },
             "repair_audio_analyses": {
                 "task": "repair_audio_analyses",
-                "schedule": timedelta(minutes=2),
+                "schedule": timedelta(minutes=3),
             },
         },
         task_serializer="json",
