17e7630cbd0c2f2d7a2035cbb5a80436072a6c78
isaac <isaac@audius.co>
2024-06-26 11:26:37 -0700

    download app email

--- packages/discovery-provider/src/tasks/create_engagement_notifications.py
+++ packages/discovery-provider/src/tasks/create_engagement_notifications.py
@@ -22,7 +22,7 @@ def get_claimable_reward_notification_group_id(user_id, challenge_id, specifier)
 
 
 @log_duration(logger)
-def _create_engagement_notifications(session):
+def create_claimable_reward_notifications(session):
     week_cooldown_interval = timedelta(weeks=1)
 
     new_notifications = []
@@ -101,7 +101,88 @@ def _create_engagement_notifications(session):
             logger.info(f"Debouncing notification for {user_challenge.user_id}")
     logger.info(f"Inserting {len(new_notifications)} claimable reward notifications")
     session.add_all(new_notifications)
-    session.commit()
+
+
+@log_duration(logger)
+def download_app_notifications(session):
+    week_cooldown_interval = timedelta(weeks=1)
+
+    new_notifications = []
+
+    user_challenges_cooldown_completed = (
+        session.query(UserChallenge)
+        .outerjoin(
+            ChallengeDisbursement,
+            and_(
+                UserChallenge.challenge_id == ChallengeDisbursement.challenge_id,
+                UserChallenge.specifier == ChallengeDisbursement.specifier,
+            ),
+        )
+        .join(Challenge, Challenge.id == UserChallenge.challenge_id)
+        .outerjoin(
+            Notification,
+            and_(
+                Notification.specifier == UserChallenge.specifier,
+                Notification.group_id
+                == func.concat(
+                    CLAIMABLE_REWARD,
+                    ":",
+                    func.cast(UserChallenge.user_id, String),
+                    ":challenge:",
+                    func.cast(UserChallenge.challenge_id, String),
+                    ":specifier:",
+                    func.cast(UserChallenge.specifier, String),
+                ),
+            ),
+        )
+        .filter(
+            UserChallenge.is_complete,
+            UserChallenge.completed_at >= START_DATETIME,
+            UserChallenge.completed_at < datetime.now() - week_cooldown_interval,
+            Challenge.cooldown_days == 7,
+            ChallengeDisbursement.specifier.is_(None),  # no disbursement yet
+            Notification.id.is_(None),  # no notification yet
+        )
+        .limit(BATCH_SIZE)
+        .all()
+    )
+    for user_challenge in user_challenges_cooldown_completed:
+        time_threshold = user_challenge.completed_at - timedelta(hours=1)
+        # Query to check if there's already a claimable reward notification for this user
+        existing_notification = (
+            session.query(Notification)
+            .filter(
+                Notification.user_ids.any(
+                    user_challenge.user_id
+                ),  # Assumes 'user_ids' can handle multiple IDs and is searchable this way
+                Notification.timestamp >= time_threshold,
+                Notification.type == CLAIMABLE_REWARD,
+            )
+            .first()
+        )
+        if not existing_notification:
+            new_notification = Notification(
+                specifier=str(user_challenge.specifier),
+                group_id=get_claimable_reward_notification_group_id(
+                    user_challenge.user_id,
+                    user_challenge.challenge_id,
+                    user_challenge.specifier,
+                ),
+                blocknumber=user_challenge.completed_blocknumber,
+                user_ids=[user_challenge.user_id],
+                type=CLAIMABLE_REWARD,
+                data={
+                    "specifier": user_challenge.specifier,
+                    "challenge_id": user_challenge.challenge_id,
+                    "amount": user_challenge.amount,
+                },
+                timestamp=user_challenge.completed_at,
+            )
+            new_notifications.append(new_notification)
+        else:
+            logger.info(f"Debouncing notification for {user_challenge.user_id}")
+    logger.info(f"Inserting {len(new_notifications)} claimable reward notifications")
+    session.add_all(new_notifications)
 
 
 # ####### CELERY TASKS ####### #
@@ -120,7 +201,8 @@ def create_engagement_notifications(self):
         have_lock = update_lock.acquire(blocking=False)
         if have_lock:
             with db.scoped_session() as session:
-                _create_engagement_notifications(session)
+                create_claimable_reward_notifications(session)
+                create_download_app_notifications(session)
 
         else:
             logger.info("Failed to acquire lock")
