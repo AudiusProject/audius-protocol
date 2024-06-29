84e02ffeb3bc2ff1b4b0e37426e6a05f5bee49b2
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-06-28 23:30:05 -0700

    Better error logging and reporting from failed simulations and send transactions
    - Updates @solana/web3.js to get access to new SendTransactionError type
    - Updates to using tsx instead of ts-node (note: can't use local version, have to use npx or else the mounted volume in linux gets confused w/ esbuild)
    - Updates errorHandler to log the error like normal pino users would, and not wrap the errors

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/middleware/errorHandler.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/middleware/errorHandler.ts
@@ -1,37 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
 
-import { InternalServerError, ResponseError } from '../errors'
+import { ResponseError } from '../errors'
 import { logger as rootLogger } from '../logger'
 
 export const errorHandlerMiddleware = (
-  err: unknown,
+  error: unknown,
   _req: Request,
   res: Response,
   _next: NextFunction
 ) => {
-  const error =
-    err instanceof ResponseError
-      ? err
-      : new InternalServerError(
-          err instanceof Error ? err.message : String(err)
-        )
+  const status = error instanceof ResponseError ? error.status : 500
   if (!res.headersSent) {
     res
-      .status(error.status)
+      .status(status)
       .set('X-Request-ID', res.locals.requestId)
       .set('Access-Control-Expose-Headers', 'X-Request-ID')
-      .send({ error: error.message })
+      .send({ error: (error as any).toString() })
   }
   // in milliseconds
   const responseTime = new Date().getTime() - res.locals.requestStartTime
   const statusCode = res.statusCode
-  const logger = res.locals.logger ?? rootLogger
-  logger.error(
-    {
-      responseTime,
-      statusCode,
-      error: error.message
-    },
-    'request completed'
-  )
+  const logger = (res.locals.logger ?? rootLogger).child({
+    responseTime,
+    statusCode
+  })
+  logger.error(error, 'request completed')
 }
