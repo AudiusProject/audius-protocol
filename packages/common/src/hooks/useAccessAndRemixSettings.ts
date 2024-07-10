876e8236121e0fa1093b2edb735bb09a30e0c30f
Saliou Diallo <saliou@audius.co>
2024-07-10 11:40:24 -0400

    Update comment

--- packages/common/src/hooks/useAccessAndRemixSettings.ts
+++ packages/common/src/hooks/useAccessAndRemixSettings.ts
@@ -37,9 +37,10 @@ export const useHasNoCollectibles = () => {
  * 1. Remixes cannot be gated tracks.
  * 2. During upload, all access options are enabled unless the track is marked as a remix,
  * in which case only Public and Hidden are enabled.
- * 3. During edit, rule of thumb is that gated tracks can only be modified to allow broader access.
- *    This means that gated tracks can only be made public.
- * 4. Hidden tracks may be gated or made public.
+ * 3. During edit, with editable access feature flag disabled,
+ * rule of thumb is that gated tracks can only be modified to allow broader access.
+ * This means that gated tracks can only be made public.
+ * With editable access feature flag enabled, it follows the same rules as during upload.
  *
  * NOTE: this logic is different from the logic using feature flags. to determine whether options should render or not; just whether or not they should be disabled
  */
