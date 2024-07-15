0820ef0bd798d3945c2d3dab630687d7f8c0520a
Sebastian Klingler <sliptype@gmail.com>
2024-07-15 15:04:42 -0500

    Filter out searchv2 history items for search v1 (#9127)

--- packages/common/src/store/search/selectors.ts
+++ packages/common/src/store/search/selectors.ts
@@ -7,7 +7,7 @@ import { isSearchItem } from './slice'
 const getBaseState = (state: CommonState) => state.search
 
 export const getSearchHistory = (state: CommonState) =>
-  getBaseState(state).history
+  getBaseState(state).history.filter((item) => !isSearchItem(item))
 
 export const getV2SearchHistory = createSelector(getSearchHistory, (history) =>
   history.filter(isSearchItem)
