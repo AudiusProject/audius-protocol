2534f818ea6e5e98c6efa7fcdc4dd07fe96d6e28
Dylan Jeffers <dylan@audius.co>
2024-07-18 14:01:18 -0700

    [QA-1453] Big user-list images (#9182)

--- packages/mobile/src/screens/user-list-screen/UserListItem.tsx
+++ packages/mobile/src/screens/user-list-screen/UserListItem.tsx
@@ -88,7 +88,7 @@ export const UserListItem = (props: UserListItemProps) => {
         onPressOut={handlePressOut}
       >
         <View style={styles.infoRoot}>
-          <ProfilePicture userId={user.user_id} />
+          <ProfilePicture userId={user.user_id} size='large' />
           <View style={styles.userInfo}>
             <Text variant='h3' style={styles.displayName}>
               {name}
