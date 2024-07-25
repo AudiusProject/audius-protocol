52885b02d053bd560a8b9eaced6b6ad1ebfb2b61
sliptype <sliptype@gmail.com>
2024-07-25 12:20:19 -0500

    Add piechart

--- packages/web/src/pages/split-donation-page/SplitDonationPage.tsx
+++ packages/web/src/pages/split-donation-page/SplitDonationPage.tsx
@@ -2,8 +2,9 @@ import { useCallback, useMemo } from 'react'
 
 import { useGetUsersByIds } from '@audius/common/api'
 import { Status } from '@audius/common/models'
-import { Button, Flex, Paper } from '@audius/harmony'
-import { Form, Formik, useFormik, useFormikContext } from 'formik'
+import { Button, Flex, LoadingSpinner, Paper, Text } from '@audius/harmony'
+import { Form, Formik, useFormikContext } from 'formik'
+import { PieChart, Pie, Tooltip, Cell } from 'recharts'
 
 import { TextField } from 'components/form-fields'
 import { SelectField } from 'components/form-fields/SelectField'
@@ -16,7 +17,10 @@ const messages = {
   description: 'Split your donation between multiple creators',
   amountInputLabel: 'Donation Amount',
   donateButtonLabel: 'Donate',
-  selectPresetLabel: 'Split Preset'
+  selectPresetLabel: 'Split Preset',
+  configureLabel: 'Configure your donation',
+  usersListLabel: 'Selected Users',
+  piechartLabel: 'Donation Breakdown'
 }
 
 const header = <Header primary={messages.title} />
@@ -33,40 +37,99 @@ const initialValues = {
   preset: 'featured'
 }
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
+
+type CustomPieChartProps = {
+  data: { name: string; value: number }[]
+}
+const CustomPieChart = (props: CustomPieChartProps) => {
+  const { data } = props
+  return (
+    <PieChart width={600} height={400} css={{ overflow: 'visible' }}>
+      <Pie
+        data={data}
+        cx={300}
+        cy={200}
+        outerRadius={150}
+        fill='#8884d8'
+        dataKey='value'
+        label={({ name, value }) => name}
+        overflow={'visible'}
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+    </PieChart>
+  )
+}
+
+export default CustomPieChart
+
 const SplitDonationForm = () => {
-  const { values } = useFormikContext()
+  const { values } = useFormikContext<typeof initialValues>()
 
   const userIds = useMemo(() => {
     if (values.preset === 'featured') {
       return FEATURED_ARTIST_IDS
     }
     return []
-  }, [])
+  }, [values])
 
   const { data: users, status: usersStatus } = useGetUsersByIds({
     ids: userIds
   })
 
+  const piechartData = useMemo(() => {
+    return (
+      users?.map((user) => ({
+        name: user.name,
+        value: 1
+      })) ?? []
+    )
+  }, [users])
+
   return (
-    <Flex gap='m'>
-      <Paper direction='column' gap='m' p='xl' flex='1 1 auto'>
-        <TextField
-          label={messages.amountInputLabel}
-          placeholder='0'
-          endAdornment='$AUDIO'
-          name='amount'
-          type='number'
-        />
-        <SelectField
-          options={presetOptions}
-          name='preset'
-          label={messages.selectPresetLabel}
-        />
-        <Button variant='primary' type='submit' fullWidth>
-          {messages.donateButtonLabel}
-        </Button>
-      </Paper>
-      <Paper direction='column' gap='m' p='xl'>
+    <Flex gap='l' alignItems='flex-start'>
+      <Flex direction='column' gap='l' flex='1 1 auto'>
+        <Paper direction='column' gap='l' p='xl'>
+          <Text variant='title' textAlign='left'>
+            {messages.configureLabel}
+          </Text>
+          <TextField
+            label={messages.amountInputLabel}
+            placeholder='0'
+            endAdornment='$AUDIO'
+            name='amount'
+            type='number'
+          />
+          <SelectField
+            options={presetOptions}
+            name='preset'
+            label={messages.selectPresetLabel}
+          />
+          <Button variant='primary' type='submit' fullWidth>
+            {messages.donateButtonLabel}
+          </Button>
+        </Paper>
+        <Paper
+          direction='column'
+          justifyContent='center'
+          gap='m'
+          p='xl'
+          alignItems='flex-start'
+        >
+          <Text variant='title'>{messages.piechartLabel}</Text>
+          {usersStatus === Status.LOADING ? (
+            <LoadingSpinner />
+          ) : (
+            <CustomPieChart data={piechartData} />
+          )}
+        </Paper>
+      </Flex>
+      <Paper direction='column' gap='m' p='xl' alignItems='flex-start'>
+        <Text variant='title'>{messages.usersListLabel}</Text>
         <UserList
           hasMore={false}
           loading={usersStatus === Status.LOADING}
