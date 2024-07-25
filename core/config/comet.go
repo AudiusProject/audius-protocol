ec0153894cbe422fb2519c441d05afa579355304
Alec Savvy <alecsavvy@outlook.com>
2024-07-24 19:50:48 -0600

    reading in genesis from embed based on env

--- core/config/comet.go
+++ core/config/comet.go
@@ -5,28 +5,15 @@ import (
 
 	"github.com/AudiusProject/audius-protocol/core/accounts"
 	"github.com/AudiusProject/audius-protocol/core/common"
+	"github.com/AudiusProject/audius-protocol/core/config/genesis"
 	"github.com/cometbft/cometbft/config"
 	"github.com/cometbft/cometbft/privval"
-	"github.com/cometbft/cometbft/types"
-	cmttime "github.com/cometbft/cometbft/types/time"
 )
 
 // reads in or creates cometbft keys from the delegate private key
 // also creates validator keys and genesis file
 func InitComet(logger *common.Logger, environment, delegatePrivateKey, homeDir string) error {
 	logger.Info("initializing comet config")
-
-	chainId := ""
-	switch environment {
-	case "prod", "production", "mainnet":
-		chainId = "audius-mainnet-1"
-	case "stage", "staging", "testnet":
-		chainId = "audius-testnet-1"
-	default:
-		chainId = fmt.Sprintf("audius-devnet-%s", environment)
-	}
-
-	logger.Infof("configuring node setup on env %s in %s", chainId, homeDir)
 
 	key, err := accounts.NewKey(delegatePrivateKey)
 	if err != nil {
@@ -53,7 +40,6 @@ func InitComet(logger *common.Logger, environment, delegatePrivateKey, homeDir s
 
 	var pv *privval.FilePV
 	if common.FileExists(privValKeyFile) {
-		pv = privval.LoadFilePV(privValKeyFile, privValStateFile)
 		logger.Info("Found private validator", "keyFile", privValKeyFile,
 			"stateFile", privValStateFile)
 	} else {
@@ -77,20 +63,11 @@ func InitComet(logger *common.Logger, environment, delegatePrivateKey, homeDir s
 	if common.FileExists(genFile) {
 		logger.Info("Found genesis file", "path", genFile)
 	} else {
-		genDoc := types.GenesisDoc{
-			ChainID:         chainId,
-			GenesisTime:     cmttime.Now(),
-			ConsensusParams: types.DefaultConsensusParams(),
-		}
-		pubKey, err := pv.GetPubKey()
+
+		genDoc, err := genesis.Read(environment)
 		if err != nil {
-			return fmt.Errorf("can't get pubkey %v", err)
+			return fmt.Errorf("error reading genesis: %v", err)
 		}
-		genDoc.Validators = []types.GenesisValidator{{
-			Address: pubKey.Address(),
-			PubKey:  pubKey,
-			Power:   10,
-		}}
 
 		if err := genDoc.SaveAs(genFile); err != nil {
 			return fmt.Errorf("saving gen file %v", err)
