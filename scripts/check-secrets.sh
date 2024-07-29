8dc5b811f27d6d4a2b8d6683485958b1d11862e2
Alec Savvy <alecsavvy@outlook.com>
2024-07-29 13:23:36 -0600

    revert secrets file

--- scripts/check-secrets.sh
+++ scripts/check-secrets.sh
@@ -1,10 +1,2 @@
-EXCLUDE_DIRS=(
-    "core/db/sql"
-    "core/infra/dev_config"
-)
-
-EXCLUDE_PATTERN=$(printf "|^%s" "${EXCLUDE_DIRS[@]}")
-EXCLUDE_PATTERN=${EXCLUDE_PATTERN:1}
-
 # scan the staged files
-git diff --cached --name-only | grep -Ev "$EXCLUDE_PATTERN" | xargs git secrets --scan --cached
+git secrets --scan --cached $(git diff --cached --name-only)
