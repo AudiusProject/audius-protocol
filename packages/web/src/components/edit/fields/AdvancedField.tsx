2bed0939e9eb32c549e526bf1ba2fc07da12857b
sliptype <sliptype@gmail.com>
2024-07-31 17:05:59 -0500

    Improve bpm precision and validation

--- packages/web/src/components/edit/fields/AdvancedField.tsx
+++ packages/web/src/components/edit/fields/AdvancedField.tsx
@@ -34,6 +34,7 @@ import { TextField } from 'components/form-fields'
 import { SegmentedControlField } from 'components/form-fields/SegmentedControlField'
 import layoutStyles from 'components/layout/layout.module.css'
 import { Tooltip } from 'components/tooltip'
+import { useBpmMaskedInput } from 'hooks/useBpmMaskedInput'
 import { env } from 'services/env'
 
 import styles from './AdvancedField.module.css'
@@ -355,6 +356,12 @@ const AdvancedModalFields = ({ isUpload }: { isUpload?: boolean }) => {
   const [{ value: isHidden }] = useField<boolean>(IS_UNLISTED)
   const [, , { setValue: setBpmValue }] = useField<string>(BPM)
 
+  const bpmMaskedInputProps = useBpmMaskedInput({
+    onChange: (e) => {
+      setBpmValue(e.target.value)
+    }
+  })
+
   const { licenseType, licenseDescription } = computeLicense(
     allowAttribution,
     commercialUse,
@@ -501,15 +508,9 @@ const AdvancedModalFields = ({ isUpload }: { isUpload?: boolean }) => {
                 <TextField
                   name={BPM}
                   type='number'
-                  onChange={(e) => {
-                    const { value } = e.nativeEvent.target as HTMLInputElement
-
-                    if (value === '' || isBpmValid(value)) {
-                      setBpmValue(value)
-                    }
-                  }}
                   label={messages.bpm.label}
                   autoComplete='off'
+                  {...bpmMaskedInputProps}
                 />
               </Flex>
               <Flex direction='column' w='100%'>
