9500340a6fc06ce4b45193a67bfc2d24fd8ee5dc
Dylan Jeffers <dylan@audius.co>
2024-08-01 16:19:49 -0700

    [C-4899] Fix isrc and iswc validation (#9352)

--- packages/mobile/src/screens/edit-track-screen/screens/IsrcIswcScreen.tsx
+++ packages/mobile/src/screens/edit-track-screen/screens/IsrcIswcScreen.tsx
@@ -1,9 +1,12 @@
+import { useFormikContext } from 'formik'
 import { View } from 'react-native'
 
 import { IconInfo } from '@audius/harmony-native'
 import { TextField } from 'app/components/fields'
 import { FormScreen } from 'app/screens/form-screen'
 import { makeStyles } from 'app/styles'
+
+import type { FormValues } from '../types'
 
 const messages = {
   title: 'ISRC/ISWC',
@@ -20,18 +23,31 @@ const useStyles = makeStyles(({ spacing }) => ({
 
 export const IsrcIswcScreen = () => {
   const styles = useStyles()
+
+  const { errors } = useFormikContext<FormValues>()
+
+  const hasErrors = Boolean(errors.isrc || errors.iswc)
+
   return (
-    <FormScreen title={messages.title} icon={IconInfo} variant='white'>
+    <FormScreen
+      title={messages.title}
+      icon={IconInfo}
+      variant='white'
+      disableSubmit={hasErrors}
+      revertOnCancel
+    >
       <View style={styles.content}>
         <TextField
           name='isrc'
           label={messages.isrc}
           placeholder='CC-XXX-YY-NNNNN'
+          errorBeforeSubmit
         />
         <TextField
           name='iswc'
           label={messages.iswc}
           placeholder='T-34524688-1'
+          errorBeforeSubmit
         />
       </View>
     </FormScreen>
