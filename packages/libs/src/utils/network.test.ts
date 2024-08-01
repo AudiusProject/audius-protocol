b2e9861db47816a7c5b77d111fd1a57dbd2edb86
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-08-01 16:07:11 -0700

    ... idk why i need to delete this

--- packages/libs/src/utils/network.test.ts
+++ packages/libs/src/utils/network.test.ts
@@ -103,7 +103,7 @@ describe('timeRequests()', () => {
 
     // Make sure there is some variance
     assert(!allResults.every((val) => val === allResults[0]))
-  }).timeout(10000)
+  })
 
   it('filterNonResponsive = true', async () => {
     const requests = [
