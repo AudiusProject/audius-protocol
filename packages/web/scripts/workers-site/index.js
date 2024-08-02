58f94e7d5ec4fb1255e38e6af29c92fda8cd01b5
Raymond Jacobson <ray@audius.co>
2024-08-02 11:22:27 -0700

    Remove POST

--- packages/web/scripts/workers-site/index.js
+++ packages/web/scripts/workers-site/index.js
@@ -311,10 +311,7 @@ async function handleEvent(request, env, ctx) {
         // are given unique hashes when they do.
         const response = new Response(asset.body, asset)
         response.headers.set('Access-Control-Allow-Origin', '*')
-        response.headers.set(
-          'Access-Control-Allow-Methods',
-          'GET, POST, OPTIONS'
-        )
+        response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
         response.headers.set('Access-Control-Allow-Headers', 'Content-Type')
         response.headers.set('cache-control', BROWSER_CACHE_TTL_SECONDS)
 
