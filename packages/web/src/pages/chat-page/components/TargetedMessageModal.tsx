950938f54bbe88d0d958a546d5a722052519b697
amendelsohn <andrew.j.mendelsohn@gmail.com>
2024-08-02 14:23:29 -0700

    ready for PR

--- packages/web/src/pages/chat-page/components/TargetedMessageModal.tsx
+++ packages/web/src/pages/chat-page/components/TargetedMessageModal.tsx
@@ -13,6 +13,7 @@ import {
   IconCaretLeft,
   ModalContent
 } from '@audius/harmony'
+import { Formik, FormikValues, useField } from 'formik'
 
 const messages = {
   title: 'Target Audience',
@@ -41,40 +42,87 @@ const targetAudienceOptions = [
 
 export const TargetedMessageModal = () => {
   const { isOpen, onClose } = useTargetedMessageModal()
+
+  const initialValues = {
+    target_audience: undefined
+  }
+
+  const handleSubmit = (values: FormikValues) => {
+    window.alert(JSON.stringify(values))
+    switch (values.target_audience) {
+      case 'followers':
+        // do something
+        break
+      case 'supporters':
+        // do something
+        break
+      case 'purchasers':
+        // do something
+        break
+      case 'remix_creators':
+        // do something
+        break
+      default:
+        break
+    }
+  }
+
   return (
     <Modal size='small' isOpen={isOpen} onClose={onClose}>
-      <ModalHeader>
-        <ModalTitle icon={<IconTowerBroadcast />} title={messages.title} />
-      </ModalHeader>
-      <ModalContent>
-        {/* <Formik> */}
-        <RadioGroup name='target_audience'>
-          <Flex direction='column' gap='xl'>
-            {targetAudienceOptions.map(({ label, value }) => (
-              <Flex as='label' alignItems='center' gap='l' key={value}>
-                <Radio value={value} />
-                <Text variant='title' size='l'>
-                  {label}
-                </Text>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {({ submitForm }) => (
+          <>
+            <ModalHeader>
+              <ModalTitle
+                icon={<IconTowerBroadcast />}
+                title={messages.title}
+              />
+            </ModalHeader>
+            <ModalContent>
+              <TargetedMessagesFields />
+            </ModalContent>
+            <ModalFooter>
+              <Flex w='100%' gap='s'>
+                <Button
+                  variant='secondary'
+                  iconLeft={IconCaretLeft}
+                  css={{ flexGrow: 1 }}
+                  onClick={onClose}
+                >
+                  {messages.back}
+                </Button>
+                <Button
+                  variant='primary'
+                  type='submit'
+                  css={{ flexGrow: 1 }}
+                  onClick={submitForm}
+                >
+                  {messages.continue}
+                </Button>
               </Flex>
-            ))}
-          </Flex>
-        </RadioGroup>
-      </ModalContent>
-      <ModalFooter>
-        <Flex w='100%' gap='s'>
-          <Button
-            variant='secondary'
-            iconLeft={IconCaretLeft}
-            css={{ flexGrow: 1 }}
-          >
-            {messages.back}
-          </Button>
-          <Button variant='primary' css={{ flexGrow: 1 }}>
-            {messages.continue}
-          </Button>
-        </Flex>
-      </ModalFooter>
+            </ModalFooter>
+          </>
+        )}
+      </Formik>
     </Modal>
+  )
+}
+
+const TargetedMessagesFields = () => {
+  const [field] = useField('target_audience')
+
+  return (
+    <RadioGroup {...field}>
+      <Flex direction='column' gap='xl'>
+        {targetAudienceOptions.map(({ label, value }) => (
+          <Flex as='label' gap='s' key={label}>
+            <Radio value={value} />
+            <Text variant='title' size='l'>
+              {label}
+            </Text>
+          </Flex>
+        ))}
+      </Flex>
+    </RadioGroup>
   )
 }
