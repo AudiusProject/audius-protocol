cdaf0f29335df7ff66693fbae268f5ea63728056
JD Francis <jd@audius.co>
2024-08-15 11:02:16 -0500

    remove comment

--- packages/web/src/components/comments/TimestampLink.tsx
+++ packages/web/src/components/comments/TimestampLink.tsx
@@ -1,5 +1,3 @@
-// TODO: move this somewhere else
-
 import { useCurrentCommentSection } from '@audius/common/context'
 import { seek } from '@audius/common/src/store/player/slice'
 import { TextLink, formatTrackTimestamp } from '@audius/harmony'
@@ -9,11 +7,10 @@ export const TimestampLink = ({ timestampMS }: { timestampMS: number }) => {
   const dispatch = useDispatch()
   const { playTrack } = useCurrentCommentSection()
   const timestampS = Math.floor(timestampMS / 1000)
-  // const currentlyPlayingTrackId = useSelector(getTrackId)
   const handleClick = () => {
     // Starts playing the current page's song
     playTrack()
-    // Seek to the timestamp
+    // Seeks to the timestamp
     dispatch(seek({ seconds: timestampS }))
   }
   return (
