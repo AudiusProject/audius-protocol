279a9b2123fcbfc8f93e19bd6a5d6944d6210b79
Steve Perkins <stereosteve@users.noreply.github.com>
2024-08-15 12:58:34 -0400

    More explicit audience columns for chat blast. (#9454)

--- comms/discovery/rpcz/chat_blast.go
+++ comms/discovery/rpcz/chat_blast.go
@@ -18,12 +18,12 @@ func chatBlast(tx *sqlx.Tx, userId int32, ts time.Time, params schema.ChatBlastR
 	// insert params.Message into messages table
 	_, err := tx.Exec(`
 		insert into chat_blast
-			(blast_id, from_user_id, audience, audience_track_id, plaintext, created_at)
+			(blast_id, from_user_id, audience, audience_content_type, audience_content_id, plaintext, created_at)
 		values
-			($1, $2, $3, $4, $5, $6)
+			($1, $2, $3, $4, $5, $6, $7)
 		on conflict (blast_id)
 		do nothing
-		`, params.BlastID, userId, params.Audience, params.AudienceTrackID, params.Message, ts)
+		`, params.BlastID, userId, params.Audience, params.AudienceContentType, params.AudienceContentID, params.Message, ts)
 	if err != nil {
 		return err
 	}
