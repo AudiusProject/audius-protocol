f2436b10211af2168f039b475e83ee825cf34cbd
Alec Savvy <alecsavvy@outlook.com>
2024-08-14 21:33:55 -0600

    same with p2p port

--- core/config/defaults.go
+++ core/config/defaults.go
@@ -2,6 +2,6 @@ package config
 
 const (
 	DefaultRPCAddress             = "tcp://0.0.0.0:26657"
-	DefaultP2PAddress             = "0.0.0.0:26656"
+	DefaultP2PAddress             = "tcp://0.0.0.0:26656"
 	DefaultTestnetPersistentPeers = "0f4be2aaa70e9570eee3485d8fa54502cf1a9fc0@34.67.210.7:26656"
 )
