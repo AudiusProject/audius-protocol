bf7fc7bdf32349abda64cbe1938b37a86aba260b
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-08-19 02:08:42 -0700

    add restore command to restore backups of id changes

--- solana-programs/scripts/build.sh
+++ solana-programs/scripts/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Intended to be run on the host dev machine.
+# 1. Replaces the program deploy keys and program IDs within them to the dev ones.
+# 2. Uses the correct Solana and Anchor versions to build each program.
+# 3. Restores the program IDs to not cause any git diff
+
 set -e
 
 # cd into solana-programs
@@ -44,4 +49,10 @@ echo
 cd payment-router/
 anchor keys sync
 anchor build
+echo
 
+# Prevent any git diff
+echo -e ">>> ${LIGHT_GREEN}Restore original program IDs...${NO_COLOR}"
+echo
+./scripts/update-keys.sh restore
+echo
--- solana-programs/scripts/update-keys.sh
+++ solana-programs/scripts/update-keys.sh
@@ -18,9 +18,9 @@ function replace_address {
             echo "Replacing $current_address with $new_address in $file"
             if [[ "$OSTYPE" =~ ^darwin ]]; then
                 # osx requires a backup file to be created when using sed
-                sed -i'.backup' "s/$current_address/$new_address/g" $1
+                sed -i'.backup' "s/$current_address/$new_address/g" $file
             else
-                sed -i "s/$current_address/$new_address/g" $1
+                sed -i "s/$current_address/$new_address/g" $file
             fi
         done
     fi
@@ -31,6 +31,13 @@ function generate_key {
         echo $2 >$1
     else
         solana-keygen new -f -s --no-bip39-passphrase -o $1
+    fi
+}
+
+function restore_backup {
+    if [[ -f "$1.backup" ]]; then
+        echo "Restoring $1..."
+        mv "$1.backup" "$1"
     fi
 }
 
@@ -45,22 +52,33 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     set +a
 fi
 
-mkdir -p ${CARGO_TARGET_DIR:-target}/deploy
+if [[ "$1" = "restore" ]]; then
+    echo "Restoring program IDs..."
+    restore_backup audius_eth_registry/src/lib.rs
+    restore_backup track_listen_count/src/lib.rs
+    restore_backup claimable-tokens/program/src/lib.rs
+    restore_backup reward-manager/program/src/lib.rs
+    restore_backup payment-router/programs/payment-router/src/lib.rs
+    restore_backup payment-router/Anchor.toml
+else
+    echo "Replacing program keys..."
+    mkdir -p ${CARGO_TARGET_DIR:-target}/deploy
 
-generate_key ${CARGO_TARGET_DIR:-target}/deploy/audius_eth_registry-keypair.json "$SOLANA_AUDIUS_ETH_REGISTRY_SECRET_KEY"
-replace_address audius_eth_registry/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/audius_eth_registry-keypair.json
+    generate_key ${CARGO_TARGET_DIR:-target}/deploy/audius_eth_registry-keypair.json "$SOLANA_AUDIUS_ETH_REGISTRY_SECRET_KEY"
+    replace_address audius_eth_registry/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/audius_eth_registry-keypair.json
 
-generate_key ${CARGO_TARGET_DIR:-target}/deploy/track_listen_count-keypair.json "$SOLANA_TRACK_LISTEN_COUNT_SECRET_KEY"
-replace_address track_listen_count/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/track_listen_count-keypair.json
+    generate_key ${CARGO_TARGET_DIR:-target}/deploy/track_listen_count-keypair.json "$SOLANA_TRACK_LISTEN_COUNT_SECRET_KEY"
+    replace_address track_listen_count/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/track_listen_count-keypair.json
 
-generate_key ${CARGO_TARGET_DIR:-target}/deploy/claimable_tokens-keypair.json "$SOLANA_CLAIMABLE_TOKENS_SECRET_KEY"
-replace_address claimable-tokens/program/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/claimable_tokens-keypair.json
+    generate_key ${CARGO_TARGET_DIR:-target}/deploy/claimable_tokens-keypair.json "$SOLANA_CLAIMABLE_TOKENS_SECRET_KEY"
+    replace_address claimable-tokens/program/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/claimable_tokens-keypair.json
 
-generate_key ${CARGO_TARGET_DIR:-target}/deploy/audius_reward_manager-keypair.json "$SOLANA_REWARD_MANAGER_SECRET_KEY"
-replace_address reward-manager/program/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/audius_reward_manager-keypair.json
+    generate_key ${CARGO_TARGET_DIR:-target}/deploy/audius_reward_manager-keypair.json "$SOLANA_REWARD_MANAGER_SECRET_KEY"
+    replace_address reward-manager/program/src/lib.rs ${CARGO_TARGET_DIR:-target}/deploy/audius_reward_manager-keypair.json
 
-generate_key ${CARGO_TARGET_DIR:-target}/deploy/payment_router-keypair.json "$SOLANA_PAYMENT_ROUTER_SECRET_KEY"
-replace_address \
-    payment-router/programs/payment-router/src/lib.rs \
-    payment-router/Anchor.toml \
-    ${CARGO_TARGET_DIR:-target}/deploy/payment_router-keypair.json
+    generate_key ${CARGO_TARGET_DIR:-target}/deploy/payment_router-keypair.json "$SOLANA_PAYMENT_ROUTER_SECRET_KEY"
+    replace_address \
+        payment-router/programs/payment-router/src/lib.rs \
+        payment-router/Anchor.toml \
+        ${CARGO_TARGET_DIR:-target}/deploy/payment_router-keypair.json
+fi
