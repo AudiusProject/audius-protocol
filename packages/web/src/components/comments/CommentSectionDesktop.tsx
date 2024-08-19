bfaa5364eeb342c193c18d8460d8eabb60cd6b8b
Isaac Solo <isaac@audius.co>
2024-08-19 10:32:20 -0700

    Enabling blocking users from commenting (#9469)

--- packages/web/src/components/comments/CommentSectionDesktop.tsx
+++ packages/web/src/components/comments/CommentSectionDesktop.tsx
@@ -1,5 +1,8 @@
 import { useCurrentCommentSection } from '@audius/common/context'
+import { chatSelectors } from '@audius/common/store'
 import { Button, Divider, Flex, Paper } from '@audius/harmony'
+
+import { useSelector } from 'utils/reducer'
 
 import { CommentForm } from './CommentForm'
 import { CommentHeader } from './CommentHeader'
@@ -8,15 +11,19 @@ import { CommentSortBar } from './CommentSortBar'
 import { CommentThread } from './CommentThread'
 import { NoComments } from './NoComments'
 
+const { getCanCreateChat } = chatSelectors
+
 export const CommentSectionDesktop = () => {
   const {
-    currentUserId,
+    artistId,
     comments,
     commentSectionLoading,
     handleLoadMoreRootComments
   } = useCurrentCommentSection()
 
-  const commentPostAllowed = currentUserId !== null
+  const { canCreateChat: commentPostAllowed } = useSelector((state) =>
+    getCanCreateChat(state, { userId: artistId })
+  )
 
   if (commentSectionLoading) {
     return <CommentSkeletons />
@@ -26,7 +33,7 @@ export const CommentSectionDesktop = () => {
     <Flex gap='l' direction='column' w='100%' alignItems='flex-start'>
       <CommentHeader commentCount={comments.length} />
       <Paper w='100%' direction='column'>
-        {commentPostAllowed !== null ? (
+        {commentPostAllowed ? (
           <>
             <Flex gap='s' p='xl' w='100%' direction='column'>
               <CommentForm />
