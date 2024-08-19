90c405b196493ebd9e4dbcd89c298567f0c7324b
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-08-19 03:58:18 -0700

    Update install script to not install full CLI if building from source, but just the test validator

--- solana-programs/scripts/install-solana.sh
+++ solana-programs/scripts/install-solana.sh
@@ -8,9 +8,6 @@ set -ex
 if [[ "$(uname -m)" == "x86_64" ]]; then
     curl -SfL https://github.com/solana-labs/solana/releases/download/v1.18.22/solana-release-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf - -C $HOME
 else
-    # Building from source requires each CLI to be installed separately
-    cargo install --locked --root $HOME/solana-release --version 1.18.22 solana-cli solana-keygen
     # solana-test-validator is pulled from the solana-validator package where it's built. The solana-test-validator package has no binaries
     cargo install --locked --root $HOME/solana-release --version 1.18.22 --bin solana-test-validator solana-validator
-    cargo install --locked --root $HOME/solana-release --version 3.4.3 spl-token-cli
 fi
