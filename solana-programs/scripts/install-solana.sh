893ed420cc128415f1f892cd248c708c7f1b1f39
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-08-19 18:22:09 -0700

    Revamp local solana-test-validator (#9482)
    Co-authored-by: Raymond Jacobson <ray@audius.co>

--- solana-programs/scripts/install-solana.sh
+++ solana-programs/scripts/install-solana.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-if [[ "$(uname -m)" == "x86_64" ]]; then
-    curl -SfL https://github.com/solana-labs/solana/releases/download/v1.10.11/solana-release-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf - -C $HOME
-else
-    cargo install --locked --root $HOME/solana-release --version 1.16.3 solana-cli solana-keygen
-    cargo install --locked --root $HOME/solana-release --version 1.16.3 --bin solana-test-validator solana-validator
-    cargo install --locked --root $HOME/solana-release --version 3.1.0 spl-token-cli
-fi
 
-cargo install --git https://github.com/coral-xyz/anchor --tag v0.28.0 anchor-cli --locked
+# Installs Solana CLIs either from the install script (for available builds like linux x86_64)
+# or using Cargo to build from source (for unavailable builds like linux aarch64)
+if [[ "$(uname -m)" == "x86_64" ]]; then
+    curl -SfL https://github.com/solana-labs/solana/releases/download/v1.18.22/solana-release-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf - -C $HOME
+else
+    # solana-test-validator is pulled from the solana-validator package where it's built. The solana-test-validator package has no binaries
+    cargo install --locked --root $HOME/solana-release --version 1.18.22 --bin solana-test-validator solana-validator
+fi
