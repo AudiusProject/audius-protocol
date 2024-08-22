18c9403790c2d9e3341937cc14ea776470a88fbe
Randy Schott <1815175+schottra@users.noreply.github.com>
2024-08-22 14:50:21 -0400

    document type adapter

--- packages/common/src/api/types.ts
+++ packages/common/src/api/types.ts
@@ -1,5 +1,9 @@
 import { ID } from '~/models'
 
+/** Helper type for accepting args to a SDK method. It strips out the signature
+ * parameters (which are passed by SDK middleware) and adds the common
+ * `currentUserId` parameter that most functions calling SDK methods will convert
+ * and pass along. */
 export type SDKRequest<T> = Omit<
   T,
   'encodedDataMessage' | 'encodedDataSignature'
