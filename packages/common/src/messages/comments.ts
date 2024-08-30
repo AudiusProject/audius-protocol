4fec14d69c620fe45e61709bda4b7a0d3b9bef40
Dylan Jeffers <dylan@audius.co>
2024-08-30 23:23:38 +0200

    Address comments

--- packages/common/src/messages/comments.ts
+++ packages/common/src/messages/comments.ts
@@ -3,6 +3,7 @@ export const commentsMessages = {
   addComment: 'Add a comment',
   firstComment: 'Be the first to comment!',
   noComments: 'Nothing here yet',
+  noCommentsDescription: 'Be the first to comment on this track',
   viewAll: 'View All',
   showMoreReplies: 'Show More Replies',
   replies: 'Replies',
