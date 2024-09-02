40c0316883f5dce0ee6b79833560832923a19a3b
alecsavvy <16739903+alecsavvy@users.noreply.github.com>
2024-09-02 11:01:44 -0600

    PROTO-1904: eth validator bridge (#9564)

--- core/common/web3.go
+++ core/common/web3.go
@@ -26,3 +26,21 @@ func PrivKeyHexToAddress(hexPrivateKey string) (string, error) {
 	address := crypto.PubkeyToAddress(*publicKey).Hex()
 	return address, nil
 }
+
+// for parity with the web3.js web3.utils.utf8ToHex() call
+func Utf8ToHex(s string) [32]byte {
+	hex := [32]byte{}
+	copy(hex[:], s)
+	return hex
+}
+
+// reverse of Utf8ToHex
+func HexToUtf8(hex [32]byte) string {
+	var end int
+	for end = range hex {
+		if hex[end] == 0 {
+			break
+		}
+	}
+	return string(hex[:end])
+}
