1168bae1c6f0c9c8072001278bc95c03203f8303
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-09-03 10:06:01 -0700

    [PAY-3324] Add copy notice around network take rate (#9608)

--- packages/web/src/pages/pay-and-earn-page/components/SalesTab.tsx
+++ packages/web/src/pages/pay-and-earn-page/components/SalesTab.tsx
@@ -2,21 +2,25 @@ import { useCallback, useState } from 'react'
 
 import { useGetSales, useGetSalesCount, Id } from '@audius/common/api'
 import { useAllPaginatedQuery } from '@audius/common/audius-query'
+import { useFeatureFlag } from '@audius/common/hooks'
 import {
   Status,
   statusIsNotFinalized,
   combineStatuses,
   USDCPurchaseDetails
 } from '@audius/common/models'
+import { FeatureFlags } from '@audius/common/services'
 import {
   accountSelectors,
   useUSDCPurchaseDetailsModal
 } from '@audius/common/store'
 import { route } from '@audius/common/utils'
+import { Flex, IconMoneyBracket, Text, useTheme } from '@audius/harmony'
 import { full } from '@audius/sdk'
 import { push as pushRoute } from 'connected-react-router'
 import { useDispatch } from 'react-redux'
 
+import { ExternalTextLink } from 'components/link'
 import { useErrorPageOnFailedStatus } from 'hooks/useErrorPageOnFailedStatus'
 import { useIsMobile } from 'hooks/useIsMobile'
 import { useMainContentRef } from 'pages/MainContentContext'
@@ -44,7 +48,10 @@ const messages = {
   noSalesBody: 'Once you make a sale, it will show up here.',
   upload: 'Upload Track',
   headerText: 'Your Sales',
-  downloadCSV: 'Download CSV'
+  downloadCSV: 'Download CSV',
+  networkSplitExplainer:
+    'You will instantly receive 90% of the retail price for every transaction.',
+  learnMore: 'Learn more.'
 }
 
 const TRANSACTIONS_BATCH_SIZE = 50
@@ -176,6 +183,10 @@ export const SalesTab = ({
 }: Omit<ReturnType<typeof useSales>, 'downloadCSV'>) => {
   const isMobile = useIsMobile()
   const mainContentRef = useMainContentRef()
+  const { color } = useTheme()
+  const { isEnabled: isNetworkCutEnabled } = useFeatureFlag(
+    FeatureFlags.NETWORK_CUT_ENABLED
+  )
 
   const columns = isMobile
     ? (['contentName', 'date', 'value'] as SalesTableColumn[])
@@ -183,6 +194,19 @@ export const SalesTab = ({
 
   return (
     <div className={styles.container}>
+      {isNetworkCutEnabled ? (
+        <Flex gap='s' ph='l' pt='xl' alignItems='center'>
+          <IconMoneyBracket width={16} height={16} fill={color.neutral.n800} />
+          <Text variant='body' size='s' textAlign='left'>
+            {messages.networkSplitExplainer + ' '}
+            <ExternalTextLink to='' variant='visible'>
+              <Text variant='body' size='s' textAlign='left'>
+                {messages.learnMore}
+              </Text>
+            </ExternalTextLink>
+          </Text>
+        </Flex>
+      ) : null}
       {isEmpty ? (
         <NoSales />
       ) : (
