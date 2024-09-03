2e621bb6fff535c5ad9d305cf718712cbb1c1126
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2024-09-03 15:28:02 -0700

    [QA-1439] Don't block gated tracks from being remixes (#9620)

--- packages/web/src/components/edit/fields/RemixSettingsField/RemixSettingsMenuFields.tsx
+++ packages/web/src/components/edit/fields/RemixSettingsField/RemixSettingsMenuFields.tsx
@@ -2,13 +2,8 @@ import { useEffect } from 'react'
 
 import { useGetTrackByPermalink } from '@audius/common/api'
 import { useGatedContentAccess } from '@audius/common/hooks'
-import {
-  isContentCollectibleGated,
-  isContentUSDCPurchaseGated
-} from '@audius/common/models'
 import { accountSelectors } from '@audius/common/store'
 import { getPathFromTrackUrl } from '@audius/common/utils'
-import { Hint, IconQuestionCircle } from '@audius/harmony'
 import { useField } from 'formik'
 import { useSelector } from 'react-redux'
 import { useThrottle } from 'react-use'
@@ -17,7 +12,6 @@ import { Divider } from 'components/divider'
 import { TextField } from 'components/form-fields'
 
 import { SwitchRowField } from '../SwitchRowField'
-import { IS_STREAM_GATED, STREAM_CONDITIONS } from '../types'
 
 import styles from './RemixSettingsField.module.css'
 import { TrackInfo } from './TrackInfo'
@@ -35,17 +29,10 @@ const messages = {
     description:
       'Link your remix to the original track to increase visibility and credit the original artist.',
     linkLabel: 'Link to original track'
-  },
-  changeAvailabilityPrefix: 'Availablity is set to ',
-  changeAvailabilitySuffix: '. To enable these options, make your track free.',
-  premium: 'Premium (Pay-to-Unlock)',
-  collectibleGated: 'Collectible Gated',
-  specialAccess: 'Special Access'
+  }
 }
 
 export const RemixSettingsMenuFields = () => {
-  const [{ value: isStreamGated }] = useField(IS_STREAM_GATED)
-  const [{ value: streamConditions }] = useField(STREAM_CONDITIONS)
   const [{ value: trackUrl }] = useField(REMIX_LINK)
   const [, , { setValue: setCanRemixParent }] = useField(CAN_REMIX_PARENT)
   const permalink = useThrottle(getPathFromTrackUrl(trackUrl), 1000)
@@ -63,8 +50,6 @@ export const RemixSettingsMenuFields = () => {
 
   const [, , { setValue: setParentTrackId }] = useField('parentTrackId')
 
-  const isUSDCPurchaseGated = isContentUSDCPurchaseGated(streamConditions)
-
   useEffect(() => {
     setParentTrackId(trackId)
     setCanRemixParent(canRemixParent)
@@ -72,16 +57,10 @@ export const RemixSettingsMenuFields = () => {
 
   return (
     <div className={styles.fields}>
-      {isUSDCPurchaseGated ? (
-        <Hint icon={IconQuestionCircle}>
-          {`${messages.changeAvailabilityPrefix} ${messages.premium}${messages.changeAvailabilitySuffix}`}
-        </Hint>
-      ) : null}
       <SwitchRowField
         name={IS_REMIX}
         header={messages.remixOf.header}
         description={messages.remixOf.description}
-        disabled={isStreamGated}
       >
         <TextField name={REMIX_LINK} label={messages.remixOf.linkLabel} />
         {track ? <TrackInfo trackId={track.track_id} /> : null}
@@ -89,23 +68,11 @@ export const RemixSettingsMenuFields = () => {
 
       <Divider />
 
-      {isStreamGated && !isUSDCPurchaseGated ? (
-        <Hint icon={IconQuestionCircle}>{`${
-          messages.changeAvailabilityPrefix
-        } ${
-          isContentCollectibleGated(streamConditions)
-            ? messages.collectibleGated
-            : messages.specialAccess
-        }${messages.changeAvailabilitySuffix}`}</Hint>
-      ) : null}
       <SwitchRowField
         name={SHOW_REMIXES}
         header={messages.hideRemix.header}
         description={messages.hideRemix.description}
         inverted
-        {...(isStreamGated && !isUSDCPurchaseGated
-          ? { checked: true, disabled: true }
-          : {})}
       />
     </div>
   )
