e613a0b860acb187e195a9be7203b66d39fcd9de
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2024-09-18 18:23:08 -0700

    [PAY-3215] ChatBlastCTA in own followers and supporters screens (#9793)

--- packages/mobile/src/screens/user-list-screen/FollowersScreen.tsx
+++ packages/mobile/src/screens/user-list-screen/FollowersScreen.tsx
@@ -1,13 +1,20 @@
 import { useCallback } from 'react'
 
+import { useGetCurrentUserId } from '@audius/common/api'
+import { useFeatureFlag } from '@audius/common/hooks'
+import { FeatureFlags } from '@audius/common/services'
 import {
   followersUserListActions,
   followersUserListSelectors
 } from '@audius/common/store'
+import { ChatBlastAudience } from '@audius/sdk'
+import { css } from '@emotion/native'
 import { useDispatch } from 'react-redux'
 
-import { IconUserFollowers } from '@audius/harmony-native'
+import { Box, IconUserFollowers } from '@audius/harmony-native'
 import { useProfileRoute } from 'app/hooks/useRoute'
+
+import { ChatBlastWithAudienceCTA } from '../chat-screen/ChatBlastFollowersCTA'
 
 import { UserList } from './UserList'
 import { UserListScreen } from './UserListScreen'
@@ -21,19 +28,36 @@ const messages = {
 export const FollowersScreen = () => {
   const { params } = useProfileRoute<'Followers'>()
   const { userId } = params
+  const { data: currentUserId } = useGetCurrentUserId({})
   const dispatch = useDispatch()
 
   const handleSetFollowers = useCallback(() => {
     dispatch(setFollowers(userId))
   }, [dispatch, userId])
+  const { isEnabled: isOneToManyDMsEnabled } = useFeatureFlag(
+    FeatureFlags.ONE_TO_MANY_DMS
+  )
 
   return (
     <UserListScreen title={messages.title} titleIcon={IconUserFollowers}>
-      <UserList
-        userSelector={getUserList}
-        tag='FOLLOWERS'
-        setUserList={handleSetFollowers}
-      />
+      <>
+        <UserList
+          userSelector={getUserList}
+          tag='FOLLOWERS'
+          setUserList={handleSetFollowers}
+        />
+        {isOneToManyDMsEnabled && currentUserId === userId ? (
+          <Box
+            style={css({
+              position: 'absolute',
+              bottom: 0,
+              width: '100%'
+            })}
+          >
+            <ChatBlastWithAudienceCTA audience={ChatBlastAudience.FOLLOWERS} />
+          </Box>
+        ) : null}
+      </>
     </UserListScreen>
   )
 }
