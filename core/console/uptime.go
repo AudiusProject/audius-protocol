682281fb234b7740101829565bd679122a2b3d2d
Alec Savvy <alecsavvy@outlook.com>
2024-09-24 08:18:01 -0600

    uptime page

--- core/console/uptime.go
+++ core/console/uptime.go
@@ -3,5 +3,5 @@ package console
 import "github.com/labstack/echo/v4"
 
 func (con *Console) uptimeFragment(c echo.Context) error {
-	return c.String(200, "sla auditor will go here :D")
+	return con.views.RenderUptimeView(c)
 }
