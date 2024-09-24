3944e963eed0b6faf53e717d4ee71a126c0227e9
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-09-24 17:52:18 -0400

    [PAY-3459] Chat blast fan-out updates unread count

--- comms/discovery/rpcz/chat_blast.go
+++ comms/discovery/rpcz/chat_blast.go
@@ -130,6 +130,12 @@ func chatBlast(tx *sqlx.Tx, userId int32, ts time.Time, params schema.ChatBlastR
 		FROM targ
 		WHERE chat_allowed(from_user_id, to_user_id)
 		ON conflict do nothing
+	),
+	update_unread_count AS (
+    UPDATE chat_member
+    SET unread_count = unread_count + 1
+    WHERE chat_id IN (SELECT chat_id FROM targ)
+		AND user_id IN (SELECT to_user_id FROM targ)
 	)
 	SELECT chat_id FROM targ;
 	`
