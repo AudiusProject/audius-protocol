fef15906d07e1d356c12243b2e41acde831d043d
endline <endline>
2024-09-25 17:10:24 -0600

    Run core as goroutine within audiusd

--- cmd/audiusd/main.go
+++ cmd/audiusd/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/AudiusProject/audius-protocol/core/common"
 	"github.com/AudiusProject/audius-protocol/core/core_pkg"
 )
 
 func main() {
-	// run forever, no crashloops
 	logger := common.NewLogger(nil)
-	if err := core_pkg.Run(logger); err != nil {
-		logger.Errorf("fatal core error: %v", err)
-	}
-	select {}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// run core
+	go func() {
+		if err := core_pkg.Run(ctx, logger); err != nil {
+			logger.Errorf("fatal core error: %v", err)
+			cancel()
+		}
+	}()
+
+	// TODO:
+	//   go mediorum()
+	//   go uptime()
+
+	<-sigChan
+	logger.Info("Received termination signal, shutting down...")
+
+	cancel()
+
+	<-ctx.Done() // run forever, no crashloops
+	logger.Info("Shutdown complete")
 }
--- core/core_pkg/pkg.go
+++ core/core_pkg/pkg.go
@@ -1,7 +1,12 @@
 package core_pkg
 
-import "github.com/AudiusProject/audius-protocol/core/common"
+import (
+	"context"
 
-func Run(logger *common.Logger) error {
-	return run(logger)
+	"github.com/AudiusProject/audius-protocol/core/common"
+)
+
+// TODO: wrap things for an easier diff leading up to pkg re-org
+func Run(ctx context.Context, logger *common.Logger) error {
+	return run(ctx, logger)
 }
