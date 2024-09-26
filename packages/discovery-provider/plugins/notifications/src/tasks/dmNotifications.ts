4f909ce25526f31e8e1cceba13ea76838de7f178
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-09-26 14:37:51 -0400

    [PAY-3344] Chat blast notifications

--- packages/discovery-provider/plugins/notifications/src/tasks/dmNotifications.ts
+++ packages/discovery-provider/plugins/notifications/src/tasks/dmNotifications.ts
@@ -1,4 +1,5 @@
 import { Knex } from 'knex'
+import { HashId } from '@audius/sdk'
 import type { RedisClientType } from 'redis'
 import { config } from './../config'
 import { logger } from './../logger'
@@ -115,6 +116,120 @@ async function getUnreadReactions(
     .andWhereRaw('chat_message_reactions.user_id != chat_member.user_id')
 }
 
+async function getNewBlasts(
+  discoveryDB: Knex,
+  minTimestamp: Date,
+  maxTimestamp: Date
+): Promise<DMNotification[]> {
+  console.log('REED at top of getNewBlasts')
+  const messages = await discoveryDB
+    .with('all_new', function () {
+      this.select(
+        'blast.from_user_id as sender_user_id',
+        'users.id as receiver_user_id',
+        'blast.created_at as timestamp'
+      )
+        .from('chat_blast as blast')
+        .where(function () {
+          // follower_audience
+          this.whereIn('from_user_id', function () {
+            this.select('followee_user_id')
+              .from('follows')
+              .where('blast.audience', 'follower_audience')
+              .andWhereRaw('follows.followee_user_id = blast.from_user_id')
+              .andWhereRaw('follows.follower_user_id = users.id')
+              .andWhere('follows.is_delete', false)
+              .andWhereRaw('follows.created_at < blast.created_at')
+          })
+            // tipper_audience
+            .orWhereIn('from_user_id', function () {
+              this.select('receiver_user_id')
+                .from('user_tips as tip')
+                .where('blast.audience', 'tipper_audience')
+                .andWhereRaw('receiver_user_id = blast.from_user_id')
+                .andWhereRaw('sender_user_id = users.id')
+                .andWhereRaw('tip.created_at < blast.created_at')
+            })
+            // remixer_audience
+            .orWhereIn('from_user_id', function () {
+              this.select('og.owner_id')
+                .from('tracks as t')
+                .join('remixes', 'remixes.child_track_id', 't.track_id')
+                .join('tracks as og', 'remixes.parent_track_id', 'og.track_id')
+                .where('blast.audience', 'remixer_audience')
+                .andWhereRaw('og.owner_id = blast.from_user_id')
+                .andWhereRaw('t.owner_id = users.id')
+                .andWhere(function () {
+                  this.whereNull('blast.audience_content_id').orWhere(
+                    function () {
+                      this.where(
+                        'blast.audience_content_type',
+                        'track'
+                      ).andWhereRaw('blast.audience_content_id = og.track_id')
+                    }
+                  )
+                })
+            })
+            // customer_audience
+            .orWhereIn('from_user_id', function () {
+              this.select('seller_user_id')
+                .from('usdc_purchases as p')
+                .where('blast.audience', 'customer_audience')
+                .andWhereRaw('p.seller_user_id = blast.from_user_id')
+                .andWhereRaw('p.buyer_user_id = users.id')
+                .andWhere(function () {
+                  this.whereNull('blast.audience_content_id').orWhere(
+                    function () {
+                      this.whereRaw(
+                        'blast.audience_content_type = p.content_type::text'
+                      ).andWhereRaw('blast.audience_content_id = p.content_id')
+                    }
+                  )
+                })
+            })
+        })
+    })
+    .select(
+      'all_new.sender_user_id',
+      'all_new.receiver_user_id',
+      'all_new.timestamp'
+    )
+    .from('all_new')
+    .join(
+      'users',
+      'users.user_id',
+      discoveryDB.raw('chat_allowed(from_user_id, users.user_id)')
+    )
+    // Javascript dates are limited to 3 decimal places (milliseconds). Truncate the postgresql timestamp to match.
+    .whereRaw(
+      `date_trunc('milliseconds', chat_message_reactions.updated_at) > greatest(chat_member.last_active_at, ?)`,
+      [minTimestamp.toISOString()]
+    )
+    .andWhereRaw(
+      `date_trunc('milliseconds', chat_message_reactions.updated_at) <= ? `,
+      [maxTimestamp.toISOString()]
+    )
+  console.log('REED before calculating chatId', { messages })
+  const messages2 = messages.map((message) => {
+    let chatId: string
+    if (message.sender_user_id < message.receiver_user_id) {
+      chatId =
+        HashId.parse(message.sender_user_id) +
+        ':' +
+        HashId.parse(message.receiver_user_id)
+    } else {
+      chatId =
+        HashId.parse(message.receiver_user_id) +
+        ':' +
+        HashId.parse(message.sender_user_id)
+    }
+    return { ...message, chatId }
+  })
+  console.log('REED after calculating chatId', { messages2 })
+
+  return messages2
+}
+
 function setLastIndexedTimestamp(
   redis: RedisClientType,
   redisKey: string,
@@ -137,6 +252,7 @@ enum DMPhase {
   START = 'START',
   GET_UNREAD_MESSAGES = 'GET_UNREAD_MESSAGES',
   GET_UNREAD_REACTIONS = 'GET_UNREAD_REACTIONS',
+  GET_NEW_BLASTS = 'GET_NEW_BLASTS',
   PUSH_NOTIFICATIONS = 'PUSH_NOTIFICATIONS',
   FINSH = 'FINSH'
 }
@@ -181,6 +297,20 @@ export async function sendDMNotifications(
       )
     }
 
+    timer.logMessage(DMPhase.GET_NEW_BLASTS)
+    const newBlasts = await getNewBlasts(
+      discoveryDB,
+      cursors.minMessageTimestamp,
+      cursors.maxTimestamp
+    )
+    if (newBlasts.length > 0) {
+      console.log(
+        `dmNotifications: new chat blast notifications: ${JSON.stringify(
+          newBlasts
+        )}`
+      )
+    }
+
     // Convert to notifications
     const messageNotifications = unreadMessages.map(
       (message) => new Message(discoveryDB, identityDB, message)
@@ -188,8 +318,12 @@ export async function sendDMNotifications(
     const reactionNotifications = unreadReactions.map(
       (reaction) => new MessageReaction(discoveryDB, identityDB, reaction)
     )
-    const notifications: Array<Message | MessageReaction> =
-      messageNotifications.concat(reactionNotifications)
+    const blastNotifications = newBlasts.map(
+      (blast) => new Message(discoveryDB, identityDB, blast)
+    )
+    const notifications: Array<Message | MessageReaction> = messageNotifications
+      .concat(reactionNotifications)
+      .concat(blastNotifications)
 
     // Sort notifications by timestamp (asc)
     notifications.sort(notificationTimestampComparator)
