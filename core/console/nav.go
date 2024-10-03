02cabc30978c04ddeff2b6db9a4e6464c15429d7
Alec Savvy <alecsavvy@outlook.com>
2024-10-03 06:46:57 -0600

    fix delayed sync status

--- core/console/nav.go
+++ core/console/nav.go
@@ -9,5 +9,15 @@ import (
 func (con *Console) navChainData(c echo.Context) error {
 	totalBlocks := fmt.Sprint(con.state.totalBlocks)
 	totalTxs := fmt.Sprint(con.state.totalTransactions)
+
+	// once synced don't ask comet again
+	if !con.state.isSyncing {
+		return con.views.RenderNavChainData(c, totalBlocks, totalTxs, con.state.isSyncing)
+	}
+
+	status, err := con.rpc.Status(c.Request().Context())
+	if err == nil {
+		con.state.isSyncing = status.SyncInfo.CatchingUp
+	}
 	return con.views.RenderNavChainData(c, totalBlocks, totalTxs, con.state.isSyncing)
 }
