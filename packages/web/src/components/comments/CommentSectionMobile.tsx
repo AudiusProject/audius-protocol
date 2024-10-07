f659697ea080d9ab92c4dd8275f11cd8708a6d9b
sliptype <sliptype@gmail.com>
2024-10-07 11:23:44 -0500

    Fix lint

--- packages/web/src/components/comments/CommentSectionMobile.tsx
+++ packages/web/src/components/comments/CommentSectionMobile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { useGetTrackById } from '@audius/common/api'
 import { useCurrentCommentSection } from '@audius/common/context'
