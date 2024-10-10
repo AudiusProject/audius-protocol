ebfb1682f35eaa09f2db4fa6ea7225cfef7c7584
KJ Shanks <kyle.j.shanks@gmail.com>
2024-10-10 14:13:46 -0400

    small update

--- packages/mobile/src/components/composer-input/ComposerInput.tsx
+++ packages/mobile/src/components/composer-input/ComposerInput.tsx
@@ -258,7 +258,10 @@ export const ComposerInput = forwardRef(function ComposerInput(
       setValue((value) => {
         const textBeforeMention = value.slice(0, autocompleteRange[0])
         const textAfterMention = value.slice(autocompleteRange[1])
-        return `${textBeforeMention}${mentionText} ${textAfterMention}`
+        const fillText =
+          mentionText + (textAfterMention.startsWith(' ') ? '' : ' ')
+
+        return `${textBeforeMention}${fillText}${textAfterMention}`
       })
       setIsAutocompleteActive(false)
     },
