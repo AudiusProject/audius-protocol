14fba3bf3ac71d3c82d89131d313aeac695f711f
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-10-16 16:46:35 -0700

    Fix typo in migration (#10097)

--- packages/discovery-provider/ddl/migrations/0095_shorten_challenge_ids.sql
+++ packages/discovery-provider/ddl/migrations/0095_shorten_challenge_ids.sql
@@ -1,15 +1,15 @@
 BEGIN;
 
-UPDATE challenges SET challenge_id = 'p' WHERE challenge_id = 'profile-completion';
-UPDATE challenges SET challenge_id = 'l' WHERE challenge_id = 'listen-streak';
-UPDATE challenges SET challenge_id = 'u' WHERE challenge_id = 'track-upload';
-UPDATE challenges SET challenge_id = 'r' WHERE challenge_id = 'referrals';
-UPDATE challenges SET challenge_id = 'rv' WHERE challenge_id = 'ref-v';
-UPDATE challenges SET challenge_id = 'rd' WHERE challenge_id = 'referred';
-UPDATE challenges SET challenge_id = 'v' WHERE challenge_id = 'connect-verified';
-UPDATE challenges SET challenge_id = 'm' WHERE challenge_id = 'mobile-install';
-UPDATE challenges SET challenge_id = 'ft' WHERE challenge_id = 'send-first-tip';
-UPDATE challenges SET challenge_id = 'fp' WHERE challenge_id = 'first-playlist';
+UPDATE challenges SET id = 'p' WHERE id = 'profile-completion';
+UPDATE challenges SET id = 'l' WHERE id = 'listen-streak';
+UPDATE challenges SET id = 'u' WHERE id = 'track-upload';
+UPDATE challenges SET id = 'r' WHERE id = 'referrals';
+UPDATE challenges SET id = 'rv' WHERE id = 'ref-v';
+UPDATE challenges SET id = 'rd' WHERE id = 'referred';
+UPDATE challenges SET id = 'v' WHERE id = 'connect-verified';
+UPDATE challenges SET id = 'm' WHERE id = 'mobile-install';
+UPDATE challenges SET id = 'ft' WHERE id = 'send-first-tip';
+UPDATE challenges SET id = 'fp' WHERE id = 'first-playlist';
 
 UPDATE user_challenges SET challenge_id = 'p' WHERE challenge_id = 'profile-completion';
 UPDATE user_challenges SET challenge_id = 'l' WHERE challenge_id = 'listen-streak';
