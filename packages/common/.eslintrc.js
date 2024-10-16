3144eac6feb9273f0f4fae580ef52270a34940fb
Randy Schott <1815175+schottra@users.noreply.github.com>
2024-10-16 12:04:26 -0400

    [PAY-3424] Restrict imports from SDK 'dist' folder in client code (#10043)

--- packages/common/.eslintrc.js
+++ packages/common/.eslintrc.js
@@ -9,6 +9,18 @@ module.exports = {
         message:
           'Do NOT use `Promise.allSettled` as it will be undefined. Use `allSettled` from `utils/allSettled.ts` instead.'
       }
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['@audius/sdk/dist*'],
+            message:
+              'Do not import from the SDK dist folder. If needed, update SDK to export the item you wish to use.'
+          }
+        ]
+      }
     ]
   }
 }
