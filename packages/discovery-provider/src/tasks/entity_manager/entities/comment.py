8b1baabb0b1da67a72e4cacea007fc1b6e2d0b00
isaac <isaac@audius.co>
2024-10-16 14:15:30 -0700

    descript

--- packages/discovery-provider/src/tasks/entity_manager/entities/comment.py
+++ packages/discovery-provider/src/tasks/entity_manager/entities/comment.py
@@ -46,7 +46,7 @@ def validate_comment_tx(params: ManageEntityParameters):
 
     mentions = params.metadata.get("mentions")
     if mentions and not all(isinstance(i, int) for i in mentions):
-        raise IndexingValidationError(f"Mentions {mentions} must be a number")
+        raise IndexingValidationError(f"Mentions {mentions} must be a list of numbers")
 
 
 def create_comment(params: ManageEntityParameters):
