da36d7e70e7990d4af272bd49505ad52fe2b52f9
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-10-16 17:26:51 -0400

    profile pic is pressable

--- packages/mobile/src/screens/chat-screen/UserChatHeader.tsx
+++ packages/mobile/src/screens/chat-screen/UserChatHeader.tsx
@@ -1,23 +1,39 @@
 import type { User } from '@audius/common/models'
 import { css } from '@emotion/native'
+import { TouchableOpacity } from 'react-native'
 
 import { Flex } from '@audius/harmony-native'
 import { ProfilePicture } from 'app/components/core'
 import { UserLink } from 'app/components/user-link'
+import { useNavigation } from 'app/hooks/useNavigation'
+
+import type { AppTabScreenParamList } from '../app-screen'
 
 export const UserChatHeader = ({ user }: { user: User }) => {
+  const navigation = useNavigation<AppTabScreenParamList>()
   return (
     <Flex
-      direction='row'
-      justifyContent='center'
-      alignItems='center'
       style={css({
         maxWidth: '70%'
       })}
-      gap='s'
     >
-      <ProfilePicture userId={user.user_id} size='small' strokeWidth='thin' />
-      <UserLink userId={user.user_id} textVariant='title' />
+      <TouchableOpacity
+        onPress={() => navigation.push('Profile', { id: user.user_id })}
+      >
+        <Flex
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+          gap='s'
+        >
+          <ProfilePicture
+            userId={user.user_id}
+            size='small'
+            strokeWidth='thin'
+          />
+          <UserLink userId={user.user_id} textVariant='title' />
+        </Flex>
+      </TouchableOpacity>
     </Flex>
   )
 }
