3c5f69e45fcbc46b4235a743f8deb4e332331197
JD Francis <jd@audius.co>
2024-10-17 13:10:57 -0500

    Fix mobile web skeletons

--- packages/web/src/components/comments/CommentSkeletons.tsx
+++ packages/web/src/components/comments/CommentSkeletons.tsx
@@ -1,34 +1,29 @@
-import { Divider, Flex, Paper, Skeleton } from '@audius/harmony'
-
-import { CommentHeader } from './CommentHeader'
+import { Flex, Skeleton } from '@audius/harmony'
 
 const CommentBlockSkeleton = () => (
   <Flex direction='row' gap='s' alignItems='center' p='l'>
-    <Skeleton w={40} h={40} css={{ borderRadius: 100 }} />
-    <Flex gap='s'>
-      <Skeleton w={20} h={240} />
-      <Skeleton w={20} h={160} />
+    <Skeleton w={40} h={40} css={{ borderRadius: 100, flexShrink: 0 }} />
+    <Flex gap='s' direction='column'>
+      <Skeleton h={20} w={240} />
+      <Skeleton h={20} w={160} />
     </Flex>
   </Flex>
 )
 
 // TODO: mobile can also go in here
 export const CommentSkeletons = () => (
-  <Flex gap='l' direction='column' w='100%' alignItems='flex-start'>
-    <CommentHeader isLoading />
-    <Paper p='xl' w='100%' direction='column' gap='xl'>
-      <Flex
-        gap='s'
-        w='100%'
-        h='60px'
-        alignItems='center'
-        justifyContent='center'
-      >
-        <Skeleton w='40px' h='40px' css={{ borderRadius: '100%' }} />
-        <Skeleton w='100%' h='60px' />
-      </Flex>
-      <Divider color='default' orientation='horizontal' />
-      <CommentBlockSkeleton />
-    </Paper>
+  <Flex
+    gap='s'
+    direction='column'
+    w='100%'
+    h='100%'
+    alignItems='flex-start'
+    mt='s'
+  >
+    <CommentBlockSkeleton />
+    <CommentBlockSkeleton />
+    <CommentBlockSkeleton />
+    <CommentBlockSkeleton />
+    <CommentBlockSkeleton />
   </Flex>
 )
