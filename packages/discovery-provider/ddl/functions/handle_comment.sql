d7154ab9a0b4b93936e1e747eaf020dc4752cbaa
Dylan Jeffers <dylan@audius.co>
2024-10-17 15:51:24 +0200

    Fix aggregates

---
+++ packages/discovery-provider/ddl/functions/handle_comment.sql
@@ -0,0 +1,44 @@
+create or replace function handle_comment() returns trigger as $$
+declare
+  entity_owner_id int;
+  parent_comment_id int;
+  parent_comment_owner_id int;
+  notification_muted boolean;
+begin
+  if new.entity_type = 'Track' then
+    insert into aggregate_track (track_id) 
+    values (new.entity_id) 
+    on conflict do nothing;
+  end if;
+
+  -- update agg track
+  if new.entity_type = 'Track' then
+    update aggregate_track 
+    set comment_count = (
+      select count(*)
+      from comments c
+      where
+          c.is_delete is false
+          and c.is_visible is true
+          and c.entity_type = new.entity_type
+          and c.entity_id = new.entity_id
+    )
+    where track_id = new.entity_id;
+  end if;
+
+  return null;
+
+exception
+    when others then
+      raise warning 'An error occurred in %: %', tg_name, sqlerrm;
+      return null;
+end;
+$$ language plpgsql;
+
+do $$ begin
+  create trigger on_comment
+  after insert on comments
+  for each row execute procedure handle_comment();
+exception
+  when others then null;
+end $$;
--- packages/discovery-provider/ddl/migrations/0100_remove_comment_triggers.sql
+++ packages/discovery-provider/ddl/migrations/0100_remove_comment_triggers.sql
@@ -1,3 +1,2 @@
-drop trigger if exists on_comment on comments;
 drop trigger if exists on_comment_thread on comment_threads;
 drop trigger if exists on_comment_mention on comment_mentions;
