47e2904ce89166320ed616d767cbbd8901550fbb
Dylan Jeffers <dylan@audius.co>
2024-10-21 14:37:49 +0200

    Remove gap

--- packages/mobile/src/components/lineup/Lineup.tsx
+++ packages/mobile/src/components/lineup/Lineup.tsx
@@ -545,7 +545,6 @@ export const Lineup = ({
       <SectionList
         {...listProps}
         {...pullToRefreshProps}
-        contentContainerStyle={{ gap: 12 }}
         ref={ref}
         onScroll={handleScroll}
         ListHeaderComponent={
