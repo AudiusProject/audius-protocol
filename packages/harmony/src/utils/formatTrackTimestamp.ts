02471e1b080201e9445bd6e870eb1744802ce4dd
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2024-10-25 15:38:27 -0700

    fix padding for duration minutes (#10237)

--- packages/harmony/src/utils/formatTrackTimestamp.ts
+++ packages/harmony/src/utils/formatTrackTimestamp.ts
@@ -7,7 +7,8 @@ export const formatTrackTimestamp = (timestampSecs: number) => {
   const minutes = Math.floor((timestampSecs / 60) % 60)
   const seconds = `${Math.floor(timestampSecs) % 60}`.padStart(2, '0')
   if (hours > 0) {
-    return `${hours}:${minutes}:${seconds}`
+    const paddedMinutes = minutes.toString().padStart(2, '0')
+    return `${hours}:${paddedMinutes}:${seconds}`
   } else {
     return `${minutes}:${seconds}`
   }
