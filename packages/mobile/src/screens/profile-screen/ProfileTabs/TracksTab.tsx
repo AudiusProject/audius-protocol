ff6a9969bc22ad30c9f092270d56243a1cf83eca
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-10-30 17:56:37 -0400

    WIP

--- packages/mobile/src/screens/profile-screen/ProfileTabs/TracksTab.tsx
+++ packages/mobile/src/screens/profile-screen/ProfileTabs/TracksTab.tsx
@@ -6,6 +6,7 @@ import {
   profilePageSelectors
 } from '@audius/common/store'
 
+import { ScreenSecondaryContent } from 'app/components/core/Screen/ScreenSecondaryContent'
 import { Lineup } from 'app/components/lineup'
 
 import { EmptyProfileTile } from '../EmptyProfileTile'
