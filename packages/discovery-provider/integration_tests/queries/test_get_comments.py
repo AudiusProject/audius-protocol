4c9b2e7e16db631b5833144e799be9361d2f70e7
sliptype <sliptype@gmail.com>
2024-10-31 13:38:59 -0500

    Reapply "[PAY-3516] Remove is_stream_gated and is_download_gated (#101… (#10295)
    This reverts commit 13c668e343672afaa203a23d9202da69cc080a62.

--- packages/discovery-provider/integration_tests/queries/test_get_comments.py
+++ packages/discovery-provider/integration_tests/queries/test_get_comments.py
@@ -45,7 +45,6 @@ test_entities = {
             "track_id": 1,
             "title": "a",
             "owner_id": 10,
-            "is_stream_gated": True,
             "stream_conditions": {
                 "usdc_purchase": {
                     "price": 100,
