9e64d8a979eeeba47e28e34cc61460b18bafd7dc
Dylan Jeffers <dylan@audius.co>
2024-10-31 21:26:09 +0100

    [C-5342] Upgrade react-native to 0.75

--- packages/mobile/src/services/createPrivateKey.ts
+++ packages/mobile/src/services/createPrivateKey.ts
@@ -1,4 +1,5 @@
-import { scrypt } from 'react-native-fast-crypto'
+import { scrypt } from 'scrypt-js'
+
 /**
  * Given a user encryptStr and initialization vector, generate a private key
  * @param encryptStr String to encrypt (can be user password or some kind of lookup key)
@@ -11,7 +12,19 @@ export const createPrivateKey = async (encryptStr: string, ivHex: string) => {
   const dkLen = 32
   const encryptStrBuffer = Buffer.from(encryptStr)
   const ivBuffer = Buffer.from(ivHex)
-  const keyBuffer = await scrypt(encryptStrBuffer, ivBuffer, N, r, p, dkLen)
+
+  // scrypt-js expects Uint8Array inputs
+  const keyBuffer = await scrypt(
+    new Uint8Array(encryptStrBuffer),
+    new Uint8Array(ivBuffer),
+    N,
+    r,
+    p,
+    dkLen
+  )
+
+  // Convert the Uint8Array to hex string
   const keyHex = Buffer.from(keyBuffer).toString('hex')
+
   return { keyHex, keyBuffer }
 }
