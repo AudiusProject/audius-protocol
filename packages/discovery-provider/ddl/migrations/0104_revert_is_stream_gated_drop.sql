6485f3885c7863a19567c444ffba3a731e58be66
Isaac Solo <isaac@audius.co>
2024-10-31 15:01:03 -0700

    Optimize revert stream gated drop (#10304)

--- packages/discovery-provider/ddl/migrations/0104_revert_is_stream_gated_drop.sql
+++ packages/discovery-provider/ddl/migrations/0104_revert_is_stream_gated_drop.sql
@@ -1,11 +1,25 @@
 begin;
-
-alter table tracks add column if not exists is_stream_gated boolean default false;
-alter table tracks add column if not exists is_download_gated boolean default false;
-alter table playlists add column if not exists is_stream_gated boolean default false;
-
-update tracks set is_stream_gated = stream_conditions is not null;
-update tracks set is_download_gated = download_conditions is not null;
-update playlists set is_stream_gated = stream_conditions is not null;
-
+alter table tracks disable trigger on_track;
+alter table tracks disable trigger trg_tracks;
+alter table playlists disable trigger on_playlist;
+alter table playlists disable trigger trg_playlists;
+alter table tracks
+add column if not exists is_stream_gated boolean default false;
+alter table tracks
+add column if not exists is_download_gated boolean default false;
+alter table playlists
+add column if not exists is_stream_gated boolean default false;
+update tracks
+set is_stream_gated = true
+where stream_conditions is not null;
+update tracks
+set is_download_gated = true
+where download_conditions is not null;
+update playlists
+set is_stream_gated = true
+where stream_conditions is not null;
+alter table tracks enable trigger on_track;
+alter table tracks enable trigger trg_tracks;
+alter table playlists enable trigger on_playlist;
+alter table playlists enable trigger trg_playlists;
 commit;
