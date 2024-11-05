2cc2e5a21ef61779033e581d4cea861c346fdf34
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-11-04 23:49:45 -0800

    Fix some audius-cmd instructions

--- packages/commands/src/follow.mjs
+++ packages/commands/src/follow.mjs
@@ -11,6 +11,7 @@ program.command("follow")
     const audiusLibs = await initializeAudiusLibs(from);
 
     try {
+      audiusLibs.EntityManager.setCurrentUserId(audiusLibs.getCurrentUser().userId)
       const response = await audiusLibs.EntityManager.followUser(userId);
 
       if (response.error) {
