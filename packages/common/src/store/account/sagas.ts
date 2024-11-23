8054b8053b1ea03a8f623396a6e486a8f98a0fde
isaac <isaac@audius.co>
2024-11-22 16:31:24 -0800

    type

--- packages/common/src/store/account/sagas.ts
+++ packages/common/src/store/account/sagas.ts
@@ -2,7 +2,7 @@ import { SagaIterator } from 'redux-saga'
 import { call, put, select, takeLatest } from 'typed-redux-saga'
 
 import { userApiFetchSaga } from '~/api/user'
-import { AccountUserMetadata, Id, Status, User } from '~/models'
+import { AccountUserMetadata, Id, Status } from '~/models'
 import { recordIP } from '~/services/audius-backend/RecordIP'
 import { accountActions, accountSelectors } from '~/store/account'
 import { getUserId, getUserHandle } from '~/store/account/selectors'
