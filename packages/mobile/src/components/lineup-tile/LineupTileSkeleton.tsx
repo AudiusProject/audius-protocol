f9901609afed2493a5450a81b7a18c58b6d680ff
Raymond Jacobson <ray@audius.co>
2024-11-22 20:04:39 -0800

    PR feedback

--- packages/mobile/src/components/lineup-tile/LineupTileSkeleton.tsx
+++ packages/mobile/src/components/lineup-tile/LineupTileSkeleton.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native'
 
-import { Flex } from '@audius/harmony-native'
+import { Box, Flex } from '@audius/harmony-native'
 import Skeleton from 'app/components/skeleton'
 
 import { LineupTileActionButtons } from './LineupTileActionButtons'
@@ -9,15 +9,14 @@ import { LineupTileRoot } from './LineupTileRoot'
 export const LineupTileSkeleton = () => {
   return (
     <LineupTileRoot>
-      <Flex direction='row' alignItems='center' style={{ padding: 8 }} gap='m'>
+      <Flex direction='row' alignItems='center' p='s' gap='m'>
         <Skeleton height={72} width={72} />
         <Flex gap='s' flex={1}>
           <Skeleton width='80%' height={20} />
           <Skeleton width='60%' height={20} />
         </Flex>
       </Flex>
-
-      <View style={{ height: 24 }} />
+      <Box h='l' />
       <View>
         <LineupTileActionButtons disabled />
       </View>
