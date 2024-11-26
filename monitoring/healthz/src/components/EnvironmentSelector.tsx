09fbdf47be725d8079988e7c21f8bbac8d0a08bf
Daniel Phelps <daniel@audius.co>
2024-11-26 14:07:47 -0800

    Rudimentary core health check, fix rollup page bug

--- monitoring/healthz/src/components/EnvironmentSelector.tsx
+++ monitoring/healthz/src/components/EnvironmentSelector.tsx
@@ -1,20 +1,20 @@
 import { useSearchParams } from 'react-router-dom'
 
 const isStageParam = 'isStaging'
-const isContentParam = 'isContent'
+const nodeTypeParam = 'nodeType'
 
 export function useEnvironmentSelection(): [
   'staging' | 'prod',
-  'content' | 'discovery'
+  'content' | 'discovery' | 'core'
 ] {
   let [searchParams] = useSearchParams()
 
   const isStage = !!searchParams.get(isStageParam)
-  const isContent = !!searchParams.get(isContentParam)
+  const nodeType = searchParams.get(nodeTypeParam) || 'discovery'
 
   return [
     isStage ? 'staging' : 'prod',
-    isContent ? 'content' : 'discovery',
+    nodeType,
   ]
 }
 
@@ -22,12 +22,20 @@ export function EnvironmentSelector() {
   let [searchParams, setSearchParams] = useSearchParams()
 
   const isStage = !!searchParams.get(isStageParam)
-  const isContent = !!searchParams.get(isContentParam)
+  const nodeType = searchParams.get(nodeTypeParam) || 'discovery'
 
   function toggleParam(name: string, value: '0' | '1') {
     setSearchParams((p) => {
       if (value == '0') p.delete(name)
       else p.set(name, value)
+      return p
+    })
+  }
+
+  function setNodeParam(value: 'content' | 'discovery' | 'core') {
+    setSearchParams((p) => {
+      if (value == 'discovery') p.delete(nodeTypeParam)
+      else p.set(nodeTypeParam, value)
       return p
     })
   }
@@ -50,16 +58,22 @@ export function EnvironmentSelector() {
       </div>
       <div className="flex">
         <button
-          className={`px-4 py-2 ${isContent ? 'bg-gray-200 text-black' : 'bg-purple-300 text-white'}`}
-          onClick={() => toggleParam(isContentParam, '0')}
+          className={`px-4 py-2 ${nodeType == 'discovery' ? 'bg-purple-300 text-white' : 'bg-gray-200 text-black'}`}
+          onClick={() => setNodeParam('discovery')}
         >
           Discovery
         </button>
         <button
-          className={`px-4 py-2 ${isContent ? 'bg-purple-300 text-white' : 'bg-gray-200 text-black'}`}
-          onClick={() => toggleParam(isContentParam, '1')}
+          className={`px-4 py-2 ${nodeType == 'content' ? 'bg-purple-300 text-white' : 'bg-gray-200 text-black'}`}
+          onClick={() => setNodeParam('content')}
         >
           Content
+        </button>
+        <button
+          className={`px-4 py-2 ${nodeType == 'core' ? 'bg-purple-300 text-white' : 'bg-gray-200 text-black'}`}
+          onClick={() => setNodeParam('core')}
+        >
+          Core
         </button>
       </div>
     </div>
