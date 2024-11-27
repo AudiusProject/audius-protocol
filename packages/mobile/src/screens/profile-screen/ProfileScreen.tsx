3cc04ddb91e3794d6a387581501621d5487bba3b
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-11-27 16:48:59 +0700

    Revert "Fix profile screen content not rendering (#10384)"
    This reverts commit ed1d2b1d8a341829896cfd64e150f2605c56d898.

--- packages/mobile/src/screens/profile-screen/ProfileScreen.tsx
+++ packages/mobile/src/screens/profile-screen/ProfileScreen.tsx
@@ -203,10 +203,8 @@ export const ProfileScreen = () => {
                   <OfflinePlaceholder />
                 </>
               ) : (
-                <>
-                  <ScreenPrimaryContent skeleton={<ProfileScreenSkeleton />}>
-                    <PortalHost name='PullToRefreshPortalHost' />
-                  </ScreenPrimaryContent>
+                <ScreenPrimaryContent skeleton={<ProfileScreenSkeleton />}>
+                  <PortalHost name='PullToRefreshPortalHost' />
                   <ScreenSecondaryContent skeleton={<ProfileScreenSkeleton />}>
                     <ProfileTabNavigator
                       renderHeader={renderHeader}
@@ -215,7 +213,7 @@ export const ProfileScreen = () => {
                       onRefresh={handleRefresh}
                     />
                   </ScreenSecondaryContent>
-                </>
+                </ScreenPrimaryContent>
               )}
             </View>
           </>
