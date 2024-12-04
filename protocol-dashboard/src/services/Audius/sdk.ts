be4a178fcf728c431b45b3efbfc2dccf27f5c60d
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-12-04 13:20:43 -0800

    simplify audius sdk in protocol-dashboard

--- protocol-dashboard/src/services/Audius/sdk.ts
+++ protocol-dashboard/src/services/Audius/sdk.ts
@@ -1,45 +1,9 @@
-import {
-  DiscoveryNodeSelector,
-  EntityManager,
-  developmentConfig,
-  productionConfig,
-  sdk,
-  stagingConfig
-} from '@audius/sdk'
+import { sdk } from '@audius/sdk'
 
-let audiusSdk = null
-
-const initAudiusSdk = () => {
-  const env = import.meta.env.VITE_ENVIRONMENT
-  const bootstrapConfig =
-    env === 'development'
-      ? developmentConfig
-      : env === 'staging'
-      ? stagingConfig
-      : productionConfig
-  const {
-    discoveryNodes,
-    entityManagerContractAddress,
-    web3ProviderUrl,
-    identityServiceUrl
-  } = bootstrapConfig
-  const dnSelector = new DiscoveryNodeSelector({
-    bootstrapServices: discoveryNodes
-  })
-  audiusSdk = sdk({
-    appName: 'Audius Protocol Dashboard',
-    services: {
-      discoveryNodeSelector: dnSelector,
-      entityManager: new EntityManager({
-        contractAddress: entityManagerContractAddress,
-        web3ProviderUrl,
-        identityServiceUrl,
-        discoveryNodeSelector: dnSelector
-      })
-    }
-  })
-}
-
-initAudiusSdk()
+const env = import.meta.env.VITE_ENVIRONMENT
+const audiusSdk = sdk({
+  appName: 'Audius Protocol Dashboard',
+  environment: env
+})
 
 export { audiusSdk }
