a5f3d89b5dd418238bab2c8091ecf01119f528d2
Reed <3893871+dharit-tan@users.noreply.github.com>
2024-12-06 01:28:22 -0500

    Remove getUserEmail from libs Account (#10632)

--- packages/libs/src/api/Account.ts
+++ packages/libs/src/api/Account.ts
@@ -32,7 +32,6 @@ export class Account extends Base {
     this.changeCredentials = this.changeCredentials.bind(this)
     this.resetPassword = this.resetPassword.bind(this)
     this.checkIfEmailRegistered = this.checkIfEmailRegistered.bind(this)
-    this.getUserEmail = this.getUserEmail.bind(this)
     this.associateTwitterUser = this.associateTwitterUser.bind(this)
     this.associateInstagramUser = this.associateInstagramUser.bind(this)
     this.associateTikTokUser = this.associateTikTokUser.bind(this)
@@ -294,14 +293,6 @@ export class Account extends Base {
   async checkIfEmailRegistered(email: string) {
     this.REQUIRES(Services.IDENTITY_SERVICE)
     return await this.identityService.checkIfEmailRegistered(email)
-  }
-
-  /**
-   * Get the current user's email address
-   */
-  async getUserEmail() {
-    this.REQUIRES(Services.IDENTITY_SERVICE)
-    return await this.identityService.getUserEmail()
   }
 
   /**
