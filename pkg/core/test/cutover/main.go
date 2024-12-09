570e57c79b47546c203f350fade506245e3b62f8
Alec Savvy <alecsavvy@outlook.com>
2024-12-09 09:59:49 -0700

    a start

--- pkg/core/test/cutover/main.go
+++ pkg/core/test/cutover/main.go
@@ -1,9 +1,53 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/jaswdr/faker"
+)
+
+func runCmd(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
 
 func run() error {
+	skipAudiusCompose := flag.Bool("skip-audius-compose", false, "Skip audius-compose calls, may affect test success")
+	flag.Parse()
+
+	if !*skipAudiusCompose {
+		// audius-compose down
+		err := runCmd("audius-compose", "down")
+		if err != nil {
+			return fmt.Errorf("could not tear down audius-compose env: %v", err)
+		}
+
+		// audius-compose up
+		err = runCmd("audius-compose", "up")
+		if err != nil {
+			return fmt.Errorf("could not stand up audius-compose env: %v", err)
+		}
+	}
+
+	f := faker.New()
+
 	// audius-cmd create-user (return as json)
+	username := f.Person().FirstName() + f.Person().LastName()
+	userFile := fmt.Sprintf("audius-cmd-%s.json", username)
+	defer runCmd("rm", "/tmp/"+userFile)
+
+	err := runCmd("audius-cmd", "create-user", username, "--output", "/tmp/"+userFile)
+	if err != nil {
+		return fmt.Errorf("could not create user: %v", err)
+	}
+
 	// audius-cmd create-track (return as json)
 
 	// GET /stream url while indexing from solana
