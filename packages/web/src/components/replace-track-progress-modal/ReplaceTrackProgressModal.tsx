4a3910285eecfd3accdb0fb8434298ecf7f4c963
Kyle Shanks <kyle.j.shanks@gmail.com>
2024-12-10 14:05:32 -0500

    [C-5479] Add error states for track replace progress (#10656)

--- packages/web/src/components/replace-track-progress-modal/ReplaceTrackProgressModal.tsx
+++ packages/web/src/components/replace-track-progress-modal/ReplaceTrackProgressModal.tsx
@@ -7,19 +7,22 @@ import {
   IconAudiusLogo,
   ProgressBar,
   IconValidationCheck,
-  LoadingSpinner
+  LoadingSpinner,
+  Button
 } from '@audius/harmony'
 
 const messages = {
   description:
     'Please hold on while we upload and replace the file for this track.',
   inProgress: 'Upload in progress',
-  finalizing: 'Finalizing...'
+  finalizing: 'Finalizing...',
+  error: 'Something went wrong. Please try again later',
+  close: 'Close'
 }
 
 export const ReplaceTrackProgressModal = () => {
   const { data, isOpen, onClose } = useReplaceTrackProgressModal()
-  const { progress } = data
+  const { progress, error } = data
 
   const uploadProgress = Math.min(progress.upload + progress.transcode, 2) / 2
   const isUploadComplete = uploadProgress >= 1
@@ -27,32 +30,45 @@ export const ReplaceTrackProgressModal = () => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size='small'>
       <ModalContent>
-        <Flex direction='column' gap='unit14'>
-          <Flex alignItems='center' direction='column' gap='xl'>
-            <IconAudiusLogo height={48} width={48} color='default' />
-            <Text variant='body' size='l'>
-              {messages.description}
-            </Text>
-          </Flex>
-          <Flex direction='column' gap='m'>
-            <Flex justifyContent='space-between'>
-              <Text variant='label' size='s'>
-                {isUploadComplete ? messages.finalizing : messages.inProgress}
+        {error ? (
+          <Flex direction='column' gap='3xl' pt='3xl'>
+            <Flex w={220} alignSelf='center' alignItems='center'>
+              <Text variant='body' size='l' color='danger' textAlign='center'>
+                {messages.error}
               </Text>
-              <Flex alignItems='center' gap='l'>
-                <Text variant='label' size='s'>
-                  {Math.round(uploadProgress * 100)}%
-                </Text>
-                {isUploadComplete ? (
-                  <IconValidationCheck size='s' />
-                ) : (
-                  <LoadingSpinner css={{ height: 16, width: 16 }} />
-                )}
-              </Flex>
             </Flex>
-            <ProgressBar value={uploadProgress * 100} />
+            <Button variant='secondary' onClick={onClose}>
+              {messages.close}
+            </Button>
           </Flex>
-        </Flex>
+        ) : (
+          <Flex direction='column' gap='unit14'>
+            <Flex alignItems='center' direction='column' gap='xl'>
+              <IconAudiusLogo height={48} width={48} color='default' />
+              <Text variant='body' size='l'>
+                {messages.description}
+              </Text>
+            </Flex>
+            <Flex direction='column' gap='m'>
+              <Flex justifyContent='space-between'>
+                <Text variant='label' size='s'>
+                  {isUploadComplete ? messages.finalizing : messages.inProgress}
+                </Text>
+                <Flex alignItems='center' gap='l'>
+                  <Text variant='label' size='s'>
+                    {Math.round(uploadProgress * 100)}%
+                  </Text>
+                  {isUploadComplete ? (
+                    <IconValidationCheck size='s' />
+                  ) : (
+                    <LoadingSpinner css={{ height: 16, width: 16 }} />
+                  )}
+                </Flex>
+              </Flex>
+              <ProgressBar value={uploadProgress * 100} />
+            </Flex>
+          </Flex>
+        )}
       </ModalContent>
     </Modal>
   )
