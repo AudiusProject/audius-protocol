d07caca403f09aff1e43d4f3bffcf273c99450d0
Dharit Tantiviramanond <dharit.tan@gmail.com>
2024-12-11 16:49:31 +0700

    mobile getUSDCUserBank

--- packages/mobile/src/services/buyCrypto.ts
+++ packages/mobile/src/services/buyCrypto.ts
@@ -1,10 +1,11 @@
 import { audiusLibs, waitForLibsInit } from './libs'
+import { audiusSdk } from './sdk/audius-sdk'
 
 export const getUSDCUserBank = async (ethWallet?: string) => {
-  await waitForLibsInit()
-  return await audiusLibs?.solanaWeb3Manager?.deriveUserBank({
-    ethAddress: ethWallet,
-    mint: 'usdc'
+  const sdk = await audiusSdk()
+  return await sdk.services.claimableTokensClient.deriveUserBank({
+    ethWallet,
+    mint: 'USDC'
   })
 }
 
