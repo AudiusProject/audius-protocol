79806b493179772f95ca6874859b5b948d288e97
Reed <3893871+dharit-tan@users.noreply.github.com>
2024-12-11 13:28:17 -0500

    Fix mobile lint (#10687)

--- packages/mobile/src/services/buyCrypto.ts
+++ packages/mobile/src/services/buyCrypto.ts
@@ -1,7 +1,5 @@
 import { audiusSdk } from 'app/services/sdk/audius-sdk'
 
-import { audiusLibs, waitForLibsInit } from './libs'
-
 export const getUSDCUserBank = async (ethWallet?: string) => {
   const sdk = await audiusSdk()
   return await sdk.services.claimableTokensClient.deriveUserBank({
