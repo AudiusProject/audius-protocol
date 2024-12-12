685539bb0837d2fc891545ed7a34534846764108
Alec Savvy <alecsavvy@outlook.com>
2024-12-12 06:51:53 -0700

    add peers and pubsub to server

---
+++ pkg/core/server/pubsub.go
@@ -0,0 +1,3 @@
+// A generic pubsub system built with go channels. Used to communicate different pieces of data across the app.
+// Latest block, stream txs, etc.
+package server
--- pkg/core/server/server.go
+++ pkg/core/server/server.go
@@ -1,12 +1,11 @@
 package server
 
 import (
-	"net/http"
-
 	"github.com/AudiusProject/audius-protocol/pkg/core/common"
 	"github.com/AudiusProject/audius-protocol/pkg/core/config"
 	"github.com/AudiusProject/audius-protocol/pkg/core/db"
 	"github.com/AudiusProject/audius-protocol/pkg/core/mempool"
+	"github.com/AudiusProject/audius-protocol/pkg/core/sdk"
 	cconfig "github.com/cometbft/cometbft/config"
 	"github.com/cometbft/cometbft/rpc/client/local"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -19,8 +18,8 @@ type Server struct {
 	logger         *common.Logger
 	rpc            *local.Local
 	db             *db.Queries
-	self           *http.Client
 	mempl          *mempool.Mempool
+	peers          []*sdk.Sdk
 }
 
 func NewServer(config *config.Config, cconfig *cconfig.Config, logger *common.Logger, rpc *local.Local, pool *pgxpool.Pool, e *echo.Echo, mempl *mempool.Mempool) (*Server, error) {
@@ -31,7 +30,7 @@ func NewServer(config *config.Config, cconfig *cconfig.Config, logger *common.Lo
 		logger:         logger.Child("http_server"),
 		db:             db.New(pool),
 		mempl:          mempl,
-		self:           nil,
+		peers:          []*sdk.Sdk{},
 	}
 
 	g := e.Group("/core")
