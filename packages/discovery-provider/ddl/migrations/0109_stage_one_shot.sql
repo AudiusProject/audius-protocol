8baab5ac1b2d1ec8490a0eb92a9eeaf883bcc09a
isaac <isaac@audius.co>
2024-12-17 15:26:15 -0800

    Create stage one shot migration

--- packages/discovery-provider/ddl/migrations/0109_stage_one_shot.sql
+++ packages/discovery-provider/ddl/migrations/0109_stage_one_shot.sql
@@ -3,12 +3,12 @@ do $$ begin -- run only on stage
 if exists (
     select *
     from "blocks"
-    where "blockhash" = '0x400faf3853ffaf086d8bc415410e833c6ba284835d43f51b13ed81fe7d71a5b1'
+    where "blockhash" = '0x65a3243860511ed28a933c3a113dea7df368ad53f721cc9d0034c0c75f996afb'
 ) then -- check whether the data has already been backfilled and return early for idempotency
 if exists (
     select 1
     from user_challenges
-    where challenge_id = 'o'
+    where challenge_id = 'o' -- not future proof but safe for now
 ) then return;
 end if;
 -- ========== one shot trigger ==========
@@ -16,12 +16,12 @@ WITH max_block AS (
     SELECT MAX(number) AS completed_blocknumber
     FROM blocks
 ),
-WITH one_shot_users as (
+one_shot_users as (
     select temp.handle_lc,
         temp.amount,
-        users.id as user_id,
-        from temp_stage_one_shot temp
-        join users on temp_stage_one_shot.handle_lc = users.handle_lc
+        users.user_id
+    from temp_stage_one_shot temp
+        join users on temp.handle_lc = users.handle_lc
 )
 INSERT INTO user_challenges (
         user_id,
@@ -32,17 +32,18 @@ INSERT INTO user_challenges (
         completed_blocknumber,
         amount,
         created_at,
-        completed_blocknumber
+        completed_at
     )
 SELECT osu.user_id,
     'o' as challenge_id,
-    'hardcoded_specifier' AS specifier,
+    concat(to_hex(user_id), ':', to_hex(1)) AS specifier,
+    -- user_id:nth_shot for future proofing
     TRUE AS is_complete,
     0 AS current_step_count,
     mb.completed_blocknumber,
-    osu.amount AS amount,
+    CAST(osu.amount AS INT) AS amount,
     NOW() AS created_at,
-    mb.completed_blocknumber
+    NOW() AS created_at
 FROM one_shot_users osu,
     max_block mb;
 end if;
