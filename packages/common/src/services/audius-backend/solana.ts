feef31ecdc1a97df8d8af22d7f75c40b2c2a327a
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2024-12-18 16:00:32 -0600

    Migrate createTransferToUserBankTransaction to SDK

--- packages/common/src/services/audius-backend/solana.ts
+++ packages/common/src/services/audius-backend/solana.ts
@@ -414,33 +414,26 @@ export const decorateCoinflowWithdrawalTransaction = async (
 }
 
 export const createTransferToUserBankTransaction = async (
-  audiusBackendInstance: AudiusBackend,
+  sdk: AudiusSdk,
   {
     userBank,
     wallet,
     amount,
     memo,
-    mint = 'wAUDIO',
-    recentBlockhash,
-    feePayer
+    mintPublicKey,
+    mintDecimals
   }: {
     userBank: PublicKey
     wallet: Keypair
     amount: bigint
     memo: string
-    mint?: MintName
-    feePayer?: PublicKey
-    recentBlockhash?: string
+    mintPublicKey: PublicKey
+    mintDecimals: number
   }
 ) => {
-  const libs = await audiusBackendInstance.getAudiusLibsTyped()
-  const mintPublicKey = libs.solanaWeb3Manager!.mints[mint]
-  const associatedTokenAccount = await findAssociatedTokenAddress(
-    audiusBackendInstance,
-    {
-      solanaAddress: wallet.publicKey.toBase58(),
-      mint
-    }
+  const associatedTokenAccount = getAssociatedTokenAddressSync(
+    mintPublicKey,
+    wallet.publicKey
   )
   // See: https://github.com/solana-labs/solana-program-library/blob/d6297495ea4dcc1bd48f3efdd6e3bbdaef25a495/memo/js/src/index.ts#L27
   const memoInstruction = new TransactionInstruction({
@@ -460,11 +453,11 @@ export const createTransferToUserBankTransaction = async (
     userBank, // destination
     wallet.publicKey, // owner
     amount, // amount
-    MINT_DECIMALS[mint] // decimals
+    mintDecimals // decimals
   )
-  const tx = new Transaction({ recentBlockhash, feePayer })
-  tx.add(memoInstruction)
-  tx.add(transferInstruction)
+  const tx = sdk.services.solanaClient.buildTransaction({
+    instructions: [memoInstruction, transferInstruction]
+  })
   return tx
 }
 
