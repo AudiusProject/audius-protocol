859e0d6d567c383d8c851a7a9b3cf0223a3576d2
Alec Savvy <alecsavvy@outlook.com>
2024-12-19 16:32:05 -0700

    no rows check

--- pkg/core/server/registry_bridge.go
+++ pkg/core/server/registry_bridge.go
@@ -313,6 +313,9 @@ func (s *Server) validateValidatorRegistration(tx *core_proto.SignedTransaction_
 
 	node, err := db.GetRegisteredNodeByCometAddress(context.Background(), cometAddr)
 	if err != nil {
+		if !errors.Is(err, pgx.ErrNoRows) {
+			return nil
+		}
 		return fmt.Errorf("validation db fail: %v", err)
 	}
 
@@ -326,7 +329,7 @@ func (s *Server) validateValidatorRegistration(tx *core_proto.SignedTransaction_
 
 // checks if the register node tx is valid
 // calls ethereum mainnet and validates signature to confirm node should be a validator
-func (s *Server) isValidRegisterNodeTx(ctx context.Context, tx *core_proto.SignedTransaction) error {
+func (s *Server) isValidRegisterNodeTx(_ctx context.Context, tx *core_proto.SignedTransaction) error {
 	sig := tx.GetSignature()
 	if sig == "" {
 		return fmt.Errorf("no signature provided for registration tx: %v", tx)
