9e93ff0f03d3670c6482087080d5d881b16e9179
Steve Perkins <stereosteve@users.noreply.github.com>
2024-12-20 13:56:16 -0500

    Combined MRVR report (#10779)

--- packages/discovery-provider/plugins/pedalboard/apps/mri/src/queries/mrvr.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/mri/src/queries/mrvr.ts
@@ -1,11 +1,11 @@
-import { Knex } from 'knex'
-import { logger as plogger } from '../logger'
-import { toCsvString } from '../csv'
-import { S3Config, publish } from '../s3'
+import fetch from 'cross-fetch'
 import dayjs from 'dayjs'
 import quarterOfYear from 'dayjs/plugin/quarterOfYear'
-import { formatDateISO, getYearMonth, getYearMonthDay, getYearMonthShorthand } from '../date'
-import fetch from 'cross-fetch'
+import { Knex } from 'knex'
+import { toCsvString } from '../csv'
+import { formatDateISO, getYearMonth } from '../date'
+import { logger as plogger } from '../logger'
+import { S3Config, publish } from '../s3'
 
 dayjs.extend(quarterOfYear)
 
@@ -29,37 +29,22 @@ type MrvrAffirmative = {
 }
 
 type MrvrCbs = {
-  'Offering': 'Downloads / Monetized Content' | 'Subscription'
-  'UserType': 'Paid'
+  Offering: 'Downloads / Monetized Content' | 'Subscription'
+  UserType: 'Paid'
   ['Subscriber Count']: 0
   ['Gross Revenue']: number
   ['Gross revenue With Deductions']: number
-  'Territory': string
-  'Has_usage_flag': boolean
+  Territory: string
+  Has_usage_flag: boolean
   'Total Downloads': number
   'Total Streams': number
-  'Currency': 'USD'
+  Currency: 'USD'
+  'Public Performance Fees': number
+  'Record Label Payments': number
+  'Average Subscription Price': number
+  'Aggregate Transmission Hours': number
+  'Tip Revenue': number
 }
-
-const MrvrAffirmativeHeader: (keyof MrvrAffirmative)[] = [
-  'Downloads - Gross Revenues without Permitted Deductions',
-  'Downloads - Gross Revenues with Permitted Deductions',
-  'Downloads - Gross Revenues without Permitted Deductions - USA Only',
-  'Downloads - Gross Revenues with Permitted Deductions - USA Only',
-  'Downloads - Public Performance Fees',
-  'Downloads - Record Label Payments',
-  'Subscription - Gross Revenues without Permitted Deductions',
-  'Subscription - Gross Revenues with Permitted Deductions',
-  'Subscription - Gross Revenues without Permitted Deductions - USA Only',
-  'Subscription - Gross Revenues with Permitted Deductions - USA Only',
-  'Subscription - Public Performance Fees',
-  'Subscription - Record Label Payments',
-  'Subscription - Average Subscription Price',
-  'Subscription - Total Subscribers',
-  'Subscription - Total Subscribers - USA Only',
-  'Aggregate Transmission Hours - USA Only',
- 
-]
 
 const MrvrCbsHeader: (keyof MrvrCbs)[] = [
   'Offering',
@@ -72,21 +57,23 @@ const MrvrCbsHeader: (keyof MrvrCbs)[] = [
   'Total Downloads',
   'Total Streams',
   'Currency',
- 
+  'Public Performance Fees',
+  'Record Label Payments',
+  'Average Subscription Price',
+  'Aggregate Transmission Hours',
+  'Tip Revenue'
 ]
 
 // gathers data from a month period prior to the provided date.
 // formats it into csv format compatible with the mri spec
 // publishes it to all the provided s3 configs
-export const mrvr = async (
-  db: Knex,
-  s3s: S3Config[],
-  date: Date
-): Promise<void> => {
+export const mrvr = async (db: Knex, date: Date): Promise<void> => {
   // Get exchange rate from usd to eur for CBS reporting
-  const usdToEurRate = await fetch('https://api.frankfurter.app/latest?base=USD')
-    .then(response => response.json())
-    .then(data => data.rates.EUR)
+  const usdToEurRate = await fetch(
+    'https://api.frankfurter.app/latest?base=USD'
+  )
+    .then((response) => response.json())
+    .then((data) => data.rates.EUR)
 
   const logger = plogger.child({ date: date.toISOString() })
   logger.info('beginning usage detail report processing')
@@ -101,102 +88,6 @@ export const mrvr = async (
     'time range'
   )
 
-  const affirmative = async () => {
-    const mrvrAffirmativeResult = await db.raw(
-      `
-      select
-        revenue."Downloads - Gross Revenues without Permitted Deductions",
-        revenue."Downloads - Gross Revenues with Permitted Deductions",
-        revenue."Downloads - Gross Revenues without Permitted Deductions - USA Only",
-        revenue."Downloads - Gross Revenues with Permitted Deductions - USA Only",
-        revenue."Downloads - Public Performance Fees",
-        revenue."Downloads - Record Label Payments",
-        revenue."Subscription - Gross Revenues without Permitted Deductions",
-        revenue."Subscription - Gross Revenues with Permitted Deductions",
-        revenue."Subscription - Gross Revenues without Permitted Deductions - USA Only",
-        revenue."Subscription - Gross Revenues with Permitted Deductions - USA Only",
-        revenue."Subscription - Public Performance Fees",
-        revenue."Subscription - Record Label Payments",
-        revenue."Subscription - Average Subscription Price",
-        revenue."Subscription - Total Subscribers",
-        revenue."Subscription - Total Subscribers - USA Only",
-        ath."Aggregate Transmission Hours - USA Only"
-      from
-        (
-          select
-            trunc(sum(("amount" + "extra_amount") / 1000000), 2) as "Downloads - Gross Revenues without Permitted Deductions",
-            trunc(sum(("amount" + "extra_amount") / 1000000), 2) as "Downloads - Gross Revenues with Permitted Deductions",
-            trunc(sum(
-              case
-                when "country" = 'United States'
-                then ("amount" + "extra_amount") / 1000000
-                else 0
-              end
-            ), 2) as "Downloads - Gross Revenues without Permitted Deductions - USA Only",
-            trunc(sum(
-              case
-                when "country" = 'United States'
-                then ("amount" + "extra_amount") / 1000000
-                else 0
-              end
-            ), 2) as "Downloads - Gross Revenues with Permitted Deductions - USA Only",
-            trunc(0, 2) as "Downloads - Public Performance Fees",
-            trunc(0, 2) as "Downloads - Record Label Payments",
-            trunc(0, 2) as "Subscription - Gross Revenues without Permitted Deductions",
-            trunc(0, 2) as "Subscription - Gross Revenues with Permitted Deductions",
-            trunc(0, 2) as "Subscription - Gross Revenues without Permitted Deductions - USA Only",
-            trunc(0, 2) as "Subscription - Gross Revenues with Permitted Deductions - USA Only",
-            trunc(0, 2) as "Subscription - Public Performance Fees",
-            trunc(0, 2) as "Subscription - Record Label Payments",
-            trunc(0, 2) as "Subscription - Average Subscription Price",
-            trunc(0, 2) as "Subscription - Total Subscribers",
-            trunc(0, 2) as "Subscription - Total Subscribers - USA Only"
-          from "usdc_purchases"
-          where
-            "created_at" >= :start
-            and "created_at" < :end
-        ) as revenue
-        cross join
-        (
-          select
-            sum(cast("count" * "duration" as float) / 3600.0) as "Aggregate Transmission Hours - USA Only"
-          from (
-            select
-              "tracks"."duration" as "duration",
-              "aggregate_monthly_plays"."count" as "count"
-            from
-              "tracks"
-            join
-              "aggregate_monthly_plays" on "tracks"."track_id" = "aggregate_monthly_plays"."play_item_id"
-            where
-              "aggregate_monthly_plays"."country" = 'United States'
-              and "timestamp" >= :start
-              and "timestamp" < :end
-          ) as ath_data
-        ) as ath;
-      `,
-      { start, end }
-    )
-  
-    const mrvrAffirmativeRows: MrvrAffirmative[] = mrvrAffirmativeResult.rows
-    const mrvrAffirmativeCsv = toCsvString(mrvrAffirmativeRows, MrvrAffirmativeHeader)
-
-    const now = new Date();
-    // Audius_MRVR_aff_YYMM_YYYYMMDD.csv
-    // YYMM = Year and Month of usage, YYYYMMDD = time of generation
-    const fileName = `Audius_MRVR_aff_${getYearMonthShorthand(start)}_${getYearMonthDay(now)}`
-  
-    const results = await publish(
-      logger,
-      s3s,
-      mrvrAffirmativeCsv,
-      fileName
-    )
-    results.forEach((objUrl) =>
-      logger.info({ objUrl, records: mrvrAffirmativeCsv.length }, 'mrvr affirmative upload result')
-    )
-  }
-  // CBS
   const cbs = async () => {
     const mrvrCbs = await db.raw(
       `
@@ -206,7 +97,8 @@ export const mrvr = async (
           content_id,
           country,
           buyer_user_id,
-          ("amount" + "extra_amount") / 1000000 as usd
+          "amount" / 1000000 as revenue_usd,
+          "extra_amount" / 1000000 as tip_usd
         from usdc_purchases
         where content_type = 'track'
           and created_at >= :start
@@ -216,19 +108,27 @@ export const mrvr = async (
 
       -- aggregate downloads by country for purchaseable content
       downloads as (
-        select country, count(*) as download_count
-        from track_downloads
+        select
+          country,
+          count(*) as download_count,
+          sum(duration) as download_ath
+        from track_downloads d
+        join tracks using (track_id)
         where track_id in (select content_id from purchases)
-          and created_at >= :start
-          and created_at < :end
+          and d.created_at >= :start
+          and d.created_at < :end
           and "country" is not null
         group by country
       ),
 
       -- aggregate streams by country for purchaseable content
       streams as (
-        select country, sum(count) as stream_count
+        select
+          country,
+          sum(count) as stream_count,
+          sum(count * duration) as stream_ath
         from aggregate_monthly_plays
+        join tracks on play_item_id = track_id
         where play_item_id in (select content_id from purchases)
           and "timestamp" >= :start
           and "timestamp" < :end
@@ -241,12 +141,18 @@ export const mrvr = async (
         "UserType",
         "Subscriber Count",
         "Gross Revenue",
-        "Gross revenue With Deductions", 
+        "Gross revenue With Deductions",
         "Territory",
         case when ("Total Downloads" > 0 or "Total Streams" > 0) then true else false end as "Has_usage_flag",
         coalesce("Total Downloads", 0) as "Total Downloads",
         coalesce("Total Streams", 0) as "Total Streams",
-        "Currency"
+        "Currency",
+
+        "Public Performance Fees",
+        "Record Label Payments",
+        "Average Subscription Price",
+        trunc("Aggregate Transmission Hours"::numeric, 2) as "Aggregate Transmission Hours",
+        "Tip Revenue"
       from (
         -- paid portion of MRVR
         select
@@ -255,34 +161,43 @@ export const mrvr = async (
           count(distinct "buyer_user_id") as "Subscriber Count",
           trunc(
             case when
-              is_country_eur("country") then sum("usd") * :usdToEurRate
-              else sum("usd")
+              is_country_eur("country") then sum("revenue_usd") * :usdToEurRate
+              else sum("revenue_usd")
             end,
             2
           ) as "Gross Revenue",
           trunc(
             case when
-              is_country_eur("country") then sum("usd") * :usdToEurRate
-              else sum("usd")
+              is_country_eur("country") then sum("revenue_usd") * :usdToEurRate
+              else sum("revenue_usd")
             end,
             2
           ) as "Gross revenue With Deductions",
           country_to_iso_alpha2(coalesce("country", '')) as "Territory",
-          (
-            select coalesce(download_count, 0)
-            from downloads td
-            where td.country = purchases.country
-          ) as "Total Downloads",
-          (
-            select coalesce(stream_count, 0)
-            from streams s
-            where s.country = purchases.country
-          ) as "Total Streams",
+          sum(download_count) as "Total Downloads",
+          sum(stream_count) as "Total Streams",
           case when
             is_country_eur("country") then 'EUR'
             else 'USD'
-          end as "Currency"
+          end as "Currency",
+
+          trunc(0, 2) as "Public Performance Fees",
+          trunc(0, 2) as "Record Label Payments",
+          trunc(0, 2) as "Average Subscription Price",
+
+          sum(coalesce(download_ath, 0) + coalesce(stream_ath, 0))::float / 3600 as "Aggregate Transmission Hours",
+
+          trunc(
+            case when
+              is_country_eur("country") then sum("tip_usd") * :usdToEurRate
+              else sum("tip_usd")
+            end,
+            2
+          ) as "Tip Revenue"
+
         from purchases
+        left join streams using (country)
+        left join downloads using (country)
         where "country" is not null
         group by "country"
 
@@ -307,11 +222,20 @@ export const mrvr = async (
           case when
             is_country_eur("country") then 'EUR'
             else 'USD'
-          end as "Currency"
+          end as "Currency",
+
+          trunc(0, 2) as "Public Performance Fees",
+          trunc(0, 2) as "Record Label Payments",
+          trunc(0, 2) as "Average Subscription Price",
+
+          sum(duration)::float / 3600 as "Aggregate Transmission Hours",
+
+          trunc(0, 2) as "Tip Revenue"
         from plays
+        join tracks on play_item_id = track_id
         where
-          created_at >= :start
-          and created_at < :end
+          plays.created_at >= :start
+          and plays.created_at < :end
           and country is not null
         group by country
       ) subq;
@@ -321,22 +245,21 @@ export const mrvr = async (
 
     const mrvrCbsRows: MrvrAffirmative[] = mrvrCbs.rows
     const mrvrCbsCsv = toCsvString(mrvrCbsRows, MrvrCbsHeader)
-    const now = new Date();
+    const now = new Date()
     // YYYYMM_Audius_yyyymmddThhmmss_summary.csv
     // YYMM = Year and Month of usage, YYYYMMDD = time of generation
-    const fileName = `${getYearMonth(start)}_Audius_${formatDateISO(now)}_summary`
+    const fileName = `inputs/revenue/${getYearMonth(
+      start
+    )}_Audius_${formatDateISO(now)}_summary.csv`
 
-    const results = await publish(
-      logger,
-      s3s,
-      mrvrCbsCsv,
-      fileName
-    )
+    const results = await publish(logger, mrvrCbsCsv, fileName)
     results.forEach((objUrl) =>
-      logger.info({ objUrl, records: mrvrCbsCsv.length }, 'mrvr cbs upload result')
+      logger.info(
+        { objUrl, records: mrvrCbsCsv.length },
+        'mrvr cbs upload result'
+      )
     )
   }
 
-  await affirmative()
   await cbs()
 }
