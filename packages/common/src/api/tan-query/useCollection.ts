156936a02973d5a544f9b3d5e3a7188f90a4fde8
Dylan Jeffers <dylan@audius.co>
2024-12-23 21:02:20 +0100

    [C-5571] Update react-query hooks to use legacy data format (#10832)

--- packages/common/src/api/tan-query/useCollection.ts
+++ packages/common/src/api/tan-query/useCollection.ts
@@ -1,6 +1,9 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 
+import { userCollectionMetadataFromSDK } from '~/adapters/collection'
 import { useAppContext } from '~/context/appContext'
+import { ID } from '~/models/Identifiers'
+import { encodeHashId } from '~/utils/hashIds'
 
 import { QUERY_KEYS } from './queryKeys'
 
@@ -8,37 +11,41 @@ type Config = {
   staleTime?: number
 }
 
-export const useCollection = (collectionId: string, config?: Config) => {
+export const useCollection = (collectionId: ID, config?: Config) => {
   const { audiusSdk } = useAppContext()
   const queryClient = useQueryClient()
 
   return useQuery({
     queryKey: [QUERY_KEYS.collection, collectionId],
     queryFn: async () => {
+      const encodedId = encodeHashId(collectionId)
+      if (!encodedId) return null
       const { data } = await audiusSdk!.full.playlists.getPlaylist({
-        playlistId: collectionId
+        playlistId: encodedId
       })
-      const collection = data?.[0]
+
+      if (!data?.[0]) return null
+      const collection = userCollectionMetadataFromSDK(data[0])
 
       if (collection) {
         // Prime user data from collection owner
         if (collection.user) {
           queryClient.setQueryData(
-            [QUERY_KEYS.user, collection.user.id],
+            [QUERY_KEYS.user, collection.user.user_id],
             collection.user
           )
         }
 
         // Prime track and user data from tracks in collection
         collection.tracks?.forEach((track) => {
-          if (track.id) {
+          if (track.track_id) {
             // Prime track data
-            queryClient.setQueryData([QUERY_KEYS.track, track.id], track)
+            queryClient.setQueryData([QUERY_KEYS.track, track.track_id], track)
 
             // Prime user data from track owner
             if (track.user) {
               queryClient.setQueryData(
-                [QUERY_KEYS.user, track.user.id],
+                [QUERY_KEYS.user, track.user.user_id],
                 track.user
               )
             }
--- packages/common/src/api/tan-query/useCollections.ts
+++ packages/common/src/api/tan-query/useCollections.ts
@@ -1,6 +1,10 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 
+import { userCollectionMetadataFromSDK } from '~/adapters/collection'
+import { transformAndCleanList } from '~/adapters/utils'
 import { useAppContext } from '~/context/appContext'
+import { ID } from '~/models/Identifiers'
+import { encodeHashId } from '~/utils/hashIds'
 
 import { QUERY_KEYS } from './queryKeys'
 
@@ -8,36 +12,45 @@ type Config = {
   staleTime?: number
 }
 
-export const useCollections = (collectionIds: string[], config?: Config) => {
+export const useCollections = (collectionIds: ID[], config?: Config) => {
   const { audiusSdk } = useAppContext()
   const queryClient = useQueryClient()
 
   return useQuery({
     queryKey: [QUERY_KEYS.collections, collectionIds],
     queryFn: async () => {
+      const encodedIds = collectionIds
+        .map(encodeHashId)
+        .filter((id): id is string => id !== null)
+      if (encodedIds.length === 0) return []
       const { data } = await audiusSdk!.full.playlists.getBulkPlaylists({
-        id: collectionIds
+        id: encodedIds
       })
 
-      data?.forEach((collection) => {
+      const collections = transformAndCleanList(
+        data,
+        userCollectionMetadataFromSDK
+      )
+
+      collections?.forEach((collection) => {
         // Prime user data from collection owner
         if (collection.user) {
           queryClient.setQueryData(
-            [QUERY_KEYS.user, collection.user.id],
+            [QUERY_KEYS.user, collection.user.user_id],
             collection.user
           )
         }
 
         // Prime track and user data from tracks in collection
         collection.tracks?.forEach((track) => {
-          if (track.id) {
+          if (track.track_id) {
             // Prime track data
-            queryClient.setQueryData([QUERY_KEYS.track, track.id], track)
+            queryClient.setQueryData([QUERY_KEYS.track, track.track_id], track)
 
             // Prime user data from track owner
             if (track.user) {
               queryClient.setQueryData(
-                [QUERY_KEYS.user, track.user.id],
+                [QUERY_KEYS.user, track.user.user_id],
                 track.user
               )
             }
@@ -45,7 +58,7 @@ export const useCollections = (collectionIds: string[], config?: Config) => {
         })
       })
 
-      return data
+      return collections
     },
     staleTime: config?.staleTime,
     enabled: !!audiusSdk && collectionIds.length > 0
