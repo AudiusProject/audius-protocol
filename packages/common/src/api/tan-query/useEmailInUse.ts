9d8a04deb3f9af46c0bd6474cd239dfec0b14a2c
amendelsohn <andrew.j.mendelsohn@gmail.com>
2025-01-07 18:06:22 -0800

    update usages

--- packages/common/src/api/tan-query/useEmailInUse.ts
+++ packages/common/src/api/tan-query/useEmailInUse.ts
@@ -1,22 +1,31 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { useAudiusQueryContext } from '~/audius-query'
+import { AudiusQueryContextType } from '~/audius-query/AudiusQueryContext'
 
 import { QUERY_KEYS } from './queryKeys'
 import { Config } from './types'
 
+export const fetchEmailInUse = async (
+  email: string | null | undefined,
+  { identityService }: AudiusQueryContextType
+) => {
+  if (!email) return { exists: false, isGuest: false }
+  return await identityService.checkIfEmailRegistered(email)
+}
+
+/**
+ * Hook to check if an email is already registered
+ */
 export const useEmailInUse = (
   email: string | null | undefined,
   config?: Config
 ) => {
-  const { identityService } = useAudiusQueryContext()
+  const context = useAudiusQueryContext()
 
   return useQuery({
     queryKey: [QUERY_KEYS.emailInUse, email],
-    queryFn: async () => {
-      if (!email) return false
-      return await identityService.checkIfEmailRegistered(email)
-    },
+    queryFn: () => fetchEmailInUse(email, context),
     staleTime: config?.staleTime,
     enabled: config?.enabled !== false && !!email
   })
--- packages/common/src/api/tan-query/useHandleInUse.ts
+++ packages/common/src/api/tan-query/useHandleInUse.ts
@@ -2,26 +2,35 @@ import { useQuery } from '@tanstack/react-query'
 import { isEmpty } from 'lodash'
 
 import { useAudiusQueryContext } from '~/audius-query'
+import { AudiusQueryContextType } from '~/audius-query/AudiusQueryContext'
 
 import { QUERY_KEYS } from './queryKeys'
 import { Config } from './types'
 
+export const fetchHandleInUse = async (
+  handle: string | null | undefined,
+  { audiusSdk }: AudiusQueryContextType
+) => {
+  if (!handle) return false
+  const sdk = await audiusSdk()
+  const { data: users = [] } = await sdk.full.users.getUserByHandle({
+    handle
+  })
+  return !isEmpty(users[0])
+}
+
+/**
+ * Hook to check if a handle is already in use
+ */
 export const useHandleInUse = (
   handle: string | null | undefined,
   config?: Config
 ) => {
-  const { audiusSdk } = useAudiusQueryContext()
+  const context = useAudiusQueryContext()
 
   return useQuery({
     queryKey: [QUERY_KEYS.handleInUse, handle],
-    queryFn: async () => {
-      if (!handle) return false
-      const sdk = await audiusSdk()
-      const { data: users = [] } = await sdk.full.users.getUserByHandle({
-        handle
-      })
-      return !isEmpty(users[0])
-    },
+    queryFn: () => fetchHandleInUse(handle, context),
     staleTime: config?.staleTime,
     enabled: config?.enabled !== false && !!handle
   })
