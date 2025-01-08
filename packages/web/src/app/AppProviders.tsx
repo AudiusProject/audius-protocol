41bcf3f344c687214df915c4c5e9e636c3ccc29d
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-01-08 17:48:52 -0500

    lint

--- packages/web/src/app/AppProviders.tsx
+++ packages/web/src/app/AppProviders.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useState } from 'react'
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
