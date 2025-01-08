0066e5588b614bfbecf9a6f0091a6a831ffcb0a8
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-01-08 15:25:11 -0500

    commit, idempotent

--- packages/discovery-provider/ddl/migrations/0113_challenge_user_id.sql
+++ packages/discovery-provider/ddl/migrations/0113_challenge_user_id.sql
@@ -1,2 +1,6 @@
-CREATE INDEX user_challenges_user_id ON user_challenges USING btree (user_id);
-CREATE INDEX challenge_disbursements_user_id ON challenge_disbursements USING btree (user_id);
+BEGIN;
+
+CREATE INDEX IF NOT EXISTS user_challenges_user_id ON user_challenges USING btree (user_id);
+CREATE INDEX IF NOT EXISTS challenge_disbursements_user_id ON challenge_disbursements USING btree (user_id);
+
+COMMIT;
