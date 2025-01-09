dfeaa2d52deac00c7217edabf62504925a76573c
Raymond Jacobson <ray@audius.co>
2025-01-09 14:20:16 -0800

    Fix path

--- packages/mobile/ios/scripts/bundleReactNative.sh
+++ packages/mobile/ios/scripts/bundleReactNative.sh
@@ -2,6 +2,6 @@ source "${PROJECT_DIR}/.xcode.env"
 
 export PROJECT_ROOT=$PWD/..
 export EXTRA_PACKAGER_ARGS=\"--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map\" 
-export SENTRY_PROPERTIES="${PROJECT_ROOT}/sentry.properties"
+export SENTRY_PROPERTIES="${PROJECT_ROOT}/ios/sentry.properties"
  
 "$NPM_BINARY" --prefix ../ run sentry-cli -- react-native xcode ../../../node_modules/react-native/scripts/react-native-xcode.sh
--- packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
+++ packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
@@ -1,6 +1,6 @@
 source "${PROJECT_DIR}/.xcode.env"
 
 export PROJECT_ROOT=$PWD/..
-export SENTRY_PROPERTIES="${PROJECT_ROOT}/sentry.properties"
+export SENTRY_PROPERTIES="${PROJECT_ROOT}/ios/sentry.properties"
 
 "$NPM_BINARY" --prefix ../ run sentry-cli -- upload-dsym
