2b09af40cd9dcebfb2c8b76bfda95989853efad4
Farid Salau <faridsalau@gmail.com>
2025-01-10 12:16:13 -0600

    Address comment

--- packages/web/src/pages/settings-page/components/desktop/ListeningHistory/ListeningHistorySettingsCard.tsx
+++ packages/web/src/pages/settings-page/components/desktop/ListeningHistory/ListeningHistorySettingsCard.tsx
@@ -1,11 +1,14 @@
 import { useCallback } from 'react'
 
+import { route } from '@audius/common/utils'
 import { Button, IconListeningHistory } from '@audius/harmony'
 import { useDispatch } from 'react-redux'
 
 import { push } from 'utils/navigation'
 
 import SettingsCard from '../SettingsCard'
+
+const { HISTORY_PAGE } = route
 
 const messages = {
   title: 'Listening History',
@@ -17,7 +20,7 @@ export const ListeningHistorySettingsCard = () => {
   const dispatch = useDispatch()
 
   const handleClick = useCallback(() => {
-    dispatch(push('/history'))
+    dispatch(push(HISTORY_PAGE))
   }, [dispatch])
 
   return (
