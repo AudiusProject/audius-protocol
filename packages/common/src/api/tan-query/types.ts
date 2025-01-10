4683eb5b61991ca9f6d2c9d4948ce4ada03db8ed
JD Francis <jd@audius.co>
2025-01-10 16:22:03 -0600

    undo type change, save for diff pr

--- packages/common/src/api/tan-query/types.ts
+++ packages/common/src/api/tan-query/types.ts
@@ -1,6 +1,9 @@
-import { UseQueryOptions } from '@tanstack/react-query'
+import { DefinedInitialDataOptions } from '@tanstack/react-query'
 
 /**
  * Standard tan-query pass-thru options that we use
  */
-export type Config = Pick<UseQueryOptions, 'staleTime' | 'enabled'>
+export type Config = Pick<
+  DefinedInitialDataOptions<any>,
+  'staleTime' | 'enabled'
+>
