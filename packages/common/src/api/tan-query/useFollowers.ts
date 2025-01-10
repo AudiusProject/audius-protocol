797f88b9b71aca3018db2886bb16b0d8e4dad842
Dylan Jeffers <dylan@audius.co>
2025-01-09 21:02:44 -0800

    [C-5648] Migrate user list to tan-query (#10990)

--- packages/common/src/api/tan-query/useFollowers.ts
+++ packages/common/src/api/tan-query/useFollowers.ts
@@ -1,42 +1,74 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { useDispatch } from 'react-redux'
 
 import { userMetadataListFromSDK } from '~/adapters/user'
-import { useAppContext } from '~/context/appContext'
+import { useAudiusQueryContext } from '~/audius-query'
 import { ID, Id, OptionalId } from '~/models/Identifiers'
+import { User } from '~/models/User'
 
 import { QUERY_KEYS } from './queryKeys'
 import { Config } from './types'
 import { useCurrentUserId } from './useCurrentUserId'
+import { primeUserData } from './utils/primeUserData'
 
-type GetFollowersArgs = {
+const LIMIT = 15
+
+type UseFollowersArgs = {
   userId: ID | null | undefined
   limit?: number
-  offset?: number
 }
 
 /**
- * Hook to fetch followers for a user with pagination support.
- * NOTE: this is not an infinite query, it only gives you data by your requested limit/offset args
- * If no userId is provided, it will use the current user's ID from the store.
+ * Hook to fetch followers for a user with infinite query support.
+ * This version supports infinite scrolling and maintains the full list of followers.
  */
 export const useFollowers = (
-  { userId, limit = 10, offset = 0 }: GetFollowersArgs,
-  options?: Config
+  { userId, limit = LIMIT }: UseFollowersArgs,
+  config?: Config
 ) => {
-  const { audiusSdk } = useAppContext()
+  const { audiusSdk } = useAudiusQueryContext()
   const { data: currentUserId } = useCurrentUserId()
+  const queryClient = useQueryClient()
+  const dispatch = useDispatch()
 
-  return useQuery({
-    queryKey: [QUERY_KEYS.followers, userId, { limit, offset }],
-    enabled: options?.enabled !== false && !!userId,
-    queryFn: async () => {
-      const { data = [] } = await audiusSdk!.full.users.getFollowers({
+  const {
+    data,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+    isLoading,
+    ...rest
+  } = useInfiniteQuery({
+    queryKey: [QUERY_KEYS.followers, userId, limit],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage: User[], allPages) => {
+      if (lastPage.length < limit) return undefined
+      return allPages.length * limit
+    },
+    queryFn: async ({ pageParam }) => {
+      const sdk = await audiusSdk()
+      const { data = [] } = await sdk.full.users.getFollowers({
         id: Id.parse(userId),
         limit,
-        offset,
+        offset: pageParam,
         userId: OptionalId.parse(currentUserId)
       })
-      return userMetadataListFromSDK(data)
-    }
+      const users = userMetadataListFromSDK(data)
+      primeUserData({ users, queryClient, dispatch })
+      return users
+    },
+    staleTime: config?.staleTime,
+    enabled: config?.enabled !== false && !!userId
   })
+
+  const flatData = data?.pages.flat() ?? []
+
+  return {
+    data: flatData,
+    hasMore: hasNextPage,
+    isLoadingMore: isFetchingNextPage,
+    loadMore: fetchNextPage,
+    isLoading,
+    ...rest
+  }
 }
---
+++ packages/web/src/components/user-list-modal/components/lists/SupportingUserList.tsx
@@ -0,0 +1,8 @@
+type SupportingUserListProps = {
+  onClose: () => void
+}
+
+export const SupportingUserList = ({ onClose }: SupportingUserListProps) => {
+  // TODO: Implement with useSupporting hook
+  return null
+}
