98477ac0fcb0e1285bee9e826b766890aa55ecf3
Dylan Jeffers <dylan@audius.co>
2025-01-11 00:49:42 -0800

    Following

--- packages/mobile/src/screens/user-list-screen/FollowingScreen.tsx
+++ packages/mobile/src/screens/user-list-screen/FollowingScreen.tsx
@@ -1,18 +1,10 @@
-import { useCallback } from 'react'
-
-import {
-  followingUserListActions,
-  followingUserListSelectors
-} from '@audius/common/store'
-import { useDispatch } from 'react-redux'
+import { useFollowing } from '@audius/common/api'
 
 import { IconUserList } from '@audius/harmony-native'
 import { useProfileRoute } from 'app/hooks/useRoute'
 
-import { UserList } from './UserList'
 import { UserListScreen } from './UserListScreen'
-const { setFollowing } = followingUserListActions
-const { getUserList } = followingUserListSelectors
+import { UserListV2 } from './UserListV2'
 
 const messages = {
   title: 'Following'
@@ -21,19 +13,11 @@ const messages = {
 export const FollowingScreen = () => {
   const { params } = useProfileRoute<'Following'>()
   const { userId } = params
-  const dispatch = useDispatch()
-
-  const handleSetFollowing = useCallback(() => {
-    dispatch(setFollowing(userId))
-  }, [dispatch, userId])
+  const query = useFollowing({ userId })
 
   return (
     <UserListScreen title={messages.title} titleIcon={IconUserList}>
-      <UserList
-        userSelector={getUserList}
-        tag='FOLLOWING'
-        setUserList={handleSetFollowing}
-      />
+      <UserListV2 {...query} tag='FOLLOWING' />
     </UserListScreen>
   )
 }
