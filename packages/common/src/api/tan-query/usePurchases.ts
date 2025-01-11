7771a7a2bac7bf5b38d2a2783636949f8593db69
Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>
2025-01-10 16:16:21 -0800

    [C-5645] Migrate trending slice to tan-query (#10997)

--- packages/common/src/api/tan-query/usePurchases.ts
+++ packages/common/src/api/tan-query/usePurchases.ts
@@ -11,33 +11,27 @@ import {
   USDCContentPurchaseType,
   USDCPurchaseDetails
 } from '~/models/USDCTransactions'
-import { Nullable } from '~/utils/typeUtils'
 
 import { QUERY_KEYS } from './queryKeys'
+import { Config } from './types'
 import { useCollections } from './useCollections'
 import { useTracks } from './useTracks'
 import { useUsers } from './useUsers'
 
+const PAGE_SIZE = 10
+
 export type GetPurchaseListArgs = {
-  userId: Nullable<ID>
+  userId: ID | null | undefined
   sortMethod?: full.GetPurchasesSortMethodEnum
   sortDirection?: full.GetPurchasesSortDirectionEnum
+  pageSize?: number
 }
 
-export const usePurchases = (
-  args: GetPurchaseListArgs,
-  options: { pageSize: number; enabled?: boolean }
-) => {
+export const usePurchases = (args: GetPurchaseListArgs, options?: Config) => {
+  const { userId, sortMethod, sortDirection, pageSize = PAGE_SIZE } = args
   const { audiusSdk } = useAudiusQueryContext()
-  const { pageSize, enabled } = options
   const queryResult = useInfiniteQuery({
-    queryKey: [
-      QUERY_KEYS.purchases,
-      args.userId,
-      args.sortMethod,
-      args.sortDirection
-    ],
-    enabled: enabled !== false && !!args.userId,
+    queryKey: [QUERY_KEYS.purchases, args],
     initialPageParam: 0,
     getNextPageParam: (
       lastPage: USDCPurchaseDetails[],
@@ -49,46 +43,53 @@ export const usePurchases = (
     queryFn: async ({ pageParam }) => {
       const sdk = await audiusSdk()
       const { data = [] } = await sdk.full.users.getPurchases({
+        id: Id.parse(userId),
+        userId: Id.parse(userId),
         limit: pageSize,
         offset: pageParam,
-        sortDirection: args.sortDirection,
-        sortMethod: args.sortMethod,
-        id: Id.parse(args.userId),
-        userId: Id.parse(args.userId)
+        sortDirection,
+        sortMethod
       })
 
       return data.map(purchaseFromSDK)
-    }
+    },
+    select: (data) => data.pages.flat(),
+    ...options,
+    enabled: options?.enabled !== false && !!userId
   })
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, ...rest } =
     queryResult
 
-  const { flatData, userIdsToFetch, trackIdsToFetch, collectionIdsToFetch } =
+  const { userIdsToFetch, trackIdsToFetch, collectionIdsToFetch } =
     useMemo(() => {
-      const flatData = data?.pages.flat() ?? []
+      if (!data)
+        return {
+          userIdsToFetch: [],
+          trackIdsToFetch: [],
+          collectionIdsToFetch: []
+        }
       return {
-        flatData,
-        userIdsToFetch: flatData.map(({ buyerUserId }) => buyerUserId),
-        trackIdsToFetch: flatData
+        userIdsToFetch: data.map(({ buyerUserId }) => buyerUserId),
+        trackIdsToFetch: data
           .filter(
             ({ contentType }) => contentType === USDCContentPurchaseType.TRACK
           )
           .map(({ contentId }) => contentId),
-        collectionIdsToFetch: flatData
+        collectionIdsToFetch: data
           .filter(
             ({ contentType }) => contentType === USDCContentPurchaseType.ALBUM
           )
           .map(({ contentId }) => contentId)
       }
-    }, [data?.pages])
+    }, [data])
 
   // Call the hooks dropping results to pre-fetch the data
   useUsers(userIdsToFetch)
   useTracks(trackIdsToFetch)
   useCollections(collectionIdsToFetch)
   return {
-    data: flatData,
+    data,
     loadMore: fetchNextPage,
     hasMore: hasNextPage,
     isLoadingMore: isFetchingNextPage,
---
+++ packages/common/src/api/tan-query/useTrending.ts
@@ -0,0 +1,81 @@
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { useDispatch } from 'react-redux'
+
+import { userTrackMetadataFromSDK } from '~/adapters/track'
+import { transformAndCleanList } from '~/adapters/utils'
+import { useAudiusQueryContext } from '~/audius-query'
+import { OptionalId } from '~/models/Identifiers'
+import { TimeRange } from '~/models/TimeRange'
+import { Track } from '~/models/Track'
+import { StringKeys } from '~/services/remote-config'
+import { Genre } from '~/utils/genres'
+
+import { QUERY_KEYS } from './queryKeys'
+import { Config } from './types'
+import { useCurrentUserId } from './useCurrentUserId'
+import { primeTrackData } from './utils/primeTrackData'
+
+const PAGE_SIZE = 10
+
+type GetTrendingArgs = {
+  timeRange: TimeRange
+  genre?: Genre | null
+  pageSize?: number
+}
+
+export const useTrending = (args: GetTrendingArgs, options: Config) => {
+  const { timeRange, genre, pageSize = PAGE_SIZE } = args
+  const { audiusSdk, remoteConfigInstance } = useAudiusQueryContext()
+  const queryClient = useQueryClient()
+  const currentUserId = useCurrentUserId()
+  const dispatch = useDispatch()
+
+  const queryResult = useInfiniteQuery({
+    queryKey: [QUERY_KEYS.trending, args],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage: Track[], allPages: Track[][]) => {
+      if (lastPage.length < pageSize) return undefined
+      return allPages.length * pageSize
+    },
+    queryFn: async ({ pageParam }) => {
+      const sdk = await audiusSdk()
+      const version = remoteConfigInstance.getRemoteVar(
+        StringKeys.TRENDING_EXPERIMENT
+      )
+
+      const args = {
+        time: timeRange,
+        genre: genre ?? undefined,
+        userId: OptionalId.parse(currentUserId),
+        limit: pageSize,
+        offset: pageParam,
+        version
+      }
+      const { data = [] } = version
+        ? await sdk.full.tracks.getTrendingTracksWithVersion({
+            ...args,
+            version
+          })
+        : await sdk.full.tracks.getTrendingTracks(args)
+
+      const tracks = transformAndCleanList(data, userTrackMetadataFromSDK)
+
+      primeTrackData({ tracks, queryClient, dispatch })
+
+      return tracks
+    },
+    select: (data) => data.pages.flat(),
+    ...options
+  })
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, ...rest } =
+    queryResult
+
+  return {
+    data,
+    loadMore: fetchNextPage,
+    hasMore: hasNextPage,
+    isLoadingMore: isFetchingNextPage,
+    ...rest
+  }
+}
