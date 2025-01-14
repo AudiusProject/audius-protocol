3b253ceb917326119daed801969df0096fe39870
Raymond Jacobson <ray@audius.co>
2025-01-13 19:29:09 -0800

    Add back new privacy policy banner (#11018)

--- packages/web/src/app/web-player/WebPlayer.jsx
+++ packages/web/src/app/web-player/WebPlayer.jsx
@@ -32,6 +32,7 @@ import AppRedirectListener from 'components/app-redirect-popover/AppRedirectList
 import { AppRedirectPopover } from 'components/app-redirect-popover/components/AppRedirectPopover'
 import { AppBannerWrapper } from 'components/banner/AppBannerWrapper'
 import { DownloadAppBanner } from 'components/banner/DownloadAppBanner'
+// import { TermsOfServiceUpdateBanner } from 'components/banner/TermsOfServiceUpdateBanner'
 import { UpdateAppBanner } from 'components/banner/UpdateAppBanner'
 import { Web3ErrorBanner } from 'components/banner/Web3ErrorBanner'
 import { ChatListener } from 'components/chat-listener/ChatListener'
