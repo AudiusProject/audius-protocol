c0a83bce8bc5853318a64e03923a6f049068111b
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-01-15 15:13:26 -0500

    [PAY-3548] Split account metadata out of user metadata (#10989)

--- packages/common/src/models/User.ts
+++ packages/common/src/models/User.ts
@@ -22,6 +22,8 @@ export type UserMetadata = {
   album_count: number
   allow_ai_attribution?: boolean
   artist_pick_track_id: Nullable<number>
+  associated_wallets_balance?: Nullable<StringWei>
+  balance?: Nullable<StringWei>
   bio: Nullable<string>
   blocknumber: number
   collectibleList?: Collectible[]
@@ -64,21 +66,13 @@ export type UserMetadata = {
   spl_usdc_payout_wallet?: Nullable<SolanaWalletAddress>
   supporter_count: number
   supporting_count: number
+  total_balance?: Nullable<StringWei>
   track_count: number
-
-  // Only present on the "current" account
-  track_save_count?: number
   user_id: number
   wallet?: string
-  balance?: Nullable<StringWei>
-  total_balance?: Nullable<StringWei>
-  associated_wallets?: Nullable<string[]>
-  associated_sol_wallets?: Nullable<string[]>
-  associated_wallets_balance?: Nullable<StringWei>
-  playlist_library?: Nullable<PlaylistLibrary>
-  userBank?: SolanaWalletAddress
+
+  // Used by redux cache
   local?: boolean
-  events?: UserEvent
 } & Timestamped
 
 export type AccountUserMetadata = {
@@ -107,9 +101,17 @@ export type ComputedUserProperties = {
 
 export type User = UserMetadata & ComputedUserProperties
 
+export type WriteableUserMetadata = User & {
+  events?: UserEvent
+  associated_sol_wallets?: Nullable<Record<string, { signature: string }>>
+  associated_wallets?: Nullable<Record<string, { signature: string }>>
+}
+
 export type CachedAccount = {
   userId: ID
   collections: AccountCollection[]
+  playlistLibrary?: Nullable<PlaylistLibrary>
+  trackSaveCount: number
   guestEmail: string | null
 }
 
