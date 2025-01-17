64c81173205180e88d466f444aa078ea7b9ac706
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-01-17 09:58:42 -0800

    add wagmi config and provider

--- packages/web/src/app/AppProviders.tsx
+++ packages/web/src/app/AppProviders.tsx
@@ -6,6 +6,7 @@ import { Provider as ReduxProvider } from 'react-redux'
 import { Router } from 'react-router-dom'
 import { CompatRouter } from 'react-router-dom-v5-compat'
 import { LastLocationProvider } from 'react-router-last-location'
+import { WagmiProvider } from 'wagmi'
 
 import { RouterContextProvider } from 'components/animated-switch/RouterContextProvider'
 import { HeaderContextProvider } from 'components/header/mobile/HeaderContextProvider'
@@ -14,6 +15,7 @@ import { ScrollProvider } from 'components/scroll-provider/ScrollProvider'
 import { ToastContextProvider } from 'components/toast/ToastContext'
 import { useIsMobile } from 'hooks/useIsMobile'
 import { MainContentContextProvider } from 'pages/MainContentContext'
+import { wagmiConfig } from 'services/audius-sdk/wagmi'
 import { queryClient } from 'services/query-client'
 import { configureStore } from 'store/configureStore'
 import { getSystemAppearance, getTheme } from 'utils/theme/theme'
@@ -55,37 +57,39 @@ export const AppProviders = ({ children }: AppProvidersProps) => {
   })
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReduxProvider store={store}>
-        <Router history={storeHistory}>
-          <CompatRouter>
-            <LastLocationProvider>
-              <RouterContextProvider>
-                <HeaderContextProvider>
-                  <NavProvider>
-                    <ScrollProvider>
-                      <ToastContextProvider>
-                        <AppContextProvider>
-                          <AudiusQueryProvider>
-                            <MainContentContextProvider>
-                              <ThemeProvider>
-                                <SvgGradientProvider>
-                                  {children}
-                                </SvgGradientProvider>
-                              </ThemeProvider>
-                            </MainContentContextProvider>
-                          </AudiusQueryProvider>
-                        </AppContextProvider>
-                      </ToastContextProvider>
-                    </ScrollProvider>
-                  </NavProvider>
-                </HeaderContextProvider>
-              </RouterContextProvider>
-            </LastLocationProvider>
-          </CompatRouter>
-        </Router>
-      </ReduxProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ReduxProvider store={store}>
+          <Router history={storeHistory}>
+            <CompatRouter>
+              <LastLocationProvider>
+                <RouterContextProvider>
+                  <HeaderContextProvider>
+                    <NavProvider>
+                      <ScrollProvider>
+                        <ToastContextProvider>
+                          <AppContextProvider>
+                            <AudiusQueryProvider>
+                              <MainContentContextProvider>
+                                <ThemeProvider>
+                                  <SvgGradientProvider>
+                                    {children}
+                                  </SvgGradientProvider>
+                                </ThemeProvider>
+                              </MainContentContextProvider>
+                            </AudiusQueryProvider>
+                          </AppContextProvider>
+                        </ToastContextProvider>
+                      </ScrollProvider>
+                    </NavProvider>
+                  </HeaderContextProvider>
+                </RouterContextProvider>
+              </LastLocationProvider>
+            </CompatRouter>
+          </Router>
+        </ReduxProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
