21a6ea6f97ab39f9ebed54b9e5399de8ab07335e
Farid Salau <43255135+faridsalau@users.noreply.github.com>
2025-01-21 13:53:48 -0600

    [QA-1902] Fix double select (#11102)

--- packages/web/src/components/nav/desktop/LeftNavLink.tsx
+++ packages/web/src/components/nav/desktop/LeftNavLink.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+
 import { Name } from '@audius/common/models'
 import { NavItem, NavItemProps } from '@audius/harmony'
 import { useDispatch } from 'react-redux'
@@ -13,12 +15,27 @@ export type LeftNavLinkProps = Omit<NavItemProps, 'isSelected'> & {
   to?: string
   disabled?: boolean
   restriction?: RestrictionType
+  exact?: boolean
 }
 
 export const LeftNavLink = (props: LeftNavLinkProps) => {
-  const { to, disabled, children, onClick, restriction, ...other } = props
+  const {
+    to,
+    disabled,
+    children,
+    onClick,
+    restriction,
+    exact = false,
+    ...other
+  } = props
   const location = useLocation()
   const dispatch = useDispatch()
+  const isSelected = useMemo(() => {
+    if (exact) {
+      return to ? location.pathname === to : false
+    }
+    return to ? location.pathname.startsWith(to) : false
+  }, [to, location.pathname, exact])
 
   const requiresAccountOnClick = useRequiresAccountOnClick(
     (e) => {
@@ -43,7 +60,7 @@ export const LeftNavLink = (props: LeftNavLinkProps) => {
     <NavLink to={to ?? ''} onClick={requiresAccountOnClick} draggable={false}>
       <NavItem
         {...other}
-        isSelected={to ? location.pathname.startsWith(to) : false}
+        isSelected={isSelected}
         css={{
           opacity: disabled ? 0.5 : 1,
           cursor: 'pointer'
