e97d310a51d9c81ad57f5254be41e46c169e5b3e
Randy Schott <1815175+schottra@users.noreply.github.com>
2025-01-22 17:35:38 -0500

    clean up types for CollectionTrackField

--- packages/web/src/pages/upload-page/types.ts
+++
@@ -1,9 +0,0 @@
-import { TrackForEdit, TrackForUpload } from '@audius/common/store'
-
-export type CollectionTrackForUpload = TrackForUpload & {
-  override: boolean
-}
-
-export type CollectionTrackForEdit = TrackForEdit & {
-  override: boolean
-}
