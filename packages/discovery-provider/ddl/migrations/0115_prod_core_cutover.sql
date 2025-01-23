284ab6e895dde54eb0e68d00b90d36bae1dc5307
Alec Savvy <alecsavvy@outlook.com>
2025-01-23 08:43:45 -0700

    comment

--- packages/discovery-provider/ddl/migrations/0115_prod_core_cutover.sql
+++ packages/discovery-provider/ddl/migrations/0115_prod_core_cutover.sql
@@ -1,6 +1,6 @@
 begin;
 do $$ begin
--- stage gate
+-- prod gate
 if exists (select * from "blocks" where "blockhash" = '0x8d5e6984014505e1e11bcbb1ca1a13bcc6ae85ac74014710a73271d82ca49f01') then
   -- jump core indexing to the expected block
   insert into core_indexed_blocks (blockhash, parenthash, chain_id, height) 
