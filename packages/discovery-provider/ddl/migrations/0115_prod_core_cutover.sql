3cbc267ad056759fbc30755796bd8c457d9c38c0
Alec Savvy <alecsavvy@outlook.com>
2025-01-23 08:41:09 -0700

    set for 2pm mst and core block

--- packages/discovery-provider/ddl/migrations/0115_prod_core_cutover.sql
+++ packages/discovery-provider/ddl/migrations/0115_prod_core_cutover.sql
@@ -5,10 +5,10 @@ if exists (select * from "blocks" where "blockhash" = '0x8d5e6984014505e1e11bcbb
   -- jump core indexing to the expected block
   insert into core_indexed_blocks (blockhash, parenthash, chain_id, height) 
   values (
-    'TODO - get from chain', 
-    'TODO - get from chain', 
-    'audius-mainnet-alpha', 
-    10000
+    'FF712E9F69D1B86879C738826C8C0B4ECDD7DFDC9696310E3BFB0D6AFA0945E7', 
+    '3B68D2046A9836FA374018F36CDDECE8DA1A02E0D1EEB5F1AC272056EFEC0004', 
+    'audius-mainnet-alpha',
+    113180
   ) 
   on conflict (chain_id, height) do nothing;
 end if;
