46eebbf4ee68d086f3acb71cd068e5a470f181dd
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-01-23 19:16:03 -0800

    fix check workflow status script

--- .circleci/scripts/check-workflow-statuses.sh
+++ .circleci/scripts/check-workflow-statuses.sh
@@ -8,13 +8,11 @@ curl -s --url "https://circleci.com/api/v2/pipeline/${CIRCLE_PIPELINE_ID}/workfl
 echo "Generating Workflow Statuses..."
 for workflow in $(jq -r -c '.items[] | select(.id != "'$CIRCLE_WORKFLOW_ID'")' /tmp/workflows.json)
 do
-  workflow_id=$(echo $workflow | jq -r '.id')
-  workflow_filename="/tmp/workflow-${workflow_id}.json"
-  curl -s 2>/dev/null --url $(echo $workflow | jq -r '"https://circleci.com/api/v2/workflow/\(.id)/job"') -H "Circle-Token: ${CIRCLE_TOKEN}" -o $workflow_filename
-  workflow_url=$(echo $workflow | jq -r '"'$PIPELINE_URL'/workflows/\(.id)"')
-  if $(jq -r '"\(if .status == "failed" then 1 else 0 end\)"') then
-    echo "Failed workflow: $workflow_id"
-    return 1
+  workflow_name=$(echo $workflow | jq -r '.name')
+  if [[ $(echo $workflow | jq -r 'if .status == "failed" then 1 else 0 end') == "1" ]]
+  then
+    echo "Failed workflow: $workflow_name"
+    exit 1
   fi
 done
-return 0
+exit 0
