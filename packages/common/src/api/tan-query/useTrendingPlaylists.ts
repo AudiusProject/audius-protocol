e33affe84a3d6c63bd02e1f2fa1161c266e4b207
Dylan Jeffers <dylan@audius.co>
2025-01-27 13:58:38 -0800

    [C-7502] Migrate trending playlists to tanstack query

---
+++ packages/common/src/api/tan-query/useTrendingPlaylists.ts
@@ -0,0 +1,95 @@
+import { OptionalId } from '@audius/sdk'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { useDispatch } from 'react-redux'
+
+import {
+  transformAndCleanList,
+  userCollectionMetadataFromSDK
+} from '~/adapters'
+import { useAudiusQueryContext } from '~/audius-query'
+import { PlaybackSource } from '~/models/Analytics'
+import {
+  trendingPlaylistsPageLineupActions,
+  trendingPlaylistsPageLineupSelectors
+} from '~/store/pages'
+
+import { QueryOptions } from './types'
+import { useCurrentUserId } from './useCurrentUserId'
+import { loadNextPage } from './utils/infiniteQueryLoadNextPage'
+import { primeCollectionData } from './utils/primeCollectionData'
+import { useLineupQuery } from './utils/useLineupQuery'
+
+const DEFAULT_PAGE_SIZE = 10
+
+type UseTrendingPlaylistsArgs = {
+  pageSize?: number
+}
+
+export const useTrendingPlaylists = (
+  { pageSize = DEFAULT_PAGE_SIZE }: UseTrendingPlaylistsArgs = {},
+  config?: QueryOptions
+) => {
+  const { audiusSdk } = useAudiusQueryContext()
+  const { data: currentUserId } = useCurrentUserId()
+  const queryClient = useQueryClient()
+  const dispatch = useDispatch()
+
+  const queryData = useInfiniteQuery({
+    queryKey: ['trendingPlaylists', pageSize],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < pageSize) return undefined
+      return allPages.length * pageSize
+    },
+    queryFn: async ({ pageParam }) => {
+      const sdk = await audiusSdk()
+
+      const { data = [] } = await sdk.full.playlists.getTrendingPlaylists({
+        userId: OptionalId.parse(currentUserId),
+        limit: pageSize,
+        offset: pageParam,
+        time: 'week' as const
+      })
+
+      const processedPlaylists = transformAndCleanList(
+        data,
+        userCollectionMetadataFromSDK
+      )
+      primeCollectionData({
+        collections: processedPlaylists,
+        queryClient,
+        dispatch
+      })
+
+      // Update lineup when new data arrives
+      dispatch(
+        trendingPlaylistsPageLineupActions.fetchLineupMetadatas(
+          pageParam,
+          pageSize,
+          false,
+          {
+            playlists: processedPlaylists
+          }
+        )
+      )
+
+      return processedPlaylists
+    },
+    staleTime: config?.staleTime,
+    enabled: config?.enabled !== false
+  })
+
+  const lineupData = useLineupQuery({
+    queryData,
+    lineupActions: trendingPlaylistsPageLineupActions,
+    lineupSelector: trendingPlaylistsPageLineupSelectors.getLineup,
+    playbackSource: PlaybackSource.PLAYLIST_TILE_TRACK
+  })
+
+  return {
+    ...queryData,
+    ...lineupData,
+    loadNextPage: loadNextPage(queryData),
+    pageSize
+  }
+}
