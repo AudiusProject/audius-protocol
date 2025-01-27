21d0269f24bbf921b7d9737b9c62c5f8f37ca392
Dylan Jeffers <dylan@audius.co>
2025-01-27 14:02:57 -0800

    [C-5703] Migrate trending underground to tan-query (#11163)

---
+++ packages/common/src/api/tan-query/useTrendingUnderground.ts
@@ -0,0 +1,83 @@
+import { OptionalId } from '@audius/sdk'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { useDispatch } from 'react-redux'
+
+import { transformAndCleanList, userTrackMetadataFromSDK } from '~/adapters'
+import { useAudiusQueryContext } from '~/audius-query'
+import { PlaybackSource } from '~/models/Analytics'
+import {
+  trendingUndergroundPageLineupActions,
+  trendingUndergroundPageLineupSelectors
+} from '~/store/pages'
+
+import { QueryOptions } from './types'
+import { useCurrentUserId } from './useCurrentUserId'
+import { loadNextPage } from './utils/infiniteQueryLoadNextPage'
+import { primeTrackData } from './utils/primeTrackData'
+import { useLineupQuery } from './utils/useLineupQuery'
+
+const DEFAULT_PAGE_SIZE = 10
+
+type UseTrendingUndergroundArgs = {
+  pageSize?: number
+}
+
+export const useTrendingUnderground = (
+  { pageSize = DEFAULT_PAGE_SIZE }: UseTrendingUndergroundArgs = {},
+  config?: QueryOptions
+) => {
+  const { audiusSdk } = useAudiusQueryContext()
+  const { data: currentUserId } = useCurrentUserId()
+  const queryClient = useQueryClient()
+  const dispatch = useDispatch()
+
+  const queryData = useInfiniteQuery({
+    queryKey: ['trendingUnderground', pageSize],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < pageSize) return undefined
+      return allPages.length * pageSize
+    },
+    queryFn: async ({ pageParam }) => {
+      const sdk = await audiusSdk()
+
+      const { data = [] } = await sdk.full.tracks.getUndergroundTrendingTracks({
+        offset: pageParam,
+        limit: pageSize,
+        userId: OptionalId.parse(currentUserId)
+      })
+
+      const tracks = transformAndCleanList(data, userTrackMetadataFromSDK)
+
+      primeTrackData({ tracks, queryClient, dispatch })
+
+      // Update lineup when new data arrives
+      dispatch(
+        trendingUndergroundPageLineupActions.fetchLineupMetadatas(
+          pageParam,
+          pageSize,
+          false,
+          { tracks }
+        )
+      )
+
+      return tracks
+    },
+    staleTime: config?.staleTime,
+    enabled: config?.enabled !== false
+  })
+
+  const lineupData = useLineupQuery({
+    queryData,
+    lineupActions: trendingUndergroundPageLineupActions,
+    lineupSelector: trendingUndergroundPageLineupSelectors.getLineup,
+    playbackSource: PlaybackSource.TRACK_TILE
+  })
+
+  return {
+    ...queryData,
+    ...lineupData,
+    loadNextPage: loadNextPage(queryData),
+    pageSize
+  }
+}
--- packages/web/src/common/store/pages/trending-underground/lineups/sagas.ts
+++ packages/web/src/common/store/pages/trending-underground/lineups/sagas.ts
@@ -1,73 +1,21 @@
-import {
-  transformAndCleanList,
-  userTrackMetadataFromSDK
-} from '@audius/common/adapters'
 import { Track } from '@audius/common/models'
-import { StringKeys } from '@audius/common/services'
 import {
-  accountSelectors,
   trendingUndergroundPageLineupSelectors,
-  trendingUndergroundPageLineupActions,
-  getContext,
-  getSDK
+  trendingUndergroundPageLineupActions
 } from '@audius/common/store'
-import { OptionalId } from '@audius/sdk'
-import { keccak_256 } from 'js-sha3'
-import { call, select } from 'typed-redux-saga'
 
-import { processAndCacheTracks } from 'common/store/cache/tracks/utils'
 import { LineupSagas } from 'common/store/lineup/sagas'
-import { waitForRead } from 'utils/sagaHelpers'
 
 const { getLineup } = trendingUndergroundPageLineupSelectors
-const getUserId = accountSelectors.getUserId
 
 function* getTrendingUnderground({
-  limit,
-  offset
+  payload
 }: {
-  limit: number
-  offset: number
-}) {
-  yield* waitForRead()
-  const sdk = yield* getSDK()
-  const remoteConfigInstance = yield* getContext('remoteConfigInstance')
-
-  yield call(remoteConfigInstance.waitForRemoteConfig)
-
-  const TF = new Set(
-    remoteConfigInstance.getRemoteVar(StringKeys.UTF)?.split(',') ?? []
-  )
-  const version = remoteConfigInstance.getRemoteVar(
-    StringKeys.UNDERGROUND_TRENDING_EXPERIMENT
-  )
-
-  const currentUserId = yield* select(getUserId)
-
-  const { data = [] } = version
-    ? yield* call(
-        [
-          sdk.full.tracks,
-          sdk.full.tracks.getUndergroundTrendingTracksWithVersion
-        ],
-        { version, offset, limit, userId: OptionalId.parse(currentUserId) }
-      )
-    : yield* call(
-        [sdk.full.tracks, sdk.full.tracks.getUndergroundTrendingTracks],
-        { offset, limit, userId: OptionalId.parse(currentUserId) }
-      )
-
-  let tracks = transformAndCleanList(data, userTrackMetadataFromSDK)
-
-  if (TF.size > 0) {
-    tracks = tracks.filter((t) => {
-      const shaId = keccak_256(t.track_id.toString())
-      return !TF.has(shaId)
-    })
+  payload?: {
+    tracks: Track[]
   }
-
-  const processed = yield* processAndCacheTracks(tracks)
-  return processed
+}) {
+  return payload?.tracks ?? []
 }
 
 class UndergroundTrendingSagas extends LineupSagas<Track> {
