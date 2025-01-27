ff6e613e95721f99aaabe4db5d82d70707830dfd
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-01-27 10:04:48 -0800

    Change CI for PRs and re-enable Playwright for release (#11147)

---
+++ .circleci/scripts/check-workflow-statuses.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Fetching pipeline workflows..."
+curl -s --url "https://circleci.com/api/v2/pipeline/${CIRCLE_PIPELINE_ID}/workflow" \
+    -H "Circle-Token: ${CIRCLE_TOKEN}" \
+    > /tmp/workflows.json
+
+for workflow in $(jq -r -c '.items[] | select(.id != "'$CIRCLE_WORKFLOW_ID'")' /tmp/workflows.json)
+do
+  workflow_name=$(echo $workflow | jq -r '.name')
+  if [[ $(echo $workflow | jq -r 'if .status == "failed" then 1 else 0 end') == "1" ]]
+  then
+    echo "Failed workflow: $workflow_name"
+    exit 1
+  fi
+done
+exit 0
