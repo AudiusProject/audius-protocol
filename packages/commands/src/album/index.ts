1452686927b65c44379e74a5b79dad61ad738579
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-01-30 17:07:18 -0800

    Fix playwright seeding commands (#11205)

--- packages/commands/src/album/index.ts
+++ packages/commands/src/album/index.ts
@@ -1,6 +1,10 @@
 import { Command } from '@commander-js/extra-typings'
 import { purchaseAlbumCommand } from './purchase-album'
+import { createAlbumCommand } from './create-album'
+import { getAlbumCommand } from './get-album'
 
 export const albumCommand = new Command('album')
   .description('Commands that create or target a specific album')
+  .addCommand(createAlbumCommand)
+  .addCommand(getAlbumCommand)
   .addCommand(purchaseAlbumCommand)
--- packages/web/e2e/data.seed.ts
+++ packages/web/e2e/data.seed.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process'
+import { existsSync, mkdirSync, readFileSync } from 'fs'
 
 import { test as setup } from './test'
-import { existsSync, mkdirSync, readFileSync } from 'fs'
 
 const dataDir = '../web/e2e/data'
 const dataFilePath = (filename: string) => `${dataDir}/${filename}`
@@ -16,43 +16,65 @@ setup('seed data', async () => {
   }
 
   // Create a user
-  console.log('Creating user')
-  audiusCmd(`create-user --output ${dataFilePath('user.json')}`)
+  console.info('Creating user')
+  audiusCmd(`user create -o json > ${dataFilePath('user.json')}`)
+
+  console.info('Getting entropy')
+  audiusCmd(`entropy > ${dataFilePath('entropy.txt')}`)
 
   // Upload a track
-  console.log('Uploading track')
-  audiusCmd(`upload-track --output ${dataFilePath('track.json')}`)
-  const track = JSON.parse(readFileSync(dataFilePath('track.json'), 'utf8'))
+  console.info('Uploading track')
+  audiusCmd(`track upload -o json > ${dataFilePath('track.json')}`)
+  const trackId = JSON.parse(
+    readFileSync(dataFilePath('track.json'), 'utf8')
+  ).trackId
+  audiusCmd(`track get ${trackId} > ${dataFilePath('track.json')}`)
 
   // Create an album
-  console.log('Creating album')
-  audiusCmd(
-    `create-playlist ${track.track_id} -a --output ${dataFilePath(
-      'album.json'
-    )}`
-  )
+  console.info('Creating album')
+  audiusCmd(`album create ${trackId} -o json > ${dataFilePath('album.json')}`)
+  const albumId = JSON.parse(
+    readFileSync(dataFilePath('album.json'), 'utf8')
+  ).playlistId
+  audiusCmd(`album get ${albumId} > ${dataFilePath('album.json')}`)
+
+  // Create an album
+  console.info('Creating album2')
+  audiusCmd(`album create ${trackId} -o json > ${dataFilePath('album2.json')}`)
+  const album2Id = JSON.parse(
+    readFileSync(dataFilePath('album2.json'), 'utf8')
+  ).playlistId
+  audiusCmd(`album get ${album2Id} > ${dataFilePath('album2.json')}`)
 
   // Create a playlist
-  console.log('Creating playlist')
+  console.info('Creating playlist')
   audiusCmd(
-    `create-playlist ${track.track_id} -a --output ${dataFilePath(
-      'playlist.json'
-    )}`
+    `playlist create ${trackId} -o json > ${dataFilePath('playlist.json')}`
   )
+  const playlistId = JSON.parse(
+    readFileSync(dataFilePath('playlist.json'), 'utf8')
+  ).playlistId
+  audiusCmd(`playlist get ${playlistId} > ${dataFilePath('playlist.json')}`)
 
   // Upload a remix
-  console.log('Uploading remix')
+  console.info('Uploading remix')
   audiusCmd(
-    `upload-track --remix ${track.track_id} --output ${dataFilePath(
-      'remix.json'
-    )}`
+    `track upload --remix-of '{"tracks":[{"parentTrackId":"${trackId}"}]}' -o json > ${dataFilePath('remix.json')}`
   )
+  const remixId = JSON.parse(
+    readFileSync(dataFilePath('remix.json'), 'utf8')
+  ).trackId
+  audiusCmd(`track get ${remixId} > ${dataFilePath('remix.json')}`)
 
   // Create another user
-  console.log('Creating user2')
-  audiusCmd(`create-user --output ${dataFilePath('user2.json')}`)
+  console.info('Creating user2')
+  audiusCmd(`user create -ai -o json > ${dataFilePath('user2.json')}`)
 
   // Upload another track
-  console.log('Uploading track2')
-  audiusCmd(`upload-track --output ${dataFilePath('track2.json')}`)
+  console.info('Uploading track2')
+  audiusCmd(`track upload -o json > ${dataFilePath('track2.json')}`)
+  const track2Id = JSON.parse(
+    readFileSync(dataFilePath('track2.json'), 'utf8')
+  ).trackId
+  audiusCmd(`track get ${track2Id} > ${dataFilePath('track2.json')}`)
 })
