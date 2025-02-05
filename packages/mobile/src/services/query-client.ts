99cf3d777d8060e4953d8c56aff18bac7aa5a1ad
Dylan Jeffers <dylan@audius.co>
2025-02-05 09:30:32 -0800

    [C-5719] Add default cache invalidation behavior (#11244) (#11266)
    Co-authored-by: Andrew Mendelsohn <andrew.j.mendelsohn@gmail.com>

--- packages/mobile/src/services/query-client.ts
+++ packages/mobile/src/services/query-client.ts
@@ -1,3 +1,15 @@
 import { QueryClient } from '@tanstack/react-query'
+import { env } from 'services/env'
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 5, // 5 minutes
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+      refetchOnMount: true,
+      throwOnError: env.ENVIRONMENT === 'development' // feature-tan-query TODO: remove before going to main?
+    }
+  }
+})
