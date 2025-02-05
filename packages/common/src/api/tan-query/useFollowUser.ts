397f12fa4aef455c413b3b597d681d5822a06eb7
amendelsohn <andrew.j.mendelsohn@gmail.com>
2025-02-04 16:57:42 -0800

    [C-5740] Follow user to tan-query

---
+++ packages/common/src/api/tan-query/useFollowUser.ts
@@ -0,0 +1,143 @@
+import { Id } from '@audius/sdk'
+import { Action } from '@reduxjs/toolkit'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useDispatch } from 'react-redux'
+
+import { useAudiusQueryContext } from '~/audius-query'
+import { useAppContext } from '~/context/appContext'
+import { Name, FollowSource } from '~/models/Analytics'
+import { Feature } from '~/models/ErrorReporting'
+import { ID } from '~/models/Identifiers'
+import { UserMetadata } from '~/models/User'
+
+import { useCurrentUserId } from '..'
+
+import { QUERY_KEYS } from './queryKeys'
+import { getUserQueryKey } from './useUser'
+import { getUserByHandleQueryKey } from './useUserByHandle'
+
+type MutationContext = {
+  previousUser: UserMetadata | undefined
+  previousAccountUser: UserMetadata | undefined
+}
+
+type FollowUserParams = {
+  followeeUserId: ID
+  source?: FollowSource
+  onSuccessActions?: Action[]
+}
+
+export const useFollowUser = () => {
+  const { audiusSdk, reportToSentry } = useAudiusQueryContext()
+  const queryClient = useQueryClient()
+  const { data: currentUserId } = useCurrentUserId()
+  const {
+    analytics: { track, make }
+  } = useAppContext()
+  const dispatch = useDispatch()
+
+  return useMutation<void, Error, FollowUserParams, MutationContext>({
+    mutationFn: async ({
+      followeeUserId,
+      source,
+      onSuccessActions
+    }: FollowUserParams) => {
+      if (!currentUserId || currentUserId === followeeUserId) {
+        return
+      }
+
+      const sdk = await audiusSdk()
+      await sdk.users.followUser({
+        userId: Id.parse(currentUserId),
+        followeeUserId: Id.parse(followeeUserId)
+      })
+
+      if (onSuccessActions) {
+        onSuccessActions.forEach((action) => dispatch(action))
+      }
+
+      if (source) {
+        track(
+          make({
+            eventName: Name.FOLLOW,
+            id: followeeUserId.toString(),
+            source
+          })
+        )
+      }
+    },
+    onMutate: async ({ followeeUserId }): Promise<MutationContext> => {
+      await queryClient.cancelQueries({
+        queryKey: [QUERY_KEYS.user, followeeUserId]
+      })
+
+      const previousUser = queryClient.getQueryData<UserMetadata>([
+        QUERY_KEYS.user,
+        followeeUserId
+      ])
+
+      const previousAccountUser = queryClient.getQueryData<UserMetadata>([
+        QUERY_KEYS.accountUser
+      ])
+
+      if (previousUser) {
+        queryClient.setQueryData(getUserQueryKey(followeeUserId), {
+          ...previousUser,
+          does_current_user_follow: true,
+          follower_count: previousUser.follower_count + 1
+        })
+
+        if (previousUser.handle) {
+          queryClient.setQueryData(
+            getUserByHandleQueryKey(previousUser.handle),
+            (old: any) => ({
+              ...old,
+              does_current_user_follow: true,
+              follower_count: old.follower_count + 1
+            })
+          )
+        }
+      }
+
+      if (previousAccountUser) {
+        queryClient.setQueryData([QUERY_KEYS.accountUser], {
+          ...previousAccountUser,
+          followee_count: previousAccountUser.followee_count + 1
+        })
+      }
+
+      return { previousUser, previousAccountUser }
+    },
+    onError: (
+      error,
+      { followeeUserId },
+      context: MutationContext | undefined
+    ) => {
+      const { previousUser, previousAccountUser } = context ?? {}
+
+      if (previousUser) {
+        queryClient.setQueryData(getUserQueryKey(followeeUserId), previousUser)
+
+        if (previousUser.handle) {
+          queryClient.setQueryData(
+            getUserByHandleQueryKey(previousUser.handle),
+            previousUser
+          )
+        }
+      }
+
+      if (previousAccountUser) {
+        queryClient.setQueryData([QUERY_KEYS.accountUser], previousAccountUser)
+      }
+
+      reportToSentry({
+        error,
+        additionalInfo: {
+          followeeUserId
+        },
+        feature: Feature.Social,
+        name: 'Follow User'
+      })
+    }
+  })
+}
