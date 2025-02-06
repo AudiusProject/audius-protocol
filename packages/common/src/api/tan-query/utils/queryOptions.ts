91ed346ecc195873a07c6206e775571a9c15777a
JD Francis <jd@audius.co>
2025-02-06 12:20:21 -0600

    comment

--- packages/common/src/api/tan-query/utils/queryOptions.ts
+++ packages/common/src/api/tan-query/utils/queryOptions.ts
@@ -1,5 +1,9 @@
 import { QueryOptions } from '../types'
 
+/**
+ * For some options (staleTime), tan-query will see an optional query option has set the value to undefined and use that value instead of our global default
+ * So instead this helper makes sure that if the staleTime is not provided, it will be set to the global default because we won't pass anything in
+ */
 export const queryOptions = (options?: QueryOptions) => {
   if (options === undefined) return options
   const queryOptions = { ...options }
