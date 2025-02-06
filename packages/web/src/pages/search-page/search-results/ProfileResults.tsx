5a49a9a1538f0e832440d0b6c346eee362bf7bd8
JD Francis <jd@audius.co>
2025-02-06 12:16:28 -0600

    undo temp scroll threshold

--- packages/web/src/pages/search-page/search-results/ProfileResults.tsx
+++ packages/web/src/pages/search-page/search-results/ProfileResults.tsx
@@ -142,7 +142,6 @@ export const ProfileResultsPage = () => {
       getScrollParent={getMainContentScrollRef}
       initialLoad={false}
       useWindow={false}
-      threshold={100}
     >
       <Flex
         direction='column'
