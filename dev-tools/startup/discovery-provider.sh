c0d20ee11772481d9936e6aac5f7c0b292a31d55
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-02-05 19:38:02 -0500

    Register DNs on local stack

--- dev-tools/startup/discovery-provider.sh
+++ dev-tools/startup/discovery-provider.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
+
+# Run register script in background
+./scripts/register.py &
+
 # Create sender for current DN
 ../../node_modules/.bin/ts-node ./scripts/createSender.ts ${replica} &
