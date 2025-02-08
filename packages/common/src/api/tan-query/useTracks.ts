0f06631bfa0c23a3e4822dbe64a16caef984d3db
Dylan Jeffers <dylan@audius.co>
2025-02-07 22:52:40 -0800

    Fix explore page featured content

--- packages/common/src/api/tan-query/useTracks.ts
+++ packages/common/src/api/tan-query/useTracks.ts
@@ -7,15 +7,12 @@ import { userTrackMetadataFromSDK } from '~/adapters/track'
 import { transformAndCleanList } from '~/adapters/utils'
 import { useAudiusQueryContext } from '~/audius-query'
 import { ID } from '~/models/Identifiers'
-import { Kind } from '~/models/Kind'
-import { addEntries } from '~/store/cache/actions'
-import { EntriesByKind } from '~/store/cache/types'
 import { removeNullable } from '~/utils/typeUtils'
 
 import { QUERY_KEYS } from './queryKeys'
 import { QueryOptions } from './types'
-import { getTrackQueryKey } from './useTrack'
-import { getUserQueryKey } from './useUser'
+import { useCurrentUserId } from './useCurrentUserId'
+import { primeTrackData } from './utils/primeTrackData'
 
 export const getTracksQueryKey = (trackIds: ID[] | null | undefined) => [
   QUERY_KEYS.tracks,
@@ -29,50 +26,28 @@ export const useTracks = (
   const { audiusSdk } = useAudiusQueryContext()
   const queryClient = useQueryClient()
   const dispatch = useDispatch()
+  const { data: currentUserId } = useCurrentUserId()
+  const encodedIds = trackIds
+    ?.map((id) => OptionalId.parse(id))
+    .filter(removeNullable)
 
   return useQuery({
     queryKey: getTracksQueryKey(trackIds),
     queryFn: async () => {
-      const encodedIds = trackIds
-        ?.map((id) => OptionalId.parse(id))
-        .filter(removeNullable)
-      if (!encodedIds || encodedIds.length === 0) return []
       const sdk = await audiusSdk()
       const { data } = await sdk.full.tracks.getBulkTracks({
-        id: encodedIds
+        id: encodedIds,
+        userId: OptionalId.parse(currentUserId)
       })
 
       const tracks = transformAndCleanList(data, userTrackMetadataFromSDK)
 
-      if (tracks?.length) {
-        const entries: EntriesByKind = {
-          [Kind.TRACKS]: {}
-        }
-
-        tracks.forEach((track) => {
-          // Prime track data
-          queryClient.setQueryData(getTrackQueryKey(track.track_id), track)
-          entries[Kind.TRACKS]![track.track_id] = track
-
-          // Prime user data from track owner
-          if (track.user) {
-            queryClient.setQueryData(
-              getUserQueryKey(track.user.user_id),
-              track.user
-            )
-            if (!entries[Kind.USERS]) entries[Kind.USERS] = {}
-            entries[Kind.USERS][track.user.user_id] = track.user
-          }
-        })
-
-        // Sync all data to Redux in a single dispatch
-        dispatch(addEntries(entries, undefined, undefined, 'react-query'))
-      }
+      primeTrackData({ tracks, queryClient, dispatch })
 
       const tracksMap = keyBy(tracks, 'track_id')
       return trackIds?.map((id) => tracksMap[id])
     },
     ...options,
-    enabled: options?.enabled !== false && !!trackIds
+    enabled: options?.enabled !== false && encodedIds && encodedIds.length > 0
   })
 }
