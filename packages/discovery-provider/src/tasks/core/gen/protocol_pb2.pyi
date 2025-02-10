b64cc80ac999bcaee317acfa46ba2c3ef9be209d
alecsavvy <16739903+alecsavvy@users.noreply.github.com>
2025-02-10 11:24:29 -0700

    use buf for relay and indexer (#11326)

--- packages/discovery-provider/src/tasks/core/gen/protocol_pb2.pyi
+++ packages/discovery-provider/src/tasks/core/gen/protocol_pb2.pyi
@@ -23,6 +23,9 @@ class SignedTransaction(google.protobuf.message.Message):
     VALIDATOR_REGISTRATION_FIELD_NUMBER: builtins.int
     SLA_ROLLUP_FIELD_NUMBER: builtins.int
     MANAGE_ENTITY_FIELD_NUMBER: builtins.int
+    VALIDATOR_DEREGISTRATION_FIELD_NUMBER: builtins.int
+    STORAGE_PROOF_FIELD_NUMBER: builtins.int
+    STORAGE_PROOF_VERIFICATION_FIELD_NUMBER: builtins.int
     signature: builtins.str
     request_id: builtins.str
     @property
@@ -33,6 +36,12 @@ class SignedTransaction(google.protobuf.message.Message):
     def sla_rollup(self) -> global___SlaRollup: ...
     @property
     def manage_entity(self) -> global___ManageEntityLegacy: ...
+    @property
+    def validator_deregistration(self) -> global___ValidatorDeregistration: ...
+    @property
+    def storage_proof(self) -> global___StorageProof: ...
+    @property
+    def storage_proof_verification(self) -> global___StorageProofVerification: ...
     def __init__(
         self,
         *,
@@ -42,10 +51,13 @@ class SignedTransaction(google.protobuf.message.Message):
         validator_registration: global___ValidatorRegistration | None = ...,
         sla_rollup: global___SlaRollup | None = ...,
         manage_entity: global___ManageEntityLegacy | None = ...,
+        validator_deregistration: global___ValidatorDeregistration | None = ...,
+        storage_proof: global___StorageProof | None = ...,
+        storage_proof_verification: global___StorageProofVerification | None = ...,
     ) -> None: ...
-    def HasField(self, field_name: typing.Literal["manage_entity", b"manage_entity", "plays", b"plays", "sla_rollup", b"sla_rollup", "transaction", b"transaction", "validator_registration", b"validator_registration"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing.Literal["manage_entity", b"manage_entity", "plays", b"plays", "request_id", b"request_id", "signature", b"signature", "sla_rollup", b"sla_rollup", "transaction", b"transaction", "validator_registration", b"validator_registration"]) -> None: ...
-    def WhichOneof(self, oneof_group: typing.Literal["transaction", b"transaction"]) -> typing.Literal["plays", "validator_registration", "sla_rollup", "manage_entity"] | None: ...
+    def HasField(self, field_name: typing.Literal["manage_entity", b"manage_entity", "plays", b"plays", "sla_rollup", b"sla_rollup", "storage_proof", b"storage_proof", "storage_proof_verification", b"storage_proof_verification", "transaction", b"transaction", "validator_deregistration", b"validator_deregistration", "validator_registration", b"validator_registration"]) -> builtins.bool: ...
+    def ClearField(self, field_name: typing.Literal["manage_entity", b"manage_entity", "plays", b"plays", "request_id", b"request_id", "signature", b"signature", "sla_rollup", b"sla_rollup", "storage_proof", b"storage_proof", "storage_proof_verification", b"storage_proof_verification", "transaction", b"transaction", "validator_deregistration", b"validator_deregistration", "validator_registration", b"validator_registration"]) -> None: ...
+    def WhichOneof(self, oneof_group: typing.Literal["transaction", b"transaction"]) -> typing.Literal["plays", "validator_registration", "sla_rollup", "manage_entity", "validator_deregistration", "storage_proof", "storage_proof_verification"] | None: ...
 
 global___SignedTransaction = SignedTransaction
 
@@ -269,6 +281,24 @@ class ValidatorRegistration(google.protobuf.message.Message):
 global___ValidatorRegistration = ValidatorRegistration
 
 @typing.final
+class ValidatorDeregistration(google.protobuf.message.Message):
+    DESCRIPTOR: google.protobuf.descriptor.Descriptor
+
+    COMET_ADDRESS_FIELD_NUMBER: builtins.int
+    PUB_KEY_FIELD_NUMBER: builtins.int
+    comet_address: builtins.str
+    pub_key: builtins.bytes
+    def __init__(
+        self,
+        *,
+        comet_address: builtins.str = ...,
+        pub_key: builtins.bytes = ...,
+    ) -> None: ...
+    def ClearField(self, field_name: typing.Literal["comet_address", b"comet_address", "pub_key", b"pub_key"]) -> None: ...
+
+global___ValidatorDeregistration = ValidatorDeregistration
+
+@typing.final
 class TrackPlay(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
 
@@ -374,6 +404,52 @@ class SlaNodeReport(google.protobuf.message.Message):
 global___SlaNodeReport = SlaNodeReport
 
 @typing.final
+class StorageProof(google.protobuf.message.Message):
+    DESCRIPTOR: google.protobuf.descriptor.Descriptor
+
+    HEIGHT_FIELD_NUMBER: builtins.int
+    ADDRESS_FIELD_NUMBER: builtins.int
+    PROVER_ADDRESSES_FIELD_NUMBER: builtins.int
+    CID_FIELD_NUMBER: builtins.int
+    PROOF_SIGNATURE_FIELD_NUMBER: builtins.int
+    height: builtins.int
+    address: builtins.str
+    cid: builtins.str
+    proof_signature: builtins.bytes
+    @property
+    def prover_addresses(self) -> google.protobuf.internal.containers.RepeatedScalarFieldContainer[builtins.str]: ...
+    def __init__(
+        self,
+        *,
+        height: builtins.int = ...,
+        address: builtins.str = ...,
+        prover_addresses: collections.abc.Iterable[builtins.str] | None = ...,
+        cid: builtins.str = ...,
+        proof_signature: builtins.bytes = ...,
+    ) -> None: ...
+    def ClearField(self, field_name: typing.Literal["address", b"address", "cid", b"cid", "height", b"height", "proof_signature", b"proof_signature", "prover_addresses", b"prover_addresses"]) -> None: ...
+
+global___StorageProof = StorageProof
+
+@typing.final
+class StorageProofVerification(google.protobuf.message.Message):
+    DESCRIPTOR: google.protobuf.descriptor.Descriptor
+
+    HEIGHT_FIELD_NUMBER: builtins.int
+    PROOF_FIELD_NUMBER: builtins.int
+    height: builtins.int
+    proof: builtins.bytes
+    def __init__(
+        self,
+        *,
+        height: builtins.int = ...,
+        proof: builtins.bytes = ...,
+    ) -> None: ...
+    def ClearField(self, field_name: typing.Literal["height", b"height", "proof", b"proof"]) -> None: ...
+
+global___StorageProofVerification = StorageProofVerification
+
+@typing.final
 class ManageEntityLegacy(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
 
@@ -383,12 +459,16 @@ class ManageEntityLegacy(google.protobuf.message.Message):
     ACTION_FIELD_NUMBER: builtins.int
     METADATA_FIELD_NUMBER: builtins.int
     SIGNATURE_FIELD_NUMBER: builtins.int
+    SIGNER_FIELD_NUMBER: builtins.int
+    NONCE_FIELD_NUMBER: builtins.int
     user_id: builtins.int
     entity_type: builtins.str
     entity_id: builtins.int
     action: builtins.str
     metadata: builtins.str
     signature: builtins.str
+    signer: builtins.str
+    nonce: builtins.int
     def __init__(
         self,
         *,
@@ -398,7 +478,9 @@ class ManageEntityLegacy(google.protobuf.message.Message):
         action: builtins.str = ...,
         metadata: builtins.str = ...,
         signature: builtins.str = ...,
+        signer: builtins.str = ...,
+        nonce: builtins.int = ...,
     ) -> None: ...
-    def ClearField(self, field_name: typing.Literal["action", b"action", "entity_id", b"entity_id", "entity_type", b"entity_type", "metadata", b"metadata", "signature", b"signature", "user_id", b"user_id"]) -> None: ...
+    def ClearField(self, field_name: typing.Literal["action", b"action", "entity_id", b"entity_id", "entity_type", b"entity_type", "metadata", b"metadata", "nonce", b"nonce", "signature", b"signature", "signer", b"signer", "user_id", b"user_id"]) -> None: ...
 
 global___ManageEntityLegacy = ManageEntityLegacy
