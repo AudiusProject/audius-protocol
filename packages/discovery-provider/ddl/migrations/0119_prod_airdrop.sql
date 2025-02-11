bed0147d5335b36916a2415b5951b8264746effa
isaac <isaac@audius.co>
2025-02-11 12:34:56 -0800

    update sql

--- packages/discovery-provider/ddl/migrations/0119_prod_airdrop.sql
+++ packages/discovery-provider/ddl/migrations/0119_prod_airdrop.sql
@@ -1,27 +1,21 @@
-begin;
-do $$ begin -- run only on prod
-if exists (
-    select *
-    from "blocks"
-    where "blockhash" = '0xe9b0f40bb2356d4fbedde96671503475c1c6c994c7eda082f046f7e9923c6e16'
-) then -- check whether the data has already been backfilled and return early for idempotency
-if exists (
-    select 1
-    from user_challenges
-    where challenge_id = 'o' -- not future proof but safe for now
-) then return;
-end if;
+BEGIN;
+DO $$ BEGIN -- run only on prod
+IF EXISTS (
+    SELECT *
+    FROM "blocks"
+    WHERE "blockhash" = '0xe9b0f40bb2356d4fbedde96671503475c1c6c994c7eda082f046f7e9923c6e16'
+) THEN -- check whether the data has already been backfilled and return early for idempotency
 -- ========== one shot trigger ==========
 WITH max_block AS (
     SELECT MAX(number) AS completed_blocknumber
     FROM blocks
 ),
-one_shot_users as (
-    select temp.handle_lc,
+one_shot_users AS (
+    SELECT temp.handle_lc,
         temp.amount,
         users.user_id
-    from temp_prod_airdrop_pt_one temp
-        join users on temp.handle_lc = users.handle_lc
+    FROM temp_prod_airdrop temp
+        JOIN users ON temp.handle_lc = users.handle_lc
 )
 INSERT INTO user_challenges (
         user_id,
@@ -35,8 +29,8 @@ INSERT INTO user_challenges (
         completed_at
     )
 SELECT osu.user_id,
-    'o' as challenge_id,
-    concat(to_hex(user_id), ':', to_hex(1)) AS specifier,
+    'o' AS challenge_id,
+    CONCAT(to_hex(user_id), ':', to_hex(1)) AS specifier,
     -- user_id:nth_shot for future proofing
     TRUE AS is_complete,
     0 AS current_step_count,
@@ -46,6 +40,6 @@ SELECT osu.user_id,
     NOW() AS created_at
 FROM one_shot_users osu,
     max_block mb;
-end if;
-end $$;
-commit;
+END IF;
+END $$;
+COMMIT;
