8f8543b78cf5d96ab063b2e25c9530e8b982514f
Alec Savvy <alecsavvy@outlook.com>
2025-02-12 12:58:50 -0700

    rm debug logs

--- packages/discovery-provider/src/tasks/entity_manager/entities/user.py
+++ packages/discovery-provider/src/tasks/entity_manager/entities/user.py
@@ -186,7 +186,6 @@ def create_user(
     cid_type: Dict[str, str],
     cid_metadata: Dict[str, Dict],
 ):
-    logger.info(f"entity_manager_event_tx {params.metadata}")
     validate_user_tx(params)
 
     user_id = params.user_id
@@ -210,13 +209,11 @@ def create_user(
         user_metadata, metadata_cid = parse_metadata(
             params.metadata, Action.UPDATE, EntityType.USER
         )
-    except Exception as e:
-        logger.info(f"entity_manager_event_tx error {e}")
+    except Exception:
         # fallback to multi tx signup if parsing fails
         pass
 
     # If parsing succeeds, we know this is a single tx signup
-    logger.info(f"entity_manager_event_tx {user_metadata}")
     if user_metadata is not None:
         validate_user_metadata(
             params.session,
