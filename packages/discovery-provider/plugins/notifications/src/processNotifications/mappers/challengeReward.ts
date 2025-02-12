27a4cace22edfabaa2fda26cbe0a8a0573fcd333
isaac <isaac@audius.co>
2025-02-12 08:09:04 -0800

    Fix airdrop push notif copy

--- packages/discovery-provider/plugins/notifications/src/processNotifications/mappers/challengeReward.ts
+++ packages/discovery-provider/plugins/notifications/src/processNotifications/mappers/challengeReward.ts
@@ -87,9 +87,7 @@ export class ChallengeReward extends BaseNotification<ChallengeRewardRow> {
         this.challengeInfoMap[this.challengeId].amount
       } $AUDIO for being referred! Invite your friends to join to earn more!`
     } else if (this.challengeId === 'o') {
-      return `You’ve earned ${
-        this.amount / AUDIO_DIVISOR
-      } $AUDIO for completing this challenge!`
+      return `Congrats!🎉 You’re eligible for the Airdrop! Claim your tokens now!`
     }
     return `You’ve earned ${
       this.challengeInfoMap[this.challengeId].amount
