96864cd6fce17d00e31f23eed82d1cbdd6497479
Farid Salau <faridsalau@gmail.com>
2025-02-18 08:52:51 -0600

    Add mobile notificiation for endless streak

--- packages/mobile/src/screens/notifications-screen/Notifications/ChallengeRewardNotification.tsx
+++ packages/mobile/src/screens/notifications-screen/Notifications/ChallengeRewardNotification.tsx
@@ -1,6 +1,7 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 
 import type { BNAudio } from '@audius/common/models'
+import { ChallengeName } from '@audius/common/models'
 import type { ChallengeRewardNotification as ChallengeRewardNotificationType } from '@audius/common/store'
 import {
   challengeRewardsConfig,
@@ -27,7 +28,11 @@ const messages = {
     ' for being referred! Invite your friends to join to earn more!',
   challengeCompleteText: ' for completing this challenge!',
   twitterShareText:
-    'I earned $AUDIO for completing challenges on @audius #AudioRewards'
+    'I earned $AUDIO for completing challenges on @audius #AudioRewards',
+  streakMilestone: (amountEarned: number, listenStreak: number) =>
+    `You've earned ${amountEarned} $AUDIO for hitting Day ${listenStreak} of your listening streak! You'll now earn an additional $AUDIO reward for every day you keep your streak going!`,
+  streakMaintenance: (amountEarned: number) =>
+    `You've earned ${amountEarned} $AUDIO for maintaining your listening streak! Keep your streak going to continue earning daily rewards!`
 }
 
 type ChallengeRewardNotificationProps = {
@@ -44,7 +49,7 @@ export const ChallengeRewardNotification = (
   props: ChallengeRewardNotificationProps
 ) => {
   const { notification } = props
-  const { challengeId } = notification
+  const { challengeId, listenStreak } = notification
   const mappedChallengeRewardsConfigKey =
     challengeId in trendingChallengeIdMapping
       ? trendingChallengeIdMapping[challengeId]
@@ -58,8 +63,31 @@ export const ChallengeRewardNotification = (
     navigation.navigate(notification)
   }, [navigation, notification])
 
+  const title = useMemo(() => {
+    if (challengeId === ChallengeName.ListenStreakEndless && listenStreak) {
+      return `${info?.title}: Day ${listenStreak}`
+    } else {
+      return info?.title
+    }
+  }, [challengeId, listenStreak, info])
+
+  const notificationText = useMemo(() => {
+    const amountEarned = Number(formatNumberCommas(amount.toString()))
+    switch (challengeId) {
+      case ChallengeName.ListenStreakEndless:
+        if (amountEarned > 1) {
+          return messages.streakMilestone(amountEarned, listenStreak ?? 0)
+        }
+        return messages.streakMaintenance(amountEarned)
+      case 'referred':
+        return `${messages.amountEarned(amount)} ${messages.referredText}`
+      default:
+        return `${messages.amountEarned(amount)} ${messages.challengeCompleteText}`
+    }
+  }, [challengeId, amount, listenStreak])
+
   if (!info) return null
-  const { title } = info
+
   return (
     <NotificationTile notification={notification} onPress={handlePress}>
       <NotificationHeader icon={IconAudiusLogo}>
@@ -69,12 +97,7 @@ export const ChallengeRewardNotification = (
             : title}
         </NotificationTitle>
       </NotificationHeader>
-      <NotificationText>
-        {messages.amountEarned(amount)}{' '}
-        {challengeId === 'referred'
-          ? messages.referredText
-          : messages.challengeCompleteText}
-      </NotificationText>
+      <NotificationText>{notificationText}</NotificationText>
       <NotificationTwitterButton
         type='static'
         shareText={messages.twitterShareText}
