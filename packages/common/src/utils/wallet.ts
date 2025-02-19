90226e5d30dbc69c14310ef1e0f11d6aea7e8b1f
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-02-19 15:41:41 -0800

    Deprecate wallet.ts and formatUtils.ts functions in favor of FixedDecimal

--- packages/common/src/utils/wallet.ts
+++ packages/common/src/utils/wallet.ts
@@ -18,54 +18,86 @@ import {
   convertFloatToWei
 } from '~/utils/formatUtil'
 import { Nullable } from '~/utils/typeUtils'
+import type { FixedDecimal } from '@audius/fixed-decimal'
 
 /** AUDIO utils */
 const WEI_DECIMALS = 18 // 18 decimals on ETH AUDIO
 const SPL_DECIMALS = 8 // 8 decimals on SPL AUDIO
 
+/** @deprecated Don't use BN in new code if possible. Use BigInt. */
 export const zeroBNWei = new BN(0) as BNWei
 
+/**
+ * @deprecated Use `AUDIO().trunc().toFixed()` from {@link FixedDecimal} instead.
+ */
 export const weiToAudioString = (bnWei: BNWei): StringAudio => {
   const stringAudio = formatWeiToAudioString(bnWei) as StringAudio
   return stringAudio
 }
 
+/**
+ * @deprecated Don't use BN to represent whole AUDIO. Use `AUDIO().trunc().toFixed()` from {@link FixedDecimal} instead.
+ */
 export const weiToAudio = (bnWei: BNWei): BNAudio => {
   const stringAudio = formatWeiToAudioString(bnWei) as StringAudio
   return stringAudioToBN(stringAudio)
 }
 
+/**
+ * @deprecated Use `AUDIO().value` from {@link FixedDecimal} instead.
+ */
 export const audioToWei = (stringAudio: StringAudio): BNWei => {
   const wei = parseWeiNumber(stringAudio) as BNWei
   return wei
 }
 
+/**
+ * @deprecated Use `AUDIO()` from {@link FixedDecimal} instead.
+ */
 export const stringWeiToBN = (stringWei: StringWei): BNWei => {
   return new BN(stringWei) as BNWei
 }
 
+/**
+ * @deprecated Use `USDC()` from {@link FixedDecimal} instead.
+ */
 export const stringUSDCToBN = (stringUSDC: StringUSDC): BNUSDC => {
   return new BN(stringUSDC) as BNUSDC
 }
 
+/**
+ * @deprecated Don't use BN to represent whole AUDIO. Use `AUDIO().toString()` from {@link FixedDecimal} instead.
+ */
 export const stringAudioToBN = (stringAudio: StringAudio): BNAudio => {
   return new BN(stringAudio) as BNAudio
 }
 
+/**
+ * @deprecated Don't use BN to represent whole AUDIO. Use `AUDIO().toString()` from {@link FixedDecimal} instead.
+ */
 export const stringWeiToAudioBN = (stringWei: StringWei): BNAudio => {
   const bnWei = stringWeiToBN(stringWei)
   const stringAudio = weiToAudioString(bnWei)
   return new BN(stringAudio) as BNAudio
 }
 
+/**
+ * @deprecated Use `AUDIO(wei).value.toString()` from {@link FixedDecimal} instead.
+ */
 export const weiToString = (wei: BNWei): StringWei => {
   return wei.toString() as StringWei
 }
 
+/**
+ * @deprecated Use `AUDIO(stringAudio).value.toString()` from {@link FixedDecimal} instead.
+ */
 export const stringAudioToStringWei = (stringAudio: StringAudio): StringWei => {
   return weiToString(audioToWei(stringAudio))
 }
 
+/**
+ * @deprecated Use `AUDIO()` from {@link FixedDecimal} instead.
+ */
 export const parseAudioInputToWei = (audio: StringAudio): Nullable<BNWei> => {
   if (!audio.length) return null
   // First try converting from float, in case audio has decimal value
@@ -80,6 +112,8 @@ export const parseAudioInputToWei = (audio: StringAudio): Nullable<BNWei> => {
 }
 
 /**
+ * @deprecated Use `AUDIO().toLocaleString()` from {@link FixedDecimal} instead.
+ *
  * Format wei BN to the full $AUDIO currency with decimals
  * @param amount The wei amount
  * @param shouldTruncate truncate decimals at truncation length
@@ -133,22 +167,38 @@ export const convertBigIntToAmountObject = (
   }
 }
 
+/**
+ * @deprecated Use `AUDIO(wAUDIO(amount))` from {@link FixedDecimal} instead.
+ */
 export const convertWAudioToWei = (amount: BN) => {
   const decimals = WEI_DECIMALS - SPL_DECIMALS
   return amount.mul(new BN('1'.padEnd(decimals + 1, '0'))) as BNWei
 }
 
+/**
+ * @deprecated Use `wAUDIO(AUDIO(amount))` from {@link FixedDecimal} instead.
+ */
 export const convertWeiToWAudio = (amount: BN) => {
   const decimals = WEI_DECIMALS - SPL_DECIMALS
   return amount.div(new BN('1'.padEnd(decimals + 1, '0')))
 }
 
 /** USDC Utils */
+/**
+ * @deprecated Use `USDC()` from {@link FixedDecimal} instead.
+ */
 export const BN_USDC_WEI = new BN('1000000')
+/**
+ * @deprecated Use `USDC()` from {@link FixedDecimal} instead.
+ */
 export const BN_USDC_CENT_WEI = new BN('10000')
 const BN_USDC_WEI_ROUNDING_FRACTION = new BN('9999')
 
-/** Round a USDC value as a BN up to the nearest cent and return as a BN */
+/**
+ * @deprecated Use `USDC(value).ceil(2)` from {@link FixedDecimal} instead
+ *
+ * Round a USDC value as a BN up to the nearest cent and return as a BN
+ */
 export const ceilingBNUSDCToNearestCent = (value: BNUSDC): BNUSDC => {
   return value
     .add(BN_USDC_WEI_ROUNDING_FRACTION)
@@ -156,15 +206,21 @@ export const ceilingBNUSDCToNearestCent = (value: BNUSDC): BNUSDC => {
     .mul(BN_USDC_CENT_WEI) as BNUSDC
 }
 
-/** Round a USDC value as a BN down to the nearest cent and return as a BN */
+/**
+ * @deprecated Use `USDC(value).floor(2)` from {@link FixedDecimal} instead.
+ *
+ * Round a USDC value as a BN down to the nearest cent and return as a BN
+ */
 export const floorBNUSDCToNearestCent = (value: BNUSDC): BNUSDC => {
   return value.div(BN_USDC_CENT_WEI).mul(BN_USDC_CENT_WEI) as BNUSDC
 }
 
-/** Formats a USDC wei string (full precision) to a fixed string suitable for
-display as a dollar amount. Note: will lose precision by rounding _up_ to nearest
-cent and will drop negative signs
-*/
+/**
+ * @deprecated Use `USDC().toLocaleString()` from {@link FixedDecimal} instead.
+ *
+ * Formats a USDC wei string (full precision) to a fixed string suitable for display as a dollar amount.
+ * Note: will lose precision by rounding _up_ to nearest cent and will drop negative signs
+ */
 export const formatUSDCWeiToUSDString = (
   amount: StringUSDC | BN,
   precision = 2
@@ -181,6 +237,8 @@ export const formatUSDCWeiToUSDString = (
 }
 
 /**
+ * @deprecated Use `Number(USDC(usdc).ceil(2).toString())` from {@link FixedDecimal} instead.
+ *
  * Formats a USDC BN (full precision) to a number of dollars.
  * Note: will lose precision by rounding _up_ to nearest cent.
  */
@@ -191,6 +249,8 @@ export const formatUSDCWeiToCeilingDollarNumber = (amount: BNUSDC) => {
 }
 
 /**
+ * @deprecated Use `Number(USDC(usdc).ceil(2).toString()) * 100` from {@link FixedDecimal} instead.
+ *
  * Formats a USDC BN (full precision) to a number of cents.
  * Note: will lose precision by rounding _up_ to nearest cent.
  */
@@ -199,6 +259,8 @@ export const formatUSDCWeiToCeilingCentsNumber = (amount: BNUSDC) => {
 }
 
 /**
+ * @deprecated Use `Number(USDC(usdc).floor(2).toString()` from {@link FixedDecimal} instead.
+ *
  * Formats a USDC BN (full precision) to a number of dollars.
  * Note: will lose precision by rounding _down_ to nearest cent.
  */
@@ -207,6 +269,8 @@ export const formatUSDCWeiToFloorDollarNumber = (amount: BNUSDC) => {
 }
 
 /**
+ * @deprecated Use `Number(USDC(usdc).floor(2).toString()) * 100` from {@link FixedDecimal} instead.
+ *
  * Formats a USDC BN (full precision) to a number of cents.
  * Note: will lose precision by rounding _down_ to nearest cent.
  */
