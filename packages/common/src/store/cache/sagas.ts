4c1d3cf45f1da368c017ea29b1083a02e04c3728
amendelsohn <andrew.j.mendelsohn@gmail.com>
2025-02-21 15:13:23 -0800

    fix and refactor syncWithReactQuery

--- packages/common/src/store/cache/sagas.ts
+++ packages/common/src/store/cache/sagas.ts
@@ -1,12 +1,22 @@
-import { call, takeEvery } from 'typed-redux-saga'
+import { call, select, takeEvery } from 'typed-redux-saga'
 
 import { FeatureFlags } from '~/services/remote-config/feature-flags'
 
 import { getContext } from '../effects'
 
-import { ADD_ENTRIES, ADD_SUCCEEDED, addEntries, addSucceeded } from './actions'
+import {
+  ADD_ENTRIES,
+  ADD_SUCCEEDED,
+  UPDATE,
+  INCREMENT,
+  addEntries,
+  addSucceeded,
+  update,
+  increment
+} from './actions'
+import { getEntry } from './selectors'
 import { syncWithReactQuery } from './syncWithReactQuery'
-import { EntryMap } from './types'
+import { EntryMap, Entry } from './types'
 
 function* watchAddSucceeded() {
   const queryClient = yield* getContext('queryClient')
@@ -58,8 +68,87 @@ function* watchAddEntries() {
   )
 }
 
+function* watchUpdate() {
+  const queryClient = yield* getContext('queryClient')
+  yield* takeEvery(UPDATE, function* (action: ReturnType<typeof update>) {
+    const { entries, kind } = action
+    const getFeatureEnabled = yield* getContext('getFeatureEnabled')
+    const isReactQuerySyncEnabled = yield* call(
+      getFeatureEnabled,
+      FeatureFlags.REACT_QUERY_SYNC
+    )
+
+    if (!isReactQuerySyncEnabled) return
+
+    // For any entity type, sync to React Query using the latest state from Redux
+    const entriesMap = {} as EntryMap
+    for (const entry of entries) {
+      // Get the latest state from Redux after the update
+      const latestEntry = yield* select(getEntry, {
+        kind,
+        id: entry.id
+      })
+      if (latestEntry) {
+        entriesMap[entry.id] = { id: entry.id, metadata: latestEntry }
+      }
+    }
+
+    syncWithReactQuery(queryClient, {
+      [kind]: entriesMap
+    })
+  })
+}
+
+function* watchIncrement() {
+  const queryClient = yield* getContext('queryClient')
+  yield* takeEvery(INCREMENT, function* (action: ReturnType<typeof increment>) {
+    const getFeatureEnabled = yield* getContext('getFeatureEnabled')
+    const isReactQuerySyncEnabled = yield* call(
+      getFeatureEnabled,
+      FeatureFlags.REACT_QUERY_SYNC
+    )
+
+    if (!isReactQuerySyncEnabled) return
+
+    // For increment actions, we need to merge the delta with existing data
+    // Transform entries into the format expected by syncWithReactQuery
+    syncWithReactQuery(queryClient, {
+      [action.kind]: {
+        ...action.entries.reduce((acc, entry) => {
+          // For increment actions, we need to get the existing data and apply the delta
+          const existingData = queryClient.getQueryData([
+            action.kind,
+            entry.id
+          ]) as Entry | undefined
+          acc[entry.id] = {
+            ...existingData,
+            ...entry,
+            metadata: {
+              ...(existingData?.metadata || {}),
+              ...Object.entries(entry.metadata || {}).reduce(
+                (metaAcc, [key, value]) => {
+                  // If the existing data has this field, add the delta
+                  if (existingData?.metadata?.[key] !== undefined) {
+                    metaAcc[key] =
+                      (existingData.metadata[key] || 0) + (value as number)
+                  } else {
+                    metaAcc[key] = value
+                  }
+                  return metaAcc
+                },
+                {} as Record<string, any>
+              )
+            }
+          }
+          return acc
+        }, {} as EntryMap)
+      }
+    })
+  })
+}
+
 const sagas = () => {
-  return [watchAddSucceeded, watchAddEntries]
+  return [watchAddSucceeded, watchAddEntries, watchUpdate, watchIncrement]
 }
 
 export default sagas
