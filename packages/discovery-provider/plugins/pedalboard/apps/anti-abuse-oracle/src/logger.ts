82004b8707e7719fbb53f1c54bca710123a09cda
Isaac Solo <isaac@audius.co>
2025-02-25 11:57:12 -0800

    Add AAO plugin with basic attest (#11453)

---
+++ packages/discovery-provider/plugins/pedalboard/apps/anti-abuse-oracle/src/logger.ts
@@ -0,0 +1,16 @@
+import pino, { stdTimeFunctions } from 'pino'
+
+const formatters = {
+  level(label: string) {
+    // Set level to string format
+    return { level: label.toUpperCase() }
+  }
+}
+
+// set config for logger here
+export const logger = pino({
+  name: `anti-abuse-oracle`,
+  base: undefined,
+  timestamp: stdTimeFunctions.isoTime,
+  formatters
+})
