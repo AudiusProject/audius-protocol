4d162220beb5f8993e134df50043a962167d7eed
Raymond Jacobson <ray@audius.co>
2025-03-07 18:24:35 -0800

    Add to useTracks, useUsers, useCollections

--- packages/common/src/api/tan-query/useTracks.ts
+++ packages/common/src/api/tan-query/useTracks.ts
@@ -44,7 +44,7 @@ export const useTracks = (
         return await batchGetTracks.fetch(trackId)
       },
       ...options,
-      enabled: options?.enabled !== false && !!trackId
+      enabled: options?.enabled !== false && !!trackId && trackId > 0
     })),
     combine: combineQueryResults<TrackMetadata[]>
   })
