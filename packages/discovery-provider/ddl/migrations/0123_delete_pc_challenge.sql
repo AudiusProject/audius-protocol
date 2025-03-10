6c54c94ff2d600b5d0b596a44d08b2206ee27a3e
Farid Salau <faridsalau@gmail.com>
2025-03-10 13:20:02 -0500

    Address comment

--- packages/discovery-provider/ddl/migrations/0123_delete_pc_challenge.sql
+++ packages/discovery-provider/ddl/migrations/0123_delete_pc_challenge.sql
@@ -1,16 +1,5 @@
 BEGIN;
 
-DO $$ 
-BEGIN 
-  -- Check if we're on the stage environment using blockhash
-  IF EXISTS (
-    SELECT *
-    FROM "blocks"
-    WHERE "blockhash" = '0x65a3243860511ed28a933c3a113dea7df368ad53f721cc9d0034c0c75f996afb'
-  ) THEN 
-    -- Delete all user challenges with challenge_id = 'pc' only on stage
-    DELETE FROM user_challenges WHERE challenge_id = 'pc';
-  END IF;
-END $$;
+DELETE FROM user_challenges WHERE challenge_id = 'pc';
 
 COMMIT;
