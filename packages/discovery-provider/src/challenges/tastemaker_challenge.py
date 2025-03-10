2e70c8f6203746b6bd766ab7c3a5f5f5ad1e7085
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-03-10 16:02:35 -0400

    item_type

--- packages/discovery-provider/src/challenges/tastemaker_challenge.py
+++ packages/discovery-provider/src/challenges/tastemaker_challenge.py
@@ -17,9 +17,10 @@ class TastemakerChallengeUpdater(ChallengeUpdater):
     def generate_specifier(self, session: Session, user_id: int, extra: Dict) -> str:
         """
         Generate a specifier for the challenge based on user_id and tastemaker_item_id
-        Format: {user_id}:{tastemaker_item_id}
+        Format: {user_id}:{tastemaker_item_type}:{tastemaker_item_id}
         """
-        return f"{user_id}:{extra['tastemaker_item_id']}"
+        item_type = "p" if extra["tastemaker_item_type"] == "playlist" else "t"
+        return f"{user_id}:{item_type}:{extra['tastemaker_item_id']}"
 
 
 tastemaker_challenge_manager = ChallengeManager("t", TastemakerChallengeUpdater())
