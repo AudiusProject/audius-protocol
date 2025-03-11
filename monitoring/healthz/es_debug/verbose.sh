dcf31b13f7eaf864b4edc07ca2915e37d68bfea0
alecsavvy <16739903+alecsavvy@users.noreply.github.com>
2025-03-11 13:44:09 -0600

    rm deregistered nodes from sdk cached list (#11580)

--- monitoring/healthz/es_debug/verbose.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while read line 
-do 
-    status=$(curl -s $line/health_check?verbose=true | jq -r '.data | [.version, .block_difference, .elasticsearch.reposts.db_block_difference, .filesystem_used, .filesystem_size, .filesystem_used/.filesystem_size*100] | @tsv')
-    echo -e "$line \t $status"
-done < "${1:-/dev/stdin}"
