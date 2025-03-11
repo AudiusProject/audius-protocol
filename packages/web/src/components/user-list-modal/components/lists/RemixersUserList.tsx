ec5ca1ad6e74e90a44d980b6ce049d1342e14e94
Dylan Jeffers <dylan@audius.co>
2025-03-11 14:46:06 -0700

    [C-5815] Migrate followers user-list to tan-query (#11546)

---
+++ packages/web/src/components/user-list-modal/components/lists/RemixersUserList.tsx
@@ -0,0 +1,8 @@
+type RemixersUserListProps = {
+  onClose: () => void
+}
+
+export const RemixersUserList = ({ onClose }: RemixersUserListProps) => {
+  // TODO: Implement with useRemixers hook
+  return null
+}
---
+++ packages/web/src/components/user-list-modal/components/lists/RepostsUserList.tsx
@@ -0,0 +1,8 @@
+type RepostsUserListProps = {
+  onClose: () => void
+}
+
+export const RepostsUserList = ({ onClose }: RepostsUserListProps) => {
+  // TODO: Implement with useReposts hook
+  return null
+}
