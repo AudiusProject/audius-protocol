886bca50232d804347112bbdb86b8cd94117405b
Steve Perkins <stereosteve@users.noreply.github.com>
2025-03-27 09:43:01 -0400

    make migration idempotent

--- packages/discovery-provider/ddl/migrations/0127_notification_announcement_index.sql
+++ packages/discovery-provider/ddl/migrations/0127_notification_announcement_index.sql
@@ -1,1 +1,1 @@
-create index ix_announcement on notification(type, timestamp) where type = 'announcement';
+create index if not exists ix_announcement on notification(type, timestamp) where type = 'announcement';
