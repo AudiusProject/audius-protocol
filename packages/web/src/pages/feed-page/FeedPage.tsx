6dbfeb77a83c6ede7153475d799936a0b1c089d2
JD Francis <jd@audius.co>
2025-04-03 14:49:08 -0500

    move mobile feed over

--- packages/web/src/pages/feed-page/FeedPage.tsx
+++ packages/web/src/pages/feed-page/FeedPage.tsx
@@ -1,4 +1,7 @@
-import { RefObject } from 'react'
+import { RefObject, useEffect } from 'react'
+
+import { useLocation } from 'react-router-dom'
+import { useNavigationType } from 'react-router-dom-v5-compat'
 
 import { useIsMobile } from 'hooks/useIsMobile'
 
@@ -13,6 +16,37 @@ type FeedPageContentProps = {
 const FeedPage = ({ containerRef }: FeedPageContentProps) => {
   const isMobile = useIsMobile()
   const content = isMobile ? FeedPageMobileContent : FeedPageContent
+
+  const location = useLocation()
+  const navigationType = useNavigationType()
+
+  useEffect(() => {
+    if (navigationType === 'POP') {
+      const savedScrollPosition = sessionStorage.getItem(
+        `scrollPosition-${location.pathname}`
+      )
+
+      console.log('POP', savedScrollPosition)
+      if (savedScrollPosition !== null) {
+        window.scrollTo(0, parseInt(savedScrollPosition, 10))
+      }
+    } else {
+      window.scrollTo(0, 0) // Reset scroll for new pages
+    }
+  }, [location.pathname, navigationType])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log('SCROLL', window.scrollY)
+      sessionStorage.setItem(
+        `scrollPosition-${location.pathname}`,
+        window.scrollY.toString()
+      )
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [location.pathname])
 
   return (
     <FeedPageProvider containerRef={containerRef}>{content}</FeedPageProvider>
