b1f74b3587c57aa860774b4b8f7a18cf289f15b3
Raymond Jacobson <ray@audius.co>
2025-04-03 12:18:11 -0700

    Add index and use indexes better in block confirmation

--- packages/discovery-provider/src/queries/get_block_confirmation.py
+++ packages/discovery-provider/src/queries/get_block_confirmation.py
@@ -1,7 +1,10 @@
 from sqlalchemy import desc
 
 from src.models.core.core_indexed_blocks import CoreIndexedBlocks
+from src.tasks.core.core_client import CoreClient, get_core_instance
 from src.utils import db_session, helpers
+
+core: CoreClient = get_core_instance()
 
 
 # returns a dictionary that represents whether
@@ -17,12 +20,13 @@ def get_block_confirmation(blockhash, blocknumber):
 
         latest_block_query = (
             session.query(CoreIndexedBlocks)
+            .filter(CoreIndexedBlocks.chain_id == core.get_node_info().chainid)
             .order_by(desc(CoreIndexedBlocks.height))
             .first()
         )
 
         if latest_block_query is None:
-            raise Exception("Expected SINGLE row marked as current")
+            raise Exception("No latest block")
 
         latest_block_record = helpers.model_to_dictionary(latest_block_query)
         latest_block_number = latest_block_record["height"] or 0
