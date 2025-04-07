dbac25009bc9a9552a4d4c4ce15fd75bc5880efc
JD Francis <jd@audius.co>
2025-04-07 15:14:06 -0500

    death to retries

--- packages/web/src/services/query-client.ts
+++ packages/web/src/services/query-client.ts
@@ -5,12 +5,15 @@ import { env } from './env'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      // Tan-query auto retries on errors like 404s. This doesnt fit with our backend design so we've disabled retries by default. They can be re-enabled at the query level if needed.
+      retry: false,
       gcTime: 1000 * 60 * 5, // 5 minutes
       staleTime: 1000 * 60 * 5, // 5 minutes
+      // All the refetches are enabled by default - they should only run every 5 minutes with our intended stale time
       refetchOnWindowFocus: true,
       refetchOnReconnect: true,
       refetchOnMount: true,
-      throwOnError: env.ENVIRONMENT === 'development', // feature-tan-query TODO: remove before going to main?,
+      throwOnError: env.ENVIRONMENT === 'development',
       experimental_prefetchInRender: true
     }
   }
