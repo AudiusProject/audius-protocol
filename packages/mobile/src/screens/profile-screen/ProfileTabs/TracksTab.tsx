8b91b0076f82dad5c0743a5ed3c79853cd0f80ad
JD Francis <jd.johndavidfrancis@gmail.com>
2025-04-10 13:07:21 -0500

    Fix TQ lineup issues on mobile (#11817)

--- packages/mobile/src/screens/profile-screen/ProfileTabs/TracksTab.tsx
+++ packages/mobile/src/screens/profile-screen/ProfileTabs/TracksTab.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react'
 import { useProfileTracks } from '@audius/common/api'
 import { profilePageTracksLineupActions as tracksActions } from '@audius/common/store'
 
-import { Lineup } from 'app/components/lineup'
+import { TanQueryLineup } from 'app/components/lineup/TanQueryLineup'
 
 import { EmptyProfileTile } from '../EmptyProfileTile'
 import { useSelectProfile } from '../selectors'
@@ -14,26 +14,43 @@ export const TracksTab = () => {
     'user_id',
     'artist_pick_track_id'
   ])
-
-  const { lineup, loadNextPage } = useProfileTracks({ handle })
+  const {
+    data,
+    lineup,
+    loadNextPage,
+    pageSize,
+    isFetching,
+    refresh: refreshQuery,
+    isPending,
+    hasNextPage
+  } = useProfileTracks({
+    handle
+  })
 
   const fetchPayload = useMemo(() => ({ userId: user_id }), [user_id])
   const extraFetchOptions = useMemo(() => ({ handle }), [handle])
 
   return (
-    <Lineup
+    <TanQueryLineup
       selfLoad
       pullToRefresh
       leadingElementId={artist_pick_track_id}
       actions={tracksActions}
       lineup={lineup}
-      tanQuery
       loadMore={loadNextPage}
       fetchPayload={fetchPayload}
       extraFetchOptions={extraFetchOptions}
       disableTopTabScroll
       LineupEmptyComponent={<EmptyProfileTile tab='tracks' />}
       showsVerticalScrollIndicator={false}
+      // Tan query props
+      pageSize={pageSize}
+      isFetching={isFetching}
+      isPending={isPending}
+      queryData={data}
+      loadNextPage={loadNextPage}
+      hasMore={hasNextPage}
+      refresh={refreshQuery}
     />
   )
 }
