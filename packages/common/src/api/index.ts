c26c9665aebefaefd3f25a2f54f2e8c88e6171b4
Dylan Jeffers <dylan@audius.co>
2025-04-15 19:35:25 -0700

    [PE-5973, PE-5968] Migrate track page lineups to tan-query (#11857)

--- packages/common/src/api/index.ts
+++ packages/common/src/api/index.ts
@@ -84,3 +84,4 @@ export * from './tan-query/useUpdateTrack'
 // Saga fetch utils, remove when migration is complete
 export * from './tan-query/saga-utils'
 export * from './tan-query/useCollectionTracksWithUid'
+export * from './tan-query/useTrackPageLineup'
--- packages/web/src/pages/track-page/TrackPageProvider.tsx
+++ packages/web/src/pages/track-page/TrackPageProvider.tsx
@@ -33,7 +33,7 @@ import {
   playerSelectors,
   playerActions
 } from '@audius/common/store'
-import { formatDate, route, Uid } from '@audius/common/utils'
+import { formatDate, route } from '@audius/common/utils'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
 
@@ -60,7 +60,7 @@ const {
   REPOSTING_USERS_ROUTE
 } = route
 const { makeGetCurrent } = queueSelectors
-const { getPlaying, getPreviewing, getBuffering } = playerSelectors
+const { getPlaying, getPreviewing } = playerSelectors
 const { setFavorite } = favoritesUserListActions
 const { setRepost } = repostsUserListActions
 const { requestOpen: requestOpenShareModal } = shareModalUIActions
@@ -313,15 +313,6 @@ class TrackPageProviderClass extends Component<
     }
   }
 
-  onMoreByArtistTracksPlay = (uid?: string) => {
-    const { play, recordPlayMoreByArtist } = this.props
-    play(uid)
-    if (uid) {
-      const trackId = Uid.fromString(uid).id
-      recordPlayMoreByArtist(trackId as number)
-    }
-  }
-
   onHeroRepost = (isReposted: boolean, trackId: ID) => {
     const { repostTrack, undoRepostTrack } = this.props
     if (!isReposted) {
@@ -378,13 +369,10 @@ class TrackPageProviderClass extends Component<
       track,
       remixParentTrack,
       user,
-      moreByArtist,
       currentQueueItem,
       playing,
       previewing,
-      buffering,
-      userId,
-      pause
+      userId
     } = this.props
     const heroPlaying =
       playing &&
@@ -448,21 +436,12 @@ class TrackPageProviderClass extends Component<
       userId,
       previewing,
       onHeroPlay: this.onHeroPlay,
-      goToAllRemixesPage: this.goToAllRemixesPage,
       onHeroRepost: this.onHeroRepost,
       onHeroShare: this.onHeroShare,
       onClickMobileOverflow: this.props.clickOverflow,
       onConfirmUnfollow: this.props.onConfirmUnfollow,
       goToFavoritesPage: this.goToFavoritesPage,
-      goToRepostsPage: this.goToRepostsPage,
-
-      // Tracks Lineup Props
-      tracks: moreByArtist,
-      currentQueueItem,
-      isPlaying: playing,
-      isBuffering: buffering,
-      play: this.onMoreByArtistTracksPlay,
-      pause
+      goToRepostsPage: this.goToRepostsPage
     }
 
     return (
@@ -500,7 +479,6 @@ function makeMapStateToProps() {
       currentQueueItem: getCurrentQueueItem(state),
       playing: getPlaying(state),
       previewing: getPreviewing(state),
-      buffering: getBuffering(state),
       pathname: getLocationPathname(state)
     }
   }
