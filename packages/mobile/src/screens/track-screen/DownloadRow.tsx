c44130386df05270768a7edcc991bf3dae82f451
Reed <3893871+dharit-tan@users.noreply.github.com>
2025-04-16 12:27:23 -0400

    Remove filename from mobile DownloadRow (#11863)

--- packages/mobile/src/screens/track-screen/DownloadRow.tsx
+++ packages/mobile/src/screens/track-screen/DownloadRow.tsx
@@ -1,15 +1,10 @@
 import { useTrack, useUser } from '@audius/common/api'
 import type { ID } from '@audius/common/models'
-import { stemCategoryFriendlyNames } from '@audius/common/models'
 import { getFilename } from '@audius/common/utils'
 import { css } from '@emotion/native'
 
 import { Flex, Text, IconReceive, Box } from '@audius/harmony-native'
 import { PlainButton } from 'app/harmony-native/components/Button/PlainButton/PlainButton'
-
-const messages = {
-  fullTrack: 'Full Track'
-}
 
 type DownloadRowProps = {
   trackId: ID
@@ -27,45 +22,23 @@ export const DownloadRow = ({
   const { data: track } = useTrack(trackId)
   const { data: user } = useUser(track?.owner_id)
 
+  const filename =
+    track && user ? getFilename({ track, user, isOriginal: true }) : null
+
   return (
-    <Flex
-      direction='row'
-      alignItems='center'
-      justifyContent='space-between'
-      borderTop='default'
-      pv='m'
-    >
-      <Flex
-        direction='row'
-        gap='xl'
-        alignItems='center'
-        justifyContent='space-between'
-        style={css({ flexShrink: 1 })}
-      >
+    <Flex row alignItems='center' justifyContent='space-between'>
+      <Flex row gap='m' alignItems='center' style={css({ flexShrink: 1 })}>
         <Text variant='body' color='subdued'>
           {index}
         </Text>
-        <Flex gap='xs' style={css({ flexShrink: 1 })}>
-          <Text variant='body'>
-            {track?.stem_of?.category
-              ? stemCategoryFriendlyNames[track?.stem_of?.category]
-              : messages.fullTrack}
-          </Text>
-          <Text
-            variant='body'
-            color='subdued'
-            ellipsizeMode='tail'
-            numberOfLines={1}
-          >
-            {track &&
-              user &&
-              getFilename({
-                track,
-                user,
-                isOriginal: true
-              })}
-          </Text>
-        </Flex>
+        <Text
+          variant='body'
+          ellipsizeMode='tail'
+          numberOfLines={1}
+          style={css({ flexShrink: 1 })}
+        >
+          {filename}
+        </Text>
       </Flex>
       {hideDownload ? null : (
         <PlainButton
