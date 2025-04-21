3317e448640db3d1adbe72e0356e0fb8283c4043
JD Francis <jd@audius.co>
2025-04-21 15:36:23 -0500

    new retry config

---
+++ packages/common/src/api/tan-query/utils/defaultRetryConfig.ts
@@ -0,0 +1,18 @@
+export const MAX_RETRIES = 3
+export const HTTP_STATUSES_TO_NOT_RETRY = [400, 401, 403, 404]
+
+export const defaultRetryConfig = (failureCount: number, error: any) => {
+  if (failureCount > MAX_RETRIES) {
+    return false
+  }
+
+  if (
+    Object.hasOwnProperty.call(error, 'status') &&
+    HTTP_STATUSES_TO_NOT_RETRY.includes(error.status)
+  ) {
+    console.log('this is a 404')
+    return false
+  }
+
+  return true
+}
--- packages/web/src/services/query-client.ts
+++ packages/web/src/services/query-client.ts
@@ -1,12 +1,12 @@
+import { defaultRetryConfig } from '@audius/common/api'
 import { QueryClient } from '@tanstack/react-query'
 
 import { env } from './env'
-
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       // Tan-query auto retries on errors like 404s. This doesnt fit with our backend design so we've disabled retries by default. They can be re-enabled at the query level if needed.
-      retry: false,
+      retry: defaultRetryConfig,
       gcTime: 1000 * 60 * 5, // 5 minutes
       staleTime: 1000 * 60 * 5, // 5 minutes
       // All the refetches are enabled by default - they should only run every 5 minutes with our intended stale time
