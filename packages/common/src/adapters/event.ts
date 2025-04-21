d8c9b657c7be07ab7564425c33989996a488ca7f
KJ <kyle.j.shanks@gmail.com>
2025-04-21 17:25:34 -0400

    [PE-6014] Add description and prize fields for remix contests (#11908)

--- packages/common/src/adapters/event.ts
+++ packages/common/src/adapters/event.ts
@@ -1,10 +1,11 @@
 import { Event as EventSDK, HashId, OptionalHashId } from '@audius/sdk'
+import camelcaseKeys from 'camelcase-keys'
 
 import { Event } from '~/models/Event'
 import { dayjs } from '~/utils'
 
 export const eventMetadataFromSDK = (input: EventSDK): Event | undefined => {
-  const { eventId, userId, entityId, ...rest } = input
+  const { eventId, userId, entityId, eventData, ...rest } = input
 
   const decodedEventId = HashId.parse(eventId)
   const decodedUserId = HashId.parse(userId)
@@ -21,6 +22,7 @@ export const eventMetadataFromSDK = (input: EventSDK): Event | undefined => {
 
   return {
     ...rest,
+    eventData: camelcaseKeys(eventData),
     eventId: decodedEventId,
     userId: decodedUserId,
     entityId: decodedEntityId ?? null,
