21aa559b55f61d2e2c638057386307b04c8115d2
KJ Shanks <kyle.j.shanks@gmail.com>
2025-04-25 15:51:02 -0400

    Add mobile web remix contest info container

---
+++ packages/web/src/pages/track-page/components/mobile/remix-contests/RemixContestDetailsTab.tsx
@@ -0,0 +1,87 @@
+import { useRemixContest } from '@audius/common/api'
+import { ID } from '@audius/common/models'
+import { UPLOAD_PAGE } from '@audius/common/src/utils/route'
+import { dayjs } from '@audius/common/utils'
+import { Button, Divider, Flex, IconCloudUpload, Text } from '@audius/harmony'
+
+import { UserGeneratedText } from 'components/user-generated-text'
+import { useNavigateToPage } from 'hooks/useNavigateToPage'
+import { useRequiresAccountCallback } from 'hooks/useRequiresAccount'
+
+const messages = {
+  due: 'Submission Due:',
+  deadline: (deadline?: string) => {
+    if (!deadline) return ''
+    const date = dayjs(deadline)
+    return `${date.format('ddd. MMM D, YYYY')} at ${date.format('h:mm A')}`
+  },
+  ended: 'Contest Ended',
+  fallbackDescription:
+    'Enter my remix contest before the deadline for your chance to win!',
+  uploadRemixButtonText: 'Upload Your Remix'
+}
+
+type RemixContestDetailsTabProps = {
+  trackId: ID
+  isOwner: boolean
+}
+
+/**
+ * Tab content displaying details about a remix contest
+ */
+export const RemixContestDetailsTab = ({
+  trackId,
+  isOwner
+}: RemixContestDetailsTabProps) => {
+  const navigate = useNavigateToPage()
+  const { data: remixContest } = useRemixContest(trackId)
+  const isContestEnded = dayjs(remixContest?.endDate).isBefore(dayjs())
+
+  const goToUploadWithRemix = useRequiresAccountCallback(() => {
+    if (!trackId) return
+
+    const state = {
+      initialMetadata: {
+        is_remix: true,
+        remix_of: {
+          tracks: [{ parent_track_id: trackId }]
+        }
+      }
+    }
+    navigate(UPLOAD_PAGE, state)
+  }, [trackId, navigate])
+
+  return (
+    <Flex column w='100%'>
+      <Flex column gap='s' p='l'>
+        <Flex row gap='xs'>
+          <Text variant='title' color='accent'>
+            {messages.due}
+          </Text>
+          <Text variant='body' strength='strong'>
+            {isContestEnded
+              ? messages.ended
+              : messages.deadline(remixContest?.endDate)}
+          </Text>
+        </Flex>
+        <UserGeneratedText variant='body'>
+          {remixContest?.eventData?.description ?? messages.fallbackDescription}
+        </UserGeneratedText>
+      </Flex>
+      <Divider />
+      {!isOwner ? (
+        <Flex p='l' pb='s'>
+          <Button
+            variant='secondary'
+            size='small'
+            fullWidth
+            onClick={goToUploadWithRemix}
+            iconLeft={IconCloudUpload}
+          >
+            {messages.uploadRemixButtonText}
+          </Button>
+        </Flex>
+      ) : null}
+    </Flex>
+  )
+}
