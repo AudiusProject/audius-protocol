b3b8fe0e734dea3cdff70aa205ccb70c45976920
JD Francis <jd.johndavidfrancis@gmail.com>
2025-04-29 14:20:38 -0500

    [PE-5943] Tan query retry only on 400s (#11774)

--- packages/common/src/api/index.ts
+++ packages/common/src/api/index.ts
@@ -123,3 +123,4 @@ export * from './tan-query/wallets/useUSDCBalance'
 
 // Saga fetch utils, remove when migration is complete
 export * from './tan-query/saga-utils'
+export * from './tan-query/utils/defaultRetryConfig'
