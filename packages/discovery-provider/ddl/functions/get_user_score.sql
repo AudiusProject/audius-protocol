c0785ff5f2081a00c653239bc96bbcd735dcf989
Isaac Solo <isaac@audius.co>
2025-05-08 09:47:11 -0700

    Fix challenge count num in AAO UI (#12036)

--- packages/discovery-provider/ddl/functions/get_user_score.sql
+++ packages/discovery-provider/ddl/functions/get_user_score.sql
@@ -1,13 +1,15 @@
 -- identical to get_user_scores but for a single user
 -- used to generate a user score for attestations and in UI tool
+drop function if exists get_user_score(integer);
 create or replace function get_user_score(target_user_id integer) returns table(
+        -- order matters
         user_id integer,
         handle_lc text,
         play_count bigint,
         distinct_tracks_played bigint,
-        follower_count bigint,
         challenge_count bigint,
         following_count bigint,
+        follower_count bigint,
         chat_block_count bigint,
         is_audius_impersonator boolean,
         score bigint
