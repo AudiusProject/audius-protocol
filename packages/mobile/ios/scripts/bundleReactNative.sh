075bc3544d6c6417bbebba259497745eaaef23d6
Dylan Jeffers <dylan@audius.co>
2025-05-14 14:43:10 -0700

    Try out new ios sentry changes

--- packages/mobile/ios/scripts/bundleReactNative.sh
+++ packages/mobile/ios/scripts/bundleReactNative.sh
@@ -1,6 +1,12 @@
 export NODE_BINARY=node 
 export SENTRY_PROPERTIES=sentry.properties 
-export EXTRA_PACKAGER_ARGS=\"--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map\" 
+export EXTRA_PACKAGER_ARGS="--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map"
 export PROJECT_ROOT=$PWD/.. 
- 
-sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh 
+export SENTRY_ALLOW_FAILURE=true
+
+WITH_ENVIRONMENT="../node_modules/react-native/scripts/xcode/with-environment.sh"
+REACT_NATIVE_XCODE="../node_modules/react-native/scripts/react-native-xcode.sh"
+SENTRY_XCODE="../node_modules/@sentry/react-native/scripts/sentry-xcode.sh"
+BUNDLE_REACT_NATIVE="/bin/sh $SENTRY_XCODE $REACT_NATIVE_XCODE"
+
+/bin/sh -c "SENTRY_ALLOW_FAILURE=true $WITH_ENVIRONMENT \"$BUNDLE_REACT_NATIVE\""
--- packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
+++ packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
@@ -1,4 +1,4 @@
 export NODE_BINARY=node
 export SENTRY_PROPERTIES=sentry.properties
 
-sentry-cli upload-dsym
+/bin/sh ../node_modules/@sentry/react-native/scripts/sentry-xcode-debug-files.sh
