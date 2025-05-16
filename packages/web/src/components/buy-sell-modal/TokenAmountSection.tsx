4aee7fef21b91e8277f1b93fe9fa6ff4400101d2
Farid Salau <faridsalau@gmail.com>
2025-05-16 11:05:32 -0500

    Update to match figma

--- packages/web/src/components/buy-sell-modal/TokenAmountSection.tsx
+++ packages/web/src/components/buy-sell-modal/TokenAmountSection.tsx
@@ -1,7 +1,11 @@
+import { useMemo } from 'react'
+
+import { USDC } from '@audius/fixed-decimal'
 import {
   Button,
   Divider,
   Flex,
+  IconButton,
   IconInfo,
   Text,
   TextInput
@@ -16,12 +20,10 @@ const messages = {
   max: 'MAX',
   amountInputLabel: (symbol: string) => `Amount (${symbol})`,
   tokenPrice: (price: string, decimalPlaces: number) => {
-    const priceNum = parseFloat(price)
-    const formattedPrice = priceNum.toLocaleString('en-US', {
+    return USDC(price).toLocaleString('en-US', {
       minimumFractionDigits: 2,
       maximumFractionDigits: decimalPlaces
     })
-    return `($${formattedPrice} ea.)`
   },
   stackedBalance: (formattedAvailableBalance: string) =>
     `${formattedAvailableBalance}  Available`,
@@ -56,10 +58,12 @@ const DefaultBalanceSection = ({
         <Text variant='heading' size='s' color='subdued'>
           {messages.available}
         </Text>
-        <IconInfo
+        <IconButton
           size='s'
           color='subdued'
           css={{ borderRadius: cornerRadius.circle }}
+          icon={IconInfo}
+          aria-label='Available balance'
         />
       </Flex>
       <Text variant='heading' size='xl'>
@@ -104,6 +108,47 @@ const StackedBalanceSection = ({
   )
 }
 
+const CryptoAmountSection = ({
+  formattedAmount,
+  tokenInfo,
+  isStablecoin,
+  priceDisplay
+}: {
+  formattedAmount: string
+  tokenInfo: TokenInfo
+  isStablecoin: boolean
+  priceDisplay?: string
+}) => {
+  const { spacing, cornerRadius } = useTheme()
+  const { icon: TokenIcon, symbol } = tokenInfo
+  const tokenTicker = messages.tokenTicker(symbol, !!isStablecoin)
+
+  return (
+    <Flex p='l' alignItems='center' gap='s'>
+      <TokenIcon
+        width={spacing.unit16}
+        height={spacing.unit16}
+        css={{ borderRadius: cornerRadius.circle }}
+      />
+      <Flex direction='column'>
+        <Flex gap='xs' justifyContent='center' alignItems='center'>
+          <Text variant='heading' size='l'>
+            {formattedAmount}
+          </Text>
+          <Text variant='heading' size='l' color='subdued'>
+            {tokenTicker}
+          </Text>
+        </Flex>
+        {priceDisplay && (
+          <Text variant='heading' size='s' color='subdued'>
+            {priceDisplay}
+          </Text>
+        )}
+      </Flex>
+    </Flex>
+  )
+}
+
 export const TokenAmountSection = ({
   title,
   tokenInfo,
@@ -124,7 +169,6 @@ export const TokenAmountSection = ({
   const { spacing, cornerRadius } = useTheme()
 
   const { icon: TokenIcon, symbol, isStablecoin } = tokenInfo
-  const tokenTicker = messages.tokenTicker(symbol, !!isStablecoin)
 
   const { formattedAvailableBalance, formattedAmount } =
     useTokenAmountFormatting({
@@ -138,80 +182,117 @@ export const TokenAmountSection = ({
   const priceDisplay =
     tokenPrice && !isTokenPriceLoading
       ? messages.tokenPrice(tokenPrice, tokenPriceDecimalPlaces)
-      : null
+      : undefined
+
+  const youPaySection = useMemo(() => {
+    return (
+      <Flex gap='s' p='l' alignItems='flex-start'>
+        <Flex direction='column' gap='xs' alignItems='flex-start'>
+          <Flex alignItems='flex-start' gap='s'>
+            <TextInput
+              label={messages.amountInputLabel(symbol)}
+              placeholder={placeholder}
+              value={amount?.toString() || ''}
+              onChange={(e) => onAmountChange?.(e.target.value)}
+              error={error}
+            />
+            <Button
+              variant='secondary'
+              css={{
+                height: spacing.unit16
+              }}
+              onClick={onMaxClick}
+            >
+              {messages.max}
+            </Button>
+          </Flex>
+          {isInput && !!errorMessage ? (
+            <Text size='xs' variant='body' color='danger'>
+              {errorMessage}
+            </Text>
+          ) : null}
+        </Flex>
+        {isDefault ? (
+          <DefaultBalanceSection
+            isStablecoin={!!isStablecoin}
+            formattedAvailableBalance={formattedAvailableBalance}
+            tokenInfo={tokenInfo}
+          />
+        ) : (
+          <StackedBalanceSection
+            formattedAvailableBalance={formattedAvailableBalance}
+            tokenInfo={tokenInfo}
+          />
+        )}
+      </Flex>
+    )
+  }, [
+    amount,
+    error,
+    errorMessage,
+    formattedAvailableBalance,
+    isDefault,
+    isInput,
+    isStablecoin,
+    onAmountChange,
+    onMaxClick,
+    placeholder,
+    spacing.unit16,
+    symbol,
+    tokenInfo
+  ])
+
+  const youReceiveSection = useMemo(() => {
+    if (isStablecoin) {
+      return (
+        <Text variant='display' size='s'>
+          {messages.tokenPrice(formattedAmount, 2)}
+        </Text>
+      )
+    }
+
+    return (
+      <CryptoAmountSection
+        formattedAmount={formattedAmount}
+        tokenInfo={tokenInfo}
+        isStablecoin={!!isStablecoin}
+        priceDisplay={priceDisplay}
+      />
+    )
+  }, [formattedAmount, isStablecoin, priceDisplay, tokenInfo])
+
+  const titleText = useMemo(() => {
+    if (isStablecoin && !isInput) {
+      return (
+        <Flex alignItems='center' gap='s'>
+          <TokenIcon size='l' />
+          <Text variant='heading' size='s' color='subdued'>
+            {title}
+          </Text>
+          <IconButton
+            size='s'
+            color='subdued'
+            css={{ borderRadius: cornerRadius.circle }}
+            icon={IconInfo}
+            aria-label='You receive'
+          />
+        </Flex>
+      )
+    }
+    return (
+      <Text variant='heading' size='s' color='subdued'>
+        {title}
+      </Text>
+    )
+  }, [TokenIcon, cornerRadius.circle, isInput, isStablecoin, title])
 
   return (
     <Flex direction='column' gap='m'>
-      <Flex alignItems='center' gap='m'>
-        <Text variant='heading' size='s' color='subdued'>
-          {title}
-        </Text>
+      <Flex alignItems='center' justifyContent='center' gap='m'>
+        {titleText}
         <Divider css={{ flexGrow: 1 }} />
       </Flex>
-      {isInput ? (
-        <Flex gap='s' p='l' alignItems='flex-start'>
-          <Flex direction='column' gap='xs' alignItems='flex-start'>
-            <Flex alignItems='flex-start' gap='s'>
-              <TextInput
-                label={messages.amountInputLabel(symbol)}
-                placeholder={placeholder}
-                value={amount?.toString() || ''}
-                onChange={(e) => onAmountChange?.(e.target.value)}
-                error={error}
-              />
-              <Button
-                variant='secondary'
-                css={{
-                  height: spacing.unit16
-                }}
-                onClick={onMaxClick}
-              >
-                {messages.max}
-              </Button>
-            </Flex>
-            {isInput && !!errorMessage ? (
-              <Text size='xs' variant='body' color='danger'>
-                {errorMessage}
-              </Text>
-            ) : null}
-          </Flex>
-          {isDefault ? (
-            <DefaultBalanceSection
-              isStablecoin={!!isStablecoin}
-              formattedAvailableBalance={formattedAvailableBalance}
-              tokenInfo={tokenInfo}
-            />
-          ) : (
-            <StackedBalanceSection
-              formattedAvailableBalance={formattedAvailableBalance}
-              tokenInfo={tokenInfo}
-            />
-          )}
-        </Flex>
-      ) : (
-        <Flex p='l' alignItems='center' gap='s'>
-          <TokenIcon
-            width={spacing.unit16}
-            height={spacing.unit16}
-            css={{ borderRadius: cornerRadius.circle }}
-          />
-          <Flex direction='column'>
-            <Text variant='heading' size='l'>
-              {formattedAmount}
-            </Text>
-            <Flex gap='xs'>
-              <Text variant='heading' size='s' color='subdued'>
-                {tokenTicker}
-              </Text>
-              {priceDisplay && (
-                <Text variant='heading' size='s' color='subdued'>
-                  {priceDisplay}
-                </Text>
-              )}
-            </Flex>
-          </Flex>
-        </Flex>
-      )}
+      {isInput ? youPaySection : youReceiveSection}
     </Flex>
   )
 }
