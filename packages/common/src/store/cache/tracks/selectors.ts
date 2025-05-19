71a99e30bd15933cc5376f4f8621e563d572acfc
Dylan Jeffers <dylan@audius.co>
2025-05-19 16:31:49 -0700

    Drop all getTracks

--- packages/common/src/store/cache/tracks/selectors.ts
+++ packages/common/src/store/cache/tracks/selectors.ts
@@ -1,9 +1,7 @@
-import { getEntry, getAllEntries } from '~/store/cache/selectors'
+import { getEntry } from '~/store/cache/selectors'
 import { CommonState } from '~/store/commonStore'
 
 import { Kind, ID, UID } from '../../../models'
-
-import { BatchCachedTracks } from './types'
 
 /** @deprecated Use useTrack instead */
 export const getTrack = (
@@ -20,42 +18,4 @@ export const getTrack = (
     ...props,
     kind: Kind.TRACKS
   })
-}
-
-/** @deprecated Use useTracks instead */
-export const getTracks = (
-  state: CommonState,
-  props: {
-    ids?: ID[] | null
-    uids?: UID[] | null
-    permalinks?: string[] | null
-  }
-): { [id: number]: BatchCachedTracks } => {
-  if (props && props.ids) {
-    const tracks: { [id: number]: BatchCachedTracks } = {}
-    props.ids.forEach((id) => {
-      const track = getTrack(state, { id })
-      if (track) {
-        tracks[id] = { metadata: track }
-      }
-    })
-    return tracks
-  } else if (props && props.uids) {
-    const tracks: { [id: number]: BatchCachedTracks } = {}
-    props.uids.forEach((uid) => {
-      const track = getTrack(state, { uid })
-      if (track) {
-        tracks[track.track_id] = { metadata: track }
-      }
-    })
-    return tracks
-  } else if (props && props.permalinks) {
-    const tracks: { [permalink: string]: BatchCachedTracks } = {}
-    props.permalinks.forEach((permalink) => {
-      const track = getTrack(state, { permalink })
-      if (track) tracks[permalink] = { metadata: track }
-    })
-    return tracks
-  }
-  return getAllEntries(state, { kind: Kind.TRACKS })
 }
