d983d1f33265c8dd00bc3f0f260d12bcad671a27
Dylan Jeffers <dylan@audius.co>
2025-05-20 14:52:05 -0700

    Fix animated-event error leading to crash

--- scripts/patch-package.sh
+++ scripts/patch-package.sh
@@ -5,3 +5,7 @@ npx patch-package --error-on-fail
 npx patch-package --patch-dir=packages/mobile/patches --error-on-fail 
 npx patch-package --patch-dir=packages/web/patches --error-on-fail 
 npx patch-package --patch-dir=packages/identity-service/patches --error-on-fail 
+
+cd packages/mobile
+npx patch-package --patch-dir=mobile-patches --error-on-fail 
+cd ...
