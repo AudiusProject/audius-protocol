9ce2140333a56ac63e27e94f0f47f9f72fdba83c
Dylan Jeffers <dylan@audius.co>
2025-05-21 13:26:07 -0700

    Fix integration test init by checking for mobile directory

--- scripts/patch-package.sh
+++ scripts/patch-package.sh
@@ -6,6 +6,8 @@ npx patch-package --patch-dir=packages/mobile/patches --error-on-fail
 npx patch-package --patch-dir=packages/web/patches --error-on-fail 
 npx patch-package --patch-dir=packages/identity-service/patches --error-on-fail 
 
-cd packages/mobile
-npx patch-package --patch-dir=mobile-patches --error-on-fail 
-cd ../..
+if [ -d "packages/mobile" ]; then
+  cd packages/mobile
+  npx patch-package --patch-dir=mobile-patches --error-on-fail 
+  cd ../..
+fi
