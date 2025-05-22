3b2a3f3ac49e0f654faaec3bc752d22f4bb511f3
Farid Salau <faridsalau@gmail.com>
2025-05-22 10:26:31 -0500

    Add managed user toast

--- packages/web/src/pages/pay-and-earn-page/components/YourCoins.tsx
+++ packages/web/src/pages/pay-and-earn-page/components/YourCoins.tsx
@@ -1,7 +1,11 @@
-import { useCallback } from 'react'
+import { useCallback, useContext } from 'react'
 
-import { useFeatureFlag, useFormattedAudioBalance } from '@audius/common/hooks'
-import { buySellMessages as messages } from '@audius/common/messages'
+import {
+  useFeatureFlag,
+  useFormattedAudioBalance,
+  useIsManagedAccount
+} from '@audius/common/hooks'
+import { buySellMessages } from '@audius/common/messages'
 import { FeatureFlags } from '@audius/common/services'
 import { useBuySellModal } from '@audius/common/store'
 import { route } from '@audius/common/utils'
@@ -18,16 +22,29 @@ import {
 import { useDispatch } from 'react-redux'
 import { push } from 'redux-first-history'
 
+import { ToastContext } from 'components/toast/ToastContext'
+
+const messages = {
+  ...buySellMessages,
+  managedAccount: "You can't do that as a managed user"
+}
+
 const DIMENSIONS = 64
 const { WALLET_AUDIO_PAGE } = route
 
-const TokensHeader = () => {
+const YourCoinsHeader = () => {
   const { color } = useTheme()
   const { onOpen: openBuySellModal } = useBuySellModal()
+  const isManagedAccount = useIsManagedAccount()
+  const { toast } = useContext(ToastContext)
 
   const handleBuySellClick = useCallback(() => {
-    openBuySellModal()
-  }, [openBuySellModal])
+    if (isManagedAccount) {
+      toast(messages.managedAccount)
+    } else {
+      openBuySellModal()
+    }
+  }, [isManagedAccount, openBuySellModal, toast])
 
   return (
     <Flex
@@ -74,7 +91,7 @@ export const YourCoins = () => {
       borderRadius='l'
       css={{ overflow: 'hidden' }}
     >
-      {isWalletUIBuySellEnabled ? <TokensHeader /> : null}
+      {isWalletUIBuySellEnabled ? <YourCoinsHeader /> : null}
       <Flex
         alignItems='center'
         justifyContent='space-between'
