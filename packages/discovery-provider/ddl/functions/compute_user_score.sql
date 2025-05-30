6e0dfe96a8fed8373f99c410c3eebe3ffa4c286f
Isaac Solo <isaac@audius.co>
2025-05-30 13:53:55 -0700

    AAO attestation threshold proportional to reward + karma (#12238)

--- packages/discovery-provider/ddl/functions/compute_user_score.sql
+++ packages/discovery-provider/ddl/functions/compute_user_score.sql
@@ -1,3 +1,13 @@
+-- drop existing compute_user_score function if it exists disregarding number of arguments
+DO $$
+DECLARE r RECORD;
+BEGIN FOR r IN
+SELECT 'DROP FUNCTION IF EXISTS ' || n.nspname || '.' || p.proname || '(' || pg_get_function_identity_arguments(p.oid) || ');' AS stmt
+FROM pg_proc p
+    JOIN pg_namespace n ON p.pronamespace = n.oid
+WHERE p.proname = 'compute_user_score' LOOP EXECUTE r.stmt;
+END LOOP;
+END $$;
 -- shared computation function used in get_user_score(s)
 create or replace function compute_user_score(
         play_count bigint,
@@ -6,9 +16,10 @@ create or replace function compute_user_score(
         chat_block_count bigint,
         following_count bigint,
         is_audius_impersonator boolean,
-        distinct_tracks_played bigint
+        distinct_tracks_played bigint,
+        karma bigint
     ) returns bigint as $$
-select (play_count / 2) + follower_count - challenge_count - (chat_block_count * 100) + case
+select (play_count / 2) + follower_count - challenge_count - (chat_block_count * 100) + karma + case
         when following_count < 5 then -1
         else 0
     end + case
--- packages/discovery-provider/ddl/functions/get_user_score.sql
+++ packages/discovery-provider/ddl/functions/get_user_score.sql
@@ -12,10 +12,11 @@ create or replace function get_user_score(target_user_id integer) returns table(
         follower_count bigint,
         chat_block_count bigint,
         is_audius_impersonator boolean,
+        karma bigint,
         score bigint
     ) language sql as $function$ with play_activity as (
         select p.user_id,
-            count(distinct date_trunc('hour', p.created_at)) as play_count,
+            count(distinct date_trunc('day', p.created_at)) as play_count,
             count(distinct p.play_item_id) as distinct_tracks_played
         from plays p
         where p.user_id = target_user_id
@@ -60,7 +61,28 @@ create or replace function get_user_score(target_user_id integer) returns table(
                 )
                 and u.is_verified = false then true
                 else false
-            end as is_audius_impersonator
+            end as is_audius_impersonator,
+            case
+                when (
+                    -- give max karma to users with more than 1000 followers
+                    -- karma is too slow for users with many followers
+                    au.follower_count > 1000
+                ) then 100
+                when (
+                    au.follower_count = 0
+                ) then 0
+                else (
+                    select LEAST(
+                            (sum(fau.follower_count) / 100)::bigint,
+                            100
+                        )
+                    from follows
+                        join aggregate_user fau on follows.follower_user_id = fau.user_id
+                    where follows.followee_user_id = target_user_id
+                        and fau.following_count < 10000 -- ignore users with too many following
+                        and follows.is_delete = false
+                )
+            end as karma
         from users u
             left join play_activity p on u.user_id = p.user_id
             left join fast_challenge_completion c on u.user_id = c.user_id
@@ -77,7 +99,8 @@ select a.*,
         a.chat_block_count,
         a.following_count,
         a.is_audius_impersonator,
-        a.distinct_tracks_played
+        a.distinct_tracks_played,
+        a.karma
     ) as score
 from aggregate_scores a;
 $function$;
