59da8fccbe7f8caa021eb7f14e7ba001677b18b5
Raymond Jacobson <ray@audius.co>
2025-05-29 17:08:04 -0700

    Implement EthRewardsManagerV2.sol

--- eth-contracts/contracts/IWormhole.sol
+++ eth-contracts/contracts/IWormhole.sol
@@ -1,14 +1,24 @@
 pragma solidity ^0.5.0;
 
-// Interface for interaction with Wormhole v2 TokenBridge
-// https://github.com/certusone/wormhole/blob/7e2cf1f9818099c63c21d101afbfedb1903ee9ba/ethereum/contracts/bridge/Bridge.sol#L93
+// Interfaces for interaction with Wormhole v2 TokenBridge
+
+// Wormhole
+// https://github.com/wormhole-foundation/wormhole/blob/bb342612d1eb0e824575a3f5dd2847dc1f220092/ethereum/contracts/interfaces/IWormhole.sol
+interface WormholeInternal {
+  function messageFee() external view returns (uint256);
+}
+
+// TokenBridge
+// https://github.com/wormhole-foundation/wormhole/blob/bb342612d1eb0e824575a3f5dd2847dc1f220092/ethereum/contracts/bridge/interfaces/ITokenBridge.sol
 interface Wormhole {
-    function transferTokens(
-        address token,
-        uint256 amount,
-        uint16 recipientChain,
-        bytes32 recipient,
-        uint256 arbiterFee,
-        uint32 nonce
-    ) external;
+  function transferTokens(
+    address token,
+    uint256 amount,
+    uint16 recipientChain,
+    bytes32 recipient,
+    uint256 arbiterFee,
+    uint32 nonce
+  ) external payable returns (uint64 sequence);
+
+  function wormhole() external view returns (WormholeInternal);
 }
---
+++ eth-contracts/migrations/14_eth_rewards_manager_v2_migration.js
@@ -0,0 +1,71 @@
+/**
+ * proxy upgrade for DelegateManager to DelegateManagerV2
+ * uses governance guardian override
+ * NOTE this is for local dev only, real deploy will require full governance proposal process
+ */
+
+const assert = require('assert')
+
+const contractConfig = require('../contract-config.js')
+const _lib = require('../utils/lib.js')
+
+const AudiusAdminUpgradeabilityProxy = artifacts.require(
+  'AudiusAdminUpgradeabilityProxy'
+)
+const EthRewardsManagerV2 = artifacts.require('EthRewardsManagerV2')
+const Governance = artifacts.require('Governance')
+
+module.exports = (deployer, network, accounts) => {
+  deployer.then(async () => {
+    const config = contractConfig[network]
+    const proxyAdminAddress = config.proxyAdminAddress || accounts[10]
+    const proxyDeployerAddress = config.proxyDeployerAddress || accounts[11]
+    const guardianAddress = config.guardianAddress || proxyDeployerAddress
+
+    const governanceAddress = process.env.governanceAddress
+    const governance = await Governance.at(governanceAddress)
+    const ethRewardsManagerV2Logic = await EthRewardsManagerV2.new({
+      from: proxyDeployerAddress
+    })
+
+    const ethRewardsManagerAddress = process.env.ethRewardsManagerAddress
+    const ethRewardsManagerProxy = await AudiusAdminUpgradeabilityProxy.at(
+      ethRewardsManagerAddress
+    )
+    /**
+     * Confirm old implementation is not the new implementation
+     */
+    const oldImplementation = await ethRewardsManagerProxy.implementation.call({
+      from: proxyAdminAddress
+    })
+    assert.notStrictEqual(oldImplementation, ethRewardsManagerV2Logic.address)
+
+    /**
+     * Submit contract proxy upgrade via governance guardian
+     */
+    const ethRewardsManagerKey = web3.utils.utf8ToHex('EthRewardsManagerProxy')
+    const callValue0 = _lib.toBN(0)
+    const functionSignature = 'upgradeTo(address)'
+    const callData = _lib.abiEncode(
+      ['address'],
+      [ethRewardsManagerV2Logic.address]
+    )
+    await governance.guardianExecuteTransaction(
+      ethRewardsManagerKey,
+      callValue0,
+      functionSignature,
+      callData,
+      { from: guardianAddress }
+    )
+
+    /**
+     * Confirm proxy is re-pointed to new logic contract
+     */
+    assert.strictEqual(
+      await ethRewardsManagerProxy.implementation.call({
+        from: proxyAdminAddress
+      }),
+      ethRewardsManagerV2Logic.address
+    )
+  })
+}
