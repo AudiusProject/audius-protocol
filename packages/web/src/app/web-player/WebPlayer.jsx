216a6304acb033049feab3a9a3cd11cb2e2eccdb
Ray Jacobson <ray@audius.co>
2025-06-02 15:29:35 -0700

    Add label UI feedback fixes (#12260)

--- packages/web/src/app/web-player/WebPlayer.jsx
+++ packages/web/src/app/web-player/WebPlayer.jsx
@@ -152,6 +152,7 @@ const {
   CHANGE_PASSWORD_SETTINGS_PAGE,
   CHANGE_EMAIL_SETTINGS_PAGE,
   ACCOUNT_VERIFICATION_SETTINGS_PAGE,
+  LABEL_ACCOUNT_SETTINGS_PAGE,
   NOTIFICATION_SETTINGS_PAGE,
   ABOUT_SETTINGS_PAGE,
   FOLLOWING_USERS_ROUTE,
@@ -848,7 +849,8 @@ const WebPlayer = (props) => {
                   SETTINGS_PAGE,
                   AUTHORIZED_APPS_SETTINGS_PAGE,
                   ACCOUNTS_YOU_MANAGE_SETTINGS_PAGE,
-                  ACCOUNTS_MANAGING_YOU_SETTINGS_PAGE
+                  ACCOUNTS_MANAGING_YOU_SETTINGS_PAGE,
+                  LABEL_ACCOUNT_SETTINGS_PAGE
                 ]}
                 isMobile={isMobile}
                 component={SettingsPage}
