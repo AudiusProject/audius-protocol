15411e310ae03c165313c51dcc47973ac6fc85f3
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-06-04 12:59:39 -0400

    Fix accountCollection is undefined

--- packages/web/src/components/nav/desktop/PlaylistLibrary/PlaylistNavItem.tsx
+++ packages/web/src/components/nav/desktop/PlaylistLibrary/PlaylistNavItem.tsx
@@ -28,9 +28,6 @@ export const PlaylistNavItem = (props: PlaylistNavItemProps) => {
   const accountCollection = useSelector((state) => {
     return state.account.collections[playlistId]
   })
-  const { name, permalink, user } = accountCollection
-
-  const isOwnedByCurrentUser = user.id === currentUserId
 
   const hasPlaylistUpdate = useSelector(
     (state) => !!selectPlaylistUpdateById(state, playlistId)
@@ -41,6 +38,12 @@ export const PlaylistNavItem = (props: PlaylistNavItemProps) => {
       dispatch(updatedPlaylistViewed({ playlistId }))
     }
   }, [hasPlaylistUpdate, dispatch, playlistId])
+  if (!accountCollection) {
+    return null
+  }
+
+  const { name, permalink, user } = accountCollection
+  const isOwnedByCurrentUser = user.id === currentUserId
 
   return (
     <CollectionNavItem
