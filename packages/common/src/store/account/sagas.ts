0a3fd04466aa86122d35ad03f84630f3556198b1
JD Francis <jd.johndavidfrancis@gmail.com>
2025-06-06 11:05:49 -0500

    Fix account wallet 404 crashing saga (#12285)

--- packages/common/src/store/account/sagas.ts
+++ packages/common/src/store/account/sagas.ts
@@ -127,7 +127,9 @@ function* initializeMetricsForUser({
   if (accountUser && accountUser.handle && web3WalletAddress) {
     const accountData = (yield* call([queryClient, queryClient.fetchQuery], {
       queryKey: getWalletAccountQueryKey(web3WalletAddress),
-      queryFn: async () => getWalletAccountQueryFn(web3WalletAddress, sdk)
+      queryFn: async () => getWalletAccountQueryFn(web3WalletAddress, sdk),
+      staleTime: Infinity,
+      gcTime: Infinity
     })) as AccountUserMetadata | undefined
     const { user: web3User } = accountData ?? {}
 
@@ -222,10 +224,15 @@ export function* fetchAccountAsync({
     return
   }
 
-  const accountData = (yield* call([queryClient, queryClient.fetchQuery], {
-    queryKey: getWalletAccountQueryKey(wallet),
-    queryFn: async () => getWalletAccountQueryFn(wallet!, sdk)
-  })) as AccountUserMetadata | undefined
+  let accountData: AccountUserMetadata | undefined
+  try {
+    accountData = (yield* call([queryClient, queryClient.fetchQuery], {
+      queryKey: getWalletAccountQueryKey(wallet),
+      queryFn: async () => getWalletAccountQueryFn(wallet!, sdk),
+      staleTime: Infinity,
+      gcTime: Infinity
+    })) as AccountUserMetadata | undefined
+  } catch (e) {}
 
   if (!accountData) {
     yield* put(resetAccount())
