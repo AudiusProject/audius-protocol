41108528b0851562ead23c13ba6d53ea3528ca1d
JD Francis <jd@audius.co>
2025-06-06 14:56:29 -0500

    lint

--- packages/mobile/src/screens/root-screen/RootScreen.tsx
+++ packages/mobile/src/screens/root-screen/RootScreen.tsx
@@ -12,7 +12,6 @@ import { PortalHost } from '@gorhom/portal'
 import { useLinkTo } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import {
-  getFinishedSignUpProcess,
   getRouteOnCompletion,
   getStartedAndFinishedSignup,
   getStartedSignUpProcess,
