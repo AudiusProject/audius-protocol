0d46305d718f9a3db9a3b4885f404e93bc957e1d
JD Francis <jd@audius.co>
2025-06-13 10:53:26 -0500

    tweak utils import in server pages

--- packages/web/src/pages/collection-page/DesktopServerCollectionPage.tsx
+++ packages/web/src/pages/collection-page/DesktopServerCollectionPage.tsx
@@ -1,7 +1,7 @@
 import { useUser } from '@audius/common/src/api/tan-query/users/useUser'
 import { Collection } from '@audius/common/src/models/Collection'
 import { formatCount } from '@audius/common/src/utils/formatUtil'
-import { route } from '@audius/common/utils'
+import { profilePage } from '@audius/common/src/utils/route'
 import IconHeart from '@audius/harmony/src/assets/icons/Heart.svg'
 import IconKebabHorizontal from '@audius/harmony/src/assets/icons/KebabHorizontal.svg'
 import IconPlay from '@audius/harmony/src/assets/icons/Play.svg'
@@ -19,8 +19,6 @@ import { Text } from '@audius/harmony/src/components/text'
 import { TextLink } from '@audius/harmony/src/components/text-link'
 
 import { ServerUserGeneratedText } from 'components/user-generated-text/ServerUserGeneratedText'
-
-const { profilePage } = route
 
 type DesktopServerCollectionPageProps = {
   collection: Collection
