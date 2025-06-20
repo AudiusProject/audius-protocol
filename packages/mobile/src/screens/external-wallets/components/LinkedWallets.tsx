8c8c26787f95abde9e4d7a8e8745862cdd1be8a7
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-06-20 10:46:44 -0400

    bigint

--- packages/mobile/src/screens/external-wallets/components/LinkedWallets.tsx
+++ packages/mobile/src/screens/external-wallets/components/LinkedWallets.tsx
@@ -71,7 +71,7 @@ export const LinkedWallets = () => {
     wallets.push({
       chain: confirmingWallet.chain,
       address: confirmingWallet.wallet,
-      balance: confirmingWallet.balance ?? 0n,
+      balance: confirmingWallet.balance ?? BigInt(0),
       collectibleCount: confirmingWallet.collectibleCount || 0,
       isConfirming: true
     })
