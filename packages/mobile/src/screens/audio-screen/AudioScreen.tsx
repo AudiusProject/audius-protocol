8ca3f27060fe4f8c3a9e71490314787b1d813692
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-06-19 22:54:49 -0400

    format commas

--- packages/mobile/src/screens/audio-screen/AudioScreen.tsx
+++ packages/mobile/src/screens/audio-screen/AudioScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect } from 'react'
 
+import { useFormattedAudioBalance } from '@audius/common/hooks'
 import {
   modalsActions,
   tokenDashboardPageActions,
@@ -7,13 +8,14 @@ import {
   walletActions,
   walletSelectors
 } from '@audius/common/store'
-import { isNullOrUndefined } from '@audius/common/utils'
+import { formatNumberCommas, isNullOrUndefined } from '@audius/common/utils'
 import { AUDIO } from '@audius/fixed-decimal'
 import { useFocusEffect } from '@react-navigation/native'
 import { Image, Linking } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import LinearGradient from 'react-native-linear-gradient'
 import { useDispatch, useSelector } from 'react-redux'
+import { useAudioBalance } from '~/api'
 
 import {
   Button,
@@ -21,7 +23,8 @@ import {
   IconCrown,
   IconInfo,
   Paper,
-  Text
+  Text,
+  LoadingSpinner
 } from '@audius/harmony-native'
 import TokenStill from 'app/assets/images/tokenSpinStill.png'
 import {
@@ -33,7 +36,6 @@ import {
 } from 'app/components/core'
 import { ScreenPrimaryContent } from 'app/components/core/Screen/ScreenPrimaryContent'
 import { ScreenSecondaryContent } from 'app/components/core/Screen/ScreenSecondaryContent'
-import LoadingSpinner from 'app/components/loading-spinner'
 import { useNavigation } from 'app/hooks/useNavigation'
 import { useToast } from 'app/hooks/useToast'
 import { makeStyles } from 'app/styles'
@@ -43,7 +45,7 @@ import { ClaimAllRewardsTile } from '../rewards-screen/ClaimAllRewardsTile'
 
 const { setVisibility } = modalsActions
 const { getBalance } = walletActions
-const { getAccountTotalBalance, getTotalBalanceLoadDidFail } = walletSelectors
+const { getTotalBalanceLoadDidFail } = walletSelectors
 const { getHasAssociatedWallets } = tokenDashboardPageSelectors
 const { fetchAssociatedWallets } = tokenDashboardPageActions
 
@@ -117,7 +119,8 @@ export const AudioScreen = () => {
   const navigation = useNavigation()
   const { toast } = useToast()
 
-  const totalBalance = useSelector(getAccountTotalBalance)
+  const { audioBalanceFormatted, isAudioBalanceLoading } =
+    useFormattedAudioBalance()
   const balanceLoadDidFail = useSelector(getTotalBalanceLoadDidFail)
 
   const hasMultipleWallets = useSelector(getHasAssociatedWallets)
@@ -158,7 +161,8 @@ export const AudioScreen = () => {
           onPress={hasMultipleWallets ? handlePressWalletInfo : undefined}
         >
           <Flex alignItems='center'>
-            {isNullOrUndefined(totalBalance) ? (
+            {isAudioBalanceLoading ||
+            isNullOrUndefined(audioBalanceFormatted) ? (
               <LoadingSpinner
                 fill={'rgba(255, 255, 255, 0.75)'}
                 style={styles.spinner}
@@ -170,7 +174,7 @@ export const AudioScreen = () => {
                 color='staticWhite'
                 strength='strong'
               >
-                {AUDIO(totalBalance).toShorthand()}
+                {audioBalanceFormatted}
               </Text>
             )}
           </Flex>
