4c18a9f2dcabe07e3a013b4c1734c544e5396581
Dylan Jeffers <dylan@audius.co>
2025-06-23 17:12:01 -0700

    Fix mobile turbo sdk build

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -9,6 +9,7 @@ npx playwright install
 echo "Running build, lint, typecheck, and test..."
 # Ensure that all public packages are in this list,
 # otherwise they won't get built/linted/tested before being published
+npx turbo dist --filter=@audius/mobile
 npx turbo run build lint typecheck test \
     --filter=create-audius-app \
     --filter=@audius/sp-actions \
