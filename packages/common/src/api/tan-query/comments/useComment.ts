d34e011f04fa4c43bbd511bf00b35b652bdcd567
KJ <kyle.j.shanks@gmail.com>
2025-07-09 16:12:21 -0400

    [PE-6458] Update SDK and getComment hook (#12487)

--- packages/common/src/api/tan-query/comments/useComment.ts
+++ packages/common/src/api/tan-query/comments/useComment.ts
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { Id } from '@audius/sdk'
+import { useQuery } from '@tanstack/react-query'
 import { useDispatch } from 'react-redux'
 
+import { commentFromSDK } from '~/adapters'
 import { useQueryContext } from '~/api/tan-query/utils'
 import { Feature, ID } from '~/models'
 import { toast } from '~/store/ui/toast/slice'
@@ -11,17 +13,21 @@ import { CommentOrReply, messages } from './types'
 import { getCommentQueryKey } from './utils'
 
 export const useComment = (commentId: ID) => {
-  const { reportToSentry } = useQueryContext()
-  const queryClient = useQueryClient()
+  const { audiusSdk, reportToSentry } = useQueryContext()
   const dispatch = useDispatch()
 
   const queryRes = useQuery({
     queryKey: getCommentQueryKey(commentId),
     enabled: !!commentId,
     queryFn: async (): Promise<CommentOrReply | {}> => {
-      // TODO: there's no backend implementation of this fetch at the moment;
-      // but we also never expect to call the backend here; we always prepopulate the data from the fetch by tracks method
-      return queryClient.getQueryData(getCommentQueryKey(commentId)) ?? {}
+      const sdk = await audiusSdk()
+      const { data: commentRes } = await sdk.full.comments.getComment({
+        commentId: Id.parse(commentId)
+      })
+
+      if (!commentRes) return {}
+
+      return commentFromSDK(commentRes[0]) ?? {}
     },
     staleTime: Infinity
   })
