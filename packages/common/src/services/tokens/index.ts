543ee7e98f784c85fcf40dfe50c34043929fcd7a
Farid Salau <43255135+faridsalau@users.noreply.github.com>
2025-07-10 16:22:23 +0100

    feat: Add core token registry infrastructure (#12488)

---
+++ packages/common/src/services/tokens/index.ts
@@ -0,0 +1,8 @@
+export type { TokenConfig, SupportedToken } from './types'
+export {
+  getOrInitializeRegistry,
+  createTokenInfoObjects,
+  generateTokenPairs,
+  safeGetTokens,
+  tokenConfigToTokenInfo
+} from './utils'
