fa52e289333bf9719389fe518096850ae7248074
Farid Salau <faridsalau@gmail.com>
2025-07-11 09:23:02 -0500

    Remove unused

--- packages/common/src/api/tan-query/jupiter/constants.ts
+++ packages/common/src/api/tan-query/jupiter/constants.ts
@@ -2,7 +2,6 @@ import { PublicKey } from '@solana/web3.js'
 
 import { Env } from '~/services/env'
 import { getOrInitializeRegistry, type TokenConfig } from '~/services/tokens'
-import { TOKEN_LISTING_MAP } from '~/store/ui/shared/tokenConstants'
 
 import { UserBankManagedTokenInfo } from './types'
 
@@ -38,25 +37,4 @@ export const createUserBankManagedTokens = (
   })
 
   return managedTokens
-}
-
-export const USER_BANK_MANAGED_TOKENS: Record<
-  string,
-  UserBankManagedTokenInfo
-> = {
-  [TOKEN_LISTING_MAP.AUDIO.address.toUpperCase()]: {
-    mintAddress: TOKEN_LISTING_MAP.AUDIO.address,
-    claimableTokenMint: 'wAUDIO',
-    decimals: TOKEN_LISTING_MAP.AUDIO.decimals
-  },
-  [TOKEN_LISTING_MAP.USDC.address.toUpperCase()]: {
-    mintAddress: TOKEN_LISTING_MAP.USDC.address,
-    claimableTokenMint: 'USDC',
-    decimals: TOKEN_LISTING_MAP.USDC.decimals
-  },
-  [TOKEN_LISTING_MAP.BONK.address.toUpperCase()]: {
-    mintAddress: TOKEN_LISTING_MAP.BONK.address,
-    claimableTokenMint: 'BONK',
-    decimals: TOKEN_LISTING_MAP.BONK.decimals
-  }
 }
