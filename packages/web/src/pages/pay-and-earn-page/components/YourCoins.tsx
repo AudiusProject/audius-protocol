10649ff0003e6e0a63bfc00b1ef834a2ba1278e6
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-07-14 21:40:54 +0700

    YourCoins supports BONK

--- packages/web/src/pages/pay-and-earn-page/components/YourCoins.tsx
+++ packages/web/src/pages/pay-and-earn-page/components/YourCoins.tsx
@@ -1,39 +1,48 @@
 import { useCallback, useContext } from 'react'
 
+import { useTokenBalance, useTokenPrice } from '@audius/common/api'
 import {
   useFeatureFlag,
   useFormattedAudioBalance,
   useIsManagedAccount
 } from '@audius/common/hooks'
 import { buySellMessages } from '@audius/common/messages'
+import { Status } from '@audius/common/models'
 import { FeatureFlags } from '@audius/common/services'
 import { useBuySellModal } from '@audius/common/store'
 import { route } from '@audius/common/utils'
+import { BONK } from '@audius/fixed-decimal'
 import {
   Button,
   Flex,
-  IconCaretRight,
   IconTokenAUDIO,
   Paper,
   Text,
   useMedia,
-  useTheme
+  useTheme,
+  IconTokenBonk,
+  Divider,
+  IconCaretRight
 } from '@audius/harmony'
 import { useDispatch } from 'react-redux'
 import { push } from 'redux-first-history'
 
 import { ToastContext } from 'components/toast/ToastContext'
 
+import { CoinCard } from './CoinCard'
+
 const messages = {
   ...buySellMessages,
-  managedAccount: "You can't do that as a managed user"
+  managedAccount: "You can't do that as a managed user",
+  findMoreCoins: 'Find More Coins',
+  exploreArtistCoins: 'Explore available artist coins on Audius.',
+  bonkTicker: '$BONK'
 }
 
 const DIMENSIONS = 64
 const { WALLET_AUDIO_PAGE } = route
 
 const YourCoinsHeader = () => {
-  const { color } = useTheme()
   const { onOpen: openBuySellModal } = useBuySellModal()
   const isManagedAccount = useIsManagedAccount()
   const { toast } = useContext(ToastContext)
@@ -51,7 +60,7 @@ const YourCoinsHeader = () => {
       alignItems='center'
       justifyContent='space-between'
       p='l'
-      css={{ borderBottom: `1px solid ${color.border.default}` }}
+      borderBottom='default'
     >
       <Text variant='heading' size='m' color='heading'>
         {messages.yourCoins}
@@ -65,8 +74,8 @@ const YourCoinsHeader = () => {
 
 export const YourCoins = () => {
   const dispatch = useDispatch()
-  const { color, spacing, motion } = useTheme()
-  const { isMobile, isExtraSmall } = useMedia()
+  const { color, spacing } = useTheme()
+  const { isMobile } = useMedia()
   const { isEnabled: isWalletUIBuySellEnabled } = useFeatureFlag(
     FeatureFlags.WALLET_UI_BUY_SELL
   )
@@ -78,6 +87,27 @@ export const YourCoins = () => {
     isAudioPriceLoading
   } = useFormattedAudioBalance()
 
+  const { data: bonkBalance, status: bonkBalanceStatus } = useTokenBalance({
+    token: 'BONK'
+  })
+  // TODO: use getTokenRegistry instead
+  const BONK_MINT = 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'
+  const { data: bonkPriceData, isPending: isBonkPriceLoading } =
+    useTokenPrice(BONK_MINT)
+
+  const bonkBalanceFormatted = bonkBalance
+    ? BONK(bonkBalance.toString()).toLocaleString('en-US', {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+      })
+    : null
+  const bonkDollarValue =
+    bonkPriceData?.price && bonkBalance
+      ? `$${(Number(bonkBalance.toString()) * Number(bonkPriceData.price)).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
+      : null
+  const isBonkLoading =
+    bonkBalanceStatus === Status.LOADING || isBonkPriceLoading
+
   const handleTokenClick = useCallback(() => {
     dispatch(push(WALLET_AUDIO_PAGE))
   }, [dispatch])
@@ -85,51 +115,48 @@ export const YourCoins = () => {
   const isLoading = isAudioBalanceLoading || isAudioPriceLoading
 
   return (
-    <Paper
-      direction='column'
-      shadow='far'
-      borderRadius='l'
-      css={{ overflow: 'hidden' }}
-    >
+    <Paper column shadow='far' borderRadius='l' css={{ overflow: 'hidden' }}>
       {isWalletUIBuySellEnabled ? <YourCoinsHeader /> : null}
       <Flex
         alignItems='center'
         justifyContent='space-between'
-        p={isMobile ? spacing.l : spacing.xl}
+        p={isMobile ? spacing.l : undefined}
         alignSelf='stretch'
-        onClick={handleTokenClick}
+      >
+        <CoinCard
+          icon={<IconTokenAUDIO width={DIMENSIONS} height={DIMENSIONS} hex />}
+          symbol={messages.audioTicker}
+          balance={audioBalanceFormatted ?? ''}
+          dollarValue={audioDollarValue ?? ''}
+          loading={isLoading}
+          onClick={handleTokenClick}
+        />
+        <Divider orientation='vertical' />
+        <CoinCard
+          icon={<IconTokenBonk width={DIMENSIONS} height={DIMENSIONS} hex />}
+          symbol={messages.bonkTicker}
+          balance={bonkBalanceFormatted ?? ''}
+          dollarValue={bonkDollarValue ?? ''}
+          loading={isBonkLoading}
+          onClick={handleTokenClick}
+        />
+      </Flex>
+      <Flex
+        p={isMobile ? spacing.l : spacing.xl}
         css={{
           cursor: 'pointer',
-          '&:hover': {
-            backgroundColor: color.background.surface2
-          }
+          '&:hover': { backgroundColor: color.background.surface2 }
         }}
       >
-        <Flex alignItems='center' gap={isExtraSmall ? 'm' : 'l'}>
-          <IconTokenAUDIO width={DIMENSIONS} height={DIMENSIONS} hex />
-          <Flex
-            direction='column'
-            gap='xs'
-            css={{
-              opacity: isLoading ? 0 : 1,
-              transition: `opacity ${motion.expressive}`
-            }}
-          >
-            <Flex gap='xs'>
-              <Text variant='heading' size='l' color='default'>
-                {audioBalanceFormatted}
-              </Text>
-              <Text variant='heading' size='l' color='subdued'>
-                {messages.audioTicker}
-              </Text>
-            </Flex>
-            <Text variant='heading' size='s' color='subdued'>
-              {audioDollarValue}
+        <Flex flex={1} alignItems='center' justifyContent='space-between'>
+          <Flex column gap='xs'>
+            <Text variant='heading' size='m' color='default'>
+              {messages.findMoreCoins}
             </Text>
+            <Text color='subdued'>{messages.exploreArtistCoins}</Text>
           </Flex>
+          <IconCaretRight size='l' color='subdued' />
         </Flex>
-
-        <IconCaretRight size='l' color='subdued' />
       </Flex>
     </Paper>
   )
