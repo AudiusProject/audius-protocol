5a9c1e713594a0150f87946bc29f2f1977a6de14
Dylan Jeffers <dylan@audius.co>
2025-07-21 21:55:30 -0700

    Set ruby bundler version (#12569)

--- packages/mobile/src/app/App.tsx
+++ packages/mobile/src/app/App.tsx
@@ -52,6 +52,7 @@ if (Platform.OS === 'android') {
 incrementSessionCount()
 
 const App = () => {
+  // adding a comment here to trigger a build
   useEffectOnce(() => {
     subscribeToNetworkStatusUpdates()
     TrackPlayer.setupPlayer({ autoHandleInterruptions: true })
