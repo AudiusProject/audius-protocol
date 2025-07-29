f264ab4429cff92743b57965fc16aaf9a0133640
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-29 11:23:41 -0700

    typesafe query

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/routes/relay/getAllowedMints.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/routes/relay/getAllowedMints.ts
@@ -1,11 +1,13 @@
 import { initializeDiscoveryDb } from '@pedalboard/basekit'
 import { config } from '../../config'
+import type { ArtistCoins } from '@pedalboard/storage'
 
 const usdcMintAddress = config.usdcMintAddress
 
 const db = initializeDiscoveryDb(config.discoveryDbConnectionString)
 
 export const getAllowedMints = async (): Promise<string[]> => {
-  const artistCoinMints = await db.select('mint').from('artist_coins')
+  const rows = await db<ArtistCoins>('artist_coins').select('mint')
+  const artistCoinMints = rows.map((row) => row.mint)
   return [usdcMintAddress, ...artistCoinMints]
 }
