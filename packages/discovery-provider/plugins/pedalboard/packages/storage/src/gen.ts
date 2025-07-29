5e3d79af37efff48a5c715914ea7ed625f7c2f65
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-29 13:51:44 -0700

    revert

--- packages/discovery-provider/plugins/pedalboard/packages/storage/src/gen.ts
+++ packages/discovery-provider/plugins/pedalboard/packages/storage/src/gen.ts
@@ -3,7 +3,7 @@ import { updateTypes } from 'knex-types'
 
 const db = knex({
   client: 'pg',
-  connection: 'postgresql://postgres:example@localhost:5432/postgres'
+  connection: 'postgresql://postgres:postgres@localhost:5432/audius_discovery'
 })
 
 updateTypes(db, { output: './src/index.ts' }).catch((err) => {
