def820c07c42ebab9fada29c5636d3aee7c09ffe
Dylan Jeffers <dylan@audius.co>
2025-07-29 15:37:29 -0700

    [QA-2177] Fix collectible deep links (#12605)
    ### Description
    
    Fixes issue where sharing collectible didn't generate the correct url
    Fixes issue where collectible urls didn't deep link on mobile

--- packages/mobile/src/screens/app-screen/ProfileTabScreen.tsx
+++ packages/mobile/src/screens/app-screen/ProfileTabScreen.tsx
@@ -1,18 +1,7 @@
-import { ProfileScreen } from 'app/screens/profile-screen'
-
 import type { AppTabScreenParamList } from './AppTabScreen'
-import { createAppTabScreenStack } from './createAppTabScreenStack'
 
 export type ProfileTabScreenParamList = AppTabScreenParamList & {
   UserProfile: undefined
   EditProfile: undefined
+  Collectibles: { collectibleId?: string }
 }
-
-export const ProfileTabScreen =
-  createAppTabScreenStack<ProfileTabScreenParamList>((Stack) => (
-    <Stack.Screen
-      name='UserProfile'
-      component={ProfileScreen}
-      initialParams={{ handle: 'accountUser' }}
-    />
-  ))
