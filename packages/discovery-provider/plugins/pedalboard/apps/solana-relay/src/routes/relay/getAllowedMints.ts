64f1991c4086d2ee1b31a47fe95e25d3c715f1b9
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-07-29 11:44:27 -0700

    cr

--- packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/routes/relay/getAllowedMints.ts
+++ packages/discovery-provider/plugins/pedalboard/apps/solana-relay/src/routes/relay/getAllowedMints.ts
@@ -2,12 +2,10 @@ import { initializeDiscoveryDb } from '@pedalboard/basekit'
 import { config } from '../../config'
 import type { ArtistCoins } from '@pedalboard/storage'
 
-const usdcMintAddress = config.usdcMintAddress
-
 const db = initializeDiscoveryDb(config.discoveryDbConnectionString)
 
 export const getAllowedMints = async (): Promise<string[]> => {
   const rows = await db<ArtistCoins>('artist_coins').select('mint')
   const artistCoinMints = rows.map((row) => row.mint)
-  return [usdcMintAddress, ...artistCoinMints]
+  return [config.usdcMintAddress, ...artistCoinMints]
 }
