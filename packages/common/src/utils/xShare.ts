1af389182f3d9cccfd5a80a231c2bc3cb65e870e
Dharit Tantiviramanond <dharit.tan@gmail.com>
2025-08-05 15:23:50 -0400

    [QA-2272] Fix X share

--- packages/common/src/utils/xShare.ts
+++ packages/common/src/utils/xShare.ts
@@ -2,5 +2,5 @@ export const makeXShareUrl = (url: string | null, text: string) => {
   const textString = `?text=${encodeURIComponent(text)}`
   const urlString = url ? `&url=${encodeURIComponent(url)}` : ''
 
-  return `http://x.com/intent/post${textString}${urlString}`
+  return `http://x.com/intent/tweet${textString}${urlString}`
 }
