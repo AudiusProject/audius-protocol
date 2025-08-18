8af0b477be59c50c583d76c1e69d5853e46ea90f
Dylan Jeffers <dylan@audius.co>
2025-08-18 16:10:00 -0700

    [PE-6726] Mobile Search QA (#12767)
    ### Description
    
    1. Fixes track-card navigation
    2. Fixes Recently Played -> Listening History navigation
    3. Fixes navigation from track-screen search metadata -> explore-screen
    4. Drops search-screen now that we are fully migrated to explore-screen
    5. Also fixes all deep links for /search and /expore

--- packages/mobile/src/screens/search-screen/searchParams.ts
+++
@@ -1,8 +0,0 @@
-import type { SearchCategory, SearchFilters } from '@audius/common/api'
-
-export type SearchParams = {
-  query?: string
-  category?: SearchCategory
-  filters?: SearchFilters
-  autoFocus?: boolean
-}
