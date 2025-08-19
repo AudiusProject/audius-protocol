041f71e73969b71e5d44076603afad1a4ff7a6fb
JD Francis <jd.johndavidfrancis@gmail.com>
2025-08-19 16:32:56 -0500

    [PE-6670] Redo discord flow with OAuth (#12772)
    ### Description
    
    - Redoes the discord flow with an oauth flow.
    - From the client we now use just an oauth link with state params
    telling the bot what tiers of coin owner we are
    - Pairs with PR in the discord repo
    - Adds disabled state to discord link for non balance holders
    
    ### How Has This Been Tested?
    
    web:prod + discord bot

---
+++ packages/common/src/hooks/useDiscordOAuthLink.ts
@@ -0,0 +1,52 @@
+import { useMemo } from 'react'
+
+import { UserCoin, useCurrentUserId, useUserCoins } from '~/api'
+import { getTierForUserNonWei } from '~/store'
+import { AUDIUS_DISCORD_OAUTH_LINK } from '~/utils/route'
+
+// TODO: make this structure more legit this if we add more coins with tiered rewards
+const COIN_TIER_MAP = {
+  AUDIO: getTierForUserNonWei
+}
+
+// Returns a json string of the user's coin tiers
+// e.g. { AUDIO: 'bronze', BONK: 'holder' }
+// The discord bot looks at this to determine what roles to give everyone
+const makeDiscordOauthStateString = (
+  userCoins: UserCoin[],
+  currentCoin: string = '$AUDIO' // uses ticker strings
+) => {
+  const coinTiers = userCoins.reduce(
+    (acc, coin) => {
+      const cleanTicker = coin.ticker.replace('$', '')
+      if (COIN_TIER_MAP[cleanTicker]) {
+        // coin tier map has the methods used to calculate the tier
+        const tier = COIN_TIER_MAP[cleanTicker](coin.balance)
+        acc[cleanTicker] = tier
+      } else {
+        // default to just "holder"
+        acc[cleanTicker] = 'holder'
+      }
+      return acc
+    },
+    {} as Record<string, string>
+  )
+
+  // Tells the discord bot which channel to redirect into
+  coinTiers.currentCoin = currentCoin.replace('$', '')
+
+  return JSON.stringify(coinTiers)
+}
+
+export const useDiscordOAuthLink = (currentCoin?: string) => {
+  const { data: currentUserId } = useCurrentUserId()
+  const { data: userCoins } = useUserCoins({ userId: currentUserId })
+
+  return useMemo(() => {
+    const tierStateString = makeDiscordOauthStateString(
+      userCoins ?? [],
+      currentCoin
+    )
+    return `${AUDIUS_DISCORD_OAUTH_LINK}&state=${tierStateString}`
+  }, [userCoins, currentCoin])
+}
