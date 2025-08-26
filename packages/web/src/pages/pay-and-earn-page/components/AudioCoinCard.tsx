af01933b5a37f2e454f67a11b201b81971fe5606
Farid Salau <faridsalau@gmail.com>
2025-08-26 14:00:25 -0500

    Show AUDIO card if user doesn't have any other assets

--- packages/web/src/pages/pay-and-earn-page/components/AudioCoinCard.tsx
+++ packages/web/src/pages/pay-and-earn-page/components/AudioCoinCard.tsx
@@ -1,12 +1,20 @@
+import { useCallback } from 'react'
+
 import { useFormattedAudioBalance } from '@audius/common/hooks'
 import { buySellMessages } from '@audius/common/messages'
 import { IconTokenAUDIO } from '@audius/harmony'
+import { useDispatch } from 'react-redux'
+import { push } from 'redux-first-history'
+
+import { env } from 'services/env'
 
 import { CoinCard } from './CoinCard'
 
 const DIMENSIONS = 64
 
-export const AudioCoinCard = ({ onClick }: { onClick?: () => void }) => {
+export const AudioCoinCard = () => {
+  const dispatch = useDispatch()
+
   const {
     audioBalanceFormatted,
     audioDollarValue,
@@ -16,6 +24,13 @@ export const AudioCoinCard = ({ onClick }: { onClick?: () => void }) => {
 
   const isLoading = isAudioBalanceLoading || isAudioPriceLoading
 
+  const handleCoinClick = useCallback(
+    (mint: string) => {
+      dispatch(push(`/wallet/${mint}`))
+    },
+    [dispatch]
+  )
+
   return (
     <CoinCard
       icon={<IconTokenAUDIO width={DIMENSIONS} height={DIMENSIONS} hex />}
@@ -23,7 +38,7 @@ export const AudioCoinCard = ({ onClick }: { onClick?: () => void }) => {
       balance={audioBalanceFormatted ?? ''}
       dollarValue={audioDollarValue ?? ''}
       loading={isLoading}
-      onClick={onClick}
+      onClick={() => handleCoinClick(env.WAUDIO_MINT_ADDRESS)}
     />
   )
 }
