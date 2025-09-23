49c341d51fcee0bfa846ef8bf85ec1d9d4104e76
Raymond Jacobson <ray@audius.co>
2025-09-23 16:08:22 -0700

    Add custom jup basepath

--- packages/common/src/services/Jupiter.ts
+++ packages/common/src/services/Jupiter.ts
@@ -1,8 +1,10 @@
 import { FixedDecimal } from '@audius/fixed-decimal'
 import {
+  Configuration,
   createJupiterApiClient,
   Instruction,
   QuoteResponse,
+  SwapApi,
   SwapMode
 } from '@jup-ag/api'
 import { PublicKey, TransactionInstruction } from '@solana/web3.js'
@@ -22,12 +24,17 @@ export type JupiterTokenSymbol = keyof typeof TOKEN_LISTING_MAP
 
 export const DEFAULT_MAX_ACCOUNTS = 20
 export const MAX_ALLOWED_ACCOUNTS = 64
+const JUP_BASE_PATH = 'https://jup.audius.co/swap/v1'
 
 let _jup: ReturnType<typeof createJupiterApiClient>
 
 const initJupiter = () => {
   try {
-    return createJupiterApiClient()
+    return new SwapApi(
+      new Configuration({
+        basePath: JUP_BASE_PATH
+      })
+    )
   } catch (e) {
     console.error('Jupiter failed to initialize', e)
     throw e
