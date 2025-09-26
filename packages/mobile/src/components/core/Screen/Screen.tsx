950fa1727ad6f17b73f330a4d97654b849730414
Dylan Jeffers <dylan@audius.co>
2025-09-26 14:49:15 -0700

    [PE-6971] Swip back on artist coin explore screen

--- packages/mobile/src/components/core/Screen/Screen.tsx
+++ packages/mobile/src/components/core/Screen/Screen.tsx
@@ -93,7 +93,6 @@ export const Screen = (props: ScreenProps) => {
       removeUndefined({
         header,
         headerShown: header ? true : undefined,
-        gestureEnabled: header ? false : undefined,
         headerLeft: topbarLeft === undefined ? undefined : () => topbarLeft,
         headerRight: topbarRight
           ? () => topbarRight
