444629c59b770d675ae3d0b41a4af18a19c1049d
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2025-10-03 09:39:50 -0700

    Bad merge?

--- packages/web/src/pages/asset-detail-page/components/mobile/ArtistCoinDetailsPage.tsx
+++ packages/web/src/pages/asset-detail-page/components/mobile/ArtistCoinDetailsPage.tsx
@@ -1,5 +1,8 @@
 import { useArtistCoinByTicker, useCoinGeckoCoin } from '@audius/common/api'
-import { formatCurrencyWithSubscript } from '@audius/common/utils'
+import {
+  formatTickerFromUrl,
+  formatCurrencyWithSubscript
+} from '@audius/common/utils'
 import { FixedDecimal } from '@audius/fixed-decimal'
 import { Divider, Flex, Text, useTheme } from '@audius/harmony'
 import { useLocation } from 'react-router-dom-v5-compat'
